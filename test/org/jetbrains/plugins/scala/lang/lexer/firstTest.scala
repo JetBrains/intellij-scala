c2b9dec3301c2ccdbc65c2430d5cf8f8ff339d89
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-09-27 15:49:47 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3475 59f64563-22e8-0310-8d2c-98a15e1539f2

--- test/org/jetbrains/plugins/scala/lang/lexer/firstTest.scala
+++ test/org/jetbrains/plugins/scala/lang/lexer/firstTest.scala
@@ -1,2 +1,21 @@
-package org.jetbrains.scala.examples;
-Summator {
+object
+package
+ object
+ object  Summator {
+
+  var поросёнок: List[int] = Nil
+  def sum(f: int => int, a: int, b: int): int =
+    if (a > b) 0 else f(a) + sum(f, a + 1, b)
+
+  def sumInts(a: int, b: int): int = sum(id, a, b)
+  def sumSquares(a: int, b: int): int = sum(square, a, b)
+  def sumPowersOfTwo(a: int, b: int): int = sum(powerOfTwo, a, b)
+
+  def id(x: int): int = x
+  def square(x: int): int = x * x
+  def powerOfTwo(x: int): int = if (x == 0) 1 else x * powerOfTwo(x - 1)
+
+  def main(args: Array[String]) = {
+    Console.println(sumSquares ( .223414, 3))678
+  }
+}
