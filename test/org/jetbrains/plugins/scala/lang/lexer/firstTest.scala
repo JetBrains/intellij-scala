823ce859aee1d175b60c2368bc692a1fe9c59ccd
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-09-29 16:26:36 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3518 59f64563-22e8-0310-8d2c-98a15e1539f2

--- test/org/jetbrains/plugins/scala/lang/lexer/firstTest.scala
+++ test/org/jetbrains/plugins/scala/lang/lexer/firstTest.scala
@@ -1,2 +1,8 @@
-print asd = 56
-dfg
+object MatchTest2 extends Application {
+  def matchTest(x: Any): Any = x match {
+    case 1 => "one"
+    case "two" => 2
+    case y: Int => "scala.Int"
+  }
+  Console.println(matchTest("two"))
+}
