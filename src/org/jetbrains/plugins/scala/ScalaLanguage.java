75b443b99dfcc47252d960a05b9015555045ece0
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-09-29 16:56:21 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3520 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/ScalaLanguage.java
+++ src/org/jetbrains/plugins/scala/ScalaLanguage.java
@@ -2,11 +2,16 @@ package org.jetbrains.plugins.scala;
 
 import com.intellij.lang.Language;
 import com.intellij.lang.ParserDefinition;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.lang.Commenter;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.scala.highlighter.ScalaSyntaxHighlighter;
+import org.jetbrains.plugins.scala.highlighter.ScalaBraceMatcher;
+import org.jetbrains.plugins.scala.highlighter.ScalaCommenter;
 
 /**
  * Author: Ilya Sergey
@@ -34,5 +39,15 @@ public class ScalaLanguage extends Language {
     @NotNull
     public SyntaxHighlighter getSyntaxHighlighter(Project project, final VirtualFile virtualFile) {
         return new ScalaSyntaxHighlighter();
+    }
+
+    @Nullable
+    public PairedBraceMatcher getPairedBraceMatcher(){
+        return new ScalaBraceMatcher();
+    }
+
+    @Nullable
+    public Commenter getCommenter(){
+        return new ScalaCommenter();
     }
 }
