764c265a4f02db6188c0f072947bd54848bfdc66
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-09-29 16:53:48 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3519 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/highlighter/ScalaBraceMatcher.java
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.scala.highlighter;
+
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.lang.BracePair;
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
+
+/**
+ * Author: Ilya Sergey
+ * Date: 29.09.2006
+ * Time: 20:26:52
+ */
+public class ScalaBraceMatcher implements PairedBraceMatcher {
+
+    private static final BracePair[] PAIRS = new BracePair[] {
+       new BracePair('(', ScalaTokenTypes.tLPARENTHIS, ')', ScalaTokenTypes.tRPARENTHIS, false), 
+       new BracePair('[',ScalaTokenTypes.tLSQBRACKET, ']', ScalaTokenTypes.tRSQBRACKET, false),
+       new BracePair('{',ScalaTokenTypes.tLBRACE, '}', ScalaTokenTypes.tRBRACE, true)
+     };
+
+    public BracePair[] getPairs() {
+        return PAIRS; 
+    }
+}
---
+++ src/org/jetbrains/plugins/scala/highlighter/ScalaCommenter.java
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.highlighter;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Author: Ilya Sergey
+ * Date: 29.09.2006
+ * Time: 20:48:30
+ */
+public class ScalaCommenter implements Commenter {
+    public String getLineCommentPrefix() {
+       return "//";
+    } 
+
+     public boolean isLineCommentPrefixOnZeroColumn() {
+       return false;
+     }
+
+     public String getBlockCommentPrefix() {
+       return "/*";
+     }
+
+     public String getBlockCommentSuffix() {
+       return "*/";
+     }
+}
