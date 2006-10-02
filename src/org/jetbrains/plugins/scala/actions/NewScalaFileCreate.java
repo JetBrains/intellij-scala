eb010d14636c24aa446a73d321359bae617e691a
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-02 12:16:38 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3539 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/actions/NewScalaFileCreate.java
+++ src/org/jetbrains/plugins/scala/actions/NewScalaFileCreate.java
@@ -2,12 +2,6 @@ package org.jetbrains.plugins.scala.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.IconLoader;
 
 /**
