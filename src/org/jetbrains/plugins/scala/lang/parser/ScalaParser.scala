0210b2dfc644e59978795289926859576fc17103
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-02 12:30:01 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3541 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.scala.lang.parser;
+
+import com.intellij.lang.PsiParser;
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.PsiBuilder;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 02.10.2006
+ * Time: 13:18:27
+ */
+public class ScalaParser implements PsiParser {
+
+    @NotNull
+    public ASTNode parse(IElementType root, PsiBuilder builder) {
+        return builder.getTreeBuilt();
+    }
+}
