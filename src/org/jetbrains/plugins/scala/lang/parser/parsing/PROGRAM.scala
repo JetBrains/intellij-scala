f202b5a45375aa8093749b85f71d9882569733af
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-02 15:05:02 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3562 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/PROGRAM.scala
@@ -0,0 +1,17 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing;
+
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.lang.PsiBuilder;
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 02.10.2006
+ * Time: 12:53:26
+ */
+
+class PROGRAM extends ScalaTokenTypes {
+    public static void parse(@NotNull final PsiBuilder builder) {
+        
+    }
+}
