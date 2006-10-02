620c3063f1da24fcb4a8e0c0c1b6f4b25723e3e2
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-02 16:13:41 +0000

    added Scala sdk views
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3570 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
@@ -1,19 +1,16 @@
 package org.jetbrains.plugins.scala.lang.parser
 
-
 import com.intellij.lang.PsiParser
 import com.intellij.lang.ASTNode
 import com.intellij.lang.PsiBuilder
 import com.intellij.psi.tree.IElementType
-import org.jetbrains.annotations.NotNull
 
 
 class ScalaParser /* extends PsiParser */ {
 
     /*
-    @NotNull
     public ASTNode parse(IElementType root, PsiBuilder builder) {
-        PROGRAM.parse(builder);
+        (new PROGRAM()).parse(builder);
         return builder.getTreeBuilt();
     }
     */
