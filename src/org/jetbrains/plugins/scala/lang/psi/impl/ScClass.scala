52d1f92d494c7cd89ff78d34ba9debba35774123
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-03 18:06:55 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3629 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/ScClass.scala
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.scala.lang.psi.impl
+
+import org.jetbrains.plugins.scala.lang.parser.ScalaPsiElement
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 03.10.2006
+ * Time: 20:11:49
+ */
+class ScClass extends ScalaPsiElement {
+}
