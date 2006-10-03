50c2a984684fd88dec7495794eaccc881bad4e55
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-03 16:34:40 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3622 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/parser/ClassBaseDeclaration.scala
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.scala.lang.parser.parsing;
-
-/**
- * User: Dmitry.Krasilschikov
- * Date: 03.10.2006
- * Time: 19:43:50
- */
-class ClassBaseDeclaration extends TypeDeclaration{
-}
