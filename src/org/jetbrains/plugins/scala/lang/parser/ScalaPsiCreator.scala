1421a51ab4032860cb83685440f330ec6133c2e2
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-03 15:41:34 +0000

    new elements added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3606 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaPsiCreator.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.lang.parser;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 03.10.2006
+ * Time: 19:28:50
+ */
+object ScalaPsiCreator {
+ // def create (node : ASTNode) : PsiElement = {
+
+ // }
+}
