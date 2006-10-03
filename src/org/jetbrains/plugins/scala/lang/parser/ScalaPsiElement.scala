be1613912a1dc8a2bfa1105bd432b83d004dcfb3
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-03 18:14:30 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3634 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaPsiElement.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.lang.parser;
+
+import com.intellij.psi.PsiElement;
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 03.10.2006
+ * Time: 21:33:21
+ */
+
+abstract class ScalaPsiElement extends PsiElement{
+ 
+}
