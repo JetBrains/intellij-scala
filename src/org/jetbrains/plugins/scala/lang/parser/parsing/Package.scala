ee4c2c66aa62dceb16ed7f404f65e6a22c7485cb
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-03 14:43:40 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3601 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/Package.scala
@@ -0,0 +1,14 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing
+
+import com.intellij.lang.PsiBuilder
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 03.10.2006
+ * Time: 18:35:12
+ */
+
+class Package {
+  def parse(builder : PsiBuilder) = {
+        
+  }
+}
