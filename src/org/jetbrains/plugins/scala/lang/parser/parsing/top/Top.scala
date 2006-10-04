39b99f9d34c0cea88dda52daa1bf19c4e66e0fff
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-04 17:29:24 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3655 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/top/Top.scala
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing.top
+
+
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import com.intellij.lang.PsiBuilder
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 04.10.2006
+ * Time: 18:08:23
+ */
+
+class Top {
+  def parse(builder: PsiBuilder): Unit = {
+
+    if ( builder.getTokenType == ScalaTokenTypes.kPACKAGE)
+    {
+        new Package().parse(builder)
+        builder.advanceLexer()
+    }
+    
+  }
+}
