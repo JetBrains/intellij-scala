a9941ef49b5a0732a15460ae3c396137ebe8a715
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-04 10:06:16 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3640 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/TypeDeclaration.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing
+
+import com.intellij.lang.ASTNode
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 03.10.2006
+ * Time: 19:43:50
+ */
+abstract class TypeDeclaration{
+}
+  
+
