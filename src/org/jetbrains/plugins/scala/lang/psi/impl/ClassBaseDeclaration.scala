32e03005f82060224f8c0a496097e2c975412d29
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-04 10:06:37 +0000

    Simple Lexer Test added.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3641 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/ClassBaseDeclaration.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/ClassBaseDeclaration.scala
@@ -1,13 +1,7 @@
-package org.jetbrains.plugins.scala.lang.parser.parsing
+case class ClassBaseDeclaration (
+  methods : => Seq[Method]
+  variables : => Seq[Variable]
+) extends TypeDeclaration {
 
-import com.intellij.lang.ASTNode
-/**
- * User: Dmitry.Krasilschikov
- * Date: 03.10.2006
- * Time: 19:43:50
- */
-abstract class ClassBaseDeclaration extends TypeDeclaration
-  case class ScObject( node : ASTNode ) extends ClassBaseDeclaration
-  case class ScTrait( node : ASTNode ) extends ClassBaseDeclaration
-  case class ScClass( node : ASTNode ) extends ClassBaseDeclaration
 
+  }
