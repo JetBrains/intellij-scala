84b9421656fd48163731a7c6d09af2a60d3ca4d9
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-09 13:22:29 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3711 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/parser/ScalaParserDefinition.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaParserDefinition.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.plugins.scala.lang.parser
 
-import com.intellij.lang.ParserDefinition
-import com.intellij.lang.PsiParser
+import com.intellij.lang.ParserDefinition, com.intellij.lang.PsiParser
 import com.intellij.lang.ASTNode
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.project.Project
