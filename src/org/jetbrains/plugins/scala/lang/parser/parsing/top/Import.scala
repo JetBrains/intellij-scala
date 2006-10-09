0117edebeb126d52dbd2b8f9cbb1ac389f8af70f
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-09 13:34:05 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3714 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/top/Import.scala
@@ -0,0 +1,39 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing.top;
+
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+
+import com.intellij.psi.tree.IElementType
+import com.intellij.lang.PsiBuilder
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 06.10.2006
+ * Time: 22:04:16
+ */
+class Import {
+
+  def parse(builder: PsiBuilder): Unit = {
+
+//Open marker for handle import
+    var marker = builder.mark()
+
+//node - IMPORT
+    builder.advanceLexer
+
+    Console.println("token in import : " + builder.getTokenType)
+    builder.getTokenType match {
+      case ScalaTokenTypes.tCOMMA => {
+        builder.advanceLexer
+        new Import parse(builder)
+      }
+
+      //handle full class name
+      case ScalaTokenTypes.tIDENTIFIER => new StableId parse(builder)
+
+      case _ => builder.error("Wrong import")
+    }
+
+    marker.done(ScalaElementTypes.IMPORT) //Close marker for import
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/top/StableId.scala
@@ -0,0 +1,33 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing.top;
+
+import com.intellij.lang.PsiBuilder
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 06.10.2006
+ * Time: 22:13:17
+ */
+class StableId {
+
+  def parse(builder : PsiBuilder) : Unit = {
+
+    val marker = builder.mark()
+    builder.advanceLexer
+
+    Console.println("token in stableID : " + builder.getTokenType)
+    builder.getTokenType match {
+      case ScalaTokenTypes.tDOT => {
+        builder.advanceLexer
+        new StableId parse(builder)
+      }
+
+      case ScalaTokenTypes.tWHITE_SPACE_LINE_TERMINATE => builder.advanceLexer
+      case ScalaTokenTypes.tSEMICOLON => builder.advanceLexer
+      case _ => builder.error("error in stableID");
+    }
+
+    marker.done(ScalaElementTypes.STABLE_ID); // Close marker for qualID
+
+  }
+}
