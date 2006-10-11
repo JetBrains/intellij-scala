c2c746dc363afd0a245051068788a304b67c3bb8
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-11 17:13:24 +0000

    Added Simple and Prefix Expressions
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3807 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/PrefixExpression.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/PrefixExpression.scala
@@ -5,6 +5,7 @@ import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.parser.bnf.BNF
 import com.intellij.psi.tree.TokenSet
 import org.jetbrains.plugins.scala.lang.parser.parsing.expressions.SimpleExpression
+import com.intellij.psi.tree.IElementType
 
 object PrefixExpression{
 
@@ -28,10 +29,11 @@ FIRST(PrefixExpression) = ScalaTokenTypes.tPLUS
 
   val FIRST = TokenSet.orSet(Array(SimpleExpression.FIRST, BNF.tPREFIXES ))
 
-  def parse(builder : PsiBuilder) : Unit = {
+  def parse(builder : PsiBuilder) : IElementType = {
 
     val marker = builder.mark()
-
+    var result : IElementType = ScalaElementTypes.WRONGWAY
+    
     builder getTokenType match {
       case ScalaTokenTypes.tPLUS
            | ScalaTokenTypes.tMINUS
@@ -41,12 +43,16 @@ FIRST(PrefixExpression) = ScalaTokenTypes.tPLUS
              builder.advanceLexer()
              prefixMarker.done(ScalaElementTypes.PREFIX)
              if (SimpleExpression.FIRST.contains(builder.getTokenType)) {
-               SimpleExpression parse (builder)
-             } else builder.error("Wrong prefix expression!")
+               result = SimpleExpression parse (builder)
+             } else {
+              builder.error("Wrong prefix expression!")
+              result = ScalaElementTypes.WRONGWAY
+             }
            }
-      case _ => SimpleExpression parse (builder)
+      case _ => result = SimpleExpression parse (builder)
     }
     marker.done(ScalaElementTypes.PREFIX_EXPR)
+    result
   }
 
 }
