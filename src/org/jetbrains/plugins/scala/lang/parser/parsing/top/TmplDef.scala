6739b3ff5bb75ca1108faaa70bb3b1d694f714d3
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-10-16 14:23:07 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@3932 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/top/TmplDef.scala
@@ -0,0 +1,146 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing.top;
+
+import com.intellij.lang.PsiBuilder
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 16.10.2006
+ * Time: 13:54:45
+ */
+
+ /*
+ TmplDef ::= [case] class ClassDef
+           | [case] object ObjectDef
+           | trait TraitDef
+
+*/
+
+object TmplDef {
+  abstract class TypeDef {
+
+      def getKeyword : ScalaElementType
+
+      def getDef :  ScalaElementType
+
+      def parseDef ( builder : PsiBuilder ) : Unit
+
+      def parse ( builder : PsiBuilder ) = {
+
+        //node : keyword
+        val keywordMarker = builder.mark()
+        builder.advanceLexer
+        keywordMarker.done(getKeyword)
+
+        //definition of class
+        val defMarker = builder.mark()
+        parseDef ( builder )
+        defMarker.done( getDef )
+
+
+        //body of class
+        /*val bodyMarker = builder.mark()
+        parseBody ( builder )
+        bodyMarker.done( getBody )
+          */
+      }
+  }
+
+  abstract class InstanceDef extends TypeDef
+
+  case class ObjectDef extends InstanceDef {
+    def getKeyword = ScalaElementTypes.OBJECT
+
+    def getDef : ScalaElementType = ScalaElementTypes.OBJECT_DEF
+
+    def parseDef ( builder : PsiBuilder ) : Unit = {
+        while ( !builder.getTokenType.equals(ScalaTokenTypes.tLBRACE) ){
+        builder.advanceLexer
+      }
+    }
+
+  }
+
+  case class ClassDef extends InstanceDef {
+    def getKeyword = ScalaElementTypes.CLASS
+
+    def getDef = ScalaElementTypes.CLASS_DEF
+
+    def parseDef ( builder : PsiBuilder ) : Unit = {
+      while ( !builder.getTokenType.equals(ScalaTokenTypes.tLBRACE) ){
+        builder.advanceLexer
+      }
+    }
+  }
+
+  case class TraitDef extends TypeDef {
+    def getKeyword = ScalaElementTypes.TRAIT
+
+    def getDef = ScalaElementTypes.TRAIT_DEF
+
+    def parseDef ( builder : PsiBuilder ) : Unit = {
+
+      while ( !builder.getTokenType.equals(ScalaTokenTypes.tLBRACE) ){
+        builder.advanceLexer
+      }
+    }
+  }
+
+  def parse(builder : PsiBuilder) : Unit = {
+    def parseInst ( builder : PsiBuilder ) : Unit = {
+        builder.getTokenType() match {
+          case ScalaTokenTypes.kCLASS => {
+            val classStmtMarker = builder.mark()
+
+          //  builder.advanceLexer
+
+            ClassDef.parse( builder )
+
+            classStmtMarker.done(ScalaElementTypes.CLASS_STMT)
+          }
+
+          case ScalaTokenTypes.kOBJECT => {
+              val objectStmtMarker = builder.mark()
+          //    builder.advanceLexer
+
+              ObjectDef.parse( builder )
+
+              objectStmtMarker.done(ScalaElementTypes.OBJECT_STMT)
+          }
+        }
+    }
+
+    val tmplDefMarker = builder.mark()
+
+    builder.getTokenType() match {
+      case ScalaTokenTypes.kCASE => {
+        val caseMarker = builder.mark();
+        builder.advanceLexer //Ate case
+
+        caseMarker.done(ScalaElementTypes.CASE);
+
+        parseInst( builder ) //handle class and object
+      }
+
+      case ScalaTokenTypes.kOBJECT | ScalaTokenTypes.kCLASS => {
+        parseInst( builder ) //handle class and object
+      }
+
+      case ScalaTokenTypes.kTRAIT => {
+        val traitStmtMarker = builder.mark();
+       
+        TraitDef.parse( builder )
+
+        traitStmtMarker.done(ScalaElementTypes.TRAIT_STMT);
+      }
+
+      case _ => builder.error("wrong type definition")
+
+    }
+
+    tmplDefMarker.done(ScalaElementTypes.TMPL_DEF)
+    //Console.println("tmplDefMareker done ")
+  }
+}
