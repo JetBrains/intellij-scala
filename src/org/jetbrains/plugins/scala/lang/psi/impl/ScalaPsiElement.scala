083baccf49951b8cd54cbb14dafdab2d52ec4e7b
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-18 14:10:09 +0000

    Fixed little bug with "super.id.id.type" statement parsing
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4002 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/ScalaPsiElement.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.lang.psi.impl
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+
+import java.util.List;
+
+
+trait ScalaPsiElement extends PsiElement {
+
+}
