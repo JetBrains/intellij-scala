fd88016bf23bdaa3f2053602fbcc3e8aafe44f4a
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-10-25 14:59:27 +0000

    Added some features with expressions
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4224 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/CompositeExpr.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/CompositeExpr.scala
@@ -15,13 +15,13 @@ object CompositeExpr {
 /*
 Composite Expression
 Default grammar
-Expr1 ::= if ‘(’ Expr1 ‘)’ [NewLine] Expr [[‘;’] else Expr]
-          | try ‘{’ Block ‘}’ [catch ‘{’ CaseClauses ‘}’] [finally Expr]
-          | while ‘(’ Expr ‘)’ [NewLine] Expr
-          | do Expr [StatementSeparator] while ‘(’ Expr ’)’
-          | for (‘(’ Enumerators ‘)’ | ‘{’ Enumerators ‘}’)[NewLine] [yield] Expr
-          | throw Expr
-          | return [Expr]
+Expr1 ::=   if ‘(’ Expr1 ‘)’ [NewLine] Expr [[‘;’] else Expr]                           (if)
+          | try ‘{’ Block ‘}’ [catch ‘{’ CaseClauses ‘}’] [finally Expr]                 (try)
+          | while ‘(’ Expr ‘)’ [NewLine] Expr                                          (while)
+          | do Expr [StatementSeparator] while ‘(’ Expr ’)’                            (do)
+          | for (‘(’ Enumerators ‘)’ | ‘{’ Enumerators ‘}’)[NewLine] [yield] Expr        (for)
+          | throw Expr                                                               (throw)
+          | return [Expr]                                                            (return)
           | [SimpleExpr ‘.’] id ‘=’ Expr                                               (b2)
           | SimpleExpr ArgumentExprs ‘=’ Expr                                         (b1)
           | PostfixExpr [‘:’ Type1]                                                   (a)
@@ -127,6 +127,70 @@ Expr1 ::= if ‘(’ Expr1 ‘)’ [NewLine] Expr [[‘;’] else Expr]
       }
     }
 
+
+    /** case (if) **/
+    def ifCase: ScalaElementType = {
+      val rollbackMarker = builder.mark() // marker to rollback
+
+      /* for mistakes processing */
+      def errorDone(msg: String): ScalaElementType = {
+        rollbackMarker.drop()
+        builder.error(msg)
+        compMarker.done(ScalaElementTypes.EXPR1)
+        ScalaElementTypes.EXPR1
+      }
+
+      def elseProcessing: ScalaElementType = {
+        if (builder.getTokenType.eq(ScalaTokenTypes.tSEMICOLON)){
+          ParserUtils.eatElement(builder, ScalaTokenTypes.tSEMICOLON)
+          ParserUtils.rollForward(builder)
+        }
+        if (builder.getTokenType.eq(ScalaTokenTypes.kELSE)) {
+          ParserUtils.eatElement(builder, ScalaTokenTypes.kELSE)
+          ParserUtils.rollForward(builder)
+          val res2 = Expr.parse(builder)
+          if (res2.eq(ScalaElementTypes.EXPR)){
+            rollbackMarker.drop()
+            compMarker.done(ScalaElementTypes.EXPR1)
+            ScalaElementTypes.EXPR1
+          } else errorDone("Wrong expression")                   
+        } else errorDone("else expected")
+      }
+
+      if (builder.getTokenType.eq(ScalaTokenTypes.kIF)){
+        ParserUtils.eatElement(builder, ScalaTokenTypes.kIF)
+        ParserUtils.rollForward(builder)
+        if (builder.getTokenType.eq(ScalaTokenTypes.tLPARENTHIS)){
+          ParserUtils.eatElement(builder, ScalaTokenTypes.tLPARENTHIS)
+          ParserUtils.rollForward(builder)
+          val res = CompositeExpr parse(builder)
+          if (res.eq(ScalaElementTypes.EXPR1)){
+            ParserUtils.rollForward(builder)
+            if (builder.getTokenType.eq (ScalaTokenTypes.tRPARENTHIS)){
+              ParserUtils.eatElement(builder, ScalaTokenTypes.tRPARENTHIS)
+              ParserUtils.rollForward(builder)
+              val res1 = Expr.parse(builder)
+              if (res1.eq(ScalaElementTypes.EXPR)){
+                ParserUtils.rollForward(builder)
+                builder.getTokenType match {
+                  case ScalaTokenTypes.kELSE | ScalaTokenTypes.tSEMICOLON => elseProcessing
+                  case _ => {
+                    rollbackMarker.drop()
+                    compMarker.done(ScalaElementTypes.EXPR1)
+                    ScalaElementTypes.EXPR1
+                  }
+                }
+              } else errorDone("Wrong expression")
+            } else errorDone(" ) expected")
+          } else errorDone("Wrong expression")
+        } else errorDone("( expected")
+
+      } else {
+        rollbackMarker.rollbackTo()
+        ScalaElementTypes.WRONGWAY
+      }
+    }
+
     var result = ScalaElementTypes.WRONGWAY
     /* Various variants of parsing */
     def variants(variantProcessing: => ScalaElementType) : Boolean = {
@@ -134,10 +198,13 @@ Expr1 ::= if ‘(’ Expr1 ‘)’ [NewLine] Expr [[‘;’] else Expr]
       result.equals(ScalaElementTypes.EXPR1)
     }
 
-    /* Parsing function body */
 
-    /* case (b) */
-    if (variants(bCase)) result
+
+    /* Parsing function body */
+    /* case (if) */
+    if (variants(ifCase)) result
+    /* cases (b1), (b2) */
+    else if (variants(bCase)) result
     /* case (a) */
     else if (variants(aCase)) result
     else {
