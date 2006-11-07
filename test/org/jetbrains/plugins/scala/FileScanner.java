c72b4cdf673dfba50947bd78b184a0aee2394ed1
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-11-07 16:40:38 +0000

    Added some features with expressions
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4397 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/FileScanner.java
@@ -0,0 +1,49 @@
+package org.jetbrains.plugins.scala;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileScanner {
+
+  /**
+   * @param path       Path to base file (often it`s a directory to scan)
+   * @param pattern    Pattern to match files
+   * @param dirAllowed true if directory is allowed.
+   * @return List of files
+   * @throws java.io.FileNotFoundException if file not found
+   */
+  public static List<File> scan(final String path, final String pattern, final boolean dirAllowed) throws FileNotFoundException {
+    List<File> myFiles = new ArrayList<File>();
+    File baseFile = new File(path);
+    if (!baseFile.exists()) {
+      throw new FileNotFoundException("File " + path + " doesn`t exist!");
+    }
+    scanForFiles(myFiles, baseFile, pattern, dirAllowed);
+    return myFiles;
+  }
+
+  @SuppressWarnings({"HardCodedStringLiteral"})
+  private static void scanForFiles(final List<File> files, final File f, final String pattern, final boolean dirAllowed) {
+    // recursively scan for all subdirectories
+    if (f.isDirectory()) {
+      for (File file : f.listFiles()) {
+        if (!file.isDirectory()) {
+          String path = file.getAbsolutePath();
+          if (!path.contains(".svn") &&
+                  !path.contains(".cvs") &&
+                  path.matches(pattern)) {
+            files.add(file);
+          }
+        } else {
+          if (dirAllowed) {
+            files.add(file);
+          }
+          scanForFiles(files, file, pattern, dirAllowed);
+        }
+      }
+    }
+  }
+
+}
