d781885f7fb876f9880773ec00946448e8ba7efd
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-11-07 12:55:19 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4388 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/tests/examples/anonimousClass.scala
@@ -0,0 +1,18 @@
+package tests.examples;
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 07.11.2006
+ * Time: 12:48:29
+ */
+class Run 
+
+class anonimousClass {
+  var a = new Run {
+    def run () : Unit = {}
+    def b = {
+      run()
+    }
+
+  } 
+}
