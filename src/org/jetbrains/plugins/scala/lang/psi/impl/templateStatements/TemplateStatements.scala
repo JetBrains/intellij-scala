db4c4249b07c3340d707e840e500e7c22354575f
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-11-08 18:23:26 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4442 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/templateStatements/TemplateStatements.scala
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.top.templateStatements {
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 07.11.2006
+ * Time: 15:28:13
+ */
+
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
+import com.intellij.lang.ASTNode
+
+  abstract class ScTemplateStatement ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
+    def statementName : String
+    override def toString: String = "Template statement" + ": " + statementName;
+  }
+
+  case class ScValStatement ( node : ASTNode ) extends ScTemplateStatement ( node ) {
+    override def statementName = "value"
+  }
+
+  case class ScVarStatement ( node : ASTNode ) extends ScTemplateStatement ( node ) {
+    override def statementName = "variable"
+  }
+
+  case class ScDefStatement ( node : ASTNode ) extends ScTemplateStatement ( node ) {
+    override def statementName = "definition"
+  }
+
+  case class ScDclStatement ( node : ASTNode ) extends ScTemplateStatement ( node ) {
+    override def statementName = "declaration"
+  }
+
+  case class ScTmplDefStatement ( node : ASTNode ) extends ScTemplateStatement ( node ) {
+    override def statementName = "value"
+  }
+}
