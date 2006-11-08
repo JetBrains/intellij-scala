0c33d68f72129abd50c77ab98547e2e971939c89
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-11-08 14:24:57 +0000

    Added case clauses
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4422 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScSimpleTypeImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScSimpleTypeImpl.scala
@@ -7,5 +7,5 @@ import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.psi._
 
 class ScSimpleTypeImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
-      override def toString: String = "Simple Type: " + getText
+      override def toString: String = "Simple Type: "
 }
