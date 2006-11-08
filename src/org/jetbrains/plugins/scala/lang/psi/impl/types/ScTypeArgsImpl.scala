010b394768a4bc6b2abca18d5bcd1f68a34c4ad9
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-11-08 17:46:41 +0000

    Fixed bugs with block expressions
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4434 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScTypeArgsImpl.scala
@@ -0,0 +1,11 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.types
+/**
+* @author Ilya Sergey
+*/
+import com.intellij.lang.ASTNode
+
+import org.jetbrains.plugins.scala.lang.psi._
+
+class ScTypeArgsImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+      override def toString: String = "Type arguments list" 
+}
