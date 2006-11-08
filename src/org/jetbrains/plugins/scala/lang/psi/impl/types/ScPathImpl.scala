9accdd2cafeb46efe7248f8d888dcea96b63c47e
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-11-08 16:49:37 +0000

    Fixed bugs with block expressions
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4431 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScPathImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScPathImpl.scala
@@ -7,5 +7,5 @@ import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.psi._
 
 class ScPathImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
-      override def toString: String = "Path: "
+      override def toString: String = "Path"
 }
--- src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScSimpleTypeImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScSimpleTypeImpl.scala
@@ -7,5 +7,5 @@ import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.psi._
 
 class ScSimpleTypeImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
-      override def toString: String = "Simple Type: "
+      override def toString: String = "Simple Type"
 }
