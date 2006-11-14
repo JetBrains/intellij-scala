3adc4073f12289f25c2c8b79beb4867057f135bd
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-11-14 14:43:08 +0000

    Added pattern sequences
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4503 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/psi/visitors/ScalaElementVisitor.scala
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.scala.lang.psi
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiReferenceExpression;
+
+abstract class ScalaFElementVisitor extends PsiElementVisitor {
+
+}
+
