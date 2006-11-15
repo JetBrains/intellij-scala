0d608b8565a18704b33bb04fcff9de85c61352e5
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2006-11-15 17:30:50 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4524 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/top/ScTop.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/top/ScTop.scala
@@ -20,8 +20,8 @@ import com.intellij.lang.ASTNode
   class ScPackaging ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
     override def toString: String = "Packaging"
 
-    /*def getPackageBlock : ScTopStatSeq = {
-      return new ScTopStatSeq( node )
+    /*def getTmplDefinitions : Array[T <: TmplDef] {
+      getChildren
     } */
   }
 
@@ -32,19 +32,7 @@ import com.intellij.lang.ASTNode
   case class ScQualId ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
     //todo
     override def toString: String = "Qualified identifier"
+
     def getQualId = getText
   }
-
-/*  case class ScTopStat ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
-    //todo
-    override def toString: String = "Top statement"
-  }
-
-  case class ScTopStatSeq ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
-    //todo
-    override def toString: String = "Top statement sequence"
-  }
-
-  */
-
 }
