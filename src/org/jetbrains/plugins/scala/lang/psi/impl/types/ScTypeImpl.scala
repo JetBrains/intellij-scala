ee9c68d4de9dc4502622b3bfde5723b4311e00ae
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-11-22 16:06:41 +0000

    Added refined types
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4622 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScTypeImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScTypeImpl.scala
@@ -7,7 +7,7 @@ import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.psi._
 
 class ScTypeImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
-      override def toString: String = "Common type"
+      override def toString: String = "Function type"
 }
 
 class ScType1Impl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
