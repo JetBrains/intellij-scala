34cc307f555a0561009296506f85ff8248fcdbf7
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-12-05 16:33:18 +0000

    Fixed bugs with result expression.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@4810 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/Program.scala
@@ -0,0 +1,71 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing
+
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import org.jetbrains.plugins.scala.lang.parser.util._
+import org.jetbrains.plugins.scala.util._
+import org.jetbrains.plugins.scala.lang.parser.parsing.expressions._
+import org.jetbrains.plugins.scala.lang.parser.parsing.types._
+import org.jetbrains.plugins.scala.util.DebugPrint
+
+import com.intellij.lang.PsiBuilder
+
+class Program extends ScalaTokenTypes {
+  def parse(builder: PsiBuilder): Unit = {
+
+    var flag = true
+
+    // Debug print mode off
+    DebugPrint.displayLog_=(false) 
+
+    def rollForward : Unit = {
+      while ( !builder.eof() && flag){
+         builder.getTokenType match{
+           case ScalaTokenTypes.tLINE_TERMINATOR
+           | ScalaTokenTypes.tSEMICOLON => builder.advanceLexer
+           case _ => flag = false
+         }
+      }
+      flag = true
+    }
+
+    def parseNext : Unit = {
+      while ( !builder.eof() ) {
+         rollForward
+
+         // Expression functionality testing
+         if (PostfixExpr.POSTFIX_FIRST.contains(builder.getTokenType)) {
+           PostfixExpr parse (builder)
+         } else builder advanceLexer
+
+       }
+    }
+
+
+    //var marker = builder.mark()
+   // rollForward
+//    ParserUtils.rollForward(builder)
+
+    //compilation unit
+    if ( !builder.eof() ){
+      CompilationUnit.parse(builder)
+    }
+
+    if (!builder.eof()) {
+      val trashMarker = builder.mark
+      while (!builder.eof()) {
+        builder error "out of compilation unit"
+        DebugPrint println ("after TopStatSeq: " + builder.getTokenType)
+        builder.advanceLexer
+      }
+      trashMarker.done(ScalaElementTypes.TRASH)
+    }
+    /*
+    parseNext
+    ParserUtils.rollForward(builder)
+    */
+    //marker.done(ScalaElementTypes.FILE)
+  }
+
+}
