287a91705c1322818655256b3d961116ad329d72
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2006-12-29 12:10:42 +0000

    Fixed bugs with result expression.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5027 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/util/ScalaToolsFactoryImpl.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala.util
+
+import com.intellij.lang.ParserDefinition
+import org.jetbrains.plugins.scala.lang.parser.ScalaParserDefinition
+import org.jetbrains.plugins.scala.lang.folding.ScalaFoldingBuilder
+
+/**
+ * Author: Ilya Sergey
+ * Date: 09.10.2006
+ * Time: 21:26:04
+ */
+class ScalaToolsFactoryImpl extends ScalaToolsFactory {
+
+  def createScalaParserDefinition: ParserDefinition = {
+    new ScalaParserDefinition()
+  }
+
+  def createScalaFoldingBuilder: ScalaFoldingBuilder = {
+    new ScalaFoldingBuilder()
+  }
+}
