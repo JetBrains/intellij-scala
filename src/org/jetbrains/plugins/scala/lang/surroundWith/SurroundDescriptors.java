552e6b7903ddae9e95ac0da01875f6b0e03ebd34
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-01-09 19:09:53 +0000

    beginning of structure view added beginning of surroundWith added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5084 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/SurroundDescriptors.java
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.lang.surroundWith;
+
+import com.intellij.lang.surroundWith.SurroundDescriptor;
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 09.01.2007
+ * Time: 16:23:10
+ */
+
+public interface SurroundDescriptors {
+  SurroundDescriptor[] getSurroundDescriptors();
+}
