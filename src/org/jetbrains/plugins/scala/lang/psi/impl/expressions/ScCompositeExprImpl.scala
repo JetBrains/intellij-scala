a041a5d6697890b34a11bd16a929904b0ae3cb1e
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-09 15:21:00 +0000

    Fixed bugs with result expression.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5076 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/expressions/ScCompositeExprImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/expressions/ScCompositeExprImpl.scala
@@ -3,51 +3,71 @@ package org.jetbrains.plugins.scala.lang.psi.impl.expressions{
 * @author Ilya Sergey
 */
 import com.intellij.lang.ASTNode
+import com.intellij.psi._
 
 import org.jetbrains.plugins.scala.lang.psi._
 
-  case class ScIfStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScIfStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "IF statement"
+      def getType() : PsiType = null
   }
-  case class ScForStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScForStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "FOR statement"
+      def getType() : PsiType = null
   }
-  case class ScDoStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScDoStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "DO statement"
+      def getType() : PsiType = null
   }
-  case class ScTryStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScTryStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "TRY statement"
+      def getType() : PsiType = null
   }
-      case class ScTryBlockImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+      case class ScTryBlockImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
           override def toString: String = "Try block"
+          def getType() : PsiType = null
       }
-      case class ScCatchBlockImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+      case class ScCatchBlockImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
           override def toString: String = "Catch block"
+          def getType() : PsiType = null
       }
-      case class ScFinallyBlockImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+      case class ScFinallyBlockImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
           override def toString: String = "Finally block"
+          def getType() : PsiType = null
       }
   
-  case class ScWhileStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScWhileStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "WHILE statement"
+      def getType() : PsiType = null
   }
-  case class ScClosureImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScClosureImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "Method closure"
+      def getType() : PsiType = null
   }
-  case class ScReturnStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScReturnStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "RETURN statement"
+      def getType() : PsiType = null
   }
-  case class ScThrowStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScThrowStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "THROW statement"
+      def getType() : PsiType = null
   }
-  case class ScMatchStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScMatchStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "MATCH statement"
+      def getType() : PsiType = null
   }
-  case class ScTypedStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScTypedStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "Typed statement"
+      def getType() : PsiType = null
   }
-  case class ScAssignStmtImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+  case class ScAssignStmtImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
       override def toString: String = "Assign statement"
+      def getType() : PsiType = null
+  }
+  
+  case class ScExprImpl( node : ASTNode ) extends ScPsiExprImpl(node) {
+    override def toString: String = "Common expression"
+    def getType() : PsiType = null
   }
 
 
