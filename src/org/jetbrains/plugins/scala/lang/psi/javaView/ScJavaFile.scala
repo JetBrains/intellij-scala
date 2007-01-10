489b70ba5ddc61e980a90d72846ba6d2f40428ca
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-01-10 13:52:36 +0000

    who deleted this???
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5102 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/psi/javaView/ScJavaFile.scala
@@ -0,0 +1,49 @@
+/**
+ * @author ven
+ */
+package org.jetbrains.plugins.scala.lang.psi.javaView {
+  import com.intellij.extapi.psi.LightPsiFileBase
+  import com.intellij.psi._
+  import com.intellij.pom.java.LanguageLevel
+  import com.intellij.lang.StdLanguages
+  import com.intellij.openapi.fileTypes.StdFileTypes
+  import com.intellij.util.ArrayUtil
+  import org.jetbrains.plugins.scala.ScalaFileType
+  import org.jetbrains.plugins.scala.lang.psi.impl.top._
+
+  class ScJavaFileImpl(viewProvider : FileViewProvider) extends LightPsiFileBase(viewProvider, StdLanguages.JAVA) with PsiJavaFile {
+    def getLanguageLevel = LanguageLevel.JDK_1_5
+
+    def findImportReferenceTo (psiClass : PsiClass) = null
+
+    def getImplicitlyImportedPackageReferences = PsiJavaCodeReferenceElement.EMPTY_ARRAY
+
+    def getImplicitlyImportedPackages = ArrayUtil.EMPTY_STRING_ARRAY
+
+    def getSingleClassImports(checkIncludes : Boolean) = PsiClass.EMPTY_ARRAY
+
+    def getOnDemandImports(includeImplicit : Boolean, checkIncludes : Boolean) = PsiElement.EMPTY_ARRAY
+
+    def getImportList = null
+
+    def getPackageStatement = null
+
+    def importClass(psiClass : PsiClass) = false
+
+    //todo
+    def getPackageName = null
+
+    override def getClasses = {
+      val scFile = viewProvider.getPsi(ScalaFileType.SCALA_FILE_TYPE.getLanguage).asInstanceOf[ScFile]
+      scFile.getTmplDefs.map (c => new ScJavaClass(c, this)).toArray[PsiClass]
+    }
+
+    def getChildren = getClasses.asInstanceOf[Array[PsiElement]]
+
+    def copyLight (newFileViewProvider : FileViewProvider) = new ScJavaFileImpl(newFileViewProvider)
+
+    def clearCaches = {}
+
+    def getFileType = StdFileTypes.JAVA
+  }
+}
