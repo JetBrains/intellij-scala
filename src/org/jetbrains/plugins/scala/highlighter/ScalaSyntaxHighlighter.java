6fea48c0281d1fd0a2ec3ecb39f293292e3bec06
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-11 15:32:01 +0000

    Added basic completion features
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5124 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/highlighter/ScalaSyntaxHighlighter.java
+++ src/org/jetbrains/plugins/scala/highlighter/ScalaSyntaxHighlighter.java
@@ -7,6 +7,7 @@ import com.intellij.psi.tree.IElementType;
 import com.intellij.lexer.Lexer;
 import org.jetbrains.plugins.scala.lang.lexer.ScalaLexer;
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
@@ -19,121 +20,126 @@ import java.util.HashMap;
  */
 public class ScalaSyntaxHighlighter extends SyntaxHighlighterBase {
 
-    // Comments
-    static final TokenSet tCOMMENTS = TokenSet.create(
-            ScalaTokenTypes.tCOMMENT,
-            ScalaTokenTypes.tBLOCK_COMMENT
-    );
+  // Comments
+  static final TokenSet tCOMMENTS = TokenSet.create(
+          ScalaTokenTypes.tCOMMENT,
+          ScalaTokenTypes.tBLOCK_COMMENT,
+          // New
+          ScalaTokenTypes.tCOMMENT_BEGIN,
+          ScalaTokenTypes.tCOMMENT_END,
+          ScalaTokenTypes.tDOC_COMMENT_BEGIN,
+          ScalaTokenTypes.tDOC_COMMENT_END,
+          ScalaTokenTypes.tCOMMENT_CONTENT
+  );
 
-    // XML tags
-    static final TokenSet tXML_TAGS = TokenSet.create(
-            ScalaTokenTypes.tOPENXMLTAG
-    );
+  // XML tags
+  static final TokenSet tXML_TAGS = TokenSet.create(
+          ScalaTokenTypes.tOPENXMLTAG
+  );
 
-    // Variables
-    static final TokenSet tVARIABLES = TokenSet.create(
-            ScalaTokenTypes.tIDENTIFIER
-    );
+  // Variables
+  static final TokenSet tVARIABLES = TokenSet.create(
+          ScalaTokenTypes.tIDENTIFIER
+  );
 
-    // Numbers
-    static final TokenSet tNUMBERS = TokenSet.create(
-            ScalaTokenTypes.tINTEGER,
-            ScalaTokenTypes.tFLOAT
-    );
+  // Numbers
+  static final TokenSet tNUMBERS = TokenSet.create(
+          ScalaTokenTypes.tINTEGER,
+          ScalaTokenTypes.tFLOAT
+  );
 
-    // Braces
-    static final TokenSet tBRACES = TokenSet.create(
-            ScalaTokenTypes.tLBRACE,
-            ScalaTokenTypes.tRBRACE,
-            ScalaTokenTypes.tLPARENTHIS,
-            ScalaTokenTypes.tRPARENTHIS,
-            ScalaTokenTypes.tLSQBRACKET,
-            ScalaTokenTypes.tRSQBRACKET
-    );
+  // Braces
+  static final TokenSet tBRACES = TokenSet.create(
+          ScalaTokenTypes.tLBRACE,
+          ScalaTokenTypes.tRBRACE,
+          ScalaTokenTypes.tLPARENTHIS,
+          ScalaTokenTypes.tRPARENTHIS,
+          ScalaTokenTypes.tLSQBRACKET,
+          ScalaTokenTypes.tRSQBRACKET
+  );
 
-    // Strings
-    static final TokenSet tSTRINGS = TokenSet.create(
-            ScalaTokenTypes.tSTRING,
-            ScalaTokenTypes.tWRONG_STRING,
-            ScalaTokenTypes.tCHAR,
-            ScalaTokenTypes.tSYMBOL
-    );
+  // Strings
+  static final TokenSet tSTRINGS = TokenSet.create(
+          ScalaTokenTypes.tSTRING,
+          ScalaTokenTypes.tWRONG_STRING,
+          ScalaTokenTypes.tCHAR,
+          ScalaTokenTypes.tSYMBOL
+  );
 
-    // Keywords
-    static final TokenSet kRESWORDS = TokenSet.create(
-            ScalaTokenTypes.kABSTRACT,
-            ScalaTokenTypes.kCASE,
-            ScalaTokenTypes.kCATCH,
-            ScalaTokenTypes.kCLASS,
-            ScalaTokenTypes.kDEF,
-            ScalaTokenTypes.kDO,
-            ScalaTokenTypes.kELSE,
-            ScalaTokenTypes.kEXTENDS,
-            ScalaTokenTypes.kFALSE,
-            ScalaTokenTypes.kFINAL,
-            ScalaTokenTypes.kFINALLY,
-            ScalaTokenTypes.kFOR,
-            ScalaTokenTypes.kIF,
-            ScalaTokenTypes.kIMPLICIT,
-            ScalaTokenTypes.kIMPORT,
-            ScalaTokenTypes.kMATCH,
-            ScalaTokenTypes.kNEW,
-            ScalaTokenTypes.kNULL,
-            ScalaTokenTypes.kOBJECT,
-            ScalaTokenTypes.kOVERRIDE,
-            ScalaTokenTypes.kPACKAGE,
-            ScalaTokenTypes.kPRIVATE,
-            ScalaTokenTypes.kPROTECTED,
-            ScalaTokenTypes.kREQUIRES,
-            ScalaTokenTypes.kRETURN,
-            ScalaTokenTypes.kSEALED,
-            ScalaTokenTypes.kSUPER,
-            ScalaTokenTypes.kTHIS,
-            ScalaTokenTypes.kTHROW,
-            ScalaTokenTypes.kTRAIT,
-            ScalaTokenTypes.kTRY,
-            ScalaTokenTypes.kTRUE,
-            ScalaTokenTypes.kTYPE,
-            ScalaTokenTypes.kVAL,
-            ScalaTokenTypes.kVAR,
-            ScalaTokenTypes.kWHILE,
-            ScalaTokenTypes.kWITH,
-            ScalaTokenTypes.kYIELD
-    );
+  // Keywords
+  static final TokenSet kRESWORDS = TokenSet.create(
+          ScalaTokenTypes.kABSTRACT,
+          ScalaTokenTypes.kCASE,
+          ScalaTokenTypes.kCATCH,
+          ScalaTokenTypes.kCLASS,
+          ScalaTokenTypes.kDEF,
+          ScalaTokenTypes.kDO,
+          ScalaTokenTypes.kELSE,
+          ScalaTokenTypes.kEXTENDS,
+          ScalaTokenTypes.kFALSE,
+          ScalaTokenTypes.kFINAL,
+          ScalaTokenTypes.kFINALLY,
+          ScalaTokenTypes.kFOR,
+          ScalaTokenTypes.kIF,
+          ScalaTokenTypes.kIMPLICIT,
+          ScalaTokenTypes.kIMPORT,
+          ScalaTokenTypes.kMATCH,
+          ScalaTokenTypes.kNEW,
+          ScalaTokenTypes.kNULL,
+          ScalaTokenTypes.kOBJECT,
+          ScalaTokenTypes.kOVERRIDE,
+          ScalaTokenTypes.kPACKAGE,
+          ScalaTokenTypes.kPRIVATE,
+          ScalaTokenTypes.kPROTECTED,
+          ScalaTokenTypes.kREQUIRES,
+          ScalaTokenTypes.kRETURN,
+          ScalaTokenTypes.kSEALED,
+          ScalaTokenTypes.kSUPER,
+          ScalaTokenTypes.kTHIS,
+          ScalaTokenTypes.kTHROW,
+          ScalaTokenTypes.kTRAIT,
+          ScalaTokenTypes.kTRY,
+          ScalaTokenTypes.kTRUE,
+          ScalaTokenTypes.kTYPE,
+          ScalaTokenTypes.kVAL,
+          ScalaTokenTypes.kVAR,
+          ScalaTokenTypes.kWHILE,
+          ScalaTokenTypes.kWITH,
+          ScalaTokenTypes.kYIELD
+  );
 
-    static final TokenSet tOPS = TokenSet.create(
-            ScalaTokenTypes.tASSIGN,
-            ScalaTokenTypes.tDIV,
-            ScalaTokenTypes.tMINUS,
-            ScalaTokenTypes.tPLUS,
-            ScalaTokenTypes.tSTAR
-    );
+  static final TokenSet tOPS = TokenSet.create(
+          ScalaTokenTypes.tASSIGN,
+          ScalaTokenTypes.tDIV,
+          ScalaTokenTypes.tMINUS,
+          ScalaTokenTypes.tPLUS,
+          ScalaTokenTypes.tSTAR
+  );
 
-    private static final Map<IElementType, TextAttributesKey> ATTRIBUTES = new HashMap<IElementType, TextAttributesKey>();
+  private static final Map<IElementType, TextAttributesKey> ATTRIBUTES = new HashMap<IElementType, TextAttributesKey>();
 
-    static {
-        fillMap(ATTRIBUTES, tCOMMENTS, DefaultHighlighter.LINE_COMMENT);
-        fillMap(ATTRIBUTES, kRESWORDS, DefaultHighlighter.KEYWORD);
-        fillMap(ATTRIBUTES, tNUMBERS, DefaultHighlighter.NUMBER);
-        fillMap(ATTRIBUTES, tSTRINGS, DefaultHighlighter.STRING);
-        fillMap(ATTRIBUTES, tBRACES, DefaultHighlighter.BRACKETS);
+  static {
+    fillMap(ATTRIBUTES, tCOMMENTS, DefaultHighlighter.LINE_COMMENT);
+    fillMap(ATTRIBUTES, kRESWORDS, DefaultHighlighter.KEYWORD);
+    fillMap(ATTRIBUTES, tNUMBERS, DefaultHighlighter.NUMBER);
+    fillMap(ATTRIBUTES, tSTRINGS, DefaultHighlighter.STRING);
+    fillMap(ATTRIBUTES, tBRACES, DefaultHighlighter.BRACKETS);
 
-        fillMap(ATTRIBUTES, tOPS, DefaultHighlighter.OPERATION_SIGN);
-        fillMap(ATTRIBUTES, tXML_TAGS, DefaultHighlighter.OPERATION_SIGN);
-
+    fillMap(ATTRIBUTES, tOPS, DefaultHighlighter.OPERATION_SIGN);
+    fillMap(ATTRIBUTES, tXML_TAGS, DefaultHighlighter.OPERATION_SIGN);
 
 //        ATTRIBUTES.put(ScalaTokenTypes.tBAD_CHARACTER, DefaultHighliter.BAD_CHARACTER);
 
-    }
+  }
 
 
-    @NotNull
-    public Lexer getHighlightingLexer() {
-        return new ScalaLexer();
-    }
+  @NotNull
+  public Lexer getHighlightingLexer() {
+    return new ScalaLexer();
+  }
 
-    @NotNull
-    public TextAttributesKey[] getTokenHighlights(IElementType iElementType) {
-        return pack(ATTRIBUTES.get(iElementType));
-    }
+  @NotNull
+  public TextAttributesKey[] getTokenHighlights(IElementType iElementType) {
+    return pack(ATTRIBUTES.get(iElementType));
+  }
 }
