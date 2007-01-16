d1e29121a50d37b96e3cf1e9403324dbb0a7552f
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-16 15:39:19 +0000

    Added nested block comments
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5185 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionData.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionData.scala
@@ -5,71 +5,112 @@ import com.intellij.codeInsight.TailType;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.codeInsight.completion.DefaultInsertHandler;
 import com.intellij.psi.filters.TrueFilter;
 import com.intellij.psi.filters.TextFilter;
+import com.intellij.psi.filters.AndFilter;
 import com.intellij.psi.filters.NotFilter;
 import com.intellij.psi.filters.position.LeftNeighbour;
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
+import org.jetbrains.plugins.scala.lang.completion.filters.LeftLeftNeighbour;
+import org.jetbrains.plugins.scala.lang.completion.filters.BeforeDotFilter;
+import _root_.scala.collection.mutable._;
 /*
 * @author Ilya Sergey
-*
 */
 
 class ScalaCompletionData extends CompletionData {
 
-  val afterDotFilter = new LeftNeighbour(new TextFilter("."))
-  var variant = new CompletionVariant(new NotFilter(afterDotFilter));
-  variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
-  variant.addCompletionFilterOnElement(TrueFilter.INSTANCE)
+/* Adding variants */
+    init
 
-  var keywords = Array (
-    "true"
-    ,"false"
-    ,"null"
-    ,"abstract"
-    ,"case"
-    ,"catch"
-    ,"class"
-    ,"def"
-    ,"do"
-    ,"else"
-    ,"extends"
-    ,"final"
-    ,"finally"
-    ,"for"
-    ,"if"
-    ,"implicit"
-    ,"import"
-    ,"match"
-    ,"new"
-    ,"object"
-    ,"override"
-    ,"package"
-    ,"private"
-    ,"protected"
-    ,"requires"
-    ,"return"
-    ,"sealed"
-    ,"super"
-    ,"this"
-    ,"throw"
-    ,"trait"
-    ,"try"
-    ,"type"
-    ,"val"
-    ,"var"
-    ,"while"
-    ,"with"
-    ,"yield"
-  )
+  /*
+  * Adding completion variants after dot
+  */
+  def afterDotCompletion = {
 
-  variant.addCompletion(keywords);
-//  variant.setInsertHandler( new DefaultInsertHandler() );
-  registerVariant(variant);
+    def register(after: IElementType, handler: DefaultInsertHandler, elems: String*) = {
+      var andFilter = new AndFilter(
+        new LeftNeighbour(new TextFilter(".")),
+        new LeftLeftNeighbour(new BeforeDotFilter(after))
+      )
+      var variant = new CompletionVariant(andFilter)
+      if (handler != null) variant.setInsertHandler(handler)
+      variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
+      for (val el <- elems) variant.addCompletion(el)
+      registerVariant(variant)
+    }
+
+    register(ScalaTokenTypes.tIDENTIFIER,
+             new DefaultInsertHandler(),
+             ScalaKeyword.THIS,
+             ScalaKeyword.SUPER)
+    register(ScalaTokenTypes.tIDENTIFIER,
+             null,
+             ScalaKeyword.TYPE)
+    register(ScalaTokenTypes.kTHIS,
+             null,
+             ScalaKeyword.TYPE)
+  }
+
+  def init = {
+
+    /* Special cases */
+    afterDotCompletion
+
+    val afterDotFilter = new LeftNeighbour(new TextFilter("."))
+    var variant = new CompletionVariant(new NotFilter(afterDotFilter));
+    variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
+    variant.addCompletionFilterOnElement(TrueFilter.INSTANCE)
+
+    var keywords = Array (
+      "true"
+      ,"false"
+      ,"null"
+      ,"abstract"
+      ,"case"
+      ,"catch"
+      ,"class"
+      ,"def"
+      ,"do"
+      ,"else"
+      ,"extends"
+      ,"final"
+      ,"finally"
+      ,"for"
+      ,"if"
+      ,"implicit"
+      ,"import"
+      ,"match"
+      ,"new"
+      ,"object"
+      ,"override"
+      ,"package"
+      ,"private"
+      ,"protected"
+      ,"requires"
+      ,"return"
+      ,"sealed"
+      ,"super"
+      ,"this"
+      ,"throw"
+      ,"trait"
+      ,"try"
+      ,"type"
+      ,"val"
+      ,"var"
+      ,"while"
+      ,"with"
+      ,"yield"
+    )
+
+    variant.addCompletion(keywords);
+    registerVariant(variant);
+
+  }
 
   override def findPrefix(insertedElement : PsiElement, offset: Int) : String = {
     WordCompletionData.findPrefixSimple(insertedElement, offset)
   }
-
 
 }
