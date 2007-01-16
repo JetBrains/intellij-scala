d4c645f49d1d9ed0c36bbf03d11f10146dae5416
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-16 11:52:32 +0000

    Added nested block comments
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5174 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/util/ScalaToolsFactoryImpl.scala
+++ src/org/jetbrains/plugins/scala/util/ScalaToolsFactoryImpl.scala
@@ -8,7 +8,7 @@ import org.jetbrains.plugins.scala.lang.folding.ScalaFoldingBuilder
 import org.jetbrains.plugins.scala.lang.surroundWith.descriptors.ScalaSurroundDescriptors
 import org.jetbrains.plugins.scala.lang.surroundWith._
 import org.jetbrains.plugins.scala.structure.ScalaStructureViewBuilder
-//import org.jetbrains.plugins.scala.lang.completion.ScalaCompletionData
+import org.jetbrains.plugins.scala.lang.completion.ScalaCompletionData
 import com.intellij.codeInsight.completion._;
 import com.intellij.ide.structureView.StructureViewBuilder
 import com.intellij.psi.PsiFile
@@ -24,7 +24,7 @@ class ScalaToolsFactoryImpl extends ScalaToolsFactory {
 
   def createScalaFoldingBuilder: ScalaFoldingBuilder = new ScalaFoldingBuilder()
 
-//  def createScalaCompletionData : CompletionData = new ScalaCompletionData()
+  def createScalaCompletionData : CompletionData = new ScalaCompletionData()
 
   def createJavaView (viewProvider : FileViewProvider) = new ScJavaFile(viewProvider)
 
