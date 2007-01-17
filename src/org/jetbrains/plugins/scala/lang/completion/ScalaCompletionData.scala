03f52ecb0666b38cc325d42aaaf49c705d23d3ae
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-17 16:44:23 +0000

    Added most intelligebt keyword completion
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5203 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionData.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionData.scala
@@ -6,11 +6,7 @@ import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.codeInsight.completion.DefaultInsertHandler;
-import com.intellij.psi.filters.TrueFilter;
-import com.intellij.psi.filters.TextFilter;
-import com.intellij.psi.filters.AndFilter;
-import com.intellij.psi.filters.OrFilter;
-import com.intellij.psi.filters.NotFilter;
+import com.intellij.psi.filters._;
 import com.intellij.psi.filters.position._;
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
 import org.jetbrains.plugins.scala.lang.completion.filters._;
@@ -68,25 +64,69 @@ class ScalaCompletionData extends CompletionData {
   def topDefinitionsCompletion = {
     val afterDotFilter = new LeftNeighbour(new TextFilter("."))
     var variant = new CompletionVariant(new NotFilter(afterDotFilter));
+/*
     variant.includeScopeClass(classOf[ScPackaging].asInstanceOf[java.lang.Class[ScPackaging]], true);
     variant.includeScopeClass(classOf[ScalaFile].asInstanceOf[java.lang.Class[ScalaFile]], true);
+*/
+    variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
     addCompletions(
       variant,
       ScalaKeyword.CLASS,
       ScalaKeyword.OBJECT,
       ScalaKeyword.TRAIT,
       ScalaKeyword.IMPORT,
-      ScalaKeyword.PACKAGE
+      ScalaKeyword.PACKAGE,
+      ScalaKeyword.ABSTRACT,
+      ScalaKeyword.SEALED,
+      ScalaKeyword.FINAL,
+      ScalaKeyword.IMPLICIT,
+      ScalaKeyword.PRIVATE,
+      ScalaKeyword.PROTECTED
     )
     registerVariant(variant)
   }
 
   /* keyword "requires" completion */
   def requiresCompletion = {
-    val filter = new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.CLASS_DEF)))
-    val variant = new CompletionVariant(filter);
-    variant.includeScopeClass(classOf[ScalaFile].asInstanceOf[java.lang.Class[ScalaFile]], true);
+    var filter = new OrFilter()
+    filter.addFilter(new AndFilter(
+        new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.CLASS_DEF, "requires"))),
+        new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.CLASS_DEF, "extends")))
+      )
+    )
+    filter.addFilter(new AndFilter(
+        new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.TRAIT_DEF, "requires"))),
+        new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.TRAIT_DEF, "extends")))
+      )
+    )
+    var filter1 = new AndFilter(filter, new NotFilter(new LeftNeighbour(new TextFilter("class"))))
+    filter1 = new AndFilter(filter1, new NotFilter(new LeftNeighbour(new TextFilter("trait"))))
+
+    val variant = new CompletionVariant(filter1);
+    //variant.includeScopeClass(classOf[ScalaFile].asInstanceOf[java.lang.Class[ScalaFile]], true);
+    variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
+// Exclude all unnecessary 
+
     variant.addCompletion(ScalaKeyword.REQUIRES)
+    registerVariant(variant)
+  }
+
+  /* keyword "extends" completion */
+  def extendsCompletion = {
+    var filter = new OrFilter()
+    filter.addFilter(new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.CLASS_DEF, "extends"))))
+    filter.addFilter(new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.OBJECT_DEF, "extends"))))
+    filter.addFilter(new LeftNeighbour(new SuperParentFilter(new ElementTypeFilter(ScalaElementTypes.TRAIT_DEF, "extends"))))
+    var filter1 = new AndFilter(filter, new NotFilter(new LeftNeighbour(new TextFilter("class"))))
+    filter1 = new AndFilter(filter1, new NotFilter(new LeftNeighbour(new TextFilter("trait"))))
+    filter1 = new AndFilter(filter1, new NotFilter(new LeftNeighbour(new TextFilter("object"))))
+    filter1 = new AndFilter(filter1, new NotFilter(new LeftNeighbour(new TextFilter("requires"))))
+    val variant = new CompletionVariant(filter1);
+    //variant.includeScopeClass(classOf[ScalaFile].asInstanceOf[java.lang.Class[ScalaFile]], true);
+    variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
+// Exclude all unnecessary
+
+    variant.addCompletion(ScalaKeyword.EXTENDS)
     registerVariant(variant)
   }
 
@@ -96,32 +136,25 @@ class ScalaCompletionData extends CompletionData {
   def inExpressionCompletion = {
     val afterDotFilter = new LeftNeighbour(new TextFilter("."))
     var variant = new CompletionVariant(new NotFilter(afterDotFilter));
-//    variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
-    variant.includeScopeClass(classOf[ScPsiExprImpl].asInstanceOf[java.lang.Class[ScPsiExprImpl]], false);
+    variant.includeScopeClass(classOf[LeafPsiElement].asInstanceOf[java.lang.Class[LeafPsiElement]], true);
+//    variant.includeScopeClass(classOf[ScPsiExprImpl].asInstanceOf[java.lang.Class[ScPsiExprImpl]], false);
     variant.addCompletionFilterOnElement(TrueFilter.INSTANCE)
     var keywords = Array (
       "true"
       ,"false"
       ,"null"
-      ,"abstract"
       ,"case"
       ,"catch"
       ,"def"
       ,"do"
       ,"else"
-      ,"extends"
-      ,"final"
       ,"finally"
       ,"for"
       ,"if"
-      ,"implicit"
       ,"match"
       ,"new"
       ,"override"
-      ,"private"
-      ,"protected"
       ,"return"
-      ,"sealed"
       ,"super"
       ,"this"
       ,"throw"
@@ -147,6 +180,7 @@ class ScalaCompletionData extends CompletionData {
     afterDotCompletion
     topDefinitionsCompletion
     requiresCompletion
+    extendsCompletion
     inExpressionCompletion
 
 
