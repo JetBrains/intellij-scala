c3c651ae79b1c77b2283af7bc985e1e3859f7183
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-22 11:16:20 +0000

    Fixed bugs with result expression.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5275 59f64563-22e8-0310-8d2c-98a15e1539f2

--- test/org/jetbrains/plugins/scala/lang/parser/ParserTest.java
+++ test/org/jetbrains/plugins/scala/lang/parser/ParserTest.java
@@ -6,53 +6,34 @@ import junit.framework.Assert;
 import junit.framework.Test;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.plugins.scala.testcases.BaseScalaFileSetTestCase;
+import org.jetbrains.plugins.scala.util.TestUtils;
 
 public class ParserTest extends BaseScalaFileSetTestCase {
   @NonNls
   private static final String DATA_PATH = "test/org/jetbrains/plugins/scala/lang/parser/data/";
+  //private static final String DATA_PATH = "test/org/jetbrains/plugins/scala/lang/parser/data/actual";
 
   public ParserTest() {
-    super(DATA_PATH);
+    super(  System.getProperty("path")!=null ?
+            System.getProperty("path") :
+            DATA_PATH
+    );
   }
 
 
   public String transform(String testName, String[] data) throws Exception {
     String fileText = data[0];
-    PsiManager psiManager = PsiManager.getInstance(project);
-    PsiElementFactory psiElementFactory = psiManager.getElementFactory();
-    Assert.assertNotNull(psiElementFactory);
-    Assert.assertNotNull(TEMP_FILE);
-    Assert.assertNotNull(fileText);
-    PsiFile psiFile = psiElementFactory.createFileFromText(TEMP_FILE, fileText);
+
+    PsiFile psiFile = TestUtils.createPseudoPhysicalFile(project, fileText);
 
     String psiTree = DebugUtil.psiToString(psiFile, false);
     System.out.println("------------------------ "+testName+" ------------------------");
     System.out.println(psiTree);
     System.out.println("");
 
-    //String psiLeafText = gatherTextFromPsiFile(psiFile);
-
     return psiTree;
 
   }
-
-    private String gatherTextFromPsiFile(PsiFile psiFile) {
-//        final StringBuffer result = new StringBuffer();
-//        PsiElementVisitor myVisitor = new ScalaElementVisitor() {
-//            public void visitElement(PsiElement element) {
-//// if child is leaf
-//                if (element.getFirstChild() == null) {
-//                    result.append(element.getText());
-//                } else {
-//                    element.acceptChildren(this);
-//                }
-//            }
-//
-//        };
-//        psiFile.accept(myVisitor);
-//        return result.toString();
-      return "";
-    }
 
   public static Test suite() {
     return new ParserTest();
