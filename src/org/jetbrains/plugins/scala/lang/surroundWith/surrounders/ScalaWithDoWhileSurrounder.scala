651dbf7a95f4ee4851b7da202d97951a7a2f09c7
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-01-22 15:02:19 +0000

    add do while surround with
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5292 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/ScalaWithDoWhileSurrounder.scala
@@ -0,0 +1,32 @@
+package org.jetbrains.plugins.scala.lang.surroundWith.surrounders
+
+/**
+ * @author: Dmitry Krasilschikov
+ */
+
+import com.intellij.psi.PsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.plugins.scala.lang.psi.impl.expressions._
+
+
+class ScalaWithDoWhileSurrounder extends ScalaExpressionSurrounder {
+  override def isApplicable(expr : ScExprImpl) : Boolean = {
+//    expr.isInstanceOf[ScExprImpl]
+    true
+  }
+
+  override def getExpressionTemplateAsString (exprAsString : String) = "do { \n " + exprAsString + "\n" + "} while (true)"
+
+  override def getTemplateDescription = "do / while"
+
+  override def getSurroundSelectionRange (doWhileNode : ASTNode ) : TextRange = {
+    val doWhileStmt = doWhileNode.getPsi.asInstanceOf[ScDoStmtImpl]
+    val conditionNode : ASTNode = doWhileStmt.condition.getNode
+
+    val startOffset = conditionNode.getTextRange.getStartOffset
+    val endOffset = conditionNode.getTextRange.getEndOffset
+
+    return new TextRange(startOffset, endOffset);
+  }
+}
