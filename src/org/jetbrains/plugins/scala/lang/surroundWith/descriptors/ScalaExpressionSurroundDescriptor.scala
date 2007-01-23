ed71d9a4b01e75e82480f180bcf62f65cb577e02
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-01-23 10:38:19 +0000

    added SurrounWithTester and data
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5325 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/descriptors/ScalaExpressionSurroundDescriptor.scala
@@ -0,0 +1,71 @@
+package org.jetbrains.plugins.scala.lang.surroundWith.descriptors;
+
+/**
+ * User: Dmitry.Krasilschikov
+ * Date: 09.01.2007
+ * Time: 16:36:32
+ */
+import com.intellij.lang.surroundWith.SurroundDescriptor;
+import com.intellij.lang.surroundWith.Surrounder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.codeInsight.CodeInsightUtil;
+import com.intellij.debugger.codeinsight.JavaWithRuntimeCastSurrounder;
+import com.intellij.featureStatistics.FeatureUsageTracker;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.PsiWhiteSpace;
+
+import org.jetbrains.plugins.scala.util.DebugPrint
+import org.jetbrains.plugins.scala.lang.psi.impl.expressions.ScExprImpl
+import org.jetbrains.plugins.scala.lang.psi.impl.expressions.ScInfixExprImpl
+import org.jetbrains.plugins.scala.lang.surroundWith.surrounders._
+
+           
+class ScalaExpressionSurroundDescriptor extends SurroundDescriptor {
+  private val SURROUNDERS : Array[Surrounder] = Array.apply (
+    new ScalaWithBracketsSurrounder("(", ")") {
+      override def getTemplateDescription = "surround with ( )"
+    }, 
+    new ScalaWithBracketsSurrounder("{", "}") {
+      override def getTemplateDescription = "surround with { }"
+    },
+    new ScalaWithIfSurrounder(),
+    new ScalaWithForSurrounder(),
+    new ScalaWithWhileSurrounder(),
+    new ScalaWithDoWhileSurrounder(),
+    new ScalaWithTrySurrounder(),
+    new ScalaWithTryCatchSurrounder()
+  )
+
+  override def getSurrounders() : Array[Surrounder] = SURROUNDERS
+
+  override def getElementsToSurround(file : PsiFile, startOffset : Int, endOffset : Int) : Array[PsiElement] = {
+    val expr : ScExprImpl = findExpressionInRange(file, startOffset, endOffset)
+    if (expr == null) return PsiElement.EMPTY_ARRAY
+
+    Array.apply(expr)
+  }
+
+  def findExpressionInRange(file : PsiFile, startOffset : Int, endOffset : Int) : ScExprImpl = {
+
+    val element1 : PsiElement = file.findElementAt(startOffset);
+    val element2 : PsiElement = file.findElementAt(endOffset - 1);
+
+    var endOffsetLocal : Int = endOffset
+    var startOffsetLocal : Int = startOffset
+
+    if (element1.isInstanceOf[PsiWhiteSpace]) {
+      startOffsetLocal = element1.getTextRange().getEndOffset();
+    }
+
+    if (element2.isInstanceOf[PsiWhiteSpace]) {
+      endOffsetLocal = element2.getTextRange().getStartOffset();
+    }
+
+    val expression : ScExprImpl = PsiTreeUtil.findElementOfClassAtRange[ScExprImpl](file, startOffsetLocal, endOffsetLocal, classOf[ScExprImpl].asInstanceOf[java.lang.Class[ScExprImpl]]);
+
+    if (expression == null || expression.getTextRange().getEndOffset() != endOffsetLocal) return null
+    
+    return expression;
+  }
+}
