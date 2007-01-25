55ba273a29fa30ea9cca06ab87ca162492d26ba2
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-25 15:57:01 +0000

    Added most intelligebt keyword completion
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5415 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/completion/filters/BeforeDotFilter.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/filters/BeforeDotFilter.scala
@@ -13,17 +13,6 @@ import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
 
 class BeforeDotFilter(elems : IElementType*) extends LeftNeighbour {
 
-  trait SeqContainer[T] requires SeqContainer[T] extends Seq[T]{
-    def contains(elem: T): Boolean = {
-      var list = this.toList
-      list.contains(elem)
-    }
-  }
-
-  def contains[T](seq: Seq[T], elem: T): Boolean = {
-    seq.toList.contains(elem)
-  }
-
   override def isAcceptable (element: java.lang.Object, context: PsiElement): Boolean = {
     if (!(element.isInstanceOf[PsiElement])) return false
     val elementType = element.asInstanceOf[PsiElement].getNode().getElementType()
