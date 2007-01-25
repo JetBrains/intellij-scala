dca2532f0f0a78447450712041612360ed9aad43
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-25 12:15:23 +0000

    Added most intelligebt keyword completion
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5391 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/ScalaWithBracketsSurrounder.scala
@@ -0,0 +1,27 @@
+ package org.jetbrains.plugins.scala.lang.surroundWith.surrounders;
+
+ /**
+  * @author: Dmitry Krasilschikov
+  */
+
+ import org.jetbrains.plugins.scala.lang.psi.impl.expressions.ScExprImpl
+ import com.intellij.lang.ASTNode
+ import com.intellij.openapi.util.TextRange
+
+/*
+ * ScalaWithBracketsSurrounder is responsible of surrounders, witch enclause expression in brackets
+*/
+
+ class ScalaWithBracketsSurrounder (lBracket : String, rBracket : String) extends ScalaExpressionSurrounder {
+
+   override def isApplicable(expr : ScExprImpl) : Boolean = true
+
+   override def getExpressionTemplateAsString (exprAsString : String) = lBracket + exprAsString + rBracket
+
+   override def getTemplateDescription = lBracket + "expression" + rBracket
+
+   override def getSurroundSelectionRange (expr : ASTNode) : TextRange = {
+     val offset = expr.getTextRange.getEndOffset
+     new TextRange(offset, offset)
+   }
+ }
---
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/ScalaWithTryCatchSurrounder.scala
@@ -0,0 +1,29 @@
+package org.jetbrains.plugins.scala.lang.surroundWith.surrounders;
+
+/**
+ * @author: Dmitry Krasilschikov
+ */
+
+import com.intellij.psi.PsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.plugins.scala.lang.psi.impl.expressions._
+
+
+class ScalaWithTryCatchSurrounder extends ScalaWithTrySurrounder {
+  override def getExpressionTemplateAsString (exprAsString : String) = "try { \n " + exprAsString + "\n" + "} catch { \n " + "case a=>b" +  "\n }"
+
+  override def getTemplateDescription = "try / catch"
+
+  override def getSurroundSelectionRange (tryNode : ASTNode ) : TextRange = {
+    val catchBlockPsiElement = tryNode.getPsi.asInstanceOf[ScTryStmtImpl].catchBlock
+
+    val caseClause = catchBlockPsiElement.caseClauses.elements.next
+
+//    val offset = caseClause.getNode.getStartOffset
+    val offset = caseClause.getTextRange.getStartOffset
+    tryNode.removeChild(caseClause.getNode)
+
+    new TextRange(offset, offset)
+  }
+}
