85582305c68b418caf67bfba1724912bdc9fba11
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-26 13:43:08 +0000

    Added most intelligebt keyword completion
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5457 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScTypeImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/types/ScTypeImpl.scala
@@ -7,11 +7,15 @@ import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.psi._
 
 class ScTypeImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
-      override def toString: String = "Function type"
+      override def toString: String = "Functional type"
 }
 
 class ScCompoundTypeImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
       override def toString: String = "Simple type"
+}
+
+class ScInfixTypeImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
+      override def toString: String = "Infix type"
 }
 
 class ScRefineStatImpl( node : ASTNode ) extends ScalaPsiElementImpl(node) {
