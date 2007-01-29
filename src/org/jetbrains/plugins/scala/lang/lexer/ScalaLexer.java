18bcfe6414a572c55a25d7bc23b108ba6087ea61
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-29 15:30:53 +0000

    Added most intelligebt keyword completion
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5503 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/lexer/ScalaLexer.java
+++ src/org/jetbrains/plugins/scala/lang/lexer/ScalaLexer.java
@@ -1,17 +1,17 @@
 package org.jetbrains.plugins.scala.lang.lexer;
 
-import com.intellij.lexer.FlexAdapter;
-
-import java.io.Reader;
+import com.intellij.lexer.MergingLexerAdapter;
 
 /**
- * Author: Ilya Sergey
- * Date: 24.09.2006
- * Time: 16:37:53
+ * Created by IntelliJ IDEA.
+ * User: Ilya.Sergey
+ * Date: 29.01.2007
+ * Time: 18:29:28
+ * To change this template use File | Settings | File Templates.
  */
-public class ScalaLexer extends FlexAdapter {
-  public ScalaLexer() {
+public class ScalaLexer extends MergingLexerAdapter {
 
-    super(new _ScalaLexer((Reader)  null));
+  public ScalaLexer() {
+    super(new ScalaFlexLexer(), ScalaTokenTypes.WHITES_SPACES_TOKEN_SET);
   }
 }
