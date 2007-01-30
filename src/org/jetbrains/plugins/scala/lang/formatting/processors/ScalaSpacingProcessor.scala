f9f28fb950cbc567a970ff8605cefb2427f8a7c5
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-30 14:17:01 +0000

    Added comment folding and advanced formatting. Added some parser features by Scala  2.3.2
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5538 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/formatting/processors/ScalaSpacingProcessor.scala
+++ src/org/jetbrains/plugins/scala/lang/formatting/processors/ScalaSpacingProcessor.scala
@@ -20,28 +20,28 @@ object ScalaSpacingProcessor extends ScalaTokenTypes {
 
 
   val NO_SPACING = Spacing.createSpacing(0, 0, 0, false, 0);
-  val SINGLE_SPACING = Spacing.createSpacing(1, 1 ,0,true, 239);
+  val SINGLE_SPACING = Spacing.createSpacing(1, 1, 0, true, 239);
 
 
 
-  def getSpacing(left: ASTNode, right: ASTNode) : Spacing = {
+  def getSpacing(left: ASTNode, right: ASTNode): Spacing = {
 
 
-    if(right.getElementType.equals(ScalaElementTypes.TYPE_PARAM_CLAUSE) ||
-       right.getElementType.equals(ScalaElementTypes.PARAM_CLAUSE) ||
-       right.getElementType.equals(ScalaElementTypes.PARAM_CLAUSES) ||
-       right.getElementType.equals(ScalaElementTypes.TYPE_ARGS) ||
-       right.getElementType.equals(ScalaElementTypes.ARG_EXPRS))
+    if (right.getElementType.equals(ScalaElementTypes.TYPE_PARAM_CLAUSE) ||
+        right.getElementType.equals(ScalaElementTypes.PARAM_CLAUSE) ||
+        right.getElementType.equals(ScalaElementTypes.PARAM_CLAUSES) ||
+        right.getElementType.equals(ScalaElementTypes.TYPE_ARGS) ||
+        right.getElementType.equals(ScalaElementTypes.ARG_EXPRS))
       return NO_SPACING
 
     if (SpacingTokens.SPACING_BEFORE.contains(right.getElementType))
-      return SINGLE_SPACING          
+      return SINGLE_SPACING
 
     if ((left.getPsi.isInstanceOf[ScInfixExprImpl] &&
         right.getElementType.equals(ScalaTokenTypes.tIDENTIFIER)) ||
         (right.getPsi.isInstanceOf[ScInfixExprImpl] &&
         left.getElementType.equals(ScalaTokenTypes.tIDENTIFIER)))
-      return SINGLE_SPACING        
+      return SINGLE_SPACING
 
     if (SpacingTokens.NO_SPACING_BEFORE.contains(right.getElementType))
       return NO_SPACING
