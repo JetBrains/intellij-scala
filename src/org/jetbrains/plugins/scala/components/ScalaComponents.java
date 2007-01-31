c7cd8360e3b6244f1fb82e547a7dfdaba94ff88b
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-01-31 09:34:02 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5580 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/components/ScalaComponents.java
@@ -0,0 +1,14 @@
+package org.jetbrains.plugins.scala.components;
+
+import org.jetbrains.annotations.NonNls;
+
+/**
+ * @author Ilya.Sergey
+ */
+public interface ScalaComponents {
+
+  @NonNls
+  final String SCALA_FILE_CREATOR = "SCALA_FILE_CREATOR";
+
+
+}
---
+++ src/org/jetbrains/plugins/scala/components/ScalaFileCreator.java
@@ -0,0 +1,50 @@
+package org.jetbrains.plugins.scala.components;
+
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.ide.fileTemplates.impl.FileTemplateImpl;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.scala.ScalaBundle;
+import org.jetbrains.plugins.scala.ScalaFileType;
+
+/**
+ * @author Ilya.Sergey
+ */
+public class ScalaFileCreator implements ApplicationComponent {
+
+
+  public static final String SCALA_FILE_TEMPLATE = ScalaBundle.message("new.scala.template");
+
+  @NonNls
+  @NotNull
+  public String getComponentName() {
+    return ScalaComponents.SCALA_FILE_CREATOR;
+  }
+
+
+  private void registerScalaScriptTemplate() {
+    String templateText = ScalaBundle.message("template.file.text");
+    FileTemplate scalaTemplate = FileTemplateManager.getInstance().addTemplate(SCALA_FILE_TEMPLATE,
+            ScalaFileType.SCALA_FILE_TYPE.getDefaultExtension());
+    ((FileTemplateImpl) scalaTemplate).setInternal(true);
+    scalaTemplate.setText(templateText);
+  }
+
+
+  public void initComponent() {
+    FileTemplateManager fileTemplateManager = FileTemplateManager.getInstance();
+    FileTemplate scalaTemplate = fileTemplateManager.getTemplate(SCALA_FILE_TEMPLATE);
+    if (scalaTemplate == null) {
+      registerScalaScriptTemplate();
+    }
+  }
+
+  public void disposeComponent() {
+    FileTemplate scalaTemplate = FileTemplateManager.getInstance().getTemplate(SCALA_FILE_TEMPLATE);
+    FileTemplateManager.getInstance().removeTemplate(scalaTemplate, false);
+  }
+
+
+}
