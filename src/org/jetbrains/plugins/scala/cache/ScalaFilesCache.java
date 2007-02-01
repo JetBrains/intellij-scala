1231d5480055414a2b8645dc2ce435b50048b430
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-01 11:24:30 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5635 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/cache/ScalaFilesCache.java
@@ -0,0 +1,9 @@
+package org.jetbrains.plugins.scala.cache;
+
+/**
+ * @author Ilya.Sergey
+ */
+public interface ScalaFilesCache {
+
+  public void init (boolean  b);
+}
---
+++ src/org/jetbrains/plugins/scala/cache/module/ScalaModuleCaches.java
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.cache.module;
+
+import org.jetbrains.plugins.scala.cache.ScalaFilesCache;
+
+/**
+ * @author Ilya.Sergey
+ */
+public interface ScalaModuleCaches extends ScalaFilesCache {
+
+
+
+}
