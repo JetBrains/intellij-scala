fcc36a449bb38897ff41ea209a41532ee04045f3
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-01 10:20:21 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5627 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/formatting/patterns/FormattingPatterns.scala
+++ src/org/jetbrains/plugins/scala/lang/formatting/patterns/FormattingPatterns.scala
@@ -22,58 +22,48 @@ package indent {
   */
   trait IfElseIndent
 
-  trait CondIndent
+  /**
+  *   For conditions
+  */
+  trait CondIndent {
+    def condition: ScExprImpl
+  }
+
 
 }
 
 package spacing {
 
+
   import com.intellij.psi.tree.TokenSet
   import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
   import org.jetbrains.plugins.scala.lang.parser._
+  import org.jetbrains.plugins.scala.lang.psi.impl.expressions._
   import org.jetbrains.plugins.scala.highlighter.ScalaSyntaxHighlighter
 
   object SpacingTokens extends ScalaTokenTypes {
 
-    val NO_SPACING_BEFORE = TokenSet.create(Array
-      (
-        ScalaTokenTypes.tDOT,
-        ScalaTokenTypes.tCOMMA,
-        ScalaTokenTypes.tSEMICOLON,
-        ScalaTokenTypes.tCOLON,
-        ScalaTokenTypes.tRPARENTHIS,
-        ScalaTokenTypes.tRSQBRACKET
-      )
-    )
+    val NO_SPACING_BEFORE = TokenSet.create(Array(ScalaTokenTypes.tDOT,
+            ScalaTokenTypes.tCOMMA,
+            ScalaTokenTypes.tSEMICOLON,
+            ScalaTokenTypes.tCOLON,
+            ScalaTokenTypes.tRPARENTHIS,
+            ScalaTokenTypes.tRSQBRACKET))
 
-    val SPACING_AFTER = TokenSet.orSet (
-        Array(
-          TokenSet.create(
-             Array(ScalaTokenTypes.tCOMMA,
-                   ScalaTokenTypes.tCOLON,
-                   ScalaTokenTypes.tSEMICOLON)
-          ),
-          ScalaSyntaxHighlighter.kRESWORDS
-        )
-      )
+    val SPACING_AFTER = TokenSet.orSet(Array(TokenSet.create(Array(ScalaTokenTypes.tCOMMA,
+            ScalaTokenTypes.tCOLON,
+            ScalaTokenTypes.tSEMICOLON)),
+            ScalaSyntaxHighlighter.kRESWORDS))
 
-    val NO_SPACING_AFTER = TokenSet.create(
-        Array(ScalaTokenTypes.tDOT,
-              ScalaTokenTypes.tLPARENTHIS,
-              ScalaTokenTypes.tLSQBRACKET
-        )
-    )
+    val NO_SPACING_AFTER = TokenSet.create(Array(ScalaTokenTypes.tDOT,
+            ScalaTokenTypes.tLPARENTHIS,
+            ScalaTokenTypes.tLSQBRACKET))
 
-    val SINGLE_SPACING_BETWEEN = TokenSet.create(
-        Array(ScalaElementTypes.INFIX_EXPR,
-              ScalaTokenTypes.tIDENTIFIER,
-              ScalaElementTypes.PATTERN3
-        )
-    )
+    val SINGLE_SPACING_BETWEEN = TokenSet.create(Array(ScalaElementTypes.INFIX_EXPR,
+            ScalaTokenTypes.tIDENTIFIER,
+            ScalaElementTypes.PATTERN3))
 
-    val SPACING_BEFORE = TokenSet.create(
-      Array(ScalaTokenTypes.tASSIGN)
-    )
+    val SPACING_BEFORE = TokenSet.create(Array(ScalaTokenTypes.tASSIGN))
 
   }
 
