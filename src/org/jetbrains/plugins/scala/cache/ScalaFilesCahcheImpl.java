ef7ac5edf04758367855eafb1b5c1ade47845875
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-01 11:28:54 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5637 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/cache/ScalaFilesCahcheImpl.java
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.cache;
+
+/**
+ * @author Ilya.Sergey
+ */
+public class ScalaFilesCahcheImpl implements ScalaFilesCache{
+  
+  public void init(boolean b) {
+    
+    refreshCache(true);
+
+  }
+
+  private void refreshCache(boolean b) {
+  }
+}
