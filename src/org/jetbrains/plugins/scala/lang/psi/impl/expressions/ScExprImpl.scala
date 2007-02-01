0ace917dffcdee5a1cfee44f3a7a99f8c2145d8f
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-01 10:24:23 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5630 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/expressions/ScExprImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/expressions/ScExprImpl.scala
@@ -24,8 +24,3 @@ trait ScResExpr extends ScBlock
 
 trait ScBlock extends PsiElement 
 
-//trait ScPostfixExprImpl extends ScExpr1Impl
-
-//trait ScPrefixExprImpl extends ScInfixExprImpl
-
-//trait ScSimpleExprImpl extends ScPrefixExprImpl
