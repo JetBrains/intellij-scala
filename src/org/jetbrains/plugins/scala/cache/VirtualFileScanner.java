af9e105761d3689d430fe8ea77c09ef0f2290055
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-02-04 11:02:15 +0000

    add file ignored check
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5698 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/cache/VirtualFileScanner.java
+++ src/org/jetbrains/plugins/scala/cache/VirtualFileScanner.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.scala.util.ScalaUtils;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.roots.ProjectFileIndex;
 
 import java.util.Set;
 import java.util.HashSet;
@@ -17,12 +18,13 @@ public class VirtualFileScanner {
    * Scans recursively for scala source files
    *
    * @param virtualFile the root file
+   * @param index
    * @return List of files
    */
   @NotNull
-  public static Set<VirtualFile> scan(final VirtualFile virtualFile) {
+  public static Set<VirtualFile> scan(final VirtualFile virtualFile, ProjectFileIndex index) {
     final Set<VirtualFile> myFiles = new HashSet<VirtualFile>();
-    addScalaFiles(virtualFile, myFiles);
+    addScalaFiles(virtualFile, myFiles, index);
     return myFiles;
   }
 
@@ -31,9 +33,10 @@ public class VirtualFileScanner {
    *
    * @param file     File or directory
    * @param allFiles Method adds all found files into this set
+   * @param index file index to check file ignored
    */
-  public static void addScalaFiles(@Nullable final VirtualFile file, @NotNull final Set<VirtualFile> allFiles) {
-    if (file == null || ScalaUtils.isVersionControlSysDir(file)) {
+  public static void addScalaFiles(@Nullable final VirtualFile file, @NotNull final Set<VirtualFile> allFiles, ProjectFileIndex index) {
+    if (file == null || ScalaUtils.isVersionControlSysDir(file) || index.isIgnored(file)) {
       return;
     }
     if (!file.isDirectory()) {
@@ -44,7 +47,7 @@ public class VirtualFileScanner {
     }
     final VirtualFile[] children = file.getChildren();
     for (final VirtualFile child : children) {
-      addScalaFiles(child, allFiles);
+      addScalaFiles(child, allFiles, index);
     }
   }
 }
