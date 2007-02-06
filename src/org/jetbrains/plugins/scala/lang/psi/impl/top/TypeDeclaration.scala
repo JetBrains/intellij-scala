51389a72b7b5affd0a0e77647acfde25134c4296
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-06 18:13:35 +0000

    Rejected commit: Default
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5778 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/top/TypeDeclaration.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/top/TypeDeclaration.scala
@@ -6,13 +6,7 @@ import com.intellij.lang.ASTNode
  * Date: 03.10.2006
  * Time: 19:43:50
  */
-
-  case class ScTmplDefIMpl ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
-    //todo
-    override def toString: String = ""
-  }
-
-  abstract class ScTypeDefImpl ( node : ASTNode ) extends ScalaPsiElementImpl ( node ) {
-    def getDefStr : String = getParent.getFirstChild.getText
-    override def toString : String = getDefStr + " :" + getFirstChild
-  }    
+abstract class ScTypeDefImpl(node: ASTNode) extends ScalaPsiElementImpl (node) {
+  def getDefStr: String = getParent.getFirstChild.getText
+  override def toString: String = getDefStr + " :" + getFirstChild
+}    
