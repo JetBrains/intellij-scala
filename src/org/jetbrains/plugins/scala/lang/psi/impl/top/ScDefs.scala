9d279053408f2ede9e292c200965155dbbf2c24f
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-07 07:54:35 +0000

    Rejected commit: Default
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5788 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/top/ScDefs.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/top/ScDefs.scala
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.scala.lang.psi.impl.top.defs {
+package org.jetbrains.plugins.scala.lang.psi.impl.top.defs
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement;
@@ -13,119 +13,120 @@ import org.jetbrains.annotations._
 import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.formatting.patterns.indent._
 import org.jetbrains.plugins.scala.lang.psi.impl.top.templateStatements.ScTemplateStatement
+import org.jetbrains.plugins.scala.lang.psi.impl.top.templates.ScTopDefTemplate
 
 
 /**
- *  Main class, that describes behaviour of scala templates, such ass class, object and trait
- *  @see ScJavaClass
- */
+*  Main class, that describes behaviour of scala templates, such ass class, object and trait
+*  @see ScJavaClass
+*/
 
-  abstract class ScTmplDef( node : ASTNode ) extends ScalaPsiElementImpl ( node ) with TemplateIndent{
-    override def toString: String = "template definition"
+abstract class ScTmplDef(node: ASTNode) extends ScalaPsiElementImpl (node) with TemplateIndent{
 
-    def nameNode = {
-      def isName = (elementType : IElementType) => (elementType == ScalaTokenTypes.tIDENTIFIER)
-      childSatisfyPredicateForElementType(isName)
+  override def toString: String = "template definition"
+
+  def nameNode = {
+    def isName = (elementType: IElementType) => (elementType == ScalaTokenTypes.tIDENTIFIER)
+    childSatisfyPredicateForElementType(isName)
+  }
+
+  /**
+  *  Return definitions of inner templates
+  *
+  */
+  def getTmplDefs: Iterable[ScTmplDef] = {
+
+    import org.jetbrains.plugins.scala.lang.psi.impl.top.templates.ScTemplateBody
+
+    val template = getTemplate
+    var body: ScTemplateBody = null
+    if (template != null) {
+      body = template.getTemplateBody
+      if (body != null) return body.childrenOfType[ScTmplDef](ScalaElementTypes.TMPL_DEF_BIT_SET)
     }
+    null
+  }
 
-    /**
-    *  Return definitions of inner templates
-    *
-    */
-    def getTmplDefs : Iterable[ScTmplDef] = {
+  override def getName = nameNode.getText
 
-      import org.jetbrains.plugins.scala.lang.psi.impl.top.templates.ScTemplateBody
+  override def getTextOffset = nameNode.getTextRange.getStartOffset
 
-      val template = getTemplate
-      var body : ScTemplateBody = null
-      if ( template != null) {
-        body = template.getTemplateBody
-        if (body != null ) return body.childrenOfType[ScTmplDef] (ScalaElementTypes.TMPL_DEF_BIT_SET)
-      }
-      null
-    }
+  def isTypeDef: boolean = { this.isInstanceOf[ScTypeDef] }
 
-    override def getName = nameNode.getText
-
-    override def getTextOffset = nameNode.getTextRange.getStartOffset
-
-    def isTypeDef : boolean = { this.isInstanceOf[ScTypeDef] }
-
-    /**
-    * Returns qualified name of current template definition
-    *
-    */
-    def getQualifiedName : String = {
-      def append (s1 : String, s2 : String) = {if (s1 == "")  s2 else s1 + "." + s2}
-      def inner (e : PsiElement) : String = {
-        val parent = e.getParent
-        parent match {
-          case pack: ScPackaging => append(inner(parent), pack.asInstanceOf[ScPackaging].getFullPackageName)
-          case tmplDef: ScTmplDef => tmplDef.asInstanceOf[ScTmplDef].getQualifiedName
-          case f: ScalaFile => {
-            val packageStatement = f.getChild(ScalaElementTypes.PACKAGE_STMT).asInstanceOf[ScPackageStatement]
-            if (packageStatement == null) "" else {
-              val packageName = packageStatement.getFullPackageName
-              if (packageName == null) "" else packageName
-            }
+  /**
+  * Returns qualified name of current template definition
+  *
+  */
+  def getQualifiedName: String = {
+    def append(s1: String, s2: String) = {if (s1 == "")  s2 else s1 + "." + s2}
+    def inner(e: PsiElement): String = {
+      val parent = e.getParent
+      parent match {
+        case pack: ScPackaging => append(inner(parent), pack.asInstanceOf[ScPackaging].getFullPackageName)
+        case tmplDef: ScTmplDef => tmplDef.asInstanceOf[ScTmplDef].getQualifiedName
+        case f: ScalaFile => {
+          val packageStatement = f.getChild(ScalaElementTypes.PACKAGE_STMT).asInstanceOf[ScPackageStatement]
+          if (packageStatement == null) "" else {
+            val packageName = packageStatement.getFullPackageName
+            if (packageName == null) "" else packageName
           }
         }
       }
-
-      append (inner (this), getName)
     }
 
-    import org.jetbrains.plugins.scala.lang.psi.impl.top.templates.ScTopDefTemplate
-
-    def getTemplate : ScTopDefTemplate = getChild(ScalaElementTypes.TOP_DEF_TEMPLATE).asInstanceOf[ScTopDefTemplate]
-
-    [Nullable]
-    def getTemplateStatements : Iterable[ScTemplateStatement] = {
-      import org.jetbrains.plugins.scala.lang.psi.impl.top.templates.ScTemplateBody
-
-      val template = getTemplate
-      var body : ScTemplateBody = null
-      if ( template != null) {
-        body = template.getTemplateBody
-        if (body != null ) return body.childrenOfType[ScTemplateStatement](ScalaElementTypes.TMPL_STMT_BIT_SET)
-      }
-      null
-    }
+    append(inner(this), getName)
   }
+
+
+
+  def getTemplate: ScTopDefTemplate = getChild(ScalaElementTypes.TOP_DEF_TEMPLATE).asInstanceOf[ScTopDefTemplate]
+
+  [Nullable]
+  def getTemplateStatements: Iterable[ScTemplateStatement] = {
+    import org.jetbrains.plugins.scala.lang.psi.impl.top.templates.ScTemplateBody
+
+    val template = getTemplate
+    var body: ScTemplateBody = null
+    if (template != null) {
+      body = template.getTemplateBody
+      if (body != null) return body.childrenOfType[ScTemplateStatement](ScalaElementTypes.TMPL_STMT_BIT_SET)
+    }
+    null
+  }
+}
 
 /**********************************************************************************************************************/
 /******************************************* Concrete templates *******************************************************/
 /**********************************************************************************************************************/
 
-  /*
-  *   Class definition implementation
-  */
-  case class ScClassDefinition( node : ASTNode ) extends ScTypeDef (node){
-    override def toString: String = super.toString + ": " + "class"
+/*
+*   Class definition implementation
+*/
+case class ScClassDefinition(node: ASTNode) extends ScTypeDef (node){
+  override def toString: String = super.toString + ": " + "class"
 
+}
+
+/*
+*   Object definition implementation
+*/
+case class ScObjectDefinition(node: ASTNode) extends ScTmplDef (node){
+  override def toString: String = super.toString + ": " + "object"
+
+}
+
+/*
+*   Trait definition implementation
+*/
+trait ScTypeDef extends ScTmplDef  with IfElseIndent{
+  def getTypeParameterClause: ScTypeParamClause = {
+    getChild(ScalaElementTypes.TYPE_PARAM_CLAUSE).asInstanceOf[ScTypeParamClause]
   }
+}
 
-  /*
-  *   Object definition implementation
-  */
-  case class ScObjectDefinition( node : ASTNode ) extends ScTmplDef ( node ){
-    override def toString: String = super.toString + ": " + "object"
-
-  }
-
-  /*
-  *   Trait definition implementation
-  */
-  trait ScTypeDef extends ScTmplDef  with IfElseIndent{
-    def getTypeParameterClause : ScTypeParamClause = {
-      getChild(ScalaElementTypes.TYPE_PARAM_CLAUSE).asInstanceOf[ScTypeParamClause]
-    }
-  }
-
-  /*
-  *   Trait definition implementation
-  */
-  case class ScTraitDefinition( node : ASTNode ) extends ScTypeDef (node) {
-    override def toString: String = super.toString + ": " + "trait"
-  }
+/*
+*   Trait definition implementation
+*/
+case class ScTraitDefinition(node: ASTNode) extends ScTypeDef (node) {
+  override def toString: String = super.toString + ": " + "trait"
 }
