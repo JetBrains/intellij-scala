3139a9a2e19be16a947abd30dbd008247d2d370b
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-02-09 13:20:32 +0000

    adding icons
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5845 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/icons/Icons.scala
+++ src/org/jetbrains/plugins/scala/icons/Icons.scala
@@ -3,7 +3,7 @@ package org.jetbrains.plugins.scala.icons
 import com.intellij.openapi.util.IconLoader
 
 object Icons {
-  val CLASS = IconLoader.getIcon("nodes/class.png")
-  val TRAIT = IconLoader.getIcon("nodes/trait.png")
-  val OBJECT = IconLoader.getIcon("nodes/object.png")
+  val CLASS = IconLoader.getIcon("org/jetbrains/plugins/scala/nodes/class.png")
+  val TRAIT = IconLoader.getIcon("org/jetbrains/plugins/scala/nodes/nodes/trait.png")
+  val OBJECT = IconLoader.getIcon("org/jetbrains/plugins/scala/nodes/nodes/object.png")
 }
