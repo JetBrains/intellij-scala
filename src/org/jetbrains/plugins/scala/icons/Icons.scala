1b0934e5ddc9b3920bd3d10f268ff9aaa73dee20
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-02-09 13:11:16 +0000

    added icons
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5844 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/icons/Icons.scala
@@ -0,0 +1,9 @@
+package org.jetbrains.plugins.scala.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+  val CLASS = IconLoader.getIcon("nodes/class.png")
+  val TRAIT = IconLoader.getIcon("nodes/trait.png")
+  val OBJECT = IconLoader.getIcon("nodes/object.png")
+}
