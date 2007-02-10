e188dd332ab2a8b1844db5a89c7f3a054b30b660
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-10 11:40:54 +0000

    Added SDK caches suuport
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5862 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/ScalaIcons.java
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author Ilya.Sergey
+ */
+public interface ScalaIcons {
+
+   public static final Icon SCALA_CLASS = IconLoader.getIcon("/org/jetbrains/plugins/scala/nodes/class.png");
+   public static final Icon SCALA_TRAIT = IconLoader.getIcon("/org/jetbrains/plugins/scala/nodes/object.png");
+   public static final Icon SCALA_OBJECT = IconLoader.getIcon("/org/jetbrains/plugins/scala/nodes/trait.png");
+
+}
