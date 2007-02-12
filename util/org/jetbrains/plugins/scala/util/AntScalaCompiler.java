91f598b9dc732a73bfa8060cd5283de81ca1342d
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-12 18:13:17 +0000

    Added VFS listener
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5920 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ util/org/jetbrains/plugins/scala/util/AntScalaCompiler.java
@@ -0,0 +1,17 @@
+package org.jetbrains.plugins.scala.util;
+
+import scala.tools.ant.Scalac;
+
+/**
+ * Author: Ilya Sergey
+ * Date: 04.10.2006
+ * Time: 16:47:02
+ */
+public class AntScalaCompiler extends Scalac {
+
+  public void execute() {
+    setAddparams("-Xgenerics");
+    //setAddparams("-target:jvm-1.5");
+    super.execute();
+  }
+}
