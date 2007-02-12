7d0612a8948ad25f28932ba80804dd04cc88fe83
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-02-12 16:12:10 +0000

    Added VFS listener
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@5915 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/cache/ScalaLibraryCachesManager.java
+++ src/org/jetbrains/plugins/scala/cache/ScalaLibraryCachesManager.java
@@ -170,7 +170,8 @@ public class ScalaLibraryCachesManager implements ProjectComponent {
    */
   @NotNull
   private String generateCacheFilePath(@NotNull final Library library, @NotNull final String name) {
-    return PathManager.getSystemPath() + "/" + SCALA_CACHE_DIR + "/" + SCALA_CACHE_FILE + "/" + name + "_" + library.hashCode();
+    return PathManager.getSystemPath() + "/" + SCALA_CACHE_DIR + "/" + SCALA_CACHE_FILE + "/" + name + "_"
+            + library.getTable().hashCode();
   }
 
 
