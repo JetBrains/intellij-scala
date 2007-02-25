b83c355ae1962469de8bf319a331f93b3efd64f8
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-02-25 10:13:54 +0000

    cleanup old code
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@6258 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/ScalaLoader.java
+++ src/org/jetbrains/plugins/scala/ScalaLoader.java
@@ -5,31 +5,20 @@ import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerAdapter;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.scala.actions.ScalaSdkChooser;
 import org.jetbrains.plugins.scala.compiler.ScalaCompiler;
 import org.jetbrains.plugins.scala.util.ScalaToolsFactory;
-
-import javax.swing.*;
 
 /**
  * Author: Ilya Sergey
  * Date: 20.09.2006
  * Time: 16:31:20
  */
-public class ScalaLoader implements ApplicationComponent, Configurable {
-  private ScalaSdkChooser sdkChooserDialog;
-
-  public ScalaLoader() {
-  }
+public class ScalaLoader implements ApplicationComponent {
+  public ScalaLoader() {}
 
   public void initComponent() {
     loadScala();
@@ -45,11 +34,6 @@ public class ScalaLoader implements ApplicationComponent, Configurable {
     );
 
 
-/*
-    System.out.println("STF="+ScalaToolsFactory.getInstance());
-    assert (ScalaToolsFactory.getInstance() != null);
-*/
-
     CompletionUtil.registerCompletionData(ScalaFileType.SCALA_FILE_TYPE,
             ScalaToolsFactory.getInstance().createScalaCompletionData());
 
@@ -64,47 +48,10 @@ public class ScalaLoader implements ApplicationComponent, Configurable {
 
   }
 
-  public void disposeComponent() {
-  }
+  public void disposeComponent() {}
 
   @NotNull
   public String getComponentName() {
     return "Scala Loader";
-  }
-
-  @Nls
-  public String getDisplayName() {
-    return "Scala options";
-  }
-
-  public Icon getIcon() {
-    return null;
-  }
-
-  @Nullable
-  @NonNls
-  public String getHelpTopic() {
-    return null;
-  }
-
-  public JComponent createComponent() {
-    sdkChooserDialog = new ScalaSdkChooser();
-
-    return sdkChooserDialog;
-  }
-
-  public boolean isModified() {
-    System.out.println("modify: " + sdkChooserDialog.getSdkGlobalPath());
-    return sdkChooserDialog.getSdkGlobalPath() != null && !sdkChooserDialog.getSdkGlobalPath().equals("");
-  }
-
-  public void apply() throws ConfigurationException {
-    System.out.println("apply: " + sdkChooserDialog.getSdkGlobalPath());
-  }
-
-  public void reset() {
-  }
-
-  public void disposeUIResources() {
   }
 }
