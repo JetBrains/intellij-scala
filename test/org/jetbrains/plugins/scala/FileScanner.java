f959d193c494a82fa9a05700444453b07b4cc8df
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-05-22 18:13:40 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@8122 59f64563-22e8-0310-8d2c-98a15e1539f2

--- test/org/jetbrains/plugins/scala/FileScanner.java
+++ test/org/jetbrains/plugins/scala/FileScanner.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2006 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.plugins.scala;
 
 import java.io.File;
@@ -32,13 +48,13 @@ public class FileScanner {
         if (!file.isDirectory()) {
           String path = file.getAbsolutePath();
           if (!path.contains(".svn") &&
-              !path.contains(".cvs") &&
-              !path.contains("ScalaBazaar") &&
-              !path.contains("DocUtil") &&
-              !path.contains("DocGenerator") &&
-              !path.contains("Logged") &&
-              !path.contains("EmitHtml") &&
-               path.matches(pattern)) {
+            !path.contains(".cvs") &&
+            !path.contains("ScalaBazaar") &&
+            !path.contains("DocUtil") &&
+            !path.contains("DocGenerator") &&
+            !path.contains("Logged") &&
+            !path.contains("EmitHtml") &&
+            path.matches(pattern)) {
             files.add(file);
           }
         } else {
