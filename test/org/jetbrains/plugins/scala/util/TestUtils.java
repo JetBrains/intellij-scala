6d4e9380417b9d974d40f26f7b7ba4f141fb12af
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-05-22 18:16:56 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@8128 59f64563-22e8-0310-8d2c-98a15e1539f2

--- test/org/jetbrains/plugins/scala/util/TestUtils.java
+++ test/org/jetbrains/plugins/scala/util/TestUtils.java
@@ -1,24 +1,77 @@
+/*
+ * Copyright 2000-2006 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.plugins.scala.util;
 
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.LocalTimeCounter;
+
+import java.net.URL;
+import java.net.URISyntaxException;
+import java.io.File;
 
 /**
  * Created by IntelliJ IDEA.
  * User: Ilya.Sergey
  */
 public class TestUtils {
+  private static final Logger LOG = Logger.getInstance("org.jetbrains.plugins.scala.util.TestUtils");
 
-  protected final static String TEMP_FILE = "temp.scala";
+  public static PsiFile createPseudoPhysicalFile(final Project project, final String text, int i) throws IncorrectOperationException {
+    String TEMP_FILE = project.getProjectFilePath() + "temp" + i + ".scala";
+    return PsiManager.getInstance(project).getElementFactory().createFileFromText(
+        TEMP_FILE,
+        FileTypeManager.getInstance().getFileTypeByFileName(TEMP_FILE),
+        text,
+        LocalTimeCounter.currentTime(),
+        true);
+  }
+
 
   public static PsiFile createPseudoPhysicalFile(final Project project, final String text) throws IncorrectOperationException {
-      return PsiManager.getInstance(project).getElementFactory().createFileFromText(TEMP_FILE, FileTypeManager.getInstance().getFileTypeByFileName(TEMP_FILE),
-              text, LocalTimeCounter.currentTime(), true);
+    String TEMP_FILE = project.getProjectFilePath() + "temp.scala";
+    return PsiManager.getInstance(project).getElementFactory().createFileFromText(
+        TEMP_FILE,
+        FileTypeManager.getInstance().getFileTypeByFileName(TEMP_FILE),
+        text,
+        LocalTimeCounter.currentTime(),
+        true);
   }
-  
+
+  private static String TEST_DATA_PATH = null;
+
+  public static String getTestDataPath() {
+    if (TEST_DATA_PATH == null) {
+      ClassLoader loader = TestUtils.class.getClassLoader();
+      URL resource = loader.getResource("testdata");
+      try {
+        TEST_DATA_PATH = new File(resource.toURI()).getPath().replace(File.separatorChar, '/');
+      } catch (URISyntaxException e) {
+        LOG.error(e);
+        return null;
+      }
+    }
+
+    return TEST_DATA_PATH;
+  }
+
 
 }
