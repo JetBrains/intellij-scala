5f5a640627a8fc760fec024bb3f67482b6d6a079
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-01-16 12:10:00 +0000

    tests for lexer added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@13757 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/lang/lexer/LexerTest.java
@@ -0,0 +1,54 @@
+package org.jetbrains.plugins.scala.lang.lexer;
+
+import com.intellij.psi.tree.IElementType;
+import junit.framework.Test;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.plugins.scala.testcases.BaseScalaFileSetTestCase;
+
+/**
+ * @author ilyas
+ */
+public class LexerTest extends BaseScalaFileSetTestCase {
+
+  @NonNls
+  private static final String DATA_PATH = "test/org/jetbrains/plugins/scala/lang/lexer/data";
+
+  public LexerTest() {
+    super(System.getProperty("path") != null ?
+        System.getProperty("path") :
+        DATA_PATH
+    );
+  }
+
+
+  public String transform(String testName, String[] data) throws Exception {
+    String fileText = data[0];
+
+    ScalaLexer lexer = new ScalaLexer();
+    lexer.start(fileText.toCharArray());
+
+    StringBuffer buffer = new StringBuffer();
+
+    IElementType type;
+    while ((type = lexer.getTokenType()) != null) {
+      buffer.append(type.toString());
+      lexer.advance();
+      if (lexer.getTokenType() != null) {
+        buffer.append("\n");
+      }
+    }
+
+    System.out.println("------------------------ " + testName + " ------------------------");
+    System.out.println(buffer.toString());
+    System.out.println("");
+
+    return buffer.toString();
+
+  }
+
+  public static Test suite() {
+    return new LexerTest();
+  }
+
+
+}
