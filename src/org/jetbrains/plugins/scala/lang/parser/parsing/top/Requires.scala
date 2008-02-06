7de5aeee61db03fe5149cc6f9bc37aca59daa2e5
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-02-06 15:52:09 +0000

    little changes with MixinParents
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@14212 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/top/Requires.scala
@@ -0,0 +1,55 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing.top
+
+import com.intellij.lang.PsiBuilder
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.IChameleonElementType
+import com.intellij.psi.tree.TokenSet
+
+import org.jetbrains.plugins.scala.util.DebugPrint
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
+import org.jetbrains.plugins.scala.lang.parser.parsing.types.Type
+import org.jetbrains.plugins.scala.lang.parser.util.ParserUtils
+import org.jetbrains.plugins.scala.lang.parser.parsing.types.SimpleType
+import org.jetbrains.plugins.scala.lang.parser.bnf.BNF
+import org.jetbrains.plugins.scala.lang.parser.parsing.top.template.TemplateBody
+import org.jetbrains.plugins.scala.lang.parser.parsing.top.template.TemplateParents
+import org.jetbrains.plugins.scala.lang.parser.parsing.top.params.VariantTypeParam
+import org.jetbrains.plugins.scala.lang.parser.parsing.top.params.TypeParamClause
+import org.jetbrains.plugins.scala.lang.parser.parsing.top.params.Param
+import org.jetbrains.plugins.scala.lang.parser.parsing.top.params.ParamClauses
+import org.jetbrains.plugins.scala.lang.parser.parsing.base.ModifierWithoutImplicit
+import org.jetbrains.plugins.scala.ScalaBundle
+
+/** 
+* Created by IntelliJ IDEA.
+* User: Alexander.Podkhalyuz
+* Date: 06.02.2008
+* Time: 17:52:50
+* To change this template use File | Settings | File Templates.
+*/
+
+object Requires {
+  def parse(builder: PsiBuilder): Boolean = {
+    val requiresMarker = builder.mark
+    builder.getTokenType match {
+      case ScalaTokenTypes.kREQUIRES => {
+        builder.advanceLexer //Ate requires
+        //parsing simple type
+        if (BNF.firstSimpleType.contains(builder.getTokenType)) {
+          SimpleType parse builder
+        }
+        else {
+          builder error ScalaBundle.message("simple.type.expected.requires", new Array[Object](0))
+        }
+        requiresMarker.done(ScalaElementTypes.REQUIRES_BLOCK)
+        return true
+      }
+      case _ => {
+        requiresMarker.drop
+        return false
+      }
+    }
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/types/AnnotType.scala
@@ -0,0 +1,40 @@
+package org.jetbrains.plugins.scala.lang.parser.parsing.types
+
+import com.intellij.lang.PsiBuilder, org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import org.jetbrains.plugins.scala.lang.parser.bnf.BNF
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.plugins.scala.lang.parser.util.ParserUtils
+import org.jetbrains.plugins.scala.lang.lexer.ScalaElementType
+import org.jetbrains.plugins.scala.lang.parser.parsing.expressions.InfixTemplate
+import org.jetbrains.plugins.scala.ScalaBundle
+
+/** 
+* Created by IntelliJ IDEA.
+* User: Alexander.Podkhalyuz
+* Date: 06.02.2008
+* Time: 18:36:27
+* To change this template use File | Settings | File Templates.
+*/
+
+/*
+ * AnnotType ::= {Annotation} SimpleType
+ */
+
+object AnnotType {
+  def parse(builder: PsiBuilder): Boolean = {
+    val annotMarker = builder.mark
+    //TODO: parse annotations
+    //parse Simple type
+    if (BNF.firstSimpleType.contains(builder.getTokenType)){
+      SimpleType parse builder
+      annotMarker.done(ScalaElementTypes.ANNOT_TYPE)
+      return true
+    }
+    else {
+      annotMarker.rollbackTo
+      return false
+    }
+  }
+}
