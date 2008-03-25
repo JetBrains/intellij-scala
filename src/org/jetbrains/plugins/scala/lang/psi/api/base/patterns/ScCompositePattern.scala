7a4f948076c0157ca16f56d3e76ca8625b87503b
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-03-25 16:24:04 +0000

    project separated on two modules
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@15221 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/api/base/patterns/ScCompositePattern.scala
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.plugins.scala.lang.psi.api.base.patterns;
-/**
-* @author ilyas 
-*/
-
-trait ScCompositePattern extends ScPattern {
-
-}
--- src/org/jetbrains/plugins/scala/lang/psi/api/expr/ScConstrBlock.scala
+++
@@ -1,9 +0,0 @@
- package org.jetbrains.plugins.scala.lang.psi.api.expr
-
- /**
-* @author Alexander.Podkhalyuzin 
-*/
-
-trait ScConstrBlock extends ScBlockExpr{
-
-}
