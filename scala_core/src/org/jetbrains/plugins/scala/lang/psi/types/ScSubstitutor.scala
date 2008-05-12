eeb140643cf26169ef2151b78229221e9c273b91
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-12 13:05:22 +0000

    Add initial substitutor support
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16281 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSubstitutor.scala
@@ -0,0 +1,32 @@
+/**
+* @author ven
+*/
+package org.jetbrains.plugins.scala.lang.psi.types
+
+import _root_.scala.collection.immutable.{EmptyMap, HashMap}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScTypeParam
+
+class ScSubstitutor {
+  private var map : Map[ScTypeParam, ScType] = Map.empty[ScTypeParam, ScType]
+  
+  def this(_map :  Map[ScTypeParam, ScType]) = {
+    this()
+    map = _map
+  }
+
+  def put(p : ScTypeParam, t : ScType) = new ScSubstitutor(map + ((p, t)))
+
+  def subst(p : ScTypeParam) = {
+    map.get(p) match {
+      case None => null //todo return type of type parameter itself
+      case Some(v) => v
+    }
+  }
+
+  def subst (t : ScType) : ScType = {
+    t match {
+      case ScFunctionType(ret, params) => new ScFunctionType(subst(ret), params map ((p : Tuple2[ScType, Boolean]) => (subst(p._1), p._2)))
+      case _ => t //todo
+    }
+  }
+}
