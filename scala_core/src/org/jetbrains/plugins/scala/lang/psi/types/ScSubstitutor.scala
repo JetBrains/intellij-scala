c27cd1b34290bca6df1ac25b0d838ba6f6ca6233
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-13 09:41:50 +0000

    advance types/name binding
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16305 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSubstitutor.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSubstitutor.scala
@@ -9,12 +9,10 @@ object ScSubstitutor {
   val empty = new ScSubstitutor
 }
 
-class ScSubstitutor {
-  private var map : Map[ScTypeParam, ScType] = Map.empty[ScTypeParam, ScType]
+class ScSubstitutor(val map : Map[ScTypeParam, ScType]) {
 
-  private def this(_map :  Map[ScTypeParam, ScType]) = {
-    this()
-    map = _map
+  def this() = {
+    this(Map.empty[ScTypeParam, ScType])
   }
 
   def put(p : ScTypeParam, t : ScType) = new ScSubstitutor(map + ((p, t)))
@@ -29,6 +27,13 @@ class ScSubstitutor {
   def subst (t : ScType) : ScType = {
     t match {
       case ScFunctionType(ret, params) => new ScFunctionType(subst(ret), params map ((p : Tuple2[ScType, Boolean]) => (subst(p._1), p._2)))
+      case ScParameterizedType (td, s) => td match {
+        case tp : ScTypeParam => subst(tp)
+        case _ => {
+          val newMap = map transform ((tp : ScTypeParam, t : ScType) => subst(t))
+          new ScParameterizedType(td, new ScSubstitutor(newMap))
+        }
+      }
       case _ => t //todo
     }
   }
