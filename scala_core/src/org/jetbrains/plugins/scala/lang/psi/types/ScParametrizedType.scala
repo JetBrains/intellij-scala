16c68bc83307e81e03b67a571773180f381064d7
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-14 10:50:26 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16365 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScParametrizedType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScParametrizedType.scala
@@ -8,8 +8,10 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
 import com.intellij.psi.PsiNamedElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypeParametersOwner
 
+//for packages
 case class ScDesignatorType (element : PsiNamedElement) extends ScType
 
+//for classes and methods
 case class ScParameterizedType(typeDef : ScTypeParametersOwner, subst : ScSubstitutor) extends ScType {
 
 }
