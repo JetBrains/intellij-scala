182e5306f233a56459fe2b6d66f46d0cb069da7f
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-14 11:32:59 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16367 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScParametrizedType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScParametrizedType.scala
@@ -9,9 +9,26 @@ import com.intellij.psi.PsiNamedElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypeParametersOwner
 
 //for packages
-case class ScDesignatorType (element : PsiNamedElement) extends ScType
+case class ScDesignatorType (element : PsiNamedElement) extends ScType {
+  override def equiv(t : ScType) = t match {
+    case ScDesignatorType(element1) => element eq element1
+    case _ => false
+  }
+}
 
-//for classes and methods
-case class ScParameterizedType(typeDef : ScTypeParametersOwner, subst : ScSubstitutor) extends ScType {
-
+//for classes
+case class ScParameterizedType(owner : ScTypeDefinition, subst : ScSubstitutor) extends ScType {
+  override def equiv(t : ScType) : boolean = t match {
+    case ScParameterizedType(owner1, subst1) => {
+      if (!(owner eq owner1)) false
+      else {
+        val params = owner.typeParameters
+        for (param <- params) {
+          if (!subst.subst(param).equiv(subst1.subst(param))) return false
+        }
+        true
+      }
+    }
+    case _ => false
+  }
 }
