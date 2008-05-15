7709320a1fd11148bbbd2b898449dc51de602e73
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-05-15 15:43:58 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16393 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/components/ScalaIconProvider.scala
+++ scala_core/src/org/jetbrains/plugins/scala/components/ScalaIconProvider.scala
@@ -17,9 +17,9 @@ class ScalaIconProvider extends IconProvider {
       for (val clazz <- defs) {
         if (name.equals(clazz.getName)) return clazz.getIcon(flags)
       }
-      return defs.firstOption match {
-        case None => null
-        case Some(clazz) => clazz.getIcon(flags)
+      return defs match {
+        case Nil => file.icon
+        case clazz::_ => clazz.getIcon(flags)
       }
     }
     null
