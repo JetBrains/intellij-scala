bc5b7eb77cf06964823f284895e9dfb8976a5d9c
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-05-22 14:48:13 +0000

    resolve in closures added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16544 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScFunctionExprImpl.scala
@@ -0,0 +1,52 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.expr
+
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
+import com.intellij.psi.tree.TokenSet
+import com.intellij.lang.ASTNode
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi._
+import com.intellij.psi.scope._
+import org.jetbrains.annotations._
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.icons.Icons
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameters
+import org.jetbrains.plugins.scala.lang.psi.api.expr._
+
+/**
+* @author Alexander Podkhalyuzin
+* Date: 06.03.2008
+*/
+
+class ScFunctionExprImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScFunctionExpr {
+
+  override def toString: String = "FunctionExpression"
+
+  def getParameters = params.params
+
+  def params = findChildByClass(classOf[ScParameters])
+
+  def result = findChildByClass(classOf[ScExpression]) match {
+    case null => None
+    case x => Some(x)
+  }
+
+  override def processDeclarations(processor: PsiScopeProcessor,
+                                  state: ResolveState,
+                                  lastParent: PsiElement,
+                                  place: PsiElement): Boolean = {
+    import org.jetbrains.plugins.scala.lang.resolve._
+
+    result match {
+      case Some(x) if x == lastParent => {
+        for (p <- getParameters) {
+          if (!processor.execute(p, state)) return false
+        }
+        true
+      }
+      case _ => true
+    }
+  }
+
+}
