b6128152d7ec1a08fd96d7c32010ecb47d808ab7
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-24 19:17:22 +0000

    on a way to processing imports
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16590 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/resolve/ResolveTargets.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/resolve/ResolveTargets.scala
@@ -5,6 +5,8 @@ case class ResolveTargets(n: Int)
 object ResolveTargets {
   val METHOD = new ResolveTargets(0)
   val VAR = new ResolveTargets(1)
-  val OBJECT = new ResolveTargets(2)
-  val CLASS = new ResolveTargets(3)
+  val VAL = new ResolveTargets(2)
+  val OBJECT = new ResolveTargets(3)
+  val CLASS = new ResolveTargets(4)
+  val PACKAGE = new ResolveTargets(5)
 }
