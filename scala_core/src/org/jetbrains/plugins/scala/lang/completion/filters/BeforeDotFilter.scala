8b1b04971644b955cc5a91c67891598ce1bbb4e3
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-24 20:49:57 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16593 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/completion/filters/BeforeDotFilter.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/completion/filters/BeforeDotFilter.scala
@@ -1,15 +1,8 @@
 package org.jetbrains.plugins.scala.lang.completion.filters
 
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
-import com.intellij.psi.filters.TrueFilter;
-import com.intellij.psi.filters.TextFilter;
-import com.intellij.psi.filters.AndFilter;
-import com.intellij.psi.filters.NotFilter;
-import com.intellij.psi.filters.position.LeftNeighbour;
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
-
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.PsiElement
+import com.intellij.psi.filters.position.LeftNeighbour
 
 class BeforeDotFilter(elems : IElementType*) extends LeftNeighbour {
   override def isAcceptable (element: java.lang.Object, context: PsiElement): Boolean =
