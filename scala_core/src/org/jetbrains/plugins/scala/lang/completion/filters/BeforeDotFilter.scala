2a6cc99ee709ca7df15bebcf16b38ee0f3519387
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-05-24 20:47:24 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16592 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/completion/filters/BeforeDotFilter.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/completion/filters/BeforeDotFilter.scala
@@ -12,12 +12,9 @@ import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
 
 
 class BeforeDotFilter(elems : IElementType*) extends LeftNeighbour {
-
-  def contains[T](seq: Seq[T], elem: T): Boolean = seq.contains(elem)
-
   override def isAcceptable (element: java.lang.Object, context: PsiElement): Boolean =
     element match {
-      case psi : PsiElement => contains(elems, psi.getNode.getElementType)
+      case psi : PsiElement => elems contains psi.getNode.getElementType
       case _ => false
     }
 }
