439e49faea360e3a4c29d67ae385e0ce69010653
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-06-03 12:05:40 +0000

    probably NPE fixed
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16824 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/resolve/BaseProcessor.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/resolve/BaseProcessor.scala
@@ -8,8 +8,8 @@ import _root_.scala.collection.Set
 import _root_.scala.collection.mutable.HashSet
 
 import org.jetbrains.plugins.scala.lang.psi.api._
-import toplevel.typedef.{ScObject, ScTypeDefinition}
-import statements.{ScValue, ScVariable}
+import toplevel.typedef. {ScObject, ScTypeDefinition}
+import statements. {ScValue, ScVariable}
 import statements.params.ScTypeParam
 
 abstract class BaseProcessor(val kinds: Set[ResolveTargets]) extends PsiScopeProcessor {
@@ -18,42 +18,45 @@ abstract class BaseProcessor(val kinds: Set[ResolveTargets]) extends PsiScopePro
 
   //java compatibility
   object MyElementClassHint extends ElementClassHint {
-    def shouldProcess(c: Class[_]): Boolean =
+    def shouldProcess(c: Class[_]): Boolean = {
+      if (kinds == null) return true
       if (c.isAssignableFrom(classOf[PsiPackage])) kinds contains ResolveTargets.PACKAGE
       else if (c.isAssignableFrom(classOf[PsiClass])) (kinds contains ResolveTargets.CLASS) || (kinds contains ResolveTargets.OBJECT)
-      else if (c.isAssignableFrom(classOf[PsiVariable])) (kinds contains ResolveTargets.VAR) || (kinds contains ResolveTargets.VAL)
-      else if (c.isAssignableFrom(classOf[PsiMethod])) kinds contains (ResolveTargets.METHOD)
-      else false
+           else if (c.isAssignableFrom(classOf[PsiVariable])) (kinds contains ResolveTargets.VAR) || (kinds contains ResolveTargets.VAL)
+                else if (c.isAssignableFrom(classOf[PsiMethod])) kinds contains (ResolveTargets.METHOD)
+                     else false
+    }
   }
 
-  def getHint [T](hintClass: Class[T]): T = {
+  def getHint[T](hintClass: Class[T]): T = {
     if (hintClass == classOf[ElementClassHint]) {
       return MyElementClassHint.asInstanceOf[T]
     }
     return null.asInstanceOf[T]
   }
 
-  def handleEvent(event: PsiScopeProcessor.Event, associated: Object) = {}
+  def handleEvent(event: PsiScopeProcessor.Event, associated: Object) = {
+  }
 
-  protected def kindMatches(element: PsiElement) : Boolean = kinds == null ||
-    (element match {
-      case _: PsiPackage => kinds contains ResolveTargets.PACKAGE
-      case _: ScObject => kinds contains ResolveTargets.OBJECT
-      case _: ScTypeParam => kinds contains ResolveTargets.CLASS
-      case _: ScTypeDefinition => kinds contains ResolveTargets.CLASS
-      case c: PsiClass if c.getLanguage == StdLanguages.JAVA => {
-        if (kinds contains ResolveTargets.CLASS) true
-        else {
-          def isStaticCorrect(clazz: PsiClass): Boolean = {
-            val cclazz = clazz.getContainingClass
-            cclazz == null || (clazz.hasModifierProperty(PsiModifier.STATIC) && isStaticCorrect(cclazz))
-          }
-          isStaticCorrect(c)
-        }
-      }
-      case _: ScValue => kinds contains ResolveTargets.VAL
-      case _: ScVariable => kinds contains ResolveTargets.VAR
-      case _: PsiMethod => kinds contains ResolveTargets.METHOD
-      case _ => false
-    })
+  protected def kindMatches(element: PsiElement): Boolean = kinds == null ||
+          (element match {
+            case _: PsiPackage => kinds contains ResolveTargets.PACKAGE
+            case _: ScObject => kinds contains ResolveTargets.OBJECT
+            case _: ScTypeParam => kinds contains ResolveTargets.CLASS
+            case _: ScTypeDefinition => kinds contains ResolveTargets.CLASS
+            case c: PsiClass if c.getLanguage == StdLanguages.JAVA => {
+              if (kinds contains ResolveTargets.CLASS) true
+              else {
+                def isStaticCorrect(clazz: PsiClass): Boolean = {
+                  val cclazz = clazz.getContainingClass
+                  cclazz == null || (clazz.hasModifierProperty(PsiModifier.STATIC) && isStaticCorrect(cclazz))
+                }
+                isStaticCorrect(c)
+              }
+            }
+            case _: ScValue => kinds contains ResolveTargets.VAL
+            case _: ScVariable => kinds contains ResolveTargets.VAR
+            case _: PsiMethod => kinds contains ResolveTargets.METHOD
+            case _ => false
+          })
 }
