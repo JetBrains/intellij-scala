02042e4c4c88494f2262587b3bb0fa8cb423b207
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-04 20:23:58 +0000

    fix getPackageName
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16877 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/packaging/ScPackageStatement.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/packaging/ScPackageStatement.scala
@@ -1,14 +1,14 @@
 package org.jetbrains.plugins.scala.lang.psi.api.toplevel.packaging
 
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
-import org.jetbrains.annotations._
+import org.jetbrains.plugins.scala.lang.psi.api.base._
 /** 
 * @author Alexander.Podkhalyuzin
 */
 
 trait ScPackageStatement extends ScalaPsiElement {
 
-  @NotNull
   def getPackageName: String
 
+  def reference = findChildByClass(classOf[ScStableCodeReferenceElement])
 }
