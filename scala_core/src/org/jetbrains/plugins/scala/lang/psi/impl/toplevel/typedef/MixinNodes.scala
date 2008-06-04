6775933642ee132fd46bf386f1beba4d6c6014da
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-04 17:24:36 +0000

    linearization
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16867 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MixinNodes.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MixinNodes.scala
@@ -1,34 +1,91 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
 
+import collection.mutable.{HashMap}
+
 abstract class MixinNodes {
   type T
-
+  def equiv(t1 : T, t2 : T) : Boolean
+  def computeHashCode(t : T) : Int
   trait Node {
     val info : T
   }
   
   case class AbstractMember(val info : T) extends Node {
-    var overrides : List[AbstractMember] = Nil
+    var overrides : Option[AbstractMember] = None
   }
 
   case class ConcreteMember(val info : T) extends Node {
     var overrides : Option[ConcreteMember] = None 
-    var implements : List[AbstractMember] = Nil
-    var hides : List[ConcreteMember] = Nil
+    var implements : Option[AbstractMember] = None
+  }
+
+  class Map extends HashMap[T, Node] {
+    override def elemHashCode(k : T) = computeHashCode(k)
+    override def elemEquals(t1 : T, t2 : T) = equiv(t1, t2)
+  }
+
+  object Map {def empty = new Map}
+
+  def mergeSupers (maps : Map*) : Map = {
+    maps.foldLeft (Map.empty) {
+      (res, curr) =>
+        for ((key, node) <- curr) {
+          res.get(key) match {
+            case None => res += Pair(key, node)
+            case Some (node1) =>
+              node1 match {
+                case a1 : AbstractMember =>
+                  node match {
+                    case c : ConcreteMember => res += Pair(key, node) //overwrite
+                    case _ =>
+                  }
+                case _ =>
+              }
+          }
+        }
+        res
+    }
+  }
+
+  def mergeWithSupers(thisMap : Map, supersMerged : Map) {
+    for ((key, n1) <- supersMerged) {
+      thisMap.get(key) match {
+        case None => thisMap += Pair(key, n1)
+        case Some(n2) =>
+          n2 match {
+            case a2 : AbstractMember =>
+              n1 match {
+                case a1 : AbstractMember => a2.overrides = Some(a1)
+                case c1 : ConcreteMember => //this is a compilation error to be catched elsewhere
+              }
+            case c2 : ConcreteMember =>
+              n1 match {
+                case a1 : AbstractMember => c2.implements = Some(a1)
+                case c1 : ConcreteMember => c2.overrides = Some(c1)
+              }
+          }
+      }
+    }
   }
 }
 
 import org.jetbrains.plugins.scala.lang.psi.types.Signature
-object MethodNodes extends MixinNodes {
+class MethodNodes extends MixinNodes {
   type T = Signature
+  def equiv(s1 : Signature, s2 : Signature) = s1 equiv s2
+  def computeHashCode(s : Signature) = s.name.hashCode
 }
 
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
-object FieldNodes extends MixinNodes {
+class FieldNodes extends MixinNodes {
   type T = ScReferencePattern
+  def equiv(p1 : ScReferencePattern, p2 : ScReferencePattern) = p1.name == p2.name
+  def computeHashCode(patt : ScReferencePattern) = patt.name.hashCode
 }
 
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAlias
-object TypeAliasNodes extends MixinNodes {
+class TypeAliasNodes extends MixinNodes {
   type T = ScTypeAlias
+  def equiv(al1 : ScTypeAlias, al2 : ScTypeAlias) = al1.name == al2.name
+  def computeHashCode(al : ScTypeAlias) = al.name.hashCode
 }
