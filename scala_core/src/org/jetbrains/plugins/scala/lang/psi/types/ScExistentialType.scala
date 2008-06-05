ffa303d3825314b1f3ca44988758a9b74f483816
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-05 17:40:04 +0000

    some java -> scala type system conversion
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16895 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScExistentialType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScExistentialType.scala
@@ -4,4 +4,8 @@ package org.jetbrains.plugins.scala.lang.psi.types
 * @author ilyas
 */
 
-case class ScExistentialType(tas: List[ScTypeAlias], vals : List[Tuple2[String, ScType]]) extends ScType
+case class ScExistentialType(tas: Seq[ScTypeAlias], vals : Seq[Tuple2[String, ScType]]) extends ScType
+
+object ScExistentialType {
+  val unbounded = new ScExistentialType(Seq.empty, Seq.empty)
+}
