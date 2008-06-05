d37cbefaf02e65bb3cff2773a618b65a9ae0c49c
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-05 18:56:52 +0000

    all predefined types added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16900 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScType.scala
@@ -17,10 +17,16 @@ case object Any extends ScType {
   override def conforms(t: ScType) = true
 }
 
+case object Null extends ScType {
+  override def equiv(t : ScType) = t == Null
+  override def conforms(t: ScType) = t == Null
+}
+
 case object AnyRef extends ScType {
   override def equiv(t : ScType) = t == AnyRef
   override def conforms(t: ScType) = t match {
     case AnyRef => true
+    case Null => true
     case _ : ScParameterizedType => true
     case _ : ScSingletonType => true
     case _ => false
@@ -31,6 +37,36 @@ case object Nothing extends ScType {
   override def equiv(t : ScType) = t == Nothing
   override def conforms(t: ScType) = t == Nothing
 }
+
+case object AnyVal extends ScType {
+  override def equiv(t : ScType) = t == AnyVal
+  override def conforms(t: ScType) = t match {
+    case AnyVal => true
+    case _ : ValType => true
+    case _ => false
+  }
+}
+
+abstract case class ValType(val name : String, val tSuper : Option[ValType]) extends ScType {
+  override def equiv(t : ScType) = t == this
+  override def conforms(t : ScType) : Boolean = {
+    if (t == this) return true
+    tSuper match {
+      case Some(tSuper) => conforms(tSuper)
+      case _ => false
+    }
+  }
+}
+
+object UnitType extends ValType("Unit", None)
+object BooleanType extends ValType("Boolean", None)
+object CharType extends ValType("Char", Some(IntType))
+object IntType extends ValType("Int", Some(LongType))
+object LongType extends ValType("Long", Some(FloatType))
+object FloatType extends ValType("Float", Some(DoubleType))
+object DoubleType extends ValType("Double", None)
+object ByteType extends ValType("Byte", Some(ShortType))
+object ShortType extends ValType("Float", Some(IntType))
 
 object ScType {
   def create(psiType : PsiType, project : Project) : ScType = {
@@ -49,6 +85,17 @@ object ScType {
           return new ScParameterizedType(arrayClass, subst)
         }
       }
+
+      case PsiType.VOID => UnitType
+      case PsiType.BOOLEAN => BooleanType
+      case PsiType.CHAR => CharType
+      case PsiType.INT => IntType
+      case PsiType.LONG => LongType
+      case PsiType.FLOAT => FloatType
+      case PsiType.DOUBLE => DoubleType
+      case PsiType.BYTE => ByteType
+      case PsiType.SHORT => ShortType
+      case PsiType.NULL => Null
     //todo other cases
     }
     null
