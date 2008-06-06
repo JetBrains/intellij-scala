ab2cdf9d239a770f096fe8770d2037a79e2668b6
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-06 06:52:11 +0000

    correct equiv
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16908 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSingletonType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSingletonType.scala
@@ -1,14 +1,23 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
-import org.jetbrains.plugins.scala.lang.psi.api.base._
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
 
 /** 
 * @author ilyas
 */
 
-case class ScSingletonType(path: ScReferenceElement) extends ScType {
+case class ScSingletonType(path: ScReferenceExpression) extends ScType {
   override def equiv(t : ScType)= t match {
-    case ScSingletonType(path1) => path eq path1
+    case ScSingletonType(path1) => {
+      def equiv(e1 : ScReferenceExpression, e2 : ScReferenceExpression) : Boolean = {
+        e1.bind == e2.bind && ((e1.qualifier, e2.qualifier) match {
+          case (Some (q1 : ScReferenceExpression), Some (q2 : ScReferenceExpression)) => equiv(q1, q2)
+          case (None, None) => true
+          case _ => false
+        })
+      }
+      equiv(path, path1)
+    }
     case _ => false
   }
 
