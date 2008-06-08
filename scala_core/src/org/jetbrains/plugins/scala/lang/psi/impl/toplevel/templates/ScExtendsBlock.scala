e2f4aa8e0f77c9c51e90fc4f394319ac1de13528
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-08 11:47:19 +0000

    getSuperTypes added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@16959 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/templates/ScExtendsBlock.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/templates/ScExtendsBlock.scala
@@ -14,19 +14,45 @@ import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.icons.Icons
 
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates._
+import org.jetbrains.plugins.scala.lang.psi.types._
+import _root_.scala.collection.mutable.ArrayBuffer
+
+import com.intellij.psi.JavaPsiFacade
 
 /** 
 * @author Alexander Podkhalyuzin
 * Date: 20.02.2008
 */
 
-class ScExtendsBlockImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScExtendsBlock{
+class ScExtendsBlockImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScExtendsBlock {
 
   override def toString: String = "ExtendsBlock"
 
   def templateBody: Option[ScTemplateBody] = findChild(classOf[ScTemplateBody])
 
-  def empty: Boolean = {
-    return getNode.getFirstChildNode == null
+  def empty = getNode.getFirstChildNode == null
+
+  def superTypes(): Seq[ScType] = {
+    val buffer = new ArrayBuffer[ScType]
+    templateParents match {
+      case None => ()
+      case Some(parents) => {
+        parents.constructor match {
+          case None => ()
+          case Some(c) => buffer += c.typeElement.getType
+        }
+        buffer ++= (parents.traits map {
+          typeElement => typeElement.getType
+        }).toArray
+      }
+    }
+    val so = scalaObject()
+    if (so != null) buffer += so
+    buffer.toArray
+  }
+
+  def scalaObject() = {
+    val so = JavaPsiFacade.getInstance(getProject).findClass("scala.ScalaObject")
+    if (so != null) new ScParameterizedType(so, ScSubstitutor.empty) else null
   }
 }
