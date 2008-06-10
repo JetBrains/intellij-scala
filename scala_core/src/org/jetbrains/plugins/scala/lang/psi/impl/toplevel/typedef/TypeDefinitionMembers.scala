477b20f4636cc5baf37e8430b700158beadbf9ca
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-10 21:44:47 +0000

    SOE fixed
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17033 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/TypeDefinitionMembers.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/TypeDefinitionMembers.scala
@@ -22,6 +22,23 @@ object TypeDefinitionMembers {
       case m if m.hasModifierProperty(PsiModifier.ABSTRACT) => true
       case _ => false
     }
+
+    def processJava(clazz : PsiClass, subst : ScSubstitutor, map : Map) =
+      for (method <- clazz.getMethods) {
+        val sig = new Signature(method, subst)
+        map += ((sig, new Node(sig)))
+      }
+
+    def processScala(td : ScTypeDefinition, subst : ScSubstitutor, map : Map) =
+      for (member <- td.members) {
+        member match {
+          case method: ScFunction => {
+            val sig = new Signature(method, subst)
+            map += ((sig, new Node(sig)))
+          }
+          case _ =>
+        }
+      }
   }
 
   import com.intellij.psi.PsiNamedElement
@@ -34,6 +51,31 @@ object TypeDefinitionMembers {
       case f : PsiField if f.hasModifierProperty(PsiModifier.ABSTRACT) => true
       case _ => false
     }
+
+    def processJava(clazz : PsiClass, subst : ScSubstitutor, map : Map) =
+      for (field <- clazz.getFields) {
+        map += ((field, new Node(field)))
+      }
+
+    def processScala(td : ScTypeDefinition, subst : ScSubstitutor, map : Map) =
+      for (member <- td.members) {
+        member match {
+          case obj: ScObject => map += ((obj, new Node(obj)))
+          case patternDef: ScPatternDefinition =>
+            for (binding <- patternDef.bindings) {
+              map += ((binding, new Node(binding)))
+            }
+          case varDef: ScVariableDefinition =>
+            for (binding <- varDef.bindings) {
+              map += ((binding, new Node(binding)))
+            }
+          case varDecl: ScVariableDeclaration =>
+            for (fieldId <- varDecl.getIdList.fieldIds) {
+              map += ((fieldId, new Node(fieldId)))
+            }
+          case _ =>
+        }
+      }
   }
 
   import org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAlias
@@ -45,129 +87,29 @@ object TypeDefinitionMembers {
       case _ : ScTypeAliasDeclaration => true
       case _ => false
     }
-  }
 
-  import ValueNodes.{Map => Vmap}, MethodNodes.{Map => Mmap}, TypeNodes.{Map => Tmap}
-  import ValueNodes.{Node => Vnode}, MethodNodes.{Node => Mnode}, TypeNodes.{Node => Tnode}
-
-  private def buildTypes(td : ScTypeDefinition) = {
-    def inner(clazz : PsiClass, subst : ScSubstitutor) : Tmap = {
-      val typesMap = new Tmap
-      val superTypes = clazz match {
-        case td : ScTypeDefinition => {
-          for (member <- td.members) {
-            member match {
-              case alias : ScTypeAlias => typesMap += ((alias, new Tnode(alias)))
-              case td : ScTypeDefinition => typesMap += ((td, new Tnode(td)))
-              case _ =>
-            }
-          }
-
-          td.superTypes
-        }
-        case _ => {
-          for (inner <- clazz.getInnerClasses) {
-            typesMap += ((inner, new Tnode(inner)))
-          }
-
-          clazz.getSuperTypes.map {psiType => ScType.create(psiType, clazz.getProject)}
-        }
+    def processJava(clazz: PsiClass, subst : ScSubstitutor, map: Map) =
+      for (inner <- clazz.getInnerClasses) {
+        map += ((inner, new Node(inner)))
       }
 
-      val superTypesBuff = new ListBuffer[Tmap]
-      for (superType <- superTypes) {
-        superType match {
-          case ScParameterizedType(superClass : PsiClass, superSubst) => {
-            superTypesBuff += inner (superClass, combine(superSubst, subst))
-          }
+    def processScala(td : ScTypeDefinition, subst : ScSubstitutor, map : Map) =
+      for (member <- td.members) {
+        member match {
+          case alias: ScTypeAlias => map += ((alias, new Node(alias)))
+          case td: ScTypeDefinition => map += ((td, new Node(td)))
           case _ =>
         }
       }
-      TypeNodes.mergeWithSupers(typesMap, TypeNodes.mergeSupers(superTypesBuff.toList))
-
-      typesMap
-    }
-    inner(td, ScSubstitutor.empty)
   }
 
-  private def buildVals(td : ScTypeDefinition) = {
-    def inner(clazz : PsiClass, subst : ScSubstitutor) : Pair[Vmap, Mmap] = {
-      val valuesMap = new Vmap
-      val methodsMap = new Mmap
-      val superTypes = clazz match {
-        case td : ScTypeDefinition => {
-          for (member <- td.members) {
-            member match {
-              case method : ScFunction => {
-                val sig = new Signature(method, subst)
-                methodsMap += ((sig, new Mnode(sig)))
-              }
-              case obj : ScObject => valuesMap += ((obj, new Vnode(obj)))
-              case patternDef : ScPatternDefinition =>
-                for (binding <- patternDef.bindings) {
-                  valuesMap += ((binding, new Vnode(binding)))
-                }
-              case varDef : ScVariableDefinition =>
-                for (binding <- varDef.bindings) {
-                  valuesMap += ((binding, new Vnode(binding)))
-                }
-              case varDecl : ScVariableDeclaration =>
-                for (fieldId <- varDecl.getIdList.fieldIds) {
-                  valuesMap += ((fieldId, new Vnode(fieldId)))
-                }
-              case _ =>
-            }
-          }
+  val valsKey : Key[CachedValue[ValueNodes.Map]] = Key.create("vals key")
+  val methodsKey : Key[CachedValue[MethodNodes.Map]] = Key.create("methods key")
+  val typesKey : Key[CachedValue[TypeNodes.Map]] = Key.create("types key")
 
-          td.superTypes
-        }
-        case _ => {
-          for (method <- clazz.getMethods) {
-            val sig = new Signature(method, subst)
-            methodsMap += ((sig, new Mnode(sig)))
-          }
-
-          for (field <- clazz.getFields) {
-            valuesMap += ((field, new Vnode(field)))
-          }
-
-          clazz.getSuperTypes.map {psiType => ScType.create(psiType, clazz.getProject)}
-        }
-      }
-
-      val superValsBuff = new ListBuffer[Vmap]
-      val superMethodsBuff = new ListBuffer[Mmap]
-      for (superType <- superTypes) {
-        superType match {
-          case ScParameterizedType(superClass : PsiClass, superSubst) => {
-            val (superVals, superMethods) = inner (superClass, combine(superSubst, subst))
-            superValsBuff += superVals
-            superMethodsBuff += superMethods
-          }
-          case _ =>
-        }
-      }
-      ValueNodes.mergeWithSupers(valuesMap, ValueNodes.mergeSupers(superValsBuff.toList))
-      MethodNodes.mergeWithSupers(methodsMap, MethodNodes.mergeSupers(superMethodsBuff.toList))
-
-      (valuesMap, methodsMap)
-    }
-    inner(td, ScSubstitutor.empty)
-  }
-
-  def combine(superSubst : ScSubstitutor, derived : ScSubstitutor) = {
-    var res : ScSubstitutor = ScSubstitutor.empty
-    for ((tp, t) <- superSubst.map) {
-      res = res + (tp, derived.subst(t))
-    }
-    res
-  }
-
-  val valsKey : Key[CachedValue[Pair[Vmap, Mmap]]] = Key.create("members key")
-  val typesKey : Key[CachedValue[Tmap]] = Key.create("types key")
-
-  def getVals (td : ScTypeDefinition) = get(td, valsKey, new MyProvider(td, {td => buildVals(td)}))
-  def getTypes(td : ScTypeDefinition) = get(td, typesKey, new MyProvider(td, {td => buildTypes(td)}))
+  def getVals (td : ScTypeDefinition) = get(td, valsKey, new MyProvider(td, {td => ValueNodes.build(td)}))
+  def getMethods (td : ScTypeDefinition) = get(td, methodsKey, new MyProvider(td, {td => MethodNodes.build(td)}))
+  def getTypes(td : ScTypeDefinition) = get(td, typesKey, new MyProvider(td, {td => TypeNodes.build(td)}))
 
   private def get[T] (td : ScTypeDefinition, key : Key[CachedValue[T]], provider : CachedValueProvider[T]) = {
     var computed = td.getUserData(key)
@@ -183,4 +125,4 @@ object TypeDefinitionMembers {
     def compute() = new CachedValueProvider.Result (builder(td),
                          Array[Object](PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT))
   }
-}
+  }
