1cf1973259a89f984057733255a372f910ce5f14
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-11 21:30:10 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17058 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScTupleTypeImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScTupleTypeImpl.scala
@@ -1,17 +1,7 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.base.types
 
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-
-
-
-
-import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi._
-
 import org.jetbrains.plugins.scala.lang.psi.api.base.types._
 import org.jetbrains.plugins.scala.lang.psi.types.ScTupleType
 
