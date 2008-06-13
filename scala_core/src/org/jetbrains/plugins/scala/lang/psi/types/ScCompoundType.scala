859b040638555c84547617c9041975fc2af1b417
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-13 18:28:52 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17076 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScCompoundType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScCompoundType.scala
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScDeclaration
-import _root_.scala.collection.mutable.HashSet
+import _root_.scala.collection.mutable.{HashSet, HashMap}
 import api.statements._
 import com.intellij.psi.PsiTypeParameter
 
@@ -9,6 +9,13 @@ case class ScCompoundType(val components: Seq[ScType], decls: Seq[ScDeclaration]
   //compound types are checked by checking the set of signatures in their refinements
   val signatureSet = new HashSet[Signature] {
     override def elemHashCode(s : Signature) = s.name.hashCode* 31 + s.types.length
+  }
+
+  type Bounds = Pair[ScType, ScType]
+  val types = new HashMap[String, Bounds]
+
+  for (typeDecl <- typeDecls) {
+    types += ((typeDecl.name, (typeDecl.lowerBound, typeDecl.upperBound)))
   }
 
   for (decl <- decls) {
@@ -31,13 +38,28 @@ case class ScCompoundType(val components: Seq[ScType], decls: Seq[ScDeclaration]
       }
     }
   }
-  //todo extract type alias information
 
   override def equiv(t: ScType) = t match {
     case other : ScCompoundType => {
       components.equalsWith (other.components) (_ equiv _) &&
-      signatureSet.equals(other.signatureSet)
+      signatureSet.equals(other.signatureSet) &&
+      typesMatch(types, other.types)
     }
     case _ => false
+  }
+
+  private def typesMatch(types1 : HashMap[String, Bounds],
+                         types2 : HashMap[String, Bounds]) : Boolean = {
+    if (types1 != types.size) return false
+    else {
+      for ((name, bounds1) <- types1) {
+        types2.get(name) match {
+          case None => return false
+          case Some (bounds2) => if (!(bounds1._1 equiv bounds2._1) ||
+                                     !(bounds1._2 equiv bounds2._2)) return false
+        }
+      }
+      true
+    }
   }
 }
