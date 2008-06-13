e6c72c1ac1ee0f665db191f410a3bedcb1334e44
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-13 06:14:46 +0000

    compound types
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17071 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScCompoundType.scala
@@ -0,0 +1,40 @@
+package org.jetbrains.plugins.scala.lang.psi.types
+
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScDeclaration
+import _root_.scala.collection.mutable.HashSet
+import api.statements._
+import com.intellij.psi.PsiTypeParameter
+
+case class ScCompoundType(val components: Seq[ScType], decls: Seq[ScDeclaration], typeDecls: Seq[ScTypeAlias]) extends ScType{
+  //compound types are checked by checking the set of signatures in their refinements
+  val signatureSet = new HashSet[Signature]
+  for (decl <- decls) {
+    decl match {
+      case fun: ScFunctionDeclaration => signatureSet += new PhysicalSignature(fun, ScSubstitutor.empty)
+      case varDecl: ScVariableDeclaration => {
+        val typeElement = varDecl.typeElement
+        for (name <- varDecl.names) {
+          signatureSet += new Signature(name, Seq.empty, Array(), ScSubstitutor.empty)
+          typeElement match {
+            case Some(te) => {
+              signatureSet += new Signature(name + "_", Seq.single(te.getType), Array(), ScSubstitutor.empty) //setter
+            }
+            case None =>
+          }
+        }
+      }
+      case valDecl: ScValueDeclaration => for (name <- valDecl.names) {
+        signatureSet += new Signature(name, Seq.empty, Array(), ScSubstitutor.empty)
+      }
+    }
+  }
+  //todo extract type alias information
+
+  override def equiv(t: ScType) = t match {
+    case other : ScCompoundType => {
+      components.equalsWith (other.components) {(t1, t2) => t1 equiv t2} &&
+      signatureSet.equals(other.signatureSet)
+    }
+    case _ => false
+  }
+}
