8ce9cde04953a84fe0c9d429d46992ef51080c5c
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-14 11:27:54 +0000

    trivial
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17082 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScFunctionType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScFunctionType.scala
@@ -4,6 +4,17 @@ package org.jetbrains.plugins.scala.lang.psi.types
 * @author ilyas
 */
 
-case class ScFunctionType(returnType: ScType, params: Seq[ScType]) extends ScType
+case class ScFunctionType(returnType: ScType, params: Seq[ScType]) extends ScType {
+  override def equiv(that : ScType) = that match {
+    case ScFunctionType(rt1, params1) => (returnType equiv rt1) &&
+                               params.equalsWith(params1) {_ equiv _}
+    case _ => false
+  }
+}
 
-case class ScTupleType(components: Seq[ScType]) extends ScType
+case class ScTupleType(components: Seq[ScType]) extends ScType {
+  override def equiv(that : ScType) = that match {
+    case ScTupleType(c1) => components.equalsWith(c1) {_ equiv _}
+    case _ => false
+  }
+}
