9789097f056c5cbf34cac2a2a6dcb567a10b8447
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-22 16:18:21 +0000

    designators are equivalent to singleton's projections
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17254 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScProjectionType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScProjectionType.scala
@@ -1,5 +1,7 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
+import resolve._
+
 /**
 * @author ilyas
 */
@@ -7,6 +9,14 @@ package org.jetbrains.plugins.scala.lang.psi.types
 case class ScProjectionType(projected: ScType, name: String) extends ScType {
   override def equiv(t : ScType) = t match {
     case ScProjectionType(p1, n1) => n1 == name && (projected equiv p1)
+    case ScDesignatorType(des) => projected match {
+      case ScSingletonType(path) => {
+        val proc = new ResolveProcessor(StdKinds.stableClass, name)
+        path.processType(projected, proc)
+        if (proc.candidates.size == 1) proc.candidates.toArray(0).element eq des else false
+      }
+      case _ => false
+    }
     case _ => false
   }
 }
