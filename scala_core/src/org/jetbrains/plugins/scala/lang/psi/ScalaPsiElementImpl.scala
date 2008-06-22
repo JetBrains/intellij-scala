ad59c9f29d73e55633823c6d6f9a8fe78033b6c9
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-22 12:06:51 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17251 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/ScalaPsiElementImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/ScalaPsiElementImpl.scala
@@ -3,12 +3,6 @@ package org.jetbrains.plugins.scala.lang.psi
 import com.intellij.psi.PsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.extapi.psi.ASTWrapperPsiElement
-import com.intellij.openapi.util.Key
-import com.intellij.psi.tree.TokenSet
-import com.intellij.psi.tree.IElementType;
-import org.jetbrains.annotations.Nullable
-
-import javax.swing.Icon
 
 /**
   @author ven
@@ -18,23 +12,9 @@ class ScalaPsiElementImpl(node: ASTNode) extends ASTWrapperPsiElement(node)
 
   // todo override in more specific cases
   override def replace(newElement: PsiElement): PsiElement = {
-    newElement match {
-      case ne: ScalaPsiElementImpl =>
-        val parent: ScalaPsiElementImpl = getParent().asInstanceOf[ScalaPsiElementImpl]
-        val parentNode = parent.getNode()
-        val myNode = this.getASTNode()
-        val newElementNode = ne.getASTNode()
-        parentNode.replaceChild(myNode, newElementNode)
-        ne
-      case _ => null
-    }
+    getParent.getNode.replaceChild(node, newElement.getNode)
+    newElement
   }
 
-  def getASTNode(): ASTNode = node
-
-  override def findChild[T >: Null <: ScalaPsiElement](clazz: Class[T]): Option[T] = findChildByClass(clazz) match {
-    case null => None
-    case e => Some(e)
-  }
-  override def toString: String = "scala psi element"
+  override def toString = "scala psi element"
 }
