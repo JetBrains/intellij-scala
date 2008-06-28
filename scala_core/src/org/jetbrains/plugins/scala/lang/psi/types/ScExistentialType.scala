161b2917ac391e61f770abd46748d0d519a1b11f
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-06-28 19:20:45 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17371 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScExistentialType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScExistentialType.scala
@@ -1,39 +1,8 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
-import api.statements.{ScTypeAliasDeclaration, ScValueDeclaration, ScDeclaration}
-import _root_.scala.collection.mutable.ListBuffer
-
 /** 
 * @author ilyas
 */
-
-object ScExistentialType {
-  def create(quantified : ScType, decls : Seq[ScDeclaration]) = {
-    val wildcards : List[Pair[String, ScWildcardType]] = {
-      var buff : ListBuffer[Pair[String, ScWildcardType]] = new ListBuffer
-      for (decl <- decls) {
-        decl match {
-          case alias : ScTypeAliasDeclaration => {
-            buff += ((alias.name, new ScWildcardType(alias.lowerBound, alias.upperBound)))
-          }
-          case value : ScValueDeclaration => {
-            value.typeElement match {
-              case Some(te) =>
-                val t = new ScCompoundType(Array(te.getType, Singleton), Seq.empty, Seq.empty)
-                for (declared <- value.declaredElements) {
-                  buff += ((declared.name, new ScWildcardType(Nothing, t)))
-                }
-              case None =>
-            }
-          }
-          case _ =>
-        }
-      }
-      buff.toList
-    }
-    new ScExistentialType(quantified, wildcards)
-  }
-}
 
 case class ScExistentialType(val quantified : ScType,
                              val wildcards : List[Pair[String, ScWildcardType]]) extends ScType {
