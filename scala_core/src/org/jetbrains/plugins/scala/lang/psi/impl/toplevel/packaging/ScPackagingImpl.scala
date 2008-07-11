338289fd9cd2b93326f20911930b1a7af97bd7ab
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-07-11 10:51:44 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17717 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/packaging/ScPackagingImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/packaging/ScPackagingImpl.scala
@@ -1,25 +1,10 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.packaging
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.tree.IElementType;
 import com.intellij.lang.ASTNode
-
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
-import org.jetbrains.plugins.scala.lang.lexer._
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
-import org.jetbrains.annotations._
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
-
-import org.jetbrains.plugins.scala.icons.Icons
-
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.packaging._
-import org.jetbrains.plugins.scala.lang.psi.api.base._
-
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel._
-
-import _root_.scala.collection.mutable._
+import com.intellij.psi._
+import com.intellij.psi.scope.PsiScopeProcessor
+import psi.ScalaPsiElementImpl
+import api.toplevel.packaging._
 
 /** 
 * @author Alexander Podkhalyuzin
@@ -27,9 +12,15 @@ import _root_.scala.collection.mutable._
 */
 
 class ScPackagingImpl(node: ASTNode) extends ScalaPsiElementImpl (node) with ScPackaging {
-
   override def toString = "ScPackaging"
 
-  @NotNull
-  def getPackageName: String = findChildByClass (classOf[ScReferenceElement]).getText
+  override def processDeclarations(processor: PsiScopeProcessor,
+                                  state: ResolveState,
+                                  lastParent: PsiElement,
+                                  place: PsiElement): Boolean = {
+    for (inner <- getTopStatements) {
+      if (!processor.execute(inner, state)) return false
+    }
+    true
+  }
 }
