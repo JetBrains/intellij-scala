85d9cc8c796e3983c42e5cfef864f892f039ee92
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-07-14 13:27:43 +0000

    some operations added for numeric types. todo:add more + add for integer types
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17767 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/synthetic/ScSyntheticClass.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/synthetic/ScSyntheticClass.scala
@@ -3,14 +3,15 @@ package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.synthetic
 import api.toplevel.ScNamedElement
 import api.statements.ScFunction
 import types._
+import resolve._
 
 import com.intellij.util.IncorrectOperationException
 import com.intellij.psi._
 import com.intellij.psi.impl.light.LightElement
 
-import _root_.scala.collection.mutable.{ListBuffer, Map, HashMap}
+import _root_.scala.collection.mutable.{ListBuffer, Map, HashMap, Set, HashSet, MultiMap}
 
-abstract class SyntheticNamedElement(manager: PsiManager, name: String)
+abstract class SyntheticNamedElement(val manager: PsiManager, name: String)
 extends LightElement(manager, ScalaFileType.SCALA_LANGUAGE) with PsiNamedElement {
   def getName = name
   def setName(newName: String) = throw new IncorrectOperationException("nonphysical element")
@@ -28,18 +29,29 @@ extends SyntheticNamedElement(manager, name) with PsiClass with PsiClassFake {
 
   override def toString = "Synthetic class"
 
-  val methods = new ListBuffer[ScSyntheticFunction]
+  object methods extends HashMap[String, Set[ScSyntheticFunction]] with MultiMap[String, ScSyntheticFunction]
 
-  def addMethod(method: ScSyntheticFunction) = methods += method
+  def addMethod(method: ScSyntheticFunction) = methods.add (method.name, method)
 
   import com.intellij.psi.scope.PsiScopeProcessor
   override def processDeclarations(processor: PsiScopeProcessor,
                                   state: ResolveState,
                                   lastParent: PsiElement,
                                   place: PsiElement): Boolean = {
-    for (method <- methods) {
-      if (!processor.execute(method, state)) return false
+    processor match {
+      case p : ResolveProcessor => {
+        val nameSet = state.get(ResolverEnv.nameKey)
+        val name = if (nameSet == null) p.name else nameSet
+        methods.get(name) match {
+          case Some(ms) => for (method <- ms) {
+            if (!processor.execute(method, state)) return false
+          }
+          case None =>
+        }
+      }
+      case _ => //todo do we want to complete those?
     }
+
     true
   }
 }
@@ -68,7 +80,7 @@ class SyntheticClasses(project: Project) extends ProjectComponent {
   def disposeComponent() {
   }
   def initComponent() {
-    m = new HashMap[String, ScSyntheticClass]
+    all = new HashMap[String, ScSyntheticClass]
     file = PsiFileFactory.getInstance(project).createFileFromText(
     "dummy." + ScalaFileType.SCALA_FILE_TYPE.getDefaultExtension(), "")
 
@@ -81,26 +93,51 @@ class SyntheticClasses(project: Project) extends ProjectComponent {
     registerClass(Singleton, "Singleton")
     registerClass(Unit, "Unit")
     registerClass(Boolean, "Boolean")
-    registerClass(Char, "Char")
-    registerClass(Int, "Int")
-    registerClass(Long, "Long")
-    registerClass(Float, "Float")
-    registerClass(Double, "Double")
-    registerClass(Byte, "Byte")
-    registerClass(Short, "Short")
+
+    registerIntegerClass(registerNumericClass(registerClass(Char, "Char")))
+    registerIntegerClass(registerNumericClass(registerClass(Int, "Int")))
+    registerIntegerClass(registerNumericClass(registerClass(Long, "Long")))
+    registerIntegerClass(registerNumericClass(registerClass(Byte, "Byte")))
+    registerIntegerClass(registerNumericClass(registerClass(Short, "Short")))
+    registerNumericClass(registerClass(Float, "Float"))
+    registerNumericClass(registerClass(Double, "Double"))
+
+    for(ic <- numeric) {
+      for (ic1 <- numeric; op <- numeric_comp_ops)
+        ic.addMethod(new ScSyntheticFunction(ic.manager, op, Boolean, Seq.singleton(ic1.t)))
+      for (ic1 <- numeric; op <- numeric_arith_ops)
+        ic.addMethod(new ScSyntheticFunction(ic.manager, op, op_type(ic, ic1), Seq.singleton(ic1.t)))
+    }
   }
 
-  var m: Map[String, ScSyntheticClass] = _
+  var all: Map[String, ScSyntheticClass] = _
+  var numeric: Set[ScSyntheticClass] = _
+  var integer : Set[ScSyntheticClass] = _
+  def op_type (ic1 : ScSyntheticClass, ic2 : ScSyntheticClass) = (ic1.t, ic2.t) match {
+    case (_, Double) | (Double, _) => Double
+    case (Float, _) | (_, Float) => Float
+    case (_, Long) | (Long, _)=> Long
+    case _ => Int
+  }
+
   var file : PsiFile = _
 
-  def registerClass(t: ScType, name: String) {
-    m + ((name, new ScSyntheticClass(PsiManager.getInstance(project), name, t)))
+  def registerClass(t: ScType, name: String) = {
+    var clazz = new ScSyntheticClass(PsiManager.getInstance(project), name, t)
+    all + ((name, clazz)); clazz
   }
 
-  def getAll() = m.values.toList.toArray
+  def registerIntegerClass(clazz : ScSyntheticClass) = {integer + clazz; clazz}
+  def registerNumericClass(clazz : ScSyntheticClass) = {numeric + clazz; clazz}
 
-  def byName(name: String) = m.get(name) match {
+
+  def getAll() = all.values.toList.toArray
+
+  def byName(name: String) = all.get(name) match {
     case Some(c) => c
     case _ => null
   }
+
+  val numeric_comp_ops = "==" :: "!=" :: "<" :: ">" :: "<=" :: ">=" :: Nil
+  val numeric_arith_ops = "+" :: "-" :: "*" :: "/" :: "%" :: Nil
 }
