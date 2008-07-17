b8678743f25459feeb6399c583e56a01b3de2911
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-07-17 06:26:46 +0000

    For exception at plugin opening
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17838 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/components/ScalaIconProvider.scala
+++ scala_core/src/org/jetbrains/plugins/scala/components/ScalaIconProvider.scala
@@ -7,7 +7,7 @@ import com.intellij.ide.IconProvider
 import org.jetbrains.annotations.Nullable
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
 
-class ScalaIconProvider extends Object with IconProvider {
+class ScalaIconProvider extends Object with IconProviderMix {
   @Nullable
   override def getIcon(element: PsiElement, flags: Int): Icon = {
 
@@ -31,3 +31,5 @@ class ScalaIconProvider extends Object with IconProvider {
   def disposeComponent = {
   }
 }
+
+trait IconProviderMix extends IconProvider
