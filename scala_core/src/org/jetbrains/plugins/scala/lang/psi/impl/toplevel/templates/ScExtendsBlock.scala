1f384a24afcc4d2442d3d25f87fbb3195909a9c0
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-07-18 13:12:06 +0000

    optimization
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17877 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/templates/ScExtendsBlock.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/templates/ScExtendsBlock.scala
@@ -1,24 +1,12 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.templates
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.tree.IElementType;
 import com.intellij.lang.ASTNode
-
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
-import org.jetbrains.plugins.scala.lang.lexer._
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
-import org.jetbrains.annotations._
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
-
-import org.jetbrains.plugins.scala.icons.Icons
-
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates._
-import org.jetbrains.plugins.scala.lang.psi.types._
-import _root_.scala.collection.mutable.ArrayBuffer
-
 import com.intellij.psi.JavaPsiFacade
 
+import psi.ScalaPsiElementImpl
+import api.toplevel.templates._
+import psi.types._
+import _root_.scala.collection.mutable.ArrayBuffer
 /** 
 * @author Alexander Podkhalyuzin
 * Date: 20.02.2008
