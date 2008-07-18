adf0b85e81c734069e3ccaf4aea6b8f0485a4c61
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-07-18 11:58:54 +0000

    SCL-114
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17874 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/overrideImplement/ScalaOverrideMethodsHandler.scala
+++ scala_core/src/org/jetbrains/plugins/scala/overrideImplement/ScalaOverrideMethodsHandler.scala
@@ -14,7 +14,7 @@ class ScalaOverrideMethodsHandler extends LanguageCodeInsightActionHandler {
   def startInWriteAction: Boolean = false
 
   def invoke(project: Project, editor: Editor, file: PsiFile) {
-    ScalaOIUtil.invokeOverride(project, editor, file)
+    //ScalaOIUtil.invokeOverride(project, editor, file)
   }
 
   def isValidFor(editor: Editor, file: PsiFile): Boolean = file != null && ScalaFileType.SCALA_FILE_TYPE == file.getFileType
