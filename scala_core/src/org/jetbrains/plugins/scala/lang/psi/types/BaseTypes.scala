7c940ca83624f5b55a1deff8dae4c0c94680abd2
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-07-21 15:27:35 +0000

    base types take 1
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17924 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/BaseTypes.scala
@@ -0,0 +1,56 @@
+package org.jetbrains.plugins.scala.lang.psi.types
+
+import _root_.scala.collection.mutable.MultiMap
+import _root_.scala.collection.mutable.Set
+import _root_.scala.collection.mutable.HashMap
+import com.intellij.psi.PsiClass
+import api.toplevel.typedef.ScTypeDefinition
+
+object BaseTypes {
+  def get(t : ScType) : Seq[ScType] = t match {
+    case classT@ScDesignatorType(td : ScTypeDefinition) => Seq.single(classT) ++ reduce(td.superTypes)
+    case classT@ScDesignatorType(c : PsiClass) => Seq.single(classT) ++ reduce(c.getSuperTypes.map{ScType.create(_, c.getProject)})
+    case ScPolymorphicType(poly, s) => get(s.subst(poly.upperBound))
+    case p : ScParameterizedType => p.designated match {
+      case td : ScTypeDefinition => td.superTypes.map {p.substitutor.subst _}
+      case clazz: PsiClass => {
+        val s = p.substitutor
+        clazz.getSuperTypes.map {t => s.subst(ScType.create(t, clazz.getProject))}
+      }
+      case _ => Seq.empty
+    }
+    case sin : ScSingletonType => get(sin.pathType)
+    case ScExistentialType(q, wilds) => get(q).map{bt => ScExistentialTypeReducer.reduce(bt, wilds)}
+    case ScCompoundType(comps, _, _) => reduce(comps)
+    case ScProjectionType(p, name) => Seq.empty //todo
+  }
+
+  def reduce (types : Seq[ScType]) : Seq[ScType] = {
+    def extractClass (t : ScType) = t match {
+      case ScDesignatorType(c : PsiClass) => Some(c)
+      case ScParameterizedType(ScDesignatorType(c : PsiClass), _) => Some(c)
+      case p : ScProjectionType => p.element match {
+        case Some(c : PsiClass) => Some(c)
+        case _ => None
+      }
+      case _ => None
+    }
+
+    val res = new HashMap[PsiClass, ScType]
+    object all extends HashMap[PsiClass, Set[ScType]] with MultiMap[PsiClass, ScType]
+    for (t <- types) {
+      extractClass(t) match {
+        case Some(c) => {
+          val isBest = all.get(c) match {
+            case None => true
+            case Some(ts) => ts.find(t1 => !Conformance.conforms(t1, t)) == None
+          }
+          if (isBest) res += ((c, t))
+          all.add(c, t)
+        }
+        case None => //not a class type
+      }
+    }
+    res.values.toList
+  }
+}
--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScProjectionType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScProjectionType.scala
@@ -7,6 +7,15 @@ import resolve._
 */
 
 case class ScProjectionType(projected: ScType, name: String) extends ScType {
+  lazy val element = projected match {
+    case sin@ScSingletonType(path) => {
+      val proc = new ResolveProcessor(StdKinds.stableClass, name)
+      path.processType(sin, proc)
+      if (proc.candidates.size == 1) Some(proc.candidates.toArray(0).element) else None
+    }
+    case _ => None
+  }
+
   override def equiv(t : ScType) = t match {
     case ScProjectionType(p1, n1) => n1 == name && (projected equiv p1)
     case ScDesignatorType(des) => projected match {
