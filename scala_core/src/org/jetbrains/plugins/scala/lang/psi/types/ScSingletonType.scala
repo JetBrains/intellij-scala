98b23ba8307d0f19184506bea2ec51d4173f7f30
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-07-21 15:27:35 +0000

    base types take 1
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17924 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSingletonType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSingletonType.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
+import api.toplevel.ScTyped
 import api.base.ScReferenceElement
 
 /** 
@@ -7,7 +8,15 @@ import api.base.ScReferenceElement
 */
 
 case class ScSingletonType(path: ScReferenceElement) extends ScType {
-  override def equiv(t : ScType)= t match {
+  val pathType = path.bind match {
+    case None => Nothing
+    case Some(r) => r.element match {
+      case typed : ScTyped => typed.calcType
+      case e => new ScDesignatorType(e)
+    }
+  }
+
+  override def equiv(t : ScType) = t match {
     case ScSingletonType(path1) => {
       def equiv(e1 : ScReferenceElement, e2 : ScReferenceElement) : Boolean = {
         e1.bind == e2.bind && ((e1.qualifier, e2.qualifier) match {
