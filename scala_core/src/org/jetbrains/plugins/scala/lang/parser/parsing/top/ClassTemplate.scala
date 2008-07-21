bd6202559a3bb054fc63d2f6fa373f51f8baf6cf
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-07-21 12:31:39 +0000

    SCL-169
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17911 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/parser/parsing/top/ClassTemplate.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/parser/parsing/top/ClassTemplate.scala
@@ -35,11 +35,14 @@ import org.jetbrains.plugins.scala.lang.parser.parsing.nl.LineTerminator
  */
 
 object ClassTemplate {
-  def parse(builder: PsiBuilder) : Unit = {
+  def parse(builder: PsiBuilder): Boolean = parse(builder, false)
+  def parse(builder: PsiBuilder, nonEmpty: Boolean) : Boolean = {
      val extendsMarker = builder.mark
+     var empty = true
      builder.getTokenType match {
       //hardly case, becase it's same token for ClassParents and TemplateBody
       case ScalaTokenTypes.tLBRACE => {
+        empty = false
         //try to parse early definition if we can't => it's template body
         if (EarlyDef parse builder) {
           ClassParents parse builder
@@ -48,12 +51,12 @@ object ClassTemplate {
             case ScalaTokenTypes.tLBRACE => {
               TemplateBody parse builder
               extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-              return
+              return !nonEmpty || !empty
             }
             case ScalaTokenTypes.tLINE_TERMINATOR => {
               if (!LineTerminator(builder.getTokenText)) {
                 extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-                return
+                return !nonEmpty || !empty
               }
               else {
                 val backup = builder.mark
@@ -62,19 +65,19 @@ object ClassTemplate {
                   case ScalaTokenTypes.tLBRACE => {
                     TemplateBody parse builder
                     extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-                    return
+                    return !nonEmpty || !empty
                   }
                   case _ => {
                     backup.rollbackTo
                     extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-                    return
+                    return !nonEmpty || !empty
                   }
                 }
               }
             }
             case _ => {
               extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-              return
+              return !nonEmpty || !empty
             }
           }
         }
@@ -82,24 +85,24 @@ object ClassTemplate {
           //parse template body
           TemplateBody parse builder
           extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-          return
+          return !nonEmpty || !empty
         }
       }
       //if we find nl => it could be TemplateBody only, but we can't find nl after extends keyword
       //In this case of course it's ClassParents
       case _ => {
-        ClassParents parse builder
+        if (ClassParents parse builder) empty = false
         //parse template body
         builder.getTokenType match {
           case ScalaTokenTypes.tLBRACE => {
             TemplateBody parse builder
             extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-            return
+            return !nonEmpty || !empty
           }
           case ScalaTokenTypes.tLINE_TERMINATOR => {
               if (!LineTerminator(builder.getTokenText)) {
                 extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-                return
+                return !nonEmpty || !empty
               }
               else {
                 val backup = builder.mark
@@ -108,19 +111,19 @@ object ClassTemplate {
                   case ScalaTokenTypes.tLBRACE => {
                     TemplateBody parse builder
                     extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-                    return
+                    return !nonEmpty || !empty
                   }
                   case _ => {
                     backup.rollbackTo
                     extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-                    return
+                    return  !nonEmpty || !empty
                   }
                 }
               }
             }
           case _ => {
             extendsMarker.done(ScalaElementTypes.EXTENDS_BLOCK)
-            return
+            return !nonEmpty || !empty
           }
         }
       }
