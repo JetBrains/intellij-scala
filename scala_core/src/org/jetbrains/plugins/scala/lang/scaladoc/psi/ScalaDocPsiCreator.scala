ba6b9e15bc0139da3ef21afc7d843f8f7a535738
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-07-23 10:45:21 +0000

    Doccomments parser tests
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@17968 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/ScalaDocPsiCreator.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/ScalaDocPsiCreator.scala
@@ -14,13 +14,12 @@ import impl._
 object ScalaDocPsiCreator {
   def createElement(node: ASTNode): PsiElement =
     node.getElementType match {
-      case ScalaDocElementTypes.SCALA_DOC_COMMENT => new ScDocCommentImpl(node)
       case ScalaDocElementTypes.DOC_TAG => new ScDocTagImpl(node)
       case ScalaDocElementTypes.DOC_INLINED_TAG => new ScDocInlinedTagImpl(node)
       case ScalaDocElementTypes.DOC_REFERENCE_ELEMENT => new ScDocReferenceElementImpl(node)
       case ScalaDocElementTypes.DOC_PARAM_REF => new ScDocParamRefImpl(node)
       case ScalaDocElementTypes.DOC_METHOD_REF => new ScDocMethodRefImpl(node)
-      case ScalaDocElementTypes.DOC_FIELD_REF =>  new ScDocFieldRefImpl(node)
+      case ScalaDocElementTypes.DOC_FIELD_REF => new ScDocFieldRefImpl(node)
       case ScalaDocElementTypes.DOC_METHOD_PARAMS => new ScDocMethodParamsImpl(node)
       case ScalaDocElementTypes.DOC_METHOD_PARAMETER => new ScDocMethodParameterImpl(node)
       case _ => new ScalaPsiElementImpl(node)
