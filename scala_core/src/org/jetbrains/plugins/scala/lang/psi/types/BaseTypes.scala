445351f30d7180383c2cd900c00fcfb20361b4ca
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-07-24 08:28:18 +0000

    'seen from' take 1
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18002 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/BaseTypes.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/BaseTypes.scala
@@ -1,10 +1,9 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
-import _root_.scala.collection.mutable.MultiMap
-import _root_.scala.collection.mutable.Set
-import _root_.scala.collection.mutable.HashMap
-import com.intellij.psi.PsiClass
+import com.intellij.psi.{PsiTypeParameter, PsiClass}
+import api.statements.params.ScTypeParam
 import api.toplevel.typedef.ScTypeDefinition
+import _root_.scala.collection.mutable.{Set, HashMap, MultiMap}
 
 object BaseTypes {
   def get(t : ScType) : Seq[ScType] = t match {
@@ -22,7 +21,12 @@ object BaseTypes {
     case sin : ScSingletonType => get(sin.pathType)
     case ScExistentialType(q, wilds) => get(q).map{bt => ScExistentialTypeReducer.reduce(bt, wilds)}
     case ScCompoundType(comps, _, _) => reduce(comps)
-    case ScProjectionType(p, name) => Seq.empty //todo
+    case proj@ScProjectionType(p, name) => proj.element match {
+      case Some(td : ScTypeDefinition) => td.superTypes.map{seenFrom(_, td, p)}
+      case Some(clazz : PsiClass) =>
+        clazz.getSuperTypes.map{st => seenFrom(ScType.create(st, clazz.getProject), clazz, p)}
+      case _ => Seq.empty
+    }
   }
 
   def reduce (types : Seq[ScType]) : Seq[ScType] = {
@@ -52,5 +56,23 @@ object BaseTypes {
       }
     }
     res.values.toList
+  }
+
+  def seenFrom(t : ScType, c : PsiClass, s : ScType) : ScType = t match {
+    case ScExistentialType(s1, wilds) => new ScExistentialType(seenFrom(t,c,s1), wilds)
+    case ScPolymorphicType(tp : ScTypeParam, _) => {
+      val (owner, i) = (tp.owner, tp.owner.typeParameters.indexOf(tp))
+      for (bt <- get(s)) {
+        bt match {
+          case p@ScParameterizedType(ScDesignatorType(`owner`), _) => return p.substitutor.subst(tp)
+          case _ =>
+        }
+      }
+      t
+    }
+    case ScFunctionType(ret, params) => new ScFunctionType(seenFrom(ret, c, s), params.map{seenFrom(_, c, s)})
+    case ScTupleType(comps) => new ScTupleType(comps.map{seenFrom(_, c, s)})
+    case ScCompoundType(comps, decls, types) => new ScCompoundType(comps.map{seenFrom(_, c, s)}, decls, types)
+    case _ => t //todo: case for ScDesignatorType(inner class) + respective projection
   }
 }
