bad045d742d9ab94a2440bf5acef4beb8addcf77
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-07-30 10:49:32 +0000

    Spacing formatting settings
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18193 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/formatting/settings/ScalaCodeStyleSettings.java
+++ scala_core/src/org/jetbrains/plugins/scala/lang/formatting/settings/ScalaCodeStyleSettings.java
@@ -9,6 +9,37 @@ import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
  */
 public class ScalaCodeStyleSettings extends CustomCodeStyleSettings {
 
+  //spcaing settings:
+  public boolean SPACE_BEFORE_COMMA = false;
+  public boolean SPACE_AFTER_COMMA = true;
+  public boolean SPACE_BEFORE_SEMICOLON = false;
+  public boolean SPACE_AFTER_SEMICOLON = true;
+  public boolean SPACE_BEFORE_COLON = false;
+  public boolean SPACE_AFTER_COLON = true;
+  public boolean SPACE_BEFORE_IF_PARENTHESES = true;
+  public boolean SPACE_BEFORE_FOR_PARENTHESES = true;
+  public boolean SPACE_BEFORE_METHOD_PARENTHESES = false;
+  public boolean SPACE_BEFORE_METHOD_CALL_PARENTHESES = false;
+  public boolean SPACE_WITHIN_FOR_PARENTHESES = false;
+  public boolean SPACE_WITHIN_IF_PARENTHESES = false;
+  public boolean SPACE_WITHIN_WHILE_PARENTHESES = false;
+  public boolean SPACE_WITHIN_PARENTHESES = false;
+  public boolean SPACE_WITHIN_METHOD_PARENTHESES = false;
+  public boolean SPACE_WITHIN_METHOD_CALL_PARENTHESES = false;
+  public boolean SPACE_WITHIN_BRACKETS = false;
+  public boolean SPACE_BEFORE_CLASS_LBRACE = true;
+  public boolean SPACE_BEFORE_METHOD_LBRACE = true;
+  public boolean SPACE_BEFORE_IF_LBRACE = true;
+  public boolean SPACE_BEFORE_WHILE_LBRACE = true;
+  public boolean SPACE_BEFORE_DO_LBRACE = true;
+  public boolean SPACE_BEFORE_FOR_LBRACE = true;
+  public boolean SPACE_BEFORE_MATCH_LBRACE = true;
+  public boolean SPACE_BEFORE_TRY_LBRACE = true;
+  public boolean SPACE_BEFORE_CATCH_LBRACE = true;
+  public boolean SPACE_BEFORE_FINALLY_LBRACE = true;
+  public boolean SPACE_BEFORE_WHILE_PARENTHESES = true;
+  public boolean SPACE_BEFORE_ELSE_LBRACE = true;
+
   public ScalaCodeStyleSettings(CodeStyleSettings container) {
     super("ScalaCodeStyleSettings", container);
   }
