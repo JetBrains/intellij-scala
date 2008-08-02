99dce23b5ef6bc0d8c2b642f4bf80d51efc9a452
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-08-02 13:38:45 +0000

    SCL-193 TypeDefMembers used to collect methods and type aliases
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18307 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/Signature.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/Signature.scala
@@ -7,18 +7,20 @@ import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
 import psi.impl.ScalaPsiManager
 
-class Signature(val name : String, val types : Seq[ScType],
-                val typeParams : Array[PsiTypeParameter], val substitutor : ScSubstitutor) {
-  def equiv(other : Signature) : Boolean = {
+class Signature(val name: String, val types: Seq[ScType],
+               val typeParams: Array[PsiTypeParameter], val substitutor: ScSubstitutor) {
+  def equiv(other: Signature): Boolean = {
     name == other.name &&
-    typeParams.length == other.typeParams.length &&
-    types.equalsWith(other.types) {(t1, t2) => {
-      val unified = unify(other.substitutor, typeParams, other.typeParams)
-      substitutor.subst(t1) equiv unified.subst(t2)}
-    }
+            typeParams.length == other.typeParams.length &&
+            types.equalsWith(other.types){
+              (t1, t2) => {
+                val unified = unify(other.substitutor, typeParams, other.typeParams)
+                substitutor.subst(t1) equiv unified.subst(t2)
+              }
+            }
   }
 
-  private def unify(subst : ScSubstitutor, tps1 : Array[PsiTypeParameter], tps2 : Array[PsiTypeParameter]) = {
+  private def unify(subst: ScSubstitutor, tps1: Array[PsiTypeParameter], tps2: Array[PsiTypeParameter]) = {
     var res = subst
     for ((tp1, tp2) <- tps1 zip tps2) {
       val manager = ScalaPsiManager.instance(tp1.getProject)
@@ -27,18 +29,21 @@ class Signature(val name : String, val types : Seq[ScType],
     res
   }
 
-  override def equals(that : Any) = that match {
-    case s : Signature => equiv(s)
+  override def equals(that: Any) = that match {
+    case s: Signature => equiv(s)
     case _ => false
   }
 }
 
 import com.intellij.psi.PsiMethod
-class PhysicalSignature(val method : PsiMethod, override val substitutor : ScSubstitutor)
-  extends Signature (method.getName,
-                     method.getParameterList.getParameters.map {p => p match {
-                                                                  case scp : ScParameter => scp.calcType
-                                                                  case _ => ScType.create(p.getType, p.getProject)
-                                                                }},
-                     method.getTypeParameters,
-                     substitutor)
+
+class PhysicalSignature(val method: PsiMethod, override val substitutor: ScSubstitutor)
+extends Signature(method.getName,
+  method.getParameterList.getParameters.map{
+    p => p match {
+      case scp: ScParameter => scp.calcType
+      case _ => ScType.create(p.getType, p.getProject)
+    }
+  },
+  method.getTypeParameters,
+  substitutor)
