e8ba0ee3d46c72b995598e6a088a84118ed501da
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-08-07 08:08:25 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18419 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSubstitutor.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScSubstitutor.scala
@@ -39,39 +39,39 @@ class ScSubstitutor(val tvMap: Map[ScTypeVariable, ScType],
     case None => new ScTypeAliasType(ta, ScSubstitutor.empty)
   }
 
-  def subst(t: ScType) : ScType = {
-    t match {
-      case ScFunctionType(ret, params) => new ScFunctionType(subst(ret), params map (subst _))
-      case ScTupleType(comps) => new ScTupleType(comps map {subst _})
-      case tv : ScTypeVariable => tvMap.get(tv) match {
-        case None => tv
-        case Some(v) => v
-      }
-      case ScDesignatorType(e) if (!e.isInstanceOf[ScTypeAlias]) => e match { //scala ticket 425
-        case c: PsiClass => {
-          val cc = c.getContainingClass
-          if (cc != null) {
-            outerMap.get(cc) match {
-              case Some(ot) => new ScProjectionType(ot, c.getName)
-              case None => t
-            }
-          } else t
-        }
-        case _ => t
-      }
-      case ScTypeAliasType(a, s) => aliasesMap.get(a.name) match {
-        case Some(v) => v
-        case None => new ScTypeAliasType(a, s.incl(this))
-      }
-      case ScParameterizedType (des, typeArgs) =>
-        new ScParameterizedType(des, typeArgs map {subst _})
-      case ScExistentialArgument(args, lower, upper) => new ScExistentialArgument(args, subst(lower), subst(upper))
-      case ex@ScExistentialType(q, wildcards) => {
-        //remove bound names 
-        val trunc = aliasesMap.excl(ex.boundNames)
-        new ScExistentialType(new ScSubstitutor(tvMap, outerMap, trunc).subst(q), wildcards)
-      }
-      case _ => t //todo
+  def subst(t: ScType) : ScType = t match {
+    case ScFunctionType(ret, params) => new ScFunctionType(subst(ret), params map (subst _))
+    case ScTupleType(comps) => new ScTupleType(comps map {subst _})
+    case ScProjectionType(proj, name) => new ScProjectionType(subst(proj), name)
+
+    case tv : ScTypeVariable => tvMap.get(tv) match {
+      case None => tv
+      case Some(v) => v
     }
+    case ScDesignatorType(e) if (!e.isInstanceOf[ScTypeAlias]) => e match { //scala ticket 425
+      case c: PsiClass => {
+        val cc = c.getContainingClass
+        if (cc != null) {
+          outerMap.get(cc) match {
+            case Some(ot) => new ScProjectionType(ot, c.getName)
+            case None => t
+          }
+        } else t
+      }
+      case _ => t
+    }
+    case ScTypeAliasType(a, s) => aliasesMap.get(a.name) match {
+      case Some(v) => v
+      case None => new ScTypeAliasType(a, s.incl(this))
+    }
+    case ScParameterizedType (des, typeArgs) =>
+      new ScParameterizedType(des, typeArgs map {subst _})
+    case ScExistentialArgument(args, lower, upper) => new ScExistentialArgument(args, subst(lower), subst(upper))
+    case ex@ScExistentialType(q, wildcards) => {
+      //remove bound names
+      val trunc = aliasesMap.excl(ex.boundNames)
+      new ScExistentialType(new ScSubstitutor(tvMap, outerMap, trunc).subst(q), wildcards)
+    }
+    case _ => t
   }
 }
