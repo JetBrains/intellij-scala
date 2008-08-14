ba639cc0b65f812952de6276cce73c2d1380f846
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-08-14 11:05:27 +0000

    scconstrexpr is really an expr, otherwise it does not get into ScFunction's body
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18543 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/expr/ScConstrExpr.scala
@@ -0,0 +1,3 @@
+package org.jetbrains.plugins.scala.lang.psi.api.expr
+
+trait ScConstrExpr extends ScExpression
---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScConstrExpr.scala
@@ -0,0 +1,13 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.expr
+
+import api.expr.{ScConstrExpr}
+import psi.ScalaPsiElementImpl
+import com.intellij.lang.ASTNode
+/**
+* @author Alexander Podkhalyuzin
+* Date: 22.02.2008
+*/
+
+class ScConstrExprImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScConstrExpr{
+  override def toString: String = "ConstructorExpression"
+}
