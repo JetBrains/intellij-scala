e91c6af132ff9ecbfb113e5e802e229e0ee410f2
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-08-14 10:31:40 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18542 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/statements/ScConstrExprImpl.scala
@@ -0,0 +1,32 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.statements
+
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
+
+
+
+
+
+
+import com.intellij.psi.tree.TokenSet
+import com.intellij.lang.ASTNode
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi._
+
+import org.jetbrains.annotations._
+
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.icons.Icons
+
+
+import org.jetbrains.plugins.scala.lang.psi.api.statements._
+
+/** 
+* @author Alexander Podkhalyuzin
+* Date: 22.02.2008
+*/
+
+class ScConstrExprImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScConstrExpr{
+  override def toString: String = "ConstructorExpression"    
+}
