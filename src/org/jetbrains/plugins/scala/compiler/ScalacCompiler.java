f6fbecdc998452dd4481f4d56c0d5d64dab49c54
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-08-14 14:15:24 +0000

    New scala compiler added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18552 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/compiler/ScalacCompiler.java
@@ -0,0 +1,311 @@
+package org.jetbrains.plugins.scala.compiler;
+
+import com.intellij.compiler.CompilerConfigurationImpl;
+import com.intellij.compiler.OutputParser;
+import com.intellij.compiler.impl.javaCompiler.ExternalCompiler;
+import com.intellij.compiler.impl.javaCompiler.ModuleChunk;
+import com.intellij.compiler.impl.javaCompiler.javac.JavacSettings;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.compiler.CompileContext;
+import com.intellij.openapi.compiler.CompileScope;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.JavaSdkType;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.impl.MockJdkWrapper;
+import com.intellij.openapi.roots.*;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.PathUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.scala.ScalaBundle;
+import org.jetbrains.plugins.scala.ScalaFileType;
+import org.jetbrains.plugins.scala.compiler.rt.ScalacRunner;
+import org.jetbrains.plugins.scala.config.ScalaConfigUtils;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author ilyas
+ */
+public class ScalacCompiler extends ExternalCompiler {
+
+  private static final Logger LOG = Logger.getInstance("#org.jetbrains.plugins.scala.compiler.ScalacCompiler");
+
+  private final Project myProject;
+  private final List<File> myTempFiles = new ArrayList<File>();
+
+  // VM properties
+  private static final String XMX_COMPILER_PROPERTY = "-Xmx300m";
+  private final String XSS_COMPILER_PROPERTY = "-Xss128m";
+
+  // Scalac parameters
+  private static final String DEBUG_INFO_LEVEL_PROPEERTY = "-g:vars";
+  private static final String VERBOSE_PROPERTY = "-verbose";
+  private static final String DESTINATION_COMPILER_PROPERTY = "-d";
+  private static final String DEBUG_PROPERTY = "-Ydebug";
+  private static final String WARNINGS_PROPERTY = "-unchecked";
+
+
+  public ScalacCompiler(Project project) {
+    myProject = project;
+  }
+
+  public boolean checkCompiler(CompileScope scope) {
+    VirtualFile[] files = scope.getFiles(ScalaFileType.SCALA_FILE_TYPE, true);
+    if (files.length == 0) return true;
+
+    final ProjectFileIndex index = ProjectRootManager.getInstance(myProject).getFileIndex();
+    Set<Module> modules = new HashSet<Module>();
+    for (VirtualFile file : files) {
+      Module module = index.getModuleForFile(file);
+      if (module != null) {
+        modules.add(module);
+      }
+    }
+
+    for (Module module : modules) {
+      final String installPath = ScalaConfigUtils.getScalaInstallPath(module);
+      if (installPath.length() == 0) {
+        Messages.showErrorDialog(myProject, ScalaBundle.message("cannot.compile.scala.files.no.facet", module.getName()), ScalaBundle.message("cannot.compile"));
+        return false;
+      }
+    }
+
+    Set<Module> nojdkModules = new HashSet<Module>();
+    for (Module module : scope.getAffectedModules()) {
+      Sdk sdk = ModuleRootManager.getInstance(module).getSdk();
+      if (sdk == null || !(sdk.getSdkType() instanceof JavaSdkType)) {
+        nojdkModules.add(module);
+      }
+    }
+
+    if (!nojdkModules.isEmpty()) {
+      final Module[] noJdkArray = nojdkModules.toArray(new Module[nojdkModules.size()]);
+      if (noJdkArray.length == 1) {
+        Messages.showErrorDialog(myProject, ScalaBundle.message("cannot.compile.scala.files.no.sdk", noJdkArray[0].getName()), ScalaBundle.message("cannot.compile"));
+      } else {
+        StringBuffer modulesList = new StringBuffer();
+        for (int i = 0; i < noJdkArray.length; i++) {
+          if (i > 0) modulesList.append(", ");
+          modulesList.append(noJdkArray[i].getName());
+        }
+        Messages.showErrorDialog(myProject, ScalaBundle.message("cannot.compile.scala.files.no.sdk.mult", modulesList.toString()), ScalaBundle.message("cannot.compile"));
+      }
+      return false;
+    }
+    return true;
+  }
+
+  @NotNull
+  public String getId() {
+    return "Scalac";
+  }
+
+  @NotNull
+  public String getPresentableName() {
+    return ScalaBundle.message("scalac.compiler.name");
+  }
+
+  @NotNull
+  public Configurable createConfigurable() {
+    // todo implement me as for javac!
+    return null;
+  }
+
+  public OutputParser createErrorParser(String outputDir) {
+    return new ScalacOutputParser();
+  }
+
+  public OutputParser createOutputParser(String outputDir) {
+    return null;
+  }
+
+  @NotNull
+  public String[] createStartupCommand(final ModuleChunk chunk, CompileContext context, final String outputPath) throws IOException, IllegalArgumentException {
+    final ArrayList<String> commandLine = new ArrayList<String>();
+    final Exception[] ex = new Exception[]{null};
+    ApplicationManager.getApplication().runReadAction(new Runnable() {
+      public void run() {
+        try {
+          createStartupCommandImpl(chunk, commandLine, outputPath);
+        }
+        catch (IllegalArgumentException e) {
+          ex[0] = e;
+        }
+        catch (IOException e) {
+          ex[0] = e;
+        }
+      }
+    });
+    if (ex[0] != null) {
+      if (ex[0] instanceof IOException) {
+        throw (IOException) ex[0];
+      } else if (ex[0] instanceof IllegalArgumentException) {
+        throw (IllegalArgumentException) ex[0];
+      } else {
+        LOG.error(ex[0]);
+      }
+    }
+    return commandLine.toArray(new String[commandLine.size()]);
+  }
+
+  private void createStartupCommandImpl(ModuleChunk chunk, ArrayList<String> commandLine, String outputPath) throws IOException {
+    final Sdk jdk = getJdkForStartupCommand(chunk);
+    final String versionString = jdk.getVersionString();
+    if (versionString == null || "".equals(versionString)) {
+      throw new IllegalArgumentException(ScalaBundle.message("javac.error.unknown.jdk.version", jdk.getName()));
+    }
+    final JavaSdkType sdkType = (JavaSdkType) jdk.getSdkType();
+
+    final String toolsJarPath = sdkType.getToolsPath(jdk);
+    if (toolsJarPath == null) {
+      throw new IllegalArgumentException(ScalaBundle.message("javac.error.tools.jar.missing", jdk.getName()));
+    }
+    if (!allModulesHaveSameScalaSdk(chunk)) {
+      throw new IllegalArgumentException(ScalaBundle.message("different.scala.sdk.in.modules"));
+    }
+
+    String javaExecutablePath = sdkType.getVMExecutablePath(jdk);
+    commandLine.add(javaExecutablePath);
+
+    //todo setup via ScalacSettings
+    commandLine.add("-cp");
+
+    String rtJarPath = PathUtil.getJarPathForClass(ScalacRunner.class);
+    final StringBuilder classPathBuilder = new StringBuilder();
+    classPathBuilder.append(rtJarPath).append(File.pathSeparator);
+    classPathBuilder.append(sdkType.getToolsPath(jdk)).append(File.pathSeparator);
+
+    String scalaPath = ScalaConfigUtils.getScalaInstallPath(chunk.getModules()[0]);
+    String libPath = (scalaPath + "/lib").replace(File.separatorChar, '/');
+
+    VirtualFile lib = LocalFileSystem.getInstance().findFileByPath(libPath);
+    if (lib != null) {
+      for (VirtualFile file : lib.getChildren()) {
+        if (required(file.getName())) {
+          classPathBuilder.append(file.getPath());
+          classPathBuilder.append(File.pathSeparator);
+        }
+      }
+    }
+
+    commandLine.add(classPathBuilder.toString());
+    commandLine.add(ScalacRunner.class.getName());
+
+
+    try {
+      File fileWithParams = File.createTempFile("scalac", ".tmp");
+      fillFileWithScalacParams(chunk, fileWithParams, outputPath);
+
+      commandLine.add(fileWithParams.getPath());
+    } catch (IOException e) {
+      LOG.error(e);
+    }
+  }
+
+
+  private void fillFileWithScalacParams(ModuleChunk chunk, File fileWithParameters, String outputPath)
+          throws FileNotFoundException {
+
+    PrintStream printer = new PrintStream(new FileOutputStream(fileWithParameters));
+
+    printer.println(VERBOSE_PROPERTY);
+    printer.println(DEBUG_PROPERTY);
+    printer.println(WARNINGS_PROPERTY);
+    printer.println(DEBUG_INFO_LEVEL_PROPEERTY);
+    printer.println(DESTINATION_COMPILER_PROPERTY);
+    printer.println(outputPath.replace('/', File.separatorChar));
+
+    //write classpath
+    printer.println("-cp");
+
+    for (Module module : chunk.getModules()) {
+      ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+      OrderEntry[] entries = moduleRootManager.getOrderEntries();
+      Set<VirtualFile> cpVFiles = new HashSet<VirtualFile>();
+      for (OrderEntry orderEntry : entries) {
+        cpVFiles.addAll(Arrays.asList(orderEntry.getFiles(OrderRootType.COMPILATION_CLASSES)));
+      }
+      for (VirtualFile file : cpVFiles) {
+        String path = file.getPath();
+        int jarSeparatorIndex = path.indexOf(JarFileSystem.JAR_SEPARATOR);
+        if (jarSeparatorIndex > 0) {
+          path = path.substring(0, jarSeparatorIndex);
+        }
+        printer.print(path);
+        printer.print(File.pathSeparator);
+      }
+    }
+    printer.println();
+    for (VirtualFile file : chunk.getFilesToCompile()) {
+      printer.println(file.getPath());
+    }
+    printer.close();
+  }
+
+
+  private boolean allModulesHaveSameScalaSdk(ModuleChunk chunk) {
+    if (chunk.getModuleCount() == 0) return false;
+    final Module[] modules = chunk.getModules();
+    final Module first = modules[0];
+    final String firstVersion = ScalaConfigUtils.getScalaVersion(ScalaConfigUtils.getScalaInstallPath(first));
+
+    for (Module module : modules) {
+      final String path = ScalaConfigUtils.getScalaInstallPath(module);
+      final String version = ScalaConfigUtils.getScalaVersion(path);
+      if (!version.equals(firstVersion)) return false;
+    }
+    return true;
+  }
+
+  public void compileFinished() {
+    FileUtil.asyncDelete(myTempFiles);
+  }
+
+  private Sdk getJdkForStartupCommand(final ModuleChunk chunk) {
+    final Sdk jdk = chunk.getJdk();
+    if (ApplicationManager.getApplication().isUnitTestMode() && JavacSettings.getInstance(myProject).isTestsUseExternalCompiler()) {
+      final String jdkHomePath = CompilerConfigurationImpl.getTestsExternalCompilerHome();
+      if (jdkHomePath == null) {
+        throw new IllegalArgumentException("[TEST-MODE] Cannot determine home directory for JDK to use javac from");
+      }
+      // when running under Mock JDK use VM executable from the JDK on which the tests run
+      return new MockJdkWrapper(jdkHomePath, jdk);
+    }
+    return jdk;
+  }
+
+  static HashSet<String> required = new HashSet<String>();
+
+  static {
+    required.add("scala");
+    required.add("sbaz");
+  }
+
+  private static boolean required(String name) {
+    name = name.toLowerCase();
+    if (!name.endsWith(".jar"))
+      return false;
+
+    name = name.substring(0, name.lastIndexOf('.'));
+    int ind = name.lastIndexOf('-');
+    if (ind != -1 && name.length() > ind + 1 && Character.isDigit(name.charAt(ind + 1))) {
+      name = name.substring(0, ind);
+    }
+
+    for (String requiredStr : required) {
+      if (name.contains(requiredStr)) return true;
+    }
+
+    return false;
+  }
+
+
+}
---
+++ src/org/jetbrains/plugins/scala/compiler/ScalacOutputParser.java
@@ -0,0 +1,140 @@
+package org.jetbrains.plugins.scala.compiler;
+
+import com.intellij.compiler.OutputParser;
+import com.intellij.openapi.compiler.CompilerMessageCategory;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import static org.jetbrains.plugins.scala.compiler.ScalacOutputParser.MESSAGE_TYPE.*;
+
+import java.io.File;
+
+/**
+ * @author ilyas
+ */
+class ScalacOutputParser extends OutputParser {
+
+  private static final String ourErrorMarker = " error:";
+  private static final String ourWarningMarker = " warning:";
+
+  private static final String ourInfoMarkerStart = "[";
+  private static final String ourInfoMarkerEnd = "]";
+  private static final String ourWroteMarker = "wrote ";
+  private static final String ourColumnMarker = "^";
+  private static final String ourParsingMarker = "parsing";
+  private static final String ourScalaInternalErrorMsg = "Scalac internal error";
+
+  // Phases
+  private static final String PHASE = "running phase ";
+  private boolean mustProcessMsg = false;
+  private boolean stopProcessing = false;
+  private int myMsgColumnMarker;
+  private MESSAGE_TYPE myMsgType = PLAIN;
+
+  static enum MESSAGE_TYPE {
+    ERROR, WARNING, PLAIN
+  }
+
+  private Integer myLineNumber;
+  private String myMessage;
+  private String myUrl;
+
+  @Override
+  public boolean processMessageLine(Callback callback) {
+    final String line = callback.getNextLine();
+    if (line == null) {
+      return false;
+    }
+
+    String text = line.trim();
+    if (text.endsWith("\r\n")) text = text.substring(0, text.length() - 2);
+
+    if (text.startsWith(ourScalaInternalErrorMsg)) {
+      callback.message(CompilerMessageCategory.ERROR, ourScalaInternalErrorMsg, "", 0, 0);
+      return false;
+    }
+
+    // Add error message to output
+    if (myMessage != null && stopMsgProcessing(text) && (mustProcessMsg || stopProcessing)) {
+      myMsgColumnMarker = myMsgColumnMarker > 0 ? myMsgColumnMarker : 1;
+      if (myMsgType == ERROR) {
+        callback.message(CompilerMessageCategory.ERROR, myMessage, myUrl, myLineNumber, myMsgColumnMarker);
+      } else if (myMsgType == WARNING) {
+        callback.message(CompilerMessageCategory.WARNING, myMessage, myUrl, myLineNumber, myMsgColumnMarker);
+      }
+      myMessage = null;
+      myMsgType = PLAIN;
+      mustProcessMsg = false;
+      stopProcessing = false;
+    }
+
+    if (text.indexOf(ourErrorMarker) > 0) { // new error occurred
+      processErrorMesssage(text, text.indexOf(ourErrorMarker), ERROR, callback);
+    } else if (text.indexOf(ourWarningMarker) > 0) {
+      processErrorMesssage(text, text.indexOf(ourWarningMarker), WARNING, callback);
+    } else if (!text.startsWith(ourInfoMarkerStart)) { //  continuing process [error | warning] message
+      if (mustProcessMsg) {
+        if (ourColumnMarker.equals(text.trim())) {
+          myMsgColumnMarker = line.indexOf(ourColumnMarker) + 1;
+          stopProcessing = true;
+        } else if (myMessage != null) {
+          if (myMsgType != WARNING) {
+            myMessage += "\n" + text;
+          }
+        } else {
+          mustProcessMsg = false;
+        }
+      }
+    } else { //verbose compiler output
+      mustProcessMsg = false;
+      if (text.endsWith(ourInfoMarkerEnd)) {
+        String info = text.substring(ourInfoMarkerStart.length(), text.length() - ourInfoMarkerEnd.length());
+        if (info.startsWith(ourParsingMarker)) { //parsing
+          callback.setProgressText(info);
+        } else if (info.startsWith(PHASE)) { // typechecker phase
+          callback.setProgressText(info);
+        } else if (info.startsWith(ourWroteMarker)) {
+          callback.setProgressText(info);
+          String outputPath = info.substring(ourWroteMarker.length());
+          final String path = outputPath.replace(File.separatorChar, '/');
+          callback.fileGenerated(path);
+        }
+      }
+    }
+    return true;
+  }
+
+  private boolean stopMsgProcessing(String text) {
+    return text.startsWith(ourInfoMarkerStart) && !text.trim().equals(ourColumnMarker) ||
+            text.indexOf(ourErrorMarker) > 0 ||
+            text.indexOf(ourWarningMarker) > 0 ||
+            stopProcessing;
+  }
+
+  /*
+ Collect information about error occurrence
+  */
+  private void processErrorMesssage(String text, int errIndex, MESSAGE_TYPE msgType, Callback callback) {
+    String errorPlace = text.substring(0, errIndex);
+    if (errorPlace.endsWith(":"))
+      errorPlace = errorPlace.substring(0, errorPlace.length() - 1); //hack over compiler output
+    int j = errorPlace.lastIndexOf(':');
+    if (j > 0) {
+      myUrl = VirtualFileManager.constructUrl(LocalFileSystem.PROTOCOL, errorPlace.substring(0, j).replace(File.separatorChar, '/'));
+      try {
+        myLineNumber = Integer.valueOf(errorPlace.substring(j + 1, errorPlace.length()));
+        myMessage = text.substring(errIndex + 1).trim();
+        mustProcessMsg = true;
+        myMsgType = msgType;
+      } catch (NumberFormatException e) {
+        callback.message(CompilerMessageCategory.INFORMATION, "", text, -1, -1);
+        myMessage = null;
+        mustProcessMsg = false;
+        myMsgType = PLAIN;
+      }
+    } else {
+      callback.message(CompilerMessageCategory.INFORMATION, "", text, -1, -1);
+      myMsgType = PLAIN;
+    }
+  }
+
+}
