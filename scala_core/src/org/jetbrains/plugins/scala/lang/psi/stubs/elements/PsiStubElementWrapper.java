9042ab635c0c92c05321ee1a0ef87f354b8066b1
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-08-16 15:22:20 +0000

    test
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18597 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/PsiStubElementWrapper.java
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/PsiStubElementWrapper.java
@@ -1,18 +1,27 @@
 package org.jetbrains.plugins.scala.lang.psi.stubs.elements;
 
 import com.intellij.extapi.psi.StubBasedPsiElementBase;
+import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.psi.stubs.StubElement;
+import scala.ScalaObject;
+
+import java.rmi.RemoteException;
 
 /**
  * @author ilyas
  */
-public class PsiStubElementWrapper<T extends StubElement<? extends PsiElement>>
-        extends StubBasedPsiElementBase<T> {
+public abstract class PsiStubElementWrapper<StubT extends StubElement<PsiT>, PsiT extends PsiElement>
+        extends StubBasedPsiElementBase<StubT> implements ScalaObject{
 
-  public PsiStubElementWrapper(T stub, IStubElementType<?, ?> nodeType) {
+  protected PsiStubElementWrapper(StubT stub, IStubElementType<StubT, PsiT> nodeType) {
     super(stub, nodeType);
   }
 
+  public PsiStubElementWrapper(ASTNode node) {
+    super(node);
+  }
+
+  public abstract int $tag() throws RemoteException;
 }
