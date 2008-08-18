7294e4770d45056ee83ac748f7648546115b217e
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-08-18 13:56:45 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18611 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScThisReferenceExpressionImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScThisReferenceExpressionImpl.scala
@@ -1,9 +1,9 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.expr
 
-import types.{ScDesignatorType, ScCompoundType, Nothing}
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
+import types.{Bounds, ScDesignatorType, ScCompoundType, Nothing}
+import psi.ScalaPsiElementImpl
+import api.expr._
 import com.intellij.lang.ASTNode
-import org.jetbrains.plugins.scala.lang.psi.api.expr._
 
 /** 
 * @author Alexander Podkhalyuzin
@@ -17,7 +17,7 @@ class ScThisReferenceImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with 
     case Some(td) => {
       val des = new ScDesignatorType(td)
       td.selfType match {
-        case Some(t) => new ScCompoundType(Array(des, t), Seq.empty, Seq.empty)
+        case Some(t) => Bounds.glb(des, t)
         case None => des
       }
     }
