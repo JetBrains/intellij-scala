1c6d34fcf7896859a8e90d8007ff9c21ab823eb2
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-08-18 14:03:41 +0000

    trivia. to be cont.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18612 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/Bounds.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/Bounds.scala
@@ -7,9 +7,18 @@ object Bounds {
     else new ScCompoundType(Array(t1, t2), Seq.empty, Seq.empty) 
   }
 
-  def lub(t1 : ScType, t2 : ScType) = {
+  def lub(t1 : ScType, t2 : ScType) : ScType = {
     if (t1.conforms(t2)) t2
     else if (t2.conforms(t1)) t1
-    else Any //todo 
+    else (t1, t2) match {
+      case (ScTypeVariable(_, Nil, _, upper), _) => lub(upper, t2)
+      case (_, ScTypeVariable(_, Nil, _, upper)) => lub(t1, upper)
+      case (ScTypeAliasType(_, Nil, _, upper), _) => lub(upper, t2)
+      case (_, ScTypeAliasType(_, Nil, _, upper)) => lub(t1, upper)
+      case (s: ScSingletonType, _) => lub(s.pathType, t2)
+      case (_, s: ScSingletonType) => lub(t1, s.pathType)
+
+      case _ => Any //todo
+    }
   }
 }
