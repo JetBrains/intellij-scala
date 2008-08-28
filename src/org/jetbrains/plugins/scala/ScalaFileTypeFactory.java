29c2c4abea54a407b3559c5953813cb85c6c728a
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-08-28 17:17:22 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18815 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/ScalaFileTypeFactory.java
+++ src/org/jetbrains/plugins/scala/ScalaFileTypeFactory.java
@@ -15,20 +15,15 @@
 
 package org.jetbrains.plugins.scala;
 
-import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
-import com.intellij.util.PairConsumer;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author ilyas
  */
 public class ScalaFileTypeFactory extends FileTypeFactory {
-  public void createFileTypes(final @NotNull PairConsumer<FileType, String> consumer) {
-    consumer.consume(ScalaFileType.SCALA_FILE_TYPE, "scala");
-  }
-  //todo Uncomment after 28th August 2008 :) 
-  /*public void createFileTypes(final @NotNull FileTypeConsumer consumer) {
+  public void createFileTypes(final @NotNull FileTypeConsumer consumer) {
     consumer.consume(ScalaFileType.SCALA_FILE_TYPE, ScalaFileType.DEFAULT_EXTENSION);
-  }*/
+  }
 }
