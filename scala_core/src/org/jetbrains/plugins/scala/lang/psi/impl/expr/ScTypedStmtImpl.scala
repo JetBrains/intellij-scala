3e755ef1bc0c4c69327cb9f96baf45c55eec8d4a
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-08-30 07:01:41 +0000

    SCL-252
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18846 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScTypedStmtImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScTypedStmtImpl.scala
@@ -29,4 +29,9 @@ import org.jetbrains.plugins.scala.lang.psi.api.expr._
 
 class ScTypedStmtImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScTypedStmt {
   override def toString: String = "TypedStatement"
+
+  override def getType = typeElement match {
+    case Some(te) => te.getType
+    case None => expr.getType
+  }
 }
