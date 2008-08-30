6098ae785c51e6acc53b8d9ea297cfdf6db2dd33
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-08-30 07:15:05 +0000

    SCL-252
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@18847 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScTypedStmtImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScTypedStmtImpl.scala
@@ -1,26 +1,9 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.expr
 
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
+import api.expr._
+import psi.ScalaPsiElementImpl
 
-
-
-
-
-
-import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi._
-
-import org.jetbrains.annotations._
-
-import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
-import org.jetbrains.plugins.scala.icons.Icons
-
-
-import org.jetbrains.plugins.scala.lang.psi.api.expr._
 
 /** 
 * @author Alexander Podkhalyuzin
