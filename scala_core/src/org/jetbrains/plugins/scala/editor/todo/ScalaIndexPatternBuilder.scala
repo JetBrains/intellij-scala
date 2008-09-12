a78b683cc5f7dc86b61e176f408ec27b76628114
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-09-12 13:45:55 +0000

    todo highlighting added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19130 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/editor/todo/ScalaIndexPatternBuilder.scala
@@ -0,0 +1,30 @@
+package org.jetbrains.plugins.scala.editor.todo
+
+import com.intellij.lexer.Lexer
+import com.intellij.psi.impl.search.IndexPatternBuilder
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.{TokenSet, IElementType}
+import lang.lexer.{ScalaLexer, ScalaTokenTypes}
+import lang.psi.ScalaFile
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 12.09.2008
+ */
+
+class ScalaIndexPatternBuilder extends IndexPatternBuilder {
+  def getIndexingLexer(file: PsiFile): Lexer = {
+    file match {
+      case _: ScalaFile => new ScalaLexer
+      case _ => null
+    }
+  }
+  def getCommentTokenSet(file: PsiFile): TokenSet = {
+    file match {
+      case _: ScalaFile => ScalaTokenTypes.COMMENTS_TOKEN_SET
+      case _ => null
+    }
+  }
+  def getCommentStartDelta(tokenType: IElementType) = 0
+  def getCommentEndDelta(tokenType: IElementType) = 0
+}
