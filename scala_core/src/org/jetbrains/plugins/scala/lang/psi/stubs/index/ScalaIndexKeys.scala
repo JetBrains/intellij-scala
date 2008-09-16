4b1bc08e30e7f678e17e5734b778a2e2e860d169
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-09-16 17:00:57 +0000

    package PSI rewrited in Scala
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19203 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/stubs/index/ScalaIndexKeys.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.lang.psi.stubs.index
+import api.toplevel.packaging.ScPackageContainer
+import com.intellij.psi.PsiClass
+import com.intellij.psi.stubs.StubIndexKey
+
+/**
+ * @author ilyas
+ */
+
+object ScalaIndexKeys {
+
+  val SHORT_NAME_KEY: StubIndexKey[String, PsiClass]  = StubIndexKey.createIndexKey("sc.class.shortName")
+  val FQN_KEY: StubIndexKey[java.lang.Integer, PsiClass]  = StubIndexKey.createIndexKey("sc.class.fqn")
+  val PACKAGE_FQN_KEY: StubIndexKey[java.lang.Integer, ScPackageContainer]  = StubIndexKey.createIndexKey("sc.package.fqn")
+
+}
