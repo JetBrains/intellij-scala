86b7344c701205a4ba52bf6eca5f73fb744407f5
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-09-21 11:02:10 +0000

    OverrideImplement: prev value and variable functionality restored
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19334 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/overrideImplement/PsiAliasMember.scala
+++ scala_core/src/org/jetbrains/plugins/scala/overrideImplement/PsiAliasMember.scala
@@ -1,13 +1,12 @@
 package org.jetbrains.plugins.scala.overrideImplement
 
+import _root_.org.jetbrains.plugins.scala.lang.psi.types.{ScType, PhysicalSignature, ScSubstitutor}
 import com.intellij.psi.{PsiMethod, PsiSubstitutor}
 import com.intellij.codeInsight.generation.PsiElementClassMember
 import com.intellij.codeInsight.generation.MemberChooserObject
 import com.intellij.codeInsight.generation.ClassMember
 import lang.psi.api.statements.{ScValue, ScTypeAlias, ScVariable}
 import lang.psi.api.toplevel.ScTyped
-import lang.psi.types.PhysicalSignature
-
 /**
 * User: Alexander Podkhalyuzin
 * Date: 11.07.2008
@@ -17,8 +16,8 @@ class ScAliasMember(member: ScTypeAlias) extends PsiElementClassMember[ScTypeAli
 
 class ScMethodMember(val sign: PhysicalSignature) extends PsiElementClassMember[PsiMethod](sign.method, sign.method.getName)
 
-class ScValueMember(member: ScValue, val element: ScTyped) extends PsiElementClassMember[ScValue](member,
-  element.name/*format with type*/)
+class ScValueMember(member: ScValue, val element: ScTyped, val substitutor: ScSubstitutor) extends PsiElementClassMember[ScValue](member,
+  element.name + ": " + ScType.presentableText(substitutor.subst(element.calcType)))
 
-class ScVariableMember(member: ScVariable, val element: ScTyped) extends PsiElementClassMember[ScVariable](member,
-  element.name/*format with type*/)
+class ScVariableMember(member: ScVariable, val element: ScTyped, val substitutor: ScSubstitutor) extends PsiElementClassMember[ScVariable](member,
+  element.name + ": " + ScType.presentableText(substitutor.subst(element.calcType)))
