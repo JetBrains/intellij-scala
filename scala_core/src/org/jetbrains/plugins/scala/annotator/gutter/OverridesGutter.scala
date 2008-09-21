30cfce85567ddead48cec8db16dcd13219197565
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-09-21 12:10:16 +0000

    OverrideImplement: fixed bug (MatchError)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19340 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/annotator/gutter/OverridesGutter.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.annotator.gutter
+
+import com.intellij.openapi.editor.markup.GutterIconRenderer
+import javax.swing.Icon
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 21.09.2008
+ */
+
+//class OverridesGutter extends GutterIconRenderer{
+//}
