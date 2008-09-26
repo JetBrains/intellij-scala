5adcf2c1e0082868caaabe489889dd5afc41b681
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-09-26 09:52:32 +0000

    Added mnemonics to IV dialog fixed comments bug in surroundWith feature
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19466 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/expression/ScalaExpressionSurrounder.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/expression/ScalaExpressionSurrounder.scala
@@ -6,17 +6,18 @@ package org.jetbrains.plugins.scala.lang.surroundWith.surrounders.expression
  *
  */
 
-import com.intellij.lang.surroundWith.Surrounder;
+import com.intellij.lang.surroundWith.Surrounder
+import com.intellij.psi.{PsiElement, PsiWhiteSpace};
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.psi.PsiElement;
+
 import com.intellij.openapi.util.TextRange
 import org.jetbrains.plugins.scala.util.DebugPrint
 import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.parser.parsing.expressions.Expr
-import com.intellij.psi.PsiWhiteSpace;
+
 
 
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
@@ -30,11 +31,13 @@ import lang.psi.api.statements._
 abstract class ScalaExpressionSurrounder extends ScalaSurrounderByExpression {
   override def isApplicable(element : PsiElement) : Boolean = {
     element match {
-      case _ : ScExpression | _: PsiWhiteSpace | _: ScValue | _: ScVariable => {
+      case _ : ScExpression | _: PsiWhiteSpace | _: ScValue | _: ScVariable  => {
         true
       }
       case e => {
-        e.getNode.getElementType == ScalaTokenTypes.tLINE_TERMINATOR
+        if (e.getNode.getElementType == ScalaTokenTypes.tLINE_TERMINATOR) true
+        else if (ScalaTokenTypes.COMMENTS_TOKEN_SET contains e.getNode.getElementType) true
+        else false
       }
     }
   }
