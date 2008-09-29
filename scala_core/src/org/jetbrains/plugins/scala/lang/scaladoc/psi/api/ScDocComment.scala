bf726ee252c6b5b1f0508670337c9c3f3a1f7851
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-09-29 12:21:43 +0000

    starting to fix docComment enterAction
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19528 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/api/ScDocComment.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/api/ScDocComment.scala
@@ -2,12 +2,13 @@ package org.jetbrains.plugins.scala.lang.scaladoc.psi.api
 
 
 import _root_.org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
+import com.intellij.psi.PsiComment
 
 /**
 * User: Alexander Podkhalyuzin
 * Date: 22.07.2008
 */
 
-trait ScDocComment extends ScalaPsiElement {
+trait ScDocComment extends PsiComment with ScalaPsiElement {
 
 }
