6e73e5d13b0392d1ca8f6363293aec1037b8eca0
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-09-29 12:46:07 +0000

    fixed build
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19529 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/impl/DocComment.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/impl/DocComment.scala
@@ -4,6 +4,9 @@ package org.jetbrains.plugins.scala.lang.scaladoc.psi.impl
 import com.intellij.lang.ASTNode
 import _root_.org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
 import api.ScDocComment
+import com.intellij.psi.tree.IElementType
+import lexer.ScalaDocTokenType
+import parser.ScalaDocElementTypes
 
 /**
  * User: Alexander Podkhalyuzin
@@ -11,5 +14,6 @@ import api.ScDocComment
  */
  
 class ScDocCommentImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScDocComment{
+  def getTokenType: IElementType = ScalaDocElementTypes.SCALA_DOC_COMMENT
   override def toString: String = "DocComment"
 }
