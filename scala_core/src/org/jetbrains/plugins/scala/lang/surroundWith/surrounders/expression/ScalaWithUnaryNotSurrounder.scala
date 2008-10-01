ba52be6d1e8f05894a4aa42de306dcbfe02b32c4
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-01 09:37:39 +0000

    Unary not surrounder
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19578 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/expression/ScalaWithUnaryNotSurrounder.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/expression/ScalaWithUnaryNotSurrounder.scala
@@ -1,10 +1,30 @@
 package org.jetbrains.plugins.scala.lang.surroundWith.surrounders.expression
 
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import psi.api.expr.ScExpression
+
 /**
  * User: Alexander Podkhalyuzin
  * Date: 29.09.2008
  */
 
-class ScalaWithUnaryNotSurrounder {
-
+class ScalaWithUnaryNotSurrounder extends ScalaExpressionSurrounder {
+  override def getTemplateAsString(elements: Array[PsiElement]): String =
+    "!(" + super.getTemplateAsString(elements) + ")"
+  override def getExpressionTemplateAsString(exprString: ASTNode): String = "!(" + exprString.getText + ")"
+  override def getTemplateDescription: String = "!(expr)"
+  override def isApplicable(elements: Array[PsiElement]): Boolean = {
+    if (elements.length != 1) return false
+    elements(0) match {
+      case x: ScExpression if x.getType == psi.types.Boolean => return true
+      case _ => return false
+    }
+  }
+  override def getSurroundSelectionRange(withUnaryNot: ASTNode): TextRange = {
+    val expr: ScExpression = withUnaryNot.getPsi.asInstanceOf[ScExpression]
+    val offset = expr.getTextRange.getEndOffset
+    new TextRange(offset,offset)
+  }
 }
