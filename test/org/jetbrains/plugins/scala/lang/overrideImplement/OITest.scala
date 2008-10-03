7bd340986b5676df49c7e5acc45fc943900ef0b4
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-03 13:05:01 +0000

    OITest logic moved to scala
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19666 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/lang/overrideImplement/OITest.scala
@@ -0,0 +1,61 @@
+package org.jetbrains.plugins.scala.lang.overrideImplement
+
+import _root_.junit.framework.Test
+import _root_.org.jetbrains.plugins.scala.overrideImplement.ScalaOIUtil
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.codeStyle.CodeStyleManager
+import psi.api.ScalaFile
+import testcases.BaseScalaFileSetTestCase
+import util.TestUtils
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 03.10.2008
+ */
+
+class OverrideImplementTestUtil {
+  private val CARET_MARKER = "<caret>"
+  private def removeMarker(text: String): String = {
+    val index = text.indexOf(CARET_MARKER);
+    return text.substring(0, index) + text.substring(index + CARET_MARKER.length());
+  }
+
+  /*
+   *  File must be like:
+   *  implement (or override) + " " +  methodName
+   *  <typeDefinition>
+   *  Use <caret> to specify caret position.
+   */
+  def transform(myProject: Project, testName: String, data: Array[String]): String = {
+    val text = data(0)
+    val i = text.indexOf("\n")
+    val info = text.substring(0, i)
+    val isImplement = info.split(" ")(0).equals("implement")
+    val methodName = info.split(" ")(1)
+    var fileText = text.substring(i + 1)
+    val offset = fileText.indexOf(CARET_MARKER)
+    fileText = removeMarker(fileText)
+    val file = TestUtils.createPseudoPhysicalScalaFile(myProject, fileText).asInstanceOf[ScalaFile]
+    val clazz = file.getTypeDefinitions()(0)
+    val method = ScalaOIUtil.getMethod(clazz, methodName, isImplement)
+    val runnable = new Runnable() {
+      def run() {
+        clazz.addMember(method, None: Option[Editor], offset);
+        val myTextRange = file.getTextRange();
+        CodeStyleManager.getInstance(myProject).reformatText(file, myTextRange.getStartOffset(), myTextRange.getEndOffset());
+      }
+    };
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      def run() {
+        CommandProcessor.getInstance().executeCommand(myProject, runnable, "test", null);
+      }
+    });
+    System.out.println("------------------------ " + testName + " ------------------------");
+    System.out.println(file.getText());
+    System.out.println("");
+    return file.getText();
+  }
+}
--- test/org/jetbrains/plugins/scala/lang/overrideImplement/OverrideImplementTest.java
+++ test/org/jetbrains/plugins/scala/lang/overrideImplement/OverrideImplementTest.java
@@ -1,18 +1,8 @@
 package org.jetbrains.plugins.scala.lang.overrideImplement;
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.util.IncorrectOperationException;
 import junit.framework.Test;
 import org.jetbrains.annotations.NonNls;
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile;
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition;
-import org.jetbrains.plugins.scala.overrideImplement.ScalaOIUtil;
 import org.jetbrains.plugins.scala.testcases.BaseScalaFileSetTestCase;
-import org.jetbrains.plugins.scala.util.TestUtils;
 import scala.None$;
 
 /**
@@ -22,7 +12,6 @@ import scala.None$;
 public class OverrideImplementTest extends BaseScalaFileSetTestCase {
   @NonNls
   private static final String DATA_PATH = "test/org/jetbrains/plugins/scala/lang/overrideImplement/data";
-  private int offset;
   private static final String CARET_MARKER = "<caret>";
 
   private String removeMarker(String text) {
@@ -38,43 +27,13 @@ public class OverrideImplementTest extends BaseScalaFileSetTestCase {
   }
 
   /*
-   *  File must be like:
-   *  implement (or override) + " " +  methodName
-   *  <typeDefinition>
-   *  Use <caret> to specify caret position.
-   */
+  *  File must be like:
+  *  implement (or override) + " " +  methodName
+  *  <typeDefinition>
+  *  Use <caret> to specify caret position.
+  */
   public String transform(String testName, String[] data) throws Exception {
-    String text = data[0];
-    final int i = text.indexOf("\n");
-    String info = text.substring(0, i);
-    boolean isImplement = info.split(" ")[0].equals("implement");
-    String methodName = info.split(" ")[1];
-    String fileText = text.substring(i + 1);
-    final int offset = fileText.indexOf(CARET_MARKER);
-    fileText = removeMarker(fileText);
-    final ScalaFile file = (ScalaFile) TestUtils.createPseudoPhysicalScalaFile(myProject, fileText);
-    final ScTypeDefinition clazz = file.getTypeDefinitions()[0];
-    final PsiElement method = ScalaOIUtil.getMethod(clazz, methodName, isImplement);
-    final Runnable runnable = new Runnable() {
-      public void run() {
-        clazz.addMember(method, new None$(), offset);
-        try {
-          TextRange myTextRange = file.getTextRange();
-          CodeStyleManager.getInstance(myProject).reformatText(file, myTextRange.getStartOffset(), myTextRange.getEndOffset());
-        } catch (IncorrectOperationException e) {
-          e.printStackTrace();
-        }
-      }
-    };
-    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-      public void run() {
-        CommandProcessor.getInstance().executeCommand(myProject, runnable, "test", null);
-      }
-    });
-    System.out.println("------------------------ " + testName + " ------------------------");
-    System.out.println(file.getText());
-    System.out.println("");
-    return file.getText();
+    return (new OverrideImplementTestUtil()).transform(myProject, testName, data);
   }
 
   public static Test suite() {
