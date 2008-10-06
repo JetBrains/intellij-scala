e46f2d36317d9933050c059c61b1c4deaffaea41
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-06 13:09:35 +0000

    ctrl+shift+F7 highlighting fixed
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19743 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/highlighter/readWriteAccess/ScalaReadWriteAccessDetector.scala
@@ -0,0 +1,63 @@
+package org.jetbrains.plugins.scala.highlighter.readWriteAccess
+
+import com.intellij.codeInsight.highlighting.ReadWriteAccessDetector
+import com.intellij.codeInsight.highlighting.ReadWriteAccessDetector.Access
+import com.intellij.psi.{PsiElement, PsiReference, PsiNamedElement}
+import lang.psi.api.expr.{ScAssignStmt, ScExpression}
+import lang.psi.api.statements.{ScValue, ScVariableDefinition, ScPatternDefinition, ScVariable}
+import lang.psi.ScalaPsiUtil
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 06.10.2008
+ */
+
+class ScalaReadWriteAccessDetector extends ReadWriteAccessDetector {
+  def getExpressionAccess(expression: PsiElement): Access = {
+    expression match {
+      case expression: ScExpression => {
+        val readAccess = ScalaReadWriteAccessDetector.isAccessedForReading(expression)
+        if (readAccess) Access.Read
+        else Access.Write
+      }
+      case _ => Access.Read
+    }
+  }
+  def isReadWriteAccessible(element: PsiElement): Boolean = {
+    element match {
+      case x: PsiNamedElement => {
+         ScalaPsiUtil.nameContext(x) match {
+           case null => false
+           case _ => true
+         }
+      }
+      case _ => false
+    }
+  }
+  def getReferenceAccess(referencedElement: PsiElement, reference: PsiReference): Access =
+    getExpressionAccess(reference.getElement)
+  def isDeclarationWriteAccess(element: PsiElement): Boolean = {
+    element match {
+      case x: PsiNamedElement => {
+         ScalaPsiUtil.nameContext(x) match {
+           case _: ScVariableDefinition | _: ScPatternDefinition => true
+           case _ => false
+         }
+      }
+      case _ => false
+    }
+  }
+}
+
+private object ScalaReadWriteAccessDetector {
+  def isAccessedForReading(expression: ScExpression): Boolean = {
+    val parent = expression.getParent
+    return !parent.isInstanceOf[ScAssignStmt] || !(expression == parent.asInstanceOf[ScAssignStmt].getLExpression)
+  }
+
+  //Now it's just inverse prev method
+  def isAccessedForWriting(expression: ScExpression): Boolean = {
+    val parent = expression.getParent
+    return parent.isInstanceOf[ScAssignStmt] && (expression == parent.asInstanceOf[ScAssignStmt].getLExpression)
+  }
+}
