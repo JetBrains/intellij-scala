5feb4bb8a61ef3f44cab1be5fb03f59a63942480
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-10-08 18:36:22 +0000

    SCL-353 test
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19913 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/lang/resolve/ResolveCallTest.java
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.scala.lang.resolve;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction;
+import org.jetbrains.plugins.scala.util.TestUtils;
+
+/**
+ * @author ven
+ */
+public class ResolveCallTest extends ScalaResolveTestCase {
+  protected String getTestDataPath() {
+    return TestUtils.getTestDataPath() + "/resolve/";
+  }
+
+  public void testObjectApply() throws Exception {
+    PsiReference ref = configureByFile("call/objectApply.scala");
+    PsiElement resolved = ref.resolve();
+    assertTrue(resolved instanceof ScFunction);
+    assertEquals("apply", ((ScFunction) resolved).getName());
+  }
+
+}
