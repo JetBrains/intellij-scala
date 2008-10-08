f44868ca159b6b84b40cc5c72a16c8d4fa3959e7
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-10-08 17:56:31 +0000

    compilation
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19912 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/compiled/ScClsTypeDefinitionImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/compiled/ScClsTypeDefinitionImpl.scala
@@ -76,11 +76,11 @@ extends ClsClassImpl(stub) with ScTypeDefinition {
         case "" => "<default>"
         case p => '(' + p + ')'
       }
-      override def getIcon(open: Boolean) = ScClsTypeDefinitionImpl.this.getIcon
+      override def getIcon(open: Boolean) = ScClsTypeDefinitionImpl.this.getIcon(0)
     }
   }
 
-  override def getIcon = _type match {
+  override def getIcon(flags : Int) = _type match {
     case CLASS => Icons.CLASS
     case OBJECT => Icons.OBJECT
     case TRAIT => Icons.TRAIT
