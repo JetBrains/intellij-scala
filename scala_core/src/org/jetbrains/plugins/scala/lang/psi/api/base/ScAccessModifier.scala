bb8e33326b18a211bd1c53e43776dcab60c7448c
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-10-08 10:40:53 +0000

    access modifier psi
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@19877 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/base/ScAccessModifier.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/base/ScAccessModifier.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.lang.psi.api.base
 
+import com.intellij.psi.PsiNamedElement
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 
 /** 
@@ -8,5 +9,13 @@ import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 */
 
 trait ScAccessModifier extends ScalaPsiElement {
+  def scope() : PsiNamedElement /* either ScTypeDefinition or PsiPackage */
 
+  type AccessType = Access.Value
+
+  def access() : AccessType 
+
+  object Access extends Enumeration {
+    val PRIVATE, PROTECTED, THIS_PRIVATE, THIS_PROTECTED = Value
+  }
 }
