b539d57f60a392ce6b563b3dca34780451dbdf99
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-11 14:52:03 +0000

    file renaming when rename class
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20017 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScNamedElement.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScNamedElement.scala
@@ -4,6 +4,7 @@ import com.intellij.psi.{PsiNamedElement, PsiElement}
 import impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import psi.ScalaPsiElement
+import typedef.ScTypeDefinition
 
 trait ScNamedElement extends ScalaPsiElement with PsiNamedElement {
 
@@ -15,7 +16,20 @@ trait ScNamedElement extends ScalaPsiElement with PsiNamedElement {
 
   override def setName(name: String): PsiElement = {
     val id = nameId.getNode
+    val oldName = id.getText
     val parent = id.getTreeParent
+    def shortName(s: String): String = {
+      if (!s.endsWith(".scala")) return null
+      else return s.substring(0, s.length - 6)
+    }
+    if (id.getText == shortName(id.getPsi.getContainingFile.getName)) {
+      var par = parent.getPsi
+      while (par != null && !par.isInstanceOf[ScTypeDefinition]) par = par.getParent
+      if (par != null) par.getParent match {
+        case x: ScalaFile => x.setName(name + ".scala")
+        case _ =>
+      }
+    }
     parent.replaceChild(id, ScalaPsiElementFactory.createIdentifier(name, getManager))
     return this
   }
