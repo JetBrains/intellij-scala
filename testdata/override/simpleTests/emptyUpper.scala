98dcb298335efa14a735095f4f2b11a4a4402402
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-11 10:05:13 +0000

    fixed name of emptyUpper test (OI)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20004 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ testdata/override/simpleTests/emptyUpper.scala
@@ -0,0 +1,27 @@
+implement too
+package test
+
+class EmptyUpper extends b {
+
+  <caret>
+
+
+  def foo(): Int = 3
+
+}
+abstract class b {
+  def too: b
+}<end>
+package test
+
+class EmptyUpper extends b {
+
+  def too: b = null
+
+
+  def foo(): Int = 3
+
+}
+abstract class b {
+  def too: b
+}
