042e027c6ba500ec6edf3961151b9ec884db03c2
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-11 09:24:07 +0000

    Ended migration to new tests type (OI)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20000 59f64563-22e8-0310-8d2c-98a15e1539f2

--- testdata/override/genericTestCases/hardSubstituting.scala
+++ testdata/override/genericTestCases/hardSubstituting.scala
@@ -3,14 +3,13 @@ class A[T] {
   def foo(x: (T) => T, y: (T, Int) => T): Double = 1.0
 }
 
-class B extends A[Float] {
+class Substituting extends A[Float] {
   <caret>
-}
-------
+}<end>
 class A[T] {
   def foo(x: (T) => T, y: (T, Int) => T): Double = 1.0
 }
 
-class B extends A[Float] {
+class Substituting extends A[Float] {
   override def foo(x: (Float) => Float, y: (Float, Int) => Float): Double = 0.0
 }
--- testdata/override/genericTestCases/simpleTypeParam.scala
+++ testdata/override/genericTestCases/simpleTypeParam.scala
@@ -2,13 +2,12 @@ implement foo
 abstract class A {
   def foo[T](x: T): T
 }
-class B extends A {
+class SimpleTypeParam extends A {
   <caret>
-}
-------
+}<end>
 abstract class A {
   def foo[T](x: T): T
 }
-class B extends A {
+class SimpleTypeParam extends A {
   def foo[T](x: T): T = null
 }
