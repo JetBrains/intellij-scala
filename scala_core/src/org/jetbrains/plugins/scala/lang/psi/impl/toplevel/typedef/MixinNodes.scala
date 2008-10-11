16d478bec78e45f1da2c1e455acb5a4bda77c7bd
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-10-11 17:47:21 +0000

    correct linearization
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20021 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MixinNodes.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MixinNodes.scala
@@ -3,6 +3,7 @@
 */
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
 
+import _root_.scala.collection.mutable.LinkedHashSet
 import api.toplevel.templates.ScExtendsBlock
 import api.toplevel.typedef.{ScTypeDefinition, ScMember}
 import collection.mutable.{HashMap, ArrayBuffer, HashSet, Set, ListBuffer}
@@ -27,13 +28,15 @@ abstract class MixinNodes {
   class MultiMap extends HashMap[T, Set[Node]] with collection.mutable.MultiMap[T, Node] {
     override def elemHashCode(t : T) = computeHashCode(t)
     override def elemEquals(t1 : T, t2 : T) = equiv(t1, t2)
+
+    override def makeSet = new LinkedHashSet[Node]
   }
 
   object MultiMap {def empty = new MultiMap}
 
   def mergeSupers (maps : List[Map]) : MultiMap = {
-    maps.foldLeft(MultiMap.empty){
-      (res, curr) => {
+    maps.foldRight(MultiMap.empty){
+      (curr, res) => {
         for ((k, node) <- curr) {
           res.add(k, node)
         }
