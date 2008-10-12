3bdabf40438306c9dd915051928882eef9bf2205
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-10-12 10:05:53 +0000

    Rename file => ScTypeDefImpl
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20027 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScNamedElement.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScNamedElement.scala
@@ -16,20 +16,7 @@ trait ScNamedElement extends ScalaPsiElement with PsiNamedElement {
 
   override def setName(name: String): PsiElement = {
     val id = nameId.getNode
-    val oldName = id.getText
     val parent = id.getTreeParent
-    def shortName(s: String): String = {
-      if (!s.endsWith(".scala")) return null
-      else return s.substring(0, s.length - 6)
-    }
-    if (id.getText == shortName(id.getPsi.getContainingFile.getName)) {
-      var par = parent.getPsi
-      while (par != null && !par.isInstanceOf[ScTypeDefinition]) par = par.getParent
-      if (par != null) par.getParent match {
-        case x: ScalaFile => x.setName(name + ".scala")
-        case _ =>
-      }
-    }
     parent.replaceChild(id, ScalaPsiElementFactory.createIdentifier(name, getManager))
     return this
   }
