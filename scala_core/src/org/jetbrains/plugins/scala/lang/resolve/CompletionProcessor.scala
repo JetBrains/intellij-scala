0d67f1ba91d520c8d9be2788995e1e91c935e8b6
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-10-21 12:52:38 +0000

    filter duplicate completion variants
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20479 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/resolve/CompletionProcessor.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/resolve/CompletionProcessor.scala
@@ -4,14 +4,18 @@ import com.intellij.psi.scope._
 import com.intellij.psi._
 
 import _root_.scala.collection.Set
-import _root_.scala.collection.immutable.HashSet
+import _root_.scala.collection.mutable.HashSet
 
 class CompletionProcessor(override val kinds: Set[ResolveTargets.Value]) extends BaseProcessor(kinds) {
-
+  val names = new HashSet[String]
   def execute(element: PsiElement, state: ResolveState): Boolean = {
+    val named = element.asInstanceOf[PsiNamedElement]
     if (kindMatches(element)) {
-      val named = element.asInstanceOf[PsiNamedElement]
-      candidatesSet += new ScalaResolveResult (named)
+      val name = named.getName
+      if (!names.contains(name)) {
+        names += name
+        candidatesSet += new ScalaResolveResult(named)
+      }
     }
     return true
   }
