35953abc670c82ddb1e86127610810d49f0def68
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-10-27 19:28:09 +0000

    add scalax.rules based decompiler. Needs bugfixing before actually restoring the components in plugin.xml
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20731 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/decompiler/ScClsStubBuilderFactory.scala
+++ scala_core/src/org/jetbrains/plugins/scala/decompiler/ScClsStubBuilderFactory.scala
@@ -2,8 +2,12 @@ package org.jetbrains.plugins.scala.decompiler
 
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.impl.compiled.ClsStubBuilderFactory
+
+import com.intellij.psi.PsiManager
 import com.intellij.psi.stubs.PsiFileStub
 import lang.psi.api.ScalaFile
+import lang.psi.impl.ScalaPsiElementFactory
+import lang.psi.stubs.ScalaFileStubBuilder
 
 /**
  * @author ilyas
@@ -12,9 +16,10 @@ import lang.psi.api.ScalaFile
 class ScClsStubBuilderFactory extends ClsStubBuilderFactory[ScalaFile] {
 
 
-  def buildFileStub(file: VirtualFile, bytes: Array[Byte]): PsiFileStub[ScalaFile] = {
-    val fileStub: PsiFileStub[ScalaFile] = ScalaDecompiler.createFileStub(file, bytes);
-    fileStub
+  def buildFileStub(vFile: VirtualFile, bytes: Array[Byte]): PsiFileStub[ScalaFile] = {
+    val text = DecompilerUtil.decompile(bytes)
+    val file = ScalaPsiElementFactory.createScalaFile(text, PsiManager.getInstance(DecompilerUtil.obtainProject))
+    new ScalaFileStubBuilder().createStubForFile(file)
   }
 
   def canBeProcessed(file: VirtualFile, bytes: Array[Byte]) = {
