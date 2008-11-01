97661dd2f6748e4cb0c2c66ea2e4d95b504aa76c
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-11-01 20:04:12 +0000

    scala decompiler injected
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20984 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/decompiler/ScClsFileViewProviderFactory.scala
+++ scala_core/src/org/jetbrains/plugins/scala/decompiler/ScClsFileViewProviderFactory.scala
@@ -10,6 +10,10 @@ import com.intellij.psi.{SingleRootFileViewProvider, PsiManager, FileViewProvide
 
 class ScClassFileViewProviderFactory extends FileViewProviderFactory {
   def createFileViewProvider(file: VirtualFile, language: Language, manager: PsiManager, physical: Boolean) =
-    if (DecompilerUtil.isScalaFile(file)) new ScClassFileViewProvider(manager, file, physical)
-    else new SingleRootFileViewProvider(manager, file, physical)
+    if (DecompilerUtil.isScalaFile(file)) {
+      new ScClassFileViewProvider(manager, file, physical)
+    }
+    else {
+      new SingleRootFileViewProvider(manager, file, physical)
+    }
 }
