a1471958f29c92b2061afc17954a4ba13635853e
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-11-01 16:30:10 +0000

    references to scalap removed frop project
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20965 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
@@ -4,13 +4,11 @@ import com.intellij.lang.PsiParser
 import com.intellij.lang.ASTNode
 import com.intellij.lang.PsiBuilder
 import com.intellij.psi.tree.IElementType
-import decompiler.ScalaDecompiler
 import org.jetbrains.plugins.scala.lang.parser.parsing.Program;
 
 class ScalaParser extends PsiParser {
 
   def parse(root: IElementType, builder: PsiBuilder): ASTNode = {
-//    builder.setDebugMode(true)
     var rootMarker = builder.mark()
     new Program parse (builder)
     rootMarker.done(root)
