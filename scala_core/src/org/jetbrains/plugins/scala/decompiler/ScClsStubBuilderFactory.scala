d5b46021f00b7b95491e44da4183904e446c405e
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-11-01 20:04:12 +0000

    scala decompiler injected
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@20984 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/decompiler/ScClsStubBuilderFactory.scala
+++ scala_core/src/org/jetbrains/plugins/scala/decompiler/ScClsStubBuilderFactory.scala
@@ -1,10 +1,13 @@
 package org.jetbrains.plugins.scala.decompiler
 
+import com.intellij.lang.LanguageParserDefinitions
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.impl.compiled.ClsStubBuilderFactory
 
-import com.intellij.psi.PsiManager
 import com.intellij.psi.stubs.PsiFileStub
+import com.intellij.psi.tree.IStubFileElementType
+import com.intellij.psi.{PsiManager, PsiElement, PsiFile}
 import lang.psi.api.ScalaFile
 import lang.psi.impl.ScalaPsiElementFactory
 import lang.psi.stubs.ScalaFileStubBuilder
@@ -15,11 +18,12 @@ import lang.psi.stubs.ScalaFileStubBuilder
 
 class ScClsStubBuilderFactory extends ClsStubBuilderFactory[ScalaFile] {
 
-
   def buildFileStub(vFile: VirtualFile, bytes: Array[Byte]): PsiFileStub[ScalaFile] = {
     val text = DecompilerUtil.decompile(bytes)
     val file = ScalaPsiElementFactory.createScalaFile(text, PsiManager.getInstance(DecompilerUtil.obtainProject))
-    new ScalaFileStubBuilder().createStubForFile(file)
+    val fType = LanguageParserDefinitions.INSTANCE.forLanguage(ScalaFileType.SCALA_LANGUAGE).getFileNodeType()
+    val stub = fType.asInstanceOf[IStubFileElementType[PsiFileStub[PsiFile]]].getBuilder().buildStubTree(file)
+    return stub.asInstanceOf[PsiFileStub[ScalaFile]]
   }
 
   def canBeProcessed(file: VirtualFile, bytes: Array[Byte]) = {
