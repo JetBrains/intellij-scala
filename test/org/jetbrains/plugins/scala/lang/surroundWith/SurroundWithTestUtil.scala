7bddfe521b7cb5859c4621e5a20c4413a7f7a6d3
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-11-08 14:38:47 +0000

    added improved for testing surroundWith test
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21129 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/lang/surroundWith/SurroundWithTestUtil.scala
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.scala.lang.surroundWith
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 08.11.2008
+ */
+
+object SurroundWithTestUtil {
+  val startMarker = "<start>"
+  val endMarkers: Array[String] = Array[String]("<if>")
+  def prepareFile(text: String): (String, Int, Int, Int) = {
+    var workingText = text
+    val start = text.indexOf(startMarker)
+    val t = endMarkers.findIndexOf(text.indexOf(_) != -1)
+    val s: String = endMarkers(t)
+    val end = text.indexOf(s) - startMarker.length
+    workingText = removeMarker(workingText, startMarker)
+    workingText = removeMarker(workingText, s)
+    (workingText, start, end, t)
+  }
+
+  private def removeMarker(text: String, marker: String): String = {
+    val i = text.indexOf(marker)
+    text.substring(0, i) + text.substring(i + marker.length)
+  }
+}
