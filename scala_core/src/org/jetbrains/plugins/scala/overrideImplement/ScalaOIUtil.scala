806409ae0fd2b104ac84f7c66ad01f1f43b1b08b
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-11-09 15:24:10 +0000

    OITests fixed
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21151 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/overrideImplement/ScalaOIUtil.scala
+++ scala_core/src/org/jetbrains/plugins/scala/overrideImplement/ScalaOIUtil.scala
@@ -182,7 +182,11 @@ object ScalaOIUtil {
     val buf2 = new ArrayBuffer[ScalaObject]
     for (element <- buf) {
       element match {
-        case FullSignature(sign: PhysicalSignature, _, _, _) => {
+        case FullSignature(_, _, _, _)  | _: PhysicalSignature => {
+          val sign: PhysicalSignature = element match {
+            case FullSignature(x: PhysicalSignature, _, _, _) => x
+            case x: PhysicalSignature => x
+          }
           sign.method match {
             case _: ScFunctionDeclaration =>
             case x if x.getName == "$tag" =>
