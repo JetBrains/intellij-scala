707a5f0353516e0523bdecdc20ebcdeba3d50e2f
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-11-10 19:06:53 +0000

    equivalence between function and tuple types and corresponding Function_i and Tuple_i
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21182 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScFunctionType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/types/ScFunctionType.scala
@@ -1,5 +1,7 @@
 package org.jetbrains.plugins.scala.lang.psi.types
 
+import com.intellij.psi.PsiClass
+
 /**
 * @author ilyas
 */
@@ -8,6 +10,10 @@ case class ScFunctionType(returnType: ScType, params: Seq[ScType]) extends ScTyp
   override def equiv(that : ScType) = that match {
     case ScFunctionType(rt1, params1) => (returnType equiv rt1) &&
                                params.equalsWith(params1) {_ equiv _}
+    case ScParameterizedType(ScDesignatorType(c : PsiClass), args)
+      if args.length > 0 && c.getQualifiedName == "scala.Function" + (args.length -1) => {
+      (returnType equiv args(args.length -1)) && args.slice(0, args.length - 1).zip(params.toArray).forall{case (t1, t2) => t1 equiv t2}
+    }
     case _ => false
   }
 }
@@ -15,6 +21,10 @@ case class ScFunctionType(returnType: ScType, params: Seq[ScType]) extends ScTyp
 case class ScTupleType(components: Seq[ScType]) extends ScType {
   override def equiv(that : ScType) = that match {
     case ScTupleType(c1) => components.equalsWith(c1) {_ equiv _}
+    case ScParameterizedType(ScDesignatorType(c : PsiClass), args)
+      if args.length > 0 && c.getQualifiedName == "scala.Tuple" + args.length => {
+      args.zip(components.toArray).forall{case (t1, t2) => t1 equiv t2}
+    }
     case _ => false
   }
 }
