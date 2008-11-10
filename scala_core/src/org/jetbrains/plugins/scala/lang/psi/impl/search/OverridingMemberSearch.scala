135e5af04f2fd99372799a3c4f70d9f1877b6629
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-11-10 11:26:39 +0000

    fixed build
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21162 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/search/OverridingMemberSearch.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/search/OverridingMemberSearch.scala
@@ -1,32 +1,52 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.search
 
-import com.intellij.psi.search.searches.{ExtensibleQueryFactory, OverridingMethodsSearch}
+import _root_.scala.collection.mutable.ArrayBuffer
+import api.statements.ScFunction
+import api.toplevel.templates.ScTemplateBody
+import com.intellij.psi.search.searches.{OverridingMethodsSearch, ClassInheritorsSearch, ExtensibleQueryFactory}
+import com.intellij.psi.{PsiMember, PsiMethod, PsiClass}
 import com.intellij.psi.search.SearchScope
-import com.intellij.psi.{PsiMember, PsiMethod}
-import com.intellij.util.{EmptyQuery, Query}
+import com.intellij.util.{QueryFactory, EmptyQuery, Query}
+import toplevel.typedef.TypeDefinitionMembers
+import types.{Bounds, FullSignature, ScSubstitutor}
 
 /**
  * User: Alexander Podkhalyuzin
  * Date: 10.11.2008
  */
 
-object OverridingMemberSearch extends ExtensibleQueryFactory[PsiMember, OverridingMemberParameters.SearchParameters] {
-  import OverridingMemberParameters.SearchParameters
-  def search(member: PsiMember, scope: SearchScope, checkDeep: Boolean): Query[PsiMember] = {
-    if (cannotBeOverriden(member)) EmptyQuery.getEmptyQuery[PsiMember]
-    createUniqueResultsQuery(new SearchParameters(member, scope, checkDeep))
+object OverridingMemberSearch {
+  def search(member: PsiMember, scope: SearchScope, deep: Boolean): Array[PsiMember] = {
+    if (!member.getParent.isInstanceOf[ScTemplateBody]) return Array[PsiMember]()
+
+    val parentClass = member.getContainingClass
+    val buffer = new ArrayBuffer[PsiMember]
+
+    def process(inheritor: PsiClass): Boolean = {
+      val substitutor = Bounds.superSubstitutor(parentClass, inheritor, ScSubstitutor.empty) match {
+        case Some(x) => x
+        case None => return true
+      }
+      member match {
+        case method: ScFunction => {
+          /*val signatures: Seq[FullSignature] = TypeDefinitionMembers.getSignatures(inheritor).values.map{n => n.info}.
+                  toArray.toSeq.map(_.asInstanceOf[FullSignature])
+          true*/
+          true
+        }
+        case _ => true
+      }
+    }
+
+    var break = false
+    for (clazz <- ClassInheritorsSearch.search(parentClass, scope, true).toArray(PsiClass.EMPTY_ARRAY) if !break) {
+      break = process(clazz)
+    }
+
+    buffer.toArray
   }
 
-  private def cannotBeOverriden(member: PsiMember): Boolean = {
-    //todo
-    true
-  }
+  def search(member: PsiMember, checkDeep: Boolean): Array[PsiMember] = search(member, member.getUseScope(), checkDeep)
 
-  def search(member: PsiMember, checkDeep: Boolean): Query[PsiMember] = search(member, member.getUseScope(), checkDeep)
-
-  def search(member: PsiMember): Query[PsiMember] = search(member, true)
-}
-
-object OverridingMemberParameters {
-  case class SearchParameters(val member: PsiMember, val searchScope: SearchScope, val checkDeep: Boolean)
+  def search(member: PsiMember): Array[PsiMember] = search(member, true)
 }
