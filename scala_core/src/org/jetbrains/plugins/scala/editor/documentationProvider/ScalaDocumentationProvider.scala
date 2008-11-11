b862c49f1aec4c2f1ed380b24b9e53efb8bbe3da
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-11-11 14:37:09 +0000

    method info provided
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21207 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/editor/documentationProvider/ScalaDocumentationProvider.scala
+++ scala_core/src/org/jetbrains/plugins/scala/editor/documentationProvider/ScalaDocumentationProvider.scala
@@ -1,10 +1,12 @@
 package org.jetbrains.plugins.scala.editor.documentationProvider
 
+import _root_.scala.collection.mutable.ArrayBuffer
 import com.intellij.lang.documentation.DocumentationProvider
 import com.intellij.psi.{PsiManager, PsiElement}
 import lang.psi.api.statements.{ScFunction, ScValue, ScTypeAlias, ScVariable}
 import lang.psi.api.toplevel.ScNamedElement
-import lang.psi.api.toplevel.typedef.ScTypeDefinition
+import lang.psi.api.toplevel.templates.ScTemplateBody
+import lang.psi.api.toplevel.typedef.{ScTypeDefinition, ScMember}
 import lang.psi.ScalaPsiUtil
 
 /**
@@ -35,19 +37,38 @@ class ScalaDocumentationProvider extends DocumentationProvider {
 }
 
 private object ScalaDocumentationProvider {
+  private def getMemberHeader(member: ScMember): String = {
+    if (!member.getParent.isInstanceOf[ScTemplateBody]) return ""
+    if (!member.getParent.getParent.getParent.isInstanceOf[ScTypeDefinition]) return ""
+    member.getContainingClass.getName + " " + member.getContainingClass.getPresentation.getLocationString + "\n"
+  }
+
   def generateClassInfo(clazz: ScTypeDefinition): String = {
     return clazz.name
   }
 
   def generateFunctionInfo(function: ScFunction): String = {
-    return function.name
+    val buffer = new StringBuilder
+    buffer.append(getMemberHeader(function))
+    buffer.append(ScalaPsiUtil.getModifiersPresentableText(function.getModifierList))
+    buffer.append("def ")
+    buffer.append(ScalaPsiUtil.getMethodPresentableText(function))
+    buffer.toString
   }
 
   def generateValueInfo(value: ScNamedElement): String = {
-    return value.name
+    val member = ScalaPsiUtil.nameContext(value) match {
+      case x: ScMember => x
+      case _ => return null
+    }
+    val buffer = new StringBuilder
+    buffer.append(getMemberHeader(member))
+    buffer.toString
   }
 
   def generateTypeAliasInfo(alias: ScTypeAlias): String = {
-    return alias.name
+    val buffer = new StringBuilder
+    buffer.append(getMemberHeader(alias))
+    buffer.toString
   }
 }
