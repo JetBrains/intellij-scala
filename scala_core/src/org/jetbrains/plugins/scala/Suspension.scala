e225d2a9112244872b5773f19b271e3fc364bd79
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-11-23 14:27:54 +0000

    not here
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21360 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/Suspension.scala
+++ scala_core/src/org/jetbrains/plugins/scala/Suspension.scala
@@ -7,11 +7,4 @@ class Suspension[T](fun: () => T) {
   def this(t: T) = this ({() => t})
 
   lazy val v = fun()
-
-  override def hashCode = v.hashCode
-
-  override def equals(p1: Any) = p1 match {
-    case s : Suspension[T] => s.v == this.v
-    case _ => false
-  }
 }
