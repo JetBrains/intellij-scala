140894d4036947ddcb8ef14d42ccb3463e789389
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-11-24 22:51:40 +0000

    stub version shifted
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21411 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/ScStubFileElementType.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/ScStubFileElementType.scala
@@ -36,5 +36,5 @@ class ScStubFileElementType(lang: Language) extends IStubFileElementWrapper[Scal
 }
 
 private[elements] object StubVersion {
-  val STUB_VERSION: Int = 10
+  val STUB_VERSION: Int = 12
 }
