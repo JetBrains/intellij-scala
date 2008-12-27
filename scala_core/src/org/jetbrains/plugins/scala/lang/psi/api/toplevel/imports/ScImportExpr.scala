fc118047dd2a986d0d4a2f634cdb2ab907b4f722
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-12-27 17:24:57 +0000

    SCL-535
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21781 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/imports/ScImportExpr.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/imports/ScImportExpr.scala
@@ -5,10 +5,10 @@ import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReferenceElement
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 
-/** 
-* @author Alexander Podkhalyuzin
-* Date: 20.02.2008
-*/
+/**
+ * @author Alexander Podkhalyuzin
+ * Date: 20.02.2008
+ */
 
 trait ScImportExpr extends ScalaPsiElement {
   def reference = findChild(classOf[ScStableCodeReferenceElement])
@@ -22,17 +22,18 @@ trait ScImportExpr extends ScalaPsiElement {
     }
   }
 
-  def singleWildcard : Boolean
+  def singleWildcard: Boolean
 
   def qualifier: ScStableCodeReferenceElement
 
   def deleteExpr
-  
+
   def getNames: Array[String] = getLastChild match {
     case s: ScImportSelectors => (for (selector <- selectors) yield selector.getText).toArray
     case _ => getNode.getLastChildNode.getText match {
       case "_" => Array[String]("_")
-      case _ => Array[String](getNode.getLastChildNode.getLastChildNode.getText)
+      case _ if getNode.getLastChildNode.getLastChildNode != null => Array[String](getNode.getLastChildNode.getLastChildNode.getText)
+      case _ => Array[String]()
     }
 
   }
