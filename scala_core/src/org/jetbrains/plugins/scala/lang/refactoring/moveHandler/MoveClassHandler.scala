b0c02910565fa8fe3f59e7db4709cee9c938402d
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-12-30 13:53:54 +0000

    changes to Move Class refactoring
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21797 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/refactoring/moveHandler/MoveClassHandler.scala
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.scala.lang.refactoring.moveHandler
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.ex.DataConstantsEx
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.{PsiDirectory, PsiElement, PsiReference}
+import com.intellij.openapi.project.Project
+import com.intellij.refactoring.move.{MoveCallback, MoveHandlerDelegate}
+import psi.api.ScalaFile
+import psi.api.toplevel.typedef.ScTypeDefinition
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 30.12.2008
+ */
+
+class MoveClassHandler extends MoveHandlerDelegate {
+  override def canMove(elements: Array[PsiElement], targetContainer: PsiElement): Boolean = {
+    for (element <- elements if !element.isInstanceOf[ScTypeDefinition] || !element.getParent.isInstanceOf[ScalaFile]) {
+      return false
+    }
+    return targetContainer == null || isValidTarget(targetContainer)
+  }
+
+  override def doMove(project: Project, elements: Array[PsiElement], targetContainer: PsiElement, callback: MoveCallback): Unit = {
+    MoveRefactoringUtil.moveClass(project, elements, targetContainer, callback)
+  }
+
+  override def isValidTarget(psiElement: PsiElement): Boolean = psiElement match {
+    case _: PsiDirectory => true
+    case _ => false
+  }
+
+  override def tryToMove(element: PsiElement, project: Project,
+                        dataContext: DataContext, reference: PsiReference, editor: Editor): Boolean = {
+    if (element.isInstanceOf[ScTypeDefinition]) {
+      MoveRefactoringUtil.moveClass(project, Array[PsiElement](element),
+        dataContext.getData(DataConstantsEx.TARGET_PSI_ELEMENT).asInstanceOf[PsiElement], null)
+      return true
+    }
+    return false
+  }
+}
---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/refactoring/moveHandler/MoveRefactoringUtil.scala
@@ -0,0 +1,133 @@
+package org.jetbrains.plugins.scala.lang.refactoring.moveHandler
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.{PsiDirectory, JavaDirectoryService, PsiElement, PsiPackage}
+import com.intellij.refactoring.move.MoveCallback
+import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesDialog
+import com.intellij.refactoring.util.TextOccurrencesUtil
+import com.intellij.refactoring.{HelpID, JavaRefactoringSettings}
+import com.jniwrapper.P
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 30.12.2008
+ */
+
+object MoveRefactoringUtil {
+  def moveClass(project: Project, elements: Array[PsiElement], initialTargetElement: PsiElement, moveCallback: MoveCallback) {
+    //todo: add checking elements
+    def isSearchTextOccurences: Boolean = {
+      for (element <- elements if TextOccurrencesUtil.isSearchTextOccurencesEnabled(element)) return true
+      false
+    }
+    val searchTextOccurences = isSearchTextOccurences
+    val initialTargetPackageName = getInitialTargetPackageName(initialTargetElement, elements)
+    val initialTargetDirectory = getInitialTargetDirectory(initialTargetElement, elements)
+    val isTargetDirectoryFixed = getContainerDirectory(initialTargetElement) != null;
+    val moveDialog =
+      new ScalaMoveClassesOrPackagesDialog(project, searchTextOccurences, elements, initialTargetElement, moveCallback)
+    val searchForTextOccurences = JavaRefactoringSettings.getInstance().MOVE_SEARCH_FOR_TEXT
+    val searchInComments = JavaRefactoringSettings.getInstance().MOVE_SEARCH_IN_COMMENTS
+    moveDialog.setData(elements, initialTargetPackageName, initialTargetDirectory, isTargetDirectoryFixed, searchInComments,
+                       searchForTextOccurences, HelpID.getMoveHelpID(elements(0)))
+    moveDialog.show()
+  }
+
+  private def getInitialTargetPackageName(initialTargetElement: PsiElement, movedElements: Array[PsiElement]): String = {
+    var name = getContainerPackageName(initialTargetElement);
+    if (name == null) {
+      if (movedElements != null) {
+        name = getTargetPackageNameForMovedElement(movedElements(0))
+      }
+      if (name == null) {
+        val commonDirectory = getCommonDirectory(movedElements)
+        if (commonDirectory != null && JavaDirectoryService.getInstance().getPackage(commonDirectory) != null) {
+          name = JavaDirectoryService.getInstance().getPackage(commonDirectory).getQualifiedName()
+        }
+      }
+    }
+    if (name == null) {
+      name = ""
+    }
+    return name
+  }
+
+  private def getTargetPackageNameForMovedElement(psiElement: PsiElement): String = {
+    psiElement match {
+      case psiPackage: PsiPackage => {
+        val parent = psiPackage.getParentPackage
+        if (parent != null) parent.getQualifiedName else ""
+      }
+      case x: PsiDirectory => {
+        val aPackage = JavaDirectoryService.getInstance().getPackage(x);
+        if  (aPackage != null) getTargetPackageNameForMovedElement(aPackage) else "";
+      }
+      case null => null
+      case _ => {
+        val aPackage = JavaDirectoryService.getInstance().getPackage(psiElement.getContainingFile().getContainingDirectory());
+        if (aPackage != null) aPackage.getQualifiedName() else "";
+      }
+    }
+  }
+
+  private def getContainerPackageName(psiElement: PsiElement): String = {
+    psiElement match {
+      case x: PsiPackage => x.getQualifiedName
+      case x: PsiDirectory => {
+        val aPackage = JavaDirectoryService.getInstance().getPackage(x)
+        return if (aPackage != null) aPackage.getQualifiedName() else ""
+      }
+      case null => return null
+      case x => {
+        val aPackage = JavaDirectoryService.getInstance().getPackage(x.getContainingFile().getContainingDirectory())
+        return if(aPackage != null) aPackage.getQualifiedName() else ""
+      }
+    }
+  }
+
+  private def getInitialTargetDirectory(initialTargetElement: PsiElement, movedElements: Array[PsiElement]): PsiDirectory = {
+    getContainerDirectory(initialTargetElement) match {
+      case null => {
+        if (movedElements != null) {
+          getCommonDirectory(movedElements) match {
+            case x if x != null => x
+            case _ => getContainerDirectory(movedElements(0));
+          }
+        } else null
+      }
+      case x => x
+    }
+  }
+
+  private def getCommonDirectory(movedElements: Array[PsiElement]): PsiDirectory = {
+    var commonDirectory: PsiDirectory = null
+
+    for (movedElement <- movedElements) {
+      val containingFile = movedElement.getContainingFile();
+      if (containingFile != null) {
+        val containingDirectory = containingFile.getContainingDirectory();
+        if (containingDirectory != null) {
+          if (commonDirectory == null) {
+            commonDirectory = containingDirectory;
+          }
+          else {
+            if (commonDirectory != containingDirectory) {
+              return null;
+            }
+          }
+        }
+      }
+    }
+    return commonDirectory
+  }
+
+
+  private def getContainerDirectory(psiElement: PsiElement): PsiDirectory = {
+    psiElement match {
+      case _: PsiPackage => null
+      case x: PsiDirectory => x
+      case x: PsiElement => x.getContainingFile.getContainingDirectory
+      case null => null
+    }
+  }
+}
