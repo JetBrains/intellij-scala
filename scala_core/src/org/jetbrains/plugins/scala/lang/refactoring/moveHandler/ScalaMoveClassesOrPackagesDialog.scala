e767d8b3283fcd7de1ad681178e2319ee74e15c0
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2008-12-30 13:53:54 +0000

    changes to Move Class refactoring
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@21797 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/refactoring/moveHandler/ScalaMoveClassesOrPackagesDialog.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.plugins.scala.lang.refactoring.moveHandler
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.BaseRefactoringProcessor
+import com.intellij.refactoring.move.MoveCallback
+import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesDialog
+import com.intellij.usageView.{UsageInfo, UsageViewDescriptor}
+import java.lang.String
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 30.12.2008
+ */
+
+class ScalaMoveClassesOrPackagesDialog(project: Project, searchTextOccurences: Boolean, psiElements: Array[PsiElement],
+                                      initialTargetElement: PsiElement, moveCallback: MoveCallback)
+        extends MoveClassesOrPackagesDialog(project, searchTextOccurences, psiElements, initialTargetElement, moveCallback) {
+  override def doAction: Unit = {
+    
+  }
+}
