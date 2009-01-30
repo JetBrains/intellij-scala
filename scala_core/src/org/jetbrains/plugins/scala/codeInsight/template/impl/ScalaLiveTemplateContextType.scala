b92116468eabc01106c7779a1a89568ceaf6d0aa
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-01-30 14:15:40 +0000

    main live template
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@22283 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/codeInsight/template/impl/ScalaLiveTemplateContextType.scala
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.scala.codeInsight.template.impl
+
+import com.intellij.codeInsight.template.FileTypeBasedContextType
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 30.01.2009
+ */
+
+class ScalaLiveTemplateContextType extends FileTypeBasedContextType("SCALA", "Scala", ScalaFileType.SCALA_FILE_TYPE)
