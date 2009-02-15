5b37147dbdc213424a0623372418d2eb91c0a2c8
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-02-15 12:42:23 +0000

    added module classpath to Script Console
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@22579 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/compiler/rt/ConsoleRunner.java
+++ src/org/jetbrains/plugins/scala/compiler/rt/ConsoleRunner.java
@@ -4,6 +4,7 @@ import scala.Some;
 import scala.tools.nsc.GenericRunnerCommand;
 import scala.tools.nsc.InterpreterLoop;
 import scala.tools.nsc.Settings;
+import scala.tools.nsc.GenericRunnerSettings;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,7 +16,10 @@ import java.io.PrintWriter;
  */
 public class ConsoleRunner {
   public static void main(String[] args) {
-    GenericRunnerCommand command = new GenericRunnerCommand(ConsoleRunnerUtil.listOf(args), ConsoleRunnerUtil.getUnitFunction());
+    //ConsoleRunnerUtil.addQuotes(args);
+    GenericRunnerCommand command = new GenericRunnerCommand(ConsoleRunnerUtil.listOf(args),
+        ConsoleRunnerUtil.getSettings(args),
+        ConsoleRunnerUtil.getUnitFunction());
     Settings settings = command.settings();
     ConsoleRunnerUtil.setParamParser(args, settings);
     (new InterpreterLoop(new Some(new BufferedReader(new InputStreamReader(System.in))), new PrintWriter(System.out))).main(settings);
