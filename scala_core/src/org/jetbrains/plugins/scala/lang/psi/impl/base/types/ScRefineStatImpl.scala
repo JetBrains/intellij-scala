54a11bb1c20df7d1a418085483d5ba9050c84c33
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-02-18 17:50:36 +0000

    SCL-745
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@22709 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScRefineStatImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScRefineStatImpl.scala
@@ -3,10 +3,6 @@ package org.jetbrains.plugins.scala.lang.psi.impl.base.types
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-
-
-
-
 import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree.IElementType;
