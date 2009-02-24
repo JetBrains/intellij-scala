bd998f15d5f2dbcd966dd29f778f9e704ab84994
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-02-24 15:44:35 +0000

    ScalaTest runner: locatable (creates configuration by location)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@22830 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/testingSupport/scalaTest/ScalaTestConfigurationType.scala
+++ src/org/jetbrains/plugins/scala/testingSupport/scalaTest/ScalaTestConfigurationType.scala
@@ -2,9 +2,14 @@ package org.jetbrains.plugins.scala.testingSupport.scalaTest
 
 import _root_.java.lang.String
 import _root_.javax.swing.Icon
-import com.intellij.execution.configurations.{ConfigurationType, ConfigurationFactory}
-import com.intellij.execution.LocatableConfigurationType
+import com.intellij.execution.configurations.{RunConfiguration, ConfigurationType, ConfigurationFactory}
+import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl
+import com.intellij.execution.{RunManager, Location, RunnerAndConfigurationSettings, LocatableConfigurationType}
 import com.intellij.openapi.util.IconLoader
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.{JavaPsiFacade, PsiElement}
+import lang.psi.api.toplevel.typedef.ScTypeDefinition
 import script.ScalaScriptRunConfigurationFactory
 
 /**
@@ -12,7 +17,7 @@ import script.ScalaScriptRunConfigurationFactory
  * Date: 22.02.2009
  */
 
-class ScalaTestConfigurationType extends ConfigurationType {//todo: locatable
+class ScalaTestConfigurationType extends LocatableConfigurationType {
   val confFactory = new ScalaTestRunConfigurationFactory(this)
 
   def getIcon: Icon = IconLoader.getIcon("/runConfigurations/junit.png")
@@ -24,4 +29,30 @@ class ScalaTestConfigurationType extends ConfigurationType {//todo: locatable
   def getConfigurationFactories: Array[ConfigurationFactory] = Array[ConfigurationFactory](confFactory)
 
   def getId: String = "ScalaTestRunConfiguration"
+
+
+  def createConfigurationByLocation(location: Location[_ <: PsiElement]): RunnerAndConfigurationSettings = {
+    val element = location.getPsiElement
+    val parent: ScTypeDefinition = PsiTreeUtil.getParentOfType(element, classOf[ScTypeDefinition])
+    val facade = JavaPsiFacade.getInstance(element.getProject)
+    val suiteClazz = facade.findClass("org.scalatest.Suite", GlobalSearchScope.allScope(element.getProject))
+    if (suiteClazz == null) return null
+    if (!parent.isInheritor(suiteClazz, true)) return null
+    val settings = RunManager.getInstance(location.getProject).createRunConfiguration(parent.getName, confFactory)
+    settings.getConfiguration.asInstanceOf[ScalaTestRunConfiguration].setTestClassPath(parent.getQualifiedName)
+    return settings
+  }
+
+  def isConfigurationByLocation(configuration: RunConfiguration, location: Location[_ <: PsiElement]): Boolean = {
+    val element = location.getPsiElement
+    val parent: ScTypeDefinition = PsiTreeUtil.getParentOfType(element, classOf[ScTypeDefinition])
+    val facade = JavaPsiFacade.getInstance(element.getProject)
+    val suiteClazz = facade.findClass("org.scalatest.Suite", GlobalSearchScope.allScope(element.getProject))
+    if (suiteClazz == null) return false
+    if (!parent.isInheritor(suiteClazz, true)) return false
+    configuration match {
+      case configuration: ScalaTestRunConfiguration => return parent.getQualifiedName == configuration.getTestClassPath
+      case _ => return false
+    }             
+  }
 }
