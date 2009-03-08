b739bc54a77eb826a8e1595749a84a58f7646446
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-08 12:35:03 +0000

    completion filtering changed to more precise version
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23023 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/resolve/CompletionProcessor.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/resolve/CompletionProcessor.scala
@@ -1,16 +1,38 @@
 package org.jetbrains.plugins.scala.lang.resolve
 
+import _root_.org.jetbrains.plugins.scala.lang.psi.types.{PhysicalSignature, Signature, ScSubstitutor}
 import com.intellij.psi.scope._
 import com.intellij.psi._
 
 import _root_.scala.collection.Set
 import _root_.scala.collection.mutable.HashSet
-
+import psi.api.base.patterns.ScBindingPattern
 class CompletionProcessor(override val kinds: Set[ResolveTargets.Value]) extends BaseProcessor(kinds) {
+  private val signatures = new HashSet[Signature]
+
   def execute(element: PsiElement, state: ResolveState): Boolean = {
     val named = element.asInstanceOf[PsiNamedElement]
     if (kindMatches(element)) {
-      candidatesSet += new ScalaResolveResult(named)
+      element match {
+        case method: PsiMethod => {
+          val sign = new PhysicalSignature(method, state.get(ScSubstitutor.key))
+          if (!signatures.contains(sign)) {
+            signatures += sign
+            candidatesSet += new ScalaResolveResult(named)
+          }
+        }
+        case patt: ScBindingPattern => {
+          import Suspension._
+          val sign = new Signature(patt.getName, Seq.empty, 0, Seq.empty.toArray, state.get(ScSubstitutor.key))
+          if (!signatures.contains(sign)) {
+            signatures += sign
+            candidatesSet += new ScalaResolveResult(named)
+          }
+        }
+        case _ => {
+          candidatesSet += new ScalaResolveResult(named)
+        }
+      }
     }
     return true
   }
