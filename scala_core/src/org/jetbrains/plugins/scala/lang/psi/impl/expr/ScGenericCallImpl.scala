41e7fd2dd348946bb724943cd9556fb31a5cd083
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-03-09 19:35:59 +0000

    minor fix
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23030 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScGenericCallImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScGenericCallImpl.scala
@@ -5,24 +5,14 @@ import api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-
-
-
-
-
-
 import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree.IElementType
 import toplevel.synthetic.ScSyntheticFunction;
 import com.intellij.psi._
-
 import org.jetbrains.annotations._
-
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.icons.Icons
-
-
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import types.Nothing
 
