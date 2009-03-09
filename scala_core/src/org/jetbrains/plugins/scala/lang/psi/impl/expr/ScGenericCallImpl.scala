b5222a092157e32a4242813af6843f154d69df47
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-09 16:52:59 +0000

    type system improvements
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23029 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScGenericCallImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScGenericCallImpl.scala
@@ -1,5 +1,7 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.expr
 
+import _root_.org.jetbrains.plugins.scala.lang.psi.types.{ScType, ScSubstitutor}
+import api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
@@ -11,7 +13,8 @@ import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
 
 import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
-import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.IElementType
+import toplevel.synthetic.ScSyntheticFunction;
 import com.intellij.psi._
 
 import org.jetbrains.annotations._
@@ -21,6 +24,7 @@ import org.jetbrains.plugins.scala.icons.Icons
 
 
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
+import types.Nothing
 
 /** 
 * @author Alexander Podkhalyuzin
@@ -29,4 +33,20 @@ import org.jetbrains.plugins.scala.lang.psi.api.expr._
 
 class ScGenericCallImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScGenericCall {
   override def toString: String = "GenericCall"
+
+
+  override def getType(): ScType = {
+    val refType = referencedExpr.getType
+    val tp: Seq[String] = referencedExpr match {
+      case expr: ScReferenceExpression => expr.resolve match {
+        case fun: ScFunction => fun.typeParameters.map(_.name)
+        case meth: PsiMethod => meth.getTypeParameters.map(_.getName)
+        case synth: ScSyntheticFunction => synth.typeParams.map(_.name)
+        case _ => return Nothing //todo:
+      }
+      case _ => return Nothing //todo:
+    }
+    val substitutor = ScalaPsiUtil.genericCallSubstitutor(tp, this)
+    substitutor.subst(refType)
+  }
 }
