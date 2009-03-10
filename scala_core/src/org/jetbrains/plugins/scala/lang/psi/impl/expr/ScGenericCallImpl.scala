271fffbddca95234423d6237ddb274360b1bd087
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-10 12:55:09 +0000

    parameterInfo refactoing GenericCall type inference for apply methods
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23045 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScGenericCallImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScGenericCallImpl.scala
@@ -1,25 +1,26 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.expr
 
-import _root_.org.jetbrains.plugins.scala.lang.psi.types.{ScType, ScSubstitutor}
-import api.statements.ScFunction
+import api.statements.{ScFunction, ScFun}
+import api.toplevel.typedef.{ScClass, ScObject}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
 import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree.IElementType
-import toplevel.synthetic.ScSyntheticFunction;
+import toplevel.synthetic.ScSyntheticFunction
+import toplevel.typedef.TypeDefinitionMembers
+import types._;
 import com.intellij.psi._
 import org.jetbrains.annotations._
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.icons.Icons
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
-import types.Nothing
 
-/** 
-* @author Alexander Podkhalyuzin
-* Date: 06.03.2008
-*/
+/**
+ * @author Alexander Podkhalyuzin
+ * Date: 06.03.2008
+ */
 
 class ScGenericCallImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScGenericCall {
   override def toString: String = "GenericCall"
@@ -34,7 +35,32 @@ class ScGenericCallImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with Sc
         case synth: ScSyntheticFunction => synth.typeParams.map(_.name)
         case _ => return Nothing //todo:
       }
-      case _ => return Nothing //todo:
+      case _ => { //here we must investigate method apply (not update, because can't be generic)
+        val parent: PsiElement = getParent
+        val params: Seq[ScExpression] = parent match {
+          case call: ScMethodCall => call.args.exprs
+          case placeholder: ScPlaceholderExpr => Seq.empty //todo:
+          case _ => Seq.empty
+        }
+        //result, we set here to stop loop
+        var res: Seq[String] = null
+        ScType.extractClassType(refType) match {
+          case clazz: PsiClass => {
+            val methods = ScalaPsiUtil.getMethodsConforsToMethodCall(ScalaPsiUtil.getApplyMethods(clazz), params)
+            if (methods.length == 1) {
+              methods(0).method match {
+                case fun: ScFunction => fun.typeParameters.map(_.name)
+                case meth: PsiMethod => meth.getTypeParameters.map(_.getName)
+              }
+            } else {
+              //todo: according to expected type choose appropriate method
+            }
+          }
+          case _ => return Nothing
+        }
+        if (res == null) return Nothing
+        res
+      }
     }
     val substitutor = ScalaPsiUtil.genericCallSubstitutor(tp, this)
     substitutor.subst(refType)
