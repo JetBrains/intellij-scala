fa27e64eca32fc7531bce354729e502b0de5c7c8
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-10 18:18:07 +0000

    Tests for GenericCall added (3 failed yet)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23073 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ testdata/typeInference/genericCall/CaseClasses.scala
@@ -0,0 +1,3 @@
+case class CaseClasses[T](x: T)
+
+/*start*/CaseClasses[Int](1)/*end*/
---
+++ testdata/typeInference/genericCall/JavaGenericFunction.scala
@@ -0,0 +1,5 @@
+import java.util.Collections
+import java.util.Vector
+
+val vector = new Vector[Int](0)
+/*start*/Collections.list[Int](vector)/*end*/
