7408b8881f2a65e4561b02deac6d1b245ae8f16f
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-11 14:43:22 +0000

    test for default package resolving added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23091 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/lang/resolve/DefaultPackageResolveTest.java
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala.lang.resolve;
+
+import com.intellij.testFramework.ResolveTestCase;
+import com.intellij.psi.PsiReference;
+import org.jetbrains.plugins.scala.util.TestUtils;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAliasDeclaration;
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 11.03.2009
+ */
+public class DefaultPackageResolveTest extends ScalaResolveTestCase {
+  protected String getTestDataPath() {
+    return TestUtils.getTestDataPath() + "/resolve/";
+  }
+
+  public void testScalaToJava() throws Exception {
+    PsiReference ref = configureByFile("defaultPackage/ScalaToJava.scala");
+    assertTrue(ref.resolve() == null);
+  }
+
+  public void testScalaToScala() throws Exception {
+    PsiReference ref = configureByFile("defaultPackage/ScalaToScala.scala");
+    assertTrue(ref.resolve() != null);
+  }
+
+  public void testScalaToScript() throws Exception {
+    PsiReference ref = configureByFile("defaultPackage/ScalaToScript.scala");
+    assertTrue(ref.resolve() == null);
+  }
+
+  public void testDefaultScalaToJava() throws Exception {
+    PsiReference ref = configureByFile("defaultPackage/DefaultScalaToJava.scala");
+    assertTrue(ref.resolve() != null);
+  }
+}
