07ece8b06ee2f75f0e6a18fc3e41b03f0f7293ea
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-12 12:17:29 +0000

    restored unused classes: ScUnit, ScUnitImpl
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23121 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/expr/ScUnit.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.lang.psi.api.expr
+
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
+
+/** 
+* @author Alexander Podkhalyuzin
+* Date: 06.03.2008
+*/
+
+trait ScUnit extends ScalaPsiElement {
+
+}
