2f0bacf923c7ac4b52f67fe8bf0001a2ef2bdb5a
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-03-12 20:42:38 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23140 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ test/org/jetbrains/plugins/scala/lang/typeInference/TypeInferenceTest2.java
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.scala.lang.typeInference;
+
+import com.intellij.testFramework.ResolveTestCase;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.plugins.scala.util.TestUtils;
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter;
+
+/**
+ * @author ilyas
+ */
+public class TypeInferenceTest2 extends ResolveTestCase {
+  @Override
+  protected String getTestDataPath() {
+    return TestUtils.getTestDataPath() + "/typeInference/";
+  }
+
+  public void testFunParameter() throws Exception {
+    PsiReference ref = configureByFile("expected/param/Param.scala");
+    PsiElement resolved = ref.resolve();
+    assertTrue(resolved instanceof ScParameter);
+    String string = (((ScParameter) resolved)).calcType().toString();
+    assertFalse(string.equals("Nothing"));
+  }
+}
