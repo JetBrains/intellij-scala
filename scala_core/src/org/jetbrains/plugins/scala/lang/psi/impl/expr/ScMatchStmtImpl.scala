b8bbc63a4c8b6de6fbd3353d1e192056a7f27500
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-03-12 19:40:30 +0000

    refucktoring
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23135 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScMatchStmtImpl.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScMatchStmtImpl.scala
@@ -15,27 +15,13 @@ import org.jetbrains.plugins.scala.icons.Icons
 
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
 
-/** 
-* @author Alexander Podkhalyuzin
-* Date: 06.03.2008
-*/
+/**
+ * @author Alexander Podkhalyuzin
+ * Date: 06.03.2008
+ */
 
 class ScMatchStmtImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScMatchStmt {
   override def toString: String = "MatchStatement"
 
-  override def getType(): ScType = {
-    //todo: it's wrong now (bounds)
-    var result: ScType = null
-    getBranches.length match {
-      case 0 => types.Nothing
-      case 1 => getBranches(0).getType
-      case _ => {
-        result = getBranches(0).getType
-        for (i <- 1 to getBranches.length - 1) {
-          result = Bounds.lub(result, getBranches(i).getType)
-        }
-        result
-      }
-    }
-  }
+  override def getType(): ScType = getBranches.foldLeft(types.Nothing: ScType)((t, b) => Bounds.lub(t, b.getType))
 }
