6045433a793cf81206759a7b910e7a4093de42aa
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-14 11:38:36 +0000

    test output fixed
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23172 59f64563-22e8-0310-8d2c-98a15e1539f2

--- testdata/parameterInfo/functionParameterInfo/simple/JavaLibrary.scala
+++ testdata/parameterInfo/functionParameterInfo/simple/JavaLibrary.scala
@@ -3,7 +3,7 @@ s.indexOf(/*caret*/)
 /*
 ch: Int
 ch: Int, fromIndex: Int
-source: Array[Char, sourceOffset: Int, sourceCount: Int, target: Array[Char], targetOffset: Int, targetCount: Int, fromIndex: Int
+source: Array[Char], sourceOffset: Int, sourceCount: Int, target: Array[Char], targetOffset: Int, targetCount: Int, fromIndex: Int
 str: String
 str: String, fromIndex: Int
 */
