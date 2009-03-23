c1d36d61f0ba70970bb4b320e799cf09662f4fa7
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-03-23 11:24:10 +0000

    scala facet settings are implemented
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23302 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/config/ScalaLibrariesConfiguration.java
@@ -0,0 +1,11 @@
+package org.jetbrains.plugins.scala.config;
+
+/**
+ * @author ilyas
+ */
+public class ScalaLibrariesConfiguration {
+
+  public boolean myExcludeCompilerFromModuleScope = false;
+  public boolean myExcludeSdkFromModuleScope = false;
+
+}
--- src/org/jetbrains/plugins/scala/config/ui/ScalaFacetTab.java
+++ src/org/jetbrains/plugins/scala/config/ui/ScalaFacetTab.java
@@ -29,6 +29,7 @@ import com.intellij.openapi.roots.impl.libraries.ProjectLibraryTable;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.scala.ScalaBundle;
+import org.jetbrains.plugins.scala.config.ScalaLibrariesConfiguration;
 
 import javax.swing.*;
 
@@ -41,19 +42,22 @@ public class ScalaFacetTab extends FacetEditorTab {
 
   private Module myModule;
   private JPanel myPanel;
+  private JCheckBox myCompilerExcludeCb;
+  private JCheckBox myLibraryExcludeCb;
   private FacetEditorContext myEditorContext;
   private FacetValidatorsManager myValidatorsManager;
+  private final ScalaLibrariesConfiguration myConfiguration;
 
-  private LibraryTable.Listener myLibraryListener;
-
-  public ScalaFacetTab(FacetEditorContext editorContext, FacetValidatorsManager validatorsManager) {
+  public ScalaFacetTab(FacetEditorContext editorContext, FacetValidatorsManager validatorsManager, ScalaLibrariesConfiguration configuration) {
     myModule = editorContext.getModule();
     myEditorContext = editorContext;
     myValidatorsManager = validatorsManager;
+
+    myConfiguration = configuration;
+
+    myCompilerExcludeCb.setSelected(myConfiguration.myExcludeCompilerFromModuleScope);
+    myLibraryExcludeCb.setSelected(myConfiguration.myExcludeSdkFromModuleScope);
     reset();
-    myLibraryListener = new MyLibraryTableListener();
-    LibraryTablesRegistrar.getInstance().getLibraryTable().addListener(myLibraryListener);
-    ProjectLibraryTable.getInstance(myModule.getProject()).addListener(myLibraryListener);
   }
 
   @Nls
@@ -66,8 +70,8 @@ public class ScalaFacetTab extends FacetEditorTab {
   }
 
   public boolean isModified() {
-    //todo
-    return false;
+    return !(myConfiguration.myExcludeCompilerFromModuleScope == myCompilerExcludeCb.isSelected() &&
+        myConfiguration.myExcludeSdkFromModuleScope == myLibraryExcludeCb.isSelected());
   }
 
   @Override
@@ -76,143 +80,20 @@ public class ScalaFacetTab extends FacetEditorTab {
   }
 
   public void onFacetInitialized(@NotNull Facet facet) {
-    fireRootsChangedEvent();
-  }
-
-  private void fireRootsChangedEvent() {
-/*    final ScalaSDKComboBox.DefaultScalaSDKComboBoxItem selectedItem =
-      (ScalaSDKComboBox.DefaultScalaSDKComboBoxItem)myComboBox.getSelectedItem();
-    final Module module = myEditorContext.getModule();
-    if (module != null) {
-      final Project project = module.getProject();
-      ApplicationManager.getApplication().runWriteAction(new Runnable() {
-        public void run() {
-          ScalaSDK sdk = null;
-          if (selectedItem instanceof ScalaSDKComboBox.ScalaSDKPointerItem) {
-            ScalaSDKComboBox.ScalaSDKPointerItem pointerItem = (ScalaSDKComboBox.ScalaSDKPointerItem)selectedItem;
-            final ScalaSDKPointer pointer = pointerItem.getPointer();
-            String name = pointerItem.getName();
-            String path = pointerItem.getPath();
-            Library library = ScalaConfigUtils.createScalaLibrary(path, name, project, true, pointer.isProjectLib());
-            if (library != null) {
-              sdk = new ScalaSDK(library, myModule, pointer.isProjectLib());
-            }
-          } else {
-            sdk = selectedItem.getScalaSDK();
-          }
-          ScalaConfigUtils.updateScalaLibInModule(module, sdk);
-
-          // create other libraries by their pointers
-          for (int i = 0; i < myComboBox.getItemCount(); i++) {
-            Object item = myComboBox.getItemAt(i);
-            if (item != selectedItem && item instanceof ScalaSDKComboBox.ScalaSDKPointerItem) {
-              ScalaSDKComboBox.ScalaSDKPointerItem pointerItem = (ScalaSDKComboBox.ScalaSDKPointerItem)item;
-              final ScalaSDKPointer pointer = pointerItem.getPointer();
-              String name = pointerItem.getName();
-              String path = pointerItem.getPath();
-              ScalaConfigUtils.createScalaLibrary(path, name, project, true, pointer.isProjectLib());
-            }
-          }
-        }
-      });
-    }*/
   }
 
   public void apply() throws ConfigurationException {
+    myConfiguration.myExcludeCompilerFromModuleScope = myCompilerExcludeCb.isSelected();
+    myConfiguration.myExcludeSdkFromModuleScope = myLibraryExcludeCb.isSelected();
   }
 
   public void reset() {
-/*
-    Module module = myEditorContext.getModule();
-    if (module != null && module.isDisposed()) return;
-    if (module != null && FacetManager.getInstance(module).getFacetByType(ScalaFacet.ID) != null) {
-      Library[] libraries = ScalaConfigUtils.getScalaCompilerLibrariesByModule(myEditorContext.getModule());
-      if (libraries.length == 0) {
-        myComboBox.setSelectedIndex(0);
-        oldScalaLibName = newScalaLibName;
-      } else {
-        Library library = libraries[0];
-        if (library != null && library.getName() != null) {
-          myComboBox.selectLibrary(library);
-          oldScalaLibName = newScalaLibName;
-        }
-      }
-    } else {
-      Library library = LibrariesUtil.getLibraryByName(ScalaApplicationSettings.getInstance().DEFAULT_SCALA_LIB_NAME);
-      if (library != null) {
-        myComboBox.selectLibrary(library);
-      }
-    }
-*/
   }
 
   public void disposeUIResources() {
-    if (myLibraryListener != null) {
-      LibraryTablesRegistrar.getInstance().getLibraryTable().removeListener(myLibraryListener);
-    }
   }
 
   private void createUIComponents() {
-  }
-
-  private void setUpComponents() {
-
-/*
-    if (myEditorContext != null && myEditorContext.getProject() != null) {
-      final Project project = myEditorContext.getProject();
-      myNewButton.addActionListener(new ActionListener() {
-        public void actionPerformed(final ActionEvent e) {
-          final FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false) {
-            public boolean isFileSelectable(VirtualFile file) {
-              return super.isFileSelectable(file) && ScalaConfigUtils.isScalaSdkHome(file);
-            }
-          };
-          final FileChooserDialog fileChooserDialog = FileChooserFactory.getInstance().createFileChooser(descriptor, project);
-          final VirtualFile[] files = fileChooserDialog.choose(null, project);
-          if (files.length > 0) {
-            String path = files[0].getPath();
-            if (ValidationResult.OK == ScalaConfigUtils.isScalaSdkHome(path)) {
-              Collection<String> versions = ScalaConfigUtils.getScalaVersions(myModule);
-              String version = ScalaConfigUtils.getScalaSDKVersion(path);
-              boolean addVersion = !versions.contains(version) ||
-                                   Messages.showOkCancelDialog(ScalaBundle.message("duplicate.scala.lib.version.add", version),
-                                                               ScalaBundle.message("duplicate.scala.lib.version"),
-                                                               Icons.BIG_ICON) == 0;
-
-              if (addVersion && !ScalaConfigUtils.UNDEFINED_VERSION.equals(version)) {
-                String name = myComboBox.generatePointerName(version);
-                final CreateLibraryDialog dialog = new CreateLibraryDialog(project, name);
-                dialog.show();
-                if (dialog.isOK()) {
-                  myComboBox.addSdk(new ScalaSDKPointer(name, path, version, dialog.isInProject()));
-                  newScalaLibName = name;
-                }
-              }
-            } else {
-              Messages.showErrorDialog(ScalaBundle.message("invalid.scala.sdk.path.message"),
-                                       ScalaBundle.message("invalid.scala.sdk.path.text"));
-            }
-          }
-        }
-      });
-    }
-*/
-  }
-
-  private class MyLibraryTableListener implements LibraryTable.Listener {
-
-    public void afterLibraryAdded(Library newLibrary) {
-    }
-
-    public void afterLibraryRenamed(Library library) {
-    }
-
-    public void afterLibraryRemoved(Library library) {
-    }
-
-    public void beforeLibraryRemoved(Library library) {
-    }
-
   }
 
 
