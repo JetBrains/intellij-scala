d2cc7cbeebdcb2dbecc703cd901bebf8eba44a60
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-23 16:05:39 +0000

    test for last fix for autoimport
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23345 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ testdata/autoImport/importIfExist/MoreComplexImport.scala
@@ -0,0 +1,27 @@
+package base.impl.statements
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 23.03.2009
+ */
+
+import api.base._
+
+class X extends /*ref*/Z {
+
+}
+/*
+package base.impl.statements
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 23.03.2009
+ */
+
+import _root_.base.types.Z
+import api.base._
+
+class X extends /*ref*/Z {
+
+}
+*/
