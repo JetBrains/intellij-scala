da684b449305598b29e038a9db2c787300b15611
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-03-23 11:57:53 +0000

    SCL-829
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23306 59f64563-22e8-0310-8d2c-98a15e1539f2

--- scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/expr/ScMatchStmt.scala
+++ scala_core/src/org/jetbrains/plugins/scala/lang/psi/api/expr/ScMatchStmt.scala
@@ -5,18 +5,22 @@ import impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import parser.ScalaPsiCreator
 
-/** 
-* @author Alexander Podkhalyuzin
-* Date: 06.03.2008
-*/
+/**
+ * @author Alexander Podkhalyuzin, ilyas
+ */
 
 trait ScMatchStmt extends ScExpression {
   def expr = findChild(classOf[ScExpression])
-  def getBranches: Seq[ScExpression] = getCaseClauses.caseClauses.map {
-    (clause: ScCaseClause) => clause.expr match {
-      case Some(expr) => expr
-      case None => ScalaPsiElementFactory.createExpressionFromText("{}", getManager)
+
+  def getBranches: Seq[ScExpression] = getCaseClauses match {
+    case null => Seq.empty
+    case c => c.caseClauses.map {
+      (clause: ScCaseClause) => clause.expr match {
+        case Some(expr) => expr
+        case None => ScalaPsiElementFactory.createExpressionFromText("{}", getManager)
+      }
     }
   }
+
   def getCaseClauses: ScCaseClauses = findChildByClass(classOf[ScCaseClauses])
 }
