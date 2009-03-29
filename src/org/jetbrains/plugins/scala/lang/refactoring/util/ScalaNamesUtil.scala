3936d67557c6eeb731cda3e63f125fd09a507f4b
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-03-29 12:01:27 +0000

    ScalaNamesUtil: Java -> Scala
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23532 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/lang/refactoring/util/ScalaNamesUtil.scala
@@ -0,0 +1,33 @@
+package org.jetbrains.plugins.scala.lang.refactoring.util
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.lexer.Lexer
+import org.jetbrains.plugins.scala.lang.lexer.ScalaLexer
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 24.06.2008
+ */
+object ScalaNamesUtil {
+  def isIdentifier(text: String): Boolean = {
+    ApplicationManager.getApplication.assertReadAccessAllowed
+    if (text == null) return false
+
+    val lexer = new ScalaLexer();
+    lexer.start(text, 0, text.length, 0)
+    if (lexer.getTokenType != ScalaTokenTypes.tIDENTIFIER) return false
+    lexer.advance
+    lexer.getTokenType == null
+  }
+
+  def isKeyword(text: String): Boolean = {
+    ApplicationManager.getApplication.assertReadAccessAllowed
+
+    val lexer = new ScalaLexer
+    lexer.start(text,0,text.length,0)
+    if (lexer.getTokenType == null || !ScalaTokenTypes.KEYWORDS.contains(lexer.getTokenType)) return false
+    lexer.advance
+    lexer.getTokenType == null
+  }
+}
