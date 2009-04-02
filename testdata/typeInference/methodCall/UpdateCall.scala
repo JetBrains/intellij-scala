2b82eb0f56fc09957040a093d01adba55cc57bbc
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-04-02 21:38:13 +0000

    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@23676 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ testdata/typeInference/methodCall/UpdateCall.scala
@@ -0,0 +1,7 @@
+object UpdateCall {
+  def update(x: Int, y: Int) = UpdateCall
+}
+
+val x = UpdateCall
+/*start*/x(1)/*end*/ = 3
+//UpdateCall
---
+++ testdata/typeInference/methodCall/simpleCall1.scala
@@ -0,0 +1,2 @@
+def foo(x: Int) = .0f
+/*start*/foo(3)/*end*/
