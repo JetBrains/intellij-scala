608a0a1540cc6c309f23aec31a969e3d222d8826
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-05-22 14:30:37 +0000

    Kaplan's usecase SCL-1007
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@24463 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScReferencePatternImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScReferencePatternImpl.scala
@@ -1,6 +1,5 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.base.patterns
 
-import _root_.org.jetbrains.plugins.scala.lang.psi.types.ScType
 import api.base.patterns._
 import api.toplevel.imports.ScImportStmt
 import com.intellij.psi.scope.PsiScopeProcessor
@@ -8,28 +7,42 @@ import psi.ScalaPsiElementImpl
 import com.intellij.lang.ASTNode
 import com.intellij.psi._
 import lang.lexer._
-import psi.types.Nothing
+import psi.types.{ScCompoundType, ScType, Nothing}
 
-/** 
-* @author Alexander Podkhalyuzin
-* Date: 28.02.2008
-*/
+/**
+ * @author Alexander Podkhalyuzin
+ * Date: 28.02.2008
+ */
 
-class ScReferencePatternImpl(node: ASTNode) extends ScBindingPatternImpl (node) with ScReferencePattern{
+class ScReferencePatternImpl(node: ASTNode) extends ScBindingPatternImpl(node) with ScReferencePattern {
   override def toString: String = "ReferencePattern"
+
   override def calcType = expectedType match {
     case Some(t) => t
     case None => Nothing
   }
 
-  override def processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement, place: PsiElement) = lastParent match {
-    case _: ScImportStmt if !isWildcard => {
-      ScType.extractClassType(calcType) match {
-        case Some((c, _)) => c.processDeclarations(processor, state, null, place)
-        case _ => true
-      }
+  override def processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement, place: PsiElement): Boolean = {
+    def processClassType(t: ScType) = ScType.extractClassType(t) match {
+      case Some((c, _)) => c.processDeclarations(processor, state, null, place)
+      case _ => true
     }
-    case _ => true
+
+    lastParent match {
+      case _: ScImportStmt => {
+        calcType match {
+          case ScCompoundType(comps, holders, aliases) => {
+            for (t <- comps) if (!processClassType(t)) return false
+            for (h <- holders; d <- h.declaredElements) if (!processor.execute(d, state)) return false
+            for (a <- aliases) if (!processor.execute(a, state)) return false
+            // todo add inner classes!
+            true
+          }
+          case t => processClassType(t)
+        }
+      }
+      case _ => true
+    }
   }
-  
+
 }
