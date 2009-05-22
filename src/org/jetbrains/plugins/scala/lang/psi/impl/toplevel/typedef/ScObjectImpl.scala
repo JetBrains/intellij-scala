b3b38eee062b375821492d9c5c08317b9448a9b1
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2009-05-22 16:20:03 +0000

    stubs for object vals added
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@24464 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/ScObjectImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/ScObjectImpl.scala
@@ -1,9 +1,20 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
 
+import api.expr.ScNewTemplateDefinition
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import api.toplevel.ScTyped
+import com.intellij.navigation.ItemPresentation
+import com.intellij.psi.impl.light.LightElement
+import com.intellij.util.IncorrectOperationException
+import java.lang.String
+import javax.swing.Icon
+import com.intellij.openapi.vcs.FileStatus
+import java.util.List
+import com.intellij.psi._
+import com.intellij.psi.scope.processor.MethodResolverProcessor
 import com.intellij.psi.scope.PsiScopeProcessor
-import com.intellij.psi.{PsiElement, PsiModifier, ResolveState}
-import stubs.elements.wrappers.DummyASTNode
-import stubs.ScTypeDefinitionStub
+import psi.stubs.elements.wrappers.DummyASTNode
+import psi.stubs.ScTypeDefinitionStub
 import com.intellij.psi.stubs.IStubElementType
 import com.intellij.psi.tree.IElementType
 import com.intellij.lang.ASTNode
@@ -17,15 +28,19 @@ import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.icons.Icons
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScModifierList
+import synthetic.{PsiMethodFake, SyntheticNamedElement}
+import types.{ScSubstitutor, ScFunctionType, ScType}
+import util.MethodSignatureBackedByPsiMethod
 
 /**
-* @author Alexander Podkhalyuzin
-* Date: 20.02.2008
-*/
+ * @author Alexander Podkhalyuzin
+ * Date: 20.02.2008
+ */
 
-class ScObjectImpl extends ScTypeDefinitionImpl with ScObject with ScTemplateDefinition{
-  def this(node: ASTNode) = {this(); setNode(node)}
-  def this(stub: ScTypeDefinitionStub) = {this(); setStub(stub); setNode(null)}
+class ScObjectImpl extends ScTypeDefinitionImpl with ScObject with ScTemplateDefinition {
+  def this(node: ASTNode) = {this (); setNode(node)}
+
+  def this(stub: ScTypeDefinitionStub) = {this (); setStub(stub); setNode(null)}
 
   override def toString: String = "ScObject"
 
@@ -51,8 +66,12 @@ class ScObjectImpl extends ScTypeDefinitionImpl with ScObject with ScTemplateDef
   override def getContainingClass() = null
 
   override def processDeclarations(processor: PsiScopeProcessor,
-                                  state: ResolveState,
-                                  lastParent: PsiElement,
-                                  place: PsiElement): Boolean =
-    super[ScTemplateDefinition].processDeclarations(processor, state, lastParent, place)
+                                   state: ResolveState,
+                                   lastParent: PsiElement,
+                                   place: PsiElement): Boolean = {
+      val proceed = super[ScTemplateDefinition].processDeclarations(processor, state, lastParent, place)
+      if (!proceed) return false;
+      true
+    }
+
 }
