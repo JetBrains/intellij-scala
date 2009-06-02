a06563f47eca744474b2a17069b6bdde7c927cf2
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-06-02 06:46:58 +0000

    another caches try
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@24599 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScTypeAliasDefinition.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScTypeAliasDefinition.scala
@@ -2,6 +2,13 @@ package org.jetbrains.plugins.scala.lang.psi.api.statements
 
 import _root_.org.jetbrains.plugins.scala.lang.psi.types.ScType
 import base.types.{ScTypeInferenceResult, ScTypeElement}
+import com.intellij.openapi.util.Key
+import com.intellij.psi.util.{PsiModificationTracker, CachedValueProvider, CachedValue}
+import com.intellij.psi.{PsiManager, PsiElement}
+
+
+
+import impl.toplevel.typedef.TypeDefinitionMembers
 import toplevel.ScNamedElement
 /**
 * @author Alexander Podkhalyuzin
@@ -18,8 +25,31 @@ trait ScTypeAliasDefinition extends ScTypeAlias {
     } else aliasedTypeElement.getType(visited + this)
   }
 
-  lazy val aliasedType: ScTypeInferenceResult = aliasedType(Set[ScNamedElement]())
+  def aliasedType: ScTypeInferenceResult = ScTypeAliasDefinitionCaches.get(
+      this, ScTypeAliasDefinitionCaches.key,
+      new ScTypeAliasDefinitionCaches.MyProvider(this, {ta: ScTypeAliasDefinition => ta.aliasedType(Set[ScNamedElement]())})
+    )
 
   def lowerBound = aliasedType(Set[ScNamedElement]())
   def upperBound = aliasedType(Set[ScNamedElement]())
+}
+
+//todo: copy from TypeDefinitionMembers, rewrite or remove dublicates
+private object ScTypeAliasDefinitionCaches {
+  val key: Key[CachedValue[ScTypeInferenceResult]] = Key.create("alised type key")
+
+  def get[Dom <: PsiElement, T](e: Dom, key: Key[CachedValue[T]], provider: => CachedValueProvider[T]): T = {
+    var computed: CachedValue[T] = e.getUserData(key)
+    if (computed == null) {
+      val manager = PsiManager.getInstance(e.getProject).getCachedValuesManager
+      computed = manager.createCachedValue(provider, false)
+      e.putUserData(key, computed)
+    }
+    computed.getValue
+  }
+
+  class MyProvider[Dom, T](e: Dom, builder: Dom => T) extends CachedValueProvider[T] {
+    def compute() = new CachedValueProvider.Result(builder(e),
+      PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT)
+  }
 }
