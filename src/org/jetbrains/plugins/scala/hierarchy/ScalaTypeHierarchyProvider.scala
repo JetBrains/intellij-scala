8485c08d406a3d710917d97b7162ad35d48a48ff
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-06-09 09:58:37 +0000

    hierarchyView
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@24791 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/hierarchy/ScalaTypeHierarchyProvider.scala
@@ -0,0 +1,15 @@
+package org.jetbrains.plugins.scala.hierarchy
+
+
+import com.intellij.codeInsight.{TargetElementUtilBase, TargetElementUtil}
+import com.intellij.openapi.actionSystem.{DataContext, PlatformDataKeys, LangDataKeys}
+import com.intellij.psi.{PsiElement, PsiDocumentManager}
+import com.intellij.ide.hierarchy.`type`.JavaTypeHierarchyProvider
+import lang.psi.api.toplevel.typedef.ScTypeDefinition
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 09.06.2009
+ */
+
+class ScalaTypeHierarchyProvider extends JavaTypeHierarchyProvider {
+}
