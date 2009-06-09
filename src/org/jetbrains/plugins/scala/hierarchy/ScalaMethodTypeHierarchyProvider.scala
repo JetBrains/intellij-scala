f48e3f6b3e3292a348b7e73d9f765fc6e6039260
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-06-09 11:43:24 +0000

    method hieratchyView (like in java, so it's buggy)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@24797 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/hierarchy/ScalaMethodTypeHierarchyProvider.scala
@@ -0,0 +1,13 @@
+package org.jetbrains.plugins.scala.hierarchy
+
+
+import com.intellij.ide.hierarchy.method.JavaMethodHierarchyProvider
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 09.06.2009
+ */
+
+class ScalaMethodHierarchyProvider extends JavaMethodHierarchyProvider {
+
+}
