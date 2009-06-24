1989d4db32a1669f288536cf925fd52bd4be0365
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-06-24 09:22:51 +0000

    Maya M1 build Scala console removed
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25175 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/console/ConsoleUserInputListener.java
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.scala.console;
-
-/**
- * User: Alexander Podkhalyuzin
- * Date: 21.05.2009
- */
-public interface ConsoleUserInputListener {
-  public void userTextPerformed(String userText);
-}
