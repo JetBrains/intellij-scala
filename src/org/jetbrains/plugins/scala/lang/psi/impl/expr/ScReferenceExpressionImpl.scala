6ce19e51571a6b99b869082252e9e8a1a65c780b
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-01 08:23:49 +0000

    fixed param info for implicits (not filter SameNameCompletionProcessor as CompletionProcessor)
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25344 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScReferenceExpressionImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScReferenceExpressionImpl.scala
@@ -103,7 +103,8 @@ class ScReferenceExpressionImpl(node: ASTNode) extends ScalaPsiElementImpl(node)
       processor.processType(e.cashedType, e, ResolveState.initial)
 
       val settings: ScalaCodeStyleSettings = CodeStyleSettingsManager.getSettings(getProject).getCustomSettings(classOf[ScalaCodeStyleSettings])
-      if (settings.CHECK_IMPLICITS && (processor.candidates.length == 0 || processor.isInstanceOf[CompletionProcessor])) {
+      if (settings.CHECK_IMPLICITS && (processor.candidates.length == 0 || processor.isInstanceOf[CompletionProcessor]) ||
+        processor.isInstanceOf[SameNameCompletionProcessor]) {
         for (t <- e.getImplicitTypes) {
           processor.processType(t, e, ResolveState.initial.put(ImportUsed.key, e.getImportsForImplicit(t)))
         }
