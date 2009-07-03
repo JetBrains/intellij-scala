d6075e3f1949222c6bafeb67c92e19fc6f33adb5
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-03 07:52:35 +0000

    Inspection for files, which hasn't corresponding class: quickfixes
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25416 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/codeInspection/fileNameInspection/ScalaRenameClassQuickFix.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.scala.codeInspection.fileNameInspection
+
+
+import com.intellij.codeInspection.{ProblemDescriptor, LocalQuickFix}
+import com.intellij.openapi.project.Project
+import com.intellij.refactoring.{RefactoringFactory, RefactoringActionHandlerFactory}
+import java.lang.String
+import lang.psi.api.toplevel.typedef.ScTypeDefinition
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 03.07.2009
+ */
+
+class ScalaRenameClassQuickFix(clazz: ScTypeDefinition, name: String) extends LocalQuickFix {
+  def applyFix(project: Project, descriptor: ProblemDescriptor): Unit = {
+    RefactoringFactory.getInstance(project).createRename(clazz, name).run
+  }
+
+  def getName: String = "Rename Type Definition " + clazz.getName + " to " + name
+
+  def getFamilyName: String = "Rename Type Definition"
+}
---
+++ src/org/jetbrains/plugins/scala/codeInspection/fileNameInspection/ScalaRenameFileQuickFix.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.scala.codeInspection.fileNameInspection
+
+
+import com.intellij.codeInspection.{ProblemDescriptor, LocalQuickFix}
+import com.intellij.openapi.project.Project
+import com.intellij.refactoring.RefactoringFactory
+import java.lang.String
+import lang.psi.api.ScalaFile
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 03.07.2009
+ */
+
+class ScalaRenameFileQuickFix(file: ScalaFile, name: String) extends LocalQuickFix {
+  def applyFix(project: Project, descriptor: ProblemDescriptor): Unit = {
+    RefactoringFactory.getInstance(project).createRename(file, name).run
+  }
+
+  def getName: String = "Rename File " + file.getName + " to " + name
+
+  def getFamilyName: String = "Rename File"
+}
