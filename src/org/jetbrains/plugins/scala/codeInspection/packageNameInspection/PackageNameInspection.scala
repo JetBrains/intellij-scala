ae28fa20fd334cff7c0d9b6cf23d6b9e8b7ef788
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-08 12:58:45 +0000

    wrong package inspection
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25474 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ src/org/jetbrains/plugins/scala/codeInspection/packageNameInspection/PackageNameInspection.scala
@@ -0,0 +1,68 @@
+package org.jetbrains.plugins.scala.codeInspection.packageNameInspection
+
+
+import collection.mutable.ArrayBuffer
+import com.intellij.codeInspection._
+
+import com.intellij.psi.{PsiPackage, JavaDirectoryService, PsiFile}
+
+import java.lang.String
+import scala.lang.psi.api.ScalaFile
+import scala.lang.psi.api.toplevel.packaging.ScPackageStatement
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 08.07.2009
+ */
+
+class PackageNameInspection extends LocalInspectionTool {
+  def getGroupDisplayName: String = InspectionsUtil.SCALA
+
+  def getDisplayName: String = "Package Name Inspection"
+
+  def getShortName: String = "Package Name"
+
+  override def isEnabledByDefault: Boolean = true
+
+  override def getStaticDescription: String = "Inspection for files with package statement which not correspond to package structure"
+
+  override def getID: String = "Package Name"
+
+  override def checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array[ProblemDescriptor] = {
+    file match {
+      case file: ScalaFile => {
+        if (file.getClasses.length == 0) return null
+        val dir = file.getContainingDirectory
+        if (dir == null) return null
+        val pack = JavaDirectoryService.getInstance().getPackage(dir)
+        if (pack == null) return null
+        file.packageStatement match {
+          case None => return null //todo: check it too
+          case Some(packSt: ScPackageStatement) => {
+            val buffer = new ArrayBuffer[LocalQuickFix]
+            val ref = packSt.reference
+            val p = ref.resolve
+            p match {
+              case p: PsiPackage => {
+                if (p.getQualifiedName != pack.getQualifiedName) {
+                  buffer += new ScalaRenamePackageQuickFix(file, pack.getQualifiedName)
+                  buffer += new ScalaMoveToPackageQuickFix(file, p)
+                }
+              }
+              case _ => {
+                buffer += new ScalaRenamePackageQuickFix(file, pack.getQualifiedName)
+              }
+            }
+            if (!buffer.isEmpty) {
+              return Array[ProblemDescriptor](manager.createProblemDescriptor(packSt.reference, "Package name doesn't correspond to directories structure",
+                buffer.toArray, ProblemHighlightType.GENERIC_ERROR_OR_WARNING))
+            } else {
+              return null
+            }
+          }
+        }
+      }
+      case _ => return null
+    }
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/codeInspection/packageNameInspection/ScalaRenamePackageQuickFix.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.plugins.scala.codeInspection.packageNameInspection
+
+
+import com.intellij.codeInspection.{ProblemDescriptor, LocalQuickFix}
+import com.intellij.openapi.project.Project
+import java.lang.String
+import lang.psi.api.ScalaFile
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 08.07.2009
+ */
+
+class ScalaRenamePackageQuickFix(file: ScalaFile, name: String) extends LocalQuickFix {
+  def applyFix(project: Project, descriptor: ProblemDescriptor): Unit = {
+    file.setPackageName(name)
+  }
+
+  def getName: String = "Rename Package to " + name
+
+  def getFamilyName: String = "Rename Package"
+}
