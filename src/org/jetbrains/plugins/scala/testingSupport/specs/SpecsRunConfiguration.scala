9060b7bf3cdcdce7593fee76942f49b503d7ce2d
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-13 12:31:10 +0000

    tests running in subpackages
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25520 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/testingSupport/specs/SpecsRunConfiguration.scala
+++ src/org/jetbrains/plugins/scala/testingSupport/specs/SpecsRunConfiguration.scala
@@ -121,7 +121,15 @@ class SpecsRunConfiguration(val project: Project, val configurationFactory: Conf
     if (clazz != null) {
       if (clazz.isInheritor(suiteClass, true)) classes += clazz
     } else {
-      for (cl <- pack.getClasses) {
+      def getClasses(pack: PsiPackage): Seq[PsiClass] = {
+        val buffer = new ArrayBuffer[PsiClass]
+        buffer ++= pack.getClasses
+        for (p <- pack.getSubPackages) {
+          buffer ++= p.getClasses
+        }
+        buffer.toSeq
+      }
+      for (cl <- getClasses(pack)) {
         if (cl.isInheritor(suiteClass, true)) classes += cl
       }
     }
