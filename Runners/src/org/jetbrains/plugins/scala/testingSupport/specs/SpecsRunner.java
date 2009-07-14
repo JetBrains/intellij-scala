f9d05afd95d103522c2c6c9a5f627db2add44faa
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-14 10:12:34 +0000

    Runners module is pure java module now.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25540 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ Runners/src/org/jetbrains/plugins/scala/testingSupport/specs/SpecsRunner.java
@@ -0,0 +1,75 @@
+package org.jetbrains.plugins.scala.testingSupport.specs;
+
+import org.specs.Specification;
+import org.specs.runner.NotifierRunner;
+import org.specs.util.Classes$;
+
+import java.util.ArrayList;
+
+import scala.Option;
+import scala.Some;
+
+public class SpecsRunner {
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("The first argument should be the specification class name");
+      return;
+    }
+    ArrayList<String> classes = new ArrayList<String>();
+    String sysFilter = ".*";
+    String exFilter = ".*";
+    int i = 0;
+    while (i < args.length) {
+      if (args[i].startsWith("-sus:")) {
+        sysFilter = args[i].substring(5);
+        i = i + 1;
+      } else if (args[i].equals("-s")) {
+        i = i + 1;
+        while (i < args.length && !args[i].startsWith("-")) {
+          classes.add(args[i]);
+          i = i + 1;
+        }
+      } else if (args[i].startsWith("-ex:")) {
+        exFilter = args[i].substring(4);
+        i = i + 1;
+      } else {
+        i = i + 1;
+      }
+    }
+
+    final String finalSysFilter = sysFilter;
+    final String finalExFilter = exFilter;
+
+    for (String clazz : classes) {
+      Option spec = Classes$.MODULE$.createObject(clazz);
+      if (spec instanceof Some) {
+        Specification s = (Specification) (spec.get());
+        (new NotifierRunner(s, new SpecsNotifier()) {
+          public String susFilterPattern() {
+            return finalSysFilter;
+          }
+
+          public String exampleFilterPattern() {
+            return finalExFilter;
+          }
+        }).reportSpecs();
+      } else {
+        spec = Classes$.MODULE$.createObject(clazz + "$");
+        if (spec instanceof Some) {
+          Specification s = (Specification) (spec.get());
+          (new NotifierRunner(s, new SpecsNotifier()) {
+            public String susFilterPattern() {
+              return finalSysFilter;
+            }
+
+            public String exampleFilterPattern() {
+              return finalExFilter;
+            }
+          }).reportSpecs();
+        } else {
+          System.out.println("Scala Plugin internal error: no test class was found");
+        }
+      }
+    }
+  }
+}
