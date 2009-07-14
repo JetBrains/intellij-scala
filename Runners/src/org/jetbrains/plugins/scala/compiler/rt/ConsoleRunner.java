314b374fa07d40789d744421be4b8fa624391389
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-14 10:12:34 +0000

    Runners module is pure java module now.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25540 59f64563-22e8-0310-8d2c-98a15e1539f2

--- Runners/src/org/jetbrains/plugins/scala/compiler/rt/ConsoleRunner.java
+++ Runners/src/org/jetbrains/plugins/scala/compiler/rt/ConsoleRunner.java
@@ -9,6 +9,10 @@ import scala.tools.nsc.GenericRunnerSettings;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import scala.Function1;
+import scala.Seq;
+import scala.runtime.BoxedArray;
+import scala.collection.mutable.ListBuffer;
 
 /**
  * User: Alexander Podkhalyuzin
@@ -16,12 +20,68 @@ import java.io.PrintWriter;
  */
 public class ConsoleRunner {
   public static void main(String[] args) {
-    //ConsoleRunnerUtil.addQuotes(args);
-    GenericRunnerCommand command = new GenericRunnerCommand(ConsoleRunnerUtil.listOf(args),
-        ConsoleRunnerUtil.getSettings(args),
-        ConsoleRunnerUtil.getUnitFunction());
+    GenericRunnerCommand command = new GenericRunnerCommand(toList(args),
+        getSettings(args),
+        getUnitFunction());
     Settings settings = command.settings();
     ConsoleRunnerUtil.setParamParser(args, settings);
     (new InterpreterLoop(new Some(new BufferedReader(new InputStreamReader(System.in))), new PrintWriter(System.out))).main(settings);
+  }
+
+  public static scala.List toList(String[] args) {
+    ListBuffer<String> res = new ListBuffer<String>();
+    for (String arg : args) {
+      res.$plus$eq(arg);
+    }
+    return res.toList();
+  }
+
+  public static Function1 getUnitFunction() {
+    return new Function1<String, Void>() {
+      public Void apply(String t) {
+        System.out.println(t);
+        return null;
+      }
+
+      public <A> Function1<A, Void> compose(Function1<A, String> f) {
+        return f.andThen(this);
+      }
+
+      public <A> Function1<String, A> andThen(Function1<Void, A> f) {
+        return f.compose(this);
+      }
+    };
+  }
+
+  public static GenericRunnerSettings getSettings(String[] args) {
+    return new GenericRunnerSettings(ConsoleRunner.getUnitFunction()) {
+      public void parseParams(String line, Function1 error) {
+        scala.List args;
+        if (line.trim().equals("")) args = toList(new String[0]);
+        else {
+          String[] newArgs = line.trim().split("[|]");
+          for (int i = 0; i < newArgs.length; i++) {
+            newArgs[i] = newArgs[i].trim();
+          }
+          args = toList(newArgs);
+        }
+        while (!args.isEmpty()) {
+          scala.List argsBuf = args;
+          if (((String) args.head()).startsWith("-")) {
+            scala.List settings = allSettings();
+            for (int i = 0; i < settings.length(); ++i) {
+              args = ((Settings.Setting) settings.apply(i)).tryToSet(args);
+            }
+            /*for (setting : allSettings())
+              args = setting.tryToSet(args);*/
+          }
+          else error.apply("Parameter '" + args.head() + "' does not start with '-'.");
+          if (argsBuf == args) {
+            error.apply("Parameter '" + args.head() + "' is not recognised by Scalac.");
+            args = args.tail();
+          }
+        }
+      }
+    };
   }
 }
