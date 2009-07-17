6b240b4b7ba9362d735e5dd611e10c088e2bba5b
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-17 10:44:43 +0000

    cleanup
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25602 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScPatternListImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScPatternListImpl.scala
@@ -1,30 +1,10 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.base
 
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
-import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-
-
-
-
-
-
-import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi._
-
-import org.jetbrains.annotations._
-
-import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
-import org.jetbrains.plugins.scala.icons.Icons
-
-
 import org.jetbrains.plugins.scala.lang.psi.api.base._
 import api.base.patterns._
-import _root_.scala.collection.mutable._
-
-/** 
+/**
 * @author Alexander Podkhalyuzin
 * Date: 22.02.2008
 */
