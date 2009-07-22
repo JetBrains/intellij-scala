943a71aa08833c88b0c23cac6a1a91313bd0f993
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-07-22 13:36:02 +0000

    Change for right error hanling in spellchecker.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25696 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/synthetic/JavaIdentifier.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/synthetic/JavaIdentifier.scala
@@ -3,9 +3,14 @@ package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.synthetic
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.impl.light.LightIdentifier
 import com.intellij.psi.{PsiElement, PsiFile}
+import java.lang.String
 
 class JavaIdentifier(scalaId : PsiElement) extends LightIdentifier(scalaId.getManager, scalaId.getText) {
   override def getTextRange = scalaId.getTextRange
+
+  override def getStartOffsetInParent: Int = scalaId.getStartOffsetInParent
+
+  override def getTextOffset: Int = scalaId.getTextOffset
 
   override def getContainingFile = scalaId.getContainingFile
 }
