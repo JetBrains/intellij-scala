42bb1223f7bb585a249344b4bbbd24c14895381c
Jason Zaugg <jzaugg@gmail.com>
2009-07-28 05:55:22 +0000

    Added a failing test for Import Tracking with implicit conversions. The import B._ is not tracked. I think this is because the type new Identity { ... } is a compound type, but I can't figure exactly how to fix it. I found this problem testing out the plugin against Scalaz.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@25783 59f64563-22e8-0310-8d2c-98a15e1539f2

---
+++ testdata/optimize/implicits/ImplicitReference2.scala
@@ -0,0 +1,52 @@
+package scatch
+
+sealed trait Identity {
+  def zero: Int = 0
+}
+
+object A {
+  implicit def IdentityTo[A](x: A) = new Identity
+}
+object B {
+  implicit def IdentityTo[A](x: A) = new Identity {
+    val value = null
+  }
+}
+
+object Scratch {
+  {
+    import A._
+    "A".zero
+  }
+  {
+    import B._
+    "B".zero
+  }
+}
+/*
+package scatch
+
+sealed trait Identity {
+  def zero: Int = 0
+}
+
+object A {
+  implicit def IdentityTo[A](x: A) = new Identity
+}
+object B {
+  implicit def IdentityTo[A](x: A) = new Identity {
+    val value = null
+  }
+}
+
+object Scratch {
+  {
+    import A._
+    "A".zero
+  }
+  {
+    import B._
+    "B".zero
+  }
+}
+*/
