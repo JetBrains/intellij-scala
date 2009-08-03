1e61f054d7fc0721691890e0b512324ed3fd9ead
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-08-03 15:36:45 +0000

    stubs improvent for highlighting performance test
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@26015 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScIdListImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScIdListImpl.scala
@@ -1,17 +1,29 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.base
 
+import api.base.{ScFieldId, ScIdList}
 import com.intellij.lang.ASTNode
+import com.intellij.util.ArrayFactory
+import parser.ScalaElementTypes
 import psi.stubs.{ScIdListStub}
 
-import org.jetbrains.plugins.scala.lang.psi.api.base._
 /**
 * @author Alexander Podkhalyuzin
 * Date: 22.02.2008
 */
 
-class ScIdListImpl private () extends ScalaStubBasedElementImpl[ScIdList] with ScIdList{
+class ScIdListImpl private () extends ScalaStubBasedElementImpl[ScIdList] with ScIdList {
   def this(node: ASTNode) = {this(); setNode(node)}
   def this(stub: ScIdListStub) = {this(); setStub(stub); setNode(null)}
+
+  def fieldIds: Seq[ScFieldId]  = {
+    val stub = getStub
+    if (stub != null) {
+      Seq(stub.getChildrenByType(ScalaElementTypes.FIELD_ID, new ArrayFactory[ScFieldId] {
+        def create(count: Int): Array[ScFieldId] = new Array[ScFieldId](count)
+      }): _*)
+    } else
+      Seq(findChildrenByClass(classOf[ScFieldId]): _*)
+  }
 
   override def toString: String = "ListOfIdentifiers"
 }
