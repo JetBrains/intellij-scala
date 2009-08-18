937879e07ea00dc78a653f59caece84b49ce8f75
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-08-18 09:45:45 +0000

    checkPsi method to avoid ClassCastException.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@26358 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/stubs/util/ScalaStubsUtil.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/util/ScalaStubsUtil.scala
@@ -15,6 +15,9 @@ import com.intellij.psi.{PsiElement, PsiClass}
 import elements.ScTypeDefinitionElementType
 import java.util.ArrayList
 import psi.impl.toplevel.templates.ScExtendsBlockImpl
+import com.intellij.util.indexing.FileBasedIndex
+import com.intellij.psi.util.{PsiUtilBase, PsiUtil}
+import com.intellij.openapi.diagnostic.Logger
 
 /**
  * User: Alexander Podkhalyuzin
@@ -26,19 +29,23 @@ object ScalaStubsUtil {
     val name: String = clazz.getName()
     if (name == null) return Seq.empty
     val inheritors = new ArrayBuffer[ScTypeDefinition]
-    val iterator: java.util.Iterator[ScExtendsBlock] =
-      StubIndex.getInstance().get(ScDirectInheritorsIndex.KEY, name, clazz.getProject(), scope).iterator
+    val iterator: java.util.Iterator[PsiElement] =
+      StubIndex.getInstance().get(ScDirectInheritorsIndex.KEY, name, clazz.getProject(), scope).iterator.
+              asInstanceOf[java.util.Iterator[PsiElement]]
     while (iterator.hasNext) {
-      val extendsBlock = iterator.next
-      val stub = extendsBlock.asInstanceOf[ScExtendsBlockImpl].getStub
-      if (stub != null) {
-        if (stub.getParentStub.getStubType.isInstanceOf[ScTypeDefinitionElementType[_ <: ScTypeDefinition]]) {
-          inheritors += stub.getParentStub.getPsi.asInstanceOf[ScTypeDefinition]
+      val extendsBlock: PsiElement = iterator.next
+      if (checkPsi(extendsBlock, classOf[ScExtendsBlock])) {
+        val stub = extendsBlock.asInstanceOf[ScExtendsBlockImpl].getStub
+        if (stub != null) {
+          if (stub.getParentStub.getStubType.isInstanceOf[ScTypeDefinitionElementType[_ <: ScTypeDefinition]]) {
+            inheritors += stub.getParentStub.getPsi.asInstanceOf[ScTypeDefinition]
+          }
         }
-      } else {
-        extendsBlock.getParent match {
-          case tp: ScTypeDefinition => inheritors += tp
-          case _ =>
+        else {
+          extendsBlock.getParent match {
+            case tp: ScTypeDefinition => inheritors += tp
+            case _ =>
+          }
         }
       }
     }
@@ -50,4 +57,20 @@ object ScalaStubsUtil {
     if (name == null) return Nil
     List.fromArray(StubIndex.getInstance.get(ScAnnotatedMemberIndex.KEY, name, clazz.getProject, scope).toArray(Array[ScMember]()))
   }
+
+  def checkPsi[T <: PsiElement](element: PsiElement, clazz: Class[T]): Boolean = {
+    element match {
+      case x: T => return true
+      case _ => {
+        val faultyContainer = PsiUtilBase.getVirtualFile(element)
+        LOG.error("Wrong Psi in Psi list: " + faultyContainer)
+        if (faultyContainer != null && faultyContainer.isValid) {
+          FileBasedIndex.getInstance.requestReindex(faultyContainer)
+        }
+        return false
+      }
+    }
+  }
+
+  private val LOG = Logger.getInstance("#org.jetbrains.plugins.scala.lang.psi.stubs.util.ScalaStubsUtil")
 }
