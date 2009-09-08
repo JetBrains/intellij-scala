e8b029194340166d976b4efe142bfb1d24846479
Jason Zaugg <jzaugg@gmail.com>
2009-09-08 22:21:54 +0000

    Find usages for Type Parameters was broken because: a) they are instances of PsiClassFake, and b) Pattern Matching is buggy in my scalac. https://lampsvn.epfl.ch/trac/scala/ticket/2337.
    When scalac is fixed, the lines "case c: ScTypeParam => (c: ScNamedElement).getName" should turn into a compile error for "unreachable code."
    
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@26950 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/findUsages/ScalaFindUsagesProvider.scala
+++ src/org/jetbrains/plugins/scala/lang/findUsages/ScalaFindUsagesProvider.scala
@@ -14,6 +14,8 @@ import psi.api.toplevel.typedef.{ScClass, ScTypeDefinition, ScTrait, ScObject}
 import com.intellij.lang.findUsages.FindUsagesProvider
 import org.jetbrains.annotations.{Nullable, NotNull}
 import com.intellij.psi.tree.TokenSet
+import psi.impl.toplevel.PsiClassFake
+import psi.api.statements.params.ScTypeParam
 
 class ScalaFindUsagesProvider extends FindUsagesProvider {
   @Nullable
@@ -39,8 +41,9 @@ class ScalaFindUsagesProvider extends FindUsagesProvider {
       case _: ScClass => "class"
       case _: ScObject => "object"
       case _: ScTrait => "trait"
-      case c: PsiClass => if (c.isInterface) "interface" else "class"
+      case RealPsiClass(c) => if (c.isInterface) "interface" else "class"
       case _: PsiMethod => "method"
+      case _: ScTypeParam => "type parameter"
       case _: ScBindingPattern => {
         var parent = element
         while (parent match {case null | _: ScValue | _: ScVariable => false case _ => true}) parent = parent.getParent
@@ -61,32 +64,36 @@ class ScalaFindUsagesProvider extends FindUsagesProvider {
   @NotNull
   override def getDescriptiveName(element: PsiElement): String = {
     val name = element match {
-      case c: PsiClass => c.getQualifiedName
+      case RealPsiClass(c) => c.getQualifiedName
       case x: PsiMethod => PsiFormatUtil.formatMethod(x, PsiSubstitutor.EMPTY,
         PsiFormatUtilBase.SHOW_NAME | PsiFormatUtilBase.SHOW_PARAMETERS,
         PsiFormatUtilBase.SHOW_TYPE) + " of " + getDescriptiveName(x.getContainingClass)
       case x: PsiVariable => x.getName
       case x: PsiFile => x.getName
-      case x: ScNamedElement => x.getName
+      case x: ScNamedElement => x.getName // todo This line should match, but doesn't. https://lampsvn.epfl.ch/trac/scala/ticket/2337
+      case x: ScTypeParam => (x: ScNamedElement).getName // todo Should even compile! Previous pattern should match! Scalac bug?
       case _ => element.getText
     }
-    if (name == null) return "anonymous" else name
+    Option(name) getOrElse "anonymous"
   }
 
   @NotNull
   override def getNodeText(element: PsiElement, useFullName: Boolean): String = {
-    element match {
-      case c: PsiClass => if (useFullName) c.getQualifiedName else c.getName
+    val name = element match {
+      case RealPsiClass(c) => if (useFullName) c.getQualifiedName else c.getName
       case c: PsiMethod => PsiFormatUtil.formatMethod(c, PsiSubstitutor.EMPTY,
               PsiFormatUtilBase.SHOW_NAME | PsiFormatUtilBase.SHOW_PARAMETERS,
               PsiFormatUtilBase.SHOW_TYPE)
       case c: PsiVariable => c.getName
       case c: PsiFile => c.getName
       case c: ScNamedElement => c.getName
-      case _ => {
-        val text = element.getText
-        if (text == null) "" else text
-      }
+      case c: ScTypeParam => (c: ScNamedElement).getName // todo Should not even compile! Previous pattern should match! Scalac bug? https://lampsvn.epfl.ch/trac/scala/ticket/2337
+      case _ => element.getText
     }
+    Option(name) getOrElse "anonymous"
+  }
+
+  object RealPsiClass {
+    def unapply(c: PsiClass) = if (c.isInstanceOf[PsiClassFake]) None else Some(c)
   }
 }
