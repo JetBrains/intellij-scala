803a269363b2c6bac40cb5a5d3f6fa299df4bd8b
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-10-04 15:36:34 +0000

    Change timing to clear difference between different agents.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@27325 59f64563-22e8-0310-8d2c-98a15e1539f2

--- test/org/jetbrains/plugins/scala/performance/highlighting/HighlightingPerformanceTest.scala
+++ test/org/jetbrains/plugins/scala/performance/highlighting/HighlightingPerformanceTest.scala
@@ -88,7 +88,7 @@ object addressbook {
     val file = PsiFileFactory.getInstance(myFixture.getProject).
             createFileFromText("dummy." + ScalaFileType.SCALA_FILE_TYPE.getDefaultExtension(),
       ScalaFileType.SCALA_LANGUAGE, text, true, false)
-    TestUtils.assertTiming("Failed highlighting performance test", 4500,
+    TestUtils.assertTiming("Failed highlighting performance test", 6000,
       new Runnable {
         def run: Unit = {
           try {
