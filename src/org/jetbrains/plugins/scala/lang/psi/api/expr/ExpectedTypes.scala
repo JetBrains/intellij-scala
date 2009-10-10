4c6ccf7df7d6c137cf4ddde522b0949b625a7862
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-10-10 09:24:18 +0000

    Improved expected types for using in Smart completion.
    git-svn-id: http://svn.jetbrains.org/idea/Trunk/scala@27343 59f64563-22e8-0310-8d2c-98a15e1539f2

--- src/org/jetbrains/plugins/scala/lang/psi/api/expr/ExpectedTypes.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/expr/ExpectedTypes.scala
@@ -5,9 +5,10 @@ package api
 package expr
 
 import base.patterns.ScCaseClause
-import resolve.ScalaResolveResult
-import statements.ScFunction
 import types.{ScType, ScFunctionType}
+import com.intellij.psi.PsiNamedElement
+import statements._
+import toplevel.ScTyped
 
 /**
  * @author ilyas
@@ -17,18 +18,22 @@ import types.{ScType, ScFunctionType}
 
 object ExpectedTypes {
   def expectedExprType(expr: ScExpression): Option[ScType] = expr.getParent match {
-  //see SLS[6.11]
+    //see SLS[6.11]
     case b: ScBlockExpr => b.lastExpr match {
       case Some(e) if e eq expr => b.expectedType
       case _ => None
     }
     //see SLS[6.16]
-    case cond: ScIfStmt => cond.expectedType
+    case cond: ScIfStmt if cond.condition.getOrElse(null: ScExpression) == expr => Some(types.Boolean)
+    case cond: ScIfStmt if cond.elseBranch != None => cond.expectedType
+    case cond: ScIfStmt => Some(types.Unit)
     //see SLA[6.22]
     case tb: ScTryBlock => tb.lastExpr match {
       case Some(e) if e eq expr => tb.expectedType
       case _ => None
     }
+    //todo: make catch block an expression with appropriate type and expected type (PartialFunction[Throwable, pt])
+    case fb: ScFinallyBlock => Some(types.Unit)
     //see SLS[8.4]
     case c: ScCaseClause => c.getParent.getParent match {
       case m: ScMatchStmt => m.expectedType
@@ -38,6 +43,53 @@ object ExpectedTypes {
     case f: ScFunctionExpr => f.expectedType match { //There is only one way to be fun's child - it's result
       case Some(ScFunctionType(rt, _)) => Some(rt)
       case _ => None
+    }
+    //SLS[6.13]
+    case t: ScTypedStmt => {
+      t.typeElement match {
+        case Some(_) => Some(t.getType)
+        case _ => None
+      }
+    }
+    //SLS[6.15]
+    case a: ScAssignStmt if a.getRExpression.getOrElse(null: ScExpression) == expr => {
+      a.getLExpression match {
+        case ref: ScReferenceExpression => {
+          ref.resolve match {
+            case named: PsiNamedElement => {
+              ScalaPsiUtil.nameContext(named) match {
+                case v: ScValue => Some(named.asInstanceOf[ScTyped].calcType)
+                case v: ScVariable => Some(named.asInstanceOf[ScTyped].calcType)
+                case f: ScFunction => None //todo: find functionName_= method and do as argument call expected type
+                case _ => None
+              }
+            }
+            case _ => None
+          }
+        }
+        case call: ScMethodCall => None//todo: as argumets call expected type
+        case _ => None
+      }
+    }
+    //SLS[4.1]
+    case v: ScPatternDefinition if v.expr == expr => {
+      v.typeElement match {
+        case Some(_) => Some(v.getType)
+        case _ => None
+      }
+    }
+    case v: ScVariableDefinition if v.expr == expr => {
+      v.typeElement match {
+        case Some(_) => Some(v.getType)
+        case _ => None
+      }
+    }
+    //SLS[4.6]
+    case v: ScFunctionDefinition if v.body.getOrElse(null: ScExpression) == expr => {
+      v.returnTypeElement match {
+        case Some(_) => Some(v.returnType)
+        case _ => None
+      }
     }
     //...
     case args: ScArgumentExprList => args.getParent match {
