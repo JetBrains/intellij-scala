3b938ecdf3baed0f8107f95c0e9556681d1c1917
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-10-31 16:36:17 +0300

    Cleanup

--- src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/TypeDefinitionMembers.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/TypeDefinitionMembers.scala
@@ -246,46 +246,10 @@ object TypeDefinitionMembers {
   }
 
   import ValueNodes.{Map => VMap}, MethodNodes.{Map => MMap}, TypeNodes.{Map => TMap}, SignatureNodes.{Map => SMap}
-  @deprecated val valsKey: Key[CachedValue[(VMap, VMap)]] = Key.create("vals key")
-  @deprecated val methodsKey: Key[CachedValue[(MMap, MMap)]] = Key.create("methods key")
-  @deprecated val typesKey: Key[CachedValue[(TMap, TMap)]] = Key.create("types key")
-  @deprecated val signaturesKey: Key[CachedValue[(SMap, SMap)]] = Key.create("signatures key")
-
-  //todo: soft references?
-  @volatile private var vals: HashMap[PsiClass, (VMap, VMap)] = new HashMap
-  @volatile private var methods: HashMap[PsiClass, (MMap, MMap)] = new HashMap
-  @volatile private var types: HashMap[PsiClass, (TMap, TMap)] = new HashMap
-  @volatile private var signatures: HashMap[PsiClass, (SMap, SMap)] = new HashMap
-
-  @volatile private var modCount: Long = 0
-
-  private def clearCache() {
-    vals.clear
-    methods.clear
-    types.clear
-    signatures.clear
-  }
-  
-  private def getFromCache[U <: PsiElement, T](elem: U, map: HashMap[U, T], calc: () => T): T = {
-    var value: Option[T] = map.get(elem)
-    val curCount: Long = elem.getManager.getModificationTracker.getOutOfCodeBlockModificationCount
-    if (value != None && modCount == curCount) {
-      return value.get
-    }
-    clearCache
-    value = Some(calc())
-    map += Tuple(elem, value.get)
-    modCount = curCount
-    return value.get
-  }
-
-  /*def getVals(clazz: PsiClass): VMap = getFromCache(clazz, vals, () => ValueNodes.build(clazz))._2
-  def getMethods(clazz: PsiClass): MMap = getFromCache(clazz, methods, () => MethodNodes.build(clazz))._2
-  def getTypes(clazz: PsiClass) = getFromCache(clazz, types, () => TypeNodes.build(clazz))._2
-  def getSignatures(c: PsiClass): SMap = getFromCache(c, signatures, () => SignatureNodes.build(c))._2
-  def getSuperVals(c: PsiClass) = getFromCache(c, vals, () => ValueNodes.build(c))._1
-  def getSuperMethods(c: PsiClass) = getFromCache(c, methods, () => MethodNodes.build(c))._1
-  def getSuperTypes(c: PsiClass) = getFromCache(c, types, () => TypeNodes.build(c))._1*/
+  val valsKey: Key[CachedValue[(VMap, VMap)]] = Key.create("vals key")
+  val methodsKey: Key[CachedValue[(MMap, MMap)]] = Key.create("methods key")
+  val typesKey: Key[CachedValue[(TMap, TMap)]] = Key.create("types key")
+  val signaturesKey: Key[CachedValue[(SMap, SMap)]] = Key.create("signatures key")
 
   def getVals(clazz: PsiClass): VMap = get(clazz, valsKey, new MyProvider(clazz, {clazz: PsiClass => ValueNodes.build(clazz)}))._2
 
@@ -301,7 +265,6 @@ object TypeDefinitionMembers {
 
   def getSuperTypes(c: PsiClass) = get(c, typesKey, new MyProvider(c, {c: PsiClass => TypeNodes.build(c)}))._1
 
-  @deprecated
   private def get[Dom <: PsiElement, T](e: Dom, key: Key[CachedValue[T]], provider: => CachedValueProvider[T]): T = {
     var computed: CachedValue[T] = e.getUserData(key)
     if (computed == null) {
@@ -312,7 +275,6 @@ object TypeDefinitionMembers {
     computed.getValue
   }
 
-  @deprecated
   class MyProvider[Dom, T](e: Dom, builder: Dom => T) extends CachedValueProvider[T] {
     def compute() = new CachedValueProvider.Result(builder(e),
       PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT)
