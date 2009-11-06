fc6c3d8a6c44debc7954da637544acee86a9f25a
jzaugg <jzaugg@gmail.com>
2009-11-06 16:30:12 +0100

    Given trait T { def me: this.type}, (new X with T).me should be of type (X with T), rather than just T. The implementation needs review, especially in ScSimpleTypeElementImpl.

--- src/org/jetbrains/plugins/scala/lang/resolve/BaseProcessor.scala
+++ src/org/jetbrains/plugins/scala/lang/resolve/BaseProcessor.scala
@@ -132,7 +132,14 @@ abstract class BaseProcessor(val kinds: Set[ResolveTargets.Value]) extends PsiSc
         }
         true
       }
-      case singl: ScSingletonType => processType(singl.pathType, place)
+      case singl: ScSingletonType => {
+        // See test ThisTypeCompound.
+        val qual = place match {
+          case ref: ScReferenceExpression => ref.qualifier
+          case _ => None
+        }
+        processType(singl.pathTypeInContext(qual), place)
+      }
       case ex: ScExistentialType => processType(ex.skolem, place)
       case _ => true
     }
