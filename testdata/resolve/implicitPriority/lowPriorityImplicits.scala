002c22f505bb1134b850c74a7f755a0387f9dd48
jzaugg <jzaugg@gmail.com>
2009-11-08 13:55:50 +0100

    SCL-1606 Failing test cases for choosing the appropriate implicit conversion based on most specific method and sub/super-class relationship between the conversion functions.

---
+++ testdata/resolve/implicitPriority/lowPriorityImplicits.scala
@@ -0,0 +1,19 @@
+object Low {
+  def bullseye = "missed!"
+}
+
+object High {
+  def bullseye = "bulls eye!"
+}
+
+trait LowPriority {
+  implicit def intToLow(a: Int): Low.type = Low
+}
+
+object HighPriority extends LowPriority {
+  implicit def intToHigh(a: Int): High.type = High
+}
+
+import HighPriority._
+val a: Int = 1
+a.<ref>bullseye
