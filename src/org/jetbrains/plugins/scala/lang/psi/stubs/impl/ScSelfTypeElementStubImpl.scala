20b53e9d7030019b865b0116d4d68292c4d83145
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-11-08 13:02:12 +0300

    Stubs fix. ScSelfTypeElementImpl.name()

--- src/org/jetbrains/plugins/scala/lang/psi/stubs/impl/ScSelfTypeElementStubImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/impl/ScSelfTypeElementStubImpl.scala
@@ -8,13 +8,25 @@ package impl
 import api.base.types.ScSelfTypeElement
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.{IStubElementType, StubElement}
+import com.intellij.util.io.StringRef
+import java.lang.String
+
 /**
  * User: Alexander Podkhalyuzin
  * Date: 19.06.2009
  */
 
 class ScSelfTypeElementStubImpl[ParentPsi <: PsiElement](parent: StubElement[ParentPsi],
-                                                  elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement])
-  extends StubBaseWrapper[ScSelfTypeElement](parent, elemType) with ScSelfTypeElementStub  {
+                                                         elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement])
+        extends StubBaseWrapper[ScSelfTypeElement](parent, elemType) with ScSelfTypeElementStub {
+  private var name: StringRef = _
 
+  def this(parent: StubElement[ParentPsi],
+           elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement],
+           name: String) = {
+    this (parent, elemType.asInstanceOf[IStubElementType[StubElement[PsiElement], PsiElement]])
+    this.name = StringRef.fromString(name)
+  }
+
+  def getName: String = StringRef.toString(name)
 }
