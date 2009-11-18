4fe94c30259d199061275340b29562a7fd661a4d
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-11-18 20:15:57 +0300

    Naming Pattern has right type Construtor Pattern infer right type for parts (according to expected type even it has type parameters)

--- src/org/jetbrains/plugins/scala/lang/psi/api/base/patterns/ScPattern.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/base/patterns/ScPattern.scala
@@ -12,6 +12,8 @@ import result.{Failure, TypeResult, TypingContext}
 import statements.{ScValue, ScVariable}
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import com.intellij.psi._
+import lang.resolve.ScalaResolveResult
+import toplevel.typedef.ScClass
 
 /**
  * @author Alexander Podkhalyuzin
@@ -49,13 +51,64 @@ trait ScPattern extends ScalaPsiElement {
     }
     case argList : ScPatternArgumentList => {
       argList.getParent match {
-        case constr : ScConstructorPattern => constr.bindParamTypes match {
-          case Some(ts) =>
-            for ((p, t) <- constr.args.patterns.elements zip ts.elements) {
-              if (p == this) return Some(t)
+        case constr : ScConstructorPattern => {
+          constr.ref.bind match {
+            case Some(ScalaResolveResult(clazz: ScClass, substitutor: ScSubstitutor)) if clazz.isCase => {
+              val i = argList.patterns.findIndexOf(_ == this)
+              clazz.constructor match {
+                case Some(constructor: ScPrimaryConstructor) => {
+                  val clauses = constructor.parameterList.clauses
+                  if (clauses.length == 0) None
+                  else {
+                    val subst = if (clazz.typeParameters.length == 0) substitutor else {
+                      constr.expectedType match {
+                        case Some(ScParameterizedType(des, args)) if (ScType.extractClassType(des) match {
+                          case Some((`clazz`, _)) => true
+                          case _ => false
+                        }) => {
+                          val s = args.zip(clazz.typeParameters).foldLeft(ScSubstitutor.empty) {
+                            (subst, pair) =>
+                              val scType = pair._1
+                              val typeParameter = pair._2
+                              subst.bindT(typeParameter.getName, scType)
+                          }
+                          substitutor.followed(s)
+                        }
+                        case _ => substitutor
+                      }
+                    }
+                    val params = clauses(0).parameters
+                    if (params.length == 0) return None
+                    def isSeqWildcard: Boolean = {
+                      this match {
+                        case naming: ScNamingPattern => {
+                          naming.getLastChild.isInstanceOf[ScSeqWildcard]
+                        }
+                        case _ => false
+                      }
+                    }
+                    if (i != argList.patterns.length - 1 || !isSeqWildcard) {
+                      if (i < params.length) Some(subst.subst(params(i).getType(TypingContext.empty).getOrElse(return None)))
+                      else if (params(params.length - 1).isRepeatedParameter) {
+                        Some(subst.subst(params(params.length - 1).getType(TypingContext.empty).getOrElse(return None)))
+                      } else None
+                    } else {
+                      if (params.length > i + 1) None
+                      else if (!params(params.length - 1).isRepeatedParameter) None
+                      else {
+                        val seqClass = JavaPsiFacade.getInstance(getProject).findClass("scala.collection.Seq", getResolveScope)
+                        if (seqClass == null) return None
+                        val tp = subst.subst(params(params.length - 1).getType(TypingContext.empty).getOrElse(return None))
+                        Some(ScParameterizedType(ScDesignatorType(seqClass), Seq(tp)))
+                      }
+                    }
+                  }
+                }
+                case _ => None
+              }
             }
-            None
-          case _ => None
+            case _ => None //todo: extractor patterns
+          }
         }
       }
     }
@@ -97,6 +150,7 @@ trait ScPattern extends ScalaPsiElement {
         case _ => None
       }
     }
+    case named: ScNamingPattern => named.expectedType
     case _ => None //todo
   }
 }
