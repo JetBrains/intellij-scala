6f2defd58a7ca9603b809da4ee6e177306f5b938
jzaugg <jzaugg@gmail.com>
2009-11-20 10:06:56 +1000

    Surround with type.

--- src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/expression/ScalaTypeSurrounder.scala
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/expression/ScalaTypeSurrounder.scala
@@ -4,35 +4,24 @@ package surroundWith
 package surrounders
 package expression
 
-import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
-import org.jetbrains.plugins.scala.lang.psi.types.ScType
-import org.jetbrains.plugins.scala.lang.surroundWith.surrounders.expression.ScalaExpressionSurrounder
-
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import psi.types.result.TypingContext
 import psi.types._
 import psi.api.expr._
+import psi.api.base.types.ScTypeElement
 
 class ScalaTypeSurrounder extends ScalaExpressionSurrounder {
   override def getTemplateAsString(elements: Array[PsiElement]): String = {
     val expression = elements(0).asInstanceOf[ScExpression]
-    def higherPrecedenceThan_:(infixOp: String) = {
-      infixOp.forall(Character.isLetter(_)) || (infixOp.length == 1 && "|^&<>=!".contains(infixOp.head))
-    }
-    val requiresParens = expression match {
-      case inf: ScInfixExpr if !higherPrecedenceThan_:(inf.operation.getText) => true
-      case _ => false
-    }
     val typeResult = expression.getType(TypingContext.empty)
     val typeText = typeResult.map(ScType.presentableText(_)).getOrElse("Any")
-    if (requiresParens)
-      "((" + super.getTemplateAsString(elements) + "): " + typeText + ")"
-    else
-      "(" + super.getTemplateAsString(elements) + ": " + typeText + ")"
+    "(" + super.getTemplateAsString(elements) + ": " + typeText + ")"
   }
+
   override def getTemplateDescription: String = "(expr: Type)"
+
   override def isApplicable(elements: Array[PsiElement]): Boolean = {
     if (elements.length != 1) return false
     elements(0) match {
@@ -40,17 +29,28 @@ class ScalaTypeSurrounder extends ScalaExpressionSurrounder {
       case _ => return false
     }
   }
+
   override def getSurroundSelectionRange(withType: ASTNode): TextRange = {
-    val element: PsiElement = withType.getPsi match {
-      case x: ScParenthesisedExpr => x.expr match {
-        case Some(y) => y
-        case _ => return x.getTextRange
-      }
-      case x => x
+    lazy val defaultRange = {
+      val expr: ScExpression = withType.getPsi.asInstanceOf[ScExpression]
+      val offset = expr.getTextRange.getEndOffset
+      new TextRange(offset, offset)
     }
 
-    val expr: ScExpression = element.asInstanceOf[ScExpression]
-    val offset = expr.getTextRange.getEndOffset
-    new TextRange(offset,offset)
+    withType.getPsi match {
+      case x: ScParenthesisedExpr => x.expr match {
+        case Some(y: ScTypedStmt) => y.typeElement match {
+          case Some(te: ScTypeElement) => {
+            if (te.getText() == "Any")
+              te.getTextRange
+            else
+              defaultRange
+          }
+          case _ => defaultRange
+        }
+        case _ => defaultRange
+      }
+      case _ => defaultRange
+    }
   }
 }
