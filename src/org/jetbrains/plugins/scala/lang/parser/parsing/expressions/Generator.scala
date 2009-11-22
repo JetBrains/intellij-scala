a6c29443b86e22ebc7ff4c11866a3466017c9fd4
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-11-22 15:07:45 +0300

    Fixed parser for Generator

--- src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/Generator.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/Generator.scala
@@ -33,8 +33,8 @@ object Generator {
         builder error ErrMsg("choose.expected")
       }
     }
-    genMarker.done(ScalaElementTypes.GENERATOR)
     if (!Expr.parse(builder)) builder error ErrMsg("wrong.expression")
+    genMarker.done(ScalaElementTypes.GENERATOR)
     builder.getTokenType match {
       case ScalaTokenTypes.kIF => Guard parse builder
       case _ => {}
