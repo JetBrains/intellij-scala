9edae6fdd0ca01d42688a59ef63b4fc088fd4a93
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2009-12-02 19:54:42 +0300

    Read action on Usages search

--- src/org/jetbrains/plugins/scala/findUsages/vals/JavaValsUsagesSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/vals/JavaValsUsagesSearcher.scala
@@ -11,6 +11,9 @@ import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
 import org.jetbrains.plugins.scala.lang.psi.fake.FakePsiMethod
 import com.intellij.psi.search.{PsiSearchHelper, UsageSearchContext, TextOccurenceProcessor}
 import com.intellij.psi.{PsiManager, PsiReferenceExpression, PsiElement, PsiReference}
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.Computable
+
 /**
  * User: Alexander Podkhalyuzin
  * Date: 08.09.2009
@@ -21,35 +24,39 @@ class JavaValsUsagesSearcher extends QueryExecutor[PsiReference, ReferencesSearc
     val scope = queryParameters.getScope
     val element = queryParameters.getElementToSearch
     val ignoreAccess = queryParameters.isIgnoreAccessScope
-    element match {
-      case vals: ScTypedDefinition if ScalaPsiUtil.nameContext(vals).isInstanceOf[ScValue] ||
-              ScalaPsiUtil.nameContext(vals).isInstanceOf[ScVariable] => {
-        val name: String = vals.getName
-        val collectedReferences: HashSet[PsiReference] = new HashSet[PsiReference]
-        val processor = new TextOccurenceProcessor {
-          def execute(element: PsiElement, offsetInElement: Int): Boolean = {
-            val references = element.getReferences
-            for (ref <- references if ref.getRangeInElement.contains(offsetInElement) && !collectedReferences.contains(ref)) {
-              ref match {
-                case refElement: PsiReferenceExpression => {
-                  refElement.resolve match {
-                    case f: FakePsiMethod if f.navElement == vals => {
-                      if (!consumer.process(refElement)) return false
+    ApplicationManager.getApplication.runReadAction(new Computable[Boolean] {
+      def compute: Boolean = {
+        element match {
+          case vals: ScTypedDefinition if ScalaPsiUtil.nameContext(vals).isInstanceOf[ScValue] ||
+                  ScalaPsiUtil.nameContext(vals).isInstanceOf[ScVariable] => {
+            val name: String = vals.getName
+            val collectedReferences: HashSet[PsiReference] = new HashSet[PsiReference]
+            val processor = new TextOccurenceProcessor {
+              def execute(element: PsiElement, offsetInElement: Int): Boolean = {
+                val references = element.getReferences
+                for (ref <- references if ref.getRangeInElement.contains(offsetInElement) && !collectedReferences.contains(ref)) {
+                  ref match {
+                    case refElement: PsiReferenceExpression => {
+                      refElement.resolve match {
+                        case f: FakePsiMethod if f.navElement == vals => {
+                          if (!consumer.process(refElement)) return false
+                        }
+                        case _ =>
+                      }
                     }
                     case _ =>
                   }
                 }
-                case _ =>
+                return true
               }
             }
-            return true
+            val helper: PsiSearchHelper = PsiManager.getInstance(vals.getProject).getSearchHelper
+            helper.processElementsWithWord(processor, scope, name, UsageSearchContext.IN_CODE, true)
           }
+          case _ =>
         }
-        val helper: PsiSearchHelper = PsiManager.getInstance(vals.getProject).getSearchHelper
-        helper.processElementsWithWord(processor, scope, name, UsageSearchContext.IN_CODE, true)
+        true
       }
-      case _ =>
-    }
-    true
+    })
   }
 }
