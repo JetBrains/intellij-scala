80c253509901e89dba3be29592e9d209a8441980
Ilya Sergey <Ilya.Sergey@jetbrains.com>
2009-12-22 19:18:45 +0300

    minor changes

--- src/org/jetbrains/plugins/scala/lang/psi/api/ScControlFlowOwner.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/ScControlFlowOwner.scala
@@ -2,7 +2,9 @@ package org.jetbrains.plugins.scala.lang.psi.api
 
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.controlFlow.Instruction
+
 /**
+ * Represents elements with control flow cached
  * @author ilyas
  */
 
