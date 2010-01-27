644cb51b06dce9b8e0bd24cc431b1a1bc22a2a4d
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-01-27 15:20:45 +0300

    DefaultHighlighter simplified

--- src/org/jetbrains/plugins/scala/highlighter/DefaultHighlighter.java
+++ src/org/jetbrains/plugins/scala/highlighter/DefaultHighlighter.java
@@ -53,7 +53,6 @@ public class DefaultHighlighter {
   static final String DOT_ID = "Dot";
   @NonNls
   static final String COMMA_ID = "Comma";
-
   @NonNls
   static final String TYPEPARAM_ID = "Type parameter";
   @NonNls
@@ -105,92 +104,48 @@ public class DefaultHighlighter {
   @NonNls
   static final String TYPE_ALIAS_ID = "Type Alias";
 
-  // Registering TextAttributes
-  static {
-    TextAttributesKey.createTextAttributesKey(LINE_COMMENT_ID, SyntaxHighlighterColors.LINE_COMMENT.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(BLOCK_COMMENT_ID, SyntaxHighlighterColors.JAVA_BLOCK_COMMENT.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(KEYWORD_ID, SyntaxHighlighterColors.KEYWORD.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(NUMBER_ID, SyntaxHighlighterColors.NUMBER.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(STRING_ID, SyntaxHighlighterColors.STRING.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(BRACKETS_ID, SyntaxHighlighterColors.BRACKETS.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(BRACKETS_ID, SyntaxHighlighterColors.BRACKETS.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(BRACES_ID, SyntaxHighlighterColors.BRACES.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(PARENTHESES_ID, SyntaxHighlighterColors.PARENTHS.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(COLON_ID, SyntaxHighlighterColors.COMMA.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(SEMICOLON_ID, SyntaxHighlighterColors.COMMA.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(DOT_ID, SyntaxHighlighterColors.DOT.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(COMMA_ID, SyntaxHighlighterColors.COMMA.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(PREDEF_ID, SyntaxHighlighterColors.COMMA.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(TYPEPARAM_ID, HighlightInfoType.TYPE_PARAMETER_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(OBJECT_ID, HighlightInfoType.CLASS_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(CLASS_ID, HighlightInfoType.CLASS_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(SCALA_DOC_TAG_ID, SyntaxHighlighterColors.DOC_COMMENT_TAG.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(SCALA_DOC_MARKUP_ID, SyntaxHighlighterColors.DOC_COMMENT_MARKUP.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(ABSTRACT_CLASS_ID, HighlightInfoType.ABSTRACT_CLASS_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(TRAIT_ID, HighlightInfoType.INTERFACE_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(LOCAL_VALUES_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(LOCAL_VARIABLES_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(LOCAL_LAZY_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(VALUES_ID, HighlightInfoType.STATIC_FIELD.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(VARIABLES_ID, HighlightInfoType.STATIC_FIELD.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(LAZY_ID, HighlightInfoType.STATIC_FIELD.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(PATTERN_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(PARAMETER_ID, HighlightInfoType.PARAMETER.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(METHOD_CALL_ID, HighlightInfoType.METHOD_CALL.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(OBJECT_METHOD_CALL_ID, HighlightInfoType.STATIC_METHOD.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(LOCAL_METHOD_CALL_ID, HighlightInfoType.METHOD_CALL.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(METHOD_DECLARATION_ID, HighlightInfoType.METHOD_DECLARATION.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(ANNOTATION_ID, HighlightInfoType.ANNOTATION_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(ANNOTATION_ATTRIBUTE_ID, HighlightInfoType.ANNOTATION_ATTRIBUTE_NAME.getAttributesKey().getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(TYPE_ALIAS_ID, HighlightInfoType.TYPE_PARAMETER_NAME.getAttributesKey().getDefaultAttributes());
+  public static TextAttributesKey LINE_COMMENT = createKey(LINE_COMMENT_ID, SyntaxHighlighterColors.LINE_COMMENT);
+  public static TextAttributesKey BLOCK_COMMENT = createKey(BLOCK_COMMENT_ID, SyntaxHighlighterColors.JAVA_BLOCK_COMMENT);
+  public static TextAttributesKey KEYWORD = createKey(KEYWORD_ID, SyntaxHighlighterColors.KEYWORD);
+  public static TextAttributesKey NUMBER = createKey(NUMBER_ID, SyntaxHighlighterColors.NUMBER);
+  public static TextAttributesKey STRING = createKey(STRING_ID, SyntaxHighlighterColors.STRING);
+  public static TextAttributesKey BRACKETS = createKey(BRACKETS_ID, SyntaxHighlighterColors.BRACKETS);
+  public static TextAttributesKey BRACES = createKey(BRACES_ID, SyntaxHighlighterColors.BRACES);
+  public static TextAttributesKey PARENTHESES = createKey(PARENTHESES_ID, SyntaxHighlighterColors.PARENTHS);
+  public static TextAttributesKey COLON = createKey(COLON_ID, SyntaxHighlighterColors.COMMA);
+  public static TextAttributesKey SEMICOLON = createKey(SEMICOLON_ID, SyntaxHighlighterColors.COMMA);
+  public static TextAttributesKey DOT = createKey(DOT_ID, SyntaxHighlighterColors.DOT);
+  public static TextAttributesKey COMMA = createKey(COMMA_ID, SyntaxHighlighterColors.COMMA);
+  public static TextAttributesKey PREDEF = createKey(PREDEF_ID, SyntaxHighlighterColors.COMMA);
+  public static TextAttributesKey TYPEPARAM = createKey(TYPEPARAM_ID, HighlightInfoType.TYPE_PARAMETER_NAME.getAttributesKey());
+  public static TextAttributesKey OBJECT = createKey(OBJECT_ID, HighlightInfoType.CLASS_NAME.getAttributesKey());
+  public static TextAttributesKey CLASS = createKey(CLASS_ID, HighlightInfoType.CLASS_NAME.getAttributesKey());
+  public static TextAttributesKey SCALA_DOC_TAG = createKey(SCALA_DOC_TAG_ID, SyntaxHighlighterColors.DOC_COMMENT_TAG);
+  public static TextAttributesKey SCALA_DOC_MARKUP = createKey(SCALA_DOC_MARKUP_ID, SyntaxHighlighterColors.DOC_COMMENT_MARKUP);
+  public static TextAttributesKey ABSTRACT_CLASS = createKey(ABSTRACT_CLASS_ID, HighlightInfoType.ABSTRACT_CLASS_NAME.getAttributesKey());
+  public static TextAttributesKey TRAIT = createKey(TRAIT_ID, HighlightInfoType.INTERFACE_NAME.getAttributesKey());
+  public static TextAttributesKey LOCAL_VALUES = createKey(LOCAL_VALUES_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey());
+  public static TextAttributesKey LOCAL_VARIABLES = createKey(LOCAL_VARIABLES_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey());
+  public static TextAttributesKey LOCAL_LAZY = createKey(LOCAL_LAZY_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey());
+  public static TextAttributesKey VALUES = createKey(VALUES_ID, HighlightInfoType.STATIC_FIELD.getAttributesKey());
+  public static TextAttributesKey VARIABLES = createKey(VARIABLES_ID, HighlightInfoType.STATIC_FIELD.getAttributesKey());
+  public static TextAttributesKey LAZY = createKey(LAZY_ID, HighlightInfoType.STATIC_FIELD.getAttributesKey());
+  public static TextAttributesKey PATTERN = createKey(PATTERN_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey());
+  public static TextAttributesKey PARAMETER = createKey(PARAMETER_ID, HighlightInfoType.PARAMETER.getAttributesKey());
+  public static TextAttributesKey METHOD_CALL = createKey(METHOD_CALL_ID, HighlightInfoType.METHOD_CALL.getAttributesKey());
+  public static TextAttributesKey OBJECT_METHOD_CALL = createKey(OBJECT_METHOD_CALL_ID, HighlightInfoType.STATIC_METHOD.getAttributesKey());
+  public static TextAttributesKey LOCAL_METHOD_CALL = createKey(LOCAL_METHOD_CALL_ID, HighlightInfoType.METHOD_CALL.getAttributesKey());
+  public static TextAttributesKey METHOD_DECLARATION = createKey(METHOD_DECLARATION_ID, HighlightInfoType.METHOD_DECLARATION.getAttributesKey());
+  public static TextAttributesKey ANNOTATION = createKey(ANNOTATION_ID, HighlightInfoType.ANNOTATION_NAME.getAttributesKey());
+  public static TextAttributesKey ANNOTATION_ATTRIBUTE = createKey(ANNOTATION_ATTRIBUTE_ID, HighlightInfoType.ANNOTATION_ATTRIBUTE_NAME.getAttributesKey());
+  public static TextAttributesKey TYPE_ALIAS = createKey(TYPE_ALIAS_ID, HighlightInfoType.TYPE_PARAMETER_NAME.getAttributesKey());
+  public static TextAttributesKey ASSIGN = createKey(ASSIGN_ID, SyntaxHighlighterColors.OPERATION_SIGN);
+  public static TextAttributesKey BAD_CHARACTER = createKey(BAD_CHARACTER_ID, HighlighterColors.BAD_CHARACTER);
+  public static TextAttributesKey DOC_COMMENT = createKey(DOC_COMMENT_ID, SyntaxHighlighterColors.DOC_COMMENT);
 
-
-    TextAttributesKey.createTextAttributesKey(ASSIGN_ID, SyntaxHighlighterColors.OPERATION_SIGN.getDefaultAttributes());
-    TextAttributesKey.createTextAttributesKey(BAD_CHARACTER_ID, HighlighterColors.BAD_CHARACTER.getDefaultAttributes());
+  private static TextAttributesKey createKey(String externalName, TextAttributesKey prototype) {
+    return TextAttributesKey.createTextAttributesKey(externalName, prototype.getDefaultAttributes());
   }
-
-  public static TextAttributesKey LINE_COMMENT = TextAttributesKey.createTextAttributesKey(LINE_COMMENT_ID);
-  public static TextAttributesKey BLOCK_COMMENT = TextAttributesKey.createTextAttributesKey(BLOCK_COMMENT_ID);
-  public static TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey(KEYWORD_ID);
-  public static TextAttributesKey NUMBER = TextAttributesKey.createTextAttributesKey(NUMBER_ID);
-  public static TextAttributesKey STRING = TextAttributesKey.createTextAttributesKey(STRING_ID);
-  public static TextAttributesKey BRACKETS = TextAttributesKey.createTextAttributesKey(BRACKETS_ID);
-  public static TextAttributesKey BRACES = TextAttributesKey.createTextAttributesKey(BRACES_ID);
-  public static TextAttributesKey PARENTHESES = TextAttributesKey.createTextAttributesKey(PARENTHESES_ID);
-  public static TextAttributesKey COLON = TextAttributesKey.createTextAttributesKey(COLON_ID);
-  public static TextAttributesKey SEMICOLON = TextAttributesKey.createTextAttributesKey(SEMICOLON_ID);
-  public static TextAttributesKey COMMA = TextAttributesKey.createTextAttributesKey(COMMA_ID);
-  public static TextAttributesKey DOT = TextAttributesKey.createTextAttributesKey(DOT_ID);
-  public static TextAttributesKey TYPEPARAM = TextAttributesKey.createTextAttributesKey(TYPEPARAM_ID);
-  public static TextAttributesKey PREDEF = TextAttributesKey.createTextAttributesKey(PREDEF_ID);
-  public static TextAttributesKey CLASS = TextAttributesKey.createTextAttributesKey(CLASS_ID);
-  public static TextAttributesKey OBJECT = TextAttributesKey.createTextAttributesKey(OBJECT_ID);
-  public static TextAttributesKey SCALA_DOC_TAG = TextAttributesKey.createTextAttributesKey(SCALA_DOC_TAG_ID);
-  public static TextAttributesKey SCALA_DOC_MARKUP = TextAttributesKey.createTextAttributesKey(SCALA_DOC_MARKUP_ID);
-  public static TextAttributesKey ABSTRACT_CLASS = TextAttributesKey.createTextAttributesKey(ABSTRACT_CLASS_ID);
-  public static TextAttributesKey TRAIT = TextAttributesKey.createTextAttributesKey(TRAIT_ID);
-  public static TextAttributesKey LOCAL_VALUES = TextAttributesKey.createTextAttributesKey(LOCAL_VALUES_ID);
-  public static TextAttributesKey LOCAL_VARIABLES = TextAttributesKey.createTextAttributesKey(LOCAL_VARIABLES_ID);
-  public static TextAttributesKey LOCAL_LAZY = TextAttributesKey.createTextAttributesKey(LOCAL_LAZY_ID);
-  public static TextAttributesKey VALUES = TextAttributesKey.createTextAttributesKey(VALUES_ID);
-  public static TextAttributesKey VARIABLES = TextAttributesKey.createTextAttributesKey(VARIABLES_ID);
-  public static TextAttributesKey LAZY = TextAttributesKey.createTextAttributesKey(LAZY_ID);
-  public static TextAttributesKey PATTERN = TextAttributesKey.createTextAttributesKey(PATTERN_ID);
-  public static TextAttributesKey PARAMETER = TextAttributesKey.createTextAttributesKey(PARAMETER_ID);
-  public static TextAttributesKey METHOD_CALL = TextAttributesKey.createTextAttributesKey(METHOD_CALL_ID);
-  public static TextAttributesKey OBJECT_METHOD_CALL = TextAttributesKey.createTextAttributesKey(OBJECT_METHOD_CALL_ID);
-  public static TextAttributesKey LOCAL_METHOD_CALL = TextAttributesKey.createTextAttributesKey(LOCAL_METHOD_CALL_ID);
-  public static TextAttributesKey METHOD_DECLARATION = TextAttributesKey.createTextAttributesKey(METHOD_DECLARATION_ID);
-  public static TextAttributesKey ANNOTATION = TextAttributesKey.createTextAttributesKey(ANNOTATION_ID);
-  public static TextAttributesKey ANNOTATION_ATTRIBUTE = TextAttributesKey.createTextAttributesKey(ANNOTATION_ATTRIBUTE_ID);                                                                                                            
-  public static TextAttributesKey TYPE_ALIAS = TextAttributesKey.createTextAttributesKey(TYPE_ALIAS_ID);
-
-  public static TextAttributesKey ASSIGN = TextAttributesKey.createTextAttributesKey(ASSIGN_ID);
-  public static TextAttributesKey BAD_CHARACTER = TextAttributesKey.createTextAttributesKey(BAD_CHARACTER_ID);
-
-  public static TextAttributesKey DOC_COMMENT = TextAttributesKey.createTextAttributesKey(DOC_COMMENT_ID,
-          SyntaxHighlighterColors.DOC_COMMENT.getDefaultAttributes());
-
 }
 
 
