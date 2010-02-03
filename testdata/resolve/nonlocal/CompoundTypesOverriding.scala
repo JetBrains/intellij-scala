dcbe0433fae880e9decdd43ca08ae1abcafdc5fa
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-02-03 11:56:08 +0300

    Fixed testdata (test failed due to names clash)

--- testdata/resolve/nonlocal/CompoundTypesOverriding.scala
+++ testdata/resolve/nonlocal/CompoundTypesOverriding.scala
@@ -1,10 +1,12 @@
-trait A {
-  def foo: Int = 34
-}
+object Z {
+  trait A {
+    def foo: Int = 34
+  }
 
-class C {
-  def foo: Int = 77
-}
-object Main {
-  val g = new A with C {override def foo: Int = super.<ref>foo}
+  class C {
+    def foo: Int = 77
+  }
+  object Main {
+    val g = new A with C {override def foo: Int = super.<ref>foo}
+  }
 }
