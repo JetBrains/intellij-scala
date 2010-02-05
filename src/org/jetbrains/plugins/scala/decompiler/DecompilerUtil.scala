5f7b69dfcfe8272e76e0a9cfa2088decb2057de1
Ilya Sergey <Ilya.Sergey@jetbrains.com>
2010-02-05 17:33:51 +0100

    compilation fixed

--- src/org/jetbrains/plugins/scala/decompiler/DecompilerUtil.scala
+++ src/org/jetbrains/plugins/scala/decompiler/DecompilerUtil.scala
@@ -14,7 +14,6 @@ import tools.scalap.scalax.rules.scalasig._
 import java.lang.String
 import scala.reflect.NameTransformer
 import collection.Seq
-import tools.nsc.io.VirtualFile
 
 /**
  * @author ilyas
