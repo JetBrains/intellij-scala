25336a0d4f5d076ce61c9c10453c894be84a3189
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-02-05 19:16:56 +0300

    resolve tests

---
+++ testdata/resolve2/import/clash/Function2.scala
@@ -0,0 +1,4 @@
+import actors.Actor.{continue => f}
+def f = {}
+
+println(/* file: Function2 */ f)
---
+++ testdata/resolve2/import/clash/Value3.scala
@@ -0,0 +1,6 @@
+import actors.Actor
+
+println(/* file: Value3, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject */ Actor.getClass)
+println(classOf[/* path: scala.actors.Actor, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTrait */ Actor])
+
+object Actor
