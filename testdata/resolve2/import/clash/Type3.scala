f67816ef5cca5b67184981d5c473ac13ed6c8ff2
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-02-05 19:16:56 +0300

    resolve tests

---
+++ testdata/resolve2/import/clash/Type3.scala
@@ -0,0 +1,6 @@
+import actors.Actor
+
+println(/* path: scala.actors.Actor, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject */ Actor.getClass)
+println(classOf[/* file: Type3, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass */ Actor])
+
+class Actor
---
+++ testdata/resolve2/import/clash/TypeAndValue2.scala
@@ -0,0 +1,6 @@
+import actors.Actor
+case class Actor
+
+println(/* file: TypeAndValue2, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass */ Actor.getClass)
+println(classOf[/* file: TypeAndValue2, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass */ Actor])
+
