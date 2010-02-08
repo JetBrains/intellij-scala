84d180ba8e941fba449205d2d7fc2eabac8760cb
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-02-08 14:20:11 +0300

    Added offset

--- testdata/resolve2/element/mix/FunctionAndClass.scala
+++ testdata/resolve2/element/mix/FunctionAndClass.scala
@@ -1,5 +1,5 @@
 def T = {}
 class T
 
-println(/*file: FunctionAndClass, type: org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition */ T.getClass)
+println(/*file: FunctionAndClass, offset: 4, type: org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition */ T.getClass)
 println(classOf[/* file: FunctionAndClass, offset: 17, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass */ T])
