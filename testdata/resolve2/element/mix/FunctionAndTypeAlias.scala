1071526fcb058e70d81d98842dc878653cb5b28c
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-02-08 15:12:40 +0300

    fixed testdata

--- testdata/resolve2/element/mix/FunctionAndTypeAlias.scala
+++ testdata/resolve2/element/mix/FunctionAndTypeAlias.scala
@@ -1,5 +1,5 @@
 def T = {}
 type T = String
 
-println(/* file: FunctionAndTypeAlias, offset: 5, type: org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition */ T.getClass)
+println(/* file: FunctionAndTypeAlias, offset: 4, type: org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition */ T.getClass)
 println(classOf[/* file: FunctionAndTypeAlias, offset: 16, type: org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAlias */ T])
