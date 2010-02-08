4bc94796bdebf63b7cdb36023285f57bfa563e0c
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-02-08 14:10:34 +0300

    Fixed testdata

--- testdata/resolve2/element/mix/CaseClassAndObject.scala
+++ testdata/resolve2/element/mix/CaseClassAndObject.scala
@@ -1,5 +1,5 @@
 case class T
 object T
 
-println(/* file: CaseClassAndObject, offset: 11, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject */ T.getClass)
+println(/* file: CaseClassAndObject, offset: 20, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject */ T.getClass)
 println(classOf[/* file: CaseClassAndObject, offset: 11, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass */ T])
--- testdata/resolve2/element/mix/FunctionAndClass.scala
+++ testdata/resolve2/element/mix/FunctionAndClass.scala
@@ -1,5 +1,5 @@
 def T = {}
 class T
 
-println(/* resolved: false */ T.getClass)
+println(/*file: FunctionAndClass, type: org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition */ T.getClass)
 println(classOf[/* file: FunctionAndClass, offset: 17, type: org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass */ T])
