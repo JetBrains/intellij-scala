679399256f232a8d191f96e81f763cff0cbcf3cf
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-02-10 14:24:20 +0300

    avoid Predef names clash

--- testdata/typeConformance/infix/InfixLeft.scala
+++ testdata/typeConformance/infix/InfixLeft.scala
@@ -1,4 +1,4 @@
-case class ::[T1, T2](x1: T1, x2: T2)
+case class :::[T1, T2](x1: T1, x2: T2)
 class B
-val a: Int :: Double :: B = ::(1, ::(1d, new B))
+val a: Int ::: Double :: B = :::(1, :::(1d, new B))
 //True
