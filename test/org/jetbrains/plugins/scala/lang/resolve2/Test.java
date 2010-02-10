c9e64f797f81f23a6f88fbb5e5057c353b3cc882
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-02-10 13:17:54 +0300

    resolve tests: function.implicit

--- test/org/jetbrains/plugins/scala/lang/resolve2/Test.java
+++ test/org/jetbrains/plugins/scala/lang/resolve2/Test.java
@@ -4,20 +4,18 @@ package org.jetbrains.plugins.scala.lang.resolve2;
  * Pavel.Fatin, 08.02.2010
  */
 public class Test {
-  void a(String s) {
+  void f(String s) {
 
   }
 
-  void foo(String s, int i) {
+  class Inner {
+    void f(String s1, String s2) {
 
+    }
+
+    void bar() {
+//      f("");
+      f("", "");
+    }
   }
-
-  void foo(int i, String s) {
-
-  }
-
-  void bar() {
-//    a(1, 1);
-  }
-
 }
