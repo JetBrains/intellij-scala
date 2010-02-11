6899cca76e71670b209732a411e7b6c5ef6d2782
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-02-11 14:24:50 +0300

    resolve tests: function.type.generic

---
+++ test/org/jetbrains/plugins/scala/lang/resolve2/FunctionTypeGenericTest.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala.lang.resolve2
+
+
+/**
+ * Pavel.Fatin, 02.02.2010
+ */
+
+class FunctionTypeGenericTest extends ResolveTestBase {
+  override def getTestDataPath: String = {
+    super.getTestDataPath + "function/type/generic/"
+  }
+
+  def testFunction1 = doTest
+  def testFunction2 = doTest
+  //TODO
+//  def testFunctionExpression1 = doTest
+  def testFunctionExpression2 = doTest
+  //TODO
+//  def testGeneric1 = doTest
+  def testGeneric2 = doTest
+}
--- testdata/resolve2/inheritance/this/element/File.scala
+++ testdata/resolve2/inheritance/this/element/File.scala
@@ -1,3 +1,4 @@
 def f = {}
 
+/* file: Object */ getClass
 println(this./* offset: 4 */f)
