4c9fcefb0c6c6b17d91c1cd10b225e1fc2407ac8
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-02-15 16:56:12 +0300

    resolve tests: inheritance.super

---
+++ test/org/jetbrains/plugins/scala/lang/resolve2/InheritanceSuperSingleTest.scala
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.scala.lang.resolve2
+
+
+/**
+ * Pavel.Fatin, 02.02.2010
+ */
+
+class InheritanceSuperSingleTest extends ResolveTestBase {
+  override def getTestDataPath: String = {
+    super.getTestDataPath + "inheritance/super/single/"
+  }
+
+  def testClashClass = doTest
+  def testClashObject = doTest
+  def testClashTrait = doTest
+
+  def testClass = doTest
+  def testObject = doTest
+  def testTrait = doTest
+}
