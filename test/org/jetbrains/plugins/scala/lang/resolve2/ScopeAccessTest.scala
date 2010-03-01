3bacc3cb2eb9841106e723dce5f6d81774c52afe
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-03-01 16:06:39 +0300

    test stub

--- test/org/jetbrains/plugins/scala/lang/resolve2/ScopeAccessTest.scala
+++ test/org/jetbrains/plugins/scala/lang/resolve2/ScopeAccessTest.scala
@@ -1,5 +1,7 @@
 package org.jetbrains.plugins.scala.lang.resolve2
 
+import junit.framework.Assert
+
 
 /**
  * Pavel.Fatin, 02.02.2010
@@ -9,6 +11,8 @@ class ScopeAccessTest extends ResolveTestBase {
   override def getTestDataPath: String = {
     super.getTestDataPath + "scope/access/"
   }
+
+  def testStub = Assert.assertTrue(true)
 
   //TODO
 //  def testPrivateCompanionClass = doTest
