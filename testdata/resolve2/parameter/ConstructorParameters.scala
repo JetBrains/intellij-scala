9d1697ba8f560a91d7b10d9c2be761365ef81bb7
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-03-04 17:32:52 +0300

    resolve tests: parameter - removed

--- testdata/resolve2/parameter/ConstructorParameters.scala
+++
@@ -1,5 +0,0 @@
-class C(a: Int, b: Int, c: Int) {
-  /* offset: 8 */ a + 1
-  /* offset: 16 */ b + 1
-  /* offset: 24 */ c + 1
-}
--- testdata/resolve2/parameter/FunctionParameters.scala
+++
@@ -1,5 +0,0 @@
-def f(a: Int, b: Int, c: Int) = {
-  /* offset: 6 */ a + 1
-  /* offset: 14 */ b + 1
-  /* offset: 22 */ c + 1
-}
