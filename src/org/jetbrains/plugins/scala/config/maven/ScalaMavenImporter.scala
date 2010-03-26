fdbc849146c4daaca8a78e7ed1c16026046fe620
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-03-26 16:21:27 +0300

    Maven update to add Scala Facet automatically

--- src/org/jetbrains/plugins/scala/config/maven/ScalaMavenImporter.scala
+++ src/org/jetbrains/plugins/scala/config/maven/ScalaMavenImporter.scala
@@ -1,8 +1,11 @@
 package org.jetbrains.plugins.scala.config.maven
 
+import _root_.java.lang.String
+import _root_.java.util.{List, Map}
+import _root_.org.jetbrains.idea.maven.importing.{FacetImporter, MavenModifiableModelsProvider, MavenRootModelAdapter, MavenImporter}
+import _root_.org.jetbrains.plugins.scala.config.{ScalaFacet, ScalaFacetConfiguration, ScalaFacetType}
 import org.jetbrains.idea.maven.project._
 import com.intellij.openapi.module.Module
-import org.jetbrains.idea.maven.importing.{MavenModifiableModelsProvider, MavenRootModelAdapter, MavenImporter}
 import org.jdom.Element
 import org.jetbrains.idea.maven.utils.MavenJDOMUtil
 
@@ -11,14 +14,8 @@ import org.jetbrains.idea.maven.utils.MavenJDOMUtil
  * Date: 26.03.2010
  */
 
-class ScalaMavenImporter extends MavenImporter("org.scala-tools", "maven-scala-plugin") {
-  def isSupportedDependency(artifact: MavenArtifact): Boolean = false
-  def preProcess(module: Module, mavenProject: MavenProject, changes: MavenProjectChanges,
-                 modifiableModelsProvider: MavenModifiableModelsProvider): Unit = {}
-  def process(modifiableModelsProvider: MavenModifiableModelsProvider, module: Module, rootModel: MavenRootModelAdapter,
-              mavenModel: MavenProjectsTree, mavenProject: MavenProject, changes: MavenProjectChanges,
-              mavenProjectToModuleName: java.util.Map[MavenProject, String],
-              postTasks: java.util.List[MavenProjectsProcessorTask]): Unit = {}
+class ScalaMavenImporter extends FacetImporter[ScalaFacet, ScalaFacetConfiguration, ScalaFacetType]("org.scala-tools",
+  "maven-scala-plugin", ScalaFacetType.INSTANCE, "Scala Facet") {
 
   override def collectSourceFolders(mavenProject: MavenProject, result: java.util.List[String]): Unit =
     collectSourceOrTestFolders(mavenProject, "add-source", "sourceDir", "src/main/scala", result)
@@ -31,4 +28,12 @@ class ScalaMavenImporter extends MavenImporter("org.scala-tools", "maven-scala-p
     val goalConfigValue = findGoalConfigValue(mavenProject, goal, goalPath)
     result.add(if (goalConfigValue == null) defaultDir else goalConfigValue)
   }
+
+  def reimportFacet(modelsProvider: MavenModifiableModelsProvider, module: Module,
+                    rootModel: MavenRootModelAdapter, facet: ScalaFacet, mavenTree: MavenProjectsTree,
+                    mavenProject: MavenProject, changes: MavenProjectChanges,
+                    mavenProjectToModuleName: Map[MavenProject, String],
+                    postTasks: List[MavenProjectsProcessorTask]): Unit = {}
+
+  def setupFacet(f: ScalaFacet, mavenProject: MavenProject): Unit = {}
 }
