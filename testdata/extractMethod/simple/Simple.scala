6365a13f2782f97518b000fd482798e9b96ca730
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-04-06 18:54:43 +0400

    Extract Method Test

---
+++ testdata/extractMethod/simple/Simple.scala
@@ -0,0 +1,22 @@
+class A {
+  def foo = {
+    /*start*/
+    1 + 1
+    2 + 3
+    /*end*/
+  }
+}
+/*
+class A {
+  def testMethodName: Unit = {
+    1 + 1
+    2 + 3
+  }
+
+  def foo = {
+    /*start*/
+    testMethodName
+    /*end*/
+  }
+}
+*/
