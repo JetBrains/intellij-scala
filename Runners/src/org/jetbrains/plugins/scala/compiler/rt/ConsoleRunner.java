20e4f2661fa6fc4a23dc0be7aa98a104d61b426b
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-04-12 14:21:17 +0400

    Dynamic classpath for running scala console: SCL-997

--- Runners/src/org/jetbrains/plugins/scala/compiler/rt/ConsoleRunner.java
+++ Runners/src/org/jetbrains/plugins/scala/compiler/rt/ConsoleRunner.java
@@ -1,24 +1,36 @@
 package org.jetbrains.plugins.scala.compiler.rt;
 
-import scala.Function1;
 import scala.Some;
-import scala.collection.mutable.ListBuffer;
-import scala.tools.nsc.GenericRunnerCommand;
-import scala.tools.nsc.GenericRunnerSettings;
 import scala.tools.nsc.InterpreterLoop;
-import scala.tools.nsc.Settings;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 
 /**
  * User: Alexander Podkhalyuzin
  * Date: 10.02.2009
  */
 public class ConsoleRunner {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    String[] newArgs;
+    if (args.length == 1 && args[0].startsWith("@")) {
+      String arg = args[0];
+      File file = new File(arg.substring(1));
+      if (!file.exists())
+        throw new java.io.FileNotFoundException(String.format("argument file %s could not be found", file.getName()));
+      FileReader fileReader = new FileReader(file);
+      StringBuffer buffer = new StringBuffer();
+      while (true) {
+        int i = fileReader.read();
+        if (i == -1) break;
+        char c = (char) i;
+        if (c == '\r') continue;
+        buffer.append(c);
+      }
+      newArgs = buffer.toString().split("[\n]");
+    } else {
+      newArgs = args;
+    }
     (new InterpreterLoop(new Some(new BufferedReader(new InputStreamReader(System.in))),
-        new PrintWriter(System.out))).main(args);
+        new PrintWriter(System.out))).main(newArgs);
   }
 }
