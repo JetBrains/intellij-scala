2a1e5427dbe6c0cff2c56945da181f12d2be6f7f
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-06-15 16:13:57 +0400

    ParametersAnnotator

---
+++ test/org/jetbrains/plugins/scala/annotator/ParametersAnnotatorTest.scala
@@ -0,0 +1,61 @@
+package org.jetbrains.plugins.scala
+package annotator
+
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+
+/**
+ * Pavel.Fatin, 18.05.2010
+ */
+
+class ParametersAnnotatorTest extends SimpleTestCase {
+  val Header = "class A; class B; class C;\n"
+  
+  def testFine {
+    assertMatches(messages("def f(a: A) {}")) {
+      case Nil =>
+    }
+    assertMatches(messages("def f(a: A*) {}")) {
+      case Nil =>
+    }
+    assertMatches(messages("def f(a: A, b: B) {}")) {
+      case Nil =>
+    }
+    assertMatches(messages("def f(a: A, b: B*) {}")) {
+      case Nil =>
+    }
+    assertMatches(messages("def f(a: A, b: B, c: C*) {}")) {
+      case Nil =>
+    }
+  }
+  
+  def testMalformed {
+    assertMatches(messages("def f(a: A*, b: B) {}")) {
+      case Error("a: A*", "*-parameter must come last") :: Nil =>
+    }
+    assertMatches(messages("def f(a: A, b: B*, c: C) {}")) {
+      case Error("b: B*", "*-parameter must come last") :: Nil =>
+    }
+    assertMatches(messages("def f(a: A*, b: B*) {}")) {
+      case Error("a: A*", "*-parameter must come last") :: Nil =>
+    }
+    assertMatches(messages("def f(a: A*, b: B*, c: C) {}")) {
+      case Error("a: A*", "*-parameter must come last") :: 
+              Error("b: B*", "*-parameter must come last") :: Nil =>
+    }
+    assertMatches(messages("def f(a: A*, c: C)(b: B*, c: C) {}")) {
+      case Error("a: A*", "*-parameter must come last") :: 
+              Error("b: B*", "*-parameter must come last") :: Nil =>
+    }
+  }
+   
+  def messages(code: String): List[Message] = {
+    val annotator = new ParametersAnnotator() {}
+    val mock = new AnnotatorHolderMock
+
+    val function = (Header + code).parse.depthFirst.findByType(classOf[ScFunctionDefinition]).get
+    
+    annotator.annotateParameters(function.paramClauses, mock)
+    mock.annotations
+  }
+}
