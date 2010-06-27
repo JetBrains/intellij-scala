173798007dea6bcdc90f2c945f4eac499f3182b9
jzaugg <jzaugg@gmail.com>
2010-06-27 08:46:02 +0200

    Filename of package.scala is okay if it contains a package object. We could also check that the package object name matches the current folder name, but this belongs in a different inspection which is currently disabled.

--- src/org/jetbrains/plugins/scala/codeInspection/fileNameInspection/FileNameInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/fileNameInspection/FileNameInspection.scala
@@ -5,12 +5,12 @@ package fileNameInspection
 
 import collection.mutable.ArrayBuffer
 import com.intellij.codeHighlighting.HighlightDisplayLevel
-import lang.psi.api.toplevel.typedef.ScTypeDefinition
 import lang.psi.api.ScalaFile
 import com.intellij.codeInspection._
 import com.intellij.codeInspection.ex.ProblemDescriptorImpl
 import com.intellij.psi.PsiFile
 import java.lang.String
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScObject, ScTypeDefinition}
 
 /**
  * User: Alexander Podkhalyuzin
@@ -36,12 +36,18 @@ class FileNameInspection extends LocalInspectionTool {
     val scalaFile = file.asInstanceOf[ScalaFile]
     var hasProblems = true
     for (clazz <- scalaFile.getClasses) {
-      if (clazz.getName == name) hasProblems = false
+      clazz match {
+        case o: ScObject if file.getName == "package.scala" && o.isPackageObject => hasProblems = false
+        case _ if clazz.getName == name => hasProblems = false
+        case _ =>
+      }
     }
+
     val res = new ArrayBuffer[ProblemDescriptor]
     if (hasProblems) {
-      for (clazz <- scalaFile.getClasses; scalaClass: ScTypeDefinition = clazz.asInstanceOf[ScTypeDefinition]) {
-        res +=  manager.createProblemDescriptor(scalaClass.nameId, "Class doesn't correspond to file name", 
+      for (clazz <- scalaFile.getClasses;
+           scalaClass: ScTypeDefinition = clazz.asInstanceOf[ScTypeDefinition]) {
+        res += manager.createProblemDescriptor(scalaClass.nameId, "Class doesn't correspond to file name",
           Array[LocalQuickFix](new ScalaRenameClassQuickFix(scalaClass, name),
             new ScalaRenameFileQuickFix(scalaFile, clazz.getName + ".scala")), ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
       }
