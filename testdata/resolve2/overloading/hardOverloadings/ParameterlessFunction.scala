dbdb5b2c218dbdcbee5fd9ec5f02119c70fcbd34
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-06-30 10:23:18 +0400

    More overloading tests

---
+++ testdata/resolve2/overloading/hardOverloadings/ParameterlessFunction.scala
@@ -0,0 +1,16 @@
+object Test extends Application {
+  class X
+  class Y extends X
+  class C {
+    def apply(x: X, y: Y) = 1
+    def apply(x: Y, y: X) = 2
+  }
+  class A {
+    def foo: C = new C
+    def foo(x: X, y: X) = 3
+  }
+
+  val a = new A
+  val z = a./* line: 9 */foo(new X, new Y)
+  print(z)
+}
