36dcd97589aad4d39586c134e42d89271b613356
Pavel Fatin <Pavel.Fatin@jetbrains.com>
2010-08-05 14:37:44 +0400

    compiler library validation in facet UI

--- src/org/jetbrains/plugins/scala/config/LibraryData.scala
+++ src/org/jetbrains/plugins/scala/config/LibraryData.scala
@@ -5,26 +5,57 @@ import java.io.File
 import FileAPI._
 import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.roots.libraries.Library
-
 /**
  * Pavel.Fatin, 26.07.2010
  */
 
 
-abstract class LibraryData(delegate: Library, prefix: String, bundle: String) {
-  def valid: Boolean = jar.isDefined
-
-  def version: String = jar.flatMap(readProperty(_, bundle, "version.number")).mkString 
+abstract class LibraryData(protected val delegate: Library, name: String, 
+                           prefix: String, bundle: String, marker: String) {
+  def version: Option[String] = jar.flatMap(readProperty(_, bundle, "version.number")) 
 
   def classpath: String = files.map(_.getPath).mkString(File.pathSeparator)
   
   def files: Seq[File] = delegate.getFiles(OrderRootType.CLASSES).map(_.toFile)
   
-  def jar: Option[File] = files.find(_.getName.startsWith(prefix)) 
+  def jar: Option[File] = jars.headOption
+  
+  private def jars: Seq[File] = files.filter(_.getName.startsWith(prefix))
+  
+  def complete: Boolean = jar.map(exists(_, marker)).getOrElse(false)
+
+  def problem: Option[String] = {
+    if(jar.isEmpty) 
+      return Some("no %s*.jar found".format(prefix))
+
+    if(jars.size > 1) 
+      return Some("multiple %s*.jar's attached".format(prefix))
+        
+    if(version.isEmpty) 
+      return Some("unable to read %s version".format(jar.get.getName))
+    
+    if(!complete) 
+      return Some("no %s classes found in %s".format(name, jar.get.getName))
+    
+    None
+  }
 }
 
-class CompilerLibraryData(delegate: Library) 
-        extends LibraryData(delegate, "scala-compiler", "compiler.properties")
+class CompilerLibraryData(delegate: Library) extends LibraryData(delegate, "Scala compiler", 
+  "scala-compiler", "compiler.properties", "scala/tools/nsc/Main.class") {
+  
+  override def problem = {
+    val standardLibraryData = new StandardLibraryData(delegate)
+    super.problem.orElse(standardLibraryData.problem).orElse {
+      val standardLibraryVersion = standardLibraryData.version
+      if (version != standardLibraryVersion) 
+        Some("versions mismatch, compiler: %s, standard library: %s".format(
+          version.mkString, standardLibraryVersion.mkString)) 
+      else 
+        None
+    }
+  }
+}
 
-class ScalaLibraryData(delegate: Library) 
-        extends LibraryData(delegate, "scala-library", "library.properties")
+class StandardLibraryData(delegate: Library) extends LibraryData(delegate, "Scala standard library", 
+  "scala-library", "library.properties", "scala/Array.class") 
