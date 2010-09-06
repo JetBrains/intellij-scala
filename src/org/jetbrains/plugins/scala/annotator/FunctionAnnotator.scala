90cc1fd79cc6015e672c426390ae11da5cc01f72
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-09-06 15:05:12 +0400

    Fixed recursive method call type inference if method has defined return type in super method.

--- src/org/jetbrains/plugins/scala/annotator/FunctionAnnotator.scala
+++ src/org/jetbrains/plugins/scala/annotator/FunctionAnnotator.scala
@@ -17,7 +17,7 @@ import lang.psi.api.base.ScReferenceElement
 
 trait FunctionAnnotator {
   def annotateFunction(function: ScFunctionDefinition, holder: AnnotationHolder, highlightErrors: Boolean) {
-    if (function.hasAssign && !function.hasExplicitType) {
+    if (!function.hasExplicitType) {
       function.depthFirst.foreach {
         case ref: ScReferenceElement if ref.isReferenceTo(function) => {
           for (target <- ref.advancedResolve; if target.isApplicable) {
