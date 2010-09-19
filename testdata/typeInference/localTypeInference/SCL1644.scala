8c403b2eca74a14fc46933939a6d32a504b42cf7
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-09-19 16:41:17 +0400

    fixed test

--- testdata/typeInference/localTypeInference/SCL1644.scala
+++ testdata/typeInference/localTypeInference/SCL1644.scala
@@ -4,6 +4,7 @@ object SCL1644 extends Application {
   override def main(args: Array[String]) {
     val files: Array[File] = new File("c:/").listFiles()
     /*start*/files.map(file => file.length)/*end*/
+    files
   }
 }
 //Array[Long]
