0398b3c124230745a2c7b2629d11db6e11162208
jzaugg <jzaugg@gmail.com>
2010-09-28 01:16:04 +0200

    simplifiy test case for SCL2381 -- the problem is with contra-variant type parameters

--- testdata/typeInference/bugs2/SCL2381.scala
+++ testdata/typeInference/bugs2/SCL2381.scala
@@ -1,36 +1,17 @@
 class S1 {def s = this}
 class S2 {def s = this}
 
-object simple {
-  implicit def m1(f: Int => Int) = new S1
-
-  implicit def m2(l: Seq[Int]) = new S2
-
-  val c = List(1).s // correctly resolves m2
-}
-
 object param1 {
-  implicit def m1[A](f: Int => A) = new S1
+  trait T[-A]
+  trait U extends T[Int]
 
-  implicit def m2[A](l: Seq[A]) = new S2
+  implicit def m1[M11](f: T[M11]) = new S1
+  implicit def m2(l: U) = new S2
 
-  val c = List(1).s // correctly resolves m2
+  val u: U = new U {}
+  u.s // incorrectly resolves as ambiguous
 }
 
-object param2 {
-  implicit def m1[I, A](f: I => A) = new S1
+/*start*/param1.c/*end*/
 
-  implicit def m2[A](l: Seq[A]) = new S2
-
-  val c = List(1).s // incorrectly resolves as ambiguous
-}
-object param3 {
-  implicit def m1[I, A](f: I => A) = new S1
-
-  implicit def m2[M[X] <: Seq[X], A](l: M[A]) = new S2
-
-  val c = List(1).s // incorrectly resolves m1
-}
-/*start*/(simple.c, param1.c, param2.c, param3.c)/*end*/
-
-//(S2, S2, S2, S2)
+//S2
