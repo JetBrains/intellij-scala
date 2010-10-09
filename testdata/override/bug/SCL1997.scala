2c1a876e0dfb8bb8dd7ee25d4ab7171a12defd33
jzaugg <jzaugg@gmail.com>
2010-10-09 14:45:01 +0200

    Test case for the already fixed SCL-1997.

---
+++ testdata/override/bug/SCL1997.scala
@@ -0,0 +1,20 @@
+implement foo
+package test
+
+trait Foo {
+  def foo(a: Any*): Any
+}
+
+trait Sub extends Foo {
+  <caret>
+}<end>
+package test
+
+trait Foo {
+  def foo(a: Any*): Any
+}
+
+trait Sub extends Foo {
+
+  def foo(a: Any*): Any = null
+}
