0e0acc9789b82edbb327a1c6522c86b382b3e2af
jzaugg <jzaugg@gmail.com>
2010-10-09 11:16:38 +0200

    Test, fix, and enable by default, the SuspiciousNewLineInMethodCall.

--- src/org/jetbrains/plugins/scala/codeInspection/SuspiciousNewLineInMethodCall.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/SuspiciousNewLineInMethodCall.scala
@@ -8,7 +8,7 @@ import org.jetbrains.plugins.scala.lang.psi.types.{ScType, Any, AnyVal}
 import com.intellij.codeInspection._
 import com.intellij.psi.{PsiElement, PsiFile}
 import org.jetbrains.plugins.scala.lang.psi.{ScalaPsiElement, ScalaPsiUtil}
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScArgumentExprList, ScMethodCall, ScInfixExpr, ScExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.expr._
 
 /**
  * Detects method calls with suspicuous new-lines:
@@ -22,56 +22,65 @@ import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScArgumentExprList, ScMeth
  *   test foo
  *   1
  *
- *   111111
+ *   1
  *   {
- *     sd
+ *     foo
  *   }
  * }
  </pre>
  */
 class SuspiciousNewLineInMethodCall extends LocalInspectionTool {
+  import SuspiciousNewLineInMethodCall._
+  
   def getGroupDisplayName: String = InspectionsUtil.SCALA
 
   def getDisplayName: String = "Suspicious New Line in Method Call"
 
   def getShortName: String = getDisplayName
 
-  override def isEnabledByDefault: Boolean = false
+  override def isEnabledByDefault: Boolean = true
 
   override def getStaticDescription: String = "Detects new-lines in method calls that are not inferred as semi-colons"
 
   override def getID: String = "SuspiciousNewLineInMethodCall"
 
   override def checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array[ProblemDescriptor] = {
-    if (!file.isInstanceOf[ScalaFile]) return Array[ProblemDescriptor]()
+    def createDescriptor(p: Problem) = manager.createProblemDescriptor(p.place, p.message, Array[LocalQuickFix](), ProblemHighlightType.INFO)
+    checkFileInternal(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean).map(createDescriptor).toArray
+  }
+
+  // For unit testing. When trying to test through the method above, I got the error: "ERROR: Non-physical PsiElement. Physical element is required to be able to anchor the problem"
+  private[codeInspection] def checkFileInternal(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Seq[Problem] = {
+    if (!file.isInstanceOf[ScalaFile]) return Seq()
 
     val scalaFile = file.asInstanceOf[ScalaFile]
-    val res = new ArrayBuffer[ProblemDescriptor]
+    val res = new ArrayBuffer[Problem]
 
     def addError(place: PsiElement) = {
-      res += manager.createProblemDescriptor(place, ScalaBundle.message("suspicicious.newline"),
-        Array[LocalQuickFix](), ProblemHighlightType.INFO)
+      res += Problem(place, ScalaBundle.message("suspicicious.newline"))
+    }
+
+    def newLineBetweenOperationAndROp(x: ScInfixExpr) = {
+      val es = ScalaPsiUtil.getElementsRange(x.operation, x.rOp)
+      val whitespace = es.drop(1).dropRight(1)
+      whitespace.exists(_.getText.contains("\n"))
     }
 
     val visitor = new ScalaRecursiveElementVisitor {
       override def visitElement(elem: ScalaPsiElement): Unit = {
         elem match {
-          case x: ScInfixExpr if {
-            val es = ScalaPsiUtil.getElementsRange(x.operation, x.rOp)
-            val whitespace = es.drop(1).dropRight(1)
-            whitespace.exists(_.getText.contains("\n"))
-          } => addError(x)
-          // TODO "Map(\n1 -> 2)" is incorrectly highlighted.
-          case mc: ScArgumentExprList if {
-            val prev = mc.exprs.headOption.flatMap(first => Option(first.getPrevSibling))
-            prev.exists(_.getText.contains("\n"))
-          } => addError(mc.getContext)
+          case x: ScInfixExpr if !elem.getContext.isInstanceOf[ScParenthesisedExpr] && newLineBetweenOperationAndROp(x) => addError(x)
+          case mc: ScArgumentExprList if mc.getText.startsWith("\n") => addError(mc.getContext)
           case _ =>
         }
         super.visitElement(elem)
       }
     }
     file.accept(visitor)
-    return res.toArray
+    return res.toSeq
   }
+}
+
+object SuspiciousNewLineInMethodCall {
+  case class Problem(place: PsiElement, message: String)
 }
