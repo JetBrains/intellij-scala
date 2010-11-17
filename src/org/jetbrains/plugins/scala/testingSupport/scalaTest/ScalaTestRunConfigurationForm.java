fd7d59b62b9b67c934d889f5ffc50e3c3783158e
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2010-11-17 17:45:46 +0300

    fixed build

--- src/org/jetbrains/plugins/scala/testingSupport/scalaTest/ScalaTestRunConfigurationForm.java
+++ src/org/jetbrains/plugins/scala/testingSupport/scalaTest/ScalaTestRunConfigurationForm.java
@@ -4,6 +4,7 @@ import com.intellij.execution.ExecutionBundle;
 import com.intellij.execution.configuration.BrowseModuleValueActionListener;
 import com.intellij.execution.ui.ClassBrowser;
 import com.intellij.execution.ui.ConfigurationModuleSelector;
+import com.intellij.ide.util.ClassFilter;
 import com.intellij.ide.util.PackageChooserDialog;
 import com.intellij.ide.util.TreeClassChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
@@ -173,8 +174,8 @@ public class ScalaTestRunConfigurationForm {
                               final TextFieldWithBrowseButton textField,
                               final Project project) {
      ClassBrowser browser = new ClassBrowser(project, title) {
-       protected TreeClassChooser.ClassFilterWithScope getFilter() throws ClassBrowser.NoFilterException {
-         return new TreeClassChooser.ClassFilterWithScope() {
+       protected ClassFilter.ClassFilterWithScope getFilter() throws ClassBrowser.NoFilterException {
+         return new ClassFilter.ClassFilterWithScope() {
            public GlobalSearchScope getScope() {
              return GlobalSearchScope.allScope(project);
            }
