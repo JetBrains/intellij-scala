c618d4441c0ce25497c8b1a89a99eecca259d9f1
Pavel Fatin <pavel.fatin@jetbrains.com>
2010-11-25 20:28:33 +0300

    SCL-2512 Language injection for Scala

---
+++ src/org/jetbrains/plugins/scala/injection/ScalaStringLiteralManipulator.java
@@ -0,0 +1,45 @@
+package org.jetbrains.plugins.scala.injection;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.AbstractElementManipulator;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral;
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression;
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory;
+
+/**
+ * Pavel Fatin
+ */
+
+public class ScalaStringLiteralManipulator extends AbstractElementManipulator<ScLiteral> {
+  public ScLiteral handleContentChange(ScLiteral expr, TextRange range, String newContent) throws IncorrectOperationException {
+    if (!(expr.getValue() instanceof String)) throw new IncorrectOperationException("cannot handle content change");
+    String oldText = expr.getText();
+    newContent = StringUtil.escapeStringCharacters(newContent);
+    String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
+    final ScExpression newExpr = ScalaPsiElementFactory.createExpressionFromText(newText, expr.getManager());
+
+    PsiElement firstChild = expr.getFirstChild();
+    assert firstChild != null && firstChild.getNextSibling() == null;
+
+    PsiElement newElement = newExpr.getFirstChild();
+    assert newElement != null;
+    firstChild.replace(newElement);
+
+    return expr;
+  }
+
+  public TextRange getRangeInElement(final ScLiteral element) {
+    final String text = element.getText();
+    return getLiteralRange(text);
+  }
+
+  public static TextRange getLiteralRange(String text) {
+    if (text.length() > 6 && text.startsWith("\"\"\"") && text.endsWith("\"\"\"")) {
+      return new TextRange(3, text.length() - 3);
+    }
+    return new TextRange(1, Math.max(1, text.length() - 1));
+  }
+}
