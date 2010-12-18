87b83d3354177e0390fc550c2b707618dc0327eb
Pavel Fatin <pavel.fatin@jetbrains.com>
2010-12-18 23:34:22 +0300

    Better injection editing for multi-line strings

--- src/org/jetbrains/plugins/scala/injection/ScalaStringLiteralManipulator.java
+++ src/org/jetbrains/plugins/scala/injection/ScalaStringLiteralManipulator.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes;
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral;
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression;
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory;
@@ -17,7 +18,10 @@ public class ScalaStringLiteralManipulator extends AbstractElementManipulator<Sc
   public ScLiteral handleContentChange(ScLiteral expr, TextRange range, String newContent) throws IncorrectOperationException {
     if (!(expr.getValue() instanceof String)) throw new IncorrectOperationException("cannot handle content change");
     String oldText = expr.getText();
-    newContent = StringUtil.escapeStringCharacters(newContent);
+
+    if(expr.getFirstChild().getNode().getElementType() != ScalaTokenTypes.tMULTILINE_STRING)
+      newContent = StringUtil.escapeStringCharacters(newContent);
+
     String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
     final ScExpression newExpr = ScalaPsiElementFactory.createExpressionFromText(newText, expr.getManager());
 
