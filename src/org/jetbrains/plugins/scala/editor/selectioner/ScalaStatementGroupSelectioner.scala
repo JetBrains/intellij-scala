92ed894dc6dd05e6da5b6ee3de272fc1d7d2b217
unknown <yole@.(none)>
2010-12-27 17:50:25 +0300

    port a couple of Ctrl-W steps for method bodies from Java to Scala (SCL-1682)

---
+++ src/org/jetbrains/plugins/scala/editor/selectioner/ScalaStatementGroupSelectioner.scala
@@ -0,0 +1,71 @@
+package org.jetbrains.plugins.scala
+package editor.selectioner
+
+import com.intellij.codeInsight.editorActions.ExtendWordSelectionHandlerBase
+import com.intellij.openapi.editor.Editor
+import java.util.ArrayList
+import com.intellij.psi._
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.LineTokenizer
+import impl.source.tree.LeafPsiElement
+import tree.IElementType
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlockStatement, ScBlock}
+
+/**
+ * @author yole
+ */
+
+class ScalaStatementGroupSelectioner extends ExtendWordSelectionHandlerBase {
+  def canSelect(e: PsiElement) = {
+    e match {
+      case _: ScBlockStatement => true
+      case _: PsiComment => true
+      case _ => false
+    }
+  }
+
+  override def select(e: PsiElement, editorText: CharSequence, cursorOffset: Int, editor: Editor): java.util.List[TextRange] = {
+    val parent: PsiElement = e.getParent
+
+    if (!(parent.isInstanceOf[ScBlock])) {
+      return new ArrayList[TextRange]
+    }
+
+    def back(e: PsiElement) = e.getPrevSibling
+    def forward(e: PsiElement) = e.getNextSibling
+    val startElement = skipWhitespace(findGroupBoundary(e, back, ScalaTokenTypes.tLBRACE), forward)
+    val endElement = skipWhitespace(findGroupBoundary(e, forward, ScalaTokenTypes.tRBRACE), back)
+
+    val range: TextRange = new TextRange(startElement.getTextRange.getStartOffset, endElement.getTextRange.getEndOffset)
+    return ExtendWordSelectionHandlerBase.expandToWholeLine(editorText, range)
+  }
+
+  def findGroupBoundary(startElement: PsiElement, step: PsiElement => PsiElement, stopAt: IElementType): PsiElement = {
+    var current: PsiElement = startElement
+    while (step(current) != null) {
+      val sibling: PsiElement = step(current)
+      sibling match {
+        case leaf: LeafPsiElement =>
+          if (leaf.getElementType == stopAt) return current
+          if (leaf.getElementType == ScalaTokenTypes.tLINE_TERMINATOR) {
+            val strings: Array[String] = LineTokenizer.tokenize(leaf.getText.toCharArray, false)
+            if (strings.length > 2) {
+              return current
+            }
+          }
+        case _ =>
+      }
+      current = sibling
+    }
+    return current
+  }
+
+  def skipWhitespace(start: PsiElement, step: PsiElement => PsiElement): PsiElement = {
+    var current = start
+    while(current.isInstanceOf[PsiWhiteSpace]) {
+      current = step(current)
+    }
+    return current
+  }
+}
