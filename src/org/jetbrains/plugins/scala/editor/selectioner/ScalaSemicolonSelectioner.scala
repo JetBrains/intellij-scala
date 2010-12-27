0d420b5e7a2586c9f10b1ea492ce0d75965dc266
Dmitry Jemerov <yole@jetbrains.com>
2010-12-27 19:08:41 +0300

    step to select statement with its trailing semicolon (SCL-2125)

---
+++ src/org/jetbrains/plugins/scala/editor/selectioner/ScalaSemicolonSelectioner.scala
@@ -0,0 +1,29 @@
+package org.jetbrains.plugins.scala.editor.selectioner
+
+import com.intellij.codeInsight.editorActions.ExtendWordSelectionHandlerBase
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScBlockStatement
+import com.intellij.openapi.editor.Editor
+import com.intellij.lang.ASTNode
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import java.util.ArrayList
+import com.intellij.openapi.util.TextRange
+
+/**
+ * Selects a statement together with its trailing semicolon.
+ *
+ * @author yole
+ */
+class ScalaSemicolonSelectioner extends ExtendWordSelectionHandlerBase {
+  def canSelect(e: PsiElement) = e.isInstanceOf[ScBlockStatement]
+
+  override def select(e: PsiElement, editorText: CharSequence, cursorOffset: Int, editor: Editor) = {
+    val treeNext: ASTNode = e.getNode.getTreeNext
+    val result = new ArrayList[TextRange]
+    if (treeNext != null && treeNext.getElementType == ScalaTokenTypes.tSEMICOLON) {
+      val r = new TextRange(e.getTextRange.getStartOffset, treeNext.getTextRange.getEndOffset)
+      result.add(r)
+    }
+    result
+  }
+}
