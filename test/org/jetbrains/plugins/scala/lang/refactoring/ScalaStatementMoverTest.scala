4839ca9c43b507852f05e8e2629d6dd13ca3e95c
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-01-13 18:43:22 +0300

    Updated to IDEA 102.132.

--- test/org/jetbrains/plugins/scala/lang/refactoring/ScalaStatementMoverTest.scala
+++ test/org/jetbrains/plugins/scala/lang/refactoring/ScalaStatementMoverTest.scala
@@ -3,7 +3,7 @@ package lang.refactoring
 
 import org.jetbrains.plugins.scala.base.SimpleTestCase
 import com.intellij.psi.PsiFile
-import org.junit.Assert
+//import org.junit.Assert
 
 /**
  * Pavel Fatin
@@ -11,6 +11,6 @@ import org.junit.Assert
 
 class ScalaStatementMoverTest extends SimpleTestCase {
   def testFoo() {
-    Assert.assertTrue(true)
+    //Assert.assertTrue(true)
   }
 }
