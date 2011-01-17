b7ec7d2a2bee543d2b254e80f211863826abb565
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-01-17 16:30:26 +0300

    More parser fixes.

--- src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/Expr1.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/Expr1.scala
@@ -315,7 +315,8 @@ object Expr1 {
       //---------------return statement-----------//
       case ScalaTokenTypes.kRETURN => {
         builder.advanceLexer //Ate return
-        Expr parse builder
+        if (!builder.newlineBeforeCurrentToken)
+          Expr parse builder
         exprMarker.done(ScalaElementTypes.RETURN_STMT)
         return true
       }
