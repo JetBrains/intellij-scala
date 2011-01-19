7125b90f248cfdf5cc3991d080332630a88bec88
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-01-19 12:33:11 +0300

    SCL-2660 patch

--- src/org/jetbrains/plugins/scala/conversion/JavaToScala.scala
+++ src/org/jetbrains/plugins/scala/conversion/JavaToScala.scala
@@ -525,6 +525,9 @@ object JavaToScala {
         }
         res.append(" ")
       }
+      case v:PsiArrayInitializerMemberValue => {
+        res.append(v.getInitializers.map(convertPsiToText(_)).mkString("Array(", ", ", ")"))
+      }
       case r: PsiReferenceParameterList => {
        if (r.getTypeParameterElements.length > 0) res.append(r.getTypeParameterElements.map(convertPsiToText(_)).mkString("[" ,", ", "]"))
       }
