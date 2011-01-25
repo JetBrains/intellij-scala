d36c9a5ad65b9dbc06709914ab12e56ba9a2451f
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-01-25 15:11:37 +0300

    SCL-2748 Highlight illegal mixing-in of non-trait

--- src/org/jetbrains/plugins/scala/annotator/TemplateDefinitionAnnotator.scala
+++ src/org/jetbrains/plugins/scala/annotator/TemplateDefinitionAnnotator.scala
@@ -2,10 +2,10 @@ package org.jetbrains.plugins.scala
 package annotator
 
 import com.intellij.lang.annotation.AnnotationHolder
-import lang.psi.api.toplevel.typedef.ScTemplateDefinition
 import lang.psi.types.ScType
 import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
 import lang.psi.api.expr.ScNewTemplateDefinition
+import lang.psi.api.toplevel.typedef.{ScTrait, ScTemplateDefinition}
 
 /**
  * Pavel Fatin
@@ -15,20 +15,26 @@ trait TemplateDefinitionAnnotator {
   def annotateTemplateDefinition(defintion: ScTemplateDefinition, holder: AnnotationHolder) {
     val block = defintion.extendsBlock
 
+    val refs = block.templateParents.toSeq.flatMap(_.typeElements).map { refElement =>
+       val psiClass = refElement.getType(TypingContext.empty).toOption.flatMap(ScType.extractClass(_))
+      (refElement, psiClass)
+    }
+
+    refs.drop(1).foreach {
+      case (refElement, Some(psiClass)) if !psiClass.isInstanceOf[ScTrait] =>
+        holder.createErrorAnnotation(refElement, "Class %s needs to be trait to be mixed in".format(psiClass.getName))
+      case _ =>
+    }
+
     if (defintion.isInstanceOf[ScNewTemplateDefinition] && block.templateBody.isEmpty) return
 
-    block.templateParents.toSeq.flatMap(_.typeElements).foreach { refElement =>
-      val classes = refElement.getType(TypingContext.empty).toOption.toSeq
-              .flatMap(ScType.extractClass(_).toSeq)
-
-      classes.filter(_.getModifierList.hasModifierProperty("final")).foreach { it =>
-        holder.createErrorAnnotation(refElement, "Illegal inheritance from final class %s".format(it.getName))
-      }
-
-      classes.filter(_.getModifierList.hasModifierProperty("sealed"))
-              .filter(_.getContainingFile != refElement.getContainingFile).foreach { it =>
-        holder.createErrorAnnotation(refElement, "Illegal inheritance from sealed type %s".format(it.getName))
-      }
+    refs.foreach {
+      case (refElement, Some(psiClass)) if psiClass.getModifierList.hasModifierProperty("final") =>
+        holder.createErrorAnnotation(refElement, "Illegal inheritance from final class %s".format(psiClass.getName))
+      case (refElement, Some(psiClass)) if psiClass.getModifierList.hasModifierProperty("sealed") &&
+              psiClass.getContainingFile != refElement.getContainingFile =>
+        holder.createErrorAnnotation(refElement, "Illegal inheritance from sealed type %s".format(psiClass.getName))
+      case _ =>
     }
   }
 }
