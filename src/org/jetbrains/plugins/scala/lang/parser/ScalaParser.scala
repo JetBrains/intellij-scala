1e2518c488241c15410e2c526c0d51feaff61a32
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-02-14 15:27:19 +0300

    Fixed reparse error on case clauses blocks.

--- src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaParser.scala
@@ -7,7 +7,7 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.PsiBuilder
 import com.intellij.psi.tree.IElementType
 import parsing.builder.ScalaPsiBuilderImpl
-import parsing.expressions.Block
+import parsing.expressions.{BlockExpr, Block}
 import parsing.Program
 
 class ScalaParser extends PsiParser {
@@ -15,7 +15,7 @@ class ScalaParser extends PsiParser {
   def parse(root: IElementType, builder: PsiBuilder): ASTNode = {
     root match {
       case ScalaElementTypes.BLOCK_EXPR =>
-        Block.parse(new ScalaPsiBuilderImpl(builder), true)
+        BlockExpr.parse(new ScalaPsiBuilderImpl(builder))
       case _ =>
         val rootMarker = builder.mark
         Program.parse(new ScalaPsiBuilderImpl(builder))
