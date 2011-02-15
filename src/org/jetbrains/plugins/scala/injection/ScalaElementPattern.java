5e91d88391d10dbdb0b3afbc52cda87e0883151c
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-02-15 15:34:59 +0300

    Language injection performance improved (pattern pre-check without method resolving)

--- src/org/jetbrains/plugins/scala/injection/ScalaElementPattern.java
+++ src/org/jetbrains/plugins/scala/injection/ScalaElementPattern.java
@@ -4,11 +4,7 @@ import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.InitialPatternCondition;
 import com.intellij.patterns.PatternCondition;
 import com.intellij.patterns.PsiJavaElementPattern;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.ResolveResult;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.psi.*;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement;
@@ -21,7 +17,7 @@ import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScArguments;
  * Pavel Fatin
  */
 
-public class ScalaElementPattern<T extends ScalaPsiElement, Self extends ScalaElementPattern<T, Self>> extends PsiJavaElementPattern<T, Self> {
+public class ScalaElementPattern<T extends ScLiteral, Self extends ScalaElementPattern<T, Self>> extends PsiJavaElementPattern<T, Self> {
   public ScalaElementPattern(Class<T> tClass) {
     super(tClass);
   }
@@ -56,32 +52,65 @@ public class ScalaElementPattern<T extends ScalaPsiElement, Self extends ScalaEl
     });
   }
 
-  public Self callArgument(final int index, final ElementPattern<? extends PsiMethod> methodPattern) {
+  public Self callArgument(final int index, final String target, final String method, final String... parameters) {
     return with(new PatternCondition<T>("callArgument") {
       public boolean accepts(@NotNull final T host, final ProcessingContext context) {
-        final PsiElement parent = host.getParent();
-        if (parent instanceof ScArguments) {
-          final ScArgumentExprList psiExpressionList = (ScArgumentExprList) parent;
-          final ScExpression[] psiExpressions = psiExpressionList.exprsArray();
-          if (!(psiExpressions.length > index && psiExpressions[index] == host)) return false;
-
-          final PsiElement element = psiExpressionList.getParent();
-          if (element instanceof ScMethodCall) {
-            final ScalaPsiElement expression = ((ScMethodCall) element).getInvokedExpr();
-            if (expression instanceof ScReferenceElement) {
-              final ScReferenceElement ref = (ScReferenceElement) expression;
-              for (ResolveResult result : ref.multiResolve(false))
-                if (methodPattern.getCondition().accepts(result.getElement(), context))
-                  return true;
-            }
-          }
-        }
-        return false;
+        return isArgumentInCall(host, method, parameters.length, index) && // check without method resolving
+            isArgumentForMethod(host, target, method, parameters); // check with method resolving
       }
     });
   }
 
-  public static class Capture<T extends ScalaPsiElement> extends ScalaElementPattern<T, Capture<T>> {
+  private static boolean isArgumentForMethod(PsiElement host, String target, String method, String... parameters) {
+    PsiElement parent = host.getParent();
+    if (parent instanceof ScArguments) {
+      PsiElement element = parent.getParent();
+      if (element instanceof ScMethodCall) {
+        ScalaPsiElement expression = ((ScMethodCall) element).getInvokedExpr();
+        if (expression instanceof ScReferenceElement) {
+          for (ResolveResult result : ((ScReferenceElement) expression).multiResolve(false)) {
+            PsiElement targetElement = result.getElement();
+            if(targetElement instanceof PsiMethod) {
+              PsiMethod targetMehtod = (PsiMethod) targetElement;
+              if(!method.equals(targetMehtod.getName())) return false;
+              PsiParameter[] methodParameters = targetMehtod.getParameterList().getParameters();
+              if(methodParameters.length != parameters.length) return false;
+              for(int i = 0; i < methodParameters.length; i++) {
+                if(!methodParameters[i].getType().getCanonicalText().equals(parameters[i])) return false;
+              }
+              PsiClass targetClass = targetMehtod.getContainingClass();
+              return targetClass != null && target.equals(targetClass.getQualifiedName());
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  private static boolean isArgumentInCall(PsiElement host, String method, int parameters, int index) {
+    PsiElement parent = host.getParent();
+
+    if (parent instanceof ScArguments) {
+      ScArgumentExprList list = (ScArgumentExprList) parent;
+      ScExpression[] arguments = list.exprsArray();
+
+      if(arguments.length != parameters) return false;
+      if (arguments[index] != host) return false;
+
+      PsiElement prevSibling = list.getPrevSibling();
+      if(prevSibling == null) return false;
+
+      PsiElement lastChild = prevSibling.getLastChild();
+      if(lastChild == null) return false;
+
+      return method.equals(lastChild.getText());
+    }
+
+    return false;
+  }
+
+  public static class Capture<T extends ScLiteral> extends ScalaElementPattern<T, Capture<T>> {
     public Capture(final Class<T> aClass) {
       super(aClass);
     }
