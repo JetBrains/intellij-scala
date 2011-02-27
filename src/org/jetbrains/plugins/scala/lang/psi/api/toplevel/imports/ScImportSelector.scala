2bedc809413ac62de4209e1da0cc39dc4c7c9cf8
jzaugg <jzaugg@gmail.com>
2011-02-27 09:42:04 +0100

    Revert 0a6a645a5891934084e7814cae13b53e6b88069f, which fixed one problem but introduced another. Imports adjustment after move class refactoring *really* needs testing and some proper treatment.

--- src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/imports/ScImportSelector.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/imports/ScImportSelector.scala
@@ -19,22 +19,4 @@ trait ScImportSelector extends ScalaPsiElement {
   def reference : ScStableCodeReferenceElement
 
   def deleteSelector: Unit
-
-  /**
-   * Delete this selector from the containing import statement, and
-   * add an import statement based on `newQualifier` that contains this selector.
-   *
-   * moveSelector("newqualifier", "f")
-   *
-   * Before:
-   *
-   * import a.b.{c, /*this*/d => e}, x
-   *
-   * After:
-   *
-   * import newqualifier.{f => e}
-   * import a.b.{c}, x
-   *
-   */
-  def moveSelector(newQualifier: String, newName: String): Unit
 }
