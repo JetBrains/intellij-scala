449f05c7ead9f5ac3a3baf5d3980cc336c4c2599
jzaugg <jzaugg@gmail.com>
2011-02-28 22:51:28 +0100

    Inject some @Language goodness into the annotator tests.

--- test/org/jetbrains/plugins/scala/annotator/AnnotatorTestBase.scala
+++ test/org/jetbrains/plugins/scala/annotator/AnnotatorTestBase.scala
@@ -5,14 +5,18 @@ import org.jetbrains.plugins.scala.base.SimpleTestCase
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import junit.framework.Assert._
 import com.intellij.psi.{PsiReference, PsiErrorElement}
+import org.intellij.lang.annotations.Language
 
 /**
  * Pavel Fatin
  */
 
 abstract class AnnotatorTestBase[T <: ScalaPsiElement](annotator: AnnotatorPart[T]) extends SimpleTestCase {
-  protected def messages(code: String): List[Message] = {
-    val s = "object Holder { class Object; " + code + " }"
+  final val Prefix = "object Holder { class Object; "
+  final val Suffix = " }"
+
+  protected def messages(@Language(value = "Scala", prefix = Prefix, suffix = Suffix) code: String): List[Message] = {
+    val s = Prefix + code + Suffix
     val mock = new AnnotatorHolderMock
     val file = s.parse
 
