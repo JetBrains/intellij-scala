7e611fee133b26e86f9449188243a57a06e640ba
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-03-01 14:54:26 +0300

    Better browser launch fallback

--- src/org/jetbrains/plugins/scala/DesktopUtils.scala
+++ src/org/jetbrains/plugins/scala/DesktopUtils.scala
@@ -1,14 +1,27 @@
 package org.jetbrains.plugins.scala
 
-import java.awt.Desktop
-import com.intellij.openapi.ui.Messages
 import java.net.{URL, URI}
+import javax.swing.event.HyperlinkEvent
+import com.intellij.notification.{NotificationListener, NotificationType, Notification, Notifications}
+import java.awt.{Toolkit, Desktop}
+import java.awt.datatransfer.StringSelection
+import org.intellij.lang.annotations.Language
 
 /**
  * Pavel Fatin
  */
 
 object DesktopUtils {
+  @Language("HTML")
+  private val MessageFormat = """
+  <html>
+  <body>
+  Unable to launch web browser, please manually open:<br>
+  %1$s (<a href="%1$s">copy to clipboard</a>)>
+  </body>
+  </html>
+  """
+
   def browse(url: URL) {
     browse(url.toExternalForm)
   }
@@ -19,8 +32,16 @@ object DesktopUtils {
     if(supported)
       Desktop.getDesktop.browse(new URI(url))
     else
-      Messages.showWarningDialog(
-        "Unable to launch a web browser, please open: %s".format(url),
-        "Problem opening web page")
+      Notifications.Bus.notify(new Notification("scala", "Problem opening web page",
+        MessageFormat.format(url), NotificationType.WARNING, Listener))
+  }
+
+   private object Listener extends NotificationListener {
+    def hyperlinkUpdate(notification: Notification, event: HyperlinkEvent) {
+      Option(event.getURL).foreach { url =>
+         val clipboard = Toolkit.getDefaultToolkit.getSystemClipboard
+         clipboard.setContents(new StringSelection(url.toExternalForm), null)
+      }
+    }
   }
 }
