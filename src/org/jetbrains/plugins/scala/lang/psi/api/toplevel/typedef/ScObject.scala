a6a831aa58fc3096ebaef413e622d839e6c86bba
jzaugg <jzaugg@gmail.com>
2011-03-01 22:37:54 +0100

    SCL-2167 Part II GotoSuperAction can navigate from an object up to the parent member.
    Made ScObject a DeclaredElementsHolder. Being conservative, I *didn't* change ScObject#getContainingClass, instead in introduced another method ScObject#containingClass. Hopefully we can merge these together.
    
    Still missing are line markers.

--- src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/typedef/ScObject.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/typedef/ScObject.scala
@@ -5,20 +5,30 @@ package api
 package toplevel
 package typedef
 
-import com.intellij.psi.PsiMethod
+import com.intellij.psi.{PsiElement, PsiMethod}
+import lexer.ScalaTokenTypes
+import statements.ScDeclaredElementsHolder
 
 /**
 * @author Alexander Podkhalyuzin
 * Date: 20.02.2008
 */
 
-trait ScObject extends ScTypeDefinition with ScTypedDefinition with ScMember {
+trait ScObject extends ScTypeDefinition with ScTypedDefinition with ScMember with ScDeclaredElementsHolder {
 
   override def getContainingClass: ScTemplateDefinition = null
+
+  // TODO jzaugg I added this method rather than redefining getContainingClass to be conservative.
+  //             Would anything break if getContainingClass didn't return null?
+  def containingClass: Option[ScTemplateDefinition] = Option(super[ScMember].getContainingClass)
 
   //Is this object generated as case class companion module
   private var isSyntheticCaseClassCompanion: Boolean = false
   def isSyntheticObject: Boolean = isSyntheticCaseClassCompanion
   def setSyntheticObject: Unit = isSyntheticCaseClassCompanion = true
   def objectSyntheticMembers: Seq[PsiMethod]
+
+  def getObjectToken: PsiElement = findFirstChildByType(ScalaTokenTypes.kOBJECT)
+
+  def declaredElements = Seq(this)
 }
