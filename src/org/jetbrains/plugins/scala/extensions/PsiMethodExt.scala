895fc585794736e835585168131ae4e193d8fdd0
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-03-04 19:39:00 +0300

    Misc

--- src/org/jetbrains/plugins/scala/extensions/PsiMethodExt.scala
+++ src/org/jetbrains/plugins/scala/extensions/PsiMethodExt.scala
@@ -11,7 +11,7 @@ class PsiMethodExt(repr: PsiMethod) {
     """(?-i)(?:get|is|can|could|has|have|to)\p{Lu}.*""".r
 
   private val MutatorNamePattern =
-    """(?-i)(?:do|add|remove|insert|delete|open|close|aquire|release)(?:\p{Lu}.*)""".r
+    """(?-i)(?:do|add|remove|insert|delete|aquire|release)(?:\p{Lu}.*)""".r
 
   def isAccessor: Boolean = {
     hasQueryLikeName && !hasVoidReturnType
