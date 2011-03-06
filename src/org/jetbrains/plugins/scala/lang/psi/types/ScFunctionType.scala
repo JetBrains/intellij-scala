e9d44c6f8ef266cadc2977fb2c316ba51428f4a2
jzaugg <jzaugg@gmail.com>
2011-03-06 13:30:54 +0100

    Refactor: Move ScTupleType to its own file.

--- src/org/jetbrains/plugins/scala/lang/psi/types/ScFunctionType.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/types/ScFunctionType.scala
@@ -12,11 +12,6 @@ import com.intellij.psi.{PsiElement, JavaPsiFacade, PsiClass}
 /**
 * @author ilyas
 */
-
-/**
- * @param shortDef is true for functions defined as follows
- *  def foo : Type = ...
- */
 case class ScFunctionType private (returnType: ScType, params: Seq[ScType]) extends ValueType {
   private var project: Project = null
   private var scope: GlobalSearchScope = GlobalSearchScope.allScope(getProject)
@@ -93,67 +88,4 @@ case class ScFunctionType private (returnType: ScType, params: Seq[ScType]) exte
       case _ => (false, undefinedSubst)
     }
   }
-}
-
-case class ScTupleType private (components: Seq[ScType]) extends ValueType {
-  private var project: Project = null
-  private var scope: GlobalSearchScope = GlobalSearchScope.allScope(getProject)
-  def getProject: Project = {
-    if (project != null) project else DecompilerUtil.obtainProject
-  }
-
-  def getScope = scope
-
-  def this(components: Seq[ScType], project: Project, scope: GlobalSearchScope) = {
-    this(components)
-    this.project = project
-    this.scope = scope
-  }
-
-  def resolveTupleTrait: Option[ScParameterizedType] = resolveTupleTrait(getProject)
-
-  def resolveTupleTrait(project: Project): Option[ScParameterizedType] = {
-    def findClass(fullyQualifiedName: String) : Option[PsiClass] = {
-        val psiFacade = JavaPsiFacade.getInstance(project)
-        Option(psiFacade.findClass(tupleTraitName, getScope))
-    }
-    findClass(tupleTraitName) match {
-      case Some(t: ScClass) => {
-        val typeParams = components.toList
-        Some(ScParameterizedType(ScDesignatorType(t), typeParams))
-      }
-      case _ => None
-    }
-  }
-
-  override def equivInner(r: ScType, uSubst: ScUndefinedSubstitutor, falseUndef: Boolean): (Boolean, ScUndefinedSubstitutor) = {
-    var undefinedSubst = uSubst
-    r match {
-      case ScTupleType(c1) if c1.length == components.length => {
-        val iter1 = components.iterator
-        val iter2 = c1.iterator
-        while (iter1.hasNext) {
-          val t = Equivalence.equivInner(iter1.next, iter2.next, undefinedSubst, falseUndef)
-          if (!t._1) return (false, undefinedSubst)
-          undefinedSubst = t._2
-        }
-        (true, undefinedSubst)
-      }
-      case p: ScParameterizedType => {
-        p.getTupleType match {
-          case Some(tuple) => this.equivInner(tuple, undefinedSubst, falseUndef)
-          case _ => (false, undefinedSubst)
-        }
-      }
-      case _ => (false, undefinedSubst)
-    }
-  }
-
-  override def removeAbstracts = ScTupleType(components.map(_.removeAbstracts))
-
-  override def updateThisType(place: PsiElement) = ScTupleType(components.map(_.updateThisType(place)))
-
-  override def updateThisType(tp: ScType) = ScTupleType(components.map(_.updateThisType(tp)))
-
-  private def tupleTraitName = "scala.Tuple" + components.length
 }
---
+++ src/org/jetbrains/plugins/scala/lang/psi/types/ScTupleType.scala
@@ -0,0 +1,76 @@
+package org.jetbrains.plugins.scala
+package lang
+package psi
+package types
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.search.GlobalSearchScope
+import api.toplevel.typedef.{ScClass, ScTrait}
+import decompiler.DecompilerUtil
+import com.intellij.psi.{PsiElement, JavaPsiFacade, PsiClass}
+
+/**
+* @author ilyas
+*/
+case class ScTupleType private (components: Seq[ScType]) extends ValueType {
+  private var project: Project = null
+  private var scope: GlobalSearchScope = GlobalSearchScope.allScope(getProject)
+  def getProject: Project = {
+    if (project != null) project else DecompilerUtil.obtainProject
+  }
+
+  def getScope = scope
+
+  def this(components: Seq[ScType], project: Project, scope: GlobalSearchScope) = {
+    this(components)
+    this.project = project
+    this.scope = scope
+  }
+
+  def resolveTupleTrait: Option[ScParameterizedType] = resolveTupleTrait(getProject)
+
+  def resolveTupleTrait(project: Project): Option[ScParameterizedType] = {
+    def findClass(fullyQualifiedName: String) : Option[PsiClass] = {
+        val psiFacade = JavaPsiFacade.getInstance(project)
+        Option(psiFacade.findClass(tupleTraitName, getScope))
+    }
+    findClass(tupleTraitName) match {
+      case Some(t: ScClass) => {
+        val typeParams = components.toList
+        Some(ScParameterizedType(ScDesignatorType(t), typeParams))
+      }
+      case _ => None
+    }
+  }
+
+  override def equivInner(r: ScType, uSubst: ScUndefinedSubstitutor, falseUndef: Boolean): (Boolean, ScUndefinedSubstitutor) = {
+    var undefinedSubst = uSubst
+    r match {
+      case ScTupleType(c1) if c1.length == components.length => {
+        val iter1 = components.iterator
+        val iter2 = c1.iterator
+        while (iter1.hasNext) {
+          val t = Equivalence.equivInner(iter1.next, iter2.next, undefinedSubst, falseUndef)
+          if (!t._1) return (false, undefinedSubst)
+          undefinedSubst = t._2
+        }
+        (true, undefinedSubst)
+      }
+      case p: ScParameterizedType => {
+        p.getTupleType match {
+          case Some(tuple) => this.equivInner(tuple, undefinedSubst, falseUndef)
+          case _ => (false, undefinedSubst)
+        }
+      }
+      case _ => (false, undefinedSubst)
+    }
+  }
+
+  override def removeAbstracts = ScTupleType(components.map(_.removeAbstracts))
+
+  override def updateThisType(place: PsiElement) = ScTupleType(components.map(_.updateThisType(place)))
+
+  override def updateThisType(tp: ScType) = ScTupleType(components.map(_.updateThisType(tp)))
+
+  private def tupleTraitName = "scala.Tuple" + components.length
+}
