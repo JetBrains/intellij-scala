fc7139256346e5294d96880a60065ba0d8de668d
jzaugg <jzaugg@gmail.com>
2011-03-10 00:09:33 +0100

    SCL-2871 For package objects (defined with the package keyword, not with object `package`), the expected packaging name is that of the parent directory.

--- src/org/jetbrains/plugins/scala/codeInspection/packageNameInspection/PackageNameInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/packageNameInspection/PackageNameInspection.scala
@@ -10,6 +10,7 @@ import com.intellij.codeInspection._
 import java.lang.String
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi._
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScObject, ScClass}
 
 /**
  * User: Alexander Podkhalyuzin
@@ -25,7 +26,7 @@ class PackageNameInspection extends LocalInspectionTool {
 
   override def isEnabledByDefault: Boolean = true
 
-  override def getStaticDescription: String = "Inspection for files with package statement which not correspond to package structure"
+  override def getStaticDescription: String = "Inspection for files with package statement which does not correspond to package structure"
 
   override def getID: String = "PackageName"
 
@@ -34,43 +35,57 @@ class PackageNameInspection extends LocalInspectionTool {
       case file: ScalaFile => {
         if (file.isScriptFile()) return null
         if (file.getClasses.length == 0) return null
+
         val dir = file.getContainingDirectory
         if (dir == null) return null
         val pack = JavaDirectoryService.getInstance().getPackage(dir)
         if (pack == null) return null
+
         val packName = file.packageName
-        def getRange: TextRange = {
-          new TextRange(0, file.getText.indexOf('\n') match {
-            case x if x < 0 => file.getText.length
-            case y => y
-          })
+        val range: TextRange = getPackagingRange(file)
+
+        def problemDescriptor(buffer: Seq[LocalQuickFix]): ProblemDescriptor =
+          manager.createProblemDescriptor(file, range,
+            "Package names doesn't correspond to directories structure, this may cause " +
+                    "problems with resolve to classes from this file", ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
+            isOnTheFly, buffer: _*)
+
+        val expectedPackageName = file.getClasses.head match {
+          case obj: ScObject if obj.hasPackageKeyword =>
+            Option(pack.getParentPackage).map(_.getQualifiedName).getOrElse("")
+          case _ =>
+            pack.getQualifiedName
         }
-        val range: TextRange = file.getPackagings.toList match {
-          case Nil => getRange
-          case h :: t => h.reference match {
-            case Some(ref) => ref.getTextRange
-            case _ => getRange
-          }
-        }
+
         if (packName == null) {
-          return Array[ProblemDescriptor](manager.createProblemDescriptor(file, range,
-            "Package names doesn't correspond to directories structure, this may cause " +
-                    "problems with resolve to classes from this file", ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-            isOnTheFly, new EnablePerformanceProblemsQuickFix(file.getProject)))
-        } else if (packName != pack.getQualifiedName) {
-          val buffer = new ArrayBuffer[LocalQuickFix]
-          buffer += new ScalaRenamePackageQuickFix(file, pack.getQualifiedName)
-          buffer += new ScalaMoveToPackageQuickFix(file, packName)
-          buffer += new EnablePerformanceProblemsQuickFix(file.getProject)
-          return Array[ProblemDescriptor](manager.createProblemDescriptor(file, range,
-            "Package names doesn't correspond to directories structure, this may cause " +
-                    "problems with resolve to classes from this file", ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-            isOnTheFly, buffer: _*))
-        } else {
-          return null
-        }
+          val fixes = Seq(new EnablePerformanceProblemsQuickFix(file.getProject))
+          Array(problemDescriptor(fixes))
+        } else if (packName != expectedPackageName) {
+          val fixes = Seq(
+            new ScalaRenamePackageQuickFix(file, expectedPackageName),
+            new ScalaMoveToPackageQuickFix(file, packName),
+            new EnablePerformanceProblemsQuickFix(file.getProject))
+          
+          Array(problemDescriptor(fixes))
+        } else null
       }
-      case _ => return null
+      case _ => null
+    }
+  }
+
+  private def getPackagingRange(file: ScalaFile): TextRange = {
+    def getRange: TextRange = {
+      new TextRange(0, file.getText.indexOf('\n') match {
+        case x if x < 0 => file.getText.length
+        case y => y
+      })
+    }
+    file.getPackagings.toList match {
+      case Nil => getRange
+      case h :: t => h.reference match {
+        case Some(ref) => ref.getTextRange
+        case _ => getRange
+      }
     }
   }
 }
