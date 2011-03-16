7426b604a11359a03e7667535b29dec2c408c0f9
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-03-16 18:37:05 +0300

    Copy/paste imports for unqualified types and type instantiations

--- src/org/jetbrains/plugins/scala/conversion/copy/ScalaCopyPastePostProcessor.scala
+++ src/org/jetbrains/plugins/scala/conversion/copy/ScalaCopyPastePostProcessor.scala
@@ -4,16 +4,16 @@ import com.intellij.openapi.editor.{RangeMarker, Editor}
 import java.lang.Boolean
 import java.awt.datatransfer.Transferable
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import com.intellij.openapi.ui.Messages
 import com.intellij.codeInsight.daemon.impl.CollectHighlightsUtil
 import collection.JavaConversions._
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
 import org.jetbrains.plugins.scala.extensions._
 import com.intellij.codeInsight.editorActions.CopyPastePostProcessor
 import com.intellij.openapi.project.{DumbService, Project}
 import com.intellij.openapi.util.{TextRange, Ref}
 import com.intellij.psi._
 import org.jetbrains.plugins.scala.conversion.copy.ScalaData.ReferenceData
+import org.jetbrains.plugins.scala.annotator.intention.ScalaImportClassFix
+import org.jetbrains.plugins.scala.lang.psi.api.base.{ScPrimaryConstructor, ScReferenceElement}
 
 /**
  * Pavel Fatin
@@ -31,7 +31,14 @@ class ScalaCopyPastePostProcessor extends CopyPastePostProcessor[ScalaData] {
         reference <- element.asOptionOf(classOf[ScReferenceElement]) if reference.qualifier.isEmpty;
         target <- reference.resolve().toOption if target.getContainingFile != file) {
       target match {
-        case t: PsiClass => refs ::= createReferenceData(element, startOffset, t.getQualifiedName)
+        case e: PsiClass =>
+          refs ::= createReferenceData(element, startOffset, e.getQualifiedName)
+        case e: ScPrimaryConstructor =>
+          e.getParent match {
+            case e: PsiClass =>
+              refs ::= createReferenceData(element, startOffset, e.getQualifiedName)
+            case _ =>
+          }
         case _ =>
       }
     }
@@ -46,47 +53,41 @@ class ScalaCopyPastePostProcessor extends CopyPastePostProcessor[ScalaData] {
   }
 
   def extractTransferableData(content: Transferable) = {
-    if(content.isDataFlavorSupported(ReferenceData.getDataFlavor))
-      content.getTransferData(ReferenceData.getDataFlavor).asInstanceOf[ScalaData]
-    else
-      null
+    content.isDataFlavorSupported(ReferenceData.getDataFlavor)
+            .ifTrue(content.getTransferData(ReferenceData.getDataFlavor).asInstanceOf[ScalaData])
+            .orNull
   }
 
   def processTransferableData(project: Project, editor: Editor, bounds: RangeMarker,
                               caretColumn: Int, indented: Ref[Boolean], value: ScalaData) {
     if (DumbService.getInstance(project).isDumb) return
 
-    val document = editor.getDocument
-    val file = PsiDocumentManager.getInstance(project).getPsiFile(document)
+    val file = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument)
 
     if (!file.isInstanceOf[ScalaFile]) return
 
     PsiDocumentManager.getInstance(project).commitAllDocuments()
 
-    val data = value.getData
-    val refs = findReferencesIn(file, bounds, data)
+    val refs = findReferencesIn(file, bounds, value.getData)
 
-    refs.foreach { it =>
+    val facade = JavaPsiFacade.getInstance(file.getProject)
 
+    for((data, Some(ref)) <- refs if ref.resolve() == null;
+        refClass <- Option(facade.findClass(data.qClassName, file.getResolveScope));
+        holder = ScalaImportClassFix.getImportHolder(ref, file.getProject)) {
+      inWriteAction {
+        holder.addImportForClass(refClass, ref)
+      }
     }
   }
 
-  private def findReferencesIn(file: PsiFile, bounds: RangeMarker,
-                               datas: Array[ReferenceData]): Seq[ScReferenceElement] = {
-    val manager = file.getManager
-    val facade = JavaPsiFacade.getInstance(manager.getProject)
-
-    var refs = List[ScReferenceElement]()
-
-    for(data <- datas; refClass <- Option(facade.findClass(data.qClassName, file.getResolveScope))) {
-      val range = new TextRange(data.startOffset, data.endOffset).shiftRight(bounds.getStartOffset)
-      val ref = file.findElementAt(range.getStartOffset)
+  private def findReferencesIn(file: PsiFile, bounds: RangeMarker, datas: Array[ReferenceData]) = {
+    for(data <- datas;
+        range = new TextRange(data.startOffset, data.endOffset).shiftRight(bounds.getStartOffset);
+        ref = file.findElementAt(range.getStartOffset)) yield
       ref match {
-        case Parent(expr: ScReferenceElement) if expr.getTextRange == range => refs ::= expr
-        case _ =>
+        case Parent(expr: ScReferenceElement) if expr.getTextRange == range => (data, Some(expr))
+        case _ => (data, None)
       }
-    }
-
-    refs.reverse
   }
 }
