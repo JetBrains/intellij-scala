870d0d2320116ce55a8cdec3967b33e900798191
jzaugg <jzaugg@gmail.com>
2011-04-05 00:19:25 +0200

    Add a few more usage type classifications.
    Not comprehensive yet, but it's a start.

--- src/org/jetbrains/plugins/scala/lang/findUsages/ScalaUsageTypeProvider.scala
+++ src/org/jetbrains/plugins/scala/lang/findUsages/ScalaUsageTypeProvider.scala
@@ -6,16 +6,67 @@ import com.intellij.psi.PsiElement
 import psi.api.toplevel.imports.ScImportExpr
 import com.intellij.usages.impl.rules.{UsageType, UsageTypeProvider}
 import org.jetbrains.plugins.scala.extensions._
+import com.intellij.psi.util.PsiTreeUtil
+import psi.api.expr.ScNewTemplateDefinition
+import psi.api.toplevel.typedef.ScTemplateDefinition
+import psi.api.statements.{ScVariable, ScValue, ScPatternDefinition, ScFunction}
+import psi.api.toplevel.templates.{ScTemplateBody, ScTemplateParents}
+import psi.api.statements.params.ScParameter
 
 final class ScalaUsageTypeProvider extends UsageTypeProvider {
-  def getUsageType(element: PsiElement) = {
-    element.containingScalaFile match {
-      case Some(_) =>
-        element match {
-        case x if x.parentsInFile.findByType(classOf[ScImportExpr]).isDefined => UsageType.CLASS_IMPORT
-        case _ => null
+  def getUsageType(element: PsiElement): UsageType = {
+    import PsiTreeUtil._
+    def parentOfType[T <: PsiElement : Manifest]: Option[T] = Option(getParentOfType[T](element, classManifest[T].erasure.asInstanceOf[Class[T]]))
+    if (element.containingScalaFile.isDefined) {
+      for (ie <- parentOfType[ScImportExpr]) {
+        return UsageType.CLASS_IMPORT
       }
-      case None => null
+
+      for (ie <- parentOfType[ScNewTemplateDefinition];
+           tp <- ie.extendsBlock.templateParents
+           if isAncestor(tp, element, false)) {
+        return UsageType.CLASS_NEW_OPERATOR
+      }
+
+      for (ie <- parentOfType[ScTemplateDefinition];
+           tp <- ie.extendsBlock.templateParents
+           if isAncestor(tp, element, false)) {
+        return UsageType.CLASS_EXTENDS_IMPLEMENTS_LIST
+      }
+
+      for (fun <- parentOfType[ScFunction];
+           tp <- fun.returnTypeElement
+           if isAncestor(tp, element, false)) {
+        return UsageType.CLASS_METHOD_RETURN_TYPE
+      }
+
+      for (value <- parentOfType[ScValue];
+           tp <- value.typeElement
+           if isAncestor(tp, element, false)) {
+        value.getContext match {
+          case _: ScTemplateBody => return UsageType.CLASS_FIELD_DECLARATION
+          case _ => return UsageType.CLASS_LOCAL_VAR_DECLARATION
+        }
+      }
+
+      for (variable <- parentOfType[ScVariable];
+           tp <- variable.typeElement
+           if isAncestor(tp, element, false)) {
+        variable.getContext match {
+          case _: ScTemplateBody => return UsageType.CLASS_FIELD_DECLARATION
+          case _ => return UsageType.CLASS_LOCAL_VAR_DECLARATION
+        }
+      }
+
+      for (param <- parentOfType[ScParameter];
+           tp <- param.typeElement
+           if isAncestor(tp, element, false)) {
+        return UsageType.CLASS_METHOD_PARAMETER_DECLARATION
+      }
+
+      // TODO more of these, including Scala specific: case class/object, pattern match, type ascription, ...
     }
+
+    return null
   }
 }
