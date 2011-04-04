fdb9fd8e8a1958085669c868829390fb2179e6c1
jzaugg <jzaugg@gmail.com>
2011-04-04 23:01:04 +0200

    SCL-3018 Support rename of real parameter from the corresponding synthetic parameter.

---
+++ src/org/jetbrains/plugins/scala/lang/refactoring/rename/RenameScalaSyntheticParamProcessor.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala
+package lang
+package refactoring
+package rename
+
+import com.intellij.refactoring.rename.RenamePsiElementProcessor
+import com.intellij.psi.PsiElement
+import psi.api.statements.params.ScParameter
+import psi.ScalaPsiUtil
+import com.intellij.openapi.editor.Editor
+
+class RenameScalaSyntheticParamProcessor extends RenamePsiElementProcessor {
+  def canProcessElement(element: PsiElement): Boolean = realParamForSyntheticParam(element).isDefined
+
+  override def substituteElementToRename(element: PsiElement, editor: Editor): PsiElement = realParamForSyntheticParam(element).orNull
+
+  private def realParamForSyntheticParam(element: PsiElement) = element match {
+    case x: ScParameter => ScalaPsiUtil.parameterForSyntheticParameter(x)
+    case _ => None
+  }
+}
