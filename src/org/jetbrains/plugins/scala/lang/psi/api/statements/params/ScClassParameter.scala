bb464b483157bd62001fbb1d007740a80dccbc22
jzaugg <jzaugg@gmail.com>
2011-04-10 20:27:37 +0200

    SCL-3055 Improve projection of Scala vals and vars to Java.
    Case class parameters, vals, and vars, can be accessed with id().
    Class Parameters are not directly exposed to Java.
    Vals/Vars are no longer exposed as Java fields.
    Bean Property handling fixed, and duplicated code removed.

--- src/org/jetbrains/plugins/scala/lang/psi/api/statements/params/ScClassParameter.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/statements/params/ScClassParameter.scala
@@ -8,9 +8,9 @@ package params
 import base.ScModifierList
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import toplevel.ScModifierListOwner
-import toplevel.typedef.ScMember
+import toplevel.typedef.{ScClass, ScMember}
 
-/** 
+/**
 * @author Alexander Podkhalyuzin
 * Date: 22.02.2008
 */
@@ -18,4 +18,16 @@ import toplevel.typedef.ScMember
 trait ScClassParameter extends ScParameter with ScModifierListOwner with ScMember {
   def isVal() : Boolean
   def isVar() : Boolean
+
+  /** Is the parmameter is explicitly marked as a val or a var; or a case class parameter that is automatically a val. */
+  def isEffectiveVal = isVal || isVar || isCaseClassVal
+
+  /** Is the parameter automatically a val, due to it's position in a case class parameter list */
+  def isCaseClassVal = getContainingClass match {
+    case c: ScClass if c.isCase =>
+      val inPrimaryParamClause = c.allClauses.take(1).exists(pc => !pc.isImplicit && pc.parameters.contains(this))
+      val hasExplicitModifier = Option(getModifierList).exists(_.hasExplicitModifiers)
+      inPrimaryParamClause && !hasExplicitModifier
+    case _ => false
+  }
 }
---
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/BeanProperty.scala
@@ -0,0 +1,64 @@
+package org.jetbrains.plugins.scala
+package lang
+package psi
+package impl
+package toplevel
+package typedef
+
+import com.intellij.psi.scope.PsiScopeProcessor
+import lang.psi.api.statements.params.ScClassParameter
+import lang.psi.api.statements.{ScVariable, ScValue, ScAnnotationsHolder}
+import lang.psi.api.toplevel.ScTypedDefinition
+import lang.psi.fake.FakePsiMethod
+import lang.psi.types.nonvalue.Parameter
+import lang.psi.types.nonvalue.Parameter._
+import lang.psi.types.result.TypingContext
+import com.intellij.openapi.util.text.StringUtil
+import lang.psi.types.{Any, ScType, Unit}
+import com.intellij.psi.{PsiMethod, ResolveState, PsiElement}
+
+
+object BeanProperty {
+  def processBeanPropertyDeclarations(annotated: ScAnnotationsHolder, context: PsiElement,
+                                      processor: PsiScopeProcessor, t: ScTypedDefinition, state: ResolveState): Boolean = {
+    processBeanPropertyDeclarationsInternal(annotated, context, t) { element =>
+      processor.execute(element, state)
+    }
+  }
+
+  def processBeanPropertyDeclarationsInternal(annotated: ScAnnotationsHolder, context: PsiElement, t: ScTypedDefinition)
+                                             (callback: (PsiMethod => Boolean)): Boolean = {
+    implicit def arr2arr(a: Array[ScType]): Array[Parameter] = a.map(Parameter("", _, false, false, false))
+    def has(annotationName: String): Boolean = annotated.hasAnnotation(annotationName).isDefined
+    val isBeanProperty = has("scala.reflect.BeanProperty")
+    val isBooleanBeanProperty = has("scala.reflect.BooleanBeanProperty")
+
+    if (!(isBeanProperty || isBooleanBeanProperty)) return true
+
+    val getterName: String = {
+      val prefix = if (isBeanProperty) "get" else "is"
+      prefix + StringUtil.capitalize(t.getName)
+    }
+    val setterName = "set" + StringUtil.capitalize(t.getName)
+    def tType = t.getType(TypingContext.empty).getOrElse(Any)
+
+    context match {
+      case value: ScValue => {
+        if (!callback(new FakePsiMethod(t, getterName, Array.empty, tType, value.hasModifierProperty _))) return false
+      }
+      case variable: ScVariable => {
+        if (!callback(new FakePsiMethod(t, getterName, Array.empty, tType, variable.hasModifierProperty _))) return false
+        if (!callback(new FakePsiMethod(t, setterName, Array[ScType](tType), Unit, variable.hasModifierProperty _))) return false
+      }
+      case param: ScClassParameter if param.isVal => {
+        if (!callback(new FakePsiMethod(t, getterName, Array.empty, tType, param.hasModifierProperty _))) return false
+      }
+      case param: ScClassParameter if param.isVar => {
+        if (!callback(new FakePsiMethod(t, getterName, Array.empty, tType, param.hasModifierProperty _))) return false
+        if (!callback(new FakePsiMethod(t, setterName, Array[ScType](tType), Unit, param.hasModifierProperty _))) return false
+      }
+      case _ =>
+    }
+    true
+  }
+}
