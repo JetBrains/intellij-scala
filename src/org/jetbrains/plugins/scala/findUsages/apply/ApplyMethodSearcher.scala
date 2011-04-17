25e0258a3cbc024cd27e47bb771fe07197fb14df
jzaugg <jzaugg@gmail.com>
2011-04-17 20:32:38 +0200

    SCL-2697 First cut at apply/unapply find usages.
    Only works in the simplest cases, if the method is in an object, and that object appears at the call-site of apply/unapply.
    
    object A {
      def apply(a: Any) = ()
      def unapply(a: Any) = false
    }
    
    A(0)
    
    0 match {
      case A() =>
    }
    
    The next step seems like a brute-force search. Maybe in the meantime we should issue a warning about find-usages or rename of these methods.

---
+++ src/org/jetbrains/plugins/scala/findUsages/apply/ApplyMethodSearcher.scala
@@ -0,0 +1,42 @@
+package org.jetbrains.plugins.scala
+package findUsages
+package apply
+
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.util.{Processor, QueryExecutor}
+import com.intellij.psi._
+import lang.psi.api.statements.ScFunctionDefinition
+import lang.psi.api.toplevel.typedef.ScObject
+import lang.psi.api.expr.ScMethodCall
+import lang.resolve.{ScalaResolveResult, ResolvableReferenceExpression}
+
+
+class ApplyMethodSearcher extends QueryExecutor[PsiReference, ReferencesSearch.SearchParameters] {
+  def execute(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor[PsiReference]): Boolean = {
+    val scope = queryParameters.getScope
+    val element = queryParameters.getElementToSearch
+    val ignoreAccess = queryParameters.isIgnoreAccessScope
+    element match {
+      case fun: ScFunctionDefinition if fun.name() == "apply" =>
+        val processor = new Processor[PsiReference] {
+          def process(ref: PsiReference): Boolean = {
+            (ref, ref.getElement.getContext) match {
+              case (sref: ResolvableReferenceExpression, x: ScMethodCall) if x.getInvokedExpr == ref.getElement =>
+                sref.bind() match {
+                  case Some(ScalaResolveResult(`fun`, _)) => consumer.process(ref)
+                  case _ => true
+                }
+              case _ => true
+            }
+          }
+        }
+        fun.containingClass match {
+          case Some(obj: ScObject) => ReferencesSearch.search(obj, scope, ignoreAccess).forEach(processor)
+          case _ =>
+        }
+        // TODO Check every ScMethodCall? Sounds expensive!
+      case _ =>
+    }
+    true
+  }
+}
