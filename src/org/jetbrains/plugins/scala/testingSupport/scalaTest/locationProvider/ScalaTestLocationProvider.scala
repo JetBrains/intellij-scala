8e1499195004f00277baea7a0123ca322f77f000
jzaugg <jzaugg@gmail.com>
2011-04-22 01:54:54 +0200

    SCL-1791 Support navigation to individual examples for Specs2.
    The Notifier interface now contains file name and line number.

--- src/org/jetbrains/plugins/scala/testingSupport/scalaTest/locationProvider/ScalaTestLocationProvider.scala
+++ src/org/jetbrains/plugins/scala/testingSupport/scalaTest/locationProvider/ScalaTestLocationProvider.scala
@@ -5,10 +5,11 @@ package scalaTest.locationProvider
 import com.intellij.execution.{Location, PsiLocation}
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.{JavaPsiFacade, PsiElement, PsiClass}
 import com.intellij.testIntegration.TestLocationProvider
 import java.lang.String
 import java.util.{ArrayList, List}
+import com.intellij.openapi.editor.Document
+import com.intellij.psi._
 
 /**
  * User: Alexander Podkhalyuzin
@@ -16,16 +17,66 @@ import java.util.{ArrayList, List}
  */
 
 class ScalaTestLocationProvider extends TestLocationProvider {
+  private val SpecsHintPattern = """(\S+)\?filelocation=(.+):(.+)""".r
+
   def getLocation(protocolId: String, locationData: String, project: Project): List[Location[_ <: PsiElement]] = {
     protocolId match {
-      case "scala" => {
-        val res = new ArrayList[Location[_ <: PsiElement]]()
-        val facade = JavaPsiFacade.getInstance(project)
-        val clazz: PsiClass = facade.findClass(locationData, GlobalSearchScope.allScope(project))
-        if (clazz != null) res.add(PsiLocation.fromPsiElement[PsiClass](project, clazz))
-        res
-      }
+      case "scala" =>
+        locationData match {
+          case SpecsHintPattern(className, fileName, lineNumber) =>
+            val facade = JavaPsiFacade.getInstance(project)
+            val clazzes: Array[PsiClass] = facade.findClasses(className, GlobalSearchScope.allScope(project))
+            val found = clazzes.find(c => Option(c.getContainingFile).map(_.getName == fileName).getOrElse(false))
+
+            found match {
+              case Some(file) =>
+                val res = new ArrayList[Location[_ <: PsiElement]]()
+                res.add(createLocationFor(project, file.getContainingFile, lineNumber.toInt))
+                res
+              case _ => searchForClassByUnqualifiedName(project, className)
+            }
+          case x => searchForClassByUnqualifiedName(project, locationData)
+        }
       case _ => new ArrayList[Location[_ <: PsiElement]]()
     }
+  }
+
+  private def searchForClassByUnqualifiedName(project: Project, locationData: String): ArrayList[Location[_ <: PsiElement]] = {
+    val res = new ArrayList[Location[_ <: PsiElement]]()
+    val facade = JavaPsiFacade.getInstance(project)
+    val clazz: PsiClass = facade.findClass(locationData, GlobalSearchScope.allScope(project))
+    if (clazz != null) res.add(PsiLocation.fromPsiElement[PsiClass](project, clazz))
+    res
+  }
+
+  private def createLocationFor(project: Project, psiFile: PsiFile , lineNum: Int): Location[_ <: PsiElement] = {
+    assert(lineNum > 0)
+    val doc: Document = PsiDocumentManager.getInstance(project).getDocument(psiFile)
+    if (doc == null) {
+      return null
+    }
+    val lineCount: Int = doc.getLineCount
+    var lineStartOffset: Int = 0
+    var endOffset: Int = 0
+    if (lineNum <= lineCount) {
+      lineStartOffset = doc.getLineStartOffset(lineNum - 1)
+      endOffset = doc.getLineEndOffset(lineNum - 1)
+    }
+    else {
+      lineStartOffset = 0
+      endOffset = doc.getTextLength
+    }
+    var offset: Int = lineStartOffset
+    var elementAtLine: PsiElement = null
+    var found = false
+    while (offset <= endOffset && !found) {
+      elementAtLine = psiFile.findElementAt(offset)
+      if (!(elementAtLine.isInstanceOf[PsiWhiteSpace])) {
+        found = true
+      }
+      var length: Int = elementAtLine.getTextLength
+      offset += (if (length > 1) length - 1 else 1)
+    }
+    return PsiLocation.fromPsiElement(project, if (elementAtLine != null) elementAtLine else psiFile)
   }
 }
