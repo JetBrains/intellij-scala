518ef5a4a5cdff4994334cf52daa91cd134fc15f
jzaugg <jzaugg@gmail.com>
2011-04-22 16:27:06 +0200

    SCL-3068 Inspection for postfix method call.

---
+++ src/org/jetbrains/plugins/scala/codeInspection/postfix/PostfixMethodCall.scala
@@ -0,0 +1,47 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package postfix
+
+import com.intellij.psi.PsiElement
+import org.intellij.lang.annotations.Language
+import lang.lexer.ScalaTokenTypes
+import lang.psi.api.expr.{ScParenthesisedExpr, ScPostfixExpr}
+import com.intellij.openapi.project.Project
+import com.intellij.codeInspection.{ProblemDescriptor, ProblemsHolder}
+import lang.psi.impl.ScalaPsiElementFactory
+import lang.psi.api.base.ScLiteral
+
+class PostfixMethodCall extends AbstractInspection("UseOfPostfixMethodCall", "Use of postfix method call"){
+  @Language("HTML")
+  val description =
+"""Postfix method invokation, <code>f a</code>, can interfere with semicolon inference.
+It is <a href="http://twitter.com/#!/odersky/status/49882758968905728">recommended</a> to use <code>f.a</code> instead."""
+
+  def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
+    case pexpr: ScPostfixExpr if !safe(pexpr) =>
+      holder.registerProblem(pexpr, getDisplayName, new AddDotFix(pexpr))
+  }
+
+  private def safe(pexpr: ScPostfixExpr): Boolean = {
+    pexpr.getContext match {
+      case _: ScParenthesisedExpr => true
+      case _ =>
+        val followedBySemicolon = pexpr.getNextSibling.getNode.getElementType == ScalaTokenTypes.tSEMICOLON
+        followedBySemicolon
+    }
+  }
+}
+
+class AddDotFix(pexpr: ScPostfixExpr) extends AbstractFix("Add dot to method call", pexpr) {
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+    val operand = pexpr.operand
+    val needParens = operand match {
+      case lit: ScLiteral if lit.getText.endsWith(".") => true
+      case _ => false // TODO others?
+    }
+    val operandText = if (needParens) "(" + operand.getText + ")" else operand.getText
+    val call = operandText + "." + pexpr.operation.getText
+    val exp = ScalaPsiElementFactory.createExpressionFromText(call, pexpr.getManager)
+    pexpr.replace(exp)
+  }
+}
