d260334a722dd03c8aa6f00e706ab2da508e531c
jzaugg <jzaugg@gmail.com>
2011-04-22 16:48:18 +0200

    SCL-3113 Inspection and quick fix for floating point literals ending in a decimal point.

---
+++ src/org/jetbrains/plugins/scala/codeInspection/literal/FloatLiteralEndingWithDecimalPoint.scala
@@ -0,0 +1,42 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package literal
+
+import com.intellij.psi.PsiElement
+import org.intellij.lang.annotations.Language
+import lang.psi.api.base.ScLiteral
+import com.intellij.openapi.project.Project
+import com.intellij.codeInspection.{ProblemDescriptor, ProblemsHolder}
+import lang.psi.impl.ScalaPsiElementFactory
+
+class FloatLiteralEndingWithDecimalPoint extends AbstractInspection("FloatLiteralEndingWithDecimalPoint", "Floating point literal ending with '.'"){
+  @Language("HTML")
+  val description: String =
+"""It is recommended to avoid floating point literals ending with a decimal point ('.'). This prevents confusion with method call syntax."""
+
+  def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
+    case lit: ScLiteral if lit.getText.endsWith(".") =>
+      holder.registerProblem(lit, getDisplayName, new MakeDoubleFix(lit), new MakeFloatFix(lit), new AddZeroAfterDecimalPoint(lit))
+  }
+}
+
+class MakeDoubleFix(lit: ScLiteral) extends AbstractFix("Convert to %s".format(lit.getText.dropRight(1) + "d"), lit) {
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+    val exp = ScalaPsiElementFactory.createExpressionFromText(lit.getText.dropRight(1) + "d", lit.getManager)
+    lit.replace(exp)
+  }
+}
+
+class MakeFloatFix(lit: ScLiteral) extends AbstractFix("Convert to %s".format(lit.getText.dropRight(1) + "f"), lit) {
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+    val exp = ScalaPsiElementFactory.createExpressionFromText(lit.getText.dropRight(1) + "f", lit.getManager)
+    lit.replace(exp)
+  }
+}
+
+class AddZeroAfterDecimalPoint(lit: ScLiteral) extends AbstractFix("Convert to %s".format(lit.getText + "0"), lit) {
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+    val exp = ScalaPsiElementFactory.createExpressionFromText(lit.getText + "0", lit.getManager)
+    lit.replace(exp)
+  }
+}
