8277d513b0d99c6825ef70fa1b1c7239528d00b9
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-04-26 14:49:07 +0400

    Plugin since-build updated. Added working directory setting for Scala Console

--- src/org/jetbrains/plugins/scala/console/ScalaScriptConsoleRunConfigurationForm.java
+++ src/org/jetbrains/plugins/scala/console/ScalaScriptConsoleRunConfigurationForm.java
@@ -1,5 +1,8 @@
 package org.jetbrains.plugins.scala.console;
 
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.RawCommandLineEditor;
 import com.intellij.openapi.project.Project;
 
@@ -14,6 +17,7 @@ public class ScalaScriptConsoleRunConfigurationForm {
   private RawCommandLineEditor javaOptionsEditor;
   private JPanel myPanel;
   private RawCommandLineEditor consoleArgsEditor;
+  private TextFieldWithBrowseButton workingDirectoryField;
   private Project myProject;
   private ScalaScriptConsoleRunConfiguration myConfiguration;
 
@@ -24,6 +28,10 @@ public class ScalaScriptConsoleRunConfigurationForm {
     javaOptionsEditor.setDialogCaption("VM options editor");
     consoleArgsEditor.setName("Console arguments");
     consoleArgsEditor.setDialogCaption("Console arguments editor");
+    addFileChooser("Choose Working Directory", workingDirectoryField, project);
+    VirtualFile baseDir = project.getBaseDir();
+    String path = baseDir != null ? baseDir.getPath() : "";
+    workingDirectoryField.setText(path);
   }
 
   public JPanel getPanel() {
@@ -41,6 +49,7 @@ public class ScalaScriptConsoleRunConfigurationForm {
   public void apply(ScalaScriptConsoleRunConfiguration configuration) {
     setJavaOptions(configuration.getJavaOptions());
     setConsoleArgs(configuration.getConsoleArgs());
+    setWorkingDirectory(configuration.getWorkingDirectory());
   }
 
   public String getConsoleArgs() {
@@ -49,5 +58,27 @@ public class ScalaScriptConsoleRunConfigurationForm {
 
   public void setConsoleArgs(String consoleArgs) {
     this.consoleArgsEditor.setText(consoleArgs);
+  }
+
+  public String getWorkingDirectory() {
+    return workingDirectoryField.getText();
+  }
+
+  public void setWorkingDirectory(String s) {
+    workingDirectoryField.setText(s);
+  }
+
+  private FileChooserDescriptor addFileChooser(final String title,
+                                               final TextFieldWithBrowseButton textField,
+                                               final Project project) {
+    final FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(false, true, false, false, false, false) {
+      @Override
+      public boolean isFileVisible(VirtualFile file, boolean showHiddenFiles) {
+        return super.isFileVisible(file, showHiddenFiles) && file.isDirectory();
+      }
+    };
+    fileChooserDescriptor.setTitle(title);
+    textField.addBrowseFolderListener(title, null, project, fileChooserDescriptor);
+    return fileChooserDescriptor;
   }
 }
