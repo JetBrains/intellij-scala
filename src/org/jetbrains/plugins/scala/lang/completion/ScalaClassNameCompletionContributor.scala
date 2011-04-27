b8d1ce0082385937bd9e5c4ba859fc0a34e397b3
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-04-27 14:20:55 +0400

    Class name completion enabled, if prefix starts from capital letter.

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaClassNameCompletionContributor.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaClassNameCompletionContributor.scala
@@ -14,44 +14,67 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScObject, ScTr
 import org.jetbrains.plugins.scala.lang.resolve.{ScalaResolveResult, ResolveUtils}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScConstructorPattern
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.Computable
 
 class ScalaClassNameCompletionContributor extends CompletionContributor {
+  def completeClassName(parameters: CompletionParameters, result: CompletionResultSet): Boolean = {
+    val insertedElement: PsiElement = parameters.getPosition
+    if (!insertedElement.getContainingFile.isInstanceOf[ScalaFile]) return true
+    val lookingForAnnotations: Boolean = psiElement.afterLeaf("@").accepts(insertedElement)
+    val isInImport = ScalaPsiUtil.getParentOfType(insertedElement, classOf[ScImportStmt]) != null
+    val refElement = ScalaPsiUtil.getParentOfType(insertedElement, classOf[ScStableCodeReferenceElement])
+    val onlyClasses = refElement != null && (refElement.getContext match {
+      case _: ScConstructorPattern => false
+      case _ => true
+    })
+    AllClassesGetter.processJavaClasses(parameters, result.getPrefixMatcher, parameters.getInvocationCount <= 1,
+      new Consumer[PsiClass] {
+        def consume(psiClass: PsiClass) {
+          def addClass(psiClass: PsiClass) {
+            val isExcluded: Boolean = ApplicationManager.getApplication.runReadAction(new Computable[Boolean] {
+              def compute: Boolean = {
+                return JavaCompletionUtil.isInExcludedPackage(psiClass)
+              }
+            }).booleanValue
+            if (isExcluded) return
+            if (lookingForAnnotations && !psiClass.isAnnotationType) return
+            psiClass match {
+              case _: ScClass | _: ScTrait if !isInImport && !onlyClasses => return
+              case _: ScObject if !isInImport && onlyClasses => return
+              case _ =>
+            }
+            for {
+              (el, _, _) <- ResolveUtils.getLookupElement(new ScalaResolveResult(psiClass),
+                isClassName = true, isInImport = isInImport)
+            } result.addElement(el)
+          }
+          //todo: filter according to position
+          ScalaPsiUtil.getCompanionModule(psiClass) match {
+            case Some(c) => addClass(c)
+            case _ =>
+          }
+          addClass(psiClass)
+        }
+      })
+    false
+  }
+
   extend(CompletionType.CLASS_NAME, psiElement, new CompletionProvider[CompletionParameters] {
     def addCompletions(parameters: CompletionParameters, matchingContext: ProcessingContext,
                        result: CompletionResultSet) {
-      val insertedElement: PsiElement = parameters.getPosition
-      if (!insertedElement.getContainingFile.isInstanceOf[ScalaFile]) return
-      val lookingForAnnotations: Boolean = psiElement.afterLeaf("@").accepts(insertedElement)
-      val isInImport = ScalaPsiUtil.getParentOfType(insertedElement, classOf[ScImportStmt]) != null
-      val refElement = ScalaPsiUtil.getParentOfType(insertedElement, classOf[ScStableCodeReferenceElement])
-      val onlyClasses = refElement != null && (refElement.getContext match {
-        case _: ScConstructorPattern => false
-        case _ => true
-      })
-      AllClassesGetter.processJavaClasses(parameters, result.getPrefixMatcher, parameters.getInvocationCount <= 1,
-        new Consumer[PsiClass] {
-          def consume(psiClass: PsiClass) {
-            def addClass(psiClass: PsiClass) {
-              if (lookingForAnnotations && !psiClass.isAnnotationType) return
-              psiClass match {
-                case _: ScClass | _: ScTrait if !isInImport && !onlyClasses => return
-                case _: ScObject if !isInImport && onlyClasses => return
-                case _ =>
-              }
-              for {
-                (el, _, _) <- ResolveUtils.getLookupElement(new ScalaResolveResult(psiClass),
-                  isClassName = true, isInImport = isInImport)
-              } result.addElement(el)
-            }
-            //todo: filter according to position
-            ScalaPsiUtil.getCompanionModule(psiClass) match {
-              case Some(c) => addClass(c)
-              case _ =>
-            }
-            addClass(psiClass)
-          }
-        })
+      if (completeClassName(parameters, result)) return
       result.stopHere()
+    }
+  })
+
+  extend(CompletionType.BASIC, PlatformPatterns.psiElement(ScalaTokenTypes.tIDENTIFIER), new CompletionProvider[CompletionParameters] {
+    def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+      val prefix = result.getPrefixMatcher.getPrefix
+      if (prefix.length() > 0 && prefix.substring(0, 1).capitalize == prefix.substring(0, 1)) {
+        completeClassName(parameters, result)
+      }
     }
   })
 }
