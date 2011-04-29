6e8133a0a8df0f73336d430d7b8c56ecd5d94d04
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-04-29 17:16:14 +0400

    Convert to block expression / Convert to argument in parenthesis

---
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/argument/ArgumentToBlockExpressionIntention.scala
@@ -0,0 +1,35 @@
+package org.jetbrains.plugins.scala.codeInsight.intention.argument
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScArgumentExprList
+import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+
+/**
+ * Pavel Fatin
+ */
+
+class ArgumentToBlockExpressionIntention extends PsiElementBaseIntentionAction {
+  def getFamilyName = "Argument Conversion"
+
+  override def getText = "Convert to block expression"
+
+  def isAvailable(project: Project, editor: Editor, element: PsiElement) = {
+    element match {
+      case Parent(list: ScArgumentExprList) if list.exprs.size == 1 => true
+      case _ => false
+    }
+  }
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement) {
+    val list = element.getParent.asInstanceOf[ScArgumentExprList]
+    val exp = list.exprs.head
+    val block = ScalaPsiElementFactory.createBlockFromExpr(exp, list.getManager)
+    exp.replace(block)
+    list.getFirstChild.delete()
+    list.getLastChild.delete()
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/argument/BlockExpressionToArgumentIntention.scala
@@ -0,0 +1,40 @@
+package org.jetbrains.plugins.scala.codeInsight.intention.argument
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlockExpr, ScArgumentExprList}
+import com.intellij.psi.{PsiWhiteSpace, PsiElement}
+
+/**
+ * Pavel Fatin
+ */
+
+class BlockExpressionToArgumentIntention extends PsiElementBaseIntentionAction {
+  def getFamilyName = "Argument Conversion"
+
+  override def getText = "Convert to argument in parenthesis"
+
+  def isAvailable(project: Project, editor: Editor, element: PsiElement) = {
+    element match {
+      case Both(Parent(block: ScBlockExpr), Parent(Parent(list: ScArgumentExprList)))
+        if list.exprs.size == 1 && block.caseClauses.isEmpty => true
+      case _ => false
+    }
+  }
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement) {
+    val block = element.getParent.asInstanceOf[ScBlockExpr]
+    val s = block.getText
+    val text = "foo(%s)".format(s.substring(1, s.length - 1).replaceAll("\n", ""))
+    val arguments = ScalaPsiElementFactory.createExpressionFromText(text, block.getManager)
+            .children.findByType(classOf[ScArgumentExprList]).get
+    val replacement = block.getParent.replace(arguments)
+    replacement.getPrevSibling match {
+      case ws: PsiWhiteSpace => ws.delete()
+      case _ =>
+    }
+  }
+}
