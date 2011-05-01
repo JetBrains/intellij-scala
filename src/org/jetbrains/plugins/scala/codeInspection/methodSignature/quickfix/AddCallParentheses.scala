4e2e6fd5be607ffac244b4015cdd12856ef5b100
jzaugg <jzaugg@gmail.com>
2011-05-01 13:25:05 +0200

    SCL-3135

--- src/org/jetbrains/plugins/scala/codeInspection/methodSignature/quickfix/AddCallParentheses.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/quickfix/AddCallParentheses.scala
@@ -2,15 +2,24 @@ package org.jetbrains.plugins.scala.codeInspection.methodSignature.quickfix
 
 import com.intellij.openapi.project.Project
 import com.intellij.codeInspection.ProblemDescriptor
-import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.codeInspection.AbstractFix
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScGenericCall, ScMethodCall, ScReferenceExpression}
 
 /**
  * Pavel Fatin
  */
 
 class AddCallParentheses(e: ScReferenceExpression) extends AbstractFix("Add call parentheses", e) {
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+    if (!e.isValid) return
+    val text = "%s()".format(e.getText)
+    val call = ScalaPsiElementFactory.createExpressionFromText(text, e.getManager)
+    e.replace(call)
+  }
+}
+
+class AddGenericCallParentheses(e: ScGenericCall) extends AbstractFix("Add call parentheses", e) {
   def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
     if (!e.isValid) return
     val text = "%s()".format(e.getText)
