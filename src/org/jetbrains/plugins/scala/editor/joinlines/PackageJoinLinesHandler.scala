2fe7e44b24ee2811a0c73d15227164fa0e898c3f
jzaugg <jzaugg@gmail.com>
2011-05-01 19:56:49 +0200

    SCL-2510 Line join handler for `package a\npackageb` => `package a.b`

---
+++ src/org/jetbrains/plugins/scala/editor/joinlines/PackageJoinLinesHandler.scala
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.scala
+package editor
+package joinlines
+
+import com.intellij.openapi.editor.Document
+import com.intellij.psi._
+import com.intellij.codeInsight.editorActions.{JoinRawLinesHandlerDelegate, JoinLinesHandlerDelegate}
+import lang.psi.api.toplevel.packaging.ScPackaging
+
+/** `package a\npackageb` => `package a.b` */
+class PackageJoinLinesHandler extends JoinRawLinesHandlerDelegate {
+  // Not very robust if there is trailing whitespace on the first line.
+  def tryJoinRawLines(document: Document, file: PsiFile, start: Int, end: Int): Int = {
+    val elementAtStartLineEnd: PsiElement = file.findElementAt(start)
+    val elementAtNextLineStart: PsiElement = file.findElementAt(end)
+    (elementAtStartLineEnd.getParent, elementAtNextLineStart.getParent) match {
+      case (p0: ScPackaging, p1: ScPackaging) if p1.getParent == p0 =>
+        p0.getPackageName
+        document.replaceString(start, end + "package ".length(), ".")
+        end
+      case _ => JoinLinesHandlerDelegate.CANNOT_JOIN
+    }
+  }
+
+  def tryJoinLines(document: Document, file: PsiFile, start: Int, end: Int): Int = {
+    JoinLinesHandlerDelegate.CANNOT_JOIN
+  }
+}
