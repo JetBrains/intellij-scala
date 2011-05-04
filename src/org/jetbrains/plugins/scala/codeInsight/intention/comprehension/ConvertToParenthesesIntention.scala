cb6bdc95e3723597233558fec884b7be2e165fd6
Pavel Fatin <pavel.fatin@jetbrains.com>
2011-05-04 16:36:31 +0400

    SCL-3153 Intention to convert between parens and braces for for-comprehensions

---
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/comprehension/ConvertToParenthesesIntention.scala
@@ -0,0 +1,51 @@
+package org.jetbrains.plugins.scala.codeInsight.intention.comprehension
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScForStatement
+import com.intellij.psi.{TokenType, PsiElement}
+
+/**
+ * Pavel Fatin
+ */
+
+class ConvertToParenthesesIntention extends PsiElementBaseIntentionAction {
+  def getFamilyName = "Parentheses Conversion"
+
+  override def getText = "Convert to parentheses"
+
+  def isAvailable(project: Project, editor: Editor, element: PsiElement) = {
+    element match {
+      case e @ Parent(_: ScForStatement) =>
+        List(ScalaTokenTypes.tLBRACE, ScalaTokenTypes.tRBRACE).contains(e.getNode.getElementType)
+      case _ => false
+    }
+  }
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement) {
+    val statement = element.getParent.asInstanceOf[ScForStatement]
+    val manager = statement.getManager
+    val block = ScalaPsiElementFactory.parseElement("(_)", manager)
+
+    for (lBrace <- Option(statement.findFirstChildByType(ScalaTokenTypes.tLBRACE))) {
+      lBrace.replace(block.getFirstChild)
+    }
+
+    for (rBrace <- Option(statement.findFirstChildByType(ScalaTokenTypes.tRBRACE))) {
+      rBrace.replace(block.getLastChild)
+    }
+
+    for (enumerators <- statement.enumerators;
+         cr <- enumerators.findChildrenByType(TokenType.WHITE_SPACE) if cr.getText.contains('\n')) {
+      cr.replace(ScalaPsiElementFactory.createSemicolon(manager))
+    }
+
+    for (cr <- statement.findChildrenByType(TokenType.WHITE_SPACE) if cr.getText.contains('\n')) {
+      cr.delete()
+    }
+  }
+}
