37614f4a6ee34cff0e016f60699f5e3cbc95ee04
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-05-05 11:46:46 +0400

    ME, NPE fixed

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaCharFilter.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaCharFilter.scala
@@ -4,16 +4,18 @@ package lang.completion
 import com.intellij.codeInsight.lookup.{Lookup, CharFilter}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameterClause
 import lang.psi.api.expr.{ScForStatement, ScEnumerator, ScEnumerators}
+import com.intellij.codeInsight.lookup.CharFilter.Result
 
 class ScalaCharFilter extends CharFilter {
-  def acceptChar(c: Char, prefixLength: Int, lookup: Lookup) = {
+  def acceptChar(c: Char, prefixLength: Int, lookup: Lookup): Result = {
+    if (lookup == null || lookup.getPsiElement == null) return null
     lookup.getPsiElement.getContext match {
       // avoids:
       //   "(va:" => "(var"
       //   "(va<SPACE>" => "(var "
-      case x: ScParameterClause if Set(' ', ':').contains(c) => CharFilter.Result.HIDE_LOOKUP
+      case x: ScParameterClause if Set(' ', ':').contains(c) => Result.HIDE_LOOKUP
       case _: ScEnumerators | _: ScEnumerator | _: ScForStatement if Set(' ').contains(c) =>
-        CharFilter.Result.HIDE_LOOKUP
+        Result.HIDE_LOOKUP
       case _ => null
     }
   }
