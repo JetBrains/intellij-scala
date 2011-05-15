1ddd73f711a725e38aac821fb4792c5508854d33
jzaugg <jzaugg@gmail.com>
2011-05-15 22:20:14 +0200

    Inspection tor redundant return, when used in a position at the end of the method.

---
+++ src/org/jetbrains/plugins/scala/codeInspection/redundantReturnInspection/RemoveRedundantReturnInspection.scala
@@ -0,0 +1,45 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package redundantReturnInspection
+
+import org.intellij.lang.annotations.Language
+import com.intellij.codeInspection._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScFunctionDefinition}
+import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScReturnStmt}
+
+
+class RemoveRedundantReturnInspection extends AbstractInspection("ScalaRedundantReturn", "Redundant Return") {
+  @Language("HTML")
+  override val description =
+"""The last statement in a method is automatically returned, use of the `return` keyword is redundant."""
+
+  def actionFor(holder: ProblemsHolder) = {
+    case function: ScFunctionDefinition =>
+    for (body <- function.body) {
+        val returns = body.calculateReturns
+        body.depthFirst(!_.isInstanceOf[ScFunction]).foreach {
+          case r: ScReturnStmt =>
+            if (returns.contains(r)) {
+              r.expr match {
+                case Some(expr) =>
+                  holder.registerProblem(r.returnKeyword, "Return keyword is redundant",
+                    ProblemHighlightType.GENERIC_ERROR_OR_WARNING, new RemoveReturnKeywordQuickFix(r, expr))
+                case _ =>
+              }
+            }
+          case _ =>
+        }
+    }
+  }
+}
+
+class RemoveReturnKeywordQuickFix(ret: ScReturnStmt, expr: ScExpression) extends LocalQuickFix {
+  def applyFix(project: Project, descriptor: ProblemDescriptor) {
+    ret.replace(expr.copy())
+  }
+
+  def getFamilyName: String = "Remove return keyword"
+
+  def getName: String = getFamilyName
+}
