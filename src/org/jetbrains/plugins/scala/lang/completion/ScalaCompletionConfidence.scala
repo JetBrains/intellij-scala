3e937911f08b430ed8bfd2ca2da5d2debbbb6be8
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-07-03 19:55:07 +0400

    No focus completion inside of method call.

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionConfidence.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionConfidence.scala
@@ -6,6 +6,7 @@ import com.intellij.psi.{PsiFile, PsiElement}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import com.intellij.psi.tree.IElementType
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlockExpr, ScParenthesisedExpr, ScTuple, ScArgumentExprList}
 
 /**
  * @author Alexander Podkhalyuzin
@@ -15,7 +16,17 @@ class ScalaCompletionConfidence extends CompletionConfidence {
   def shouldFocusLookup(parameters: CompletionParameters): ThreeState = {
     if (parameters.getOriginalPosition.getText == "_") return ThreeState.NO //SCL-3290 _ <space> =>
     parameters.getPosition.getParent match {
-      case ref: ScReferenceElement => ThreeState.YES
+      case ref: ScReferenceElement => {
+        ref.getParent match {
+          case args: ScArgumentExprList => ThreeState.NO //possible anonymous method parameter name
+          case parent@(_: ScTuple | _: ScParenthesisedExpr | _: ScBlockExpr) =>
+            parent.getParent match {
+              case args: ScArgumentExprList => ThreeState.NO
+              case _ => ThreeState.YES
+            }
+          case _ => ThreeState.YES
+        }
+      }
       case _ => ThreeState.NO //keyword completion can be here in case of broken tree
     }
   }
