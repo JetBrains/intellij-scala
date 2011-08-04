f112a78fd7a3c4577aef6fee3f12662b49b02b5b
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-08-04 16:11:11 +0400

    SCL-3522 quickfix for postfix call to add parentheses.

--- src/org/jetbrains/plugins/scala/codeInspection/methodSignature/quickfix/AddCallParentheses.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/quickfix/AddCallParentheses.scala
@@ -4,7 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.codeInspection.ProblemDescriptor
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.codeInspection.AbstractFix
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScGenericCall, ScMethodCall, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScPostfixExpr, ScGenericCall, ScMethodCall, ScReferenceExpression}
 
 /**
  * Pavel Fatin
@@ -13,9 +13,13 @@ import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScGenericCall, ScMethodCal
 class AddCallParentheses(e: ScReferenceExpression) extends AbstractFix("Add call parentheses", e) {
   def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
     if (!e.isValid) return
-    val text = "%s()".format(e.getText)
+    val exprToFix = e.getParent match {
+      case postf: ScPostfixExpr => postf
+      case _ => e
+    }
+    val text = "%s()".format(exprToFix.getText)
     val call = ScalaPsiElementFactory.createExpressionFromText(text, e.getManager)
-    e.replace(call)
+    exprToFix.replace(call)
   }
 }
 
