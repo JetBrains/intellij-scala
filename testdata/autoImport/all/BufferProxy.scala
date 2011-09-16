423be0edb68622e1be96031a7b5e6188396a861b
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-09-16 13:12:36 +0400

    Fixed wrong testdata in autoimport test.

--- testdata/autoImport/all/BufferProxy.scala
+++ testdata/autoImport/all/BufferProxy.scala
@@ -1,10 +1,10 @@
 import collection.mutable.ArrayBuffer
 
 val x = new ArrayBuffer[Int]
-val y = new /*ref*/CloneableCollection
+val y = new /*ref*/ImmutableMapAdaptor
 /*
-import collection.mutable.{CloneableCollection, ArrayBuffer}
+import collection.mutable.{ImmutableMapAdaptor, ArrayBuffer}
 
 val x = new ArrayBuffer[Int]
-val y = new /*ref*/CloneableCollection
+val y = new /*ref*/ImmutableMapAdaptor
 */
