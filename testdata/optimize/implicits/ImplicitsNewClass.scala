d99f681c20ed3972bea6488b01ddb9c67797972f
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-10-24 14:03:13 +0400

    Test for SCL-3473

---
+++ testdata/optimize/implicits/ImplicitsNewClass.scala
@@ -0,0 +1,38 @@
+package overloaded
+
+object ImplicitsNewClass {
+  class A
+  class B
+  
+  class C(x: B) {
+    def this(x: Int) = this(new B)
+  }
+  object Implicits {
+    implicit def a2b(x: A): B = new B
+  }
+
+  object Test {
+    import Implicits.a2b
+
+    new C(new A)
+  }
+}
+/*package overloaded
+
+object ImplicitsNewClass {
+  class A
+  class B
+
+  class C(x: B) {
+    def this(x: Int) = this(new B)
+  }
+  object Implicits {
+    implicit def a2b(x: A): B = new B
+  }
+
+  object Test {
+    import Implicits.a2b
+
+    new C(new A)
+  }
+}*/
