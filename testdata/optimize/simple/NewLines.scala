11a52eb80142ba609c694f75080d2626041072e3
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-10-24 15:31:41 +0400

    Test for SCL-2937

---
+++ testdata/optimize/simple/NewLines.scala
@@ -0,0 +1,11 @@
+import scala.Enumeration
+import java.util.Map
+
+object MyEnum extends Enumeration {
+  val Item = Value
+}
+/*import scala.Enumeration
+
+object MyEnum extends Enumeration {
+  val Item = Value
+}*/
