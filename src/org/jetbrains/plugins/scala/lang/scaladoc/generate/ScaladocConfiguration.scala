ca9dd03ffb2dcb98ba91716d2e4e4ab002896362
Dmitry Naydanov <ndmitry91@gmail.com>
2011-10-26 11:51:08 +0400

    Generate  scaladoc action

---
+++ src/org/jetbrains/plugins/scala/lang/scaladoc/generate/ScaladocConfiguration.scala
@@ -0,0 +1,41 @@
+package org.jetbrains.plugins.scala.lang.scaladoc.generate
+
+import com.intellij.openapi.project.Project
+import com.intellij.analysis.AnalysisScope
+import com.intellij.openapi.module.Module
+import com.intellij.execution.Executor
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.configurations.{RunProfileState, ModuleRunProfile}
+import javax.swing.Icon
+
+/**
+ * User: Dmitry Naidanov
+ * Date: 12.10.11
+ */
+class ScaladocConfiguration(private val form: ScaladocConsoleRunConfigurationForm, private val project: Project,
+                            private val scope: AnalysisScope) extends ModuleRunProfile {
+
+  def getModules: Array[Module] = {
+    Module.EMPTY_ARRAY
+  }
+
+  def getState(executor: Executor, env: ExecutionEnvironment): RunProfileState = {
+    val state: ScaladocCommandLineState = new ScaladocCommandLineState(env, project)
+    state.setAdditionalScaladocFlags(form.getAdditionalFlags)
+    state.setScope(scope)
+    state.setVerbose(form.isVerbose)
+    state.setDocTitle(form.getDocTitle)
+    state.setMaxHeapSize(form.getMaxHeapSize)
+    state.setShowInBrowser(form.isShowInBrowser)
+    state.setOutputDir(form.getOutputDir)
+
+    state
+  }
+
+  def getName: String = "Generate Scaladoc"
+
+  def getIcon: Icon = null
+
+  def checkConfiguration() {}
+
+}
---
+++ src/org/jetbrains/plugins/scala/lang/scaladoc/generate/ScaladocSettings.java
@@ -0,0 +1,42 @@
+package org.jetbrains.plugins.scala.lang.scaladoc.generate;
+
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+
+/**
+ * User: Dmitry Naidanov
+ * Date: 22.10.11
+ */
+
+@State(
+    name = "ScaladocSettings",
+    storages = {
+        @Storage(file = "$PROJECT_FILE$")
+    }
+)
+public class ScaladocSettings implements PersistentStateComponent<ScaladocSettings> {
+
+  public String outputDir = null;
+  public String additionalFlags = null;
+  public String maxHeapSize = null;
+  public String docTitle = null;
+  public Boolean verbose = null;
+  public Boolean openInBrowser = null;
+
+  public ScaladocSettings getState() {
+    return this;
+  }
+
+  public void loadState(ScaladocSettings state) {
+    XmlSerializerUtil.copyBean(state, this);
+  }
+
+  public static ScaladocSettings getInstance(Project project) {
+    return ServiceManager.getService(project, ScaladocSettings.class);
+  }
+
+}
