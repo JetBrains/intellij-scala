0e58c3c9ee632a0ed5730611c3fe74976cc90f18
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-10-28 11:24:58 +0400

    Fixed test.

--- testdata/resolve2/function/type/generic/Function1.scala
+++ testdata/resolve2/function/type/generic/Function1.scala
@@ -1,8 +1,8 @@
 def f(p: String => Unit) {}
-def f(p: Int => Unit) {}
+def f(p: Int => Unit)(x: String) {}
 
 def a(p: String) {}
 def b(p: Int) {}
 
 println(/* line: 1 */f(a(_)))
-println(/* line: 2 */f(b(_)))
+println(/* line: 2 */f(b(_))(""))
