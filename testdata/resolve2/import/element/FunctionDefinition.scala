5cf7c3ec47520e41265e9be763a54b51284336aa
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-10-28 11:51:14 +0400

    fixed testdata

--- testdata/resolve2/import/element/FunctionDefinition.scala
+++ testdata/resolve2/import/element/FunctionDefinition.scala
@@ -1,4 +1,4 @@
 import actors.Actor.continue
 
-println(/* file: Actor, type:org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition  */ continue)
+println(/* file: Combinators, type:org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition  */ continue)
 println(classOf[/* resolved: false  */ continue])
