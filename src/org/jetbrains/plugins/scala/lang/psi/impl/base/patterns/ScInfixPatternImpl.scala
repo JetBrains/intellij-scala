f9bd70917f0a25e60e89ecad95f7433cd9190438
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-10-31 08:47:03 +0400

    SCL-3735 type for infix pattern.

--- src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScInfixPatternImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScInfixPatternImpl.scala
@@ -10,14 +10,15 @@ import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
 import com.intellij.lang.ASTNode
 import com.intellij.psi._
 import api.ScalaElementVisitor
+import psi.types.result.{Success, Failure, TypingContext}
 
-/** 
+/**
 * @author Alexander Podkhalyuzin
 * Date: 28.02.2008
 */
 
 class ScInfixPatternImpl(node: ASTNode) extends ScalaPsiElementImpl (node) with ScInfixPattern {
-  override def accept(visitor: PsiElementVisitor): Unit = {
+  override def accept(visitor: PsiElementVisitor) {
     visitor match {
       case visitor: ScalaElementVisitor => super.accept(visitor)
       case _ => super.accept(visitor)
@@ -26,5 +27,10 @@ class ScInfixPatternImpl(node: ASTNode) extends ScalaPsiElementImpl (node) with 
 
   override def toString: String = "InfixPattern"
 
-  //todo: getType
+  override def getType(ctx: TypingContext) = {
+    expectedType match {
+      case Some(x) => Success(x, Some(this))
+      case _ => Failure("cannot define expected type", Some(this))
+    }
+  }
 }
