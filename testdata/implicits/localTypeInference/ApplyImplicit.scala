e7e010d00bd89170dcdc5ef7f18f44087e412897
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-02 16:52:43 +0400

    Test for SCL-3469

---
+++ testdata/implicits/localTypeInference/ApplyImplicit.scala
@@ -0,0 +1,17 @@
+object tedfdst {
+  def apply[A](a: A): Option[A] = null
+
+  implicit def s2i(s: String): Int = 0
+
+  val thiss = this
+  val y: Option[Int] = thiss(/*start*/""/*end*/)
+}
+/*
+Seq(any2ArrowAssoc,
+    any2Ensuring,
+    any2stringadd,
+    augmentString,
+    s2i,
+    wrapString),
+Some(s2i)
+*/
