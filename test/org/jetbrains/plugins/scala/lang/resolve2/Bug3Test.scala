f0add4be9e73b41bcb872c51c2fa029333159a5f
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-03 16:10:38 +0400

    Test for SCL-2509

--- test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
+++ test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
@@ -15,4 +15,5 @@ class Bug3Test extends ResolveTestBase {
   def testFromCompanion() = doTest()
   def testValueFunction11() = doTest()
   def testSCL2169() = doTest()
+  def testSCL2509() {doTest()}
 }
