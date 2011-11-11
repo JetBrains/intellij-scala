db133154b752f4ff5fceaa44c8bbcb3ed7d1b9a4
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-11 08:31:59 +0400

    Read action access.

--- src/org/jetbrains/plugins/scala/findUsages/NonMemberMethodUsagesSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/NonMemberMethodUsagesSearcher.scala
@@ -8,6 +8,7 @@ import search.{UsageSearchContext, SearchScope, SearchRequestCollector}
 import com.intellij.openapi.application.QueryExecutorBase
 import com.intellij.util.Processor
 import org.jetbrains.annotations.NotNull
+import org.jetbrains.plugins.scala.util.ScalaUtils
 
 /**
  * Searches for scala methods defined inside of local scopes, ie methods for which .getContainingClass == null.
@@ -19,7 +20,13 @@ class NonMemberMethodUsagesSearcher extends QueryExecutorBase[PsiReference, Meth
     val method: PsiMethod = p.getMethod
     val collector: SearchRequestCollector = p.getOptimizer
     val searchScope: SearchScope = p.getScope
-    if (method.isConstructor) {
+    var result = false
+    ScalaUtils.runReadAction(new Runnable {
+      def run() {
+        result = method.isConstructor
+      }
+    }, method.getProject, "NonMemberMethodUsagesSearcher")
+    if (result) {
       return
     }
     ReferencesSearch.searchOptimized(method, searchScope, false, collector, consumer)
