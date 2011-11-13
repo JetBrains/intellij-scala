388166cb7cb7dfdb1e7a940e72c09b0aa3f428a0
jzaugg <jzaugg@gmail.com>
2011-11-13 16:00:06 +0100

    SCL-3808 Preserve type parameter clauses of method type parameters in override/implement.

--- test/org/jetbrains/plugins/scala/lang/overrideImplement/OverrideImplementTest.java
+++ test/org/jetbrains/plugins/scala/lang/overrideImplement/OverrideImplementTest.java
@@ -180,6 +180,11 @@ public class OverrideImplementTest extends PsiTestCase {
     runTest(name);
   }
 
+  public void testSCL3808() throws Exception {
+    String name = "bug/SCL3808.scala";
+    runTest(name);
+  }
+
   public void testSCL3305() throws Exception {
     String name = "bug/SCL3305.scala";
     runTest(name);
