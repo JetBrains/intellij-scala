2278c9900c692c37b6e633999f25482e9399b20d
jzaugg <jzaugg@gmail.com>
2011-11-13 17:17:42 +0100

    SCL-3773 Resolve parameters referred to in return types.
    Path dependent types.
    
    Supported by Scalac 2.10-SNAPSHOT or by 2.9.x with a -Y option; but the plugin allows it unconditionally.

--- src/org/jetbrains/plugins/scala/lang/psi/impl/statements/ScFunctionDefinitionImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/statements/ScFunctionDefinitionImpl.scala
@@ -42,7 +42,7 @@ class ScFunctionDefinitionImpl extends ScFunctionImpl with ScFunctionDefinition 
                                    state: ResolveState,
                                    lastParent: PsiElement,
                                    place: PsiElement): Boolean = {
-    //process function's type parameters
+    //process function's parameters for dependent method types, and process type parameters
     if (!super[ScFunctionImpl].processDeclarations(processor, state, lastParent, place)) return false
 
     //do not process parameters for default parameters, only for function body
