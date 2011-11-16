07a1f892b269010facb08a16ec147d7f9a222589
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-16 08:47:43 +0400

    Searchers in read action (X)

--- src/org/jetbrains/plugins/scala/findUsages/apply/ApplyMethodSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/apply/ApplyMethodSearcher.scala
@@ -9,6 +9,7 @@ import lang.psi.api.statements.ScFunctionDefinition
 import lang.psi.api.toplevel.typedef.ScObject
 import lang.psi.api.expr.ScMethodCall
 import lang.resolve.{ScalaResolveResult, ResolvableReferenceExpression}
+import org.jetbrains.plugins.scala.util.ScalaUtil
 
 
 class ApplyMethodSearcher extends QueryExecutor[PsiReference, ReferencesSearch.SearchParameters] {
@@ -16,26 +17,28 @@ class ApplyMethodSearcher extends QueryExecutor[PsiReference, ReferencesSearch.S
     val scope = queryParameters.getScope
     val element = queryParameters.getElementToSearch
     val ignoreAccess = queryParameters.isIgnoreAccessScope
-    element match {
-      case fun: ScFunctionDefinition if fun.name == "apply" =>
-        val processor = new Processor[PsiReference] {
-          def process(ref: PsiReference): Boolean = {
-            (ref, ref.getElement.getContext) match {
-              case (sref: ResolvableReferenceExpression, x: ScMethodCall) if x.getInvokedExpr == ref.getElement =>
-                sref.bind() match {
-                  case Some(ScalaResolveResult(`fun`, _)) => consumer.process(ref)
-                  case _ => true
-                }
-              case _ => true
+    ScalaUtil.readAction(element.getProject) {
+      element match {
+        case fun: ScFunctionDefinition if fun.name == "apply" =>
+          val processor = new Processor[PsiReference] {
+            def process(ref: PsiReference): Boolean = {
+              (ref, ref.getElement.getContext) match {
+                case (sref: ResolvableReferenceExpression, x: ScMethodCall) if x.getInvokedExpr == ref.getElement =>
+                  sref.bind() match {
+                    case Some(ScalaResolveResult(`fun`, _)) => consumer.process(ref)
+                    case _ => true
+                  }
+                case _ => true
+              }
             }
           }
-        }
-        fun.containingClass match {
-          case Some(obj: ScObject) => ReferencesSearch.search(obj, scope, ignoreAccess).forEach(processor)
-          case _ =>
-        }
+          fun.containingClass match {
+            case Some(obj: ScObject) => ReferencesSearch.search(obj, scope, ignoreAccess).forEach(processor)
+            case _ =>
+          }
         // TODO Check every ScMethodCall? Sounds expensive!
-      case _ =>
+        case _ =>
+      }
     }
     true
   }
