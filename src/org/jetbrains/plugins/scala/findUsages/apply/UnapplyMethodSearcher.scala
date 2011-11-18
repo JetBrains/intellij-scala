3c30faf8f886120caf9d04d8b7607071a1ba09ca
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-18 12:23:23 +0400

    Searchers in read action (X) (cherry picked from commit 07a1f892b269010facb08a16ec147d7f9a222589)

--- src/org/jetbrains/plugins/scala/findUsages/apply/UnapplyMethodSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/apply/UnapplyMethodSearcher.scala
@@ -10,34 +10,36 @@ import lang.psi.api.toplevel.typedef.ScObject
 import lang.resolve.ScalaResolveResult
 import lang.psi.api.base.patterns.ScConstructorPattern
 import lang.psi.api.base.ScStableCodeReferenceElement
+import org.jetbrains.plugins.scala.util.ScalaUtil
 
 class UnapplyMethodSearcher extends QueryExecutor[PsiReference, ReferencesSearch.SearchParameters] {
   def execute(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor[PsiReference]): Boolean = {
     val scope = queryParameters.getScope
     val element = queryParameters.getElementToSearch
     val ignoreAccess = queryParameters.isIgnoreAccessScope
-    element match {
-      case fun: ScFunctionDefinition if Set("unapply", "unapplySeq").contains(fun.name) =>
-        val processor = new Processor[PsiReference] {
-          def process(ref: PsiReference): Boolean = {
-            (ref, ref.getElement.getContext) match {
-              case (sref: ScStableCodeReferenceElement, x: ScConstructorPattern) =>
-                sref.bind() match {
-                  case Some(ScalaResolveResult(`fun`, _)) => consumer.process(ref)
-                  case _ => true
-                }
-              case _ => true
+    ScalaUtil.readAction(element.getProject) {
+      element match {
+        case fun: ScFunctionDefinition if Set("unapply", "unapplySeq").contains(fun.name) =>
+          val processor = new Processor[PsiReference] {
+            def process(ref: PsiReference): Boolean = {
+              (ref, ref.getElement.getContext) match {
+                case (sref: ScStableCodeReferenceElement, x: ScConstructorPattern) =>
+                  sref.bind() match {
+                    case Some(ScalaResolveResult(`fun`, _)) => consumer.process(ref)
+                    case _ => true
+                  }
+                case _ => true
+              }
             }
           }
-        }
 
-        fun.containingClass match {
-          case Some(obj: ScObject) => ReferencesSearch.search(obj, scope, ignoreAccess).forEach(processor)
-          case _ =>
-        }
-
+          fun.containingClass match {
+            case Some(obj: ScObject) => ReferencesSearch.search(obj, scope, ignoreAccess).forEach(processor)
+            case _ =>
+          }
         // TODO check every other ScConstructorPattern known to man?
-      case _ =>
+        case _ =>
+      }
     }
     true
   }
