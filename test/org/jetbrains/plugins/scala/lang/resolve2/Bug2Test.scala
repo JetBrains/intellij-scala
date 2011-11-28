7e007f16ee74a8860cd3186482b07818821f3961
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-28 11:07:45 +0400

    Removed problematic tests

--- test/org/jetbrains/plugins/scala/lang/resolve2/Bug2Test.scala
+++ test/org/jetbrains/plugins/scala/lang/resolve2/Bug2Test.scala
@@ -58,4 +58,15 @@ class Bug2Test extends ResolveTestBase {
   def testSCL3485() {doTest()}
   def testSCL3539() {doTest()}
   def testSCL3546() {doTest()}
+  def testSCL1707() = doTest()
+  def testSCL2073() = doTest()
+  def testSCL2386A() = doTest()
+  def testSCL2386B() = doTest()
+  def testSCL2386C() = doTest()
+  def testSCL2386D() = doTest()
+  def testSCL2386E() = doTest()
+  def testSCL2456() = doTest()
+  def testFromCompanion() = doTest()
+  def testValueFunction11() = doTest()
+  def testSCL2169() = doTest()
 }
