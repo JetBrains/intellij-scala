6d8ab7c094413e105da309063637cac60069c9c7
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-30 10:05:10 +0400

    Basic Completion2 tests -> Completion3 test

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionContributor.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaCompletionContributor.scala
@@ -41,7 +41,7 @@ class ScalaCompletionContributor extends CompletionContributor {
       val expectedTypesAfterNew: Array[ScType] =
       if (afterNewPattern.accepts(parameters.getPosition, context)) {
         val element = parameters.getPosition
-        val newExpr = PsiTreeUtil.getParentOfType(element, classOf[ScNewTemplateDefinition])
+        val newExpr: ScNewTemplateDefinition = PsiTreeUtil.getParentOfType(element, classOf[ScNewTemplateDefinition])
         newExpr.expectedTypes().map(tp => tp match {
           case ScAbstractType(_, lower, upper) => upper
           case _ => tp
--- testdata/completion2/basic/inImport/ImportSelector.scala
+++
@@ -1,2 +0,0 @@
-import scala.collection.immutable.{VBuil/*caret*/}
-//import scala.collection.immutable.{VectorBuilder/*caret*/}
