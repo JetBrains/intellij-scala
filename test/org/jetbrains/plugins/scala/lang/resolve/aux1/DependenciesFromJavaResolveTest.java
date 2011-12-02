6ed90abfb7692b1f5ee98eebce9b7518ff469577
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-12-02 13:27:12 +0400

    Test for SCL-3876

--- test/org/jetbrains/plugins/scala/lang/resolve/aux1/DependenciesFromJavaResolveTest.java
+++ test/org/jetbrains/plugins/scala/lang/resolve/aux1/DependenciesFromJavaResolveTest.java
@@ -1,8 +1,6 @@
 package org.jetbrains.plugins.scala.lang.resolve.aux1;
 
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
+import com.intellij.psi.*;
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass;
 import org.jetbrains.plugins.scala.lang.resolve.ScalaResolveTestCase;
 
@@ -33,5 +31,26 @@ public class DependenciesFromJavaResolveTest extends ScalaResolveTestCase {
     final String name = supers[0].getName();
     assertTrue(name.equals("ScalaObject"));
 
+  }
+  
+  public void testScalaPrivateTag() throws Exception {
+    PsiReference ref = findReferenceAtCaret();
+    final PsiElement resolved = ref.resolve();
+    assertNotNull(resolved);
+    assertTrue(resolved instanceof PsiMethod);
+  }
+
+  public void testScalaPublicTag1() throws Exception {
+    PsiReference ref = findReferenceAtCaret();
+    final PsiElement resolved = ref.resolve();
+    assertNotNull(resolved);
+    assertTrue(resolved instanceof PsiField);
+  }
+
+  public void testScalaPublicTag2() throws Exception {
+    PsiReference ref = findReferenceAtCaret();
+    final PsiElement resolved = ref.resolve();
+    assertNotNull(resolved);
+    assertTrue(resolved instanceof PsiMethod);
   }
 }
---
+++ testdata/resolve/aux1/idea/test/ScalaPublicTag2.scala
@@ -0,0 +1,8 @@
+package test
+
+import idea.Tag
+
+class O {
+  val t = new Tag
+  t.<ref>mag()
+}
