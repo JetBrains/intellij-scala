e96c5534cd94d266bc3a783d8ac923b3d52231c5
unknown <Dmitry.Naydanov@unit-224.Labs.IntelliJ.Net>
2011-12-10 11:23:13 +0400

    some tests, better tag name completion, resolving for @throws tag

---
+++ testdata/resolve/scaladoc/MethodTypeParamNoResolve.scala
@@ -0,0 +1,16 @@
+class a[T, U, E] {
+
+  /**
+   * @tparam <testref>T asdf
+   * @tparam <testref>U
+   * @tparam <testref>E
+   * @tparam PP<testref>C ksdlafksjdlh
+   * @tparam P
+   * @tparam <testref>P
+   * @tparam Q
+   */
+  def f[P, Q](a: P,  b: Q, c: T): T = {
+    c
+  }
+}
+
