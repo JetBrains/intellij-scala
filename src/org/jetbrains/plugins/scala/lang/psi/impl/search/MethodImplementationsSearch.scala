a3769c64344a6150dda596bdd1384867f92499c0
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-01-04 14:29:14 +0400

    Read action fixed.

--- src/org/jetbrains/plugins/scala/lang/psi/impl/search/MethodImplementationsSearch.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/search/MethodImplementationsSearch.scala
@@ -4,34 +4,35 @@ package psi
 package impl
 package search
 
-import _root_.scala.collection.mutable.ArrayBuffer
-import api.statements.ScFunction
+import collection.mutable.ArrayBuffer
 import api.toplevel.ScNamedElement
-import com.intellij.psi.search.searches.OverridingMethodsSearch
-import com.intellij.psi.{PsiMember, PsiMethod, PsiElement, PsiNamedElement}
+import com.intellij.psi.{PsiElement, PsiNamedElement}
 import com.intellij.util.{QueryExecutor, Processor}
+import extensions.inReadAction
+
 /**
  * User: Alexander Podkhalyuzin
  * Date: 16.12.2008
  */
-
 class MethodImplementationsSearch extends QueryExecutor[PsiElement, PsiElement] {
   override def execute(sourceElement: PsiElement, consumer: Processor[PsiElement]): Boolean = {
     if (sourceElement.isInstanceOf[ScNamedElement]) {
       for (implementation <- getOverridingMethods(sourceElement.asInstanceOf[ScNamedElement])) {
-        if ( !consumer.process(implementation) ) {
+        if (!consumer.process(implementation)) {
           return false
         }
       }
     }
-    return true;
+    true
   }
 
   def getOverridingMethods(method: ScNamedElement): Array[PsiNamedElement] = {
     val result = new ArrayBuffer[PsiNamedElement]
-    for (psiMethod <- ScalaOverridengMemberSearch.search(method, true)) {
-      result += psiMethod
+    inReadAction {
+      for (psiMethod <- ScalaOverridengMemberSearch.search(method, true)) {
+        result += psiMethod
+      }
     }
-    return result.toArray
+    result.toArray
   }
 }
