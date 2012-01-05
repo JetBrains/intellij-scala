7719aae8ea9b27dd646c291313bbaf11d1d65b6b
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-01-05 12:09:16 +0400

    SCL-3954

---
+++ src/org/jetbrains/plugins/scala/lang/completion/weighter/ScalaClassObjectWeigher.scala
@@ -0,0 +1,31 @@
+package org.jetbrains.plugins.scala.lang.completion.weighter
+
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.completion.CompletionLocation
+import com.intellij.psi.util.proximity.ProximityWeigher
+import com.intellij.psi.util.{PsiTreeUtil, ProximityLocation}
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
+import com.intellij.psi.{PsiClass, PsiElement}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 05.01.12
+ */
+
+class ScalaClassObjectWeigher extends ProximityWeigher {
+  def weigh(element: PsiElement, location: ProximityLocation): Comparable[_] = {
+    val elem = location.getPosition
+    val ref = PsiTreeUtil.getParentOfType(elem, classOf[ScReferenceElement])
+    if (ref == null) return null
+    element match {
+      case o: ScObject => 
+        if (ref.isInstanceOf[ScReferenceExpression]) return 1
+      case p: PsiClass =>
+        if (!ref.isInstanceOf[ScReferenceExpression]) return 1
+      case _ =>
+    }
+    0
+  }
+}
