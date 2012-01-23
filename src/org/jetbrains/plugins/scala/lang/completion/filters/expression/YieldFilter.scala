6279711ee29e99d57be808d8d9f0662b0b2fb5b6
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-01-23 14:35:35 +0400

    fixed NPE

--- src/org/jetbrains/plugins/scala/lang/completion/filters/expression/YieldFilter.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/filters/expression/YieldFilter.scala
@@ -4,15 +4,10 @@ package completion
 package filters.expression
 
 import lexer.ScalaTokenTypes
-import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.filters.ElementFilter;
 import org.jetbrains.annotations.NonNls;
-import org.jetbrains.plugins.scala.lang.psi._
 import com.intellij.psi._
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
-import org.jetbrains.plugins.scala.lang.parser._
 import org.jetbrains.plugins.scala.lang.completion.ScalaCompletionUtil._
 
 /**
@@ -21,34 +16,40 @@ import org.jetbrains.plugins.scala.lang.completion.ScalaCompletionUtil._
 */
 
 class YieldFilter extends ElementFilter {
+  private def leafText(i: Int,  context: PsiElement): String = {
+    val elem = getLeafByOffset(i, context)
+    if (elem == null) return ""
+    elem.getText
+  }
+
   def isAcceptable(element: Object, context: PsiElement): Boolean = {
     if (context.isInstanceOf[PsiComment]) return false
-    val leaf = getLeafByOffset(context.getTextRange().getStartOffset(), context);
+    val leaf = getLeafByOffset(context.getTextRange.getStartOffset, context);
     if (leaf != null) {
-      val parent = leaf.getParent()
-      if (parent.isInstanceOf[ScExpression] && parent.getParent().isInstanceOf[ScForStatement]) {
-        var i = context.getTextRange().getStartOffset() - 1
+      val parent = leaf.getParent
+      if (parent.isInstanceOf[ScExpression] && parent.getParent.isInstanceOf[ScForStatement]) {
+        var i = context.getTextRange.getStartOffset - 1
         while (i > 0 && (context.getContainingFile.getText.charAt(i) == ' ' ||
                  context.getContainingFile.getText.charAt(i) == '\n')) i = i - 1
-        if (getLeafByOffset(i, context).getText == "yield") return false
-        i = context.getTextRange.getEndOffset()
+        if (leafText(i, context) == "yield") return false
+        i = context.getTextRange.getEndOffset
         while (i < context.getContainingFile.getText.length - 1 && (context.getContainingFile.getText.charAt(i) == ' ' ||
                  context.getContainingFile.getText.charAt(i) == '\n')) i = i + 1
-        if (getLeafByOffset(i, context).getText == "yield") return false
+        if (leafText(i, context) == "yield") return false
         for (child <- parent.getParent.getNode.getChildren(null) if child.getElementType == ScalaTokenTypes.kYIELD) return false
         return ScalaCompletionUtil.checkAnyWith(parent.getParent, "yield true", context.getManager) ||
           ScalaCompletionUtil.checkReplace(parent.getParent, "yield", context.getManager)
       }
     }
-    return false;
+    false;
   }
 
   def isClassAcceptable(hintClass: java.lang.Class[_]): Boolean = {
-    return true;
+    true;
   }
 
   @NonNls
-  override def toString(): String = {
-    return "'yield' keyword filter"
+  override def toString: String = {
+    "'yield' keyword filter"
   }
 }
