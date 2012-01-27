5d63e1c35bf4b040b709747a9bca55bbdddc2395
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-01-27 13:21:05 +0400

    Right isReferenceTo for Java classes (to find constructors).

--- src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/templates/ScClassParents.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/templates/ScClassParents.scala
@@ -4,15 +4,15 @@ package psi
 package api
 package toplevel
 package templates
+
+
 import org.jetbrains.plugins.scala.lang.psi.api.base.{types, ScConstructor}
-import types.ScTypeElement
 
 /**
 * @author Alexander Podkhalyuzin
 * Date: 22.02.2008
 * Time: 9:24:03
 */
-
 trait ScClassParents extends ScTemplateParents {
   def constructor = findChild(classOf[ScConstructor])
 }
