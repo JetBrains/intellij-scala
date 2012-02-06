2287a8448c9eab2b9552c65a6d14d48a126e1b9e
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-02-06 12:49:23 +0400

    Type parameter shadowing quickfix.

---
+++ src/org/jetbrains/plugins/scala/codeInspection/shadow/RenameElementQuickfix.scala
@@ -0,0 +1,52 @@
+package org.jetbrains.plugins.scala.codeInspection.shadow
+
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
+import org.jetbrains.plugins.scala.codeInspection.AbstractFix
+import com.intellij.openapi.project.Project
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.refactoring.actions.RenameElementAction
+import org.jetbrains.plugins.scala.extensions._
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil
+import com.intellij.injected.editor.EditorWindow
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.impl.text.TextEditorPsiDataProvider
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem._
+import com.intellij.psi.PsiElement
+
+/**
+ * User: Alefas
+ * Date: 06.02.12
+ */
+
+class RenameElementQuickfix(ref: PsiElement, name: String) extends AbstractFix(name, ref) {
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+    if (!ref.isValid) return
+    val action: AnAction = new RenameElementAction
+    val event: AnActionEvent = actionEventForElement(descriptor, project, action)
+    invokeLater {
+      action.actionPerformed(event)
+    }
+  }
+
+  private def actionEventForElement(descriptor: ProblemDescriptor, project: Project, action: AnAction): AnActionEvent = {
+    import collection.JavaConversions._
+    import collection.mutable
+
+    val map = mutable.Map[String, AnyRef]()
+    val containingFile = ref.getContainingFile
+    val editor: Editor = InjectedLanguageUtil.openEditorFor(containingFile, project)
+    if (editor.isInstanceOf[EditorWindow]) {
+      map.put(PlatformDataKeys.EDITOR.getName, editor)
+      map.put(LangDataKeys.PSI_ELEMENT.getName, ref)
+    } else if (ApplicationManager.getApplication.isUnitTestMode) {
+      val element = new TextEditorPsiDataProvider().getData(LangDataKeys.PSI_ELEMENT.getName,
+        editor, containingFile.getVirtualFile)
+      map.put(LangDataKeys.PSI_ELEMENT.getName, element)
+    }
+    val dataContext = SimpleDataContext.getSimpleContext(map, DataManager.getInstance.getDataContext)
+    new AnActionEvent(null, dataContext, "", action.getTemplatePresentation, ActionManager.getInstance, 0)
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/codeInspection/shadow/TypeParameterShadowInspection.scala
@@ -0,0 +1,46 @@
+package org.jetbrains.plugins.scala.codeInspection.shadow
+
+import org.jetbrains.plugins.scala.codeInspection.AbstractInspection
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScTypeParam
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypeParametersOwner
+
+/**
+ * User: Alefas
+ * Date: 06.02.12
+ */
+
+class TypeParameterShadowInspection extends AbstractInspection("TypeParameterShadowInspection", "Suspicious shadowing by a Type Parameter") {
+  val description: String = """Detects a Type Parameter that shadows other Type Parameter defined in the enclosing scope."""
+
+  def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
+    case refPat: ScTypeParam => check(refPat, holder)
+  }
+  
+  private def isShadowing(refPat: ScTypeParam): Option[ScTypeParametersOwner] = {
+    var parent: PsiElement = refPat.getParent
+    val owner = refPat.owner
+    while (parent != null) {
+      parent match {
+        case t: ScTypeParametersOwner if t != owner =>
+          for (param <- t.typeParameters) {
+            if (refPat.name == param.name) return Some(t)
+          }
+        case _ =>
+      }
+      parent = parent.getParent
+    }
+    None
+  }
+
+  private def check(typeParam: ScTypeParam, holder: ProblemsHolder) {
+    isShadowing(typeParam) match {
+      case Some(_) =>
+        holder.registerProblem(typeParam.nameId, getDisplayName + ": " + typeParam.name, new RenameTypeParameterFix(typeParam))
+      case _ =>
+    }
+  }
+}
+
+class RenameTypeParameterFix(tp: ScTypeParam) extends RenameElementQuickfix(tp, "Rename Variable Pattern")
