5f5dba25258769e7b9b711067f37cc56a27737cd
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-02-09 12:36:48 +0400

    Unnecessary semicolon inspection.

--- src/org/jetbrains/plugins/scala/codeInspection/methodSignature/AbstractMethodSignatureInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/AbstractMethodSignatureInspection.scala
@@ -1,14 +1,9 @@
 package org.jetbrains.plugins.scala.codeInspection.methodSignature
 
-import com.intellij.codeInspection._
-import org.intellij.lang.annotations.Language
-import com.intellij.psi.PsiElement
-import org.jetbrains.plugins.scala.extensions.VisitorWrapper
-import org.jetbrains.plugins.scala.codeInspection.{AbstractInspection, InspectionsUtil}
+import org.jetbrains.plugins.scala.codeInspection.AbstractInspection
 
 /**
  * Pavel Fatin
  */
 abstract class AbstractMethodSignatureInspection(id: String, name: String) extends AbstractInspection(id, name) {
-  override final def getGroupDisplayName = InspectionsUtil.MethodSignature
 }
