4791b0a355f401c41b4ecbb1da7c91cee709106a
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-03-05 14:42:26 +0400

    Fixed console enter action. Now it's always right before just enter action. So if it's not choose lookup action then it's our execute action.

--- src/org/jetbrains/plugins/scala/console/ScalaLanguageConsoleView.scala
+++ src/org/jetbrains/plugins/scala/console/ScalaLanguageConsoleView.scala
@@ -1,36 +1,90 @@
 package org.jetbrains.plugins.scala.console
 
-import com.intellij.openapi.project.Project
-import com.intellij.execution.runners.{AbstractConsoleRunnerWithHistory, ConsoleExecuteActionHandler}
-import com.intellij.execution.process.ProcessHandler
 import java.lang.String
 import com.intellij.execution.console.{ConsoleHistoryController, LanguageConsoleViewImpl}
-/**
- * User: Alexander Podkhalyuzin
- * Date: 14.04.2010
- */
+import com.intellij.execution.process.{ConsoleHistoryModel, ProcessHandler}
+import java.io.{IOException, OutputStream}
+import com.intellij.openapi.project.{DumbAwareAction, Project}
+import java.util.Comparator
+import com.intellij.openapi.editor.actions.EnterAction
+import com.intellij.openapi.actionSystem._
+import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.util.{IconLoader, TextRange}
+import org.jetbrains.plugins.scala.extensions
 
+/**
+ * @author Alexander Podkhalyuzin
+ * @since 14.04.2010
+ */
 class ScalaLanguageConsoleView(project: Project) extends {
   val scalaConsole = new ScalaLanguageConsole(project, ScalaLanguageConsoleView.SCALA_CONSOLE)
 } with LanguageConsoleViewImpl(project, scalaConsole) {
   override def attachToProcess(processHandler: ProcessHandler) {
     super.attachToProcess(processHandler)
-    val handler = new ConsoleExecuteActionHandler(processHandler, false) {
-      override def processLine(line: String) {
-        line.split('\n').foreach(line => {
-          if (line != "") {
-            super.processLine(line)
-          }
-          scalaConsole.textSent(line + "\n")
-        })
+    val model = new ConsoleHistoryModel
+    new ConsoleHistoryController("scala", null, scalaConsole, model).install()
+    val action = new ScalaExecuteConsoleEnterAction(scalaConsole, processHandler, model)
+    EmptyAction.setupAction(action, "Console.Execute", this)
+    action.registerCustomShortcutSet(action.getShortcutSet, this)
+  }
+
+  override def getData(dataId: String): AnyRef = {
+    if (PlatformDataKeys.ACTIONS_SORTER.is(dataId)) {
+      ScalaLanguageConsoleView.CONSOLE_ACTIONS_COMPARATOR
+    } else super.getData(dataId)
+  }
+}
+
+class ScalaExecuteConsoleEnterAction(console: ScalaLanguageConsole, processHandler: ProcessHandler,
+                                     model: ConsoleHistoryModel) extends DumbAwareAction(null, null, IconLoader.getIcon("/actions/execute.png")) {
+  def actionPerformed(e: AnActionEvent) {
+    val editor = PlatformDataKeys.EDITOR.getData(e.getDataContext)
+    if (editor != null) {
+      val document = editor.getDocument
+      val text = document.getText
+
+      // Process input and add to history
+      val range: TextRange = new TextRange(0, document.getTextLength)
+      editor.getSelectionModel.setSelection(range.getStartOffset, range.getEndOffset)
+      console.addCurrentToHistory(range, false, true)
+      model.addToHistory(text)
+
+      editor.getCaretModel.moveToOffset(0)
+      extensions.inWriteAction {
+        editor.getDocument.setText("")
       }
+
+      text.split('\n').foreach(line => {
+        if (line != "") {
+          val outputStream: OutputStream = processHandler.getProcessInput
+          try {
+            var bytes: Array[Byte] = (line + "\n").getBytes
+            outputStream.write(bytes)
+            outputStream.flush()
+          }
+          catch {
+            case e: IOException => //ignore
+          }
+        }
+        console.textSent(line + "\n")
+      })
     }
-    new ConsoleHistoryController("scala", null, scalaConsole, handler.getConsoleHistoryModel).install()
-    val action = AbstractConsoleRunnerWithHistory.createConsoleExecAction(scalaConsole, processHandler, handler)
-    action.registerCustomShortcutSet(action.getShortcutSet, scalaConsole.getComponent)
+  }
+
+  override def update(e: AnActionEvent) {
+    val editor: EditorEx = console.getConsoleEditor
+    e.getPresentation.setEnabled(!editor.isRendererMode && !processHandler.isProcessTerminated)
   }
 }
 
 object ScalaLanguageConsoleView {
   val SCALA_CONSOLE = "Scala Console"
+
+  private val CONSOLE_ACTIONS_COMPARATOR: Comparator[AnAction] = new Comparator[AnAction] {
+    def compare(o1: AnAction, o2: AnAction): Int = {
+      if (o1.isInstanceOf[ScalaExecuteConsoleEnterAction] && o2.isInstanceOf[EnterAction]) -1
+      else if (o1.isInstanceOf[ScalaExecuteConsoleEnterAction] || o2.isInstanceOf[ScalaExecuteConsoleEnterAction]) 1
+      else 0
+    }
+  }
 }
