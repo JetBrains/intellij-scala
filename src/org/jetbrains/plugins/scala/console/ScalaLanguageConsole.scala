0cd9feb5004f937de8445a3a1319c09ae28720ca
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-03-29 16:14:44 +0400

    SCL-4072 redefined variables are not resolve in scala console.

--- src/org/jetbrains/plugins/scala/console/ScalaLanguageConsole.scala
+++ src/org/jetbrains/plugins/scala/console/ScalaLanguageConsole.scala
@@ -5,6 +5,11 @@ import org.jetbrains.plugins.scala.ScalaFileType
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import collection.mutable.HashMap
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTrait, ScClass, ScObject}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScTypeAlias, ScFunction, ScVariable, ScValue}
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
 
 /**
  * User: Alefas
@@ -19,6 +24,37 @@ class ScalaLanguageConsole(project: Project, title: String)
 
   private[console] def textSent(text: String) {
     textBuffer.append(text)
+    scalaFile = ScalaPsiElementFactory.createScalaFileFromText(textBuffer.toString() + ";\n1", project)
+    val types = new HashMap[String, TextRange]
+    val values = new HashMap[String, (TextRange, Boolean)]
+    def addValue(name: String, range: TextRange, replaceWithPlaceholder: Boolean) {
+      values.get(name) match {
+        case Some((oldRange, r)) =>
+          val newText = if (r) "_" + StringUtil.repeatSymbol(' ', oldRange.getLength - 1) else StringUtil.repeatSymbol(' ', oldRange.getLength)
+          textBuffer.replace(oldRange.getStartOffset, oldRange.getEndOffset, newText)
+        case None =>
+      }
+      values.put(name, ((range, replaceWithPlaceholder)))
+    }
+    def addType(name: String, range: TextRange) {
+      types.get(name) match {
+        case Some(oldRange) =>
+          val newText = StringUtil.repeatSymbol(' ', oldRange.getLength)
+          textBuffer.replace(oldRange.getStartOffset, oldRange.getEndOffset, newText)
+        case None =>
+      }
+      types.put(name, range)
+    }
+    scalaFile.getChildren.foreach {
+      case v: ScValue => v.declaredElements.foreach(td => addValue(td.name, td.nameId.getTextRange, true))
+      case v: ScVariable => v.declaredElements.foreach(td => addValue(td.name, td.nameId.getTextRange, true))
+      case f: ScFunction => addValue(f.name, f.getTextRange, false)
+      case o: ScObject => addValue(o.name, o.getTextRange, false)
+      case c: ScClass =>  addType(c.name, c.nameId.getTextRange)
+      case c: ScTrait =>  addType(c.name, c.nameId.getTextRange)
+      case t: ScTypeAlias => addType(t.name, t.nameId.getTextRange)
+      case _ => //do nothing
+    }
     scalaFile = ScalaPsiElementFactory.createScalaFileFromText(textBuffer.toString() + ";\n1", project)
     myFile.asInstanceOf[ScalaFile].setContext(scalaFile, scalaFile.getLastChild)
   }
