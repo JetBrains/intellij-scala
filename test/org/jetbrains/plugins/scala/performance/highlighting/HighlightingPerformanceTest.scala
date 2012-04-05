23814e305e892f1d3f84dabcaa456fb32a180b3a
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-04-05 14:47:23 +0400

    Lexer performance test added.

--- test/org/jetbrains/plugins/scala/performance/highlighting/HighlightingPerformanceTest.scala
+++ test/org/jetbrains/plugins/scala/performance/highlighting/HighlightingPerformanceTest.scala
@@ -56,7 +56,7 @@ object addressbook {
      """ + "\"\"\"" + """table { border-right: 1px solid #cccccc; }
         th { background-color: #cccccc; }
         td { border-left: 1px solid #acacac; }
-        td { border-bottom: 1px solid #acacac;""" + "\"\"\"" + """}
+        td { border-bottom: 1px solid #acacac; }""" + "\"\"\"" + """}
       </style>
     </head>;
 
