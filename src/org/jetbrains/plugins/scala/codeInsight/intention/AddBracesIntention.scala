2f7e6d0698c2a4f37f22c45892dd8db2617c7a8d
jzaugg <jzaugg@gmail.com>
2012-04-07 22:33:13 +0200

    SCL-3595 Intention to add braces around one-liners
    Works for def, val, if, else, try, and finally, while, and do.

---
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/AddBracesIntention.scala
@@ -0,0 +1,81 @@
+package org.jetbrains.plugins.scala
+package codeInsight.intention
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import com.intellij.psi.PsiElement
+import lang.psi.ScalaPsiUtil
+import lang.psi.api.statements.{ScFunctionDefinition, ScPatternDefinition}
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.impl.source.codeStyle.CodeEditUtil
+import lang.psi.api.expr._
+
+/**
+ * Jason Zaugg
+ */
+
+class AddBracesIntention extends PsiElementBaseIntentionAction {
+  def getFamilyName = "Add braces"
+
+  override def getText = "Add braces around single line expression"
+
+  def isAvailable(project: Project, editor: Editor, element: PsiElement) = {
+    check(project, editor, element).isDefined
+  }
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement) {
+    if (element == null || !element.isValid) return
+    check(project, editor, element) match {
+      case Some(x) => x()
+      case None =>
+    }
+  }
+
+  private def check(project: Project, editor: Editor, element: PsiElement): Option[() => Unit] = {
+    val containing = ScalaPsiUtil.getParentOfType(element, true,
+      classOf[ScPatternDefinition], classOf[ScIfStmt], classOf[ScFunctionDefinition], classOf[ScTryBlock],
+      classOf[ScFinallyBlock], classOf[ScWhileStmt], classOf[ScDoStmt])
+
+    def isAncestorOfElement(ancestor: PsiElement) = PsiTreeUtil.isContextAncestor(ancestor, element, false)
+
+    val expr: Option[ScExpression] = containing match {
+      case pattern: ScPatternDefinition if isAncestorOfElement(pattern.expr) =>
+        Some(pattern.expr)
+      case ifStmt: ScIfStmt =>
+        ifStmt.thenBranch.filter(isAncestorOfElement).orElse(ifStmt.elseBranch.filter(isAncestorOfElement))
+      case funDef: ScFunctionDefinition =>
+        funDef.body.filter(isAncestorOfElement)
+      case tryBlock: ScTryBlock if !tryBlock.hasRBrace =>
+        tryBlock.statements match {
+          case Seq(x: ScExpression) if isAncestorOfElement(x) => Some(x)
+          case _ => None
+        }
+      case finallyBlock: ScFinallyBlock =>
+        finallyBlock.expression.filter(isAncestorOfElement)
+      case whileStmt: ScWhileStmt =>
+        whileStmt.body.filter(isAncestorOfElement)
+      case doStmt: ScDoStmt =>
+        doStmt.getExprBody.filter(isAncestorOfElement)
+      case _ => None
+    }
+    val oneLinerExpr: Option[ScExpression] = expr.filter {
+      x =>
+        val startLine = editor.getDocument.getLineNumber(x.getTextRange.getStartOffset)
+        val endLine = editor.getDocument.getLineNumber(x.getTextRange.getEndOffset)
+        val isBlock = x match {
+          case _: ScBlockExpr => true
+          case _ => false
+
+        }
+        startLine == endLine && !isBlock
+    }
+    oneLinerExpr.map {
+      expr => () => {
+        val replacement = ScalaPsiElementFactory.createExpressionFromText("{\n%s}".format(expr.getText), expr.getManager)
+        CodeEditUtil.replaceChild(expr.getParent.getNode, expr.getNode, replacement.getNode)
+      }
+    }
+  }
+}
