b8701651879964559cee6468f488dd3cd4624249
jzaugg <jzaugg@gmail.com>
2012-04-08 19:12:36 +0200

    SCL-3100 Test case.

--- test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
+++ test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
@@ -17,6 +17,7 @@ class Bug3Test extends ResolveTestBase {
   def testSCL2169() {doTest()}
   def testSCL2509() {doTest()}
   def testSCL3053() {doTest()}
+  def testSCL3100() {doTest()}
   def testSCL3592A() {doTest()}
   def testSCL3592B() {doTest()}
   def testSCL3592C() {doTest()}
