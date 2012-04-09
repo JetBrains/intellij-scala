f2941ea05087d0725eefd6da93121275927ff9c3
jzaugg <jzaugg@gmail.com>
2012-04-09 21:38:13 +0200

    Hide the import popup when '{' is typed after `import a.b.`.

--- src/org/jetbrains/plugins/scala/lang/completion/ScalaCharFilter.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/ScalaCharFilter.scala
@@ -4,13 +4,23 @@ package lang.completion
 import com.intellij.codeInsight.lookup.{Lookup, CharFilter}
 import com.intellij.codeInsight.lookup.CharFilter.Result
 import lang.psi.api.ScalaFile
+import extensions.Parent
+import lang.psi.api.toplevel.imports.ScImportExpr
 
 class ScalaCharFilter extends CharFilter {
   def acceptChar(c: Char, prefixLength: Int, lookup: Lookup): Result = {
     if (lookup == null || lookup.getPsiElement == null) return null
     val file = lookup.getPsiFile
     if (!file.isInstanceOf[ScalaFile]) return null
-    if (c == '[' || c == '{' || c == ')' || c == ']' || c == '}') return Result.SELECT_ITEM_AND_FINISH_LOOKUP
+    if (c == '[' || c == '{' || c == ')' || c == ']' || c == '}') {
+      lookup.getPsiElement match {
+        case Parent(importExpr: ScImportExpr) =>
+          // import a.<caret/>
+          return Result.HIDE_LOOKUP
+        case _ =>
+          return Result.SELECT_ITEM_AND_FINISH_LOOKUP
+      }
+    }
     if (c == '`') return Result.ADD_TO_PREFIX
     if (c == ':') return Result.HIDE_LOOKUP
     null
