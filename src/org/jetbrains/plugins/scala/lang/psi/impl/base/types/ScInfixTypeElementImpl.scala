2142a7d082da2c29e74a7b9a0c4ca4836007b2c2
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-04-10 12:50:22 +0400

    SCL-4214 calculate path dependent type for infix types.

--- src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScInfixTypeElementImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScInfixTypeElementImpl.scala
@@ -28,13 +28,13 @@ class ScInfixTypeElementImpl(node: ASTNode) extends ScalaPsiElementImpl(node) wi
 
   protected def innerType(ctx: TypingContext): TypeResult[ScType] = for {
     rop <- wrap(rOp)(ScalaBundle.message("no.right.operand.found"))
-    element <- wrap(ref.bind.map(_.element))("cannot.resolve.infix.operator")
+    element <- wrap(ref.bind().map(_.element))("cannot.resolve.infix.operator")
     rType <- rop.getType(ctx)
     lType <- lOp.getType(ctx)
    // TODO SCL-4179 Is this sufficient? Compare with ScParameterizedTypeImpl#innerType.
     desType = element match {
       case pt: ScTypeParam => new ScTypeParameterType(pt, ScSubstitutor.empty)
-      case _ => ScType.designator(element)
+      case _ => ScSimpleTypeElementImpl.calculateReferenceType(ref, false).getOrElse(ScType.designator(element))
     }
   } yield new ScParameterizedType(desType, Seq(lType, rType))
 
