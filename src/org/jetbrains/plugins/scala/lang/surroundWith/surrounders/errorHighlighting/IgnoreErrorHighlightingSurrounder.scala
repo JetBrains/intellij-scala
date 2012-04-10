46fba52ae53b9eb3c7bb8611122cb6284c52845f
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-04-10 16:18:23 +0400

    Possibility to disable type-aware error highlighting for any region.

---
+++ src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/errorHighlighting/IgnoreErrorHighlightingSurrounder.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.lang.surroundWith.surrounders.errorHighlighting
+
+import com.intellij.lang.surroundWith.Surrounder
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.openapi.util.TextRange
+
+/**
+ * @author Alefas
+ * @since 10.04.12
+ */
+
+class IgnoreErrorHighlightingSurrounder extends Surrounder {
+  def getTemplateDescription: String = "/*_*/.../*_*/"
+
+  def isApplicable(elements: Array[PsiElement]): Boolean = true
+
+  def surroundElements(project: Project, editor: Editor, elements: Array[PsiElement]): TextRange = {
+    val start = elements(0).getTextRange.getStartOffset
+    val end = elements(elements.length - 1).getTextRange.getEndOffset
+    val text = "/*_*/"
+    editor.getDocument.insertString(end, text)
+    editor.getDocument.insertString(start, text)
+    new TextRange(end + 2 * text.length, end + 2 * text.length)
+  }
+}
