702cb3b656855a1e45376cd88d7c1bc842a084bc
jzaugg <jzaugg@gmail.com>
2012-04-14 00:26:32 +0200

    SCL-4145 SCL-3675 Find usage/rename of package.
     - only working if the directory structure mirrors the package structure.
     - references in qualified access modifiers are updated.
     - manually tested for packages containing a mix of Java/Scala files.

---
+++ src/org/jetbrains/plugins/scala/findUsages/ScalaPackageUsagesSearcher.scala
@@ -0,0 +1,47 @@
+package org.jetbrains.plugins.scala
+package findUsages
+
+import com.intellij.openapi.application.QueryExecutorBase
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi._
+import com.intellij.psi.search._
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.util.Processor
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
+import org.jetbrains.plugins.scala.extensions.toPsiNamedElementExt
+import lang.psi.impl.ScPackageImpl
+
+
+class ScalaPackageUsagesSearcher extends QueryExecutorBase[PsiReference, ReferencesSearch.SearchParameters](true) {
+
+  def processQuery(@NotNull parameters: ReferencesSearch.SearchParameters, @NotNull consumer: Processor[PsiReference]) {
+    val target: PsiElement = parameters.getElementToSearch
+    val scPack = target match {
+      case directory: PsiDirectory =>
+        JavaDirectoryService.getInstance.getPackage(directory) match {
+          case pack: PsiPackage =>
+            ScPackageImpl(pack)
+          case _ => return
+        }
+      case pack: PsiPackage =>
+        ScPackageImpl(pack)
+      case _ => return
+    }
+    val name = scPack.name
+    if (name == null || StringUtil.isEmptyOrSpaces(name)) return
+    val scope: SearchScope = parameters.getEffectiveSearchScope // TODO PsiUtil.restrictScopeToGroovyFiles(parameters.getEffectiveSearchScope)
+    val collector: SearchRequestCollector = parameters.getOptimizer
+    val session: SearchSession = collector.getSearchSession
+    collector.searchWord(name, scope, UsageSearchContext.IN_CODE, true, new MyProcessor(scPack, null, session))
+  }
+
+  private class MyProcessor(myTarget: PsiElement, @Nullable prefix: String, mySession: SearchSession) extends RequestResultProcessor(myTarget, prefix) {
+    def processTextOccurrence(element: PsiElement, offsetInElement: Int, consumer: Processor[PsiReference]): Boolean = {
+      val reference: PsiReference = element.getReference
+      if (reference == null || !reference.isReferenceTo(myTarget)) {
+        true
+      } else consumer.process(reference)
+    }
+  }
+}
