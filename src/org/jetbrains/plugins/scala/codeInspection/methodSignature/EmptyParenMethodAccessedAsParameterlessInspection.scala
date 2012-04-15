e8d8003375fc98ddd2e97794b9c5a3e9f9c24d35
jzaugg <jzaugg@gmail.com>
2012-04-15 14:53:28 +0200

    SCL-3389 Don't warn about missing argument parens in case of eta-expansion.
    This time for generic calls.
    
    def foo[A]() = 1
    foo[Int] : () => Int // okay
    foo[Int]  // warn

--- src/org/jetbrains/plugins/scala/codeInspection/methodSignature/EmptyParenMethodAccessedAsParameterlessInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/EmptyParenMethodAccessedAsParameterlessInspection.scala
@@ -7,13 +7,29 @@ import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.extensions.Parent
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import quickfix.{AddGenericCallParentheses, AddCallParentheses}
-import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
 import org.jetbrains.plugins.scala.lang.psi.types.{ScFunctionType, ScType}
+import org.jetbrains.plugins.scala.lang.psi.types.result.{TypeResult, TypingContext}
 
 /**
-  * Pavel Fatin
-  */
-
+ * Pavel Fatin
+ *
+ * TODO test:
+ * {{{
+ *   object A {
+ *     def foo(): Int = 1
+ *     foo // warn
+ *
+ *     def goo(x: () => Int) = 1
+ *     goo(foo) // okay
+ *
+ *     foo : () => Int // okay
+ *
+ *     def bar[A]() = 0
+ *     bar[Int] // warn
+ *     bar[Int]: () => Any // okay
+ *   }
+ * }}}
+ */
 class EmptyParenMethodAccessedAsParameterlessInspection extends AbstractMethodSignatureInspection(
   "ScalaEmptyParenMethodAccessedAsParameterless", "Empty-paren method accessed as parameterless") {
 
@@ -22,27 +38,26 @@ class EmptyParenMethodAccessedAsParameterlessInspection extends AbstractMethodSi
       e.getParent match {
         case gc: ScGenericCall =>
           ScalaPsiUtil.findCall(gc) match {
-            case None =>
-              e.resolve() match {
-                case (f: ScFunction) if f.isEmptyParen =>
-                  holder.registerProblem(e.nameId, getDisplayName, new AddGenericCallParentheses(gc))
-                case _ =>
-              }
+            case None => check(e, holder, gc.getType(TypingContext.empty))
             case Some(_) =>
           }
         case _: ScMethodCall | _: ScInfixExpr | _: ScPrefixExpr | _: ScUnderscoreSection => // okay
-        case _ => e.resolve() match {
-          case (f: ScFunction) if f.isEmptyParen =>
-            e.getType(TypingContext.empty).toOption.flatMap(ScType.extractFunctionType) match {
-              case Some(ScFunctionType(_, Seq())) =>
-                // might have been eta-expanded to () => A, so don't worn.
-                // this avoids false positives. To be more accurate, we would need an 'etaExpanded'
-                // flag in ScalaResolveResult.
-              case None =>
-                holder.registerProblem(e.nameId, getDisplayName, new AddCallParentheses(e))
-            }
-          case _ =>
-        }
+        case _ => check(e, holder, e.getType(TypingContext.empty))
       }
+  }
+
+  private def check(e: ScReferenceExpression, holder: ProblemsHolder, callType: TypeResult[ScType]) {
+    e.resolve() match {
+      case (f: ScFunction) if f.isEmptyParen =>
+        callType.toOption.flatMap(ScType.extractFunctionType) match {
+          case Some(ScFunctionType(_, Seq())) =>
+          // might have been eta-expanded to () => A, so don't worn.
+          // this avoids false positives. To be more accurate, we would need an 'etaExpanded'
+          // flag in ScalaResolveResult.
+          case None =>
+            holder.registerProblem(e.nameId, getDisplayName, new AddCallParentheses(e))
+        }
+      case _ =>
+    }
   }
 }
