b2a1d40b276d17d910f2d957759d148579c48b53
ksenia.sautina <ksenia.sautina@jetbrains.com>
2012-04-20 14:36:37 +0400

    Intention for replacing 'equals' with '==' and vice versa.

---
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/expression/ReplaceEqualsWithQualityIntention.scala
@@ -0,0 +1,103 @@
+package org.jetbrains.plugins.scala
+package codeInsight.intention.expression
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.openapi.util.TextRange
+import lang.psi.api.expr.{ScReferenceExpression, ScMethodCall, ScInfixExpr}
+import lang.psi.impl.ScalaPsiElementFactory
+import extensions._
+import com.intellij.psi.{PsiDocumentManager, PsiElement}
+
+/**
+ * @author Ksenia.Sautina
+ * @since 4/20/12
+ */
+
+object ReplaceEqualsWithQualityIntention {
+  def familyName = "Replace 'equals' with '=='"
+}
+
+class ReplaceEqualsWithQualityIntention extends PsiElementBaseIntentionAction {
+  def getFamilyName = ReplaceEqualsWithQualityIntention.familyName
+
+  override def getText: String = getFamilyName
+
+  def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
+    val infixExpr: ScInfixExpr = PsiTreeUtil.getParentOfType(element, classOf[ScInfixExpr], false)
+    if (infixExpr != null) {
+      if (infixExpr.operation.nameId.getText != "equals") return false
+
+      val range: TextRange = infixExpr.operation.nameId.getTextRange
+      val offset = editor.getCaretModel.getOffset
+      if (!(range.getStartOffset <= offset && offset <= range.getEndOffset)) return false
+
+      return true
+    } else {
+      val methodCallExpr: ScMethodCall = PsiTreeUtil.getParentOfType(element, classOf[ScMethodCall], false)
+      if (methodCallExpr == null) return false
+
+      if (!methodCallExpr.getInvokedExpr.isInstanceOf[ScReferenceExpression]) return false
+      if (methodCallExpr.getInvokedExpr.asInstanceOf[ScReferenceExpression].nameId.getText != "equals") return false
+
+      val range: TextRange = methodCallExpr.getInvokedExpr.asInstanceOf[ScReferenceExpression].nameId.getTextRange
+      val offset = editor.getCaretModel.getOffset
+      if (!(range.getStartOffset <= offset && offset <= range.getEndOffset)) return false
+      if (((methodCallExpr.getInvokedExpr).asInstanceOf[ScReferenceExpression]).isQualified) return true
+
+      return false
+    }
+    false
+  }
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement) {
+    val infixExpr: ScInfixExpr = PsiTreeUtil.getParentOfType(element, classOf[ScInfixExpr], false)
+
+    if (infixExpr != null) {
+      if (!infixExpr.isValid) return
+
+      val start = infixExpr.getTextRange.getStartOffset
+
+      val expr = new StringBuilder
+      expr.append(infixExpr.getBaseExpr.getText).append(" == ").append(infixExpr.getArgExpr.getText)
+
+      val newInfixExpr = ScalaPsiElementFactory.createExpressionFromText(expr.toString(), element.getManager)
+
+      val size = newInfixExpr.asInstanceOf[ScInfixExpr].operation.nameId.getTextRange.getStartOffset -
+              newInfixExpr.getTextRange.getStartOffset
+
+      inWriteAction {
+        infixExpr.replace(newInfixExpr)
+        editor.getCaretModel.moveToOffset(start + size)
+        PsiDocumentManager.getInstance(project).commitDocument(editor.getDocument)
+      }
+    } else {
+      val methodCallExpr: ScMethodCall = PsiTreeUtil.getParentOfType(element, classOf[ScMethodCall], false)
+      if (methodCallExpr == null || !methodCallExpr.isValid) return
+
+      val start = methodCallExpr.getTextRange.getStartOffset
+
+      val expr = new StringBuilder
+      expr.append(methodCallExpr.asInstanceOf[ScMethodCall].getInvokedExpr.asInstanceOf[ScReferenceExpression].
+              qualifier.get.getText).append(".==").append(methodCallExpr.args.getText)
+
+      val newMethodCallExpr = ScalaPsiElementFactory.createExpressionFromText(expr.toString(), element.getManager)
+
+      val size = newMethodCallExpr.asInstanceOf[ScMethodCall].getInvokedExpr.asInstanceOf[ScReferenceExpression].nameId.
+              getTextRange.getStartOffset - newMethodCallExpr.getTextRange.getStartOffset
+
+      inWriteAction {
+        methodCallExpr.replace(newMethodCallExpr)
+        editor.getCaretModel.moveToOffset(start + size)
+        PsiDocumentManager.getInstance(project).commitDocument(editor.getDocument)
+      }
+
+
+    }
+
+  }
+
+}
+
