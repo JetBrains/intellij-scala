f6db7e7c033abccb619c89767813dcacad2667f6
ksenia.sautina <ksenia.sautina@jetbrains.com>
2012-04-23 21:22:39 +0400

    SCL-4249. Fixed "Remove braces from import statement containing only one import" inspection for aliased imports.

--- src/org/jetbrains/plugins/scala/codeInspection/imports/SingleImportInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/imports/SingleImportInspection.scala
@@ -2,11 +2,10 @@ package org.jetbrains.plugins.scala
 package codeInspection.imports
 
 import com.intellij.psi.PsiElementVisitor
-import lang.psi.api.ScalaElementVisitor
 import lang.psi.api.toplevel.imports.ScImportExpr
 import com.intellij.codeInspection.{ProblemHighlightType, ProblemsHolder, LocalInspectionTool}
 import codeInspection.InspectionBundle
-import lang.psi.api.base.ScStableCodeReferenceElement
+import lang.psi.api.ScalaElementVisitor
 
 /**
  * @author Ksenia.Sautina
@@ -19,8 +18,10 @@ class SingleImportInspection extends LocalInspectionTool {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = {
     new ScalaElementVisitor {
       override def visitImportExpr(importExpr: ScImportExpr) {
-        if (importExpr.selectors.length == 1 &&
-                (importExpr.selectors(0).getLastChild.isInstanceOf[ScStableCodeReferenceElement])) {
+        if (!importExpr.selectorSet.isEmpty && importExpr.selectors.length + (if (importExpr.singleWildcard) 1 else 0) == 1) {
+          if (importExpr.selectors.length == 1 && importExpr.selectors(0).isAliasedImport) {
+            return
+          }
           holder.registerProblem(holder.getManager.createProblemDescriptor(importExpr.selectorSet.get,
             InspectionBundle.message("single.import"),
             new RemoveBracesForSingleImportQuickFix(importExpr),
