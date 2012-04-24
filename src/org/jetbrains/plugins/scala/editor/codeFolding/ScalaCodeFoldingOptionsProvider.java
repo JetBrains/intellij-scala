3984f22c3d81bd6c8eae99f106c52a8eaedaeb42
ksenia.sautina <ksenia.sautina@jetbrains.com>
2012-04-24 20:31:50 +0400

    SCL-4273. Folding for multiline blocks. SCL-1937. Fixed code folding settings.

---
+++ src/org/jetbrains/plugins/scala/editor/codeFolding/ScalaCodeFoldingOptionsProvider.java
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.scala.editor.codeFolding;
+
+/**
+ * @author Ksenia.Sautina
+ * @since 4/24/12
+ */
+
+import com.intellij.application.options.editor.CodeFoldingOptionsProvider;
+import com.intellij.openapi.options.BeanConfigurable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.scala.editor.EditorBundle;
+import org.jetbrains.plugins.scala.settings.ScalaCodeFoldingSettings;
+
+public class ScalaCodeFoldingOptionsProvider extends BeanConfigurable<ScalaCodeFoldingSettings> implements CodeFoldingOptionsProvider {
+
+  public ScalaCodeFoldingOptionsProvider() {
+    super(ScalaCodeFoldingSettings.getInstance());
+    checkBox("CollapseShellComments", EditorBundle.message("checkbox.collapse.shell.comments"));
+    checkBox("CollapseBlockComments", EditorBundle.message("checkbox.collapse.block.comments"));
+    checkBox("CollapseMethodCallBodies", EditorBundle.message("checkbox.collapse.method.call.bodies"));
+    checkBox("CollapseTemplateBodies", EditorBundle.message("checkbox.collapse.template.bodies"));
+    checkBox("CollapseTypeLambdas", EditorBundle.message("checkbox.collapse.type.lambdas"));
+    checkBox("CollapsePackagings", EditorBundle.message("checkbox.collapse.packagings"));
+    checkBox("CollapseMultilineStrings", EditorBundle.message("checkbox.collapse.multiline.strings"));
+    checkBox("CollapseCustomRegions", EditorBundle.message("checkbox.collapse.custom.regions"));
+    checkBox("CollapseMultilineBlocks", EditorBundle.message("checkbox.collapse.multiline.blocks"));
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/settings/ScalaCodeFoldingSettings.java
@@ -0,0 +1,171 @@
+package org.jetbrains.plugins.scala.settings;
+
+/**
+ * @author Ksenia.Sautina
+ * @since 4/24/12
+ */
+
+import com.intellij.codeInsight.folding.CodeFoldingSettings;
+import com.intellij.codeInsight.folding.JavaCodeFoldingSettings;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.components.*;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+@State(
+    name = "ScalaApplicationSettings",
+    storages = {
+        @Storage(
+            id = "scala_folding_settings",
+            file = "$APP_CONFIG$/scala_folding_settings.xml"
+        )}
+)
+
+public class ScalaCodeFoldingSettings implements PersistentStateComponent<ScalaCodeFoldingSettings>, ExportableComponent {
+
+  public boolean FOLD_ARGUMENT_BLOCK = false;
+  public boolean FOLD_TEMPLATE_BODIES = false;
+  public boolean FOLD_SHELL_COMMENTS = true;
+  public boolean FOLD_BLOCK_COMMENTS = false;
+  public boolean FOLD_PACKAGINGS = false;
+  public boolean FOLD_TYPE_LAMBDA = false;
+  public boolean FOLD_MULTILINE_STRING = false;
+  public boolean FOLD_CUSTOM_REGION = false;
+  public boolean FOLD_MULTILINE_BLOCKS = false;
+
+  public boolean isCollapseFileHeaders() {
+    return CodeFoldingSettings.getInstance().COLLAPSE_FILE_HEADER;
+  }
+
+  public void setCollapseFileHeaders(boolean value) {
+    CodeFoldingSettings.getInstance().COLLAPSE_FILE_HEADER = value;
+  }
+
+  public boolean isCollapseImports() {
+    return  CodeFoldingSettings.getInstance().COLLAPSE_IMPORTS;
+  }
+
+  public void setCollapseImports(boolean value) {
+    CodeFoldingSettings.getInstance().COLLAPSE_IMPORTS = value;
+  }
+
+  public boolean isCollapseCustomRegions() {
+    return FOLD_CUSTOM_REGION;
+  }
+
+  public void setCollapseCustomRegions(boolean value) {
+    FOLD_CUSTOM_REGION = value;
+  }
+
+  public boolean isCollapseShellComments() {
+    return FOLD_SHELL_COMMENTS;
+  }
+
+  public void setCollapseShellComments(boolean value) {
+    FOLD_SHELL_COMMENTS = value;
+  }
+
+  public boolean isCollapseBlockComments() {
+    return FOLD_BLOCK_COMMENTS;
+  }
+
+  public void setCollapseBlockComments(boolean value) {
+    FOLD_BLOCK_COMMENTS = value;
+  }
+
+  public boolean isCollapseMethodCall() {
+    return JavaCodeFoldingSettings.getInstance().isCollapseMethods();
+  }
+
+  public void setCollapseMethodCall(boolean value) {
+    JavaCodeFoldingSettings.getInstance().setCollapseMethods(value);
+  }
+
+  public boolean isCollapseMethodCallBodies() {
+    return FOLD_ARGUMENT_BLOCK;
+  }
+
+  public void setCollapseMethodCallBodies(boolean value) {
+    FOLD_ARGUMENT_BLOCK = value;
+  }
+
+  public boolean isCollapseTemplateBodies() {
+    return FOLD_TEMPLATE_BODIES;
+  }
+
+  public void setCollapseTemplateBodies(boolean value) {
+    FOLD_TEMPLATE_BODIES = value;
+  }
+
+  public boolean isCollapseTypeLambdas() {
+    return FOLD_TYPE_LAMBDA;
+  }
+
+  public void setCollapseTypeLambdas(boolean value) {
+    FOLD_TYPE_LAMBDA = value;
+  }
+
+  public boolean isCollapsePackagings() {
+    return FOLD_PACKAGINGS;
+  }
+
+  public void setCollapsePackagings(boolean value) {
+    FOLD_PACKAGINGS = value;
+  }
+
+  public boolean isCollapseMultilineStrings() {
+    return FOLD_MULTILINE_STRING;
+  }
+
+  public void setCollapseMultilineStrings(boolean value) {
+    FOLD_MULTILINE_STRING = value;
+  }
+
+  public boolean isCollapseLineComments() {
+    return JavaCodeFoldingSettings.getInstance().isCollapseEndOfLineComments();
+  }
+
+  public void setCollapseLineComments(boolean value) {
+    JavaCodeFoldingSettings.getInstance().setCollapseEndOfLineComments(value);
+  }
+
+  public boolean isCollapseScalaDocComments() {
+    return CodeFoldingSettings.getInstance().COLLAPSE_DOC_COMMENTS;
+  }
+
+  public void setCollapseScalaDocComments(boolean value) {
+    CodeFoldingSettings.getInstance().COLLAPSE_DOC_COMMENTS = value;
+  }
+
+  public boolean isCollapseMultilineBlocks() {
+    return FOLD_MULTILINE_BLOCKS;
+  }
+
+  public void setCollapseMultilineBlocks(boolean value) {
+    FOLD_MULTILINE_BLOCKS = value;
+  }
+
+  public ScalaCodeFoldingSettings getState() {
+    return this;
+  }
+
+  public void loadState(ScalaCodeFoldingSettings scalaCodeFoldingSettings) {
+    XmlSerializerUtil.copyBean(scalaCodeFoldingSettings, this);
+  }
+
+  public static ScalaCodeFoldingSettings getInstance() {
+    return ServiceManager.getService(ScalaCodeFoldingSettings.class);
+  }
+
+  @NotNull
+  public File[] getExportFiles() {
+    return new File[]{PathManager.getOptionsFile("scala_folding_settings")};
+  }
+
+  @NotNull
+  public String getPresentableName() {
+    return "Code Folding Settings";
+  }
+}
