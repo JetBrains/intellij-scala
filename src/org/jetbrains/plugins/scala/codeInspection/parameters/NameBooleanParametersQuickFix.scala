e9d89058122e77d54e291d14ab2f64990a878a75
ksenia.sautina <ksenia.sautina@jetbrains.com>
2012-05-12 15:59:21 +0400

    SCL-4332. Inspection for detecting and naming boolean parameters.

---
+++ src/org/jetbrains/plugins/scala/codeInspection/parameters/NameBooleanParametersQuickFix.scala
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.scala
+package codeInspection.parameters
+
+import com.intellij.openapi.project.Project
+import com.intellij.codeInspection.{ProblemDescriptor, LocalQuickFix}
+import lang.psi.api.base.ScLiteral
+import lang.psi.api.expr.ScMethodCall
+import org.jetbrains.plugins.scala.util.IntentionUtils
+
+/**
+ * @author Ksenia.Sautina
+ * @since 5/10/12
+ */
+
+class NameBooleanParametersQuickFix(expr: ScMethodCall, element: ScLiteral) extends LocalQuickFix {
+  def getName = "Name boolean parameters"
+
+  def getFamilyName = "Name boolean parameters"
+
+  def applyFix(project: Project, descriptor: ProblemDescriptor) {
+    if (!element.isValid) return
+
+    IntentionUtils.check(project, element) match {
+      case Some(x) => x()
+      case None =>
+    }
+  }
+}
