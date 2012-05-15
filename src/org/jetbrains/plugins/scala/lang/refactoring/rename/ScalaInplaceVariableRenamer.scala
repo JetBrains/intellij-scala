8cb6e258e96841c1deed0ba6ed2048523ecb6d49
jzaugg <jzaugg@gmail.com>
2012-05-15 08:47:05 +0200

    SCL-4336 Disable in-place variable renamer if there are backticked references.

--- src/org/jetbrains/plugins/scala/lang/refactoring/rename/ScalaInplaceVariableRenamer.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/rename/ScalaInplaceVariableRenamer.scala
@@ -4,7 +4,6 @@ package refactoring
 package rename
 
 import _root_.scala.collection.mutable.ArrayBuffer
-import com.intellij.psi.PsiElement
 import com.intellij.refactoring.util.TextOccurrencesUtil
 import com.intellij.usageView.UsageInfo
 import java.util.ArrayList
@@ -17,6 +16,9 @@ import psi.ScalaPsiUtil
 import com.intellij.psi.util.PsiTreeUtil
 import psi.api.expr.ScFunctionExpr
 import psi.api.statements.params.{ScParameters, ScClassParameter, ScParameter}
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.{PsiReference, PsiElement}
+import com.intellij.util.Processor
 
 /**
  * User: Alexander Podkhalyuzin
@@ -40,12 +42,22 @@ object ScalaInplaceVariableRenamer {
           case elem if isOk(elem) => {
             val stringToSearch = name.name
             val usages = new ArrayList[UsageInfo]
+            // See: SCL-4336
+            var hasBackTickedRef = false
             if (stringToSearch != null) {
               TextOccurrencesUtil.addUsagesInStringsAndComments(element, stringToSearch, usages, new TextOccurrencesUtil.UsageInfoFactory {
                 def createUsageInfo(usage: PsiElement, startOffset: Int, endOffset: Int): UsageInfo = new UsageInfo(usage)
               });
+              ReferencesSearch.search(element).forEach(new Processor[PsiReference] {
+                def process(t: PsiReference): Boolean = {
+                  if (t.getElement.getText.contains("`")) {
+                    hasBackTickedRef = true
+                    false
+                  } else true
+                }
+              })
             }
-            usages.isEmpty
+            usages.isEmpty && !hasBackTickedRef
           }
           case _ => false
         }
