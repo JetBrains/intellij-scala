4814fabe0d70231ea4350cdfde49c22b50889217
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-05-17 18:59:46 +0400

    Fixed NPE

--- src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScPatternDefinition.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScPatternDefinition.scala
@@ -27,6 +27,9 @@ trait ScPatternDefinition extends ScValue {
 
 object ScPatternDefinition {
   object expr {
-    def unapply(definition: ScPatternDefinition) = definition.expr
+    def unapply(definition: ScPatternDefinition): Option[ScExpression] = {
+      if (definition == null) None
+      else definition.expr
+    }
   }
 }
