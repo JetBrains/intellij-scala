38c7bcaaa3a66df8a64e7790818c3cfe450917a2
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-05-22 18:22:45 +0400

    RedundantCastInspection

--- src/org/jetbrains/plugins/scala/codeInspection/AbstractInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/AbstractInspection.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.plugins.scala.codeInspection
 
 import com.intellij.codeInspection._
-import org.intellij.lang.annotations.Language
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.extensions.VisitorWrapper
 
@@ -9,6 +8,10 @@ import org.jetbrains.plugins.scala.extensions.VisitorWrapper
  * Pavel Fatin
  */
 abstract class  AbstractInspection(id: String, name: String) extends LocalInspectionTool {
+  def this(name: String) {
+    this(getClass.getSimpleName.stripSuffix("Inspection"), name)
+  }
+
   override def getDisplayName: String = name
 
   override final def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = VisitorWrapper(actionFor(holder))
