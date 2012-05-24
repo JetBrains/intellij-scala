e02b43c2bb865c3ca15d6c8c6cd65f904ce70228
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-05-24 16:40:57 +0400

    Fixed class name completion for expressions in the following case: 1. You have imported immutable.HashSet. 2. You want to import mutable.HashSet with class name completion.

--- test/org/jetbrains/plugins/scala/lang/completion3/ScalaClassNameCompletionTest.scala
+++ test/org/jetbrains/plugins/scala/lang/completion3/ScalaClassNameCompletionTest.scala
@@ -2,6 +2,8 @@ package org.jetbrains.plugins.scala.lang.completion3
 
 import com.intellij.codeInsight.completion.CompletionType
 import org.junit.Assert
+import org.jetbrains.plugins.scala.lang.completion.lookups.ScalaLookupItem
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
 
 /**
  * User: Alefas
@@ -29,6 +31,41 @@ class ScalaClassNameCompletionTest extends ScalaCompletionTestBase {
       """.stripMargin.replaceAll("\r", "").trim()
 
     completeLookupItem(activeLookup.find(le => le.getLookupString == "BLLLL").get, '\t')
+    checkResultByText(resultText)
+  }
+
+  def testClassSameName() {
+    val fileText =
+      """
+        |import collection.immutable.HashSet
+        |
+        |object Sandbox extends App {
+        |  val x: HashSet[Int] = new HashSet[Int]
+        |  HashSet<caret>
+        |}
+      """.stripMargin.replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    val (activeLookup, _) = complete(1, CompletionType.CLASS_NAME)
+
+    val resultText =
+      """
+        |import collection.immutable.HashSet
+        |import collection.mutable
+        |
+        |object Sandbox extends App {
+        |  val x: HashSet[Int] = new HashSet[Int]
+        |  mutable.HashSet<caret>
+        |}
+      """.stripMargin.replaceAll("\r", "").trim()
+
+    completeLookupItem(activeLookup.find {
+      case le: ScalaLookupItem =>
+        le.element match {
+          case c: ScObject if c.qualifiedName == "scala.collection.mutable.HashSet" => true
+          case _ => false
+        }
+      case _ => false
+    }.get, '\t')
     checkResultByText(resultText)
   }
 }
