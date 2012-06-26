589b96fd8d0a011dc02e3e03555c98cac7c26f0e
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-06-26 12:34:47 +0400

    SCL-4232 Removed dollar sign from copy reference for objects feature.

---
+++ src/org/jetbrains/plugins/scala/actions/ScalaQualifiedNameProvider.scala
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.scala.actions
+
+import com.intellij.ide.actions.{JavaQualifiedNameProvider, QualifiedNameProvider}
+import com.intellij.psi.PsiElement
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScTypeDefinition}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
+
+/**
+ * @author Alefas
+ * @since 25.06.12
+ */
+
+class ScalaQualifiedNameProvider extends QualifiedNameProvider {
+  def adjustElementToCopy(element: PsiElement) = null
+
+  def getQualifiedName(element: PsiElement): String = {
+    element match {
+      case clazz: ScTypeDefinition => clazz.qualifiedName
+      case named: ScNamedElement =>
+        val clazz = ScalaPsiUtil.nameContext(named) match {
+          case member: ScMember => member.containingClass
+          case _ => null
+        }
+        if (clazz != null) {
+          clazz.qualifiedName + "#" + named.name
+        } else {
+          named.name
+        }
+      case _ => null
+    }
+  }
+
+  def qualifiedNameToElement(fqn: String, project: Project) = {
+    new JavaQualifiedNameProvider().qualifiedNameToElement(fqn, project) //todo:
+  }
+
+  def insertQualifiedName(fqn: String, element: PsiElement, editor: Editor, project: Project) {
+    new JavaQualifiedNameProvider().insertQualifiedName(fqn, element, editor, project) //todo:
+  }
+}
