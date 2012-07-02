86d9115e8ff18b6699691c6b3ada2f0f29f00a46
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-07-02 17:51:12 +0400

    ScFunctionDefinitionImplTest

---
+++ test/org/jetbrains/plugins/scala/lang/psi/impl/ScFunctionDefinitionImplTest.scala
@@ -0,0 +1,46 @@
+package org.jetbrains.plugins.scala
+package lang.psi.impl
+
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+import org.intellij.lang.annotations.Language
+import org.junit.Assert._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.RecursionType.{OrdinaryRecursion, TailRecursion, NoRecursion}
+import lang.psi.api.statements.{ScFunctionDefinition, RecursionType}
+
+/**
+ * Pavel Fatin
+ */
+class ScFunctionDefinitionImplTest extends SimpleTestCase {
+  def testNoRecursion() {
+    assertRecursionTypeIs("def f(n: Int) = n", NoRecursion)
+  }
+
+  def testLinearRecursion() {
+    assertRecursionTypeIs("def f(n: Int): Int = 1 + f(n)", OrdinaryRecursion)
+  }
+
+  def testTailRecursion() {
+    assertRecursionTypeIs("def f(n: Int): Int = f(n + 1)", TailRecursion)
+  }
+
+  def testReturn() {
+    assertRecursionTypeIs("def f(n: Int): Int = return f(n + 1)", TailRecursion)
+  }
+
+  def testAndAnd() {
+    assertRecursionTypeIs("def f(n: Int): Boolean = n > 0 && f(n)", TailRecursion)
+  }
+
+  def testOrOr() {
+    assertRecursionTypeIs("def f(n: Int): Boolean = n > 0 || f(n)", TailRecursion)
+  }
+
+  def testOtherInfixOperator() {
+    assertRecursionTypeIs("def f(n: Int): Boolean = n > 0 ** f(n)", OrdinaryRecursion)
+  }
+
+  private def assertRecursionTypeIs(@Language("Scala") code: String, expectation: RecursionType) {
+    val function = code.parse[ScFunctionDefinition]
+    assertEquals(expectation, function.recursionType)
+  }
+}
