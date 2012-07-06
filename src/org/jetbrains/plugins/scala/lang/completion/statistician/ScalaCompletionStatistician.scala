7531408406f7ae62545f288fea88fc049a47d6fc
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-07-06 18:10:05 +0400

    Disabled JavaCompletionStatistician.

---
+++ src/org/jetbrains/plugins/scala/lang/completion/statistician/ScalaCompletionStatistician.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.lang.completion.statistician
+
+import com.intellij.psi.statistics.{StatisticsInfo, Statistician}
+import com.intellij.codeInsight.completion.{CompletionLocation, CompletionStatistician}
+import com.intellij.codeInsight.lookup.LookupElement
+
+/**
+ * @author Alefas
+ * @since 06.07.12
+ */
+
+class ScalaCompletionStatistician extends CompletionStatistician {
+  def serialize(element: LookupElement, location: CompletionLocation): StatisticsInfo = {
+    StatisticsInfo.EMPTY //todo:
+  }
+}
