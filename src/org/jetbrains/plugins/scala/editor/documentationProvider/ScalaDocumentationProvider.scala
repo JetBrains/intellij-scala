dc809086abfac52e9d467321e5cc8894c71ac3fb
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-08-08 12:51:09 +0400

    EA-37543

--- src/org/jetbrains/plugins/scala/editor/documentationProvider/ScalaDocumentationProvider.scala
+++ src/org/jetbrains/plugins/scala/editor/documentationProvider/ScalaDocumentationProvider.scala
@@ -3,7 +3,7 @@ package editor.documentationProvider
 
 import com.intellij.codeInsight.javadoc.JavaDocUtil
 import com.intellij.lang.java.JavaDocumentationProvider
-import com.intellij.openapi.module.ModuleUtil
+import com.intellij.openapi.module.{ModuleUtilCore, ModuleUtil}
 import com.intellij.psi._
 import javadoc.{PsiDocTag, PsiDocComment}
 import lang.psi.api.expr.ScAnnotation
@@ -84,7 +84,9 @@ class ScalaDocumentationProvider extends CodeDocumentationProvider {
 
   def generateDoc(element: PsiElement, originalElement: PsiElement): String = {
     if (!element.getContainingFile.isInstanceOf[ScalaFile]) return null
-    val e = getDocedElement(element).getNavigationElement
+    val docedElement = getDocedElement(element)
+    if (docedElement == null) return null
+    val e = docedElement.getNavigationElement
     e match {
       case clazz: ScTypeDefinition => {
         val buffer: StringBuilder = new StringBuilder("")
@@ -733,7 +735,7 @@ object ScalaDocumentationProvider {
 
   def generateClassInfo(clazz: ScTypeDefinition, subst: ScSubstitutor): String = {
     val buffer = new StringBuilder
-    val module = ModuleUtil.findModuleForPsiElement(clazz)
+    val module = ModuleUtilCore.findModuleForPsiElement(clazz)
     if (module != null) {
       buffer.append('[').append(module.getName).append("] ")
     }
