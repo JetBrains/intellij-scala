a13d7cc4200fd11da209ad04a63760ae29a326b4
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-08-10 17:46:24 +0400

    Fixed SCL-4550. 'val' keyword in for statements was deprecated.

--- src/org/jetbrains/plugins/scala/codeInspection/caseClassParamInspection/RemoveValQuickFix.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/caseClassParamInspection/RemoveValQuickFix.scala
@@ -7,6 +7,10 @@ import com.intellij.psi.codeStyle.CodeStyleManager
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScClassParameter
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import com.intellij.codeInspection.{ProblemDescriptor, LocalQuickFix}
+import lang.psi.api.expr.{ScGenerator, ScEnumerator}
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiFile
 
 class RemoveValQuickFix(param: ScClassParameter) extends LocalQuickFix{
   def applyFix(project: Project, descriptor: ProblemDescriptor) {
@@ -20,4 +24,34 @@ class RemoveValQuickFix(param: ScClassParameter) extends LocalQuickFix{
   def getName: String = "Remove 'val'"
 
   def getFamilyName: String = "Remove 'val'"
+}
+
+class RemoveValFromEnumeratorIntentionAction(enum: ScEnumerator) extends IntentionAction {
+  def getText = "Remove unnecessary 'val'"
+
+  def isAvailable(project: Project, editor: Editor, file: PsiFile) = true
+
+  def invoke(project: Project, editor: Editor, file: PsiFile) {
+    if (!enum.isValid) return
+    enum.findChildrenByType(ScalaTokenTypes.kVAL).foreach(_.delete())
+  }
+
+  def startInWriteAction() = true
+
+  def getFamilyName: String = "Remove 'val' from enumerator"
+}
+
+class RemoveValFromGeneratorIntentionAction(enum: ScGenerator) extends IntentionAction {
+  def getText = "Remove unnecessary 'val'"
+
+  def isAvailable(project: Project, editor: Editor, file: PsiFile) = true
+
+  def invoke(project: Project, editor: Editor, file: PsiFile) {
+    if (!enum.isValid) return
+    enum.findChildrenByType(ScalaTokenTypes.kVAL).foreach(_.delete())
+  }
+
+  def startInWriteAction() = true
+
+  def getFamilyName: String = "Remove 'val' from generator"
 }
