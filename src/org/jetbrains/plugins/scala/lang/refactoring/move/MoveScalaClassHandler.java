aba5979e3fa515af69a94181b84900f487440952
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-08-13 14:29:59 +0400

    Move Class refactoring: clean orphan imports

--- src/org/jetbrains/plugins/scala/lang/refactoring/move/MoveScalaClassHandler.java
+++ src/org/jetbrains/plugins/scala/lang/refactoring/move/MoveScalaClassHandler.java
@@ -3,9 +3,10 @@ package org.jetbrains.plugins.scala.lang.refactoring.move;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.javadoc.PsiDocComment;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassHandler;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.util.IncorrectOperationException;
@@ -13,14 +14,11 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.scala.ScalaFileType;
 import org.jetbrains.plugins.scala.conversion.copy.Associations;
 import org.jetbrains.plugins.scala.conversion.copy.ScalaCopyPastePostProcessor;
+import org.jetbrains.plugins.scala.editor.importOptimizer.ScalaImportOptimizer;
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile;
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaRecursiveElementVisitor;
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement;
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 public class MoveScalaClassHandler implements MoveClassHandler {
   private static final ScalaCopyPastePostProcessor PROCESSOR = new ScalaCopyPastePostProcessor();
@@ -37,6 +35,7 @@ public class MoveScalaClassHandler implements MoveClassHandler {
             aClass.getTextRange().getStartOffset(), aClass.getProject());
         aClass.putCopyableUserData(ASSOCIATIONS_KEY, null);
       }
+      new ScalaImportOptimizer().processFile(aClass.getContainingFile()).run();
     }
   }
 
@@ -53,22 +52,19 @@ public class MoveScalaClassHandler implements MoveClassHandler {
 
   public PsiClass doMoveClass(@NotNull final PsiClass aClass, @NotNull PsiDirectory moveDestination) throws IncorrectOperationException {
     PsiFile file = aClass.getContainingFile();
-    final PsiPackage newPackage = JavaDirectoryService.getInstance().getPackage(moveDestination);
 
     PsiClass newClass = null;
     if (file instanceof ScalaFile) {
       if (!moveDestination.equals(file.getContainingDirectory()) &&
            moveDestination.findFile(file.getName()) != null) {
         // moving second of two classes which were in the same file to a different directory (IDEADEV-3089)
-//        correctSelfReferences(aClass, newPackage);
         final PsiFile newFile = moveDestination.findFile(file.getName());
         LOG.assertTrue(newFile != null);
         newClass = (PsiClass)newFile.add(aClass);
-        correctOldClassReferences(newClass, aClass);
+
         aClass.delete();
       }
       else if (((ScalaFile)file).typeDefinitionsArray().length > 1) {
-//        correctSelfReferences(aClass, newPackage);
         final PsiClass created = ScalaDirectoryService.createClassFromTemplate(moveDestination, ((ScNamedElement) aClass).name(), "Scala Class", false);
 //        if (aClass.getDocComment() == null) {
           final PsiDocComment createdDocComment = created.getDocComment();
@@ -78,60 +74,10 @@ public class MoveScalaClassHandler implements MoveClassHandler {
           }
 //        }
         newClass = (PsiClass)created.replace(aClass);
-        correctOldClassReferences(newClass, aClass);
         aClass.delete();
       }
     }
     return newClass;
-  }
-
-  private static void correctOldClassReferences(final PsiClass newClass, final PsiClass oldClass) {
-    final Set<PsiImportStatementBase> importsToDelete = new HashSet<PsiImportStatementBase>();
-    newClass.getContainingFile().accept(new ScalaRecursiveElementVisitor() {
-      @Override
-      public void visitReference(ScReferenceElement reference) {
-        if (reference.isReferenceTo(oldClass)) {
-          final PsiImportStatementBase importStatement = PsiTreeUtil.getParentOfType(reference, PsiImportStatementBase.class);
-          if (importStatement != null) {
-            importsToDelete.add(importStatement);
-            return;
-          }
-          try {
-            reference.bindToElement(newClass);
-          }
-          catch (IncorrectOperationException e) {
-            LOG.error(e);
-          }
-        }
-        super.visitReference(reference);
-      }
-    });
-    for (PsiImportStatementBase importStatement : importsToDelete) {
-      importStatement.delete();
-    }
-  }
-
-  private static void correctSelfReferences(final PsiClass aClass, final PsiPackage newContainingPackage) {
-    final PsiPackage aPackage = JavaDirectoryService.getInstance().getPackage(aClass.getContainingFile().getContainingDirectory());
-    if (aPackage != null) {
-      aClass.accept(new JavaRecursiveElementWalkingVisitor() {
-        @Override
-        public void visitReferenceElement(PsiJavaCodeReferenceElement reference) {
-          if (reference.isQualified() && reference.isReferenceTo(aClass)) {
-            final PsiElement qualifier = reference.getQualifier();
-            if (qualifier instanceof PsiJavaCodeReferenceElement && ((PsiJavaCodeReferenceElement)qualifier).isReferenceTo(aPackage)) {
-              try {
-                ((PsiJavaCodeReferenceElement)qualifier).bindToElement(newContainingPackage);
-              }
-              catch (IncorrectOperationException e) {
-                LOG.error(e);
-              }
-            }
-          }
-          super.visitReferenceElement(reference);
-        }
-      });
-    }
   }
 
   public String getName(PsiClass clazz) {
