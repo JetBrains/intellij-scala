9b6f7ae7ae737cf80cd781105bebc0e523f7290e
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-08-31 18:19:55 +0400

    ArraySizeCallInspection

---
+++ src/org/jetbrains/plugins/scala/codeInspection/arraySize/ArraySizeCallInspection.scala
@@ -0,0 +1,30 @@
+package org.jetbrains.plugins.scala
+package codeInspection.arraySize
+
+import lang.psi.impl.ScalaPsiElementFactory
+import com.intellij.codeInspection.{ProblemHighlightType, ProblemDescriptor, ProblemsHolder}
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import codeInspection.{AbstractFix, AbstractInspection}
+import extensions._
+
+/**
+ * Pavel Fatin
+ */
+
+class ArraySizeCallInspection extends AbstractInspection {
+  def actionFor(holder: ProblemsHolder) = {
+    case reference @ ReferenceTarget(Member("size", "scala.collection.SeqLike")) =>
+      reference.depthFirst.toList.reverse.find(_.getText == "size").foreach { id =>
+        holder.registerProblem(id, "Call to Array.size",
+          ProblemHighlightType.GENERIC_ERROR_OR_WARNING, new QuickFix(id))
+      }
+  }
+
+  private class QuickFix(id: PsiElement) extends AbstractFix("Replace with \"length\"", id) {
+    def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
+      val ref = ScalaPsiElementFactory.createIdentifier("length", id.getManager)
+      id.replace(ref.getPsi)
+    }
+  }
+}
