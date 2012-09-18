417b2e7e7968068b75a779e4f00405e7719112fc
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-09-18 22:53:22 +0400

    test for SCL-3548

--- test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
+++ test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
@@ -25,6 +25,7 @@ class Bug3Test extends ResolveTestBase {
   def testSCL3273() {doTest()}
   def testSCL3374() {doTest()}
   def testSCL3450() {doTest()}
+  def testSCL3548() {doTest()}
   def testSCL3583() {doTest()}
   def testSCL3592A() {doTest()}
   def testSCL3592B() {doTest()}
