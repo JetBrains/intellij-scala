dafc2ed1e1a1f1b7b7d07b6ae0e200be3434d264
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-10-05 16:28:01 +0400

    Fixed testdata

--- testdata/resolve2/element/clash/StatementForValues.scala
+++ testdata/resolve2/element/clash/StatementForValues.scala
@@ -1,5 +1,5 @@
 for (a: String <- List[String]();
      a: String <- List[String]()) {
   println( /* line: 2 */ a.getClass)
-  println(classOf[ /* line: 2 */ a])
+  println(classOf[ /* resolved: false */ a])
 }
