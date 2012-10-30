522c2a8975d06b2cfe6712f807b99df2187511ea
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-10-30 17:19:42 +0400

    Test for SCL-4613

---
+++ testdata/move/scl4613/after/moveRefactoring/container/ObjUser.scala
@@ -0,0 +1,11 @@
+package moveRefactoring.container
+
+import moveRefactoring.foo.A
+import moveRefactoring.bar.B
+
+object ObjUser {
+  def main(args: Array[String]) {
+    val a: A = new A
+    val b: B = new B
+  }
+}
