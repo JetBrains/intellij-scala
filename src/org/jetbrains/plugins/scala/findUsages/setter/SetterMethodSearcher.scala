9c475f0508cd931bbbf1f87f318057f67ef6ea91
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-10-31 18:28:42 +0400

    Fixed PIEAE

--- src/org/jetbrains/plugins/scala/findUsages/setter/SetterMethodSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/setter/SetterMethodSearcher.scala
@@ -8,7 +8,7 @@ import com.intellij.psi._
 import lang.psi.api.statements.ScFunctionDefinition
 import org.jetbrains.plugins.scala.util.ScalaUtil
 import com.intellij.psi.search.{TextOccurenceProcessor, PsiSearchHelper, UsageSearchContext}
-import extensions.Parent
+import extensions.{inReadAction, Parent}
 import lang.psi.api.expr.ScAssignStmt
 import lang.resolve.ResolvableReferenceElement
 
@@ -17,31 +17,32 @@ class SetterMethodSearcher extends QueryExecutor[PsiReference, ReferencesSearch.
   def execute(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor[PsiReference]): Boolean = {
     val scope = queryParameters.getScope
     val element = queryParameters.getElementToSearch
-    val ignoreAccess = queryParameters.isIgnoreAccessScope
     val SetterSuffix = "_="
-    ScalaUtil.readAction(element.getProject) {
-      element match {
-        case fun: ScFunctionDefinition if fun.name endsWith SetterSuffix =>
-          val processor = new TextOccurenceProcessor {
-            def execute(element: PsiElement, offsetInElement: Int): Boolean = {
-              element match {
-                case Parent(Parent(assign: ScAssignStmt)) => assign.getLExpression match {
-                  case ref: ResolvableReferenceElement => ref.bind() match {
-                    case Some(x) if x.element.getNavigationElement == fun =>
-                      if (!consumer.process(ref)) return false
+    inReadAction {
+      if (element.isValid) {
+        element match {
+          case fun: ScFunctionDefinition if fun.name endsWith SetterSuffix =>
+            val processor = new TextOccurenceProcessor {
+              def execute(element: PsiElement, offsetInElement: Int): Boolean = {
+                element match {
+                  case Parent(Parent(assign: ScAssignStmt)) => assign.getLExpression match {
+                    case ref: ResolvableReferenceElement => ref.bind() match {
+                      case Some(x) if x.element.getNavigationElement == fun =>
+                        if (!consumer.process(ref)) return false
+                      case _ =>
+                    }
                     case _ =>
                   }
                   case _ =>
                 }
-                case _ =>
+                true
               }
-              true
             }
-          }
 
-          val helper: PsiSearchHelper = PsiSearchHelper.SERVICE.getInstance(fun.getProject)
-          helper.processElementsWithWord(processor, scope, fun.name.stripSuffix(SetterSuffix), UsageSearchContext.IN_CODE, true)
-        case _ =>
+            val helper: PsiSearchHelper = PsiSearchHelper.SERVICE.getInstance(fun.getProject)
+            helper.processElementsWithWord(processor, scope, fun.name.stripSuffix(SetterSuffix), UsageSearchContext.IN_CODE, true)
+          case _ =>
+        }
       }
     }
     true
