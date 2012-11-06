a1ac28133d543d3d4f1a3d32ac0476ade46f323f
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-11-06 14:05:54 +0400

    Move class to inner now filtered with warning, not with exception.

--- src/org/jetbrains/plugins/scala/lang/refactoring/move/ScalaMoveClassesOrPackagesHandler.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/move/ScalaMoveClassesOrPackagesHandler.scala
@@ -3,12 +3,13 @@ package lang.refactoring.move
 
 import com.intellij.refactoring.move.moveClassesOrPackages.JavaMoveClassesOrPackagesHandler
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
+import com.intellij.psi.{PsiClass, PsiElement}
 import lang.psi.api.ScalaFile
 import lang.psi.light.PsiClassWrapper
-import lang.psi.api.toplevel.typedef.ScObject
+import lang.psi.api.toplevel.typedef.{ScTypeDefinition, ScObject}
 import com.intellij.refactoring.move.MoveCallback
 import lang.psi.impl.ScalaFileImpl
+import com.intellij.openapi.ui.Messages
 
 /**
  * @author Alefas
@@ -16,6 +17,19 @@ import lang.psi.impl.ScalaFileImpl
  */
 class ScalaMoveClassesOrPackagesHandler extends JavaMoveClassesOrPackagesHandler {
   override def doMove(project: Project, elements: Array[PsiElement], targetContainer: PsiElement, callback: MoveCallback) {
+    def refactoringIsNotSupported() {
+      Messages.showErrorDialog(ScalaBundle.message("move.to.inner.is.not.supported"), ScalaBundle.message("move.to.inner.is.not.supported.title"))
+    }
+    targetContainer match {
+      case td: ScTypeDefinition =>
+        refactoringIsNotSupported()
+        return
+      case clazz: PsiClass =>
+        if (elements.find(_.isInstanceOf[ScTypeDefinition]).isDefined) {
+          refactoringIsNotSupported()
+          return
+        }
+    }
     ScalaFileImpl.performMoveRefactoring {
       super.doMove(project, elements, targetContainer, callback)
     }
