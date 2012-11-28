13657bf8bc008b24a142f8d5d2f858c7e7973d2a
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-11-28 17:08:29 +0400

    Rename class improvement, no "Preview Usages" on class with companion object rename with import usage.

--- src/org/jetbrains/plugins/scala/lang/refactoring/rename/ScalaRenameUtil.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/rename/ScalaRenameUtil.scala
@@ -1,13 +1,18 @@
 package org.jetbrains.plugins.scala
 package lang.refactoring.rename
 
-import com.intellij.psi.PsiReference
+import com.intellij.psi.{PsiElement, PsiReference}
 import lang.resolve.ResolvableReferenceElement
-import java.util.Collection
 import collection.JavaConverters.{asJavaCollectionConverter, iterableAsScalaIterableConverter}
+import java.util
+import lang.psi.api.base.ScStableCodeReferenceElement
+import lang.psi.api.toplevel.typedef.ScTypeDefinition
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.util.PsiTreeUtil
+import lang.psi.api.toplevel.imports.ScImportStmt
 
 object ScalaRenameUtil {
-  def filterAliasedReferences(allReferences: Collection[PsiReference]): Collection[PsiReference] = {
+  def filterAliasedReferences(allReferences: util.Collection[PsiReference]): util.Collection[PsiReference] = {
     val filtered = allReferences.asScala.filter {
       case resolvableReferenceElement: ResolvableReferenceElement =>
         resolvableReferenceElement.bind() match {
@@ -20,4 +25,37 @@ object ScalaRenameUtil {
     }
     filtered.asJavaCollection
   }
+
+  def replaceImportClassReferences(allReferences: util.Collection[PsiReference]): util.Collection[PsiReference] = {
+    allReferences.asScala.map {
+      case ref: ScStableCodeReferenceElement =>
+        val isInImport = PsiTreeUtil.getParentOfType(ref, classOf[ScImportStmt]) != null
+        if (isInImport && ref.resolve() == null) {
+          val multiResolve = ref.multiResolve(false)
+          if (multiResolve.length > 1 && multiResolve.forall(_.getElement.isInstanceOf[ScTypeDefinition])) {
+            new PsiReference {
+              def getVariants: Array[AnyRef] = ref.getVariants
+
+              def getCanonicalText: String = ref.getCanonicalText
+
+              def getElement: PsiElement = ref.getElement
+
+              def isReferenceTo(element: PsiElement): Boolean = ref.isReferenceTo(element)
+
+              def bindToElement(element: PsiElement): PsiElement = ref.bindToElement(element)
+
+              def handleElementRename(newElementName: String): PsiElement = ref.handleElementRename(newElementName)
+
+              def isSoft: Boolean = ref.isSoft
+
+              def getRangeInElement: TextRange = ref.getRangeInElement
+
+              def resolve(): PsiElement = multiResolve.apply(0).getElement
+            }
+          } else ref
+        } else ref
+      case ref: PsiReference => ref
+    }.asJavaCollection
+  }
+
 }
