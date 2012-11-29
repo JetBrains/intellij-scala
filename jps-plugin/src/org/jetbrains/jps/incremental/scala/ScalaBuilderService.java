6700736288528587ab5a8148cd4ce902f7f29113
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-11-29 12:20:38 +0400

    JPS: substitute default Java compiler

--- jps-plugin/src/org/jetbrains/jps/incremental/scala/ScalaBuilderService.java
+++ jps-plugin/src/org/jetbrains/jps/incremental/scala/ScalaBuilderService.java
@@ -1,9 +1,15 @@
 package org.jetbrains.jps.incremental.scala;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.jps.incremental.BuilderService;
-import org.jetbrains.jps.incremental.ModuleLevelBuilder;
+import org.jetbrains.jps.builders.BuildOutputConsumer;
+import org.jetbrains.jps.builders.BuildTargetType;
+import org.jetbrains.jps.builders.DirtyFilesHolder;
+import org.jetbrains.jps.builders.java.JavaSourceRootDescriptor;
+import org.jetbrains.jps.incremental.*;
+import org.jetbrains.jps.model.JpsProject;
+import org.jetbrains.jps.model.java.JpsJavaExtensionService;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,5 +21,41 @@ public class ScalaBuilderService extends BuilderService {
   @Override
   public List<? extends ModuleLevelBuilder> createModuleLevelBuilders() {
     return Collections.singletonList(new ScalaBuilder());
+  }
+
+  @NotNull
+  @Override
+  public List<? extends TargetBuilder<?, ?>> createBuilders() {
+    return Collections.singletonList(new StubTargetBuilder());
+  }
+
+  private static class StubTargetBuilder extends TargetBuilder<JavaSourceRootDescriptor, ModuleBuildTarget> {
+    public StubTargetBuilder() {
+      super(Collections.<BuildTargetType<ModuleBuildTarget>>emptyList());
+    }
+
+    @Override
+    public void buildStarted(CompileContext context) {
+      JpsProject project = context.getProjectDescriptor().getProject();
+
+      // Disable default Java compiler for a project with Scala facets
+      JpsJavaExtensionService.getInstance()
+          .getOrCreateCompilerConfiguration(project)
+          .setJavaCompilerId("scala");
+    }
+
+    @Override
+    public void build(@NotNull ModuleBuildTarget target,
+                      @NotNull DirtyFilesHolder<JavaSourceRootDescriptor, ModuleBuildTarget> holder,
+                      @NotNull BuildOutputConsumer outputConsumer,
+                      @NotNull CompileContext context) throws ProjectBuildException, IOException {
+      // do nothing
+    }
+
+    @NotNull
+    @Override
+    public String getPresentableName() {
+      return "Scala Stub Builder";
+    }
   }
 }
