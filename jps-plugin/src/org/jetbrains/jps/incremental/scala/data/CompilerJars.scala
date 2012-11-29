632788ed6334765fe58cc224c63afccacee67c37
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-11-29 12:18:38 +0400

    JPS: Scala compiler is now optional (for building  Java modules)

---
+++ jps-plugin/src/org/jetbrains/jps/incremental/scala/data/CompilerJars.scala
@@ -0,0 +1,9 @@
+package org.jetbrains.jps.incremental.scala
+package data
+
+import java.io.File
+
+/**
+ * @author Pavel Fatin
+ */
+case class CompilerJars(library: File, compiler: File, extra: Seq[File])
