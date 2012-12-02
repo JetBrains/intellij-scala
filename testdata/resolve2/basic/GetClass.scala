beddfc8f90967879cb1dd11b846273dcb6f3d5f6
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2012-12-02 16:19:09 +0400

    Fixed testdata

--- testdata/resolve2/basic/GetClass.scala
+++ testdata/resolve2/basic/GetClass.scala
@@ -3,13 +3,13 @@ class A {
   /* */getClass
   this./* */getClass
   super./* */getClass
-  super[ScalaObject]./* */getClass
+  super[Object]./* */getClass
   val a: A = new A
   a./* */getClass
   val x: AnyRef = new A
   x./* */getClass
   val z: Any = 1
-  z./* resolved: false */getClass
+  z./* resolved: true */getClass
 
   collection.JavaConversions./* */getClass
 }
