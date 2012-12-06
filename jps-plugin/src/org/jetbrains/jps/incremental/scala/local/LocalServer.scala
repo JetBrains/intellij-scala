fe44f84499ee63a22ef32b39caebe48ec31d6afe
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-12-06 14:54:06 +0400

    JPS: store cached compiler factory in LocalServer instance

--- jps-plugin/src/org/jetbrains/jps/incremental/scala/local/LocalServer.scala
+++ jps-plugin/src/org/jetbrains/jps/incremental/scala/local/LocalServer.scala
@@ -10,8 +10,10 @@ import org.jetbrains.jps.incremental.ModuleLevelBuilder.ExitCode
  * @author Pavel Fatin
  */
 class LocalServer extends Server {
+  private var cachedCompilerFactory: Option[CompilerFactory] = None
+
   def compile(sbtData: SbtData, compilerData: CompilerData, compilationData: CompilationData, client: Client): ExitCode = {
-    val compilerFactory = LocalServer.compilerFactory(sbtData)
+    val compilerFactory = compilerFactoryFrom(sbtData)
 
     client.progress("Instantiating compiler...")
     val compiler = compilerFactory.createCompiler(compilerData, client, LocalServer.createAnalysisStore)
@@ -23,18 +25,15 @@ class LocalServer extends Server {
 
     ExitCode.OK
   }
-}
 
-object LocalServer {
-  // Globally cached instance of CompilerFactory
-  private var cachedCompilerFactory: Option[CompilerFactory] = None
-
-  private def compilerFactory(sbtData: SbtData): CompilerFactory = cachedCompilerFactory.getOrElse {
+  private def compilerFactoryFrom(sbtData: SbtData): CompilerFactory = cachedCompilerFactory.getOrElse {
     val factory = new CachingFactory(new CompilerFactoryImpl(sbtData), 5, 5)
     cachedCompilerFactory = Some(factory)
     factory
   }
+}
 
+object LocalServer {
   private def createAnalysisStore(cacheFile: File): AnalysisStore = {
     import sbinary.DefaultProtocol.{immutableMapFormat, immutableSetFormat, StringFormat, tuple2Format}
     import sbt.inc.AnalysisFormats._
