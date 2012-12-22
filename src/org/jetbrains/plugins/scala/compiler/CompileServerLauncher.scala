1843431c0c034ebfacc957c4e675c721a51ff9ea
Pavel Fatin <pavel.fatin@jetbrains.com>
2012-12-22 02:10:47 -0800

    JPS: better reporting of absent compile server libraries

--- src/org/jetbrains/plugins/scala/compiler/CompileServerLauncher.scala
+++ src/org/jetbrains/plugins/scala/compiler/CompileServerLauncher.scala
@@ -5,9 +5,10 @@ import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.projectRoots.{Sdk, JavaSdkType}
 import collection.JavaConverters._
 import com.intellij.util.PathUtil
-import java.io.{FileNotFoundException, File}
+import java.io.File
 import com.intellij.openapi.application.ApplicationManager
 import extensions._
+import com.intellij.notification.{Notifications, NotificationType, Notification}
 
 /**
  * @author Pavel Fatin
@@ -46,40 +47,43 @@ class CompileServerLauncher extends ApplicationComponent {
        sdkType.getVMExecutablePath(sdk)
      }
 
-     val classpath = {
-       val files = {
-         val ideaRoot = (new File(PathUtil.getJarPathForClass(classOf[ApplicationManager]))).getParent
-         val pluginRoot = (new File(PathUtil.getJarPathForClass(getClass))).getParent
-         val jpsRoot = new File(pluginRoot, "jps")
+     val files = {
+       val ideaRoot = (new File(PathUtil.getJarPathForClass(classOf[ApplicationManager]))).getParent
+       val pluginRoot = (new File(PathUtil.getJarPathForClass(getClass))).getParent
+       val jpsRoot = new File(pluginRoot, "jps")
 
-         Seq(
-           new File(ideaRoot, "jps-server.jar"),
-           new File(ideaRoot, "trove4j.jar"),
-           new File(ideaRoot, "util.jar"),
-           new File(pluginRoot, "scala-library.jar"),
-           new File(pluginRoot, "scala-plugin-runners.jar"),
-           new File(pluginRoot, "nailgun.jar"),
-           new File(jpsRoot, "sbt-interface.jar"),
-           new File(jpsRoot, "incremental-compiler.jar"),
-           new File(jpsRoot, "jline.jar"),
-           new File(jpsRoot, "scala-jps-plugin.jar"))
-       }
-
-       files.foreach { file =>
-         if (!file.exists) throw new FileNotFoundException(file.getCanonicalPath)
-       }
-
-       files.map(_.getCanonicalPath).mkString(File.pathSeparator)
+       Seq(
+         new File(ideaRoot, "jps-server.jar"),
+         new File(ideaRoot, "trove4j.jar"),
+         new File(ideaRoot, "util.jar"),
+         new File(pluginRoot, "scala-library.jar"),
+         new File(pluginRoot, "scala-plugin-runners.jar"),
+         new File(pluginRoot, "nailgun.jar"),
+         new File(jpsRoot, "sbt-interface.jar"),
+         new File(jpsRoot, "incremental-compiler.jar"),
+         new File(jpsRoot, "jline.jar"),
+         new File(jpsRoot, "scala-jps-plugin.jar"))
      }
 
-     val commands = java +: "-cp" +: classpath +: jvmParameters :+
-             "org.jetbrains.plugins.scala.nailgun.NailgunRunner" :+ settings.COMPILE_SERVER_PORT
+     files.partition(_.exists) match {
+       case (presentFiles, Seq()) =>
+         val classpath = presentFiles.map(_.getPath).mkString(File.pathSeparator)
 
-     val process = new ProcessBuilder(commands.asJava).start()
+         val commands = java +: "-cp" +: classpath +: jvmParameters :+
+                 "org.jetbrains.plugins.scala.nailgun.NailgunRunner" :+ settings.COMPILE_SERVER_PORT
 
-     instance = Some(ServerInstance(process, settings.COMPILE_SERVER_PORT.toInt))
+         val process = new ProcessBuilder(commands.asJava).start()
 
-     watcher.watch(process)
+         instance = Some(ServerInstance(process, settings.COMPILE_SERVER_PORT.toInt))
+
+         watcher.watch(process)
+
+       case (_, absentFiles) =>
+         val paths = absentFiles.map(_.getPath).mkString(", ")
+
+         Notifications.Bus.notify(new Notification("scala", "Cannot start Scala compile server",
+           "Required file(s) not found: " + paths, NotificationType.ERROR))
+     }
    }
 
    // TODO stop server more gracefully
