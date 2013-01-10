62112c08232b28b4d65b908992d0996c46a8600b
ksenia.sautina <ksenia.sautina@jetbrains.com>
2013-01-10 20:22:41 +0400

    #SCL-5131 Fixed.

--- src/org/jetbrains/plugins/scala/actions/NewScalaScriptAction.scala
+++ src/org/jetbrains/plugins/scala/actions/NewScalaScriptAction.scala
@@ -5,7 +5,9 @@ import com.intellij.openapi.project.DumbAware
 import icons.Icons
 import com.intellij.ide.fileTemplates.actions.CreateFromTemplateAction
 import com.intellij.ide.fileTemplates.FileTemplateManager
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.{LangDataKeys, AnActionEvent}
+import config.ScalaFacet
+import com.intellij.openapi.module.Module
 
 /**
  * Pavel Fatin
@@ -16,6 +18,10 @@ class NewScalaScriptAction
         with DumbAware {
   override def update(e: AnActionEvent) {
     super.update(e)
+    val module: Module = e.getDataContext.getData(LangDataKeys.MODULE.getName).asInstanceOf[Module]
+    val isEnabled: Boolean = if (module == null) false else ScalaFacet.isPresentIn(module)
+    e.getPresentation.setEnabled(isEnabled)
+    e.getPresentation.setVisible(isEnabled)
     e.getPresentation.setIcon(Icons.SCRIPT_FILE_LOGO)
   }
 }
--- src/org/jetbrains/plugins/scala/worksheet/actions/NewScalaWorksheetAction.scala
+++ src/org/jetbrains/plugins/scala/worksheet/actions/NewScalaWorksheetAction.scala
@@ -4,8 +4,10 @@ package worksheet.actions
 import com.intellij.ide.fileTemplates.actions.CreateFromTemplateAction
 import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.openapi.project.DumbAware
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.{LangDataKeys, AnActionEvent}
 import org.jetbrains.plugins.scala.icons.Icons
+import config.ScalaFacet
+import com.intellij.openapi.module.Module
 
 /**
  * @author Ksenia.Sautina
@@ -16,6 +18,10 @@ class NewScalaWorksheetAction extends CreateFromTemplateAction(FileTemplateManag
 with DumbAware {
   override def update(e: AnActionEvent) {
     super.update(e)
+    val module: Module = e.getDataContext.getData(LangDataKeys.MODULE.getName).asInstanceOf[Module]
+    val isEnabled: Boolean = if (module == null) false else ScalaFacet.isPresentIn(module)
+    e.getPresentation.setEnabled(isEnabled)
+    e.getPresentation.setVisible(isEnabled)
     e.getPresentation.setIcon(Icons.WORKSHEET_LOGO)
   }
 }
