a7f44df31b61bc4b137f6da284a126c44d76f7d8
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-01-17 15:41:23 +0400

    EA-42800 Fixed.

--- src/org/jetbrains/plugins/scala/findUsages/factory/ScalaFindUsagesHandlerFactory.scala
+++ src/org/jetbrains/plugins/scala/findUsages/factory/ScalaFindUsagesHandlerFactory.scala
@@ -7,6 +7,12 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinitio
 import com.intellij.psi.{PsiMethod, PsiElement}
 import com.intellij.find.findUsages.{FindUsagesHandlerFactory, JavaFindUsagesHandlerFactory, JavaFindUsagesHandler, FindUsagesHandler}
 import org.jetbrains.plugins.scala.lang.psi.light._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
+import scala.Array
+import com.intellij.openapi.ui.Messages
+import org.jetbrains.plugins.scala.ScalaBundle
+import com.intellij.CommonBundle
+import javax.swing.SwingUtilities
 
 /**
  * User: Alexander Podkhalyuzin
@@ -29,6 +35,45 @@ class ScalaFindUsagesHandlerFactory(project: Project) extends FindUsagesHandlerF
   }
 
   override def createFindUsagesHandler(element: PsiElement, forHighlightUsages: Boolean): FindUsagesHandler = {
-    new ScalaFindUsagesHandler(element)
+    var replacedElement = element match {
+      case wrapper: PsiClassWrapper => wrapper.definition
+      case p: PsiTypedDefinitionWrapper => p.typedDefinition
+      case p: StaticPsiTypedDefinitionWrapper => p.typedDefinition
+      case f: ScFunctionWrapper => f.function
+      case f: FakePsiMethod => f.navElement
+      case s: StaticPsiMethodWrapper => s.method
+      case _ => element
+    }
+    replacedElement match {
+      case function: ScFunction if !forHighlightUsages =>
+        val signs = function.superSignatures
+        if (signs.length == 0 || signs.last.namedElement.isEmpty) Array(function)
+        else {
+          def showDialog() {
+            val result = Messages.showYesNoCancelDialog(element.getProject,
+              ScalaBundle.message("find.usages.method.has.supers", function.name), "Warning", Messages.getQuestionIcon)
+            result match {
+              case 0 =>
+                val elem = signs.last.namedElement.get
+                replacedElement = elem
+              case 1 => //do nothing, it's ok
+              case _ => replacedElement = null
+            }
+          }
+
+          if (SwingUtilities.isEventDispatchThread) {
+            showDialog()
+          } else {
+            SwingUtilities.invokeAndWait(new Runnable {
+              def run() {
+                showDialog()
+              }
+            })
+          }
+        }
+      case _ =>
+    }
+    if (replacedElement == null) return FindUsagesHandler.NULL_HANDLER
+    new ScalaFindUsagesHandler(replacedElement)
   }
 }
