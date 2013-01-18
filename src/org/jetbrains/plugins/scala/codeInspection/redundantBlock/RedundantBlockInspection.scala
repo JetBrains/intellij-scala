223725b0ce36fcd41d2915ba4e93bd4641edbdea
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-01-18 14:31:45 +0400

    #SCL-5158 Fixed

--- src/org/jetbrains/plugins/scala/codeInspection/redundantBlock/RedundantBlockInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/redundantBlock/RedundantBlockInspection.scala
@@ -5,19 +5,36 @@ import com.intellij.codeInspection.{ProblemDescriptor, ProblemsHolder}
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import codeInspection.{AbstractFix, AbstractInspection}
-import lang.psi.api.expr.ScBlockExpr
+import lang.psi.api.expr.{ScThisReference, ScReferenceExpression, ScBlockExpr}
 import extensions._
+import lang.refactoring.util.ScalaNamesUtil
 
 /**
  * Pavel Fatin
  */
 
 class RedundantBlockInspection extends AbstractInspection {
-  private val SimpleBlockPattern = "\\{[A-z]+\\}".r
 
   def actionFor(holder: ProblemsHolder) = {
-    case block: ScBlockExpr if block.getChildren.length == 3 && SimpleBlockPattern.matches(block.getText) =>
-      holder.registerProblem(block, "The enclosing block is redundant", new QuickFix(block))
+    case block: ScBlockExpr if block.getChildren.length == 3 =>
+      val child: PsiElement = block.getChildren.apply(1)
+      val probablyRedundant = child match {
+        case ref: ScReferenceExpression if ref.qualifier.isEmpty => true
+        case t: ScThisReference if t.reference.isEmpty => true
+        case _ => false
+      }
+      if (probablyRedundant) {
+        val next: PsiElement = block.getNextSibling
+        val isRedundant =
+          if (next == null) true
+          else {
+            val refName: String = child.getText + next.getText.apply(0)
+            !ScalaNamesUtil.isIdentifier(refName)
+          }
+        if (isRedundant) {
+          holder.registerProblem(block, "The enclosing block is redundant", new QuickFix(block))
+        }
+      }
   }
 
   private class QuickFix(e: PsiElement) extends AbstractFix("Unwrap the expression", e) {
