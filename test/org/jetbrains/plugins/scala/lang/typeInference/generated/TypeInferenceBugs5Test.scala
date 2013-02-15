12d47b8c9ab02be078a1a180859ce5c1d0dbd9cf
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-02-15 17:32:50 +0400

    Test for #SCL-2493

--- test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
+++ test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
@@ -37,6 +37,8 @@ class TypeInferenceBugs5Test extends TypeInferenceTestBase {
 
   def testSCL2480() {doTest()}
 
+  def testSCL2493() {doTest()}
+
   def testSCL2656() {doTest()}
   
   def testSCL2777A() {doTest()}
