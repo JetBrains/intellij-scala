bdc2e2d7fa17c50fbd995b39f6472caa5bf4fffb
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-02-26 15:10:32 +0400

    Improved testdata for #SCL-5357

--- testdata/resolve2/bug3/SCL5357.scala
+++ testdata/resolve2/bug3/SCL5357.scala
@@ -4,7 +4,16 @@ trait Test {
   }
 
   type Setting <: SettingValue
+  type S1 = Setting
+  val x: S1 {} = ???
 
-  // can't resolve `value`
-  def foo(x: Setting {}) = x./* line: 3 */value
+  // all work now
+  def bar(x: Setting {}) = x./* line: 3 */value
+  def bar2(x: S1 {}) = x./* line: 3 */value
+  def bar3(x: x.type) = x./* line: 3 */value
+
+  // okay
+  def foo(x: SettingValue {}) = x./* line: 3 */value
+  def foo(x: Setting) = x./* line: 3 */value
+
 }
