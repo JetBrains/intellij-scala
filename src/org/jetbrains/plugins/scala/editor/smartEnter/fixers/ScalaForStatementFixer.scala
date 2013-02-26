722c338c36d93c592e1ed56ac316a38552d7879e
ksenia.sautina <ksenia.sautina@jetbrains.com>
2013-02-26 13:59:22 +0400

    For SCL-5179. ScalaForStatementFixer: removed unnecessary comments.

--- src/org/jetbrains/plugins/scala/editor/smartEnter/fixers/ScalaForStatementFixer.scala
+++ src/org/jetbrains/plugins/scala/editor/smartEnter/fixers/ScalaForStatementFixer.scala
@@ -42,53 +42,6 @@ class ScalaForStatementFixer extends ScalaFixer {
     } else if (rightParenthesis == null) {
       doc.insertString(condition.getTextRange.getEndOffset, ")")
     }
-
-    /*if (leftParenthesis == null && rightParenthesis == null) {
-      val stopOffset: Int = doc.getLineEndOffset(doc.getLineNumber(forStatement.getTextRange.getStartOffset))
-      doc.replaceString(forStatement.getTextRange.getStartOffset, stopOffset, "for () {\n}")
-      processor.registerUnresolvedError(forStatement.getTextRange.getStartOffset + "for (".length)
-    }
-    else if (leftParenthesis != null && rightParenthesis == null) {
-      doc.insertString(forStatement.getTextRange.getEndOffset, ") {\n}")
-      processor.registerUnresolvedError(leftParenthesis.getTextRange.getEndOffset)
-    } else {
-      processor.registerUnresolvedError(leftParenthesis.getTextRange.getEndOffset)
-    } */
-
-//    if (leftParenthesis == null || rightParenthesis == null) {
-//      val textRange: TextRange = forStatement.getTextRange
-//      val stopOffset: Int = doc.getLineEndOffset(doc.getLineNumber(forStatement.getTextRange.getStartOffset))
-//      editor.getDocument.replaceString(textRange.getStartOffset, stopOffset, "for () {\n}")
-//      processor.registerUnresolvedError(textRange.getStartOffset + "for (".length)
-//      return
-//    }
-//    val initialization: PsiStatement = forStatement.getInitialization
-//    if (initialization == null) {
-//      processor.registerUnresolvedError(leftParenthesis.getTextRange.getEndOffset)
-//      return
-//    }
-//    val condition: PsiExpression = forStatement.getCondition
-//    if (condition == null) {
-//      registerErrorOffset(editor, processor, initialization, forStatement)
-//      return
-//    }
-//
-//    if (forStatement.getUpdate == null) {
-//      registerErrorOffset(editor, processor, condition, forStatement)
-//    }
-               //todo old version
-    //    if (leftParenthesis == null && rightParenthesis == null) {
-//      val stopOffset: Int = doc.getLineEndOffset(doc.getLineNumber(forStatement.getTextRange.getStartOffset))
-//      //      stopOffset = Math.min(stopOffset, ifStatement.getTextRange.getEndOffset)
-//      doc.replaceString(forStatement.getTextRange.getStartOffset, stopOffset, "for () {\n}")
-//      processor.registerUnresolvedError(forStatement.getTextRange.getStartOffset + "for (".length)
-//    }
-//    else if (leftParenthesis != null && rightParenthesis == null) {
-//      doc.insertString(forStatement.getTextRange.getEndOffset, ") {\n}")
-//      processor.registerUnresolvedError(leftParenthesis.getTextRange.getEndOffset)
-//    } else {
-////      processor.registerUnresolvedError(leftParenthesis.getTextRange.getEndOffset)
-//    }
   }
 
 }
