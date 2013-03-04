f624ad20da8d263f6e90858ff2e4bc517402ae86
ksenia.sautina <ksenia.sautina@jetbrains.com>
2013-03-04 22:09:04 +0400

    Tests for format (complete statement).

--- test/org/jetbrains/plugins/scala/lang/completeStatement/ScalaCompleteFormatTest.scala
+++ test/org/jetbrains/plugins/scala/lang/completeStatement/ScalaCompleteFormatTest.scala
@@ -27,6 +27,56 @@ class ScalaCompleteFormatTest extends ScalaCodeInsightTestBase {
     checkResultByText(resultText)
   }
 
+  def testFormat2() {
+    val fileText =
+      """
+        |class B {
+        |  if (true) {
+        |    val d=7+7+7+7+7
+        |    val dd =6+6+6+6+6<caret>
+        |  }
+        |}
+      """.stripMargin('|').replaceAll("\r", "").trim()
+    val resultText =
+      """
+        |class B {
+        |  if (true) {
+        |    val d=7+7+7+7+7
+        |    val dd = 6 + 6 + 6 + 6 + 6<caret>
+        |  }
+        |}
+      """.stripMargin('|').replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    invokeSmartEnter()
+    checkResultByText(resultText)
+  }
+
+  def testFormat3() {
+    val fileText =
+      """
+        |class B {
+        |  if (true) {
+        |    val d=7+7+7+7+7
+        |    val dd =6+6+6+6+6<caret>
+        |    val ddd =6+6+6+6+6
+        |  }
+        |}
+      """.stripMargin('|').replaceAll("\r", "").trim()
+    val resultText =
+      """
+        |class B {
+        |  if (true) {
+        |    val d=7+7+7+7+7
+        |    val dd = 6 + 6 + 6 + 6 + 6<caret>
+        |    val ddd =6+6+6+6+6
+        |  }
+        |}
+      """.stripMargin('|').replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    invokeSmartEnter()
+    checkResultByText(resultText)
+  }
+
   def testFormatJava() {
     val fileText =
       """
