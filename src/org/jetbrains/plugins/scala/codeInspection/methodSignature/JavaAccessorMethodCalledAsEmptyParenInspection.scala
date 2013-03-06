e83eac665cb0b74027f83e3665307d6e3c4825fc
ksenia.sautina <ksenia.sautina@jetbrains.com>
2013-03-06 18:51:51 +0400

    For SCL-5283.

--- src/org/jetbrains/plugins/scala/codeInspection/methodSignature/JavaAccessorMethodCalledAsEmptyParenInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/JavaAccessorMethodCalledAsEmptyParenInspection.scala
@@ -1,11 +1,12 @@
 package org.jetbrains.plugins.scala
 package codeInspection.methodSignature
 
-import com.intellij.codeInspection._
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScMethodCall, ScReferenceExpression}
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import com.intellij.psi.PsiMethod
 import quickfix.RemoveCallParentheses
+import com.intellij.codeInspection._
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
+import org.jetbrains.plugins.scala.extensions._
 
 /**
  * Pavel Fatin
@@ -19,7 +20,7 @@ class JavaAccessorMethodCalledAsEmptyParenInspection extends AbstractMethodSigna
       case call: ScMethodCall if (call.getParent == null || !call.getParent.isInstanceOf[ScMethodCall]) =>
       e.resolve() match {
         case _: ScalaPsiElement => // do nothing
-        case m: PsiMethod => holder.registerProblem(e.nameId, getDisplayName, new RemoveCallParentheses(call))
+        case (m: PsiMethod) if m.isAccessor => holder.registerProblem(e.nameId, getDisplayName, new RemoveCallParentheses(call))
         case _ =>
       }
       case _ =>
