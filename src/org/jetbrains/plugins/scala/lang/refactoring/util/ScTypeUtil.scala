9f3c0298201880634f5ebf08330bb70c739effff
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-04-15 12:15:24 +0400

    Fixes for SCL-5048 and SCL-5419. Now designator types will be much better. Fixed existize method for block expressions. Parameter designators improved.

--- src/org/jetbrains/plugins/scala/lang/refactoring/util/ScTypeUtil.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/util/ScTypeUtil.scala
@@ -3,7 +3,11 @@ package lang
 package refactoring
 package util
 
-import psi.types.{ScParameterizedType, ScType}
+import org.jetbrains.plugins.scala.lang.psi.types.{ScProjectionType, ScDesignatorType, ScParameterizedType, ScType}
+import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScFieldId
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScBindingPattern
 
 
 /**
@@ -18,5 +22,21 @@ object ScTypeUtil {
   def stripTypeArgs(tp: ScType): ScType = tp match {
     case ScParameterizedType(designator, _) => designator
     case t => t
+  }
+  
+  def removeTypeDesignator(tp: ScType): Option[ScType] = {
+    tp match {
+      case ScDesignatorType(v: ScBindingPattern) => v.getType(TypingContext.empty).toOption.flatMap(removeTypeDesignator)
+      case ScDesignatorType(v: ScFieldId) => v.getType(TypingContext.empty).toOption.flatMap(removeTypeDesignator)
+      case ScDesignatorType(p: ScParameter) => p.getType(TypingContext.empty).toOption.flatMap(removeTypeDesignator)
+      case p: ScProjectionType =>
+        p.actualElement match {
+          case v: ScBindingPattern => v.getType(TypingContext.empty).map(p.actualSubst.subst(_)).toOption.flatMap(removeTypeDesignator)
+          case v: ScFieldId => v.getType(TypingContext.empty).map(p.actualSubst.subst(_)).toOption.flatMap(removeTypeDesignator)
+          case v: ScParameter => v.getType(TypingContext.empty).map(p.actualSubst.subst(_)).toOption.flatMap(removeTypeDesignator)
+          case _ => Some(tp)
+        }
+      case _ => Some(tp)
+    }
   }
 }
