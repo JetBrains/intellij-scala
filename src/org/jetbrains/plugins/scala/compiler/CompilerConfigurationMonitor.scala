7943661687afff7b657671e7b7ebaf60b681a9cf
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-04-19 19:47:02 +0400

    Re-enable parallel compilation. Suppress auto-make only when compile server is enabled.

--- src/org/jetbrains/plugins/scala/compiler/CompilerConfigurationMonitor.scala
+++ src/org/jetbrains/plugins/scala/compiler/CompilerConfigurationMonitor.scala
@@ -15,25 +15,14 @@ import com.intellij.openapi.compiler.{CompilerMessageCategory, CompileContext, C
 class CompilerConfigurationMonitor(project: Project) extends ProjectComponent {
   CompilerManager.getInstance(project).addBeforeTask(new CompileTask {
     def execute(context: CompileContext): Boolean = {
-      val scalaProject = isScalaProject
-      val automake = compilerConfiguration.MAKE_PROJECT_ON_SAVE
-      val parallelCompilation = compilerConfiguration.PARALLEL_COMPILATION
-
-      if (scalaProject) {
-        if (automake) {
-          val message = "Automake is not supported for Scala projects. " +
-                  "Please turn off \"Project Setings / Compiler / Make project automatically\"."
-          context.addMessage(CompilerMessageCategory.ERROR, message, null, -1, -1)
-        }
-
-        if (parallelCompilation) {
-          val message = "Parallel compilation is not supported for Scala projects. " +
-                  "Please turn off \"Project Setings / Compiler / Compile independent modules in parallel\"."
-          context.addMessage(CompilerMessageCategory.ERROR, message, null, -1, -1)
-        }
+      if (isScalaProject && isCompileServerEnabled && isAutomakeEnabled) {
+        val message = "Automake is not supported with Scala compile server. " +
+                "Please either disable the compile server or turn off \"Project Setings / Compiler / Make project automatically\"."
+        context.addMessage(CompilerMessageCategory.ERROR, message, null, -1, -1)
+        false
+      } else {
+        true
       }
-
-      !(scalaProject && (automake || parallelCompilation))
     }
   })
 
@@ -41,7 +30,13 @@ class CompilerConfigurationMonitor(project: Project) extends ProjectComponent {
 
   private def compilerConfiguration = CompilerWorkspaceConfiguration.getInstance(project)
 
+  private def compileServerConfiguration = ScalaApplicationSettings.getInstance
+
   private def isScalaProject = ScalacBackendCompiler.isScalaProject(ModuleManager.getInstance(project).getModules)
+
+  private def isCompileServerEnabled = compileServerConfiguration.COMPILE_SERVER_ENABLED
+
+  private def isAutomakeEnabled = compilerConfiguration.MAKE_PROJECT_ON_SAVE
 
   def getComponentName = getClass.getSimpleName
 
@@ -52,9 +47,8 @@ class CompilerConfigurationMonitor(project: Project) extends ProjectComponent {
   def projectOpened() {
     registry.registerListener(ScalaFacet.Id, FacetListener)
 
-    if (isScalaProject) {
+    if (isScalaProject && isCompileServerEnabled) {
       disableAutomake()
-      disableParallelCompilation()
     }
   }
 
@@ -66,14 +60,11 @@ class CompilerConfigurationMonitor(project: Project) extends ProjectComponent {
     compilerConfiguration.MAKE_PROJECT_ON_SAVE = false
   }
 
-  private def disableParallelCompilation() {
-    compilerConfiguration.PARALLEL_COMPILATION = false
-  }
-
   private object FacetListener extends ProjectWideFacetAdapter[ScalaFacet]() {
     override def facetAdded(facet: ScalaFacet) {
-      disableAutomake()
-      disableParallelCompilation()
+      if (isCompileServerEnabled) {
+        disableAutomake()
+      }
     }
   }
 }
