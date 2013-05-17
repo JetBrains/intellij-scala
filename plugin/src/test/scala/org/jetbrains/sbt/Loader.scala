f0b8edcdc6ad27d64e0725f64a5b985034078c2f
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-05-17 19:24:20 +0400

    Plugin: misc

--- plugin/src/test/scala/org/jetbrains/sbt/Loader.scala
+++ plugin/src/test/scala/org/jetbrains/sbt/Loader.scala
@@ -7,8 +7,9 @@ import scala.io.Source
  * @author Pavel Fatin
  */
 object Loader {
-  private val JavaVM = new File(new File(new File(System.getProperty("java.home")), "bin"), "java").getAbsolutePath
-  private val SbtLauncher = new File("sbt-launch.jar").getAbsolutePath
+  private val JavaVM = canonicalPath(new File(new File(new File(System.getProperty("java.home")), "bin"), "java"))
+  private val SbtLauncher = canonicalPath(new File("sbt-launch.jar"))
+  private val SbtPlugin = canonicalPath(new File("target/scala-2.9.2/sbt-0.12/classes/"))
   private val JavaOpts = Option(System.getenv("JAVA_OPTS")).getOrElse("")
 
   def load(project: File): String = {
@@ -17,8 +18,7 @@ object Loader {
 
     val command = JavaVM + " " + JavaOpts + """ -jar """ + SbtLauncher +
       """ "; set artifactPath := new File(\"""" + canonicalPath(tempFile) +
-      """\") ; apply -cp """ + canonicalPath("""target\scala-2.9.2\sbt-0.12\classes\""") +
-      """ org.jetbrains.sbt.Plugin""""
+      """\") ; apply -cp """ + SbtPlugin + """ org.jetbrains.sbt.Plugin""""
 
     Runtime.getRuntime.exec(command, null, project).waitFor()
 
@@ -26,8 +26,6 @@ object Loader {
 
     Source.fromFile(tempFile).getLines().mkString("\n")
   }
-
-  private def canonicalPath(path: String): String = canonicalPath(new File(path))
 
   private def canonicalPath(file: File): String = file.getAbsolutePath.replace('\\', '/')
 }
