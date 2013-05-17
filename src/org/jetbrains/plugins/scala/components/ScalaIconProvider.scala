42fa151720a32acd4675d78dda9152777cf79644
niktrop <Nikolay.Tropin@jetbrains.com>
2013-05-17 14:45:18 +0400

    Type checks replaced with pattern matching + some cleaning

--- src/org/jetbrains/plugins/scala/components/ScalaIconProvider.scala
+++ src/org/jetbrains/plugins/scala/components/ScalaIconProvider.scala
@@ -13,20 +13,21 @@ class ScalaIconProvider extends IconProvider {
   @Nullable
   override def getIcon(element: PsiElement, flags: Int): Icon = {
     ProgressManager.checkCanceled()
-    if (element == null) return null
-    if (element.isInstanceOf[ScalaFile]) {
-      val file = element.asInstanceOf[ScalaFile]
-      if (file.isWorksheetFile) return Icons.WORKSHEET_LOGO
-      if (file.isScriptFile()) return Icons.SCRIPT_FILE_LOGO
-      if (file.getVirtualFile == null) return Icons.SCRIPT_FILE_LOGO
-      val name = file.getVirtualFile.getNameWithoutExtension
-      val defs = file.typeDefinitions
-      val clazzIterator = defs.iterator
-      while (clazzIterator.hasNext) {
-        val clazz = clazzIterator.next()
-        if (name.equals(clazz.name)) return clazz.getIcon(flags)
-      }
-      if (!defs.isEmpty) return defs(0).getIcon(flags)
+    element match {
+      case null => null
+      case file: ScalaFile =>
+        if (file.isWorksheetFile) return Icons.WORKSHEET_LOGO
+        if (file.isScriptFile()) return Icons.SCRIPT_FILE_LOGO
+        if (file.getVirtualFile == null) return Icons.SCRIPT_FILE_LOGO
+        val name = file.getVirtualFile.getNameWithoutExtension
+        val defs = file.typeDefinitions
+        val clazzIterator = defs.iterator
+        while (clazzIterator.hasNext) {
+          val clazz = clazzIterator.next()
+          if (name.equals(clazz.name)) return clazz.getIcon(flags)
+        }
+        if (!defs.isEmpty) return defs(0).getIcon(flags)
+      case _ =>
     }
     null
   }
