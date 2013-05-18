01d594be21644d2cb66ff6fbc2aca59288809012
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-05-18 14:51:14 +0400

    Handle relative paths in data parser

--- src/org/jetbrains/sbt/project/model/Parser.scala
+++ src/org/jetbrains/sbt/project/model/Parser.scala
@@ -3,47 +3,65 @@ package project.model
 
 import scala.xml.Node
 import java.io.File
+import FS._
 
 /**
  * @author Pavel Fatin
  */
+
+case class FS(home: File, base: Option[File] = None) {
+  def withBase(base: File): FS = copy(base = Some(base))
+}
+
+object FS {
+  def file(path: String)(implicit fs: FS): File = {
+    if (path.startsWith("~/")) {
+      new File(fs.home, path.substring(2))
+    } else {
+      fs.base.map(new File(_, path)).getOrElse(new File(path))
+    }
+  }
+}
+
 object Parser {
-  def parse(node: Node): StructureData = {
+  def parse(node: Node, home: File): StructureData = {
+    implicit val fs = new FS(home)
+
     val project = parseProject(node ! "project")
     val repository = parseRepository(node ! "repository")
 
     StructureData(project, repository)
   }
 
-  private def parseProject(node: Node): ProjectData = {
+  private def parseProject(node: Node)(implicit fs: FS): ProjectData = {
     val name = (node \ "name").text
     val organization = (node \ "organization").text
     val version = (node \ "version").text
     val base = new File((node \ "base").text)
-    val configurations = (node \ "configuration").map(parseConfiguration)
-    val scala = (node \ "scala").headOption.map(parseScala)
+    val configurations = (node \ "configuration").map(parseConfiguration(_)(fs.withBase(base)))
+    val scala = (node \ "scala").headOption.map(parseScala(_)(fs.withBase(base)))
     val projects = (node \ "project").map(parseProject)
 
     ProjectData(name, organization, version, base, configurations, scala, projects)
   }
 
-  private def parseScala(node: Node): ScalaData = {
+  private def parseScala(node: Node)(implicit fs: FS): ScalaData = {
     val version = (node \ "version").text
-    val library = new File((node \ "library").text)
-    val compiler = new File((node \ "compiler").text)
-    val extra = (node \ "extra").map(e => new File(e.text))
+    val library = file((node \ "library").text)
+    val compiler = file((node \ "compiler").text)
+    val extra = (node \ "extra").map(e => file(e.text))
 
     ScalaData(version, library, compiler, extra)
   }
 
-  private def parseConfiguration(node: Node): ConfigurationData = {
+  private def parseConfiguration(node: Node)(implicit fs: FS): ConfigurationData = {
     val id = (node \ "@id").text
-    val sources = (node \ "sources").map(e => new File(e.text))
-    val resources = (node \ "resources").map(e => new File(e.text))
-    val classes = new File((node ! "classes").text)
+    val sources = (node \ "sources").map(e => file(e.text))
+    val resources = (node \ "resources").map(e => file(e.text))
+    val classes = file((node ! "classes").text)
     val dependencies = (node \ "dependency").map(_.text)
     val modules = (node \ "module").map(parseModuleIdentifier)
-    val jars = (node \ "jar").map(e => new File(e.text))
+    val jars = (node \ "jar").map(e => file(e.text))
 
     ConfigurationData(id, sources, resources, classes, dependencies, modules, jars)
   }
@@ -56,13 +74,13 @@ object Parser {
     ModuleIdData(organization, name, revision)
   }
 
-  private def parseRepository(node: Node): RepositoryData = {
+  private def parseRepository(node: Node)(implicit fs: FS): RepositoryData = {
     val modules = (node \ "module").map { it =>
       val identifier = parseModuleIdentifier(node)
 
-      val binaries = (it \ "jar").map(e => new File(e.text))
-      val docs = (it \ "doc").map(e => new File(e.text))
-      val sources = (it \ "src").map(e => new File(e.text))
+      val binaries = (it \ "jar").map(e => file(e.text))
+      val docs = (it \ "doc").map(e => file(e.text))
+      val sources = (it \ "src").map(e => file(e.text))
 
       ModuleData(identifier, binaries, docs, sources)
     }
