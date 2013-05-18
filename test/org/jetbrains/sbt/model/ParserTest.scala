98a1503af4782dab2f793b4f14d124738eeef110
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-05-18 20:08:47 +0400

    ParserTest

---
+++ test/org/jetbrains/sbt/model/ParserTest.scala
@@ -0,0 +1,61 @@
+package org.jetbrains.sbt
+package model
+
+import org.junit.{Test, Assert}
+import scala.xml.XML
+import org.jetbrains.sbt.project.model._
+import java.io.File
+
+/**
+ * @author Pavel Fatin
+ */
+class ParserTest {
+  @Test
+  def testDataParsing() {
+    val xml = XML.load(getClass.getResource("structure.xml"))
+    val actual = Parser.parse(xml, new File("$HOME"))
+    val expected = ParserTest.createExpectedStructure
+
+    Assert.assertEquals(expected, actual)
+  }
+}
+
+object ParserTest {
+  def createExpectedStructure: Structure = {
+    val moduleId = ModuleId("org.scala-lang", "scala-library", "2.10.1")
+
+    val configuration = Configuration(
+      id = "compile",
+      sources = Seq(new File("$BASE/src/main/scala"), new File("$BASE/src/main/java")),
+      resources = Seq(new File("$BASE/src/main/resources")),
+      classes = new File("$BASE/target/scala-2.10/classes"),
+      dependencies = Seq.empty,
+      modules = Seq(moduleId),
+      jars = Seq.empty)
+
+    val scala = Scala(
+      version = "2.10.1",
+      libraryJar = new File("$HOME/.sbt/boot/scala-2.10.1/lib/scala-library.jar"),
+      compilerJar = new File("$HOME/.sbt/boot/scala-2.10.1/lib/scala-compiler.jar"),
+      extraJars = Seq(new File("$HOME/.sbt/boot/scala-2.10.1/lib/scala-reflect.jar")))
+
+    val project = Project(
+      name = "some-name",
+      organization = "some-organization",
+      version = "1.2.3",
+      base = new File("$BASE"),
+      configurations = Seq(configuration),
+      scala = Some(scala),
+      projects = Seq())
+
+    val module = Module(
+      id = moduleId,
+      binaries = Seq(new File("$HOME/.sbt/boot/scala-2.10.1/lib/scala-library.jar")),
+      docs = Seq(new File("$HOME/.ivy2/cache/org.scala-lang/scala-library/docs/scala-library-2.10.1-javadoc.jar")),
+      sources = Seq(new File("$HOME/.ivy2/cache/org.scala-lang/scala-library/srcs/scala-library-2.10.1-sources.jar")))
+
+    val repository = Repository(new File("."), Seq(module))
+
+    Structure(project, repository)
+  }
+}
