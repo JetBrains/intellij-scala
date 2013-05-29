f01e6d647ce418977c2ff5b56f8bdf5d1ef97609
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-05-29 14:03:33 +0400

    SBT constants

---
+++ src/org/jetbrains/sbt/Sbt.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.sbt
+
+import com.intellij.icons.AllIcons
+
+/**
+ * @author Pavel Fatin
+ */
+object Sbt {
+  val Name = "SBT"
+
+  val FileExtension = "sbt"
+
+  val FileDescription = "SBT files"
+
+  val BuildFile = "build.sbt"
+
+  val ProjectDirectory = "project"
+
+  val ProjectDescription = "SBT project"
+
+  val Icon = AllIcons.Actions.Compile
+}
