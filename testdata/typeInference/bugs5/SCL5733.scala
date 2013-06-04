4f8310387fdabc3d088c7918d7ed8b83700ebddf
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-06-04 08:03:21 +0400

    Test for #SCL-5733 Fixed

---
+++ testdata/typeInference/bugs5/SCL5733.scala
@@ -0,0 +1,11 @@
+object SCL5733 {
+  class A
+  class B extends A
+  class C extends A
+  val b = new B
+  val c = new C
+  val seq1 = Seq((b, 1), (c, 1))
+  val seq2 = Seq(b -> 1, c -> 1)
+  val tuple = b -> 1
+  /*start*/(seq1, seq2, tuple)/*end*/
+//(Seq[(SCL5733.A, Int)], Seq[(SCL5733.A, Int)], (SCL5733.B, Int))
