0beb1a051eb9360dcdf107dc771f08a37a661077
niktrop <Nikolay.Tropin@jetbrains.com>
2013-06-11 15:43:59 +0400

    Check for conflicts with class parameters added #SCL-2128 Fixed

--- src/org/jetbrains/plugins/scala/lang/refactoring/util/ScalaVariableValidator.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/util/ScalaVariableValidator.scala
@@ -4,13 +4,14 @@ import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScBindingPattern
 import _root_.scala.collection.mutable.ArrayBuffer
 import com.intellij.openapi.project.Project
 import com.intellij.psi.{PsiFile, PsiElement}
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTypeDefinition, ScTrait, ScClass}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTypeDefinition, ScClass}
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.lang.psi.api.statements._
-import params.{ScParameters, ScParameter}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScClassParameter, ScParameters, ScParameter}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBody
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.editor.Editor
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 
 
 /**
@@ -68,32 +69,44 @@ class ScalaVariableValidator(conflictsReporter: ConflictsReporter,
     conflicts.length == 0 || conflictsReporter.reportConflicts(conflicts, myProject)
   }
 
-  def isOKImpl(name: String, allOcc: Boolean): Array[String] = {
+  def isOKImpl(name: String, allOcc: Boolean): Array[String] =
+    findConflicts(name, allOcc).
+            toSet.filter(_._1 != selectedElement).
+            map(_._2).toArray
+
+  def findConflicts(name: String, allOcc: Boolean): Array[(ScNamedElement, String)] = { //returns declaration and message
     val container = enclosingContainer(allOcc)
     if (container == null) return Array()
-    val buf = new ArrayBuffer[String]
+    val buf = new ArrayBuffer[(ScNamedElement, String)]
     buf ++= validateDown(container, name, allOcc)
     buf ++= validateUp(container, name)
     var cl = container
-    while (cl != null && !cl.isInstanceOf[ScClass] && !cl.isInstanceOf[ScTrait]) cl = cl.getParent
+    while (cl != null && !cl.isInstanceOf[ScTypeDefinition]) cl = cl.getParent
     if (cl != null) {
       cl match {
         case x: ScTypeDefinition => {
           for (member <- x.members) {
             member match {
               case x: ScVariable => for (el <- x.declaredElements if el.name == name)
-                buf += ScalaBundle.message("introduced.variable.will.conflict.with.field", el.name)
+                buf += ((el, messageForField(el.name)))
               case x: ScValue => for (el <- x.declaredElements if el.name == name)
-                buf += ScalaBundle.message("introduced.variable.will.conflict.with.field", el.name)
+                buf += ((el, messageForField(el.name)))
               case _ =>
             }
           }
-          for (function <- x.functions) {
-            function match {
-              case x: ScFunction if x.name == name && x.parameters.size == 0 =>
-                buf += ScalaBundle.message("introduced.variable.will.conflict.with.field", x.name)
-              case _ =>
-            }
+          for (function <- x.functions; if function.name == name && function.parameters.size == 0) {
+            buf += ((x, messageForField(x.name)))
+          }
+          x match {
+            case scClass: ScClass =>
+              for {
+                constructor <- scClass.constructor
+                parameter <- constructor.parameters
+                if parameter.name == name
+              } {
+                buf += ((parameter, messageForClassParameter(parameter.name)))
+              }
+            case _ =>
           }
         }
       }
@@ -101,15 +114,15 @@ class ScalaVariableValidator(conflictsReporter: ConflictsReporter,
     buf.toArray
   }
 
-  private def validateUp(element: PsiElement, name: String): Array[String] = {
-    val buf = new ArrayBuffer[String]
+  private def validateUp(element: PsiElement, name: String): Array[(ScNamedElement, String)] = {
+    val buf = new ArrayBuffer[(ScNamedElement, String)]
     val parent = if (element.getPrevSibling != null) element.getPrevSibling else element.getParent
     element match {
       case x: ScVariableDefinition => {
         val elems = x.declaredElements
         for (elem <- elems) {
           if (elem.name == name) {
-            buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", elem.name)
+            buf += ((elem, messageForLocal(elem.name)))
           }
         }
       }
@@ -117,19 +130,19 @@ class ScalaVariableValidator(conflictsReporter: ConflictsReporter,
         val elems = x.declaredElements
         for (elem <- elems) {
           if (elem.name == name) {
-            buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", elem.name)
+            buf += ((elem, messageForLocal(elem.name)))
           }
         }
       }
       case x: ScParameters => {
         for (parameter <- x.params)
         if (parameter.name == name) {
-          buf += ScalaBundle.message("introduced.variable.will.conflict.with.parameter", parameter.name)
+          buf += ((parameter, messageForParameter(parameter.name)))
         }
       }
       case x: ScFunctionDefinition => {
         if (x.name == name && x.parameters.size == 0) {
-          buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", x.name)
+          buf += ((x, messageForLocal(x.name)))
         }
       }
       case _ =>
@@ -144,26 +157,19 @@ class ScalaVariableValidator(conflictsReporter: ConflictsReporter,
     buf.toArray
   }
 
-  private def validateDown(element: PsiElement, name: String, allOcc: Boolean): Array[String] = {
+  private def validateDown(element: PsiElement, name: String, allOcc: Boolean): Array[(ScNamedElement, String)] = {
     val container = enclosingContainer(allOcc)
-    val buf = new ArrayBuffer[String]
+    val buf = new ArrayBuffer[(ScNamedElement, String)]
     for (child <- element.getChildren) {
       child match {
-        case x: ScParameter => {
-          if (x.name == name) {
-            buf += ScalaBundle.message("introduced.variable.will.conflict.with.parameter", x.name)
-          }
-        }
-        case x: ScFunctionDefinition => {
-          if (x.name == name && x.parameters.size == 0) {
-            buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", x.name)
-          }
-        }
-        case x: ScBindingPattern => {
-          if (x.name == name) {
-            buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", x.name)
-          }
-        }
+        case x: ScClassParameter if x.name == name =>
+          buf += ((x, messageForClassParameter(x.name)))
+        case x: ScParameter if x.name == name =>
+          buf += ((x, messageForParameter(x.name)))
+        case x: ScFunctionDefinition if x.name == name && x.parameters.size == 0 =>
+          buf += (if (x.isLocal) (x, messageForLocal(x.name)) else (x, messageForField(x.name)))
+        case x: ScBindingPattern if x.name == name =>
+          buf += (if (x.isClassMember) (x, messageForField(x.name)) else (x, messageForLocal(x.name)))
         case _ =>
       }
     }
@@ -190,19 +196,13 @@ class ScalaVariableValidator(conflictsReporter: ConflictsReporter,
         fromDoubles match {
           case x: ScVariableDefinition => {
             val elems = x.declaredElements
-            for (elem <- elems) {
-              if (elem.name == name) {
-                buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", elem.name)
-              }
-            }
+            for (elem <- elems; if elem.name == name)
+              buf += (if (x.isLocal) (elem, messageForLocal(elem.name)) else (elem, messageForField(elem.name)))
           }
           case x: ScPatternDefinition => {
             val elems = x.declaredElements
-            for (elem <- elems) {
-              if (elem.name == name) {
-                buf += ScalaBundle.message("introduced.variable.will.conflict.with.local", elem.name)
-              }
-            }
+            for (elem <- elems; if elem.name == name)
+              buf += (if (x.isLocal) (elem, messageForLocal(elem.name)) else (elem, messageForField(elem.name)))
           }
           case _ =>
         }
@@ -219,14 +219,19 @@ class ScalaVariableValidator(conflictsReporter: ConflictsReporter,
   def validateName(name: String, increaseNumber: Boolean): String = {
     if (noOccurrences) return name
     var res = name
-    if (isOKImpl(res, false).length == 0) return res
+    if (isOKImpl(res, allOcc = false).length == 0) return res
     if (!increaseNumber) return ""
     var i = 1
     res = name + i
-    while (!(isOKImpl(res, true).length == 0)) {
+    while (!(isOKImpl(res, allOcc = true).length == 0)) {
       i = i + 1
       res = name + i
     }
     res
   }
+
+  private def messageForField(name: String) = ScalaBundle.message("introduced.variable.will.conflict.with.field", name)
+  private def messageForLocal(name: String) = ScalaBundle.message("introduced.variable.will.conflict.with.local", name)
+  private def messageForParameter(name: String) = ScalaBundle.message("introduced.variable.will.conflict.with.parameter", name)
+  private def messageForClassParameter(name: String) = ScalaBundle.message("introduced.variable.will.conflict.with.class.parameter", name)
 }
