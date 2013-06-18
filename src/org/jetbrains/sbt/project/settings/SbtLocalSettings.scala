a10911bc81ddadff729fade19d1762c41485eda7
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-06-18 16:52:16 +0400

    Persistent SbtSettings

--- src/org/jetbrains/sbt/project/settings/SbtLocalSettings.scala
+++ src/org/jetbrains/sbt/project/settings/SbtLocalSettings.scala
@@ -3,7 +3,7 @@ package project.settings
 
 import com.intellij.openapi.externalSystem.settings.AbstractExternalSystemLocalSettings
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.components.{ServiceManager, State, Storage, StoragePathMacros}
+import com.intellij.openapi.components._
 
 /**
  * @author Pavel Fatin
@@ -14,7 +14,13 @@ import com.intellij.openapi.components.{ServiceManager, State, Storage, StorageP
     new Storage(file = StoragePathMacros.WORKSPACE_FILE)
   )
 )
-class SbtLocalSettings extends AbstractExternalSystemLocalSettings
+class SbtLocalSettings extends AbstractExternalSystemLocalSettings with PersistentStateComponent[AbstractExternalSystemLocalSettings.State] {
+  def getState = {
+    val state = new AbstractExternalSystemLocalSettings.State
+    fillState(state)
+    state
+  }
+}
 
 object SbtLocalSettings {
   def getInstance(project: Project) = ServiceManager.getService(project, classOf[SbtLocalSettings])
