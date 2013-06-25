6b58c8c2c41feab844f963bbdfd4317824ffd867
niktrop <Nikolay.Tropin@jetbrains.com>
2013-06-25 11:37:36 +0400

    Inspections for methods defined like procedure or declared without return type

---
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/DeclarationHasNoExplicitTypeInspection.scala
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.scala
+package codeInspection.methodSignature
+
+import com.intellij.codeInspection.ProblemsHolder
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDeclaration
+import org.jetbrains.plugins.scala.codeInspection.methodSignature.quickfix.AddUnitTypeToDeclaration
+import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
+
+/**
+ * Nikolay.Tropin
+ * 6/24/13
+ */
+class DeclarationHasNoExplicitTypeInspection extends AbstractMethodSignatureInspection(
+  "DeclarationHasNoExplicitType", InspectionBundle.message("declaration.has.no.explicit.type.name")) {
+
+  def actionFor(holder: ProblemsHolder) = {
+    case f: ScFunctionDeclaration if f.hasUnitResultType && !f.hasExplicitType =>
+      holder.registerProblem(f.nameId, getDisplayName, new AddUnitTypeToDeclaration(f))
+  }
+}
---
+++ src/org/jetbrains/plugins/scala/codeInspection/methodSignature/quickfix/InsertReturnTypeAndEquals.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala
+package codeInspection.methodSignature.quickfix
+
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+import org.jetbrains.plugins.scala.codeInspection.{InspectionBundle, AbstractFix}
+import com.intellij.openapi.project.Project
+import com.intellij.codeInspection.ProblemDescriptor
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+
+/**
+ * Nikolay.Tropin
+ * 6/24/13
+ */
+class InsertReturnTypeAndEquals(functionDef: ScFunctionDefinition)
+        extends AbstractFix(InspectionBundle.message("insert.return.type.and.equals"), functionDef) {
+
+  def doApplyFix(project: Project, descriptor: ProblemDescriptor): Unit = {
+    functionDef.removeAssignment()
+    functionDef.removeExplicitType()
+    val manager = functionDef.getManager
+    val fakeDecl = ScalaPsiElementFactory.createDeclaration("x", "Unit", isVariable = false, null, manager)
+    val colon = fakeDecl.findFirstChildByType(ScalaTokenTypes.tCOLON)
+    val assign = fakeDecl.findFirstChildByType(ScalaTokenTypes.tASSIGN)
+    functionDef.addRangeBefore(colon, assign, functionDef.body.get)
+  }
+}
