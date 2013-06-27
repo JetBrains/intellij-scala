f31d430766dba312260663e155b5af290857c525
niktrop <Nikolay.Tropin@jetbrains.com>
2013-06-27 12:47:50 +0400

    Checkbox for ignoring clarifying parentheses added and turned on by default for UnnecessaryParenthesesInspection

--- src/org/jetbrains/plugins/scala/codeInsight/intention/expression/RemoveUnnecessaryParenthesesIntention.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/expression/RemoveUnnecessaryParenthesesIntention.scala
@@ -26,14 +26,15 @@ class RemoveUnnecessaryParenthesesIntention extends PsiElementBaseIntentionActio
 
   def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
     Option(PsiTreeUtil.getParentOfType(element, classOf[ScParenthesisedExpr], false)).exists {
-      UnnecessaryParenthesesUtil.canBeStripped(_)
+      UnnecessaryParenthesesUtil.canBeStripped(_, ignoreClarifying = false)
     }
   }
 
   def invoke(project: Project, editor: Editor, element: PsiElement) {
     Option(PsiTreeUtil.getParentOfType(element, classOf[ScParenthesisedExpr])).map {
-      case expr if UnnecessaryParenthesesUtil.canBeStripped(expr) =>
-        val newExpr = ScalaPsiElementFactory.createExpressionFromText(UnnecessaryParenthesesUtil.getTextOfStripped(expr), expr.getManager)
+      case expr if UnnecessaryParenthesesUtil.canBeStripped(expr, ignoreClarifying = false) =>
+        val stripped: String = UnnecessaryParenthesesUtil.getTextOfStripped(expr, ignoreClarifying = false)
+        val newExpr = ScalaPsiElementFactory.createExpressionFromText(stripped, expr.getManager)
         inWriteAction {
           expr.replaceExpression(newExpr, removeParenthesis = true)
         }
