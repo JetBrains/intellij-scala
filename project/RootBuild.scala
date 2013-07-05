4a73d1921e851d9d01a264f6ef1c2e53ee669c8a
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-07-05 12:11:31 +0400

    Distribution archive creation

--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -2,7 +2,8 @@ import sbt._
 import Keys._
 
 object RootBuild extends Build {
-  lazy val rootProject = Project(id = "root", base = file(".")) aggregate(ideaPluginProject, sbtPluginProject)
+  lazy val rootProject = Project(id = "root", base = file("."), settings = Defaults.defaultSettings :+ distTask)
+    .aggregate(ideaPluginProject, sbtPluginProject)
 
   lazy val ideaPluginProject = Project(id = "idea-plugin", base = file("idea-plugin"))
 
@@ -17,4 +18,31 @@ object RootBuild extends Build {
     sys.env.get("SCALA_PLUGIN_HOME").map(file).getOrElse {
       sys.error("""Please either set "scala-plugin-directory" or define "SCALA_PLUGIN_HOME"""")
     }
+
+  val dist = TaskKey[File]("dist", "Creates a zip file.")
+
+  val distTask = dist <<= (streams,
+    target,
+    name.in(ideaPluginProject),
+    baseDirectory.in(ideaPluginProject),
+    baseDirectory.in(sbtPluginProject),
+    packageBin.in(ideaPluginProject).in(Compile),
+    packageBin.in(sbtPluginProject).in(Compile)) map { (s, target, pluginName, ideaPluginBase, sbtPluginBase, ideaPluginJar, sbtPluginJar) =>
+
+    val files = Seq((ideaPluginJar, "lib/" + pluginName + ".jar"),
+      (ideaPluginBase / "lib" / "external-system.jar", "lib/external-system.jar"),
+      (sbtPluginBase / "sbt-launch.jar", "launcher/sbt-launch.jar"),
+      (sbtPluginJar, "launcher/sbt-structure.jar"))
+
+    val archive = target / (pluginName + ".zip")
+
+    s.log.info("Creating a distribution archive " + archive.getPath + " ...")
+
+    IO.delete(archive)
+    IO.zip(files.map(p => (p._1, pluginName + "/" + p._2)), archive)
+
+    s.log.info("Done creating the distribution archive.")
+
+    archive
+  }
 }
