1b6875d67c5e8a105801eef5c67fe22a5bc1f2e1
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-07-05 10:12:40 +0400

    SBT project definition

--- idea-plugin/build.sbt
+++ idea-plugin/build.sbt
@@ -6,7 +6,6 @@ version := "0.1"
 
 scalaVersion := "2.10.2"
 
-unmanagedJars in Compile <++= baseDirectory map { base =>
-	val dirs = file("d:\\tools\\idea\\lib") +++ file("D:\\Dropbox\\scala-plugin\\classes\\artifacts\\Scala\\lib")
-	(dirs ** "*.jar").classpath
-}
+unmanagedJars in Compile <++= ideaDirectory.map(base => ((base / "lib") ** "*.jar").classpath)
+
+unmanagedJars in Compile <++= scalaPluginDirectory.map(base => ((base / "classes" / "artifacts" / "Scala" / "lib") ** "*.jar").classpath)
--- project/RootBuild.scala
+++ project/RootBuild.scala
@@ -7,4 +7,14 @@ object RootBuild extends Build {
   lazy val ideaPluginProject = Project(id = "idea-plugin", base = file("idea-plugin"))
 
   lazy val sbtPluginProject = Project(id = "sbt-plugin", base = file("sbt-plugin"))
+
+  val ideaDirectory = SettingKey[File]("idea-directory", "The base directory of IDEA.") ??
+    sys.env.get("IDEA_HOME").map(file).getOrElse {
+      sys.error("""Please either set "idea-directory" or define "IDEA_HOME"""")
+    }
+
+  val scalaPluginDirectory = SettingKey[File]("scala-plugin-directory", "The base directory of Scala plugin.") ??
+    sys.env.get("SCALA_PLUGIN_HOME").map(file).getOrElse {
+      sys.error("""Please either set "scala-plugin-directory" or define "SCALA_PLUGIN_HOME"""")
+    }
 }
