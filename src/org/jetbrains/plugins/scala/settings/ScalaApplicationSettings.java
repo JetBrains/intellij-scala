5cdb44a53799aafa27722c43b5bf18408fd3a26b
niktrop <Nikolay.Tropin@jetbrains.com>
2013-07-09 10:54:44 +0400

    Renamed settings

--- src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
+++ src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
@@ -22,14 +22,20 @@ import com.intellij.util.xmlb.XmlSerializerUtil;
 public class ScalaApplicationSettings implements PersistentStateComponent<ScalaApplicationSettings> {
 
   public String DEFAULT_SCALA_LIB_NAME = null;
-  public boolean SPECIFY_TYPE_EXPLICITLY = true;
-  public boolean INTRODUCE_LOCAL_CREATE_VARIABLE = false;
+  public boolean INTRODUCE_VARIABLE_EXPLICIT_TYPE = true;
+  public boolean INTRODUCE_VARIABLE_IS_VAR = false;
+  public boolean INTRODUCE_FIELD_EXPLICIT_TYPE = true;
+  public boolean INTRODUCE_FIELD_IS_VAR = false;
+  public boolean INTRODUCE_FIELD_REPLACE_ALL = false;
+
   public boolean SPECIFY_RETURN_TYPE_EXPLICITLY = true;
   public boolean INTRODUCE_PARAMETER_CREATE_DEFAULT = true;
   public boolean ADD_UNAMBIGUOUS_IMPORTS_ON_THE_FLY = false;
   public boolean SHOW_IMPORT_POPUP = true;
   public int ADD_IMPORTS_ON_PASTE = CodeInsightSettings.ASK;
   public boolean RENAME_SEARCH_IN_COMMENTS_AND_STRINGS = false;
+  public enum AccessLevel {DEFAULT, PROTECTED, PRIVATE}
+  public AccessLevel INTRODUCE_FIELD_MODIFIER = AccessLevel.DEFAULT;
 
   public ScalaApplicationSettings getState() {
     return this;
