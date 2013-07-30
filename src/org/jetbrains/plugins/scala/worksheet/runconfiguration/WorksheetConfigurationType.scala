d531373db6e1737ddaa16c66e4fb7ef0c0ef6a73
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-07-30 19:52:06 +0400

    Migrated location API.

--- src/org/jetbrains/plugins/scala/worksheet/runconfiguration/WorksheetConfigurationType.scala
+++ src/org/jetbrains/plugins/scala/worksheet/runconfiguration/WorksheetConfigurationType.scala
@@ -27,31 +27,4 @@ class WorksheetConfigurationType  extends ConfigurationType {
   def getConfigurationFactories: Array[ConfigurationFactory] = Array[ConfigurationFactory](confFactory)
 
   def getId: String = "WorksheetRunConfiguration"
-
-  def createConfigurationByLocation(location: Location[_ <: PsiElement]): RunnerAndConfigurationSettings = {
-    val file = location.getPsiElement.getContainingFile
-    file match {
-      case scalaFile: ScalaFile if (scalaFile.isWorksheetFile) => {
-        val settings = RunManager.getInstance(location.getProject).createRunConfiguration("WS: " + scalaFile.getName, confFactory)
-        val conf: WorksheetRunConfiguration = settings.getConfiguration.asInstanceOf[WorksheetRunConfiguration]
-        val module = ModuleUtilCore.findModuleForFile(scalaFile.getVirtualFile, scalaFile.getProject)
-        if (module == null || !ScalaFacet.isPresentIn(module)) return null
-        conf.setModule(module)
-        conf.setWorksheetField(scalaFile.getVirtualFile.getPath)
-        settings
-      }
-      case _ => null
-    }
-  }
-
-  def isConfigurationByLocation(configuration: RunConfiguration, location: Location[_ <: PsiElement]): Boolean = {
-    configuration match {
-      case conf: WorksheetRunConfiguration => {
-        val file: PsiFile = location.getPsiElement.getContainingFile
-        if (file == null || !file.isInstanceOf[ScalaFile]) return false
-        conf.getWorksheetField.trim == file.getVirtualFile.getPath.trim
-      }
-      case _ => false
-    }
-  }
 }
