3f5b58e01bfe79a3fa22e0bb5eddba396316bec0
niktrop <Nikolay.Tropin@jetbrains.com>
2013-08-05 10:21:10 +0400

    Added modifier to ScalaDuplexEvaluator

--- src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaDuplexEvaluator.scala
+++ src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaDuplexEvaluator.scala
@@ -13,17 +13,22 @@ import com.intellij.debugger.engine.evaluation.{EvaluateExceptionUtil, Evaluatio
  * Tries to use first evaluator first. If gets exception or null, uses second one.
  */
 class ScalaDuplexEvaluator(first: Evaluator, second: Evaluator) extends Evaluator {
+  private var myModifier: Modifier = null
 
   def evaluate(context: EvaluationContextImpl): AnyRef = {
     var result: AnyRef = null
-    try
+    try {
       result = first.evaluate(context)
+      myModifier = first.getModifier
+    }
     catch {
-      case e: Exception => result = null
+      case e: Exception => result = null; myModifier = null
     }
     if (result == null) {
-      try
+      try {
         result = second.evaluate(context)
+        myModifier = second.getModifier
+      }
       catch {
         case e: Exception => throw EvaluateExceptionUtil.createEvaluateException(e)
       }
@@ -31,5 +36,5 @@ class ScalaDuplexEvaluator(first: Evaluator, second: Evaluator) extends Evaluato
     result
   }
 
-  def getModifier: Modifier = null
+  def getModifier: Modifier = myModifier
 }
