003aa2c2836ee62b401201a0743ea9628eb551ec
Dmitry.Naydanov <dmitry.naydanov@jetbrains.com>
2013-08-09 16:53:43 +0400

    SCL-5961 {Play 2 routes file entry with query param flagged as error: "Cannot resolve: under construction"}

--- src/org/jetbrains/plugins/scala/lang/psi/api/base/patterns/ScPattern.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/base/patterns/ScPattern.scala
@@ -29,6 +29,8 @@ import extensions.toPsiClassExt
  */
 
 trait ScPattern extends ScalaPsiElement {
+  this: PsiNamedElement =>
+  
   def isIrrefutableFor(t: Option[ScType]): Boolean = false
 
   def getType(ctx: TypingContext): TypeResult[ScType] = Failure("Cannot type pattern", Some(this))
