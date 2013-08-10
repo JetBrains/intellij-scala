7664def968e298107cfa6ba1a98be200e01af050
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-08-10 11:06:59 +0400

    Code refactoring (moved method for sharing).

--- src/org/jetbrains/plugins/scala/lang/psi/ScalaPsiUtil.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/ScalaPsiUtil.scala
@@ -518,22 +518,7 @@ object ScalaPsiUtil {
   }
 
   def collectImplicitObjects(_tp: ScType, place: PsiElement): Seq[ScType] = {
-    @tailrec
-    def updateAliases(tp: ScType): ScType = {
-      var updated = false
-      val res = tp.recursiveUpdate { t =>
-        t.isAliasType match {
-          case Some(AliasType(ta, _, upper)) if ta.isInstanceOf[ScTypeAliasDefinition] =>
-            updated = true
-            (true, upper.getOrAny)
-          case _ => (false, t)
-        }
-      }
-      if (!updated) tp
-      else updateAliases(res)
-    }
-
-    val tp = updateAliases(_tp)
+    val tp = ScType.removeAliasDefinitions(_tp)
     val projectOpt = Option(place).map(_.getProject)
     val parts: ListBuffer[ScType] = new ListBuffer[ScType]
     val visited = new mutable.HashSet[ScType]()
