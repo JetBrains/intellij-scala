6e803528244dc8cd43177e500c0a4bad34a9f2bc
niktrop <Nikolay.Tropin@jetbrains.com>
2013-08-19 16:08:47 +0400

    Some refactoring

--- src/org/jetbrains/plugins/scala/codeInsight/generation/ScalaGenerateCompanionObjectAction.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/generation/ScalaGenerateCompanionObjectAction.scala
@@ -1,18 +1,9 @@
 package org.jetbrains.plugins.scala
 package codeInsight.generation
 
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.editor.Editor
-import com.intellij.psi.PsiFile
 
 /**
  * Nikolay.Tropin
  * 8/17/13
  */
-class ScalaGenerateCompanionObjectAction extends ScalaBaseGenerateAction(new ScalaGenerateCompanionObjectHandler) {
-
-  override protected def isValidForFile(project: Project, editor: Editor, file: PsiFile): Boolean =
-    handler.isValidFor(editor, file) && super.isValidForFile(project, editor, file)
-
-
-}
+class ScalaGenerateCompanionObjectAction extends ScalaBaseGenerateAction(new ScalaGenerateCompanionObjectHandler)
