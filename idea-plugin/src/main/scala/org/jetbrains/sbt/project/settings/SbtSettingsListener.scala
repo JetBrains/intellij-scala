dd767f1a468edaa380219c94e466815462c60490
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-08-22 19:55:34 +0400

    SbtSettingsListener simplified

--- idea-plugin/src/main/scala/org/jetbrains/sbt/project/settings/SbtSettingsListener.scala
+++ idea-plugin/src/main/scala/org/jetbrains/sbt/project/settings/SbtSettingsListener.scala
@@ -2,17 +2,8 @@ package org.jetbrains.sbt
 package project.settings
 
 import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListener
-import java.util
 
 /**
  * @author Pavel Fatin
  */
-trait SbtSettingsListener extends ExternalSystemSettingsListener[SbtProjectSettings] {
-  def onProjectsLinked(settings: util.Collection[SbtProjectSettings]) {}
-
-  def onUseAutoImportChange(currentValue: Boolean, linkedProjectPath: String) {}
-
-  def onBulkChangeStart() {}
-
-  def onBulkChangeEnd() {}
-}
+trait SbtSettingsListener extends ExternalSystemSettingsListener[SbtProjectSettings]
