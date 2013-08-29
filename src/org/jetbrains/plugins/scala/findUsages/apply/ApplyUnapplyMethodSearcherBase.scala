bed87ed99e1a968d7e42026139d15cbc4e8040a5
niktrop <Nikolay.Tropin@jetbrains.com>
2013-08-29 19:54:08 +0400

    Apply/Unapply method searchers refactored

---
+++ src/org/jetbrains/plugins/scala/findUsages/apply/ApplyUnapplyMethodSearcherBase.scala
@@ -0,0 +1,46 @@
+package org.jetbrains.plugins.scala
+package findUsages.apply
+
+import com.intellij.util.{Processor, QueryExecutor}
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.search.searches.ReferencesSearch.SearchParameters
+import org.jetbrains.plugins.scala.util.ScalaUtil
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+import org.jetbrains.plugins.scala.lang.resolve.{ResolvableReferenceElement, ScalaResolveResult}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
+
+/**
+ * Nikolay.Tropin
+ * 8/29/13
+ */
+abstract class ApplyUnapplyMethodSearcherBase extends QueryExecutor[PsiReference, ReferencesSearch.SearchParameters] {
+
+  protected val names: Set[String]
+
+  protected def checkAndTransform(ref: PsiReference): Option[ResolvableReferenceElement]
+
+  def execute(queryParameters: SearchParameters, consumer: Processor[PsiReference]): Boolean = {
+    val scope = queryParameters.getEffectiveSearchScope
+    val element = queryParameters.getElementToSearch
+    val ignoreAccess = queryParameters.isIgnoreAccessScope
+    ScalaUtil.readAction(element.getProject) {
+      element match {
+        case fun: ScFunctionDefinition if names.contains(fun.name) =>
+          val processor = new Processor[PsiReference] {
+            def process(ref: PsiReference): Boolean = {
+              checkAndTransform(ref).flatMap(_.bind()) match {
+                case Some(ScalaResolveResult(`fun`, _)) => consumer.process(ref)
+                case _ => true
+              }
+            }
+          }
+          fun.containingClass match {
+            case obj: ScObject => ReferencesSearch.search(obj, scope, ignoreAccess).forEach(processor)
+            case _ => true
+          }
+        case _ => true
+      }
+    }
+  }
+}
