b925318046e8150e531366193a281a258e52c063
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-08-30 21:19:40 +0400

    Use JavaBuilder.addCompilationOptions to create Javac options (cherry picked from commit c56a44b)

--- jps-plugin/src/org/jetbrains/jps/incremental/scala/data/CompilationData.scala
+++ jps-plugin/src/org/jetbrains/jps/incremental/scala/data/CompilationData.scala
@@ -4,13 +4,13 @@ package data
 import java.io.File
 import org.jetbrains.jps.incremental.{ModuleBuildTarget, CompileContext}
 import org.jetbrains.jps.{ProjectPaths, ModuleChunk}
+import org.jetbrains.jps.incremental.java.JavaBuilder
 import org.jetbrains.jps.incremental.scala.SettingsManager
 import collection.JavaConverters._
 import org.jetbrains.jps.incremental.scala.model.Order
 import org.jetbrains.jps.builders.java.JavaModuleBuildTargetType
 import org.jetbrains.jps.model.java.JpsJavaExtensionService
-import org.jetbrains.jps.model.java.LanguageLevel._
-import org.jetbrains.jps.model.module.JpsModule
+import java.util
 import java.util.Collections
 
 /**
@@ -55,56 +55,24 @@ object CompilationData {
           Option(encoding).map(Seq("-encoding", _)).getOrElse(Seq.empty)
         }
 
-        val javaOptions = javaOptionsFor(module)
+        val javaOptions = javaOptionsFor(context, chunk)
 
         CompilationData(sources, classpath, output, commonOptions ++ scalaOptions, commonOptions ++ javaOptions, order, cacheFile, relevantOutputToCacheMap)
       }
     }
   }
 
-  // TODO expect future JPS API to provide a public API for Java options retrieval
-  private def javaOptionsFor(module: JpsModule): Seq[String] = {
-    val config = {
-      val project = module.getProject
+  private def javaOptionsFor(context: CompileContext, chunk: ModuleChunk): Seq[String] = {
+    val annotationProcessingProfile = {
+      val project = context.getProjectDescriptor.getProject
       val compilerConfig = JpsJavaExtensionService.getInstance.getOrCreateCompilerConfiguration(project)
-      compilerConfig.getCurrentCompilerOptions
+      val module = chunk.representativeTarget.getModule
+      compilerConfig.getAnnotationProcessingProfile(module)
     }
 
-    var options: Vector[String] = Vector.empty
-
-    if (config.DEBUGGING_INFO) {
-      options :+= "-g"
-    }
-
-    if (config.DEPRECATION) {
-      options :+= "-deprecation"
-    }
-
-    if (config.GENERATE_NO_WARNINGS) {
-      options :+= "-nowarn"
-    }
-
-    javaLanguageLevelFor(module).foreach { level =>
-      options :+= "-source"
-      options :+= level
-    }
-
-    if (!config.ADDITIONAL_OPTIONS_STRING.isEmpty) {
-      options ++= config.ADDITIONAL_OPTIONS_STRING.split("\\s+").toSeq
-    }
-
-    options
-  }
-
-  private def javaLanguageLevelFor(module: JpsModule): Option[String] = {
-    Option(JpsJavaExtensionService.getInstance.getLanguageLevel(module)).collect {
-      case JDK_1_3 => "1.3"
-      case JDK_1_4 => "1.4"
-      case JDK_1_5 => "1.5"
-      case JDK_1_6 => "1.6"
-      case JDK_1_7 => "1.7"
-      case JDK_1_8 => "8"
-    }
+    val options = new util.ArrayList[String]()
+    JavaBuilder.addCompilationOptions(options, context, chunk, annotationProcessingProfile)
+    options.asScala
   }
 
   private def createOutputToCacheMap(context: CompileContext): Either[String, Map[File, File]] = {
