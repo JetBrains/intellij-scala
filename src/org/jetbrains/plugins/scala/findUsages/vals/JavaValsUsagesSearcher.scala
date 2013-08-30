cc6c57331ca82ab987e286bb0fcebd389211329c
niktrop <Nikolay.Tropin@jetbrains.com>
2013-08-30 11:03:46 +0400

    Minor refactorings in ReferenceSearchers

--- src/org/jetbrains/plugins/scala/findUsages/vals/JavaValsUsagesSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/vals/JavaValsUsagesSearcher.scala
@@ -5,7 +5,6 @@ import com.intellij.util.{Processor, QueryExecutor}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypedDefinition
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScVariable, ScValue}
-import collection.mutable.HashSet
 import org.jetbrains.plugins.scala.lang.psi.fake.FakePsiMethod
 import com.intellij.psi.search.{PsiSearchHelper, UsageSearchContext, TextOccurenceProcessor}
 import com.intellij.psi.{PsiReferenceExpression, PsiElement, PsiReference}
@@ -29,19 +28,17 @@ class JavaValsUsagesSearcher extends QueryExecutor[PsiReference, ReferencesSearc
           case vals: ScTypedDefinition if ScalaPsiUtil.nameContext(vals).isInstanceOf[ScValue] ||
                   ScalaPsiUtil.nameContext(vals).isInstanceOf[ScVariable] =>
             val name: String = vals.getName
-            val collectedReferences: HashSet[PsiReference] = new HashSet[PsiReference]
             val processor = new TextOccurenceProcessor {
               def execute(element: PsiElement, offsetInElement: Int): Boolean = {
                 val references = element.getReferences
-                for (ref <- references if ref.getRangeInElement.contains(offsetInElement) && !collectedReferences.contains(ref)) {
+                for (ref <- references if ref.getRangeInElement.contains(offsetInElement)) {
                   ref match {
                     case refElement: PsiReferenceExpression => {
                       refElement.resolve match {
                         case f: FakePsiMethod if f.navElement == vals =>
                           if (!consumer.process(refElement)) return false
-                        case t: PsiTypedDefinitionWrapper if t.getNavigationElement == vals =>
-                          if (!consumer.process(refElement)) return false
-                        case t: StaticPsiTypedDefinitionWrapper if t.getNavigationElement == vals =>
+                        case t @ (_: StaticPsiTypedDefinitionWrapper | _: PsiTypedDefinitionWrapper)
+                          if t.getNavigationElement == vals =>
                           if (!consumer.process(refElement)) return false
                         case _ =>
                       }
@@ -56,18 +53,17 @@ class JavaValsUsagesSearcher extends QueryExecutor[PsiReference, ReferencesSearc
             helper.processElementsWithWord(processor, scope, name, UsageSearchContext.IN_CODE, true)
           case wrapper: PsiTypedDefinitionWrapper => //only this is added for find usages factory
             val name: String = wrapper.getName
-            val collectedReferences: HashSet[PsiReference] = new HashSet[PsiReference]
             val processor = new TextOccurenceProcessor {
               def execute(element: PsiElement, offsetInElement: Int): Boolean = {
                 val references = element.getReferences
-                for (ref <- references if ref.getRangeInElement.contains(offsetInElement) && !collectedReferences.contains(ref)) {
+                for (ref <- references if ref.getRangeInElement.contains(offsetInElement)) {
                   ref match {
                     case refElement: PsiReferenceExpression => {
                       refElement.resolve match {
                         case t: PsiTypedDefinitionWrapper if t.getNavigationElement == wrapper.getNavigationElement &&
                           t.getName == wrapper.getName =>
                           if (!consumer.process(refElement)) return false
-                        case t: StaticPsiTypedDefinitionWrapper if t.getNavigationElement == t.getNavigationElement &&
+                        case t: StaticPsiTypedDefinitionWrapper if t.getNavigationElement == wrapper.getNavigationElement &&
                           t.getName == wrapper.getName =>
                           if (!consumer.process(refElement)) return false
                         case _ =>
