3e362b62b80e1f5a108c5a59c1f1e259bf95be8b
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-09-04 16:32:06 +0400

    Parameters completion for super constructors and super method calls.

---
+++ src/org/jetbrains/plugins/scala/lang/completion/SameSignatureCallParametersProvider.scala
@@ -0,0 +1,179 @@
+package org.jetbrains.plugins.scala
+package lang.completion
+
+import com.intellij.codeInsight.completion._
+import com.intellij.patterns.PlatformPatterns
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScSuperReference, ScMethodCall, ScArgumentExprList, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.base.{ScMethodLike, ScConstructor}
+import com.intellij.util.ProcessingContext
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScTemplateDefinition}
+import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
+import org.jetbrains.plugins.scala.lang.psi.types.ScType
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScParameterizedTypeElement
+import com.intellij.psi.{PsiParameter, PsiMethod, PsiClass}
+import scala.collection.mutable.ArrayBuffer
+import org.jetbrains.plugins.scala.icons.Icons
+import com.intellij.ui.LayeredIcon
+import com.intellij.codeInsight.lookup.{LookupElement, LookupElementBuilder}
+import org.jetbrains.plugins.scala.lang.psi.types.result.Success
+import org.jetbrains.plugins.scala.extensions.toPsiNamedElementExt
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
+import org.jetbrains.plugins.scala.lang.resolve.ScalaResolveResult
+
+/**
+ * @author Alefas
+ * @since 03.09.13
+ */
+class SameSignatureCallParametersProvider extends CompletionContributor {
+  val constructorFilter = PlatformPatterns.psiElement().withParent(classOf[ScReferenceExpression]).
+          withSuperParent(2, classOf[ScArgumentExprList]).withSuperParent(3, classOf[ScConstructor])
+
+  val superCallFilter = PlatformPatterns.psiElement().withParent(classOf[ScReferenceExpression]).
+          withSuperParent(2, classOf[ScArgumentExprList]).withSuperParent(3, classOf[ScMethodCall])
+
+  extend(CompletionType.BASIC, constructorFilter, new CompletionProvider[CompletionParameters]() {
+    def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+      addConstructorCompletions(parameters, result)
+    }
+  })
+
+  extend(CompletionType.SMART, constructorFilter, new CompletionProvider[CompletionParameters]() {
+    def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+      addConstructorCompletions(parameters, result)
+    }
+  })
+
+  extend(CompletionType.BASIC, superCallFilter, new CompletionProvider[CompletionParameters]() {
+    def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+      addSuperCallCompletions(parameters, result)
+    }
+  })
+
+  extend(CompletionType.SMART, superCallFilter, new CompletionProvider[CompletionParameters]() {
+    def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+      addSuperCallCompletions(parameters, result)
+    }
+  })
+
+  private def addSuperCallCompletions(parameters: CompletionParameters, result: CompletionResultSet): Unit = {
+    val position = parameters.getPosition
+    val elementType = position.getNode.getElementType
+    if (elementType != ScalaTokenTypes.tIDENTIFIER) return
+    val call = PsiTreeUtil.getParentOfType(position, classOf[ScMethodCall])
+    val index = PsiTreeUtil.getParentOfType(position, classOf[ScArgumentExprList]).invocationCount - 1
+    call.deepestInvokedExpr match {
+      case ref: ScReferenceExpression =>
+        ref.qualifier match {
+          case Some(s: ScSuperReference) =>
+            val function = PsiTreeUtil.getParentOfType(ref, classOf[ScFunction])
+            if (function.name == ref.refName) {
+              val variants = ref.getSimpleVariants(implicits = false, filterNotNamedVariants = false)
+              val signatures = variants.toSeq.map {
+                case ScalaResolveResult(fun: ScMethodLike, subst) =>
+                  val params = fun.effectiveParameterClauses
+                  if (params.length > index) params(index).parameters.map(p =>
+                    (p.name, subst.subst(p.getType(TypingContext.empty).getOrAny)))
+                  else Seq.empty
+                case ScalaResolveResult(method: PsiMethod, subst) =>
+                  if (index != 0) Seq.empty
+                  else method.getParameterList.getParameters.toSeq.map {
+                    case p: PsiParameter =>
+                      (p.name, subst.subst(ScType.create(p.getType, position.getProject, position.getResolveScope)))
+                  }
+                case _ => Seq.empty
+              }.filter(_.length > 1)
+
+              if (signatures.isEmpty) return
+
+              checkSignatures(signatures, function, result)
+            } else return
+          case _ => return
+        }
+      case _ => return
+    }
+  }
+
+  private def addConstructorCompletions(parameters: CompletionParameters, result: CompletionResultSet): Unit = {
+    val position = parameters.getPosition
+    val elementType = position.getNode.getElementType
+    if (elementType != ScalaTokenTypes.tIDENTIFIER) return
+    PsiTreeUtil.getParentOfType(position, classOf[ScTemplateDefinition]) match {
+      case c: ScClass =>
+        val args = PsiTreeUtil.getParentOfType(position, classOf[ScArgumentExprList])
+        val constructor = args.getParent.asInstanceOf[ScConstructor]
+        val index = constructor.arguments.indexOf(args)
+        val typeElement = constructor.typeElement
+        typeElement.getType(TypingContext.empty) match {
+          case Success(tp, _) =>
+            val signatures = ScType.extractClassType(tp, Some(position.getProject)) match {
+              case Some((clazz: ScClass, subst)) if !clazz.hasTypeParameters || (clazz.hasTypeParameters &&
+                      typeElement.isInstanceOf[ScParameterizedTypeElement]) =>
+                clazz.constructors.toSeq.map {
+                  case fun: ScMethodLike =>
+                    val params = fun.effectiveParameterClauses
+                    if (params.length > index) params(index).parameters.map(p =>
+                      (p.name, subst.subst(p.getType(TypingContext.empty).getOrAny)))
+                    else Seq.empty
+                }.filter(_.length > 1)
+              case Some((clazz: PsiClass, subst)) if !clazz.hasTypeParameters || (clazz.hasTypeParameters &&
+                      typeElement.isInstanceOf[ScParameterizedTypeElement]) =>
+                clazz.getConstructors.toSeq.map {
+                  case c: PsiMethod =>
+                    if (index != 0) Seq.empty
+                    else c.getParameterList.getParameters.toSeq.map {
+                      case p: PsiParameter =>
+                        (p.name, subst.subst(ScType.create(p.getType, typeElement.getProject, typeElement.getResolveScope)))
+                    }
+                }.filter(_.length > 1)
+              case _ => Seq.empty
+            }
+
+            if (signatures.isEmpty) return
+
+            c.constructor match {
+              case Some(constr) => checkSignatures(signatures, constr, result)
+              case _ => return
+            }
+          case _ => return
+        }
+      case _ =>
+    }
+  }
+
+  private def checkSignatures(signatures: Seq[Seq[(String, ScType)]], methodLike: ScMethodLike, result: CompletionResultSet): Unit = {
+    for (signature <- signatures) {
+      val names = new ArrayBuffer[String]()
+      val res = signature.map {
+        case (name: String, tp: ScType) =>
+          methodLike.parameterList.params.find(_.name == name) match {
+            case Some(param) if param.getType(TypingContext.empty).getOrAny.conforms(tp) =>
+              names += name
+              name
+            case _ => names += ""
+          }
+      }.mkString(", ")
+      if (!names.contains("")) {
+        val w: Int = Icons.PARAMETER.getIconWidth
+        val icon: LayeredIcon = new LayeredIcon(2)
+        icon.setIcon(Icons.PARAMETER, 0, 2 * w / 5, 0)
+        icon.setIcon(Icons.PARAMETER, 1)
+        val element = LookupElementBuilder.create(res).withIcon(icon).withInsertHandler(new InsertHandler[LookupElement] {
+          def handleInsert(context: InsertionContext, item: LookupElement) {
+            val completionChar = context.getCompletionChar
+            if (completionChar == ')') return
+
+            val file = context.getFile
+            val element = file.findElementAt(context.getStartOffset)
+            val exprs = PsiTreeUtil.getParentOfType(element, classOf[ScArgumentExprList])
+            if (exprs == null) return
+            context.getEditor.getCaretModel.moveToOffset(exprs.getTextRange.getEndOffset) // put caret after )
+          }
+        })
+        element.putUserData(JavaCompletionUtil.SUPER_METHOD_PARAMETERS, java.lang.Boolean.TRUE)
+        result.addElement(element)
+      }
+    }
+  }
+}
---
+++ test/org/jetbrains/plugins/scala/lang/completion3/ScalaSuperParametersTest.scala
@@ -0,0 +1,177 @@
+package org.jetbrains.plugins.scala
+package lang.completion3
+
+import org.jetbrains.plugins.scala.codeInsight.ScalaCodeInsightTestBase
+import com.intellij.codeInsight.completion.CompletionType
+
+/**
+ * @author Alefas
+ * @since 04.09.13
+ */
+class ScalaSuperParametersTest extends ScalaCodeInsightTestBase {
+  def testConstructorCall() {
+    val fileText =
+      """
+class TUI {
+  class A(x: Int, y: Int) {
+    def this(x: Int, y: Int, z: Int) = this(x, y)
+  }
+
+  class B(x: Int, y: Int, z: Int) extends A(<caret>)
+}
+      """.replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    val (activeLookup, _) = complete(1, CompletionType.BASIC)
+
+    val resultText =
+      """
+class TUI {
+  class A(x: Int, y: Int) {
+    def this(x: Int, y: Int, z: Int) = this(x, y)
+  }
+
+  class B(x: Int, y: Int, z: Int) extends A(x, y, z)<caret>
+}
+      """.replaceAll("\r", "").trim()
+
+    completeLookupItem(activeLookup.find(le => le.getLookupString == "x, y, z").get)
+    checkResultByText(resultText)
+  }
+
+  def testConstructorCall2() {
+    val fileText =
+      """
+class TUI {
+  class A(x: Int, y: Int) {
+    def this(x: Int, y: Int, z: Int) = this(x, y)
+  }
+
+  class B(x: Int, y: Int, z: Int) extends A(<caret>)
+}
+      """.replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    val (activeLookup, _) = complete(1, CompletionType.BASIC)
+
+    val resultText =
+      """
+class TUI {
+  class A(x: Int, y: Int) {
+    def this(x: Int, y: Int, z: Int) = this(x, y)
+  }
+
+  class B(x: Int, y: Int, z: Int) extends A(x, y)<caret>
+}
+      """.replaceAll("\r", "").trim()
+
+    completeLookupItem(activeLookup.find(le => le.getLookupString == "x, y").get)
+    checkResultByText(resultText)
+  }
+
+  def testConstructorCall2Smart() {
+    val fileText =
+      """
+class TUI {
+  class A(x: Int, y: Int) {
+    def this(x: Int, y: Int, z: Int) = this(x, y)
+  }
+
+  class B(x: Int, y: Int, z: Int) extends A(<caret>)
+}
+      """.replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    val (activeLookup, _) = complete(1, CompletionType.SMART)
+
+    val resultText =
+      """
+class TUI {
+  class A(x: Int, y: Int) {
+    def this(x: Int, y: Int, z: Int) = this(x, y)
+  }
+
+  class B(x: Int, y: Int, z: Int) extends A(x, y)<caret>
+}
+      """.replaceAll("\r", "").trim()
+
+    completeLookupItem(activeLookup.find(le => le.getLookupString == "x, y").get)
+    checkResultByText(resultText)
+  }
+
+  def testSuperCall() {
+    val fileText =
+      """
+class TUI {
+  class A {
+    def foo(x: Int, y: Int, z: Int) = 1
+    def foo(x: Int, y: Int) = 2
+  }
+
+  class B extends A {
+    override def foo(x: Int, y: Int, z: Int) = {
+      super.foo(<caret>)
+    }
+  }
+}
+      """.replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    val (activeLookup, _) = complete(1, CompletionType.BASIC)
+
+    val resultText =
+      """
+class TUI {
+  class A {
+    def foo(x: Int, y: Int, z: Int) = 1
+    def foo(x: Int, y: Int) = 2
+  }
+
+  class B extends A {
+    override def foo(x: Int, y: Int, z: Int) = {
+      super.foo(x, y)<caret>
+    }
+  }
+}
+      """.replaceAll("\r", "").trim()
+
+    completeLookupItem(activeLookup.find(le => le.getLookupString == "x, y").get)
+    checkResultByText(resultText)
+  }
+
+  def testSuperCall2() {
+    val fileText =
+      """
+class TUI {
+  class A {
+    def foo(x: Int, y: Int, z: Int) = 1
+    def foo(x: Int, y: Int) = 2
+  }
+
+  class B extends A {
+    override def foo(x: Int, y: Int, z: Int) = {
+      super.foo(<caret>)
+    }
+  }
+}
+      """.replaceAll("\r", "").trim()
+    configureFromFileTextAdapter("dummy.scala", fileText)
+    val (activeLookup, _) = complete(1, CompletionType.BASIC)
+
+    val resultText =
+      """
+class TUI {
+  class A {
+    def foo(x: Int, y: Int, z: Int) = 1
+    def foo(x: Int, y: Int) = 2
+  }
+
+  class B extends A {
+    override def foo(x: Int, y: Int, z: Int) = {
+      super.foo(x, y, z)<caret>
+    }
+  }
+}
+      """.replaceAll("\r", "").trim()
+
+    completeLookupItem(activeLookup.find(le => le.getLookupString == "x, y, z").get)
+    checkResultByText(resultText)
+  }
+
+}
