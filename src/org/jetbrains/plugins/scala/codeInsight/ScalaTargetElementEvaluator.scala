307e686e9468e2979a63e07388e0718832d5b14f
niktrop <Nikolay.Tropin@jetbrains.com>
2013-09-04 12:22:47 +0400

    Caret adjustment for all scala identifiers

--- src/org/jetbrains/plugins/scala/codeInsight/ScalaTargetElementEvaluator.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/ScalaTargetElementEvaluator.scala
@@ -1,18 +1,19 @@
 package org.jetbrains.plugins.scala
 package codeInsight
 
-import com.intellij.codeInsight.TargetElementEvaluator
-import com.intellij.psi.{PsiElement, PsiReference}
+import com.intellij.codeInsight.TargetElementEvaluatorEx
+import com.intellij.psi.{PsiFile, PsiElement, PsiReference}
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScBindingPattern
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReferenceElement
 import org.jetbrains.plugins.scala.lang.resolve.ScalaResolveResult
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 
 /**
  * Nikolay.Tropin
  * 9/2/13
  */
-class ScalaTargetElementEvaluator extends TargetElementEvaluator {
+class ScalaTargetElementEvaluator extends TargetElementEvaluatorEx {
 
   def includeSelfInGotoImplementation(element: PsiElement): Boolean = true
 
@@ -34,5 +35,10 @@ class ScalaTargetElementEvaluator extends TargetElementEvaluator {
         case _ => None
       }
     }
+  }
+
+  def isIdentifierPart(file: PsiFile, text: CharSequence, offset: Int): Boolean = {
+    val child: PsiElement = file.findElementAt(offset)
+    child != null && ScalaTokenTypes.IDENTIFIER_TOKEN_SET.contains(child.getNode.getElementType )
   }
 }
