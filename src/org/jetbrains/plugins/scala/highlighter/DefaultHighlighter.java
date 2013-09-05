b4246fd9b218fc30b4878bfe9de6acf636148bcd
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-09-05 18:22:17 +0400

    No deprecated API in highlighter. Implicit conversion highlighting is less aggressive now in Darcula scheme #SCL-6021 Fixed

--- src/org/jetbrains/plugins/scala/highlighter/DefaultHighlighter.java
+++ src/org/jetbrains/plugins/scala/highlighter/DefaultHighlighter.java
@@ -18,19 +18,121 @@ package org.jetbrains.plugins.scala.highlighter;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.HighlighterColors;
-import com.intellij.openapi.editor.SyntaxHighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.EffectType;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import org.jetbrains.annotations.NonNls;
-
-import java.awt.*;
 
 /**
  * @author ilyas
  *         Date: 24.09.2006
  */
 public class DefaultHighlighter {
+  @NonNls
+  static final String LINE_COMMENT_NAME = "Line comment";
+  @NonNls
+  static final String BLOCK_COMMENT_NAME = "Block comment";
+  @NonNls
+  static final String KEYWORD_NAME = "Keyword";
+  @NonNls
+  static final String NUMBER_NAME = "Number";
+  @NonNls
+  static final String STRING_NAME = "String";
+  @NonNls
+  static final String VALID_STRING_ESCAPE_NAME = "Valid escape in string";
+  @NonNls
+  static final String INVALID_STRING_ESCAPE_NAME = "Invalid escape in string";
+  @NonNls
+  static final String BRACKETS_NAME = "Brackets";
+  @NonNls
+  static final String BRACES_NAME = "Braces";
+  @NonNls
+  static final String COLON_NAME = "Colon";
+  @NonNls
+  static final String PARENTHESES_NAME = "Parentheses";
+  @NonNls
+  static final String ASSIGN_NAME = "Assign";
+  @NonNls
+  static final String SEMICOLON_NAME = "Semicolon";
+  @NonNls
+  static final String DOT_NAME = "Dot";
+  @NonNls
+  static final String COMMA_NAME = "Comma";
+  @NonNls
+  static final String INTERPOLATED_STRING_INJECTION_NAME = "Interpolated String Injection";
+  @NonNls
+  static final String MUTABLE_COLLECTION_NAME = "Mutable Collection";
+  @NonNls
+  static final String IMMUTABLE_COLLECTION_NAME = "Immutable Collection";
+  @NonNls
+  static final String JAVA_COLLECTION_NAME = "Standart Java Collection";
+  @NonNls
+  static final String TYPEPARAM_NAME = "Type parameter";
+  @NonNls
+  static final String PREDEF_NAME = "Predefined types";
+  @NonNls
+  static final String OBJECT_NAME = "Object";
+  @NonNls
+  static final String CLASS_NAME = "Class";
+  @NonNls
+  static final String BAD_CHARACTER_NAME = "Bad character";
+  @NonNls
+  static final String DOC_COMMENT_NAME = "ScalaDoc comment";
+  @NonNls
+  static final String SCALA_DOC_TAG_NAME = "ScalaDoc comment tag";
+  @NonNls
+  static final String SCALA_DOC_HTML_TAG_NAME = "ScalaDoc html tag";
+  @NonNls
+  static final String SCALA_DOC_WIKI_SYNTAX_NAME = "ScalaDoc wiki syntax elements";
+  @NonNls
+  static final String SCALA_DOC_HTML_ESCAPE_NAME = "ScalaDoc html escape sequences";
+  @NonNls
+  static final String SCALA_DOC_MARKUP_NAME = "ScalaDoc comment markup";
+  @NonNls
+  static final String SCALA_DOC_TAG_PARAM_VALUE_NAME = "ScalaDoc @param value";
+  @NonNls
+  static final String IMPLICIT_CONVERSIONS_NAME = "Implicit conversion";
+  @NonNls
+  static final String ABSTRACT_CLASS_NAME = "Abstract class";
+  @NonNls
+  static final String TRAIT_NAME = "Trait";
+  @NonNls
+  static final String LOCAL_VALUES_NAME = "Local value";
+  @NonNls
+  static final String LOCAL_VARIABLES_NAME = "Local variable";
+  @NonNls
+  static final String LOCAL_LAZY_NAME = "Local lazy val/var";
+  @NonNls
+  static final String VALUES_NAME = "Template val";
+  @NonNls
+  static final String VARIABLES_NAME = "Template var";
+  @NonNls
+  static final String LAZY_NAME = "Template lazy val/var";
+  @NonNls
+  static final String PARAMETER_NAME = "Parameter";
+  @NonNls
+  static final String METHOD_CALL_NAME = "Class method call";
+  @NonNls
+  static final String OBJECT_METHOD_CALL_NAME = "Object method call";
+  @NonNls
+  static final String LOCAL_METHOD_CALL_NAME = "Local method call";
+  @NonNls
+  static final String METHOD_DECLARATION_NAME = "Method declaration";
+  @NonNls
+  static final String ANNOTATION_NAME = "Annotation name";
+  @NonNls
+  static final String ANNOTATION_ATTRIBUTE_NAME = "Annotation attribute name";
+  @NonNls
+  static final String PATTERN_NAME = "Pattern value";
+  @NonNls
+  static final String TYPE_ALIAS_NAME = "Type Alias";
+  @NonNls
+  static final String XML_TEXT_NAME = "XML Text";
+  @NonNls
+  static final String XML_TAG_NAME = "XML Tag";
+  @NonNls
+  static final String IMPLICIT_FIRST_PART_NAME = "Implicit conversion first part";
+  @NonNls
+  static final String IMPLICIT_SECOND_PART_NAME = "Implicit conversion second part";
+  
   // Comments
   @NonNls
   static final String LINE_COMMENT_ID = "Scala Line comment";
@@ -95,7 +197,7 @@ public class DefaultHighlighter {
   @NonNls
   static final String SCALA_DOC_TAG_PARAM_VALUE_ID = "ScalaDoc @param value";
   @NonNls
-  static final String SCALA_IMPLICITS_ID = "Implicit conversion";
+  static final String IMPLICIT_CONVERSIONS_ID = "Implicit conversion";
   @NonNls
   static final String ABSTRACT_CLASS_ID = "Scala Abstract class";
   @NonNls
@@ -153,14 +255,10 @@ public class DefaultHighlighter {
   public static TextAttributesKey SEMICOLON = createKey(SEMICOLON_ID, DefaultLanguageHighlighterColors.COMMA);
   public static TextAttributesKey DOT = createKey(DOT_ID, DefaultLanguageHighlighterColors.DOT);
   public static TextAttributesKey COMMA = createKey(COMMA_ID, DefaultLanguageHighlighterColors.COMMA);
-  public static TextAttributesKey INTERPOLATED_STRING_INJECTION = TextAttributesKey.createTextAttributesKey(INTERPOLATED_STRING_INJECTION_ID,
-      new TextAttributes(new Color(0, 184, 187), null, null, null, Font.BOLD));
-  public static TextAttributesKey MUTABLE_COLLECTION = TextAttributesKey.createTextAttributesKey(MUTABLE_COLLECTION_ID,
-      new TextAttributes(null, null, new Color(226, 158, 194), EffectType.BOLD_DOTTED_LINE, Font.PLAIN));
-  public static TextAttributesKey IMMUTABLE_COLLECTION = TextAttributesKey.createTextAttributesKey(IMMUTABLE_COLLECTION_ID,
-      new TextAttributes(null, null, new Color(172, 203, 121), EffectType.BOLD_DOTTED_LINE, Font.PLAIN));
-  public static TextAttributesKey JAVA_COLLECTION = TextAttributesKey.createTextAttributesKey(JAVA_COLLECTION_ID,
-      new TextAttributes(null, null, new Color(129, 194, 195), EffectType.BOLD_DOTTED_LINE, Font.PLAIN));
+  public static TextAttributesKey INTERPOLATED_STRING_INJECTION = createKey(INTERPOLATED_STRING_INJECTION_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
+  public static TextAttributesKey MUTABLE_COLLECTION = createKey(MUTABLE_COLLECTION_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
+  public static TextAttributesKey IMMUTABLE_COLLECTION = createKey(IMMUTABLE_COLLECTION_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
+  public static TextAttributesKey JAVA_COLLECTION = createKey(JAVA_COLLECTION_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
   public static TextAttributesKey PREDEF = createKey(PREDEF_ID, DefaultLanguageHighlighterColors.COMMA);
   public static TextAttributesKey TYPEPARAM = createKey(TYPEPARAM_ID, HighlightInfoType.TYPE_PARAMETER_NAME.getAttributesKey());
   public static TextAttributesKey OBJECT = createKey(OBJECT_ID, HighlightInfoType.CLASS_NAME.getAttributesKey());
@@ -171,8 +269,7 @@ public class DefaultHighlighter {
   public static TextAttributesKey SCALA_DOC_HTML_ESCAPE = createKey(SCALA_DOC_HTML_ESCAPE_ID, DefaultLanguageHighlighterColors.DOC_COMMENT_MARKUP);
   public static TextAttributesKey SCALA_DOC_MARKUP = createKey(SCALA_DOC_MARKUP_ID, DefaultLanguageHighlighterColors.DOC_COMMENT_MARKUP);
   public static TextAttributesKey SCALA_DOC_TAG_PARAM_VALUE = createKey(SCALA_DOC_TAG_PARAM_VALUE_ID, DefaultLanguageHighlighterColors.DOC_COMMENT_TAG);
-  public static TextAttributesKey IMPLICIT_CONVERSIONS = TextAttributesKey.createTextAttributesKey(SCALA_IMPLICITS_ID,
-      new TextAttributes(null, null, Color.LIGHT_GRAY, EffectType.LINE_UNDERSCORE, Font.PLAIN));
+  public static TextAttributesKey IMPLICIT_CONVERSIONS = createKey(IMPLICIT_CONVERSIONS_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
   public static TextAttributesKey ABSTRACT_CLASS = createKey(ABSTRACT_CLASS_ID, HighlightInfoType.ABSTRACT_CLASS_NAME.getAttributesKey());
   public static TextAttributesKey TRAIT = createKey(TRAIT_ID, HighlightInfoType.INTERFACE_NAME.getAttributesKey());
   public static TextAttributesKey LOCAL_VALUES = createKey(LOCAL_VALUES_ID, HighlightInfoType.LOCAL_VARIABLE.getAttributesKey());
@@ -195,11 +292,11 @@ public class DefaultHighlighter {
   public static TextAttributesKey DOC_COMMENT = createKey(DOC_COMMENT_ID, DefaultLanguageHighlighterColors.DOC_COMMENT);
   public static TextAttributesKey XML_TEXT = createKey(XML_TEXT_ID, DefaultLanguageHighlighterColors.STRING);
   public static TextAttributesKey XML_TAG = createKey(XML_TAG_ID, DefaultLanguageHighlighterColors.DOC_COMMENT_TAG);
-  public static TextAttributesKey IMPLICIT_FIRST_PART = TextAttributesKey.createTextAttributesKey(IMPLICIT_FIRST_PART_ID, new TextAttributes(new Color(187, 223, 255), null, null, null, Font.PLAIN));
-  public static TextAttributesKey IMPLICIT_SECOND_PART = TextAttributesKey.createTextAttributesKey(IMPLICIT_SECOND_PART_ID, new TextAttributes(new Color(223, 240, 255), null, null, null, Font.PLAIN));
+  public static TextAttributesKey IMPLICIT_FIRST_PART = createKey(IMPLICIT_FIRST_PART_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
+  public static TextAttributesKey IMPLICIT_SECOND_PART = createKey(IMPLICIT_SECOND_PART_ID, DefaultLanguageHighlighterColors.IDENTIFIER);
 
   private static TextAttributesKey createKey(String externalName, TextAttributesKey prototype) {
-    return TextAttributesKey.createTextAttributesKey(externalName, prototype.getDefaultAttributes());
+    return TextAttributesKey.createTextAttributesKey(externalName, prototype);
   }
 }
 
