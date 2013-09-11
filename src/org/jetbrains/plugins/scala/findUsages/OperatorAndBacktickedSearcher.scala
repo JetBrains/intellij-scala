e2204b3421e488c1b317e7c610ff2ffbb61eb041
niktrop <Nikolay.Tropin@jetbrains.com>
2013-09-11 15:45:32 +0400

    Correct rename of scala identifiers with backticks or operator characters #SCL-5913 fixed #SCL-2939 fixed

---
+++ src/org/jetbrains/plugins/scala/findUsages/OperatorAndBacktickedSearcher.scala
@@ -0,0 +1,102 @@
+package org.jetbrains.plugins.scala
+package findUsages
+
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.util.{CommonProcessors, QueryExecutor, Processor}
+import com.intellij.psi.{PsiElement, PsiReference}
+import com.intellij.psi.search.{GlobalSearchScope, UsageSearchContext, PsiSearchHelper, TextOccurenceProcessor}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
+import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
+import com.intellij.openapi.project.IndexNotReadyException
+import com.intellij.psi.impl.PsiManagerEx
+import com.intellij.psi.impl.search.PsiSearchHelperImpl
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.util.Condition
+import com.intellij.openapi.roots.FileIndexFacade
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.openapi.application.ReadActionProcessor
+import java.util
+import com.intellij.psi.impl.cache.impl.id.{IdIndex, IdIndexEntry}
+import com.intellij.util.indexing.FileBasedIndex
+
+/**
+ * Nikolay.Tropin
+ * 9/10/13
+ */
+class OperatorAndBacktickedSearcher extends QueryExecutor[PsiReference, ReferencesSearch.SearchParameters] {
+  def execute(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor[PsiReference]): Boolean = {
+    val scope = queryParameters.getEffectiveSearchScope
+    val element = queryParameters.getElementToSearch
+    extensions.inReadAction{
+      if (!element.isValid) return true
+      val toProcess = element match {
+        case isBackticked(name) => Seq((element, name), (element, s"`$name`"))
+        case named: ScNamedElement if named.name.exists(ScalaNamesUtil.isOpCharacter) => Seq((named, named.name))
+        case _ => Nil
+      }
+      toProcess.foreach { case (elem, name) =>
+        val processor = new TextOccurenceProcessor {
+          def execute(element: PsiElement, offsetInElement: Int): Boolean = {
+            val references = element.getReferences
+            for (ref <- references if ref.getRangeInElement.contains(offsetInElement)) {
+              if (ref.isReferenceTo(elem) || ref.resolve() == elem) {
+                if (!consumer.process(ref)) return false
+              }
+            }
+            true
+          }
+        }
+        val helper: PsiSearchHelper = new ScalaPsiSearchHelper(elem.getManager.asInstanceOf[PsiManagerEx])
+        try {
+          helper.processElementsWithWord(processor, scope, name, UsageSearchContext.IN_CODE, true)
+        }
+        catch {
+          case ignore: IndexNotReadyException =>
+        }
+      }
+      true
+    }
+  }
+
+  private val isBackticked = new {
+    def unapply(named: ScNamedElement): Option[String] = {
+      val name = named.name
+      if (name.startsWith("`") && name.endsWith("`")) Some(name.substring(1, name.length - 1))
+      else None
+    }
+  }
+
+  private class ScalaPsiSearchHelper(manager: PsiManagerEx) extends PsiSearchHelperImpl(manager) {
+    override def processFilesWithText(scope: GlobalSearchScope,
+                                      searchContext: Short,
+                                      caseSensitively: Boolean,
+                                      text: String,
+                                      processor: Processor[VirtualFile]): Boolean = {
+      val entries = getWordEntries(text, caseSensitively)
+      if (entries.isEmpty) return true
+      val collectProcessor: CommonProcessors.CollectProcessor[VirtualFile] = new CommonProcessors.CollectProcessor[VirtualFile]
+      val checker = new Condition[Integer] {
+        def value(integer: Integer): Boolean = (integer.intValue & searchContext) != 0
+      }
+      extensions.inReadAction {
+        FileBasedIndex.getInstance.processFilesContainingAllKeys(IdIndex.NAME, entries, scope, checker, collectProcessor)
+      }
+      val index: FileIndexFacade = FileIndexFacade.getInstance(manager.getProject)
+      ContainerUtil.process(collectProcessor.getResults, new ReadActionProcessor[VirtualFile] {
+        def processInReadAction(virtualFile: VirtualFile): Boolean = {
+          !index.shouldBeFound(scope, virtualFile) || processor.process(virtualFile)
+        }
+      })
+    }
+
+    /**
+     * Only this method is actually differs from PsiSearchHelperImpl,
+     * because it works only for java identifiers there.
+     */
+    private def getWordEntries(name: String, caseSensitively: Boolean): util.List[IdIndexEntry] = {
+      val keys = new util.ArrayList[IdIndexEntry]
+      if (ScalaNamesUtil.isIdentifier(name)) keys.add(new IdIndexEntry(name, caseSensitively))
+      keys
+    }
+  }
+}
