2f19b27edfe226aec3a714ed0d880d1fd5d63910
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-09-12 11:31:48 +0400

    Fixed processing compound types, which includes existential types #SCL-5982 Fixed

--- test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
+++ test/org/jetbrains/plugins/scala/lang/resolve2/Bug3Test.scala
@@ -65,6 +65,7 @@ class Bug3Test extends ResolveTestBase {
   def testSCL5377() {doTest()}
   def testSCL5418() {doTest()}
   def testSCL5971() {doTest()}
+  def testSCL5982() {doTest()}
   def testSCL5987() {doTest()}
   def testStringInterpolatorPrefix() {doTest()}
   def testShadowedImport() {doTest()}
