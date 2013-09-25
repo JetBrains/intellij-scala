a49fc3c87a90a7009b9c05faba8510fd71f80f8b
niktrop <Nikolay.Tropin@jetbrains.com>
2013-09-25 18:24:00 +0400

    rename tests

---
+++ testdata/rename3/objectAndTraitToOpChars/after/tests/+++.scala
@@ -0,0 +1,21 @@
+package tests
+
+object +++ {
+  val x = 0
+  def qqq() = {}
+  def apply() = {}
+  def unapply(i: Int) = Some(i)
+}
+
+trait +++ {
+  def baz(): Int = 1
+}
+
+object Test {
+  def main(args: Array[String]) {
+    +++()
+    1 match {
+      case +++(i) =>
+    }
+  }
+}
