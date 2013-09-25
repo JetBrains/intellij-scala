2c30a285f9d1a226773261aa381b07aadfdeb76b
niktrop <Nikolay.Tropin@jetbrains.com>
2013-09-25 18:24:00 +0400

    rename tests

---
+++ testdata/rename3/overrideDef/after/tests/Baz.scala
@@ -0,0 +1,21 @@
+package tests
+
+class Baz {
+  def NameAfterRename() = 0
+}
+
+object Baz2 extends Baz {
+  override val NameAfterRename = 3
+}
+
+object Baz3 extends {override var NameAfterRename = 1} with Baz {}
+
+class Baz4 extends Baz {
+  override def NameAfterRename() = 1
+}
+
+object Test {
+  Baz2.NameAfterRename
+  Baz3.NameAfterRename
+  Baz3.NameAfterRename_=(3)
+}
