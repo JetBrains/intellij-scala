60261ac94f516b0a4b1cd17487f2612cacbd1e3d
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-10-07 18:05:26 +0400

    Partial fix, Tag and other classes are now resolved. #SCL-6022 Comment

--- src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScSuperReferenceImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScSuperReferenceImpl.scala
@@ -44,12 +44,12 @@ class ScSuperReferenceImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with
               val path = commentText.substring(2, commentText.length - 2)
               val classes = ScalaPsiManager.instance(getProject).getCachedClasses(getResolveScope, path)
               if (classes.length == 1) {
-                drvTemplate.exists(!_.isInheritor(classes(0), deep = false))
+                drvTemplate.exists(td => !ScalaPsiUtil.cachedDeepIsInheritor(td, classes(0)))
               } else {
                 val clazz: Option[PsiClass] = classes.find(!_.isInstanceOf[ScObject])
                 clazz match {
                   case Some(psiClass) =>
-                    drvTemplate.exists(!_.isInheritor(psiClass, deep = false))
+                    drvTemplate.exists(td => !ScalaPsiUtil.cachedDeepIsInheritor(td, psiClass))
                   case _ => false
                 }
               }
