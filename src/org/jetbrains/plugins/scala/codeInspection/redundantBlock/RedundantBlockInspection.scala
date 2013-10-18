d97d9cceaea333d8015a693096f8564c0e1141d2
niktrop <Nikolay.Tropin@jetbrains.com>
2013-10-18 11:34:25 +0400

    Remove redundant braces in case clause inspection #SCL-6151  fixed

--- src/org/jetbrains/plugins/scala/codeInspection/redundantBlock/RedundantBlockInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/redundantBlock/RedundantBlockInspection.scala
@@ -5,8 +5,11 @@ import com.intellij.codeInspection.{ProblemDescriptor, ProblemsHolder}
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import codeInspection.{AbstractFix, AbstractInspection}
-import lang.psi.api.expr.{ScThisReference, ScReferenceExpression, ScBlockExpr}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlock, ScThisReference, ScReferenceExpression, ScBlockExpr}
 import lang.refactoring.util.ScalaNamesUtil
+import org.jetbrains.plugins.scala.extensions.childOf
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScCaseClause
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 
 /**
  * Pavel Fatin
@@ -15,6 +18,9 @@ import lang.refactoring.util.ScalaNamesUtil
 class RedundantBlockInspection extends AbstractInspection {
 
   def actionFor(holder: ProblemsHolder) = {
+    case (block: ScBlock) childOf ((blockOfExpr: ScBlock) childOf (_: ScCaseClause))
+      if blockOfExpr.getChildren.length == 1 && block.hasRBrace =>
+      holder.registerProblem(block, "Remove redundant braces", new InCaseClauseQuickFix(block))
     case block: ScBlockExpr if block.getChildren.length == 3 =>
       val child: PsiElement = block.getChildren.apply(1)
       val probablyRedundant = child match {
@@ -40,6 +46,21 @@ class RedundantBlockInspection extends AbstractInspection {
   private class QuickFix(e: PsiElement) extends AbstractFix("Unwrap the expression", e) {
     def doApplyFix(project: Project, descriptor: ProblemDescriptor) {
       e.replace(e.getChildren.apply(1))
+    }
+  }
+  
+  private class InCaseClauseQuickFix(block: ScBlock) extends AbstractFix("Remove redundant braces", block) {
+    def doApplyFix(project: Project, descriptor: ProblemDescriptor): Unit = {
+      val stmts = block.statements
+      if (stmts.length == 1)
+        block.replace(stmts(0))
+      else {
+        for (stmt <- stmts; parent = block.getParent) {
+          parent.addBefore(ScalaPsiElementFactory.createNewLine(parent.getManager), block)
+          parent.addBefore(stmt, block)
+        }
+        block.delete()
+      }
     }
   }
 }
