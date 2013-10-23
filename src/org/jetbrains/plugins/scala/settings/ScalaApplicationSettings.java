893549fb362a5a6967f3a6c0d748a477567b4dbf
niktrop <Nikolay.Tropin@jetbrains.com>
2013-10-23 15:18:25 +0400

    Setting for autoclosing quotes for multiline strings

--- src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
+++ src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
@@ -41,6 +41,8 @@ public class ScalaApplicationSettings implements PersistentStateComponent<ScalaA
 
   public boolean INTRODUCE_FIELD_INITIALIZE_IN_DECLARATION = true;
 
+  public boolean INSERT_MULTILINE_QUOTES = true;
+
   public ScalaApplicationSettings getState() {
     return this;
   }
