f1414e73385848413bdf85029ba2221f43907112
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-10-30 17:08:16 +0400

    Scala library properties simplified, properties persistence

--- src/org/jetbrains/plugins/scala/configuration/ScalaLibraryProperties.scala
+++ src/org/jetbrains/plugins/scala/configuration/ScalaLibraryProperties.scala
@@ -1,34 +1,37 @@
 package org.jetbrains.plugins.scala
 package configuration
 
-import com.intellij.openapi.roots.libraries.LibraryProperties
-import collection.JavaConverters._
 import java.io.File
+import com.intellij.openapi.roots.libraries.LibraryProperties
 import com.intellij.openapi.util.io.FileUtil._
 import com.intellij.openapi.vfs.VfsUtilCore._
 
 /**
  * @author Pavel Fatin
  */
-class ScalaLibraryProperties extends LibraryProperties[ScalaLibraryState] {
-  var compilerClasspath: Seq[File] = Seq.empty
+class ScalaLibraryProperties extends LibraryProperties[ScalaLibraryPropertiesState] {
+  var languageLevel: ScalaLanguageLevel = _
+  var compilerClasspath: Seq[File] = _
 
-  def loadState(state: ScalaLibraryState) {
-    val paths = state.compilerClasspath.asScala
-    compilerClasspath = paths.map(path => new File(urlToPath(path)))
+  loadState(new ScalaLibraryPropertiesState())
+
+  def loadState(state: ScalaLibraryPropertiesState) {
+    languageLevel = state.languageLevel
+    compilerClasspath = state.compilerClasspath.map(path => new File(urlToPath(path)))
   }
 
   def getState = {
-    val paths = compilerClasspath.map(file => pathToUrl(toCanonicalPath(file.getAbsolutePath)))
-    val state = new ScalaLibraryState()
-    state.compilerClasspath = paths.asJava
+    val state = new ScalaLibraryPropertiesState()
+    state.languageLevel = languageLevel
+    state.compilerClasspath = compilerClasspath.map(file => pathToUrl(toCanonicalPath(file.getAbsolutePath))).toArray
     state
   }
 
   override def equals(obj: scala.Any) = obj match {
-    case other: ScalaLibraryProperties => compilerClasspath == other.compilerClasspath
+    case that: ScalaLibraryProperties =>
+      languageLevel == that.languageLevel && compilerClasspath == that.compilerClasspath
     case _ => false
   }
 
-  override def hashCode() = compilerClasspath.hashCode
+  override def hashCode() = languageLevel.hashCode * 31 + compilerClasspath.hashCode
 }
--- src/org/jetbrains/plugins/scala/configuration/ScalaLibraryPropertiesEditor.scala
+++ src/org/jetbrains/plugins/scala/configuration/ScalaLibraryPropertiesEditor.scala
@@ -2,45 +2,23 @@ package org.jetbrains.plugins.scala
 package configuration
 
 import com.intellij.openapi.roots.libraries.ui.{LibraryPropertiesEditor, LibraryEditorComponent}
-import com.intellij.openapi.projectRoots.ui.PathEditor
-import com.intellij.openapi.fileChooser.FileChooserDescriptor
-import com.intellij.ui.TabbedPaneWrapper
-import com.intellij.openapi.vfs.{VfsUtilCore, VfsUtil}
-import collection.JavaConverters._
-import com.intellij.openapi.util.Disposer
 
 /**
  * @author Pavel Fatin
  */
 private class ScalaLibraryPropertiesEditor(editorComponent: LibraryEditorComponent[ScalaLibraryProperties]) extends LibraryPropertiesEditor {
-  private val disposable = Disposer.newDisposable()
-  private val chooserDescriptor = new FileChooserDescriptor(true, false, true, false, false, true)
-  private val classpathEditor = new PathEditor(chooserDescriptor)
-  private val pluginsEditor = new PathEditor(chooserDescriptor)
+  private lazy val form = new ScalaLibraryEditorForm()
 
-  def createComponent() = {
-    val tabs = new TabbedPaneWrapper(disposable)
-    tabs.addTab("Classpath", classpathEditor.createComponent())
-    tabs.addTab("Options", new CompilerOptionsTab().getComponent)
-    tabs.addTab("Plugins", pluginsEditor.createComponent())
+  def createComponent() = form.getComponent
 
-    new ScalaLibraryEditorForm(tabs.getComponent).getComponent
-  }
-
-  override def disposeUIResources() {
-    Disposer.dispose(disposable)
-  }
-
-  def isModified = classpathEditor.isModified
+  def isModified = form.getState != properties.getState
 
   def reset() {
-    val files = properties.compilerClasspath.map(VfsUtil.findFileByIoFile(_, true))
-    classpathEditor.resetPath(files.asJava)
+    form.setState(properties.getState)
   }
 
   def apply() {
-    val roots = classpathEditor.getRoots.toSeq
-    properties.compilerClasspath = roots.map(VfsUtilCore.virtualToIoFile)
+    properties.loadState(form.getState)
   }
 
   private def properties = editorComponent.getProperties
