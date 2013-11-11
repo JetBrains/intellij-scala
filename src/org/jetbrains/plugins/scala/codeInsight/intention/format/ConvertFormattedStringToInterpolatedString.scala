25c32277b73f20ba0a508f8785a0094bb7f8d8f7
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-11-11 18:18:35 +0400

    New language level API is used in the string format intentions

--- src/org/jetbrains/plugins/scala/codeInsight/intention/format/ConvertFormattedStringToInterpolatedString.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/format/ConvertFormattedStringToInterpolatedString.scala
@@ -1,13 +1,11 @@
-package org.jetbrains.plugins.scala.codeInsight.intention.format
+package org.jetbrains.plugins.scala
+package codeInsight.intention.format
 
 import org.jetbrains.plugins.scala.format.{InterpolatedStringFormatter, FormattedStringParser}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
-import com.intellij.openapi.roots.{ProjectRootManager, ProjectFileIndex}
-import com.intellij.openapi.module.Module
-import org.jetbrains.plugins.scala.config.ScalaFacet
-import org.jetbrains.plugins.scala.lang.languageLevel.ScalaLanguageLevel
+import extensions._
 
 /**
  * Pavel Fatin
@@ -18,6 +16,6 @@ class ConvertFormattedStringToInterpolatedString extends AbstractFormatConversio
 
   override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
     if (!super.isAvailable(project: Project, editor: Editor, element: PsiElement)) return false
-    ScalaLanguageLevel.getLanguageLevel(element).isThoughScala2_10
+    element.languageLevel.isSinceScala2_10
   }
 }
