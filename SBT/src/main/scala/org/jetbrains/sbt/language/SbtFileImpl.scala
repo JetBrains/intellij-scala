39d661d91e4519835b9ba6c2f8b2156781bbf94e
Dmitry.Naydanov <dmitry.naydanov@jetbrains.com>
2013-11-26 15:40:26 +0400

    SCL-6294 and similar (val and def declarations and Seq[Setting[_]] in build files)

--- SBT/src/main/scala/org/jetbrains/sbt/language/SbtFileImpl.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/language/SbtFileImpl.scala
@@ -5,20 +5,21 @@ import com.intellij.psi.{PsiElement, ResolveState, FileViewProvider}
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaFileImpl
 import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.openapi.module.{ModuleManager, ModuleUtilCore}
+import org.jetbrains.plugins.scala.lang.psi.ScDeclarationSequenceHolder
 
 /**
  * @author Pavel Fatin
  */
-class SbtFileImpl(provider: FileViewProvider) extends ScalaFileImpl(provider, SbtFileType) {
+class SbtFileImpl(provider: FileViewProvider) extends ScalaFileImpl(provider, SbtFileType) with ScDeclarationSequenceHolder{
   override def isScriptFile(withCashing: Boolean) = false
 
   override def immediateTypeDefinitions = Seq.empty
 
   override def packagings = Seq.empty
 
-  override def processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement, place: PsiElement): Boolean = {
-    super.processDeclarations(processor, state, lastParent, place)
-  }
+  override def processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement, place: PsiElement): Boolean = 
+    super[ScalaFileImpl].processDeclarations(processor, state, lastParent, place) && 
+    super[ScDeclarationSequenceHolder].processDeclarations(processor, state, lastParent, place)
 
   override def implicitlyImportedPackages = super.implicitlyImportedPackages :+ "sbt"
 
