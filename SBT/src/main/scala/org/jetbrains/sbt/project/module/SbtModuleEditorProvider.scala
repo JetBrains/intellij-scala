522ceae042bc455ed5db005f8332f2cab2724c69
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-12-19 16:39:24 +0400

    SbtModuleEditorProvider simplified, no output editor

--- SBT/src/main/scala/org/jetbrains/sbt/project/module/SbtModuleEditorProvider.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/module/SbtModuleEditorProvider.scala
@@ -2,28 +2,19 @@ package org.jetbrains.sbt
 package project.module
 
 import com.intellij.openapi.roots.ui.configuration._
-import com.intellij.openapi.module.{ModuleType, Module, ModuleConfigurationEditor}
-import com.intellij.openapi.roots.ModifiableRootModel
-import java.util
+import com.intellij.openapi.module.{ModuleType, ModuleConfigurationEditor}
 
 /**
  * User: Dmitry Naydanov
  * Date: 11/22/13
  */
 class SbtModuleEditorProvider extends ModuleConfigurationEditorProvider {
-  def createEditors(state: ModuleConfigurationState): Array[ModuleConfigurationEditor] = {
-    val rootModel: ModifiableRootModel = state.getRootModel
-    val module: Module = rootModel.getModule
-    val moduleName: String = module.getName
+  def createEditors(state: ModuleConfigurationState) = {
+    val module = state.getRootModel.getModule
 
-    if (!ModuleType.get(module).isInstanceOf[SbtModuleType]) return ModuleConfigurationEditor.EMPTY
-
-    val editors = new util.ArrayList[ModuleConfigurationEditor]
-
-    editors add new ContentEntriesEditor(moduleName, state)
-    editors add new DefaultModuleConfigurationEditorFactoryImpl().createOutputEditor(state)
-    editors add new ClasspathEditor(state)
-//    editors add new SbtModuleEditor(state)
-    editors.toArray(new Array[ModuleConfigurationEditor](editors.size))
+    if (ModuleType.get(module).isInstanceOf[SbtModuleType])
+      Array(new ContentEntriesEditor(module.getName, state), new ClasspathEditor(state))
+    else
+      ModuleConfigurationEditor.EMPTY
   }
 }
