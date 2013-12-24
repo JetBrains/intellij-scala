3fa199356c8a83d275366542f665669febc46655
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-12-24 18:37:25 +0400

    SBT: handle unresolved libraries (cherry picked from commit c28d11f)

--- SBT/src/main/scala/org/jetbrains/sbt/project/SbtProjectResolver.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/SbtProjectResolver.scala
@@ -48,18 +48,28 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
 
     projectNode.add(new ScalaProjectNode(javaHome, javacOptions))
 
-    val libraries =
-      data.repository.map(_.modules).getOrElse(projects.flatMap(modulesIn)).map(createLibrary) ++
-        projects.flatMap(_.scala).distinct.map(createCompilerLibrary)
+    val libraries = {
+      val repositoryModules = data.repository.map(_.modules).getOrElse(Seq.empty)
 
-    projectNode.addAll(libraries)
+      val otherModuleIds = projects.flatMap(_.configurations.flatMap(_.modules)).toSet --
+              repositoryModules.map(_.id).toSet
+
+      repositoryModules.map(createResolvedLibrary) ++ otherModuleIds.map(createUnresolvedLibrary)
+    }
+
+    val compilerLibraries = {
+      val scalas = projects.flatMap(_.scala).distinctBy(_.version)
+      scalas.map(createCompilerLibrary)
+    }
+
+    projectNode.addAll(libraries ++ compilerLibraries)
 
     val moduleFilesDirectory = projectNode.getIdeProjectFileDirectoryPath.toFile / ".idea" / "modules"
 
     val moduleNodes: Seq[ModuleNode] = projects.map { project =>
       val moduleNode = createModule(project, moduleFilesDirectory)
       moduleNode.add(createContentRoot(project))
-      moduleNode.addAll(createLibraryDependencies(project)(moduleNode, libraries.map(t => t.data)))
+      moduleNode.addAll(createLibraryDependencies(project)(moduleNode, libraries.map(_.data)))
       moduleNode.addAll(project.scala.map(createFacet(project, _)).toSeq)
       moduleNode.addAll(createUnmanagedDependencies(project)(moduleNode))
       moduleNode
@@ -80,11 +90,6 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
     projectNode
   }
 
-  private def modulesIn(project: Project): Seq[Module] = {
-    val ids = project.configurations.flatMap(_.modules)
-    ids.map(id => Module(id, Seq.empty, Seq.empty, Seq.empty))
-  }
-
   private def createFacet(project: Project, scala: Scala): ScalaFacetNode = {
     val basePackage = Some(project.organization).filter(_.contains(".")).mkString
 
@@ -97,8 +102,17 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
     result
   }
 
-  private def createLibrary(module: Module): LibraryNode = {
-    val result = new LibraryNode(nameFor(module.id))
+  private def createUnresolvedLibrary(moduleId: ModuleId): LibraryNode = {
+    val module = Module(moduleId, Seq.empty, Seq.empty, Seq.empty)
+    createLibrary(module, resolved = false)
+  }
+
+  private def createResolvedLibrary(module: Module): LibraryNode = {
+    createLibrary(module, resolved = true)
+  }
+
+  private def createLibrary(module: Module, resolved: Boolean): LibraryNode = {
+    val result = new LibraryNode(nameFor(module.id), resolved)
     result.addPaths(LibraryPathType.BINARY, module.binaries.map(_.path))
     result.addPaths(LibraryPathType.DOC, module.docs.map(_.path))
     result.addPaths(LibraryPathType.SOURCE, module.sources.map(_.path))
@@ -108,7 +122,7 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
   private def nameFor(id: ModuleId) = s"${id.organization}:${id.name}:${id.revision}"
 
   private def createCompilerLibrary(scala: Scala): LibraryNode = {
-    val result = new LibraryNode(nameFor(scala))
+    val result = new LibraryNode(nameFor(scala), resolved = true)
     // TODO don't use custom delimiter either when the external system will preserve compiler libraries
     // or when we will adopt the new Scala project configuration scheme
     // (see processOrphanProjectLibraries in ExternalSystemUtil)
@@ -254,7 +268,7 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
   private def createModuleLevelDependency(name: String, binaries: Seq[String], scope: DependencyScope)
                                          (moduleData: ModuleData): LibraryDependencyNode = {
 
-    val libraryNode = new LibraryNode(name)
+    val libraryNode = new LibraryNode(name, resolved = true)
     libraryNode.addPaths(LibraryPathType.BINARY, binaries)
 
     val result = new LibraryDependencyNode(moduleData, libraryNode, LibraryLevel.MODULE)
