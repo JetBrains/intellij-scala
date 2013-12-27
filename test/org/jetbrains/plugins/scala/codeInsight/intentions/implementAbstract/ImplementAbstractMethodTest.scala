7e2d920403065e1e86004a785c11434bc1713c2f
niktrop <Nikolay.Tropin@jetbrains.com>
2013-12-27 16:27:27 +0400

    Implement abstract method intention fixed #SCL-6450 fixed

---
+++ test/org/jetbrains/plugins/scala/codeInsight/intentions/implementAbstract/ImplementAbstractMethodTest.scala
@@ -0,0 +1,114 @@
+package org.jetbrains.plugins.scala
+package codeInsight.intentions.implementAbstract
+
+import org.jetbrains.plugins.scala.codeInsight.intentions.ScalaIntentionTestBase
+import com.intellij.codeInsight.intention.impl.ImplementAbstractMethodAction
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+
+/**
+ * Nikolay.Tropin
+ * 12/27/13
+ */
+class ImplementAbstractMethodTest extends ScalaIntentionTestBase {
+  def familyName: String = new ImplementAbstractMethodAction().getFamilyName
+
+  val START = CodeInsightTestFixture.SELECTION_START_MARKER
+  val END = CodeInsightTestFixture.SELECTION_END_MARKER
+
+  def testFromTrait() {
+    val text =
+      """
+        |trait A {
+        |  def <caret>f: Int
+        |}
+        |
+        |class AA extends A
+      """
+    val result =
+      s"""
+        |trait A {
+        |  def f: Int
+        |}
+        |
+        |class AA extends A {
+        |  def f: Int = $START???$END
+        |}"""
+    doTest(text, result)
+  }
+
+  def testFromAbstractClass() {
+    val text =
+      """
+        |abstract class A {
+        |  def <caret>f: Int
+        |}
+        |
+        |class AA extends A {}
+      """
+    val result =
+      s"""
+        |abstract class A {
+        |  def f: Int
+        |}
+        |
+        |class AA extends A {
+        |  def f: Int = $START???$END
+        |}"""
+    doTest(text, result)
+  }
+
+  def testParameterizedTrait() {
+    val text =
+      """
+        |trait A[T] {
+        |  def <caret>f: T
+        |}
+        |
+        |class AA extends A[Int] {}
+      """
+    val result =
+      s"""
+        |trait A[T] {
+        |  def f: T
+        |}
+        |
+        |class AA extends A[Int] {
+        |  def f: Int = $START???$END
+        |}"""
+    doTest(text, result)
+  }
+
+  def testFunDefInTrait() {
+    val text =
+      """
+        |trait A {
+        |  def <caret>f: Int = 0
+        |}
+        |
+        |class AA extends A
+      """
+    checkIntentionIsNotAvailable(text)
+  }
+
+  def testUnitReturn() {
+    val text =
+      """
+        |trait A {
+        |  def <caret>f
+        |}
+        |
+        |class AA extends A
+      """
+    val result =
+      s"""
+        |trait A {
+        |  def f
+        |}
+        |
+        |class AA extends A {
+        |  def f: Unit = $START???$END
+        |}"""
+    doTest(text, result)
+  }
+
+}
