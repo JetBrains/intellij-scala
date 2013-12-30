f42b9b1744b9118b7daf900010f983b15d4f7522
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-12-30 16:05:40 +0400

    Fixed find usages searcher to be faster.

--- src/org/jetbrains/plugins/scala/findUsages/function/JavaFunctionUsagesSearcher.scala
+++ src/org/jetbrains/plugins/scala/findUsages/function/JavaFunctionUsagesSearcher.scala
@@ -5,9 +5,10 @@ import com.intellij.util.{Processor, QueryExecutor}
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.Computable
 import com.intellij.psi.search.{UsageSearchContext, PsiSearchHelper, TextOccurenceProcessor}
-import com.intellij.psi.{PsiMethod, PsiReferenceExpression, PsiElement, PsiReference}
+import com.intellij.psi._
 import org.jetbrains.plugins.scala.lang.psi.light.{StaticPsiMethodWrapper, ScFunctionWrapper}
 import scala.collection.mutable
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 
 /**
  * @author Alefas
@@ -21,33 +22,29 @@ class JavaFunctionUsagesSearcher extends QueryExecutor[PsiReference, ReferencesS
         val element = queryParameters.getElementToSearch
         if (!element.isValid) return true
         element match {
-          case p: PsiMethod => {
-            val name: String = p.getName
+          case method: PsiMethod if method.isInstanceOf[ScFunction] || !method.hasModifierProperty(PsiModifier.STATIC) =>
+            val name: String = method.getName
             val collectedReferences: mutable.HashSet[PsiReference] = new mutable.HashSet[PsiReference]
             val processor = new TextOccurenceProcessor {
               def execute(element: PsiElement, offsetInElement: Int): Boolean = {
                 val references = element.getReferences
                 for (ref <- references if ref.getRangeInElement.contains(offsetInElement) && !collectedReferences.contains(ref)) {
                   ref match {
-                    case refElement: PsiReferenceExpression => {
+                    case refElement: PsiReferenceExpression =>
                       refElement.resolve match {
-                        case f: ScFunctionWrapper if f.function == p =>
-                          if (!consumer.process(refElement)) return false
-                        case t: StaticPsiMethodWrapper if t.getNavigationElement == p =>
-                          if (!consumer.process(refElement)) return false
+                        case f: ScFunctionWrapper if f.function == method && !consumer.process(refElement) => return false
+                        case t: StaticPsiMethodWrapper if t.getNavigationElement == method && !consumer.process(refElement) => return false
                         case _ =>
                       }
-                    }
                     case _ =>
                   }
                 }
                 true
               }
             }
-            val helper: PsiSearchHelper = PsiSearchHelper.SERVICE.getInstance(p.getProject)
+            val helper: PsiSearchHelper = PsiSearchHelper.SERVICE.getInstance(method.getProject)
             if (name == "") return true
             helper.processElementsWithWord(processor, scope, name, UsageSearchContext.IN_CODE, true)
-          }
           case _ =>
         }
         true
