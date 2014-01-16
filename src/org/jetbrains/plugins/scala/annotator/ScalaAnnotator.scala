1e154e3cbd169abae6374f530f6c72edcbcf5e1e
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-01-16 12:26:08 +0400

    Cleanup

--- src/org/jetbrains/plugins/scala/annotator/ScalaAnnotator.scala
+++ src/org/jetbrains/plugins/scala/annotator/ScalaAnnotator.scala
@@ -427,10 +427,10 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
               case t: ScTryStmt =>
                 t.expectedTypeEx(fromUnderscore = false) match {
                   case Some((tp: ScType, _)) if tp equiv psi.types.Unit => //do nothing
-                  case Some((tp: ScType, typeElement)) => {
+                  case Some((tp: ScType, typeElement)) =>
                     import org.jetbrains.plugins.scala.lang.psi.types._
                     val returnType = candidates(0) match {
-                      case ScalaResolveResult(fun: ScFunction, subst) => fun.returnType.map(subst.subst(_))
+                      case ScalaResolveResult(fun: ScFunction, subst) => fun.returnType.map(subst.subst)
                       case _ => return
                     }
                     val expectedType = Success(tp, None)
@@ -447,7 +447,6 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
                         }
                       }
                     }
-                  }
                   case _ => //do nothing
                 }
               case _ =>
@@ -562,27 +561,23 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
       val resolveResult = resolve(0).asInstanceOf[ScalaResolveResult]
       refElement match {
         case e: ScReferenceExpression if e.getParent.isInstanceOf[ScPrefixExpr] &&
-                e.getParent.asInstanceOf[ScPrefixExpr].operation == e => {
+                e.getParent.asInstanceOf[ScPrefixExpr].operation == e =>
           resolveResult.implicitFunction match {
-            case Some(fun) => {
+            case Some(fun) =>
               val pref = e.getParent.asInstanceOf[ScPrefixExpr]
               val expr = pref.operand
               highlightImplicitMethod(expr, resolveResult, refElement, fun, holder)
-            }
             case _ =>
           }
-        }
         case e: ScReferenceExpression if e.getParent.isInstanceOf[ScInfixExpr] &&
-                e.getParent.asInstanceOf[ScInfixExpr].operation == e => {
+                e.getParent.asInstanceOf[ScInfixExpr].operation == e =>
           resolveResult.implicitFunction match {
-            case Some(fun) => {
+            case Some(fun) =>
               val inf = e.getParent.asInstanceOf[ScInfixExpr]
               val expr = if (inf.isLeftAssoc) inf.rOp else inf.lOp
               highlightImplicitMethod(expr, resolveResult, refElement, fun, holder)
-            }
             case _ =>
           }
-        }
         case _ =>
       }
     }
@@ -648,13 +643,12 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
   private def checkSelfInvocation(self: ScSelfInvocation, holder: AnnotationHolder) {
     self.bind match {
       case Some(elem) =>
-      case None => {
+      case None =>
         if (isAdvancedHighlightingEnabled(self)) {
           val annotation: Annotation = holder.createErrorAnnotation(self.thisElement,
             "Cannot find constructor for this call")
           annotation.setHighlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
         }
-      }
     }
   }
 
@@ -672,11 +666,10 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
             resolve.length == 1) {
       val resolveResult = resolve(0).asInstanceOf[ScalaResolveResult]
       resolveResult.implicitFunction match {
-        case Some(fun) => {
+        case Some(fun) =>
           val qualifier = refElement.qualifier.get
           val expr = qualifier.asInstanceOf[ScExpression]
           highlightImplicitMethod(expr, resolveResult, refElement, fun, holder)
-        }
         case _ =>
       }
     }
@@ -792,7 +785,7 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
         case i: ScIfStmt if i.elseBranch != None =>
         case fun: ScFunctionExpr =>
         case tr: ScTryStmt =>
-        case _ => {
+        case _ =>
           expr.getParent match {
             case a: ScAssignStmt if a.getRExpression == Some(expr) && a.isDynamicNamedAssignment => return
             case args: ScArgumentExprList => return
@@ -816,11 +809,11 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
 
           expr.expectedTypeEx(fromUnderscore) match {
             case Some((tp: ScType, _)) if tp equiv psi.types.Unit => //do nothing
-            case Some((tp: ScType, typeElement)) => {
+            case Some((tp: ScType, typeElement)) =>
               import org.jetbrains.plugins.scala.lang.psi.types._
               val expectedType = Success(tp, None)
               implicitFunction match {
-                case Some(fun) => {
+                case Some(fun) =>
                   //todo:
                   /*val typeFrom = expr.getType(TypingContext.empty).getOrElse(Any)
                   val typeTo = exprType.getOrElse(Any)
@@ -828,7 +821,6 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
                   val range = expr.getTextRange
                   showImplicitUsageAnnotation(exprText, typeFrom, typeTo, fun, range, holder,
                     EffectType.LINE_UNDERSCORE, Color.LIGHT_GRAY)*/
-                }
                 case None => //do nothing
               }
               val conformance = ScalaAnnotator.smartCheckConformance(expectedType, exprType)
@@ -854,10 +846,8 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
                   }
                 }
               }
-            }
             case _ => //do nothing
           }
-        }
       }
     }
     if (ScUnderScoreSectionUtil.isUnderscoreFunction(expr)) {
@@ -868,7 +858,7 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
 
   private def checkExpressionImplicitParameters(expr: ScExpression, holder: AnnotationHolder) {
     expr.findImplicitParameters match {
-      case Some(seq) => {
+      case Some(seq) =>
         for (resolveResult <- seq) {
           if (resolveResult != null) {
             ImportTracker.getInstance(expr.getProject).registerUsedImports(
@@ -876,7 +866,6 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
             registerUsedElement(expr, resolveResult, checkWrite = false)
           }
         }
-      }
       case _ =>
     }
   }
@@ -906,16 +895,14 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
   private def checkExplicitTypeForReturnStatement(ret: ScReturnStmt, holder: AnnotationHolder) {
     val fun: ScFunction = PsiTreeUtil.getParentOfType(ret, classOf[ScFunction])
     fun match {
-      case null => {
+      case null =>
         val error = ScalaBundle.message("return.outside.method.definition")
         val annotation: Annotation = holder.createErrorAnnotation(ret.returnKeyword, error)
         annotation.setHighlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
-      }
-      case _ if !fun.hasAssign || fun.returnType.exists(_ == psi.types.Unit) => {
+      case _ if !fun.hasAssign || fun.returnType.exists(_ == psi.types.Unit) =>
         return
-      }
       case _ => fun.returnTypeElement match {
-        case Some(x: ScTypeElement) => {
+        case Some(x: ScTypeElement) =>
           import org.jetbrains.plugins.scala.lang.psi.types._
           val funType = fun.returnType
           funType match {
@@ -927,7 +914,6 @@ class ScalaAnnotator extends Annotator with FunctionAnnotator with ScopeAnnotato
             case None => ExpressionTypeResult(Success(psi.types.Unit, None), Set.empty, None)
           }
           ImportTracker.getInstance(ret.getProject).registerUsedImports(ret.getContainingFile.asInstanceOf[ScalaFile], importUsed)
-        }
         case _ =>
       }
     }
