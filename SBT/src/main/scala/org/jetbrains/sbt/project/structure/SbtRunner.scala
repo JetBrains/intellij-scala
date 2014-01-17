7df6c9f9ab7e1861a0404d60eaf02d0ce8daad0d
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-01-17 14:57:29 +0400

    SBT: require sbt-structure v2.1.1 #SCL-6479

--- SBT/src/main/scala/org/jetbrains/sbt/project/structure/SbtRunner.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/structure/SbtRunner.scala
@@ -43,7 +43,7 @@ class SbtRunner(vmOptions: Seq[String], customLauncher: Option[File]) {
           FileUtil.writeToFile(new File(globalPluginsDirectory.getPath, "build.sbt"),
             """resolvers += "sbt-releases" at "http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"
 
-              addSbtPlugin("org.jetbrains" % "sbt-structure" % "2.1.0")""".replace("\r", ""))
+              addSbtPlugin("org.jetbrains" % "sbt-structure" % "2.1.1")""".replace("\r", ""))
 
           writeLinesTo(commandsFile,
             s"set artifactPath := file(\042${path(structureFile)}\042)",
