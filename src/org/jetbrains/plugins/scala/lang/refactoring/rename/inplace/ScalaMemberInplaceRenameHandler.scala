c0383ffe690a9c76176b4153c6d73ca3e6455412
niktrop <Nikolay.Tropin@jetbrains.com>
2014-01-20 14:55:29 +0400

    Global search removed from renaming local elements

--- src/org/jetbrains/plugins/scala/lang/refactoring/rename/inplace/ScalaMemberInplaceRenameHandler.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/rename/inplace/ScalaMemberInplaceRenameHandler.scala
@@ -15,49 +15,43 @@ import com.intellij.psi.util.PsiUtilBase
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScNewTemplateDefinition
+import org.jetbrains.plugins.scala.lang.refactoring.rename.ScalaRenameUtil
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import org.jetbrains.plugins.scala.lang.psi.light.{LightScalaMethod, PsiClassWrapper}
 import javax.swing.JList
+import com.intellij.psi.search.LocalSearchScope
 
 /**
  * Nikolay.Tropin
  * 6/20/13
  */
-class ScalaMemberInplaceRenameHandler extends MemberInplaceRenameHandler{
-
-  def renameProcessor(element: PsiElement): RenamePsiElementProcessor = {
-    val processor = if (element != null && element.getLanguage.getID == "Scala") RenamePsiElementProcessor.forElement(element) else null
-    if (processor != RenamePsiElementProcessor.DEFAULT) processor else null
-  }
+class ScalaMemberInplaceRenameHandler extends MemberInplaceRenameHandler with ScalaInplaceRenameHandler {
 
   override def isAvailable(element: PsiElement, editor: Editor, file: PsiFile): Boolean = {
     val processor = renameProcessor(element)
-    editor.getSettings.isVariableInplaceRenameEnabled && processor != null && processor.canProcessElement(element)
+    editor.getSettings.isVariableInplaceRenameEnabled && processor != null && processor.canProcessElement(element) && 
+            !element.getUseScope.isInstanceOf[LocalSearchScope]
   }
 
   protected override def createMemberRenamer(substituted: PsiElement,
                                              elementToRename: PsiNameIdentifierOwner,
                                              editor: Editor): MemberInplaceRenamer = {
     substituted match {
+      case clazz: PsiClass if elementToRename.isInstanceOf[PsiClassWrapper] =>
+        new ScalaMemberInplaceRenamer(elementToRename, clazz, editor)
       case clazz: PsiClass =>
         val companion = ScalaPsiUtil.getBaseCompanionModule(clazz)
-        if (companion.isDefined) new ScalaMemberInplaceRenamer(clazz, companion.get, editor)
-        else new ScalaMemberInplaceRenamer(clazz, clazz, editor)
+        new ScalaMemberInplaceRenamer(clazz, companion.getOrElse(clazz), editor)
       case subst: PsiNamedElement => new ScalaMemberInplaceRenamer(elementToRename, subst, editor)
       case _ => throw new IllegalArgumentException("Substituted element for renaming has no name")
     }
   }
 
   override def doRename(elementToRename: PsiElement, editor: Editor, dataContext: DataContext): InplaceRefactoring = {
-    def specialMethodPopup(fun: ScFunction): Unit = {
-      val clazz = fun.containingClass
-      val clazzType = clazz match {
-        case _: ScObject => "object"
-        case _: ScClass => "class"
-        case _: ScTrait => "trait"
-      }
-      val title = ScalaBundle.message("rename.special.method.title")
-      val renameClass = ScalaBundle.message("rename.special.method.rename.class", clazzType)
-      val cancel = ScalaBundle.message("rename.special.method.cancel")
-      val list: JList[_] = new JBList(renameClass, cancel).asInstanceOf[JList[_]]
+    def showSubstitutePopup(title: String, positive: String, subst: => PsiNamedElement): Unit = {
+      val cancel = ScalaBundle.message("rename.cancel")
+      val list: JList[_] = new JBList(positive, cancel).asInstanceOf[JList[_]]
       JBPopupFactory.getInstance.createListPopupBuilder(list)
               .setTitle(title)
               .setMovable(false)
@@ -66,12 +60,36 @@ class ScalaMemberInplaceRenameHandler extends MemberInplaceRenameHandler{
               .setItemChoosenCallback(new Runnable {
         def run(): Unit = {
           list.getSelectedValue match {
-            case s: String if s == renameClass =>
-              doDialogRename(clazz, editor.getProject, null, editor)
+            case s: String if s == positive =>
+              val file = subst.getContainingFile.getVirtualFile
+              if (FileDocumentManager.getInstance.getDocument(file) == editor.getDocument) {
+                editor.getCaretModel.moveToOffset(subst.getTextOffset)
+                doRename(subst, editor, dataContext)
+              } else {
+                doDialogRename(subst, editor.getProject, null, editor)
+              }
             case s: String if s == cancel =>
           }
         }
       }).createPopup.showInBestPositionFor(editor)
+    }
+
+    def specialMethodPopup(fun: ScFunction): Unit = {
+      val clazz = fun.containingClass
+      val clazzType = clazz match {
+        case _: ScObject => "object"
+        case _: ScClass => "class"
+        case _: ScTrait => "trait"
+        case _: ScNewTemplateDefinition => "instance"
+      }
+      val title = ScalaBundle.message("rename.special.method.title")
+      val positive = ScalaBundle.message("rename.special.method.rename.class", clazzType)
+      showSubstitutePopup(title, positive, ScalaRenameUtil.findSubstituteElement(elementToRename))
+    }
+    def aliasedElementPopup(ref: ScReferenceElement): Unit = {
+      val title = ScalaBundle.message("rename.aliased.title")
+      val positive = ScalaBundle.message("rename.aliased.rename.actual")
+      showSubstitutePopup(title, positive, ScalaRenameUtil.findSubstituteElement(elementToRename))
     }
 
     val atCaret = PsiUtilBase.getElementAtCaret(editor)
@@ -81,17 +99,20 @@ class ScalaMemberInplaceRenameHandler extends MemberInplaceRenameHandler{
       case named: ScNamedElement => named.nameId
       case _ => null
     }
-    elementToRename match {
+    ScalaRenameUtil.findSubstituteElement(elementToRename) match {
       case fun: ScFunction
         if nameId != null && nameId.getText == fun.name && Seq("apply", "unapply", "unapplySeq").contains(fun.name) || fun.isConstructor =>
           specialMethodPopup(fun)
           null
-      case _ => super.doRename(elementToRename, editor, dataContext)
+      case elem =>
+        if (nameId != null) nameId.getParent match {
+          case ref: ScReferenceElement if ScalaRenameUtil.isAliased(ref) =>
+            aliasedElementPopup(ref)
+            return null
+          case _ =>
+        }
+        super.doRename(elem, editor, dataContext)
     }
-
   }
 
-  protected def doDialogRename(element: PsiElement, project: Project, nameSuggestionContext: PsiElement, editor: Editor): Unit = {
-    PsiElementRenameHandler.rename(element, project, nameSuggestionContext, editor)
-  }
 }
