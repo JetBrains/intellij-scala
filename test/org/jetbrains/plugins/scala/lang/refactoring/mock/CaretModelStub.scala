f3050aa42faea455fb78c1f280d5de3bdbdb7540
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-02-07 14:57:40 +0400

    Fixed compilation

--- test/org/jetbrains/plugins/scala/lang/refactoring/mock/CaretModelStub.scala
+++ test/org/jetbrains/plugins/scala/lang/refactoring/mock/CaretModelStub.scala
@@ -1,8 +1,10 @@
 package org.jetbrains.plugins.scala.lang.refactoring.mock
 
-import com.intellij.openapi.editor.{VisualPosition, LogicalPosition, CaretModel}
+import com.intellij.openapi.editor.{Caret, VisualPosition, LogicalPosition, CaretModel}
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.editor.event.CaretListener
+import java.util
+import com.intellij.openapi.util.Segment
 
 /**
  * Pavel Fatin
@@ -36,4 +38,24 @@ class CaretModelStub extends CaretModel {
   def moveCaretRelatively(columnShift: Int, lineShift: Int, withSelection: Boolean, blockSelection: Boolean, scrollToCaret: Boolean): Unit = {}
 
   def isUpToDate = false
+
+  override def runForEachCaret(runnable: Runnable): Unit = ???
+
+  override def setCarets(caretPositions: util.List[LogicalPosition], selections: util.List[_ <: Segment]): Unit = ???
+
+  override def removeSecondaryCarets(): Unit = ???
+
+  override def removeCaret(caret: Caret): Boolean = ???
+
+  override def addCaret(pos: VisualPosition): Caret = ???
+
+  override def getCaretAt(pos: VisualPosition): Caret = ???
+
+  override def getAllCarets: util.Collection[Caret] = ???
+
+  override def getPrimaryCaret: Caret = ???
+
+  override def getCurrentCaret: Caret = ???
+
+  override def supportsMultipleCarets(): Boolean = false
 }
