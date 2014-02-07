ff43640e1c6713450494e595b1d07c82efe82c17
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-02-07 14:53:17 +0400

    Fixed compilation

--- test/org/jetbrains/plugins/scala/lang/refactoring/mock/CaretModelMock.scala
+++ test/org/jetbrains/plugins/scala/lang/refactoring/mock/CaretModelMock.scala
@@ -1,6 +1,8 @@
 package org.jetbrains.plugins.scala.lang.refactoring.mock
 
-import com.intellij.openapi.editor.LogicalPosition
+import com.intellij.openapi.editor.{VisualPosition, Caret, LogicalPosition}
+import java.util
+import com.intellij.openapi.util.Segment
 
 /**
  * Pavel Fatin
@@ -10,4 +12,24 @@ class CaretModelMock(offset: Int, pos: LogicalPosition) extends CaretModelStub {
   override def getOffset: Int = offset
 
   override def getLogicalPosition: LogicalPosition = pos
+
+  override def getAllCarets: util.Collection[Caret] = ???
+
+  override def getPrimaryCaret: Caret = ???
+
+  override def getCurrentCaret: Caret = ???
+
+  override def supportsMultipleCarets(): Boolean = false
+
+  override def runForEachCaret(runnable: Runnable): Unit = ???
+
+  override def setCarets(caretPositions: util.List[LogicalPosition], selections: util.List[_ <: Segment]): Unit = ???
+
+  override def removeSecondaryCarets(): Unit = ???
+
+  override def removeCaret(caret: Caret): Boolean = ???
+
+  override def addCaret(pos: VisualPosition): Caret = ???
+
+  override def getCaretAt(pos: VisualPosition): Caret = ???
 }
