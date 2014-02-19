82f7c333fca66e01de930db1b108819a17f6fd30
Dmitry.Naydanov <dmitry.naydanov@jetbrains.com>
2014-02-19 21:00:12 +0400

    Different macro printers for 2.10/2.11 + one default; proper range processing for psi with preceding comments

--- src/org/jetbrains/plugins/scala/worksheet/processor/WorksheetSourceProcessor.scala
+++ src/org/jetbrains/plugins/scala/worksheet/processor/WorksheetSourceProcessor.scala
@@ -10,6 +10,9 @@ import com.intellij.psi.{PsiComment, PsiElement, PsiWhiteSpace}
 import com.intellij.openapi.editor.Editor
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportStmt
 import com.intellij.openapi.util.text.StringUtil
+import scala.annotation.tailrec
+import com.intellij.openapi.module.ModuleUtilCore
+import org.jetbrains.plugins.scala.config.ScalaFacet
 
 /**
  * User: Dmitry Naydanov
@@ -44,14 +47,27 @@ object WorksheetSourceProcessor {
     val name = s"A$$A$iterNumber"
     val instanceName = s"inst$$A$$A"
     
-    val macroPrinterName = "worksheet$$macro$$printer"
+//    val macroPrinterName = "MacroPrinter210" // "worksheet$$macro$$printer"
+    
+    val macroPrinterName = Option(ModuleUtilCore.findModuleForFile(srcFile.getVirtualFile, srcFile.getProject)) flatMap {
+      case module => ScalaFacet findIn module flatMap {
+        case facet => facet.compiler flatMap {
+          case c =>
+            c.version collect {
+              case v if v.startsWith("2.10") => "MacroPrinter210"
+              case v if v.startsWith("2.11") => "MacroPrinter211"
+            }
+        }
+      }
+    } getOrElse "MacroPrinter"
+    
     val runPrinterName = "worksheet$$run$$printer"
 
     val printMethodName = "println"
 
     val ifDocument = ifEditor map (_.getDocument)
     val classPrologue = name // s"$name ${if (iterNumber > 0) s"extends A${iterNumber - 1}" }" //todo disabled until I implement incremental code generation
-    val objectPrologue = s"import _root_.org.jetbrains.plugins.scala.worksheet.MacroPrinter\n\n object $name {val $macroPrinterName = MacroPrinter  \n"
+    val objectPrologue = s"import _root_.org.jetbrains.plugins.scala.worksheet.$macroPrinterName\n\n object $name { \n"
     
     val startText = ""
     
@@ -70,30 +86,76 @@ object WorksheetSourceProcessor {
       case _ => ";"
     }
     
-    @inline def psiToLineNumbers(psi: PsiElement): Option[(Int, Int)] = ifDocument map {
+    @inline def psiToLineNumbers(psi: PsiElement): Option[String] = ifDocument map {
       case document =>
-        val range = psi.getTextRange
-        val start = range.getStartOffset
-        val end = range.getEndOffset
-        (document getLineNumber start, document getLineNumber end)
+        var actualPsi = psi
+        
+        actualPsi.getFirstChild match {
+          case _: PsiComment =>
+            @tailrec
+            def iter(wsOrComment: PsiElement): PsiElement = {
+              wsOrComment match {
+                case comment: PsiComment => 
+                  appendPsiComment(comment)
+                  iter(comment.getNextSibling)
+                case ws: PsiWhiteSpace =>
+                  appendPsiWhitespace(ws)
+                  iter(ws.getNextSibling)
+                case a: PsiElement if a.getTextRange.isEmpty => iter(a.getNextSibling)
+                case a: PsiElement => a
+                case _ => psi  
+              }
+            }
+
+            actualPsi = iter(actualPsi.getFirstChild)
+          case _ =>
+        }
+        
+        
+        val start = actualPsi.getTextRange.getStartOffset //actualPsi for start and psi for end - it is intentional
+        val end = psi.getTextRange.getEndOffset
+        s"${document getLineNumber start}|${document getLineNumber end}"
     }
     
-    @inline def appendPsiLineInfo(psi: PsiElement) {
-      objectRes append printMethodName append "(\"" append END_TOKEN_MARKER append (psiToLineNumbers(psi) map {
-        case (a, b) => s"$a|$b"
-      } getOrElse "") append "\")\n"
+    @inline def appendPsiLineInfo(psi: PsiElement, numberStr: Option[String] = None) {
+      val lineNumbers = numberStr getOrElse psiToLineNumbers(psi)
+      
+      objectRes append printMethodName append "(\"" append END_TOKEN_MARKER append lineNumbers append "\")\n"
     }
     
     @inline def appendDeclaration(psi: ScalaPsiElement) {
       classRes append psi.getText append insertNlsFromWs(psi)
     }
     
-    @inline def appendAll(psi: ScalaPsiElement) {
+    @inline def appendPsiComment(comment: PsiComment) {
+      val range = comment.getTextRange
+      ifDocument map {
+        document => document.getLineNumber(range.getEndOffset) - document.getLineNumber(range.getStartOffset) + 1
+      } map {
+        case differ => for (_ <- 0 until differ) objectRes append printMethodName append "()\n"
+      } getOrElse {
+        val count = comment.getText count (_ == '\n')
+        for (_ <- 0 until count) objectRes append printMethodName append "()\n"
+      }
+    }
+    
+    @inline def appendPsiWhitespace(ws: PsiWhiteSpace) {
+      val count = ws.getText count (_ == '\n')
+      for (_ <- 1 until count) objectRes append printMethodName append "()\n"
+    }
+    
+    @inline def appendAll(psi: ScalaPsiElement, numberStr: Option[String] = None) {
       appendDeclaration(psi)
-      appendPsiLineInfo(psi)
+      appendPsiLineInfo(psi, numberStr)
     }
     
     @inline def withPrint(text: String) = printMethodName + "(\"" + startText + text + "\")\n" 
+    
+    @inline def withPrecomputeLines(psi: ScalaPsiElement, body: => Unit) {
+      val lineNum = psiToLineNumbers(psi)
+      body
+      appendAll(psi, lineNum)
+    }
     
     def withTempVar(callee: String) = 
       "{val $$temp$$ = " + instanceName + "." + callee + s"; $macroPrinterName.printDefInfo(" + "$$temp$$" + ")" + 
@@ -101,63 +163,54 @@ object WorksheetSourceProcessor {
     
     srcFile.getChildren foreach {
       case tpe: ScTypeAlias =>
-        objectRes append withPrint(s"defined type alias ${tpe.name}")
-        
-        appendAll(tpe)
+        withPrecomputeLines(tpe, {
+          objectRes append withPrint(s"defined type alias ${tpe.name}")
+        } )
       case fun: ScFunction =>
-        objectRes append (printMethodName + "(\"" + fun.getName + ": \" + " + macroPrinterName + 
-          ".printDefInfo(" + instanceName + "." + fun.getName + " _)" + eraseClassName + ")\n") 
-        
-        appendAll(fun)
+        withPrecomputeLines(fun, {
+          objectRes append (printMethodName + "(\"" + fun.getName + ": \" + " + macroPrinterName +
+            ".printDefInfo(" + instanceName + "." + fun.getName + " _)" + eraseClassName + ")\n")
+        })
       case tpeDef: ScTypeDefinition =>
-        val keyword = tpeDef match {
-          case _: ScClass => "class"
-          case _: ScTrait => "trait"
-          case _ => "module"
-        }
+        withPrecomputeLines(tpeDef, {
+          val keyword = tpeDef match {
+            case _: ScClass => "class"
+            case _: ScTrait => "trait"
+            case _ => "module"
+          }
 
-        objectRes append withPrint(s"defined $keyword ${tpeDef.name}")
-
-        appendAll(tpeDef)
+          objectRes append withPrint(s"defined $keyword ${tpeDef.name}")
+        })
       case valDef: ScPatternDefinition =>
-        valDef.bindings foreach {
-          case p => 
-            val pName = p.name
-            val defName = s"get$$$$instance$$$$$pName"
-            
-            classRes append s"def $defName = $pName;$END_GENERATED_MARKER"
-            objectRes append (printMethodName + "(\"" + startText + pName + ": \" + " + withTempVar(defName) + ")\n")
-        }
-        
-        appendAll(valDef)
-      case comm: PsiComment =>
-        val range = comm.getTextRange
-        ifDocument map {
-          document => document.getLineNumber(range.getEndOffset) - document.getLineNumber(range.getStartOffset) + 1
-        } map {
-          case differ => for (_ <- 0 until differ) objectRes append printMethodName append "()\n"
-        } getOrElse {
-          val count = comm.getText count (_ == '\n')
-          for (_ <- 0 until count) objectRes append printMethodName append "()\n"
-        }
+        withPrecomputeLines(valDef, {
+          valDef.bindings foreach {
+            case p =>
+              val pName = p.name
+              val defName = s"get$$$$instance$$$$$pName"
+
+              classRes append s"def $defName = $pName;$END_GENERATED_MARKER"
+              objectRes append (printMethodName + "(\"" + startText + pName + ": \" + " + withTempVar(defName) + ")\n")
+          }
+        })
+      case comm: PsiComment => appendPsiComment(comm)
       case imp: ScImportStmt => 
         val text = imp.getText
+        val lineNums = psiToLineNumbers(imp)
         
         objectRes append s"$printMethodName($macroPrinterName.printImportInfo({$text;}))\n"
 
         classRes append  "import _root_."  append text.substring("import ".length) append insertNlsFromWs(imp)
-        appendPsiLineInfo(imp)
+        appendPsiLineInfo(imp, lineNums)
       case expr: ScExpression =>
         val resName = s"get$$$$instance$$$$res$resCount"
+        val lineNums = psiToLineNumbers(expr)
 
         classRes append s"def $resName = $END_GENERATED_MARKER${expr.getText}${insertNlsFromWs(expr)}" 
         objectRes append (printMethodName + "(\"res" + startText + resCount + ": \" + " + withTempVar(resName) + ")\n")
-        appendPsiLineInfo(expr)
+        appendPsiLineInfo(expr, lineNums)
         
         resCount += 1
-      case ws: PsiWhiteSpace =>
-        val count = ws.getText count (_ == '\n')
-        for (_ <- 1 until count) objectRes append printMethodName append "()\n"
+      case ws: PsiWhiteSpace => appendPsiWhitespace(ws)
       case scalaPsi: ScalaPsiElement =>
         
       case _ => 
