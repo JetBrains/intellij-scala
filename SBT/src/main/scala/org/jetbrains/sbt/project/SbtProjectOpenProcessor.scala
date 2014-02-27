71ed5e762cbfe5302c79d2f4145e4edd822b6794
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-02-27 17:00:41 +0100

    SBT: additional fixes for #SCL-6464, refactoring (cherry picked from commit 9522dae)

--- SBT/src/main/scala/org/jetbrains/sbt/project/SbtProjectOpenProcessor.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/SbtProjectOpenProcessor.scala
@@ -13,14 +13,15 @@ import com.intellij.ide.wizard.Step
  * @author Pavel Fatin
  */
 class SbtProjectOpenProcessor(builder: SbtProjectImportBuilder) extends ProjectOpenProcessorBase[SbtProjectImportBuilder](builder) {
-  // Actual detection is done via the canOpenProject method
-  // (to "open" projects without build.sbt file).
-  def getSupportedExtensions = Array(Sbt.BuildFile)
+  // Actual detection is done via the canOpenProject method (to "open" projects without build.sbt file).
+  // However, these "extensions" are used inside ProjectOpenProcessorBase.doOpenProject to determine a project root directory.
+  // TODO Don't depend on file extensions in ProjectOpenProcessorBase.doOpenProject to discover a project root (IDEA)
+  def getSupportedExtensions = Array(Sbt.BuildFile, Sbt.ProjectDirectory)
 
   override def canOpenProject(file: VirtualFile) = SbtProjectImportProvider.canImport(file)
 
   override def doQuickImport(file: VirtualFile, wizardContext: WizardContext) = {
-    val path = if (file.isDirectory) file.getPath else file.getParent.getPath
+    val path = SbtProjectImportProvider.projectRootOf(file).getPath
 
     val dialog = new AddModuleWizard(null, path, new SbtProjectImportProvider(getBuilder))
 
