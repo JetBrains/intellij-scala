5c50cb909b973b19ae3371584935584105cea2e0
Konstantin Fedorov <k.a.feodorov@gmail.com>
2014-03-15 22:30:58 +0400

    autocomplete works in q case pattern for first literal

--- src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScInterpolationPatternImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScInterpolationPatternImpl.scala
@@ -4,20 +4,17 @@ package lang.psi.impl.base.patterns
 import com.intellij.lang.ASTNode
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns._
 import com.intellij.psi._
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElementImpl
-import org.jetbrains.plugins.scala.lang.psi.api.base.{ScLiteral, ScStableCodeReferenceElement}
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReferenceElement
 import org.jetbrains.plugins.scala.lang.psi.impl.expr.ScInterpolatedPrefixReference
-import org.jetbrains.plugins.scala.lang.psi.impl.base.ScInterpolationStableCodeReferenceElementImpl
+import org.jetbrains.plugins.scala.lang.psi.impl.base.{ScLiteralImpl, ScInterpolationStableCodeReferenceElementImpl}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
-import org.jetbrains.plugins.scala.lang.psi.types.ScType
-import com.intellij.psi.impl.source.tree.injected.StringLiteralEscaper
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
 
 /**
  * @author kfeodorov
  * @since 01.03.14.
  */
-class ScInterpolationPatternImpl (_node: ASTNode) extends ScalaPsiElementImpl(_node) with ScInterpolationPattern {
+class ScInterpolationPatternImpl (_node: ASTNode) extends ScLiteralImpl(_node) with ScInterpolationPattern {
 
   override def accept(visitor: PsiElementVisitor) {
     visitor match {
@@ -36,30 +33,13 @@ class ScInterpolationPatternImpl (_node: ASTNode) extends ScalaPsiElementImpl(_n
 
     prefix.multiResolve(incomplete = false).headOption match {
       case Some(applyFuncResolve) => applyFuncResolve.getElement.getParent.getChildren.filter{_.isInstanceOf[ScFunctionDefinition]}.lastOption match {
-          case Some(unapplyFunc) => new ScInterpolationStableCodeReferenceElementImpl(unapplyFunc.getNode)
-          case None => null
-        }
+        case Some(unapplyFunc) => new ScInterpolationStableCodeReferenceElementImpl(unapplyFunc.getNode)
+        case None => null
+      }
       case None => null
     }
   }
 
   override def getValue: AnyRef = args.getText.drop(1) //drop leading quote
-
-  override def getAnnotationOwner(annotationOwnerLookUp: (ScLiteral) => Option[PsiAnnotationOwner with PsiElement]): Option[PsiAnnotationOwner] = None
-
-  override def isMultiLineString: Boolean = args.getText.endsWith("\"\"\"")
-
-  override def isString: Boolean = true
-
-  /**
-   * This method works only for null literal (to avoid possibly dangerous usage)
-   * @param tp type, which should be returned by method getTypeWithouImplicits
-   */
-  override def setTypeWithoutImplicits(tp: Option[ScType]): Unit = {}
-
-  override def createLiteralTextEscaper(): LiteralTextEscaper[_ <: PsiLanguageInjectionHost] = new StringLiteralEscaper(this)
-
-  override def updateText(text: String): PsiLanguageInjectionHost = this
-
-  override def isValidHost: Boolean = getValue.isInstanceOf[String]
+  override def isMultiLineString: Boolean = getText.endsWith("\"\"\"")
 }
