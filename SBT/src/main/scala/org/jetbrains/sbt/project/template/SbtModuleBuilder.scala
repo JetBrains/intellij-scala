7743b97f8b35f7cc0c8c1ba232dd68f1a0a4b647
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-03-26 14:32:47 +0100

    SbtModuleBuilder refactoring (cherry picked from commit b828b4e)

--- SBT/src/main/scala/org/jetbrains/sbt/project/template/SbtModuleBuilder.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/template/SbtModuleBuilder.scala
@@ -1,68 +1,62 @@
 package org.jetbrains.sbt
 package project.template
 
-import com.intellij.openapi.externalSystem.service.project.wizard.AbstractExternalModuleBuilder
 import org.jetbrains.sbt.project.settings.SbtProjectSettings
-import com.intellij.openapi.module.{JavaModuleType, Module, ModifiableModuleModel, ModuleType}
-import com.intellij.ide.util.projectWizard.ModuleBuilder
-import com.intellij.openapi.vfs.{LocalFileSystem, VirtualFile}
-import java.io.File
-import com.intellij.openapi.util.io.{FileUtilRt, FileUtil}
-import javax.swing.Icon
 import org.jetbrains.sbt.project.SbtProjectSystem
+import com.intellij.openapi.module.{JavaModuleType, ModifiableModuleModel}
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.util.io.FileUtil._
 import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.externalSystem.service.project.wizard.AbstractExternalModuleBuilder
 import com.intellij.openapi.externalSystem.settings.{ExternalSystemSettingsListener, AbstractExternalSystemSettings}
+import java.io.File
 
 /**
- * User: Dmitry Naydanov
+ * User: Dmitry Naydanov, Pavel Fatin
  * Date: 11/23/13
  */
 class SbtModuleBuilder extends AbstractExternalModuleBuilder[SbtProjectSettings](SbtProjectSystem.Id, new SbtProjectSettings) {
-  def getExternalProjectConfigFile(contentRootDir: VirtualFile): VirtualFile = null
+  def getModuleType = JavaModuleType.getModuleType
 
-  def getTemplateConfigName(settings: SbtProjectSettings): String = null
-
-  def getModuleType: ModuleType[_ <: ModuleBuilder] = JavaModuleType.getModuleType
-
-  override def createModule(moduleModel: ModifiableModuleModel): Module = {
-    createSbtStub()
+  override def createModule(moduleModel: ModifiableModuleModel) = {
+    val directory = getModuleFileDirectory.toFile
+    
+    if (directory.exists) {
+      createProjectTemplateIn(directory, getName)
+    }
+    
     super.createModule(moduleModel)
   }
   
-  private def createSbtStub() {
-    val modulePath = getModuleFileDirectory
-    val moduleName = getName
+  private def createProjectTemplateIn(directory: File, name: String) {
+    val buildFile = directory / Sbt.BuildFile
+    val projectDir = directory / Sbt.ProjectDirectory
+    val pluginsFile = projectDir / Sbt.PluginsFile
 
-    val dir = new File(modulePath)
+    if (!buildFile.createNewFile() ||
+            !projectDir.mkdir() ||
+            !pluginsFile.createNewFile()) return
 
-    if (!dir.exists() || !dir.isDirectory) return
-    
-    val buildFile = new File(dir, "build.sbt")
-    val projectDir = new File(dir, "project")
-    val pluginsFile = new File(projectDir, "plugins.sbt")
-
-    if (!buildFile.createNewFile() || !FileUtil.createDirectory(projectDir) || !pluginsFile.createNewFile()) return
-
-    FileUtil.writeToFile(buildFile, SbtModuleBuilder buildSbt moduleName)
-    FileUtil.writeToFile(pluginsFile, SbtModuleBuilder.pluginsSbt)
+    writeToFile(buildFile, SbtModuleBuilder.formatProjectDefinition(name))
+    writeToFile(pluginsFile, SbtModuleBuilder.PluginsDefinition)
   }
 
-  override def getNodeIcon: Icon = Sbt.Icon
+  override def getNodeIcon = Sbt.Icon
 
   override def setupRootModel(model: ModifiableRootModel) {
     val contentPath = getContentEntryPath
-    if (StringUtil isEmpty contentPath) return
+    if (StringUtil.isEmpty(contentPath)) return
 
-    val contentRootDir = new File(contentPath)
-    FileUtilRt createDirectory contentRootDir
+    val contentRootDir = contentPath.toFile
+    createDirectory(contentRootDir)
 
     val fileSystem = LocalFileSystem.getInstance
-    val vContentRootDir = fileSystem refreshAndFindFileByIoFile contentRootDir
+    val vContentRootDir = fileSystem.refreshAndFindFileByIoFile(contentRootDir)
     if (vContentRootDir == null) return
 
-    model addContentEntry vContentRootDir
+    model.addContentEntry(vContentRootDir)
     model.inheritSdk()
     val settings =
       ExternalSystemApiUtil.getSettings(model.getProject, SbtProjectSystem.Id).
@@ -72,19 +66,19 @@ class SbtModuleBuilder extends AbstractExternalModuleBuilder[SbtProjectSettings]
 
     val externalProjectSettings = getExternalProjectSettings
 
-    externalProjectSettings setExternalProjectPath getContentEntryPath
-    externalProjectSettings setCreateEmptyContentRootDirectories true //create empty dirs anyway as src in our template is empty
+    externalProjectSettings.setExternalProjectPath(getContentEntryPath)
+    externalProjectSettings.setCreateEmptyContentRootDirectories(true) //create empty dirs anyway as src in our template is empty
 
-    settings linkProject externalProjectSettings
+    settings.linkProject(externalProjectSettings)
   }
 }
 
-object SbtModuleBuilder {
-  def buildSbt(name: String) =
+private object SbtModuleBuilder {
+  def formatProjectDefinition(name: String) =
     s"""name := "$name"
       |
       |version := "1.0"
     """.stripMargin
   
-  def pluginsSbt = "logLevel := Level.Warn"
+  def PluginsDefinition = "logLevel := Level.Warn"
 }
