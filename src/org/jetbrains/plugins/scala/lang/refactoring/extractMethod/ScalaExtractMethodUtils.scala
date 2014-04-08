d23be867038946b2c1ef862f32564a0f04a72ef3
niktrop <Nikolay.Tropin@jetbrains.com>
2014-04-08 11:19:07 +0400

    ScalaExtractMethodUtils refactored

--- src/org/jetbrains/plugins/scala/lang/refactoring/extractMethod/ScalaExtractMethodUtils.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/extractMethod/ScalaExtractMethodUtils.scala
@@ -1,14 +1,13 @@
 package org.jetbrains.plugins.scala.lang
 package refactoring.extractMethod
 
-import _root_.com.intellij.psi._
-import _root_.org.jetbrains.annotations.Nullable
-import _root_.org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
-import _root_.org.jetbrains.plugins.scala.lang.psi.{ScalaPsiUtil, ScalaPsiElement}
-import _root_.org.jetbrains.plugins.scala.lang.resolve.ScalaResolveResult
+import com.intellij.psi._
+import org.jetbrains.annotations.Nullable
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
+import org.jetbrains.plugins.scala.lang.psi.{ScalaPsiUtil, ScalaPsiElement}
+import org.jetbrains.plugins.scala.lang.resolve.ScalaResolveResult
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
-import java.util.ArrayList
-import com.intellij.refactoring.util.{VariableData, ParameterTablePanel}
+import com.intellij.refactoring.util.VariableData
 import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.lang.psi.fake.FakePsiParameter
 import org.jetbrains.plugins.scala.lang.psi.dataFlow.impl.reachingDefs.VariableInfo
@@ -24,8 +23,8 @@ import scala.util.Sorting
 import psi.types.nonvalue.Parameter
 import psi.types._
 import org.jetbrains.plugins.scala.extensions.toPsiNamedElementExt
-import com.intellij.openapi.project.Project
 import java.util
+import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
 
 /**
  * User: Alexander Podkhalyuzin
@@ -34,9 +33,8 @@ import java.util
 
 object ScalaExtractMethodUtils {
   def createMethodFromSettings(settings: ScalaExtractMethodSettings): ScFunction = {
-    val builder: StringBuilder = new StringBuilder
-    builder.append(settings.visibility)
-    builder.append("def ").append(settings.methodName)
+    val accessMod = settings.visibility
+    val methodName = settings.methodName
     val tp: ArrayBuffer[ScTypeParam] = new ArrayBuffer
     var elem: PsiElement = settings.elements.apply(0)
     while (elem != null && !(elem.getTextRange.contains(settings.nextSibling.getTextRange) &&
@@ -48,100 +46,79 @@ object ScalaExtractMethodUtils {
       }
       elem = elem.getParent
     }
-    if (tp.length != 0) {
-      builder.append(tp.reverse.map(_.getText).mkString("[", ", ", "]"))
+    val typeParamsText = if (tp.length != 0) tp.reverse.map(_.getText).mkString("[", ", ", "]") else ""
+
+    def paramText(param: ExtractMethodParameter): String = {
+      val name = if (!param.needMirror) param.oldName else s"_${param.newName}"
+      val colon = if (ScalaNamesUtil.isOpCharacter(name.last)) " : " else ": "
+      val byNameArrow = if (param.isCallByNameParameter) "=> " else ""
+      val typeText = param.tp.presentableText
+      s"$name$colon$byNameArrow$typeText"
     }
-    if (settings.parameters.filter(_.passAsParameter).length != 0) {
-      builder.append("(")
-      for (param <- settings.parameters) {
-        if (param.passAsParameter) {
-          builder.append(if (!param.needMirror) param.oldName else "_" + param.newName).append(": ").
-                  append(if (param.isCallByNameParameter) "=> " else "").
-                  append(ScType.presentableText(param.tp)).append(", ")
-        }
-      }
-      builder.delete(builder.length - 2, builder.length)
-      builder.append(")")
+
+    val parameters = settings.parameters.filter(_.passAsParameter).map(paramText)
+    val paramsText = if (parameters.nonEmpty) parameters.mkString("(", ", ",")") else ""
+
+    val retType = if (settings.calcReturnTypeIsUnit) "" else s": ${settings.calcReturnTypeText} ="
+
+    def notPassedOrWithMirrorParamText(param: ExtractMethodParameter) = {
+      val keyword = if (param.needMirror) "var " else "val "
+      val name = param.oldName
+      val colon = if (ScalaNamesUtil.isOpCharacter(name.last)) " : " else ": "
+      val typeText = param.tp.presentableText
+      val expr = if (!param.passAsParameter) "_" else if (param.needMirror) s"_${param.newName}"
+      s"$keyword$name$colon$typeText = $expr\n"
     }
-    if (settings.calcReturnTypeIsUnit) {
-        builder.append("{\n")
-    } else {
-        builder.append(": ")
-        builder.append(settings.calcReturnTypeText)
-        builder.append(" = {\n")
-    }
-    for (param <- settings.parameters) {
-      if (!param.passAsParameter) {
-        builder.append(if (param.needMirror) "var " else "val ").append(param.oldName).append(": ").
-                append(ScType.presentableText(param.tp)).
-                append(" = _\n")
-      } else if (param.needMirror) {
-        builder.append("var ").append(param.oldName).append(": ").append(ScType.presentableText(param.tp)).
-                append(" = _").append(param.newName).append("\n")
-      }
-    }
-    val offset = builder.length
+    val notPassedParams = settings.parameters.filter(p => !p.passAsParameter || p.needMirror).map(notPassedOrWithMirrorParamText)
+    val notPassedParamsText = notPassedParams.mkString
+
     val elementsToAdd: Iterator[PsiElement] = settings.elements.toSeq match {
       case Seq(x: ScBlockExpr) => x.children.toSeq.drop(1).dropRight(1).toIterator // drop '{' and '}'
       case x => x.toIterator
     }
-    for (element <- elementsToAdd) {
-      builder.append(element.getText)
-    }
-    if (!settings.lastReturn) {
-      settings.returnType match {
-        case Some(psi.types.Unit) =>
-          if (settings.returns.length == 1) {
-            builder.append("\n(false, Some(").append(settings.returns(0).oldParamName).append("))")
-          }
-          else if (settings.returns.length > 1) {
-            builder.append("\n(false, Some(").append(settings.returns.map(_.oldParamName).mkString("(", ", ", ")")).
-              append("))")
-          } else builder.append("\nfalse")
-        case Some(_) =>
-          if (settings.returns.length == 1) {
-            builder.append("\n(None, Some(").append(settings.returns(0).oldParamName).append("))")
-          }
-          else if (settings.returns.length > 1) {
-            builder.append("\n(None, Some(").append(settings.returns.map(_.oldParamName).mkString("(", ", ", ")")).
-              append("))")
-          } else builder.append("\nNone")
-        case _ =>
-          if (settings.returns.length == 1) {
-            builder.append("\n").append(settings.returns(0).oldParamName)
-          }
-          else if (settings.returns.length > 1) {
-            builder.append("\n").append(settings.returns.map(_.oldParamName).mkString("(", ", ", ")"))
-          }
-      }
-    }
-    builder.append("\n}")
-    val method = ScalaPsiElementFactory.createMethodFromText(builder.toString(), settings.elements.apply(0).getManager)
+    val elementsText = elementsToAdd.map(_.getText).mkString("")
 
+    val returnText =
+      if (settings.lastReturn) ""
+      else {
+        def params = settings.returns.map(_.oldParamName).mkString("(", ", ", ")")
+        def byReturnsSize(ifZero: => String, ifOne: => String, ifMany: => String): String = {
+          settings.returns.length match {
+            case 0 => ifZero
+            case 1 => ifOne
+            case _ => ifMany
+          }
+        }
+        settings.returnType match {
+          case Some(psi.types.Unit) => byReturnsSize("\nfalse", s"\n(false, Some$params)", s"\n(false, Some($params))")
+          case Some(_) => byReturnsSize("\nNone", s"\n(None, Some$params)", s"\n(false, Some($params))")
+          case _ => byReturnsSize("", s"\n${settings.returns(0).oldParamName}", s"\n$params")
+        }
+      }
+
+    val firstPart = s"${accessMod}def $methodName$typeParamsText$paramsText$retType {\n$notPassedParamsText"
+    val offset = firstPart.length
+    val secondPart = s"$elementsText$returnText\n}"
+    val method = ScalaPsiElementFactory.createMethodFromText(firstPart + secondPart, settings.elements.apply(0).getManager)
 
     val returnVisitor = new ScalaRecursiveElementVisitor {
       override def visitReturnStatement(ret: ScReturnStmt) {
         if (ret.returnFunction != Some(method)) return
         val text = ret.expr match {
-          case Some(t) if !settings.lastReturn => "Some(" + t.getText + ")"
+          case Some(t) if !settings.lastReturn => s"Some(${t.getText})"
           case Some(t) => t.getText
           case _ if !settings.lastReturn => "true"
           case _ => ""
         }
-        val retText = new StringBuilder("return ")
-        if (settings.returns.length == 0 || settings.lastReturn) retText.append(text)
-        else retText.append("(").append(text).append(", None)")
-        val retElem = ScalaPsiElementFactory.createExpressionFromText(retText.toString(), ret.getManager)
+        val retText =
+          if (settings.returns.length == 0 || settings.lastReturn) s"return $text"
+          else s"return ($text, None)"
+        val retElem = ScalaPsiElementFactory.createExpressionFromText(retText, ret.getManager)
         ret.replace(retElem)
         super.visitReturnStatement(ret)
       }
     }
     returnVisitor.visitElement(method: ScalaPsiElement)
-
-    settings.returnType match {
-      case Some(_) =>
-      case _ => //Nothing to do
-    }
 
     val visitor = new ScalaRecursiveElementVisitor() {
       override def visitReference(ref: ScReferenceElement) {
@@ -194,15 +171,9 @@ object ScalaExtractMethodUtils {
       override def visitElement(element: ScalaPsiElement) {
         element match {
           case named: PsiNamedElement if named != method && named.getTextOffset < offset =>
-            var break = false
-            for (param <- settings.parameters if !break) {
-              if (param.oldName == named.name) {
-                if (param.oldName != param.newName) {
-                  bindTo += ((named, param.newName))
-                }
-                break = true
-              }
-            }
+            settings.parameters.find(p => p.oldName == named.name)
+                    .filter(p => p.oldName != p.newName)
+                    .foreach(p => bindTo += ((named, p.newName)))
           case _ =>
         }
         super.visitElement(element)
@@ -289,33 +260,52 @@ object ScalaExtractMethodUtils {
     if (returns.length == 0 && returnType == None && lastMeaningful != None) {
       return prepareResult(lastMeaningful.get)
     }
+    def byReturnsSize[T](ifZero: => T, ifOne: => T, ifMany: => T): T = {
+      returns.length match {
+        case 0 => ifZero
+        case 1 => ifOne
+        case _ => ifMany
+      }
+    }
+    def tupleTypeText = returns.map(_.returnType.presentableText).mkString("(", ", ", ")")
+    def typeText = returns(0).returnType.presentableText
     returnType match {
-      case Some(psi.types.Unit) =>
-        if (returns.length == 0) (false, "Boolean")
-        else if (returns.length == 1) (false, "(Boolean, Option[" + ScType.presentableText(returns.apply(0).returnType) + "])")
-        else (false, "(Boolean, Option[" + returns.map(r => ScType.presentableText(r.returnType)).mkString("(", ", ", ")") + "])")
-      case Some(tp) =>
-        if (returns.length == 0) (false, "Option[" + ScType.presentableText(tp) + "]")
-        else if (returns.length == 1) (false, "(Option[" + ScType.presentableText(tp) +
-                "], Option[" + ScType.presentableText(returns.apply(0).returnType) + "])")
-        else (false, "(Option[" + ScType.presentableText(tp) +"]," +
-                " Option[" + returns.map(r => ScType.presentableText(r.returnType)).mkString("(", ", ", ")") + "])")
-      case None =>
-        if (returns.length == 1) prepareResult(returns.apply(0).returnType)
-        else if (returns.length == 0) (true, "Unit")
-        else (false, returns.map(r => ScType.presentableText(r.returnType)).mkString("(", ", ", ")"))
+      case Some(psi.types.Unit) => byReturnsSize(
+        (false, "Boolean"),
+        (false, s"(Boolean, Option[$typeText])"),
+        (false, s"(Boolean, Option[$tupleTypeText])")
+      )
+      case Some(tp) => byReturnsSize(
+        (false, s"Option[${tp.presentableText}]"),
+        (false, s"(Option[${tp.presentableText}], Option[$typeText])"),
+        (false, s"(Option[${tp.presentableText}], Option[$tupleTypeText])")
+      )
+      case None => byReturnsSize(
+        (true, "Unit"),
+        prepareResult(returns(0).returnType),
+        (false, tupleTypeText)
+      )
     }
   }
 
   def getParameter(d: VariableData, variableData: ScalaVariableData): ExtractMethodParameter = {
-    new ExtractMethodParameter(d.variable.name, d.name, false, d.`type`.asInstanceOf[FakePsiType].tp,
-        variableData.isMutable, d.passAsParameter, variableData.vari.isInstanceOf[ScFunction],
-        variableData.vari.isInstanceOf[ScFunction] && variableData.vari.asInstanceOf[ScFunction].parameters.length == 0,
-        ScalaPsiUtil.nameContext(variableData.vari) match {
-          case v: ScValue if v.hasModifierProperty("lazy") => true
-          case p: ScParameter if p.isCallByNameParameter => true
-          case _ => false
-        })
+    val isEmptyParamFun = variableData.vari.isInstanceOf[ScFunction] && variableData.vari.asInstanceOf[ScFunction].parameters.length == 0
+    val isCallByName = ScalaPsiUtil.nameContext(variableData.vari) match {
+      case v: ScValue if v.hasModifierProperty("lazy") => true
+      case p: ScParameter if p.isCallByNameParameter => true
+      case _ => false
+    }
+    ExtractMethodParameter(
+      oldName = d.variable.name,
+      newName = d.name,
+      isRef = false,
+      tp = d.`type`.asInstanceOf[FakePsiType].tp,
+      needMirror = variableData.isMutable,
+      passAsParameter = d.passAsParameter,
+      isFunction = variableData.vari.isInstanceOf[ScFunction],
+      isEmptyParamFunction = isEmptyParamFun,
+      isCallByNameParameter = isCallByName
+    )
   }
 
   /**
