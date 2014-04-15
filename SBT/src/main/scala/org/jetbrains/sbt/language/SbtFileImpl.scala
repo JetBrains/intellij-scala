e134406b45847dc5879630980d43de67716189ea
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-04-15 16:37:08 +0200

    SBT: resolve implicitly imported SBT plugin definitions #SCL-6921

--- SBT/src/main/scala/org/jetbrains/sbt/language/SbtFileImpl.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/language/SbtFileImpl.scala
@@ -2,13 +2,14 @@ package org.jetbrains.sbt
 package language
 
 import com.intellij.psi.{PsiClass, PsiElement, ResolveState, FileViewProvider}
-import org.jetbrains.plugins.scala.lang.psi.impl.{ScalaPsiManager, ScalaFileImpl}
+import org.jetbrains.plugins.scala.lang.psi.impl.{ScalaPsiElementFactory, ScalaPsiManager, ScalaFileImpl}
 import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.openapi.module.{Module, ModuleManager, ModuleUtilCore}
 import org.jetbrains.plugins.scala.lang.psi.ScDeclarationSequenceHolder
 import com.intellij.psi.search.searches.ClassInheritorsSearch
 import org.jetbrains.plugins.scala.extensions.toPsiClassExt
 import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.sbt.project.module.SbtModule
 import collection.JavaConverters._
 
 /**
@@ -23,12 +24,21 @@ class SbtFileImpl(provider: FileViewProvider) extends ScalaFileImpl(provider, Sb
 
   override def processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement, place: PsiElement): Boolean = 
     super[ScalaFileImpl].processDeclarations(processor, state, lastParent, place) && 
-    super[ScDeclarationSequenceHolder].processDeclarations(processor, state, lastParent, place)
+    super[ScDeclarationSequenceHolder].processDeclarations(processor, state, lastParent, place) &&
+    processImplicitImports(processor, state, lastParent,place)
 
-  override def implicitlyImportedPackages = super.implicitlyImportedPackages :+ "sbt"
+  private def processImplicitImports(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement, place: PsiElement): Boolean = {
+    val expressions = implicitImportExpressions ++ localObjectsWithDefinitions.map(_.qualifiedName + "._")
 
-  override def implicitlyImportedObjects = super.implicitlyImportedObjects ++ Seq("sbt", "sbt.Process", "sbt.Keys") ++
-          localObjectsWithDefinitions.map(_.qualifiedName)
+    expressions.isEmpty || {
+      val code = s"import ${expressions.mkString(", ")};"
+      val file = ScalaPsiElementFactory.parseFile(code, getManager)
+      file.processDeclarations(processor, state, file.lastChild.get, place)
+    }
+  }
+
+  private def implicitImportExpressions = projectDefinitionModule.orElse(fileModule)
+          .fold(Seq.empty[String])(SbtModule.getImportsFrom)
 
   private def localObjectsWithDefinitions: Seq[PsiClass] = {
     projectDefinitionModule.fold(Seq.empty[PsiClass]) { module =>
@@ -45,9 +55,9 @@ class SbtFileImpl(provider: FileViewProvider) extends ScalaFileImpl(provider, Sb
   override def getFileResolveScope: GlobalSearchScope =
     projectDefinitionModule.fold(super.getFileResolveScope)(_.getModuleWithLibrariesScope)
 
-  private def projectDefinitionModule: Option[Module] = {
-    Option(ModuleUtilCore.findModuleForPsiElement(this)).flatMap { module =>
-      Option(ModuleManager.getInstance(getProject).findModuleByName(module.getName + Sbt.BuildModuleSuffix))
-    }
+  private def projectDefinitionModule: Option[Module] = fileModule.flatMap { module =>
+    Option(ModuleManager.getInstance(getProject).findModuleByName(module.getName + Sbt.BuildModuleSuffix))
   }
+
+  private def fileModule: Option[Module] = Option(ModuleUtilCore.findModuleForPsiElement(this))
 }
