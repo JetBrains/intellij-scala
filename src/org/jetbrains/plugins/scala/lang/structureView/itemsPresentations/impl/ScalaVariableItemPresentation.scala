8693d1551df2b091e67856a4d5161824245ab34c
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-04-29 18:35:11 +0400

    Reworked structure view. Now it's possible to use structure view tool window.

--- src/org/jetbrains/plugins/scala/lang/structureView/itemsPresentations/impl/ScalaVariableItemPresentation.scala
+++ src/org/jetbrains/plugins/scala/lang/structureView/itemsPresentations/impl/ScalaVariableItemPresentation.scala
@@ -5,12 +5,10 @@ package itemsPresentations
 package impl
 
 import com.intellij.openapi.editor.colors.{TextAttributesKey, CodeInsightColors}
-import org.jetbrains.plugins.scala.lang.psi._
-import org.jetbrains.plugins.scala.lang.psi.api.statements._
 import com.intellij.psi._
 import org.jetbrains.plugins.scala.icons.Icons
 
-import javax.swing._;
+import javax.swing._
 
 /**
 * @author Alexander Podkhalyuzin
@@ -18,15 +16,15 @@ import javax.swing._;
 */
 
 class ScalaVariableItemPresentation(private val element: PsiElement, isInherited: Boolean) extends ScalaItemPresentation(element) {
-  def getPresentableText(): String = {
-    return ScalaElementPresentation.getPresentableText(myElement)
+  def getPresentableText: String = {
+    ScalaElementPresentation.getPresentableText(myElement)
   }
 
   override def getIcon(open: Boolean): Icon = {
     Icons.VAR
   }
 
-  override def getTextAttributesKey(): TextAttributesKey = {
-    return if(isInherited) CodeInsightColors.NOT_USED_ELEMENT_ATTRIBUTES else null
+  override def getTextAttributesKey: TextAttributesKey = {
+    if(isInherited) CodeInsightColors.NOT_USED_ELEMENT_ATTRIBUTES else null
   }
 }
