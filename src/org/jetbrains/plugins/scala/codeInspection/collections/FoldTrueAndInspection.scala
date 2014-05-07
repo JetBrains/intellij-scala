370dd50bbec034de9e42b23dfc7891cbdf308b29
niktrop <Nikolay.Tropin@jetbrains.com>
2014-05-07 13:35:17 +0400

    All fold and reduce methods are handled in simplification inspections; product, min and max added #SCL-6994 fixed #SCL-6996 fixed

--- src/org/jetbrains/plugins/scala/codeInspection/collections/FoldTrueAndInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/collections/FoldTrueAndInspection.scala
@@ -8,19 +8,19 @@ import org.jetbrains.plugins.scala.codeInspection.collections.OperationOnCollect
  * Nikolay.Tropin
  * 2014-05-05
  */
-class FoldLeftTrueAndInspection extends OperationOnCollectionInspection {
+class FoldTrueAndInspection extends OperationOnCollectionInspection {
   override def possibleSimplificationTypes: Array[SimplificationType] =
-    Array(new FoldLeftTrueAnd(this))
+    Array(new FoldTrueAnd(this))
 }
 
-class FoldLeftTrueAnd(inspection: OperationOnCollectionInspection) extends SimplificationType(inspection){
+class FoldTrueAnd(inspection: OperationOnCollectionInspection) extends SimplificationType(inspection){
 
-  def hint = InspectionBundle.message("foldLeft.true.and.hint")
+  def hint = InspectionBundle.message("fold.true.and.hint")
 
   override def getSimplification(last: MethodRepr, second: MethodRepr): List[Simplification] = {
     (last.optionalMethodRef, second.optionalMethodRef) match {
       case (None, Some(secondRef))
-        if List("foldLeft", "/:").contains(secondRef.refName) &&
+        if foldMethodNames.contains(secondRef.refName) &&
                 isLiteral(second.args, "true") &&
                 last.args.size == 1 &&
                 checkResolve(secondRef, likeCollectionClasses) =>
