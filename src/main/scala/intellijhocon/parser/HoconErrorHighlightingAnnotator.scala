5cc3b6757820e6961cd94991ffae7293b0b76eb2
ghik <romeqjanoosh@gmail.com>
2014-05-20 01:47:55 +0200

    validation in number parsing, error messages for bad escapes in quoted strings

--- src/main/scala/intellijhocon/parser/HoconErrorHighlightingAnnotator.scala
+++ src/main/scala/intellijhocon/parser/HoconErrorHighlightingAnnotator.scala
@@ -1,11 +1,12 @@
 package intellijhocon.parser
 
 import com.intellij.lang.annotation.{AnnotationHolder, Annotator}
-import com.intellij.psi.{TokenType, PsiElement}
+import com.intellij.psi.{StringEscapesTokenTypes, TokenType, PsiElement}
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree.IElementType
 import intellijhocon.lexer.HoconTokenType
 import intellijhocon.Util
+import com.intellij.lexer.StringLiteralLexer
 
 class HoconErrorHighlightingAnnotator extends Annotator {
 
@@ -16,8 +17,27 @@ class HoconErrorHighlightingAnnotator extends Annotator {
 
   def annotate(element: PsiElement, holder: AnnotationHolder) {
     element.getNode.getElementType match {
-      case Value =>
+      case QuotedString =>
+        val lexer = new StringLiteralLexer('"', QuotedString)
+        lexer.start(element.getText)
 
+        Iterator.continually {
+          val range = TextRange(lexer.getTokenStart, lexer.getTokenEnd)
+            .shiftRight(element.getTextRange.getStartOffset)
+          val result = (lexer.getTokenType, range)
+          lexer.advance()
+          result
+        }.takeWhile {
+          case (tokenType, _) => tokenType != null
+        } foreach {
+          case (StringEscapesTokenTypes.INVALID_CHARACTER_ESCAPE_TOKEN, range) =>
+            holder.createErrorAnnotation(range, "invalid escape character")
+          case (StringEscapesTokenTypes.INVALID_UNICODE_ESCAPE_TOKEN, range) =>
+            holder.createErrorAnnotation(range, "invalid unicode escape")
+          case _ =>
+        }
+
+      case Value =>
         def validateConcatenation(constrainingToken: IElementType, child: ASTNode): Unit = if (child != null) {
           (constrainingToken, child.getElementType) match {
             case (_, Substitution | BadCharacter | TokenType.ERROR_ELEMENT | TokenType.WHITE_SPACE) =>
