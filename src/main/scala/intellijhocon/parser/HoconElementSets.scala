2ef858341402271e5b40185249f94f930e614ef6
ghik <romeqjanoosh@gmail.com>
2014-05-20 00:29:46 +0200

    concatenation validation

--- src/main/scala/intellijhocon/parser/HoconElementSets.scala
+++ src/main/scala/intellijhocon/parser/HoconElementSets.scala
@@ -2,12 +2,15 @@ package intellijhocon.parser
 
 import intellijhocon.Util
 import com.intellij.psi.TokenType
+import intellijhocon.lexer.HoconTokenType
 
 object HoconElementSets {
 
   import HoconElementType._
+  import HoconTokenType._
   import Util._
 
   val Path = FieldPath | SubstitutionPath
+  val StringValue = UnquotedString | QuotedString | MultilineString
   val ForcedLeafBlock = Path | UnquotedString | Number | Null | Boolean | TokenType.ERROR_ELEMENT
 }
--- src/main/scala/intellijhocon/parser/HoconErrorHighlightingAnnotator.scala
+++ src/main/scala/intellijhocon/parser/HoconErrorHighlightingAnnotator.scala
@@ -1,12 +1,43 @@
 package intellijhocon.parser
 
 import com.intellij.lang.annotation.{AnnotationHolder, Annotator}
-import com.intellij.psi.PsiElement
+import com.intellij.psi.{TokenType, PsiElement}
+import com.intellij.lang.ASTNode
+import com.intellij.psi.tree.IElementType
+import intellijhocon.lexer.HoconTokenType
+import intellijhocon.Util
 
 class HoconErrorHighlightingAnnotator extends Annotator {
 
+  import HoconTokenType._
+  import HoconElementType._
+  import HoconElementSets._
+  import Util._
+
   def annotate(element: PsiElement, holder: AnnotationHolder) {
     element.getNode.getElementType match {
+      case Value =>
+
+        def validateConcatenation(constrainingToken: IElementType, child: ASTNode): Unit = if (child != null) {
+          (constrainingToken, child.getElementType) match {
+            case (_, Substitution | BadCharacter | TokenType.ERROR_ELEMENT | TokenType.WHITE_SPACE) =>
+
+              validateConcatenation(constrainingToken, child.getTreeNext)
+
+            case (StringValue.extractor(), StringValue.extractor()) |
+                 (Object, Object) | (Array, Array) | (null, _) =>
+
+              validateConcatenation(child.getElementType, child.getTreeNext)
+
+            case (required, actual) =>
+
+              holder.createErrorAnnotation(child, s"cannot concatenate ${uncaps(required.toString)} with ${uncaps(actual.toString)}")
+              validateConcatenation(actual, child.getTreeNext)
+
+          }
+        }
+
+        validateConcatenation(null, element.getNode.getFirstChildNode)
 
       case _ =>
 
