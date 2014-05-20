15d718d54c6b9084fd220f0e9de37be8c947751e
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-05-20 13:00:56 +0400

    fixed testdata

--- testdata/optimize/simple/SortedInPackage.scala
+++ testdata/optimize/simple/SortedInPackage.scala
@@ -21,8 +21,7 @@ import java.sql.Connection
 import java.util.Date
 
 abstract class SortedInPackage {
-  import java.sql.Blob
-  import java.sql.Clob
+  import java.sql.{Blob, Clob}
 
   val d: Date
   val s: Connection
