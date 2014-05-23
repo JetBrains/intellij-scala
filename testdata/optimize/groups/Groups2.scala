c4c49ce4b0d179f0bfba970cdb89918eab02b1bb
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-05-23 19:50:28 +0400

    Tests for optimize imports grouping.

---
+++ testdata/optimize/groups/Groups2.scala
@@ -0,0 +1,26 @@
+package pack {
+class G
+}
+
+import scala.collection.mutable.ArrayBuffer
+import java.util.ArrayList
+import pack.G
+
+class Groups2 {
+  val b: ArrayBuffer[Int] = null
+  val c: ArrayList[Int] = null
+}
+/*
+package pack {
+class G
+}
+
+import java.util.ArrayList
+
+import scala.collection.mutable.ArrayBuffer
+
+class Groups2 {
+  val b: ArrayBuffer[Int] = null
+  val c: ArrayList[Int] = null
+}
+ */
---
+++ testdata/optimize/groups/Groups3.scala
@@ -0,0 +1,26 @@
+package pack {
+class G
+}
+
+import scala.collection.mutable.ArrayBuffer
+import java.util.ArrayList
+import pack.G
+
+class Groups3 {
+  val b: ArrayBuffer[Int] = null
+  val d: G = null
+}
+/*
+package pack {
+class G
+}
+
+import pack.G
+
+import scala.collection.mutable.ArrayBuffer
+
+class Groups3 {
+  val b: ArrayBuffer[Int] = null
+  val d: G = null
+}
+ */
