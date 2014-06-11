e44003fba0ed6d99b10589eb767460c5a1b0e18f
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-06-11 19:59:42 +0200

    SBT: Scala SDK import is simplified

--- src/org/jetbrains/plugins/scala/configuration/ScalaLanguageLevel.java
+++ src/org/jetbrains/plugins/scala/configuration/ScalaLanguageLevel.java
@@ -1,25 +1,28 @@
 package org.jetbrains.plugins.scala.configuration;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * @author Pavel Fatin
  */
 public enum ScalaLanguageLevel implements Named {
-  SCALA_2_9("Scala 2.9", false),
-  SCALA_2_10("Scala 2.10", false),
-  SCALA_2_10_VIRTUALIZED("Scala 2.10 virtualized", true),
-  SCALA_2_11("Scala 2.11", false),
-  SCALA_2_11_VIRTUALIZED("Scala 2.11 virtualized", true);
+  SCALA_2_9("2.9", false),
+  SCALA_2_10("2.10", false),
+  SCALA_2_10_VIRTUALIZED("2.10", true),
+  SCALA_2_11("2.11", false),
+  SCALA_2_11_VIRTUALIZED("2.11", true);
 
-  private String myName;
+  private String myVersion;
   private boolean myVirtualized;
 
-  ScalaLanguageLevel(String name, boolean virtualized) {
-    myName = name;
+  ScalaLanguageLevel(String version, boolean virtualized) {
+    myVersion = version;
     myVirtualized = virtualized;
   }
 
   public String getName() {
-    return myName;
+    String prefix = "Scala " + myVersion;
+    return myVirtualized ? prefix + " virtualized" : prefix;
   }
 
   public boolean isVirtualized() {
@@ -36,5 +39,13 @@ public enum ScalaLanguageLevel implements Named {
 
   public static ScalaLanguageLevel getDefault() {
     return SCALA_2_10;
+  }
+
+  @Nullable
+  public static ScalaLanguageLevel from(String version, boolean withDefault) {
+    for (ScalaLanguageLevel level : ScalaLanguageLevel.values()) {
+      if (version.startsWith(level.myVersion)) return level;
+    }
+    return withDefault ? getDefault() : null;
   }
 }
