c1ced2808818d8fd926feb7acfcb01efbfa3a5ce
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-06-11 20:00:14 +0200

    Gradle: Scala SDK import is simplified

--- src/org/jetbrains/plugins/scala/config/ScalaGradleDataService.scala
+++ src/org/jetbrains/plugins/scala/config/ScalaGradleDataService.scala
@@ -2,14 +2,12 @@ package org.jetbrains.plugins.scala
 package config
 
 import java.util
-import java.io.File
 import collection.JavaConverters._
-import com.intellij.openapi.externalSystem.model.{ProjectKeys, DataNode}
+import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.service.project.{ProjectStructureHelper, PlatformFacade}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.libraries.Library
 import org.jetbrains.plugins.gradle.model.data.ScalaModelData
-import ScalaGradleDataService._
 import configuration._
 
 /**
@@ -20,52 +18,37 @@ class ScalaGradleDataService(platformFacade: PlatformFacade, helper: ProjectStru
 
   def doImportData(toImport: util.Collection[DataNode[ScalaModelData]], project: Project) {
     toImport.asScala.foreach { dataNode =>
-      val module = {
-        val moduleName = dataNode.getData(ProjectKeys.MODULE).getName
-        helper.findIdeModule(moduleName, project)
-      }
+    }
+  }
 
-      val scalaData = dataNode.getData
+  private def doImport(scalaNode: DataNode[ScalaModelData], project: Project) {
+    val scalaData = scalaNode.getData
 
-      val compilerClasspath = scalaData.getScalaClasspath.asScala.toSet
-//      val compilerOptions = compilerOptionsFrom(scalaData) TODO handle
+    val compilerOptions = ScalaGradleDataService.compilerOptionsFrom(scalaData) // TODO
 
-      val scalaSdk = findScalaSdkIn(project, compilerClasspath).orElse {
-        val standardLibrary = findScalaStandardLibraryIn(project, compilerClasspath)
-        standardLibrary.map(_.convertToScalaSdkWith(compilerClasspath.toSeq))
-      }
+    val compilerClasspath = scalaData.getScalaClasspath.asScala.toSeq
 
-      scalaSdk.foreach { properSdk =>
-        val existingScalaSdk = module.scalaSdk
+    val compilerVersion = compilerClasspath.find(_.getName.startsWith("scala-library")).collect {
+      case JarVersion(version) => version
+    }
 
-        existingScalaSdk match {
-          case None =>
-            module.attach(properSdk)
-          case Some(sdk) if sdk.library != properSdk.library =>
-            module.detach(sdk)
-            module.attach(properSdk)
-          case _ =>
-        }
+    compilerVersion.foreach { version =>
+      val matchedLibrary = project.libraries.find(_.scalaVersion == Some(version))
+
+      for (library <- matchedLibrary if !library.isScalaSdk) {
+        val languageLevel = ScalaLanguageLevel.from(version, true)
+        library.convertToScalaSdkWith(languageLevel, compilerClasspath)
       }
     }
   }
 
   def doRemoveData(toRemove: util.Collection[_ <: Library], project: Project) {
-    // TODO
+
   }
 }
 
-object ScalaGradleDataService {
-  def findScalaSdkIn(project: Project, compilerClasspath: Set[File]): Option[ScalaSdk] =
-    project.scalaSdks.find(_.compilerClasspath.toSet == compilerClasspath)
-
-  def findScalaStandardLibraryIn(project: Project, compilerClasspath: Set[File]): Option[Library] = {
-    val compilerStandardLibraryFile = compilerClasspath.find(_.getName.startsWith("scala-library"))
-
-    compilerStandardLibraryFile.flatMap(file => project.libraries.find(_.classes.contains(file)))
-  }
-
-  def compilerOptionsFrom(data: ScalaModelData): Seq[String] = {
+private object ScalaGradleDataService {
+  private def compilerOptionsFrom(data: ScalaModelData): Seq[String] = {
     val options = data.getScalaCompileOptions
 
     val presentations = Seq(
