192eadcc6fab2486de1ac3dbdf1847da09f2a494
Jan Machacek <jan.machacek@gmail.com>
2014-06-26 17:02:44 +0100

    Everything is a problem

--- intellij-scalastyle/src/main/scala/intellijscalastyle/language/ScalaFileTypeFactory.scala
+++
@@ -1,8 +0,0 @@
-package intellijscalastyle.language
-
-import com.intellij.openapi.fileTypes.{FileTypeConsumer, FileTypeFactory}
-import org.jetbrains.plugins.scala.ScalaFileType
-
-class ScalaFileTypeFactory extends FileTypeFactory {
-  override def createFileTypes(consumer: FileTypeConsumer): Unit = ???
-}
