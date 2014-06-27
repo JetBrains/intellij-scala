6db5d46b0846014664d119c3c83df4c227be4d6c
Jan Machacek <jan.machacek@gmail.com>
2014-06-27 14:04:54 +0100

    Plugin checking

--- intellij-scalastyle/src/main/scala/intellijscalastyle/codeInspection/ScalastyleCodeInspection.scala
+++ intellij-scalastyle/src/main/scala/intellijscalastyle/codeInspection/ScalastyleCodeInspection.scala
@@ -2,24 +2,47 @@ package intellijscalastyle
 package codeInspection
 
 import com.intellij.codeInspection._
-import com.intellij.psi.PsiFile
+import com.intellij.psi.{PsiDocumentManager, PsiElement, PsiElementVisitor, PsiFile}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.scalastyle._
+
+object ScalastyleCodeInspection {
+  val configuration = ScalastyleConfiguration.getDefaultConfiguration()
+
+}
 
 class ScalastyleCodeInspection extends LocalInspectionTool {
+
 
   override def checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array[ProblemDescriptor] = {
     if (!file.isInstanceOf[ScalaFile]) return Array.empty
     println("Checking " + file.getText)
     val scalaFile = file.asInstanceOf[ScalaFile]
+    val result = new ScalastyleChecker().checkFiles(ScalastyleCodeInspection.configuration, Seq(new SourceSpec(file.getName, file.getText)))
+    val document = PsiDocumentManager.getInstance(file.getProject).getDocument(file)
 
-    val s = scalaFile.getFirstChild
-    val e = scalaFile.getLastChild
-    val errors = scalaFile.getChildren
-    Array(
-      //manager.createProblemDescriptor(file, "Scala codestyle problems", false, Array.empty[LocalQuickFix], ProblemHighlightType.ERROR),
-      manager.createProblemDescriptor(s, "Scala codestyle problem", Array.empty[LocalQuickFix], ProblemHighlightType.ERROR, false, false)
-      // manager.createProblemDescriptor(s, e, "Generic Scala codestyle problem", ProblemHighlightType.ERROR, false)
-    )
+    def findPsiElements(line: Int, column: Option[Int]): Option[(PsiElement, PsiElement)]= {
+      (for {
+        element <- scalaFile.depthFirst
+        psiLine =  document.getLineNumber(element.getTextOffset()) + 1
+        if line == psiLine
+        // document.getLineStartOffset(element.getTextOffset)
+      } yield (element, element)).toList.headOption
+    }
+
+    result.flatMap {
+      case StyleError(_, _, key, level, args, Some(line), column, msg) =>
+        val Some((s, e)) = findPsiElements(line, column)
+        Some(manager.createProblemDescriptor(s, "Scala codestyle problem", Array.empty[LocalQuickFix], ProblemHighlightType.ERROR, false, false))
+      case _ => None
+    }.toArray
+
+//    val s = scalaFile.getFirstChild
+//    val e = scalaFile.getLastChild
+//    val errors = scalaFile.getChildren
+//    Array(
+//      manager.createProblemDescriptor(s, "Scala codestyle problem", Array.empty[LocalQuickFix], ProblemHighlightType.ERROR, false, false)
+//    )
 
   }
 }
