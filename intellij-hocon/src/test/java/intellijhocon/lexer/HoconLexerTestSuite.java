af80572614b3591eebedca2fa7ad90713995b3b4
ghik <romeqjanoosh@gmail.com>
2014-07-13 20:09:48 +0200

    HOCON: lexer tests

---
+++ intellij-hocon/src/test/java/intellijhocon/lexer/HoconLexerTestSuite.java
@@ -0,0 +1,13 @@
+package intellijhocon.lexer;
+
+import junit.framework.Test;
+
+/**
+ * Author: ghik
+ * Created: 2014-07-13
+ */
+public class HoconLexerTestSuite extends HoconLexerTest implements Test {
+  public static Test suite() {
+    return new HoconLexerTestSuite();
+  }
+}
