fc741b8c58ca23caae8fd60d240dba1478eded7c
Nick Obedin <dancingrobot84@gmail.com>
2014-07-23 19:49:41 +0400

    Resolvers form is done

--- SBT/src/main/scala/org/jetbrains/sbt/project/settings/SbtResolversConfigurable.java
+++ SBT/src/main/scala/org/jetbrains/sbt/project/settings/SbtResolversConfigurable.java
@@ -1,6 +1,7 @@
 package org.jetbrains.sbt.project.settings;
 
 import com.intellij.openapi.options.BaseConfigurable;
+import com.intellij.ui.table.JBTable;
 
 import javax.swing.*;
 
@@ -10,6 +11,11 @@ import javax.swing.*;
  */
 public class SbtResolversConfigurable extends BaseConfigurable {
   private JPanel myPanel;
+  private JBTable myIndicesTable;
+  private JButton myAddButton;
+  private JButton myEditButton;
+  private JButton myRemoveButton;
+  private JButton myUpdateButton;
 
   public String getDisplayName() {
     return "Resolvers";
