9106c3bfaa3939cd566f60f8311535f61ee1ec7d
Nick Obedin <dancingrobot84@gmail.com>
2014-07-23 19:07:12 +0400

    Added project settings panel for SBT resolvers

--- SBT/src/main/scala/org/jetbrains/sbt/project/settings/SbtExternalSystemConfigurable.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/settings/SbtExternalSystemConfigurable.scala
@@ -2,6 +2,7 @@ package org.jetbrains.sbt
 package project.settings
 
 import com.intellij.openapi.externalSystem.service.settings.AbstractExternalSystemConfigurable
+import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.project.Project
 import org.jetbrains.sbt.project.SbtProjectSystem
 import org.jetbrains.sbt.project.settings.Context.Configuration
@@ -11,7 +12,10 @@ import org.jetbrains.sbt.project.settings.Context.Configuration
  * Date: 11/25/13
  */
 class SbtExternalSystemConfigurable(project: Project) 
-  extends AbstractExternalSystemConfigurable[SbtProjectSettings, SbtSettingsListener, SbtSettings](project, SbtProjectSystem.Id) {
+  extends AbstractExternalSystemConfigurable[SbtProjectSettings, SbtSettingsListener, SbtSettings](project, SbtProjectSystem.Id)
+  with SearchableConfigurable.Parent {
+
+  private val myConfigurables = Seq(new SbtResolversConfigurable)
 
   def createProjectSettingsControl(settings: SbtProjectSettings) = new SbtProjectSettingsControl(Configuration, settings)
 
@@ -22,4 +26,10 @@ class SbtExternalSystemConfigurable(project: Project)
   def getId = "sbt.project.settings.configurable"
 
   def getHelpTopic = null
+
+  def getConfigurables = myConfigurables.toArray
+
+  def isVisible = true
+
+  def hasOwnContent = true
 }
