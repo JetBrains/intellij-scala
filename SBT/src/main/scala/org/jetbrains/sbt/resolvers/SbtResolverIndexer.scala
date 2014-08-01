5670d35f6580f5a9575bffb2de3ffd0235df1880
Nick Obedin <dancingrobot84@gmail.com>
2014-08-01 14:14:05 +0400

    + indexing of local maven repositories

--- SBT/src/main/scala/org/jetbrains/sbt/resolvers/SbtResolverIndexer.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/resolvers/SbtResolverIndexer.scala
@@ -4,9 +4,9 @@ package resolvers
 import java.io.{Closeable, File}
 
 import com.intellij.openapi.progress.ProgressIndicator
-import org.apache.maven.index.context.{IndexCreator, IndexUtils}
+import org.apache.maven.index._
+import org.apache.maven.index.context.{IndexingContext, IndexCreator, IndexUtils}
 import org.apache.maven.index.updater.{IndexUpdateRequest, IndexUpdater, WagonHelper}
-import org.apache.maven.index.{ArtifactInfo, Indexer}
 import org.apache.maven.wagon.Wagon
 import org.apache.maven.wagon.events.TransferEvent
 import org.apache.maven.wagon.observers.AbstractTransferListener
@@ -16,15 +16,12 @@ import org.codehaus.plexus.DefaultPlexusContainer
  * @author Nikolay Obedin
  * @since 7/28/14.
  */
-class SbtResolverIndexer private (val root: String, val indexDir: File, val cacheDir: File) extends Closeable {
+class SbtResolverIndexer private (val root: String, val indexDir: File) extends Closeable {
   import scala.collection.JavaConverters._
 
   indexDir.mkdirs()
-  cacheDir.mkdirs()
   if (!indexDir.exists || !indexDir.isDirectory)
     throw new RuntimeException("Can't create Maven Indexer index dir: %s" format indexDir.absolutePath)
-  if (!cacheDir.exists || !cacheDir.isDirectory)
-    throw new RuntimeException("Can't create Maven Indexer cache dir: %s" format cacheDir.absolutePath)
 
   // This lines are absolutely necessary
   // Otherwise Plexus container won't find maven-indexer interfaces' implementations
@@ -42,11 +39,15 @@ class SbtResolverIndexer private (val root: String, val indexDir: File, val cach
     container.lookup(classOf[IndexCreator], "maven-plugin")
   ).asJava
 
-  private val context = indexer.createIndexingContext(
-    root.shaDigest, root.shaDigest,
-    cacheDir, indexDir,
-    root, null, true, true, indexers
-  )
+  private var context = {
+    val repoUrl = if (root.startsWith("file:")) null else root
+    val repoDir = if (root.startsWith("file:")) new File(root.substring(5)) else null
+    indexer.createIndexingContext(
+      root.shaDigest, root.shaDigest,
+      repoDir, indexDir,
+      repoUrl, null, true, true, indexers
+    )
+  }
 
 
   def close() {
@@ -56,6 +57,41 @@ class SbtResolverIndexer private (val root: String, val indexDir: File, val cach
   }
 
   def update(progressIndicator: Option[ProgressIndicator]) {
+    if (context.getRepositoryUrl == null)
+      updateLocal(progressIndicator)
+    else
+      updateRemote(progressIndicator)
+  }
+
+  private def updateLocal(progressIndicator: Option[ProgressIndicator]) {
+    val scannerListener = new ArtifactScanningListener {
+      override def scanningStarted(p1: IndexingContext) = progressIndicator foreach { indicator =>
+        indicator.setText2("Scanning files")
+        indicator.setFraction(0.0)
+      }
+      override def scanningFinished(p1: IndexingContext, p2: ScanningResult) =
+        progressIndicator foreach { _.setFraction(0.5) }
+      override def artifactError(p1: ArtifactContext, p2: Exception) {}
+      override def artifactDiscovered(p1: ArtifactContext) {}
+    }
+
+    val repoDir = context.getRepository
+    indexer.closeIndexingContext(context, false)
+
+      // FIXME: when guys from maven-indexer fix their code (or at least Scanner class will work as it should)
+      val nexusIndexer = container.lookup(classOf[NexusIndexer])
+      val nexusContext = nexusIndexer.addIndexingContext(root.shaDigest, root.shaDigest, context.getRepository, indexDir, null, null, indexers)
+      nexusIndexer.scan(nexusContext, scannerListener, true)
+      nexusIndexer.removeIndexingContext(nexusContext, false)
+
+    context = indexer.createIndexingContext(
+      root.shaDigest, root.shaDigest,
+      repoDir, indexDir,
+      null, null, true, true, indexers
+    )
+  }
+
+  private def updateRemote(progressIndicator: Option[ProgressIndicator]) {
     val transferListener = new AbstractTransferListener {
       var downloadedBytes = 0
       override def transferProgress(evt: TransferEvent, bytes: Array[Byte], length: Int) =
@@ -95,11 +131,10 @@ class SbtResolverIndexer private (val root: String, val indexDir: File, val cach
 
 object SbtResolverIndexer {
   object Paths {
-    val CACHE_DIR = "cache"
     val INDEX_DIR = "index"
   }
 
   def apply(root: String, indexDir: File) =
-    new SbtResolverIndexer(root, indexDir / Paths.INDEX_DIR, indexDir / Paths.CACHE_DIR)
+    new SbtResolverIndexer(root, indexDir / Paths.INDEX_DIR)
 }
 
