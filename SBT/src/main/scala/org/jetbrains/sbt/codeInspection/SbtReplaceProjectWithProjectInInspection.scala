92ca70cd815bae037ef4cd7256c11b3411a30516
Nick Obedin <dancingrobot84@gmail.com>
2014-08-07 12:12:47 +0400

    = renamed inspection class + implemented quickfix

--- SBT/src/main/scala/org/jetbrains/sbt/codeInspection/SbtReplaceProjectWithProjectInInspection.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/codeInspection/SbtReplaceProjectWithProjectInInspection.scala
@@ -1,7 +1,8 @@
 package org.jetbrains.sbt
 package codeInspection
 
-import com.intellij.codeInspection.{ProblemHighlightType, ProblemsHolder}
+import com.intellij.codeInspection.{ProblemDescriptor, LocalQuickFix, ProblemHighlightType, ProblemsHolder}
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.codeInspection.AbstractInspection
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaRecursiveElementVisitor
@@ -9,12 +10,13 @@ import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScPatternDefinition
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 
 /**
  * @author Nikolay Obedin
  * @since 8/5/14.
  */
-class SbtReplaceProjectCallWithProjectInShortcutInspection extends AbstractInspection {
+class SbtReplaceProjectWithProjectInInspection extends AbstractInspection {
 
   def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
     case defn: ScPatternDefinition =>
@@ -22,22 +24,31 @@ class SbtReplaceProjectCallWithProjectInShortcutInspection extends AbstractInspe
 
       (defn.expr, defn.bindings) match {
         case (Some(call: ScMethodCall), Seq(varPat: ScReferencePattern)) =>
-
           val visitor = new ScalaRecursiveElementVisitor {
             override def visitMethodCallExpression(call: ScMethodCall) = call match {
               case ScMethodCall(expr, Seq(nameLit: ScLiteral, pathElt))
                   if expr.getText == "Project" && nameLit.isString && nameLit.getValue == varPat.getText =>
-                // TODO: add quick fix
                 // TODO: put message into bundle
                 holder.registerProblem(call, "Replace Project() with project.in()",
-                  ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
+                  ProblemHighlightType.GENERIC_ERROR_OR_WARNING, new SbtReplaceProjectWithProjectInQuickFix(call))
               case _ =>
                 super.visitMethodCallExpression(call)
             }
           }
-
           call.accept(visitor)
         case _ => // do nothing
       }
+  }
+}
+
+class SbtReplaceProjectWithProjectInQuickFix(val place: ScMethodCall) extends LocalQuickFix {
+  def getName = "Replace Project() with project.in()" // TODO: put in bundle
+
+  def getFamilyName = getName
+
+  def applyFix(project: Project, descriptor: ProblemDescriptor) = place match {
+    case ScMethodCall(_, Seq(_, pathElt)) =>
+      place.replace(ScalaPsiElementFactory.createExpressionFromText("project.in(" + pathElt.getText + ")", place.getManager))
+    case _ => // do nothing
   }
 }
