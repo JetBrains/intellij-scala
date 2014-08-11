efbef6e42e1d1e01211d4d07c15f7cfac0a6fe29
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-08-11 15:23:37 +0200

    SbtNotificationListener: check project system ID #SCL-7405

--- SBT/src/main/scala/org/jetbrains/sbt/project/SbtNotificationListener.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/SbtNotificationListener.scala
@@ -10,9 +10,17 @@ import com.intellij.notification.{NotificationType, Notifications, Notification}
 // TODO Rely on the immediate UI interaction API when IDEA-123007 will be implemented
 class SbtNotificationListener extends ExternalSystemTaskNotificationListenerAdapter {
   override def onTaskOutput(id: ExternalSystemTaskId, text: String, stdOut: Boolean) {
+    // TODO this check must be performed in the External System itself (see SCL-7405)
+    if (id.getProjectSystemId == SbtProjectSystem.Id) {
+      processOutput(text)
+    }
+  }
+
+  private def processOutput(text: String) {
     text match {
       case WarningMessage(message) =>
         Notifications.Bus.notify(new Notification("scala", "SBT project import", message, NotificationType.WARNING))
+      case _ => // do nothing
     }
   }
 }
