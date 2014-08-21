772aa6bfdae20d5b5aacbfa7c1e6b25c9562ba6b
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-08-21 13:23:54 +0200

    Rely on the project template code for Scala SDK creation

--- src/org/jetbrains/plugins/scala/configuration/ScalaLibraryType.scala
+++ src/org/jetbrains/plugins/scala/configuration/ScalaLibraryType.scala
@@ -5,7 +5,7 @@ import com.intellij.openapi.roots.libraries.{LibraryTypeService, LibraryType}
 import com.intellij.openapi.roots.libraries.ui.{LibraryPropertiesEditor, LibraryEditorComponent}
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ui.configuration.libraryEditor.DefaultLibraryRootsComponentDescriptor
+import org.jetbrains.plugins.scala.configuration.template.ScalaLibraryDescription
 import org.jetbrains.plugins.scala.icons.Icons
 import javax.swing.JComponent
 
@@ -18,8 +18,7 @@ class ScalaLibraryType extends LibraryType[ScalaLibraryProperties](ScalaLibraryK
   def getCreateActionName = "Scala SDK"
 
   def createNewLibrary(parentComponent: JComponent, contextDirectory: VirtualFile, project: Project) =
-    LibraryTypeService.getInstance.createLibraryFromFiles(new DefaultLibraryRootsComponentDescriptor(),
-      parentComponent, contextDirectory, this, project)
+    ScalaLibraryDescription.createNewLibrary(parentComponent, contextDirectory)
 
   def createPropertiesEditor(editorComponent: LibraryEditorComponent[ScalaLibraryProperties]): LibraryPropertiesEditor =
     new ScalaLibraryPropertiesEditor(editorComponent)
