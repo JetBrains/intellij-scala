46ae465f5f5e70b5235098a3aa9205f337b01be6
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-08-25 13:15:47 +0200

    Guess Scala distribution location (draft)

--- src/org/jetbrains/plugins/scala/configuration/template/ScalaLibraryDescription.scala
+++ src/org/jetbrains/plugins/scala/configuration/template/ScalaLibraryDescription.scala
@@ -1,12 +1,14 @@
 package org.jetbrains.plugins.scala
 package configuration.template
 
+import java.io.File
 import java.util.Collections
 import javax.swing.JComponent
 
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.roots.ui.configuration.libraries.CustomLibraryDescription
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.{VfsUtil, VirtualFile}
 import org.jetbrains.plugins.scala.configuration.ScalaLibraryKind
 
 /**
@@ -16,7 +18,9 @@ object ScalaLibraryDescription extends CustomLibraryDescription {
   def getSuitableLibraryKinds = Collections.singleton(ScalaLibraryKind)
 
   def createNewLibrary(parentComponent: JComponent, contextDirectory: VirtualFile) = {
-    val files = FileChooser.chooseFiles(new ScalaFilesChooserDescriptor(), null, null)
+    val initialDirectory = guessScalaHome.flatMap(path => Option(VfsUtil.findFileByIoFile(new File(path), false)))
+
+    val files = FileChooser.chooseFiles(new ScalaFilesChooserDescriptor(), null, initialDirectory.orNull)
 
     if (files.length > 0) ScalaSdkDescriptor.from(allFilesWithin(files.toSeq)) match {
       case Left(message) => throw new ValidationException(message)
@@ -27,4 +31,39 @@ object ScalaLibraryDescription extends CustomLibraryDescription {
   }
 
 //  override def getDefaultLevel = LibrariesContainer.LibraryLevel.GLOBAL // TODO
+
+  // TODO improve, support multiple variants
+  def guessScalaHome: Option[String] = {
+    val fromHome = env("SCALA_HOME").flatMap(existing)
+
+    val osDefault = if (SystemInfo.isWindows) {
+      env("ProgramFiles").flatMap(existing).flatMap(findScalaInDirectory).orElse(
+        env("ProgramFiles(x86)").flatMap(existing).flatMap(findScalaInDirectory))
+    } else if (SystemInfo.isMac) {
+      existing("/opt/").flatMap(findScalaInDirectory)
+    } else if (SystemInfo.isLinux) {
+      existing("/usr/share/java/")
+    } else {
+      None
+    }
+
+    val fromCommandPath = env("PATH").flatMap(findScalaInCommandPath).flatMap(existing)
+
+    fromHome.orElse(osDefault).orElse(fromCommandPath)
+  }
+
+  private def env(name: String): Option[String] = Option(System.getenv(name))
+
+  private def existing(path: String): Option[String] = if (new File(path).exists) Some(path) else None
+
+  private def findScalaInDirectory(directory: String): Option[String] = {
+    val subdirectories = new File(directory).listFiles.toSeq
+    subdirectories.find(_.getName.toLowerCase.startsWith("scala")).map(_.getPath)
+  }
+
+  private def findScalaInCommandPath(path: String): Option[String] =
+    path.split(File.pathSeparator)
+            .find(_.toLowerCase.contains("scala"))
+            .map(_.replaceFirst("""[/\\]?bin[/\\]?$""", ""))
+
 }
