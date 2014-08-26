335bf05a800be0c1ba4d7d27a32ff4b5d5c9e135
niktrop <Nikolay.Tropin@jetbrains.com>
2014-08-26 14:18:06 +0400

    Annotation about default arguments and repeated parameters added

--- src/org/jetbrains/plugins/scala/annotator/ParametersAnnotator.scala
+++ src/org/jetbrains/plugins/scala/annotator/ParametersAnnotator.scala
@@ -10,15 +10,22 @@ import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScParameter, 
  */
 trait ParametersAnnotator {
   def annotateParameters(parameters: ScParameters, holder: AnnotationHolder) {
-    def repeatedParamMustComeLast {
-      parameters.clauses.foreach {
-        _.parameters.dropRight(1).filter(_.isRepeatedParameter).foreach {
-          holder.createErrorAnnotation(_, "*-parameter must come last")
+    def checkRepeatedParams() {
+      parameters.clauses.foreach { cl =>
+        cl.parameters.dropRight(1).foreach {
+          case p if p.isRepeatedParameter => holder.createErrorAnnotation(p, "*-parameter must come last")
+          case _ =>
+        }
+        cl.parameters.lastOption match {
+          case Some(p) if p.isRepeatedParameter && cl.parameters.exists(_.isDefaultParam) =>
+            holder.createErrorAnnotation(cl, "Parameter section with *-parameter cannot have default arguments")
+          case _ =>
         }
       }
+      ""
     }
 
-    repeatedParamMustComeLast
+    checkRepeatedParams()
   }
   
   def annotateParameter(parameter: ScParameter, holder: AnnotationHolder) {
