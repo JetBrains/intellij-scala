77fb27401f7ac0adcf26af020ed1d332f6dc9869
Nick Obedin <dancingrobot84@gmail.com>
2014-08-27 15:27:25 +0400

    + serialization/deserialization of resolver + persistent saving resolvers list

--- SBT/src/main/scala/org/jetbrains/sbt/resolvers/SbtResolver.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/resolvers/SbtResolver.scala
@@ -1,11 +1,28 @@
 package org.jetbrains.sbt
 package resolvers
 
+import java.util.regex.Pattern
+
 /**
  * @author Nikolay Obedin
  * @since 7/25/14.
  */
 case class SbtResolver(name: String, root: String) {
+  import SbtResolver._
+
   def associatedIndex = SbtResolverIndexesManager().find(this)
+
+  override def toString = s"$root$DELIMITER$name"
+}
+
+object SbtResolver {
+  val DELIMITER = "|"
+  def fromString(str: String): Option[SbtResolver] = {
+    str.split(Pattern.quote(DELIMITER), 2).toSeq match {
+      case Seq(root, name) => Some(new SbtResolver(name, root))
+      case Seq(root) => Some(new SbtResolver("", root))
+      case _ => None
+    }
+  }
 }
 
