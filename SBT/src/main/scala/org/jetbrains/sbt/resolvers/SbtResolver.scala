27dcf4677c3770a06c956a9d83e5ba13431d3d84
Nick Obedin <dancingrobot84@gmail.com>
2014-08-28 18:56:29 +0400

    = fixes after rebase

--- SBT/src/main/scala/org/jetbrains/sbt/resolvers/SbtResolver.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/resolvers/SbtResolver.scala
@@ -12,13 +12,13 @@ case class SbtResolver(kind: SbtResolver.Kind.Value, name: String, root: String)
   import SbtResolver._
   def associatedIndex = SbtResolverIndexesManager().find(this)
 
-  override def toString = s"$root$DELIMITER$name"
+  override def toString = s"$root$DELIMITER$kind$DELIMITER$name"
 }
 
 object SbtResolver {
   object Kind extends Enumeration {
-    val Maven = Value("maven")
-    val Ivy   = Value("ivy")
+    val Maven = Value(0, "maven")
+    val Ivy   = Value(1, "ivy")
   }
 
   val localCacheResolver = {
@@ -28,9 +28,9 @@ object SbtResolver {
 
   val DELIMITER = "|"
   def fromString(str: String): Option[SbtResolver] = {
-    str.split(Pattern.quote(DELIMITER), 2).toSeq match {
-      case Seq(root, name) => Some(new SbtResolver(name, root))
-      case Seq(root) => Some(new SbtResolver("", root))
+    str.split(Pattern.quote(DELIMITER), 3).toSeq match {
+      case Seq(root, kind, name) => Some(new SbtResolver(Kind.withName(kind), name, root))
+      case Seq(root, kind) => Some(new SbtResolver(Kind.withName(kind), "", root))
       case _ => None
     }
   }
