ed96b043153371b1704863c745c458a16b7fb723
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-08-28 18:36:32 +0400

    ScalaFileStubBuilder: Java -> Scala.

---
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/ScalaFileStubBuilder.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.plugins.scala
+package lang.psi.stubs
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.stubs.DefaultStubBuilder
+import com.intellij.psi.stubs.StubElement
+import com.intellij.util.io.StringRef
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.stubs.impl.ScFileStubImpl
+
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 12.02.2010
+ */
+class ScalaFileStubBuilder extends DefaultStubBuilder {
+  protected override def createStubForFile(file: PsiFile): StubElement[_ <: PsiElement] = {
+    val s: ScalaFile = file.asInstanceOf[ScalaFile]
+    new ScFileStubImpl(s, StringRef.fromString(s.packageName), StringRef.fromString(s.sourceName), s.isCompiled,
+      s.isScriptFile(withCashing = false))
+  }
+}
