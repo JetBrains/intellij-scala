5cd0056b561be3eff33991da82c3aa6ad54e3bd1
Nick Obedin <dancingrobot84@gmail.com>
2014-08-28 11:23:16 +0400

    + recursive extraction of paths

--- SBT/src/main/scala/org/jetbrains/sbt/language/references/SbtSubprojectReferenceProvider.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/language/references/SbtSubprojectReferenceProvider.scala
@@ -1,13 +1,17 @@
 package org.jetbrains.sbt
 package language.references
 
+import java.io.File
+
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.io.FileUtil
 import com.intellij.psi._
 import com.intellij.psi.search.{FilenameIndex, GlobalSearchScope}
 import com.intellij.util.ProcessingContext
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaRecursiveElementVisitor
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScMethodCall, ScNewTemplateDefinition, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.base.{ScConstructor, ScLiteral}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScInfixExpr, ScMethodCall, ScNewTemplateDefinition, ScReferenceExpression}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScPatternDefinition
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScClassParents
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTemplateDefinition
@@ -18,16 +22,21 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTemplateDefin
  */
 class SbtSubprojectReferenceProvider extends PsiReferenceProvider {
 
+  import org.jetbrains.sbt.language.references.SbtSubprojectReferenceProvider._
+
   def getReferencesByElement(element: PsiElement, context: ProcessingContext): Array[PsiReference] = {
     if (element.getContainingFile.getFileType.getName != Sbt.Name) return Array.empty
+    extractSubprojectPath(element).flatMap { path =>
+      findBuildFile(path, element.getProject).map(new SbtSubprojectReference(element, _))
+    }.toArray
+  }
 
-    extractSubprojectPath(element).map { path =>
-      FilenameIndex.getFilesByName(element.getProject, "build.sbt", GlobalSearchScope.allScope(element.getProject)).filter { file =>
-        Option(file.getParent).map(_.getVirtualFile.getCanonicalPath).fold(false)(_.endsWith(path))
-      }.map { file =>
-        new SbtSubprojectReference(element, file)
-      }.toSeq
-    }.getOrElse(Seq.empty).toArray
+  private def findBuildFile(subprojectPath: String, project: Project): Option[PsiFile] = {
+    FilenameIndex.getFilesByName(project, "build.sbt", GlobalSearchScope.allScope(project)).find { file =>
+      val relativeToProjectPath = project.getBasePath + File.separator + subprojectPath
+      val absolutePath = FileUtil.toSystemIndependentName(FileUtil.toCanonicalPath(relativeToProjectPath))
+      Option(file.getParent).map(_.getVirtualFile.getPath).fold(false)(FileUtil.comparePaths(_, absolutePath) == 0)
+    }
   }
 
   private def extractSubprojectPath(element: PsiElement): Option[String] = {
@@ -36,48 +45,103 @@ class SbtSubprojectReferenceProvider extends PsiReferenceProvider {
       patternDef <- Option(listOfPatterns.getParent)
     } yield patternDef match {
       case ScPatternDefinition.expr(e) => e match {
-        case call: ScMethodCall =>
-          var result: Option[String] = None
-          val visitor = new ScalaRecursiveElementVisitor {
-            override def visitMethodCallExpression(call: ScMethodCall) = call match {
-              case ScMethodCall(expr, Seq(_: ScLiteral, pathElt)) if expr.getText == "Project" =>
-                result = extractPathFromParam(pathElt)
-              case ScMethodCall(expr, Seq(pathElt)) if expr.getText.matches("^project.+?in$") =>
-                result = extractPathFromParam(pathElt)
-              case ScMethodCall(expr, _) if expr.getText.startsWith("project") =>
-                result = Some(element.getText)
-                super.visitMethodCallExpression(call)
-              case _ =>
-                super.visitMethodCallExpression(call)
-            }
-          }
-          call.accept(visitor)
-          result
-        case expr: ScReferenceExpression => Some(element.getText)
+        case expr: ScReferenceExpression if expr.getText == "project" => Some(element.getText)
+        case call: ScMethodCall => extractSubprojectPathFromProjectCall(call, element)
         case _ => None
       }
       case _ => None
     }
   }.flatten
 
-  // Extract path from file params like vars and `new File(...)` ctors
-  private def extractPathFromParam(element: PsiElement): Option[String] = element match {
-    case ScTemplateDefinition.ExtendsBlock(block) if element.isInstanceOf[ScNewTemplateDefinition] =>
-      block.getChildren.toSeq.headOption match {
-        case Some(classParent : ScClassParents) =>
-          classParent.constructor.map { ctor =>
-            ctor.args.map(_.exprs).collect {
-              case Seq(pathLit : ScLiteral) if pathLit.isString =>
-                pathLit.getValue.asInstanceOf[String]
-            }
-          }.flatten
-        case _ => None
+  private def extractSubprojectPathFromProjectCall(call: ScMethodCall, element: PsiElement) = {
+    var result: Option[String] = None
+    val visitor = new ScalaRecursiveElementVisitor {
+      override def visitMethodCallExpression(call: ScMethodCall) = call match {
+        case ScMethodCall(expr, Seq(_: ScLiteral, pathElt)) if expr.getText == "Project" =>
+          result = extractPathFromFileParam(pathElt)
+        case ScMethodCall(expr, Seq(pathElt)) if expr.getText.matches("^project.+?in$") =>
+          result = extractPathFromFileParam(pathElt)
+        case ScMethodCall(expr, _) if expr.getText.startsWith("project") =>
+          result = Some(element.getText)
+          super.visitMethodCallExpression(call)
+        case _ =>
+          super.visitMethodCallExpression(call)
       }
+    }
+    call.accept(visitor)
+    result
+  }
+
+  // TODO: extract these methods into another class and use them to write path completion
+
+  private def extractPathFromFileParam(element: PsiElement): Option[String] = element match {
+    case ScTemplateDefinition.ExtendsBlock(block) if element.isInstanceOf[ScNewTemplateDefinition] =>
+      block.getChildren.toSeq.headOption.collect {
+        case classParent : ScClassParents => classParent.constructor.flatMap(extractPathFromFileCtor)
+      }.flatten
+    case expr@ScInfixExpr(_, op, _) if op.getText == "/" =>
+      extractPathFromConcatenation(expr)
+    case expr : ScReferenceExpression =>
+      Option(expr.resolve()).flatMap(extractPathFromReference)
     case _ => None
   }
+
+  private def extractPathFromFileCtor(ctor: ScConstructor): Option[String] = {
+    ctor.args.map(_.exprs).flatMap {
+      case Seq(pathLit : ScLiteral) =>
+        pathLit.stringValue
+      case Seq(parentLit : ScLiteral, childLit : ScLiteral) =>
+        for {
+          parent <- parentLit.stringValue
+          child  <- childLit.stringValue
+        } yield parent + File.separator + child
+      case Seq(parentElt, childLit : ScLiteral) if childLit.isString =>
+        for {
+          parent <- extractPathFromFileParam(parentElt)
+          child  <- childLit.stringValue
+        } yield parent + File.separator + child
+      case _ => None
+    }
+  }
+
+  private def extractPathFromConcatenation(concatExpr: ScInfixExpr): Option[String] =
+    concatExpr.rOp match {
+      case partLit : ScLiteral =>
+        for {
+          parent <- extractPathFromFileParam(concatExpr.lOp)
+          child  <- partLit.stringValue
+        } yield parent + File.separator + child
+      case partRef : ScReferenceExpression =>
+        for {
+          parent <- extractPathFromFileParam(concatExpr.lOp)
+          child  <- extractPathFromFileParam(partRef)
+        } yield parent + File.separator + child
+      case _ => None
+    }
+
+  private def extractPathFromReference(ref: PsiElement): Option[String] = {
+    for {
+      listOfPatterns <- Option(ref.getParent)
+      patternDef <- Option(listOfPatterns.getParent)
+    } yield patternDef match {
+      case ScPatternDefinition.expr(e) => extractPathFromFileParam(e)
+      case _ => None
+    }
+  }.flatten
 }
 
 private class SbtSubprojectReference[T <: PsiElement](val element: T, val sbtFile: PsiFile)
         extends PsiReferenceBase.Immediate[T](element,
           TextRange.create(element.getStartOffsetInParent, element.getStartOffsetInParent + element.getTextLength),
           sbtFile)
+
+private object SbtSubprojectReferenceProvider {
+  implicit class RichScLiteral(literal: ScLiteral) {
+    def stringValue: Option[String] = {
+      if (literal.isString)
+        Some(literal.getValue.asInstanceOf[String])
+      else
+        None
+    }
+  }
+}
