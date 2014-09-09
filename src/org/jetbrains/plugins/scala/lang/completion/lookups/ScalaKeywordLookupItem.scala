269c8a325aa25a5a35056745e74a7659b571b298
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-09-09 12:56:20 +0400

    Fixed keyword completion tests.

--- src/org/jetbrains/plugins/scala/lang/completion/lookups/ScalaKeywordLookupItem.scala
+++ src/org/jetbrains/plugins/scala/lang/completion/lookups/ScalaKeywordLookupItem.scala
@@ -14,65 +14,67 @@ import org.jetbrains.plugins.scala.ScalaFileType
  */
 object ScalaKeywordLookupItem {
   def getLookupElement(keyword: String, position: PsiElement): LookupElement = {
-    val keywordPsi: PsiElement = new ScalaLightKeyword(position.getManager, keyword)
+    val keywordPsi: PsiElement = ScalaLightKeyword(position.getManager, keyword)
     LookupElementBuilder.create(keywordPsi, keyword).withBoldness(true).withIcon(new EmptyIcon(16, 16)).
-      withInsertHandler(new InsertHandler[LookupElement] {
-      override def handleInsert(context: InsertionContext, item: LookupElement): Unit = {
-        import org.jetbrains.plugins.scala.lang.completion.ScalaKeyword._
-        val parentheses = Set(IF, FOR, WHILE)
-        val braces = Set(CATCH, ELSE, EXTENDS, FINALLY, FOR, FOR_SOME, NEW, TRY, DO, YIELD)
-        val editor = context.getEditor
-        val document = editor.getDocument
-        val offset = context.getStartOffset + keyword.length
-        keyword match {
-          case THIS | FALSE | TRUE | NULL | SUPER => // do nothing
-          case _ =>
-            def addSpace(addCompletionChar: Boolean = false) {
-              context.setAddCompletionChar(addCompletionChar)
-              if (document.getTextLength <= offset || document.getText.charAt(offset) != ' ')
-                document.insertString(offset, " ")
-              editor.getCaretModel.moveToOffset(offset + 1)
-            }
-            val settings = CodeStyleSettingsManager.getInstance(context.getProject).getCurrentSettings.getCommonSettings(ScalaFileType.SCALA_LANGUAGE)
-            context.getCompletionChar match {
-              case '(' if parentheses.contains(keyword) =>
-                val add = keyword match {
-                  case IF => settings.SPACE_BEFORE_IF_PARENTHESES
-                  case FOR => settings.SPACE_BEFORE_FOR_PARENTHESES
-                  case WHILE => settings.SPACE_BEFORE_WHILE_PARENTHESES
-                }
-                if (add) addSpace(addCompletionChar = true)
-              case '{' if braces.contains(keyword) =>
-                val add = keyword match {
-                  case CATCH => settings.SPACE_BEFORE_CATCH_LBRACE
-                  case ELSE => settings.SPACE_BEFORE_ELSE_LBRACE
-                  case EXTENDS => true
-                  case FINALLY => settings.SPACE_BEFORE_FINALLY_LBRACE
-                  case FOR => settings.SPACE_BEFORE_FOR_LBRACE
-                  case FOR_SOME => true
-                  case NEW => true
-                  case TRY => settings.SPACE_BEFORE_TRY_LBRACE
-                  case DO => settings.SPACE_BEFORE_DO_LBRACE
-                  case YIELD => settings.SPACE_BEFORE_FOR_LBRACE
-                }
-                if (add) addSpace(addCompletionChar = true)
-              case '[' =>
-                keyword match {
-                  case PRIVATE | PROTECTED => //do nothing
-                  case _ => addSpace(addCompletionChar = false)
-                }
-              case _ => addSpace()
-            }
-            if (keyword == CASE) {
-              val manager = PsiDocumentManager.getInstance(context.getProject)
-              manager.commitDocument(document)
-              val file = manager.getPsiFile(document)
-              if (file == null) return
-              CodeStyleManager.getInstance(context.getProject).
-                adjustLineIndent(file, new TextRange(context.getStartOffset, offset))
-            }
-        }
+      withInsertHandler(new KeywordInsertHandler(keyword))
+  }
+
+  class KeywordInsertHandler(keyword: String) extends InsertHandler[LookupElement] {
+    override def handleInsert(context: InsertionContext, item: LookupElement): Unit = {
+      import org.jetbrains.plugins.scala.lang.completion.ScalaKeyword._
+      val parentheses = Set(IF, FOR, WHILE)
+      val braces = Set(CATCH, ELSE, EXTENDS, FINALLY, FOR, FOR_SOME, NEW, TRY, DO, YIELD)
+      val editor = context.getEditor
+      val document = editor.getDocument
+      val offset = context.getStartOffset + keyword.length
+      keyword match {
+        case THIS | FALSE | TRUE | NULL | SUPER => // do nothing
+        case _ =>
+          def addSpace(addCompletionChar: Boolean = false) {
+            context.setAddCompletionChar(addCompletionChar)
+            if (document.getTextLength <= offset || document.getText.charAt(offset) != ' ')
+              document.insertString(offset, " ")
+            editor.getCaretModel.moveToOffset(offset + 1)
+          }
+          val settings = CodeStyleSettingsManager.getInstance(context.getProject).getCurrentSettings.getCommonSettings(ScalaFileType.SCALA_LANGUAGE)
+          context.getCompletionChar match {
+            case '(' if parentheses.contains(keyword) =>
+              val add = keyword match {
+                case IF => settings.SPACE_BEFORE_IF_PARENTHESES
+                case FOR => settings.SPACE_BEFORE_FOR_PARENTHESES
+                case WHILE => settings.SPACE_BEFORE_WHILE_PARENTHESES
+              }
+              if (add) addSpace(addCompletionChar = true)
+            case '{' if braces.contains(keyword) =>
+              val add = keyword match {
+                case CATCH => settings.SPACE_BEFORE_CATCH_LBRACE
+                case ELSE => settings.SPACE_BEFORE_ELSE_LBRACE
+                case EXTENDS => true
+                case FINALLY => settings.SPACE_BEFORE_FINALLY_LBRACE
+                case FOR => settings.SPACE_BEFORE_FOR_LBRACE
+                case FOR_SOME => true
+                case NEW => true
+                case TRY => settings.SPACE_BEFORE_TRY_LBRACE
+                case DO => settings.SPACE_BEFORE_DO_LBRACE
+                case YIELD => settings.SPACE_BEFORE_FOR_LBRACE
+              }
+              if (add) addSpace(addCompletionChar = true)
+            case '[' =>
+              keyword match {
+                case PRIVATE | PROTECTED => //do nothing
+                case _ => addSpace(addCompletionChar = false)
+              }
+            case _ => addSpace()
+          }
+          if (keyword == CASE) {
+            val manager = PsiDocumentManager.getInstance(context.getProject)
+            manager.commitDocument(document)
+            val file = manager.getPsiFile(document)
+            if (file == null) return
+            CodeStyleManager.getInstance(context.getProject).
+              adjustLineIndent(file, new TextRange(context.getStartOffset, offset))
+          }
       }
-    })
+    }
   }
 }
