c59f48f8037063042a1d84dc80a6a5b874301dc0
niktrop <Nikolay.Tropin@jetbrains.com>
2014-09-12 17:00:43 +0400

    bug with default argument in change signature fixed

--- test/org/jetbrains/plugins/scala/refactoring/changeSignature/ChangeSignatureInScalaTest.scala
+++ test/org/jetbrains/plugins/scala/refactoring/changeSignature/ChangeSignatureInScalaTest.scala
@@ -17,7 +17,7 @@ class ChangeSignatureInScalaTest extends ChangeSignatureTestBase {
                          newName: String,
                          newReturnType: String,
                          newParams: => Seq[Seq[ParameterInfo]]): ChangeSignatureProcessorBase = {
-    scalaProcessor(newVisibility, newName, newReturnType, newParams, isAddDefaultValue = false)
+    scalaProcessor(newVisibility, newName, newReturnType, newParams, isAddDefaultValue)
   }
 
   override def mainFileName(testName: String): String = testName + ".scala"
@@ -30,28 +30,45 @@ class ChangeSignatureInScalaTest extends ChangeSignatureTestBase {
   }
 
   def testVisibility(): Unit = {
+    isAddDefaultValue = false
     val params = Seq(parameterInfo("i", -1, types.Int, "1"))
     doTest("protected", "foo", null, Seq(params))
   }
 
   def testAddRepeatedParam(): Unit = {
+    isAddDefaultValue = false
     val params = Seq(parameterInfo("i", 0, types.Int), parameterInfo("b", 1, types.Boolean),
       parameterInfo("xs", -1, types.Int, isRep = true, defVal = "1"))
     doTest(null, "foo", null, Seq(params))
   }
 
   def testAddRepeatedWithoutDefault(): Unit = {
+    isAddDefaultValue = false
     val params = Seq(parameterInfo("i", 0, types.Int), parameterInfo("xs", -1, types.Int, isRep = true))
     doTest(null, "foo", null, Seq(params))
   }
 
   def testMakeRepeatedParam(): Unit = {
+    isAddDefaultValue = false
     val params = Seq(parameterInfo("i", 0, types.Int), parameterInfo("b", 1, types.Boolean, isRep = true))
     doTest(null, "foo", null, Seq(params))
   }
 
   def testRemoveRepeatedParam(): Unit = {
+    isAddDefaultValue = false
     val params = Seq(parameterInfo("i", 0, types.Int), parameterInfo("b", 1, types.Boolean))
+    doTest(null, "foo", null, Seq(params))
+  }
+
+  def testNoDefaultArg(): Unit = {
+    isAddDefaultValue = true
+    val params = Seq(parameterInfo("i", 0, types.Int), parameterInfo("j", -1, types.Int))
+    doTest(null, "foo", null, Seq(params))
+  }
+
+  def testNoDefaultArg2(): Unit = {
+    isAddDefaultValue = false
+    val params = Seq(parameterInfo("i", 0, types.Int), parameterInfo("j", -1, types.Int))
     doTest(null, "foo", null, Seq(params))
   }
 
