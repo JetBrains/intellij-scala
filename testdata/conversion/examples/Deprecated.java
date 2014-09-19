43010a8324c5a0940b5fa2ccd778e83a3f0e71f8
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-09-19 13:47:46 +0400

    Deprecated annotation conversion.

---
+++ testdata/conversion/examples/Deprecated.java
@@ -0,0 +1,14 @@
+@Deprecated
+public class Day {
+  @Deprecated
+  public String s() {
+    return "text";
+  }
+}
+/*
+@deprecated class Day {
+  @deprecated def s: String = {
+    return "text"
+  }
+}
+ */
