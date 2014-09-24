0ad32671e0a9d688123f361eb92e05b72f7c8702
niktrop <Nikolay.Tropin@jetbrains.com>
2014-09-24 10:48:49 +0400

    Quick fix for adding LUB return type added for annotation of function with no return type and with return keyword. #SCL-3161 fixed

--- src/org/jetbrains/plugins/scala/annotator/FunctionAnnotator.scala
+++ src/org/jetbrains/plugins/scala/annotator/FunctionAnnotator.scala
@@ -5,11 +5,11 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.annotator.AnnotatorUtils._
 import org.jetbrains.plugins.scala.annotator.quickfix.modifiers.AddModifierQuickFix
-import org.jetbrains.plugins.scala.annotator.quickfix.{RemoveElementQuickFix, ReportHighlightingErrorQuickFix}
+import org.jetbrains.plugins.scala.annotator.quickfix.{AddReturnTypeFix, RemoveElementQuickFix, ReportHighlightingErrorQuickFix}
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 import org.jetbrains.plugins.scala.lang.psi.types.result.{Success, TypeResult, TypingContext}
-import org.jetbrains.plugins.scala.lang.psi.types.{ScType, Any => AnyType, Unit => UnitType}
+import org.jetbrains.plugins.scala.lang.psi.types.{Any => AnyType, Unit => UnitType, Bounds, ScType}
 
 /**
  * Pavel.Fatin, 18.05.2010
@@ -87,12 +87,18 @@ trait FunctionAnnotator {
 
       def needsTypeAnnotation() = {
         val message = ScalaBundle.message("function.must.define.type.explicitly", function.name)
-        holder.createErrorAnnotation(usage.asInstanceOf[ScReturnStmt].returnKeyword, message)
+        val returnTypes = function.returnUsages(withBooleanInfix = false).toSeq.collect {
+          case retStmt: ScReturnStmt => retStmt.expr.flatMap(_.getType().toOption).getOrElse(AnyType)
+          case expr: ScExpression => expr.getType().getOrAny
+        }
+        val lub = Bounds.lub(returnTypes)
+        val annotation = holder.createErrorAnnotation(usage.asInstanceOf[ScReturnStmt].returnKeyword, message)
+        annotation.registerFix(new AddReturnTypeFix(function, lub))
       }
 
       def redundantReturnExpression() = {
         val message = ScalaBundle.message("return.expression.is.redundant", usageType.presentableText)
-        holder.createWarningAnnotation(usage.asInstanceOf[ScReturnStmt].expr.get, message);
+        holder.createWarningAnnotation(usage.asInstanceOf[ScReturnStmt].expr.get, message)
       }
 
       def typeMismatch() {
@@ -102,7 +108,7 @@ trait FunctionAnnotator {
           val returnExpression = if (explicitReturn) usage.asInstanceOf[ScReturnStmt].expr else None
           val expr = returnExpression.getOrElse(usage) match {
             case b: ScBlockExpr => b.getRBrace.map(_.getPsi).getOrElse(b)
-            case expr => expr
+            case e => e
           }
           val annotation = holder.createErrorAnnotation(expr, message)
           annotation.registerFix(ReportHighlightingErrorQuickFix)
