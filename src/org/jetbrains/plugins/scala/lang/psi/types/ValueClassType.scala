436e3d927d9e78d40c1968119eed016784f1568c
niktrop <Nikolay.Tropin@jetbrains.com>
2014-10-02 13:33:05 +0400

    Extractor for value class types added

---
+++ src/org/jetbrains/plugins/scala/lang/psi/types/ValueClassType.scala
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.scala
+package lang.psi.types
+
+import org.jetbrains.plugins.scala.extensions.{PsiClassExt, PsiParameterExt}
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScPrimaryConstructor
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
+import org.jetbrains.plugins.scala.lang.psi.types.ScType.ExtractClass
+
+/**
+ * Nikolay.Tropin
+ * 2014-10-02
+ */
+object ValueClassType {
+  def unapply(tp: ScType): Option[ScType] = {
+    tp match {
+      case _: ValType => None
+      case ExtractClass(cl: ScClass) if isValueClass(cl) =>
+        cl.constructors match {
+          case Array(pc: ScPrimaryConstructor) =>
+            pc.parameters.headOption.map(_.exactParamType())
+          case _ => None
+        }
+      case _ => None
+    }
+  }
+
+  def isValueClass(cl: ScClass) = cl.supers.map(_.qualifiedName).contains("scala.AnyVal")
+}
