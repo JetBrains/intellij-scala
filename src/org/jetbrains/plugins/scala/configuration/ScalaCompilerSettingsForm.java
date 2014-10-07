e582f8d3ea8369d45673f73ae0ac62004b106f06
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-10-07 14:25:58 +0200

    Language feature settings for Scala compiler

--- src/org/jetbrains/plugins/scala/configuration/ScalaCompilerSettingsForm.java
+++ src/org/jetbrains/plugins/scala/configuration/ScalaCompilerSettingsForm.java
@@ -23,6 +23,14 @@ public class ScalaCompilerSettingsForm {
   private JCheckBox myContinuations;
   private JComboBox<CompileOrder> myCompileOrder;
   private JComboBox<IncrementalityType> myIncrementalityType;
+  private JCheckBox myMacros;
+  private JCheckBox myDynamics;
+  private JCheckBox myPostfixOps;
+  private JCheckBox myReflectiveCalls;
+  private JCheckBox myImplicitConversions;
+  private JCheckBox myHigherKinds;
+  private JCheckBox myExistentials;
+  private JCheckBox myFeatureWarnings;
 
   private MyPathEditor myPluginsEditor = new MyPathEditor(new FileChooserDescriptor(true, false, true, true, false, true));
 
@@ -42,26 +50,46 @@ public class ScalaCompilerSettingsForm {
 
   public ScalaCompilerSettingsState getState() {
     ScalaCompilerSettingsState state = new ScalaCompilerSettingsState();
+
+    state.macros = myMacros.isSelected();
+    state.dynamics = myDynamics.isSelected();
+    state.postfixOps = myPostfixOps.isSelected();
+    state.reflectiveCalls = myReflectiveCalls.isSelected();
+    state.implicitConversions = myImplicitConversions.isSelected();
+    state.higherKinds = myHigherKinds.isSelected();
+    state.existentials = myExistentials.isSelected();
+
     state.incrementalityType = (IncrementalityType) myIncrementalityType.getSelectedItem();
     state.compileOrder = (CompileOrder) myCompileOrder.getSelectedItem();
     state.warnings = myWarnings.isSelected();
     state.deprecationWarnings = myDeprecationWarnings.isSelected();
     state.uncheckedWarnings = myUncheckedWarnings.isSelected();
+    state.featureWarnings = myFeatureWarnings.isSelected();
     state.optimiseBytecode = myOptimiseBytecode.isSelected();
     state.explainTypeErrors = myExplainTypeErrors.isSelected();
     state.continuations = myContinuations.isSelected();
     state.debuggingInfoLevel = (DebuggingInfoLevel) myDebuggingInfoLevel.getSelectedItem();
     state.additionalCompilerOptions = myAdditionalCompilerOptions.getText();
     state.plugins = myPluginsEditor.getPaths();
+
     return state;
   }
 
   public void setState(ScalaCompilerSettingsState state) {
+    myMacros.setSelected(state.macros);
+    myDynamics.setSelected(state.dynamics);
+    myPostfixOps.setSelected(state.postfixOps);
+    myReflectiveCalls.setSelected(state.reflectiveCalls);
+    myImplicitConversions.setSelected(state.implicitConversions);
+    myHigherKinds.setSelected(state.higherKinds);
+    myExistentials.setSelected(state.existentials);
+
     myIncrementalityType.setSelectedItem(state.incrementalityType);
     myCompileOrder.setSelectedItem(state.compileOrder);
     myWarnings.setSelected(state.warnings);
     myDeprecationWarnings.setSelected(state.deprecationWarnings);
     myUncheckedWarnings.setSelected(state.uncheckedWarnings);
+    myFeatureWarnings.setSelected(state.featureWarnings);
     myOptimiseBytecode.setSelected(state.optimiseBytecode);
     myExplainTypeErrors.setSelected(state.explainTypeErrors);
     myContinuations.setSelected(state.continuations);
