1a695154ed8ebb11e37330b90aff94df47b4adc7
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-10-09 22:47:22 +0400

    deduplicate build files, add community test configurations

---
+++ project/common.scala
@@ -0,0 +1,30 @@
+import sbt._
+import Keys._
+
+object common extends Build {
+  lazy val ideaBaseJars = SettingKey[Classpath]("")
+  lazy val ideaICPluginJars = SettingKey[Classpath]("")
+  lazy val ideaIUPluginJars = SettingKey[Classpath]("")
+  lazy val allIdeaJars = SettingKey[Classpath]("")
+
+  lazy val packagePlugin = taskKey[Unit]("package scala plugin locally")
+  lazy val packageStructure = taskKey[Map[File, String]]("plugin artifact structure")
+
+  // merge multiple jars in one and return it
+  def merge(files: File*): File = {
+    IO.withTemporaryDirectory({ tmp =>
+      files.foreach(IO.unzip(_, tmp))
+      val zipFile = IO.temporaryDirectory / "sbt-merge-result.jar"
+      zipFile.delete()
+      IO.zip((tmp ***) pair (relativeTo(tmp), false), zipFile)
+      zipFile
+    })
+  }
+
+  def readIdeaPropery(key: String): String = {
+    import java.util.Properties
+    val prop = new Properties()
+    IO.load(prop, file("idea.properties"))
+    prop.getProperty(key)
+  }
+}
