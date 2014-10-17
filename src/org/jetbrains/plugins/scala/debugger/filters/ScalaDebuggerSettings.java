197dc08f3fae7a98449ff9415bbd8ef6f73c5c07
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2014-10-17 19:49:24 +0400

    Compilation fixed

--- src/org/jetbrains/plugins/scala/debugger/filters/ScalaDebuggerSettings.java
+++ src/org/jetbrains/plugins/scala/debugger/filters/ScalaDebuggerSettings.java
@@ -27,6 +27,7 @@ public class ScalaDebuggerSettings extends XDebuggerSettings<ScalaDebuggerSettin
   public Boolean DEBUG_DISABLE_SPECIFIC_SCALA_METHODS = true;
   public Boolean FRIENDLY_COLLECTION_DISPLAY_ENABLED = true;
   public Boolean FRIENDLY_OBJECT_REF_DISPLAY_ENABLED = true;
+  public Boolean DONT_SHOW_RUNTIME_REFS = false;
   public Boolean DO_NOT_DISPLAY_STREAMS = true;
   public Integer COLLECTION_START_INDEX = 0;
   public Integer COLLECTION_END_INDEX = 49;
