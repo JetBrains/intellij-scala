181625dd4b367ab4b265ed84201b2728e23c1a9a
Nick Obedin <dancingrobot84@gmail.com>
2014-10-23 17:34:54 +0400

    = updated sbt-structure and library name format

--- SBT/src/main/scala/org/jetbrains/sbt/project/SbtProjectResolver.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/SbtProjectResolver.scala
@@ -164,7 +164,7 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
   }
 
   private def nameFor(id: ModuleId) =
-    s"${id.organization}:${id.name}:${id.revision}:${id.artifactType}" + id.classifier.map(":"+_).getOrElse("")
+    s"${id.organization}:${id.name}:${id.revision}" + id.classifier.map(":"+_).getOrElse("") + s".${id.artifactType}"
 
   private def createCompilerLibrary(scala: Scala): LibraryNode = {
     val result = new LibraryNode(nameFor(scala), resolved = true)
