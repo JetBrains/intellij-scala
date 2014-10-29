c55dc973c032432a9d7738b0c4c629afaf0d83cf
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-10-29 16:54:03 +0300

    Fixed compilation

--- src/org/jetbrains/plugins/scala/lang/psi/impl/ScalaPsiElementFactory.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/ScalaPsiElementFactory.scala
@@ -76,9 +76,6 @@ class ScalaPsiElementFactoryImpl(manager: PsiManager) extends JVMElementFactory 
 
   def createField(name: String, `type`: PsiType): PsiField = throw new IncorrectOperationException
 
-  override def createMethod(s: String, psiType: PsiType, psiElement: PsiElement): PsiMethod =
-    throw new IncorrectOperationException
-
   def createMethod(name: String, returnType: PsiType): PsiMethod = throw new IncorrectOperationException
 
   def createMethod(name: String, returnType: PsiType, context: PsiElement): PsiMethod = throw new IncorrectOperationException
