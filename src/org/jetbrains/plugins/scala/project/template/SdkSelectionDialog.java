5593ab3067aa1d769ef5d5e15a0829dd96733146
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-10-30 15:15:32 +0300

    update forms

--- src/org/jetbrains/plugins/scala/project/template/SdkSelectionDialog.java
+++ src/org/jetbrains/plugins/scala/project/template/SdkSelectionDialog.java
@@ -1,6 +1,9 @@
 package org.jetbrains.plugins.scala.project.template;
 
 import com.intellij.ui.table.TableView;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import scala.Option;
 
 import javax.swing.*;
@@ -11,97 +14,147 @@ import java.awt.event.*;
 import java.util.List;
 
 public class SdkSelectionDialog extends JDialog {
-  private JPanel contentPane;
-  private JButton buttonOK;
-  private JButton buttonCancel;
-  private JButton buttonBrowse;
-  private TableView<SdkChoice> myTable;
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JButton buttonBrowse;
+    private TableView<SdkChoice> myTable;
 
-  private final JComponent myParent;
-  private final SdkTableModel myTableModel = new SdkTableModel();
-  private ScalaSdkDescriptor mySelectedSdk;
+    private final JComponent myParent;
+    private final SdkTableModel myTableModel = new SdkTableModel();
+    private ScalaSdkDescriptor mySelectedSdk;
 
-  public SdkSelectionDialog(JComponent parent, List<SdkChoice> sdks) {
-    super((Window) parent.getTopLevelAncestor());
+    public SdkSelectionDialog(JComponent parent, List<SdkChoice> sdks) {
+        super((Window) parent.getTopLevelAncestor());
 
-    myParent = parent;
+        myParent = parent;
 
-    setTitle("Select JAR's for the new Scala SDK");
+        setTitle("Select JAR's for the new Scala SDK");
 
-    setContentPane(contentPane);
-    setModal(true);
-    getRootPane().setDefaultButton(buttonOK);
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
 
-    buttonBrowse.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        onBrowse();
-      }
-    });
-    buttonOK.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        onOK();
-      }
-    });
-    buttonCancel.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        onCancel();
-      }
-    });
+        buttonBrowse.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onBrowse();
+            }
+        });
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
 
-    myTable.getSelectionModel().addListSelectionListener(new SdkSelectionListener());
+        myTable.getSelectionModel().addListSelectionListener(new SdkSelectionListener());
 
-    setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-    addWindowListener(new WindowAdapter() {
-      public void windowClosing(WindowEvent e) {
-        onCancel();
-      }
-    });
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
 
-    contentPane.registerKeyboardAction(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        onCancel();
-      }
-    }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-    myTableModel.setItems(sdks);
+        myTableModel.setItems(sdks);
 
-    myTable.setModelAndUpdateColumns(myTableModel);
+        myTable.setModelAndUpdateColumns(myTableModel);
 
-    if (!sdks.isEmpty()) {
-      myTable.getSelectionModel().setSelectionInterval(0, 0);
+        if (!sdks.isEmpty()) {
+            myTable.getSelectionModel().setSelectionInterval(0, 0);
+        }
     }
-  }
 
-  private void onBrowse() {
-    Option<ScalaSdkDescriptor> result = SdkSelection.chooseScalaSdkFiles(myTable);
+    private void onBrowse() {
+        Option<ScalaSdkDescriptor> result = SdkSelection.chooseScalaSdkFiles(myTable);
 
-    if (result.isDefined()) {
-      mySelectedSdk = result.get();
-      dispose();
+        if (result.isDefined()) {
+            mySelectedSdk = result.get();
+            dispose();
+        }
     }
-  }
 
-  private void onOK() {
-    mySelectedSdk = myTableModel.getItems().get(myTable.getSelectedRow()).sdk();
-    dispose();
-  }
-
-  private void onCancel() {
-    mySelectedSdk = null;
-    dispose();
-  }
-
-  public ScalaSdkDescriptor open() {
-    pack();
-    setLocationRelativeTo(myParent.getTopLevelAncestor());
-    setVisible(true);
-    return mySelectedSdk;
-  }
-
-  private class SdkSelectionListener implements ListSelectionListener {
-    @Override
-    public void valueChanged(ListSelectionEvent e) {
-      buttonOK.setEnabled(myTable.getSelectedRow() >= 0);
+    private void onOK() {
+        mySelectedSdk = myTableModel.getItems().get(myTable.getSelectedRow()).sdk();
+        dispose();
     }
-  }
+
+    private void onCancel() {
+        mySelectedSdk = null;
+        dispose();
+    }
+
+    public ScalaSdkDescriptor open() {
+        pack();
+        setLocationRelativeTo(myParent.getTopLevelAncestor());
+        setVisible(true);
+        return mySelectedSdk;
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        contentPane = new JPanel();
+        contentPane.setLayout(new GridLayoutManager(2, 1, new Insets(10, 10, 10, 10), -1, -1));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+        contentPane.add(panel1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel1.add(spacer1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1, true, false));
+        panel1.add(panel2, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        buttonOK = new JButton();
+        buttonOK.setText("OK");
+        panel2.add(buttonOK, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        buttonCancel = new JButton();
+        buttonCancel.setText("Cancel");
+        panel2.add(buttonCancel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        buttonBrowse = new JButton();
+        buttonBrowse.setText("Browse...");
+        panel1.add(buttonBrowse, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        contentPane.add(panel3, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel3.add(scrollPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        myTable = new TableView();
+        scrollPane1.setViewportView(myTable);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return contentPane;
+    }
+
+    private class SdkSelectionListener implements ListSelectionListener {
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            buttonOK.setEnabled(myTable.getSelectedRow() >= 0);
+        }
+    }
 }
