9f52f5b7f394f7331eeca3f1109baed3fba3750d
niktrop <Nikolay.Tropin@jetbrains.com>
2014-11-06 17:12:42 +0300

    Path to vm executable is added to sbt project settings

--- SBT/src/main/scala/org/jetbrains/sbt/project/settings/SbtSettingsListener.scala
+++ SBT/src/main/scala/org/jetbrains/sbt/project/settings/SbtSettingsListener.scala
@@ -9,6 +9,8 @@ import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListen
 trait SbtSettingsListener extends ExternalSystemSettingsListener[SbtProjectSettings] {
   def onJdkChanged(oldValue: String, newValue: String)
 
+  def onVmExecutableChanged(oldValue: String, newValue: String)
+
   def onResolveClassifiersChanged(oldValue: Boolean, newValue: Boolean)
 
   def onResolveSbtClassifiersChanged(oldValue: Boolean, newValue: Boolean)
