5166e452038e6307c3c958adfbf62bce7eda25ee
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-11-07 19:45:43 +0300

    fix hocon tests running

--- intellij-hocon/build.sbt
+++ intellij-hocon/build.sbt
@@ -5,3 +5,8 @@ organization := "JetBrains"
 scalaVersion := "2.11.2"
 
 unmanagedResourceDirectories in Compile += baseDirectory.value /  "resources"
+
+baseDirectory in Test := baseDirectory.value.getParentFile
+
+fork := true
+
--- intellij-hocon/src/test/java/intellijhocon/parser/HoconParserTestSuite.java
+++ intellij-hocon/src/test/java/intellijhocon/parser/HoconParserTestSuite.java
@@ -1,7 +1,10 @@
 package intellijhocon.parser;
 
 import junit.framework.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
 
+@RunWith(AllTests.class)
 public class HoconParserTestSuite {
   public static Test suite() {
     return new HoconParserTest();
