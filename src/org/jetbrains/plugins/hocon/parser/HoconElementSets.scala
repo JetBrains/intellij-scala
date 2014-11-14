d354f8469c724ea4e3636d72eea9e72fffde385d
ghik <romeqjanoosh@gmail.com>
2014-11-15 00:29:33 +0100

    moved hocon classes into org.jetbrains.plugins.hocon

---
+++ src/org/jetbrains/plugins/hocon/parser/HoconElementSets.scala
@@ -0,0 +1,14 @@
+package org.jetbrains.plugins.hocon.parser
+
+import com.intellij.psi.TokenType
+
+object HoconElementSets {
+
+  import org.jetbrains.plugins.hocon.Util._
+  import org.jetbrains.plugins.hocon.parser.HoconElementType._
+
+  val Path = FieldPath | SubstitutionPath
+  val Literal = Null | Boolean | Number | String
+  val Value = Literal | Object | Array | Substitution | Concatenation
+  val ForcedLeafBlock = Path | UnquotedString | Number | Null | Boolean | TokenType.ERROR_ELEMENT
+}
---
+++ test/org/jetbrains/plugins/hocon/lexer/HoconLexerTestSuite.java
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.hocon.lexer;
+
+import junit.framework.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+
+/**
+ * Author: ghik
+ * Created: 2014-07-13
+ */
+@RunWith(AllTests.class)
+public class HoconLexerTestSuite {
+    public static Test suite() {
+        return new HoconLexerTest();
+    }
+}
