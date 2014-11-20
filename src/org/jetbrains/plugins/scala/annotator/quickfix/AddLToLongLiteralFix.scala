a16e988c70404c98db716e89c8c4e5036fb5597d
Ye Xianjin <advancedxy@gmail.com>
2014-11-20 23:06:11 +0800

    polish AddLToLongLiteralFix.scala.

--- src/org/jetbrains/plugins/scala/annotator/quickfix/AddLToLongLiteralFix.scala
+++ src/org/jetbrains/plugins/scala/annotator/quickfix/AddLToLongLiteralFix.scala
@@ -21,8 +21,11 @@ class AddLToLongLiteralFix(literal: ScLiteral) extends IntentionAction {
 
   def isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean = literal.isValid && literal.getManager.isInProject(file)
 
-  def invoke(project: Project, editor: Editor, file: PsiFile) {
-    literal.updateText(literal.getText + 'L')
+  def invoke(project: Project, editor: Editor, file: PsiFile): Unit = {
+    if (!literal.isValid) return
+    val psi = ScalaPsiElementFactory.createExpressionFromText(literal.getText + "L", literal.getManager)
+    literal.replace(psi)
+    UndoUtil.markPsiFileForUndo(file)
   }
 }
 
