5426c3b455a83c83d9b993bc5aa8a4ca9bd440c7
niktrop <Nikolay.Tropin@jetbrains.com>
2014-11-21 13:53:45 +0300

    change signature fix: rename parameter names in inheritors only if they are the same as in base method #SCL-7899 fixed

--- src/org/jetbrains/plugins/scala/lang/refactoring/changeSignature/ScalaChangeSignatureUsageHandler.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/changeSignature/ScalaChangeSignatureUsageHandler.scala
@@ -358,11 +358,22 @@ private[changeSignature] trait ScalaChangeSignatureUsageHandler {
         case _ => None
       }
     }
+    def newParamName(p: ParameterInfo) = {
+      val oldIdx = p.getOldIndex
+      change match {
+        case jc: JavaChangeInfo if oldIdx >= 0 =>
+          val oldNameOfChanged = jc.getOldParameterNames()(oldIdx)
+          val oldNameOfCurrent = usage.parameters(oldIdx).name
+          if (oldNameOfChanged != oldNameOfCurrent) oldNameOfCurrent
+          else p.getName
+        case _ => p.getName
+      }
+    }
 
     val paramInfos = change.getNewParameters.toSeq
     val project = change.getMethod.getProject
     val params = paramInfos.map { p =>
-      val typedName = ScalaExtractMethodUtils.typedName(p.getName, paramType(p), project, byName = false)
+      val typedName = ScalaExtractMethodUtils.typedName(newParamName(p), paramType(p), project, byName = false)
       val default = scalaDefaultValue(p).fold("")(" = " + _)
       typedName + default
     }
