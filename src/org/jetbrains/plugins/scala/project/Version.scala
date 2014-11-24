0ba2fe49e9406806554360b1e1ca392f6ac1805d
Pavel Fatin <pavel.fatin@jetbrains.com>
2014-11-24 12:41:24 +0100

    Scala versions loader

--- src/org/jetbrains/plugins/scala/project/Version.scala
+++ src/org/jetbrains/plugins/scala/project/Version.scala
@@ -4,22 +4,27 @@ package project
 /**
  * @author Pavel Fatin
  */
-case class Version(number: String) extends AnyVal {
+case class Version(number: String) extends AnyVal with Comparable[Version] {
+  def digits: Stream[Int] = Version.IntegerPattern.findAllIn(number).map(_.toInt).toStream
+
+  def compareTo(other: Version) = (digits, other.digits).zipped.collectFirst {
+    case (a, b) if a != b => a.compareTo(b)
+  } getOrElse {
+    0
+  }
+
+  def >(other: Version): Boolean = compareTo(other) > 0
+
+  def >=(other: Version): Boolean = compareTo(other) >= 0
+
+  def <(other: Version): Boolean = compareTo(other) < 0
+
+  def <=(other: Version): Boolean = compareTo(other) < 0
+
   def toLanguageLevel: Option[ScalaLanguageLevel] = ScalaLanguageLevel.from(this)
 }
 
 object Version {
-  implicit object VersionOrdering extends Ordering[Version] {
-    private val IntegerPattern = "\\d+".r
-
-    override def compare(x: Version, y: Version) = (integersIn(x.number), integersIn(y.number)).zipped.collectFirst {
-      case (a, b) if a != b => a.compareTo(b)
-    } getOrElse {
-      0
-    }
-
-    private def integersIn(number: String): Stream[Int] =
-      IntegerPattern.findAllIn(number).map(_.toInt).toStream
-  }
+  private val IntegerPattern = "\\d+".r
 }
 
