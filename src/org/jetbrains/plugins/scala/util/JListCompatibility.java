46e9274271b6ce2b28a7e197acb1ec929dec2bb8
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-11-27 12:43:13 +0300

    Fixed compilation with Java 1.7.

--- src/org/jetbrains/plugins/scala/util/JListCompatibility.java
+++ src/org/jetbrains/plugins/scala/util/JListCompatibility.java
@@ -13,89 +13,93 @@ import java.util.ArrayList;
  */
 public class JListCompatibility {
   public static DefaultListModel createDefaultListModel() {
-      return new DefaultListModel();
+    return new DefaultListModel();
   }
 
-    public static class CollectionListModelWrapper {
-        public CollectionListModelWrapper(CollectionListModel<String> model) {
-            this.model = model;
-        }
-
-        public CollectionListModel<String> getModel() {
-            return model;
-        }
-
-        CollectionListModel<String> model;
-
-        public ListModel getModelRaw() {
-            return model;
-        }
+  public static class CollectionListModelWrapper {
+    public CollectionListModelWrapper(CollectionListModel<String> model) {
+      this.model = model;
     }
 
-    public static ListModel createCollectionListModel() {
-        return new CollectionListModel<String>(new ArrayList<String>());
+    public CollectionListModel<String> getModel() {
+      return model;
     }
 
-    public static JList createJListFromModel(DefaultListModel model) {
-        return new JList(model);
+    CollectionListModel<String> model;
+
+    public ListModel getModelRaw() {
+      return model;
+    }
+  }
+
+  public static ListModel createCollectionListModel() {
+    return new CollectionListModel<String>(new ArrayList<String>());
+  }
+
+  public static JList createJListFromModel(DefaultListModel model) {
+    return new JList(model);
+  }
+
+  public static JList createJBListFromModel(DefaultListModel model) {
+    return new JBList(model);
+  }
+
+  public static JList createJBListFromListData(Object... listData) {
+    return new JBList(listData);
+  }
+
+  public static class GoToImplicitConversionAction {
+    private static JList list = null;
+
+    public static JList getList() {
+      return list;
     }
 
-    public static JList createJBListFromModel(DefaultListModel model) {
-        return new JBList(model);
+    public static void setList(JList list) {
+      GoToImplicitConversionAction.list = list;
+    }
+  }
+
+  public static class JListContainer {
+    public JListContainer(JList list) {
+      this.list = list;
     }
 
-    public static JList createJBListFromListData(Object... listData) {
-        return new JBList(listData);
+    public JList getList() {
+      return list;
     }
 
-    public static class GoToImplicitConversionAction {
-        private static JList list = null;
-
-        public static JList getList() {
-            return list;
-        }
-
-        public static void setList(JList list) {
-            GoToImplicitConversionAction.list = list;
-        }
-    }
-
-    public static class JListContainer {
-        public JListContainer(JList list) {
-            this.list = list;
-        }
-
-        public JList getList() {
-            return list;
-        }
-
-        private JList list;
-    }
+    private JList list;
+  }
 
 
-    public static DefaultListModel getDefaultListModel(ListModel model) {
-        if (model instanceof DefaultListModel) {
-            return (DefaultListModel) model;
-        } else return null;
-    }
+  public static DefaultListModel getDefaultListModel(ListModel model) {
+    if (model instanceof DefaultListModel) {
+      return (DefaultListModel) model;
+    } else return null;
+  }
 
-    public static void addElement(DefaultListModel model, Object element) {
-        model.addElement(element);
-    }
+  public static void addElement(DefaultListModel model, Object element) {
+    model.addElement(element);
+  }
 
-    public static void add(DefaultListModel model, int index, Object element) {
-        model.add(index, element);
-    }
+  public static void add(DefaultListModel model, int index, Object element) {
+    model.add(index, element);
+  }
 
-    public static void setCellRenderer(JList list, ListCellRenderer renderer) {
-        list.setCellRenderer(renderer);
-    }
+  public static void setCellRenderer(JList list, ListCellRenderer renderer) {
+    list.setCellRenderer(renderer);
+  }
 
-    public static void setModel(JList list, ListModel model) {
-        list.setModel(model);
-    }
+  public static void setModel(JList list, ListModel model) {
+    list.setModel(model);
+  }
 
-    public static ListCellRenderer getGotoFileCellRenderer(int maxSize) {
-        return new GotoFileCellRenderer(maxSize);
-    }
+  public static void setModel(JBList list, ListModel model) {
+    list.setModel(model);
+  }
+
+  public static ListCellRenderer getGotoFileCellRenderer(int maxSize) {
+    return new GotoFileCellRenderer(maxSize);
+  }
 }
