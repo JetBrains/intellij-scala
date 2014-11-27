76737c3acdd146de637f1ee112a3b7ffd6301f2d
niktrop <Nikolay.Tropin@jetbrains.com>
2014-11-27 10:03:50 +0300

    extension point for evaluator compile helper

---
+++ src/org/jetbrains/plugins/scala/debugger/evaluation/EvaluatorCompileHelper.scala
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.scala.debugger.evaluation
+
+import java.io.File
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.module.Module
+
+/**
+ * Nikolay.Tropin
+ * 2014-11-27
+ */
+object EvaluatorCompileHelper {
+  val EP_NAME = ExtensionPointName.create[EvaluatorCompileHelper]("org.intellij.scala.evaluatorCompileHelper")
+
+  def needCompileServer = EP_NAME.getExtensions.isEmpty
+}
+
+trait EvaluatorCompileHelper {
+
+  /**
+   * @return Array of all classfiles generated from a given source with corresponding dot-separated full qualified names
+   *         (like "java.lang.Object" or "scala.None$")
+   */
+  def compile(fileText: String, module: Module): Array[(File, String)]
+}
