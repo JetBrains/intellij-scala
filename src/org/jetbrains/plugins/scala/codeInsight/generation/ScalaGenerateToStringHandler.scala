5b27f27440db66873f6392ae48ec0de6768c810b
Rado Buransky <radoburansky@gmail.com>
2014-12-06 14:31:44 +0100

    ToString wizard.

--- src/org/jetbrains/plugins/scala/codeInsight/generation/ScalaGenerateToStringHandler.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/generation/ScalaGenerateToStringHandler.scala
@@ -2,10 +2,12 @@ package org.jetbrains.plugins.scala.codeInsight.generation
 
 import com.intellij.codeInsight.CodeInsightUtilBase
 import com.intellij.lang.LanguageCodeInsightActionHandler
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import org.jetbrains.plugins.scala.codeInsight.generation.ui.{ScalaGenerateToStringWizard, ScalaGenerateEqualsWizard}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
@@ -29,7 +31,7 @@ class ScalaGenerateToStringHandler extends LanguageCodeInsightActionHandler {
 
     try {
       val aClass = GenerationUtil.classAtCaret(editor, psiFile).getOrElse(return)
-      val toStringMethod = Some(createToString(aClass))
+      val toStringMethod = Some(createToString(aClass, project))
 
       extensions.inWriteAction {
         GenerationUtil.addMembers(aClass, toStringMethod.toList, editor.getDocument)
@@ -42,17 +44,27 @@ class ScalaGenerateToStringHandler extends LanguageCodeInsightActionHandler {
 
   override def startInWriteAction(): Boolean = true
 
-  private def createToString(aClass: ScClass): ScFunction = {
+  private def createToString(aClass: ScClass, project: Project): ScFunction = {
     val declText = "def toString: String"
 
-    val fields = getFields(aClass).map("$" + _.name)
+    val fields = getFields(aClass, project).map("$" + _.name)
     val fieldsText = fields.mkString(s"${aClass.getName}(", ", ", ")")
 
     val methodText = s"""override def toString = s"$fieldsText""""
     ScalaPsiElementFactory.createMethodWithContext(methodText, aClass, aClass.extendsBlock)
   }
 
-  private def getFields(aClass: ScClass): Seq[ScNamedElement] = {
-    GenerationUtil.getAllFields(aClass)
+  private def getFields(aClass: ScClass, project: Project): Seq[ScNamedElement] = {
+    if (ApplicationManager.getApplication.isUnitTestMode) {
+      GenerationUtil.getAllFields(aClass)
+    }
+    else {
+      val wizard = new ScalaGenerateToStringWizard(project, aClass)
+      wizard.show()
+      if (wizard.isOK)
+        wizard.getToStringFields
+      else
+        Nil
+    }
   }
 }
