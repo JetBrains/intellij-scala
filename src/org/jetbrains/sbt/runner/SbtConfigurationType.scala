34b5f6877477140a75d70639ad361c9a30a26f13
Nick Obedin <dancingrobot84@gmail.com>
2014-12-15 15:49:35 +0300

    = small fixes for sbt tasks run configuration

--- src/org/jetbrains/sbt/runner/SbtConfigurationType.scala
+++ src/org/jetbrains/sbt/runner/SbtConfigurationType.scala
@@ -3,7 +3,7 @@ package org.jetbrains.sbt.runner
 import javax.swing.Icon
 
 import com.intellij.execution.configurations.{ConfigurationFactory, ConfigurationType}
-import org.jetbrains.sbt.Sbt
+import org.jetbrains.sbt.{SbtBundle, Sbt}
 
 /**
  * Configuration running of sbt task.
@@ -13,9 +13,9 @@ class SbtConfigurationType extends ConfigurationType {
   
   def getIcon: Icon = Sbt.Icon
 
-  def getDisplayName: String = "SBT"
+  def getDisplayName: String = SbtBundle("sbt.runner.displayName")
 
-  def getConfigurationTypeDescription: String = "Run SBT task"
+  def getConfigurationTypeDescription: String = SbtBundle("sbt.runner.description")
 
   def getConfigurationFactories: Array[ConfigurationFactory] = Array[ConfigurationFactory](confFactory)
 
