9d68b9b73d51e88a826fd6f40343ab3460d229aa
niktrop <Nikolay.Tropin@jetbrains.com>
2014-12-17 15:20:04 +0300

    Don't step into method if it does not exist in a source file (trait forwarders, methods generated from fields etc)

--- src/org/jetbrains/plugins/scala/debugger/ScalaSyntheticSteppingFilter.scala
+++ src/org/jetbrains/plugins/scala/debugger/ScalaSyntheticSteppingFilter.scala
@@ -6,8 +6,6 @@ import com.sun.jdi.Location
 import com.sun.jdi.request.StepRequest
 import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.api.statements.ScDeclaredElementsHolder
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTemplateDefinition
 
 /**
@@ -19,25 +17,24 @@ class ScalaSyntheticSteppingFilter extends ExtraSteppingFilter {
     val debugProcess = context.getDebugProcess
     val positionManager = new ScalaPositionManager(debugProcess)
     val location = context.getFrameProxy.location()
-    isSyntheticMethod(location, positionManager)
+    inReadAction {
+      isSyntheticMethod(location, positionManager)
+    }
   }
 
   override def getStepRequestDepth(context: SuspendContext): Int = StepRequest.STEP_INTO
 
   private def isSyntheticMethod(location: Location, positionManager: ScalaPositionManager): Boolean = {
-    val classInSource = inReadAction {
-      val sourcePosition = positionManager.getSourcePosition(location)
-      if (!sourcePosition.getFile.getLanguage.is(ScalaLanguage.Instance)) return false
+    val sourcePosition = positionManager.getSourcePosition(location)
+    if (!sourcePosition.getFile.getLanguage.is(ScalaLanguage.Instance)) return false
 
-      PsiTreeUtil.getParentOfType(sourcePosition.getElementAt, classOf[ScTemplateDefinition], false)
-    }
+    val classInSource = PsiTreeUtil.getParentOfType(sourcePosition.getElementAt, classOf[ScTemplateDefinition], false)
+    if (classInSource == null) return false
 
     val name = location.method().name()
-    val membersInSource = classInSource.members.filter {
-      case named: ScNamedElement if named.name == name => true
-      case holder: ScDeclaredElementsHolder if  holder.declaredElements.exists(_.name == name) => true
-      case _ => false
-    }
-    membersInSource.isEmpty
+    if (name.endsWith("$lzycompute")) return false //should step into the body of a lazy val
+
+    val functionsWithName = classInSource.functions.filter(_.name == name)
+    functionsWithName.isEmpty
   }
 }
