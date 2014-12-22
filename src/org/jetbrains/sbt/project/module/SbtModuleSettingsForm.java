eb8e76549b1a548db8c2658a54386cbc3cec6823
Nick Obedin <dancingrobot84@gmail.com>
2014-12-22 13:23:05 +0300

    = fixed SBT module prefs in case of huge amount of imports
    (cherry picked from commit ec1c945173bdcac09af2e4d719427516b65958e9)

--- src/org/jetbrains/sbt/project/module/SbtModuleSettingsForm.java
+++ src/org/jetbrains/sbt/project/module/SbtModuleSettingsForm.java
@@ -67,8 +67,6 @@ public class SbtModuleSettingsForm {
     final JPanel panel3 = new JPanel();
     panel3.setLayout(new GridLayoutManager(4, 1, new Insets(0, 0, 0, 0), -1, -1));
     mainPanel.add(panel3, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-    sbtImportsList = new JBList();
-    panel3.add(sbtImportsList, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
     final TitledSeparator titledSeparator2 = new TitledSeparator();
     titledSeparator2.setText(ResourceBundle.getBundle("org/jetbrains/sbt/SbtBundle").getString("sbt.settings.sbtVersionInModule"));
     panel3.add(titledSeparator2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
@@ -80,6 +78,10 @@ public class SbtModuleSettingsForm {
     final TitledSeparator titledSeparator3 = new TitledSeparator();
     titledSeparator3.setText(ResourceBundle.getBundle("org/jetbrains/sbt/SbtBundle").getString("sbt.settings.implicitImports"));
     panel3.add(titledSeparator3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+    final JBScrollPane jBScrollPane2 = new JBScrollPane();
+    panel3.add(jBScrollPane2, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+    sbtImportsList = new JBList();
+    jBScrollPane2.setViewportView(sbtImportsList);
   }
 
   /**
