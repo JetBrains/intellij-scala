a3e6455b99fd8aa7c573b375f7feabd0d46fb334
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-12-26 17:54:15 +0300

    add legacy quasiqoutes support #SCL-7994 fix

---
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/SyntheticMembersInjector.scala
@@ -0,0 +1,32 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
+
+import com.intellij.psi.{PsiElement, PsiMethod}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScTypeDefinition}
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.project.ScalaCompilerSettings
+
+/**
+ * @author Mikhail.Mutcianko
+ *         date 26.12.14
+ */
+object SyntheticMembersInjector {
+
+  def inject(source: ScTypeDefinition): Seq[PsiMethod] = processRules(source)
+
+  private def processRules(source: ScTypeDefinition): Seq[PsiMethod] = {
+    source match {
+      // legacy macro emulation - in 2.10 quasiquotes were implemented by a compiler plugin
+      // so we need to manually add QQ interpolator stub
+      case c:ScClass if c.qualifiedName == "scala.StringContext" && needQQEmulation(c) =>
+        val template = "def q(args: Any*): scala.reflect.runtime.universe.Tree = ???"
+        try {
+          val method = ScalaPsiElementFactory.createMethodWithContext(template, c, c)
+          method.setSynthetic(c)
+          Seq(method)
+        } catch { case  e: Exception => Seq()}
+      case _ => Seq()
+    }
+  }
+
+  private def needQQEmulation(e: PsiElement) = ScalaCompilerSettings.instanceIn(e.getProject).plugins.exists(_.contains("paradise_2.10"))
+}
