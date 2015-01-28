c1e652cc116deaedb7f0975d89a799808113f960
Pavel Fatin <pavel.fatin@jetbrains.com>
2015-01-28 10:19:33 +0100

    JPS: skip resources in SBT & source modules

--- jps-plugin/src/org/jetbrains/jps/incremental/scala/ScalaBuilderService.scala
+++ jps-plugin/src/org/jetbrains/jps/incremental/scala/ScalaBuilderService.scala
@@ -4,12 +4,22 @@ import java.util
 
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.jps.incremental._
+import org.jetbrains.jps.incremental.resources.{StandardResourceBuilderEnabler, ResourcesBuilder}
+import org.jetbrains.jps.incremental.scala.sources.{SbtModuleType, SharedSourcesModuleType}
+import org.jetbrains.jps.model.module.JpsModule
 
 /**
  * Nikolay.Tropin
  * 11/19/13
  */
 class ScalaBuilderService extends BuilderService {
+  ResourcesBuilder.registerEnabler(new StandardResourceBuilderEnabler {
+    def isResourceProcessingEnabled(module: JpsModule): Boolean = {
+      val moduleType = module.getModuleType
+      moduleType != SbtModuleType.INSTANCE && moduleType != SharedSourcesModuleType.INSTANCE
+    }
+  })
+
   @NotNull
   override def createModuleLevelBuilders: util.List[_ <: ModuleLevelBuilder] = {
     util.Arrays.asList[ModuleLevelBuilder](
