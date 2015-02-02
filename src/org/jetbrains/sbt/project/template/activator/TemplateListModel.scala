67bd528fb59513331ead169c106c5e23835a87ef
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-02-02 16:33:20 +0300

    Activator project builder finished

---
+++ src/org/jetbrains/sbt/project/template/activator/TemplateListModel.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.sbt.project.template.activator
+
+import javax.swing.AbstractListModel
+
+/**
+ * User: Dmitry.Naydanov
+ * Date: 21.01.15.
+ */
+class TemplateListModel(val items: Array[(String, ActivatorRepoProcessor.DocData)]) extends AbstractListModel {
+  private val temp = 12
+
+  override def getSize: Int = Math.max(items.length, temp)
+
+  override def getElementAt(i: Int): AnyRef = items(i)._1
+
+  def getAuthorAt(i: Int) = items(i)._2.author
+
+  def getDescriptionAt(i: Int) = items(i)._2.desc
+
+  def getSourceAt(i: Int) = items(i)._2.src
+
+  def getTagsAt(i: Int) = items(i)._2.tags
+}
