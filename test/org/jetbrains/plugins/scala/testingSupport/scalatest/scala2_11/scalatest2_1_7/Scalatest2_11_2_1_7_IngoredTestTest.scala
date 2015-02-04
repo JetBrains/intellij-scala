b44469909947049ee746052a082f95cdccdda016
jamesbrain <marso.des@gmail>
2015-02-04 07:54:34 +0300

    Added tests for some scalaTest versions.

---
+++ test/org/jetbrains/plugins/scala/testingSupport/scalatest/scala2_11/scalatest2_1_7/Scalatest2_11_2_1_7_IngoredTestTest.scala
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.scala.testingSupport.scalatest.scala2_11.scalatest2_1_7
+
+import org.jetbrains.plugins.scala.testingSupport.scalatest.IgnoredSpecTest
+
+/**
+  * @author Roman.Shein
+  * @since 22.01.2015.
+  */
+class Scalatest2_11_2_1_7_IngoredTestTest extends Scalatest2_11_2_1_7_Base with IgnoredSpecTest {
+ }
