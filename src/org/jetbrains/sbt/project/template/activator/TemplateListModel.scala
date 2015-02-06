94ada3b63255ad0de1d8c1857be191b9411f082d
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2015-02-06 15:16:57 +0300

    Fixed source compatibility with 1.7

--- src/org/jetbrains/sbt/project/template/activator/TemplateListModel.scala
+++ src/org/jetbrains/sbt/project/template/activator/TemplateListModel.scala
@@ -1,17 +1,15 @@
 package org.jetbrains.sbt.project.template.activator
 
-import javax.swing.AbstractListModel
-
 /**
  * User: Dmitry.Naydanov
  * Date: 21.01.15.
  */
-class TemplateListModel(val items: Array[(String, ActivatorRepoProcessor.DocData)]) extends AbstractListModel {
+class TemplateListModel(val items: Array[(String, ActivatorRepoProcessor.DocData)]) extends JavaAbstractListModel {
   private val temp = 12
 
   override def getSize: Int = Math.max(items.length, temp)
 
-  override def getElementAt(i: Int): AnyRef = items(i)._1
+  override def getElementAtAdapter(i: Int): AnyRef = items(i)._1
 
   def getAuthorAt(i: Int) = items(i)._2.author
 
