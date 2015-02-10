35910c26e73e20ab07952541654538299f9ff29d
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2015-02-10 20:11:13 +0300

    Add default parameters for syntax sugar of implicit classes. #SCL-8280 Fixed

--- test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
+++ test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
@@ -651,6 +651,8 @@ class TypeInferenceBugs5Test extends TypeInferenceTestBase {
 
   def testSCL8191() {doTest()}
 
+  def testSCL8280(): Unit = doTest()
+
   def testSOE() {doTest()}
 
   def testSOE2() {doTest()}
