8b8dff6f4f9637f3b54ba8c2aa6476d1029cb70a
jamesbrain <marso.des@gmail>
2015-02-11 18:15:56 +0300

    Added tests for 'run whole test suite' test case Removed redundant tests for scalaTest with dynamic classpath

---
+++ test/org/jetbrains/plugins/scala/testingSupport/scalatest/scala2_10/scalatest2_1_7/Scalatest2_10_2_1_7_WholeSuiteTest.scala
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.scala.testingSupport.scalatest.scala2_10.scalatest2_1_7
+
+import org.jetbrains.plugins.scala.testingSupport.scalatest.ScalaTestWholeSuiteTest
+
+/**
+ * @author Roman.Shein
+ * @since 11.02.2015.
+ */
+class Scalatest2_10_2_1_7_WholeSuiteTest extends Scalatest2_10_2_1_7_Base with ScalaTestWholeSuiteTest{
+}
---
+++ test/org/jetbrains/plugins/scala/testingSupport/scalatest/scala2_10/scalatest2_2_1/Scalatest2_10_2_2_1_WholeSuiteTest.scala
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.scala.testingSupport.scalatest.scala2_10.scalatest2_2_1
+
+import org.jetbrains.plugins.scala.testingSupport.scalatest.ScalaTestWholeSuiteTest
+
+/**
+ * @author Roman.Shein
+ * @since 11.02.2015.
+ */
+class Scalatest2_10_2_2_1_WholeSuiteTest extends Scalatest2_10_2_2_1_Base with ScalaTestWholeSuiteTest {
+}
