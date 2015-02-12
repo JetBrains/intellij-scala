3f21d4165aaab75a42f28d416d8075c6f3aa1768
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-02-12 14:38:58 +0300

    idea host skeleton

---
+++ src/org/jetbrains/plugins/scala/metahost/trees/TreeAdapter.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.metahost.trees
+
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
+
+import scala.meta._
+
+object TreeAdapter {
+  def ideaToMeta(tree: ScalaPsiElement): Tree = {
+    tree match {
+
+
+      case _ =>
+    }
+    ???
+  }
+}
