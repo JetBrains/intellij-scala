ee84479dcc18d9faa5f48da09641bcd8272a8324
jamesbrain <marso.des@gmail>
2015-02-12 18:47:23 +0300

    Fixed uTest runner(now works with ID-based console view too). Slightly altered uTest test tree presentation. #SCL-8201 - fixed

--- src/org/jetbrains/plugins/scala/testingSupport/test/utest/UTestConfigurationProducer.scala
+++ src/org/jetbrains/plugins/scala/testingSupport/test/utest/UTestConfigurationProducer.scala
@@ -55,15 +55,15 @@ class UTestConfigurationProducer extends {
       return TestConfigurationUtil.packageSettings(element, location, confFactory, ScalaBundle.message("test.in.scope.utest.presentable.text", name))
     }
 
-    val (testClassPath, testClassName) = getLocationClassAndTest(location)
+    val (testClassPath, testName) = getLocationClassAndTest(location)
     if (testClassPath == null) return null
     val settings = RunManager.getInstance(location.getProject).
             createRunConfiguration(StringUtil.getShortName(testClassPath) +
-            (if (testClassName != null) "\\" + testClassName else ""), confFactory)
+            (if (testName != null) "\\" + testName else ""), confFactory)
     val runConfiguration = settings.getConfiguration.asInstanceOf[UTestRunConfiguration]
     runConfiguration.setTestClassPath(testClassPath)
-    if (testClassName != null) runConfiguration.setTestName(testClassName)
-    val kind = if (testClassName == null) TestKind.CLASS else TestKind.TEST_NAME
+    if (testName != null) runConfiguration.setTestName(testName)
+    val kind = if (testName == null) TestKind.CLASS else TestKind.TEST_NAME
     runConfiguration.setTestKind(kind)
     try {
       val module = ScalaPsiUtil.getModule(element)
