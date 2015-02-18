29ff615b50c955499591d4963b682dab23c897fe
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-02-18 15:59:44 +0300

    split tests, add defn test

---
+++ test/org/jetbrains/plugins/scala/meta/TreeConverterDefnTest.scala
@@ -0,0 +1,52 @@
+package org.jetbrains.plugins.scala.meta
+
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+
+import scala.meta.internal.ast._
+
+class TreeConverterDefnTest extends SimpleTestCase with TreeConverterTestBase {
+
+  def testValInit() {
+    doTest(
+      "val x = 2",
+      Defn.Val(Nil, Pat.Var.Term(Term.Name("x")) :: Nil, None, Lit.Int(2))
+    )
+  }
+
+  def testVarInit(): Unit = {
+    doTest(
+      "var x = 2",
+      Defn.Var(Nil, Pat.Var.Term(Term.Name("x")) :: Nil, None, Some(Lit.Int(2)))
+    )
+  }
+
+  def testMultiValInit(): Unit = {
+    doTest(
+      "val x, y = 2",
+      Defn.Val(Nil, Pat.Var.Term(Term.Name("x")) :: Pat.Var.Term(Term.Name("y")) :: Nil,
+        None, Lit.Int(2))
+    )
+  }
+
+  def testTypedValInit(): Unit = {
+    doTest(
+      "val x: Int = 2",
+      Defn.Val(Nil, Pat.Var.Term(Term.Name("x")) :: Nil, Some(Type.Name("Int")), Lit.Int(2))
+    )
+  }
+
+  def testEscapedNameVal(): Unit = {
+    doTest(
+      "val `x`: Int = 2",
+      Defn.Val(Nil, Pat.Var.Term(Term.Name("x")) :: Nil, Some(Type.Name("Int")), Lit.Int(2))
+    )
+  }
+
+  def testInitializeVarEmpty(): Unit = {
+    doTest(
+      "var x: Int = _",
+      Defn.Var(Nil, Pat.Var.Term(Term.Name("x")) :: Nil, Some(Type.Name("Int")), None)
+    )
+  }
+
+}
