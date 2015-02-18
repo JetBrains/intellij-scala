51e729455d034ae0a5d09b448efecd0951923ab7
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-02-18 15:59:44 +0300

    split tests, add defn test

--- test/org/jetbrains/plugins/scala/meta/TreeConverterDeclTest.scala
+++ test/org/jetbrains/plugins/scala/meta/TreeConverterDeclTest.scala
@@ -1,47 +1,11 @@
 package org.jetbrains.plugins.scala.meta
 
-import com.intellij.psi.{PsiFileFactory, PsiManager}
-import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.base.SimpleTestCase
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import org.jetbrains.plugins.scala.meta.trees.TreeAdapter
 
-import scala.::
-import scala.collection.immutable.::
-import scala.meta.Dialect
 import scala.meta.internal.ast._
 
 
-class TreeConverterTest extends SimpleTestCase {
-
-  implicit def psiFromText(text: String): ScalaPsiElement = {
-    val manager = PsiManager.getInstance(fixture.getProject)
-    val dummyFile: ScalaFile = PsiFileFactory.getInstance(manager.getProject).
-      createFileFromText("DUMMY" + ScalaFileType.SCALA_FILE_TYPE.getDefaultExtension,
-        ScalaFileType.SCALA_FILE_TYPE, text).asInstanceOf[ScalaFile]
-    dummyFile.getFirstChild.asInstanceOf[ScalaPsiElement]
-  }
-
-  private def structuralEquals(tree1: Tree, tree2: Tree): Boolean = {
-    // NOTE: for an exhaustive list of tree field types see
-    // see /foundation/src/main/scala/org/scalameta/ast/internal.scala
-    def loop(x1: Any, x2: Any): Boolean = (x1, x2) match {
-      case (x1: Tree, x2: Tree) => structuralEquals(x1, x2)
-      case (Some(x1), Some(x2)) => loop(x1, x2)
-      case (Seq(xs1 @ _*), Seq(xs2 @ _*)) => xs1.zip(xs2).forall{ case (x1, x2) => loop(x1, x2) }
-      case (x1, x2) => x1 == x2
-    }
-    def tagsEqual = tree1.$tag == tree2.$tag
-    def fieldsEqual = tree1.productIterator.toList.zip(tree2.productIterator.toList).forall{ case (x1, x2) => loop(x1, x2) }
-    tagsEqual && fieldsEqual
-  }
-
-  def doTest(text: String, tree: Tree) = {
-    val converted = TreeAdapter.ideaToMeta(text)
-    assert(structuralEquals(converted, tree), s"$converted <=> $tree")
-    assert(converted.toString() ==  tree.toString(), s"TEXT: $converted <=> $tree")
-  }
+class TreeConverterDeclTest extends SimpleTestCase with TreeConverterTestBase {
 
   def testVal() {
     doTest(
@@ -60,7 +24,7 @@ class TreeConverterTest extends SimpleTestCase {
 
   def testMultiVal() {
     doTest(
-    "val x, y: Int",
+      "val x, y: Int",
       Decl.Val(Nil, List(Pat.Var.Term(Term.Name("x")), Pat.Var.Term(Term.Name("y"))), Type.Name("Int"))
     )
   }
@@ -81,7 +45,7 @@ class TreeConverterTest extends SimpleTestCase {
 
   def testTypeUpperBound() {
     doTest(
-    "type T <: Any",
+      "type T <: Any",
       Decl.Type(Nil, Type.Name("T"), Nil, Type.Bounds(None, Some(Type.Name("Any"))))
     )
   }
@@ -187,7 +151,7 @@ class TreeConverterTest extends SimpleTestCase {
         Type.Name("Unit"))
     )
   }
-  
+
   def testDefFunctionalTypeParam() {
     doTest(
       "def f(a: Int => Any)",
@@ -205,7 +169,7 @@ class TreeConverterTest extends SimpleTestCase {
           Some(Type.Function(List(Type.Name("Int"), Type.Name("Any")), Type.Name("Any"))), None))), Type.Name("Unit"))
     )
   }
-  
+
   def testDefVararg() {
     doTest(
       "def f (a: Int*)",
---
+++ test/org/jetbrains/plugins/scala/meta/TreeConverterTestBase.scala
@@ -0,0 +1,39 @@
+package org.jetbrains.plugins.scala.meta
+import com.intellij.psi.{PsiFileFactory, PsiManager}
+import org.jetbrains.plugins.scala.ScalaFileType
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.meta.trees.TreeAdapter
+
+import scala.meta.internal.ast.Tree
+
+trait TreeConverterTestBase {
+
+  implicit def psiFromText(text: String): ScalaPsiElement = {
+    val manager = PsiManager.getInstance(fixture.getProject)
+    val dummyFile: ScalaFile = PsiFileFactory.getInstance(manager.getProject).
+      createFileFromText("DUMMY" + ScalaFileType.SCALA_FILE_TYPE.getDefaultExtension,
+        ScalaFileType.SCALA_FILE_TYPE, text).asInstanceOf[ScalaFile]
+    dummyFile.getFirstChild.asInstanceOf[ScalaPsiElement]
+  }
+
+  def structuralEquals(tree1: Tree, tree2: Tree): Boolean = {
+    // NOTE: for an exhaustive list of tree field types see
+    // see /foundation/src/main/scala/org/scalameta/ast/internal.scala
+    def loop(x1: Any, x2: Any): Boolean = (x1, x2) match {
+      case (x1: Tree, x2: Tree) => structuralEquals(x1, x2)
+      case (Some(x1), Some(x2)) => loop(x1, x2)
+      case (Seq(xs1@_*), Seq(xs2@_*)) => xs1.zip(xs2).forall { case (x1, x2) => loop(x1, x2)}
+      case (x1, x2) => x1 == x2
+    }
+    def tagsEqual = tree1.$tag == tree2.$tag
+    def fieldsEqual = tree1.productIterator.toList.zip(tree2.productIterator.toList).forall { case (x1, x2) => loop(x1, x2)}
+    tagsEqual && fieldsEqual
+  }
+
+  def doTest(text: String, tree: Tree) = {
+    val converted = TreeAdapter.ideaToMeta(text)
+    assert(structuralEquals(converted, tree), s"$converted <=> $tree")
+    assert(converted.toString() == tree.toString(), s"TEXT: $converted <=> $tree")
+  }
+}
