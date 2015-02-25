d379aac6278431b717477d84cf69cb038f798a16
niktrop <Nikolay.Tropin@jetbrains.com>
2015-02-25 16:59:12 +0300

    use single directory to compiled compiler interfaces, don't delete old ones on compiler interface sources change

--- compiler-settings/src/org/jetbrains/jps/incremental/scala/data/SbtData.scala
+++ compiler-settings/src/org/jetbrains/jps/incremental/scala/data/SbtData.scala
@@ -14,7 +14,9 @@ case class SbtData(interfaceJar: File,
                    javaClassVersion: String)
 
 object SbtData {
-  def from(classLoader: ClassLoader, pluginRoot: File, systemRoot: File, javaClassVersion: String): Either[String, SbtData] = {
+  val dataRoot = new File(System.getProperty("user.home"), ".idea-build")
+
+  def from(classLoader: ClassLoader, pluginRoot: File, javaClassVersion: String): Either[String, SbtData] = {
     Either.cond(pluginRoot.exists, pluginRoot,
       "SBT home directory does not exist: " + pluginRoot).flatMap { sbtHome =>
 
@@ -34,58 +36,14 @@ object SbtData {
               .toRight("Unable to read SBT version from JVM classpath")
               .map { sbtVersion =>
 
-              val interfacesHome = new File(new File(systemRoot, "scala-compiler-interfaces"), sbtVersion + "-idea")
-
-              val sourcesTimestamp = new File(interfacesHome, "sourcesTimestamp.dat")
               val currentTimestamp = sourceJar.lastModified()
-              if (readLong(sourcesTimestamp) != Some(currentTimestamp)) {
-                clean(interfacesHome)
-                writeLongTo(sourcesTimestamp, currentTimestamp)
-              }
+              val interfacesHome = new File(new File(dataRoot, "scala-compiler-interfaces"), sbtVersion + "-idea-" + currentTimestamp)
 
               new SbtData(interfaceJar, sourceJar, interfacesHome, javaClassVersion)
             }
           }
         }
       }
-    }
-  }
-
-  private def readLong(file: File): Option[Long] = {
-    if (!file.exists()) None
-    else {
-      using(new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) { in =>
-        try {
-          Some(in.readLong())
-        } catch {
-          case _: IOException | _: IllegalArgumentException | _: NullPointerException => None
-        }
-      }
-    }
-  }
-
-  private def writeLongTo(file: File, long: Long): Unit = {
-    try {
-      if (!file.exists()) file.createNewFile()
-
-      using(new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-        _.writeLong(long)
-      }
-    }
-    catch {
-      case _: IOException =>
-    }
-  }
-
-  private def clean(dir: File): Unit = {
-    try {
-      if (!dir.exists()) return
-      val files = dir.listFiles()
-      if (files != null)
-        files.foreach(_.delete())
-    }
-    catch {
-      case _: IOException =>
     }
   }
 
