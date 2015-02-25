1062eace9507284607c0f61a8d24be7d08762d1e
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-02-25 15:41:05 +0300

    Play2: Sbt watcher shouldn't be visible for non-imported or non-play projects

--- src/org/jetbrains/plugins/scala/compiler/ServerWidgetEP.java
+++ src/org/jetbrains/plugins/scala/compiler/ServerWidgetEP.java
@@ -2,6 +2,7 @@ package org.jetbrains.plugins.scala.compiler;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.project.Project;
 
 /**
  * User: Dmitry.Naydanov
@@ -14,5 +15,5 @@ public abstract class ServerWidgetEP {
     return EP_NAME.getExtensions();
   }
 
-  public abstract AnAction[] getAdditionalActions();
+  public abstract AnAction[] getAdditionalActions(Project project);
 }
