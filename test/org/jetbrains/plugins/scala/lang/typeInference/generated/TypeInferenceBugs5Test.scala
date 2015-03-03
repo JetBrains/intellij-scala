06a96534e38980d799faa9c5ac8366da54061d82
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2015-03-03 18:51:20 +0300

    Fixed implicit conversions for tupling to Unit #SCL-8317 Fixed

--- test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
+++ test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
@@ -659,6 +659,8 @@ class TypeInferenceBugs5Test extends TypeInferenceTestBase {
 
   def testSCL8282(): Unit = doTest()
 
+  def testSCL8317(): Unit = doTest()
+
   def testSCL8359(): Unit = doTest()
 
   def testSOE() {doTest()}
