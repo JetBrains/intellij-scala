c21987621da3ba53722aa9d76677d9180f2be37b
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-03-27 13:20:32 +0300

    Implement another way of using our autoimport instead of built-in one
    Override setUseAutoImport and isUseAutoImport in SbtProjectSettings.
    Revert "Add useOurOwnAutoImport setting instead of IDEA's one"

--- src/org/jetbrains/sbt/project/settings/SbtProjectSettingsControl.scala
+++ src/org/jetbrains/sbt/project/settings/SbtProjectSettingsControl.scala
@@ -47,16 +47,7 @@ class SbtProjectSettingsControl(context: Context, initialSettings: SbtProjectSet
 
   private val resolveSbtClassifiersCheckBox = new JCheckBox(SbtBundle("sbt.settings.resolveSbtClassifiers"))
 
-  private val useOurOwnAutoImportCheckBox = new JCheckBox(ExternalSystemBundle.message("settings.label.use.auto.import"))
-
-  private var oldAutoImportBox: Option[Component] = None
-
   def fillExtraControls(@NotNull content: PaintAwarePanel, indentLevel: Int) {
-    oldAutoImportBox = Some(content.getComponent(content.getComponentCount-2))
-    content.add(useOurOwnAutoImportCheckBox,
-      ExternalSystemUiUtil.getFillLineConstraints(indentLevel),
-      content.getComponentCount-2)
-
     val label = new JLabel("Project \u001BSDK:")
     label.setLabelFor(jdkComboBox)
 
@@ -77,16 +68,12 @@ class SbtProjectSettingsControl(context: Context, initialSettings: SbtProjectSet
     val settings = getInitialSettings
 
     selectedJdkName != settings.jdkName ||
-      useOurOwnAutoImportCheckBox.isSelected != settings.useOurOwnAutoImport ||
       resolveClassifiersCheckBox.isSelected != settings.resolveClassifiers ||
       resolveSbtClassifiersCheckBox.isSelected != settings.resolveClassifiers
   }
 
   protected def resetExtraSettings(isDefaultModuleCreation: Boolean) {
     val settings = getInitialSettings
-
-    oldAutoImportBox.foreach(_.setVisible(false))
-    useOurOwnAutoImportCheckBox.setSelected(settings.useOurOwnAutoImport)
 
     val jdk = settings.jdkName.flatMap(name => Option(ProjectJdkTable.getInstance.findJdk(name)))
     jdkComboBox.setSelectedJdk(jdk.orNull)
@@ -100,8 +87,6 @@ class SbtProjectSettingsControl(context: Context, initialSettings: SbtProjectSet
   }
 
   protected def applyExtraSettings(settings: SbtProjectSettings) {
-    settings.setUseAutoImport(false)
-    settings.useOurOwnAutoImport = useOurOwnAutoImportCheckBox.isSelected
     settings.jdk = selectedJdkName.orNull
     settings.resolveClassifiers = resolveClassifiersCheckBox.isSelected
     settings.resolveSbtClassifiers = resolveSbtClassifiersCheckBox.isSelected
