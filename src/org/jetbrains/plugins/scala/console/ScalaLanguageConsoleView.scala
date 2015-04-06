6e57a97220bdc2af5f8189dfeee8cfbf20f4dc7c
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-04-06 16:46:27 +0300

    'Execute Statement in Console' and 'Send Selection to Console' fixed

--- src/org/jetbrains/plugins/scala/console/ScalaLanguageConsoleView.scala
+++ src/org/jetbrains/plugins/scala/console/ScalaLanguageConsoleView.scala
@@ -1,9 +1,12 @@
 package org.jetbrains.plugins.scala.console
 
+import com.intellij.execution.console.ConsoleRootType
+
 /**
  * @author Alexander Podkhalyuzin
  * @since 14.04.2010
  */
 object ScalaLanguageConsoleView {
   val SCALA_CONSOLE = "Scala Console"
+  val SCALA_CONSOLE_ROOT_TYPE = new ConsoleRootType("scala", SCALA_CONSOLE) {}
 }
