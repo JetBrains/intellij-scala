af18a12b461b6d9674bab93ba861ee45d0097ee0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-04-23 18:24:24 +0300

    add scalameta interpreter dependency, update artifact

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -39,6 +39,7 @@ object Dependencies {
   val scalariform_2_11: ModuleID = "org.scalariform" % "scalariform_2.11" % "0.1.7"
   val macroParadise: ModuleID = "org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full
   val scalaMetaCore: ModuleID = "org.scalameta" % "scalameta_2.11" % "0.1.0-SNAPSHOT"
+  val scalaMetaInterpreter: ModuleID = "org.scalameta" %% "interpreter" % "0.1.0-SNAPSHOT"
 
   val nailgun: ModuleID = "org.jetbrains" % "nailgun-patched" % "1.0.0"
   val compilerInterfaceSources: ModuleID = "org.jetbrains" % "compiler-interface-sources" % "1.0.0"
@@ -67,6 +68,7 @@ object DependencyGroups {
     scalaReflect,
     scalaXml,
     scalaMetaCore,
+    scalaMetaInterpreter,
     scalaParserCombinators,
     sbtStructureCore,
     evoInflector,
