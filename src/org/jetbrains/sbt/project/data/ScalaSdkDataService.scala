b6e5c1b3baac455b011865f28d032b93a08a659c
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-06-09 19:02:26 +0300

    Refactor ScalaSdkDataService and ScalaGradleDataService
    (cherry picked from commit 3e5c66c5e2d724f3be28b9d4a56a4e06eb4d9c0b)

--- src/org/jetbrains/sbt/project/data/ScalaSdkDataService.scala
+++ src/org/jetbrains/sbt/project/data/ScalaSdkDataService.scala
@@ -1,8 +1,10 @@
 package org.jetbrains.sbt
 package project.data
 
+import java.io.File
 import java.util
 import com.intellij.openapi.externalSystem.model.{ProjectKeys, ExternalSystemException, DataNode}
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.openapi.externalSystem.service.project.{ProjectStructureHelper, PlatformFacade}
@@ -12,48 +14,38 @@ import collection.JavaConverters._
 /**
  * @author Pavel Fatin
  */
-class ScalaSdkDataService(platformFacade: PlatformFacade, helper: ProjectStructureHelper)
-  extends AbstractDataService[ScalaSdkData, Library](ScalaSdkData.Key) {
+class ScalaSdkDataService(val helper: ProjectStructureHelper)
+  extends AbstractDataService[ScalaSdkData, Library](ScalaSdkData.Key)
+  with SafeProjectStructureHelper {
 
-  def doImportData(toImport: util.Collection[DataNode[ScalaSdkData]], project: Project) {
+  def doImportData(toImport: util.Collection[DataNode[ScalaSdkData]], project: Project) =
     toImport.asScala.foreach(doImport(_, project))
-  }
 
-  private def doImport(sdkNode: DataNode[ScalaSdkData], project: Project) {
-    val sdkData = sdkNode.getData
-
-    val module = {
-      val moduleData = sdkNode.getData(ProjectKeys.MODULE)
-      helper.findIdeModule(moduleData.getExternalName, project)
-    }
-
-    if (module == null)
-      return
-
-    module.configureScalaCompilerSettingsFrom("SBT", sdkData.compilerOptions)
-
-    val compilerVersion = sdkData.scalaVersion
-
-    val scalaLibraries = project.libraries.filter(_.getName.contains("scala-library"))
-
-    if (scalaLibraries.isEmpty)
-      return
-
-    // TODO Why SBT's scala-libary module version sometimes differs from SBT's declared scalaVersion?
-    val scalaLibrary = scalaLibraries
-            .find(_.scalaVersion == Some(compilerVersion))
-            .orElse(scalaLibraries.find(_.scalaVersion.exists(_.toLanguageLevel == compilerVersion.toLanguageLevel)))
-            .getOrElse(throw new ExternalSystemException("Cannot find project Scala library " +
-                         compilerVersion.number + " for module " + sdkNode.getData(ProjectKeys.MODULE).getExternalName))
-
-    if (!scalaLibrary.isScalaSdk) {
-      val languageLevel = compilerVersion.toLanguageLevel.getOrElse(ScalaLanguageLevel.Default)
-      val compilerClasspath = sdkData.compilerClasspath
-      scalaLibrary.convertToScalaSdkWith(languageLevel, compilerClasspath)
+  private def doImport(sdkNode: DataNode[ScalaSdkData], project: Project): Unit = {
+    for {
+      module <- getIdeModuleByNode(sdkNode, project)
+      sdkData = sdkNode.getData
+      compilerOptions = sdkData.compilerOptions
+      compilerClasspath = sdkData.compilerClasspath
+      compilerVersion = sdkData.scalaVersion
+    } {
+      module.configureScalaCompilerSettingsFrom("SBT", compilerOptions)
+      configureScalaSdk(module, project.scalaLibraries, compilerVersion, compilerClasspath)
     }
   }
 
-  def doRemoveData(toRemove: util.Collection[_ <: Library], project: Project) {
+  private def configureScalaSdk(module: Module, scalaLibraries: Seq[Library], compilerVersion: Version, compilerClasspath: Seq[File]): Unit =
+    if (scalaLibraries.nonEmpty) {
+      // TODO Why SBT's scala-libary module version sometimes differs from SBT's declared scalaVersion?
+      val scalaLibrary = scalaLibraries
+              .find(_.scalaVersion == Some(compilerVersion))
+              .orElse(scalaLibraries.find(_.scalaVersion.exists(_.toLanguageLevel == compilerVersion.toLanguageLevel)))
+              .getOrElse(throw new ExternalSystemException("Cannot find project Scala library " +
+                           compilerVersion.number + " for module " + module.getName))
 
-  }
+      if (!scalaLibrary.isScalaSdk)
+        scalaLibrary.convertToScalaSdkWith(scalaLibrary.scalaLanguageLevel.getOrElse(ScalaLanguageLevel.Default), compilerClasspath)
+    }
+
+  def doRemoveData(toRemove: util.Collection[_ <: Library], project: Project) {}
 }
