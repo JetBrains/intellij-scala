b4d1867652b56fe56dee7a647126137c77c81b21
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-06-10 15:05:58 +0300

    method like denotations generation

--- src/org/jetbrains/plugins/scala/meta/trees/SymbolTable.scala
+++ src/org/jetbrains/plugins/scala/meta/trees/SymbolTable.scala
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.scala.meta.trees
 import java.util.UUID._
 
 import com.intellij.psi.{PsiPackage, PsiElement, PsiNamedElement}
+import org.jetbrains.plugins.scala.debugger.evaluation.util.DebuggerUtil
 
 import scala.{Seq => _}
 import scala.collection.immutable.Seq
@@ -15,23 +16,27 @@ trait SymbolTable {
   self: Converter =>
 
 
-  def isGlobal(elem: PsiElement): Boolean = {
+  def isLocal(elem: PsiElement): Boolean = {
     elem match {
-      case e: p.toplevel.typedef.ScTypeDefinition => ! e.isLocal
-      case _ => true
+      case e: p.toplevel.typedef.ScTypeDefinition => e.isLocal
+      case _ => false
     }
   }
 
   def toSymbol(elem: PsiElement): h.Symbol = {
     elem match {
-      case _ if !isGlobal(elem) => h.Symbol.Local(randomUUID().toString)
-      case td: p.toplevel.typedef.ScTypeDefinition if !td.qualifiedName.contains(".") =>
+      case _ if isLocal(elem) => h.Symbol.Local(randomUUID().toString)
+      case td: p.toplevel.typedef.ScTypeDefinition if !td.qualifiedName.contains(".") => // empty package defn
         h.Symbol.Global(h.Symbol.Empty, td.name, h.Signature.Type)
       case td: p.toplevel.typedef.ScTypeDefinition => h.Symbol.Global(toSymbol(td.parent.get), td.name, h.Signature.Type)
+      case td: p.statements.ScFunction =>
+        val jvmsig = DebuggerUtil.getFunctionJVMSignature(td).getName(null)
+        h.Symbol.Global(toSymbol(td.containingClass), td.name, h.Signature.Method(jvmsig))
       case pc: p.toplevel.packaging.ScPackaging => toSymbol(pc.reference.get.bind().get.element)
       case pp: PsiPackage if pp.getName == null => h.Symbol.Root
       case pc: PsiPackage => h.Symbol.Global(toSymbol(pc.getParentPackage), pc.getName, h.Signature.Type)
       case cr: p.base.ScStableCodeReferenceElement => toSymbol(cr.resolve())
+      case _ => ???
     }
   }
 }
