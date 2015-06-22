3f33cf95ac8d8d000778d4766b9229797c4ca1be
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-06-22 17:58:40 +0300

    Activator templates now shown and sorted by title

--- src/org/jetbrains/sbt/project/template/activator/TemplateListModel.scala
+++ src/org/jetbrains/sbt/project/template/activator/TemplateListModel.scala
@@ -5,17 +5,18 @@ package org.jetbrains.sbt.project.template.activator
  * Date: 21.01.15.
  */
 class TemplateListModel(val items: Array[(String, ActivatorRepoProcessor.DocData)]) extends JavaAbstractListModel {
+  private val sortedItems = items.sortBy(a => a._2.title)
   private val temp = 12
 
-  override def getSize: Int = Math.max(items.length, temp)
+  override def getSize: Int = Math.max(sortedItems.length, temp)
 
-  override def getElementAtAdapter(i: Int): AnyRef = items(i)._1
+  override def getElementAtAdapter(i: Int): AnyRef = sortedItems(i)._2.title
 
-  def getAuthorAt(i: Int) = items(i)._2.author
+  def getAuthorAt(i: Int) = sortedItems(i)._2.author
 
-  def getDescriptionAt(i: Int) = items(i)._2.desc
+  def getDescriptionAt(i: Int) = sortedItems(i)._2.desc
 
-  def getSourceAt(i: Int) = items(i)._2.src
+  def getSourceAt(i: Int) = sortedItems(i)._2.src
 
-  def getTagsAt(i: Int) = items(i)._2.tags
+  def getTagsAt(i: Int) = sortedItems(i)._2.tags
 }
