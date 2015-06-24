f9b68563f4133f16e02662338e2a811eab9fc665
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2015-06-24 15:33:57 +0300

    check language level before replacing with option.contains #SCL-8835 fixed (cherry picked from commit d004b05)

--- src/org/jetbrains/plugins/scala/codeInspection/collections/OptionEqualsSomeToContainsInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/collections/OptionEqualsSomeToContainsInspection.scala
@@ -2,6 +2,7 @@ package org.jetbrains.plugins.scala.codeInspection.collections
 
 import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import org.jetbrains.plugins.scala.project.{ProjectPsiElementExt, ScalaLanguageLevel}
 
 /**
  * @author Nikolay.Tropin
@@ -15,6 +16,7 @@ object OptionEqualsSomeToContains extends SimplificationType {
   override def hint: String = InspectionBundle.message("replace.with.contains")
 
   override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case _ if expr.scalaLanguageLevel.exists(_ <= ScalaLanguageLevel.Scala_2_10) => None
     case qual `==` `scalaSome`(elem) if isOption(qual) =>
       Some(replace(expr).withText(s"${qual.getText}.contains(${elem.getText})").highlightAll)
     case `scalaSome`(elem) `==` qual if isOption(qual) =>
