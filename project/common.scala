ee3da9d75c138e86d988f416901039d44a21dc20
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-06-25 18:31:27 +0300

    Refactor `newProject`, remove customSettings field

--- project/common.scala
+++ project/common.scala
@@ -4,7 +4,7 @@ import scala.language.implicitConversions
 import scala.language.postfixOps
 
 object Common {
-  def newProject(projectName: String, basePath: String)(customSettings: Setting[_]*): Project = {
+  def newProject(projectName: String, basePath: String): Project =
     Project(projectName, file(basePath)).settings(
       name := projectName,
       organization := "JetBrains",
@@ -12,11 +12,10 @@ object Common {
       unmanagedSourceDirectories in Compile += baseDirectory.value / "src",
       unmanagedSourceDirectories in Test += baseDirectory.value / "test",
       unmanagedResourceDirectories in Compile += baseDirectory.value / "resources"
-    ).settings(customSettings:_*)
-  }
+    )
 
-  def newProject(projectName: String)(customSettings: Setting[_]*): Project =
-    newProject(projectName, projectName)(customSettings:_*)
+  def newProject(projectName: String): Project =
+    newProject(projectName, projectName)
 
   def unmanagedJarsFrom(directories: String*): Def.Initialize[Task[Classpath]] = Def.task {
     val sdkPathFinder = directories.foldLeft(PathFinder.empty) { (finder, dir) =>
