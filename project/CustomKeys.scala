d0273f0ef7fa41c2cbc2a138cb2c22c09a92b69d
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-07-02 19:17:42 +0300

    Clean up code

--- project/CustomKeys.scala
+++ project/CustomKeys.scala
@@ -3,8 +3,8 @@ import sbt._
 import Packaging.PackageEntry
 
 object CustomKeys {
-  lazy val sdkDirectory = settingKey[File]("Path to SDK directory where unmanagedJars and IDEA are located")
-  lazy val packageStructure = settingKey[Seq[PackageEntry]]("Structure of plugin package")
-  lazy val packagePlugin = taskKey[File]("Package scala plugin locally")
-  lazy val packagePluginZip = taskKey[File]("Package and compress scala plugin locally")
+  lazy val sdkDirectory = SettingKey[File]("sdk-directory", "Path to SDK directory where unmanagedJars and IDEA are located")
+  lazy val packageStructure = SettingKey[Seq[PackageEntry]]("package-structure", "Structure of plugin's package")
+  lazy val packagePlugin = TaskKey[File]("package-plugin", "Package scala plugin locally")
+  lazy val packagePluginZip = TaskKey[File]("package-plugin-zip", "Package and compress scala plugin locally")
 }
