b4879a4d180712e2a9772ea090c537b61a38d47d
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-07-03 12:31:15 +0300

    Refactor packaging again
    Use subprojects instead of custom keys because keys can not be defined twice

--- project/common.scala
+++ project/common.scala
@@ -3,8 +3,6 @@ import Keys._
 import scala.language.implicitConversions
 import scala.language.postfixOps
 
-import CustomKeys._
-
 object Common {
   def newProject(projectName: String, basePath: String): Project =
     Project(projectName, file(basePath)).settings(
@@ -19,9 +17,9 @@ object Common {
   def newProject(projectName: String): Project =
     newProject(projectName, projectName)
 
-  def unmanagedJarsFromSdk(directories: String*): Def.Initialize[Task[Classpath]] = Def.task {
-    val sdkPathFinder = directories.foldLeft(PathFinder.empty) { (finder, dir) =>
-      finder +++ (sdkDirectory.in(ThisBuild).value / dir)
+  def unmanagedJarsFrom(sdkDirectory: File, subdirectories: String*): Classpath = {
+    val sdkPathFinder = subdirectories.foldLeft(PathFinder.empty) { (finder, dir) =>
+      finder +++ (sdkDirectory / dir)
     }
     (sdkPathFinder * globFilter("*.jar")).classpath
   }
