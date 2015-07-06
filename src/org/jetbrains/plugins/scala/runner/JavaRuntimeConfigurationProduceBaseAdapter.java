e811afd45288b1caa22a087f16da5fad86099f12
Roman Shein <marso.des@gmail.com>
2015-07-06 18:22:58 +0300

    Improved performance of right-click in tests Migrated ScalaApplicationConfigurationProducer to newer API

--- src/org/jetbrains/plugins/scala/runner/JavaRuntimeConfigurationProduceBaseAdapter.java
+++ src/org/jetbrains/plugins/scala/runner/JavaRuntimeConfigurationProduceBaseAdapter.java
@@ -1,19 +1,15 @@
 package org.jetbrains.plugins.scala.runner;
 
 import com.intellij.execution.configurations.ConfigurationType;
-import com.intellij.execution.junit.JavaRuntimeConfigurationProducerBase;
+import com.intellij.execution.configurations.ModuleBasedConfiguration;
+import com.intellij.execution.junit.JavaRunConfigurationProducerBase;
 
 /**
  * @author Alefas
  * @since 02.03.12
  */
-public abstract class JavaRuntimeConfigurationProduceBaseAdapter extends JavaRuntimeConfigurationProducerBase {
+public abstract class JavaRuntimeConfigurationProduceBaseAdapter<T extends ModuleBasedConfiguration> extends JavaRunConfigurationProducerBase<T> {
   protected JavaRuntimeConfigurationProduceBaseAdapter(ConfigurationType configurationType) {
     super(configurationType);
-  }
-
-  @Override
-  public int compareTo(Object o) {
-    return -1;
   }
 }
