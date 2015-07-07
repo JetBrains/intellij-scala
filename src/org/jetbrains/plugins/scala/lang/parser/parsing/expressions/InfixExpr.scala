567483059a0c58231fe8cc4570e08bb14156aef1
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2015-07-07 17:47:28 +0300

    Infix expressions can have type args #SCL-7465 Fixed

--- src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/InfixExpr.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/InfixExpr.scala
@@ -7,6 +7,7 @@ package expressions
 import com.intellij.lang.PsiBuilder
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.parser.parsing.builder.ScalaPsiBuilder
+import org.jetbrains.plugins.scala.lang.parser.parsing.types.TypeArgs
 
 
 /**
@@ -16,7 +17,7 @@ import org.jetbrains.plugins.scala.lang.parser.parsing.builder.ScalaPsiBuilder
 
 /*
  * InfixExpr ::= PrefixExpr
- *             | InfixExpr id [nl] InfixExpr
+ *             | InfixExpr id [TypeArgs] [nl] InfixExpr
  */
 
 object InfixExpr {
@@ -64,6 +65,7 @@ object InfixExpr {
       val setMarker = builder.mark
       val opMarker = builder.mark
       builder.advanceLexer() //Ate id
+      TypeArgs.parse(builder, isPattern = false)
       opMarker.done(ScalaElementTypes.REFERENCE_EXPRESSION)
       if (builder.twoNewlinesBeforeCurrentToken) {
         setMarker.rollbackTo()
