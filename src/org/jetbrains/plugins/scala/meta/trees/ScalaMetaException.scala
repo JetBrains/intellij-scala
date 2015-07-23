34b60a7f6cfc83b125738dc3a225dcd3c41e1482
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-07-23 15:28:59 +0300

    fix compilation, remove obsolete references

--- src/org/jetbrains/plugins/scala/meta/trees/ScalaMetaException.scala
+++ src/org/jetbrains/plugins/scala/meta/trees/ScalaMetaException.scala
@@ -8,10 +8,10 @@ class ScalaMetaException(message: String) extends Exception(message)
 
 class ScalaMetaUnreachableException(reason: String = "") extends ScalaMetaException(s"This code should be unreachable${if (reason.nonEmpty) s": $reason"}")
 
-class ScalaMetaUnexpectedPSI(val elem: PsiElement) extends ScalaMetaException(s"Got unexpected ${elem.getClass} at \"${elem.getText}\"")
+class ScalaMetaUnexpectedPSI(val elem: PsiElement) extends ScalaMetaException(s"Got unexpected ${elem.getClass} at ${elem.getText}")
 
-class ScalaMetaUnexpectedTree(val tree: m.Tree) extends ScalaMetaException(s"Got unexpected ${tree.getClass} at \"${tree.toString()}\"")
+class ScalaMetaUnexpectedTree(val tree: m.Tree) extends ScalaMetaException(s"Got unexpected ${tree.getClass} at ${tree.toString()}")
 
-class ScalaMetaResolveError(elem: PsiElement) extends ScalaMetaException(s"Cannot resolve ${elem.getClass} at \"${elem.toString}\"")
+class ScalaMetaResolveError(elem: PsiElement) extends ScalaMetaException(s"Cannot resolve ${elem.getClass} at ${elem.toString}")
 
-class ScalaMetaTypeResultFailure(elem: Option[PsiElement], cause: String) extends ScalaMetaException(s"Cannot calculate type at \"${elem.map(_.getText).getOrElse("UNKNOWN")}\"($cause)")
+class ScalaMetaTypeResultFailure(elem: Option[PsiElement], cause: String) extends ScalaMetaException(s"Cannot calculate type at ${elem.map(_.getText).getOrElse("UNKNOWN")}($cause)")
