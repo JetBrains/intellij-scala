e9bc22e66511325a57dfa6800e8754e1a846dc66
Nikolay Obedin <dancingrobot84@gmail.com>
2015-07-24 15:25:44 +0300

    Add SbtAnnotatorTest

---
+++ test/org/jetbrains/sbt/annotator/SbtAnnotatorTest.scala
@@ -0,0 +1,55 @@
+package org.jetbrains.sbt.annotator
+
+import com.intellij.lang.annotation.Annotator
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.annotator._
+import org.jetbrains.sbt.SbtBundle
+import junit.framework.Assert.assertTrue
+
+
+/**
+ * @author Nikolay Obedin
+ * @since 7/23/15.
+ */
+
+class SbtAnnotatorMock(sbtVersion: String) extends SbtAnnotator {
+  override def getSbtVersion(element: PsiElement): String = sbtVersion
+}
+
+abstract class SbtAnnotatorTestBase extends AnnotatorTestBase {
+  protected def annotator: Annotator
+
+  protected def doTest(messages: Seq[Message]) {
+    val mock = new AnnotatorHolderMock
+    annotator.annotate(loadTestFile(), mock)
+    println(mock.annotations)
+    assertTrue(messages.forall(mock.annotations.contains))
+  }
+}
+
+
+class SbtAnnotatorTest012 extends SbtAnnotatorTestBase {
+  override def annotator = new SbtAnnotatorMock("0.12.4")
+
+  def testSbtAnnotator =
+    doTest(Seq(
+      Error("version := \"SNAPSHOT\"", SbtBundle("sbt.annotation.blankLineRequired", "0.12.4")),
+      Error("lazy val foo = project.in(file(\"foo\"))", SbtBundle("sbt.annotation.sbtFileMustContainOnlyExpressions"))
+    ))
+}
+
+class SbtAnnotatorTest013 extends SbtAnnotatorTestBase {
+  override def annotator = new SbtAnnotatorMock("0.13.1")
+
+  def testSbtAnnotator =
+    doTest(Seq(
+      Error("version := \"SNAPSHOT\"", SbtBundle("sbt.annotation.blankLineRequired", "0.13.1"))
+    ))
+}
+
+class SbtAnnotatorTest0137 extends SbtAnnotatorTestBase {
+  override def annotator = new SbtAnnotatorMock("0.13.7")
+
+  def testSbtAnnotator =
+    doTest(Seq.empty)
+}
---
+++ testdata/annotator/Sbt/SbtAnnotator.sbt
@@ -0,0 +1,4 @@
+name := "someName"
+version := "SNAPSHOT"
+
+lazy val foo = project.in(file("foo"))
