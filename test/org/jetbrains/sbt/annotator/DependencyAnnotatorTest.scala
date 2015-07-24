cec4a4278746b3e7c1dd70c8e1192c84c46dc3e8
Nikolay Obedin <dancingrobot84@gmail.com>
2015-07-24 15:24:56 +0300

    Refactor AnnotatorTestBase and DependencyAnnotatorTest

--- test/org/jetbrains/sbt/annotator/DependencyAnnotatorTest.scala
+++ test/org/jetbrains/sbt/annotator/DependencyAnnotatorTest.scala
@@ -1,9 +1,12 @@
 package org.jetbrains.sbt
 package annotator
 
+import _root_.junit.framework.Assert._
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.util.io.FileUtil
-import org.jetbrains.plugins.scala.annotator.Error
+import org.jetbrains.plugins.scala.annotator.{AnnotatorHolderMock, Message, Error}
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaRecursiveElementVisitor
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.sbt.project.module.SbtModule
 import org.jetbrains.sbt.resolvers.{SbtResolver, SbtResolverIndexesManager}
 
@@ -12,9 +15,19 @@ import org.jetbrains.sbt.resolvers.{SbtResolver, SbtResolverIndexesManager}
  * @author Nikolay Obedin
  * @since 8/4/14.
  */
-class DependencyAnnotatorTest extends AnnotatorTestBase(classOf[SbtDependencyAnnotator]) {
+class DependencyAnnotatorTest extends AnnotatorTestBase {
 
   val testResolver = new SbtResolver(SbtResolver.Kind.Maven, "Test repo", "file:/%s/sbt/resolvers/testRepository" format baseRootPath)
+
+  def testDoNotAnnotateIndexedDep =
+    doTest(Seq.empty)
+
+  def testAnnotateUnresolvedDep = {
+    val msg = SbtBundle("sbt.annotation.unresolvedDependency")
+    doTest(Seq(Error("\"org.jetbrains\"", msg),
+      Error("\"unknown-lib\"", msg),
+      Error("\"0.0.0\"", msg)))
+  }
 
   override def setUp() = {
     super.setUp()
@@ -35,12 +48,18 @@ class DependencyAnnotatorTest extends AnnotatorTestBase(classOf[SbtDependencyAnn
     FileUtil.delete(SbtResolverIndexesManager.DEFAULT_INDEXES_DIR)
   }
 
-  def testDoNotAnnotateIndexedDep =
-    doTest(Seq.empty)
-  def testAnnotateUnresolvedDep = {
-    val msg = SbtBundle("sbt.annotation.unresolvedDependency")
-    doTest(Seq(Error("\"org.jetbrains\"", msg),
-               Error("\"unknown-lib\"", msg),
-               Error("\"0.0.0\"", msg)))
+  private def doTest(messages: Seq[Message]) {
+    val element = loadTestFile()
+    val mock = new AnnotatorHolderMock
+    val annotator = new SbtDependencyAnnotator
+
+    val visitor = new ScalaRecursiveElementVisitor {
+      override def visitLiteral(lit: ScLiteral) {
+        annotator.annotate(lit, mock)
+        super.visitLiteral(lit)
+      }
+    }
+    element.accept(visitor)
+    assertEquals(messages, mock.annotations)
   }
 }
