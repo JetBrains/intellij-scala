18ee5eb70265a4abe53816e3938cc819102cdc76
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-07-25 18:39:35 +0300

    macro expansion ensugaring

--- src/org/jetbrains/plugins/scala/util/DummyAction.scala
+++ src/org/jetbrains/plugins/scala/util/DummyAction.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.util
 
 import java.io.{EOFException, ObjectInputStream, FileInputStream}
+import java.util.regex.Pattern
 
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
 import com.intellij.openapi.application.PathManager
@@ -8,6 +9,8 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.psi.{PsiDocumentManager, PsiFile}
 import org.jetbrains.plugin.scala.util.MacroExpansion
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+
+import scala.annotation.tailrec
 
 
 // Stub for lazy people, who want to test some stuff on a key press
@@ -25,7 +28,31 @@ class DummyAction extends AnAction {
     }
 
     val expansions = deserializeExpansions(e)
+    for (expansion <- expansions) {
+      ensugarExpansion(expansion.body)
+    }
     ""
+  }
+
+  def ensugarExpansion(text: String): String = {
+
+    @tailrec
+    def applyRules(rules: Seq[(String, String)], input: String = text): String = {
+      def pat(p: String) = Pattern.compile(p, Pattern.DOTALL | Pattern.MULTILINE)
+      rules match {
+        case (pattern, replacement)::xs => applyRules(xs, pat(pattern).matcher(input).replaceAll(replacement))
+        case Nil                        => input
+      }
+    }
+
+    val rules = Seq(
+      "\\<init\\>"          -> "this",    // replace constructor names
+      " *\\<[a-z]+\\> *"    -> "",        // remove compiler attributes
+      "super\\.this\\(\\);" -> "this();", // replace super constructor calls
+      "def this\\(\\) = \\{\\s*this\\(\\);\\s*\\(\\)\\s*\\};" -> "" // remove invalid super constructor calls
+    )
+
+    applyRules(rules)
   }
 
   def deserializeExpansions(event: AnActionEvent): Seq[MacroExpansion] = {
