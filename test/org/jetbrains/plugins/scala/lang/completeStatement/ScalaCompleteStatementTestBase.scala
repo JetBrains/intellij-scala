b7f95c7fe664fb18d8f7685cc13750b0e14268c7
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-07-28 15:21:32 +0300

    Complete statement tests refactoring

---
+++ test/org/jetbrains/plugins/scala/lang/completeStatement/ScalaCompleteStatementTestBase.scala
@@ -0,0 +1,34 @@
+package org.jetbrains.plugins.scala.lang.completeStatement
+
+import com.intellij.psi.codeStyle.CodeStyleSettingsManager
+import org.jetbrains.plugins.scala.codeInsight.ScalaCodeInsightTestBase
+
+/**
+ * User: Dmitry.Naydanov
+ * Date: 27.07.15.
+ */
+abstract class ScalaCompleteStatementTestBase extends ScalaCodeInsightTestBase {
+  protected def getDefaultScalaFileName = "dummy.scala"
+
+  protected def getDefaultJavaFileName = "dummy.java"
+
+  protected def configureAndCheckFile(fileText: String, resultText: String, fileName: String) {
+    //We should change this setting in order to be sure EnterProcessor works without 'swap-settings-hack'
+    //@see org.jetbrains.plugins.scala.editor.smartEnter.ScalaSmartEnterProcessor#moveCaretInsideBracesIfAny
+    CodeStyleSettingsManager.getSettings(getProjectAdapter).KEEP_SIMPLE_BLOCKS_IN_ONE_LINE = true
+
+    configureFromFileTextAdapter(fileName, fileText)
+    invokeSmartEnter()
+    checkResultByText(resultText)
+
+    CodeStyleSettingsManager.getSettings(getProjectAdapter).KEEP_SIMPLE_BLOCKS_IN_ONE_LINE = false
+  }
+
+  def checkScalaFileByText(fileText: String, resultText: String) {
+    configureAndCheckFile(fileText, resultText, getDefaultScalaFileName)
+  }
+
+  def checkJavaFileByText(fileText: String, resultText: String) {
+    configureAndCheckFile(fileText, resultText, getDefaultJavaFileName)
+  }
+}
