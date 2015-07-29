b25e9210c636f8f9e10a5e3fb56122c6c1542a37
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2015-07-29 13:30:44 +0300

    getting project component instance fixed (cherry picked from commit 14db828)

--- src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorCache.scala
+++ src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorCache.scala
@@ -5,7 +5,7 @@ import com.intellij.codeInsight.PsiEquivalenceUtil
 import com.intellij.debugger.engine.evaluation.expression.ExpressionEvaluator
 import com.intellij.debugger.impl.{DebuggerManagerAdapter, DebuggerSession}
 import com.intellij.debugger.{DebuggerManagerEx, SourcePosition}
-import com.intellij.openapi.components.{AbstractProjectComponent, ServiceManager}
+import com.intellij.openapi.components.AbstractProjectComponent
 import com.intellij.openapi.project.Project
 import com.intellij.psi.{PsiElement, PsiFile}
 
@@ -36,7 +36,7 @@ class ScalaEvaluatorCache(project: Project) extends AbstractProjectComponent(pro
   def get(position: SourcePosition, element: PsiElement): Option[ExpressionEvaluator] = {
     val file = position.getFile
     val offset = position.getOffset
-    if (cachedStamp.get(file) != Some(file.getModificationStamp)) {
+    if (!cachedStamp.get(file).contains(file.getModificationStamp)) {
       cachedStamp(file) = file.getModificationStamp
       cachedEvaluators.filterKeys(_._1 == file).foreach {
         case (pos, map) =>
@@ -67,5 +67,5 @@ class ScalaEvaluatorCache(project: Project) extends AbstractProjectComponent(pro
 }
 
 object ScalaEvaluatorCache {
-  def getInstance(project: Project) = ServiceManager.getService(project, classOf[ScalaEvaluatorCache])
+  def getInstance(project: Project) = project.getComponent(classOf[ScalaEvaluatorCache])
 }
