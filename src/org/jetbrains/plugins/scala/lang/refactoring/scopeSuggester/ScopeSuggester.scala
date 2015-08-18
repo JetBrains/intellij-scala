437b39e23984811f60c77c2e3ef1b24a4b299892
Kate Ustyuzhanina <katejimme@gmail.com>
2015-08-18 17:35:20 +0300

    replace occurrences available from companion object

--- src/org/jetbrains/plugins/scala/lang/refactoring/scopeSuggester/ScopeSuggester.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/scopeSuggester/ScopeSuggester.scala
@@ -4,18 +4,17 @@ package refactoring
 package scopeSuggester
 
 import java.util
+
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.{PsiFile, PsiElement}
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.{PsiElement, PsiFile}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBody
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTemplateDefinition, ScClass, ScObject}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScObject, ScTemplateDefinition, ScTrait}
 import org.jetbrains.plugins.scala.lang.refactoring.namesSuggester.NameSuggester
-import org.jetbrains.plugins.scala.lang.refactoring.util.{ConflictsReporter, ScalaTypeValidator, ScalaValidator, ScalaRefactoringUtil}
-
-import scala.collection.mutable.ArrayBuffer
+import org.jetbrains.plugins.scala.lang.refactoring.util.{ConflictsReporter, ScalaRefactoringUtil, ScalaTypeValidator, ScalaValidator}
 
 
 /**
@@ -40,6 +39,7 @@ object ScopeSuggester {
 
     var result: List[ScopeItem] = List()
     while (parent != null) {
+      var occInCompanionObj: Array[ScTypeElement] = Array[ScTypeElement]()
       val name = parent match {
         case fileType: ScalaFile => "file " + fileType.getName
         case _ =>
@@ -47,7 +47,10 @@ object ScopeSuggester {
             case classType: ScClass =>
               "class " + classType.getName
             case objectType: ScObject =>
+              occInCompanionObj = getOccurrencesFromCompanionObject(curerntElement, objectType)
               "object " + objectType.getName.substring(0, objectType.getName.length - 1)
+            case traitType: ScTrait =>
+              "trait " + traitType.getName
           }
       }
 
@@ -55,18 +58,42 @@ object ScopeSuggester {
       val validator = ScalaTypeValidator(conflictsReporter, project, editor, file, curerntElement, parent, occurrences.isEmpty)
       val possibleNames = NameSuggester.namesByType(curerntElement.calcType)(validator)
 
-      result = result :+ new ScopeItem(name, parent, occurrences, validator, possibleNames.toList.reverse.toArray)
+      result = result :+ new ScopeItem(name, parent, occurrences, occInCompanionObj, validator, possibleNames.toList.reverse.toArray)
       parent = getParent(parent, isScriptFile)
     }
     import scala.collection.JavaConversions.asJavaCollection
     new util.ArrayList[ScopeItem](result.toIterable)
   }
+
+  private def getOccurrencesFromCompanionObject(typeElement: ScTypeElement,
+                                                objectType: ScObject): Array[ScTypeElement] = {
+    val parent: PsiElement = objectType.getParent
+    val name = objectType.getName.substring(0, objectType.getName.length - 1)
+    val companion = parent.getChildren.find({
+      case classType: ScClass if classType.getName == name =>
+        true
+      case traitType: ScTrait if traitType.getName == name =>
+        true
+      case _ => false
+    })
+
+    if (companion.isDefined)
+      ScalaRefactoringUtil.getTypeElementOccurrences(typeElement, companion.get)
+    else
+      Array[ScTypeElement]()
+  }
 }
 
-class ScopeItem(name: String, encloser: PsiElement, inOccurrences: Array[ScTypeElement], inValidator: ScalaValidator, inAvailablenames: Array[String]) {
+class ScopeItem(name: String,
+                encloser: PsiElement,
+                inOccurrences: Array[ScTypeElement],
+                inOccInCompanionObj: Array[ScTypeElement],
+                inValidator: ScalaValidator,
+                inAvailablenames: Array[String]) {
   val fileEncloser: PsiElement = encloser
   val scopeName: String = name
   val occurrences: Array[ScTypeElement] = inOccurrences
+  val occInCompanionObj: Array[ScTypeElement] = inOccInCompanionObj
   val validator: ScalaValidator = inValidator
   val possibleNames: Array[String] = inAvailablenames
 
