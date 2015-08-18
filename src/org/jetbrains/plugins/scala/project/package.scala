0167a8b4064a56894f87bdd4ad38ed26a0de3e45
Nikolay Obedin <nikolay.obedin@jetbrains.com>
2015-08-18 18:31:13 +0300

    Fix Scala SDK detection failure
    Abstract:
        Due to eviction, sometimes module can have scala-library version different
        to the one in `scalaInstance`. When both versions of scala-library present
        in project libraries, such module may have Scala SDK unset.
    
    Example:
        Say module A has scala-library version 2.11.2 in `scalaInstance`, but, due
        to eviction, it depends on scala-library 2.11.6. There is also module
        B which depends on 2.11.2. When Scala SDK is being detected, we scan through
        project libraries searching for scala-library, then compare its version to
        the scalaInstance's one, then compare language levels, then fail. This way
        module B will get its Scala SDK alright because it depends on 2.11.2.
        However, module A won't get it. We find 2.11.2 first and discard 2.11.6,
        because 2.11.2 match scalaInstance version. This leaves module A with 2.11.6
        which is not being transformed into Scala SDK.
    
    Solution:
        Scan through module's library dependencies instead of all project libraries.

--- src/org/jetbrains/plugins/scala/project/package.scala
+++ src/org/jetbrains/plugins/scala/project/package.scala
@@ -1,21 +1,24 @@
 package org.jetbrains.plugins.scala
 
 import java.io.File
+
+import com.intellij.openapi.module.{Module, ModuleManager, ModuleUtilCore}
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots._
+import com.intellij.openapi.roots.impl.libraries.{LibraryEx, ProjectLibraryTable}
+import com.intellij.openapi.roots.libraries.Library
+import com.intellij.openapi.roots.ui.configuration.libraryEditor.{ExistingLibraryEditor, NewLibraryEditor}
+import com.intellij.openapi.vfs.{VfsUtil, VfsUtilCore}
+import com.intellij.psi.{PsiElement, PsiFile}
+import com.intellij.util.CommonProcessors.CollectProcessor
 import com.intellij.util.Processor
-import org.jetbrains.plugins.scala.project.settings.{ScalaCompilerSettings, ScalaCompilerConfiguration}
+import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.project.settings.{ScalaCompilerConfiguration, ScalaCompilerSettings}
 
 import scala.annotation.tailrec
-import com.intellij.psi.{PsiFile, PsiElement}
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.module.{ModuleUtilCore, ModuleManager, Module}
-import com.intellij.openapi.roots.libraries.Library
-import com.intellij.openapi.roots.impl.libraries.{ProjectLibraryTable, LibraryEx}
-import com.intellij.openapi.roots._
-import com.intellij.openapi.roots.ui.configuration.libraryEditor.{NewLibraryEditor, ExistingLibraryEditor}
-import com.intellij.openapi.vfs.{VfsUtil, VfsUtilCore}
 import scala.collection.immutable.HashSet
+import scala.collection.JavaConverters._
 import scala.util.matching.Regex
-import extensions._
 
 /**
  * @author Pavel Fatin
@@ -92,6 +95,12 @@ package object project {
       })
 
       libraries
+    }
+
+    def scalaLibraries: Seq[Library] = {
+      val collector = new CollectProcessor[Library]()
+      OrderEnumerator.orderEntries(module).librariesOnly().forEachLibrary(collector)
+      collector.getResults.asScala.filter(_.getName.contains(ScalaLibraryName)).toSeq
     }
 
     def attach(library: Library) {
