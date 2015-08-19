f51b0462868913d32e5b6405c94f58fa8d328030
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2015-08-19 12:06:51 +0300

    many elements marked as generating class for by-name argument fixed

--- src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorBuilderUtil.scala
+++ src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaEvaluatorBuilderUtil.scala
@@ -1183,7 +1183,7 @@ object ScalaEvaluatorBuilderUtil {
         case (g: ScGuard) childOf (_: ScEnumerators) => true
         case (g: ScGenerator) childOf (enums: ScEnumerators) if !enums.generators.headOption.contains(g) => true
         case e: ScEnumerator => true
-        case expr: ScExpression if ScalaPsiUtil.parameterOf(expr).exists(_.isByName) => true
+        case expr: ScExpression if ScalaPsiUtil.isByNameArgument(expr) => true
         case ScalaPsiUtil.MethodValue(_) => true
         case _ => false
       }
