1c5bdc664e466603bcd6da37fbf3983725ed22f6
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2015-09-18 19:00:05 +0300

    Support for syncronized caching

--- macroAnnotations/src/org/jetbrains/plugins/scala/macroAnnotations/Cached.scala
+++ macroAnnotations/src/org/jetbrains/plugins/scala/macroAnnotations/Cached.scala
@@ -8,19 +8,38 @@ import scala.reflect.macros.whitebox
  * Author: Svyatoslav Ilinskiy
  * Date: 9/18/15.
  */
-class Cached extends StaticAnnotation {
-  def macroTransform(annottees: Any*) = macro Cached.impl
+class Cached(synchronized: Boolean = false) extends StaticAnnotation {
+  def macroTransform(annottees: Any*) = macro CachedMacro.impl
 }
 
-object Cached {
+object CachedMacro {
   def impl(c: whitebox.Context)(annottees: c.Tree*): c.Expr[Any] = {
     import c.universe._
 
+    //todo: caching two function with the same name
+    //todo: cache with parameters
     annottees.toList match {
       case d@DefDef(mods, name, tpParams, params, retTp, rhs) :: Nil if params.forall(_.isEmpty) =>
         //it's an empty parameter function
+
+        val synchronized: Boolean = c.prefix.tree match {
+          case q"new Cached(synchronized = $b)" => c.eval[Boolean](c.Expr(b))
+          case q"new Cached($b)" => c.eval[Boolean](c.Expr(b))
+          case q"new Cached()" => false
+        }
+
         val cacheVarName = TermName(name.toString + "Cache_")
         val modCountVarName = TermName(name.toString + "ModCount_")
+
+        val functionContents = q"""
+            val modCount = getManager.getModificationTracker.getModificationCount
+            if ($cacheVarName.isDefined && $modCountVarName == modCount) $cacheVarName.get
+            else {
+              $cacheVarName = Some(calc())
+              $modCountVarName = modCount
+              $cacheVarName.get
+            }
+          """
         val res = q"""
 
           @volatile
@@ -32,15 +51,11 @@ object Cached {
           $mods def $name[..$tpParams](): $retTp = {
             def calc(): $retTp = $rhs
 
-            val modCount = getManager.getModificationTracker.getModificationCount
-            if ($cacheVarName.isDefined && $modCountVarName == modCount) {
-              return $cacheVarName.get
-            }
-            $cacheVarName = Some(calc)
-            $modCountVarName = modCount
-            return $cacheVarName.get
+            ${ if (synchronized) q"synchronized { $functionContents }" else functionContents }
           }
+
           """
+        println(res)
         c.Expr(res)
       case _ =>
         c.abort(c.enclosingPosition, "You can only annotate one function!")
--- test/org/jetbrains/plugins/scala/macroAnnotations/CachedTest.scala
+++ test/org/jetbrains/plugins/scala/macroAnnotations/CachedTest.scala
@@ -1,5 +1,8 @@
 package org.jetbrains.plugins.scala.macroAnnotations
 
+import java.lang.Thread.UncaughtExceptionHandler
+import java.util.concurrent.locks.ReentrantLock
+
 import org.junit.Assert
 
 
@@ -9,6 +12,11 @@ import org.junit.Assert
  */
 class CachedTest extends CachedTestBase {
   def testNoParametersSingleThread(): Unit = {
+    class Foo extends Managed {
+      @Cached(synchronized = false)
+      def currentTime(): Long = System.currentTimeMillis()
+    }
+
     val foo = new Foo
     val firstRes: Long = foo.currentTime()
     Thread.sleep(1)
@@ -24,8 +32,56 @@ class CachedTest extends CachedTestBase {
     Assert.assertEquals(secondRes, foo.currentTime())
   }
 
-  class Foo extends Managed {
-    @Cached
-    def currentTime(): Long = System.currentTimeMillis()
+  def testNoParametersSynchronized(): Unit = {
+    object Foo extends Managed {
+      @volatile
+      var cachedFunctionHasBeenRan: Boolean = false
+
+      //volatile should not be needed, since the main thread waits for all others to finish, but let's keep it here for extra safety
+      @volatile
+      var assertsFailed = 0
+
+      val allThreadsStartedLock: ReentrantLock = new ReentrantLock()
+
+      @Cached(synchronized = true)
+      def runSyncronized(): Unit = {
+        allThreadsStartedLock.lock()
+        try {
+          Assert.assertTrue(!cachedFunctionHasBeenRan)
+          cachedFunctionHasBeenRan = true
+        } finally {
+          allThreadsStartedLock.unlock()
+        }
+      }
+    }
+
+    def setUpThreads(): (Thread, Thread) = {
+      Foo.allThreadsStartedLock.lock()
+      val thread1 = new Thread(new Runnable {
+        override def run(): Unit = Foo.runSyncronized()
+      })
+      val thread2 = new Thread(new Runnable {
+        override def run(): Unit = Foo.runSyncronized()
+      })
+
+      val eh = new UncaughtExceptionHandler {
+        override def uncaughtException(t: Thread, e: Throwable): Unit = e match {
+          case _: AssertionError => Foo.assertsFailed += 1
+          case _ =>
+        }
+      }
+      thread1.setUncaughtExceptionHandler(eh)
+      thread2.setUncaughtExceptionHandler(eh)
+
+      thread1.start() //this thread should be waiting on acquiring the lock
+      thread2.start() //this thread should be waiting outside synchronize block
+      Foo.allThreadsStartedLock.unlock()
+      (thread1, thread2)
+    }
+
+    val (t1, t2) = setUpThreads()
+    t1.join()
+    t2.join()
+    Assert.assertEquals(0, Foo.assertsFailed)
   }
 }
