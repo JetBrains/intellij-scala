371208865732748a894aec2369445470cab80cbc
katejim <katejim@mail.ru>
2015-09-18 18:54:28 +0300

    delete unused

--- src/org/jetbrains/plugins/scala/lang/refactoring/introduceVariable/IntroduceTypeAliasData.scala
+++ src/org/jetbrains/plugins/scala/lang/refactoring/introduceVariable/IntroduceTypeAliasData.scala
@@ -3,9 +3,7 @@ package org.jetbrains.plugins.scala.lang.refactoring.introduceVariable
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAlias
-import org.jetbrains.plugins.scala.lang.refactoring.introduceVariable.ScopeItem
 
 /**
  * Created by Kate Ustyuzhanina
@@ -15,7 +13,6 @@ object IntroduceTypeAliasData {
   var currentScope: ScopeItem = null
   var initialInfo: (String, Int) = null
   var possibleScopes: Array[ScopeItem] = null
-  var typeElementRanges: TextRange = null
   var typeAliasInfo: (PsiFile, TextRange) = null
 
   def setTypeAlias(inTypeAlias: ScTypeAlias) = {
@@ -28,13 +25,11 @@ object IntroduceTypeAliasData {
     currentScope = null
     initialInfo = null
     possibleScopes = null
-    typeElementRanges = null
     typeAliasInfo = null
   }
 
   def isData: Boolean = {
-    currentScope != null || initialInfo != null || possibleScopes != null ||
-      typeElementRanges != null || typeAliasInfo != null
+    currentScope != null || initialInfo != null || possibleScopes != null || typeAliasInfo != null
   }
 
   def addScopeElement(item: ScopeItem): Unit = {
@@ -49,10 +44,6 @@ object IntroduceTypeAliasData {
 
   def setPossibleScopes(inPossibleScopes: Array[ScopeItem]): Unit = {
     possibleScopes = inPossibleScopes
-  }
-
-  def getTypeElement(file: PsiFile): ScTypeElement = {
-    PsiTreeUtil.findElementOfClassAtRange(file, typeElementRanges.getStartOffset, typeElementRanges.getEndOffset, classOf[ScTypeElement])
   }
 
   def getNamedElement: ScTypeAlias = {
