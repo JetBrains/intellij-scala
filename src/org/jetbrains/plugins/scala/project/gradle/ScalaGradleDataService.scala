0fe0f7ebcbc8dee143e13b799c0df0ea6df8eb6c
Nikolay Obedin <nikolay.obedin@jetbrains.com>
2015-09-21 17:03:07 +0300

    Fix ProjectDataServices according to changes in External System
    Also introduce Importer trait

--- src/org/jetbrains/plugins/scala/project/gradle/ScalaGradleDataService.scala
+++ src/org/jetbrains/plugins/scala/project/gradle/ScalaGradleDataService.scala
@@ -4,97 +4,103 @@ package project.gradle
 import java.io.File
 import java.util
 
+import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.model.{DataNode, ExternalSystemException, ProjectKeys}
 import com.intellij.openapi.externalSystem.service.notification.{ExternalSystemNotificationManager, NotificationSource, NotificationCategory, NotificationData}
-import com.intellij.openapi.externalSystem.service.project.{PlatformFacade, ProjectStructureHelper}
+import com.intellij.openapi.externalSystem.service.project.{IdeModifiableModelsProvider, IdeModelsProvider}
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.libraries.Library
 import org.jetbrains.plugins.gradle.model.data.ScalaModelData
 import org.jetbrains.plugins.gradle.util.GradleConstants
 import org.jetbrains.plugins.scala.project._
-import org.jetbrains.sbt.project.data.service.{SafeProjectStructureHelper, AbstractDataService}
+import org.jetbrains.sbt.project.data.service.{Importer, AbstractImporter, AbstractDataService}
 
 import scala.collection.JavaConverters._
 
 /**
  * @author Pavel Fatin
  */
-class ScalaGradleDataService(val helper: ProjectStructureHelper)
-        extends AbstractDataService[ScalaModelData, Library](ScalaModelData.KEY)
-        with SafeProjectStructureHelper {
-
-  import ScalaGradleDataService._
-
-  def doImportData(toImport: util.Collection[DataNode[ScalaModelData]], project: Project) {
-    toImport.asScala.foreach(doImport(_, project))
-  }
-
-  private def doImport(scalaNode: DataNode[ScalaModelData], project: Project): Unit =
-    for {
-      module <- getIdeModuleByNode(scalaNode, project)
-      compilerOptions = compilerOptionsFrom(scalaNode.getData)
-      compilerClasspath = scalaNode.getData.getScalaClasspath.asScala.toSeq
-    } {
-      module.configureScalaCompilerSettingsFrom("Gradle", compilerOptions)
-      configureScalaSdk(project, module, project.scalaLibraries, compilerClasspath)
-    }
-
-  private def configureScalaSdk(project: Project, module: Module, scalaLibraries: Seq[Library], compilerClasspath: Seq[File]): Unit = {
-    val compilerVersionOption = findScalaLibraryIn(compilerClasspath).flatMap(getVersionFromJar)
-    if (compilerVersionOption.isEmpty) {
-      showWarning(project, ScalaBundle.message("gradle.dataService.scalaVersionCantBeDetected", module.getName))
-      return
-    }
-    val compilerVersion = compilerVersionOption.get
-
-    val scalaLibraryOption = scalaLibraries.find(_.scalaVersion.contains(compilerVersion))
-    if (scalaLibraryOption.isEmpty) {
-      showWarning(project, ScalaBundle.message("gradle.dataService.scalaLibraryIsNotFound", compilerVersion.number, module.getName))
-      return
-    }
-    val scalaLibrary = scalaLibraryOption.get
-
-    if (!scalaLibrary.isScalaSdk) {
-      val languageLevel = scalaLibrary.scalaLanguageLevel.getOrElse(ScalaLanguageLevel.Default)
-      scalaLibrary.convertToScalaSdkWith(languageLevel, compilerClasspath)
-    }
-  }
-
-  def doRemoveData(toRemove: util.Collection[_ <: Library], project: Project) {}
+class ScalaGradleDataService extends AbstractDataService[ScalaModelData, Library](ScalaModelData.KEY) {
+  override def createImporter(toImport: Seq[DataNode[ScalaModelData]],
+                              projectData: ProjectData,
+                              project: Project,
+                              modelsProvider: IdeModifiableModelsProvider): Importer[ScalaModelData] =
+    new ScalaGradleDataService.Importer(toImport, projectData, project, modelsProvider)
 }
 
 private object ScalaGradleDataService {
 
-  private def findScalaLibraryIn(classpath: Seq[File]): Option[File] =
-    classpath.find(_.getName.startsWith(ScalaLibraryName))
+  private class Importer(dataToImport: Seq[DataNode[ScalaModelData]],
+                         projectData: ProjectData,
+                         project: Project,
+                         modelsProvider: IdeModifiableModelsProvider)
+    extends AbstractImporter[ScalaModelData](dataToImport, projectData, project, modelsProvider) {
 
-  private def getVersionFromJar(scalaLibrary: File): Option[Version] =
-    JarVersion.findFirstIn(scalaLibrary.getName).map(Version(_))
+    override def importData(): Unit =
+      dataToImport.foreach(doImport)
 
-  private def compilerOptionsFrom(data: ScalaModelData): Seq[String] = {
-    val options = data.getScalaCompileOptions
+    private def doImport(scalaNode: DataNode[ScalaModelData]): Unit =
+      for {
+        module <- getIdeModuleByNode(scalaNode)
+        compilerOptions = compilerOptionsFrom(scalaNode.getData)
+        compilerClasspath = scalaNode.getData.getScalaClasspath.asScala.toSeq
+      } {
+        module.configureScalaCompilerSettingsFrom("Gradle", compilerOptions)
+        configureScalaSdk(module, getScalaLibraries, compilerClasspath)
+      }
 
-    val presentations = Seq(
-      options.isDeprecation -> "-deprecation",
-      options.isUnchecked -> "-unchecked",
-      options.isOptimize -> "-optimise",
-      !isEmpty(options.getDebugLevel) -> s"-g:${options.getDebugLevel}",
-      !isEmpty(options.getEncoding) -> s"-encoding ${options.getEncoding}",
-      !isEmpty(data.getTargetCompatibility) -> s"-target:jvm-${data.getTargetCompatibility}")
+    private def configureScalaSdk(module: Module, scalaLibraries: Set[Library], compilerClasspath: Seq[File]): Unit = {
+      val compilerVersionOption = findScalaLibraryIn(compilerClasspath).flatMap(getVersionFromJar)
+      if (compilerVersionOption.isEmpty) {
+        showWarning(ScalaBundle.message("gradle.dataService.scalaVersionCantBeDetected", module.getName))
+        return
+      }
+      val compilerVersion = compilerVersionOption.get
 
-    val additionalOptions =
-      if (options.getAdditionalParameters != null) options.getAdditionalParameters.asScala else Seq.empty
+      val scalaLibraryOption = scalaLibraries.find(_.scalaVersion.contains(compilerVersion))
+      if (scalaLibraryOption.isEmpty) {
+        showWarning(ScalaBundle.message("gradle.dataService.scalaLibraryIsNotFound", compilerVersion.number, module.getName))
+        return
+      }
+      val scalaLibrary = scalaLibraryOption.get
 
-    presentations.flatMap((include _).tupled) ++ additionalOptions
-  }
+      if (!scalaLibrary.isScalaSdk) {
+        val languageLevel = scalaLibrary.scalaLanguageLevel.getOrElse(ScalaLanguageLevel.Default)
+        convertToScalaSdk(scalaLibrary, languageLevel, compilerClasspath)
+      }
+    }
 
-  private def isEmpty(s: String) = s == null || s.isEmpty
+    private def findScalaLibraryIn(classpath: Seq[File]): Option[File] =
+      classpath.find(_.getName.startsWith(ScalaLibraryName))
 
-  private def include(b: Boolean, s: String): Seq[String] = if (b) Seq(s) else Seq.empty
+    private def getVersionFromJar(scalaLibrary: File): Option[Version] =
+      JarVersion.findFirstIn(scalaLibrary.getName).map(Version(_))
 
-  private def showWarning(project: Project, message: String): Unit = {
-    val notification = new NotificationData("Gradle Sync", message, NotificationCategory.WARNING, NotificationSource.PROJECT_SYNC);
-    ExternalSystemNotificationManager.getInstance(project).showNotification(GradleConstants.SYSTEM_ID, notification);
+    private def compilerOptionsFrom(data: ScalaModelData): Seq[String] = {
+      val options = data.getScalaCompileOptions
+
+      val presentations = Seq(
+        options.isDeprecation -> "-deprecation",
+        options.isUnchecked -> "-unchecked",
+        options.isOptimize -> "-optimise",
+        !isEmpty(options.getDebugLevel) -> s"-g:${options.getDebugLevel}",
+        !isEmpty(options.getEncoding) -> s"-encoding ${options.getEncoding}",
+        !isEmpty(data.getTargetCompatibility) -> s"-target:jvm-${data.getTargetCompatibility}")
+
+      val additionalOptions =
+        if (options.getAdditionalParameters != null) options.getAdditionalParameters.asScala else Seq.empty
+
+      presentations.flatMap((include _).tupled) ++ additionalOptions
+    }
+
+    private def isEmpty(s: String) = s == null || s.isEmpty
+
+    private def include(b: Boolean, s: String): Seq[String] = if (b) Seq(s) else Seq.empty
+
+    private def showWarning(message: String): Unit = {
+      val notification = new NotificationData("Gradle Sync", message, NotificationCategory.WARNING, NotificationSource.PROJECT_SYNC);
+      ExternalSystemNotificationManager.getInstance(project).showNotification(GradleConstants.SYSTEM_ID, notification);
+    }
   }
 }
--- src/org/jetbrains/sbt/project/data/service/SbtModuleDataService.scala
+++ src/org/jetbrains/sbt/project/data/service/SbtModuleDataService.scala
@@ -1,51 +1,57 @@
 package org.jetbrains.sbt.project.data.service
 
-import java.util
-
 import com.intellij.openapi.externalSystem.model.DataNode
-import com.intellij.openapi.externalSystem.service.project.ProjectStructureHelper
+import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import org.jetbrains.sbt.project.data.SbtModuleData
 import org.jetbrains.sbt.project.module.SbtModule
 import org.jetbrains.sbt.resolvers.{SbtResolver, SbtResolverIndex, SbtResolverIndexesManager}
 
-import scala.collection.JavaConverters._
-
 /**
  * @author Pavel Fatin
  */
-class SbtModuleDataService(val helper: ProjectStructureHelper)
-  extends AbstractDataService[SbtModuleData, Module](SbtModuleData.Key)
-  with SafeProjectStructureHelper {
+class SbtModuleDataService extends AbstractDataService[SbtModuleData, Module](SbtModuleData.Key) {
+  override def createImporter(toImport: Seq[DataNode[SbtModuleData]],
+                              projectData: ProjectData,
+                              project: Project,
+                              modelsProvider: IdeModifiableModelsProvider): Importer[SbtModuleData] =
+    new SbtModuleDataService.Importer(toImport, projectData, project, modelsProvider)
+}
 
-  def doImportData(toImport: util.Collection[DataNode[SbtModuleData]], project: Project) {
-    toImport.asScala.foreach { moduleNode =>
-      for {
-        module <- getIdeModuleByNode(moduleNode, project)
-        imports = moduleNode.getData.imports
-        resolvers = moduleNode.getData.resolvers
-      } {
-        SbtModule.setImportsTo(module, imports)
-        setResolvers(module, resolvers)
-        updateLocalResolvers(resolvers)
+object SbtModuleDataService {
+  private class Importer(dataToImport: Seq[DataNode[SbtModuleData]],
+                         projectData: ProjectData,
+                         project: Project,
+                         modelsProvider: IdeModifiableModelsProvider)
+    extends AbstractImporter[SbtModuleData](dataToImport, projectData, project, modelsProvider) {
+
+    override def importData(): Unit =
+      dataToImport.foreach { moduleNode =>
+        for {
+          module <- getIdeModuleByNode(moduleNode)
+          imports = moduleNode.getData.imports
+          resolvers = moduleNode.getData.resolvers
+        } {
+          SbtModule.setImportsTo(module, imports)
+          setResolvers(module, resolvers)
+          updateLocalResolvers(resolvers)
+        }
       }
+
+    private def setResolvers(module: Module, resolvers: Set[SbtResolver]): Unit = {
+      SbtModule.setResolversTo(module, resolvers)
+      resolvers.foreach(SbtResolverIndexesManager().add)
+    }
+
+    private def updateLocalResolvers(resolvers: Set[SbtResolver]): Unit = {
+      val localResolvers = resolvers.filter { resolver =>
+        resolver.associatedIndex.exists { index =>
+          index.isLocal && index.timestamp == SbtResolverIndex.NO_TIMESTAMP
+        }
+      }
+      SbtResolverIndexesManager().update(localResolvers.toSeq)
     }
   }
-
-  private def setResolvers(module: Module, resolvers: Set[SbtResolver]): Unit = {
-    SbtModule.setResolversTo(module, resolvers)
-    resolvers.foreach(SbtResolverIndexesManager().add)
-  }
-
-  private def updateLocalResolvers(resolvers: Set[SbtResolver]): Unit = {
-    val localResolvers = resolvers.filter { resolver =>
-      resolver.associatedIndex.exists { index =>
-        index.isLocal && index.timestamp == SbtResolverIndex.NO_TIMESTAMP
-      }
-    }
-    SbtResolverIndexesManager().update(localResolvers.toSeq)
-  }
-
-  def doRemoveData(toRemove: util.Collection[_ <: Module], project: Project) {}
 }
