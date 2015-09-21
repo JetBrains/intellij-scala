4ebb6cf36db0bad5e7dec8c6b22ad522061ccba7
Nikolay Obedin <nikolay.obedin@jetbrains.com>
2015-09-21 17:03:07 +0300

    Fix ProjectDataServices according to changes in External System
    Also introduce Importer trait

--- src/org/jetbrains/sbt/project/data/service/AbstractDataService.scala
+++ src/org/jetbrains/sbt/project/data/service/AbstractDataService.scala
@@ -1,42 +1,119 @@
 package org.jetbrains.sbt.project.data.service
 
+import java.io.File
 import java.util
 
-import com.intellij.openapi.components.ComponentManager
-import com.intellij.openapi.externalSystem.model.{DataNode, Key}
-import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataService
+import com.intellij.facet.ModifiableFacetModel
+import com.intellij.openapi.externalSystem.model.project.{ModuleData, ProjectData}
+import com.intellij.openapi.externalSystem.model.{DataNode, Key, ProjectKeys}
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
+import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
 import com.intellij.openapi.externalSystem.util.{DisposeAwareProjectChange, ExternalSystemApiUtil}
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.roots.impl.libraries.LibraryEx
+import com.intellij.openapi.roots.libraries.Library
+import com.intellij.util.CommonProcessors.CollectProcessor
+import org.jetbrains.plugins.scala.project.{ScalaLibraryType, ScalaLanguageLevel, ScalaSdk, ScalaLibraryProperties, ScalaLibraryName}
+
+import scala.collection.JavaConversions._
 
 /**
  * @author Pavel Fatin
  */
-abstract class AbstractDataService[E, I](key: Key[E]) extends ProjectDataService[E, I] {
+abstract class AbstractDataService[E, I](key: Key[E]) extends AbstractProjectDataService[E, I] {
+
+  def createImporter(toImport: Seq[DataNode[E]],
+                     projectData: ProjectData,
+                     project: Project,
+                     modelsProvider: IdeModifiableModelsProvider): Importer[E]
+
   def getTargetDataKey = key
 
-  final def importData(toImport: util.Collection[DataNode[E]], project: Project, synchronous: Boolean) {
-    AbstractDataService.invoke(synchronous, project) {
-      doImportData(toImport, project)
-    }
-  }
-
-  def doImportData(toImport: util.Collection[DataNode[E]], project: Project)
-
-  final def removeData(toRemove: util.Collection[_ <: I], project: Project, synchronous: Boolean) {
-    AbstractDataService.invoke(synchronous, project) {
-      doRemoveData(toRemove, project)
-    }
-  }
-
-  def doRemoveData(toRemove: util.Collection[_ <: I], project: Project)
+  override final def importData(toImport: util.Collection[DataNode[E]],
+                                projectData: ProjectData,
+                                project: Project,
+                                modelsProvider: IdeModifiableModelsProvider): Unit =
+    createImporter(toImport.toSeq, projectData, project, modelsProvider).importData()
 }
 
-object AbstractDataService {
-  def invoke(synchronous: Boolean, manager: ComponentManager)(block: => Unit) {
-    ExternalSystemApiUtil.executeProjectChangeAction(synchronous, new DisposeAwareProjectChange(manager) {
-      def execute() {
-        block
-      }
+/**
+ * The purposes of this trait are the following:
+ *    - Incapsulate logic necessary for importing specified data
+ *    - Wrap "unsafe" methods from IdeModifiableModelsProvider
+ *    - Collect import parameters as class fields to eliminate necessity of
+ *      dragging them into each and every method of ProjectDataService
+ *    - Abstract from External System's API which is rather unstable
+ */
+trait Importer[E] {
+  val dataToImport: Seq[DataNode[E]]
+  val projectData: ProjectData
+  val project: Project
+  val modelsProvider: IdeModifiableModelsProvider
+
+  def importData(): Unit
+
+  // IdeModifiableModelsProvider wrappers
+
+  def findIdeModule(name: String): Option[Module] =
+    Option(modelsProvider.findIdeModule(name))
+
+  def findIdeModule(data: ModuleData): Option[Module] =
+    Option(modelsProvider.findIdeModule(data))
+
+  def getModifiableFacetModel(module: Module): ModifiableFacetModel =
+    modelsProvider.getModifiableFacetModel(module)
+
+  def getModifiableLibraryModel(library: Library): Library.ModifiableModel =
+    modelsProvider.getModifiableLibraryModel(library)
+
+  def getModifiableRootModel(module: Module): ModifiableRootModel =
+    modelsProvider.getModifiableRootModel(module)
+
+  def getModules: Array[Module] =
+    modelsProvider.getModules
+
+  // FIXME: should be implemented in External System
+  def getModifiableLibraryModelEx(library: Library): LibraryEx.ModifiableModelEx =
+    modelsProvider.getModifiableLibraryModel(library).asInstanceOf[LibraryEx.ModifiableModelEx]
+
+  // Utility methods
+
+  def getIdeModuleByNode(node: DataNode[_]): Option[Module] =
+    for {
+      moduleData <- Option(node.getData(ProjectKeys.MODULE))
+      module <- findIdeModule(moduleData)
+    } yield module
+
+  def getScalaLibraries: Set[Library] =
+    modelsProvider.getAllLibraries.filter(_.getName.contains(ScalaLibraryName)).toSet
+
+  def getScalaLibraries(module: Module): Set[Library] = {
+    val collector = new CollectProcessor[Library]()
+    getModifiableRootModel(module).orderEntries().librariesOnly().forEachLibrary(collector)
+    collector.getResults.toSet.filter(_.getName.contains(ScalaLibraryName))
+  }
+
+  def executeProjectChangeAction(action: => Unit): Unit =
+    ExternalSystemApiUtil.executeProjectChangeAction(new DisposeAwareProjectChange(project) {
+      override def execute(): Unit = action
     })
+
+  def convertToScalaSdk(library: Library, languageLevel: ScalaLanguageLevel, compilerClasspath: Seq[File]): ScalaSdk = {
+    val properties = new ScalaLibraryProperties()
+    properties.languageLevel = languageLevel
+    properties.compilerClasspath = compilerClasspath
+
+    val model = getModifiableLibraryModelEx(library)
+    model.setKind(ScalaLibraryType.instance.getKind)
+    model.setProperties(properties)
+
+    new ScalaSdk(library)
   }
 }
+
+abstract class AbstractImporter[E](val dataToImport: Seq[DataNode[E]],
+                                   val projectData: ProjectData,
+                                   val project: Project,
+                                   val modelsProvider: IdeModifiableModelsProvider) extends Importer[E]
--- test/org/jetbrains/sbt/project/data/service/ProjectDataServiceTestCase.scala
+++ test/org/jetbrains/sbt/project/data/service/ProjectDataServiceTestCase.scala
@@ -4,7 +4,7 @@ import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.model.{DataNode, ProjectSystemId}
 import com.intellij.openapi.externalSystem.service.notification.{ExternalSystemNotificationManager, NotificationCategory, NotificationSource}
-import com.intellij.openapi.externalSystem.service.project.PlatformFacade
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProviderImpl
 import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataManager
 import com.intellij.openapi.externalSystem.util.{DisposeAwareProjectChange, ExternalSystemApiUtil}
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx
@@ -23,8 +23,7 @@ abstract class ProjectDataServiceTestCase extends PlatformTestCase {
         ProjectRootManagerEx.getInstanceEx(getProject).mergeRootsChangesDuring(new Runnable() {
           override def run(): Unit = {
             val projectDataManager = ServiceManager.getService(classOf[ProjectDataManager])
-            val platformFacade = ServiceManager.getService(classOf[PlatformFacade])
-            projectDataManager.importData(projectData, getProject, platformFacade, true)
+            projectDataManager.importData(projectData, getProject, new IdeModifiableModelsProviderImpl(getProject), true);
           }
         })
     })
