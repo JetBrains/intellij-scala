3e3b0f33c8028e6f3b9e4d88edbec5f41c2daef3
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2015-09-25 19:53:06 +0300

    ScalaSyntheticSteppingFilter refactored: use cached data (cherry picked from commit abd8d8c)

--- src/org/jetbrains/plugins/scala/debugger/ScalaSyntheticSteppingFilter.scala
+++ src/org/jetbrains/plugins/scala/debugger/ScalaSyntheticSteppingFilter.scala
@@ -1,14 +1,13 @@
 package org.jetbrains.plugins.scala.debugger
 
-import com.intellij.debugger.NoDataException
-import com.intellij.debugger.engine.{ExtraSteppingFilter, SuspendContext}
+import com.intellij.debugger.engine.{DebugProcess, ExtraSteppingFilter, SuspendContext}
 import com.intellij.psi.PsiElement
 import com.sun.jdi.Location
 import com.sun.jdi.request.StepRequest
-import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.debugger.evaluation.ScalaEvaluatorBuilderUtil
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTemplateDefinition
 import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
 
 /**
@@ -18,45 +17,46 @@ class ScalaSyntheticSteppingFilter extends ExtraSteppingFilter {
 
   override def isApplicable(context: SuspendContext): Boolean = {
     val debugProcess = context.getDebugProcess
-    val positionManager = new ScalaPositionManager(debugProcess)
-    val location = context.getFrameProxy.location()
-    isSyntheticMethod(location, positionManager)
+    val frameProxy = context.getFrameProxy
+    if (debugProcess == null || frameProxy == null) return false
+
+    val location = frameProxy.location()
+    isSynthetic(location, debugProcess)
   }
 
   override def getStepRequestDepth(context: SuspendContext): Int = StepRequest.STEP_INTO
 
-  private def isSyntheticMethod(location: Location, positionManager: ScalaPositionManager): Boolean = {
-    val name = location.method().name()
+  def isSynthetic(location: Location, debugProcess: DebugProcess): Boolean = {
+    val positionManager = new ScalaPositionManager(debugProcess)
+
+    val method = location.method()
+    val name = method.name()
     if (name.endsWith("$lzycompute")) return false //should step into the body of a lazy val
 
     if (positionManager.shouldSkip(location)) return true
 
-    if (name.startsWith("apply")) return false
+    if (name.startsWith("apply") || ScalaPositionManager.isIndyLambda(method)) return false
 
-    if (name == "<init>") return false
+    if (method.isConstructor) return false
 
     inReadAction {
-      val sourcePosition =
-        try {
-          positionManager.getSourcePosition(location)
-        } catch {
-          case _: NoDataException => return false
-        }
-
-      if (!sourcePosition.getFile.getLanguage.is(ScalaLanguage.Instance)) return false
-
-      val classInSource = ScalaPositionManager.findGeneratingClassOrMethodParent(sourcePosition.getElementAt)
-      if (classInSource == null) return false
-
-      !existsInSource(name, classInSource)
+      positionManager.findElementByReferenceType(location.declaringType()) match {
+        case Some(td: ScTemplateDefinition) =>
+          td.functions.forall(f => !nameMatches(name, f.name)) && !hasLocalFun(name, td)
+        case _ => true
+      }
     }
   }
 
-  private def existsInSource(name: String, td: PsiElement): Boolean = {
+  private def hasLocalFun(name: String, td: PsiElement): Boolean = {
     td.depthFirst(elem => elem == td || !ScalaEvaluatorBuilderUtil.isGenerateClass(elem)).exists {
-      case fun: ScFunction if fun.isLocal => name.contains(ScalaNamesUtil.toJavaName(fun.name))
-      case fun: ScFunction => ScalaNamesUtil.toJavaName(fun.name) == name
+      case fun: ScFunction if fun.isLocal => nameMatches(name, fun.name)
       case _ => false
     }
+  }
+
+  private def nameMatches(jvmName: String, funName: String) = {
+    val encoded = ScalaNamesUtil.toJavaName(funName)
+    encoded == jvmName || jvmName.startsWith(encoded + "$") || jvmName.contains("$$" + encoded + "$")
   }
 }
