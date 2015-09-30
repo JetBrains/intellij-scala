d83aff10b8d28908fd2d19efbdb05e440db70759
Nikolay Obedin <nikolay.obedin@jetbrains.com>
2015-09-30 10:43:14 +0300

    Fix SBT version detection for sbt-launch.jar 1.0.0

--- src/org/jetbrains/sbt/project/structure/SbtRunner.scala
+++ src/org/jetbrains/sbt/project/structure/SbtRunner.scala
@@ -20,8 +20,6 @@ class SbtRunner(vmExecutable: File, vmOptions: Seq[String], environment: Map[Str
                 customLauncher: Option[File], customStructureDir: Option[String]) {
   private val LauncherDir = getSbtLauncherDir
   private val SbtLauncher = customLauncher.getOrElse(LauncherDir / "sbt-launch.jar")
-  private val DefaultSbtVersion = "0.13"
-  private val SinceSbtVersion = "0.12.4"
 
   private val cancellationFlag: AtomicBoolean = new AtomicBoolean(false)
 
@@ -32,17 +30,14 @@ class SbtRunner(vmExecutable: File, vmOptions: Seq[String], environment: Map[Str
           (listener: (String) => Unit): Either[Exception, Elem] = {
 
     val options = download.seq("download") ++
-            resolveClassifiers.seq("resolveClassifiers") ++
-            resolveSbtClassifiers.seq("resolveSbtClassifiers")
+      resolveClassifiers.seq("resolveClassifiers") ++
+      resolveSbtClassifiers.seq("resolveSbtClassifiers")
 
     checkFilePresence.fold(read0(directory, options.mkString(", "))(listener))(it => Left(new FileNotFoundException(it)))
   }
 
   private def read0(directory: File, options: String)(listener: (String) => Unit): Either[Exception, Elem] = {
-    val sbtVersion = sbtVersionIn(directory)
-            .orElse(implementationVersionOf(SbtLauncher))
-            .getOrElse(DefaultSbtVersion)
-
+    val sbtVersion = detectSbtVersion(directory, SbtLauncher)
     val majorSbtVersion = numbersOf(sbtVersion).take(2).mkString(".")
 
     if (compare(sbtVersion, SinceSbtVersion) < 0) {
@@ -73,12 +68,12 @@ class SbtRunner(vmExecutable: File, vmOptions: Seq[String], environment: Map[Str
 
       val processCommandsRaw =
         path(vmExecutable) +:
-        "-Djline.terminal=jline.UnsupportedTerminal" +:
-        "-Dsbt.log.noformat=true" +:
-        "-Dfile.encoding=UTF-8" +:
-        (vmOptions ++ SbtOpts.loadFrom(directory)) :+
-        "-jar" :+
-        path(SbtLauncher)
+          "-Djline.terminal=jline.UnsupportedTerminal" +:
+          "-Dsbt.log.noformat=true" +:
+          "-Dfile.encoding=UTF-8" +:
+          (vmOptions ++ SbtOpts.loadFrom(directory)) :+
+          "-jar" :+
+          path(SbtLauncher)
       val processCommands = processCommandsRaw.filterNot(_.isEmpty)
 
       try {
@@ -153,6 +148,9 @@ object SbtRunner {
 
   def getDefaultLauncher = getSbtLauncherDir / "sbt-launch.jar"
 
+  private[structure] val DefaultSbtVersion = "0.13.8"
+  private val SinceSbtVersion = "0.12.4"
+
   private def numbersOf(version: String): Seq[String] = version.split("\\.").toSeq
 
   private def compare(v1: String, v2: String): Int = numbersOf(v1).zip(numbersOf(v2)).foldLeft(0) {
@@ -160,23 +158,61 @@ object SbtRunner {
     case (acc, _) => acc
   }
 
-  private def implementationVersionOf(jar: File): Option[String] = {
+  private[structure] def detectSbtVersion(directory: File, sbtLauncher: File): String =
+    sbtVersionIn(directory)
+      .orElse(sbtVersionInBootPropertiesOf(sbtLauncher))
+      .orElse(implementationVersionOf(sbtLauncher))
+      .getOrElse(DefaultSbtVersion)
+
+  private def implementationVersionOf(jar: File): Option[String] =
     readManifestAttributeFrom(jar, "Implementation-Version")
-  }
 
   private def readManifestAttributeFrom(file: File, name: String): Option[String] = {
     val jar = new JarFile(file)
     try {
-      using(new BufferedInputStream(jar.getInputStream(new JarEntry("META-INF/MANIFEST.MF")))) { input =>
+      Option(jar.getJarEntry("META-INF/MANIFEST.MF")).flatMap { entry =>
+        val input = new BufferedInputStream(jar.getInputStream(entry))
         val manifest = new java.util.jar.Manifest(input)
         val attributes = manifest.getMainAttributes
         Option(attributes.getValue(name))
       }
     }
     finally {
-      if (jar.isInstanceOf[Closeable]) {
-        jar.close()
+      jar.close()
+    }
+  }
+
+  private def sbtVersionInBootPropertiesOf(jar: File): Option[String] = {
+    val appProperties = readSectionFromBootPropertiesOf(jar, sectionName = "app")
+    for {
+      name <- appProperties.get("name")
+      if name == "sbt"
+      versionStr <- appProperties.get("version")
+      version <- "\\d+(\\.\\d+)+".r.findFirstIn(versionStr)
+    } yield version
+  }
+
+  private def readSectionFromBootPropertiesOf(launcherFile: File, sectionName: String): Map[String, String] = {
+    val Property = "^\\s*(\\w+)\\s*:(.+)".r.unanchored
+
+    def findProperty(line: String): Option[(String, String)] = {
+      line match {
+        case Property(name, value) => Some((name, value.trim))
+        case _ => None
       }
+    }
+
+    val jar = new JarFile(launcherFile)
+    try {
+      Option(jar.getEntry("sbt/sbt.boot.properties")).fold(Map.empty[String, String]) { entry =>
+        val lines = scala.io.Source.fromInputStream(jar.getInputStream(entry)).getLines()
+        val sectionLines = lines
+          .dropWhile(_.trim != s"[$sectionName]").drop(1)
+          .takeWhile(!_.trim.startsWith("["))
+        sectionLines.flatMap(findProperty).toMap
+      }
+    } finally {
+      jar.close()
     }
   }
 
@@ -193,3 +229,4 @@ object SbtRunner {
     }
   }
 }
+
