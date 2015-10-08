3280eff1088f9c55a74960454f2a8f67d999e8c5
katejim <katejim@mail.ru>
2015-10-08 14:03:44 +0300

    support multiple package mode

--- test/org/jetbrains/plugins/scala/refactoring/introduceVariable/AbstractIntroduceVariableTestBase.java
+++ test/org/jetbrains/plugins/scala/refactoring/introduceVariable/AbstractIntroduceVariableTestBase.java
@@ -131,9 +131,9 @@ abstract public class AbstractIntroduceVariableTestBase extends ActionTestBase {
         ScExpression selectedExpr = null;
         ScType[] types = null;
         if (ScalaRefactoringUtil.getExpression(getProject(), myEditor, myFile, startOffset, endOffset) instanceof Some) {
-          Some temp = (Some) ScalaRefactoringUtil.getExpression(getProject(), myEditor, myFile, startOffset, endOffset);
-          selectedExpr = IntroduceVariableTestUtil.extract1((Tuple2<ScExpression, ScType[]>) temp.get());
-          types = IntroduceVariableTestUtil.extract2((Tuple2<ScExpression, ScType[]>) temp.get());
+          Some expression = (Some) ScalaRefactoringUtil.getExpression(getProject(), myEditor, myFile, startOffset, endOffset);
+          selectedExpr = IntroduceVariableTestUtil.extract1((Tuple2<ScExpression, ScType[]>) expression.get());
+          types = IntroduceVariableTestUtil.extract2((Tuple2<ScExpression, ScType[]>) expression.get());
         }
         Assert.assertNotNull("Selected expression reference points to null", selectedExpr);
 
@@ -171,7 +171,7 @@ abstract public class AbstractIntroduceVariableTestBase extends ActionTestBase {
                   replaceCompanionObjOccurrences, replaceOccurrencesFromInheritors, scopes[0]);
 
           introduceVariableHandler.runRefactoringForTypes(myFile, myEditor, typeElement,
-                  typeName, occurrences, scopes[0].fileEncloser(),scopes[0].isPackage());
+                  typeName, occurrences, scopes[0].fileEncloser(),scopes[0].isPackage(), scopes[0].needDirectoryCreating());
 
           result = removeTypenameComment(myEditor.getDocument().getText());
         }
