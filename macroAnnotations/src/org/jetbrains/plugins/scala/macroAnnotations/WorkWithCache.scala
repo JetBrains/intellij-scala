6df3ca6f57f11125caa108d990fafe10c642fb70
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2015-10-20 16:32:38 +0300

    Make ClearCache more generic. Now it's WorkWithCache, where you can define your own operation So if previously you would def foo(): Unit = ClearCache.clearCache("clearCachesForThisFun") now you do this: def foo(): Unit = WorkWithCache.workWithCache("clear()", "clearCachesForThisFun")

---
+++ macroAnnotations/src/org/jetbrains/plugins/scala/macroAnnotations/WorkWithCache.scala
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.scala.macroAnnotations
+
+import scala.language.experimental.macros
+import scala.reflect.macros.whitebox
+
+/**
+  * Calling this def macro will generate code that clears all maps associated with methodNames passed in
+  *
+  * NOTE: methodNames passed in should be in the same class and have @Cached annotation
+  *
+  * Author: Svyatoslav Ilinskiy
+  * Date: 10/20/15.
+  */
+object WorkWithCache {
+  def workWithCache(operation: String, methodNames: String*) = macro workWithCacheImpl
+  def workWithCacheImpl(c: whitebox.Context)(operation: c.Expr[String], methodNames: c.Expr[String]*): c.Expr[Unit] = {
+    import c.universe._
+
+    val statements = methodNames.map { s =>
+      val name: TermName = TermName(c.eval[String](c.Expr[String](s.tree)))
+      val op: TermName = TermName(c.eval[String](c.Expr[String](operation.tree)))
+      q"${TermName(name + CachedMacro.cachedMapPostfix)}.$op"
+    }
+    val res = q"..$statements"
+    CachedMacro.println(res)
+    c.Expr(res)
+  }
+}
