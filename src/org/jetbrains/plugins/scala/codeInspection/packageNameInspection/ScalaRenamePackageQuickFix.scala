10e7e68f51872fe2c1f5d627ac2dad5b183b3009
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2015-10-28 09:31:45 +0300

    Fix several memory leaks in inspection, remove deprecated and unused AbstractFix

--- src/org/jetbrains/plugins/scala/codeInspection/packageNameInspection/ScalaRenamePackageQuickFix.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/packageNameInspection/ScalaRenamePackageQuickFix.scala
@@ -3,28 +3,26 @@ package codeInspection
 package packageNameInspection
 
 
-import com.intellij.codeInspection.{LocalQuickFix, ProblemDescriptor}
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.util.ScalaUtils
-
-import scala.tools.scalap.scalax.util.StringUtil
 
 /**
  * User: Alexander Podkhalyuzin
  * Date: 08.07.2009
  */
 
-class ScalaRenamePackageQuickFix(file: ScalaFile, name: String) extends LocalQuickFix {
-  def applyFix(project: Project, descriptor: ProblemDescriptor): Unit = {
+class ScalaRenamePackageQuickFix(myFile: ScalaFile, name: String)
+      extends AbstractFixOnPsiElement(if (name == null || name.isEmpty) "Remove package statement" else s"Rename Package to $name", myFile) {
+  def doApplyFix(project: Project): Unit = {
+    val file = getElement
+    if (!file.isValid) return
     ScalaUtils.runWriteAction(new Runnable {
-      def run: Unit = {
+      def run(): Unit = {
         file.setPackageName(name)
       }
     }, project, "Rename Package QuickFix")
   }
 
-  def getName: String = if (name == null || name.isEmpty) "Remove package statement" else s"Rename Package to $name"
-
-  def getFamilyName: String = "Rename Package"
+  override def getFamilyName: String = "Rename Package"
 }
