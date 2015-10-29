535a3e327e44969cf9258f96623892ba5409734d
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-10-29 20:24:40 +0300

    #EA-74314 fixed

--- src/org/jetbrains/plugins/hocon/psi/HoconPsiFile.scala
+++ src/org/jetbrains/plugins/hocon/psi/HoconPsiFile.scala
@@ -2,9 +2,10 @@ package org.jetbrains.plugins.hocon.psi
 
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.impl.source.PsiFileImpl
-import com.intellij.psi.{FileViewProvider, PsiElement, PsiElementVisitor}
+import com.intellij.psi.{PsiComment, FileViewProvider, PsiElement, PsiElementVisitor}
 import org.jetbrains.plugins.hocon.lang.HoconFileType
 import org.jetbrains.plugins.hocon.parser.HoconElementType.HoconFileElementType
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
 
 class HoconPsiFile(provider: FileViewProvider) extends PsiFileImpl(HoconFileElementType, HoconFileElementType, provider) {
   def accept(visitor: PsiElementVisitor): Unit =
@@ -13,9 +14,16 @@ class HoconPsiFile(provider: FileViewProvider) extends PsiFileImpl(HoconFileElem
   def getFileType: FileType =
     HoconFileType
 
-  def toplevelEntries = getFirstChild match {
-    case obj: HObject => obj.entries
-    case ets: HObjectEntries => ets
+  def toplevelEntries = {
+    def entriesInner(child: PsiElement): HObjectEntries = child match {
+      case obj: HObject => obj.entries
+      case ets: HObjectEntries => ets
+      case comment: PsiComment => entriesInner(comment.getNextSiblingNotWhitespace)
+    }7
+    
+    
+    
+    entriesInner(getFirstChild)
   }
 
   def toplevelObject = getFirstChild match {
