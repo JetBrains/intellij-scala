fe4571a4b390cad7b58b02b6a5bdabe54098829e
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-10-30 17:16:44 +0300

    fix exception in monocle injector #EA-75306 fixed

--- src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MonocleInjector.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MonocleInjector.scala
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTypeDefinition, ScObject, ScClass}
 import org.jetbrains.plugins.scala.lang.psi.impl.base.ScLiteralImpl
 import org.jetbrains.plugins.scala.lang.psi.impl.statements.params.ScClassParameterImpl
+import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -29,7 +30,7 @@ class MonocleInjector extends SyntheticMembersInjector {
     }
     fields.foreach({ i =>
       val template = if (clazz.typeParameters.isEmpty)
-        s"def $prefix${i.name}: _root_.monocle.Lens[${clazz.qualifiedName}, ${i.typeElement.get.calcType}] = ???"
+        s"def $prefix${i.name}: _root_.monocle.Lens[${clazz.qualifiedName}, ${i.getType(TypingContext.empty).map(_.canonicalText).getOrElse("Any")}] = ???"
       else {
         val tparams = s"[${clazz.typeParameters.map(_.getText).mkString(",")}]"
         s"def $prefix${i.name}$tparams: _root_.monocle.Lens[${clazz.qualifiedName}$tparams, ${i.typeElement.get.calcType}] = ???"
