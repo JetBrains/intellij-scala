62380fd539809ee87af3058a35c93d9591f68af2
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2015-11-10 16:28:25 +0300

    Fix to fix... Hack for Monocle to avoid recursion #SCL-9420 Fixed

--- src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/TypeDefinitionMembers.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/TypeDefinitionMembers.scala
@@ -933,7 +933,7 @@ object TypeDefinitionMembers {
           val shouldCheckSynthetics = processor match {
             case c: CompletionProcessor => true
             case o: ImplicitProcessor => true
-            case b: BaseProcessor => b.candidates.nonEmpty
+            case b: BaseProcessor => b.candidates.isEmpty
             case _ => true
           }
           if (shouldCheckSynthetics) {
@@ -945,12 +945,6 @@ object TypeDefinitionMembers {
         }
       }
       true
-    }
-
-    if (processOnlyStable) {
-      if (!process(parameterlessSignatures())) return false
-    } else {
-      if (!process(signatures())) return false
     }
 
     if (shouldProcessTypes(processor)) {
@@ -976,6 +970,13 @@ object TypeDefinitionMembers {
         }
       }
     }
+
+    if (processOnlyStable) {
+      if (!process(parameterlessSignatures())) return false
+    } else {
+      if (!process(signatures())) return false
+    }
+
     //inner classes
     if (shouldProcessJavaInnerClasses(processor)) {
       if (decodedName != "") {
