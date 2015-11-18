0377a9b015c2ee111d8a555e8e55ec331dae8c05
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2015-11-18 16:32:57 +0300

    [Performance Tests]Update revision on our project, so zip file is small 6 MB vs 250+ MB Also, adjust timings for performance tests

--- test/org/jetbrains/plugins/scala/performance/highlighting/projectHighlighting/ScalaCommunityHighlightingPerformanceTest.scala
+++ test/org/jetbrains/plugins/scala/performance/highlighting/projectHighlighting/ScalaCommunityHighlightingPerformanceTest.scala
@@ -17,9 +17,9 @@ class ScalaCommunityHighlightingPerformanceTest extends PerformanceSbtProjectHig
 
   override def githubRepoName: String = "intellij-scala"
 
-  override def revision: String = "493444f6465d0eddea75ac5cd5a848cc30d48ae5"
+  override def revision: String = "752b3c8575ff506a249159c86bbfba3cdb96a317"
 
-  def testPerformanceScalaCommunityScalaPsiUtil() = doTest("ScalaPsiUtil.scala", 24.seconds)
+  def testPerformanceScalaCommunityScalaPsiUtil() = doTest("ScalaPsiUtil.scala", 25.seconds)
 
   def testPerformanceScalaCommunityScalaAnnotator() = doTest("ScalaAnnotator.scala", 10.seconds)
 
@@ -28,7 +28,7 @@ class ScalaCommunityHighlightingPerformanceTest extends PerformanceSbtProjectHig
 
   def testPerformanceScalaCommunityConformance() = doTest("Conformance.scala", 11.seconds)
 
-  def testPerformanceScalaCommunityScalaSpacingProcessor() = doTest("ScalaSpacingProcessor.scala", 6.seconds)
+  def testPerformanceScalaCommunityScalaSpacingProcessor() = doTest("ScalaSpacingProcessor.scala", 5.seconds)
 
   override def doTest(path: String, timeout: Int): Unit = {
     VfsRootAccess.SHOULD_PERFORM_ACCESS_CHECK = false
