5666214c56fd9419f6e06f16e4d4a9698dd652b2
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2015-11-27 15:34:22 +0300

    Updated dependency.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Versions {
   val scalaVersion = "2.11.6"
-  val ideaVersion = "143.869.1"
+  val ideaVersion = "143.1015.7"
   val sbtStructureVersion = "4.3.2"
   val luceneVersion = "4.8.1"
   val aetherVersion = "1.0.0.v20140518"
