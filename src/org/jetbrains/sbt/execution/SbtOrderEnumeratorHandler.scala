d59e4f5ceb6cd144246038858abab083e24b9fc1
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2015-12-08 16:20:02 +0300

    disable SbtOrderEnumeratorHandler, transitive module dependencies should be explicitly added first

--- src/org/jetbrains/sbt/execution/SbtOrderEnumeratorHandler.scala
+++ src/org/jetbrains/sbt/execution/SbtOrderEnumeratorHandler.scala
@@ -1,9 +1,8 @@
 package org.jetbrains.sbt.execution
 
-import com.intellij.openapi.module.{Module, ModuleManager}
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.OrderEnumerationHandler
-import org.jetbrains.sbt.settings.SbtSystemSettings
 
 /**
   * @author Nikolay.Tropin
@@ -16,10 +15,12 @@ class SbtOrderEnumeratorHandlerFactory extends OrderEnumerationHandler.Factory {
   override def createHandler(module: Module): OrderEnumerationHandler = new SbtOrderEnumeratorHandler
 
   override def isApplicable(project: Project): Boolean = {
-    ModuleManager.getInstance(project).getModules.exists(isApplicable)
+//    ModuleManager.getInstance(project).getModules.exists(isApplicable)
+    false
   }
 
   override def isApplicable(module: Module): Boolean = {
-    SbtSystemSettings.getInstance(module.getProject).getLinkedProjectSettings(module).isDefined
+//    SbtSystemSettings.getInstance(module.getProject).getLinkedProjectSettings(module).isDefined
+    false
   }
 }
