a738b1e690201a51301d8a9101cceddf6672d6a9
Mikhail Mutcianko <mutcianko.m@gmail.com>
2015-12-09 12:00:59 +0300

    metahost: fix existential type test

--- test/scala/meta/converter/TreeConverterDeclTestSlow.scala
+++ test/scala/meta/converter/TreeConverterDeclTestSlow.scala
@@ -9,11 +9,11 @@ import scala.{Seq => _}
 class TreeConverterDeclTestSlow extends TreeConverterTestBaseWithLibrary {
   def testExistentialType() {
     doTest(
-      "def foo(x: Map[T, U] forSome {type T >: Nothing <: A; type U})",
+      "def foo(x: Map[T, U] forSome {type T >: Nothing <: Int; type U})",
       Decl.Def(Nil, Term.Name("foo"), Nil, Seq(Seq(Term.Param(Nil, Term.Name("x"),
         Some(Type.Existential(Type.Apply(Type.Name("Map"), Seq(Type.Name("T"), Type.Name("U"))),
           Seq(Decl.Type(Nil, Type.Name("T"), Nil, Type.Bounds(Some(Type.Name("Nothing")),
-            Some(Type.Name("A")))), Decl.Type(Nil, Type.Name("U"), Nil, Type.Bounds(None, None))))), None))),
+            Some(Type.Name("Int")))), Decl.Type(Nil, Type.Name("U"), Nil, Type.Bounds(None, None))))), None))),
         Type.Name("Unit"))
     )
   }
