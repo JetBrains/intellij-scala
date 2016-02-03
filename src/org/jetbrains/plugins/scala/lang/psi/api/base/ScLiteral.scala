aae6573df248d2454bf3176df6569b5ed7c5bdf6
Pavel Fatin <pavel.fatin@jetbrains.com>
2016-02-03 11:53:41 +0100

    A few useful base classes

--- src/org/jetbrains/plugins/scala/lang/psi/api/base/ScLiteral.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/base/ScLiteral.scala
@@ -5,7 +5,9 @@ package api
 package base
 
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.tree.IElementType
 import com.intellij.psi.{PsiAnnotationOwner, PsiElement, PsiLanguageInjectionHost, PsiLiteral}
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes._
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
 
@@ -31,3 +33,22 @@ trait ScLiteral extends ScExpression with PsiLiteral with PsiLanguageInjectionHo
 object ScLiteral {
   def unapply(literal: ScLiteral) = Some(literal.getValue)
 }
+
+class ScLiteralValueExtractor[T](literalTypes: IElementType*)(f: AnyRef => T) {
+  private val types = literalTypes.toSet
+
+  def unapply(literal: ScLiteral): Option[T] = {
+    val literalType = literal.getFirstChild.getNode.getElementType
+    if (types.contains(literalType)) Some(f(literal.getValue)) else None
+  }
+}
+
+object ScIntLiteral extends ScLiteralValueExtractor(tINTEGER)(_.asInstanceOf[java.lang.Integer].intValue)
+
+object ScFloatLiteral extends ScLiteralValueExtractor(tFLOAT)(_.asInstanceOf[java.lang.Float].floatValue)
+
+object ScCharLiteral extends ScLiteralValueExtractor(tCHAR)(_.asInstanceOf[java.lang.Character].charValue)
+
+object ScBooleanLiteral extends ScLiteralValueExtractor(kTRUE, kFALSE)(_.asInstanceOf[java.lang.Boolean].booleanValue)
+
+object ScStringLiteral extends ScLiteralValueExtractor(tSTRING, tWRONG_STRING, tMULTILINE_STRING)(_.asInstanceOf[String])
