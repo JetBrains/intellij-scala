7dc5e60a9d88613f863fefe735afe5d10d7c7ea0
ssdmitriev <ssdmitriev@yandex-team.ru>
2016-02-05 02:57:14 +0300

    do inspections

--- src/org/jetbrains/plugins/scala/codeInspection/dataFlow/UnusedFunctionParameterInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/dataFlow/UnusedFunctionParameterInspection.scala
@@ -3,12 +3,12 @@ package org.jetbrains.plugins.scala.codeInspection.dataFlow
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.codeInspection.AbstractInspection
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaRecursiveElementVisitor
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunctionDefinition, ScFunction}
-import org.jetbrains.plugins.scala.lang.psi.impl.statements.ScFunctionDefinitionImpl
+import org.jetbrains.plugins.scala.lang.psi.impl.statements.params.ScParameterImpl
+
 
 /**
   * Created by ssdmitriev on 04.02.16.
@@ -16,19 +16,28 @@ import org.jetbrains.plugins.scala.lang.psi.impl.statements.ScFunctionDefinition
 class UnusedFunctionParameterInspection
   extends AbstractInspection("UnusedFunctionParameter", "function parameter not used") {
   override def actionFor(holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
-    case p: ScFunctionDefinition => findUnusedParameters(p)
+    case expr: ScFunctionDefinition =>
+      expr.parameters.foreach(parameter =>
+        if (findUnusedParameter(expr, parameter)) {
+          val message = "function parameter not used"
+          holder.registerProblem(parameter, message)
+        })
   }
-  def findUnusedParameters(f: ScFunctionDefinition) = {
-    val visitor = new ScalaRecursiveElementVisitor(){
-      val localF = f
-      //Override also visitReferenceExpression! and visitTypeProjection!
-      override def visitReference(ref: ScReferenceElement): Unit = {
-        ""
-      }
+
+  def findUnusedParameter(f: ScFunctionDefinition, parameter: ScParameter): Boolean = {
+    var unusedParameter = true
+    val visitor = new ScalaRecursiveElementVisitor() {
       override def visitReferenceExpression(ref: ScReferenceExpression): Unit = {
-        ""
+        if (ref.resolve().getText == parameter.getText) {
+          ref.resolve() match {
+            case p: ScParameterImpl =>
+              unusedParameter = false
+            case _ =>
+          }
+        }
       }
     }
     f.body.foreach(_.accept(visitor))
+    unusedParameter
   }
 }
