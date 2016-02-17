5944f62c90d2d7e392c398504e7a4de15487f1d4
katejim <katejim@mail.ru>
2016-02-17 20:07:45 +0300

    take scope into consideration while completion. fix SCL-6617, this include sort by scope with parent scopes (cherry picked from commit 1005d18)

---
+++ src/org/jetbrains/plugins/scala/lang/completion/weighter/ScalaScopeWeigher.scala
@@ -0,0 +1,38 @@
+package org.jetbrains.plugins.scala.lang.completion.weighter
+
+import com.intellij.codeInsight.completion.{CompletionLocation, CompletionWeigher}
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
+import org.jetbrains.plugins.scala.lang.completion.ScalaCompletionUtil
+import org.jetbrains.plugins.scala.lang.completion.lookups.ScalaLookupItem
+
+/**
+  * Created by kate
+  * on 2/17/16
+  */
+class ScalaScopeWeigher extends CompletionWeigher {
+  def computeLevelsBetween(it: Iterator[PsiElement], y: PsiElement): Option[Int] = {
+    val idx = it.indexOf(y)
+    if (idx == -1) None else Some(-idx)
+  }
+
+  def checkByContext(first: PsiElement, second: PsiElement): Option[Int] = {
+    if (PsiTreeUtil.isContextAncestor(second, first, true))
+      computeLevelsBetween(first.contexts, second)
+    else None
+  }
+
+  override def weigh(element: LookupElement, location: CompletionLocation): Comparable[_] = {
+    val completionPosition = ScalaCompletionUtil.positionFromParameters(location.getCompletionParameters)
+
+    ScalaLookupItem.original(element) match {
+      case sl: ScalaLookupItem =>
+        if (sl.element.scopes.hasNext)
+          checkByContext(completionPosition, sl.element.scopes.next()).getOrElse(Int.MinValue)
+        else null
+      case _ => null
+    }
+  }
+}
