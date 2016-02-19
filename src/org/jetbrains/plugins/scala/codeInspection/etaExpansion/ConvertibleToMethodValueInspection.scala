5f33f338b3a3e6d22fbf4ffd0535f5901dd80686
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-02-19 19:53:07 +0300

    ConvertibleToMethodValueInspection disabled if there is a sub-expression which may change it's value #SCL-9682 fixed

--- src/org/jetbrains/plugins/scala/codeInspection/etaExpansion/ConvertibleToMethodValueInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/etaExpansion/ConvertibleToMethodValueInspection.scala
@@ -3,13 +3,19 @@ package codeInspection.etaExpansion
 
 import com.intellij.codeInspection.{ProblemHighlightType, ProblemsHolder}
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
+import com.intellij.psi._
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.plugins.scala.codeInspection.collections.MethodRepr
 import org.jetbrains.plugins.scala.codeInspection.etaExpansion.ConvertibleToMethodValueInspection._
 import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, AbstractInspection, InspectionBundle}
+import org.jetbrains.plugins.scala.extensions.{Both, PsiModifierListOwnerExt, ResolvesTo}
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScConstructor
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScVariable
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScClassParameter
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportStmt
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.lang.psi.types.ScFunctionType
 import org.jetbrains.plugins.scala.lang.psi.types.result.Success
@@ -29,16 +35,40 @@ class ConvertibleToMethodValueInspection extends AbstractInspection(inspectionId
     case MethodRepr(expr, _, Some(ref), _)
       if ref.bind().exists(srr => srr.implicitType.nonEmpty || srr.implicitFunction.nonEmpty) =>
       //do nothing if implicit conversions are involved
-    case MethodRepr(expr, _, Some(_), args) =>
-      if (args.nonEmpty && args.forall(arg => arg.isInstanceOf[ScUnderscoreSection] && ScUnderScoreSectionUtil.isUnderscore(arg)))
+    case MethodRepr(expr, Some(qual), Some(_), args) =>
+      if (allArgsUnderscores(args) && onlyStableValuesUsed(qual))
         registerProblem(holder, expr, InspectionBundle.message("convertible.to.method.value.anonymous.hint"))
     case und: ScUnderscoreSection if und.bindingExpr.isDefined =>
       val isInParameterOfParameterizedClass = PsiTreeUtil.getParentOfType(und, classOf[ScClassParameter]) match {
         case null => false
         case cp => cp.containingClass.hasTypeParameters
       }
-      if (!isInParameterOfParameterizedClass)
+      def checkStable() = und.bindingExpr.get match {
+        case ScReferenceExpression.withQualifier(qual) => onlyStableValuesUsed(qual)
+        case e => onlyStableValuesUsed(e)
+      }
+      if (!isInParameterOfParameterizedClass && checkStable())
         registerProblem(holder, und, InspectionBundle.message("convertible.to.method.value.eta.hint"))
+  }
+
+  private def allArgsUnderscores(args: Seq[ScExpression]): Boolean = {
+    args.nonEmpty && args.forall(arg => arg.isInstanceOf[ScUnderscoreSection] && ScUnderScoreSectionUtil.isUnderscore(arg))
+  }
+
+  private def onlyStableValuesUsed(qual: ScExpression): Boolean = {
+    def isStable(named: PsiNamedElement) = ScalaPsiUtil.nameContext(named) match {
+      case cp: ScClassParameter => !cp.isVar
+      case f: PsiField => f.hasFinalModifier
+      case o: ScObject => o.isLocal || ScalaPsiUtil.hasStablePath(o)
+      case _: PsiMethod | _: ScVariable => false
+      case _ => true
+    }
+
+    qual.depthFirst(e => !e.isInstanceOf[ScImportStmt]).forall {
+      case _: ScNewTemplateDefinition => false
+      case Both(_: ScReferenceExpression | ScConstructor.byReference(_), ResolvesTo(named: PsiNamedElement)) => isStable(named)
+      case _ => true
+    }
   }
 
   private def registerProblem(holder: ProblemsHolder, expr: ScExpression, hint: String) {
