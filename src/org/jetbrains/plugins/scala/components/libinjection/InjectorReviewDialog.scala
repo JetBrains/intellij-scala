c95af33fcb9e25adf35d818d809260b359e776b0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-03-01 17:38:03 +0300

    libinjection: add file navigation buttons to review dialog

--- src/org/jetbrains/plugins/scala/components/libinjection/InjectorReviewDialog.scala
+++ src/org/jetbrains/plugins/scala/components/libinjection/InjectorReviewDialog.scala
@@ -1,20 +1,24 @@
 package org.jetbrains.plugins.scala.components.libinjection
 
 import java.awt.event.{ActionEvent, ActionListener}
-import java.awt.{BorderLayout, Button, CardLayout}
-import javax.swing.{JComponent, JDialog, JPanel}
+import java.awt.{BorderLayout, Button, CardLayout, ComponentOrientation}
+import javax.swing.{Action, JComponent, JDialog, JPanel}
 
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.ui.JBCardLayout
 
 /**
   * Created by mucianm on 01.03.16.
   */
 class InjectorReviewDialog(project: Project, manifest: LibraryInjectorLoader#AttributedManifest) extends DialogWrapper(project, false) {
 
+  val layout = new CardLayout()
+  var editorsPanel: JPanel = null
   val editors = {
     val jar = manifest._1.jarPath.dropRight(1)
     manifest._2
@@ -31,20 +35,20 @@ class InjectorReviewDialog(project: Project, manifest: LibraryInjectorLoader#Att
 
   override def createCenterPanel(): JComponent = {
     val panel = new JPanel(new BorderLayout())
-    val layout = new CardLayout()
-    val editorsPanel = new JPanel(layout)
+    editorsPanel = new JPanel(layout)
     panel.add(editorsPanel, BorderLayout.CENTER)
-    editors.headOption.map(e=>editorsPanel.add(e.getComponent))
-    val nextbutton = new Button("Next")
-    nextbutton.addActionListener(new ActionListener {
-      override def actionPerformed(actionEvent: ActionEvent): Unit = layout.next(editorsPanel)
-    })
-    val prevbutton = new Button("Next")
-    prevbutton.addActionListener(new ActionListener {
-      override def actionPerformed(actionEvent: ActionEvent): Unit = layout.next(editorsPanel)
-    })
-    panel.add(prevbutton, BorderLayout.PAGE_START)
+    editors.map(e=>editorsPanel.add(e.getComponent))
     panel
+  }
+
+  override def createLeftSideActions(): Array[Action] = {
+    val next = new DialogWrapperAction("Next") {
+      override def doAction(e: ActionEvent): Unit = layout.next(editorsPanel)
+    }
+    val prev = new DialogWrapperAction("Prev") {
+      override def doAction(e: ActionEvent): Unit = layout.previous(editorsPanel)
+    }
+    Array(next, prev)
   }
 
   override def dispose(): Unit = {
