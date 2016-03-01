39174e653bfdce4fc93d987cdbc1343ba5d2b3be
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-01 11:18:15 +0300

    NameBooleanParametersInspection optimized (check if expression is a boolean literal first) and simplified #SCL-9916 fixed

--- src/org/jetbrains/plugins/scala/codeInspection/parameters/NameBooleanParametersInspectionBase.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/parameters/NameBooleanParametersInspectionBase.scala
@@ -2,17 +2,15 @@ package org.jetbrains.plugins.scala
 package codeInspection.parameters
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemHighlightType, ProblemsHolder}
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{PsiElement, PsiElementVisitor}
+import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
 import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScBooleanLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
+import org.jetbrains.plugins.scala.lang.psi.types.StdType
 import org.jetbrains.plugins.scala.lang.psi.types.nonvalue.Parameter
-import org.jetbrains.plugins.scala.lang.psi.types.result.TypingContext
 import org.jetbrains.plugins.scala.util.IntentionUtils
 
 import scala.collection.Seq
@@ -28,66 +26,48 @@ abstract class NameBooleanParametersInspectionBase extends LocalInspectionTool {
     new ScalaElementVisitor {
       override def visitMethodCallExpression(mc: ScMethodCall) {
         if (mc == null || mc.args == null || mc.args.exprs.isEmpty) return
-        if (isIgnoreSingleParameter) mc.getInvokedExpr match {
-          case ref: ScReferenceExpression => ref.bind() match {
-            case Some(srr) =>
-              val targets = Seq(srr.element) ++ srr.innerResolveResult.map(_.getElement)
-              if (targets.exists {
-                case fun: ScFunction => fun.parameters.size == 1
-                case _ => false
-              }) return
-            case _ =>
-          }
-          case _ =>
-        }
-        for (expr <- mc.args.exprs) {
+        if (isIgnoreSingleParameter && isSingleParamMethodCall(mc)) return
+        val argList = mc.args
+        for (expr <- argList.exprs) {
           expr match {
-            case literal: ScLiteral if isBooleanType(expr) &&
-                    IntentionUtils.addNameToArgumentsFix(literal, onlyBoolean = true).isDefined &&
-                    (expr.getNode.getFirstChildNode.getElementType == ScalaTokenTypes.kTRUE ||
-                    expr.getNode.getFirstChildNode.getElementType == ScalaTokenTypes.kFALSE) =>
-              holder.registerProblem(holder.getManager.createProblemDescriptor(expr,
-                InspectionBundle.message("name.boolean"),
-                new NameBooleanParametersQuickFix(literal),
-                ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly))
+            case lit @ ScBooleanLiteral(_) if isArgForBooleanParam(expr, argList) &&
+                    IntentionUtils.addNameToArgumentsFix(expr, onlyBoolean = true).isDefined =>
+              val descriptor = holder.getManager.createProblemDescriptor(expr, InspectionBundle.message("name.boolean"),
+                new NameBooleanParametersQuickFix(lit), ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly)
+              holder.registerProblem(descriptor)
             case _ =>
           }
         }
       }
 
-      def isBooleanType(element: PsiElement): Boolean = {
-        val containingArgList: Option[ScArgumentExprList] = element.parents.collectFirst {
-          case al: ScArgumentExprList if !al.isBraceArgs => al
-        }
-        containingArgList match {
-          case Some(al) =>
-            val index = al.exprs.indexWhere(argExpr => PsiTreeUtil.isAncestor(argExpr, element, false))
-            index match {
-              case -1 => false
-              case i =>
-                val argExprsToNamify = al.exprs.drop(index)
-                val argsAndMatchingParams: Seq[(ScExpression, Option[Parameter])] = argExprsToNamify.map {
-                  arg => (arg, al.parameterOf(arg))
-                }
-                argsAndMatchingParams.exists {
-                  case (expr, Some(param)) =>
-                    val paramInCode = param.paramInCode.orNull
-                    if (paramInCode == null) return false
-                    if (!paramInCode.isValid) return false //todo: find why it can be invalid?
-                    val realParameterType = paramInCode.getRealParameterType(TypingContext.empty).getOrElse(null)
-                    if (realParameterType == null) return false
-                    else if (realParameterType.canonicalText == "Boolean") return true
-                    else return false
-                  case _ => return false
-                }
-            }
-          case None => false
+      def isArgForBooleanParam(expr: ScExpression, argList: ScArgumentExprList): Boolean = {
+        argList.parameterOf(expr).exists(isBooleanParam)
+      }
+
+      def isBooleanParam(p: Parameter): Boolean = {
+        if (p.isRepeated) false
+        else {
+          val typeElem = p.paramInCode.flatMap(_.typeElement)
+          typeElem.exists(_.calcType.equiv(StdType.BOOLEAN))
         }
       }
+
+      def isSingleParamMethodCall(mc: ScMethodCall): Boolean = {
+        mc.getInvokedExpr match {
+          case ref: ScReferenceExpression =>
+            ref.bind().exists { srr =>
+              val targets = (Seq(srr.element) ++ srr.innerResolveResult.map(_.getElement)).filterBy(classOf[ScFunction])
+              targets.exists(_.parameters.size == 1)
+            }
+          case _ => false
+        }
+      }
+
     }
   }
 
   def isIgnoreSingleParameter: Boolean
+
   def setIgnoreSingleParameter(value: Boolean)
 
 }
