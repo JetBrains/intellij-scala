32e3cb074df713900a74793108e1436564afd0d3
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-15 12:40:56 +0300

    missed condition fixed #SCL-9116 fixed (cherry picked from commit 9ff355f)

--- src/org/jetbrains/plugins/scala/codeInspection/collections/OptionEqualsSomeToContainsInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/collections/OptionEqualsSomeToContainsInspection.scala
@@ -29,6 +29,7 @@ object OptionNotEqualsSomeToNotContains extends SimplificationType {
   override def hint: String = InspectionBundle.message("replace.with.not.contains")
 
   override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case _ if expr.scalaLanguageLevel.exists(_ <= ScalaLanguageLevel.Scala_2_10) => None
     case qual `!=` `scalaSome`(elem) if isOption(qual) =>
       Some(replace(expr).withText(s"!${qual.getText}.contains(${elem.getText})").highlightAll)
     case `scalaSome`(elem) `!=` qual if isOption(qual) =>
