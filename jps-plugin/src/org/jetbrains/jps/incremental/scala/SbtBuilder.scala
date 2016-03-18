49f7982194ce44654c9409c91df8d8c083d41d05
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-18 20:23:59 +0300

    disable sbt incremental compiler for non-scala project + possible NPE fixed #SCL-10039 fixed (cherry picked from commit faa82bc)

--- jps-plugin/src/org/jetbrains/jps/incremental/scala/SbtBuilder.scala
+++ jps-plugin/src/org/jetbrains/jps/incremental/scala/SbtBuilder.scala
@@ -117,7 +117,7 @@ class SbtBuilder extends ModuleLevelBuilder(BuilderCategory.TRANSLATOR) {
   }
 
   private def isDisabled(context: CompileContext): Boolean = {
-    projectSettings(context).getIncrementalityType != IncrementalityType.SBT
+    projectSettings(context).getIncrementalityType != IncrementalityType.SBT || !isScalaProject(context.getProjectDescriptor.getProject)
   }
 
   private def hasDirtyFilesOrDependencies(context: CompileContext, chunk: ModuleChunk,
