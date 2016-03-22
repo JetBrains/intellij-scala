3bbbfb0273e02925909c32026bde563d31d3b56b
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2016-03-22 20:20:22 +0300

    Test for #SCL-9212 created

---
+++ test/org/jetbrains/plugins/scala/failed/checkAccess/CheckAccessTest.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.failed.checkAccess
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.checkers.checkPrivateAccess.CheckPrivateAccessTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * User: Dmitry.Naydanov
+  * Date: 22.03.16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class CheckAccessTest extends CheckPrivateAccessTestBase {
+  override def folderPath: String = super.folderPath + "failed/"
+  
+  def testSCL9212() = doTest()
+}
