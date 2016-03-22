e5cf151e6d73ed9fde7460920d05e4fd16c77d34
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-22 16:27:01 +0300

    Test for parser error SCL-5343

---
+++ test/org/jetbrains/plugins/scala/failed/parser/FailedParserTest.java
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.scala.failed.parser;
+
+/**
+ * @author Nikolay.Tropin
+ */
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.DebugUtil;
+import junit.framework.Test;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.plugins.scala.PerfCycleTests;
+import org.jetbrains.plugins.scala.testcases.BaseScalaFileSetTestCase;
+import org.jetbrains.plugins.scala.util.TestUtils;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+
+@RunWith(AllTests.class)
+@Category(value = {PerfCycleTests.class})
+public class FailedParserTest extends BaseScalaFileSetTestCase {
+    @NonNls
+    private static final String DATA_PATH = "/parser/failed";
+
+    public FailedParserTest() {
+        super(System.getProperty("path") != null ?
+                System.getProperty("path") :
+                TestUtils.getTestDataPath() + DATA_PATH
+        );
+    }
+
+    public String transform(String testName, String[] data) throws Exception {
+        String fileText = data[0];
+        PsiFile psiFile = TestUtils.createPseudoPhysicalScalaFile(getProject(), fileText);
+
+        return DebugUtil.psiToString(psiFile, false).replace(":" + psiFile.getName(), "");
+
+    }
+
+    public static Test suite() {
+        return new FailedParserTest();
+    }
+
+}
+
