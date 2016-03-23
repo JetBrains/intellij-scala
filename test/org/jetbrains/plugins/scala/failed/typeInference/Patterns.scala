cf2b099f8c8d88aaf39557afc09807d30d7ad228
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-03-23 12:56:40 +0300

    add test for #SCL-9137

--- test/org/jetbrains/plugins/scala/failed/typeInference/Patterns.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/Patterns.scala
@@ -1,11 +1,14 @@
 package org.jetbrains.plugins.scala.failed.typeInference
 
 import junit.framework.Assert
+import org.jetbrains.plugins.scala.PerfCycleTests
 import org.jetbrains.plugins.scala.lang.typeInference.TypeInferenceTestBase
+import org.junit.experimental.categories.Category
 
 /**
   * Created by mucianm on 22.03.16.
   */
+@Category(Array(classOf[PerfCycleTests]))
 class Patterns extends TypeInferenceTestBase {
   override def folderPath: String = super.folderPath + "bugs5/"
 
