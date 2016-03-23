08e22397c5c5e2c9ee1624086d1ea3f80baadc60
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-23 16:42:37 +0300

    test for SCL-8083

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/ConstructorFromJavaResolveTest.scala
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReferenceElement
+import org.jetbrains.plugins.scala.lang.resolve.ScalaResolveTestCase
+import org.junit.Assert._
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Nikolay.Tropin
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class ConstructorFromJavaResolveTest extends ScalaResolveTestCase {
+  override def folderPath(): String = s"${super.folderPath()}resolve/failed/constructorFromJava/"
+
+  override def rootPath(): String = folderPath()
+
+  def testScl8083(): Unit = {
+    findReferenceAtCaret match {
+      case st: ScStableCodeReferenceElement =>
+        val variants = st.resolveAllConstructors
+        assertTrue("Single resolve expected", variants.length == 1)
+    }
+  }
+
+}
