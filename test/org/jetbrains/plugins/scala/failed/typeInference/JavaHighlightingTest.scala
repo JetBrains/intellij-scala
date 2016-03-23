d3cc3d40595e1f4278fbb7837dd095d9a8cd82b6
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-03-23 15:49:11 +0300

    add test for #SCL-7525

--- test/org/jetbrains/plugins/scala/failed/typeInference/JavaHighlightingTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/JavaHighlightingTest.scala
@@ -1,6 +1,8 @@
 package org.jetbrains.plugins.scala.failed.typeInference
 
+import com.intellij.psi.PsiDocumentManager
 import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.annotator.{AnnotatorHolderMock, Error, Message, ScalaAnnotator}
 import org.jetbrains.plugins.scala.javaHighlighting.JavaHighltightingTestBase
 import org.junit.experimental.categories.Category
 
@@ -28,5 +30,27 @@ class JavaHighlightingTest extends JavaHighltightingTestBase {
         |}
       """.stripMargin
     assertNoErrors(messagesFromJavaCode(scala, java, "Test"))
+  }
+
+  def testSCL7525() = {
+    val scala =
+      """
+        |package SCL7525
+        |object Test {
+        |  new Foo(new Foo.ArgsBar)
+        |}
+      """.stripMargin
+
+    val java =
+      """
+        |package SCL7525;
+        |public class Foo {
+        |    public Foo(Args a) { }
+        |    public static class Args<T extends Args<T>> { }
+        |    public static class ArgsBar extends Args<ArgsBar> { }
+        |}
+      """.stripMargin
+
+    assertNoErrors(messagesFromScalaCode(scala, java))
   }
 }
