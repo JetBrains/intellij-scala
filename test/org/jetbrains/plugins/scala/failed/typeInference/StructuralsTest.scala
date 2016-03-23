8e20c8cb52440a53e1279837ba3f8c9bb8f1110a
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2016-03-23 18:48:17 +0300

    Test for #SCL-5423 created

--- test/org/jetbrains/plugins/scala/failed/typeInference/StructuralsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/StructuralsTest.scala
@@ -13,4 +13,22 @@ class StructuralsTest extends TypeInferenceTestBase {
   override def folderPath: String = super.folderPath + "bugs5/"
 
   def testSCL8689(): Unit = doTest()
+  
+  def testSCL5423() = doTest {
+    """
+      |trait Test {
+      |  trait SettValue {
+      |    type T
+      |    def value: T
+      |  }
+      |  trait Foo { def foo: Int }
+      |  type Sett <: SettValue
+      |  type BSetting <: Sett { type T = Foo }
+      |  def foo(b: BSetting) = /*start*/b.value.foo/*end*/
+      |  
+      |}
+      |
+      |//Int
+    """.stripMargin
+  }
 }
