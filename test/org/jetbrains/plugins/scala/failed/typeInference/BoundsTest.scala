46f92051a7f9ec952d1e83ee474493a26da0dc59
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-24 16:13:09 +0300

    #SCL-9755 Test created

--- test/org/jetbrains/plugins/scala/failed/typeInference/BoundsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/BoundsTest.scala
@@ -15,4 +15,20 @@ class BoundsTest extends TypeInferenceTestBase {
   def testSCL4373(): Unit = doTest() //blinking test
 
   def testSCL7085(): Unit = doTest()
+
+  def testSCL9755(): Unit = {
+    val text =
+      s"""object IntelliJ {
+        |  trait Base[T]
+        |  case object StringExample extends Base[String]
+        |
+        |  implicit val baseStringEvidence = StringExample
+        |
+        |  def apply[T: Base](semantic: Base[T]): T = semantic match {
+        |    case StringExample => $START"string"$END
+        |  }
+        |}
+        |//T""".stripMargin
+    doTest(text)
+  }
 }
