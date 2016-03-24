c266215f6ff686e03a1fb06b108ea325eb7d4543
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2016-03-24 17:49:18 +0300

    Test for #SCL-2045 created

--- test/org/jetbrains/plugins/scala/failed/typeInference/Primitives.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/Primitives.scala
@@ -12,4 +12,15 @@ class Primitives extends TypeInferenceTestBase {
   override def folderPath: String = super.folderPath + "bugs5/"
 
   def testSCL7521() = doTest()
+  
+  def testSCL2045() = doTest(
+    """    def returnsANumber = {
+      |      if (1==1) {
+      |        /*start*/0/*end*/
+      |      } else {
+      |        0.0
+      |      }
+      |    }
+      |    
+      |    //Double""".stripMargin)
 }
