82526e6f7564bf8ebb37f6f9cd911ed8837ca31a
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-03-25 16:20:04 +0300

    add test for #SCL-7878

---
+++ test/org/jetbrains/plugins/scala/failed/lexer/LexerTest.scala
@@ -0,0 +1,35 @@
+package org.jetbrains.plugins.scala.failed.lexer
+
+import com.intellij.lexer.Lexer
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+import org.jetbrains.plugins.scala.lang.lexer.ScalaLexer
+import org.junit.experimental.categories.Category
+
+
+/**
+  * @author mucianm 
+  * @since 25.03.16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class LexerTest extends SimpleTestCase {
+  protected var lexer: Lexer = new ScalaLexer()
+
+  def testSCL7878(): Unit = {
+    assert(compare(
+      "val x = ???//TODO",
+        Seq("val", " ", "x", " ", "=", " ", "???", "//", "TODO")
+      )
+    )
+  }
+
+  protected def compare(text: String, tokens: Seq[String]): Boolean = {
+    lexer.start(text)
+    val buffer = scala.collection.mutable.ArrayBuffer[String]()
+    while(lexer.getTokenType != null) {
+      buffer += lexer.getTokenSequence.toString
+      lexer.advance()
+    }
+    buffer == tokens
+  }
+}
