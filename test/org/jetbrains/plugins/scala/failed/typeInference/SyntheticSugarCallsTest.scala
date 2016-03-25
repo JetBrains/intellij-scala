9afbbe53814808610f5f85b5f67d29a199b4b0d0
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2016-03-25 16:46:25 +0300

    Test for #SCL-7112 tag: Test created

--- test/org/jetbrains/plugins/scala/failed/typeInference/SyntheticSugarCallsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/SyntheticSugarCallsTest.scala
@@ -13,4 +13,32 @@ class SyntheticSugarCallsTest extends TypeInferenceTestBase {
   override def folderPath: String = super.folderPath + "bugs5/"
 
   def testSCL7503(): Unit = doTest()
+
+  def testSCL7112(): Unit = doTest(
+    """
+      |case class Property[T](val name: String="", private val default: Option[T]=None) {
+      |
+      |  private var currentValue = default
+      |
+      |  def value = currentValue
+      |  def value_=(newValue: Option[T]): Boolean = {
+      |    val oldValue = currentValue
+      |    currentValue = newValue
+      |    if(oldValue != newValue) {}
+      |    false
+      |  }
+      |}
+      |
+      |class WPModel {
+      |
+      |  val slid = new Property[String]("slid") {
+      |
+      |    override def value_=(user: Option[String]) = {
+      |      /*start*/super.value = user.map(_.toUpperCase())/*end*/
+      |    }
+      |  }
+      |}
+      |//Boolean
+    """.stripMargin.trim
+  )
 }
