07de0a90c83a236ce10572d5caa72449124c3ab4
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-03-25 15:07:47 +0300

    add test for #SCL-3074

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/PrimitivesConformanceTest.scala
@@ -0,0 +1,19 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.typeConformance.TypeConformanceTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * @author mucianm 
+  * @since 25.03.16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class PrimitivesConformanceTest extends TypeConformanceTestBase{
+  def test3074() = doTest(
+    """
+      |val a: Array[Byte] = Array(1, 2, 3)
+      |
+      |/* True */
+    """.stripMargin)
+}
