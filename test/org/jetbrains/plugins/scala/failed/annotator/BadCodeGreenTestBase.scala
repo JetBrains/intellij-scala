2214f92a05b4f7c93969f1d5ab89d709fdd7ee31
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-25 14:28:30 +0300

    #SCL-9402 Test created

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/BadCodeGreenTestBase.scala
@@ -0,0 +1,24 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+
+import scala.collection.JavaConverters._
+
+/**
+  * @author Nikolay.Tropin
+  */
+abstract class BadCodeGreenTestBase extends ScalaLightCodeInsightFixtureTestAdapter {
+  def doTest(text: String): Unit = {
+    myFixture.configureByText("dummy.scala", text)
+    val caretIndex = text.indexOf(CARET_MARKER)
+    def isAroundCaret(info: HighlightInfo) = caretIndex == -1 || new TextRange(info.getStartOffset, info.getEndOffset).contains(caretIndex)
+    val infos = myFixture.doHighlighting().asScala
+
+    val warnings = infos.filter(i => StringUtil.isNotEmpty(i.getDescription) && isAroundCaret(i))
+
+    assert(warnings.nonEmpty, "No highlightings found")
+  }
+}
---
+++ test/org/jetbrains/plugins/scala/failed/annotator/TypeArgInferenceTest.scala
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Nikolay.Tropin
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class TypeArgInferenceTest extends BadCodeGreenTestBase {
+  def testScl9402(): Unit = {
+    val text =
+      s"""object ErrorHighlightingExample extends App {
+        |  import scala.language.existentials
+        |
+        |  class Network {
+        |
+        |    class Member(val name: String)
+        |
+        |    def join(name: String) = ???
+        |  }
+        |
+        |  def processSpecificNetwork[M <: n.Member forSome {val n : Network}](m1: M, m2: M) = (m1, m2)
+        |
+        |  val chatter = new Network
+        |  val myFace = new Network
+        |
+        |  val fred: chatter.Member = chatter.join("Fred")
+        |  val barney: myFace.Member = myFace.join("Barney")
+        |
+        |  processSpecificNetwork(fred, bar${CARET_MARKER}ney)
+        |}
+      """.stripMargin
+    doTest(text)
+  }
+}
