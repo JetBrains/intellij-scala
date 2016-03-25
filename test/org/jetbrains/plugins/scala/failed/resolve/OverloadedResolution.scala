febfcf63a4f32a773e36a304172319efbb5c6250
katejim <katejim@mail.ru>
2016-03-25 16:28:56 +0300

    add test for #SCL-2911 tag: Test created

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/OverloadedResolution.scala
@@ -0,0 +1,13 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by kate on 3/25/16.
+  */
+
+@Category(Array(classOf[PerfCycleTests]))
+class OverloadedResolution extends FailedResolveTest("overloadedResolution") {
+  def testSCL2911(): Unit = doTest()
+}
