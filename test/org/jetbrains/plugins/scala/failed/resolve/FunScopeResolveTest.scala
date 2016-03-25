3ee975c8ea853b2d5d09e0577b71f1e26fdc3029
Roman Shein <marso.des@gmail.com>
2016-03-25 05:41:32 +0300

    Test for #SCL-5869

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/FunScopeResolveTest.scala
@@ -0,0 +1,35 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.intellij.lang.annotations.Language
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.annotator._
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBody
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 24.03.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class FunScopeResolveTest extends SimpleTestCase {
+  def testSCL5869(): Unit = {
+    val code =
+      """
+        |class SCL5869(private[this] var param:Int) {
+        |  def param():Int = this.param
+        |}
+      """.stripMargin
+    assertNothing(messages(code))
+  }
+
+  def messages(@Language(value = "Scala") code: String): List[Message] = {
+    val annotator = new ScopeAnnotator {}
+    val mock = new AnnotatorHolderMock
+
+    val templateBody = code.parse.depthFirst.find(_.isInstanceOf[ScTemplateBody]).get.asInstanceOf[ScTemplateBody]
+
+    annotator.annotateScope(templateBody, mock)
+    mock.annotations
+  }
+}
