00d6e9d75ffffe48bcbe46293b722f68d838c18b
Roman Shein <marso.des@gmail.com>
2016-03-28 20:04:27 +0300

    Test for #SCL-3807

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/OverridingAnnotatorTest2.scala
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 28.03.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class OverridingAnnotatorTest2 extends ScalaLightCodeInsightFixtureTestAdapter {
+  //TODO: the issue does not reproduce when test is performed  using OverridingAnnotatorTest
+  def testSCL3807(): Unit = {
+    checkTextHasNoErrors(
+      """
+        |trait A {
+        |  def foo(f: (=> A) => Int) = {_: A => 42}
+        |}
+        |
+        |object A extends A{
+        |  def foo(f: (A) => Int) = null
+        |}
+      """.stripMargin)
+  }
+}
