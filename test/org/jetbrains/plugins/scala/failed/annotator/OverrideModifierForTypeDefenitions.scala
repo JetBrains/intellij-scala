4212bb86426c0000eab822539bb3455ac443c553
katejim <katejim@mail.ru>
2016-03-28 18:55:18 +0300

    add category

--- test/org/jetbrains/plugins/scala/failed/annotator/OverrideModifierForTypeDefenitions.scala
+++ test/org/jetbrains/plugins/scala/failed/annotator/OverrideModifierForTypeDefenitions.scala
@@ -1,12 +1,15 @@
 package org.jetbrains.plugins.scala.failed.annotator
 
+import org.jetbrains.plugins.scala.PerfCycleTests
 import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.junit.experimental.categories.Category
 
 /**
   * Created by kate on 3/28/16.
   */
 
 //check in ScalaAnnotator with ModifierChecker.checkModifiers
+@Category(Array(classOf[PerfCycleTests]))
 class OverrideModifierForTypeDefenitions extends ScalaLightCodeInsightFixtureTestAdapter{
   def testSCL9700(): Unit = {
     checkTextHasNoErrors(
