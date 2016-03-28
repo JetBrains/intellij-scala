f1fa8375d276114552bfcb6b825d0146987fe983
Roman Shein <marso.des@gmail.com>
2016-03-28 18:20:03 +0300

    Test for #SCL-3856

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/VarArgsInCurlyBracesTest.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.{ScalaLightCodeInsightFixtureTestAdapter, SimpleTestCase}
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 28.03.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class VarArgsInCurlyBracesTest extends ScalaLightCodeInsightFixtureTestAdapter {
+  def testSCL3856() = {
+    val code =
+      """
+        |val array = Array[String]()
+        |Set.apply[String]{ array: _* }
+      """.stripMargin
+    checkTextHasNoErrors(code)
+  }
+}
