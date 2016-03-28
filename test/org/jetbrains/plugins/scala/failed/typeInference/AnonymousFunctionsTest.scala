c6bf0f3f13170d70c1dc4abcad04f9b561121e4b
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2016-03-28 15:14:10 +0300

    Test for #SCL-9432 tag: Test created

--- test/org/jetbrains/plugins/scala/failed/typeInference/AnonymousFunctionsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/AnonymousFunctionsTest.scala
@@ -13,4 +13,15 @@ class AnonymousFunctionsTest extends TypeInferenceTestBase {
   override def folderPath: String = super.folderPath + "bugs5/"
 
   def testSCL8267(): Unit = doTest()
+
+  def testSCL9432(): Unit = doTest {
+    """
+      |object SCL9432 {
+      |  def f(int: Int): Option[Int] = if (int % 2 == 0) Some(int) else None
+      |  def g(as: List[Int])(b: Int): Option[Int] = if (as contains b) None else f(b)
+      |  /*start*/List(1) flatMap g(List(2, 4))/*end*/
+      |}
+      |//List[Int]
+    """.stripMargin.trim
+  }
 }
