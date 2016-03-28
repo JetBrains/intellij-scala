28283cf79e427e1a82e1baf50886e182d3a0fcb9
Roman Shein <marso.des@gmail.com>
2016-03-28 03:13:11 +0300

    Test for #SCL-4990

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/ParameterizedTypeTest.scala
@@ -0,0 +1,29 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+
+/**
+  * @author Roman.Shein
+  * @since 28.03.2016.
+  */
+class ParameterizedTypeTest extends ScalaLightCodeInsightFixtureTestAdapter {
+
+  def testSCL4990() = {
+    val text =
+      """
+        |object Example {
+        |  def mangle[A, M[_]](m: M[A]) = {}
+        |
+        |  case class OneParameterType[A](value: A)
+        |  case class TwoParameterType[S, A](value: (S, A))
+        |  type Alias[A] = TwoParameterType[String, A]
+        |
+        |  val a: OneParameterType[Int] = OneParameterType(1)
+        |  val b: Alias[Int] = TwoParameterType(("s", 1))
+        |  mangle(a)
+        |  mangle(b)
+        |}
+      """.stripMargin
+    checkTextHasNoErrors(text)
+  }
+}
