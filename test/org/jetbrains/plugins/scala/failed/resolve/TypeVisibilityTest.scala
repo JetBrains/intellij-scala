289867b4a896a4d31be7d0a0c8d510f4cdd1abe8
Roman Shein <marso.des@gmail.com>
2016-03-28 04:19:22 +0300

    Test for #SCL-9178

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/TypeVisibilityTest.scala
@@ -0,0 +1,44 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 28.03.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class TypeVisibilityTest extends ScalaLightCodeInsightFixtureTestAdapter {
+  def testSCL9178() = {
+    val text =
+      """
+        |trait Example {
+        |  protected type EXAMPLE <: Example
+        |  def example() : EXAMPLE
+        |}
+        |
+        |trait ExtendedExample extends Example {
+        |  final protected type EXAMPLE = ExtendedExample
+        |  def myDef() : Unit
+        |}
+        |
+        |trait Something extends Example {
+        |  val exampleVal = example()
+        |  def falseErrorHere() = {
+        |    val extendedSomethingInstance = new ExtendedSomethingClass
+        |    extendedSomethingInstance.exampleVal.myDef  //<-- Cannot resolve symbol myDef
+        |  }
+        |}
+        |
+        |trait ExtendedSomething extends Something with ExtendedExample {
+        |}
+        |
+        |class ExtendedSomethingClass extends ExtendedSomething {
+        |  def myDef() : Unit = {}
+        |  def example() = new ExtendedSomethingClass
+        |}
+      """.stripMargin
+    checkTextHasNoErrors(text)
+  }
+}
