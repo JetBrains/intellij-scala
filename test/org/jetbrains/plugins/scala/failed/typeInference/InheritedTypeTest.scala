9ef02c901b8ae846860321e633d50dd288bf97d7
Roman Shein <marso.des@gmail.com>
2016-03-28 04:48:10 +0300

    Test for #SCL-8411 Recovered lost testdata

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/InheritedTypeTest.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.typeInference.TypeInferenceTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 28.03.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class InheritedTypeTest extends TypeInferenceTestBase {
+  override def folderPath: String = super.folderPath + "bugs5/"
+
+  def testSCL8411(): Unit = doTest()
+}
---
+++ testdata/resolve/failed/scope/SCL5869.scala
@@ -0,0 +1,4 @@
+class SCL5869(private[this] var param:Int) {
+  def <ref>param():Int = this.param
+  def param_= (param:Int) {this.param = param}
+}
