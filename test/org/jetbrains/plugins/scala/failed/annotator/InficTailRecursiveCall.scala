8c7b8ec7e0271e54d04ada9c0d91f52bcd23d6ae
katejim <katejim@mail.ru>
2016-03-29 18:52:54 +0300

    add test for #SCL-8792 tag: Test created

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/InficTailRecursiveCall.scala
@@ -0,0 +1,34 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by user on 3/29/16.
+  */
+
+@Category(Array(classOf[PerfCycleTests]))
+class InficTailRecursiveCall extends ScalaLightCodeInsightFixtureTestAdapter{
+  def testSCL8792(): Unit = {
+    checkTextHasNoErrors(
+      """
+        |import scala.annotation.tailrec
+        |
+        |case class Cursor(coord: Seq[Int]) {
+        |
+        |  def head = coord.head
+        |
+        |  def tail = Cursor(coord.tail)
+        |
+        |  @tailrec
+        |  final def compare(b: Cursor): Int = {
+        |    if (head == b.head) tail compare b.tail //Error only with infix notation
+        |    else 0
+        |  }
+        |}
+        |
+      """.stripMargin
+    )
+  }
+}
