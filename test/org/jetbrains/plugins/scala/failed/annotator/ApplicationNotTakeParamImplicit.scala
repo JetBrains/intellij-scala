3efc599c15fe92bb761ef846c30018448b05497f
katejim <katejim@mail.ru>
2016-03-29 15:42:49 +0300

    add test for #SCL-9931 tag: Test created

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/ApplicationNotTakeParamImplicit.scala
@@ -0,0 +1,33 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by kate on 3/29/16.
+  */
+
+@Category(Array(classOf[PerfCycleTests]))
+class ApplicationNotTakeParamImplicit extends ScalaLightCodeInsightFixtureTestAdapter {
+  def testSCL9931(): Unit = {
+    checkTextHasNoErrors(
+      """
+        |trait Foo {
+        |  def foo(a: Int) = 1
+        |}
+        |
+        |object Foo{
+        |  def foo = 0.2
+        |
+        |  implicit def defImpl(x: Foo.type):Foo = FooImpl
+        |}
+        |
+        |object FooImpl extends Foo
+        |
+        |object Bar {
+        |  Foo.foo(1) //in (1): Application does not takes parameters
+        |}
+      """.stripMargin)
+  }
+}
