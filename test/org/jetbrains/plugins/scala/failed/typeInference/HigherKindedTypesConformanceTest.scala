f2c24bb254d1c5e68fea8502ee36ddee8472c309
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2016-03-29 13:44:32 +0300

    Test for #SCL-7319 tag: Test created

--- test/org/jetbrains/plugins/scala/failed/typeInference/HigherKindedTypesConformanceTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/HigherKindedTypesConformanceTest.scala
@@ -25,4 +25,30 @@ class HigherKindedTypesConformanceTest extends TypeConformanceTestBase {
     """.stripMargin
   )
 
+  def testSCL7319(): Unit = doTest {
+    """
+      |object SCL7319 {
+      |
+      |  trait XIndexedStateT[F[+_], -S1, +S2, +A] {
+      |    def lift[M[+_]]: XIndexedStateT[({type λ[+α]=M[F[α]]})#λ, S1, S2, A] = ???
+      |  }
+      |
+      |  type XStateT[F[+_], S, +A] = XIndexedStateT[F, S, S, A]
+      |
+      |  type XId[+X] = X
+      |
+      |
+      |  implicit def example[S, A](s: XStateT[XId, S, A]): XStateT[Option, S, A] = {
+      |    object Temp {
+      |      def foo(s: XStateT[Option, S, A]): Int = 1
+      |      def foo(s: Boolean): Boolean = false
+      |    }
+      |    /*start*/Temp.foo(s.lift[Option])/*end*/
+      |    s.lift[Option]
+      |  }
+      |}
+      |//Int
+    """.stripMargin.trim
+  }
+
 }
