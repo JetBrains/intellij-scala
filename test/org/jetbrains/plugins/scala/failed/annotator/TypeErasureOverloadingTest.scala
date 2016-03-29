c036eac3c23dac7a2d45ce47fb805473975b1a6e
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-29 20:14:10 +0300

    #SCL-9276 test created

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/TypeErasureOverloadingTest.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Nikolay.Tropin
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class TypeErasureOverloadingTest extends BadCodeGreenTestBase {
+
+  def testSCL9276(): Unit = {
+    doTest(
+      """class Example {
+        |  def foo(arg: => String) = Unit
+        |  def foo(arg: => Option[String]) = Unit
+        |  def foo(arg: => Int) = Unit
+        |  def foo(arg: => Boolean) = Unit
+        |}
+      """.stripMargin)
+  }
+}
