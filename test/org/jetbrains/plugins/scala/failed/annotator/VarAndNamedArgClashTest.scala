0265acc3700a32aa486fdf2f14c63976d5b0178d
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-30 15:28:08 +0300

    #SCL-2194 test created

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/VarAndNamedArgClashTest.scala
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Nikolay.Tropin
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class VarAndNamedArgClashTest extends BadCodeGreenTestBase {
+  def testSCL2194(): Unit = {
+    doTest(
+      """object t {
+        |  def f(x: Int) = x
+        |}
+        |object test {
+        |  var x = t.f(x = 2)
+        |}""".stripMargin)
+  }
+}
