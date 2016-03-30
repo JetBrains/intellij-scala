49c0a95008c98ee5787cf48d94cdc3c182f6f9f2
Roman Shein <marso.des@gmail.com>
2016-03-30 21:52:18 +0300

    Fixed compilation

--- test/org/jetbrains/plugins/scala/failed/resolve/MissingParamterClass.scala
+++ test/org/jetbrains/plugins/scala/failed/resolve/MissingParamterClass.scala
@@ -7,7 +7,7 @@ import org.junit.experimental.categories.Category
   * Created by kate on 3/30/16.
   */
 
-@Category(Array(classOf[PerfCycleTests])
+@Category(Array(classOf[PerfCycleTests]))
 class MissingParamterClass extends FailedResolveTest("missingParameter") {
   def testSCL8967(): Unit = doTest()
 }
