759bfdd79997193494239895bb19e4f7e7a1736c
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-03-30 19:14:35 +0300

    #SCL-7475 test created

--- test/org/jetbrains/plugins/scala/failed/typeInference/ImplicitsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/ImplicitsTest.scala
@@ -116,4 +116,32 @@ class ImplicitsTest extends TypeInferenceTestBase {
       |//Prop
     """.stripMargin
   }
+
+  def testSCL7475() = doTest {
+     s"""object tag {
+        |  def apply[U] = new Tagger[U]
+        |}
+        |
+        |trait Tagged[U]
+        |
+        |type WithTag[+T, U] = T with Tagged[U]
+        |
+        |class Tagger[U] {
+        |  def apply[T](t : T) : WithTag[T, U] = t.asInstanceOf[WithTag[T, U]]
+        |}
+        |
+        |sealed trait NoConversion
+        |
+        |object NoConversion {
+        |  import scala.language.implicitConversions
+        |
+        |  implicit def toNoConversion[T](x: T): WithTag[T, NoConversion] = tag[NoConversion](x)
+        |}
+        |
+        |def foo(withTag: WithTag[BigDecimal, NoConversion]) = println()
+        |
+        |foo(${START}BigDecimal(1.1)$END)
+        |//WithTag[BigDecimal, NoConversion]""".stripMargin
+  }
+
 }
