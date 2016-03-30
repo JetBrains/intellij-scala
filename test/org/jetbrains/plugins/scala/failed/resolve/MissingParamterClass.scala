4f180af6163978eb677ea5da40e92b594ed3e758
katejim <katejim@mail.ru>
2016-03-30 19:03:27 +0300

    add test for #SCL-8967 tag: Test created

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/MissingParamterClass.scala
@@ -0,0 +1,13 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by kate on 3/30/16.
+  */
+
+@Category(Array(classOf[PerfCycleTests])
+class MissingParamterClass extends FailedResolveTest("missingParameter") {
+  def testSCL8967(): Unit = doTest()
+}
