3d65ae49543f0e8c82836fcd8b7091d2d98d23a7
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-04-03 15:45:36 +0300

    type bounds text

--- src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScTypeBoundsOwner.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScTypeBoundsOwner.scala
@@ -4,6 +4,8 @@ package psi
 package api
 package toplevel
 
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes._
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
 import org.jetbrains.plugins.scala.lang.psi.types.result.TypeResult
@@ -13,12 +15,27 @@ trait ScTypeBoundsOwner extends ScalaPsiElement {
   def upperBound: TypeResult[ScType]
   def viewBound: Seq[ScType] = Nil
   def contextBound: Seq[ScType] = Nil
+
+  def hasBounds: Boolean = lowerTypeElement.nonEmpty || upperTypeElement.nonEmpty
   def hasImplicitBound: Boolean = viewTypeElement.nonEmpty || contextBoundTypeElement.nonEmpty
 
-  def upperTypeElement: Option[ScTypeElement] = None
   def lowerTypeElement: Option[ScTypeElement] = None
+
+  def upperTypeElement: Option[ScTypeElement] = None
   def viewTypeElement: Seq[ScTypeElement] = Nil
   def contextBoundTypeElement: Seq[ScTypeElement] = Nil
 
   def removeImplicitBounds() {}
+
+  def boundsText: String = {
+    def toString(bounds: Traversable[ScTypeElement], elementType: IElementType) = bounds.map {
+      case e => s"${elementType.toString} ${e.getText}"
+    }
+
+    (toString(lowerTypeElement, tLOWER_BOUND) ++
+      toString(upperTypeElement, tUPPER_BOUND) ++
+      toString(viewTypeElement, tVIEW) ++
+      toString(contextBoundTypeElement, tCOLON))
+      .mkString(" ")
+  }
 }
