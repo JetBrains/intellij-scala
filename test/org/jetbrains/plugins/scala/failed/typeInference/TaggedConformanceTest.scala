0a3d498e1f5ebdbf9ab41fec07c0d492dfaf643a
Roman Shein <marso.des@gmail.com>
2016-04-04 06:43:55 +0300

    Test for #SCL-8585

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/TaggedConformanceTest.scala
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.typeConformance.TypeConformanceTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 02.04.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class TaggedConformanceTest extends TypeConformanceTestBase {
+  def testSCL8585(): Unit = doTest(
+    s"""
+       |import Tag.@@
+       |import Bar.LRBaz
+       |
+       |case class LR(left:Int, right:Int, result:Int)
+       |
+       |trait Baz
+       |
+       |object Bar {
+       |  type LRBaz = LR @@ Baz
+       |}
+       |object Tag{
+       |  type Tagged[U] = { type Tag = U }
+       |  type @@[T, U] = T with Tagged[U]
+       |  @inline def apply[T,U](t: T): T @@ U = t.asInstanceOf[T @@ U]
+       |}
+       |
+       |object Foo {
+       |  ${caretMarker}val baz:LRBaz = Tag(LR(1,2,3)) //good code red
+       |}
+       |//true
+       """.stripMargin)
+}
