d216f12e15f84c49586f3da0c205eb8fe74c53b9
Roman Shein <marso.des@gmail.com>
2016-04-04 06:43:11 +0300

    Test for #SCL-9347

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/RelatedGenericsTest.scala
@@ -0,0 +1,45 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Roman.Shein
+  * @since 02.04.2016.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class RelatedGenericsTest extends ScalaLightCodeInsightFixtureTestAdapter {
+  def testSCL9347() = checkTextHasNoErrors(
+    """
+      |object SCL9347 {
+      |  trait Record
+      |  trait MutableRecord[TR <: Record] {
+      |    def table: Table[TR, MutableRecord[TR]]
+      |  }
+      |
+      |  // Important: MTR must be related to TR
+      |  class Table[TR <: Record, MTR <: MutableRecord[TR]] {selfTable =>
+      |    class Field {
+      |      def table = selfTable
+      |    }
+      |  }
+      |
+      |  type AnyTable = Table[TR, _ <: MutableRecord[TR]] forSome {type TR <: Record}
+      |
+      |  def needTable(table: AnyTable) = ???
+      |
+      |  def foo(field: AnyTable#Field) = {
+      |    field.table // Good code red
+      |  }
+      |
+      |  def foo2(field: Table[TR, MTR]#Field forSome {type TR <: Record; type MTR <: MutableRecord[TR]}) = {
+      |    field.table // Good code red
+      |  }
+      |
+      |  def bar(mutableRecord: MutableRecord[_ <: Record]) {
+      |    needTable(mutableRecord.table) // Good code red
+      |  }
+      |}
+    """.stripMargin)
+}
