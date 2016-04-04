ede03dc269d234ea6dfbe181c06c61f1a18bc122
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-04-04 15:13:16 +0300

    add test for #SCL-4824

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/CompoundTypesTest.scala
@@ -0,0 +1,37 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.typeConformance.TypeConformanceTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * @author mucianm 
+  * @since 04.04.16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class CompoundTypesTest extends TypeConformanceTestBase {
+
+  def testSCL4824_A(): Unit = {
+    doTest(
+      """
+      |trait B
+      |trait A extends B
+      |class T[T]
+      |val a: T[A with B] = new T[B with A]
+      |//True
+    """.
+        stripMargin)
+    }
+
+  def testSCL4824_B(): Unit = {
+    doTest(
+      """
+        |trait B
+        |trait A extends B { def z = 1 }
+        |class T[T]
+        |val a: T[A] = new T[B with A {def z: Int}]
+        |//True
+      """.stripMargin)
+  }
+
+}
