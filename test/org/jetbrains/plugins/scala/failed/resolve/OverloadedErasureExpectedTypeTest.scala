8ce0849bfd8ca47e2bfe9837990e6bacd208856b
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-04-07 19:24:45 +0300

    add test for #SCL-7222

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/OverloadedErasureExpectedTypeTest.scala
@@ -0,0 +1,33 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.jetbrains.plugins.scala.lang.resolve.SimpleResolveTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * @author mucianm 
+  * @since 07.04.16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class OverloadedErasureExpectedTypeTest extends ScalaLightCodeInsightFixtureTestAdapter with SimpleResolveTestBase {
+  import SimpleResolveTestBase._
+
+  def testSCL7222(): Unit ={
+    doResolveTest(
+      s"""
+        |trait Test {
+        |  class Foo
+        |  class Bar
+        |  implicit object I1
+        |  implicit object I2
+        |  def bar(foo: Foo): Bar
+        |  def ${REFTGT}foo(f: Foo => Bar)(implicit i1: I1.type)
+        |  def foo(f: Foo => (Foo => Bar))(implicit i2: I2.type)
+        |
+        |  ${REFSRC}foo(f => bar(f))
+        |}
+      """.stripMargin)
+  }
+
+}
