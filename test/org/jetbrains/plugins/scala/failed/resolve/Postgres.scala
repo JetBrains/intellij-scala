ccb3d9df30d6d7fa878367d026dbcde08f4fe745
katejim <katejim@mail.ru>
2016-04-11 16:13:27 +0300

    add annotation for SCL-8556

--- test/org/jetbrains/plugins/scala/failed/resolve/Postgres.scala
+++ test/org/jetbrains/plugins/scala/failed/resolve/Postgres.scala
@@ -1,10 +1,14 @@
 package org.jetbrains.plugins.scala.failed.resolve
 
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
 /**
   * Created by kate on 4/7/16.
   */
 
 //lots of self type in library, maybe this is cause of problem
+@Category(Array(classOf[PerfCycleTests]))
 class Postgres extends FailedResolveTest("postgresql") {
   override protected def additionalLibraries(): Array[String] = Array("postgresql")
 
