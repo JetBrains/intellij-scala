736ba0947b433307deb55b80b38c6a61d9591852
yalishevant <anton.yalyshev@jetbrains.com>
2016-04-13 11:22:08 +0300

    add test for #SCL-10116

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/EscapeSymbolsTest.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by Anton Yalyshev on 13/04/16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class EscapeSymbolsTest extends FailedResolveTest("escapeSymbols") {
+  def testSCL10116(): Unit = doTest()
+}
