b81b48aacca9f94929e01c96108c7140c2dba9ab
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2016-04-18 19:24:52 +0300

    ProjectComponent for ScalaPsiManager made responsible for disposing ScalaPsiManager itself. (cherry picked from commit 8912f39)

--- src/org/jetbrains/plugins/scala/lang/psi/impl/ScalaPsiManager.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/ScalaPsiManager.scala
@@ -40,7 +40,7 @@ import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 
 import scala.collection.{Seq, mutable}
 
-class ScalaPsiManager(project: Project) extends AbstractProjectComponent(project) {
+class ScalaPsiManager(val project: Project) {
 
   private val clearCacheOnChange = new mutable.ArrayBuffer[util.Map[_ <: Any, _ <: Any]]()
   private val clearCacheOnLowMemory = new mutable.ArrayBuffer[util.Map[_ <: Any, _ <: Any]]()
@@ -242,7 +242,7 @@ class ScalaPsiManager(project: Project) extends AbstractProjectComponent(project
     syntheticPackages.clear()
   }
 
-  override def projectOpened(): Unit = {
+  private[impl] def projectOpened(): Unit = {
     import ScalaPsiManager._
 
     subscribeToPsiModification(project)
@@ -347,7 +347,7 @@ class ScalaPsiManager(project: Project) extends AbstractProjectComponent(project
 object ScalaPsiManager {
   val TYPE_VARIABLE_KEY: Key[ScTypeParameterType] = Key.create("type.variable.key")
 
-  def instance(project: Project) = project.getComponent(classOf[ScalaPsiManager])
+  def instance(project: Project): ScalaPsiManager = project.getComponent(classOf[ScalaPsiManagerComponent]).instance
 
   def typeVariable(tp: PsiTypeParameter): ScTypeParameterType = instance(tp.getProject).typeVariable(tp)
 
@@ -392,6 +392,22 @@ object ScalaPsiManager {
         manager.clearOnLowMemory()
       }
     }, project)
+  }
+}
+
+class ScalaPsiManagerComponent(project: Project) extends AbstractProjectComponent(project) {
+  private var manager = new ScalaPsiManager(project)
+
+  def instance: ScalaPsiManager =
+    if (manager != null) manager
+    else throw new IllegalStateException("ScalaPsiManager cannot be used after disposing.")
+
+  override def projectOpened(): Unit = {
+    manager.projectOpened()
+  }
+
+  override def disposeComponent(): Unit = {
+    manager = null
   }
 }
 
