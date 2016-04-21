e4701c283bba28e8c3da55f9d4a41bc03a364b1c
yalishevant <anton.yalyshev@jetbrains.com>
2016-04-21 14:43:11 +0300

    add test for #SCL-5768

--- test/org/jetbrains/plugins/scala/failed/annotator/UnspecifiedValueParamImplicitTest.scala
+++ test/org/jetbrains/plugins/scala/failed/annotator/UnspecifiedValueParamImplicitTest.scala
@@ -22,4 +22,19 @@ class UnspecifiedValueParamImplicitTest extends ScalaLightCodeInsightFixtureTest
         |class ReproDep(private implicit val i: Int)
       """.stripMargin)
   }
+
+  def testSCL5768(): Unit = {
+    checkTextHasNoErrors(
+      """
+        |object Foo {
+        |    trait Tx
+        |    trait Foo { def apply()(implicit tx: Tx): Bar }
+        |    trait Bar { def update(value: Int): Unit }
+        |
+        |    def test(foo: Foo)(implicit tx: Tx) {
+        |      foo()() = 1  // second () is highlighted red
+        |    }
+        |  }
+      """.stripMargin)
+  }
 }
