22edcb5c6669b5876622b5093b6dceb27c6b0642
yalishevant <anton.yalyshev@jetbrains.com>
2016-04-21 15:51:07 +0300

    add test for #SCL-5789

--- test/org/jetbrains/plugins/scala/failed/annotator/AlreadyDefinedTest.scala
+++ test/org/jetbrains/plugins/scala/failed/annotator/AlreadyDefinedTest.scala
@@ -17,4 +17,13 @@ class AlreadyDefinedTest extends ScalaLightCodeInsightFixtureTestAdapter {
         |    def name {""}
         |}
       """.stripMargin)
+
+  def testSCL5789(): Unit =
+    checkTextHasNoErrors(
+      """
+        |class Test {
+        |  private[this] val x = 1
+        |  def x() = 2
+        |}
+      """.stripMargin)
 }
