d2db35bb6ed421247d7813e30e626a635e717dbe
yalishevant <anton.yalyshev@jetbrains.com>
2016-04-25 17:06:11 +0300

    add test for #SCL-5464

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/PartialFunctionResolveTest.scala
@@ -0,0 +1,25 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by Anton Yalyshev on 15/04/16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class PartialFunctionResolveTest extends FailedResolveCaretTestBase {
+
+  def testSCL5464(): Unit = {
+    doResolveCaretTest(
+      """
+        |class A {
+        |    def m() {  }
+        |    def m(i: Int) { }
+        |  }
+        |
+        |  object Main extends App {
+        |    val fun = (_: A).<caret>m _
+        |  }
+      """.stripMargin)
+  }
+}
