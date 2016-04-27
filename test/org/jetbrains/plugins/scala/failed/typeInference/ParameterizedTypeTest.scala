4c9eeed3c9da26e1bfe1a236705a3a68667ee754
yalishevant <anton.yalyshev@jetbrains.com>
2016-04-27 17:39:24 +0300

    add test for #SCL-8031

--- test/org/jetbrains/plugins/scala/failed/typeInference/ParameterizedTypeTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/ParameterizedTypeTest.scala
@@ -207,4 +207,50 @@ class ParameterizedTypeTest extends ScalaLightCodeInsightFixtureTestAdapter {
         |}""".stripMargin
     )
   }
+
+  def testSCL8031(): Unit = {
+    checkTextHasNoErrors(
+      """
+         |import One.HList.:::
+         |
+         |object One extends App{
+         |
+         |  trait Fold[T, V] {
+         |    type Apply[N <: T, Acc <: V] <: V
+         |    def apply[N <: T, Acc <: V](n: N, acc: Acc): Apply[N, Acc]
+         |  }
+         |
+         |  sealed trait HList {
+         |    type Foldr[V, F <: Fold[Any, V], I <: V] <: V
+         |    def foldr[V, F <: Fold[Any, V], I <: V](f: F, i: I): Foldr[V, F, I]
+         |  }
+         |
+         |  final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
+         |    def ::[E](v: E) = HCons(v, this)
+         |    type Foldr[V, F <: Fold[Any, V], I <: V] = F#Apply[H, tail.Foldr[V, F, I]]
+         |    def foldr[V, F <: Fold[Any, V], I <: V](f: F, i: I): Foldr[V, F, I] =
+         |      f(head, tail.foldr[V, F, I](f, i))
+         |  }
+         |
+         |  object HList {
+         |    type ::[H, T <: HList] = HCons[H, T]
+         |    val :: = HCons
+         |    type :::[A <: HList, B <: HList] = A#Foldr[HList, FoldHCons.type, B]
+         |    implicit def concat[B <: HList](b: B): Concat[B] =
+         |      new Concat[B] {
+         |        def :::[A <: HList](a: A): A#Foldr[HList, FoldHCons.type, B] =
+         |          a.foldr[HList, FoldHCons.type, B](FoldHCons, b)
+         |      }
+         |
+         |    object FoldHCons extends Fold[Any, HList] {
+         |      type Apply[N <: Any, H <: HList] = N :: H
+         |      def apply[A, B <: HList](a: A, b: B) = HCons(a, b)
+         |    }
+         |  }
+         |
+         |  sealed trait Concat[B <: HList] { def :::[A <: HList](a: A): A ::: B }
+         |}
+      """.stripMargin.trim
+    )
+  }
 }
