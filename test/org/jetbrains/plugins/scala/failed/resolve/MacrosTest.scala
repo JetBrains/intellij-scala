f7a803f8d8dc2220bbd40597fe0113deee56c4d5
yalishevant <anton.yalyshev@jetbrains.com>
2016-04-29 14:24:37 +0300

    add test for #SCL-8507

--- test/org/jetbrains/plugins/scala/failed/resolve/MacrosTest.scala
+++ test/org/jetbrains/plugins/scala/failed/resolve/MacrosTest.scala
@@ -39,4 +39,19 @@ class MacrosTest extends FailedResolveCaretTestBase {
       """.stripMargin)
   }
 
+  def testSCL8507(): Unit = doResolveCaretTest(
+    s"""
+       |object x extends App {
+       |  import shapeless._
+       |  case class Foo(i: Int, s: String, b:Boolean)
+       |  val foo = Foo(23, "foo", true)
+       |
+       |  val gen = Generic[Foo]
+       |  val hfoo = gen.to(foo)
+       |
+       |  val foo2 = gen.from(hfoo.<caret>head :: "bar" :: hfoo.tail.tail)
+       |
+       |  println(foo2)
+       |}
+    """.stripMargin)
 }
