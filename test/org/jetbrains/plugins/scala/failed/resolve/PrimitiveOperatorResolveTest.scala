d8d26a4915ec2f6659900d71ca97bbff6f3dd93a
ayalyshev <1q2W3e4R>
2016-05-02 16:32:03 +0300

    added test for SCL-9645

---
+++ test/org/jetbrains/plugins/scala/failed/resolve/PrimitiveOperatorResolveTest.scala
@@ -0,0 +1,29 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by Anton.Yalyshev on 02/05/16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class PrimitiveOperatorResolveTest extends FailedResolveCaretTestBase {
+
+  def testSCL9645(): Unit = {
+    doResolveCaretTest(
+      """
+        |class Base[+T](final val value: T)
+        |
+        |class Derived[+T](value: T) extends Base(value)
+        |
+        |abstract class Container[+E] {
+        |  def element: E
+        |}
+        |
+        |object Test {
+        |  val c: Container[Derived[Int]] = ???
+        |  c.element.value <caret>* 2
+        |}
+      """.stripMargin)
+  }
+}
