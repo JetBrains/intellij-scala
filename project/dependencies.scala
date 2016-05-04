3cc9583816e5c142e51a390e04297eed995e18f9
Roman Shein <marso.des@gmail.com>
2016-05-04 18:43:25 +0300

    Updated idea version

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Versions {
   val scalaVersion = "2.11.6"
   val sbtVersion = "0.13.9"
-  val ideaVersion = "145.844.1"
+  val ideaVersion = "145.970.4"
   val sbtStructureVersion = "5.1.2"
   val luceneVersion = "4.8.1"
   val aetherVersion = "1.0.0.v20140518"
