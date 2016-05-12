887b53d5ffb6ac496ad73b92d24d4c34151357f9
yalishevant <anton.yalyshev@jetbrains.com>
2016-05-12 15:54:23 +0300

    added test for #SCL-10242

--- test/org/jetbrains/plugins/scala/failed/typeInference/PartialFunctionTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/PartialFunctionTest.scala
@@ -13,4 +13,19 @@ class PartialFunctionTest extends TypeInferenceTestBase {
   override def folderPath: String = super.folderPath + "bugs5/"
 
   def testSCL6716() = doTest()  // require PartialFunction, not function
+
+  def testSCL10242(): Unit = doTest {
+    """
+      |class cl {
+      |  def tapWith[A](f: A => Unit)(a: A): A = {
+      |    f(a)
+      |    a
+      |  }
+      |
+      |  val g1: Int => Int = tapWith { /*start*/n => n.toLong/*end*/
+      |  } _
+      |}
+      |//(_A) => Unit
+    """.stripMargin.trim
+  }
 }
