38f410c4de82ec0d962debf35de00a3cbe496c48
katejim <katejim@mail.ru>
2016-05-17 19:11:12 +0300

    Adding import before resolving leads to empty resolve for elements which has same package and member. Move import addition after resolving. #SCL-10286 fixed.

--- test/org/jetbrains/plugins/scala/codeInsight/intentions/imports/ImportAllMembersIntentionTest.scala
+++ test/org/jetbrains/plugins/scala/codeInsight/intentions/imports/ImportAllMembersIntentionTest.scala
@@ -244,4 +244,38 @@ class ImportAllMembersIntentionTest extends ScalaIntentionTestBase {
     doTest(text, result)
     settings.setCollectImports(true)
   }
+
+  def testPackageNameConflictWithMethod(): Unit ={
+    val text =
+      """package foo {
+        |
+        |  object A {
+        |    def foo(): Unit = {
+        |
+        |    }
+        |  }
+        |
+        |}
+        |
+        |object B {
+        |  foo.<caret>A.foo()
+        |}""".stripMargin
+
+    val result =
+      """import foo.A._
+        |package foo {
+        |
+        |  object A {
+        |    def foo(): Unit = {
+        |
+        |    }
+        |  }
+        |
+        |}
+        |
+        |object B {
+        |  foo()
+        |}""".stripMargin
+    doTest(text, result)
+  }
 }
