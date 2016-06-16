ed24684c2ce5e608f4ab3e18f88b3409b6cbd3f8
yalishevant <anton.yalyshev@jetbrains.com>
2016-06-16 12:15:08 +0300

    add test for SCL-10414

---
+++ test/org/jetbrains/plugins/scala/failed/typeInference/CollectionsTest.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.scala.failed.typeInference
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.jetbrains.plugins.scala.lang.typeInference.TypeInferenceTestBase
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by Anton Yalyshev on 15.06.16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class CollectionsTest extends TypeInferenceTestBase {
+
+  def testSCL10414(): Unit = {
+    val text =
+      s"""
+          |val s1 : Set[Class[_]] = Set()
+          |val s2 : Set[java.lang.Class[_]] = Set()
+          |
+          |if(!s1.union(${START}s2$END).isEmpty) println(5)
+          |//GenSet[Class[_]]""".stripMargin
+    doTest(text)
+  }
+}
