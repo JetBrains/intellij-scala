6a06554f17662c142022b425dbb451f8986667c0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-06-28 01:26:02 +0300

    compiler crash workaround wip

--- src/org/jetbrains/plugins/scala/project/settings/ScalaCompilerConfigurable.scala
+++ src/org/jetbrains/plugins/scala/project/settings/ScalaCompilerConfigurable.scala
@@ -1,9 +1,10 @@
 package org.jetbrains.plugins.scala.project.settings
 
-import javax.swing.JPanel
+import javax.swing.JComponent
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.compiler.server.BuildManager
+import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.project.AbstractConfigurable
 
@@ -12,27 +13,4 @@ import scala.collection.JavaConverters._
 /**
  * @author Pavel Fatin
  */
-class ScalaCompilerConfigurable(project: Project, configuration: ScalaCompilerConfiguration) extends AbstractConfigurable("Scala Compiler")  {
-  private val form = new ScalaCompilerConfigurationPanel(project)
-  
-  private val profiles = form.getProfilesPanel
-
-  def createComponent(): JPanel = form.getContentPanel
-
-  def isModified: Boolean = form.getIncrementalityType != configuration.incrementalityType ||
-          profiles.getDefaultProfile.getSettings.getState != configuration.defaultProfile.getSettings.getState ||
-          !profiles.getModuleProfiles.asScala.corresponds(configuration.customProfiles)(_.getSettings.getState == _.getSettings.getState)
-
-  def reset() {
-    form.setIncrementalityType(configuration.incrementalityType)
-    profiles.initProfiles(configuration.defaultProfile, configuration.customProfiles.asJava)
-  }
-
-  def apply() {
-    configuration.incrementalityType = form.getIncrementalityType
-    configuration.defaultProfile = profiles.getDefaultProfile
-    configuration.customProfiles = profiles.getModuleProfiles.asScala
-    DaemonCodeAnalyzer.getInstance(project).restart()
-    BuildManager.getInstance().clearState(project)
-  }
-}
+class ScalaCompilerConfigurablbt
