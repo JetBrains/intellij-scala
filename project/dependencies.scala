3443fe7f424164e4e9cc2a098ad6344cc2c97fa3
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-07-11 16:53:34 +0300

    update idea to 162.1121.32

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Versions {
   val scalaVersion = "2.11.6"
   val sbtVersion = "0.13.11"
-  val ideaVersion = "162.1120.6"
+  val ideaVersion = "162.1121.32"
   val sbtStructureVersion = "5.1.2"
   val luceneVersion = "4.8.1"
   val aetherVersion = "1.0.0.v20140518"
