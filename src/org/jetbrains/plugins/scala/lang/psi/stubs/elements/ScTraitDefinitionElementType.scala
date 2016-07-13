88ccd2a8f294a57f8fc71357193dcb1a10256685
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-07-13 18:59:47 +0300

    type definitions element types refactored for Dotty

--- src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/ScTraitDefinitionElementType.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/ScTraitDefinitionElementType.scala
@@ -5,7 +5,6 @@ package stubs
 package elements
 
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTrait
 import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.ScTraitImpl
 
@@ -13,6 +12,7 @@ import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.ScTraitImpl
  * @author ilyas
  */
 class ScTraitDefinitionElementType extends ScTemplateDefinitionElementType[ScTrait]("trait definition") {
-  def createElement(node: ASTNode): PsiElement = new ScTraitImpl(node)
-  def createPsi(stub: ScTemplateDefinitionStub) = new ScTraitImpl(stub)
+  def createElement(node: ASTNode): ScTrait = new ScTraitImpl(node)
+
+  def createPsi(stub: ScTemplateDefinitionStub): ScTrait = new ScTraitImpl(stub)
 }
