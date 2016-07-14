81566f62489966480acc2175aec13383e116d79e
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-07-14 18:04:17 +0300

    TokenSets cleanup

--- src/org/jetbrains/plugins/scala/lang/TokenSets.scala
+++ src/org/jetbrains/plugins/scala/lang/TokenSets.scala
@@ -2,11 +2,10 @@ package org.jetbrains.plugins.scala
 package lang
 
 /**
- * @author ilyas
- */
+  * @author ilyas
+  */
 
 import com.intellij.psi.tree.TokenSet
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes._
 import org.jetbrains.plugins.scala.lang.parser.ScalaElementTypes._
 import org.jetbrains.plugins.scala.lang.scaladoc.parser.ScalaDocElementTypes._
@@ -15,80 +14,80 @@ import org.jetbrains.plugins.scala.util.MemberElementTypesExtension
 object TokenSets {
   val WHITESPACE_OR_COMMENT_SET = TokenSet.create(tWHITE_SPACE_IN_LINE, tLINE_COMMENT, tBLOCK_COMMENT, tDOC_COMMENT, SCALA_DOC_COMMENT)
 
-  val MODIFIERS = TokenSet.create(ScalaTokenTypes.kCASE, ScalaTokenTypes.kABSTRACT, ScalaTokenTypes.kLAZY,
-    ScalaTokenTypes.kIMPLICIT, ScalaTokenTypes.kFINAL, ScalaTokenTypes.kOVERRIDE, ScalaTokenTypes.kPROTECTED,
-    ScalaTokenTypes.kPRIVATE, ScalaTokenTypes.kSEALED)
+  val MODIFIERS = TokenSet.create(kCASE, kABSTRACT, kLAZY,
+    kIMPLICIT, kFINAL, kOVERRIDE, kPROTECTED,
+    kPRIVATE, kSEALED)
 
   val TYPE_DEFINITIONS_SET = TokenSet.create(OBJECT_DEF, CLASS_DEF, TRAIT_DEF)
 
   private val EXPR1_SET: TokenSet = TokenSet.create(IF_STMT,
-      FOR_STMT,
-      WHILE_STMT,
-      DO_STMT,
-      TRY_STMT,
-      TRY_BLOCK,
-      CATCH_BLOCK,
-      FINALLY_BLOCK,
-      RETURN_STMT,
-      THROW_STMT,
-      ASSIGN_STMT,
-      MATCH_STMT,
-      TYPED_EXPR_STMT,
+    FOR_STMT,
+    WHILE_STMT,
+    DO_STMT,
+    TRY_STMT,
+    TRY_BLOCK,
+    CATCH_BLOCK,
+    FINALLY_BLOCK,
+    RETURN_STMT,
+    THROW_STMT,
+    ASSIGN_STMT,
+    MATCH_STMT,
+    TYPED_EXPR_STMT,
+    POSTFIX_EXPR,
+    INFIX_EXPR,
+    PLACEHOLDER_EXPR,
+    PREFIX_EXPR)
+
+  val EXPRESSION_SET = TokenSet.orSet(EXPR1_SET,
+    TokenSet.create(LITERAL,
+      STRING_LITERAL,
+      BOOLEAN_LITERAL,
+      PREFIX_EXPR,
+      PREFIX,
       POSTFIX_EXPR,
       INFIX_EXPR,
       PLACEHOLDER_EXPR,
-      PREFIX_EXPR)
-
-  val EXPRESSION_SET = TokenSet.orSet(EXPR1_SET,
-      TokenSet.create(LITERAL,
-          STRING_LITERAL,
-          BOOLEAN_LITERAL,
-          PREFIX_EXPR,
-          PREFIX,
-          POSTFIX_EXPR,
-          INFIX_EXPR,
-          PLACEHOLDER_EXPR,
-          EXPR1,
-          FUNCTION_EXPR,
-          AN_FUN,
-          GENERATOR,
-          ENUMERATOR,
-          ENUMERATORS,
-          EXPRS,
-          ARG_EXPRS,
-          BLOCK_EXPR,
-          ERROR_STMT,
-          BLOCK,
-          PARENT_EXPR,
-          METHOD_CALL,
-          REFERENCE_EXPRESSION,
-          THIS_REFERENCE,
-          SUPER_REFERENCE,
-          GENERIC_CALL))
+      EXPR1,
+      FUNCTION_EXPR,
+      AN_FUN,
+      GENERATOR,
+      ENUMERATOR,
+      ENUMERATORS,
+      EXPRS,
+      ARG_EXPRS,
+      BLOCK_EXPR,
+      ERROR_STMT,
+      BLOCK,
+      PARENT_EXPR,
+      METHOD_CALL,
+      REFERENCE_EXPRESSION,
+      THIS_REFERENCE,
+      SUPER_REFERENCE,
+      GENERIC_CALL))
 
   val ID_SET = TokenSet.create(tIDENTIFIER, tUNDER)
 
-  val SELF_TYPE_ID = TokenSet.create(ScalaTokenTypes.kTHIS, ScalaTokenTypes.tIDENTIFIER, ScalaTokenTypes.tUNDER)
+  val SELF_TYPE_ID = TokenSet.create(kTHIS, tIDENTIFIER, tUNDER)
 
   val ALIASES_SET = TokenSet.create(TYPE_DECLARATION, TYPE_DEFINITION)
 
   val FUNCTIONS = TokenSet.create(FUNCTION_DECLARATION, FUNCTION_DEFINITION, MACRO_DEFINITION)
 
-  private val VALUES = TokenSet.create(VALUE_DECLARATION, PATTERN_DEFINITION)
+  val VALUES = TokenSet.create(VALUE_DECLARATION, PATTERN_DEFINITION)
 
-  private val VARIABLES = TokenSet.create(VARIABLE_DECLARATION, VARIABLE_DEFINITION)
+  val VARIABLES = TokenSet.create(VARIABLE_DECLARATION, VARIABLE_DEFINITION)
 
   val TEMPLATE_PARENTS = TokenSet.create(CLASS_PARENTS, TRAIT_PARENTS)
 
   val MEMBERS = TokenSet.orSet(TokenSet.orSet(FUNCTIONS, TokenSet.orSet(
-      ALIASES_SET, TokenSet.orSet(
+    ALIASES_SET, TokenSet.orSet(
       TYPE_DEFINITIONS_SET, TokenSet.orSet(
-          VALUES, TokenSet.orSet(
+        VALUES, TokenSet.orSet(
           VARIABLES, TokenSet.create(PRIMARY_CONSTRUCTOR)
-          )
         )
       )
-    )), MemberElementTypesExtension.getAllElementTypes
+    )
+  )), MemberElementTypesExtension.getAllElementTypes
   )
 
   val DECLARED_ELEMENTS_HOLDER = TokenSet.orSet(FUNCTIONS, TokenSet.orSet(VALUES, VARIABLES))
