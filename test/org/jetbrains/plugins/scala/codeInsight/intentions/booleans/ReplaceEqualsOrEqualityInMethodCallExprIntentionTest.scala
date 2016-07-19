25fd58a0820e21fe50dcf9973a183927f5f9d089
neowulf33 <snv.kommuri@gmail.com>
2016-07-19 13:10:14 -0700

    All test cases pass

--- test/org/jetbrains/plugins/scala/codeInsight/intentions/booleans/ReplaceEqualsOrEqualityInMethodCallExprIntentionTest.scala
+++ test/org/jetbrains/plugins/scala/codeInsight/intentions/booleans/ReplaceEqualsOrEqualityInMethodCallExprIntentionTest.scala
@@ -35,28 +35,28 @@ class ReplaceEqualsOrEqualityInMethodCallExprIntentionTest extends ScalaIntentio
 
   def testReplaceTuple() = {
     val text = "(true, false).equ<caret>als  (false, false)"
-    //TODO desired (true, false) <caret>== (false, false)
-    val resultText = "(true, false) <caret>==(false, false)"
+    val resultText = "(true, false) <caret>== (false, false)"
 
     doTest(text, resultText)
   }
 
   def testReplaceTuple_2() = {
     val text = "if((true, false).equ<caret>als  (false, false)) return "
-    //TODO desired (true, false) <caret>== (false, false)
-    val resultText = "if ((true, false) <caret>==(false, false)) return"
+    val resultText = "if ((true, false) <caret>== (false, false)) return"
 
     doTest(text, resultText)
   }
 
   def testReplaceEqualityInExpression() = {
-    val x = 1
-    val y = 2
-    val z1 = x.equals(y).toString
-    val z = (x == y).toString
-
-    val text = "x.eq<caret>uals(y).toString"
+    val text = "(x.eq<caret>uals(y)).toString"
     val resultText = "(x <caret>== y).toString"
+
+    doTest(text, resultText)
+  }
+
+  def testReplaceEqualityInExpression_1() = {
+    val text = "x.eq<caret>uals(y).toString"
+    val resultText = "x <caret>== y.toString"
 
     doTest(text, resultText)
   }
