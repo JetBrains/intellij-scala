d983c0a88209ade03bc4ac82df0e8f67952c1237
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-08-01 20:19:47 +0300

    re-enable dataservice and sbt completions tests

--- test/org/jetbrains/sbt/lang/completion/CompletionDependenciesTest.scala
+++ test/org/jetbrains/sbt/lang/completion/CompletionDependenciesTest.scala
@@ -1,34 +1,32 @@
-//package org.jetbrains.sbt
-//package lang.completion
-//
-//import com.intellij.openapi.module.ModuleManager
-//import org.jetbrains.sbt.project.module.SbtModule
-//import org.jetbrains.sbt.resolvers.{SbtResolver, SbtResolverIndexesManager}
-//
-///**
-// * @author Nikolay Obedin
-// * @since 8/1/14.
-// */
-//class CompletionDependenciesTest extends CompletionTestBase {
-//
-//  val testResolver = new SbtResolver(SbtResolver.Kind.Maven, "Test repo", "file:/%s/sbt/resolvers/testRepository" format baseRootPath)
-//
-//  override def setUp() = {
-//    super.setUp()
-//    SbtResolverIndexesManager().update(Seq(testResolver))
-//    val moduleManager = Option(ModuleManager.getInstance(getProjectAdapter))
-//    moduleManager.foreach { manager =>
-//      manager.getModules.toSeq.foreach { module =>
-//        val resolvers = SbtModule.getResolversFrom(module)
-//        SbtModule.setResolversTo(module, resolvers + testResolver)
-//      }
-//    }
-//  }
-//
-//  def testCompleteArtifact() =
-//    doTest()
-//  def testCompleteGroup() =
-//    doTest()
-//  def testCompleteVersion() =
-//    doTest()
-//}
+package org.jetbrains.sbt
+package lang.completion
+
+import com.intellij.openapi.module.ModuleManager
+import org.jetbrains.plugins.scala.util.TestUtils
+import org.jetbrains.sbt.project.module.SbtModule
+import org.jetbrains.sbt.resolvers.SbtIvyResolver
+
+/**
+ * @author Nikolay Obedin
+ * @since 8/1/14.
+ */
+class CompletionDependenciesTest extends CompletionTestBase {
+
+  val testResolver = new SbtIvyResolver("Test repo", "/%s/sbt/resolvers/testIvyCache" format TestUtils.getTestDataPath)
+
+  override def setUp() = {
+    super.setUp()
+    testResolver.getIndex.doUpdate()(getProjectAdapter)
+    val moduleManager = Option(ModuleManager.getInstance(getProjectAdapter))
+    moduleManager.foreach { manager =>
+      manager.getModules.toSeq.foreach { module =>
+        val resolvers = SbtModule.getResolversFrom(module)
+        SbtModule.setResolversTo(module, resolvers + testResolver)
+      }
+    }
+  }
+
+  def testCompleteArtifact()  = doTest()
+  def testCompleteGroup()     = doTest()
+  def testCompleteVersion()   = doTest()
+}
