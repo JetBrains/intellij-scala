208cf8f8948a7256b0f1f0c554f4450dabaf3836
yalishevant <anton.yalyshev@jetbrains.com>
2016-08-09 14:11:00 +0300

    add test for SCL-7255

---
+++ test/org/jetbrains/plugins/scala/failed/annotator/ConstructorTest.scala
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala.failed.annotator
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * @author Anton Yalyshev
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class ConstructorTest extends BadCodeGreenTestBase {
+
+  def testScl7255(): Unit = {
+    val text =
+      s"""
+         |// private constructor
+         |class Recipe private(val ingredients: List[String] = List.empty,
+         |                     val directions: List[String] = List.empty) {
+         |  println("something")
+         |}
+         |
+         |object Recipe {
+         |  def make(ingredients: List[String], directions: List[String]): Recipe =
+         |    new Recipe(ingredients, directions)
+         |}
+         |
+          |object Cookbook {
+         |  // no warnings
+         |  val pbj = ${CARET_MARKER}new Recipe(
+         |    List("peanut butter", "jelly", "bread"),
+         |    List("put the peanut butter and jelly on the bread"))
+         |}
+      """.stripMargin
+    doTest(text)
+  }
+
+}
