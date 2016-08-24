0a41221b48b007b171c698e94459241e86e04805
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2016-08-24 13:44:36 +0300

    tests fixed (cherry picked from commit 89f564f)

--- src/org/jetbrains/plugins/scala/lang/psi/types/ValueClassType.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/types/ValueClassType.scala
@@ -3,7 +3,9 @@ package lang.psi.types
 
 import com.intellij.psi.PsiClass
 import org.jetbrains.plugins.scala.extensions.PsiClassExt
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScPrimaryConstructor
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScClassParameter
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
 import org.jetbrains.plugins.scala.lang.psi.types.api.{AnyVal, ExtractClass, TypeSystem, ValType}
 
@@ -30,9 +32,21 @@ object ValueClassType {
   def isValueClass(cl: PsiClass): Boolean = cl match {
     case scClass: ScClass =>
       scClass.parameters match {
-        case Seq(p) if p.isVal || p.isCaseClassVal => scClass.supers.map(_.qualifiedName).contains("scala.AnyVal")
+        case Seq(p) if isValOrCompiled(p) => extendsAnyVal(cl)
         case _ => false
       }
     case _ => false
+  }
+
+  def extendsAnyVal(cl: PsiClass): Boolean = cl.getSupers.map(_.qualifiedName).contains("scala.AnyVal")
+
+  private def isValOrCompiled(p: ScClassParameter) = {
+    if (p.isVal || p.isCaseClassVal) true
+    else {
+      p.containingFile match {
+        case Some(f: ScalaFile) => f.isCompiled // ScalaSigPrinter doesn't show `val` for value class parameters
+        case _ => false
+      }
+    }
   }
 }
