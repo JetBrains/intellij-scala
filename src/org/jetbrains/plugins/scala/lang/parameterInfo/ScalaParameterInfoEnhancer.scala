ba059b04d735d80504dce8bbc300940c4bb55a04
Alexander.Podkhalyuzin <alexander.podkhalyuzin@jetbrains.com>
2016-08-26 17:35:00 +0300

    Code with possibility to restart parameter info hint.

--- src/org/jetbrains/plugins/scala/lang/parameterInfo/ScalaParameterInfoEnhancer.scala
+++ src/org/jetbrains/plugins/scala/lang/parameterInfo/ScalaParameterInfoEnhancer.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.lang.parameterInfo
 
 import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import org.jetbrains.plugins.scala.lang.psi.types.PhysicalSignature
 
@@ -10,6 +11,8 @@ import org.jetbrains.plugins.scala.lang.psi.types.PhysicalSignature
   */
 abstract class ScalaParameterInfoEnhancer {
   def enhance(signature: PhysicalSignature, arguments: Seq[ScExpression]): Seq[PhysicalSignature]
+
+  def restartHint(element: PsiElement, position: Int): Boolean
 }
 
 object ScalaParameterInfoEnhancer {
