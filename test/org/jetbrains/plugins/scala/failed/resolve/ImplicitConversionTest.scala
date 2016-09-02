9fb5b1fe50ee66938fcb76f8efcd2c6f0360d440
Roman Shein <marso.des@gmail.com>
2016-09-02 16:26:15 +0300

    Moved fixed tests #SCL-10670 Fixed #SCL-7974 Fixed
    (cherry picked from commit 17ddee8)

--- test/org/jetbrains/plugins/scala/failed/resolve/ImplicitConversionTest.scala
+++ test/org/jetbrains/plugins/scala/failed/resolve/ImplicitConversionTest.scala
@@ -11,8 +11,6 @@ class ImplicitConversionTest extends FailedResolveTest("implicitConversion") {
   def testScl8709(): Unit = doTest()
 
   def testScl9527(): Unit = doTest()
-  
-  def testScl7974(): Unit = doTest()
 
   def testSCL9224(): Unit = doTest()
 
@@ -28,11 +26,7 @@ class ImplicitConversionTest extends FailedResolveTest("implicitConversion") {
 
   def testSCL10387(): Unit = doTest()
 
-  def testSCL10432(): Unit = doTest()
-
   def testSCL10447(): Unit = doTest()
 
   def testSCL10549(): Unit = doTest()
-
-  def testSCL10670(): Unit = doTest()
 }
