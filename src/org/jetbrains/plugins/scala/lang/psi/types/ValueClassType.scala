e9269e4315ad420253c47fb4f348627e8a90234e
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-09-16 17:08:06 +0300

    containing file common method

--- src/org/jetbrains/plugins/scala/lang/psi/types/ValueClassType.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/types/ValueClassType.scala
@@ -3,11 +3,10 @@ package lang.psi.types
 
 import com.intellij.psi.PsiClass
 import org.jetbrains.plugins.scala.extensions.PsiClassExt
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScPrimaryConstructor
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScClassParameter
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
-import org.jetbrains.plugins.scala.lang.psi.types.api.{AnyVal, ExtractClass, TypeSystem, ValType}
+import org.jetbrains.plugins.scala.lang.psi.types.api.{ExtractClass, TypeSystem, ValType}
 
 /**
  * Nikolay.Tropin
@@ -42,11 +41,6 @@ object ValueClassType {
 
   private def isValOrCompiled(p: ScClassParameter) = {
     if (p.isVal || p.isCaseClassVal) true
-    else {
-      p.containingFile match {
-        case Some(f: ScalaFile) => f.isCompiled // ScalaSigPrinter doesn't show `val` for value class parameters
-        case _ => false
-      }
-    }
+    else p.containingScalaFile.exists(_.isCompiled)
   }
 }
