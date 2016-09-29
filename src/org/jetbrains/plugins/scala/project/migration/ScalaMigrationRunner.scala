8ca7a5ab100eec231ff00aeef6f4fc6e674bc7c2
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2016-09-29 18:15:34 +0300

    Scala Migrator: better file tree iterator

--- src/org/jetbrains/plugins/scala/project/migration/ScalaMigrationRunner.scala
+++ src/org/jetbrains/plugins/scala/project/migration/ScalaMigrationRunner.scala
@@ -1,17 +1,20 @@
 package org.jetbrains.plugins.scala.project.migration
 
-import com.intellij.codeInsight.actions.FileTreeIterator
 import com.intellij.codeInspection.{InspectionManager, ProblemsHolder}
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.progress.{ProgressIndicator, Task}
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.psi.{PsiDirectory, PsiFile, PsiManager}
 import org.jetbrains.plugins.scala.codeInspection.AbstractFixOnPsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.project.migration.api.{MigrationApiService, MigrationReport}
 import org.jetbrains.plugins.scala.project.migration.apiimpl.MigrationLocalFixHolderImpl
 import org.jetbrains.plugins.scala.project.migration.defaultimpl.MyRecursiveVisitorWrapper
 
+import scala.collection.mutable
 import scala.util.{Failure, Success, Try}
 
 /**
@@ -38,12 +41,11 @@ class ScalaMigrationRunner(project: Project) {
         migrators.foreach {
           migrator =>
             catchRun(migrator.migrateGlobal)
-          //            indicatorHandler.tic()
         }
 
        
         val inspectionManager = InspectionManager.getInstance(project)
-        val myIterator = projectStructure.inReadAction(new FileTreeIterator(project))
+        val myIterator = projectStructure.inReadAction(new FilteringFileIterator/*new FileTreeIterator(project)*/)
 
         while (myIterator.hasNext) projectStructure.inReadAction(myIterator.next()) match {
           case scalaFile: ScalaFile if !scalaFile.isCompiled && !scalaFile.isWorksheetFile =>
@@ -73,7 +75,6 @@ class ScalaMigrationRunner(project: Project) {
               case _ =>
             }
 
-          //                  indicatorHandler.tic()
           case _ =>
         }
 
@@ -84,5 +85,64 @@ class ScalaMigrationRunner(project: Project) {
     new Task.Modal(project, "Migrating...", false) {
       override def run(indicator: ProgressIndicator): Unit = action.apply(indicator)
     }.queue()
+  }
+  
+  
+  //we need our own iterator as there is no way to filter excluded dirs
+  private class FilteringFileIterator() {
+    val myCurrentFiles = mutable.Queue[PsiFile]()
+    val (myCurrentDirs, myExcludedDirs) = {
+      val startDirs = mutable.Queue[PsiDirectory]()
+      val excludedDirs = mutable.HashSet[PsiDirectory]()
+      
+      val modules = ModuleManager.getInstance(project).getModules
+      val psiManager = PsiManager.getInstance(project)
+
+      modules foreach {
+        module =>
+          val moduleRootManager = ModuleRootManager getInstance module
+          
+          moduleRootManager getSourceRoots true foreach {
+            root =>
+              val dir = psiManager findDirectory root
+              if (dir != null) startDirs enqueue dir
+          }
+          
+          moduleRootManager.getExcludeRoots foreach {
+            root => 
+              val excluded = psiManager findDirectory root
+              if (excluded != null) excludedDirs add excluded
+          }
+      }
+      
+      (startDirs, excludedDirs)
+    }
+    
+    expandDirectoriesUntilFilesNotEmpty()
+
+    private def expandDirectory(dir: PsiDirectory) {
+      myCurrentFiles ++= dir.getFiles
+      myCurrentDirs ++= dir.getSubdirectories.filter(d => !myExcludedDirs.contains(d))
+    }
+
+    private def expandDirectoriesUntilFilesNotEmpty() {
+      while (myCurrentFiles.isEmpty && myCurrentDirs.nonEmpty) {
+        val dir = myCurrentDirs.dequeue()
+        expandDirectory(dir)
+      }
+    }
+
+    def next(): PsiFile = {
+      if (myCurrentFiles.isEmpty) throw new NoSuchElementException
+      
+      val current: PsiFile = myCurrentFiles.dequeue()
+      expandDirectoriesUntilFilesNotEmpty()
+      
+      println(current.getName + " in " + current.getContainingDirectory.getName)
+      
+      current
+    }
+
+    def hasNext: Boolean = myCurrentFiles.nonEmpty
   }
 }
