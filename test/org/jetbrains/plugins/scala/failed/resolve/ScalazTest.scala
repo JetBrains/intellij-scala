4ae92ea4c6d92577535e7a9213a92f6f9c7c292a
Roman Shein <marso.des@gmail.com>
2016-10-03 01:35:31 +0300

    Moved passing tests Disabled type inference test for SCL-10237
    (cherry picked from commit ef240fb)

--- test/org/jetbrains/plugins/scala/failed/resolve/ScalazTest.scala
+++ test/org/jetbrains/plugins/scala/failed/resolve/ScalazTest.scala
@@ -11,15 +11,7 @@ import org.junit.experimental.categories.Category
 class ScalazTest extends FailedResolveTest("scalaz"){
   override protected def additionalLibraries(): Array[String] = Array("scalaz")
 
-  def testSCL5842A(): Unit = doTest()
-
-  def testSCL5842B(): Unit = doTest()
-
-  def testSCL9752(): Unit = doTest()
-
   def testSCL7213(): Unit = doTest()
-
-  def testSCL10087(): Unit = doTest()
 
   def testSCL7227(): Unit = doTest()
 }
