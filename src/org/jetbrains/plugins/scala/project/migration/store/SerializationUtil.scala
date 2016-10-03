05ffea5be115a8515f41c77de23bb82cbe4ed09f
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2016-10-03 16:55:06 +0300

    Scala Migrator: loading bundled migrators

---
+++ src/org/jetbrains/plugins/scala/project/migration/store/SerializationUtil.scala
@@ -0,0 +1,30 @@
+package org.jetbrains.plugins.scala.project.migration.store
+
+import java.io.File
+
+import org.jetbrains.plugins.scala.project.migration.store.ManifestAttributes._
+import org.jetbrains.plugins.scala.util.ManifestHandler
+
+/**
+  * User: Dmitry.Naydanov
+  * Date: 29.09.16.
+  */
+object SerializationUtil {
+  private val allAttributes = Seq(MigratorFqnAttribute, InspectionPackageAttribute)
+  
+  def discoverIn(filePath: String): Seq[(ManifestAttribute, String)] = {
+    val path = stripPath(filePath)
+    if (!path.endsWith(".jar")) return Seq.empty
+    
+    val ioFile = new File(path)
+    if (!ioFile.exists()) return Seq.empty
+    
+    val manifest = ManifestHandler(ioFile)
+
+    allAttributes.flatMap {
+      attribute => manifest.getArbitraryAttribute(attribute.name).map(v => (attribute, v))
+    }
+  }
+  
+  def stripPath(filePath: String) = filePath.stripSuffix("/").stripSuffix("\\").stripSuffix("!")
+}
