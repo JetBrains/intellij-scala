3f5fcf3f41b1f3d8052634ecdadc711ffc6d9dc3
wannabe <smelov.kirill@gmail.com>
2016-10-05 06:00:53 +1000

    Some remarks
    - Disable highlighting for matches with guard
    - Add wildcard processing
    - Add inspection description
    - Refactoring

--- test/org/jetbrains/plugins/scala/codeInspection/booleans/SimplifyBooleanMatchInspectionTest.scala
+++ test/org/jetbrains/plugins/scala/codeInspection/booleans/SimplifyBooleanMatchInspectionTest.scala
@@ -2,21 +2,22 @@ package org.jetbrains.plugins.scala.codeInspection.booleans
 
 import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
 
-class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestAdapter {
+class SimplifyBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestAdapter {
   val s = ScalaLightCodeInsightFixtureTestAdapter.SELECTION_START
   val e = ScalaLightCodeInsightFixtureTestAdapter.SELECTION_END
-  val annotation = "Simplify trivial match to if"
+  val annotation = "Trivial match can be simplified"
+  val quickFixHint = "Simplify match to if statement"
 
   private def check(text: String) {
-    checkTextHasError(text, annotation, classOf[SimpleBooleanMatchInspection])
+    checkTextHasError(text, annotation, classOf[SimplifyBooleanMatchInspection])
   }
 
-  private def testFix(text: String, result: String, hint: String) {
-    testQuickFix(text.replace("\r", ""), result.replace("\r", ""), hint, classOf[SimpleBooleanMatchInspection])
+  private def testFix(text: String, result: String) {
+    testQuickFix(text.replace("\r", ""), result.replace("\r", ""), quickFixHint, classOf[SimplifyBooleanMatchInspection])
   }
 
   private def checkHasNoErrors(text: String) {
-    checkTextHasNoErrors(text, annotation, classOf[SimpleBooleanMatchInspection])
+    checkTextHasNoErrors(text, annotation, classOf[SimplifyBooleanMatchInspection])
   }
 
   def test_SingleTrueWithParentlessPatternSimpleBranches() {
@@ -44,7 +45,7 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
   }
 
   def test_SingleTrueWithParentlessPatternSimpleBranchesBracesBlock() {
@@ -76,7 +77,7 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
   }
 
   def test_SingleFalseWithParentlessPatternSimpleBranches() {
@@ -104,7 +105,7 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
   }
 
   def test_TrueFalseWithParentlessPatternSimpleBranches() {
@@ -136,7 +137,71 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
+  }
+
+  def test_TrueWildcardWithParentlessPatternSimpleBranches() {
+    val selectedText =
+      s"""
+         |val a = true
+         |val b = ${s}a match {
+         |  case true => 1
+         |  case _ => 4
+         |}$e
+       """.stripMargin
+    check(selectedText)
+
+    val text =
+      """
+        |val a = true
+        |val b = a match {
+        | case true => 1
+        | case _ => 4
+        | }
+      """.stripMargin
+    val result =
+      """
+        |val a = true
+        |val b = if (a) {
+        |  1
+        |} else {
+        |  4
+        |}
+      """.stripMargin
+
+    testFix(text, result)
+  }
+
+  def test_FalseWildcardWithParentlessPatternSimpleBranches() {
+    val selectedText =
+      s"""
+         |val a = true
+         |val b = ${s}a match {
+         |  case false => 1
+         |  case _ => 4
+         |}$e
+       """.stripMargin
+    check(selectedText)
+
+    val text =
+      """
+        |val a = true
+        |val b = a match {
+        | case false => 1
+        | case _ => 4
+        | }
+      """.stripMargin
+    val result =
+      """
+        |val a = true
+        |val b = if (a) {
+        |  4
+        |} else {
+        |  1
+        |}
+      """.stripMargin
+
+    testFix(text, result)
   }
 
   def test_TrueFalseWithParentlessPatternBlockBranches() {
@@ -179,7 +244,7 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
   }
 
   def test_SingleTrueWithParenthesisedPatternSimpleBranches() {
@@ -204,7 +269,7 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
   }
 
   def test_SingleFalseWithParenthesisedPatternSimpleBranches() {
@@ -229,7 +294,7 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
         |}
       """.stripMargin
 
-    testFix(text, result, "Simplify trivial match to if")
+    testFix(text, result)
   }
 
   def test_SingleComplexBranch() {
@@ -272,6 +337,51 @@ class SimpleBooleanMatchInspectionTest extends ScalaLightCodeInsightFixtureTestA
          |  case a + 2 == 3 {
          |    1
          |  }
+         |}
+       """.stripMargin
+
+    checkHasNoErrors(text)
+  }
+
+  def test_OnlyWildcardExpr() {
+    val text =
+      s"""
+         |val a = true
+         |val b = a match {
+         |  case _ {
+         |    1
+         |  }
+         |}
+       """.stripMargin
+
+    checkHasNoErrors(text)
+  }
+
+  def test_TwoWildcatdsExpr() {
+    val text =
+      s"""
+         |val a = true
+         |val b = a match {
+         |  case _ {
+         |    1
+         |  }
+         |  case _ {
+         |    2
+         |  }
+         |}
+       """.stripMargin
+
+    checkHasNoErrors(text)
+  }
+
+  def test_MatchWithGuard() {
+    val text =
+      s"""
+         |val a = true
+         |val c = false
+         |val b = a match {
+         |  case true if c => 1
+         |  case false => 4
          |}
        """.stripMargin
 
