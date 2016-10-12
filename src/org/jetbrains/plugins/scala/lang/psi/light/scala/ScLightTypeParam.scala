42739a42d0933e211093cd23097f8b7a9a7bf1c8
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2016-10-12 15:27:06 +0300

    Fixed problem with few same libraries as dependencies and type parameters.

--- src/org/jetbrains/plugins/scala/lang/psi/light/scala/ScLightTypeParam.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/light/scala/ScLightTypeParam.scala
@@ -15,7 +15,7 @@ import org.jetbrains.plugins.scala.lang.psi.types.result.{Success, TypeResult}
  * @author Alefas
  * @since 03/04/14.
  */
-class ScLightTypeParam(t: TypeParameter, tParam: ScTypeParam)
+class ScLightTypeParam(t: TypeParameter, val tParam: ScTypeParam)
   extends LightElement(tParam.getManager, tParam.getLanguage) with ScTypeParam with PsiClassFake {
   override def nameId: PsiElement = tParam.nameId
 
@@ -41,8 +41,6 @@ class ScLightTypeParam(t: TypeParameter, tParam: ScTypeParam)
   override def getContainingFileName: String = tParam.getContainingFileName
 
   override def getOffsetInFile: Int = tParam.getOffsetInFile
-
-  override def getPsiElementId: Long = tParam.getPsiElementId
 
   override def owner: ScTypeParametersOwner = tParam.owner
 
