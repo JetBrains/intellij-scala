3072e82cff404d8ffc00d771fbc6fc3e23a5aa47
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-10-12 19:05:00 +0300

    reference pattern stub simplified

--- src/org/jetbrains/plugins/scala/lang/psi/stubs/impl/ScReferencePatternStubImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/impl/ScReferencePatternStubImpl.scala
@@ -4,35 +4,19 @@ package psi
 package stubs
 package impl
 
-
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.{IStubElementType, StubBase, StubElement}
 import com.intellij.util.io.StringRef
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
 
 /**
- * User: Alexander Podkhalyuzin
- * Date: 17.07.2009
- */
+  * User: Alexander Podkhalyuzin
+  * Date: 17.07.2009
+  */
+class ScReferencePatternStubImpl(parent: StubElement[_ <: PsiElement],
+                                 elementType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement],
+                                 private val nameRef: StringRef)
+  extends StubBase[ScReferencePattern](parent, elementType) with ScReferencePatternStub {
 
-class ScReferencePatternStubImpl[ParentPsi <: PsiElement](parent: StubElement[ParentPsi],
-                                                  elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement])
-  extends StubBase[ScReferencePattern](parent, elemType) with ScReferencePatternStub {
-  private var name: StringRef = _
-
-  def this(parent: StubElement[ParentPsi],
-          elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement],
-          name: String) = {
-    this(parent, elemType.asInstanceOf[IStubElementType[StubElement[PsiElement], PsiElement]])
-    this.name = StringRef.fromString(name)
-  }
-
-  def this(parent: StubElement[ParentPsi],
-          elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement],
-          name: StringRef) = {
-    this(parent, elemType.asInstanceOf[IStubElementType[StubElement[PsiElement], PsiElement]])
-    this.name = name
-  }
-
-  def getName: String = StringRef.toString(name)
+  def getName: String = StringRef.toString(nameRef)
 }
