e66077425806cc4e1406210fb8d308a8eedb285f
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-10-14 16:59:06 +0300

    primary constructor stub simplified

--- src/org/jetbrains/plugins/scala/lang/psi/stubs/impl/ScPrimaryConstructorStubImpl.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/impl/ScPrimaryConstructorStubImpl.scala
@@ -4,15 +4,14 @@ package psi
 package stubs
 package impl
 
-
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.{IStubElementType, StubBase, StubElement}
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScPrimaryConstructor
-/**
- * User: Alexander Podkhalyuzin
- * Date: 17.06.2009
- */
 
-class ScPrimaryConstructorStubImpl [ParentPsi <: PsiElement](parent: StubElement[ParentPsi],
-                                                  elemType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement])
-  extends StubBase[ScPrimaryConstructor](parent, elemType) with ScPrimaryConstructorStub
+/**
+  * User: Alexander Podkhalyuzin
+  * Date: 17.06.2009
+  */
+class ScPrimaryConstructorStubImpl(parent: StubElement[_ <: PsiElement],
+                                   elementType: IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement])
+  extends StubBase[ScPrimaryConstructor](parent, elementType) with ScPrimaryConstructorStub
