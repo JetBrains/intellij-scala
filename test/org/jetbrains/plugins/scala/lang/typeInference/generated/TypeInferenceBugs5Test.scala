0768af0637874c541dd36d5a2540090240a8e870
Roman Shein <marso.des@gmail.com>
2016-10-18 16:03:52 +0300

    Proper equivalence for Nothing Partial fix for SCL-10857 Some refactorings in conformance Equivalence for undefined types and parameterized types with undefined designators checks aliases without expansion first Not all tests are passing, this is an intermediate commit Moved passing tests (not all previously fixed tests are passing now though)

--- test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
+++ test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
@@ -862,4 +862,18 @@ class TypeInferenceBugs5Test extends TypeInferenceTestBase {
       """.stripMargin
     doTest(code)
   }
+
+  def testSCL7474(): Unit = doTest(
+    """
+      | object Repro {
+      |
+      |   import scala.collection.generic.IsTraversableLike
+      |
+      |   def head[A](a: A)(implicit itl: IsTraversableLike[A]): itl.A = itl.conversion(a).head
+      |
+      |   val one: Int = /*start*/head(Vector(1, 2, 3))/*end*/
+      | }
+      |
+      | //Int""".stripMargin
+  )
 }
