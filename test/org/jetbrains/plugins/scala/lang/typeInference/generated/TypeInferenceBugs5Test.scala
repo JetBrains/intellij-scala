671a7502b380319b2435c7a8893e25e129990f32
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2016-10-19 15:17:02 +0300

    Fixed expected type in case of abstract expected type for tuples.

--- test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
+++ test/org/jetbrains/plugins/scala/lang/typeInference/generated/TypeInferenceBugs5Test.scala
@@ -876,6 +876,10 @@ class TypeInferenceBugs5Test extends TypeInferenceTestBase {
 
   def testTupleAnonymous(): Unit = doTest()
 
+  def testTupleExpectedType(): Unit = doTest()
+
+  def testTupleExpectedType2(): Unit = doTest()
+
   def test10471A(): Unit = {
     val code =
       """
