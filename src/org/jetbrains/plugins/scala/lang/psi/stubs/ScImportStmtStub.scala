6606acf03e7162193b1784ab74a7bb993ebad9ad
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2016-10-31 13:45:12 +0300

    import statement stub simplified

--- src/org/jetbrains/plugins/scala/lang/psi/stubs/ScImportStmtStub.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/ScImportStmtStub.scala
@@ -3,15 +3,13 @@ package lang
 package psi
 package stubs
 
-
 import com.intellij.psi.stubs.StubElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportStmt
 
 /**
- * User: Alexander Podkhalyuzin
- * Date: 18.06.2009
- */
-
+  * User: Alexander Podkhalyuzin
+  * Date: 18.06.2009
+  */
 trait ScImportStmtStub extends StubElement[ScImportStmt] {
-  def getImportText: String
+  def importText: String
 }
--- src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/ScImportStmtElementType.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/stubs/elements/ScImportStmtElementType.scala
@@ -7,6 +7,7 @@ package elements
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.{StubElement, StubInputStream, StubOutputStream}
+import com.intellij.util.io.StringRef.fromString
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportStmt
 import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.imports.ScImportStmtImpl
 import org.jetbrains.plugins.scala.lang.psi.stubs.impl.ScImportStmtStubImpl
@@ -15,17 +16,18 @@ import org.jetbrains.plugins.scala.lang.psi.stubs.impl.ScImportStmtStubImpl
   * User: Alexander Podkhalyuzin
   * Date: 18.06.2009
   */
-class ScImportStmtElementType[Func <: ScImportStmt]
-  extends ScStubElementType[ScImportStmtStub, ScImportStmt]("import statement") {
+class ScImportStmtElementType extends ScStubElementType[ScImportStmtStub, ScImportStmt]("import statement") {
   override def serialize(stub: ScImportStmtStub, dataStream: StubOutputStream): Unit = {
-    dataStream.writeName(stub.getImportText)
+    dataStream.writeName(stub.importText)
   }
 
   override def deserialize(dataStream: StubInputStream, parentStub: StubElement[_ <: PsiElement]): ScImportStmtStub =
-    new ScImportStmtStubImpl(parentStub, this, dataStream.readName.toString)
+    new ScImportStmtStubImpl(parentStub, this,
+      importTextRef = dataStream.readName)
 
-  override def createStub(psi: ScImportStmt, parentStub: StubElement[_ <: PsiElement]): ScImportStmtStub =
-    new ScImportStmtStubImpl(parentStub, this, psi.getText)
+  override def createStub(statement: ScImportStmt, parentStub: StubElement[_ <: PsiElement]): ScImportStmtStub =
+    new ScImportStmtStubImpl(parentStub, this,
+      importTextRef = fromString(statement.getText))
 
   override def createElement(node: ASTNode): ScImportStmt = new ScImportStmtImpl(node)
 
