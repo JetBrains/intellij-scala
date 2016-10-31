000e472ecb44fa28c38507153a1938c5c3526f3b
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-10-31 22:07:30 +0300

    metahost: add dynamic conformance checks for quiasiquote parameters

--- src/scala/meta/intellij/QuasiquoteInferUtil.scala
+++ src/scala/meta/intellij/QuasiquoteInferUtil.scala
@@ -1,11 +1,14 @@
 package scala.meta.intellij
 
-import com.intellij.psi.PsiManager
+import com.intellij.psi.{PsiElementFactory, PsiManager}
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScInterpolated, ScInterpolatedStringLiteral}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.lang.psi.impl.base.patterns.ScInterpolationPatternImpl
+import org.jetbrains.plugins.scala.lang.psi.impl.expr.{ScMethodCallImpl, ScReferenceExpressionImpl}
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
+import org.jetbrains.plugins.scala.lang.psi.types.nonvalue.Parameter
 import org.jetbrains.plugins.scala.lang.psi.types.result.{Failure, TypeResult}
 import org.jetbrains.plugins.scala.lang.resolve.{ResolvableReferenceElement, ScalaResolveResult}
 
@@ -31,6 +34,17 @@ object QuasiquoteInferUtil {
     fqnO.exists(_.startsWith("scala.meta.quasiquotes.Api.XtensionQuasiquote"))
   }
 
+  def extractQQFromParts(ref: ScReferenceExpression): String = {
+
+    ref.qualifier match {
+      case Some(mc: ScMethodCallImpl) => mc.argumentExpressions.zipWithIndex.foldLeft("") {
+        case (a,(expr, i)) => s"$a $$__meta$i${expr.text}"
+      }
+      case _ => ""
+    }
+  }
+
+
   def parseQQExpr(prefix: String, text: String, dialect: m.Dialect): Parsed[m.Tree] = {
     val p = dialect(text)
     prefix match {
@@ -55,6 +69,38 @@ object QuasiquoteInferUtil {
     }
   }
 
+  def getMetaQQExpectedTypes(stringContextApplicationRef: ScReferenceExpression): Seq[Parameter] = {
+    val joined = stringContextApplicationRef.qualifier match {
+      case Some(mc: ScMethodCallImpl) => mc.argumentExpressions.zipWithIndex.foldLeft("") {
+        case (a, (expr, i)) if i > 0 =>
+          s"$a $$__meta$i${expr.text.replaceAll("^\"\"\"", "").replaceAll("\"\"\"$", "")}"
+        case (_, (expr, i)) if i == 0 =>
+          expr.text.replaceAll("^\"\"\"", "").replaceAll("\"\"\"$", "")
+      }
+      case _ => ""
+    }
+    val qqdialect = if (joined.contains("\n"))
+      m.Dialect.forName("QuasiquoteTerm(Scala211, Multi)")
+    else
+      m.Dialect.forName("QuasiquoteTerm(Scala211, Single)")
+    val typeStrings = parseQQExpr(stringContextApplicationRef.refName, joined, qqdialect) match {
+      case Parsed.Success(qqparts) =>
+        val parts = collectQQParts(qqparts)
+        val classes = parts.map(_.pt)
+        classes.map(classToScTypeString)
+      case Parsed.Error(_, cause, exc) =>
+        Seq.empty
+    }
+    val types = typeStrings.map(ScalaPsiElementFactory.createTypeFromText(_, stringContextApplicationRef, null))
+    val treeType = ScalaPsiElementFactory.createTypeFromText("scala.meta.Tree", stringContextApplicationRef, null)
+    types.zipWithIndex.map {
+      case (Some(tp), i) =>
+        new Parameter(s"__meta$i", None, tp, isDefault = false, isRepeated = false, isByName = false, index = i)
+      case (None, i) =>
+        new Parameter(s"__meta$i", None, treeType.get, isDefault = false, isRepeated = false, isByName = false, index = i)
+    }
+  }
+
   def getMetaQQExprType(pat: ScInterpolatedStringLiteral): TypeResult[ScType] = {
     val patternText = escapeQQ(pat)
     val qqdialect = if (pat.isMultiLineString)
@@ -73,14 +119,15 @@ object QuasiquoteInferUtil {
     }
   }
 
+  private def collectQQParts(t: scala.meta.Tree): Seq[m.internal.ast.Quasi] = {
+    t.children.flatMap {
+      case qq: m.internal.ast.Quasi => Some(qq)
+      case other => collectQQParts(other)
+    }
+  }
+
   def getMetaQQPatternTypes(pat: ScInterpolationPatternImpl): Seq[String] = {
 
-    def collectQQParts(t: scala.meta.Tree): Seq[m.internal.ast.Quasi] = {
-      t.children.flatMap {
-        case qq: m.internal.ast.Quasi => Some(qq)
-        case other => collectQQParts(other)
-      }
-    }
 
     val prefix = pat.ref.refName
     val patternText = escapeQQ(pat)
