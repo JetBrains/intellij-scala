9a0cdc44741863f4ae5d6fa180770ff3aafdd97e
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2016-11-02 19:39:44 +0300

    don't throw exception inside write actions, use stackless exception everywhere to not pollute logs

--- src/org/jetbrains/plugins/scala/util/UIFreezingGuard.scala
+++ src/org/jetbrains/plugins/scala/util/UIFreezingGuard.scala
@@ -8,9 +8,9 @@ import java.util.concurrent.atomic.AtomicLong
 import com.intellij.concurrency.JobScheduler
 import com.intellij.diagnostic.PerformanceWatcher
 import com.intellij.ide.IdeEventQueue
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.{ApplicationManager, ModalityState}
 import com.intellij.openapi.components.ApplicationComponent
-import com.intellij.openapi.progress.{EmptyProgressIndicator, ProcessCanceledException, ProgressManager}
+import com.intellij.openapi.progress._
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.util.UIFreezingGuard._
 
@@ -20,21 +20,6 @@ import scala.util.control.NoStackTrace
   * @author Nikolay.Tropin
   */
 class UIFreezingGuard extends ApplicationComponent {
-
-  private object progress extends EmptyProgressIndicator {
-    private val counter = new AtomicLong()
-    
-    def timestamp: Long = counter.get()
-
-    override def start(): Unit = {
-      counter.incrementAndGet()
-      super.start()
-    }
-
-    def cancel(l: Long): Unit = {
-      if (timestamp == l) super.cancel()
-    }
-  }
 
   private val periodMs = 10
 
@@ -58,17 +43,17 @@ object UIFreezingGuard {
 
   //used in macro!
   def withResponsibleUI[T](body: => T): T = {
-    val app = ApplicationManager.getApplication
-    val progressManager = ProgressManager.getInstance()
-
     if (!isAlreadyGuarded) {
-
-      if (hasPendingUserInput) throw pceInstance
-
       val start = System.currentTimeMillis()
       try {
-        if (!app.isWriteAccessAllowed && !progressManager.hasProgressIndicator)
-          progressManager.runProcess(body, ourProgress)
+        val progressManager = ProgressManager.getInstance()
+        if (!ApplicationManager.getApplication.isWriteAccessAllowed && !progressManager.hasProgressIndicator) {
+
+          if (hasPendingUserInput)
+            throw pceInstance
+
+          progressManager.runProcess(body, progress)
+        }
         else
           body
       } finally {
@@ -81,7 +66,7 @@ object UIFreezingGuard {
   //used in macro to reduce number of `withResponsibleUI` calls in the stacktrace
   def isAlreadyGuarded: Boolean = {
     val edt = ApplicationManager.getApplication.isDispatchThread
-    edt && ourProgress.isRunning || !edt
+    edt && progress.isRunning || !edt
   }
 
   private def dumpThreads(ms: Long): Unit = {
@@ -91,8 +76,6 @@ object UIFreezingGuard {
     }
   }
 
-  private def ourProgress = ApplicationManager.getApplication.getComponent(classOf[UIFreezingGuard]).progress
-
   private def hasPendingUserInput: Boolean = {
     val queue = IdeEventQueue.getInstance()
     val userEventIds = Seq(Event.KEY_ACTION, Event.KEY_PRESS, MouseEvent.MOUSE_PRESSED, MouseEvent.MOUSE_WHEEL)
@@ -100,5 +83,54 @@ object UIFreezingGuard {
     userEventIds.exists(queue.peekEvent(_) != null)
   }
 
-  private val pceInstance = new ProcessCanceledException() with NoStackTrace
+  private val pceInstance = new ProcessCanceledException() with NoStackTrace {
+    override def getMessage: String = "Long scala calculation on UI thread canceled"
+  }
+
+  private object progress extends StandardProgressIndicator {
+    val delegate = new EmptyProgressIndicator()
+
+    private val counter = new AtomicLong()
+
+    def timestamp: Long = counter.get()
+
+    def start(): Unit = {
+      counter.incrementAndGet()
+      delegate.start()
+    }
+
+    def cancel(l: Long): Unit = {
+      if (timestamp == l) delegate.cancel()
+    }
+
+    //to avoid long stacktraces in log and keep write actions
+    def checkCanceled(): Unit = {
+      if (isCanceled && !ApplicationManager.getApplication.isWriteAccessAllowed)
+        throw pceInstance
+    }
+
+    //EmptyProgressIndicator is good enough, but it has final `checkCanceled()` method
+    def cancel(): Unit = delegate.cancel()
+    def isRunning: Boolean = delegate.isRunning
+    def pushState(): Unit = delegate.pushState()
+    def setIndeterminate(indeterminate: Boolean): Unit = delegate.setIndeterminate(indeterminate)
+    def finishNonCancelableSection(): Unit = delegate.finishNonCancelableSection()
+    def setModalityProgress(modalityProgress: ProgressIndicator): Unit = delegate.setModalityProgress(modalityProgress)
+    def isCanceled: Boolean = delegate.isCanceled
+    def isIndeterminate: Boolean = delegate.isIndeterminate
+    def isModal: Boolean = delegate.isModal
+    def setFraction(fraction: Double): Unit = delegate.setFraction(fraction)
+    def stop(): Unit = delegate.stop()
+    def getText: String = delegate.getText
+    def setText(text: String): Unit = delegate.setText(text)
+    def isPopupWasShown: Boolean = delegate.isPopupWasShown
+    def setText2(text: String): Unit = delegate.setText2(text)
+    def startNonCancelableSection(): Unit = delegate.startNonCancelableSection()
+    def getModalityState: ModalityState = delegate.getModalityState
+    def getFraction: Double = delegate.getFraction
+    def popState(): Unit = delegate.popState()
+    def getText2: String = delegate.getText2
+    def isShowing: Boolean = delegate.isShowing
+  }
+
 }
