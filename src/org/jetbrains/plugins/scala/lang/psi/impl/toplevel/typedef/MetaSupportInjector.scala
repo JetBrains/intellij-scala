360dcceca32e453c271484c8fb4467476a1ff6fd
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-11-09 20:51:50 +0300

    metahost: drop processdeclarations-based approach in favour of members injector
    should be more reliable at the cost of not being able to delete or modify existing members
    
    (cherry picked from commit 8733b30)

--- src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MetaSupportInjector.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/MetaSupportInjector.scala
@@ -1,7 +1,10 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
-import org.jetbrains.plugins.scala.lang.psi.api.statements.ScAnnotationsHolder
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScObject, ScTypeDefinition}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScParameter, ScParameterClause}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScAnnotationsHolder, ScFunction, ScValue}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScObject, ScTypeDefinition}
+
+import scala.meta._
 
 class MetaSupportInjector extends SyntheticMembersInjector {
   /**
@@ -37,13 +40,88 @@ class MetaSupportInjector extends SyntheticMembersInjector {
   }
 
   override def injectMembers(source: ScTypeDefinition): Seq[String] = {
-    source match {
+    injectForCompanionObject(source) ++ injectForThis(source)
+  }
+
+  // theese two implementations must produce identical results on equivalent trees of PSI and scala.meta
+  private def hash(member: ScMember): Int = {
+    def paramHash(p: ScParameter) = p.typeElement.map(_.text.hashCode).getOrElse(0)
+    def paramClauseHash(pc: ScParameterClause) = pc.parameters.foldLeft(0)((a,b)=>a+paramHash(b))
+    member match {
+      case td: ScTypeDefinition => td.getName.hashCode
+      case vd: ScValue => vd.declaredNames.foldLeft(0)((a,b) => a+b.hashCode)
+      case fu: ScFunction =>
+        fu.name.hashCode +
+          13*fu.paramClauses.clauses.foldLeft(0)((a,b)=>a+paramClauseHash(b)) +
+          31*fu.returnTypeElement.map(_.text.hashCode).getOrElse(0)
+      case other => other.getName.hashCode
+    }
+  }
+
+  // theese two implementations must produce identical results on equivalent trees of PSI and scala.meta
+  private def hash(member: Defn): Int = {
+    def collectAllNames(tree: Tree): Seq[Name] = tree match {
+      case t:Term.Name => Seq(t)
+      case t:Type.Name => Seq(t)
+      case other => other.children.flatMap(collectAllNames)
+    }
+    def patHash(pat: Pat) = collectAllNames(pat).foldLeft(0)((a,b)=>a+b.hashCode())
+    member match {
+      case Defn.Def(_, name, _, paramss, tpe, _) =>
+        name.value.hashCode +
+        13*paramss.foldLeft(0)((a, b) => a+b.foldLeft(0)((c,d) => c+ d.name.value.hashCode)) +
+        31*tpe.map(_.toString().hashCode).getOrElse(0)
+      case Defn.Val(_, pats, _, _) => pats.foldLeft(0)((a,b)=>a+patHash(b))
+      case Defn.Var(_, pats, _, _) => pats.foldLeft(0)((a,b)=>a+patHash(b))
+      case Defn.Class(_, Type.Name(value), _, _, _) => value.hashCode
+      case Defn.Trait(_, Type.Name(value), _, _, _) => value.hashCode
+      case Defn.Object(_, Term.Name(value), _) => value.hashCode
+      case _ => 0
+    }
+  }
+
+  private def getDiff(a: ScTypeDefinition, b: Template): Seq[Tree] = {
+    val aMembers = a.extendsBlock.members.map(hash).toSet
+    val bMembers = b.stats.getOrElse(Seq.empty).flatMap {
+      case m: Defn => Some(m)
+      case _ => None
+    }
+    bMembers.filter(m => !aMembers.contains(hash(m))).map(trimBodies)
+  }
+
+
+  // this method injects members extracted from the expansion of companion class of current object
+  private def injectForCompanionObject(source: ScTypeDefinition): Seq[String] = {
+    val companionExpansion: Either[String, Tree] = source match {
       case obj: ScObject => obj.fakeCompanionClassOrCompanionClass match {
-        case ah: ScAnnotationsHolder =>
-          ScalaPsiUtil.getMetaCompanionObject(ah).map(_.members.map(_.getText)).getOrElse(Seq.empty)
-        case _ => Seq.empty
+        case ah: ScAnnotationsHolder => ah.getMetaExpansion
+        case _ => Left("")
       }
+      case _ => Left("")
+    }
+    companionExpansion match {
+      case Right(Term.Block(Seq(_, obj: Defn.Object))) => getDiff(source, obj.templ).map(_.toString())
       case _ => Seq.empty
     }
+  }
+
+  private def trimBodies(tree: Tree): Tree = tree match {
+    case Defn.Val(mods, pats, decltpe, _) => Defn.Val(mods, pats, decltpe, Term.Name("???"))
+    case Defn.Var(mods, pats, decltpe, _) => Defn.Var(mods, pats, decltpe, Some(Term.Name("???")))
+    case Defn.Def(mods, name, tparams, paramss, tpe, _) => Defn.Def(mods, name, tparams, paramss, tpe, Term.Name("???"))
+    case other => other
+  }
+
+  private def injectForThis(source: ScTypeDefinition): Seq[String] = {
+    val template = source.getMetaExpansion match {
+      case Right(Defn.Class(_, _, _, _, templ))                     => Some(templ)
+      case Right(Defn.Trait(_, _, _, _, templ))                     => Some(templ)
+      case Right(Defn.Object(_, _, templ))                          => Some(templ)
+      case Right(Term.Block(Seq(Defn.Class(_, _, _, _, templ), _))) => Some(templ)
+      case Right(Term.Block(Seq(Defn.Trait(_, _, _, _, templ), _))) => Some(templ)
+      case Right(Term.Block(Seq(Defn.Object(_, _, templ), _)))      => Some(templ)
+      case _ => None
+    }
+    template.map(getDiff(source, _)).map(defns=>defns.map(t=>trimBodies(t).toString())).getOrElse(Seq.empty)
   }
 }
