32b7566b46f4a57bd89b3a2a3188de49beac24c0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-12-07 17:18:33 +0300

    metahost: fix return type elements conversion fix #SCL-11099
    (cherry picked from commit 4922b26)

--- test/scala/meta/annotations/MetaAnnotationTestBase.scala
+++ test/scala/meta/annotations/MetaAnnotationTestBase.scala
@@ -1,52 +1,57 @@
 package scala.meta.annotations
 
+import java.io.File
+
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.module.{JavaModuleType, Module}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
 import com.intellij.testFramework.{PsiTestUtil, VfsTestUtil}
-import org.jetbrains.plugins.scala.base.{DisposableScalaLibraryLoader, ScalaFixtureTestCase}
+import org.jetbrains.plugins.scala.base.DisposableScalaLibraryLoader
 import org.jetbrains.plugins.scala.debugger.{Compilable, DebuggerTestUtil}
 import org.jetbrains.plugins.scala.extensions
-import org.jetbrains.plugins.scala.util.TestUtils.ScalaSdkVersion
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScAnnotationsHolder
+import org.jetbrains.plugins.scala.util.TestUtils
+import org.junit.Assert
 
 import scala.meta.ScalametaUtils
 
-abstract class MetaAnnotationTestBase extends ScalaFixtureTestCase(scalaVersion = ScalaSdkVersion._2_11_8,
-  loadReflect = true) with Compilable with ScalametaUtils {
+abstract class MetaAnnotationTestBase extends JavaCodeInsightFixtureTestCase with Compilable with ScalametaUtils {
 
   override def getCompileableProject: Project = myFixture.getProject
-  override def getMainModule: Module = myFixture.getModule
+  override def getMainModule: Module = myModule
   override def getRootDisposable: Disposable = getTestRootDisposable
   override def getTestName: String = getTestName(false)
-  override protected def rootPath = null
-  val metaSourceRoot = "meta"
 
-  lazy val root = extensions.inWriteAction {
-    Option(myFixture.getProject.getBaseDir.findChild(metaSourceRoot))
-      .getOrElse(myFixture.getProject.getBaseDir.createChildDirectory(null, metaSourceRoot))
-  }
+  override protected def getTestDataPath: String = TestUtils.getTestDataPath + "/scalameta"
 
   override def setUp(): Unit = {
     super.setUp()
     setUpCompler()
-    val metaModule = PsiTestUtil.addModule(myFixture.getProject, JavaModuleType.getModuleType, metaSourceRoot, root)
+  }
+
+  def compileMetaSource(source: String): List[String] = {
+    val root = extensions.inWriteAction {
+      Option(myFixture.getProject.getBaseDir.findChild("meta")).getOrElse(myFixture.getProject.getBaseDir.createChildDirectory(null, "meta"))
+    }
+    val metaModule = PsiTestUtil.addModule(myFixture.getProject, JavaModuleType.getModuleType, "meta", root)
     val loader = new DisposableScalaLibraryLoader(getProject, metaModule, null, true, Some(DebuggerTestUtil.findJdk8()))
     loader.loadScala(scalaSdkVersion)
     addAllMetaLibraries(metaModule)
+    enableParadisePlugin(myFixture.getProject)
     extensions.inWriteAction {
       val modifiableRootModel = ModuleRootManager.getInstance(myModule).getModifiableModel
-      enableParadisePlugin(myFixture.getProject)
-      modifiableRootModel.setSdk(DebuggerTestUtil.findJdk8())
       modifiableRootModel.addModuleOrderEntry(metaModule)
       modifiableRootModel.commit()
-      val modifiableMetaRootModel = ModuleRootManager.getInstance(metaModule).getModifiableModel
-      modifiableMetaRootModel.setSdk(DebuggerTestUtil.findJdk8())
-      modifiableMetaRootModel.commit()
     }
-  }
-
-  def compileMetaSource(source: String) = {
+    extensions.inWriteAction {
+      val modifiableRootModel = ModuleRootManager.getInstance(metaModule).getModifiableModel
+      modifiableRootModel.setSdk(DebuggerTestUtil.findJdk8())
+      modifiableRootModel.commit()
+    }
     VfsTestUtil.createFile(root, "meta.scala", source)
     try {
       make()
@@ -55,4 +60,15 @@ abstract class MetaAnnotationTestBase extends ScalaFixtureTestCase(scalaVersion 
     }
   }
 
+  def compileMetaSource(): List[String] = compileMetaSource(FileUtil.loadFile(new File(getTestDataPath, s"$getTestName.scala")))
+
+  def checkExpansionEquals(code: String, expectedExpansion: String): Unit = {
+    myFixture.configureByText(s"Usage$getTestName.scala", code)
+    val holder = ScalaPsiUtil.getParentOfType(myFixture.getElementAtCaret, classOf[ScAnnotationsHolder]).asInstanceOf[ScAnnotationsHolder]
+    holder.getMetaExpansion match {
+      case Right(tree)                      => Assert.assertEquals(expectedExpansion, tree.toString())
+      case Left(reason) if reason.nonEmpty  => Assert.fail(reason)
+      case Left("")                         => Assert.fail("Expansion was empty - did annotation even run?")
+    }
+  }
 }
