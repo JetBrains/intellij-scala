8516041e07b5eba168ad9c058280f2ac25e180cc
Anton.Yalyshev <yalishev.ant@gmail.com>
2016-12-19 17:01:35 +0300

    added test for SCL-11140

--- test/org/jetbrains/plugins/scala/failed/typeInference/SetConformanceTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/SetConformanceTest.scala
@@ -54,4 +54,23 @@ class SetConformanceTest extends ScalaLightCodeInsightFixtureTestAdapter {
        |}
        |//true
     """.stripMargin)
+
+  def testSCL11140(): Unit = checkTextHasNoErrors(
+    s"""
+       |import scala.collection.{Map, mutable}
+       |import scala.collection.generic.CanBuildFrom
+       |
+       |object IntelliBugs {
+       |  implicit class MapOps[K2, V2, M[K, V] <: Map[K, V]](val m: M[K2, V2]) extends AnyVal {
+       |    def mapValuesStrict[V3](f: V2 => V3)(implicit cbf: CanBuildFrom[M[K2, V2], (K2, V3), M[K2, V3]]) =
+       |      m.map { case (k, v) => k -> f(v) }
+       |  }
+       |
+       |  def main(args: Array[String]): Unit = {
+       |    val m = mutable.HashMap.empty[String, Int]
+       |    val m2: Map[String, Long] = m.mapValuesStrict(_.toLong)
+       |  }
+       |}
+       |//true
+    """.stripMargin)
 }
