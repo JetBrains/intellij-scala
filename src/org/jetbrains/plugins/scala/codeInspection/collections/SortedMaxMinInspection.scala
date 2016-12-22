f204ecc220fd0f203f2575714b9dae9c292c2af3
Markus Hauck <markus1189@gmail.com>
2016-12-22 15:11:53 +0300

    Inspections for sort{ed,By} with {head,last} #SCL-11148 fixed

---
+++ src/org/jetbrains/plugins/scala/codeInspection/collections/SortedMaxMinInspection.scala
@@ -0,0 +1,52 @@
+package org.jetbrains.plugins.scala.codeInspection.collections
+
+import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+
+/**
+ * @author Markus.Hauck
+ */
+class SortedMaxMinInspection extends OperationOnCollectionInspection {
+  override def possibleSimplificationTypes: Array[SimplificationType] =
+    Array(SortedHeadIsMin, SortedLastIsMax, SortByHeadIsMinBy, SortByLastIsMaxBy)
+}
+
+object SortedHeadIsMin extends SimplificationType {
+  override def hint: String = InspectionBundle.message("replace.sorted.head.with.min")
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case qual`.sorted`()`.head`() =>
+      Some(replace(expr).withText(invocationText(qual, "min")).highlightFrom(qual))
+    case _ => None
+  }
+}
+
+object SortedLastIsMax extends SimplificationType {
+  override def hint: String = InspectionBundle.message("replace.sorted.last.with.max")
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case qual`.sorted`()`.last`() =>
+      Some(replace(expr).withText(invocationText(qual, "max")).highlightFrom(qual))
+    case _ => None
+  }
+}
+
+object SortByHeadIsMinBy extends SimplificationType {
+  override def hint: String = InspectionBundle.message("replace.sortBy.head.with.minBy")
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case qual`.sortBy`(f)`.head`() =>
+      Some(replace(expr).withText(invocationText(qual, "minBy", f)).highlightFrom(qual))
+    case _ => None
+  }
+}
+
+object SortByLastIsMaxBy extends SimplificationType {
+  override def hint: String = InspectionBundle.message("replace.sortBy.last.with.maxBy")
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case qual`.sortBy`(f)`.last`() =>
+      Some(replace(expr).withText(invocationText(qual, "maxBy", f)).highlightFrom(qual))
+    case _ => None
+  }
+}
