b3a518fc63634d9a8833615f35019661c16c3a94
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-01-13 17:03:37 +0100

    DesugarForIntention: rely on ExpandForComprehension transformation

--- src/org/jetbrains/plugins/scala/codeInsight/intention/comprehension/DesugarForIntention.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/comprehension/DesugarForIntention.scala
@@ -1,38 +1,12 @@
-package org.jetbrains.plugins.scala
-package codeInsight
-package intention
-package comprehension
+package org.jetbrains.plugins.scala.codeInsight.intention.comprehension
 
-import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
-import com.intellij.psi.codeStyle.CodeStyleManager
+import org.jetbrains.plugins.scala.codeInsight.intention.AbstractIntention
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScForStatement
-import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.lang.transformation.general.ExpandForComprehension
 
-class DesugarForIntention extends PsiElementBaseIntentionAction {
-  def getFamilyName = "Convert to desugared expression"
-
-  override def getText = "Convert for comprehension to desugared expression"
-
-  def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
-    element match {
-      case Parent(_: ScForStatement) => true
-      case _ => false
-    }
+class DesugarForIntention extends AbstractIntention("Desugar for comprehension", "Convert to desugared expression")(
+  (project, _) => {
+    case Parent(statement: ScForStatement) => new ExpandForComprehension().transformation(project)(statement)
   }
-
-  override def invoke(project: Project, editor: Editor, element: PsiElement) {
-    val statement = element.getParent.asInstanceOf[ScForStatement]
-    statement.getDesugarizedExprText(forDisplay = true) match {
-      case Some(expText) =>
-        val desugared = ScalaPsiElementFactory.createExpressionWithContextFromText(expText, statement.getContext, statement)
-        val result = statement.replace(desugared.copy())
-        val manager: CodeStyleManager = CodeStyleManager.getInstance(project)
-        manager.reformat(result)
-     case None =>
-    }
-  }
-}
+)
