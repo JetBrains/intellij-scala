45c959e713922695ac5457af08d13ff82efe690d
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-01-13 17:02:21 +0100

    AbstractIntention base class

---
+++ src/org/jetbrains/plugins/scala/codeInsight/intention/AbstractIntention.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala.codeInsight.intention
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+
+abstract class AbstractIntention(text: String, familyName: String)
+                                (f: (Project, Editor) => PartialFunction[PsiElement, Unit])
+  extends PsiElementBaseIntentionAction {
+
+  override def getText: String = text
+
+  override def getFamilyName: String = familyName
+
+  override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean =
+    f(project, editor).isDefinedAt(element)
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement): Unit =
+    f.apply(project, editor)(element)
+}
