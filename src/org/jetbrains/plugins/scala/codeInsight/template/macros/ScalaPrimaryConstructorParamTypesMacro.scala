f8936a85baca7e3db79ed8e1a72a43cd9172d932
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2017-01-17 15:32:43 +0300

    NPE fixed

--- src/org/jetbrains/plugins/scala/codeInsight/template/macros/ScalaPrimaryConstructorParamTypesMacro.scala
+++ src/org/jetbrains/plugins/scala/codeInsight/template/macros/ScalaPrimaryConstructorParamTypesMacro.scala
@@ -2,17 +2,20 @@ package org.jetbrains.plugins.scala.codeInsight.template.macros
 
 import com.intellij.codeInsight.template._
 import org.jetbrains.plugins.scala.codeInsight.template.util.MacroUtil
+import org.jetbrains.plugins.scala.extensions.ObjectExt
 
 /**
   * @author Roman.Shein
   *         Date: 21.12.2015
   */
 class ScalaPrimaryConstructorParamTypesMacro extends Macro {
-  override def calculateResult(params: Array[Expression], context: ExpressionContext): Result =
-    params.headOption.map(_.calculateResult(context).toString).map(MacroUtil.paramPairs(_).map(_._2)) match {
+  override def calculateResult(params: Array[Expression], context: ExpressionContext): Result = {
+    val result = params.headOption.flatMap(_.calculateResult(context).toOption).map(_.toString)
+    result.map(MacroUtil.paramPairs(_).map(_._2)) match {
       case Some(head::tail) => new TextResult(addParens(tail.foldLeft(head)(_ + ", " + _), tail.nonEmpty))
       case _ => null
     }
+  }
 
   def getName: String = MacroUtil.scalaIdPrefix + "primaryConstructorParamTypes"
 
