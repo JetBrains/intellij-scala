2fd61151126bbff54997ef8ee791870b005cc7c9
Noam Almog <noama@wix.com>
2017-01-26 17:03:52 +0300

    specs2 matcher inspection
    (cherry picked from commit 188d1fb)

---
+++ test/org/jetbrains/plugins/scala/codeInspection/specs2/BuiltinMatcherExistsInspectionTest.scala
@@ -0,0 +1,134 @@
+package org.jetbrains.plugins.scala.codeInspection.specs2
+
+import com.intellij.testFramework.EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
+import org.jetbrains.plugins.scala.codeInspection.{InspectionBundle, ScalaQuickFixTestBase}
+
+class BuiltinMatcherExistsInspectionTest extends ScalaQuickFixTestBase {
+
+  override protected val description: String = InspectionBundle.message("specs2.use.builtin.matcher")
+
+  private val hint = InspectionBundle.message("specs2.builtin.matcher.alternative.exists")
+  protected val classOfInspection = classOf[BuiltinMatcherExistsInspection]
+
+  //OperationOnCollectionInspectionBase
+  def testMustBeSomeSimplification(): Unit = {
+    Seq("be_===", "be_==", "beEqualTo", "equalTo", "beTypedEqualTo", "typedEqualTo").foreach { matcher =>
+      val code =
+        s"""
+          |expr must $START$matcher(Some("123"))$END
+        """.stripMargin
+      val expected =
+        """
+          |expr must beSome("123")
+        """.stripMargin
+
+      checkTextHasError(code)
+
+      testQuickFix(code, expected, hint)
+    }
+  }
+
+  def testMustBeNoneSimplification(): Unit = {
+    Seq("be_===", "be_==", "beEqualTo", "equalTo", "beTypedEqualTo", "typedEqualTo").foreach { matcher =>
+      val code =
+        s"""
+          |expr must $START$matcher(None)$END
+        """.stripMargin
+      val expected =
+        """
+          |expr must beNone
+        """.stripMargin
+
+      checkTextHasError(code)
+
+      testQuickFix(code, expected, hint)
+    }
+  }
+
+  def testMustBeLeftSimplification(): Unit = {
+    Seq("be_===", "be_==", "beEqualTo", "equalTo", "beTypedEqualTo", "typedEqualTo").foreach { matcher =>
+      val code =
+        s"""
+           |expr must $START$matcher(Left("123"))$END
+        """.stripMargin
+      val expected =
+        """
+          |expr must beLeft("123")
+        """.stripMargin
+
+      checkTextHasError(code)
+
+      testQuickFix(code, expected, hint)
+    }
+  }
+
+  def testMustBeRightSimplification(): Unit = {
+    Seq("be_===", "be_==", "beEqualTo", "equalTo", "beTypedEqualTo", "typedEqualTo").foreach { matcher =>
+      val code =
+        s"""
+           |expr must $START$matcher(Right("123"))$END
+        """.stripMargin
+      val expected =
+        """
+          |expr must beRight("123")
+        """.stripMargin
+
+      checkTextHasError(code)
+
+      testQuickFix(code, expected, hint)
+    }
+  }
+
+  def testMustEqualBeSomeSimplification(): Unit = {
+    Seq("must_===", "must_==", "mustEqual").foreach { matcher =>
+      val code =
+        s"""
+           |${START}expr must_=== Some("123")$END
+        """.stripMargin
+      val expected =
+        """
+          |expr must beSome("123")
+        """.stripMargin
+
+      checkTextHasError(code)
+
+      testQuickFix(code, expected, hint)
+    }
+  }
+
+  def testMustEqualBeNoneSimplification(): Unit = {
+    Seq("must_===", "must_==", "mustEqual").foreach { matcher =>
+      val code =
+        s"""
+           |${START}expr must_=== None$END
+        """.stripMargin
+      val expected =
+        """
+          |expr must beNone
+        """.stripMargin
+
+      checkTextHasError(code)
+
+      testQuickFix(code, expected, hint)
+    }
+  }
+
+  def testMustEqualBeEitherSimplification(): Unit = {
+    Seq("must_===", "must_==", "mustEqual").foreach { matcher =>
+      Seq("Left", "Right").foreach { either =>
+        val code =
+          s"""
+             |${START}expr must_=== $either("123")$END
+          """.stripMargin
+        val expected =
+          s"""
+            |expr must be$either("123")
+          """.stripMargin
+
+        checkTextHasError(code)
+
+        testQuickFix(code, expected, hint)
+      }
+    }
+  }
+}
