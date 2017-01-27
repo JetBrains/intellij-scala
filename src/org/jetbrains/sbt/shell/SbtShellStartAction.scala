db38ffb528983d6227bc19c420727f84928b4a55
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-01-27 03:25:45 +0100

    The SBT Shell action should only be shown for sbt projects in the Tools menu, and only on the sbt projects toolwindow

--- src/org/jetbrains/sbt/shell/SbtShellStartAction.scala
+++ src/org/jetbrains/sbt/shell/SbtShellStartAction.scala
@@ -1,18 +1,35 @@
 package org.jetbrains.sbt.shell
 
-import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
-import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.externalSystem.action.ExternalSystemAction
+import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys
 import org.jetbrains.plugins.scala.icons.Icons
+import org.jetbrains.sbt.project.SbtProjectSystem
+import org.jetbrains.sbt.settings.SbtSystemSettings
 
 /**
   * Created by jast on 2016-11-04.
   */
-class SbtShellStartAction extends DumbAwareAction("Run SBT Shell") {
+class SbtShellStartAction extends ExternalSystemAction {
 
-  override def update(e: AnActionEvent): Unit =
+  setText("Run SBT Shell")
+
+  override def update(e: AnActionEvent): Unit = {
+    super.update(e)
     e.getPresentation.setIcon(Icons.SCALA_CONSOLE)
+  }
 
   override def actionPerformed(event: AnActionEvent): Unit = {
     SbtShellRunner.run(event.getProject)
   }
+
+  override def isEnabled(e: AnActionEvent): Boolean =
+    true
+
+  // hide for non-sbt project toolwindows
+  override def isVisible(e: AnActionEvent): Boolean =
+    super.isVisible(e) &&
+    (SbtProjectSystem.Id == getSystemId(e) ||
+      ExternalSystemDataKeys.VIEW.getData(e.getDataContext) == null &&
+      !SbtSystemSettings.getInstance(e.getProject).getLinkedProjectsSettings.isEmpty)
 }
