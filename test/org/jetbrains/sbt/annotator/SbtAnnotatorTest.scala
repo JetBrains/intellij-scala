5520326f705a5519f4d489104b7b85780e6d34b0
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2017-02-03 15:41:26 +0300

    sbt, monocle libraries loaders

--- test/org/jetbrains/sbt/annotator/SbtAnnotatorTest.scala
+++ test/org/jetbrains/sbt/annotator/SbtAnnotatorTest.scala
@@ -14,6 +14,7 @@ import com.intellij.psi.PsiManager
 import com.intellij.testFramework.UsefulTestCase
 import com.intellij.util.Consumer
 import org.jetbrains.plugins.scala.annotator.{Error, _}
+import org.jetbrains.plugins.scala.base.libraryLoaders.{LibraryLoader, SourcesLoader}
 import org.jetbrains.plugins.scala.util.TestUtils
 import org.jetbrains.sbt.language.SbtFileImpl
 import org.jetbrains.sbt.project.module.SbtModuleType
@@ -24,15 +25,15 @@ import scala.collection.JavaConverters._
 
 
 /**
- * @author Nikolay Obedin
- * @since 7/23/15.
- */
+  * @author Nikolay Obedin
+  * @since 7/23/15.
+  */
 
 abstract class SbtAnnotatorTestBase extends AnnotatorTestBase with MockSbt {
 
   override protected def setUp(): Unit = {
     super.setUp()
-    addSbtAsModuleDependency(createBuildModule())
+    addSbtLibrary(createBuildModule())
     addTestFileToModuleSources()
     setUpProjectSettings()
     inWriteAction(StartupManager.getInstance(getProject).asInstanceOf[StartupManagerImpl].startCacheUpdate())
@@ -52,7 +53,7 @@ abstract class SbtAnnotatorTestBase extends AnnotatorTestBase with MockSbt {
   protected def runTest(sbtVersion: String, expectedMessages: Seq[Message]): Unit = {
     setSbtVersion(sbtVersion)
     val actualMessages = annotate().asJava
-    UsefulTestCase.assertSameElements(actualMessages, expectedMessages:_*)
+    UsefulTestCase.assertSameElements(actualMessages, expectedMessages: _*)
   }
 
   protected def setSbtVersion(sbtVersion: String): Unit = {
@@ -91,28 +92,33 @@ abstract class SbtAnnotatorTestBase extends AnnotatorTestBase with MockSbt {
         model.addContentEntry(testdataUrl).addSourceFolder(testdataUrl, false)
       }
     })
-    preventLeakageOfVfsPointers()
+
+    LibraryLoader.storePointers()
   }
 }
 
 
 class SbtAnnotatorTest_0_12_4 extends SbtAnnotatorTestBase {
-  override def sbtVersion: String = "0.12.4"
+  override implicit val sbtVersion: String = "0.12.4"
+
   def test(): Unit = runTest(sbtVersion, Expectations.sbt012)
 }
 
 class SbtAnnotatorTest_0_13_1 extends SbtAnnotatorTestBase {
-  override def sbtVersion: String = "0.13.1"
+  override implicit val sbtVersion: String = "0.13.1"
+
   def test(): Unit = runTest(sbtVersion, Expectations.sbt012_013(sbtVersion))
 }
 
 class SbtAnnotatorTest_0_13_7 extends SbtAnnotatorTestBase {
-  override def sbtVersion: String = "0.13.7"
+  override implicit val sbtVersion: String = "0.13.7"
+
   def test(): Unit = runTest(sbtVersion, Expectations.sbt0137)
 }
 
 class SbtAnnotatorTest_latest extends SbtAnnotatorTestBase {
-  override def sbtVersion: String = Sbt.LatestVersion
+  override implicit val sbtVersion: String = Sbt.LatestVersion
+
   def test(): Unit = runTest(sbtVersion, Expectations.sbt0137)
 }
 
@@ -132,7 +138,7 @@ object Expectations {
     Error("???", SbtBundle("sbt.annotation.expectedExpressionTypeSbt0136"))
   )
 
-  def sbt012_013(sbtVersion:String): Seq[Error] = sbtAll ++ Seq(
+  def sbt012_013(sbtVersion: String): Seq[Error] = sbtAll ++ Seq(
     Error("organization", SbtBundle("sbt.annotation.expressionMustConform", "SettingKey[String]")),
     Error(""""some string"""", SbtBundle("sbt.annotation.expressionMustConform", "String")),
     Error("null", SbtBundle("sbt.annotation.expectedExpressionType")),
@@ -141,7 +147,8 @@ object Expectations {
   )
 
   def sbt012: Seq[Error] = sbt012_013("0.12.4") ++ Seq(
-    Error("""lazy val foo = project.in(file("foo")).enablePlugins(sbt.plugins.JvmPlugin)""",
+    Error(
+      """lazy val foo = project.in(file("foo")).enablePlugins(sbt.plugins.JvmPlugin)""",
       SbtBundle("sbt.annotation.sbtFileMustContainOnlyExpressions"))
   )
 }
