d08d45c8e1c3dce9ef0c0f46adf2f024711b9a87
Kate Ustyuzhanina <katejim@mail.ru>
2017-02-03 15:58:31 +0300

    Support opportunity to have errors in scala or java files when pasting on project view or pasting java code to scala file. Supported errors: text without last semicolon or without "}" Assume that valid scala code couldn't contains ";\n" or ";" when oneliner. #SCL-11177 fixed

--- src/org/jetbrains/plugins/scala/conversion/copy/plainText/PlainTextCopyUtil.scala
+++ src/org/jetbrains/plugins/scala/conversion/copy/plainText/PlainTextCopyUtil.scala
@@ -1,21 +1,31 @@
 package org.jetbrains.plugins.scala.conversion.copy.plainText
 
+import com.intellij.codeInsight.daemon.JavaErrorMessages
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.project.Project
-import com.intellij.psi.{PsiErrorElement, PsiFile, PsiFileFactory, PsiJavaFile}
+import com.intellij.psi._
 import org.jetbrains.plugins.scala.debugger.evaluation.ScalaCodeFragment
 import org.jetbrains.plugins.scala.extensions.{ObjectExt, PsiElementExt}
+import org.jetbrains.plugins.scala.lang.parser.ErrMsg
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 
 import scala.collection.mutable
 
 /**
   * Created by Kate Ustyuzhanina on 12/27/16.
+  *
+  * This code assume that Scala files don't have semicolon at the end of line at all and
+  * Java files could miss semicolon at the end of line or last curly bracket.
+  * Java file without any semicolon could be treated as scala
   */
 object PlainTextCopyUtil {
-
+  /**
+    * Treat scala file as valid if it doesn't contain ";\n" && parsed correctly
+    */
   def isValidScalaFile(text: String, project: Project): Boolean = {
-    if (text == null || project == null) false
+    def withLastSemicolon(text: String): Boolean = (!text.contains("\n") && text.contains(";")) || text.contains(";\n")
+
+    if (text == null || project == null || withLastSemicolon(text)) false
     else createScalaFile(text, project).exists(isParsedCorrectly)
   }
 
@@ -24,7 +34,27 @@ object PlainTextCopyUtil {
     else createJavaFile(text, project).exists(isParsedCorrectly)
   }
 
-  def isParsedCorrectly(file: PsiFile): Boolean = !file.depthFirst().exists(_.isInstanceOf[PsiErrorElement])
+  def isParsedCorrectly(file: PsiFile): Boolean = {
+    lazy val javaPossibleErrors: mutable.HashSet[String] = mutable.HashSet[String](
+      JavaErrorMessages.message("expected.semicolon"), JavaErrorMessages.message("expected.rbrace"))
+
+    lazy val scalaPossibleErrors: mutable.HashSet[String] = mutable.HashSet[String](
+      ErrMsg("rbrace.expected"), ErrMsg("semi.expected"))
+
+    def handleFile(errors: mutable.HashSet[String]) = {
+      !file.depthFirst().exists {
+        case err: PsiErrorElement if errors.contains(err.getErrorDescription) => false
+        case _: PsiErrorElement => true
+        case _ => false
+      }
+    }
+
+    file match {
+      case _: ScalaFile =>
+        handleFile(scalaPossibleErrors)
+      case _ => handleFile(javaPossibleErrors)
+    }
+  }
 
   def createScalaFile(text: String, project: Project): Option[ScalaFile] = Some(new ScalaCodeFragment(project, text))
 
