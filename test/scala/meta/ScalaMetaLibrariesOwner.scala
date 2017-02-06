f89e4d898abee3e2cd13b538001a137c278ed41a
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2017-02-06 18:45:33 +0300

    Compilable abstraction removed

--- test/scala/meta/ScalaMetaLibrariesOwner.scala
+++ test/scala/meta/ScalaMetaLibrariesOwner.scala
@@ -1,17 +1,15 @@
 package scala.meta
 
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyLibraryLoaderAdapter, ThirdPartyLibraryLoader}
 import org.jetbrains.plugins.scala.debugger.ScalaVersion
-import org.jetbrains.plugins.scala.project.settings.ScalaCompilerConfiguration
 import org.jetbrains.plugins.scala.util.TestUtils.ScalaSdkVersion
 
-trait ScalametaUtils extends ScalaVersion {
+trait ScalaMetaLibrariesOwner extends ScalaVersion {
 
   protected override def scalaSdkVersion: ScalaSdkVersion = ScalaSdkVersion._2_11_8
 
-  import ScalametaUtils._
+  import ScalaMetaLibrariesOwner._
 
   protected def additionalLibraries(module: Module): Array[ThirdPartyLibraryLoader] = {
     implicit val m = module
@@ -30,20 +28,11 @@ trait ScalametaUtils extends ScalaVersion {
       MetaTreesLoader()
     )
   }
-
-  protected def enableParadisePlugin(module: Module)
-                                    (implicit project: Project): Unit = {
-    val profile = ScalaCompilerConfiguration.instanceIn(project).defaultProfile
-    val settings = profile.getSettings
-
-    settings.plugins :+= MetaParadiseLoader()(module).path
-    profile.setSettings(settings)
-  }
 }
 
-object ScalametaUtils {
+object ScalaMetaLibrariesOwner {
 
-  private abstract class MetaBaseLoader(implicit module: Module) extends IvyLibraryLoaderAdapter {
+  abstract class MetaBaseLoader(implicit module: Module) extends IvyLibraryLoaderAdapter {
     override protected val version: String = "1.3.0"
     override protected val vendor: String = "org.scalameta"
 
@@ -94,18 +83,4 @@ object ScalametaUtils {
   private case class MetaTreesLoader(implicit val module: Module) extends MetaBaseLoader {
     override protected val name: String = "trees"
   }
-
-  private case class MetaParadiseLoader(implicit val module: Module) extends MetaBaseLoader {
-    override protected val name: String = "paradise"
-    override protected val version: String = "3.0.0-M5"
-
-    override protected def folder(implicit version: ScalaSdkVersion): String =
-      s"${name}_${version.getMinor}"
-
-
-    override def path(implicit version: ScalaSdkVersion): String = super.path
-
-    override def init(implicit version: ScalaSdkVersion): Unit = {}
-  }
-
 }
