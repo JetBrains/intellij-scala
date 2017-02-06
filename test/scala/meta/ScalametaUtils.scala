7c6535f160136b224741fbd7e17f8373e0de340f
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2017-02-06 18:17:49 +0300

    meta library loaders

--- test/scala/meta/ScalametaUtils.scala
+++ test/scala/meta/ScalametaUtils.scala
@@ -1,50 +1,111 @@
 package scala.meta
 
-import java.io.File
-
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
-import com.intellij.testFramework.PsiTestUtil
+import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyLibraryLoaderAdapter, ThirdPartyLibraryLoader}
+import org.jetbrains.plugins.scala.debugger.ScalaVersion
 import org.jetbrains.plugins.scala.project.settings.ScalaCompilerConfiguration
-import org.jetbrains.plugins.scala.util.TestUtils
 import org.jetbrains.plugins.scala.util.TestUtils.ScalaSdkVersion
 
-trait ScalametaUtils {
+trait ScalametaUtils extends ScalaVersion {
 
-  protected val metaVersion = "1.3.0"
-  protected val paradiseVersion = "3.0.0-M5"
-  protected def scalaSdkVersion: ScalaSdkVersion = ScalaSdkVersion._2_11_8
+  protected override def scalaSdkVersion: ScalaSdkVersion = ScalaSdkVersion._2_11_8
 
-  def getMetaLibraries: Seq[(String, String, Seq[String])] = {
-    val scala = scalaSdkVersion.getMajor
-    def getLibEntry(component: String) = {
-      (s"scalameta-$component", s"org.scalameta/${component}_$scala/jars", Seq(s"${component}_$scala-$metaVersion.jar"))
-    }
-    Seq("common", "dialects", "inline",
-      "inputs", "parsers", "quasiquotes", "scalameta",
-      "tokenizers", "tokens", "transversers", "trees").map(getLibEntry)
+  import ScalametaUtils._
+
+  protected def additionalLibraries(module: Module): Array[ThirdPartyLibraryLoader] = {
+    implicit val m = module
+
+    Array(
+      MetaCommonLoader(),
+      MetaDialectsLoader(),
+      MetaInlineLoader(),
+      MetaInputsLoader(),
+      MetaParsersLoader(),
+      MetaQuasiquotesLoader(),
+      MetaScalametaLoader(),
+      MetaTokenizersLoader(),
+      MetaTokensLoader(),
+      MetaTransversersLoader(),
+      MetaTreesLoader()
+    )
   }
 
-  protected def addIvyCacheLibraryToModule(libraryName: String, libraryPath: String, jarNames: Seq[String])
-                                          (implicit module: Module): Unit = {
-    val libsPath = TestUtils.getIvyCachePath
-    val pathExtended = s"$libsPath/$libraryPath/"
-    VfsRootAccess.allowRootAccess(pathExtended)
-    PsiTestUtil.addLibrary(module, libraryName, pathExtended, jarNames: _*)
-  }
-
-  protected def addAllMetaLibraries(implicit module: Module): Unit = {
-    val addToModule = addIvyCacheLibraryToModule _
-    getMetaLibraries.foreach(addToModule.tupled)
-  }
-
-  protected def enableParadisePlugin(implicit project: Project): Unit = {
+  protected def enableParadisePlugin(module: Module)
+                                    (implicit project: Project): Unit = {
     val profile = ScalaCompilerConfiguration.instanceIn(project).defaultProfile
     val settings = profile.getSettings
-    val paradisePath = s"${TestUtils.getIvyCachePath}/org.scalameta/paradise_2.11.8/jars/paradise_2.11.8-$paradiseVersion.jar"
-    assert(new File(paradisePath).exists(), "Can't compile testdata - paradise plugin not found")
-    settings.plugins :+= paradisePath
+
+    settings.plugins :+= MetaParadiseLoader()(module).path
     profile.setSettings(settings)
   }
+}
+
+object ScalametaUtils {
+
+  private abstract class MetaBaseLoader(implicit module: Module) extends IvyLibraryLoaderAdapter {
+    override protected val version: String = "1.3.0"
+    override protected val vendor: String = "org.scalameta"
+
+    override protected def path(implicit version: ScalaSdkVersion): String =
+      super.path(ScalaSdkVersion._2_11_8)
+  }
+
+  private case class MetaCommonLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "common"
+  }
+
+  private case class MetaDialectsLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "dialects"
+  }
+
+  private case class MetaInlineLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "inline"
+  }
+
+  private case class MetaInputsLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "inputs"
+  }
+
+  private case class MetaParsersLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "parsers"
+  }
+
+  private case class MetaQuasiquotesLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "quasiquotes"
+  }
+
+  private case class MetaScalametaLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "scalameta"
+  }
+
+  private case class MetaTokenizersLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "tokenizers"
+  }
+
+  private case class MetaTokensLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "tokens"
+  }
+
+  private case class MetaTransversersLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "transversers"
+  }
+
+  private case class MetaTreesLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "trees"
+  }
+
+  private case class MetaParadiseLoader(implicit val module: Module) extends MetaBaseLoader {
+    override protected val name: String = "paradise"
+    override protected val version: String = "3.0.0-M5"
+
+    override protected def folder(implicit version: ScalaSdkVersion): String =
+      s"${name}_${version.getMinor}"
+
+
+    override def path(implicit version: ScalaSdkVersion): String = super.path
+
+    override def init(implicit version: ScalaSdkVersion): Unit = {}
+  }
+
 }
