3aa0aa64bcb2124ff3633c39e85cb6f2637dad34
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2017-02-09 17:56:26 +0300

    abstract test case for type parameter info

--- test/org/jetbrains/plugins/scala/lang/parameterInfo/typeParameterInfo/TypeParameterInfoTestBase.scala
+++ test/org/jetbrains/plugins/scala/lang/parameterInfo/typeParameterInfo/TypeParameterInfoTestBase.scala
@@ -3,84 +3,17 @@ package lang
 package parameterInfo
 package typeParameterInfo
 
-import java.awt.Color
-import java.io.File
-
-import com.intellij.codeInsight.hint.{HintUtil, ShowParameterInfoContext}
-import com.intellij.lang.parameterInfo.ParameterInfoUIContext
-import com.intellij.openapi.fileEditor.{FileEditorManager, OpenFileDescriptor}
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.openapi.vfs.{CharsetToolkit, LocalFileSystem}
-import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-
-import _root_.scala.util.Sorting
-import scala.collection.mutable.ArrayBuffer
+import com.intellij.lang.parameterInfo.ParameterInfoHandlerWithTabActionSupport
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.{ScTypeArgs, ScTypeElement}
 
 /**
- * @author Aleksander Podkhalyuzin
- * @since 26.04.2009
- */
-abstract class TypeParameterInfoTestBase extends ScalaLightPlatformCodeInsightTestCaseAdapter {
-  val caretMarker = "/*caret*/"
+  * @author Aleksander Podkhalyuzin
+  * @since 26.04.2009
+  */
+abstract class TypeParameterInfoTestBase extends ParameterInfoTestBase[ScTypeArgs, ScTypeElement] {
 
-  protected def folderPath = baseRootPath() + "parameterInfo/typeParameterInfo/"
+  override protected def folderPath: String = super.folderPath + "typeParameterInfo/"
 
-  protected def doTest() {
-    import _root_.junit.framework.Assert._
-    val filePath = folderPath + getTestName(false) + ".scala"
-    val file = LocalFileSystem.getInstance.findFileByPath(filePath.replace(File.separatorChar, '/'))
-    assert(file != null, "file " + filePath + " not found")
-    val fileText = StringUtil.convertLineSeparators(FileUtil.loadFile(new File(file.getCanonicalPath), CharsetToolkit.UTF8))
-    configureFromFileTextAdapter(getTestName(false) + ".scala", fileText)
-    val scalaFile = getFileAdapter.asInstanceOf[ScalaFile]
-    val offset = fileText.indexOf(caretMarker)
-    assert(offset != -1, "Not specified caret marker in test case. Use /*caret*/ in scala file for this.")
-    val fileEditorManager = FileEditorManager.getInstance(getProjectAdapter)
-    val editor = fileEditorManager.openTextEditor(new OpenFileDescriptor(getProjectAdapter, file, offset), false)
-    val context = new ShowParameterInfoContext(editor, getProjectAdapter, scalaFile, offset, -1)
-    val handler = new ScalaTypeParameterInfoHandler
-    val leafElement = scalaFile.findElementAt(offset)
-    val element = PsiTreeUtil.getParentOfType(leafElement, handler.getArgumentListClass)
-    handler.findElementForParameterInfo(context)
-    val items = new ArrayBuffer[String]
-
-    for (item <- context.getItemsToShow) {
-      val uiContext = new ParameterInfoUIContext {
-        def getDefaultParameterColor: Color = HintUtil.INFORMATION_COLOR
-        def setupUIComponentPresentation(text: String, highlightStartOffset: Int, highlightEndOffset: Int,
-                                        isDisabled: Boolean, strikeout: Boolean, isDisabledBeforeHighlight: Boolean,
-                                        background: Color): String = {
-          items.append(text)
-          text
-        }
-        def isUIComponentEnabled: Boolean = false
-        def getCurrentParameterIndex: Int = 0
-        def getParameterOwner: PsiElement = element
-        def setUIComponentEnabled(enabled: Boolean) {}
-      }
-      handler.updateUI(item, uiContext)
-    }
-
-    val itemsArray = items.toArray
-    Sorting.quickSort[String](itemsArray)
-
-    val res = new StringBuilder("")
-
-    for (item <- itemsArray) res.append(item).append("\n")
-    if (res.nonEmpty) res.replace(res.length - 1, res.length, "")
-    val lastPsi = scalaFile.findElementAt(scalaFile.getText.length - 1)
-    val text = lastPsi.getText
-    val output = lastPsi.getNode.getElementType match {
-      case ScalaTokenTypes.tLINE_COMMENT => text.substring(2).trim
-      case ScalaTokenTypes.tBLOCK_COMMENT | ScalaTokenTypes.tDOC_COMMENT =>
-        text.substring(2, text.length - 2).trim
-      case _ => assertTrue("Test result must be in last comment statement.", false)
-    }
-    assertEquals(output, res.toString())
-  }
+  override protected def createHandler: ParameterInfoHandlerWithTabActionSupport[ScTypeArgs, Any, ScTypeElement] =
+    new ScalaTypeParameterInfoHandler
 }
