5f1cf53b5d42518da506f6d7adc4ecf522df76c7
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-03-08 12:19:21 +0100

    VersionedArtifactHandlerBase.isVersionMoreSpecific -> v1 ~= v2

--- src/org/jetbrains/plugins/scala/project/Version.scala
+++ src/org/jetbrains/plugins/scala/project/Version.scala
@@ -7,10 +7,8 @@ import org.jetbrains.plugins.scala.project.Version._
  * @author Pavel Fatin
  */
 case class Version(presentation: String) extends AnyVal with Comparable[Version] {
-
-  def groups: Seq[Seq[Int]] = {
+  private def groups: Seq[Seq[Int]] =
     presentation.split('-').map(findNumbers)
-  }
 
   def compareTo(other: Version): Int = { //0.1.1 > 0.1-20170107
     groups.zip(other.groups).collectFirst {
@@ -25,6 +23,17 @@ case class Version(presentation: String) extends AnyVal with Comparable[Version]
   def <(other: Version): Boolean = compareTo(other) < 0
 
   def <=(other: Version): Boolean = compareTo(other) <= 0
+
+  /*
+   Returns whether all the comparable parts are equal, e.g:
+     2.10   ~=  2.10
+     2.10   ~=  2.10.1
+     2.10.1 ~=  2.10
+
+     2.10  !~=  2.11
+  */
+  def ~=(version: Version): Boolean =
+    groups.zip(version.groups).forall(==)
 
   def toLanguageLevel: Option[ScalaLanguageLevel] = ScalaLanguageLevel.from(this)
 }
