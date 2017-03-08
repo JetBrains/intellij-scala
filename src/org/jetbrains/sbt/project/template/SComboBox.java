56f05976d00d6b320cbd8e6991f65276f8232bb2
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-03-08 15:46:25 +0100

    SComboBox.setTextRenderer

--- src/org/jetbrains/sbt/project/template/SComboBox.java
+++ src/org/jetbrains/sbt/project/template/SComboBox.java
@@ -1,6 +1,9 @@
 package org.jetbrains.sbt.project.template;
 
+import scala.Function1;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Pavel Fatin
@@ -17,5 +20,20 @@ public class SComboBox extends JComboBox {
   public <T extends Object> void setItems(T[] items) {
     //noinspection unchecked
     super.setModel(new DefaultComboBoxModel(items));
+  }
+
+  public void setTextRenderer(final Function1<String, String> renderer) {
+    //noinspection unchecked
+    setRenderer(new DefaultListCellRenderer() {
+      @Override
+      public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        if (value != null && !(value instanceof String)) {
+          throw new IllegalArgumentException("Not a String value: " + value);
+        }
+        Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        setText(renderer.apply((String) value));
+        return component;
+      }
+    });
   }
 }
