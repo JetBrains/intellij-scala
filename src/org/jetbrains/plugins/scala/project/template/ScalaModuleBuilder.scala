aacbb5d7f9c69468bb1aa311e46eb08fef0d12b8
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-03-13 19:40:07 +0100

    ScalaModuleBuilder: SDK -> JDK
    (cherry picked from commit f1c6928)

--- src/org/jetbrains/plugins/scala/project/template/ScalaModuleBuilder.scala
+++ src/org/jetbrains/plugins/scala/project/template/ScalaModuleBuilder.scala
@@ -2,7 +2,7 @@ package org.jetbrains.plugins.scala
 package project.template
 
 import java.util
-import javax.swing.JComponent
+import javax.swing.{JComponent, JLabel}
 
 import com.intellij.facet.impl.ui.libraries.{LibraryCompositionSettings, LibraryOptionsPanel}
 import com.intellij.framework.library.FrameworkLibraryVersionFilter
@@ -13,6 +13,7 @@ import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.openapi.roots.ui.configuration.projectRoot.{LibrariesContainer, LibrariesContainerFactory}
 import com.intellij.openapi.util.Disposer
+import org.jetbrains.plugins.scala.extensions._
 
 /**
  * @author Pavel Fatin
@@ -41,6 +42,15 @@ class ScalaModuleBuilder extends JavaModuleBuilder {
       FrameworkLibraryVersionFilter.ALL, librariesContainer, false)
 
     settingsStep.addSettingsField("Scala S\u001BDK:", libraryPanel.getSimplePanel)
+
+    // TODO Remove the label patching when JavaModuleBuilder will use the proper label natively
+    Option(libraryPanel.getSimplePanel.getParent).foreach { parent =>
+      parent.getComponents.toSeq.foreachDefined {
+        case label: JLabel if label.getText == "Project SDK:" =>
+          label.setText("JDK")
+          label.setDisplayedMnemonic('J')
+      }
+    }
 
     override def updateDataModel() {
       libraryCompositionSettings = libraryPanel.apply()
