f8b3f022cc3a6a21e809e205735ce411b8449828
Kate Ustyuzhanina <katejim@mail.ru>
2017-03-14 20:30:39 +0300

    Fix conversion super.equals(x). #SCL-11313 Fixed.
    (cherry picked from commit a8cf37f)

---
+++ testdata/conversion/examples/SCL11313.java
@@ -0,0 +1,12 @@
+class Test {
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
+
+/*
+class Test {
+  override def equals(o: Any): Boolean = super.equlas(o)
+}
+*/
