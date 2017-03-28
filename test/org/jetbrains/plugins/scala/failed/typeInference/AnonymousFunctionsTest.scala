0994ae51fe82ceee9408f9abe86d467dbc0dfec9
Anton.Yalyshev <anton.yalyshev@jetbrains.com>
2017-03-28 18:41:49 +0300

    added test for SCL-11637

--- test/org/jetbrains/plugins/scala/failed/typeInference/AnonymousFunctionsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/AnonymousFunctionsTest.scala
@@ -37,4 +37,49 @@ class AnonymousFunctionsTest extends TypeInferenceTestBase {
     """.stripMargin.trim
   }
 
+  def testSCL11637(): Unit = doTest {
+    """
+      |trait IA {
+      |  type T <: IAT
+      |  trait IAT {
+      |    //...
+      |  }
+      |}
+      |
+      |object A {
+      |  def Make(): IA = new IA {
+      |    case class T() extends IAT {
+      |      // ...
+      |    }
+      |  }
+      |}
+      |
+      |trait IB[V] {
+      |  type T <: IBT[V]
+      |  trait IBT[K] {
+      |    def filter(f: K => Boolean): T
+      |  }
+      |  val empty: T
+      |}
+      |
+      |object B {
+      |  def Make(D: IA): IB[D.T] = new IB[D.T] {
+      |    case class T() extends IBT[D.T] {
+      |      def filter(f: D.T => Boolean): T = { this }
+      |    }
+      |    val empty: T = T()
+      |  }
+      |}
+      |
+      |package object global {
+      |  val MA = A.Make()
+      |  val MB = B.Make(MA)
+      |
+      |  def test(k: MA.T): Boolean = true
+      |  MB.empty.filter(k => test(/*start*/k/*end*/) )   // here the type of 'k' is inferenced to IA#T, and it does not interoperable with the type MA.T in the editor.
+      |}
+      |//global.MA.T
+    """.stripMargin.trim
+  }
+
 }
