ace26e1de065adbccbb516e7bbe9157c7e201fb1
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-03-28 12:14:11 +0200

    Project View error highlighting, #SCL-2340 fixed

---
+++ src/org/jetbrains/plugins/scala/annotator/ScalaProblemFileHighlightFilter.scala
@@ -0,0 +1,37 @@
+package org.jetbrains.plugins.scala.annotator
+
+import com.intellij.openapi.compiler.CompilerManager
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Condition
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes
+import org.jetbrains.plugins.scala.ScalaFileType
+import org.jetbrains.plugins.scala.annotator.ScalaProblemFileHighlightFilter._
+
+/**
+  * @author Pavel Fatin
+  */
+class ScalaProblemFileHighlightFilter(project: Project) extends Condition[VirtualFile] {
+  /**
+    * @see [[com.intellij.codeInsight.problems.DefaultProblemFileHighlightFilter]]
+    */
+  override def value(file: VirtualFile): Boolean =
+    isScalaSourceFile(project, file) &&
+      !CompilerManager.getInstance(project).isExcludedFromCompilation(file)
+}
+
+private object ScalaProblemFileHighlightFilter {
+  /**
+    * @see [[com.intellij.openapi.roots.FileIndexUtil]]
+    */
+  private def isScalaSourceFile(project: Project, file: VirtualFile): Boolean = {
+    if (!file.isDirectory && file.getFileType == ScalaFileType.INSTANCE && !FileTypeManager.getInstance.isFileIgnored(file)) {
+      val fileIndex = ProjectRootManager.getInstance(project).getFileIndex
+      fileIndex.isUnderSourceRootOfType(file, JavaModuleSourceRootTypes.SOURCES) || fileIndex.isInLibrarySource(file)
+    } else {
+      false
+    }
+  }
+}
