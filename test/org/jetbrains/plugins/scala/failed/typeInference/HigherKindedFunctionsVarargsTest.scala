e7a0ed5311092cac230e5278852d739387c61108
Anton.Yalyshev <anton.yalyshev@jetbrains.com>
2017-03-28 15:11:34 +0300

    added test for SCL-11489

--- test/org/jetbrains/plugins/scala/failed/typeInference/HigherKindedFunctionsVarargsTest.scala
+++ test/org/jetbrains/plugins/scala/failed/typeInference/HigherKindedFunctionsVarargsTest.scala
@@ -21,4 +21,26 @@ class HigherKindedFunctionsVarargsTest extends TypeInferenceTestBase {
       """.stripMargin)
   }
 
+  def testSCL11489(): Unit = {
+    doTest(
+      s"""
+         |  trait AAA {
+         |    def whatever(i: Int): Unit = ()
+         |  }
+         |
+         |  trait BBB[+A] extends AAA
+         |
+         |  object DDD {
+         |    def aa(y: AAA): Unit = println("hi there")
+         |  }
+         |
+         |  class CCC[X[_] <: BBB[_]] {
+         |    def a[B](x: X[B]): Unit = {
+         |      DDD.aa(${START}x$END)
+         |      ()
+         |    }
+         |  }
+         |//AAA
+      """.stripMargin)
+  }
 }
