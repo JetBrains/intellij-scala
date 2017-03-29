214cac5cd26884027e8a289aa6176e4f68f3ae3e
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2017-03-29 20:05:59 +0300

    Compilation after bad auto merge fixed

--- src/org/jetbrains/plugins/scala/worksheet/ui/WorksheetIncrementalEditorPrinter.scala
+++ src/org/jetbrains/plugins/scala/worksheet/ui/WorksheetIncrementalEditorPrinter.scala
@@ -294,10 +294,6 @@ object WorksheetIncrementalEditorPrinter {
     DaemonCodeAnalyzer.getInstance(file.getProject).restart(file)
   }
   
-  def rehighlight(file: PsiFile) {
-    DaemonCodeAnalyzer.getInstance(file.getProject).restart(file)
-  }
-  
   case class MessageStart(msg: String)
   case class MessageInfo(text: String, verOffset: Int, horOffset: Int, severity: WorksheetCompiler.CompilationMessageSeverity)
   
