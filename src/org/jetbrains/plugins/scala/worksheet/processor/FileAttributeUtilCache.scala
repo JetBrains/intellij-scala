b830cb94f6ad82517604b20655bef670e3843862
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2017-03-30 20:53:06 +0300

    Missed isWorksheetReplModeLight method

--- src/org/jetbrains/plugins/scala/worksheet/processor/FileAttributeUtilCache.scala
+++ src/org/jetbrains/plugins/scala/worksheet/processor/FileAttributeUtilCache.scala
@@ -4,6 +4,7 @@ package worksheet.processor
 import com.intellij.openapi.vfs.newvfs.FileAttribute
 import com.intellij.openapi.vfs.{VirtualFile, VirtualFileWithId}
 import com.intellij.psi.PsiFile
+import com.intellij.testFramework.LightVirtualFile
 
 import scala.collection.mutable
 
@@ -13,11 +14,24 @@ import scala.collection.mutable
  */
 object FileAttributeUtilCache {
   private val lightKeys = mutable.WeakHashMap[VirtualFile, mutable.HashMap[FileAttribute, String]]()
+  
+  private def getAttribute(file: VirtualFile, attribute: FileAttribute) = 
+    Option(attribute readAttributeBytes file) map (new String(_))
 
   def readAttribute(attribute: FileAttribute, file: PsiFile): Option[String] = {
     file.getVirtualFile match {
-      case normalFile: VirtualFileWithId => Option(attribute readAttributeBytes normalFile) map (new String(_))
+      case normalFile: VirtualFileWithId => getAttribute(normalFile, attribute)
       case other => lightKeys get other flatMap (map => map get attribute)
+    }
+  }
+  
+  //We need this method as sometimes (e.g. during completion) normal virtual file is "hidden" beneath light file
+  def readAttributeLight(attribute: FileAttribute, file: PsiFile): Option[String] = {
+    readAttribute(attribute, file).orElse {
+      file.getViewProvider.getVirtualFile match {
+        case light: LightVirtualFile => Option(light.getOriginalFile) flatMap (normalFile => getAttribute(normalFile, attribute))
+        case _ => None
+      }
     }
   }
 
