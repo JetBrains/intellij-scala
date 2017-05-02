bf32f46a0ef192abb66a823d807f368168f29f5c
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-05-02 14:58:49 +0200

    updated ProjectDataManager. #SCL-11901 fixed

--- src/org/jetbrains/sbt/project/SbtProjectImportBuilder.scala
+++ src/org/jetbrains/sbt/project/SbtProjectImportBuilder.scala
@@ -6,7 +6,7 @@ import java.io.File
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ProjectData
-import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataManager
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
 import com.intellij.openapi.externalSystem.service.project.wizard.AbstractExternalProjectImportBuilder
 import com.intellij.openapi.project.Project
 
