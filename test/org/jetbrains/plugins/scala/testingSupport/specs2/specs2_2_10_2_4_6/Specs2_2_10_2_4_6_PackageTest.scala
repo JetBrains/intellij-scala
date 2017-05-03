d05297ca869724caf60e2f01ab8c751a9c784763
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-05-03 13:25:05 +0200

    Specs2_2_10_2_4_6_PackageTest.compilerVmOptions = -Xmx1280, #SCL-11899 fixed

--- test/org/jetbrains/plugins/scala/testingSupport/specs2/specs2_2_10_2_4_6/Specs2_2_10_2_4_6_PackageTest.scala
+++ test/org/jetbrains/plugins/scala/testingSupport/specs2/specs2_2_10_2_4_6/Specs2_2_10_2_4_6_PackageTest.scala
@@ -7,5 +7,5 @@ import org.jetbrains.plugins.scala.testingSupport.specs2.Specs2PackageTest
  * @since 06.09.2015.
  */
 class Specs2_2_10_2_4_6_PackageTest extends Specs2PackageTest with Specs2_2_10_2_4_6_Base {
-
+  override protected def compilerVmOptions = Some("-Xmx1280")
 }
