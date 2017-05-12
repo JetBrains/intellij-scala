9cbb471adc17a608e7b6d9daef760758c71445c3
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-05-12 17:45:43 +0200

    substitute $ characters in sbt imports "option" in SbtModule. Prevents "undefined path variables" error popping up in the UI. #SCL-10961 fixed

--- src/org/jetbrains/sbt/project/module/SbtModule.scala
+++ src/org/jetbrains/sbt/project/module/SbtModule.scala
@@ -15,10 +15,15 @@ object SbtModule {
   private val ResolversKey = "sbt.resolvers"
 
   def getImportsFrom(module: Module): Seq[String] =
-    Option(module.getOptionValue(ImportsKey)).filter(_.nonEmpty).fold(Sbt.DefaultImplicitImports)(_.split(Delimiter))
+    Option(module.getOptionValue(ImportsKey))
+      .filter(_.nonEmpty)
+      .map(v => unsubstituteOptionString(v).split(Delimiter).toSeq)
+      .getOrElse(Sbt.DefaultImplicitImports)
 
-  def setImportsTo(module: Module, imports: Seq[String]) =
-    Option(module.setOption(ImportsKey, imports.mkString(Delimiter)))
+  def setImportsTo(module: Module, imports: Seq[String]): Unit = {
+    val v = substituteOptionString(imports.mkString(Delimiter))
+    module.setOption(ImportsKey, v)
+  }
 
   def getResolversFrom(module: Module): Set[SbtResolver] =
     Option(module.getOptionValue(ResolversKey)).map { str =>
@@ -29,4 +34,18 @@ object SbtModule {
 
   def setResolversTo(module: Module, resolvers: Set[SbtResolver]) =
     Option(module.setOption(ResolversKey, resolvers.map(_.toString).mkString(Delimiter)))
+
+  // substitution of dollars is necessary because IDEA will interpret a string in the form of $something$ as a path variable
+  // and warn the user of "undefined path variables" (SCL-10691)
+  val substitutePrefix = "SUB:"
+  val substituteDollar = "DOLLAR"
+  def substituteOptionString(raw: String): String =
+    raw
+      .replace(substitutePrefix, substitutePrefix+substitutePrefix)
+      .replace("$",substitutePrefix+substituteDollar)
+
+  def unsubstituteOptionString(substituted: String): String =
+    substituted
+      .replace(substitutePrefix+substitutePrefix, substitutePrefix)
+      .replace(substitutePrefix+substituteDollar,"$")
 }
