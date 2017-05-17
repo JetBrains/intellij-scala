3ed9bde69a80985b9ea87b815332872ebbacb4aa
Mikhail Mutcianko <mutcianko.m@gmail.com>
2017-05-17 14:34:38 +0300

    metahost: recompile testdata for 2.12 annotations

--- test/scala/meta/annotations/MetaAnnotationJarTest212.scala
+++ test/scala/meta/annotations/MetaAnnotationJarTest212.scala
@@ -8,5 +8,4 @@ import org.jetbrains.plugins.scala.debugger.{ScalaSdkOwner, ScalaVersion, Scala_
   */
 class MetaAnnotationJarTest212 extends MetaAnnotationJarTest {
   override implicit val version = Scala_2_12
-  override protected val jarPath = "/addFoo_2.12_3.0.0-M7.jar"
 }
