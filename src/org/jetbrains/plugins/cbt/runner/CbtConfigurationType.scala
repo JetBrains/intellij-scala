bb08d3430f7473c3a0cd84f7cd04bece796d808f
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-06-02 10:28:37 +0300

    Add CBT to run cofigurations menu

---
+++ src/org/jetbrains/plugins/cbt/runner/CbtConfigurationType.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.cbt.runner
+
+import javax.swing.Icon
+
+import com.intellij.execution.configurations.{ConfigurationFactory, ConfigurationType}
+import org.jetbrains.sbt.Sbt
+import org.jetbrains.sbt.runner.SbtRunConfigurationFactory
+
+class CbtConfigurationType extends ConfigurationType {
+  val confFactory = new SbtRunConfigurationFactory(this)
+
+  def getIcon: Icon = Sbt.Icon
+
+  def getDisplayName: String = "CBT Task"
+
+  def getConfigurationTypeDescription: String = "Run CBT Task"
+
+  def getConfigurationFactories: Array[ConfigurationFactory] = Array[ConfigurationFactory](confFactory)
+
+  def getId: String = "CbtRunConfiguration"
+}
