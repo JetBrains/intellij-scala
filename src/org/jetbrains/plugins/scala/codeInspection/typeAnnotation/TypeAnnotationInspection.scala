feb98b92accfd6a85d71841f3eacef6c92119d63
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2017-06-13 12:16:33 +0300

    inspection fix #SCL-12111 fixed

--- src/org/jetbrains/plugins/scala/codeInspection/typeAnnotation/TypeAnnotationInspection.scala
+++ src/org/jetbrains/plugins/scala/codeInspection/typeAnnotation/TypeAnnotationInspection.scala
@@ -33,18 +33,19 @@ class TypeAnnotationInspection extends AbstractInspection {
   // TODO Treat "simple" expressions just like any other expressions
   override def actionFor(implicit holder: ProblemsHolder): PartialFunction[PsiElement, Unit] = {
     case value: ScPatternDefinition if value.isSimple && !value.hasExplicitType =>
-      inspect(value, value.bindings.head, "value", value.expr)
+      inspect(value, value.bindings.head, value.expr)
     case variable: ScVariableDefinition if variable.isSimple && !variable.hasExplicitType =>
-      inspect(variable, variable.bindings.head, "variable", variable.expr)
+      inspect(variable, variable.bindings.head, variable.expr)
     case method: ScFunctionDefinition if functionIsValid(method) =>
-      inspect(method, method.nameId, "method", method.body)
+      inspect(method, method.nameId, method.body)
   }
-
 }
 
 object TypeAnnotationInspection {
 
   import TypeAnnotationUtil._
+
+  private[typeAnnotation] val Description = "Explicit type annotation required (according to Code Style settings)"
 
   private def functionIsValid(function: ScFunctionDefinition): Boolean =
     function.hasAssign && !function.hasExplicitType &&
@@ -54,55 +55,30 @@ object TypeAnnotationInspection {
 
   private def inspect(member: ScMember,
                       anchor: PsiElement,
-                      kind: String,
                       maybeExpression: Option[ScExpression])
                      (implicit holder: ProblemsHolder) {
-    val (overridingPolicyIsRegular, simplePolicyIsRegular, isRequired) = policies(member)
-    if (isRequired &&
-      (simplePolicyIsRegular || !maybeExpression.exists(isSimple)) &&
-      (overridingPolicyIsRegular || !isOverriding(member))) {
-      val modifier = member match {
-        case _ if member.isLocal => "Local"
-        case _ if member.isPrivate => "Private"
-        case _ if member.isProtected => "Protected"
-        case _ => "Public"
-      }
-
-      holder.registerProblem(anchor, s"$modifier $kind requires an explicit type annotation (according to Code Style settings)",
+    if (isRequired(member)) {
+      holder.registerProblem(anchor, Description,
         new AddTypeAnnotationQuickFix(anchor),
         new LearnWhyQuickFix,
         new ModifyCodeStyleQuickFix)
     }
   }
 
-  private[this] def policies(member: ScMember)
-                            (implicit holder: ProblemsHolder): (Boolean, Boolean, Boolean) = {
-    val settings = ScalaCodeStyleSettings.getInstance(holder.getProject)
-
-    val (overridingPolicy, simplePolicy, requirement) = member match {
-      case _: ScPatternDefinition | _: ScVariableDefinition => (
-        settings.OVERRIDING_PROPERTY_TYPE_ANNOTATION,
-        settings.SIMPLE_PROPERTY_TYPE_ANNOTATION,
-        requirementForProperty(member, settings)
-      )
-      case _ => (
-        settings.OVERRIDING_METHOD_TYPE_ANNOTATION,
-        settings.SIMPLE_METHOD_TYPE_ANNOTATION,
-        requirementForMethod(member, settings)
-      )
+  private[this] def isRequired(member: ScMember)
+                              (implicit holder: ProblemsHolder): Boolean = {
+    def requirement: (ScMember, ScalaCodeStyleSettings) => Int = member match {
+      case _: ScPatternDefinition | _: ScVariableDefinition => requirementForProperty
+      case _ => requirementForMethod
     }
 
-    val regularPolicy = TypeAnnotationPolicy.Regular.ordinal
-    (
-      overridingPolicy == regularPolicy,
-      simplePolicy == regularPolicy,
-      requirement == TypeAnnotationRequirement.Required.ordinal
-    )
+    val settings = ScalaCodeStyleSettings.getInstance(holder.getProject)
+    requirement(member, settings) == TypeAnnotationRequirement.Required.ordinal
   }
 }
 
 class AddTypeAnnotationQuickFix(element: PsiElement)
-  extends AbstractFixOnPsiElement("Add type annotation", element) with BatchQuickFix[CommonProblemDescriptor] {
+  extends AbstractFixOnPsiElement(AddTypeAnnotationQuickFix.Name, element) with BatchQuickFix[CommonProblemDescriptor] {
 
   import AddTypeAnnotationQuickFix._
 
@@ -132,6 +108,8 @@ class AddTypeAnnotationQuickFix(element: PsiElement)
 }
 
 object AddTypeAnnotationQuickFix {
+
+  val Name = "Add type annotation"
 
   private def updateProcessIndicator(text: String, quickFixesCount: Int): Unit = {
     val indicator = ProgressManager.getInstance().getProgressIndicator
@@ -175,6 +153,7 @@ object AddTypeAnnotationQuickFix {
         .map((_, anchor))
     }
   }
+
 }
 
 class LearnWhyQuickFix extends LocalQuickFixBase("Learn Why...") {
