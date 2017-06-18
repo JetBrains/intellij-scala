ff95e4baf19fc9c6073a111a4c5cdb94af63e2d8
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-06-18 22:04:28 +0300

    Change cbt proccess call

--- src/org/jetbrains/plugins/cbt/CBT.scala
+++ src/org/jetbrains/plugins/cbt/CBT.scala
@@ -2,12 +2,35 @@ package org.jetbrains.plugins.cbt
 
 import java.io.File
 
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.{ProcessAdapter, ProcessEvent, ProcessHandlerFactory, ProcessOutputTypes}
+import com.intellij.openapi.util.Key
+
 import scala.xml._
-import sys.process._
 
 object CBT {
-  def runAction(action: Seq[String], root: File): String =
-    Process(Seq("cbt") ++ action, root) !!
+  def runAction(action: Seq[String], root: File): String = {
+    val factory = ProcessHandlerFactory.getInstance
+    val commandLine = new GeneralCommandLine("cbt" +: action: _*)
+      .withWorkDirectory(root)
+
+    val buffer = new StringBuffer()
+    val listener = new ProcessAdapter {
+      override def onTextAvailable(event: ProcessEvent, outputType: Key[_]) {
+        val text = event.getText
+        outputType match {
+          case ProcessOutputTypes.STDOUT => buffer.append(text)
+          case _ =>
+        }
+      }
+    }
+
+    val handler = factory.createColoredProcessHandler(commandLine)
+    handler.addProcessListener(listener)
+    handler.startNotify()
+    handler.waitFor()
+    buffer.toString
+  }
 
   def projectBuidInfo(root: File): Node =
     XML.loadString(runAction(Seq("buildInfoXml"), root))
