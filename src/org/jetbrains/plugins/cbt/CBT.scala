166096e2b8c40f672753e34f8324b768ea8fd51d
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-06-26 20:20:12 +0300

    Change CBT call process

--- src/org/jetbrains/plugins/cbt/CBT.scala
+++ src/org/jetbrains/plugins/cbt/CBT.scala
@@ -2,14 +2,16 @@ package org.jetbrains.plugins.cbt
 
 import java.io.File
 
-import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.process.{ProcessAdapter, ProcessEvent, ProcessHandlerFactory, ProcessOutputTypes}
-import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListener}
+import com.intellij.execution.process.AnsiEscapeDecoder.ColoredTextAcceptor
+import com.intellij.execution.process.{AnsiEscapeDecoder, ProcessOutputTypes}
+import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationEvent, ExternalSystemTaskNotificationListener}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.plugins.cbt.project.settings.CbtSystemSettings
 import org.jetbrains.sbt.RichVirtualFile
+
+import scala.sys.process.{Process, ProcessLogger}
 
 
 object CBT {
@@ -17,41 +19,19 @@ object CBT {
     runAction(action, root, None)
 
   def runAction(action: Seq[String], root: File,
-                taskListener: Option[(ExternalSystemTaskId, ExternalSystemTaskNotificationListener)]): String = {
-    val factory = ProcessHandlerFactory.getInstance
-    val commandLine = new GeneralCommandLine("cbt" +: action: _*)
-      .withWorkDirectory(root)
-
-    val buffer = new StringBuffer()
-    val errorBuffer = new StringBuffer()
-    val listener = new ProcessAdapter {
-      override def onTextAvailable(event: ProcessEvent, outputType: Key[_]) {
-        val text = event.getText
-        outputType match {
-          case ProcessOutputTypes.STDOUT => buffer.append(text)
-          case ProcessOutputTypes.STDERR =>
-          //            taskListener.foreach { case (id, l) =>
-          //              if (text.contains('\n')) {
-          //                val (prefix, suffix) = text.span {
-          //                  _ != '\n'
-          //                }
-          //                errorBuffer.append(prefix)
-          //                l.onStatusChange(new ExternalSystemTaskNotificationEvent(id, errorBuffer.toString))
-          //                errorBuffer.setLength(0)
-          //                errorBuffer.append(suffix)
-          //              } else {
-          //                errorBuffer.append(text)
-          //              }
-          //            }
+                taskListener: Option[(ExternalSystemTaskId, ExternalSystemTaskNotificationListener)]): String =    {
+    val colorEncoder = new AnsiEscapeDecoder
+    val logger = ProcessLogger(
+      { _ => }, { text =>
+        taskListener.foreach {
+          case (id, l) =>
+            colorEncoder.escapeText(text, ProcessOutputTypes.STDERR, new ColoredTextAcceptor {
+              override def coloredTextAvailable(text: String, attributes: Key[_]): Unit =
+                l.onStatusChange(new ExternalSystemTaskNotificationEvent(id, text))
+            })
         }
-      }
-    }
-
-    val handler = factory.createColoredProcessHandler(commandLine)
-    handler.addProcessListener(listener)
-    handler.startNotify()
-    handler.waitFor()
-    buffer.toString
+      })
+    Process(Seq("cbt") ++ action, root) !! logger
   }
 
   def isCbtModuleDir(entry: VirtualFile): Boolean =
