db083af1fcb47d3d6a466407f04a2bbcc6e154b1
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-07-12 15:17:36 +0300

    WIP

--- src/org/jetbrains/plugins/cbt/project/CbtNotificationListener.scala
+++ src/org/jetbrains/plugins/cbt/project/CbtNotificationListener.scala
@@ -2,7 +2,9 @@ package org.jetbrains.plugins.cbt.project
 
 import com.intellij.notification.{Notification, NotificationType, Notifications}
 import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListenerAdapter}
+import com.intellij.openapi.externalSystem.service.notification.{ExternalSystemNotificationManager, NotificationCategory, NotificationData, NotificationSource}
 import org.jetbrains.plugins.cbt.project.structure.CbtProjectImporingException
+import org.jetbrains.sbt.project.SbtProjectSystem
 
 class CbtNotificationListener extends ExternalSystemTaskNotificationListenerAdapter {
   override def onFailure(id: ExternalSystemTaskId, e: Exception): Unit = {
@@ -10,7 +12,14 @@ class CbtNotificationListener extends ExternalSystemTaskNotificationListenerAdap
       e match {
         case importEx: CbtProjectImporingException =>
           val title = "CBT project importing failure"
-          Notifications.Bus.notify(new Notification(title, title, e.getMessage, NotificationType.ERROR))
+          val text = importEx.getMessage
+          Notifications.Bus.notify(new Notification(title, title, text, NotificationType.ERROR))
+          Option(id.findProject)
+            .foreach { project =>
+              val notification = new NotificationData("CBT project import", text,
+                NotificationCategory.ERROR, NotificationSource.PROJECT_SYNC)
+              ExternalSystemNotificationManager.getInstance(project).showNotification(SbtProjectSystem.Id, notification)
+            }
       }
     }
   }
