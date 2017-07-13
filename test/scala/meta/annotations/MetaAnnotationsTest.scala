202484e1fc81130ad4841e58d5db4ebd13b5d580
Mikhail Mutcianko <mutcianko.m@gmail.com>
2017-07-13 17:36:33 +0300

    metahost: add tests
    wip #SCL-12110

--- test/scala/meta/annotations/MetaAnnotationsTest.scala
+++ test/scala/meta/annotations/MetaAnnotationsTest.scala
@@ -1,17 +1,19 @@
 package scala.meta.annotations
 
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
+import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.openapi.editor.markup.GutterIconRenderer
-import com.intellij.testFramework.TestActionEvent
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
 import org.jetbrains.plugins.scala.{ScalaBundle, ScalaFileType}
 import org.junit.Assert
-import org.junit.Assert.assertEquals
 
 /**
   * @author mutcianm
   * @since 31.10.16.
   */
 class MetaAnnotationsTest extends MetaAnnotationTestBase {
+  import MetaAnnotationTestBase._
 
   def testAddMethodToObject(): Unit = {
     val mynewcoolmethod = "myNewCoolMethod"
@@ -72,6 +74,59 @@ class MetaAnnotationsTest extends MetaAnnotationTestBase {
     Assert.assertEquals("Wrong tooltip text: compiled class not detected", ScalaBundle.message("scala.meta.expand"), getGutter.getTooltipText)
 //    typeInAnnotationFile()
 //    Assert.assertEquals("Wrong tooltip text: out of date not detected after file edit", ScalaBundle.message("scala.meta.recompile"), getGutter.getTooltipText)
+  }
+
+  def testInjectSuper(): Unit = {
+    val superName = "Foo"
+    val superMethodName = "foo"
+    compileMetaSource(
+      s"""
+        |import scala.meta._
+        |class addSuper extends scala.annotation.StaticAnnotation {
+        |  inline def apply(defn: Any): Any = meta {
+        |    val q"class $$name" = defn
+        |    q"class $$name extends $superName"
+        |  }
+        |}
+      """.stripMargin
+    )
+    myFixture.configureByText("Test.scala",
+    s"""
+      |class $superName { def $superMethodName = 42 }
+      |
+      |@addSuper
+      |class Test
+    """.stripMargin)
+
+    val clazz = myFixture.findClass("Test").asInstanceOf[ScClass]
+    Assert.assertTrue("Method from injected superclass not found", clazz.findMethodsByName(superMethodName, checkBases = true).nonEmpty)
+  }
+
+  def testRemoveMethod(): Unit = {
+    val removedMethodName = "foo"
+    compileMetaSource(mkAnnot(annotName,
+      """
+        |val q"class $name { ..$_ }" = defn
+        |q"class $name { def fooBar = 42 }"
+      """.stripMargin)
+    )
+    myFixture.configureByText(s"$testClassName.scala",
+      s"""
+      |@$annotName
+      |class $testClassName { def $removedMethodName = 42 }
+      |
+      |new $testClassName().$removedMethodName<caret>
+    """.stripMargin)
+
+    elementAtCaret match {
+      case ref: ScReferenceExpression if ref.bind().isDefined => Assert.fail("Deleted method still resolves")
+      case _ =>
+    }
+    myFixture.completeBasic()
+    myFixture.getElementAtCaret match {
+      case fun: ScFunctionDefinition if fun.name == "fooBar" =>
+      case _ => Assert.fail("Generated method does not resolve")
+    }
   }
 
 }
