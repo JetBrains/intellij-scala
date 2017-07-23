a24168ed984a17373ef0f9477cd7e89781d4543f
Ilya Kirillov <darthorimar@users.noreply.github.com>
2017-07-23 12:59:06 +0300

    Move importproject out to separate method

--- test/org/jetbrains/plugins/cbt/project/ProjectImportTest.scala
+++ test/org/jetbrains/plugins/cbt/project/ProjectImportTest.scala
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.cbt.project
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
 import org.jetbrains.plugins.cbt.project.model.{CbtProjectConverter, CbtProjectInfo}
 import org.jetbrains.plugins.cbt.project.settings.CbtExecutionSettings
@@ -12,13 +13,8 @@ import org.junit.Test
 import scala.util.Success
 
 class ProjectImportTest {
-
   @Test
   def testSimple(): Unit = {
-    val name = "testSimple"
-    val testFixture = IdeaTestFixtureFactory.getFixtureFactory.createFixtureBuilder(name).getFixture
-    testFixture.setUp()
-    val project = testFixture.getProject
     val buildInfo =
       """<project name="NAME" root="ROOT" rootModule="module">
         |    <modules>
@@ -29,17 +25,25 @@ class ProjectImportTest {
         |            </sourceDirs>
         |        </module>
         |    </modules>
-        |</project>""".stripMargin.replaceAllLiterally("NAME", name).replaceAllLiterally("ROOT", project.getBasePath)
+        |</project>""".stripMargin
+    val project = importProject("testSimple", buildInfo)
+    val moduleNames = ModuleManager.getInstance(project).getModules.map(_.getName).toSeq
+    assertEquals(Seq("rootModule"), moduleNames)
+  }
 
+  private def importProject(name: String, buildInfo: String): Project = {
+    val testFixture = IdeaTestFixtureFactory.getFixtureFactory.createFixtureBuilder(name).getFixture
+    testFixture.setUp()
+    val project = testFixture.getProject
+    val xml = scala.xml.XML.loadString(buildInfo.replaceAllLiterally("NAME", name).replaceAllLiterally("ROOT", project.getBasePath))
     val settings = new CbtExecutionSettings(project.getBasePath, false, true, true, Seq.empty)
-    val xml = Success(scala.xml.XML.loadString(buildInfo))
-    xml.map(CbtProjectInfo(_))
+    Success(xml)
+      .map(CbtProjectInfo(_))
       .flatMap(CbtProjectConverter(_, settings))
       .map { projectNode =>
         ServiceManager.getService(classOf[ProjectDataManager]).importData(projectNode, project, true)
       }
       .get
-    val moduleNames = ModuleManager.getInstance(project).getModules.map(_.getName).toSeq
-    assertEquals(Seq("rootModule"), moduleNames)
+    project
   }
 }
