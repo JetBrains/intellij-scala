f5c5c34e4154b9da5ae3ed542c9af8ebf3763b25
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-08-04 10:17:52 +0200

    ignore failing performance tests. #SCL-12231

--- test/org/jetbrains/plugins/scala/performance/highlighting/projectHighlighting/ScalaCommunityPerformanceRehighlightingTest.scala
+++ test/org/jetbrains/plugins/scala/performance/highlighting/projectHighlighting/ScalaCommunityPerformanceRehighlightingTest.scala
@@ -10,21 +10,19 @@ import org.junit.experimental.categories.Category
   * Author: Svyatoslav Ilinskiy
   * Date: 11/18/2015
   */
+@Ignore
 @Category(Array(classOf[SlowTests]))
 class ScalaCommunityPerformanceRehighlightingTest extends RehighlightingPerformanceTypingTestBase
   with ScalaCommunityGithubRepo {
 
-  @Ignore
   def testTypingInScalaPsiUtil(): Unit = {
     doTest("ScalaPsiUtil.scala", 4.seconds, Seq("val i = Some(10)\n"), new LogicalPosition(80, 1), Some("def foo() = {\n"))
   }
 
-  @Ignore
   def testTypingInScalaPsiUtilInClassBody(): Unit = {
     doTest("ScalaPsiUtil.scala", 40.seconds, Seq("val i = Some(10)\n"), new LogicalPosition(80, 1), None)
   }
 
-  @Ignore
   def testTypingInsideFunctionWithDefinedReturnType(): Unit = {
     doTest("ScalaPsiUtil.scala", 3.seconds, Seq("val i = Some(10)\n"), new LogicalPosition(80, 1), Some("def foo(): Unit = {\n"))
   }
