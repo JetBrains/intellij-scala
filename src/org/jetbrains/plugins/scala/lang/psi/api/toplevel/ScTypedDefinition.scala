08b8f8e62d8b734524df4ffe7e64b80c95bdba06
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2017-08-17 15:04:12 +0300

    wrong parameter name for macro annotation fixed

--- src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScTypedDefinition.scala
+++ src/org/jetbrains/plugins/scala/lang/psi/api/toplevel/ScTypedDefinition.scala
@@ -93,7 +93,7 @@ trait ScTypedDefinition extends ScNamedElement with Typeable {
 
   import org.jetbrains.plugins.scala.lang.psi.light.PsiTypedDefinitionWrapper.DefinitionRole._
 
-  @Cached(modificationCount = ModCount.getBlockModificationCount, this)
+  @Cached(ModCount.getBlockModificationCount, this)
   def getTypedDefinitionWrapper(isStatic: Boolean, isInterface: Boolean, role: DefinitionRole,
                                 cClass: Option[PsiClass] = None): PsiTypedDefinitionWrapper = {
     new PsiTypedDefinitionWrapper(this, isStatic, isInterface, role, cClass)
