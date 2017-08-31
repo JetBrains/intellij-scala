a762fee4968680c493f6a80a78c6c313d2cbad1b
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2017-08-31 12:49:21 +0300

    some tests moved from failing category

--- test/org/jetbrains/plugins/scala/annotator/ApplicationAnnotatorTest.scala
+++ test/org/jetbrains/plugins/scala/annotator/ApplicationAnnotatorTest.scala
@@ -109,4 +109,54 @@ class ApplicationAnnotatorTest extends ApplicationAnnotatorTestBase {
       case Error("Any", "Too many type arguments for f") :: Nil =>
     }
   }
+
+  def testSCL11227() = {
+    val code =
+      """
+        |object Demo1b {
+        |  class Foo[T, F[_]]
+        |
+        |  def meh[M[_[_]], F[_]](x: M[F]): M[F] = x
+        |
+        |  meh(new Foo[Int, List])
+        |}
+        |
+        |object Demo1c {
+        |  trait TC[T]
+        |  class Foo[F[_], G[_]]
+        |
+        |  def meh[M[_[_]]](x: M[TC]): M[TC] = x
+        |
+        |  meh(new Foo[TC, TC])
+        |}
+        |
+        |object Demo1d {
+        |  trait TC[F[_]]
+        |  trait TC2[F[_]]
+        |  class Foo[F[_[_]], G[_[_]]]
+        |  new Foo[TC, TC2]
+        |
+        |  def meh[M[_[_[_]]]](x: M[TC2]): M[TC2] = x
+        |
+        |  meh(new Foo[TC, TC2])
+        |}
+      """.stripMargin
+    assert(messages(code).isEmpty)
+  }
+
+  def testSCL11306() = {
+    val code =
+      """
+        |object T {
+        |  class A[F[_]]
+        |  class B[F[_]]
+        |  class C[F[_]]
+        |
+        |  final case class Prod[F[_[_]], G[_[_]], A[_]](fa: F[A], ga: G[A])
+        |
+        |  val x = Prod(new A[List], Prod(new B[List], new C[List]))
+        |}
+      """.stripMargin
+    assert(messages(code).isEmpty)
+  }
 }
