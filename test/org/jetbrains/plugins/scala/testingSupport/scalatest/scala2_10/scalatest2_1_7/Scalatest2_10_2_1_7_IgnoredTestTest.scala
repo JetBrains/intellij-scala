3c5d752903bf5343b27ead7a0f8ffe65c5a40b01
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-09-15 14:34:33 +0200

    fix various test tests failing.
    they seemed to break due to a NoSuchMethodError using scala.collection.JavaConverters.seqAsJavaList from Java. But why did they work before?

--- test/org/jetbrains/plugins/scala/testingSupport/scalatest/scala2_10/scalatest2_1_7/Scalatest2_10_2_1_7_IgnoredTestTest.scala
+++ test/org/jetbrains/plugins/scala/testingSupport/scalatest/scala2_10/scalatest2_1_7/Scalatest2_10_2_1_7_IgnoredTestTest.scala
@@ -6,5 +6,5 @@ import org.jetbrains.plugins.scala.testingSupport.scalatest.IgnoredSpecTest
  * @author Roman.Shein
  * @since 22.01.2015.
  */
-class Scalatest2_10_2_1_7_IngoredTestTest extends Scalatest2_10_2_1_7_Base with IgnoredSpecTest {
+class Scalatest2_10_2_1_7_IgnoredTestTest extends Scalatest2_10_2_1_7_Base with IgnoredSpecTest {
 }
