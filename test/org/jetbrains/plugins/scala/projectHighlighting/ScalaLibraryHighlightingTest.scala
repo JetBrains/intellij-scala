00630cecce10d317d451cc457cfb21a98660f8fe
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2017-09-27 15:43:07 +0300

    scala-library highlighting test added #SCL-12641

---
+++ test/org/jetbrains/plugins/scala/projectHighlighting/ScalaLibraryHighlightingTest.scala
@@ -0,0 +1,41 @@
+package org.jetbrains.plugins.scala.projectHighlighting
+
+import com.intellij.openapi.vfs.{VfsUtilCore, VirtualFile}
+import com.intellij.psi.PsiManager
+import com.intellij.util.Processor
+import org.jetbrains.plugins.scala.{ScalaFileType, SlowTests}
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.jetbrains.plugins.scala.base.libraryLoaders.IvyLibraryLoader.Sources
+import org.jetbrains.plugins.scala.base.libraryLoaders.ScalaLibraryLoader
+import org.jetbrains.plugins.scala.debugger.{ScalaVersion, Scala_2_12}
+import org.jetbrains.plugins.scala.util.reporter.ProgressReporter
+import org.junit.experimental.categories.Category
+
+
+/**
+  * Nikolay.Tropin
+  * 27-Sep-17
+  */
+abstract class ScalaLibraryHighlightingTest extends ScalaLightCodeInsightFixtureTestAdapter {
+
+  def testHighlightScalaLibrary(): Unit = {
+    val reporter = ProgressReporter.newInstance(reportSuccess = false)
+
+    val sources = ScalaLibraryLoader.ScalaRuntimeLoader(Sources).rootFiles.head
+    VfsUtilCore.processFilesRecursively(sources, (vFile: VirtualFile) => {
+      if (vFile.getFileType == ScalaFileType.INSTANCE) {
+        reporter.notify(VfsUtilCore.getRelativePath(vFile, sources))
+
+        val psiFile = PsiManager.getInstance(getProject).findFile(vFile)
+        AllProjectHighlightingTest.annotateFile(psiFile, reporter)
+      }
+      true
+    })
+    reporter.reportResults()
+  }
+}
+
+@Category(Array(classOf[SlowTests]))
+class ScalaLibraryHighlightingTest_2_12 extends ScalaLibraryHighlightingTest {
+  override implicit val version: ScalaVersion = Scala_2_12
+}
