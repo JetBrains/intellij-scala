f6da3bdff977a632e1e632f83b585b7d59b79c36
Maris <maris.alex93@gmail.com>
2017-10-23 14:07:21 +0300

    small fix in enable hydra action

--- scala/scala-impl/src/org/jetbrains/plugins/scala/actions/EnableHydraSettingsAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/actions/EnableHydraSettingsAction.scala
@@ -1,20 +1,31 @@
 package org.jetbrains.plugins.scala.actions
 
-import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys}
+import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys, ToggleAction}
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.compiler.HydraCompilerSettings
 
 /**
   * @author Maris Alexandru
   */
-class EnableHydraSettingsAction extends AnAction{
-  def actionPerformed(e: AnActionEvent) {
+class EnableHydraSettingsAction extends ToggleAction{
+
+  override def setSelected(e: AnActionEvent, toSet: Boolean): Unit = {
     CommonDataKeys.PROJECT.getData(e.getDataContext) match {
       case project: Project => {
         val settings = HydraCompilerSettings.getInstance(project)
-        settings.isHydraSettingsEnabled = !settings.isHydraSettingsEnabled
+        settings.isHydraSettingsEnabled = toSet
       }
       case _ =>
+    }
+  }
+
+  override def isSelected(e: AnActionEvent): Boolean = {
+    CommonDataKeys.PROJECT.getData(e.getDataContext) match {
+      case project: Project => {
+        val settings = HydraCompilerSettings.getInstance(project)
+        settings.isHydraSettingsEnabled
+      }
+      case _ => false
     }
   }
 }
