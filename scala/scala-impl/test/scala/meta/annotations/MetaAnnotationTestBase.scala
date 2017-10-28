c394785d84706c483828f3ec0a69c56bec6476cb
Mikhail Mutcianko <mutcianko.m@gmail.com>
2017-10-28 23:55:26 +0300

    use up-to-date scala versions in meta annotation tests

--- scala/scala-impl/test/scala/meta/annotations/MetaAnnotationTestBase.scala
+++ scala/scala-impl/test/scala/meta/annotations/MetaAnnotationTestBase.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import com.intellij.ProjectTopics
 import com.intellij.compiler.server.BuildManager
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.module.{JavaModuleType, Module}
 import com.intellij.openapi.project.Project
@@ -30,6 +31,7 @@ import org.junit.Assert
 import org.junit.Assert.fail
 import org.junit.experimental.categories.Category
 
+import scala.collection.JavaConverters.collectionAsScalaIterableConverter
 import scala.meta.ScalaMetaLibrariesOwner.MetaBaseLoader
 import scala.meta.{Compilable, ScalaMetaLibrariesOwner}
 
@@ -95,7 +97,7 @@ abstract class MetaAnnotationTestBase extends JavaCodeInsightFixtureTestCase wit
 
   protected def compileMetaSource(source: String = FileUtil.loadFile(new File(getTestDataPath, s"${getTestName(false)}.scala"))): List[String] = {
     addMetaSource(source)
-    val cache = new CompilationCache(metaModule, Seq(version.major, ScalaMetaLibrariesOwner.metaVersion))
+    val cache = new CompilationCache(metaModule, Seq(version.minor, ScalaMetaLibrariesOwner.metaVersion))
     cache.withModuleOutputCache(List[String]()) {
       setUpCompiler
       enableParadisePlugin()
@@ -112,10 +114,20 @@ abstract class MetaAnnotationTestBase extends JavaCodeInsightFixtureTestCase wit
   protected def enableParadisePlugin(): Unit = {
     val profile = ScalaCompilerConfiguration.instanceIn(project).defaultProfile
     val settings = profile.getSettings
-    settings.plugins :+= MetaParadiseLoader()(metaModule).path
+    val path = MetaParadiseLoader()(metaModule).path
+    assert(new File(path).exists(), "Paradise plugin not found, aborting compilation")
+    settings.plugins :+= path
     profile.setSettings(settings)
   }
 
+  protected def checkNoErrorHighlights(expectedMessagePrefix: String = ""): Unit = {
+    val errors = myFixture.doHighlighting(HighlightSeverity.ERROR).asScala
+    val (related, unrelated) = errors.partition(_.getDescription.startsWith(expectedMessagePrefix))
+    val suffix = if (unrelated.size > 1) s"\nOther errors found:\n${unrelated.mkString("\n")}" else ""
+    val prefix = related.mkString("\n")
+    if (related.nonEmpty)
+      Assert.fail(prefix + suffix)
+  }
 
   protected def checkExpansionEquals(code: String, expectedExpansion: String): Unit = {
     import scala.meta.intellij.psiExt._
