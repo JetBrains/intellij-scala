11bcd63627585f9acfec050f3a7041945729ece8
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2017-11-01 14:30:07 +0300

    Ammonite: pop-up suggestion to import all $ivy dependencies #SCL-12792 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/ammonite/AmmoniteUnresolvedLibraryInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/ammonite/AmmoniteUnresolvedLibraryInspection.scala
@@ -5,6 +5,7 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.codeInspection.AbstractInspection
 import org.jetbrains.plugins.scala.extensions.implementation.iterator.ParentsIterator
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScReferenceElement, ScStableCodeReferenceElement}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.{ScImportExpr, ScImportSelector}
 
@@ -25,6 +26,7 @@ class AmmoniteUnresolvedLibraryInspection extends AbstractInspection("Unresolved
   
   private def processExpr(ref: ScReferenceElement, qualifier: ScStableCodeReferenceElement, holder: ProblemsHolder) {
     if (qualifier == null || qualifier.refName != "$ivy" || ref.resolve() != null) return
+    AmmoniteScriptWrappersHolder.getInstance(ref.getProject).registerProblemIn(ref.getContainingFile.asInstanceOf[ScalaFile])
     holder.registerProblem(ref, "Cannot resolve import", ProblemHighlightType.WEAK_WARNING, null: TextRange,
       new CreateImportedLibraryQuickFix(ref))
   }
