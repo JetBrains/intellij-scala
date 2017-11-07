ae8f962e00bddb3cff2a1c7d5f75ff2244a004fe
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2017-11-07 13:59:46 +0300

    "Option with constant" inspection converted to intention #SCL-11890 fixed

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/intention/expression/OptionWithLiteralToSomeIntention.scala
@@ -0,0 +1,49 @@
+package org.jetbrains.plugins.scala.codeInsight.intention.expression
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
+import org.jetbrains.plugins.scala.codeInspection.collections.{`scalaOption`, literal}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+
+/**
+  * Nikolay.Tropin
+  * 07-Nov-17
+  */
+class OptionWithLiteralToSomeIntention extends PsiElementBaseIntentionAction  {
+  import OptionWithLiteralToSomeIntention._
+
+  override def getFamilyName: String = familyName
+
+  override def getText: String = InspectionBundle.message("replace.with.some")
+
+  override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = element match {
+    case OptionLiteral(_, _) => true
+    case _ => false
+  }
+
+  override def invoke(project: Project, editor: Editor, element: PsiElement): Unit = element match {
+    case OptionLiteral(opt, constant) =>
+      val newExpr = ScalaPsiElementFactory.createExpressionFromText(s"Some($constant)")(project)
+      opt.replaceExpression(newExpr, removeParenthesis = true)
+    case _ =>
+  }
+}
+
+object OptionWithLiteralToSomeIntention {
+
+  val familyName: String = InspectionBundle.message("replace.option.with.some")
+
+  object OptionLiteral {
+    def unapply(element: PsiElement): Option[(ScMethodCall, String)] =  {
+      PsiTreeUtil.getParentOfType(element, classOf[ScMethodCall]) match {
+        case opt @ `scalaOption`(literal(constant)) if constant != "null" => Some((opt, constant))
+        case _ => None
+      }
+    }
+  }
+}
