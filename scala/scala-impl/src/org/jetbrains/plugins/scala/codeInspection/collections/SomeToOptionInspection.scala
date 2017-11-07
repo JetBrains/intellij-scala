fc923e7e183e12f597d95894ae92893b0e371482
Toshiaki Kameyama <kmymtsak@gmail.com>
2017-11-07 20:02:43 +0900

    Inspection added: Replace Some(a) with Option(a)
    SCL-9625 fixed

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/SomeToOptionInspection.scala
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.scala.codeInspection.collections
+
+import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+
+/**
+  * @author t-kameyama
+  */
+class SomeToOptionInspection extends OperationOnCollectionInspection {
+  override def possibleSimplificationTypes: Array[SimplificationType] = Array(SomeToOption)
+}
+
+object SomeToOption extends SimplificationType {
+  override def hint: String = InspectionBundle.message("replace.with.option")
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case `scalaSome`(e) => Some(replace(expr).withText(s"Option(${e.getText})").highlightFrom(expr))
+    case _ => None
+  }
+}
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/collections/SomeToOptionTest.scala
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package collections
+
+import com.intellij.testFramework.EditorTestUtil
+
+/**
+  * @author t-kameyama
+  */
+class SomeToOptionTest extends OperationsOnCollectionInspectionTest {
+
+  import EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
+
+  override protected val classOfInspection: Class[_ <: OperationOnCollectionInspection] =
+    classOf[SomeToOptionInspection]
+
+  override protected val hint: String = InspectionBundle.message("replace.with.option")
+
+  def testVal(): Unit = {
+    doTest(
+      s"""val i = 1; ${START}Some(i)$END""",
+      """val i = 1; Some(i)""",
+      """val i = 1; Option(i)"""
+    )
+  }
+
+  def testBinaryExpression(): Unit = {
+    doTest(
+      s"""val i = 1; ${START}Some(i * 2)$END""",
+      """val i = 1; Some(i * 2)""",
+      """val i = 1; Option(i * 2)"""
+    )
+  }
+
+  def testConstant(): Unit = {
+    doTest(
+      s"""${START}Some("constant")$END""",
+      """Some("constant")""",
+      """Option("constant")"""
+    )
+  }
+
+}
