02bf5874dadc0380e703dae5f216460dcb2c2eeb
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-11-08 17:52:01 +0100

    warnings and error reporting in build toolwindow in sbt shell output #SCL-11525

--- scala/scala-impl/src/org/jetbrains/sbt/project/structure/SbtShellDump.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/project/structure/SbtShellDump.scala
@@ -1,11 +1,15 @@
 package org.jetbrains.sbt.project.structure
 
 import java.io.File
-import java.util.UUID
+import java.util.{Collections, UUID}
 
+import com.intellij.build.SyncViewManager
+import com.intellij.build.events.MessageEvent
+import com.intellij.build.events.impl.MessageEventImpl
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.externalSystem.model.ExternalSystemException
-import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListener}
 import com.intellij.openapi.externalSystem.model.task.event._
+import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListener}
 import org.jetbrains.sbt.shell.SbtShellCommunication
 import org.jetbrains.sbt.shell.SbtShellCommunication.{EventAggregator, Output, TaskComplete, TaskStart}
 
@@ -15,21 +19,26 @@ import scala.util.Try
 
 object SbtShellDump {
 
-  def dumpFromShell(id: ExternalSystemTaskId, dir: File,
-                    shell: SbtShellCommunication,
+  def dumpFromShell(id: ExternalSystemTaskId,
+                    dir: File,
                     structureFilePath: String,
                     options: Seq[String],
-                    notifications: ExternalSystemTaskNotificationListener): Try[String] = {
+                    notifications: ExternalSystemTaskNotificationListener,
+                   ): Try[String] = {
 
     notifications.onStart(id, dir.getCanonicalPath)
 
+    val project = id.findProject() // assume responsibility of caller not to call dumpFromShell with null project
+    val viewManager = ServiceManager.getService(project, classOf[SyncViewManager])
+    val shell = SbtShellCommunication.forProject(project)
+
     val optString = options.mkString(" ")
     val setCmd = s"""set _root_.org.jetbrains.sbt.StructureKeys.sbtStructureOptions in Global := "$optString""""
-    val cmd = s";reload; $setCmd ;*/*:dumpStructureTo $structureFilePath"
+    val cmd = s";reload; $setCmd ;*/*:dumpStructureTo $structureFilePath; session clear-all"
 
     val taskDescriptor =
       new TaskOperationDescriptorImpl("dump project structure from sbt shell", System.currentTimeMillis(), "project-structure-dump")
-    val aggregator = messageAggregator(id, s"dump:${UUID.randomUUID()}", taskDescriptor, shell, notifications)
+    val aggregator = messageAggregator(id, s"dump:${UUID.randomUUID()}", taskDescriptor, shell, notifications, viewManager)
     val output = shell.command(cmd, new StringBuilder, aggregator, showShell = false)
 
     Await.ready(output, Duration.Inf) // TODO some kind of timeout / cancel mechanism
@@ -41,7 +50,9 @@ object SbtShellDump {
                                 dumpTaskId: String,
                                 taskDescriptor: TaskOperationDescriptor,
                                 shell: SbtShellCommunication,
-                                notifications: ExternalSystemTaskNotificationListener): EventAggregator[StringBuilder] = {
+                                notifications: ExternalSystemTaskNotificationListener,
+                                viewManager: SyncViewManager
+                               ): EventAggregator[StringBuilder] = {
     case (messages, TaskStart) =>
       val startEvent = new ExternalSystemStartEventImpl[TaskOperationDescriptor](dumpTaskId, null, taskDescriptor)
       val event = new ExternalSystemTaskExecutionEvent(id, startEvent)
@@ -50,21 +61,32 @@ object SbtShellDump {
     case (messages, TaskComplete) =>
       val finishEvent = new ExternalSystemFinishEventImpl[TaskOperationDescriptor](
         dumpTaskId, null, taskDescriptor,
-        new SuccessResultImpl(0, System.currentTimeMillis(), true)
+        new SuccessResultImpl(taskDescriptor.getEventTime, System.currentTimeMillis(), true)
       )
       val event = new ExternalSystemTaskExecutionEvent(id, finishEvent)
       notifications.onStatusChange(event)
       messages
     case (messages, Output(text)) =>
       if (text.contains("(i)gnore")) {
+        // TODO check if this works correctly at all, or why not
         shell.send("i" + System.lineSeparator)
-        val ex = new ExternalSystemException("Error importing sbt project. Please check sbt shell output.")
-        notifications.onFailure(id, ex)
-        notifications.onFailure(id,ex)
-        //      } else if (text.startsWith("[warn]")) {
-        //        val messageEvent = new MessageEventImpl(dumpTaskId, MessageEvent.Kind.WARNING, null, text.stripPrefix("[warning]"))
-        //        val event = new ExternalSystemTaskExecutionEvent(id, messageEvent)
-        //        notifications.onStatusChange(event)
+        val msg = "Error importing sbt project. Please check sbt shell output."
+        val ex = new ExternalSystemException(msg)
+        val failure = new FailureImpl(msg, "error during sbt import", Collections.emptyList())
+        val timestamp = System.currentTimeMillis()
+        val finishEvent = new ExternalSystemFinishEventImpl[TaskOperationDescriptor](
+          dumpTaskId, null, taskDescriptor,
+          new FailureResultImpl(taskDescriptor.getEventTime, timestamp, Collections.singletonList(failure))
+        )
+        val event = new ExternalSystemTaskExecutionEvent(id, finishEvent)
+        notifications.onFailure(id, ex) // TODO redundant?
+        notifications.onStatusChange(event)
+      } else if (text.startsWith("[warn]")) {
+        val messageEvent = new MessageEventImpl(dumpTaskId, MessageEvent.Kind.WARNING, "warnings", text.stripPrefix("[warn]"))
+        viewManager.onEvent(messageEvent)
+      } else if (text.startsWith("[error]")) {
+        val messageEvent = new MessageEventImpl(dumpTaskId, MessageEvent.Kind.ERROR, "errors", text.stripPrefix("[error]"))
+        viewManager.onEvent(messageEvent)
       } else {
 
         val progressEvent = new ExternalSystemStatusEventImpl[TaskOperationDescriptor](
