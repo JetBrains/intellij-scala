c6912e622241c4edf9d8ffa4107fac8adfe26248
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-11-10 16:44:39 +0100

    correctly determine sbt major version for versions >= 1.1 #SCL-12901 fixed

--- scala/scala-impl/src/org/jetbrains/sbt/SbtUtil.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/SbtUtil.scala
@@ -49,7 +49,7 @@ object SbtUtil {
     if ((sbtVersion ~= Version("1.0.0")) && sbtVersion.presentation.contains("-M"))
       sbtVersion
     // sbt uses binary version x.0 for [x.0,x+1.0[
-    else if (sbtVersion.major(1) >= Version("1.0")) Version("1.0")
+    else if (sbtVersion.major(1) == Version("1")) Version("1.0")
     else sbtVersion.major(2)
 
   def detectSbtVersion(directory: File, sbtLauncher: => File): String =
