990772ddb6001da3987de5e2406c69ca7f37abc0
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2017-11-20 19:52:18 +0300

    Ammonite: 'create dependency from $ivy import' quick fix should work for -M and -RC versions too

--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/ammonite/AmmoniteUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/ammonite/AmmoniteUtil.scala
@@ -142,7 +142,7 @@ object AmmoniteUtil {
     }
     
     qual(refElement) match {
-      case Some(q) if scriptResolvePlugin(q) && refElement.getReference.getCanonicalText == ROOT_IVY => //refElement.getReference.getCanonicalText
+      case Some(q) if scriptResolvePlugin(q) && refElement.getReference.getCanonicalText == ROOT_IVY => 
         Option(refElement.getContainingFile.getContainingDirectory)
       case Some(q) if scriptResolveIvy(q) || scriptResolvePlugin(q) =>
         findLibrary(refElement) flatMap {
@@ -158,10 +158,15 @@ object AmmoniteUtil {
     AmmoniteUtil.extractLibInfo(refElement).flatMap {
       case LibInfo(group, name, version, scalaVersion) =>
         val existsPredicate = (f: File) => f.exists()
-        val nv = s"${name}_$scalaVersion"
         
-        val ivyPath = s"$group/$nv/jars|bundles/$version.jar"
-        val mavenPath = s"maven2/${group.replace('.', '/')}/$nv|$name/$version/$nv-$version.jar|$name-$version.jar"
+        val nv = s"${name}_$scalaVersion"
+        val fullVersion = s"$nv|$nv.${ScalaUtil.getScalaVersion(refElement.getContainingFile).flatMap(_.split('.').lastOption).getOrElse("0")}"
+        
+        val ivyPath = s"$group/$fullVersion/jars|bundles"
+        val mavenPath = s"maven2/${group.replace('.', '/')}/$nv|$name/$version"
+        
+        val prefixPatterns = Seq(name, version)
+        
         
         def tryIvy() = findFileByPattern(
           s"$getDefaultCachePath/$ivyPath", 
@@ -172,11 +177,17 @@ object AmmoniteUtil {
           s"$getCoursierCachePath/https/repo1.maven.org/$mavenPath",
           existsPredicate
         )
-        
-        tryIvy() orElse tryCoursier() flatMap { //todo more variants? 
-          jarModuleRoot =>
-            val jarRoot = JarFileSystem.getInstance().findLocalVirtualFileByPath(jarModuleRoot.getCanonicalPath)
-            Option(jarRoot)
+
+        tryIvy() orElse tryCoursier() flatMap {
+          parent =>
+            parent.listFiles().find{
+              cf =>
+                val name = cf.getName
+                prefixPatterns.exists(name.startsWith) && name.endsWith(".jar")
+            } flatMap { //todo more variants? 
+              jarModuleRoot =>
+                Option(JarFileSystem.getInstance().findLocalVirtualFileByPath(jarModuleRoot.getCanonicalPath))
+            }
         }
     }
   }
