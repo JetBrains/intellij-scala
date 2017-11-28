0d0cfcce97e89377deb105a83ab92a6fd2a72130
Pavel Fatin <pavel.fatin@jetbrains.com>
2017-11-28 15:00:54 +0100

    ComparingLengthInspection: simplifications, #SCL-8300

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/ComparingLengthInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/ComparingLengthInspection.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.codeInspection.collections
 
 import org.jetbrains.plugins.scala.codeInspection.InspectionBundle
+import org.jetbrains.plugins.scala.codeInspection.collections.ComparingLengthInspection._
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 
 /**
@@ -10,20 +11,19 @@ class ComparingLengthInspection extends OperationOnCollectionInspection{
   override def possibleSimplificationTypes: Array[SimplificationType] = Array(ComparingLength)
 }
 
-object ComparingLength extends SimplificationType {
-  override def hint: String = InspectionBundle.message("replace.with.lengthCompare")
+private object ComparingLengthInspection {
+  private val ComparingLength: SimplificationType = new SimplificationType() {
+    override def hint: String = InspectionBundle.message("replace.with.lengthCompare")
 
-  override def getSimplification(expr: ScExpression): Option[Simplification] = {
-    (expr match {
-      case qual `.sizeOrLength` () `>` n if isSeq(qual) => Some(qual, n, ">")
-      case qual `.sizeOrLength` () `>=` n if isSeq(qual) => Some(qual, n, ">=")
-      case qual `.sizeOrLength` () `==` n if isSeq(qual) => Some(qual, n, "==")
-      case qual `.sizeOrLength` () `!=` n if isSeq(qual) => Some(qual, n, "!=")
-      case qual `.sizeOrLength` () `<` n if isSeq(qual) => Some(qual, n, "<")
-      case qual `.sizeOrLength` () `<=` n if isSeq(qual) => Some(qual, n, "<=")
-      case _ => None
-    }).map { case (qual, n, op) =>
-      replace(expr).withText(s"${invocationText(qual, "lengthCompare", n)} $op 0").highlightFrom(qual)
+    override def getSimplification(e: ScExpression): Option[Simplification] = Some(e).collect {
+      case q `.sizeOrLength` () `>` n if isSeq(q) => (q, ">", n)
+      case q `.sizeOrLength` () `>=` n if isSeq(q) => (q, ">=", n)
+      case q `.sizeOrLength` () `==` n if isSeq(q) => (q, "==", n)
+      case q `.sizeOrLength` () `!=` n if isSeq(q) => (q, "!=", n)
+      case q `.sizeOrLength` () `<` n if isSeq(q) => (q, "<", n)
+      case q `.sizeOrLength` () `<=` n if isSeq(q) => (q, "<=", n)
+    } map { case (q, op, n) =>
+      replace(e).withText(s"${invocationText(q, "lengthCompare", n)} $op 0").highlightFrom(q)
     }
   }
 }
