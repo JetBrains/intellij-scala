c9a98a384f51581634e1e8042c8887a2d09c88e8
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2017-12-04 21:07:30 +0300

    add notification about incompatibility with HaskForce plugin #SCL-12563

--- scala/scala-impl/src/org/jetbrains/plugins/scala/components/PluginVersionVerifier.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/PluginVersionVerifier.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala
 package components
 
+import java.io.File
 import javax.swing.event.HyperlinkEvent
 
 import com.intellij.ide.plugins._
@@ -9,13 +10,14 @@ import com.intellij.notification._
 import com.intellij.openapi.application.{Application, ApplicationManager}
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.extensions.{ExtensionPointName, PluginId}
+import com.intellij.util.PathUtil
 import org.jetbrains.plugins.scala.extensions.invokeLater
 
 /**
- * @author Alefas
- * @since 31.10.12
- */
+  * @author Alefas
+  * @since 31.10.12
+  */
 abstract class ScalaPluginVersionVerifier {
   def getSinceVersion: String
 
@@ -65,8 +67,9 @@ object ScalaPluginVersionVerifier {
         PluginManager.getPlugin(pluginLoader.getPluginId).asInstanceOf[IdeaPluginDescriptorImpl]
       case other => throw new RuntimeException(s"Wrong plugin classLoader: $other")
     }
-
   }
+
+  def scalaPluginId: String = getPluginDescriptor.getPluginId.getIdString
 }
 
 object ScalaPluginVersionVerifierApplicationComponent {
@@ -74,82 +77,135 @@ object ScalaPluginVersionVerifierApplicationComponent {
 }
 
 class ScalaPluginVersionVerifierApplicationComponent extends ApplicationComponent {
-  import ScalaPluginVersionVerifier._
 
   override def getComponentName: String = "ScalaPluginVersionVerifierApplicationComponent"
 
   override def initComponent(): Unit = {
-
-    def checkVersion() {
-      ScalaPluginVersionVerifier.getPluginVersion match {
-        case Some(version) =>
-          val extensions = ScalaPluginVersionVerifier.EP_NAME.getExtensions
-
-          for (extension <- extensions) {
-            var failed = false
-            def wrongVersion() {
-              failed = true
-              extension.getClass.getClassLoader match {
-                case pluginLoader: PluginClassLoader =>
-                  val plugin = PluginManager.getPlugin(pluginLoader.getPluginId)
-                  val message =
-                    s"Plugin ${plugin.getName} of version ${plugin.getVersion} is " +
-                      s"icompatible with Scala plugin of version $version. Do you want to disable ${plugin.getName} plugin?\n" +
-                      s"""<p/><a href="Yes">Yes, disable it</a>\n""" +
-                      s"""<p/><a href="No">No, leave it enabled</a>"""
-                  if (ApplicationManager.getApplication.isUnitTestMode) {
-                    ScalaPluginVersionVerifierApplicationComponent.LOG.error(message)
-                  } else {
-                    val Scala_Group = "Scala Plugin Incompatibility"
-                    val app: Application = ApplicationManager.getApplication
-                    if (!app.isDisposed) {
-                      app.getMessageBus.syncPublisher(Notifications.TOPIC).register(Scala_Group, NotificationDisplayType.STICKY_BALLOON)
-                    }
-                    NotificationGroup.balloonGroup(Scala_Group)
-                    val notification = new Notification(Scala_Group, "Incompatible plugin detected", message, NotificationType.ERROR, new NotificationListener {
-                      def hyperlinkUpdate(notification: Notification, event: HyperlinkEvent) {
-                        notification.expire()
-                        val description = event.getDescription
-                        description match {
-                          case "Yes" =>
-                            PluginManagerCore.disablePlugin(plugin.getPluginId.getIdString)
-                            PluginManagerConfigurable.showRestartDialog()
-                          case "No"  => //do nothing it seems all is ok for the user
-                          case _     => //do nothing it seems all is ok for the user
-                        }
-                      }
-                    })
-
-                    Notifications.Bus.notify(notification)
-                  }
-              }
-            }
-            Version.parse(extension.getSinceVersion) match {
-              case Some(sinceVersion) =>
-                if (sinceVersion != version && version < sinceVersion) {
-                  wrongVersion()
-                }
-              case _                  =>
-            }
-
-            Version.parse(extension.getUntilVersion) match {
-              case Some(untilVersion) =>
-                if (untilVersion != version && untilVersion < version) {
-                  wrongVersion()
-                }
-              case _                  =>
-            }
-          }
-        case None          =>
-      }
-      ScalaPluginUpdater.askUpdatePluginBranch()
-    }
     invokeLater {
       ScalaPluginUpdater.upgradeRepo()
       checkVersion()
+      checkHaskForcePlugin()
       ScalaPluginUpdater.postCheckIdeaCompatibility()
       ScalaPluginUpdater.setupReporter()
     }
   }
 
+  private def checkVersion() {
+    import ScalaPluginVersionVerifier._
+
+    ScalaPluginVersionVerifier.getPluginVersion match {
+      case Some(version) =>
+        val extensions = ScalaPluginVersionVerifier.EP_NAME.getExtensions
+
+        for (extension <- extensions) {
+          var failed = false
+          def wrongVersion() {
+            failed = true
+            extension.getClass.getClassLoader match {
+              case pluginLoader: PluginClassLoader =>
+                val plugin = PluginManager.getPlugin(pluginLoader.getPluginId)
+                val message =
+                  s"Plugin ${plugin.getName} of version ${plugin.getVersion} is " +
+                    s"incompatible with Scala plugin of version $version. Do you want to disable ${plugin.getName} plugin?\n" +
+                    s"""<p/><a href="Yes">Yes, disable it</a>\n""" +
+                    s"""<p/><a href="No">No, leave it enabled</a>"""
+
+                showIncompatiblePluginNotification(plugin, message) {
+                  case "Yes" =>
+                    disablePlugin(plugin.getPluginId.getIdString)
+                  case "No" => //do nothing it seems all is ok for the user
+                  case _ => //do nothing it seems all is ok for the user
+                }
+            }
+          }
+          Version.parse(extension.getSinceVersion) match {
+            case Some(sinceVersion) =>
+              if (sinceVersion != version && version < sinceVersion) {
+                wrongVersion()
+              }
+            case _                  =>
+          }
+
+          Version.parse(extension.getUntilVersion) match {
+            case Some(untilVersion) =>
+              if (untilVersion != version && untilVersion < version) {
+                wrongVersion()
+              }
+            case _                  =>
+          }
+        }
+      case None          =>
+    }
+    ScalaPluginUpdater.askUpdatePluginBranch()
+  }
+
+  private def showIncompatiblePluginNotification(plugin: IdeaPluginDescriptor, message: String)(callback: String => Unit): Unit = {
+    if (ApplicationManager.getApplication.isUnitTestMode) {
+      ScalaPluginVersionVerifierApplicationComponent.LOG.error(message)
+    } else {
+      val Scala_Group = "Scala Plugin Incompatibility"
+      val app: Application = ApplicationManager.getApplication
+      if (!app.isDisposed) {
+        app.getMessageBus.syncPublisher(Notifications.TOPIC).register(Scala_Group, NotificationDisplayType.STICKY_BALLOON)
+      }
+      NotificationGroup.balloonGroup(Scala_Group)
+      val notification = new Notification(Scala_Group, "Incompatible plugin detected", message, NotificationType.ERROR, new NotificationListener {
+        def hyperlinkUpdate(notification: Notification, event: HyperlinkEvent) {
+          notification.expire()
+          val description = event.getDescription
+          callback(description)
+        }
+      })
+
+      Notifications.Bus.notify(notification)
+    }
+  }
+
+  private def checkHaskForcePlugin(): Unit = {
+    val haskforceId = "com.haskforce"
+    val plugin = PluginManager.getPlugin(PluginId.findId(haskforceId))
+    if (plugin == null || !plugin.isEnabled)
+      return
+
+    val hasScala211 = try {
+      val scalaClass = plugin.getPluginClassLoader.loadClass("scala.Option")
+      val pathToJar = PathUtil.getJarPathForClass(scalaClass)
+      val file = new File(pathToJar)
+      file.getName.contains("-2.11")
+    } catch {
+      case _: ClassNotFoundException => false
+      case e: Exception =>
+        ScalaPluginVersionVerifierApplicationComponent.LOG.debug(e)
+        false
+    }
+    if (hasScala211) {
+      val DisableHaskForcePlugin = "Disable HaskForce Plugin"
+      val DisableScalaPlugin = "Disable Scala Plugin"
+      val Ignore = "Ignore"
+      val message =
+        s"""Plugin ${plugin.getName} of version ${plugin.getVersion} is
+           |incompatible with Scala Plugin for IDEA 2017.3
+           |${hyperlink(DisableHaskForcePlugin)}
+           |${hyperlink(DisableScalaPlugin)}
+           |${hyperlink(Ignore)}
+           |""".stripMargin
+      showIncompatiblePluginNotification(plugin, message) {
+        case DisableHaskForcePlugin =>
+          disablePlugin(haskforceId)
+        case DisableScalaPlugin =>
+          disablePlugin(ScalaPluginVersionVerifier.scalaPluginId)
+        case Ignore =>
+        case _ =>
+      }
+    }
+
+  }
+
+  private def hyperlink(description: String): String =
+    s"""<p/><a href="$description">$description</a>"""
+
+  private def disablePlugin(id: String) = {
+    PluginManagerCore.disablePlugin(id)
+    PluginManagerConfigurable.showRestartDialog()
+  }
 }
