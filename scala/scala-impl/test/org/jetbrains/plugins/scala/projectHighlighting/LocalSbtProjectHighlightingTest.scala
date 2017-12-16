53e5141f0d114a75f8afb62cff403f44afa73154
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2017-12-16 17:54:49 +0300

    tests refactored to work with local sbt projects and java files, akka-samples test added #SCL-12994 fixed

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/LocalSbtProjectHighlightingTest.scala
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.scala.projectHighlighting
+
+import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.scala.HighlightingTests
+import org.jetbrains.plugins.scala.performance.ImportingProjectTestCase
+import org.jetbrains.plugins.scala.util.TestUtils
+import org.junit.experimental.categories.Category
+
+/**
+  * Nikolay.Tropin
+  * 14-Dec-17
+  */
+abstract class LocalSbtProjectHighlightingTest extends ImportingProjectTestCase with AllProjectHighlightingTest {
+  override def getProject: Project = myProject
+
+  override def rootDirPath = s"${TestUtils.getTestDataPath}/localProjects"
+
+  def testHighlighting(): Unit = doAllProjectHighlightingTest()
+
+  override def getProjectFixture = codeInsightFixture
+}
+
+@Category(Array(classOf[HighlightingTests]))
+class AkkaSamplesTest extends LocalSbtProjectHighlightingTest {
+  override def projectName = "akka-samples"
+}
