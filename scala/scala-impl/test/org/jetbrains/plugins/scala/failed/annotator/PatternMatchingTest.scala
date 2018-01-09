c6f1133c87f75aff47d6505fafa40635611deef1
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2018-01-10 00:08:03 +0300

    added test for SCL-13141, SCL-13151

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/annotator/PatternMatchingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/annotator/PatternMatchingTest.scala
@@ -28,4 +28,28 @@ class PatternMatchingTest extends ScalaLightCodeInsightFixtureTestAdapter {
       """.stripMargin
     checkTextHasNoErrors(text)
   }
+
+  def testSCL13151() = {
+    val text =
+      """
+        |class Term[A]
+        |class Number(val n: Int) extends Term[Int]
+        |object X {
+        |  def f[B](t: Term[B]): B = t match {
+        |    case y: Number => y.n // False error: Expression of type Int doesn't conform to expected type B
+        |  }
+        |}
+      """.stripMargin
+    checkTextHasNoErrors(text)
+  }
+
+  def testSCL13141() = {
+    val text =
+      """
+        |object X {
+        |  def f(x : List[_]): Any = x match { case z : List[a] => { val e : a = z.head; e } }
+        |}
+      """.stripMargin
+    checkTextHasNoErrors(text)
+  }
 }
