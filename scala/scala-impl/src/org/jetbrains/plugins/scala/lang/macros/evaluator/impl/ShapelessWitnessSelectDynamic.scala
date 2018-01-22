bd251210d7cecbe17230234f2981290b6b94ec8a
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2018-01-22 12:14:51 +0300

    better separation of logic after revew #SCL-13239

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/macros/evaluator/impl/ShapelessWitnessSelectDynamic.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/macros/evaluator/impl/ShapelessWitnessSelectDynamic.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.lang.macros.evaluator.impl
 
+import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.lang.macros.evaluator.{MacroContext, ScalaMacroTypeable}
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScInterpolatedStringLiteral, ScLiteral, ScStableCodeReferenceElement}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
@@ -13,32 +14,36 @@ import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
   */
 object ShapelessWitnessSelectDynamic extends ScalaMacroTypeable with ShapelessUtils {
 
-  override def checkMacro(macros: ScFunction, context: MacroContext): Option[ScType] = {
-    val ref = context.place match {
+  override def checkMacro(macros: ScFunction, context: MacroContext): Option[ScType] =
+    backtickedLiteralIn(context.place)
+      .flatMap(typeCarrierType(_, macros))
+
+  private def backtickedLiteralIn(e: PsiElement): Option[ScLiteral] = {
+    val ref = e match {
       case r: ScStableCodeReferenceElement => r
       case _ => return None
     }
     ref.refName match {
       case ScalaNamesUtil.isBacktickedName(literalText) =>
-        val literal = createExpressionWithContextFromText(literalText, ref.getContext, ref) match {
-          case _: ScInterpolatedStringLiteral => return None
-          case lit: ScLiteral => lit
-          case _ => return None
+        createExpressionWithContextFromText(literalText, ref.getContext, ref) match {
+          case _: ScInterpolatedStringLiteral => None
+          case lit: ScLiteral => Some(lit)
+          case _ => None
         }
-        //todo: replace with singleton literal type when supported
-        val typeOfLiteral = literal.`type`().getOrAny.canonicalText
-        val typeCarrierText =
-          s"""
-             |object `$literalText` {
-             |  type T = $typeOfLiteral
-             |  type Field[V] = $fqFieldType[$typeOfLiteral, V]
-             |  type ->>[V] = Field[V]
-             |}
-          """.stripMargin
-        val tempObject = createObjectWithContext(typeCarrierText, macros.getContext, macros)
-
-        tempObject.`type`().toOption
       case _ => None
     }
+  }
+
+  private def typeCarrierType(literal: ScLiteral, insertionPlace: PsiElement): Option[ScType] ={
+    //todo: replace with singleton literal type when supported
+    val literalType = literal.`type`().getOrAny.canonicalText
+    val text = s"""
+                  |object `${literal.getText}` {
+                  |  type T = $literalType
+                  |  type Field[V] = $fqFieldType[$literalType, V]
+                  |  type ->>[V] = Field[V]
+                  |}""".stripMargin
+    val typeCarrier = createObjectWithContext(text, insertionPlace.getContext, insertionPlace)
+    typeCarrier.`type`().toOption
   }
 }
