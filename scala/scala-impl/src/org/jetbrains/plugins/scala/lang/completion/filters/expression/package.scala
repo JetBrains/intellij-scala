b7bcbd3a36a5a78568ac92b952535d8a54724c74
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2018-01-26 11:47:42 +0300

    less duplication after review #SCL-13251

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/filters/expression/package.scala
@@ -0,0 +1,44 @@
+package org.jetbrains.plugins.scala.lang.completion.filters
+
+import com.intellij.psi.{PsiComment, PsiElement}
+import org.jetbrains.plugins.scala.extensions.PsiFileExt
+import org.jetbrains.plugins.scala.lang.completion.ScalaCompletionUtil.getLeafByOffset
+import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocComment
+
+/**
+  * Nikolay.Tropin
+  * 26-Jan-18
+  */
+package object expression {
+
+  def getPrevNotWhitespaceAndComment(index: Int, context: PsiElement): Int = {
+    if (index < 0) return 0
+    val file = context.getContainingFile
+    val fileChars = file.charSequence
+    var i = index
+    while (i > 0 && fileChars.charAt(i).isWhitespace) {
+      i = i - 1
+    }
+    getLeafByOffset(i, context) match {
+      case comment @ (_: PsiComment | _: ScDocComment) =>
+        getPrevNotWhitespaceAndComment(comment.getTextRange.getStartOffset - 1, context)
+      case _ => i
+    }
+  }
+
+  def getNextNotWhitespaceAndComment(index: Int, context: PsiElement): Int = {
+    val file = context.getContainingFile
+    if (index >= file.getTextLength - 1) return file.getTextLength - 2
+    val fileChars = file.charSequence
+
+    var i = index
+    while (i < fileChars.length - 1 && fileChars.charAt(i).isWhitespace) {
+      i = i + 1
+    }
+    getLeafByOffset(i, context) match {
+      case comment @ (_: PsiComment | _: ScDocComment) =>
+        getNextNotWhitespaceAndComment(comment.getTextRange.getEndOffset, context)
+      case _ => i
+    }
+  }
+}
