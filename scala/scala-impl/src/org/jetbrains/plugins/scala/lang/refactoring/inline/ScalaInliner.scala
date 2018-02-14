c9c774d0060a0162d18bd7e4122e0295b69ff4ce
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2018-02-14 14:47:35 +0300

    ScalaInliner: use `matchedParameters` method instead of custom implementation #SCL-8951

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/inline/ScalaInliner.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/inline/ScalaInliner.scala
@@ -8,101 +8,87 @@ import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.{PsiElement, PsiReference}
 import com.intellij.usageView.UsageInfo
 import com.intellij.util.containers.MultiMap
-import org.jetbrains.plugins.scala.extensions.{Parent, childOf}
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
+import org.jetbrains.plugins.scala.extensions.{Parent, TraversableExt, childOf}
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScInterpolatedStringLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScAssignStmt, ScExpression, ScMethodCall}
+import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.{createExpressionFromText, createTypeElementFromText}
 import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaRefactoringUtil.highlightOccurrences
-import org.jetbrains.plugins.scala.project.ProjectContext
 
 import scala.collection.JavaConverters._
 
 class ScalaInliner(replacementValue: String) extends InlineHandler.Inliner {
 
+  override def getConflicts(reference: PsiReference, referenced: PsiElement): MultiMap[PsiElement, String] = MultiMap.empty()
+
   override def inlineUsage(usage: UsageInfo, referenced: PsiElement): Unit = {
-    usage.getReference.getElement match {
-      case Parent(call: ScMethodCall) if call.argumentExpressions.isEmpty => replace(call)
-      case Parent(call: ScMethodCall) => replaceFunctionCall(call, referenced)
-      case e: ScExpression => replace(e)
-      case Parent(reference: ScTypeElement) => replace(reference)
-      case _ => ()
+    val newValue = usage.getReference.getElement match {
+      case Parent(call: ScMethodCall) if call.argumentExpressions.isEmpty =>
+        replace(call)
+      case Parent(call: ScMethodCall) =>
+        replaceFunctionCall(call, referenced)
+      case expr: ScExpression =>
+        replace(expr)
+      case Parent(typeElement: ScTypeElement) =>
+        replace(typeElement)
+      case _ => None
     }
+    newValue.foreach(postProcess)
   }
 
-  private def replaceFunctionCall(call: ScMethodCall, referenced: PsiElement): Unit = {
-    referenced.copy match {
+  private def replaceFunctionCall(call: ScMethodCall, referenced: PsiElement): Option[ScExpression] = {
+    referenced match {
       case function: ScFunctionDefinition =>
-        replaceReferencesInFunctionBody(call, function)
-        function.body.foreach { body =>
-          val newValue = call match {
-            case expression: ScExpression =>
-              expression.replaceExpression(body, removeParenthesis = true)
-            case _ => call.replace(body)
-          }
-
-          postProcess(newValue)
-        }
-      case _ => ()
+        val replacement = replacementForCall(call, function)
+        Some(call.replaceExpression(replacement, removeParenthesis = true))
+      case _ =>
+        None
     }
   }
 
-  private def replaceReferencesInFunctionBody(call: ScMethodCall, function: ScFunctionDefinition): Unit = {
-    val scope = new LocalSearchScope(function)
-    resolveParametersReplacements(call, function).foreach {
-      case (parameter, replacement) =>
-        val references = ReferencesSearch.search(parameter, scope).asScala
-        references.foreach(_.getElement.replace(replacement))
+  private def replacementForCall(call: ScMethodCall, function: ScFunctionDefinition): ScExpression = {
+    val funBodyCopy = function.body match {
+      case Some(body) =>
+        ScalaPsiElementFactory.createExpressionWithContextFromText(body.getText, function, body)
+      case _ => return call
     }
-  }
-
-  private def resolveParametersReplacements(call: ScMethodCall, function: ScFunctionDefinition) = {
-    val parameters = function.parameters
-    val paramsByName = parameters.map(parmeter => parmeter.name -> parmeter).toMap
-    val paramNameToParameterWithReplacement = (parameters zip call.argumentExpressions).map {
-      case (_, ScAssignStmt(argumentName, Some(argumentValue))) =>
-        paramsByName(argumentName.getText) -> argumentValue
-
-      case pair => pair
-    }.map {
-      case (parameter, replacement) =>
-        parameter.name -> (parameter, replacement)
-    }.toMap
-
-    val missingParametersWithDefaultValues =
-      parameters.filterNot(p => paramNameToParameterWithReplacement.keySet.contains(p.name))
-        .map(p => p -> (
-            p.getDefaultExpression match {
-              case Some(defaultValue) => defaultValue
-              case None => p
-            }
-          )
-        )
-
-    paramNameToParameterWithReplacement.values ++ missingParametersWithDefaultValues
-  }
-
-  override def getConflicts(reference: PsiReference, referenced: PsiElement): MultiMap[PsiElement, String] =
-    new com.intellij.util.containers.MultiMap[PsiElement, String]()
-
-  private def replace(replacement: ScalaPsiElement): PsiElement = {
-    implicit val projectContext: ProjectContext = replacement.projectContext
-    val newValue = replacement match {
-      case expression: ScExpression =>
-        val oldValue = expression match {
-          case _ childOf (_: ScInterpolatedStringLiteral) =>
-            s"{" + replacementValue + "}"
-          case _ =>
-            replacementValue
-        }
-        expression.replaceExpression(createExpressionFromText(oldValue), removeParenthesis = true)
-      case _: ScTypeElement =>
-        replacement.replace(createTypeElementFromText(replacementValue))
+    val paramToReplacement = call.matchedParameters.flatMap {
+      case (expr, p) => p.paramInCode.map((_, expr))
     }
 
-    postProcess(newValue)
+    val scope = new LocalSearchScope(funBodyCopy)
+    var result: ScExpression = funBodyCopy
+    for {
+      (parameter, replacement) <- paramToReplacement
+      reference <- ReferencesSearch.search(parameter, scope).asScala.filterBy[ScReferenceExpression]
+    } {
+      if (reference == funBodyCopy)
+        result = replacement
+      else
+        reference.replaceExpression(replacement, removeParenthesis = true)
+    }
+
+    result
+  }
+
+  private def replace(expr: ScExpression): Some[ScExpression] = {
+    import expr.projectContext
+
+    val oldValue = expr match {
+      case _ childOf (_: ScInterpolatedStringLiteral) =>
+        s"{" + replacementValue + "}"
+      case _ =>
+        replacementValue
+    }
+    Some(expr.replaceExpression(createExpressionFromText(oldValue), removeParenthesis = true))
+  }
+
+  private def replace(te: ScTypeElement): Some[PsiElement] = {
+    import te.projectContext
+
+    Some(te.replace(createTypeElementFromText(replacementValue)))
   }
 
   private def postProcess(newValue: PsiElement) = {
