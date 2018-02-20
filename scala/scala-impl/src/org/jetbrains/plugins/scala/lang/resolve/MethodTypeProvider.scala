b1dc391f3ba0a0785f9c83652ce0ba881620ed60
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-02-20 14:35:46 +0300

    Parameter name should not be empty #SCL-13356 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/resolve/MethodTypeProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/resolve/MethodTypeProvider.scala
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.scala.lang.resolve
 
 import com.intellij.psi._
-import org.jetbrains.plugins.scala.extensions.{PsiMethodExt, PsiParameterExt, PsiTypeExt}
+import org.jetbrains.plugins.scala.extensions.{PsiMethodExt, PsiTypeExt}
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScMethodLike, ScPrimaryConstructor}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScParameterClause, ScParameters}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFun, ScFunction}
@@ -154,12 +154,13 @@ object MethodTypeProvider {
     }
   }
 
-  private case class JavaMethodProvider(element: PsiMethod)(override implicit val scope: ElementScope) 
+  private case class JavaMethodProvider(element: PsiMethod)
+                                       (override implicit val scope: ElementScope)
     extends MethodTypeProvider[PsiMethod] {
     def typeParameters: Seq[PsiTypeParameter] = element.getTypeParameters
 
     def methodType(returnType: Option[ScType] = None): ScType = {
-      val retType: ScType = returnType.getOrElse(computeReturnType)
+      val retType = returnType.getOrElse(computeReturnType)
       ScMethodType(retType, parameters, isImplicit = false)
     }
 
@@ -170,17 +171,7 @@ object MethodTypeProvider {
 
     private def parameters: Seq[Parameter] = element match {
       case f: FakePsiMethod => f.params.toSeq
-      case _ => element.parameters.map(toParameter)
-    }
-
-    private def toParameter(psiParameter: PsiParameter) = {
-      val scType = psiParameter.paramType()
-      Parameter("", None, scType, scType,
-        isDefault = false,
-        isRepeated = psiParameter.isVarArgs,
-        isByName = false,
-        psiParameter.index,
-        Some(psiParameter))
+      case _ => element.parameters.map(Parameter(_))
     }
   }
 
