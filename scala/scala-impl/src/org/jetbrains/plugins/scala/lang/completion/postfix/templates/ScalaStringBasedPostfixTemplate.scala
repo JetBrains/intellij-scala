94f8756b1d67ae73c05d8c90467745103ac8c0bb
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2018-03-12 18:57:16 +0300

    usage of deprecated API fixed, it's going to be removed in master

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/postfix/templates/ScalaStringBasedPostfixTemplate.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/postfix/templates/ScalaStringBasedPostfixTemplate.scala
@@ -1,12 +1,15 @@
 package org.jetbrains.plugins.scala.lang.completion.postfix.templates
 
-import com.intellij.codeInsight.template.postfix.templates.{StringBasedPostfixTemplate, PostfixTemplateExpressionSelectorBase}
+import com.intellij.codeInsight.template.postfix.templates.{PostfixTemplateExpressionSelectorBase, StringBasedPostfixTemplate}
+import com.intellij.psi.PsiElement
 
 /**
   * @author Roman.Shein
   * @since 14.09.2015.
   */
 abstract class ScalaStringBasedPostfixTemplate(val name: String, val example: String,
-                                               val selector: PostfixTemplateExpressionSelectorBase) extends StringBasedPostfixTemplate(name, example, selector) {
-  override def shouldRemoveParent = false
+                                               val selector: PostfixTemplateExpressionSelectorBase)
+  extends StringBasedPostfixTemplate(name, example, selector, null) {
+
+  override def getElementToRemove(expr: PsiElement): PsiElement = expr
 }
