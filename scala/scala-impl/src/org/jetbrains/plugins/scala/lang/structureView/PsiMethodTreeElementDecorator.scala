e59f894c4bff3fc8da69ab8ce2d8b9cc742c4871
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-03-19 12:12:03 +0100

    PsiMethodTreeElementDecorator: precompile regex, #SCL-13403, #SCL-13473

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/structureView/PsiMethodTreeElementDecorator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/structureView/PsiMethodTreeElementDecorator.scala
@@ -4,25 +4,32 @@ import java.util.regex.Pattern
 
 import com.intellij.ide.structureView.impl.java.PsiMethodTreeElement
 import com.intellij.psi.PsiMethod
-import org.jetbrains.plugins.scala.lang.structureView.PsiMethodTreeElementDecorator.replacePrimitiveTypesIn
 
 /**
   * @author Pavel Fatin
   */
 private class PsiMethodTreeElementDecorator(method: PsiMethod, inherited: Boolean) extends PsiMethodTreeElement(method, inherited) {
   override def getPresentableText: String =
-    replacePrimitiveTypesIn(super.getPresentableText)
-      .replaceAll("(\\w+)\\[\\]", "Array[$1]")
-      .replaceAll("\\.\\.\\.", "*")
-      .replaceAll("\\<", "[")
-      .replaceAll("\\>", "]")
-      .replaceAll("\\?", "_")
-      .replaceAll("\\bextends\\b", "<:")
-      .replaceAll("\\bsuper\\b", ">:")
+    PsiMethodTreeElementDecorator.asScalaType(super.getPresentableText)
 }
 
 private object PsiMethodTreeElementDecorator {
   private val PrimitiveType = Pattern.compile("\\b(?:void|boolean|byte|char|short|int|long|float|double)\\b")
+
+  private val Transformations = Seq(
+    "(\\w+)\\[\\]" -> "Array[$1]",
+    "\\.\\.\\." -> "*",
+    "\\<" -> "[",
+    "\\>" -> "]",
+    "\\?" -> "_",
+    "\\bextends\\b" -> "<:",
+    "\\bsuper\\b" -> ">:"
+  ).map {
+    case (regex, replacement) => (Pattern.compile(regex), replacement)
+  }
+
+  private def asScalaType(javaType: String) =
+    applyTransformationsTo(replacePrimitiveTypesIn(javaType))
 
   private def replacePrimitiveTypesIn(text: String) = {
     val matcher = PrimitiveType.matcher(text)
@@ -40,5 +47,9 @@ private object PsiMethodTreeElementDecorator {
     matcher.appendTail(buffer)
 
     buffer.toString
+  }
+
+  private def applyTransformationsTo(javaType: String) = Transformations.foldLeft(javaType) {
+    case (acc, (regex, replacement)) => regex.matcher(acc).replaceAll(replacement)
   }
 }
