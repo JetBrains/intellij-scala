60cd025e77eb7584f8d566172dc34e9745ea5174
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-03-19 14:47:44 +0100

    PsiMethodTreeElementDecorator simplification, #SCL-13403

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/structureView/PsiMethodTreeElementDecorator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/structureView/PsiMethodTreeElementDecorator.scala
@@ -1,7 +1,5 @@
 package org.jetbrains.plugins.scala.lang.structureView
 
-import java.util.regex.Pattern
-
 import com.intellij.ide.structureView.impl.java.PsiMethodTreeElement
 import com.intellij.psi.PsiMethod
 
@@ -14,42 +12,18 @@ private class PsiMethodTreeElementDecorator(method: PsiMethod, inherited: Boolea
 }
 
 private object PsiMethodTreeElementDecorator {
-  private val PrimitiveType = Pattern.compile("\\b(?:void|boolean|byte|char|short|int|long|float|double)\\b")
-
   private val Transformations = Seq(
-    "(\\w+)\\[\\]" -> "Array[$1]",
-    "\\.\\.\\." -> "*",
-    "\\<" -> "[",
-    "\\>" -> "]",
-    "\\?" -> "_",
-    "\\bextends\\b" -> "<:",
-    "\\bsuper\\b" -> ">:"
-  ).map {
-    case (regex, replacement) => (Pattern.compile(regex), replacement)
-  }
+    "\\bboolean|byte|char|short|int|long|float|double\\b".r -> "\\u$0",
+    "\\bvoid\\b".r -> "Unit",
+    "(\\w+)\\[\\]".r -> "Array[$1]",
+    "\\.\\.\\.".r -> "*",
+    "\\<".r -> "[",
+    "\\>".r -> "]",
+    "\\?".r -> "_",
+    "\\bextends\\b".r -> "<:",
+    "\\bsuper\\b".r -> ">:")
 
-  private def asScalaType(javaType: String) =
-    applyTransformationsTo(replacePrimitiveTypesIn(javaType))
-
-  private def replacePrimitiveTypesIn(text: String) = {
-    val matcher = PrimitiveType.matcher(text)
-
-    val buffer = new StringBuffer()
-
-    while (matcher.find()) {
-      val replacement = matcher.group() match {
-        case "void" => "Unit"
-        case s => s.capitalize
-      }
-      matcher.appendReplacement(buffer, replacement)
-    }
-
-    matcher.appendTail(buffer)
-
-    buffer.toString
-  }
-
-  private def applyTransformationsTo(javaType: String) = Transformations.foldLeft(javaType) {
-    case (acc, (regex, replacement)) => regex.matcher(acc).replaceAll(replacement)
+  private def asScalaType(javaType: String) = Transformations.foldLeft(javaType) {
+    case (acc, (regex, replacement)) => regex.replaceAllIn(acc, replacement)
   }
 }
