bbf1f00944408684f2e43656319a85e6731684de
Sugak Andrey <sugak.andr3y@gmail.com>
2018-03-22 13:28:32 +0300

    Add a couple of test cases for ToggleTypeAnnotationIntention

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInsight/intention/types/ToggleTypeAnnotationIntentionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInsight/intention/types/ToggleTypeAnnotationIntentionTest.scala
@@ -28,4 +28,88 @@ class ToggleTypeAnnotationIntentionTest extends ScalaIntentionTestBase {
     "val v = Option.empty[String].to[Option]",
     "val v: Option[String] = Option.empty[String].to[Option]"
   )
+   
+  def testCompoundType(): Unit = doTest(
+    """
+      |val foo = new Runnable {
+      |  def helper(): Unit = ???
+      |
+      |  override def run(): Unit = ???
+      |}""".stripMargin,
+    """
+      |val foo: Runnable = new Runnable {
+      |  def helper(): Unit = ???
+      |
+      |  override def run(): Unit = ???
+      |}""".stripMargin
+  )
+  
+  def testCompoundTypeWithTypeMember(): Unit = doTest(
+    s"""
+       |trait Foo {
+       |  type X
+       |}
+       |
+       |val f${caretTag}oo = new Foo {
+       |  override type X = Int
+       |  
+       |  def helper(x: X): Unit = ???
+       |}
+     """.stripMargin,
+    s"""
+       |trait Foo {
+       |  type X
+       |}
+       |
+       |val f${caretTag}oo: Foo {
+       |  type X = Int
+       |} = new Foo {
+       |  override type X = Int
+       |
+       |  def helper(x: X): Unit = ???
+       |}
+     """.stripMargin
+  )
+  
+  def testInfixType(): Unit = doTest(
+    s"""
+       |trait A
+       |
+       |trait B
+       |
+       |def foo(): =:=[A, <:<[B, =:=[=:=[B, B], A]]] = ???
+       |val ba${caretTag}r = foo()
+     """.stripMargin,
+    s"""
+       |trait A
+       |
+       |trait B
+       |
+       |def foo(): =:=[A, <:<[B, =:=[=:=[B, B], A]]] = ???
+       |val ba${caretTag}r: A =:= (B <:< (B =:= B =:= A)) = foo()
+     """.stripMargin
+  )
+  
+  def testInfixDifferentAssociativity(): Unit = doTest(
+    s"""
+       |trait +[A, B]
+       |
+       |trait ::[A, B]
+       |
+       |trait A
+       |
+       |def foo(): ::[+[A, +[::[A, A], A]], +[A, ::[A, A]]] = ???
+       |val ba${caretTag}r = foo()
+     """.stripMargin,
+    s"""
+       |trait +[A, B]
+       |
+       |trait ::[A, B]
+       |
+       |trait A
+       |
+       |def foo(): ::[+[A, +[::[A, A], A]], +[A, ::[A, A]]] = ???
+       |val ba${caretTag}r: (A + ((A :: A) + A)) :: (A + (A :: A)) = foo()
+     """.stripMargin
+  )
 }
