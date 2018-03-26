f14951ed550682ac8ab2b5174fc3d7df86afb0c4
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2018-03-26 13:38:39 +0300

    FakePsiType is not a PsiPrimitiveType, PsiPrimitiveType constructor is going to be removed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/extractMethod/ScalaVariableData.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/extractMethod/ScalaVariableData.scala
@@ -1,13 +1,15 @@
 package org.jetbrains.plugins.scala.lang.refactoring.extractMethod
 
-import com.intellij.psi.{PsiAnnotation, PsiPrimitiveType}
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.{PsiAnnotation, PsiType, PsiTypeVisitor}
 import com.intellij.refactoring.util.VariableData
 import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypedDefinition
 import org.jetbrains.plugins.scala.lang.psi.fake.FakePsiParameter
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
 import org.jetbrains.plugins.scala.lang.psi.types.nonvalue.Parameter
-import org.jetbrains.plugins.scala.lang.refactoring._
+import org.jetbrains.plugins.scala.lang.refactoring.ScTypePresentationExt
+import org.jetbrains.plugins.scala.lang.refactoring.extractMethod.ScalaVariableData._
 
 /**
  * Nikolay.Tropin
@@ -16,16 +18,34 @@ import org.jetbrains.plugins.scala.lang.refactoring._
 class ScalaVariableData(val element: ScTypedDefinition,
                         val isInsideOfElements: Boolean,
                         val scType: ScType) 
-        extends {
-          private val parameter = Parameter(scType, isRepeated = false, index = -1)
-          private val fakeParam = new FakePsiParameter(element.getManager, ScalaLanguage.INSTANCE, parameter, element.name)
-
-        } with VariableData(fakeParam, new FakePsiType(scType)) {
+        extends VariableData(createFakeParameter(element, scType), new FakePsiType(scType)) {
 
   passAsParameter = true
-  name = fakeParam.getName
+  name = element.name
 }
 
-private class FakePsiType(val tp: ScType) extends PsiPrimitiveType("fakeForScala", PsiAnnotation.EMPTY_ARRAY) {
-  override def getPresentableText(boolean: Boolean): String = tp.codeText
+object ScalaVariableData {
+  private def createFakeParameter(element: ScTypedDefinition, scType: ScType): FakePsiParameter = {
+    val parameter = Parameter(scType, isRepeated = false, index = -1)
+    new FakePsiParameter(element.getManager, ScalaLanguage.INSTANCE, parameter, element.name)
+  }
+
+  private class FakePsiType(val tp: ScType) extends PsiType(PsiAnnotation.EMPTY_ARRAY) {
+
+    override def getPresentableText(boolean: Boolean): String = getPresentableText
+
+    override def getPresentableText: String = tp.codeText
+
+    override def getCanonicalText: String = tp.canonicalCodeText
+
+    override def isValid: Boolean = true
+
+    override def equalsToText(text: String): Boolean = false
+
+    override def accept[A](visitor: PsiTypeVisitor[A]): A = visitor.visitType(this)
+
+    override def getResolveScope: GlobalSearchScope = null
+
+    override def getSuperTypes: Array[PsiType] = Array.empty
+  }
 }
