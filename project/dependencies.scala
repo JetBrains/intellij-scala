b995625bf68b78997ee1f2038b5f48e008aa6ac9
Justin Kaeser <justin.kaeser@jetbrains.com>
2018-03-27 18:38:50 +0200

    update latest sbt version to 1.1.2

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -48,7 +48,7 @@ object Versions {
 
     val latest_0_12 = "0.12.4"
     val latest_0_13 = "0.13.17"
-    val latest_1_0 = "1.1.1"
+    val latest_1_0 = "1.1.2"
     val latest: String = latest_1_0
 
     def scalaVersion(v: String): String =
