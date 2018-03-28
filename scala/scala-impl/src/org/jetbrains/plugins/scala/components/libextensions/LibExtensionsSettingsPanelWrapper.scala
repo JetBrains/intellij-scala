28f89d4d99f2a07aba30bc168e027a53407af70f
Mikhail Mutcianko <mutcianko.m@gmail.com>
2018-03-28 17:39:14 +0300

    inject: add search pattern editing dialog

--- scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/LibExtensionsSettingsPanelWrapper.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/LibExtensionsSettingsPanelWrapper.scala
@@ -1,14 +1,19 @@
 package org.jetbrains.plugins.scala.components.libextensions
 
 import java.awt.BorderLayout
+import java.awt.event.ActionEvent
+import java.util
+import java.util.Collections
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.{DialogBuilder, InputValidatorEx, Messages}
 import com.intellij.ui._
 import com.intellij.ui.components.JBList
 import com.intellij.util.ui.{JBUI, UIUtil}
 import javax.swing._
+import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 
 class LibExtensionsSettingsPanelWrapper(private val rootPanel: JPanel, private val project: Project) {
 
@@ -31,8 +36,32 @@ class LibExtensionsSettingsPanelWrapper(private val rootPanel: JPanel, private v
     override def getElementAt(i: Int): Extension = myExtensions(i)
   }
 
+  class EditPatternsPanel(data: util.List[String]) extends AddEditDeleteListPanel[String]("Library extension search patterns", data) {
+    override protected def editSelectedItem(item: String): String = showEditDialog(item)
+    override def findItemToAdd(): String = showEditDialog("")
+    private def showEditDialog(initialValue: String) =
+      Messages.showInputDialog(this, "", "Enter search pattern", Messages.getQuestionIcon, initialValue, new InputValidatorEx {
+        override def getErrorText(inputString: String): String = if (!checkInput(inputString)) "org, module and version must be separated with %" else null
+        override def checkInput(inputString: String): Boolean = inputString.matches("^.*%.*%.*$")
+        override def canClose(inputString: String) = true
+      })
+    def getPatterns: util.Enumeration[String] = myListModel.elements()
+  }
+
   def build(): Unit = {
     import com.intellij.util.ui.UI
+
+    def showPatternManageDialog(e: ActionEvent): Unit = {
+      val builder = new DialogBuilder(project)
+      val settings = ScalaProjectSettings.getInstance(project)
+      val listView = new EditPatternsPanel(settings.getLextSearchPatterns)
+      listView.setPreferredSize(JBUI.size(350, 135))
+      builder.setCenterPanel(UI.PanelFactory.panel(listView)
+        .withComment("Use SBT dependency format to define the pattern")
+        .createPanel())
+      builder.show()
+      settings.setLextSearchPatterns(Collections.list(listView.getPatterns))
+    }
 
     rootPanel.setLayout(new BorderLayout())
     UIUtil.addBorder(rootPanel, JBUI.Borders.empty(10))
@@ -46,7 +75,9 @@ class LibExtensionsSettingsPanelWrapper(private val rootPanel: JPanel, private v
 
     val settingsPanel = new JPanel(new BorderLayout())
     settingsPanel.add(checkBoxes, BorderLayout.CENTER)
-    settingsPanel.add(new JButton("Manage Search Patterns"), BorderLayout.LINE_END)
+    val button = new JButton("Manage Search Patterns")
+    button.addActionListener(showPatternManageDialog)
+    settingsPanel.add(button, BorderLayout.LINE_END)
 
     rootPanel.add(settingsPanel, BorderLayout.PAGE_START)
 
@@ -75,9 +106,6 @@ class LibExtensionsSettingsPanelWrapper(private val rootPanel: JPanel, private v
       .disableRemoveAction()
       .addExtraAction(pauseAction)
       .createPanel()
-
-
-
 
     val listsPane = new JBSplitter(true, 0.6f)
     listsPane.setFirstComponent(librariesPane)
