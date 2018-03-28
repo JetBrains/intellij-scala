7e355dbc5187ba66d573f57305843e61318d3448
Mikhail Mutcianko <mutcianko.m@gmail.com>
2018-03-28 17:43:47 +0300

    inject: add SBT refresh finished trigger

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/LibraryExtensionsSBTListener.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala.components.libextensions
+
+import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListenerAdapter}
+import com.intellij.openapi.module.ModuleManager
+import org.jetbrains.sbt.project.module.SbtModule
+import org.jetbrains.sbt.resolvers.SbtResolver
+
+class LibraryExtensionsSBTListener extends ExternalSystemTaskNotificationListenerAdapter {
+
+  private var allProjectResolvers: Set[SbtResolver] = Set.empty
+
+  override def onSuccess(id: ExternalSystemTaskId): Unit = {
+    val project = id.findProject()
+    if (project == null || project.isDisposed) return
+    ModuleManager.getInstance(project).getModules.foreach {
+      allProjectResolvers ++= SbtModule.getResolversFrom(_)
+    }
+
+    LibraryExtensionsManager.getInstance(project).searchExtensions(allProjectResolvers)
+  }
+}
