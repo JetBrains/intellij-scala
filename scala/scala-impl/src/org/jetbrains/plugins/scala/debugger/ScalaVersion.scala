e5b85a3967a40960f69c22db2500ed62281e657f
Mikhail Mutcianko <mutcianko.m@gmail.com>
2018-04-05 16:22:11 +0300

    inject: update scalaVersions

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/ScalaVersion.scala
@@ -28,3 +28,7 @@ case object Scala_2_12 extends ScalaVersion {
   override final val minor: String = "2.12.3"
 }
 
+case object Scala_2_13 extends ScalaVersion {
+  override final val major: String = "2.13"
+  override final val minor: String = "2.13.0-M3"
+}
