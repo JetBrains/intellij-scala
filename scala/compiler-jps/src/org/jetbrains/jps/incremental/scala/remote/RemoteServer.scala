fe4796ef8bbb61e6d86ecb4f5c397a3224fb5084
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-05-04 12:57:21 +0200

    RemoteServer: take a possible "fallback local mode" into account, #SCL-13584
    (cherry picked from commit e1af0a8)

--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/remote/RemoteServer.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/remote/RemoteServer.scala
@@ -13,7 +13,7 @@ import org.jetbrains.jps.incremental.scala.remote.RemoteServer._
  */
 class RemoteServer(val address: InetAddress, val port: Int) extends Server with RemoteResourceOwner {
   def compile(sbtData: SbtData, compilerData: CompilerData, compilationData: CompilationData, client: Client): ExitCode = {
-    val token = readStringFrom(tokenPathFor(port))
+    val token = readStringFrom(tokenPathFor(port)).getOrElse("NO_TOKEN")
 
     val arguments = Arguments(token, sbtData, compilerData, compilationData, Seq.empty).asStrings
 
@@ -38,7 +38,8 @@ class RemoteServer(val address: InetAddress, val port: Int) extends Server with 
 }
 
 private object RemoteServer {
-  private def readStringFrom(path: Path) = new String(Files.readAllBytes(path))
+  private def readStringFrom(path: Path): Option[String] =
+    if (path.toFile.exists) Some(new String(Files.readAllBytes(path))) else None
 
   private def tokenPathFor(port: Int) =
     Paths.get(System.getProperty("user.home"), ".idea-build", "tokens", port.toString)
