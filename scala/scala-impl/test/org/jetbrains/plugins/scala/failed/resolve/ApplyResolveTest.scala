87f87230ce205dc3390895e8468a47c066310408
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2018-05-11 11:25:42 +0300

    added test for SCL-13705

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/ApplyResolveTest.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.failed.resolve
+
+import org.jetbrains.plugins.scala.PerfCycleTests
+import org.junit.experimental.categories.Category
+
+/**
+  * Created by Anton Yalyshev on 15/04/16.
+  */
+@Category(Array(classOf[PerfCycleTests]))
+class ApplyResolveTest extends FailedResolveCaretTestBase {
+
+  def testSCL13705(): Unit = {
+    doResolveCaretTest(
+      """
+        |case class Test(c: String)
+        |
+        |trait Factory {
+        |  def apply(c: String): String = c
+        |}
+        |
+        |object Test extends Factory {
+        |  Test("").<caret>toLowerCase
+        |}
+      """.stripMargin)
+  }
+
+}
