f8accd2d22f6489ca31ecfa66e70b7cb8277993b
Nikolay.Tropin <Nikolay.Tropin@jetbrains.com>
2018-05-25 14:50:02 +0300

    Unnecessary parentheses inspection: regression fixed, correct precedence for typed patterns #SCL-13784 fixed #SCL-13785 fixed #SCL-13787 fixed #SCL-13752 fixed #SCL-13739 fixed #SCL-13745 fixed
    (cherry picked from commit 3a6b9ca)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/ParenthesizedElement.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/ParenthesizedElement.scala
@@ -4,8 +4,9 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.lang.parser.util.ParserUtils
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns._
 import org.jetbrains.plugins.scala.lang.psi.api.base.types._
-import org.jetbrains.plugins.scala.lang.psi.api.base.{ScInfixElement, ScParenthesizedElement}
+import org.jetbrains.plugins.scala.lang.psi.api.base.{ScConstructor, ScInfixElement, ScParenthesizedElement}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScParenthesisedExpr, ScSugarCallExpr}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScClassParents
 import org.jetbrains.plugins.scala.lang.psi.{ScalaPsiElement, ScalaPsiUtil}
 
 import scala.annotation.tailrec
@@ -28,11 +29,11 @@ object ParenthesizedElement {
       * ie they clarify precedence between operators that have different precedences.
       */
     def isParenthesisClarifying: Boolean = parenthesized match {
-      case ParentAndInner(_: ScSugarCallExpr, _: ScSugarCallExpr)                            => true
-      case _: ScExpression                                                                   => false
-      case ParentAndInner(_: ScCompositePattern | _: ScNamingPattern | _: ScTuplePattern, _) => false
-      case ParentAndInner(p, c) if !isIndivisible(c) && getPrecedence(p) != getPrecedence(c) => true
-      case _                                                                                 => false
+      case SameKindParentAndInner(_: ScSugarCallExpr, _: ScSugarCallExpr)                            => true
+      case _: ScExpression                                                                           => false
+      case SameKindParentAndInner(_: ScCompositePattern | _: ScNamingPattern | _: ScTuplePattern, _) => false
+      case SameKindParentAndInner(p, c) if !isIndivisible(c) && getPrecedence(p) != getPrecedence(c) => true
+      case _                                                                                         => false
     }
 
     def getTextOfStripped(ignoreClarifying: Boolean = false): String = {
@@ -62,45 +63,46 @@ object ParenthesizedElement {
       }
     }
 
+    def isParenthesisRedundant: Boolean = !isParenthesisNeeded
+
     /** Returns true if the parentheses represented by this node cannot be stripped
       * without changing the semantics of the AST.
       */
     def isParenthesisNeeded: Boolean = {
       parenthesized match {
-        case expr@ScParenthesisedExpr(inner)                                                   => ScalaPsiUtil.needParentheses(expr, inner)
-        case _ if hasHigherPrecedence                                                          => false
-        case ParentAndInner(_: ScInfixElement, _: ScInfixElement) if !haveCorrectAssociativity => true
-        case _ if isFunctionTypeSingleParam                                                    => true
-        case _                                                                                 => false
+        case expr@ScParenthesisedExpr(inner)               => ScalaPsiUtil.needParentheses(expr, inner)
+        case SameKindParentAndInner(parent, inner)         => !parenthesesRedundant(parent, inner)
+        case ChildOf(_: ScConstructor | _: ScClassParents) => true
+        case _                                             => false
       }
     }
 
-    //shouldn't be called on expression
-    def hasHigherPrecedence: Boolean = parenthesized match {
-      case ParentAndInner(p, i) if isIndivisible(i)                    => true
-      case ParentAndInner(p, i) if getPrecedence(i) < getPrecedence(p) => true
-      case ParentAndInner(p, i) if getPrecedence(i) > getPrecedence(p) => true
+    def isFunctionTypeSingleParam: Boolean = parenthesized match {
+      case SameKindParentAndInner(ScFunctionalTypeElement(`parenthesized`, _), _) => true
       case _ => false
+    }
+
+    //shouldn't be called on expression
+    private def parenthesesRedundant[Kind <: ScParenthesizedElement#Kind](parent: Kind, inner: Kind): Boolean = {
+      isIndivisible(inner) ||
+        getPrecedence(inner) < getPrecedence(parent) ||
+        getPrecedence(inner) == getPrecedence(parent) && innerFirstAssociativity(parent, inner) ||
+        isFunctionTypeSingleParam
     }
 
     /**Infix chain with same precedence:
       * - If the two operators have different associativities, then the parentheses are required
       * - If they have the same associativity, then right- or left- associativity applies depending on the operator
       * */
-    private def haveCorrectAssociativity: Boolean = parenthesized match {
-      case ParentAndInner(p: ScInfixElement, c: ScInfixElement) if p.isLeftAssoc != c.isLeftAssoc => false
+    private def innerFirstAssociativity(parent: PsiElement, inner: PsiElement): Boolean = (parent, inner) match {
+      case (p: ScInfixElement, c: ScInfixElement) if p.isLeftAssoc != c.isLeftAssoc        => false
 
-      case ParentAndInner(ifx @ ScInfixElement(_, _, Some(`parenthesized`)), _: ScInfixElement) => ifx.isRightAssoc
-      case ParentAndInner(ifx @ ScInfixElement(`parenthesized`, _, _), _: ScInfixElement)       => ifx.isLeftAssoc
-      case _ => false
+      case (ifx@ScInfixElement(_, _, Some(`parenthesized`)), _: ScInfixElement)            => ifx.isRightAssoc
+      case (ifx@ScInfixElement(`parenthesized`, _, _), _: ScInfixElement)                  => ifx.isLeftAssoc
+
+      case (ScFunctionalTypeElement(_, Some(`parenthesized`)), _: ScFunctionalTypeElement) => true
+      case _                                                                               => false
     }
-
-    def isFunctionTypeSingleParam: Boolean = parenthesized.sameTreeParent match {
-      case Some(ScFunctionalTypeElement(`parenthesized`, _)) => true
-      case _ => false
-    }
-
-    def isParenthesisRedundant: Boolean = !isParenthesisNeeded
 
     /** Gets the precedence of the tree member. Lower int value is applied first (higher precedence).
       * The highest precedence is 0. Nodes with precedence 0 are indivisible.
@@ -132,7 +134,8 @@ object ParenthesizedElement {
   }
 
   private def patternPrecedence(pattern: ScPattern): Int = pattern match {
-    case _: ScCompositePattern       => 12
+    case _: ScCompositePattern       => 13
+    case _: ScTypedPattern           => 12
     case _: ScNamingPattern          => 11
     case ScInfixPattern(_, ifxOp, _) => 1 + ParserUtils.priority(ifxOp.getText) // varies from 1 to 10
     case _                           => 0
@@ -145,7 +148,7 @@ object ParenthesizedElement {
     case _                             => node
   }
 
-  private object ParentAndInner {
+  private object SameKindParentAndInner {
     def unapply(p: ScParenthesizedElement): Option[(p.Kind, p.Kind)] =
       for (parent <- p.sameTreeParent; inner <- p.innerElement) yield (parent, inner)
   }
