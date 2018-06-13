1ac3303a71fed9de6adc6b310f58e3fdf6c0fcf1
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-06-13 17:02:56 +0300

    restrict Move members refactoring to proper members of objects only #SCL-13305

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/move/members/ScalaMoveMembersHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/move/members/ScalaMoveMembersHandler.scala
@@ -3,31 +3,33 @@ package org.jetbrains.plugins.scala.lang.refactoring.move.members
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.{PsiElement, PsiReference}
 import com.intellij.refactoring.move.MoveHandlerDelegate
-import org.jetbrains.plugins.scala.ScalaLanguage
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScObject}
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScClassParameter
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScObject, ScTypeDefinition}
+import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaRefactoringUtil
 
 class ScalaMoveMembersHandler extends MoveHandlerDelegate {
 
   override def tryToMove(element: PsiElement, project: Project, dataContext: DataContext, reference: PsiReference, editor: Editor): Boolean = {
-    if (!element.getLanguage.equals(ScalaLanguage.INSTANCE)) {
-      false
-    } else {
-      element match {
-        case _: ScObject => false
-        case el: ScMember  =>
-          Option(PsiTreeUtil.getParentOfType(el, classOf[ScObject])) match {
-            case Some(scObj) =>
-              val dialog = new ScalaMoveMembersDialog(project, true, scObj, el)
-              dialog.show()
-              true
-            case None => false
-          }
-        case _ => false
-      }
+    element match {
+      case _: ScTypeDefinition |
+           _: ScClassParameter => false
+      case el: ScMember =>
+        el.containingClass match {
+          case scObj: ScObject =>
+            val dialog = new ScalaMoveMembersDialog(project, true, scObj, el)
+            dialog.show()
+          case _ =>
+            val message = ScalaBundle.message("move.members.supported.only.objects")
+            val refactoringName = "Move members"
+            ScalaRefactoringUtil.showErrorHint(message, refactoringName, null)(project, editor)
+        }
+        true
+      case _ => false
     }
+
   }
 
 }
