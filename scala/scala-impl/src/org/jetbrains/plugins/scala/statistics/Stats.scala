bc770675162d3ce43e8770aea977114d96d2de82
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-06-14 15:23:09 +0300

    don't use deprecated API, it may be removed at any moment

--- scala/scala-impl/src/org/jetbrains/plugins/scala/statistics/Stats.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/statistics/Stats.scala
@@ -1,15 +1,9 @@
 package org.jetbrains.plugins.scala.statistics
 
-import com.intellij.internal.statistic.UsageTrigger
-import com.intellij.openapi.components.ServiceManager
-
 object Stats {
-  private def enabled: Boolean = ServiceManager.getService(classOf[UsageTrigger]) != null
+  private def enabled: Boolean = false
 
   def trigger(feature: FeatureKey): Unit = {
-    if (enabled) {
-      UsageTrigger.trigger(feature.s)
-    }
   }
 
   def trigger(condition: Boolean, feature: FeatureKey): Unit = {
