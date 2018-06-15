e54ac3d41e95c7047fc946c1bca8824fd180448a
Mikhail Mutcianko <mutcianko.m@gmail.com>
2018-06-15 13:01:06 +0300

    inject: disable caching of extension points, fix project leaks
    also it was useless anyway and introduced more overhead than plain
    Map.get()

--- scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/DynamicExtensionPoint.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/DynamicExtensionPoint.scala
@@ -1,13 +1,11 @@
 package org.jetbrains.plugins.scala.components.libextensions
 
-import org.jetbrains.plugins.scala.macroAnnotations.Cached
 import org.jetbrains.plugins.scala.project.ProjectContext
 
 import scala.reflect.ClassTag
 
 class DynamicExtensionPoint[T](implicit val tag: ClassTag[T]) {
 
-  @Cached(LibraryExtensionsManager.MOD_TRACKER, null)
   def getExtensions(implicit context: ProjectContext): Seq[T] = {
     LibraryExtensionsManager.getInstance(context.project).getExtensions(tag.runtimeClass).asInstanceOf[Seq[T]]
   }
