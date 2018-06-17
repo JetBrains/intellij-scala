c140d7d775ea81d209fbf8dd8daf2073344a6e2d
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-06-17 19:44:15 +0300

    don't trim backticks in getRangeInElement, they are part of identifier; fixes test after change in TargetElementUtil

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/base/ScReferenceElement.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/base/ScReferenceElement.scala
@@ -64,10 +64,7 @@ trait ScReferenceElement extends ScalaPsiElement with PsiPolyVariantReference {
   def getRangeInElement: TextRange = {
     val start = nameId.getTextRange.getStartOffset - getTextRange.getStartOffset
     val len = getTextLength
-    if (isBackQuoted && patternNeedBackticks(refName.drop(1).dropRight(1)))
-      new TextRange(start + 1, len - 1)
-    else
-      new TextRange(start, len)
+    new TextRange(start, len)
   }
 
   def getCanonicalText: String = {
