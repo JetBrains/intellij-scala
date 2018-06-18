58d21c81e2c5d824208d76294a2294b503803015
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-06-18 12:23:10 +0300

    DynamicExtensionPoint: use openapi Project instead of ProjectContext

--- scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/DynamicExtensionPoint.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/DynamicExtensionPoint.scala
@@ -1,14 +1,14 @@
 package org.jetbrains.plugins.scala.components.libextensions
 
-import org.jetbrains.plugins.scala.project.ProjectContext
+import com.intellij.openapi.project.Project
 
 import scala.reflect.ClassTag
 
 class DynamicExtensionPoint[T](implicit val tag: ClassTag[T]) {
 
-  def getExtensions(implicit context: ProjectContext): Seq[T] = {
-    LibraryExtensionsManager.getInstance(context.project).getExtensions(tag.runtimeClass).asInstanceOf[Seq[T]]
+  def getExtensions(implicit project: Project): Seq[T] = {
+    LibraryExtensionsManager.getInstance(project).getExtensions(tag.runtimeClass).asInstanceOf[Seq[T]]
   }
 
-  def getExtension(implicit project: ProjectContext): T = getExtensions.head
+  def getExtension(implicit project: Project): T = getExtensions.head
 }
