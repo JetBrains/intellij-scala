a76ac2d7937fac8233a097a904d942ade55e3675
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-06-20 12:54:49 +0300

    run ImplicitHintsPass after annotator, it causes a race condition otherwise #SCL-13914 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsPassFactory.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsPassFactory.scala
@@ -1,6 +1,6 @@
 package org.jetbrains.plugins.scala.codeInsight.implicits
 
-import com.intellij.codeHighlighting.{TextEditorHighlightingPass, TextEditorHighlightingPassFactory, TextEditorHighlightingPassRegistrar}
+import com.intellij.codeHighlighting.{Pass, TextEditorHighlightingPass, TextEditorHighlightingPassFactory, TextEditorHighlightingPassRegistrar}
 import com.intellij.openapi.components.AbstractProjectComponent
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
@@ -10,7 +10,9 @@ import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 class ImplicitHintsPassFactory(project: Project, registrar: TextEditorHighlightingPassRegistrar)
   extends AbstractProjectComponent(project) with TextEditorHighlightingPassFactory {
 
-  registrar.registerTextEditorHighlightingPass(this, null, null, false, -1)
+  private val runAfterAnnotator = Array(Pass.UPDATE_ALL)
+
+  registrar.registerTextEditorHighlightingPass(this, runAfterAnnotator, null, false, -1)
 
   override def createHighlightingPass(file: PsiFile, editor: Editor): TextEditorHighlightingPass = file match {
     case file: ScalaFile if !ImplicitHints.isUpToDate(editor, file) => new ImplicitHintsPass(editor, file)
