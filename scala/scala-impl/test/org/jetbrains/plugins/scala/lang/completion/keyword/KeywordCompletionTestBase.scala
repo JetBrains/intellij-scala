8b91f4ad2c72aaa2fcef21b44ef153be7d384b5a
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-07-03 17:39:55 +0300

    another way to filter keywords #SCL-5540

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/completion/keyword/KeywordCompletionTestBase.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/completion/keyword/KeywordCompletionTestBase.scala
@@ -1,58 +1,71 @@
-package org.jetbrains.plugins.scala.lang.completion.keyword
+package org.jetbrains.plugins.scala
+package lang
+package completion
+package keyword
 
 import java.io.File
 
 import com.intellij.codeInsight.completion.{CodeCompletionHandlerBase, CompletionType}
+import com.intellij.codeInsight.lookup.LookupManager
 import com.intellij.codeInsight.lookup.impl.LookupImpl
-import com.intellij.codeInsight.lookup.{LookupElement, LookupManager}
 import com.intellij.openapi.fileEditor.{FileEditorManager, OpenFileDescriptor}
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.{CharsetToolkit, LocalFileSystem}
 import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
+import org.jetbrains.plugins.scala.lang.completion.lookups.ScalaLightKeyword
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import org.jetbrains.plugins.scala.lang.refactoring.ScalaNamesValidator.isKeyword
+import org.junit.Assert._
+
+import scala.collection.JavaConverters
 
 /**
  * @author Alexander Podkhalyuzin
  */
 
 abstract class KeywordCompletionTestBase extends ScalaLightPlatformCodeInsightTestCaseAdapter {
-  private val caretMarker = "/*caret*/"
 
   def folderPath: String = baseRootPath() + "keywordCompletion/"
 
   protected def doTest() {
-    import org.junit.Assert._
-
     val filePath = folderPath + getTestName(false) + ".scala"
     val file = LocalFileSystem.getInstance.findFileByPath(filePath.replace(File.separatorChar, '/'))
-    assert(file != null, "file " + filePath + " not found")
+    assertNotNull(s"file $filePath not found", file)
+
     val fileText = StringUtil.convertLineSeparators(FileUtil.loadFile(new File(file.getCanonicalPath), CharsetToolkit.UTF8))
     configureFromFileTextAdapter(getTestName(false) + ".scala", fileText)
+
     val scalaFile = getFileAdapter.asInstanceOf[ScalaFile]
-    val offset = fileText.indexOf(caretMarker)
-    assert(offset != -1, "Not specified end marker in test case. Use /*caret*/ in scala file for this.")
-    val fileEditorManager = FileEditorManager.getInstance(getProjectAdapter)
-    val editor = fileEditorManager.openTextEditor(new OpenFileDescriptor(getProjectAdapter, getVFileAdapter, offset), false)
-    val myType = CompletionType.BASIC
-    new CodeCompletionHandlerBase(myType, false, false, true).invokeCompletion(getProjectAdapter, editor)
-    val lookup: LookupImpl = LookupManager.getActiveLookup(editor).asInstanceOf[LookupImpl]
-    val items: Array[String] =
-      if (lookup == null) Array.empty
-      else lookup.getItems.toArray(LookupElement.EMPTY_ARRAY).map(_.getLookupString)
+    val offset = fileText.indexOf("/*caret*/")
+    assertNotEquals("Not specified end marker in test case. Use /*caret*/ in scala file for this.", offset, -1)
 
-    val res = items.filter(isKeyword(_)).sorted.mkString("\n")
+    val project = getProjectAdapter
+    val editor = FileEditorManager.getInstance(project)
+      .openTextEditor(new OpenFileDescriptor(project, getVFileAdapter, offset), false)
+    new CodeCompletionHandlerBase(CompletionType.BASIC, false, false, true)
+      .invokeCompletion(project, editor)
 
-    val lastPsi = scalaFile.findElementAt(scalaFile.getText.length - 1)
-    val text = lastPsi.getText
-    val output = lastPsi.getNode.getElementType match {
-      case ScalaTokenTypes.tLINE_COMMENT => text.substring(2).trim
-      case ScalaTokenTypes.tBLOCK_COMMENT | ScalaTokenTypes.tDOC_COMMENT =>
-        text.substring(2, text.length - 2).trim
-      case _ => assertTrue("Test result must be in last comment statement.", false)
+    val items = LookupManager.getActiveLookup(editor) match {
+      case impl: LookupImpl =>
+        import JavaConverters._
+        impl.getItems.asScala.collect {
+          case item if item.getObject.isInstanceOf[ScalaLightKeyword] => item.getLookupString
+        }
+      case _ => Seq.empty
     }
-    assertEquals(output, res.trim)
+
+    val actual = items.sorted.mkString("\n")
+
+    val lastPsi = scalaFile.findElementAt(scalaFile.getTextLength - 1)
+    val delta = lastPsi.getNode.getElementType match {
+      case ScalaTokenTypes.tLINE_COMMENT => 0
+      case ScalaTokenTypes.tBLOCK_COMMENT | ScalaTokenTypes.tDOC_COMMENT => 2
+      case _ => -1
+    }
+    assertNotEquals("Test result must be in last comment statement.", delta, -1)
+
+    val text = lastPsi.getText
+    assertEquals(text.substring(2, text.length - delta).trim, actual.trim)
   }
 }
