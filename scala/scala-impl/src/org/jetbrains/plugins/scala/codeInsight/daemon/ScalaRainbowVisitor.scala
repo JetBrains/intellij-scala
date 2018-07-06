9fcaa7c4e2eb10ab89213964e7c1e5d35928bf0c
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-07-06 12:54:59 +0300

    smooth refactoring #SCL-13738

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/daemon/ScalaRainbowVisitor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/daemon/ScalaRainbowVisitor.scala
@@ -12,50 +12,50 @@ import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScPatternList
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScFunctionExpr, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.statements._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScClassParameter, ScParameter}
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScValue, ScValueOrVariable, ScVariable}
 import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocTagValue
 
-class ScalaRainbowVisitor extends RainbowVisitor {
+final class ScalaRainbowVisitor extends RainbowVisitor {
 
-  override final def suitableForFile(file: PsiFile): Boolean = file match {
+  import ScalaRainbowVisitor._
+
+  override def suitableForFile(file: PsiFile): Boolean = file match {
     case _: ScalaFile => true
     case _ => false
   }
 
-  override final def visit(element: PsiElement): Unit = element match {
+  override def visit(element: PsiElement): Unit = element match {
     case parameter: ScParameter =>
       addInfo(parameter, parameter.nameId)
-    case element: ScValueOrVariable =>
-      addInfo(element, element.declaredElements.map(_.nameId): _*)
+    case valueOrVariable: ScValueOrVariable =>
+      addInfo(valueOrVariable, valueOrVariable.declaredElements.map(_.nameId): _*)
     case reference@ScReferenceExpression(parameter: ScParameter) =>
       addReferenceInfo(parameter, reference)
-    case reference@ScReferenceExpression((_: ScReferencePattern) childOf ((_: ScPatternList) childOf (element: ScValueOrVariable))) =>
-      addReferenceInfo(element, reference)
+    case reference@ScReferenceExpression((_: ScReferencePattern) childOf ((_: ScPatternList) childOf (valueOrVariable: ScValueOrVariable))) =>
+      addReferenceInfo(valueOrVariable, reference)
     case docTag: ScDocTagValue =>
       addInfo(docTag, docTag)
     case _ =>
   }
 
-  override final def clone(): ScalaRainbowVisitor = new ScalaRainbowVisitor
+  override def clone(): ScalaRainbowVisitor = new ScalaRainbowVisitor
 
   private def addReferenceInfo(element: PsiElement, expression: ScReferenceExpression): Unit =
     addInfo(element, expression.nameId)
 
-  private def addInfo(element: PsiElement, rainbowElements: PsiElement*): Unit = {
-    import PsiTreeUtil.getContextOfType
-    import ScalaRainbowVisitor.ColorKey
-
-    for {
-      ColorKey(colorKey) <- Some(element)
-      context <- Option(getContextOfType(element, true, classOf[ScFunction], classOf[ScFunctionExpr]))
-      rainbowElement <- rainbowElements
-      info = getInfo(context, rainbowElement, rainbowElement.getText, colorKey)
-    } addInfo(info)
-  }
+  private def addInfo(element: PsiElement, rainbowElements: PsiElement*): Unit = for {
+    ColorKey(colorKey) <- Some(element)
+    context <- functionContext(element)
+    rainbowElement <- rainbowElements
+    info = getInfo(context, rainbowElement, rainbowElement.getText, colorKey)
+  } addInfo(info)
 }
 
 private object ScalaRainbowVisitor {
+
+  def functionContext(element: PsiElement) =
+    Option(PsiTreeUtil.getContextOfType(element, classOf[ScFunction], classOf[ScFunctionExpr]))
 
   object ColorKey {
 
@@ -74,4 +74,5 @@ private object ScalaRainbowVisitor {
       case _ => None
     }
   }
+
 }
