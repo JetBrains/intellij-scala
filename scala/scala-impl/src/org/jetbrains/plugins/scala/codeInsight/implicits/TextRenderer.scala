671fd562170cb7ef520a7256980d13be6b5ed089
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-07-06 13:13:46 +0200

    MouseHandler: more reliable Text discovery, #SCL-13994 fixed
    Handle cases with preceding hint(s).

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/TextRenderer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/TextRenderer.scala
@@ -131,8 +131,10 @@ private class TextRenderer(private var parts: Seq[Text], menu: Option[String])
     val m = getMargin(editor)
     val p = getPadding(editor)
     val fontMetrics = getFontMetrics0(editor).getMetrics
-    val widths = parts.map(it => fontMetrics.stringWidth(it.string)).scanLeft(m.left + p.left)(_ + _)
-    widths.find(_ >= x).map(widths.indexOf).flatMap(i => parts.lift(i - 1)).orElse(parts.headOption)
+    val xs = parts.map(it => fontMetrics.stringWidth(it.string)).scanLeft(m.left + p.left)(_ + _)
+    parts.zip(xs.zip(xs.tail)).collectFirst {
+      case (text, (start, end)) if start <= x && x <= end => text
+    }
   }
 
   def replace(text: Text, replacement: Seq[Text]): Unit = {
