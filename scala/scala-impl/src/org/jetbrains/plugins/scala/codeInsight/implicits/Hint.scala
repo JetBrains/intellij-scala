e0c2afadaf02da26ffa6ccdbdbdcad222d074817
Pavel Fatin <pavel.fatin@jetbrains.com>
2018-07-12 22:44:27 +0200

    Implicit hints: set correct order of inlay hints with the same offset, #SCL-14061 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/Hint.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/implicits/Hint.scala
@@ -1,9 +1,14 @@
 package org.jetbrains.plugins.scala.codeInsight.implicits
 
+import java.lang.reflect.{Field, Modifier}
+
 import com.intellij.openapi.editor.{Inlay, InlayModel}
 import com.intellij.openapi.util.Key
 import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.codeInsight.implicits.Hint._
 import org.jetbrains.plugins.scala.extensions.ObjectExt
+
+import scala.collection.JavaConverters._
 
 private case class Hint(parts: Seq[Text],
                         element: PsiElement,
@@ -11,8 +16,11 @@ private case class Hint(parts: Seq[Text],
                         menu: Option[String] = None) {
 
   def addTo(model: InlayModel): Inlay = {
+    val offset = if (suffix) element.getTextRange.getEndOffset else element.getTextRange.getStartOffset
+
+    val existingInlays = model.getInlineElementsInRange(offset, offset).asScala.filter(isImplicitHint)
+
     val inlay = {
-      val offset = if (suffix) element.getTextRange.getEndOffset else element.getTextRange.getStartOffset
       val renderer = new TextRenderer(parts, menu)
       if (ImplicitHints.expanded) {
         renderer.expand()
@@ -21,6 +29,16 @@ private case class Hint(parts: Seq[Text],
       val relatesToPrecedingText = false
       model.addInlineElement(offset, relatesToPrecedingText, renderer)
     }
+
+    if (existingInlays.nonEmpty) {
+      // InlayImpl.myOriginalOffset is used solely for inlay sorting by InlayModelImpl
+      // TODO Support user-defined order of inlays with the same offset in IDEA API
+      Hint.myOriginalOffsetField.foreach { field =>
+        val offsets = existingInlays.map(field.getInt)
+        field.setInt(inlay, if (suffix) offsets.min + 1 else offsets.max - 1)
+      }
+    }
+
     inlay.putUserData(Hint.ElementKey, element)
     inlay
   }
@@ -33,4 +51,20 @@ private object Hint {
   private val ElementKey: Key[PsiElement] = Key.create("SCALA_IMPLICIT_HINT_ELEMENT")
 
   def elementOf(inlay: Inlay): PsiElement = ElementKey.get(inlay)
+
+  def isImplicitHint(inlay: Inlay): Boolean = inlay.getUserData(Hint.ElementKey) != null
+
+  private val myOriginalOffsetField: Option[Field] = try {
+    val inlayImplClass = Class.forName("com.intellij.openapi.editor.impl.InlayImpl")
+    val myOriginalOffsetField = inlayImplClass.getDeclaredField("myOriginalOffset")
+    myOriginalOffsetField.setAccessible(true)
+
+    val modifiersField = classOf[Field].getDeclaredField("modifiers")
+    modifiersField.setAccessible(true)
+    modifiersField.setInt(myOriginalOffsetField, myOriginalOffsetField.getModifiers & ~Modifier.FINAL)
+
+    Some(myOriginalOffsetField)
+  } catch {
+    case _: Throwable => None
+  }
 }
