9ba24d51e52a539fcab4a277d4cd660fa56d79dd
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-07-12 15:21:30 +0300

    NestedStatefulMonadsInspection refactored #SCL-13870

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/monads/NestedStatefulMonadsInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/monads/NestedStatefulMonadsInspection.scala
@@ -2,31 +2,39 @@ package org.jetbrains.plugins.scala.codeInspection.monads
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
-import org.jetbrains.plugins.scala.codeInspection.AbstractInspection
-import org.jetbrains.plugins.scala.codeInspection.monads.NestedStatefulMonadsInspection._
-import org.jetbrains.plugins.scala.codeInspection.monads.StatefulMonads._
+import org.jetbrains.plugins.scala.codeInspection.{AbstractInspection, InspectionsUtil}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall
+import org.jetbrains.plugins.scala.lang.psi.types.ScType
 import org.jetbrains.plugins.scala.lang.psi.types.api.ParameterizedType
 import org.jetbrains.plugins.scala.lang.psi.types.result._
+import org.jetbrains.plugins.scala.project.ProjectContext
+
 
 /**
- * @author Sergey Tolmachev (tolsi.ru@gmail.com)
- * @since 29.09.15
- */
-object NestedStatefulMonadsInspection {
-  private[monads] final val Annotation = "Nested stateful monads"
-}
+  * @author Sergey Tolmachev (tolsi.ru@gmail.com)
+  * @since 29.09.15
+  */
+final class NestedStatefulMonadsInspection extends AbstractInspection(NestedStatefulMonadsInspection.Description) {
 
-final class NestedStatefulMonadsInspection extends AbstractInspection(Annotation) {
+  import NestedStatefulMonadsInspection._
 
   override def actionFor(implicit holder: ProblemsHolder): PartialFunction[PsiElement, Unit] = {
     case call: ScMethodCall =>
-      val project = call.getProject
-      call.`type`().getOrAny match {
-        case outer@ParameterizedType(_, typeArgs)
-          if isStatefulMonadType(outer, project) && typeArgs.exists(isStatefulMonadType(_, project)) =>
-          holder.registerProblem(call, Annotation)
-        case _ =>
-      }
+      import call.projectContext
+      for {
+        Typeable(genericType@ParameterizedType(_, arguments)) <- Some(call)
+        if isStatefulMonadType(genericType) && arguments.exists(isStatefulMonadType)
+      } holder.registerProblem(call, Description)
   }
+}
+
+object NestedStatefulMonadsInspection {
+
+  private[monads] final val Description = "Nested stateful monads"
+
+  private final val StatefulMonadsTypesNames = Set("scala.concurrent.Future", "scala.util.Try")
+
+  private def isStatefulMonadType(scType: ScType)
+                                 (implicit context: ProjectContext): Boolean =
+    StatefulMonadsTypesNames.exists(InspectionsUtil.conformsToTypeFromClass(scType, _, context))
 }
--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/monads/NestedStatefulMonadsInspectionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/monads/NestedStatefulMonadsInspectionTest.scala
@@ -6,9 +6,9 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.testFramework.EditorTestUtil
 
 /**
- * @author Sergey Tolmachev (tolsi.ru@gmail.com)
- * @since 29.09.15
- */
+  * @author Sergey Tolmachev (tolsi.ru@gmail.com)
+  * @since 29.09.15
+  */
 class NestedStatefulMonadsInspectionTest extends ScalaInspectionTestBase {
 
   import EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
@@ -17,97 +17,84 @@ class NestedStatefulMonadsInspectionTest extends ScalaInspectionTestBase {
     classOf[NestedStatefulMonadsInspection]
 
   override protected val description: String =
-    NestedStatefulMonadsInspection.Annotation
+    NestedStatefulMonadsInspection.Description
 
-  def test_1(): Unit = {
-    val text =
+  def test_1(): Unit = checkTextHasNoErrors(
+    text =
       """import scala.util.Try
         |Try {}"""
-    checkTextHasNoErrors(text)
-  }
+  )
 
-  def test_2(): Unit = {
-    val text =
+  def test_2(): Unit = checkTextHasNoErrors(
+    text =
       """import scala.concurrent.Future
         |Future {}"""
-    checkTextHasNoErrors(text)
-  }
+  )
 
-  def test_3(): Unit = {
-    val text =
+  def test_3(): Unit = checkTextHasError(
+    text =
       s"""import scala.util.Try
-        |${START}Try { Try {} }$END"""
-    checkTextHasError(text)
-  }
+         |${START}Try { Try {} }$END"""
+  )
 
-  def test_4(): Unit = {
-    val text =
+  def test_4(): Unit = checkTextHasError(
+    text =
       s"""import scala.concurrent.Future
-        |${START}Future { Future {} }$END"""
-    checkTextHasError(text)
-  }
+         |${START}Future { Future {} }$END"""
+  )
 
-  def test_5(): Unit = {
-    val text = "Array.fill(5)(Array.fill(5)(0))"
-    checkTextHasNoErrors(text)
-  }
+  def test_5(): Unit = checkTextHasNoErrors(
+    text = "Array.fill(5)(Array.fill(5)(0))"
+  )
 
-  def test_6(): Unit = {
-    val text =
+  def test_6(): Unit = checkTextHasError(
+    text =
       s"""import scala.concurrent.Future
-          |val a = Future { }
-          |${START}Future { a }$END"""
-    checkTextHasError(text)
-  }
+         |val a = Future { }
+         |${START}Future { a }$END"""
+  )
 
-  def test_7(): Unit = {
-    val text =
+  def test_7(): Unit = checkTextHasError(
+    text =
       s"""import scala.util._
-          |${START}Try { Success() }$END"""
-    checkTextHasError(text)
-  }
+         |${START}Try { Success() }$END"""
+  )
 
-  def test_8(): Unit = {
-    val text =
+  def test_8(): Unit = checkTextHasError(
+    text =
       s"""import scala.util._
-          |import scala.concurrent.Future
-          |${START}Try { Future.successful() }$END"""
-    checkTextHasError(text)
-  }
+         |import scala.concurrent.Future
+         |${START}Try { Future.successful() }$END"""
+  )
 
-  def test_9(): Unit = {
-    val text =
+  def test_9(): Unit = checkTextHasError(
+    text =
       s"""import scala.util._
-        |${START}Success(Success(1))$END"""
-    checkTextHasError(text)
-  }
+         |${START}Success(Success(1))$END"""
+  )
 
-  def test_10(): Unit = {
-    val text =
+  def test_10(): Unit = checkTextHasError(
+    text =
       s"""import scala.util._
-          |import scala.concurrent.Future
-          |${START}Future { Success() }$END"""
-    checkTextHasError(text)
-  }
+         |import scala.concurrent.Future
+         |${START}Future { Success() }$END"""
+  )
 
-  def test_11(): Unit = {
-    val text =
+  def test_11(): Unit = checkTextHasNoErrors(
+    text =
       """import scala.util._
         |Array(Failure(1))"""
-    checkTextHasNoErrors(text)
-  }
+  )
 
-  def test_12(): Unit = {
-    val text =
+  def test_12(): Unit = checkTextHasNoErrors(
+    text =
       """import scala.util._
         |Success(Array(1))"""
-    checkTextHasNoErrors(text)
-  }
+  )
 
-  def test_13(): Unit = {
-    val text =
+  def test_13(): Unit = checkTextHasError(
+    text =
       s"""import scala.util._
-        |Array(${START}Success(Failure(1))$END)"""
-    checkTextHasError(text)
-  }
+         |Array(${START}Success(Failure(1))$END)"""
+  )
 }
