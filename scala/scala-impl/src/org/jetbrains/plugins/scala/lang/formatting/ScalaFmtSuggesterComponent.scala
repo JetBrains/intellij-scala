1522c758a3f5e4f9428eab37134391c99b7af784
Roman Shein <marso.des@gmail.com>
2018-07-18 17:59:23 +0300

    Use hyperlinks instead of notification actions #SCL-14004 in progress

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/ScalaFmtSuggesterComponent.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/ScalaFmtSuggesterComponent.scala
@@ -1,14 +1,13 @@
 package org.jetbrains.plugins.scala.lang.formatting
 
 import com.intellij.application.options.codeStyle.CodeStyleSchemesModel
-import com.intellij.notification.{Notification, NotificationAction, NotificationGroup}
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.components.{AbstractProjectComponent, PersistentStateComponent}
+import com.intellij.notification._
+import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
 import org.jetbrains.plugins.scala.lang.formatting.processors.ScalaFmtPreFormatProcessor
 import org.jetbrains.plugins.scala.lang.formatting.settings.ScalaCodeStyleSettings
 import com.intellij.openapi.components._
+import javax.swing.event.HyperlinkEvent
 
 import scala.beans.BeanProperty
 
@@ -22,8 +21,10 @@ class ScalaFmtSuggesterComponent(val project: Project) extends ProjectComponent 
     if (!settings.USE_SCALAFMT_FORMATTER && ScalaFmtPreFormatProcessor.projectDefaultConfig(project).nonEmpty && state.enableForCurrentProject) {
       //suggest the feature automatically
       settings.DETECT_SCALAFMT match {
-        case ScalaCodeStyleSettings.ASK_SCALAFMT_ENABLE => createNotification.notify(project)
-        case ScalaCodeStyleSettings.ALWAYS_SCALAFMT_ENABLE => enableForProject(project)
+        case ScalaCodeStyleSettings.ASK_SCALAFMT_ENABLE =>
+          createNotification.notify(project)
+        case ScalaCodeStyleSettings.ALWAYS_SCALAFMT_ENABLE =>
+          enableForProject()
         case _ =>
       }
     }
@@ -39,19 +40,57 @@ class ScalaFmtSuggesterComponent(val project: Project) extends ProjectComponent 
     this.state = state
   }
 
-  private def createNotification: Notification =
-    notificationGroup.createNotification("Scalafmt configuration detected. Enable scalafmt formatting for current project?", MessageType.INFO)
-      .addAction(enableForProjectAction(project)).addAction(enableAlwaysAction(project)).addAction(disableForProjectAction)
+  private def enableForProject(): Unit = {
+    val codeStyleSchemesModel = new CodeStyleSchemesModel(project)
+    var scheme = codeStyleSchemesModel.getSelectedScheme
+    if (!codeStyleSchemesModel.isProjectScheme(scheme)) {
+      codeStyleSchemesModel.copyToProject(scheme)
+      scheme = codeStyleSchemesModel.getProjectScheme
+    }
+    val newSettings = scheme.getCodeStyleSettings.getCustomSettings(classOf[ScalaCodeStyleSettings])
+    newSettings.FORMATTER = ScalaCodeStyleSettings.SCALAFMT_FORMATTER
+    codeStyleSchemesModel.apply()
+    notificationGroup.createNotification("Scalafmt formatting enabled for current project", NotificationType.INFORMATION).notify(project)
+  }
 
-  private val disableForProjectAction: NotificationAction = {
-    new NotificationAction("Don't show for current project") {
-      override def actionPerformed(e: AnActionEvent, notification: Notification): Unit = {
-        val newState = new ScalaFmtSuggesterComponent.State()
-        newState.enableForCurrentProject = false
-        loadState(newState)
-        notification.expire()
+  private def createNotification: Notification = {
+    val builder = new StringBuilder()
+    builder.append("Enable scalafmt formatting for current project?<br/>").append(wrapInRef(enableProjectDescription, enableProjectText))
+    if (!isProjectLevelConfiguration) builder.append(wrapInRef(enableAllDescription, enableAllText))
+    builder.append(wrapInRef(dontShowDescription, dontShowText))
+    notificationGroup.createNotification("Scalafmt configuration detected", builder.toString(), NotificationType.INFORMATION, listener)
+  }
+
+  private val listener: NotificationListener = new NotificationListener.Adapter {
+    override def hyperlinkActivated(notification: Notification, e: HyperlinkEvent): Unit = {
+      notification.expire()
+      e.getDescription match {
+        case `enableProjectDescription` =>
+          enableForProject()
+        case `enableAllDescription` =>
+          val settings = ScalaCodeStyleSettings.getInstance(project)
+          settings.DETECT_SCALAFMT = ScalaCodeStyleSettings.ALWAYS_SCALAFMT_ENABLE
+          settings.FORMATTER = ScalaCodeStyleSettings.SCALAFMT_FORMATTER
+        case `dontShowDescription` =>
+          val newState = new ScalaFmtSuggesterComponent.State()
+          newState.enableForCurrentProject = false
+          loadState(newState)
+        case _ =>
       }
     }
+  }
+
+  private def wrapInRef(description: String, text: String) = s"""<a href="$description">$text</a><br/>"""
+  private val enableProjectDescription = "enable"
+  private val enableProjectText = "Enable for current project"
+  private val enableAllDescription = "enableAll"
+  private val enableAllText = "Enable for all projects with scalafmt configurations"
+  private val dontShowDescription = "dont show"
+  private val dontShowText = "Don't show for current project"
+
+  private def isProjectLevelConfiguration: Boolean = {
+    val schemesModel = new CodeStyleSchemesModel(project)
+    schemesModel.isUsePerProjectSettings
   }
 }
 
@@ -63,35 +102,4 @@ object ScalaFmtSuggesterComponent {
   }
 
   val notificationGroup: NotificationGroup = NotificationGroup.balloonGroup("Scalafmt suggester")
-
-  private def enableForProject(project: Project): Unit = {
-    val codeStyleSchemesModel = new CodeStyleSchemesModel(project)
-    var scheme = codeStyleSchemesModel.getSelectedScheme
-    if (!codeStyleSchemesModel.isProjectScheme(scheme)) {
-      codeStyleSchemesModel.copyToProject(scheme)
-      scheme = codeStyleSchemesModel.getProjectScheme
-    }
-    val newSettings = scheme.getCodeStyleSettings.getCustomSettings(classOf[ScalaCodeStyleSettings])
-    newSettings.FORMATTER = ScalaCodeStyleSettings.SCALAFMT_FORMATTER
-    codeStyleSchemesModel.apply()
-  }
-
-  private def enableForProjectAction(project: Project): NotificationAction = {
-    new NotificationAction("Enable for current project") {
-      override def actionPerformed(e: AnActionEvent, notification: Notification): Unit = {
-        enableForProject(project)
-        notification.expire()
-      }
-    }
-  }
-
-  private def enableAlwaysAction(project: Project): NotificationAction = {
-    new NotificationAction("Enable where applicable") {
-      override def actionPerformed(e: AnActionEvent, notification: Notification): Unit = {
-        enableForProject(project)
-        ScalaCodeStyleSettings.getInstance(project).DETECT_SCALAFMT = ScalaCodeStyleSettings.ALWAYS_SCALAFMT_ENABLE
-        notification.expire()
-      }
-    }
-  }
 }
