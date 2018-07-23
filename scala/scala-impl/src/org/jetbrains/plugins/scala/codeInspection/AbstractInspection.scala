c640c4a116fc50095b53ccf927d6dac6022c28e0
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-07-23 14:52:16 +0300

    actionFor is marked as deprecated API #SCL-14139

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/AbstractInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/AbstractInspection.scala
@@ -10,16 +10,46 @@ import com.intellij.psi.{PsiElement, PsiElementVisitor}
 abstract class AbstractInspection protected(override final val getDisplayName: String = AbstractInspection.formatName(getClass.getSimpleName))
   extends LocalInspectionTool {
 
+  /**
+    * use {@link AbstractInspection.PureFunctionVisitorVisitor#problemDescriptor(PsiElement, Option[LocalQuickFix], String, ProblemHighlightType)} instead
+    */
+  @Deprecated
   protected def actionFor(implicit holder: ProblemsHolder): PartialFunction[PsiElement, Any]
 
-  override final def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
-    new PsiElementVisitor {
+  override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = new PartialFunctionVisitor(holder)
 
-      override def visitElement(element: PsiElement): Unit = actionFor(holder) match {
-        case action if action.isDefinedAt(element) => action(element)
-        case _ =>
+  protected final class PureFunctionVisitorVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) extends PsiElementVisitor {
+
+    protected def problemDescriptor(element: PsiElement,
+                                    maybeQuickFix: Option[LocalQuickFix] = None,
+                                    descriptionTemplate: String = getDisplayName,
+                                    highlightType: ProblemHighlightType = ProblemHighlightType.GENERIC_ERROR_OR_WARNING): Option[ProblemDescriptor] = {
+      val fixes = maybeQuickFix match {
+        case Some(quickFix) => Array(quickFix)
+        case _ => LocalQuickFix.EMPTY_ARRAY
       }
+      val descriptor = holder.getManager.createProblemDescriptor(element, descriptionTemplate, isOnTheFly, fixes, highlightType)
+      Some(descriptor)
     }
+
+    override def visitElement(element: PsiElement): Unit = problemDescriptor(element) match {
+      case Some(descriptor) => holder.registerProblem(descriptor)
+      case _ =>
+    }
+  }
+
+  /**
+    * use {@link AbstractInspection.PureFunctionVisitorVisitor} instead
+    */
+  @Deprecated
+  protected final class PartialFunctionVisitor(holder: ProblemsHolder) extends PsiElementVisitor {
+
+    override def visitElement(element: PsiElement): Unit = actionFor(holder) match {
+      case action if action.isDefinedAt(element) => action(element)
+      case _ =>
+    }
+  }
+
 }
 
 object AbstractInspection {
