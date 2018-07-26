d1ef9b3d9c45e5e2e8e7f1e655e065c133ddef05
Sugak Andrey <sugak.andr3y@gmail.com>
2018-07-26 12:57:56 +0300

    Fix attr. printing in decompiler, refactor ScalaDeprecationInspection
    #SCL-13919

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/deprecation/ScalaDeprecationInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/deprecation/ScalaDeprecationInspection.scala
@@ -6,88 +6,72 @@ import com.intellij.psi._
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScReferenceElement
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeProjection
-import org.jetbrains.plugins.scala.lang.psi.api.base.{ScPrimaryConstructor, ScReferenceElement}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScAnnotationsHolder, ScFunction}
 import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
 import org.jetbrains.plugins.scala.lang.resolve.ScalaResolveResult
 
-
 /**
  * User: Alexander Podkhalyuzin
  * Date: 13.04.2010
  */
-
 class ScalaDeprecationInspection extends LocalInspectionTool {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = {
-    def checkDeprecated(result: ScalaResolveResult, elementToHighlight: PsiElement, name: String) {
-      val refElement = result.element
-      refElement match {
-        case param: ScParameter if result.isNamedParameter &&
-          !ScalaNamesUtil.equivalent(param.name, name) =>
-          param.deprecatedName match {
-            case Some(deprecatedName) =>
-              val description: String = s"Parameter name $deprecatedName is deprecated"
-              holder.registerProblem(holder.getManager.createProblemDescriptor(elementToHighlight, description, true,
-                ProblemHighlightType.LIKE_DEPRECATED, isOnTheFly))
-              return
-            case _ =>
-          }
-        case _: PsiNamedElement =>
-        case _ => return
-      }
-      val context = ScalaPsiUtil.nameContext(refElement.asInstanceOf[PsiNamedElement])
-      context match {
-        case doc: PsiDocCommentOwner =>
-          doc match {
-            case _: ScPrimaryConstructor =>
-            case f: PsiMethod if f.isConstructor =>
-            case _ => if (!doc.isDeprecated) return
-          }
-          if (!doc.isDeprecated && !Option(doc.containingClass).exists(_.isDeprecated)) return
-        case _ => return
-      }
-      val message = for {
-        holder <- context.asOptionOf[ScAnnotationsHolder]
-        annotation <- holder.annotations("scala.deprecated").headOption
-        message <- ScalaPsiUtil.readAttribute(annotation, "value")
-      } yield message
-      
-      val description: String = Seq(Some("Symbol " + name + " is deprecated"),  message).flatten.mkString(". ")
-      holder.registerProblem(holder.getManager.createProblemDescriptor(elementToHighlight, description, true,
-        ProblemHighlightType.LIKE_DEPRECATED, isOnTheFly))
+    def registerDeprecationProblem(description: String, elementToHighlight: PsiElement): Unit = {
+      val descriptor = holder.getManager.createProblemDescriptor(
+        elementToHighlight,
+        description,
+        true,
+        ProblemHighlightType.LIKE_DEPRECATED,
+        isOnTheFly
+      )
+      holder.registerProblem(descriptor)
     }
 
+    def checkDeprecated(result: ScalaResolveResult, elementToHighlight: PsiElement, name: String): Unit =
+      result.element match {
+        case param: ScParameter if result.isNamedParameter && !ScalaNamesUtil.equivalent(param.name, name) =>
+          param.deprecatedName.foreach { deprecatedName =>
+            val description = s"Parameter name: $deprecatedName is deprecated."
+            registerDeprecationProblem(description, elementToHighlight)
+          }
+        case named: PsiNamedElement =>
+          val context = ScalaPsiUtil.nameContext(named)
+
+          val isDeprecated = context.asOptionOf[PsiDocCommentOwner].exists {
+            case m: PsiMethod if m.isConstructor => m.isDeprecated || m.containingClass.toOption.exists(_.isDeprecated)
+            case other                           => other.isDeprecated
+          }
+
+          if (isDeprecated) {
+            val message = for {
+              holder     <- context.asOptionOf[ScAnnotationsHolder] if isDeprecated
+              annotation <- holder.annotations("scala.deprecated").headOption
+              message    <- ScalaPsiUtil.readAttribute(annotation, "value")
+            } yield message
+
+            val description = s"Symbol $name is deprecated. ${message.getOrElse("")}"
+            registerDeprecationProblem(description, elementToHighlight)
+          }
+        case _ => ()
+      }
+
     new ScalaElementVisitor {
-      override def visitFunction(fun: ScFunction) {
+      override def visitFunction(fun: ScFunction): Unit = {
         //todo: check super method is deprecated
       }
 
-      override def visitReference(ref: ScReferenceElement) {
-        if (!ref.isValid) return
-        ref.bind().foreach {
-          checkDeprecated(_, ref.nameId, ref.refName)
-        }
-      }
+      override def visitReference(ref: ScReferenceElement): Unit =
+        if (ref.isValid) ref.bind().foreach { checkDeprecated(_, ref.nameId, ref.refName) }
 
-      override def visitReferenceExpression(ref: ScReferenceExpression) {
-        visitReference(ref)
-      }
-
-      override def visitTypeProjection(proj: ScTypeProjection) {
-        visitReference(proj)
-      }
+      override def visitReferenceExpression(ref: ScReferenceExpression): Unit = visitReference(ref)
+      override def visitTypeProjection(proj:     ScTypeProjection): Unit      = visitReference(proj)
     }
   }
 
-  override def getID: String = {
-    "ScalaDeprecation"
-  }
-
-  override def isEnabledByDefault: Boolean = {
-    true
-  }
-
+  override def getID: String               = "ScalaDeprecation"
+  override def isEnabledByDefault: Boolean = true
 }
