b65b481b7db64fa5e858eaf2908cc3530668d548
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-08-03 19:10:30 +0300

    EmptyParenOverrideInspection implemented #SCL-14139

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/methodSignature/EmptyParenOverrideInspection.scala
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package methodSignature
+
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.psi.PsiMethod
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
+
+sealed abstract class EmptyParenOverrideInspection extends AbstractMethodSignatureInspection {
+
+  override protected def isApplicable(function: ScFunction): Boolean =
+    function.isEmptyParen && function.superMethods.headOption.exists(isValidSuperMethod)
+
+  protected def isValidSuperMethod(method: PsiMethod): Boolean
+
+  override protected def createQuickFix(function: ScFunction): Option[LocalQuickFix] =
+    Some(new quickfix.RemoveParentheses(function))
+}
+
+object EmptyParenOverrideInspection {
+
+  final class JavaAccessorMethodOverriddenAsEmptyParenInspection extends EmptyParenOverrideInspection {
+
+    override protected def isValidSuperMethod(method: PsiMethod): Boolean =
+      quickfix.isAccessor(method)
+  }
+
+  final class ParameterlessMemberOverriddenAsEmptyParenInspection extends EmptyParenOverrideInspection {
+
+    override protected def isValidSuperMethod(method: PsiMethod): Boolean = method match {
+      case method: ScFunction if !method.isInCompiledFile => method.isParameterless
+      case _ => false
+    }
+  }
+
+}
