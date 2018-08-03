5dad12e478ce92c74a48e12f7bc352055405bead
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-08-03 16:12:48 +0300

    methods moved #SCL-14139

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/methodSignature/JavaMutatorMethodOverriddenAsParameterlessInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/methodSignature/JavaMutatorMethodOverriddenAsParameterlessInspection.scala
@@ -4,22 +4,17 @@ package methodSignature
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
-import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 
 /**
- * Pavel Fatin
- */
+  * Pavel Fatin
+  */
 final class JavaMutatorMethodOverriddenAsParameterlessInspection extends AbstractInspection("Java mutator method overridden as parameterless") {
 
+  import quickfix._
+
   override def actionFor(implicit holder: ProblemsHolder): PartialFunction[PsiElement, Unit] = {
-    case f: ScFunction if f.isParameterless =>
-      f.superMethods.headOption match { // f.superMethod returns None for some reason
-        case Some(_: ScalaPsiElement) => // do nothing
-        case Some(method) if method.isMutator =>
-          holder.registerProblem(f.nameId, getDisplayName, new quickfix.AddEmptyParentheses(f))
-        case _ =>
-      }
+    case f: ScFunction if f.isParameterless && f.superMethods.headOption.exists(isMutator) =>
+      holder.registerProblem(f.nameId, getDisplayName, new AddEmptyParentheses(f))
   }
 }
