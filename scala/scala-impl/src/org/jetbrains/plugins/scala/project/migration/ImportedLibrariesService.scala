6089c08e1922752ea1d9268a9ad45cc293b4091a
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2018-08-08 20:59:30 +0300

    #EA-123713 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/migration/ImportedLibrariesService.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/migration/ImportedLibrariesService.scala
@@ -1,26 +1,16 @@
 package org.jetbrains.plugins.scala.project.migration
 
-import java.io.File
 import java.util
 
-import com.intellij.openapi.externalSystem.model.project.{LibraryData, LibraryDependencyData, LibraryPathType, ProjectData}
+import com.intellij.openapi.externalSystem.model.project.{LibraryData, LibraryDependencyData, ProjectData}
 import com.intellij.openapi.externalSystem.model.{DataNode, Key, ProjectKeys}
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.LibraryOrderEntry
-import com.intellij.openapi.vfs.JarFileSystem
-import org.jetbrains.plugins.scala.codeInspection.bundled.BundledInspectionBase
-import org.jetbrains.plugins.scala.components.libinjection.LibraryInjectorLoader
-import org.jetbrains.plugins.scala.project.migration.api.{MigrationApiService, MigrationReport, SettingsDescriptor}
-import org.jetbrains.plugins.scala.project.migration.apiimpl.MigrationApiImpl
-import org.jetbrains.plugins.scala.project.migration.handlers.{ArtifactHandlerComponent, ScalaLibraryMigrationHandler}
-import org.jetbrains.plugins.scala.project.migration.store.ManifestAttributes.ManifestAttribute
-import org.jetbrains.plugins.scala.project.migration.store.{ManifestAttributes, SerializationUtil}
 import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 
 import scala.collection.mutable
-import scala.util.{Failure, Success, Try}
 
 /**
   * User: Dmitry.Naydanov
@@ -40,6 +30,6 @@ class ImportedLibrariesService extends AbstractProjectDataService[LibraryDepende
     val libAcc = mutable.HashSet[LibraryData]()
     while (it.hasNext) libAcc += it.next().getData.getTarget
     
-    BundledCodeStoreComponent.getInstance(project).setLibraryData(libAcc)
+    Option(BundledCodeStoreComponent getInstance project).foreach(_ setLibraryData libAcc)
   }
 }
