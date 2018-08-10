3d4df6e218b4793cbe6066e43b4ecd24bb2cb828
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-08-10 14:44:34 +0300

    `stubOrPsiChildren` may return only stub elements, should not be used for comparing implicit definition and context; method `nextSibilingsWithSelf` renamed to `withNextSiblings` #SCL-14224 fixed
    (cherry picked from commit 3295dd6641f29ae3cb3785978ba5134c164e9744)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/ScalaPsiUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/ScalaPsiUtil.scala
@@ -694,11 +694,10 @@ object ScalaPsiUtil {
 
     firstDifference.exists {
       case (beforeAncestor, afterAncestor) =>
+        val beforeInContext = beforeAncestor.sameElementInContext
+        val afterInContext = afterAncestor.sameElementInContext
 
-        val topChildren = beforeAncestor.getContext
-          .stubOrPsiChildren(TokenSet.ANY, PsiElement.ARRAY_FACTORY)
-
-        topChildren.indexOf(beforeAncestor.sameElementInContext) <= topChildren.indexOf(afterAncestor.sameElementInContext)
+        beforeInContext.withNextSiblings.contains(afterInContext)
     }
   }
 
