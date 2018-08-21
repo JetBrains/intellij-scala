1fba636764020345d3c0ed115d8525cd534e08e0
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2018-08-21 14:55:53 +0300

    Worksheet: create new file action should suggest default name

--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/NewScalaWorksheetAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/NewScalaWorksheetAction.scala
@@ -1,8 +1,10 @@
 package org.jetbrains.plugins.scala
 package worksheet.actions
 
-import com.intellij.openapi.actionSystem.{AnActionEvent, LangDataKeys}
+import com.intellij.ide.fileTemplates.actions.AttributesDefaults
+import com.intellij.openapi.actionSystem.{AnActionEvent, CommonDataKeys, DataContext, LangDataKeys}
 import com.intellij.openapi.module.Module
+import com.intellij.psi.PsiDirectory
 import org.jetbrains.plugins.scala.actions.LazyFileTemplateAction
 import org.jetbrains.plugins.scala.icons.Icons
 import org.jetbrains.plugins.scala.project._
@@ -13,6 +15,12 @@ import org.jetbrains.plugins.scala.project._
  */
 
 class NewScalaWorksheetAction extends LazyFileTemplateAction("Scala Worksheet", Icons.WORKSHEET_LOGO) {
+  override def getAttributesDefaults(dataContext: DataContext): AttributesDefaults = 
+    CommonDataKeys.PSI_ELEMENT.getData(dataContext) match {
+      case dir: PsiDirectory =>
+        suggestDefaultName(dir).map(new AttributesDefaults(_)).orNull
+      case _ => null
+    }
 
   override def update(e: AnActionEvent) {
     super.update(e)
@@ -21,5 +29,18 @@ class NewScalaWorksheetAction extends LazyFileTemplateAction("Scala Worksheet", 
     e.getPresentation.setEnabled(isEnabled)
     e.getPresentation.setVisible(isEnabled)
     e.getPresentation.setIcon(Icons.WORKSHEET_LOGO)
+  }
+  
+  private def suggestDefaultName(dir: PsiDirectory): Option[String] = {
+    val vFile = dir.getVirtualFile
+    val name = "untitled"
+
+    if (vFile.findChild(s"$name.${ScalaFileType.WORKSHEET_EXTENSION}") != null) {
+      for (i <- 1 to 1000) if (vFile.findChild(s"$name$i.${ScalaFileType.WORKSHEET_EXTENSION}") == null) return Option(s"$name$i")
+      
+      return None
+    } 
+
+    Option(name)
   }
 }
