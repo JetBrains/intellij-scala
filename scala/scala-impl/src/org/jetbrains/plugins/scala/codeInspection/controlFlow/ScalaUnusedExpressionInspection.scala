c4446707aabc1cf09c4815c9e4a10625d839c16e
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-08-22 16:45:14 +0300

    ScalaUnusedExpressionInspection is an AbstractRegisteredInspection #SCL-14139

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/controlFlow/ScalaUnusedExpressionInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/controlFlow/ScalaUnusedExpressionInspection.scala
@@ -1,100 +1,71 @@
 package org.jetbrains.plugins.scala
-package codeInspection.controlFlow
+package codeInspection
+package controlFlow
 
-import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection._
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, AbstractInspection, InspectionBundle}
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.{ScCaseClause, ScCaseClauses}
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunctionDefinition, ScPatternDefinition, ScVariableDefinition}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
-import org.jetbrains.plugins.scala.lang.psi.types.result._
-import org.jetbrains.plugins.scala.util.IntentionAvailabilityChecker
-import org.jetbrains.plugins.scala.util.SideEffectsUtil._
+import org.jetbrains.plugins.scala.util.{IntentionAvailabilityChecker, SideEffectsUtil}
 
 /**
   * Nikolay.Tropin
   * 2014-09-22
   */
-class ScalaUnusedExpressionInspection extends AbstractInspection("Unused expression") {
+final class ScalaUnusedExpressionInspection extends AbstractRegisteredInspection {
 
   import ScalaUnusedExpressionInspection._
+  import SideEffectsUtil.{hasNoSideEffects, mayOnlyThrow}
 
-  override protected def actionFor(implicit holder: ProblemsHolder): PartialFunction[PsiElement, Any] = {
-    case expr: ScExpression if IntentionAvailabilityChecker.checkInspection(this, expr.getParent) =>
+  override protected def problemDescriptor(element: PsiElement,
+                                           maybeQuickFix: Option[LocalQuickFix],
+                                           descriptionTemplate: String,
+                                           highlightType: ProblemHighlightType)
+                                          (implicit manager: InspectionManager, isOnTheFly: Boolean): Option[ProblemDescriptor] =
+    element match {
+      case expression: ScExpression if IntentionAvailabilityChecker.checkInspection(this, expression.getParent) &&
+        canResultInSideEffectsOnly(expression) =>
 
-      val valueIsNotUsed = canResultInSideEffectsOnly(expr)
-
-      if (valueIsNotUsed) {
-
-        val message =
-          if (hasNoSideEffects(expr)) Some(InspectionBundle.message("unused.expression.no.side.effects"))
-          else if (mayOnlyThrow(expr)) Some(InspectionBundle.message("unused.expression.throws"))
+        for {
+          descriptionTemplate <- if (hasNoSideEffects(expression)) Some(InspectionBundle.message("unused.expression.no.side.effects"))
+          else if (mayOnlyThrow(expression)) Some(InspectionBundle.message("unused.expression.throws"))
           else None
 
-        message.foreach { m =>
-          val removeElemFix = new RemoveElementQuickFix(expr)
-          val addReturnKeywordFix = PsiTreeUtil.getParentOfType(expr, classOf[ScFunctionDefinition]) match {
-            case null => Seq.empty
-            case fun if !fun.returnType.getOrAny.isUnit => Seq(new AddReturnQuickFix(expr))
-            case _ => Seq.empty
-          }
-
-          holder.registerProblem(expr, m, removeElemFix +: addReturnKeywordFix: _*)
-        }
-      }
-  }
-
+        } yield manager.createProblemDescriptor(expression, descriptionTemplate, isOnTheFly, createQuickFixes(expression), highlightType)
+      case _ => None
+    }
 }
 
 object ScalaUnusedExpressionInspection {
 
-  private def isLastInBlock(expr: ScExpression): Boolean = expr match {
-    case ChildOf(bl: ScBlock) => bl.lastExpr.contains(expr)
-    case ChildOf(_: ScPatternDefinition | _: ScFunctionDefinition | _: ScVariableDefinition) =>
-      !expr.isInstanceOf[ScBlock]
-    case _ => false
+  private def canResultInSideEffectsOnly(expression: ScExpression): Boolean =
+    isNonLastInBlock(expression) ||
+      parents(expression).exists {
+        case e: ScExpression => isNonLastInBlock(e)
+        case _ => false
+      } ||
+      isInUnitFunctionReturnPosition(expression)
+
+  private def findDefiningFunction(expression: ScExpression) =
+    expression.parentOfType(classOf[ScFunctionDefinition])
+
+  private def isUnit(definition: ScFunctionDefinition) =
+    definition.returnType.exists(_.isUnit)
+
+  private def createQuickFixes(expression: ScExpression): Array[LocalQuickFix] = new RemoveExpressionQuickFix(expression) match {
+    case quickFix if findDefiningFunction(expression).forall(isUnit) => Array(quickFix)
+    case quickFix => Array(quickFix, new AddReturnQuickFix(expression))
   }
 
-  private def isInBlock(expr: ScExpression): Boolean = expr match {
-    case ChildOf(_: ScBlock) => true
-    case _ => false
-  }
-
-  private def canResultInSideEffectsOnly(expr: ScExpression): Boolean = {
-    def isNotLastInBlock: Boolean = {
-      val parents = expr.parentsInFile.takeWhile {
-        case ms: ScMatchStmt if ms.expr.exists(PsiTreeUtil.isAncestor(_, expr, false)) => false
-        case ifStmt: ScIfStmt if ifStmt.condition.exists(PsiTreeUtil.isAncestor(_, expr, false)) => false
-        case _: ScBlock | _: ScParenthesisedExpr | _: ScIfStmt |
-             _: ScCaseClause | _: ScCaseClauses | _: ScMatchStmt |
-             _: ScTryStmt | _: ScCatchBlock => true
-        case _ => false
-      }
-      (expr +: parents.toSeq).exists {
-        case e: ScExpression => isInBlock(e) && !isLastInBlock(e)
-        case _ => false
-      }
-    }
-
-    def isInReturnPositionForUnitFunction: Boolean = {
-      Option(PsiTreeUtil.getParentOfType(expr, classOf[ScFunctionDefinition])) match {
-        case Some(fun) if fun.returnType.exists(_.isUnit) => fun.returnUsages(expr)
-        case _ => false
-      }
-    }
-
-    isNotLastInBlock || isInReturnPositionForUnitFunction
-  }
-
-  class AddReturnQuickFix(expression: ScExpression) extends AbstractFixOnPsiElement(
+  private[this] class AddReturnQuickFix(expression: ScExpression) extends AbstractFixOnPsiElement(
     "Add return keyword",
     expression
   ) {
-
     override protected def doApplyFix(expression: ScExpression)
                                      (implicit project: Project): Unit = {
       val retStmt = ScalaPsiElementFactory.createExpressionWithContextFromText(s"return ${expression.getText}", expression.getContext, expression)
@@ -102,15 +73,41 @@ object ScalaUnusedExpressionInspection {
     }
   }
 
-  class RemoveElementQuickFix(expression: ScExpression) extends AbstractFixOnPsiElement(
+  private[this] class RemoveExpressionQuickFix(expression: ScExpression) extends AbstractFixOnPsiElement(
     "Remove expression",
     expression
   ) {
-
     override protected def doApplyFix(expression: ScExpression)
                                      (implicit project: Project): Unit = {
       expression.delete()
     }
   }
 
+  private[this] def isNonLastInBlock(expression: ScExpression) = expression.getParent match {
+    case block: ScBlock => !block.lastExpr.contains(expression)
+    case _ => false
+  }
+
+  private[this] def parents(expression: ScExpression) = {
+    def isNotAncestor(maybeExpression: Option[ScExpression]) =
+      maybeExpression.forall(!PsiTreeUtil.isAncestor(_, expression, false))
+
+    expression.parentsInFile.takeWhile {
+      case statement: ScMatchStmt => isNotAncestor(statement.expr)
+      case statement: ScIfStmt => isNotAncestor(statement.condition)
+      case _: ScBlock |
+           _: ScParenthesisedExpr |
+           _: ScCaseClause |
+           _: ScCaseClauses |
+           _: ScTryStmt |
+           _: ScCatchBlock => true
+      case _ => false
+    }
+  }
+
+  private[this] def isInUnitFunctionReturnPosition(expression: ScExpression) = {
+    findDefiningFunction(expression).exists { definition =>
+      isUnit(definition) && definition.returnUsages(expression)
+    }
+  }
 }
--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/controlFlow/UnusedExpressionInspectionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/controlFlow/UnusedExpressionInspectionTest.scala
@@ -6,319 +6,288 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.testFramework.EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
 
 /**
- * Nikolay.Tropin
- * 2014-09-23
- */
-class UnusedExpressionInspectionTest extends ScalaInspectionTestBase {
+  * Nikolay.Tropin
+  * 2014-09-23
+  */
+abstract class UnusedExpressionInspectionTestBase extends ScalaInspectionTestBase {
+
   override protected val classOfInspection: Class[_ <: LocalInspectionTool] =
     classOf[ScalaUnusedExpressionInspection]
+}
 
-  override protected val description: String =
+class UnusedExpressionInspectionTest extends UnusedExpressionInspectionTestBase {
+
+  override protected val description =
     InspectionBundle.message("unused.expression.no.side.effects")
 
-  def testLiteral(): Unit = {
-    val text = s"""def foo(): Int = {
-                 |    if (true) return 1
-                 |    else ${START}2$END
-                 |
+  def testLiteral(): Unit = checkTextHasError {
+    s"""def foo(): Int = {
+       |    if (true) return 1
+       |    else ${START}2$END
+       |
                  |    0
-                 |}"""
-    checkTextHasError(text)
+       |}"""
   }
 
-  def testTuple(): Unit = {
-    val text = s"""def foo(): Unit = {
-                 |    var x = 0
-                 |    $START(0, 2)$END
-                 |    0
-                 |}"""
-    checkTextHasError(text)
+  def testTuple(): Unit = checkTextHasError {
+    s"""def foo(): Unit = {
+       |    var x = 0
+       |    $START(0, 2)$END
+       |    0
+       |}"""
   }
 
-  def testReference(): Unit = {
-    val text = s"""def foo(): Unit = {
-                 |    $START(0, 2)._1$END
-                 |    0
-                 |}"""
-    checkTextHasError(text)
+  def testReference(): Unit = checkTextHasError {
+    s"""def foo(): Unit = {
+       |    $START(0, 2)._1$END
+       |    0
+       |}"""
   }
 
-  def testReferenceToVal(): Unit = {
-    val text = s"""def foo(): Unit = {
-                 |  val a = 1
-                 |  ${START}a$END
-                 |  0
-                 |}"""
-    checkTextHasError(text)
+  def testReferenceToVal(): Unit = checkTextHasError {
+    s"""def foo(): Unit = {
+       |  val a = 1
+       |  ${START}a$END
+       |  0
+       |}"""
   }
 
-  def testTypedAndParenthesized(): Unit = {
-    val text = s"""def foo(): Unit = {
-                   |  val s = "aaa"
-                   |  $START(s: String).substring(0)$END
-                   |  0
-                   |}"""
-    checkTextHasError(text)
+  def testTypedAndParenthesized(): Unit = checkTextHasError {
+    s"""def foo(): Unit = {
+       |  val s = "aaa"
+       |  $START(s: String).substring(0)$END
+       |  0
+       |}"""
   }
 
-  def testReferenceToByNameParam(): Unit = {
-    val text = s"""def foo(i: => Int): Int = {
-                 |  ${START}i$END
-                 |  0
-                 |}"""
-    checkTextHasNoErrors(text)
+  def testReferenceToByNameParam(): Unit = checkTextHasNoErrors {
+    s"""def foo(i: => Int): Int = {
+       |  ${START}i$END
+       |  0
+       |}"""
   }
 
-  def testStringBuffer(): Unit = {
-    val text = s"""def foo(): Int = {
-                 |  val b = new StringBuffer()
-                 |  ${START}b.append("a")$END
-                 |  0
-                 |}"""
-    checkTextHasNoErrors(text)
+  def testStringBuffer(): Unit = checkTextHasNoErrors {
+    s"""def foo(): Int = {
+       |  val b = new StringBuffer()
+       |  ${START}b.append("a")$END
+       |  0
+       |}"""
   }
 
-  def testObjectMethodWithSideEffects(): Unit = {
-    val text = s"""def foo(): Int = {
-                 |  $START"1".wait()$END
-                 |  0
-                 |}"""
-    checkTextHasNoErrors(text)
+  def testObjectMethodWithSideEffects(): Unit = checkTextHasNoErrors {
+    s"""def foo(): Int = {
+       |  $START"1".wait()$END
+       |  0
+       |}"""
   }
 
-  def testImmutableCollection(): Unit = {
-    val text = s"""def foo(): Int = {
-                  |  0 match {
-                  |    case 0 => ${START}List(1)$END
-                  |    case 1 =>
-                  |  }
-                  |  1
-                  |}"""
-    checkTextHasError(text)
+  def testImmutableCollection(): Unit = checkTextHasError {
+    s"""def foo(): Int = {
+       |  0 match {
+       |    case 0 => ${START}List(1)$END
+       |    case 1 =>
+       |  }
+       |  1
+       |}"""
   }
 
-  def testImmutableCollection2(): Unit = {
-    val text = s"""def foo(): Int = {
-                  |  0 match {
-                  |    case 0 => ${START}List(1).dropRight(2)$END
-                  |    case 1 =>
-                  |  }
-                  |  1
-                  |}"""
-    checkTextHasError(text)
+  def testImmutableCollection2(): Unit = checkTextHasError {
+    s"""def foo(): Int = {
+       |  0 match {
+       |    case 0 => ${START}List(1).dropRight(2)$END
+       |    case 1 =>
+       |  }
+       |  1
+       |}"""
   }
 
-  def testImmutableCollection3(): Unit = {
-    val text = s"""def foo(): Int = {
-                  |  val f = (i: Int) => i + 1
-                  |  0 match {
-                  |    case 0 => ${START}List(1).foreach(f)$END
-                  |    case 1 =>
-                  |  }
-                  |  1
-                  |}"""
-    checkTextHasNoErrors(text)
+  def testImmutableCollection3(): Unit = checkTextHasNoErrors {
+    s"""def foo(): Int = {
+       |  val f = (i: Int) => i + 1
+       |  0 match {
+       |    case 0 => ${START}List(1).foreach(f)$END
+       |    case 1 =>
+       |  }
+       |  1
+       |}"""
   }
 
-  def testImmutableCollection4(): Unit = {
-    val text = s"""def foo(): Int = {
-                   |  0 match {
-                   |    case 0 => ${START}List(1).map(_ + 2)$END
-                   |    case 1 =>
-                   |  }
-                   |  1
-                   |}"""
-    checkTextHasError(text)
+  def testImmutableCollection4(): Unit = checkTextHasError {
+    s"""def foo(): Int = {
+       |  0 match {
+       |    case 0 => ${START}List(1).map(_ + 2)$END
+       |    case 1 =>
+       |  }
+       |  1
+       |}"""
   }
 
-  def testThisReference(): Unit = {
-    val text = s"""class A {
-                  |  val x = 1
-                  |
-                  |  def foo(): Int = {
-                  |    0 match {
-                  |      case 0 => ${START}this.x$END
-                  |      case 1 =>
-                  |    }
-                  |    1
-                  |  }
-                  |}"""
-    checkTextHasError(text)
+  def testThisReference(): Unit = checkTextHasError {
+    s"""class A {
+       |  val x = 1
+       |
+       |  def foo(): Int = {
+       |    0 match {
+       |      case 0 => ${START}this.x$END
+       |      case 1 =>
+       |    }
+       |    1
+       |  }
+       |}"""
   }
 
-  def testFunctionalParam(): Unit = {
-    val text =
-      s"""def foo(f: Int => Unit): Unit = {
-         |  ${START}List(1) foreach f$END
-         |}
+  def testFunctionalParam(): Unit = checkTextHasNoErrors {
+    s"""def foo(f: Int => Unit): Unit = {
+       |  ${START}List(1) foreach f$END
+       |}
        """
-    checkTextHasNoErrors(text)
   }
 
-  def testStringMethod(): Unit = {
-    val text = s"""def foo(): Int = {
-                    |  0 match {
-                    |    case 0 => $START"1".substring(1)$END
-                    |    case 1 =>
-                    |  }
-                    |  1
-                    |}"""
-    checkTextHasError(text)
+  def testStringMethod(): Unit = checkTextHasError {
+    s"""def foo(): Int = {
+       |  0 match {
+       |    case 0 => $START"1".substring(1)$END
+       |    case 1 =>
+       |  }
+       |  1
+       |}"""
   }
 
-  def testNoForAssignment(): Unit = {
-    val text = s"""def foo(): Int = {
-                 |    var x = 0
-                 |    ${START}x += 1$END
-                 |    0
-                 |}"""
-    checkTextHasNoErrors(text)
+  def testNoForAssignment(): Unit = checkTextHasNoErrors {
+    s"""def foo(): Int = {
+       |    var x = 0
+       |    ${START}x += 1$END
+       |    0
+       |}"""
   }
 
-  def testNoForAssignment2(): Unit = {
-    val text = s"""def foo(): Int = {
-                 |    var x = 0
-                 |    ${START}x = 1$END
-                 |    0
-                 |}"""
-    checkTextHasNoErrors(text)
+  def testNoForAssignment2(): Unit = checkTextHasNoErrors {
+    s"""def foo(): Int = {
+       |    var x = 0
+       |    ${START}x = 1$END
+       |    0
+       |}"""
   }
 
-  def testUnitFunction(): Unit = {
-    val text = s"""def foo(): Unit = {
-                 |  var z = 0
-                 |  if (true) z = 1
-                 |  else ${START}2$END
-                 |}"""
-    checkTextHasError(text)
+  def testUnitFunction(): Unit = checkTextHasError {
+    s"""def foo(): Unit = {
+       |  var z = 0
+       |  if (true) z = 1
+       |  else ${START}2$END
+       |}"""
   }
 
-  def testUnitFunction2(): Unit = {
-    val text = s"""def foo(): Unit = {
-                 |  $START"1".wait()$END
-                 |}"""
-    checkTextHasNoErrors(text)
+  def testUnitFunction2(): Unit = checkTextHasNoErrors {
+    s"""def foo(): Unit = {
+       |  $START"1".wait()$END
+       |}"""
   }
 
-  def testImplicitClass(): Unit = {
-    val text =
-      s"""implicit class StringOps(val s: String) {
-         |  def print(): Unit = println(s)
-         |}
-         |
-         |def foo(): Unit = {
-         |  $START"1".print()$END
-         |}"""
-    checkTextHasNoErrors(text)
+  def testImplicitClass(): Unit = checkTextHasNoErrors {
+    s"""implicit class StringOps(val s: String) {
+       |  def print(): Unit = println(s)
+       |}
+       |
+       |def foo(): Unit = {
+       |  $START"1".print()$END
+       |}"""
   }
 
-  def testImplicitFunction(): Unit = {
-    val text =
-      s"""implicit def stringToInteger(s: String): Integer = Integer.valueOf(s.length)
-        |
-        |def foo(): Unit = {
-        |  $START"1".intValue()$END
-        |}"""
-    checkTextHasNoErrors(text)
+  def testImplicitFunction(): Unit = checkTextHasNoErrors {
+    s"""implicit def stringToInteger(s: String): Integer = Integer.valueOf(s.length)
+       |
+       |def foo(): Unit = {
+       |  $START"1".intValue()$END
+       |}"""
   }
 
-  def testObject(): Unit = {
-    val text =
+  def testObject(): Unit = checkTextHasNoErrors {
+    """
+      |class Test {
+      |  def foo(): Unit = {
+      |    A
+      |    println("world")
+      |  }
+      |}
+      |
+      |object A {
+      |  println("hello")
+      |}
+      |
       """
-        |class Test {
-        |  def foo(): Unit = {
-        |    A
-        |    println("world")
-        |  }
-        |}
-        |
-        |object A {
-        |  println("hello")
-        |}
-        |
-      """
-    checkTextHasNoErrors(text)
   }
 
-  def testConstructorCall(): Unit = {
-    val text =
+  def testConstructorCall(): Unit = checkTextHasNoErrors {
+    """
+      |class Test {
+      |  def foo(): Unit = {
+      |    new A
+      |    println("world")
+      |  }
+      |}
+      |
+      |class A {
+      |  println("hello")
+      |}
+      |
       """
-        |class Test {
-        |  def foo(): Unit = {
-        |    new A
-        |    println("world")
-        |  }
-        |}
-        |
-        |class A {
-        |  println("hello")
-        |}
-        |
-      """
-    checkTextHasNoErrors(text)
   }
 
-  def testUnderscoreApply(): Unit = {
-    val text =
-      """
-        |object ToDo {
-        |  var todo: Option[() => Unit] = None
-        |
-        |  def doIt() = todo.foreach(_ ())
-        |  def doItToo() = todo.foreach(_.apply())
-        |  todo.foreach(_())
-        |}
-      """
-    checkTextHasNoErrors(text)
+  def testUnderscoreApply(): Unit = checkTextHasNoErrors {
+    """
+      |object ToDo {
+      |  var todo: Option[() => Unit] = None
+      |
+      |  def doIt() = todo.foreach(_ ())
+      |  def doItToo() = todo.foreach(_.apply())
+      |  todo.foreach(_())
+      |}
+    """
   }
 
-  def testStringGetChars(): Unit = {
-    val text =
-      """
-        |def foo(): Unit = {
-        |  val target = Array.empty[Char]
-        |  "abcde".getChars(0, 1, target, 0)
-        |}
-      """
-    checkTextHasNoErrors(text)
+  def testStringGetChars(): Unit = checkTextHasNoErrors {
+    """
+      |def foo(): Unit = {
+      |  val target = Array.empty[Char]
+      |  "abcde".getChars(0, 1, target, 0)
+      |}
+    """
   }
 }
 
-class UnusedExpressionThrowsInspectionTest extends ScalaInspectionTestBase {
-  override protected val description = InspectionBundle.message("unused.expression.throws")
+class UnusedExpressionThrowsInspectionTest extends UnusedExpressionInspectionTestBase {
 
-  override protected val classOfInspection = classOf[ScalaUnusedExpressionInspection]
+  override protected val description =
+    InspectionBundle.message("unused.expression.throws")
 
-  def testUnsafeGet(): Unit = {
-    checkTextHasError(
-      s"""
-        |def foo: Unit = {
-        |  import scala.util.Try
-        |  val tr = Try {
-        |    throw new IllegalStateException()
-        |  }
-        |  ${START}tr.get$END
-        |}
-      """.stripMargin)
+  def testUnsafeGet(): Unit = checkTextHasError {
+    s"""
+       |def foo: Unit = {
+       |  import scala.util.Try
+       |  val tr = Try {
+       |    throw new IllegalStateException()
+       |  }
+       |  ${START}tr.get$END
+       |}
+      """
   }
 
-  def testUnsafeHead(): Unit = {
-    checkTextHasError(
-      s"""
-        |def foo: Unit = {
-        |  val list: List[String] = Nil
-        |  ${START}list.head$END
-        |}
-      """.stripMargin)
+  def testUnsafeHead(): Unit = checkTextHasError {
+    s"""
+       |def foo: Unit = {
+       |  val list: List[String] = Nil
+       |  ${START}list.head$END
+       |}
+      """
   }
 
-  def testUnsafeGetProjection(): Unit = {
-    checkTextHasError(
-      s"""
-        |def foo: Unit = {
-        |  val left = Left("a")
-        |  ${START}Left("a").right.get$END
-        |}
-      """.stripMargin)
+  def testUnsafeGetProjection(): Unit = checkTextHasError {
+    s"""
+       |def foo: Unit = {
+       |  val left = Left("a")
+       |  ${START}Left("a").right.get$END
+       |}
+      """
   }
 }
