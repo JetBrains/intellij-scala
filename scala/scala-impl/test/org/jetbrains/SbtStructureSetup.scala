3abd9235bc3b758734673205526e35573cef0d4a
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-08-22 12:57:16 +0300

    allow using new versions of sbt in tests #SCL-14082

--- scala/scala-impl/test/org/jetbrains/SbtStructureSetup.scala
+++ scala/scala-impl/test/org/jetbrains/SbtStructureSetup.scala
@@ -2,13 +2,12 @@ package org.jetbrains
 
 import _root_.java.io.File
 
-import _root_.org.jetbrains.plugins.scala.util.TestUtils
-import _root_.org.jetbrains.sbt._
-import _root_.org.jetbrains.sbt.settings.SbtSystemSettings
 import _root_.org.jetbrains.plugins.scala.DependencyManager
 import _root_.org.jetbrains.plugins.scala.DependencyManagerBase._
+import _root_.org.jetbrains.plugins.scala.util.TestUtils
+import _root_.org.jetbrains.sbt.settings.SbtSystemSettings
+import _root_.org.jetbrains.plugins.scala.buildinfo.BuildInfo
 import com.intellij.openapi.project.Project
-import plugins.scala.buildinfo.BuildInfo
 
 /**
  * Utility to use with tests involving setup of sbt-launch or/and sbt-structure
@@ -20,20 +19,14 @@ object SbtStructureSetup {
   val IvyCacheDir: File = new File(TestUtils.getIvyCachePath)
 
   def setUpSbtLauncherAndStructure(project: Project): Unit = {
-    val sbtVersion = Sbt.Latest_0_13 // hardcode latest version of sbt 0.13, still need to make test 1.0-capable
-    val launcherVersion = Sbt.LatestVersion
-    val sbtStructureVersion = BuildInfo.sbtStructureVersion
-    val customSbtLauncher = DependencyManager.resolveSingle("org.scala-sbt"  % "sbt-launch" % launcherVersion.presentation).file
-    val customSbtStructure = IvyCacheDir / "scala_2.10" / "sbt_0.13" / "org.jetbrains" / "sbt-structure-extractor" / "jars" / s"sbt-structure-extractor-$sbtStructureVersion.jar"
+    val launcherVersion: String = BuildInfo.sbtLatestVersion
+    val customSbtLauncher = DependencyManager.resolveSingle("org.scala-sbt"  % "sbt-launch" % launcherVersion).file
 
     assert(customSbtLauncher.isFile, s"sbt launcher not found at $customSbtLauncher")
-    assert(customSbtStructure.isFile, s"sbt-structure not found at $customSbtStructure")
 
     val systemSettings = SbtSystemSettings.getInstance(project).getState
     systemSettings.setCustomLauncherEnabled(true)
-    systemSettings.setCustomLauncherPath(customSbtLauncher.canonicalPath)
-    systemSettings.setCustomSbtStructurePath(customSbtStructure.canonicalPath)
-    systemSettings.vmParameters += s" -Dsbt.version=$sbtVersion"
+    systemSettings.setCustomLauncherPath(customSbtLauncher.getCanonicalPath)
     Option(System.getProperty("sbt.ivy.home")).foreach { ivyHome =>
       systemSettings.vmParameters += s" -Dsbt.ivy.home=$ivyHome"
     }
