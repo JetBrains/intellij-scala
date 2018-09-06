ae616a6728cb06f68ae54f8054dc102574020f5b
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2018-09-06 18:37:37 +0300

    test for SCL-13747 doesn't fail anymore

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/CollectionsResolveTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/CollectionsResolveTest.scala
@@ -56,23 +56,4 @@ class CollectionsResolveTest extends FailedResolveCaretTestBase {
     s"""
        |val foo = <caret>Array[Byte](1, 2, 3)
     """.stripMargin)
-
-  def testSCL13747(): Unit = doResolveCaretTest(
-    s"""
-       |import java.util.Random
-       |  class State[S,A](run: S => (A, S)) {
-       |    def map[B](f: A=>B): State[S,B] = new State ( s => {
-       |      val (a,s2) = run(s)
-       |      (f(a), s2)
-       |    })
-       |  }
-       |  class RNGXX(rnd: Random) extends State[RNGXX, Int] (
-       |    rng => (rng.nextInt, new RNGXX(rnd))
-       |  ) {
-       |    val nextInt: Int = rnd.nextInt()
-       |  }
-       |  val r1 = new RNGXX(new Random(5))
-       |
-       |  r1.<caret>map(x => x + 1)
-    """.stripMargin)
 }
