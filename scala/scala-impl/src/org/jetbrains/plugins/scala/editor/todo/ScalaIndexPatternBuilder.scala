6cff63115d07a2a0f49016b704a0877527f560db
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-09-06 16:18:09 +0300

    todo index pattern builder fixed to enable multiline todos #SCL-14345 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/editor/todo/ScalaIndexPatternBuilder.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/editor/todo/ScalaIndexPatternBuilder.scala
@@ -5,34 +5,48 @@ import com.intellij.lexer.Lexer
 import com.intellij.psi.PsiFile
 import com.intellij.psi.impl.search.IndexPatternBuilder
 import com.intellij.psi.tree.{IElementType, TokenSet}
-import org.jetbrains.plugins.scala.lang.lexer.{ScalaLexer, ScalaTokenTypes}
+import org.jetbrains.plugins.scala.lang.lexer.ScalaLexer
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.scaladoc.parser.ScalaDocElementTypes
 
 /**
- * User: Alexander Podkhalyuzin
- * Date: 12.09.2008
- */
+  * User: Alexander Podkhalyuzin
+  * Date: 12.09.2008
+  */
 class ScalaIndexPatternBuilder extends IndexPatternBuilder {
   def getIndexingLexer(file: PsiFile): Lexer = {
     file match {
       case _: ScalaFile => new ScalaLexer
-      case _ => null
+      case _            => null
     }
   }
 
   def getCommentTokenSet(file: PsiFile): TokenSet = {
     file match {
       case _: ScalaFile => ScalaTokenTypes.COMMENTS_TOKEN_SET
-      case _ => null
+      case _            => null
     }
   }
 
-  def getCommentStartDelta(tokenType: IElementType) = 0
+  def getCommentStartDelta(tokenType: IElementType): Int = tokenType match {
+    case ScalaTokenTypes.tLINE_COMMENT |
+         ScalaTokenTypes.tBLOCK_COMMENT         => 2
+    case ScalaTokenTypes.tDOC_COMMENT |
+         ScalaDocElementTypes.SCALA_DOC_COMMENT => 3
+    case _                                      => 0
+  }
 
   def getCommentEndDelta(tokenType: IElementType): Int = tokenType match {
-    case ScalaTokenTypes.tBLOCK_COMMENT => 2
-    case ScalaDocElementTypes.SCALA_DOC_COMMENT => 2
-    case _ => 0
+    case ScalaTokenTypes.tBLOCK_COMMENT |
+         ScalaDocElementTypes.SCALA_DOC_COMMENT => 2
+    case _                                      => 0
+  }
+
+  override def getCharsAllowedInContinuationPrefix(tokenType: IElementType): String = tokenType match {
+    case ScalaTokenTypes.tBLOCK_COMMENT |
+         ScalaTokenTypes.tDOC_COMMENT |
+         ScalaDocElementTypes.SCALA_DOC_COMMENT => "*"
+    case _                                      => ""
   }
 }
