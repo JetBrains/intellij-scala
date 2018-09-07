183aa9829c4d5199c5150557cb3ed224c754a7a1
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2018-09-07 16:05:04 +0300

    test for SCL-6573 doesn't fail anymore

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/typeInference/MixinTypeTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/typeInference/MixinTypeTest.scala
@@ -13,22 +13,6 @@ class MixinTypeTest extends ScalaLightCodeInsightFixtureTestAdapter{
 
   override protected def shouldPass: Boolean = false
 
-  def testSCL6573() = {
-    val text =
-      """
-        |class SCL6573 {
-        |  def foo = {
-        |    trait A
-        |
-        |    trait B
-        |
-        |    new A with B
-        |  }
-        |}
-      """.stripMargin
-    checkTextHasNoErrors(text)
-  }
-
   def testSCL13112() = {
     val text =
       """
