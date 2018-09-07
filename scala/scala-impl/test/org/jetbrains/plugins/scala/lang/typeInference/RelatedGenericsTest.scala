7aeb1c92fc2b4863f8df8dc2bf3a0265e3f0a122
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2018-09-07 16:28:53 +0300

    test for SCL-9347 doesn't fail anymore

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/RelatedGenericsTest.scala
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.scala.lang.typeInference
+
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+
+/**
+  * @author Anton Yalyshev
+  * @since 07.09.2018.
+  */
+class RelatedGenericsTest extends ScalaLightCodeInsightFixtureTestAdapter {
+
+  def testSCL9347() = checkTextHasNoErrors(
+    """
+      |object SCL9347 {
+      |  trait Record
+      |  trait MutableRecord[TR <: Record] {
+      |    def table: Table[TR, MutableRecord[TR]]
+      |  }
+      |
+      |  // Important: MTR must be related to TR
+      |  class Table[TR <: Record, MTR <: MutableRecord[TR]] {selfTable =>
+      |    class Field {
+      |      def table = selfTable
+      |    }
+      |  }
+      |
+      |  type AnyTable = Table[TR, _ <: MutableRecord[TR]] forSome {type TR <: Record}
+      |
+      |  def needTable(table: AnyTable) = ???
+      |
+      |  def foo(field: AnyTable#Field) = {
+      |    field.table // Good code red
+      |  }
+      |
+      |  def foo2(field: Table[TR, MTR]#Field forSome {type TR <: Record; type MTR <: MutableRecord[TR]}) = {
+      |    field.table // Good code red
+      |  }
+      |
+      |  def bar(mutableRecord: MutableRecord[_ <: Record]) {
+      |    needTable(mutableRecord.table) // Good code red
+      |  }
+      |}
+    """.stripMargin)
+}
