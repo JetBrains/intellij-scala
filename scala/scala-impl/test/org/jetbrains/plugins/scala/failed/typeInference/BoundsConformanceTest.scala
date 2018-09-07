9d835e4dfc872c94c2131fab9344352bc2287736
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2018-09-07 15:32:01 +0300

    tests for SCL-10029 and SCL-11261 don't fail anymore

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/typeInference/BoundsConformanceTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/typeInference/BoundsConformanceTest.scala
@@ -13,21 +13,6 @@ class BoundsConformanceTest extends ScalaLightCodeInsightFixtureTestAdapter {
 
   override protected def shouldPass: Boolean = false
 
-  def testSCL10029(): Unit = {
-    checkTextHasNoErrors(
-      """
-        |sealed trait Feeling
-        |sealed trait Hungry extends Feeling
-        |sealed trait Thirsty extends Feeling
-        |
-        |class Person[F <: Feeling] {
-        |  def eat[T >: F <: Hungry] = println("Chomp!")
-        |  def drink[T >: F <: Thirsty] = println("Glug!")
-        |}
-      """.stripMargin
-    )
-  }
-
   def testSCL10692(): Unit = {
     checkTextHasNoErrors(
       """
@@ -41,20 +26,6 @@ class BoundsConformanceTest extends ScalaLightCodeInsightFixtureTestAdapter {
         |  val a: A = ???
         |  val b: a.B[Int] = a.c
         |}
-      """.stripMargin
-    )
-  }
-
-  def testSCL11261(): Unit = {
-    checkTextHasNoErrors(
-      """
-        |  sealed trait TOption
-        |
-        |  sealed trait TNumericLowerTypeBound
-        |
-        |  def sum[T2 >: TOption, T1 >: TNumericLowerTypeBound <: T2, A1, A2]
-        |  (b: Map[A1, T1])
-        |  (implicit f: Map[A2, T2]) = {}
       """.stripMargin
     )
   }
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/BoundsConformanceTest.scala
@@ -0,0 +1,39 @@
+package org.jetbrains.plugins.scala.lang.typeInference
+
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+
+/**
+  * Created by Anton Yalyshev on 07/09/18.
+  */
+
+class BoundsConformanceTest extends ScalaLightCodeInsightFixtureTestAdapter {
+
+  def testSCL10029(): Unit = {
+    checkTextHasNoErrors(
+      """
+        |sealed trait Feeling
+        |sealed trait Hungry extends Feeling
+        |sealed trait Thirsty extends Feeling
+        |
+        |class Person[F <: Feeling] {
+        |  def eat[T >: F <: Hungry] = println("Chomp!")
+        |  def drink[T >: F <: Thirsty] = println("Glug!")
+        |}
+      """.stripMargin
+    )
+  }
+
+  def testSCL11261(): Unit = {
+    checkTextHasNoErrors(
+      """
+        |  sealed trait TOption
+        |
+        |  sealed trait TNumericLowerTypeBound
+        |
+        |  def sum[T2 >: TOption, T1 >: TNumericLowerTypeBound <: T2, A1, A2]
+        |  (b: Map[A1, T1])
+        |  (implicit f: Map[A2, T2]) = {}
+      """.stripMargin
+    )
+  }
+}
