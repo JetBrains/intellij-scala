cda16cd711afcbe4dbc741ad907b127f3b2530ca
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2018-10-05 19:48:09 +0300

    withFilter, flatMap added (required for for comprehensions) #SCL-13049

--- scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/NullSafe.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/NullSafe.scala
@@ -1,13 +1,12 @@
 package org.jetbrains.plugins.scala.extensions
 
-import org.jetbrains.plugins.scala.extensions.NullSafe.{Null, constNull}
-
-
 /** This type is handy for working with possible null values.
   * It has no overhead of converting to Option, and allows to chain several nullable functions in a safe way
   * using `map` and `collect` methods.
   */
-class NullSafe[+A >: Null](val a: A) extends AnyVal {
+final class NullSafe[+A >: Null] private(val a: A) extends AnyVal {
+
+  import NullSafe.Null
 
   @inline def notNull: Boolean =
     a != null
@@ -19,32 +18,36 @@ class NullSafe[+A >: Null](val a: A) extends AnyVal {
 
   @inline def orNull: A = a
 
-  @inline def map[S >: Null](f: A => S): NullSafe[S] =
+  @inline def map[B >: Null](f: A => B): NullSafe[B] =
     if (notNull) NullSafe(f(a)) else Null
 
+  @inline def flatMap[B >: Null](f: A => NullSafe[B]): NullSafe[B] =
+    if (notNull) f(a) else Null
+
   @inline def collect[B >: Null](pf: scala.PartialFunction[A, B]): NullSafe[B] =
-    if (notNull) NullSafe(pf.applyOrElse(a, constNull)) else Null
+    if (notNull) NullSafe(pf.applyOrElse(a, Function.const(null))) else Null
 
   @inline def getOrElse[B >: A](other: => B): B =
     if (notNull) a else other
 
   @inline def exists(p: A => Boolean): Boolean =
-    if (notNull) p(a) else false
+    notNull && p(a)
 
   @inline def forall(p: A => Boolean): Boolean =
-    if (notNull) p(a) else true
+    isNull || p(a)
 
   @inline def foreach(f: A => Unit): Unit =
     if (notNull) f(a)
 
+  @inline def withFilter(p: A => Boolean): NullSafe[A] =
+    if (notNull && p(a)) this else Null
+
   @inline def filter(p: A => Boolean): NullSafe[A] =
-    if (notNull && p(a)) NullSafe(a) else Null
+    withFilter(p)
 }
 
 object NullSafe {
   def apply[A >: Null](a: A) = new NullSafe(a)
 
-  def Null: NullSafe[Null] = NullSafe(null)
-
-  private val constNull: Any => Null = _ => null
+  val Null: NullSafe[Null] = new NullSafe(null)
 }
