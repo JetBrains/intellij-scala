cba2134afafab6812e5993c31bc18925190f6a4a
tkroman <rmn.tk.ml@gmail.com>
2018-10-07 00:00:39 +0300

    add circe plugin

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/CirceCodecInjector.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/CirceCodecInjector.scala
@@ -1,52 +1,54 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef
 
 import scala.util.Random
-import com.intellij.psi.PsiClass
-import org.jetbrains.plugins.scala.extensions.{PsiClassExt, ResolvesTo}
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScAnnotation, ScExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScAnnotation
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScTypeParam
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
 import org.jetbrains.plugins.scala.lang.psi.types.ScalaTypePresentation
 
 
 /**
- * Support for https://github.com/circe/circe
- *
- * Chooses to skip encoder/decoder derivation (and requirements) in order to
- * provide a much faster editing experience. Users may experience
- * deriving failures when they run the real compiler.
- *
- * @author tkroman
- * @since  06/10/2018
- */
+  * Support for https://github.com/circe/circe
+  *
+  * Does not do typechecking etc
+  * (in particular, no implicit search for Configuration),
+  * so users may face errors when running actual compilation.
+  *
+  * @author tkroman
+  * @since  06/10/2018
+  */
 class CirceCodecInjector extends SyntheticMembersInjector {
-  private val jc: String = "JsonCodec"
-  private val cjc: String = "ConfiguredJsonCodec"
-  private val fqnJc: String = "io.circe.generic.JsonCodec"
-  private val fqnCjc: String = "io.circe.generic.extras.ConfiguredJsonCodec"
+  private val Jc: String = "JsonCodec"
+  private val Cjc: String = "ConfiguredJsonCodec"
+  private val FqnJc: String = "io.circe.generic.JsonCodec"
+  private val FqnCjc: String = "io.circe.generic.extras.ConfiguredJsonCodec"
+  private val Enc: String = "_root_.io.circe.Encoder"
+  private val Dec: String = "_root_.io.circe.Decoder"
+  private val Dummy: String = "_root_.scala.Predef.???"
 
   // fast check
   private def hasCodecs(source: ScTypeDefinition): Boolean = {
-    (source.findAnnotationNoAliases(jc) != null) ||
-      (source.findAnnotationNoAliases(fqnJc) != null) ||
-      (source.findAnnotationNoAliases(cjc) != null) ||
-      (source.findAnnotationNoAliases(fqnCjc) != null)
+    (source.findAnnotationNoAliases(Jc) != null) ||
+      (source.findAnnotationNoAliases(FqnJc) != null) ||
+      (source.findAnnotationNoAliases(Cjc) != null) ||
+      (source.findAnnotationNoAliases(FqnCjc) != null)
   }
 
   // slower, more correct
   private def getDeriving(source: ScTypeDefinition): Option[ScAnnotation] = {
-    source.annotations(jc).headOption orElse
-      source.annotations(cjc).headOption orElse
-      source.annotations(fqnJc).headOption orElse
-      source.annotations(fqnCjc).headOption
+    source.annotations(Jc).headOption orElse
+      source.annotations(Cjc).headOption orElse
+      source.annotations(FqnJc).headOption orElse
+      source.annotations(FqnCjc).headOption
   }
 
   // so annotated sealed traits will generate a companion
-  override def needsCompanionObject(source: ScTypeDefinition): Boolean =
+  override def needsCompanionObject(source: ScTypeDefinition): Boolean = {
     hasCodecs(source)
+  }
 
   // add implicits to case object / case class companions
-  override def injectFunctions(source: ScTypeDefinition): Seq[String] =
+  override def injectFunctions(source: ScTypeDefinition): Seq[String] = {
     source match {
       case cob: ScObject if hasCodecs(cob) =>
         genImplicits(cob, cob.typeParameters)
@@ -58,10 +60,7 @@ class CirceCodecInjector extends SyntheticMembersInjector {
         }
       case _ => Nil
     }
-
-  private val Enc = "_root_.io.circe.Encoder"
-  private val Dec = "_root_.io.circe.Decoder"
-  private val Dummy = "_root_.scala.Predef.???"
+  }
 
   private def genImplicits(clazz: ScTypeDefinition, typarams: Seq[ScTypeParam]): Seq[String] = {
     val tparams: Seq[String] = typarams.map(tp => tp.name)
@@ -90,6 +89,7 @@ class CirceCodecInjector extends SyntheticMembersInjector {
       s"implicit def enc_$id$encCtxBounds: $Enc[$nme$encParams] = $Dummy",
       s"implicit def dec_$id$decCtxBounds: $Dec[$nme$decParams] = $Dummy"
     )
+    Seq.empty
   }
 
   private def join(xs: Seq[String]): String = xs.mkString(",")
