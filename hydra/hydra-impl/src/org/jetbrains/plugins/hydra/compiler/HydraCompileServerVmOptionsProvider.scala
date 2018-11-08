a8e2af576e6e5a83ace66eadf9b7fa1ad0e0a8d5
Mirco Dotta <mirco.dotta@gmail.com>
2018-11-08 16:04:23 +0100

    Moved Hydra support outside of the intellij-scala repo
    The only pending aspect is figuring out how to remove the reference to the
    hydra-compiler-jps.jar from the `ILoopWrapperFactoryHandler` used by the worksheet.

--- hydra/hydra-impl/src/org/jetbrains/plugins/hydra/compiler/HydraCompileServerVmOptionsProvider.scala
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.hydra.compiler
-
-import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.scala.compiler.CompileServerVmOptionsProvider
-
-class HydraCompileServerVmOptionsProvider extends CompileServerVmOptionsProvider {
-  override def vmOptionsFor(project: Project): Seq[String] =
-    HydraCompilerSettingsManager.getHydraLogJvmParameter(project).toSeq
-}
