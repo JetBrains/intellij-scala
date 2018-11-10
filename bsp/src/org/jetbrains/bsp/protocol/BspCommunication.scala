e5067b37b3befac4f3e77bde860f6dc89047e937
Justin Kaeser <justin.kaeser@jetbrains.com>
2018-11-10 02:17:31 +0100

    implement BSP connection discovery protocol. #SCL-14553
    (cherry picked from commit cad982be8a70a4192bd5afd57f893fdbfac0911e)

--- bsp/src/org/jetbrains/bsp/protocol/BspCommunication.scala
+++ bsp/src/org/jetbrains/bsp/protocol/BspCommunication.scala
@@ -4,6 +4,8 @@ import java.io.File
 import java.net.URI
 import java.nio.file._
 
+import ch.epfl.scala.bsp4j.BspConnectionDetails
+import com.google.gson.Gson
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
@@ -11,16 +13,19 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.{Project, ProjectUtil}
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.text.StringUtil.defaultIfEmpty
+import com.intellij.util.SystemProperties
 import com.intellij.util.net.NetUtils
 import org.jetbrains.bsp.protocol.BspCommunication._
 import org.jetbrains.bsp.protocol.BspNotifications.BspNotification
 import org.jetbrains.bsp.protocol.BspServerConnector._
 import org.jetbrains.bsp.protocol.BspSession.{BspSessionTask, NotificationAggregator, NotificationCallback}
 import org.jetbrains.bsp.settings.{BspExecutionSettings, BspProjectSettings, BspSettings}
-import org.jetbrains.bsp.{BSP, BspError}
+import org.jetbrains.bsp.{BSP, BspError, BspErrorMessage}
 
 import scala.concurrent.{Future, Promise}
-import scala.util.Random
+import scala.io.Source
+import scala.util.{Failure, Random, Try}
 
 class BspCommunicationComponent(project: Project) extends ProjectComponent {
 
@@ -154,16 +159,79 @@ object BspCommunication {
       }
       else tcpMethod
 
+    val connectionDetails = findBspConfigs(base)
     val bloopConfigDir = new File(base, ".bloop").getCanonicalFile
 
     val connector =
-      if (bloopConfigDir.exists()) new BloopConnector(bspExecutionSettings.bloopExecutable, base, capabilities)
+      if (connectionDetails.nonEmpty) new GenericConnectorSync(base, capabilities)
+      else if (bloopConfigDir.exists()) new BloopConnector(bspExecutionSettings.bloopExecutable, base, capabilities)
       else {
         // TODO need a protocol to detect generic bsp server
         new GenericConnectorSync(base, capabilities)
       }
 
     connector.connect(preferredMethod, tcpMethod)
+  }
+
+  private def findBspConfigs(projectBase: File): Seq[BspConnectionDetails] = {
+
+    val workspaceConfigDir = new File(projectBase, ".bsp")
+    val workspaceConfigs = listFiles(List(workspaceConfigDir))
+    val systemConfigs = systemDependentConnectionFiles
+
+    val potentialConfigs = tryReadingConnectionFiles(workspaceConfigs ++ systemConfigs)
+
+    potentialConfigs.flatMap(_.toOption)
+  }
+
+  private def systemDependentConnectionFiles = {
+    val basePaths =
+      if (SystemInfo.isWindows) windowsBspFiles()
+      else if (SystemInfo.isMac) macBspFiles()
+      else if (SystemInfo.isUnix) unixBspFiles()
+      else Nil
+
+    listFiles(bspDirs(basePaths))
+  }
+
+  private def tryReadingConnectionFiles(files: Seq[File]): Seq[Try[BspConnectionDetails]] = {
+    val gson = new Gson()
+    files.map { file =>
+      if (file.canRead) {
+        val reader = Source.fromFile(file).bufferedReader()
+        Try(gson.fromJson(reader, classOf[BspConnectionDetails]))
+      } else Failure(BspErrorMessage(s"file not readable: $file"))
+    }
+  }
+
+  private val BspDirName = "bsp"
+
+  private def windowsBspFiles() = {
+    val localAppData = System.getenv("LOCALAPPDATA")
+    val programData = System.getenv("PROGRAMDATA")
+    List(localAppData, programData)
+  }
+
+  private def unixBspFiles() = {
+    val xdgDataHome = System.getenv("XDG_DATA_HOME")
+    val xdgDataDirs = System.getenv("XDG_DATA_DIRS")
+    val dataHome = defaultIfEmpty(xdgDataHome, SystemProperties.getUserHome + "/.local/share")
+    val dataDirs = defaultIfEmpty(xdgDataDirs, "/usr/local/share:/usr/share").split(":").toList
+    dataHome :: dataDirs
+  }
+
+  private def macBspFiles() = {
+    val userHome = SystemProperties.getUserHome
+    val userData = userHome + "/Library/Application Support"
+    val systemData = "/Library/Application Support"
+    List(userData, systemData)
+  }
+
+  private def bspDirs(basePaths: List[String]): List[File] = basePaths.map(new File(_, BspDirName))
+
+  private def listFiles(dirs: List[File]): List[File] = dirs.flatMap { dir =>
+    if (dir.isDirectory) dir.listFiles()
+    else Array.empty[File]
   }
 
 
