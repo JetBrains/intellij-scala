37175781d898de6f72bb5480addbc09b4ab91eea
andrey.sugak <andrey.sugak@jetbrains.com>
2018-11-14 13:59:35 +0300

    Use ActionPromoter EP to fix shortcut conflict with Expr. Type action

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/actions/implicitArguments/ShowImplicitArgumentsActionPromoter.scala
@@ -0,0 +1,17 @@
+package org.jetbrains.plugins.scala.actions.implicitArguments
+
+import java.util
+
+import com.intellij.codeInsight.hint.actions.ShowExpressionTypeAction
+import com.intellij.openapi.actionSystem.{ActionPromoter, AnAction, DataContext}
+
+import scala.collection.JavaConverters._
+
+class ShowImplicitArgumentsActionPromoter extends ActionPromoter {
+  override def promote(actions: util.List[AnAction], context: DataContext): util.List[AnAction] =
+    actions.asScala.sortBy {
+      case _: ShowExpressionTypeAction    => 2
+      case _: ShowImplicitArgumentsAction => 1
+      case _                              => 0
+    }.asJava
+}
