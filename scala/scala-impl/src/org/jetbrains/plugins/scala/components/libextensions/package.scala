4bc65ac43473cf1534f9b60a13ab2ddc036bb3d6
Mikhail Mutcianko <mutcianko.m@gmail.com>
2018-11-27 22:41:22 +0300

    SCL-14588: extract data structures into the package object
    (cherry picked from commit 236f62af55d2f69b5dbc4fc2b5ac8f1720c97b03)

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/libextensions/package.scala
@@ -0,0 +1,18 @@
+package org.jetbrains.plugins.scala.components
+
+import java.io.File
+
+import scala.collection.mutable.ArrayBuffer
+
+
+package object libextensions {
+  class ExtensionException(message: String) extends Exception(message)
+  class ExtensionNotRegisteredException(iface: Class[_]) extends ExtensionException(s"No extensions registered for class $iface")
+  class InvalidExtensionException(iface: Class[_], impl: Class[_]) extends ExtensionException(s"Extension $impl doesn't inherit $iface")
+  class ExtensionAlreadyLoadedException(file: File) extends ExtensionException(s"Extensions jar $file is already loaded")
+  class NoManifestInExtensionJarException(file: File) extends ExtensionException(s"Extensions jar $file has no manifest")
+  class BadManifestException(file: File, cause: Throwable) extends ExtensionException(s"Failed to parse extension manifest from jar $file:\n$cause")
+  class BadExtensionDescriptor(file: File, error: String) extends ExtensionException(s"Failed to extract extensions descriptors from xml in file $file: $error")
+
+  case class ExtensionJarData(descriptor: LibraryDescriptor, file: File, loadedExtensions: Map[Class[_], ArrayBuffer[Any]])
+}
