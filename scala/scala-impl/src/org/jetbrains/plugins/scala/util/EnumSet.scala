139b319eb364014b5507b4de20017d0e285b4909
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2018-12-10 19:08:31 +0300

    utility for creating java.lang.Enum based bit sets, it is typesafe and is an Int at runtime #SCL-14670

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/EnumSet.scala
@@ -0,0 +1,92 @@
+package org.jetbrains.plugins.scala.util
+
+import sun.misc.SharedSecrets
+
+import scala.language.higherKinds
+import scala.reflect.ClassTag
+
+/** This trait is necessary for "opaque type" pattern, which allows to use EnumSet[E]
+  * as a type-safe alternative to Int
+  **/
+trait EnumSetProvider {
+  type EnumSet[E <: Enum[E]] <: Int
+
+  def empty[E <: Enum[E]]: EnumSet[E]
+
+  def single[E <: Enum[E]](e: E): EnumSet[E]
+
+  def union[E <: Enum[E]](set1: EnumSet[E], set2: EnumSet[E]): EnumSet[E]
+
+  def add[E <: Enum[E]](set:EnumSet[E], e: E): EnumSet[E]
+
+  def contains[E <: Enum[E]](set: EnumSet[E], e: E): Boolean
+
+  //for deserialization only
+  def readFromInt[E <: Enum[E]](i: Int): EnumSet[E]
+}
+
+object EnumSetProvider {
+
+  val instance: EnumSetProvider =
+    new EnumSetProvider {
+
+      type EnumSet[E <: Enum[E]] = Int
+
+      def empty[E <: Enum[E]]: EnumSet[E] = 0
+
+      def single[E <: Enum[E]](e: E): EnumSet[E] = 1 << e.ordinal()
+
+      def union[E <: Enum[E]](set1: EnumSet[E], set2: EnumSet[E]): EnumSet[E] = set1 | set2
+
+      def intersect[E <: Enum[E]](set1: EnumSet[E], set2: EnumSet[E]): EnumSet[E] = set1 & set2
+
+      def add[E <: Enum[E]](set: EnumSet[E], e: E): EnumSet[E] = union(set, single(e))
+
+      def contains[E <: Enum[E]](set: EnumSet[E], e: E): Boolean = intersect(set, single(e)) == single(e)
+
+      def readFromInt[E <: Enum[E]](i: Int): EnumSet[E] = i
+    }
+
+}
+
+object EnumSet {
+  import EnumSetProvider.instance
+
+  type EnumSet[E <: Enum[E]] = EnumSetProvider.instance.EnumSet[E]
+
+  def empty[E <: Enum[E]]: EnumSet[E] = instance.empty
+
+  def apply[E <: Enum[E]](e: E): EnumSet[E] = instance.single(e)
+
+  def apply[E <: Enum[E]](e1: E, e2: E): EnumSet[E] = EnumSet(e1) ++ e2
+
+  def apply[E <: Enum[E]](e1: E, e2: E, e3: E): EnumSet[E] = EnumSet(e1) ++ e2 ++ e3
+
+  def apply[E <: Enum[E]](elems: E*): EnumSet[E] = elems.foldLeft(empty[E])(_ ++ _)
+
+  def readFromInt[E <: Enum[E]](i: Int): EnumSet[E] = instance.readFromInt(i)
+
+  private def values[E <: Enum[E]](implicit classTag: ClassTag[E]): Array[E] = {
+    val aClass = classTag.runtimeClass.asInstanceOf[Class[E]]
+
+    //magic from java.util.EnumSet
+    SharedSecrets.getJavaLangAccess.getEnumConstantsShared(aClass)
+  }
+
+  implicit class EnumSetOps[E <: Enum[E]](val set: EnumSet[E]) extends AnyVal {
+    //I would prefer `+` here, but it clashes with int addition
+    def ++(e: E): EnumSet[E] = instance.add(set, e)
+
+    def ++(e: EnumSet[E]): EnumSet[E] = instance.union(set, e)
+
+    def contains(e: E): Boolean = instance.contains(set, e)
+
+    def isEmpty: Boolean = set == EnumSet.empty
+
+    def foreach(f: E => Unit)(implicit classTag: ClassTag[E]): Unit = toArray.foreach(f)
+
+    def toArray(implicit classTag: ClassTag[E]): Array[E] = {
+      values[E].filter(set.contains)
+    }
+  }
+}
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/util/EnumSetTest.scala
@@ -0,0 +1,44 @@
+package org.jetbrains.plugins.scala.util
+
+import junit.framework.TestCase
+import org.junit.Assert._
+import org.jetbrains.plugins.scala.util.EnumSet._
+
+class EnumSetTest extends TestCase {
+  import JavaEnum._
+
+  def testEnumSet(): Unit = {
+
+    val ab = EnumSet(A, B, B, B, B, A, B)
+    val ab2 = EnumSet.empty[JavaEnum] ++ A ++ B
+
+    assertTrue(ab.contains(A) && ab.contains(B) && !ab.contains(C) && !ab.contains(D))
+    assertTrue(ab2.contains(A) && ab2.contains(B) && !ab2.contains(C) && !ab2.contains(D))
+
+    assertTrue(ab == ab2)
+
+    val bc = EnumSet(B) ++ EnumSet(C)
+
+    assertTrue(!bc.contains(A) && bc.contains(B) && bc.contains(C) && !bc.contains(D))
+
+    val abc = ab ++ bc
+
+    assertTrue(abc.contains(A) && abc.contains(B) && abc.contains(C) && !abc.contains(D))
+  }
+
+  def testEnumSetToArray(): Unit = {
+    val acd = EnumSet(D) ++ EnumSet(A) ++ C
+
+    assertTrue(acd.toArray sameElements Array(A, C, D))
+  }
+
+  def testEnumSetIsEmpty(): Unit = {
+    val empty1 = EnumSet.empty[JavaEnum]
+    val empty2 = EnumSet[JavaEnum]()
+    val empty3 = EnumSet.readFromInt[JavaEnum](0)
+
+    assertTrue(empty1.isEmpty && empty2.isEmpty && empty3.isEmpty)
+    assertTrue(empty1 == empty2 && empty2 == empty3)
+  }
+
+}
