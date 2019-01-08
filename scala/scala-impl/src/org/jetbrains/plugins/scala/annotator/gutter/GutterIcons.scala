43132a7fbef3e2de2a6c8179e733de9586e8dc7e
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-01-08 20:04:47 +0100

    fix gutter icon paths SCL-14806 fixed
    (cherry picked from commit ac2f5f59baa11b07041fee1ee0ba236a441651e3)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/gutter/GutterIcons.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/gutter/GutterIcons.scala
@@ -4,6 +4,7 @@ package gutter
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
 
 /**
  * User: Alexander Podkhalyuzin
@@ -11,14 +12,14 @@ import com.intellij.openapi.util.IconLoader
  */
 
 object GutterIcons {
-  val RECURSION_ICON = IconLoader.getIcon("/org/jetbrains/plugins/scala/gutter/recursion.svg")
-  val TAIL_RECURSION_ICON = IconLoader.getIcon("/org/jetbrains/plugins/scala/gutter/tail-recursion.svg")
+  val RECURSION_ICON: Icon = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/recursion.svg")
+  val TAIL_RECURSION_ICON: Icon = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/tail-recursion.svg")
 
-  val OVERRIDING_METHOD_ICON = AllIcons.Gutter.OverridingMethod
-  val IMPLEMENTING_METHOD_ICON = AllIcons.Gutter.ImplementingMethod
+  val OVERRIDING_METHOD_ICON: Icon = AllIcons.Gutter.OverridingMethod
+  val IMPLEMENTING_METHOD_ICON: Icon = AllIcons.Gutter.ImplementingMethod
 
-  val OVERRIDDEN_METHOD_MARKER_RENDERER = AllIcons.Gutter.OverridenMethod
-  val IMPLEMENTED_METHOD_MARKER_RENDERER = AllIcons.Gutter.ImplementedMethod
-  val IMPLEMENTED_INTERFACE_MARKER_RENDERER = IMPLEMENTED_METHOD_MARKER_RENDERER
-  val SUBCLASSED_CLASS_MARKER_RENDERER = OVERRIDDEN_METHOD_MARKER_RENDERER
+  val OVERRIDDEN_METHOD_MARKER_RENDERER: Icon = AllIcons.Gutter.OverridenMethod
+  val IMPLEMENTED_METHOD_MARKER_RENDERER: Icon = AllIcons.Gutter.ImplementedMethod
+  val IMPLEMENTED_INTERFACE_MARKER_RENDERER: Icon = IMPLEMENTED_METHOD_MARKER_RENDERER
+  val SUBCLASSED_CLASS_MARKER_RENDERER: Icon = OVERRIDDEN_METHOD_MARKER_RENDERER
 }
