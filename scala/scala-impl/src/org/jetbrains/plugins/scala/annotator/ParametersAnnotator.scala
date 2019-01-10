e1a07233813b7a27a814317031ddff70e0118db2
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-01-10 14:23:25 +0100

    ScalaAnnotator: ScParameterAnnotator extracted
    SCL-14809
    
    (cherry picked from commit 53c7f424c75a301a319da941e01d697234d48094)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ParametersAnnotator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ParametersAnnotator.scala
@@ -1,9 +1,7 @@
 package org.jetbrains.plugins.scala.annotator
 
 import com.intellij.lang.annotation.AnnotationHolder
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScMethodLike
-import org.jetbrains.plugins.scala.lang.psi.api.expr.ScFunctionExpr
-import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScClassParameter, ScParameter, ScParameters}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameters
 
 /**
  * Pavel.Fatin, 15.06.2010
@@ -26,44 +24,5 @@ trait ParametersAnnotator {
     }
 
     checkRepeatedParams()
-  }
-
-  def annotateParameter(parameter: ScParameter, holder: AnnotationHolder): Unit = {
-    parameter.owner match {
-      case null =>
-        holder.createErrorAnnotation(parameter, "Parameter hasn't owner: " + parameter.name)
-      case _: ScMethodLike =>
-        parameter.typeElement match {
-          case None =>
-            holder.createErrorAnnotation(parameter, "Missing type annotation for parameter: " + parameter.name)
-          case _ =>
-        }
-        if (parameter.isCallByNameParameter)
-          annotateCallByNameParameter(parameter, holder: AnnotationHolder)
-      case _: ScFunctionExpr =>
-        parameter.typeElement match {
-          case None =>
-            parameter.expectedParamType match {
-              case None =>
-                holder.createErrorAnnotation(parameter, "Missing parameter type: " + parameter.name)
-              case _ =>
-            }
-          case _ =>
-        }
-    }
-  }
-
-  private def annotateCallByNameParameter(parameter: ScParameter, holder: AnnotationHolder): Any = {
-    def errorWithMessageAbout(topic: String) = {
-      val message = s"$topic parameters may not be call-by-name"
-      holder.createErrorAnnotation(parameter, message)
-    }
-    parameter match {
-      case cp: ScClassParameter if cp.isVal => errorWithMessageAbout("\'val\'")
-      case cp: ScClassParameter if cp.isVar => errorWithMessageAbout("\'var\'")
-      case cp: ScClassParameter if cp.isCaseClassVal => errorWithMessageAbout("case class")
-      case p if p.isImplicitParameter => errorWithMessageAbout("implicit")
-      case _ =>
-    }
   }
 }
