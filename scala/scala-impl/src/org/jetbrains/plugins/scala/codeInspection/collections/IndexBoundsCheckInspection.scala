449427f9dca85aaa8b987de5784208ff69ebd8cb
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2019-01-16 12:58:49 +0100

    Generalized tests and implementation of IndexBoundsCheckInspection
    SCL-8299

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/IndexBoundsCheckInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/IndexBoundsCheckInspection.scala
@@ -19,16 +19,33 @@ private object IndexBoundsCheckInspection {
     override def hint: String = InspectionBundle.message("ifstmt.to.lift")
 
     override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
-      case IfStmt(Condition(seq1, index1), Then(seq2, index2), scalaNone())
+      case IfStmt(IndexLessThanLengthCondition(seq1, index1), Then(seq2, index2), scalaNone())
         if areElementsEquivalent(seq1, seq2) && areElementsEquivalent(index1, index2) =>
-        Some(replace(expr).withText(invocationText(seq1, "lift", index1)).highlightFrom(expr))
+        createSimplification(expr, seq1, index1)
+
+      case IfStmt(IndexGreaterEqualsLengthCondition(seq1, index1), scalaNone(), Then(seq2, index2))
+        if areElementsEquivalent(seq1, seq2) && areElementsEquivalent(index1, index2) =>
+        createSimplification(expr, seq1, index1)
+
       case _ => None
     }
 
-    private object Condition {
+    private def createSimplification(expr: ScExpression, seq: ScExpression, index: ScExpression): Option[Simplification] = {
+      Some(replace(expr).withText(invocationText(seq, "lift", index)).highlightFrom(expr))
+    }
+
+    private object IndexLessThanLengthCondition {
       def unapply(expr: ScExpression): Option[(ScExpression, ScExpression)] = expr match {
         case index `<` seq `.sizeOrLength` () if isSeq(seq) => Some(seq, index)
         case seq `.sizeOrLength` () `>` index if isSeq(seq) => Some(seq, index)
+        case _ => None
+      }
+    }
+
+    private object IndexGreaterEqualsLengthCondition {
+      def unapply(expr: ScExpression): Option[(ScExpression, ScExpression)] = expr match {
+        case index `>=` seq `.sizeOrLength` () if isSeq(seq) => Some(seq, index)
+        case seq `.sizeOrLength` () `<=` index if isSeq(seq) => Some(seq, index)
         case _ => None
       }
     }
--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/collections/IndexBoundsCheckTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/collections/IndexBoundsCheckTest.scala
@@ -16,7 +16,7 @@ class IndexBoundsCheckTest extends OperationsOnCollectionInspectionTest {
   override protected val hint: String =
     InspectionBundle.message("ifstmt.to.lift")
 
-  def test(): Unit = {
+  def testSeqLess(): Unit = {
     doTest(
       s"val seq = Seq(1, 2, 3); val i = 1; val s = ${START}if (i < seq.length) Some(seq(i)) else None$END",
       "val seq = Seq(1, 2, 3); val i = 1; val s = if (i < seq.length) Some(seq(i)) else None",
@@ -24,12 +24,35 @@ class IndexBoundsCheckTest extends OperationsOnCollectionInspectionTest {
     )
   }
 
-  def test2(): Unit = {
+  def testListConstLess(): Unit = {
     doTest(
-      s"val seq = Seq(1, 2, 3); val i = 1; val s = ${START}if (seq.length > i) Some(seq(i)) else None$END",
-      "val seq = Seq(1, 2, 3); val i = 1; val s = if (seq.length > i) Some(seq(i)) else None",
-      "val seq = Seq(1, 2, 3); val i = 1; val s = seq.lift(i)"
+      s"val list = List(1, 2, 3); val s = ${START}if (1 < list.length) Some(list(1)) else None$END",
+      "val list = List(1, 2, 3); val s = if (1 < list.length) Some(list(1)) else None",
+      "val list = List(1, 2, 3); val s = list.lift(1)"
     )
   }
 
+  def testVectorGreater(): Unit = {
+    doTest(
+      s"val vec = Vector(1, 2, 3); val i = 1; val s = ${START}if (vec.length > i) Some(vec(i)) else None$END",
+      "val vec = Vector(1, 2, 3); val i = 1; val s = if (vec.length > i) Some(vec(i)) else None",
+      "val vec = Vector(1, 2, 3); val i = 1; val s = vec.lift(i)"
+    )
+  }
+
+  def testStreamLessElse(): Unit = {
+    doTest(
+      s"val s = Stream(1, 2, 3); val i = 1; val res = ${START}if (s.length <= i) None else Some(s(i))$END",
+      "val s = Stream(1, 2, 3); val i = 1; val res = if (s.length <= i) None else Some(s(i))",
+      "val s = Stream(1, 2, 3); val i = 1; val res = s.lift(i)"
+    )
+  }
+
+  def testRangeConstantGreaterElse(): Unit = {
+    doTest(
+      s"val rng = Range(1, 2, 3); val s = ${START}if (1 >= rng.length) None else Some(rng(1))$END",
+      "val rng = Range(1, 2, 3); val s = if (1 >= rng.length) None else Some(rng(1))",
+      "val rng = Range(1, 2, 3); val s = rng.lift(1)"
+    )
+  }
 }
