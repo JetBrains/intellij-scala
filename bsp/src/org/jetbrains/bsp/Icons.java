1d23072598154cefb28c6dbf376021736698a0d7
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-01-18 22:06:20 +0100

    update sbt and bsp icons. #SCL-14806 fixed

--- bsp/src/org/jetbrains/bsp/Icons.java
+++ bsp/src/org/jetbrains/bsp/Icons.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 
 public interface Icons {
     Icon BSP = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocol.svg");
-    Icon BSP_TARGET = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocol_target.svg");
+    Icon BSP_TARGET = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocolTarget.svg");
     Icon BSP_TOOLWINDOW = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/toolWindowBuildServerProtocol.svg");
 }
