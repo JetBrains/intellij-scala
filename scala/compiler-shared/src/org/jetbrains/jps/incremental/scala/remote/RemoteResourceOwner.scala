0976a1d2d4235bc336fd3f4bf8d3f3be44ee0574
Krzysztof Romanowski <romanowski.kr@gmail.com>
2019-01-18 10:23:23 +0100

    Limit number of opened sockets

--- scala/compiler-shared/src/org/jetbrains/jps/incremental/scala/remote/RemoteResourceOwner.scala
+++ scala/compiler-shared/src/org/jetbrains/jps/incremental/scala/remote/RemoteResourceOwner.scala
@@ -1,16 +1,32 @@
 package org.jetbrains.jps.incremental.scala.remote
 
 import java.io._
+import java.net.InetSocketAddress
 import java.net.{InetAddress, Socket}
+import java.nio.channels.SocketChannel
+import java.util.concurrent.Semaphore
 
 import com.martiansoftware.nailgun.NGConstants
 import org.jetbrains.jps.incremental.scala._
+
+object RemoteResourceOwner {
+  val parallelResourcesLimitPropertyName = "org.jetbrains.jps.parallelResourcesLimit"
+  val defaultParallelResourcesLimit = 20
+  val parallelResourcesLimit = Option(System.getProperty(parallelResourcesLimitPropertyName))
+  private val socketSemaphore = new Semaphore(parallelResourcesLimit.fold(defaultParallelResourcesLimit)(_.toInt))
+
+  def withConstrainedResources(block: => Unit): Unit = {
+    socketSemaphore.acquire(1)
+    try block finally socketSemaphore.release(1)
+  }
+}
 
 /**
  * @author Pavel Fatin
  * @author Dmitry Naydanov
  */
 trait RemoteResourceOwner {
+  import RemoteResourceOwner._
 
   protected val address: InetAddress
   protected val port: Int
@@ -18,18 +34,27 @@ trait RemoteResourceOwner {
   protected val currentDirectory = System.getProperty("user.dir")
   protected val serverAlias = "compile-server"
 
-  def send(command: String, arguments: Seq[String], client: Client) {
-    using(new Socket(address, port)) { socket =>
-      using(new DataOutputStream(new BufferedOutputStream(socket.getOutputStream))) { output =>
-        createChunks(command, arguments).foreach(_.writeTo(output))
-        output.flush()
-        if (client != null) {
-          using(new DataInputStream(new BufferedInputStream(socket.getInputStream))) { input =>
-            handle(input, client)
+  def send(command: String, arguments: Seq[String], client: Client): Unit = {
+    val channel = SocketChannel.open(new InetSocketAddress(address, port))
+    withConstrainedResources {
+      using(channel.socket()) { socket =>
+        using(outputStream(socket)) { output =>
+          createChunks(command, arguments).foreach(_.writeTo(output))
+          output.flush()
+          if (client != null) {
+            using(inputStream(socket))(input => handle(input, client))
           }
         }
       }
     }
+  }
+
+  private def inputStream(socket: Socket) = {
+    new DataInputStream(new BufferedInputStream(socket.getInputStream))
+  }
+
+  private def outputStream(socket: Socket) = {
+    new DataOutputStream(new BufferedOutputStream(socket.getOutputStream))
   }
 
   protected def handle(input: DataInputStream, client: Client) {
