d388b4c27e4288992d608646975eb4249637543b
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2019-01-23 18:16:14 +0300

    add highlighting test for fomkin/korolev project #SCL-13174

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/KorolevProjectHighlightingTest.scala
@@ -0,0 +1,19 @@
+package org.jetbrains.plugins.scala.projectHighlighting
+
+import org.jetbrains.plugins.scala.HighlightingTests
+import org.junit.experimental.categories.Category
+
+@Category(Array(classOf[HighlightingTests]))
+class KorolevProjectHighlightingTest extends GithubSbtAllProjectHighlightingTest {
+  override def githubUsername = "fomkin"
+
+  override def githubRepoName = "korolev"
+
+  //v.0.9.0
+  override def revision = "7d4d41113b574061aedb5a791747b0fa5d122fdf"
+
+  override def filesWithProblems = Map(
+    "GuineaPigScenarios.scala" -> Set(),
+    "package.scala" -> Set((8106, 8143), (12316, 12317))
+  )
+}
