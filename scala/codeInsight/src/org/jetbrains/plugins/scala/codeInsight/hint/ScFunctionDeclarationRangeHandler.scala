abc93ccebc7ae9d7da8b2939b605b6e96b115410
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-01-25 15:10:59 +0300

    range handlers moved #SCL-14873

---
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hint/ScFunctionDeclarationRangeHandler.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala
+package codeInsight
+package hint
+
+import com.intellij.codeInsight.hint.DeclarationRangeHandler
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
+
+/**
+  * @author Alefas
+  * @since 29/06/16
+  */
+final class ScFunctionDeclarationRangeHandler extends DeclarationRangeHandler[ScFunction] {
+
+  override def getDeclarationRange(function: ScFunction): TextRange = {
+    val startOffset = function.getModifierList.getTextRange match {
+      case null => function.getTextOffset
+      case range => range.getStartOffset
+    }
+
+    val endOffset = function.returnTypeElement
+      .fold(function.paramClauses.getTextRange.getEndOffset) {
+        _.getTextRange.getEndOffset
+      }
+    new TextRange(startOffset, endOffset)
+  }
+}
---
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hint/ScTemplateDeclarationRangeHandler.scala
@@ -0,0 +1,42 @@
+package org.jetbrains.plugins.scala
+package codeInsight
+package hint
+
+import com.intellij.codeInsight.hint.DeclarationRangeHandler
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScNewTemplateDefinition
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTemplateDefinition, ScTypeDefinition}
+
+/**
+  * @author Alefas
+  * @since 30/06/16
+  */
+final class ScTemplateDeclarationRangeHandler extends DeclarationRangeHandler[ScTemplateDefinition] {
+
+  import ScTemplateDeclarationRangeHandler._
+
+  override def getDeclarationRange(template: ScTemplateDefinition): TextRange =
+    template match {
+      case definition: ScTypeDefinition =>
+        val startOffset = template.getModifierList.getTextRange match {
+          case null => definition.getTextOffset
+          case range => range.getStartOffset
+        }
+
+        new TextRange(startOffset, endOffset(definition))
+      case definition: ScNewTemplateDefinition =>
+        val startOffset = definition.getTextRange.getStartOffset
+
+        new TextRange(startOffset, endOffset(definition))
+    }
+}
+
+object ScTemplateDeclarationRangeHandler {
+
+  private def endOffset(definition: ScTemplateDefinition) =
+    definition.extendsBlock
+      .templateBody
+      .fold(definition.getTextRange.getEndOffset) {
+        _.getTextRange.getStartOffset
+      }
+}
