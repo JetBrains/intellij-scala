f39b6b19690e7e78aa0717564c3dc62041cd03aa
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-01-25 17:28:57 +0300

    GenerateToStringScalaClassFilter moved #SCL-14873

---
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/generation/GenerateToStringScalaClassFilter.scala
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.scala
+package codeInsight
+package generation
+
+import com.intellij.psi.PsiClass
+import org.jetbrains.generate.tostring.GenerateToStringClassFilter
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTemplateDefinition
+import org.jetbrains.plugins.scala.lang.psi.light.PsiClassWrapper
+
+/**
+  * @author Nikolay.Tropin
+  */
+final class GenerateToStringScalaClassFilter extends GenerateToStringClassFilter {
+
+  override def canGenerateToString(clazz: PsiClass): Boolean = clazz match {
+    case _: ScTemplateDefinition |
+         _: PsiClassWrapper => false
+    case _ => true
+  }
+}
