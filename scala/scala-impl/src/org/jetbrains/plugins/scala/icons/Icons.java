3284669f04c22c920f0faeb1997b951854eb4255
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-01-29 11:57:23 +0100

    remove scala16.svg icon. #UX-377

--- scala/scala-impl/src/org/jetbrains/plugins/scala/icons/Icons.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/icons/Icons.java
@@ -24,7 +24,7 @@ public interface Icons {
 
   Icon COMPILE_SERVER = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/compileServer.svg");
 
-  Icon FILE_TYPE_LOGO = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/scala16.svg");
+  Icon FILE_TYPE_LOGO = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/file_scala.svg");
   //todo worksheet logo
   Icon WORKSHEET_LOGO = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/file_scala.svg");
   Icon SCALA_SMALL_LOGO = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/scala-small-logo.svg");
@@ -36,7 +36,7 @@ public interface Icons {
   //SDK configuration
   Icon SCALA_SDK = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/scala_sdk.svg");
   Icon NO_SCALA_SDK = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/no_scala_sdk.svg");
-  
+
   //Toplevel nodes
   Icon FILE = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/file_scala.svg");
   Icon CLASS = IconLoader.getIcon("/org/jetbrains/plugins/scala/images/class_scala.svg");
