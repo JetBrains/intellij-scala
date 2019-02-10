b004a8b929e10bfea0ae7f1c826807a482856ebc
andrey.sugak <andrey.sugak@jetbrains.com>
2019-02-10 18:59:40 +0300

    Parse flags for scalac highlighting tests
    #SCL-14956

--- scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/SeveralFilesHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/SeveralFilesHighlightingTest.scala
@@ -2,12 +2,15 @@ package org.jetbrains.plugins.scala.projectHighlighting
 
 import java.io.File
 
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import org.apache.commons.io.FilenameUtils
 import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.extensions.{inWriteAction, using}
 import org.jetbrains.plugins.scala.util.PsiFileTestUtil
 import org.jetbrains.plugins.scala.util.reporter.ProgressReporter
+import org.jetbrains.plugins.scala.project._
 
 import scala.io.Source
 
@@ -19,18 +22,24 @@ trait SeveralFilesHighlightingTest {
 
   def getProject: Project
 
+  def module: Module
+
   def filesToHighlight: Array[File]
 
   val reporter: ProgressReporter
 
   def doTest(): Unit = {
-    val files = filesToHighlight.filter(f => f.isDirectory || isScalaFile(f))
+    val allFiles = filesToHighlight
+      .filter(f => f.isDirectory || isScalaFile(f) || isFlagsFile(f))
+      .groupBy(f => FilenameUtils.removeExtension(f.getPath))
 
-    for ((file, idx) <- files.zipWithIndex) {
-      if (files.length > 1) {
-        reporter.notify(s"${file.getName} (${idx + 1} of ${files.length})")
+    var idx = 0
+    for ((basePath, files) <- allFiles) {
+      if (allFiles.size > 1) {
+        reporter.notify(s"$basePath (${idx + 1} of ${allFiles.size})")
       }
-      annotateFiles(file, reporter)
+      annotateFiles(files, reporter)
+      idx += 1
     }
     reporter.reportResults()
   }
@@ -47,22 +56,32 @@ trait SeveralFilesHighlightingTest {
     inWriteAction(psiFile.delete())
   }
 
-  private def annotateFiles(file: File, reporter: ProgressReporter): Unit = {
+  private def annotateFiles(files: Array[File], reporter: ProgressReporter): Unit = {
     def allFiles(f: File): Seq[File] =
       if (f.isDirectory) f.listFiles.flatMap(allFiles)
-      else Seq(f)
+      else               Seq(f)
 
-    val root = if (file.isDirectory) file else file.getParentFile
+    def parseScalacFlags(f: File): Seq[String] =
+      using(Source.fromFile(f, "UTF-8"))(_.getLines().map(_.trim).filter(_.nonEmpty).toList)
 
-    val addedFiles = allFiles(file).map(addFileToProject(_, relativeTo = root))
+    val root = files match {
+      case Array(file) if file.isDirectory => file
+      case Array(file, _*)                 => file.getParentFile
+    }
+
+    val (flagFiles, sourceFiles) = files.flatMap(allFiles).partition(isFlagsFile)
+    val addedFiles = sourceFiles.map(addFileToProject(_, relativeTo = root))
 
     try {
+      module.scalaCompilerSettings.additionalCompilerOptions = flagFiles.flatMap(parseScalacFlags)
       addedFiles.foreach(AllProjectHighlightingTest.annotateFile(_, reporter))
     } finally {
       addedFiles.foreach(removeFile)
+      module.scalaCompilerSettings.additionalCompilerOptions = Seq.empty
     }
   }
 
   private def isScalaFile(f: File) = f.getName.endsWith(ScalaFileType.INSTANCE.getDefaultExtension)
+  private def isFlagsFile(f: File) = f.getName.endsWith("flags")
 
 }
