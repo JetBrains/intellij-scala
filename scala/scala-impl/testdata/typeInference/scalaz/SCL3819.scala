f8f43e2b37b394bea339f10d0a08352a79b72a8f
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2019-02-14 13:11:33 +0300

    adapt testdata for scalaz 7.1.0, it doesn't have `Pure` trait #SCL-14952

--- scala/scala-impl/testdata/typeInference/scalaz/SCL3819.scala
+++ scala/scala-impl/testdata/typeInference/scalaz/SCL3819.scala
@@ -2,11 +2,13 @@ import scalaz._
 import scalaz.Scalaz._
 
 class MyApp[S] {
-  def foo(p: Pure[({type λ[α]=State[S, α]})#λ]) = 1
+  def foo(p: Applicative[({type λ[α]=State[S, α]})#λ]) = 1
   def foo(p: Int) = ""
 
-  /*start*/foo(new Pure[({type λ[α]=State[S, α]})#λ] {
-    def pure[A](a: => A) = a.state[S]
+  /*start*/foo(new Applicative[({type λ[α]=State[S, α]})#λ] {
+    def point[A](a: => A) = a.state[S]
+
+    def ap[A, B](fa: => State[S, A])(f: => State[S, A => B]): State[S, B] = ???
   })/*end*/
 }
 //Int
