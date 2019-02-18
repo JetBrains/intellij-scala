48c560ceb25c4a5d8292cc1a0d416dd2674e82e3
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-02-18 16:02:31 +0100

    bsp: generator tests for mergeModule, projectNode. Update some generators.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -7,7 +7,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val zincVersion = "1.1.1"
   val ideaVersion = "191.5701.16"
-  val bspVersion = "2.0.0-M3+28-d311d820"
+  val bspVersion = "2.0.0-M3+29-e5a065c9"
   val sbtStructureVersion: String = "2018.2.1+4-88400d3f"
   val sbtIdeaShellVersion: String = "2018.3"
   val sbtIdeaCompilerIndicesVersion = "0.1.1"
