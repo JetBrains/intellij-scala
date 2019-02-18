321486712712244c57b3231a6910bc9509271aa1
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-02-18 16:02:31 +0100

    bsp: generator tests for mergeModule, projectNode. Update some generators.

--- bsp/test/org/jetbrains/bsp/project/resolver/BspResolverLogicProperties.scala
+++ bsp/test/org/jetbrains/bsp/project/resolver/BspResolverLogicProperties.scala
@@ -4,10 +4,10 @@ import java.io.File
 import java.nio.file.Path
 
 import ch.epfl.scala.bsp.testkit.gen.Bsp4jGenerators._
-import ch.epfl.scala.bsp.testkit.gen.UtilGenerators._
 import ch.epfl.scala.bsp.testkit.gen.bsp4jArbitrary._
 import ch.epfl.scala.bsp4j._
 import com.google.gson.{Gson, GsonBuilder}
+import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.util.io.FileUtil
 import org.jetbrains.bsp.project.resolver.BspResolverDescriptors.{ModuleDescription, ScalaModule, SourceDirectory}
 import org.jetbrains.bsp.project.resolver.BspResolverLogic._
@@ -41,7 +41,7 @@ object BspResolverLogicProperties extends Properties("BspResolverLogic functions
   }
 
   property("calculateModuleDescriptions succeeds for build targets with Scala") =
-    forAll(Gen.listOf(genScalaBuildTarget(List(BuildTargetTag.NO_IDE)))) { buildTargets: List[BuildTarget] =>
+    forAll(Gen.listOf(genScalaBuildTargetWithoutTags(List(BuildTargetTag.NO_IDE)))) { buildTargets: List[BuildTarget] =>
       forAll { (optionsItems: List[ScalacOptionsItem], sourcesItems: List[SourcesItem], dependencySourcesItems: List[DependencySourcesItem]) =>
         val descriptions = calculateModuleDescriptions(buildTargets, optionsItems, sourcesItems, dependencySourcesItems)
         (buildTargets.nonEmpty && buildTargets.exists(_.getBaseDirectory != null)) ==> descriptions.nonEmpty
@@ -60,33 +60,53 @@ object BspResolverLogicProperties extends Properties("BspResolverLogic functions
     }
 
   property("createScalaModuleDescription") =
-    forAll(Gen.listOf(genBuildTargetTag)) { tags: List[String] =>
-      forAll { (target: BuildTarget, moduleBase: File, outputPath: Option[File], sourceRoots: List[SourceDirectory], classpath: List[File], dependencySources: List[File]) =>
-        val description = createScalaModuleDescription(target, tags, moduleBase, outputPath, sourceRoots, classpath, dependencySources)
+    forAll(genPath, Gen.listOf(genBuildTargetTag)) { (basePath: Path, tags: List[String]) =>
+      forAll(Gen.listOf(genSourceDirectory(basePath))) { sourceRoots: List[SourceDirectory] =>
+        forAll { (target: BuildTarget, moduleBase: File, outputPath: Option[File], classpath: List[File], dependencySources: List[File]) =>
+          val description = createScalaModuleDescription(target, tags, moduleBase, outputPath, sourceRoots, classpath, dependencySources)
 
-        val p1 = (description.basePath == moduleBase) :| "base path should be set"
-        val p2 = (tags.contains(BuildTargetTag.LIBRARY) || tags.contains(BuildTargetTag.APPLICATION)) ==>
-          (description.output == outputPath &&
-          description.targetDependencies == target.getDependencies.asScala &&
-          description.classPathSources == dependencySources &&
-          description.sourceDirs == sourceRoots &&
-          description.classPath == classpath) :|
-            s"data not correctly set for library or application tags. Result data was: $description"
-        val p3 = tags.contains(BuildTargetTag.TEST) ==>
-          (description.testOutput == outputPath &&
-          description.targetTestDependencies == target.getDependencies.asScala &&
-          description.testClassPathSources == dependencySources &&
-          description.testSourceDirs == sourceRoots &&
-          description.testClassPath == classpath) :|
-            s"data not correctly set for test tag. Result data was: $description"
+          val p1 = (description.basePath == moduleBase) :| "base path should be set"
+          val p2 = (tags.contains(BuildTargetTag.LIBRARY) || tags.contains(BuildTargetTag.APPLICATION)) ==>
+            (description.output == outputPath &&
+              description.targetDependencies == target.getDependencies.asScala &&
+              description.classpathSources == dependencySources &&
+              description.sourceDirs == sourceRoots &&
+              description.classpath == classpath) :|
+              s"data not correctly set for library or application tags. Result data was: $description"
+          val p3 = tags.contains(BuildTargetTag.TEST) ==>
+            (description.testOutput == outputPath &&
+              description.targetTestDependencies == target.getDependencies.asScala &&
+              description.testClasspathSources == dependencySources &&
+              description.testSourceDirs == sourceRoots &&
+              description.testClasspath == classpath) :|
+              s"data not correctly set for test tag. Result data was: $description"
 
-        p1 && p2 && p3
+          p1 && p2 && p3
+        }
       }
     }
 
-  property("mergeModules") = forAll { descriptions: List[ModuleDescription] =>
-    false
+  property("mergeModules succeeds") = forAll { (description1: ModuleDescription, description2: ModuleDescription) =>
+    val data1 = description1.data
+    val data2 = description2.data
+    val merged = mergeModules(List(description1, description2))
+    val data = merged.data
+
+    // TODO more thorough properties
+    data.basePath == data1.basePath &&
+      data.targets == (data1.targets ++ data2.targets).sortBy(_.getId.getUri)
   }
 
+  property("projectNode succeeds") = forAll {
+    (root: Path, moduleFilesDir: Path, moduleDescriptions: List[ModuleDescription]) =>
+
+    val projectRootPath = root.toString
+    val node = projectNode(projectRootPath, moduleFilesDir.toString, moduleDescriptions)
+
+    node.getChildren.size >= moduleDescriptions.size
+      node.getChildren.asScala.exists { node =>
+        node.getData(ProjectKeys.MODULE).getLinkedExternalProjectPath == projectRootPath
+      }
+  }
 
 }
