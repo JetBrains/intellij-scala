8a6eacb1db693ffa98fd3976b6cafe4fb4008069
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-03-07 21:49:53 +0300

    MakeNonPrivateQuickFix rewritten #EA-138818

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/ModifierQuickFix.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/ModifierQuickFix.scala
@@ -8,7 +8,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.tree.IElementType
-import com.intellij.psi.{PsiElement, PsiFile}
+import com.intellij.psi.{PsiDocumentManager, PsiElement, PsiFile}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaModifier
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScModifierList
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScModifierListOwner
@@ -16,30 +16,28 @@ import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 
 sealed abstract class ModifierQuickFix(listOwner: ScModifierListOwner)
                                       (key: String, nameId: PsiElement)
-                                      (modifier: ScalaModifier, value: Boolean)
+                                      (modifier: ScalaModifier, value: Boolean = false)
   extends intention.IntentionAction {
 
-  override final def isAvailable(project: Project,
-                                 editor: Editor,
-                                 file: PsiFile): Boolean =
+  override final def isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean =
     listOwner.isValid &&
+      listOwner.getContainingFile == file &&
       listOwner.getManager.isInProject(file)
 
-  override final def invoke(project: Project,
-                            editor: Editor,
-                            file: PsiFile): Unit = listOwner.getModifierList match {
-    case null => ModifierQuickFix.logger.error("Illegal modifier list in: " + listOwner)
-    case modifierList => onModifierList(modifierList)(project, file)
-  }
+  override final def invoke(project: Project, editor: Editor, file: PsiFile): Unit =
+    listOwner.getModifierList match {
+      case null => ModifierQuickFix.logger.error("Illegal modifier list in: " + listOwner)
+      case modifierList => onModifierList(modifierList)(project, editor, file)
+    }
 
   protected def onModifierList(modifierList: ScModifierList)
-                              (implicit project: Project, file: PsiFile): Unit = {
+                              (implicit project: Project, editor: Editor, file: PsiFile): Unit = {
     modifierList.setModifierProperty(modifier.text, value)
   }
 
   override final def startInWriteAction = true
 
-  override final val getFamilyName: String = {
+  override final val getText: String = {
     val modifierText = modifier.text
     nameId match {
       case null => ScalaBundle.message(key, modifierText)
@@ -47,18 +45,20 @@ sealed abstract class ModifierQuickFix(listOwner: ScModifierListOwner)
     }
   }
 
-  override final def getText: String = getFamilyName
+  override def getFamilyName: String = getText
 }
 
 object ModifierQuickFix {
 
+  import ScalaModifier._
+
   private val logger = Logger.getInstance(getClass)
 
   final class Remove(listOwner: ScModifierListOwner, nameId: PsiElement, modifier: ScalaModifier)
-    extends ModifierQuickFix(listOwner)("remove.modifier.fix", nameId)(modifier, value = false) {
+    extends ModifierQuickFix(listOwner)("remove.modifier.fix", nameId)(modifier) {
 
     override def onModifierList(modifierList: ScModifierList)
-                               (implicit project: Project, file: PsiFile): Unit = {
+                               (implicit project: Project, editor: Editor, file: PsiFile): Unit = {
       super.onModifierList(modifierList)
 
       // Should be handled by auto formatting
@@ -75,10 +75,10 @@ object ModifierQuickFix {
     extends ModifierQuickFix(listOwner)("add.modifier.fix", nameId)(modifier, value = true)
 
   final class AddWithKeyword(listOwner: ScModifierListOwner, nameId: PsiElement, keywordType: IElementType)
-    extends Add(listOwner, nameId, ScalaModifier.Override) {
+    extends Add(listOwner, nameId, Override) {
 
     override def onModifierList(modifierList: ScModifierList)
-                               (implicit project: Project, file: PsiFile): Unit = {
+                               (implicit project: Project, editor: Editor, file: PsiFile): Unit = {
       val keyword = ScalaPsiElementFactory.createDeclarationFromText(
         keywordType.toString + " x",
         listOwner.getParent,
@@ -86,6 +86,33 @@ object ModifierQuickFix {
       ).findFirstChildByType(keywordType)
       listOwner.addAfter(keyword, modifierList)
 
+      super.onModifierList(modifierList)
+    }
+  }
+
+  sealed abstract class MakeNonPrivate(listOwner: ScModifierListOwner, key: String)
+                                      (modifier: ScalaModifier, value: Boolean)
+    extends ModifierQuickFix(listOwner)(key, null)(modifier, value) {
+
+    override protected def onModifierList(modifierList: ScModifierList)
+                                         (implicit project: Project, editor: Editor, file: PsiFile): Unit = {
+      super.onModifierList(modifierList)
+      PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(editor.getDocument)
+      CodeStyleManager.getInstance(project).adjustLineIndent(file, modifierList.getTextRange.getStartOffset)
+    }
+
+    override def getFamilyName: String = ScalaBundle.message("make.non.private.title")
+  }
+
+  final class MakePublic(listOwner: ScModifierListOwner)
+    extends MakeNonPrivate(listOwner, "make.public.fix")(Private, false)
+
+  final class MakeProtected(listOwner: ScModifierListOwner)
+    extends MakeNonPrivate(listOwner, "make.protected.fix")(Protected, value = true) {
+
+    override protected def onModifierList(modifierList: ScModifierList)
+                                         (implicit project: Project, editor: Editor, file: PsiFile): Unit = {
+      modifierList.setModifierProperty(PRIVATE, false)
       super.onModifierList(modifierList)
     }
   }
--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/template/PrivateBeanProperty.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/template/PrivateBeanProperty.scala
@@ -1,39 +1,44 @@
 package org.jetbrains.plugins.scala
-package annotator.template
+package annotator
+package template
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.plugins.scala.annotator.AnnotatorPart
-import org.jetbrains.plugins.scala.annotator.quickfix.modifiers.MakeNonPrivateQuickFix
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScAnnotation
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScValue, ScVariable}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScValueOrVariable
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScMember
 
 /**
- * Nikolay.Tropin
- * 11/6/13
- */
+  * Nikolay.Tropin
+  * 11/6/13
+  */
 object PrivateBeanProperty extends AnnotatorPart[ScAnnotation] {
-  def annotate(element: ScAnnotation, holder: AnnotationHolder, typeAware: Boolean = false): Unit = {
-    if (!isBeanPropertyAnnotation(element)) return
-    val member = PsiTreeUtil.getParentOfType(element, classOf[ScMember])
-    def registerProblem() = {
-      val toPublicFix = new MakeNonPrivateQuickFix(member, toProtected = false)
-      val toProtectedFix = new MakeNonPrivateQuickFix(member, toProtected = true)
-      val privateMod = member.getModifierList.accessModifier.get
-      val errorAnnotation = holder.createErrorAnnotation(privateMod, "Bean property should not be private")
-      errorAnnotation.registerFix(toPublicFix)
-      errorAnnotation.registerFix(toProtectedFix)
-    }
-    member match {
-      case v: ScVariable if v.isPrivate => registerProblem()
-      case v: ScValue if v.isPrivate => registerProblem()
-      case _ =>
-    }
-  }
 
-  private def isBeanPropertyAnnotation(annot: ScAnnotation) = {
-    val text = annot.getText
-    text == "@BeanProperty" || text == "@BooleanBeanProperty"
-  }
+  def annotate(annotation: ScAnnotation,
+               holder: AnnotationHolder,
+               typeAware: Boolean = false): Unit =
+    if (isBeanPropertyAnnotation(annotation)) {
+
+      PsiTreeUtil.getParentOfType(annotation, classOf[ScMember]) match {
+        case property: ScValueOrVariable =>
+          for {
+            privateModifier <- property.getModifierList.accessModifier
+            if privateModifier.isPrivate
+
+            errorAnnotation = holder.createErrorAnnotation(privateModifier, "Bean property should not be private")
+          } {
+            import quickfix.ModifierQuickFix._
+            errorAnnotation.registerFix(new MakePublic(property))
+            errorAnnotation.registerFix(new MakeProtected(property))
+          }
+        case _ =>
+      }
+    }
+
+  private def isBeanPropertyAnnotation(annotation: ScAnnotation) =
+    annotation.getText match {
+      case "@BeanProperty" |
+           "@BooleanBeanProperty" => true
+      case _ => false
+    }
 }
