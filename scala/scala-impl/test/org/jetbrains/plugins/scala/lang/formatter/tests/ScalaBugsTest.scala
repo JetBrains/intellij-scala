1b03745a33dd0655024830a20f39baf90cfc1533
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-03-11 12:06:10 +0300

    do not indent closing bracket when trailing comma is present #SCL-15090 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/formatter/tests/ScalaBugsTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/formatter/tests/ScalaBugsTest.scala
@@ -3273,6 +3273,44 @@ bars foreach {case (x, y) => list.add(x + y)}
     doTextTest(before)
   }
 
+  def testSCL15090_1_WithTrailingComma(): Unit = {
+    getCommonSettings.ALIGN_MULTILINE_PARAMETERS_IN_CALLS = true
+    val before =
+      """List(
+        |    1123123,
+        |  42,
+        |  312321323,
+        |          )
+      """.stripMargin
+    val after =
+      """List(
+        |  1123123,
+        |  42,
+        |  312321323,
+        |)
+      """.stripMargin
+    doTextTest(before, after)
+  }
+
+  def testSCL15090_2_WithoutTrailingComma()(): Unit = {
+    getCommonSettings.ALIGN_MULTILINE_PARAMETERS_IN_CALLS = true
+    val before =
+      """List(
+        |    1123123,
+        |  42,
+        |  312321323
+        |          )
+      """.stripMargin
+    val after =
+      """List(
+        |  1123123,
+        |  42,
+        |  312321323
+        |)
+      """.stripMargin
+    doTextTest(before, after)
+  }
+
 //  def testSCL12461(): Unit = {
 //    getCommonSettings.ALIGN_MULTILINE_PARAMETERS_IN_CALLS = true
 //    val before =
