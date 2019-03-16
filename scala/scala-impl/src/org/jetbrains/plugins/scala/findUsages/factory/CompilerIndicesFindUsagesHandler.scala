3f374d35eb42bf11da93798c4a0977bfee91af7c
andrey.sugak <andrey.sugak@jetbrains.com>
2019-03-17 01:46:20 +0300

    Fix usages search for inherited members via bytecode indices
    #SCL-15114 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/factory/CompilerIndicesFindUsagesHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/factory/CompilerIndicesFindUsagesHandler.scala
@@ -4,11 +4,15 @@ import java.util
 import java.util.Collections
 
 import com.intellij.find.findUsages.FindUsagesOptions
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.{PsiElement, PsiNamedElement}
+import com.intellij.psi.search.searches.ClassInheritorsSearch
+import com.intellij.psi.{PsiClass, PsiElement, PsiMethod, PsiNamedElement}
 import com.intellij.usageView.UsageInfo
 import com.intellij.util.Processor
+import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.findUsages.compilerReferences.search.CompilerIndicesReferencesSearch
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
 
 /**
   * Find usages handler, which relies solely on compiler indices.
@@ -19,17 +23,43 @@ class CompilerIndicesFindUsagesHandler(
   e:       PsiElement,
   factory: ScalaFindUsagesHandlerFactory
 ) extends ScalaFindUsagesHandlerBase(e, factory) {
+  private[this] val pfindex = ProjectFileIndex.getInstance(e.getProject)
+
+  private[this] def isInLibrary(element: PsiElement): Boolean =
+    (for {
+      file  <- element.getContainingFile.toOption
+      vfile <- file.getVirtualFile.toOption
+    } yield pfindex.isInLibrary(vfile)).getOrElse(false)
+
+  private[this] def searchInCompilerIndices(
+    e:         PsiNamedElement,
+    processor: Processor[UsageInfo]
+  ): Boolean =
+    CompilerIndicesReferencesSearch
+      .search(e)
+      .forEach(ref => processor.process(new UsageInfo(ref)))
 
   override def processElementUsages(
     element:   PsiElement,
     processor: Processor[UsageInfo],
     options:   FindUsagesOptions
   ): Boolean = element match {
-    case named: PsiNamedElement =>
-      CompilerIndicesReferencesSearch
-        .search(named)
-        .forEach(ref => processor.process(new UsageInfo(ref)))
-    case _ => false
+    case (named: PsiNamedElement) && ContainingClass(cls: ScTypeDefinition) if isInLibrary(element) =>
+      val res = searchInCompilerIndices(named, processor)
+      // this inheritor-traversing logic is already present in ScalaCompilerReferenceReader,
+      // but as library jars are not indexed, we have to partially rely on standard searchers instead.
+      if (res) {
+        ClassInheritorsSearch.search(cls).forEach(inheritor => {
+          val inherited = named match {
+            case cls: PsiClass     => inheritor.findInnerClassByName(cls.name, true).toOption
+            case method: PsiMethod => inheritor.findMethodBySignature(method, true).toOption
+            case _                 => inheritor.findMethodsByName(named.name, true).find(m => !m.hasParameters)
+          }
+          inherited.forall(searchInCompilerIndices(_, processor))
+        })
+      } else false
+    case named: PsiNamedElement => searchInCompilerIndices(named, processor)
+    case _                      => false
   }
 
   override def processUsagesInText(
