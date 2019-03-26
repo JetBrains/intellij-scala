1a3453b5141d9a68cb8f41cbe3f0261e0c027839
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-03-26 17:35:04 +0300

    ScalaLibraryHighlightingTest_2_12 refactored #SCL-15156

--- scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/ScalaLibraryHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/ScalaLibraryHighlightingTest.scala
@@ -1,47 +1,58 @@
-package org.jetbrains.plugins.scala.projectHighlighting
+package org.jetbrains.plugins.scala
+package projectHighlighting
 
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.{VfsUtilCore, VirtualFile}
 import com.intellij.psi.PsiManager
-import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
-import org.jetbrains.plugins.scala.base.libraryLoaders.ScalaSDKLoader
-import org.jetbrains.plugins.scala.debugger.{CustomVersion, ScalaVersion}
-import org.jetbrains.plugins.scala.util.reporter.ProgressReporter
-import org.jetbrains.plugins.scala.{HighlightingTests, ScalaFileType}
 import org.junit.experimental.categories.Category
 
-
 /**
-  * Nikolay.Tropin
-  * 27-Sep-17
-  */
-abstract class ScalaLibraryHighlightingTest extends ScalaLightCodeInsightFixtureTestAdapter {
+ * Nikolay.Tropin
+ * 27-Sep-17
+ */
+@Category(Array(classOf[HighlightingTests]))
+class ScalaLibraryHighlightingTest extends base.ScalaLightCodeInsightFixtureTestAdapter {
 
-  val filesWithProblems: Map[String, Set[TextRange]]
+  import base.libraryLoaders.ScalaSDKLoader
+  import util.reporter.ProgressReporter
+
+  private val filesWithProblems = Map(
+    "scala/Responder.scala" -> Set[TextRange]((1987, 1988), (2133, 2134), (2278, 2279))
+  )
+  //noinspection TypeAnnotation
+  override implicit val version = debugger.CustomVersion("2.12", "2.12.8")
 
   def testHighlightScalaLibrary(): Unit = {
-    val reporter = ProgressReporter.newInstance(getClass.getSimpleName, filesWithProblems, reportStatus = false)
+    val reporter = ProgressReporter.newInstance(
+      getClass.getSimpleName,
+      filesWithProblems,
+      reportStatus = false
+    )
     val sources = ScalaSDKLoader().resolveSources
-    VfsUtilCore.processFilesRecursively(sources, (vFile: VirtualFile) => {
-      if (vFile.getFileType == ScalaFileType.INSTANCE) {
-        val relPath = VfsUtilCore.getRelativePath(vFile, sources)
-        reporter.notify(relPath)
 
-        val psiFile = PsiManager.getInstance(getProject).findFile(vFile)
-        AllProjectHighlightingTest.annotateFile(psiFile, reporter, Some(relPath))
+    VfsUtilCore.processFilesRecursively(
+      sources,
+      (file: VirtualFile) => {
+        annotateFile(file, sources)(reporter)
+        true
       }
-      true
-    })
+    )
     reporter.reportResults()
   }
-}
 
-@Category(Array(classOf[HighlightingTests]))
-class ScalaLibraryHighlightingTest_2_12 extends ScalaLibraryHighlightingTest {
+  private def annotateFile(file: VirtualFile,
+                           ancestor: VirtualFile)
+                          (implicit reporter: ProgressReporter): Unit =
+    file.getFileType match {
+      case ScalaFileType.INSTANCE =>
+        val relPath = VfsUtilCore.getRelativePath(file, ancestor)
+        reporter.notify(relPath)
 
-  override val filesWithProblems = Map(
-    "scala/Responder.scala" -> Set((1987, 1988),(2133, 2134),(2278, 2279))
-  )
-
-  override implicit val version: ScalaVersion = CustomVersion("2.12", "2.12.8")
+        AllProjectHighlightingTest.annotateFile(
+          PsiManager.getInstance(getProject).findFile(file),
+          reporter,
+          Some(relPath)
+        )
+      case _ =>
+    }
 }
