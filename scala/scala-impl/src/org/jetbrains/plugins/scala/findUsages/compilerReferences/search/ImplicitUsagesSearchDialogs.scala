c91ac6a409dd4d1ad194f1125dce161afb7461d4
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-04-02 15:23:06 +0200

    ImplicitUsagesSearchDialogs: checkbox focus & mnemonic, inline settings link #SCL-15078

--- scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/compilerReferences/search/ImplicitUsagesSearchDialogs.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/compilerReferences/search/ImplicitUsagesSearchDialogs.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.findUsages.compilerReferences.search
 
+import java.awt.FlowLayout
 import java.awt.event.ActionEvent
 
 import com.intellij.openapi.application.ex.ApplicationManagerEx
@@ -7,10 +8,11 @@ import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.psi.PsiNamedElement
+import com.intellij.ui.HyperlinkLabel
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.labels.LinkLabel
 import com.intellij.util.ui.FormBuilder
-import javax.swing.{Action, JComponent, JLabel}
+import javax.swing._
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.findUsages.compilerReferences.SearchTargetExtractors.UsageType
 import org.jetbrains.plugins.scala.findUsages.compilerReferences.settings.{CompilerIndicesConfigurable, CompilerIndicesSbtSettings, CompilerIndicesSettings}
@@ -70,42 +72,52 @@ private object ImplicitUsagesSearchDialogs {
     title:       String
   ) extends DialogWrapper(element.getProject, canBeParent, DialogWrapper.IdeModalityType.PROJECT) {
 
-    private[this] val shouldCompileCB = new JBCheckBox(
-      "Pre-compile modules in use scope before searching",
-      !CompilerIndicesSbtSettings().useManualConfiguration
-    )
-
-    private[this] val settingsLink =
-      new LinkLabel[AnyRef]("Settings", null) {
-        setListener({
-          case (_, _) =>
-            ShowSettingsUtil.getInstance().showSettingsDialog(element.getProject, classOf[CompilerIndicesConfigurable])
-        }, null)
-      }
+    private[this] val shouldCompileCB = {
+      val checkBox = new JBCheckBox(
+        "Pre-compile modules in use scope before searching",
+        !CompilerIndicesSbtSettings().useManualConfiguration)
+      checkBox.setMnemonic('P')
+      checkBox
+    }
 
     def shouldCompile: Boolean = shouldCompileCB.isSelected
-
-    private[this] val indicesStatusMessage: String =
-      s"""|<html>
-          |<body>
-          |This search relies on the bytecode, which is not up-to-date. <br>
-          |Results may be incomplete without a compilation.
-          |</body>
-          |</html>
-          |""".stripMargin
 
     setTitle(ScalaBundle.message("find.usages.compiler.indices.dialog.title", title))
     setResizable(false)
     init()
 
-    override def createCenterPanel(): JComponent =
+    override def createCenterPanel(): JComponent = {
+      val firstLine = {
+        val settingsLink = {
+          val link = new HyperlinkLabel("bytecode")
+          link.setToolTipText("Settings | Bytecode Indices")
+          link.addHyperlinkListener(_ => ShowSettingsUtil.getInstance().showSettingsDialog(element.getProject, classOf[CompilerIndicesConfigurable]))
+
+          val linkPanel = new JPanel()
+          linkPanel.setLayout(new BoxLayout(linkPanel, BoxLayout.Y_AXIS))
+          linkPanel.add(Box.createVerticalStrut(2))
+          linkPanel.add(link)
+
+          linkPanel
+        }
+
+        val line = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 0))
+        line.add(new JLabel("This search relies on the "))
+        line.add(settingsLink)
+        line.add(new JLabel(", which is not up-to-date."))
+        line
+      }
+
       FormBuilder
         .createFormBuilder()
-        .addComponent(new JLabel(indicesStatusMessage))
-        .addVerticalGap(3)
+        .addComponent(firstLine)
+        .addVerticalGap(1)
+        .addComponent(new JLabel("Results may be incomplete without a compilation."))
+        .addVerticalGap(1)
         .addComponent(shouldCompileCB)
-        .addVerticalGap(5)
-        .addComponent(settingsLink)
         .getPanel
+    }
+
+    override def getPreferredFocusedComponent: JComponent = shouldCompileCB
   }
 }
