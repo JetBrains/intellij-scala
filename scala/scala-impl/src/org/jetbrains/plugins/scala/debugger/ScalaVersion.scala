b049027b3efd2125d995b42ad12522caabd02859
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-04-04 12:51:39 +0300

    ScalaVersion.minor is ScalaLanguageLevel.getVersion #SCL-15156
    (cherry picked from commit e48c9c26098d8f3026a19cdc71ab259d60a9e062)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/ScalaVersion.scala
@@ -1,34 +1,44 @@
-package org.jetbrains.plugins.scala.debugger
+package org.jetbrains.plugins.scala
+package debugger
+
+import org.jetbrains.plugins.scala.project.ScalaLanguageLevel
 
 /**
  * @author Nikolay.Tropin
  */
-sealed trait ScalaVersion {
-  val major: String
-  val minor: String
+sealed abstract class ScalaVersion(val languageLevel: ScalaLanguageLevel,
+                                   val minor: String) {
+  def major: String = languageLevel.getVersion
 }
 
-case object Scala_2_9 extends ScalaVersion {
-  override final val major: String = "2.9"
-  override final val minor: String = "2.9.3"
+case object Scala_2_9 extends ScalaVersion(
+  ScalaLanguageLevel.Scala_2_9,
+  "2.9.3"
+)
+
+case object Scala_2_10 extends ScalaVersion(
+  ScalaLanguageLevel.Scala_2_10,
+  "2.10.7"
+)
+
+case object Scala_2_11 extends ScalaVersion(
+  ScalaLanguageLevel.Scala_2_11,
+  "2.11.12"
+)
+
+case object Scala_2_12 extends ScalaVersion(
+  ScalaLanguageLevel.Scala_2_12,
+  "2.12.3"
+)
+
+case object Scala_2_13 extends ScalaVersion(
+  ScalaLanguageLevel.Scala_2_13,
+  "2.13.0-M4"
+) {
+  override val major: String = minor
 }
 
-case object Scala_2_10 extends ScalaVersion {
-  override final val major: String = "2.10"
-  override final val minor: String = "2.10.7"
-}
-
-case object Scala_2_11 extends ScalaVersion {
-  override final val major: String = "2.11"
-  override final val minor: String = "2.11.12"
-}
-
-case object Scala_2_12 extends ScalaVersion {
-  override final val major: String = "2.12"
-  override final val minor: String = "2.12.3"
-}
-
-case object Scala_2_13 extends ScalaVersion {
-  override final val major: String = "2.13.0-M4"
-  override final val minor: String = "2.13.0-M4"
-}
+case object Scala_3_0 extends ScalaVersion(
+  ScalaLanguageLevel.Scala_3_0,
+  "0.13.0-RC1"
+)
