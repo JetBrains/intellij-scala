54d63bb78348bf4022585c0395281e7f0bd64f94
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-04-09 19:06:38 +0300

    dotty specific psi removed #SCL-14439
    (cherry picked from commit bd2bc0dfe1a66a944940264dd10ee39e2dd649a6)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/ScalaElementType.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/ScalaElementType.scala
@@ -4,7 +4,6 @@ package parser
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree._
-import org.jetbrains.plugins.dotty.lang.psi.impl.base.types._
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaPsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.{ScNamingPattern, ScReferencePattern, ScTypedPattern}
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
@@ -383,14 +382,14 @@ object ScalaElementType {
   }
 
   val REFINED_TYPE: ScalaElementType = new ScalaElementType("Dotty refined type") {
-    override def createElement(node: ASTNode) = new DottyRefinedTypeElementImpl(node)
+    override def createElement(node: ASTNode): ScalaPsiElement = ???
   }
 
   val WITH_TYPE: ScalaElementType = new ScalaElementType("Dotty with type") {
-    override def createElement(node: ASTNode) = new DottyAndTypeElementImpl(node)
+    override def createElement(node: ASTNode): ScalaPsiElement = ???
   }
 
   val TYPE_ARGUMENT_NAME: ScalaElementType = new ScalaElementType("Dotty type argument name") {
-    override def createElement(node: ASTNode) = new DottyTypeArgumentNameElementImpl(node)
+    override def createElement(node: ASTNode): ScalaPsiElement = ???
   }
 }
