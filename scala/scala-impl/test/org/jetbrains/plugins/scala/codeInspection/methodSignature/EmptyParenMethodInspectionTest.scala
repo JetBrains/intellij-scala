e82549493e12da71df7d17dfb495d0a081d2e2b9
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2019-04-11 20:16:29 +0200

    Add tests for ParameterlessOverrideInspections
     #SCL-15297 fixed

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/methodSignature/EmptyParenMethodInspectionTest.scala
@@ -0,0 +1,88 @@
+package org.jetbrains.plugins.scala.codeInspection.methodSignature
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.testFramework.EditorTestUtil
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import org.jetbrains.plugins.scala.codeInspection.{InspectionBundle, ScalaQuickFixTestBase}
+
+class EmptyParenMethodInspectionTest extends ScalaQuickFixTestBase {
+
+  import CodeInsightTestFixture.{CARET_MARKER => CARET}
+  import EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
+
+  protected override val classOfInspection: Class[_ <: LocalInspectionTool] =
+    classOf[ParameterlessOverrideInspection.EmptyParenMethod]
+
+  protected override val description: String =
+    InspectionBundle.message("method.signature.parameterless.override.empty.paren")
+
+  private val hint = InspectionBundle.message("empty.parentheses")
+
+
+  def test(): Unit = {
+    checkTextHasError(
+      text =
+        s"""
+           |class Impl extends Base {
+           |  def ${START}blub$END: Int = 0"
+           |}
+           |
+           |trait Base {
+           |  def blub(): Int
+           |}
+         """.stripMargin
+    )
+
+    testQuickFix(
+      text =
+        s"""
+           |class Impl extends Base {
+           |  def bl${CARET}ub: Int = 0"
+           |}
+           |
+           |trait Base {
+           |  def blub(): Int
+           |}
+         """.stripMargin,
+      expected =
+        s"""
+           |class Impl extends Base {
+           |  def blub(): Int = 0"
+           |}
+           |
+           |trait Base {
+           |  def blub(): Int
+           |}
+         """.stripMargin,
+      hint
+    )
+  }
+
+  def test_ok(): Unit = {
+    checkTextHasNoErrors(
+      text =
+        s"""
+           |class Impl extends Base {
+           |  def blub(): Int = 0"
+           |}
+           |
+           |trait Base {
+           |  def blub(): Int
+           |}
+         """.stripMargin
+    )
+
+    checkTextHasNoErrors(
+      text =
+        s"""
+           |class Impl extends Base {
+           |  def blub: Int = 0"
+           |}
+           |
+           |trait Base {
+           |  def blub: Int
+           |}
+         """.stripMargin
+    )
+  }
+}
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/methodSignature/JavaMutatorParameterlessOverrideInspectionTest.scala
@@ -0,0 +1,110 @@
+package org.jetbrains.plugins.scala.codeInspection.methodSignature
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.testFramework.EditorTestUtil
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import org.jetbrains.plugins.scala.codeInspection.{InspectionBundle, ScalaQuickFixTestBase}
+
+class JavaMutatorInspectionTest extends ScalaQuickFixTestBase {
+
+  import CodeInsightTestFixture.{CARET_MARKER => CARET}
+  import EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
+
+  protected override val classOfInspection: Class[_ <: LocalInspectionTool] =
+    classOf[ParameterlessOverrideInspection.JavaMutator]
+
+  protected override val description: String =
+    InspectionBundle.message("method.signature.parameterless.override.java.mutator")
+
+  private val hint = InspectionBundle.message("empty.parentheses")
+
+
+  def test_non_unit_with_mutator_name(): Unit = {
+    getFixture.configureByText("JBase.java",
+      """
+        |public class JBase {
+        |    public int addFoo() {}
+        |}
+      """.stripMargin
+    )
+
+    checkTextHasError(
+      text =
+        s"""
+           |class Impl extends JBase {
+           |  override def ${START}addFoo$END: Int = 0
+           |}
+         """.stripMargin
+    )
+
+    testQuickFix(
+      text =
+        s"""
+           |class Impl extends JBase {
+           |  def add${CARET}Foo: Int = 0
+           |}
+         """.stripMargin,
+      expected =
+        s"""
+           |class Impl extends JBase {
+           |  def addFoo(): Int = 0
+           |}
+         """.stripMargin,
+      hint
+    )
+  }
+
+  def test_unit_with_non_mutator_name(): Unit = {
+    getFixture.configureByText("JBase.java",
+      """
+        |public class JBase {
+        |    public void foo() {}
+        |}
+      """.stripMargin)
+
+    checkTextHasError(
+      text =
+        s"""
+           |class Impl extends JBase {
+           |  def ${START}foo$END: Unit = 0
+           |}
+         """.stripMargin
+    )
+  }
+
+  def test_unit_with_mutator_name(): Unit = {
+    getFixture.configureByText("JBase.java",
+      """
+        |public class JBase {
+        |    public void addFoo() {}
+        |}
+      """.stripMargin
+    )
+
+    checkTextHasNoErrors(
+      text =
+        """
+          |class Impl extends JBase {
+          |  def addFoo(): Unit = ()
+          |}
+        """.stripMargin
+    )
+  }
+
+  def test_with_non_mutator_name(): Unit = {
+    getFixture.configureByText("JBase.java",
+      """
+        |public class JBase {
+        |    public int foo() { return 0; }
+        |}
+      """.stripMargin)
+
+    checkTextHasNoErrors(
+      """
+        |class Impl extends JBase {
+        |  def foo: Int = 0
+        |}
+      """.stripMargin
+    )
+  }
+}
