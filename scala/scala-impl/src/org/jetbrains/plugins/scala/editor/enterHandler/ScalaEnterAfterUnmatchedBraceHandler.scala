ccc604a43935132261885e475139356fb58cda10
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-05-14 23:14:48 +0300

    insert unmatched brace in the end of ??? #SCL-15394 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/editor/enterHandler/ScalaEnterAfterUnmatchedBraceHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/editor/enterHandler/ScalaEnterAfterUnmatchedBraceHandler.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.editor.enterHandler
 
 import com.intellij.codeInsight.editorActions.enter.EnterAfterUnmatchedBraceHandler
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.Pair
 import com.intellij.psi.util.PsiUtilCore
 import com.intellij.psi.{PsiElement, PsiFile, TokenType}
@@ -12,8 +13,27 @@ class ScalaEnterAfterUnmatchedBraceHandler extends EnterAfterUnmatchedBraceHandl
     file.isInstanceOf[ScalaFile]
 
   // ATTENTION!
-  // implementation of this method was copied from EnterAfterUnmatchedBraceHandler.java
-  // (version 191.6183.87) the only changed place is marked with `CHANGE` marker
+  // implementation of this method was copied from EnterAfterUnmatchedBraceHandler.java (version 191.6183.87)
+  // list of changes:
+  // 1) do not filter ignore question mark sign
+  override protected def getRBraceOffset(file: PsiFile, editor: Editor, caretOffset: Int): Int = {
+    val text = editor.getDocument.getCharsSequence
+    var offset = CharArrayUtil.shiftForward(text, caretOffset, " \t")
+    val fileLength = text.length
+    val blacklistChars = ")];,%<"
+    if (offset < fileLength && blacklistChars.indexOf(text.charAt(offset)) < 0) {
+      offset = calculateOffsetToInsertClosingBrace(file, text, offset).second
+    }
+    Math.min(offset, fileLength)
+  }
+
+  // ATTENTION!
+  // implementation of this method was copied from EnterAfterUnmatchedBraceHandler.java (version 191.6183.87)
+  // list of changes:
+  // 1) using element.getTextRange.getStartOffset instead of element.getTextOffset
+  //    see getTextOffset documentation, looks like this is a typo in Intellij platform, because
+  //    getTextOffset can be greater than get getTextRange.getStartOffset and we will not
+  //    add closing brace in the end of such element in such case
   override protected def calculateOffsetToInsertClosingBrace(file: PsiFile, text: CharSequence, offset: Int): Pair[PsiElement, Integer] = {
     var element = PsiUtilCore.getElementAtOffset(file, offset)
     val node = element.getNode
@@ -32,11 +52,7 @@ class ScalaEnterAfterUnmatchedBraceHandler extends EnterAfterUnmatchedBraceHandl
       }
     }
 
-    // CHANGE:
-    //   using element.getTextRange.getStartOffset instead of element.getTextOffset
-    //   see getTextOffset documentation, looks like this is a typo in Intellij platform, because
-    //   getTextOffset can be greater than get getTextRange.getStartOffset and we will not
-    //   add closing brace in the end of such element in such case
+    // CHANGE
     if (element.getTextRange.getStartOffset != offset) {
       Pair.create(null, CharArrayUtil.shiftForwardUntil(text, offset, "\n"))
     } else {
