b1cb60d44a6aab31a184cb43012c3978cc152d6c
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-05-22 14:57:50 +0300

    convert PassthroughLiteralEscaper.java to PassthroughLiteralEscaper.scala #SCL-11710
    (cherry picked from commit 761366533c5d0c5df455f54599285b4bdd5a8e1e)

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/PassthroughLiteralEscaper.scala
@@ -0,0 +1,31 @@
+package org.jetbrains.plugins.scala.lang.psi.impl.base
+
+import java.lang
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.LiteralTextEscaper
+
+/**
+ * Pavel Fatin
+ */
+class PassthroughLiteralEscaper(val literal: ScLiteralImpl) extends LiteralTextEscaper[ScLiteralImpl](literal) {
+  override def decode(rangeInsideHost: TextRange, outChars: lang.StringBuilder): Boolean = {
+    TextRange.assertProperRange(rangeInsideHost)
+    outChars.append(myHost.getText, rangeInsideHost.getStartOffset, rangeInsideHost.getEndOffset)
+    true
+  }
+
+  override def getOffsetInHost(offsetInDecoded: Int, rangeInsideHost: TextRange): Int = {
+    var offset = offsetInDecoded + rangeInsideHost.getStartOffset
+    if (offset < rangeInsideHost.getStartOffset) offset = rangeInsideHost.getStartOffset
+    if (offset > rangeInsideHost.getEndOffset) offset = rangeInsideHost.getEndOffset
+    offset
+  }
+
+  override def isOneLine: Boolean = {
+    myHost.getValue match {
+      case str: String => str.indexOf('\n') < 0
+      case _ => false
+    }
+  }
+}
