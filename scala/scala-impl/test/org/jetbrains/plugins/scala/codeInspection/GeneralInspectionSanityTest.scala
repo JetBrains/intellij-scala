3ea4db574cd08383fa5eab1b5e18025f1f6719b6
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2019-05-23 17:12:19 +0200

    added test to check if all inspections have a description
     #SCL-10047

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/GeneralInspectionSanityTest.scala
@@ -0,0 +1,216 @@
+package org.jetbrains.plugins.scala.codeInspection
+
+import com.intellij.codeInspection.LocalInspectionEP
+import com.intellij.codeInspection.ex.LocalInspectionToolWrapper
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+
+class GeneralInspectionSanityTest extends SimpleTestCase {
+
+  def acquireAllInspectionEPs(): Seq[LocalInspectionEP] =
+    LocalInspectionEP.LOCAL_INSPECTION
+      .getExtensions()
+      .toSeq
+
+  def acquireAllScalaInspectionEPs(): Seq[LocalInspectionEP] =
+    acquireAllInspectionEPs().filter(_.language == "Scala")
+
+  def test_no_lowercase_language_used(): Unit = {
+    assert(!acquireAllInspectionEPs().exists(_.language == "scala"))
+  }
+
+  def test_inspection_list_is_correct(): Unit = {
+    val inspectionEPs = acquireAllScalaInspectionEPs()
+
+    assert(expectedInspections.sorted == expectedInspections, "Please keep expectedInspections sorted!")
+    val doubleExpectedInspections =
+      expectedInspections
+        .groupBy(identity)
+        .mapValues(_.length)
+        .filter(_._2 > 1)
+        .keys
+        .toSeq
+    assert(doubleExpectedInspections.isEmpty,
+      s"The following expectedInspections are duplicated: ${doubleExpectedInspections.mkString(", ")}")
+
+    val expectedInspectionSet = expectedInspections.toSet
+    // check if all expected inspections are present
+    val foundInspections = inspectionEPs.map(_.implementationClass.split('.').last).toSet
+
+    val nonExistentInspections = expectedInspectionSet -- foundInspections
+    val unregisteredInspections = foundInspections -- expectedInspectionSet
+
+    assert(nonExistentInspections.isEmpty,
+      s"The following expected inspections were not found: ${nonExistentInspections.mkString(", ")}")
+
+    assert(unregisteredInspections.isEmpty,
+      s"Please add the following inspections to expectedInspections: ${unregisteredInspections.mkString(", ")}")
+  }
+
+  def test_all_inspections_have_descriptions(): Unit = {
+    val inspectionsWithoutProperDescription =
+      acquireAllScalaInspectionEPs().filter { inspectionEP =>
+        val inspectionWrapper = new LocalInspectionToolWrapper(inspectionEP)
+        val description = inspectionWrapper.loadDescription()
+        description == null ||
+          description.length <= 5
+      }. map(insp => s"${insp.getShortName} (${insp.getDisplayName})")
+
+    assert(inspectionsWithoutProperDescription.isEmpty,
+      s"The following inspection do not have a description file:\n  ${inspectionsWithoutProperDescription.mkString(",\n  ")}")
+  }
+
+  def test_all_shortNames_are_unique(): Unit = {
+    val allShortNames = acquireAllInspectionEPs().map(_.shortName).groupBy(identity).mapValues(_.length)
+    val scalaShortNames = acquireAllScalaInspectionEPs().map(_.shortName)
+
+    scalaShortNames.foreach { scalaShortName =>
+      assert(allShortNames(scalaShortName) == 1, s"shortName $scalaShortName exists multiple times!")
+    }
+  }
+
+  val expectedInspections = Seq(
+    "AbstractValueInTraitInspection",
+    "AccessorLikeMethodInspection$EmptyParentheses",
+    "AccessorLikeMethodInspection$UnitReturnType",
+    "AmmoniteUnresolvedLibraryInspection",
+    "AnnotatorBasedErrorInspection",
+    "ApparentResultTypeRefinementInspection",
+    "AppliedTypeLambdaCanBeSimplifiedInspection",
+    "AutoTuplingInspection",
+    "BuiltinMatcherExistsInspection",
+    "BundledCompoundInspection",
+    "CaseClassParamInspection",
+    "ChainedPackageInspection",
+    "CollectHeadOptionInspection",
+    "ComparingDiffCollectionKindsInspection",
+    "ComparingLengthInspection",
+    "ComparingUnrelatedTypesInspection",
+    "ConcealedApplyCall",
+    "ConvertExpressionToSAMInspection",
+    "ConvertibleToMethodValueInspection",
+    "CorrespondsUnsortedInspection",
+    "DangerousCatchAllInspection",
+    "DeprecatedViewBoundInspection",
+    "DoubleNegationInspection",
+    "DropTakeToSliceInspection",
+    "EmptyCheckInspection",
+    "EmptyParenOverrideInspection$JavaAccessorMethodOverriddenAsEmptyParenInspection",
+    "EmptyParenOverrideInspection$ParameterlessMemberOverriddenAsEmptyParenInspection",
+    "EmulateFlattenInspection",
+    "EqualityToSameElementsInspection",
+    "ExistsEqualsInspection",
+    "ExistsForallReplaceInspection",
+    "FieldFromDelayedInitInspection",
+    "FilterEmptyCheckInspection",
+    "FilterHeadOptionInspection",
+    "FilterOtherContainsInspection",
+    "FilterSetContainsInspection",
+    "FilterSizeInspection",
+    "FindAndMapToApplyInspection",
+    "FindEmptyCheckInspection",
+    "FloatLiteralEndingWithDecimalPointInspection",
+    "FoldTrueAndInspection",
+    "ForwardReferenceInspection",
+    "FunctionTupleSyntacticSugarInspection",
+    "GetGetOrElseInspection",
+    "GetOrElseNullInspection",
+    "HashCodeUsesVarInspection",
+    "HeadOrLastOptionInspection",
+    "IfElseToFilteredOptionInspection",
+    "IfElseToOptionInspection",
+    "IndexBoundsCheckInspection",
+    "JavaAccessorEmptyParenCallInspection",
+    "KindProjectorSimplifyTypeProjectionInspection",
+    "KindProjectorUseCorrectLambdaKeywordInspection",
+    "LanguageFeatureInspection",
+    "LastIndexToLastInspection",
+    "LegacyStringFormattingInspection",
+    "LoopVariableNotUpdatedInspection",
+    "MapFlattenInspection",
+    "MapGetGetInspection",
+    "MapGetOrElseBooleanInspection",
+    "MapGetOrElseInspection",
+    "MapKeysInspection",
+    "MapLiftInspection",
+    "MapToBooleanContainsInspection",
+    "MapValuesInspection",
+    "MatchToPartialFunctionInspection",
+    "MultipleArgLists",
+    "NameBooleanParametersInspection",
+    "NestedStatefulMonadsInspection",
+    "NoTailRecursionAnnotationInspection",
+    "NotImplementedCodeInspection",
+    "OptionEqualsSomeToContainsInspection",
+    "ParameterlessAccessInspection$EmptyParenMethod",
+    "ParameterlessAccessInspection$JavaMutator",
+    "ParameterlessOverrideInspection$EmptyParenMethod",
+    "ParameterlessOverrideInspection$JavaMutator",
+    "ParameterlessOverrideInspection$MutatorLikeMethod",
+    "PostfixMethodCallInspection",
+    "RangeToIndicesInspection",
+    "RedundantBlockInspection",
+    "RedundantCollectionConversionInspection",
+    "RedundantDefaultArgumentInspection",
+    "RedundantHeadOrLastOptionInspection",
+    "RedundantNewCaseClassInspection",
+    "ReferenceMustBePrefixedInspection",
+    "RelativeImportInspection",
+    "RemoveRedundantReturnInspection",
+    "ReplaceToWithUntilInspection",
+    "ReverseIteratorInspection",
+    "ReverseMapInspection",
+    "ReverseTakeReverseInspection",
+    "SameElementsToEqualsInspection",
+    "SbtReplaceProjectWithProjectInInspection",
+    "ScalaDefaultFileTemplateUsageInspection",
+    "ScalaDeprecatedIdentifierInspection",
+    "ScalaDeprecationInspection",
+    "ScalaDocInlinedTagInspection",
+    "ScalaDocMissingParameterDescriptionInspection",
+    "ScalaDocParserErrorInspection",
+    "ScalaDocUnbalancedHeaderInspection",
+    "ScalaDocUnclosedTagWithoutParserInspection",
+    "ScalaDocUnknownParameterInspection",
+    "ScalaDocUnknownTagInspection",
+    "ScalaFileNameInspection",
+    "ScalaMalformedFormatStringInspection",
+    "ScalaPackageNameInspection",
+    "ScalaRedundantCastInspection",
+    "ScalaRedundantConversionInspection",
+    "ScalaUnnecessaryParenthesesInspection",
+    "ScalaUnnecessarySemicolonInspection",
+    "ScalaUnreachableCodeInspection",
+    "ScalaUnusedExpressionInspection",
+    "ScalaUnusedSymbolInspection",
+    "ScalaXmlUnmatchedTagInspection",
+    "ScalastyleCodeInspection",
+    "SideEffectsInMonadicTransformationInspection",
+    "SimplifiableFoldOrReduceInspection",
+    "SimplifyBooleanInspection",
+    "SimplifyBooleanMatchInspection",
+    "SingleImportInspection",
+    "SizeToLengthInspection",
+    "SomeToOptionInspection",
+    "SortFilterInspection",
+    "SortedMaxMinInspection",
+    "SourceNotClosedInspection",
+    "ToSetAndBackInspection",
+    "TypeAnnotationInspection",
+    "TypeCheckCanBeMatchInspection",
+    "TypeParameterShadowInspection",
+    "UnitInMapInspection",
+    "UnitMethodInspection$ExplicitAssignment",
+    "UnitMethodInspection$ExplicitType",
+    "UnitMethodInspection$FunctionDefinition",
+    "UnitMethodInspection$Parameterless",
+    "UnitMethodInspection$ProcedureDeclaration",
+    "UnitMethodInspection$ProcedureDefinition",
+    "UnnecessaryPartialFunctionInspection",
+    "UnzipSingleElementInspection",
+    "VarCouldBeValInspection",
+    "VariableNullInitializerInspection",
+    "VariablePatternShadowInspection",
+    "ZeroIndexToHeadInspection",
+    "ZipWithIndexInspection",
+  )
+}
