496e4194f16cff65dc64436c82eb123d54abab08
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-05-24 18:24:55 +0300

    mutable state removed #SCL-15263

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScLiteralImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScLiteralImpl.scala
@@ -64,97 +64,97 @@ class ScLiteralImpl(node: ASTNode) extends expr.ScExpressionImplBase(node)
 
   @CachedInUserData(this, util.PsiModificationTracker.MODIFICATION_COUNT)
   def getValue: AnyRef = {
-
-    var text = getText
-    val textLength = getTextLength
-
-    def getValueOfNode(e: ASTNode, isNegative: Boolean = false): AnyRef = {
-      e.getElementType match {
-        case T.tSTRING | T.tWRONG_STRING if !isNegative =>
-          if (!text.startsWith("\"")) return null
-          text = text.substring(1)
-          if (text.endsWith("\"")) {
-            text = text.substring(0, text.length - 1)
-          }
-          try {
-            StringContext.treatEscapes(text) // for octal escape sequences
-          } catch {
-            case _: StringContext.InvalidEscapeException => StringUtil.unescapeStringCharacters(text)
-          }
-        case T.tMULTILINE_STRING if !isNegative =>
-          if (!text.startsWith("\"\"\"")) return null
-          text = text.substring(3)
-          if (text.endsWith("\"\"\"")) {
-            text = text.substring(0, text.length - 3)
-          }
-          text
-        case T.kTRUE if !isNegative => jl.Boolean.TRUE
-        case T.kFALSE if !isNegative => jl.Boolean.FALSE
-        case T.tCHAR if !isNegative =>
-          val diff = if (StringUtil.endsWithChar(getText, '\'')) {
-            if (textLength == 1) return null
-            1
-          } else 0
-
-          text = text.substring(1, textLength - diff)
-          val chars = new jl.StringBuilder
-          val success = java.PsiLiteralExpressionImpl.parseStringCharacters(
-            text,
-            chars,
-            null
-          )
-
-          if (success && chars.length == 1) Character.valueOf(chars.charAt(0))
-          else null
-        case T.tINTEGER =>
-          val isLong = e.getText.matches(".*[lL]$")
-          text = if (isLong) text.substring(0, text.length - 1) else text
-
-          val (number, base) = text match {
-            case t if t.startsWith("0x") || t.startsWith("0X") => (t.substring(2), 16)
-            case t if t.startsWith("0") && t.length >= 2 => (t.substring(0), 8)
-            case t => (t, 10)
-          }
-
-          val limit = if (isLong) jl.Long.MAX_VALUE else jl.Integer.MAX_VALUE
-          val divider = if (base == 10) 1 else 2
-          var value = 0l
-          for (d <- number.map(_.asDigit)) {
-            if (value < 0 ||
-              limit / (base / divider) < value / divider ||
-              limit - (d / divider) < value * (base / divider)
-            ) {
-              return null
+    def getValueOfNode(node: ASTNode,
+                       text: String,
+                       isNegative: Boolean = false): AnyRef = node.getElementType match {
+      case T.tSTRING |
+           T.tWRONG_STRING if !isNegative =>
+        trimQuotes(text, SingleLineQuote)() match {
+          case null => null
+          case stringText =>
+            try {
+              StringContext.treatEscapes(stringText) // for octal escape sequences
+            } catch {
+              case _: StringContext.InvalidEscapeException => StringUtil.unescapeStringCharacters(text)
             }
-            value = value * base + d
+        }
+      case T.tMULTILINE_STRING if !isNegative =>
+        trimQuotes(text, MultiLineQuote)()
+      case T.kTRUE if !isNegative => jl.Boolean.TRUE
+      case T.kFALSE if !isNegative => jl.Boolean.FALSE
+      case T.tCHAR if !isNegative =>
+        text match {
+          case CharQuote => null
+          case _ =>
+            val chars = new jl.StringBuilder
+            val success = java.PsiLiteralExpressionImpl.parseStringCharacters(
+              trimQuotes(text, CharQuote)(),
+              chars,
+              null
+            )
+
+            if (success && chars.length == 1) Character.valueOf(chars.charAt(0))
+            else null
+        }
+      case T.tINTEGER =>
+        val isLong = node.getText.matches(".*[lL]$")
+        val numberText = text.substring(
+          0,
+          text.length - (if (isLong) 1 else 0)
+        )
+
+        val (number, base) = numberText match {
+          case t if t.startsWith("0x") || t.startsWith("0X") => (t.substring(2), 16)
+          case t if t.startsWith("0") && t.length >= 2 => (t.substring(0), 8)
+          case t => (t, 10)
+        }
+
+        val limit = if (isLong) jl.Long.MAX_VALUE else jl.Integer.MAX_VALUE
+        val divider = if (base == 10) 1 else 2
+        var value = 0l
+        for (d <- number.map(_.asDigit)) {
+          if (value < 0 ||
+            limit / (base / divider) < value / divider ||
+            limit - (d / divider) < value * (base / divider)
+          ) {
+            return null
           }
+          value = value * base + d
+        }
 
-          if (isNegative) value = -value
+        if (isNegative) value = -value
 
-          if (isLong) jl.Long.valueOf(value)
-          else Integer.valueOf(value.toInt)
-        case T.tFLOAT =>
-          val isFloat = e.getText.matches(".*[fF]$")
-          val number = if (isFloat) text.substring(0, text.length - 1) else text
+        if (isLong) jl.Long.valueOf(value)
+        else Integer.valueOf(value.toInt)
+      case T.tFLOAT =>
+        val isFloat = node.getText.matches(".*[fF]$")
+        val numberText = text.substring(
+          0,
+          text.length - (if (isFloat) 1 else 0)
+        )
 
-          try {
-            val text = (if (isNegative) "-" else "") + number
-            if (isFloat) jl.Float.valueOf(text)
-            else jl.Double.valueOf(text)
-          } catch {
-            case _: NumberFormatException => null
-          }
-        case T.tSYMBOL if !isNegative =>
-          if (text.startsWith("\'")) Symbol(text.substring(1))
-          else null
-        case T.tIDENTIFIER if e.getText == "-" && !isNegative =>
-          text = text.substring(1)
-          getValueOfNode(e.getTreeNext, isNegative = true)
-        case _ => null
-      }
+        try {
+          val number = (if (isNegative) "-" else "") + numberText
+          if (isFloat) jl.Float.valueOf(number)
+          else jl.Double.valueOf(number)
+        } catch {
+          case _: NumberFormatException => null
+        }
+      case T.tSYMBOL if !isNegative =>
+        trimQuotes(text, CharQuote)("") match {
+          case null => null
+          case symbolText => Symbol(symbolText)
+        }
+      case T.tIDENTIFIER if node.getText == "-" && !isNegative =>
+        getValueOfNode(
+          node.getTreeNext,
+          text.substring(1),
+          isNegative = true
+        )
+      case _ => null
     }
 
-    getValueOfNode(getFirstChild.getNode)
+    getValueOfNode(getFirstChild.getNode, getText)
   }
 
   def updateText(text: String): ScLiteralImpl = {
@@ -208,9 +208,11 @@ class ScLiteralImpl(node: ASTNode) extends expr.ScExpressionImplBase(node)
   def getAnnotationOwner(annotationOwnerLookUp: ScLiteral => Option[PsiAnnotationOwner with PsiElement]): Option[PsiAnnotationOwner] = {
     if (!isString) return None
 
-    if (System.currentTimeMillis > expirationTime || myAnnotationOwner.exists(!_.isValid)) {
-      myAnnotationOwner = annotationOwnerLookUp(this)
-      expirationTime = System.currentTimeMillis + (2 + ExpTimeLengthGenerator.nextInt(8)) * 1000
+    System.currentTimeMillis match {
+      case currentTimeMillis if currentTimeMillis <= expirationTime && myAnnotationOwner.forall(_.isValid) =>
+      case currentTimeMillis =>
+        myAnnotationOwner = annotationOwnerLookUp(this)
+        expirationTime = currentTimeMillis + (2 + ExpTimeLengthGenerator.nextInt(8)) * 1000
     }
 
     myAnnotationOwner
@@ -236,4 +238,14 @@ object ScLiteralImpl {
     val quoteLength = quote.length
     Some(quoteLength, quoteLength)
   }
+
+  private def trimQuotes(text: String, startQuote: String)
+                        (endQuote: String = startQuote) =
+    if (text.startsWith(startQuote))
+      text.substring(
+        startQuote.length,
+        text.length - (if (text.endsWith(endQuote)) endQuote.length else 0)
+      )
+    else
+      null
 }
