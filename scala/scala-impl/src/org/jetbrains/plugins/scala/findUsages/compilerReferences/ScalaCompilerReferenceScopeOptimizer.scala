83e8dd9ac80d997a5460a88bb3eaf1e97cf045a4
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2019-05-27 19:45:04 +0300

    ScopeOptimizer should return null if it cannot restrict scope #SCL-15522 fixed
    (cherry picked from commit 2080a80d08a2b51c1ec3a7f4e15942b307e69e48)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/compilerReferences/ScalaCompilerReferenceScopeOptimizer.scala
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.scala.findUsages.compilerReferences
-
-import com.intellij.psi.PsiElement
-import com.intellij.psi.search.{GlobalSearchScope, ScopeOptimizer}
-
-class ScalaCompilerReferenceScopeOptimizer extends ScopeOptimizer {
-  override def getScopeToExclude(element: PsiElement): GlobalSearchScope =
-    ScalaCompilerReferenceService(element.getProject).scopeWithoutReferences(element)
-}
