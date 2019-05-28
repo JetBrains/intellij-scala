dd881947511ba428379118e981802b0b021c9948
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-05-28 20:07:14 +0300

    delegations to PsiLiteralExpressionImpl #SCL-15263

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScLiteralImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScLiteralImpl.scala
@@ -12,6 +12,7 @@ import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi._
 import com.intellij.psi.impl.source.tree.{LeafElement, java}
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.util.PsiLiteralUtil
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.plugins.scala.lang.psi.types._
@@ -47,10 +48,9 @@ class ScLiteralImpl(node: ASTNode) extends expr.ScExpressionImplBase(node)
   @volatile
   private[this] var typeForNullWithoutImplicits_ = Option.empty[ScType]
 
-  override def typeForNullWithoutImplicits_=(`type`: Option[ScType]): Unit = {
-    if (firstElementType != T.kNULL)
-      throw new jl.AssertionError(s"Only null literals accepted, type: $firstElementType")
-    typeForNullWithoutImplicits_ = `type`
+  override def typeForNullWithoutImplicits_=(`type`: Option[ScType]): Unit = literalElementType match {
+    case T.kNULL => typeForNullWithoutImplicits_ = `type`
+    case elementType => throw new AssertionError(s"Only null literals accepted, type: $elementType")
   }
 
   override def typeForNullWithoutImplicits: Option[ScType] = typeForNullWithoutImplicits_
@@ -63,10 +63,52 @@ class ScLiteralImpl(node: ASTNode) extends expr.ScExpressionImplBase(node)
     ScLiteralType.inferType(this)
 
   @CachedInUserData(this, util.PsiModificationTracker.MODIFICATION_COUNT)
-  def getValue: AnyRef = nodeValue(getFirstChild.getNode, getText)
+  def getValue: AnyRef = {
+    val node = literalNode
 
-  def updateText(text: String): ScLiteralImpl = {
-    getNode.getFirstChildNode match {
+    node.getElementType match {
+      case T.kTRUE => jl.Boolean.TRUE
+      case T.kFALSE => jl.Boolean.FALSE
+      case T.tSTRING |
+           T.tWRONG_STRING =>
+        trimQuotes(SingleLineQuote)() match {
+          case null => null
+          case stringText =>
+            try {
+              StringContext.treatEscapes(stringText) // for octal escape sequences
+            } catch {
+              case _: StringContext.InvalidEscapeException => StringUtil.unescapeStringCharacters(getText)
+            }
+        }
+      case T.tMULTILINE_STRING =>
+        trimQuotes(MultiLineQuote)()
+      case T.tCHAR =>
+        trimQuotes(CharQuote)() match {
+          case null => null
+          case chars =>
+            val outChars = new jl.StringBuilder
+            val success = java.PsiLiteralExpressionImpl.parseStringCharacters(
+              chars,
+              outChars,
+              null
+            )
+
+            if (success && outChars.length == 1) Character.valueOf(outChars.charAt(0))
+            else null
+        }
+      case T.tSYMBOL =>
+        trimQuotes(CharQuote)("") match {
+          case null => null
+          case symbolText => Symbol(symbolText)
+        }
+      case T.tIDENTIFIER if node.getText == "-" =>
+        nodeNumberValue(node.getTreeNext.getElementType)
+      case elementType => nodeNumberValue(elementType)
+    }
+  }
+
+  def updateText(text: String): ScLiteral = {
+    literalNode match {
       case leaf: LeafElement => leaf.replaceWithText(text)
     }
     this
@@ -76,23 +118,25 @@ class ScLiteralImpl(node: ASTNode) extends expr.ScExpressionImplBase(node)
     if (isMultiLineString) new PassthroughLiteralEscaper(this)
     else new ScLiteralEscaper(this)
 
-  private def firstElementType: IElementType = getFirstChild.getNode.getElementType
+  private def literalNode = getNode.getFirstChildNode
 
-  override def isString: Boolean = firstElementType match {
+  private def literalElementType = literalNode.getElementType
+
+  override def isString: Boolean = literalElementType match {
     case T.tMULTILINE_STRING | T.tSTRING => true
     case _ => false
   }
 
-  override def isMultiLineString: Boolean = firstElementType == T.tMULTILINE_STRING
+  override def isMultiLineString: Boolean = literalElementType == T.tMULTILINE_STRING
 
-  override def isSymbol: Boolean = firstElementType == T.tSYMBOL
+  override def isSymbol: Boolean = literalElementType == T.tSYMBOL
 
-  override def isChar: Boolean = firstElementType == T.tCHAR
+  override def isChar: Boolean = literalElementType == T.tCHAR
 
   override def getReferences: Array[PsiReference] = PsiReferenceService.getService.getContributedReferences(this)
 
   override def contentRange: TextRange = {
-    val maybeShifts = firstElementType match {
+    val maybeShifts = literalElementType match {
       case T.tSTRING => stringShifts(SingleLineQuote)
       case T.tMULTILINE_STRING => stringShifts(MultiLineQuote)
       case T.tCHAR => Some(1, 1)
@@ -125,11 +169,36 @@ class ScLiteralImpl(node: ASTNode) extends expr.ScExpressionImplBase(node)
 
     myAnnotationOwner
   }
+
+  private def nodeNumberValue(elementType: IElementType): Number = {
+    def parseNumber(suffix: Char)
+                   (function1: String => Number)
+                   (function2: String => Number) = getText match {
+      case text if endsWithIgnoreCase(text, suffix) => function1(text)
+      case text => function2(text)
+    }
+
+    import PsiLiteralUtil._
+    elementType match {
+      case T.tFLOAT => parseNumber('f')(parseFloat)(parseDouble)
+      case T.tINTEGER => parseNumber('l')(parseLong)(parseInteger)
+      case _ => null
+    }
+  }
+
+  private def trimQuotes(startQuote: String)
+                        (endQuote: String = startQuote) = getText match {
+    case text if text.startsWith(startQuote) =>
+      val beginIndex = startQuote.length
+      text.length - (if (text.endsWith(endQuote)) endQuote.length else 0) match {
+        case endIndex if endIndex < beginIndex => null
+        case endIndex => text.substring(beginIndex, endIndex)
+      }
+    case _ => null
+  }
 }
 
 object ScLiteralImpl {
-
-  import lang.lexer.{ScalaTokenTypes => T}
 
   private val ExpTimeLengthGenerator = new ju.Random(System.currentTimeMillis)
 
@@ -149,120 +218,8 @@ object ScLiteralImpl {
     Some(quoteLength, quoteLength)
   }
 
-  private def nodeValue(node: ASTNode,
-                        text: String): AnyRef = node.getElementType match {
-    case T.tSTRING |
-         T.tWRONG_STRING =>
-      trimQuotes(text, SingleLineQuote)() match {
-        case null => null
-        case stringText =>
-          try {
-            StringContext.treatEscapes(stringText) // for octal escape sequences
-          } catch {
-            case _: StringContext.InvalidEscapeException => StringUtil.unescapeStringCharacters(text)
-          }
-      }
-    case T.tMULTILINE_STRING =>
-      trimQuotes(text, MultiLineQuote)()
-    case T.kTRUE => jl.Boolean.TRUE
-    case T.kFALSE => jl.Boolean.FALSE
-    case T.tCHAR =>
-      text match {
-        case CharQuote => null
-        case _ =>
-          val chars = new jl.StringBuilder
-          val success = java.PsiLiteralExpressionImpl.parseStringCharacters(
-            trimQuotes(text, CharQuote)(),
-            chars,
-            null
-          )
-
-          if (success && chars.length == 1) Character.valueOf(chars.charAt(0))
-          else null
-      }
-    case T.tSYMBOL =>
-      trimQuotes(text, CharQuote)("") match {
-        case null => null
-        case symbolText => Symbol(symbolText)
-      }
-    case T.tIDENTIFIER if node.getText == "-" =>
-      nodeNumberValue(
-        node.getTreeNext,
-        text.substring(1),
-        isNegative = true
-      )
-    case _ => nodeNumberValue(node, text, isNegative = false)
-  }
-
-  private[this] def nodeNumberValue(node: ASTNode,
-                                    text: String,
-                                    isNegative: Boolean): jl.Number = node.getElementType match {
-    case T.tFLOAT =>
-      val isFloat = text.matches(".*[fF]$")
-      val numberText = text.substring(
-        0,
-        text.length - (if (isFloat) 1 else 0)
-      )
-
-      try {
-        val number = (if (isNegative) "-" else "") + numberText
-        if (isFloat) jl.Float.valueOf(number)
-        else jl.Double.valueOf(number)
-      } catch {
-        case _: NumberFormatException => null
-      }
-    case T.tINTEGER =>
-      val isLong = text.matches(".*[lL]$")
-      val numberText = text.substring(
-        0,
-        text.length - (if (isLong) 1 else 0)
-      )
-
-      val (beginIndex, base, divider) = numberText match {
-        case t if t.startsWith("0x") || t.startsWith("0X") => (2, 16, 2)
-        case t if t.startsWith("0") && t.length >= 2 => (1, 8, 2)
-        case _ => (0, 10, 1)
-      }
-
-      var value = stringToNumber(
-        numberText.substring(beginIndex),
-        if (isLong) jl.Long.MAX_VALUE else jl.Integer.MAX_VALUE,
-        base,
-        divider
-      )(0, 0L)
-
-      if (isNegative) value = -value
-
-      if (isLong) jl.Long.valueOf(value)
-      else Integer.valueOf(value.toInt)
-    case _ => null
-  }
-
-  @annotation.tailrec
-  private[this] def stringToNumber(number: String, limit: Long,
-                                   base: Int, divider: Int)
-                                  (index: Int, accumulator: Long): jl.Long =
-    if (index == number.length) accumulator
-    else {
-      number(index).asDigit match {
-        case digit if accumulator < 0 ||
-          limit / (base / divider) < accumulator / divider ||
-          limit - (digit / divider) < accumulator * (base / divider) => null
-        case digit =>
-          stringToNumber(number, limit, base, divider)(
-            index + 1,
-            accumulator * base + digit
-          )
-      }
-    }
-
-  private[this] def trimQuotes(text: String, startQuote: String)
-                              (endQuote: String = startQuote) =
-    if (text.startsWith(startQuote))
-      text.substring(
-        startQuote.length,
-        text.length - (if (text.endsWith(endQuote)) endQuote.length else 0)
-      )
-    else
-      null
+  private def endsWithIgnoreCase(text: String,
+                                 suffix: Char) =
+    StringUtil.endsWithChar(text, suffix) ||
+      StringUtil.endsWithChar(text, suffix.toUpper)
 }
