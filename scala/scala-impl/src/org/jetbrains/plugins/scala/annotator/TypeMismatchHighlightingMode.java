afe9b683603bfe99d9b6f306daf2120311bba38c
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-05-31 19:12:29 +0200

    TypeMismatchHighlightingMode.SHOW_TYPE_MISMATCH_HINT
    SCL-15250

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchHighlightingMode.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchHighlightingMode.java
@@ -5,7 +5,8 @@ import org.jetbrains.plugins.scala.settings.ScalaProjectSettings;
 
 public enum TypeMismatchHighlightingMode {
     HIGHLIGHT_EXPRESSION("Highlight expression"),
-    HIGHLIGHT_TYPE_HINT("Highlight type hint");
+    HIGHLIGHT_TYPE_HINT("Highlight type hint"),
+    SHOW_TYPE_MISMATCH_HINT("Show type mismatch hint");
 
     private String myName;
 
