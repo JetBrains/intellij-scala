242fbb7f23f82b58fdab77a0f219cea7b17e9204
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-06-03 13:52:15 +0200

    TypeMismatchError: allow for element remapping on putUserData
    SCL-15250

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/annotationHolder/DelegateAnnotationHolder.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/annotationHolder/DelegateAnnotationHolder.scala
@@ -5,10 +5,16 @@ import com.intellij.lang.annotation.{Annotation, AnnotationHolder, AnnotationSes
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 
-class DelegateAnnotationHolder(transformRange: TextRange => TextRange, holder: AnnotationHolder, session: AnnotationSession) extends AnnotationHolderImpl(session, holder.isBatchMode) {
-  def this(elem: PsiElement, holder: AnnotationHolder, session: AnnotationSession) = this(_ => elem.getTextRange, holder, session)
+class DelegateAnnotationHolder(transformRange: TextRange => TextRange, holder: AnnotationHolder, session: AnnotationSession, private val element: Option[PsiElement] = None)
+  extends AnnotationHolderImpl(session, holder.isBatchMode) {
+
+  def this(elem: PsiElement, holder: AnnotationHolder, session: AnnotationSession) = this(_ => elem.getTextRange, holder, session, Some(elem))
 
   override def createAnnotation(severity: HighlightSeverity, range: TextRange, message: String, tooltip: String): Annotation = {
     holder.createAnnotation(severity, transformRange(range), message, tooltip)
   }
+}
+
+object DelegateAnnotationHolder {
+  def unapply(holder: DelegateAnnotationHolder): Option[PsiElement] = holder.element
 }
