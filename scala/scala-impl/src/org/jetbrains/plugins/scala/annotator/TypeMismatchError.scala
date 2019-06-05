8bdc623d16374652251abb61fdeef748bc22c4bd
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-06-05 12:38:46 +0200

    Type mismatch errors: widen literal types when necessary
    SCL-15571

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchError.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchError.scala
@@ -14,8 +14,8 @@ import org.jetbrains.plugins.scala.annotator.quickfix.ReportHighlightingErrorQui
 import org.jetbrains.plugins.scala.caches.CachesUtil
 import org.jetbrains.plugins.scala.caches.CachesUtil.fileModCount
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScBlockExpr
-import org.jetbrains.plugins.scala.lang.psi.types.ScType
 import org.jetbrains.plugins.scala.lang.psi.types.api.ScTypePresentation
+import org.jetbrains.plugins.scala.lang.psi.types.{ScLiteralType, ScType}
 
 case class TypeMismatchError(expectedType: Option[ScType], actualType: Option[ScType], tooltip: String, modificationCount: Long)
 
@@ -25,6 +25,16 @@ object TypeMismatchError {
   def apply(element: PsiElement): Option[TypeMismatchError] = Option(element.getUserData(TypeMismatchErrorKey))
 
   def register(holder: AnnotationHolder, element: PsiElement, expectedType: ScType, actualType: ScType, blockLevel: Int = 0)(formatMessage: (String, String) => String): Annotation = {
+    // When expected type is not literal type, widen actual literal type (as it looks more sane)
+    val wideActualType = (expectedType, actualType) match {
+      case (_: ScLiteralType, t2: ScLiteralType) => t2
+      case (_, t2: ScLiteralType) => t2.wideType
+      case (_, t2) => t2
+    }
+    register0(holder, element, expectedType, wideActualType, blockLevel)(formatMessage)
+  }
+
+  private def register0(holder: AnnotationHolder, element: PsiElement, expectedType: ScType, actualType: ScType, blockLevel: Int)(formatMessage: (String, String) => String): Annotation = {
     val (actualTypeText, expectedTypeText) = ScTypePresentation.different(actualType, expectedType)
 
     // TODO update the test data, SCL-15483
