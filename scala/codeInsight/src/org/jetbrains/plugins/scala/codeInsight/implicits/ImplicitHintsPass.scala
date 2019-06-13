beab1cdbf1937aa2212ac0d74e80cba692f56418
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-06-13 10:21:38 +0200

    Hint & Text utilities in annotator.hints, #SCL-15593

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsPass.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsPass.scala
@@ -1,19 +1,15 @@
 package org.jetbrains.plugins.scala.codeInsight.implicits
 
-import java.awt.Color
-
 import com.intellij.codeHighlighting.EditorBoundHighlightingPass
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.colors.{CodeInsightColors, EditorColors, EditorColorsScheme}
+import com.intellij.openapi.editor.colors.EditorColorsScheme
 import com.intellij.openapi.editor.ex.util.CaretVisualPositionKeeper
-import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.pom.Navigatable
 import com.intellij.psi.PsiElement
 import com.intellij.util.DocumentUtil
-import com.intellij.util.ui.StartupUiUtil
 import org.jetbrains.plugins.scala.annotator.ScalaAnnotator
 import org.jetbrains.plugins.scala.annotator.hints._
 import org.jetbrains.plugins.scala.codeInsight.implicits.ImplicitHintsPass._
@@ -292,11 +288,6 @@ private object ImplicitHintsPass {
     }
   }
 
-  private def errorAttributes(implicit scheme: EditorColorsScheme) = scheme.getAttributes(CodeInsightColors.ERRORS_ATTRIBUTES)
-
-  private def likeWrongReference(implicit scheme: EditorColorsScheme) =
-    Option(scheme.getAttributes(CodeInsightColors.WRONG_REFERENCES_ATTRIBUTES))
-
   private def typeSuffix(parameter: ScalaResolveResult): String = {
     val paramType = parameter.implicitSearchState.map(_.presentableTypeText).getOrElse("NotInferred")
     s": $paramType"
@@ -316,58 +307,7 @@ private object ImplicitHintsPass {
     }
   }
 
-
   private def ambiguousTooltip(parameter: ScalaResolveResult): String =
     "Ambiguous implicits for parameter " + paramWithType(parameter)
-
-  private[implicits] val foldedString: String = "..."
-
-  // Add custom colors for folding inside inlay hints (SCL-13996)?
-  private def adjusted(attributes: TextAttributes): TextAttributes = {
-    def average(c1: Color, c2: Color) = {
-      val r = (c1.getRed + c2.getRed) / 2
-      val g = (c1.getGreen + c2.getGreen) / 2
-      val b = (c1.getBlue + c2.getBlue) / 2
-      val alpha = c1.getAlpha
-      new Color(r, g, b, alpha)
-    }
-
-    val result = attributes.clone()
-    if (StartupUiUtil.isUnderDarcula && result.getBackgroundColor != null) {
-      val notSoBright = result.getBackgroundColor.brighter
-      val tooBright = notSoBright.brighter
-      result.setBackgroundColor(average(notSoBright, tooBright))
-    }
-    result
-  }
-
-  private[implicits] def foldedAttributes(error: Boolean)
-                                (implicit scheme: EditorColorsScheme): Option[TextAttributes] = {
-    val plainFolded =
-      scheme.getAttributes(EditorColors.FOLDED_TEXT_ATTRIBUTES)
-        .toOption
-        .map(adjusted)
-
-    if (error) plainFolded.map(_ + errorAttributes)
-    else plainFolded
-  }
-
-  private implicit class SeqTextExt(val parts: Seq[Text]) extends AnyVal {
-
-    def withAttributes(attr: TextAttributes): Seq[Text] = parts.map(_.withAttributes(attr))
-
-    //nested text may have more specific tooltip
-    def withErrorTooltipIfEmpty(tooltip: String): Seq[Text] = parts.map { p =>
-      if (p.errorTooltip.isEmpty) p.withErrorTooltip(tooltip) else p
-    }
-
-    def withErrorTooltipIfEmpty(tooltip: Option[String]): Seq[Text] = tooltip.map(parts.withErrorTooltipIfEmpty).getOrElse(parts)
-
-    def parenthesized: Seq[Text] = Text("(") +: parts :+ Text(")")
-  }
-
-  private implicit class TextExt(val text: Text) extends AnyVal {
-    def seq: Seq[Text] = Seq(text)
-  }
 }
 
