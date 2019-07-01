467c3b5948f2932d58e7766a5574f8147c4ae346
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-01 12:49:05 +0300

    reformat some testingSupport test code #SCL-15312, #SCL-10908
    (cherry picked from commit 69f63e20c71feb1b39de600e20ddfdcd358d1de4)

--- scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/scalatest/generators/FlatSpecGenerator.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/scalatest/generators/FlatSpecGenerator.scala
@@ -3,9 +3,9 @@ package org.jetbrains.plugins.scala.testingSupport.scalatest.generators
 import org.jetbrains.plugins.scala.testingSupport.scalatest.ScalaTestTestCase
 
 /**
-  * @author Roman.Shein
-  * @since 10.02.2015.
-  */
+ * @author Roman.Shein
+ * @since 10.02.2015.
+ */
 trait FlatSpecGenerator extends ScalaTestTestCase {
   val flatSpecClassName = "FlatSpecTest"
   val behaviourFlatClassName = "BehaviorFlatSpec"
@@ -16,74 +16,72 @@ trait FlatSpecGenerator extends ScalaTestTestCase {
   val testItFlatFileName = testItFlatClassName + ".scala"
 
   addSourceFile(flatSpecFileName,
-    s"""
-      |import org.scalatest._
-      |
-      |class $flatSpecClassName extends FlatSpec with GivenWhenThen {
-      | "A FlatSpecTest" should "be able to run single test" in {
-      |   Given("an empty test case")
-      |   val resultToPrint = ">>TEST: OK<<"
-      |
-      |   When("the result line is printed")
-      |   print(resultToPrint)
-      |
-      |   Then("nothing happens in the test")
-      |
-      |   info("and that's quite right")
-      | }
-      |
-      | it should "not run other tests" in {
-      |   print(">>TEST: FAILED<<")
-      | }
-      |
-      | it should "run tagged tests" taggedAs(FlatSpecTag) in {}
-      |}
-      |
-      |object FlatSpecTag extends Tag("MyTag")
-    """.stripMargin.trim()
+    s"""import org.scalatest._
+       |
+       |class $flatSpecClassName extends FlatSpec with GivenWhenThen {
+       | "A FlatSpecTest" should "be able to run single test" in {
+       |   Given("an empty test case")
+       |   val resultToPrint = ">>TEST: OK<<"
+       |
+       |   When("the result line is printed")
+       |   print(resultToPrint)
+       |
+       |   Then("nothing happens in the test")
+       |
+       |   info("and that's quite right")
+       | }
+       |
+       | it should "not run other tests" in {
+       |   print(">>TEST: FAILED<<")
+       | }
+       |
+       | it should "run tagged tests" taggedAs(FlatSpecTag) in {}
+       |}
+       |
+       |object FlatSpecTag extends Tag("MyTag")
+       |""".stripMargin.trim()
   )
 
   addSourceFile(behaviourFlatFileName,
-    s"""
-      |import org.scalatest._
-      |
-      |class $behaviourFlatClassName extends FlatSpec with GivenWhenThen {
-      |  behavior of "FlatSpec"
-      |
-      |  it should "run scopes" in {
-      |
-      |  }
-      |
-      |  it should "do other stuff" in {
-      |
-      |  }
-      |
-      |  it should "tag" taggedAs(BehaviorTag) in {
-      |
-      |  }
-      |}
-      |
-      |object BehaviorTag extends Tag("MyTag")
-    """.stripMargin.trim()
+    s"""import org.scalatest._
+       |
+       |class $behaviourFlatClassName extends FlatSpec with GivenWhenThen {
+       |  behavior of "FlatSpec"
+       |
+       |  it should "run scopes" in {
+       |
+       |  }
+       |
+       |  it should "do other stuff" in {
+       |
+       |  }
+       |
+       |  it should "tag" taggedAs(BehaviorTag) in {
+       |
+       |  }
+       |}
+       |
+       |object BehaviorTag extends Tag("MyTag")
+       |""".stripMargin.trim()
   )
 
   addSourceFile(testItFlatFileName,
-    s"""
-      |import org.scalatest._
-      |
-      |class $testItFlatClassName extends FlatSpec with GivenWhenThen {
-      | it should "run test with correct name" in {
-      | }
-      |
-      | it should "tag" taggedAs(ItTag) in {
-      | }
-      |
-      | "Test" should "be fine" in {}
-      |
-      | it should "change name" in {}
-      |}
-      |
-      |object ItTag extends Tag("MyTag")
-    """.stripMargin.trim())
+    s"""import org.scalatest._
+       |
+       |class $testItFlatClassName extends FlatSpec with GivenWhenThen {
+       | it should "run test with correct name" in {
+       | }
+       |
+       | it should "tag" taggedAs(ItTag) in {
+       | }
+       |
+       | "Test" should "be fine" in {}
+       |
+       | it should "change name" in {}
+       |}
+       |
+       |object ItTag extends Tag("MyTag")
+       |""".stripMargin.trim()
+  )
 
 }
