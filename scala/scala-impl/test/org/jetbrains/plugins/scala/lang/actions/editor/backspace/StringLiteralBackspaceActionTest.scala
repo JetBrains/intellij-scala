f9521ab60991c7ac14709bf7d61f867904e09b27
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-01 19:23:33 +0300

    added smart key setting to disable removing of multiline string closing quotes #SCL-15686 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/backspace/StringLiteralBackspaceActionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/backspace/StringLiteralBackspaceActionTest.scala
@@ -1,21 +1,68 @@
 package org.jetbrains.plugins.scala
 package lang.actions.editor.backspace
 
+import org.jetbrains.plugins.scala.settings.ScalaApplicationSettings
+
 class StringLiteralBackspaceActionTest extends ScalaBackspaceHandlerBaseTest {
+  private val DoubleQuotes = "\"\""
+  private val TripleQuotes = "\"\"\""
 
-  def testSimpleMultiLine(): Unit = doTest(
-    s"val x = ${"\"\"\""}$CARET${"\"\"\""}",
-    s"val x = ${"\"\""}$CARET"
-  )
-
-  def testInterpolated(): Unit = doTest(
-    s"val x = s${"\"\"\""}$CARET${"\"\"\""}",
-    s"val x = s${"\"\""}$CARET"
-  )
-
-  def testSimpleInterpolated(): Unit = doTest(
+  // Single line
+  def testRemovePairQuotesForInterpolatedString_EmptyFile(): Unit = doTest(
     s"""val x = s"$CARET"""",
-    s"val x = s$CARET"
+    s"""val x = s$CARET"""
+  )
+
+  // Multiline Remove
+  def testRemovePairQuotesForMultilineString(): Unit = doTest(
+    s"""val x = $TripleQuotes$CARET$TripleQuotes""",
+    s"""val x = $DoubleQuotes$CARET"""
+  )
+
+  def testRemovePairQuotesForInterpolatedMultilineString(): Unit = doTest(
+    s"""val x = s$TripleQuotes$CARET$TripleQuotes""",
+    s"""val x = s$DoubleQuotes$CARET"""
+  )
+
+  def testRemovePairQuotesForMultilineString_EmptyFile(): Unit = doTest(
+    s"""$TripleQuotes$CARET$TripleQuotes""",
+    s"""$DoubleQuotes$CARET"""
+  )
+
+  def testRemovePairQuotesForInterpolatedMultilineString_EmptyFile(): Unit = doTest(
+    s"""s$TripleQuotes$CARET$TripleQuotes""",
+    s"""s$DoubleQuotes$CARET"""
+  )
+
+  // Multiline Not Remove
+  private def doTestNotRemove(before: String, after: String): Unit = {
+    val settingBefore = ScalaApplicationSettings.getInstance.REMOVE_MULTILINE_QUOTES
+    ScalaApplicationSettings.getInstance.REMOVE_MULTILINE_QUOTES = false
+    try {
+      super.doTest(before, after)
+    } finally {
+      ScalaApplicationSettings.getInstance.REMOVE_MULTILINE_QUOTES = settingBefore
+    }
+  }
+
+  def testNotRemovePairQuotesForMultilineString(): Unit = doTestNotRemove(
+    s"""val x = $TripleQuotes$CARET$TripleQuotes""",
+    s"""val x = $DoubleQuotes$CARET$TripleQuotes"""
+  )
+
+  def testNotRemovePairQuotesForInterpolatedMultilineString(): Unit = doTestNotRemove(
+    s"""val x = s$TripleQuotes$CARET$TripleQuotes""",
+    s"""val x = s$DoubleQuotes$CARET$TripleQuotes"""
+  )
+
+  def testNotRemovePairQuotesForMultilineString_EmptyFile(): Unit = doTestNotRemove(
+    s"""$TripleQuotes$CARET$TripleQuotes""",
+    s"""$DoubleQuotes$CARET$TripleQuotes"""
+  )
+
+  def testNotRemovePairQuotesForInterpolatedMultilineString_EmptyFile(): Unit = doTestNotRemove(
+    s"""s$TripleQuotes$CARET$TripleQuotes""",
+    s"""s$DoubleQuotes$CARET$TripleQuotes"""
   )
 
 }
