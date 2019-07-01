083b8391151080a820de834c250c67ff81dc03df
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-01 12:30:19 +0300

    ScalaTestUtil.scala: add ItVerbString, add AsyncFlatSpecLike hierarchy #SCL-15312, #SCL-10908
    (cherry picked from commit f07a712293863bdf50c14dcbfbe0e042182b65d9)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestUtil.scala
@@ -5,16 +5,30 @@ package org.jetbrains.plugins.scala.testingSupport.test.scalatest
  * @since 10.04.2015.
  */
 object ScalaTestUtil {
-  val itWordFqns: Set[String] = Set(
+  val flatSpecItWordFqns: Set[String] = Set(
     "org.scalatest.FlatSpecLike.ItWord",
-    "org.scalatest.FunSpecLike.ItWord",
-    "org.scalatest.WordSpecLike.ItWord",
-    "org.scalatest.fixture.FlatSpecLike.ItWord",
-    "org.scalatest.fixture.FunSpecLike.ItWord",
-    "org.scalatest.fixture.WordSpecLike.ItWord",
-    "org.scalatest.path.FunSpecLike.ItWord",
     "org.scalatest.FlatSpecLike.ItVerbStringTaggedAs",
-    "org.scalatest.fixture.FlatSpecLike.ItVerbStringTaggedAs"
+    "org.scalatest.FlatSpecLike.ItVerbString",
+    "org.scalatest.fixture.FlatSpecLike.ItWord",
+    "org.scalatest.fixture.FlatSpecLike.ItVerbStringTaggedAs",
+    "org.scalatest.fixture.FlatSpecLike.ItVerbString"
+  )
+
+  val asyncFlatSpecItWordFqns: Set[String] = Set(
+    "org.scalatest.AsyncFlatSpecLike.ItWord",
+    "org.scalatest.AsyncFlatSpecLike.ItVerbStringTaggedAs",
+    "org.scalatest.AsyncFlatSpecLike.ItVerbString",
+    "org.scalatest.fixture.AsyncFlatSpecLike.ItWord",
+    "org.scalatest.fixture.AsyncFlatSpecLike.ItVerbStringTaggedAs",
+    "org.scalatest.fixture.AsyncFlatSpecLike.ItVerbString",
+  )
+
+  val itWordFqns: Set[String] = flatSpecItWordFqns ++ asyncFlatSpecItWordFqns ++ Set(
+    "org.scalatest.FunSpecLike.ItWord",
+    "org.scalatest.fixture.FunSpecLike.ItWord",
+    "org.scalatest.path.FunSpecLike.ItWord",
+    "org.scalatest.WordSpecLike.ItWord",
+    "org.scalatest.fixture.WordSpecLike.ItWord",
   )
 
   val funSuiteBases: List[String] = List(
