0e82e4aaa0042537e1280b31a502b5075feaeb16
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-02 12:22:14 +0300

    rewrite ScalaEditorSmartKeysConfigurable using BeanConfigurable #SCL-15743 fixed
    (cherry picked from commit ecf998e66787c09bb7ef98c198126ca73db99866)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/settings/ScalaEditorSmartKeysConfigurable.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/settings/ScalaEditorSmartKeysConfigurable.scala
@@ -1,61 +1,30 @@
 package org.jetbrains.plugins.scala.settings
 
-import com.intellij.openapi.options.{BaseConfigurable, SearchableConfigurable}
-import com.intellij.uiDesigner.core.{GridConstraints, GridLayoutManager}
+import com.intellij.openapi.options.{BeanConfigurable, SearchableConfigurable}
+import com.intellij.ui.IdeBorderFactory
 import javax.swing._
-import javax.swing.border.EmptyBorder
 import org.jetbrains.plugins.scala.ScalaBundle
 
-class ScalaEditorSmartKeysConfigurable extends BaseConfigurable with SearchableConfigurable {
-  private var myChbInsertMultilineQuotes: JCheckBox = _
-  private var myChbWrapSingleExpressionBody: JCheckBox = _
-  private var myChbUpgradeToInterpolated: JCheckBox = _
-
+class ScalaEditorSmartKeysConfigurable extends BeanConfigurable[ScalaApplicationSettings](ScalaApplicationSettings.getInstance) with SearchableConfigurable {
   override def getId: String = "ScalaSmartKeys"
   override def getDisplayName: String = "Scala"
   override def getHelpTopic: String = null
   override def enableSearch(option: String): Runnable = null
+  override def disposeUIResources(): Unit = super.disposeUIResources()
+
+  init()
+
+  def init(): Unit = {
+    val settings: ScalaApplicationSettings = getInstance();
+    checkBox(ScalaBundle.message("insert.pair.multiline.quotes"), () => settings.INSERT_MULTILINE_QUOTES, settings.INSERT_MULTILINE_QUOTES = _)
+    checkBox(ScalaBundle.message("wrap.single.expression.body"), () => settings.WRAP_SINGLE_EXPRESSION_BODY, settings.WRAP_SINGLE_EXPRESSION_BODY = _)
+    checkBox(ScalaBundle.message("upgrade.to.interpolated"), () => settings.UPGRADE_TO_INTERPOLATED, settings.UPGRADE_TO_INTERPOLATED = _)
+  }
 
   override def createComponent: JComponent = {
-    myChbInsertMultilineQuotes = new JCheckBox(ScalaBundle.message("insert.pair.multiline.quotes"))
-    myChbWrapSingleExpressionBody = new JCheckBox(ScalaBundle.message("wrap.single.expression.body"))
-    myChbUpgradeToInterpolated = new JCheckBox(ScalaBundle.message("upgrade.to.interpolated"))
-
-    val allCheckBoxes: Seq[JCheckBox] = Seq(
-      myChbInsertMultilineQuotes,
-      myChbUpgradeToInterpolated,
-      myChbWrapSingleExpressionBody
-    )
-
-    val totalRows: Int = allCheckBoxes.size
-    val panel = new JPanel(new GridLayoutManager(totalRows, 1))
-    panel.setBorder(BorderFactory.createTitledBorder(new EmptyBorder(0, 0, 0, 0), "Scala"))
-
-    val settings = ScalaApplicationSettings.getInstance
-    myChbInsertMultilineQuotes.addActionListener(_ => settings.INSERT_MULTILINE_QUOTES = myChbInsertMultilineQuotes.isSelected)
-    myChbWrapSingleExpressionBody.addActionListener(_ => settings.WRAP_SINGLE_EXPRESSION_BODY= myChbWrapSingleExpressionBody.isSelected)
-    myChbUpgradeToInterpolated.addActionListener(_ => settings.UPGRADE_TO_INTERPOLATED = myChbUpgradeToInterpolated.isSelected)
-
-    allCheckBoxes.zipWithIndex.foreach { case (checkBox, idx) =>
-      panel.add(checkBox, new GridConstraints(idx, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 1, false))
-    }
-
-    panel
+    val result = super.createComponent
+    assert(result != null, "ScalaEditorSmartKeysConfigurable panel was not created")
+    result.setBorder(IdeBorderFactory.createTitledBorder("Scala"))
+    result
   }
-
-  override def apply(): Unit = {
-    val settings = ScalaApplicationSettings.getInstance
-    settings.INSERT_MULTILINE_QUOTES = myChbInsertMultilineQuotes.isSelected
-    settings.WRAP_SINGLE_EXPRESSION_BODY = myChbWrapSingleExpressionBody.isSelected
-    settings.UPGRADE_TO_INTERPOLATED = myChbUpgradeToInterpolated.isSelected
-  }
-
-  override def reset(): Unit = {
-    val settings = ScalaApplicationSettings.getInstance
-    myChbInsertMultilineQuotes.setSelected(settings.INSERT_MULTILINE_QUOTES)
-    myChbWrapSingleExpressionBody.setSelected(settings.WRAP_SINGLE_EXPRESSION_BODY)
-    myChbUpgradeToInterpolated.setSelected(settings.UPGRADE_TO_INTERPOLATED)
-  }
-
-  override def disposeUIResources(): Unit = ()
 }
