c5d4ae2d36ed405182d257835d204f91571bfdf3
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-07-09 02:59:26 +0300

    basic implementation from UastLanguagePlugin #SCL-14071

--- scala/uast/src/org/jetbrains/plugins/scala/lang/psi/uast/ScalaUastPlugin.scala
+++ scala/uast/src/org/jetbrains/plugins/scala/lang/psi/uast/ScalaUastPlugin.scala
@@ -5,11 +5,13 @@ package uast
 
 import com.intellij.openapi.fileTypes.ExtensionFileNameMatcher
 import com.intellij.psi.{PsiClassInitializer, PsiElement, PsiMethod, PsiVariable}
-import org.jetbrains.uast.{UElement, UExpression, UastEmptyExpression, UastLanguagePlugin}
+import org.jetbrains.uast._
 
 final class ScalaUastPlugin extends UastLanguagePlugin {
 
   import UastLanguagePlugin._
+
+  import reflect.{ClassTag, classTag}
 
   override val getLanguage: ScalaLanguage = ScalaLanguage.INSTANCE
 
@@ -27,11 +29,18 @@ final class ScalaUastPlugin extends UastLanguagePlugin {
                                             fqName: String): ResolvedConstructor = null
 
   override def getInitializerBody(classInitializer: PsiClassInitializer): UExpression =
-    new UastEmptyExpression(null)
+    as[UClassInitializer](classInitializer)
+      .map(_.getUastBody)
+      .orNull
 
-  override def getInitializerBody(variable: PsiVariable): UExpression = null
+  override def getInitializerBody(variable: PsiVariable): UExpression =
+    as[UVariable](variable)
+      .fold(new UastEmptyExpression(null): UExpression)(_.getUastInitializer)
 
-  override def getMethodBody(psiMethod: PsiMethod): UExpression = null
+  override def getMethodBody(method: PsiMethod): UExpression =
+    as[UMethod](method)
+      .map(_.getUastBody)
+      .orNull
 
   override def getMethodCallExpression(element: PsiElement,
                                        containingClassFqName: String,
@@ -41,4 +50,10 @@ final class ScalaUastPlugin extends UastLanguagePlugin {
 
   override def isFileSupported(fileName: String): Boolean =
     fileNameMatcher.acceptsCharSequence(fileName)
+
+  private def as[U <: UElement : ClassTag](element: PsiElement): Option[U] =
+    if (classTag.runtimeClass.isInstance(element))
+      Some(element.asInstanceOf[U])
+    else
+      Option(convertElementWithParent(element, null))
 }
