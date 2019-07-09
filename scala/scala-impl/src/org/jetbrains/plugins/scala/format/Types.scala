236fb9a82d5e3d4a0fb0bdf2afeeb6f52fe57e1b
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2019-07-09 13:25:22 +0200

    Added more types to be supported by ScalaMalformedStringInspection
     #SCL-15648 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/format/Types.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/format/Types.scala
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.scala
 package format
 
-import java.util.{Calendar, Date}
+import java.{lang, util}
 
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
@@ -17,6 +17,7 @@ object Types {
     import stdTypes._
 
     aType match {
+      case Null => null
       case Boolean => true
       case Byte => 0.toByte
       case Char => 'c'
@@ -27,10 +28,18 @@ object Types {
       case Double => 0D
       case ScDesignatorType(element) => element.name match {
         case "String" => ""
+        case "Boolean" => new lang.Boolean(true)
+        case "Byte" => new lang.Byte(0.toByte)
+        case "Character" => new lang.Character('c')
+        case "Short" => new lang.Short(0.toShort)
+        case "Integer" => new lang.Integer(0)
+        case "Long" => new lang.Long(0L)
+        case "Float" => new lang.Float(0.0)
+        case "Double" => new lang.Double(0D)
         case "BigInt" | "BigInteger" => BigInt(0)
         case "BigDecimal" => BigDecimal(0)
-        case "Calendar" => Calendar.getInstance
-        case "Date" => new Date()
+        case "Calendar" => util.Calendar.getInstance
+        case "Date" => new util.Date()
         case _ => new Object()
       }
       case _ => new Object()
