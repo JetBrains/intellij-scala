aafb3ac52731834cf7122a9698e3ea613494d3e6
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2019-07-10 11:10:15 +0200

    Extracted 2.10 tests from PredefElementTest
     #SCL-15634

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolve2/PredefElementTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolve2/PredefElementTest.scala
@@ -1,22 +1,28 @@
-package org.jetbrains.plugins.scala.lang.resolve2
+package org.jetbrains.plugins.scala
+package lang
+package resolve2
 
-
-/**
- * Pavel.Fatin, 02.02.2010
- */
-
-class PredefElementTest extends ResolveTestBase {
+abstract class PredefElementTestBase extends ResolveTestBase {
   override def folderPath: String = {
     super.folderPath + "predef/element/"
   }
-
-  def testClass() = doTest()
-  //TODO getClass
-//  def testCompanionObject = doTest
-  def testFunction() = doTest()
-  def testObject() = doTest()
-  //TODO packageobject
-//  def testPackage = doTest
-  def testTrait() = doTest()
-  def testTypeAlias() = doTest()
 }
+
+class PredefElementTest extends PredefElementTestBase {
+  def testClass(): Unit = doTest()
+  //TODO getClass
+  //def testCompanionObject = doTest
+  def testFunction(): Unit = doTest()
+  def testObject(): Unit = doTest()
+  //TODO packageobject
+  //def testPackage = doTest
+  def testTypeAlias(): Unit = doTest()
+}
+
+
+class PredefElementTest_with_ScalaObject extends PredefElementTestBase {
+  override protected def supportedIn(version: ScalaVersion): Boolean = version < Scala_2_11
+
+  def testTrait(): Unit = doTest()
+}
+
