7a77b941b5e4e54a29994a14852379e240e1404c
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-18 16:46:50 +0300

    scala console: NPE fix on clean action #SCL-15858

--- scala/scala-impl/src/org/jetbrains/plugins/scala/console/actions/ScalaConsoleCleanAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/console/actions/ScalaConsoleCleanAction.scala
@@ -1,18 +1,18 @@
 package org.jetbrains.plugins.scala.console.actions
 
-import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys}
-import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.actionSystem.{EditorAction, EditorWriteActionHandler}
+import com.intellij.openapi.editor.{Caret, Editor}
 import org.jetbrains.plugins.scala.console.ScalaConsoleInfo
 
-class ScalaConsoleCleanAction extends AnAction with DumbAware {
+class ScalaConsoleCleanAction extends EditorAction(new EditorWriteActionHandler(false) {
 
-  override def actionPerformed(e: AnActionEvent): Unit = {
-    val editor = e.getData(CommonDataKeys.EDITOR)
-    if(editor == null || !ScalaConsoleInfo.isConsole(editor)) return
+  override def isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext): Boolean =
+    ScalaConsoleInfo.isConsole(editor)
 
-    val console = ScalaConsoleInfo.getConsole(editor)
-    if(console == null) return
-
-    console.clear()
-  }
-}
+  override def executeWriteAction(editor: Editor, caret: Caret, dataContext: DataContext): Unit =
+    ScalaConsoleInfo.getConsole(editor) match {
+      case null =>
+      case console => console.clear()
+    }
+})
