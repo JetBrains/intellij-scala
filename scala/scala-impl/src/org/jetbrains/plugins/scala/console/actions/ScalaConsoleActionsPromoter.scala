1d3f240b23775b2cd29bfaf675f0ffece8e3e805
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-24 16:49:20 +0300

    scala console: promote clean action in console editor #SCL-15774 #SCL-15921 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/console/actions/ScalaConsoleActionsPromoter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/console/actions/ScalaConsoleActionsPromoter.scala
@@ -4,17 +4,23 @@ import java.util
 import java.util.stream.Collectors
 
 import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction
-import com.intellij.openapi.actionSystem.{ActionPromoter, AnAction, DataContext}
+import com.intellij.openapi.actionSystem.{ActionPromoter, AnAction, CommonDataKeys, DataContext}
+import org.jetbrains.plugins.scala.console.ScalaConsoleInfo
 import org.jetbrains.plugins.scala.extensions.ObjectExt
 
 class ScalaConsoleActionsPromoter extends ActionPromoter {
   override def promote(actions: util.List[AnAction], context: DataContext): util.List[AnAction] = {
-    actions.stream()
-      .filter(_.is[
-        ScalaConsoleExecuteAction,
-        ScalaConsoleNewLineAction,
-        ChooseItemAction
-      ])
-      .collect(Collectors.toList())
+    val isScalaConsoleEditor = Option(context.getData(CommonDataKeys.EDITOR)).exists(ScalaConsoleInfo.isConsole)
+    if (isScalaConsoleEditor) {
+      actions.stream()
+        .filter(_.is[
+          ScalaConsoleExecuteAction,
+          ScalaConsoleNewLineAction,
+          ChooseItemAction
+        ])
+        .collect(Collectors.toList())
+    } else {
+      null
+    }
   }
 }
