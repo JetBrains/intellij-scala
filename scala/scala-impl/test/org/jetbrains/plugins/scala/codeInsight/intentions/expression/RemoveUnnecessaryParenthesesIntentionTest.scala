a8c4e1f89ac2c35f44d97822d31d4a07ac9c6744
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-25 16:15:40 +0300

    RemoveUnnecessaryParenthesesIntentionTest.scala: use CARET tag variable #SCL-15911

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInsight/intentions/expression/RemoveUnnecessaryParenthesesIntentionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInsight/intentions/expression/RemoveUnnecessaryParenthesesIntentionTest.scala
@@ -4,33 +4,29 @@ package codeInsight.intentions.expression
 import org.jetbrains.plugins.scala.codeInsight.intention.expression.RemoveUnnecessaryParenthesesIntention
 import org.jetbrains.plugins.scala.codeInsight.intentions.ScalaIntentionTestBase
 
-/**
- * Nikolay.Tropin
- * 4/29/13
- */
 class RemoveUnnecessaryParenthesesIntentionTest extends ScalaIntentionTestBase {
   def familyName: String = RemoveUnnecessaryParenthesesIntention.familyName
 
   def test_1() {
-    val text = "(<caret>1 + 1)"
+    val text   = s"(${CARET}1 + 1)"
     val result = "1 + 1"
     doTest(text, result)
   }
 
   def test_2() {
-    val text = "1 + (1 * 2<caret>)"
+    val text   = s"1 + (1 * 2$CARET)"
     val result = "1 + 1 * 2"
     doTest(text, result)
   }
 
   def test_3() {
-    val text =
-      """
-        |def f(n: Int): Int = n match {
-        |  case even if (<caret>even % 2 == 0) => (even + 1)
-        |  case odd =>  1 + (odd * 3)
-        |}
-      """
+    val text   =
+      s"""
+         |def f(n: Int): Int = n match {
+         |  case even if (${CARET}even % 2 == 0) => (even + 1)
+         |  case odd =>  1 + (odd * 3)
+         |}
+       """
     val result =
       """
         |def f(n: Int): Int = n match {
@@ -42,13 +38,13 @@ class RemoveUnnecessaryParenthesesIntentionTest extends ScalaIntentionTestBase {
   }
 
   def test_4() {
-    val text =
-      """
-        |def f(n: Int): Int = n match {
-        |  case even if (even % 2 == 0) => (even + 1<caret>)
-        |  case odd => 1 + (odd * 3)
-        |}
-      """
+    val text   =
+      s"""
+         |def f(n: Int): Int = n match {
+         |  case even if (even % 2 == 0) => (even + 1$CARET)
+         |  case odd => 1 + (odd * 3)
+         |}
+       """
     val result =
       """
         |def f(n: Int): Int = n match {
@@ -60,13 +56,13 @@ class RemoveUnnecessaryParenthesesIntentionTest extends ScalaIntentionTestBase {
   }
 
   def test_5() {
-    val text =
-      """
-        |def f(n: Int): Int = n match {
-        |  case even if (even % 2 == 0) => (even + 1)
-        |  case odd =>  1 + (odd * 3<caret>)
-        |}
-      """
+    val text   =
+      s"""
+         |def f(n: Int): Int = n match {
+         |  case even if (even % 2 == 0) => (even + 1)
+         |  case odd =>  1 + (odd * 3$CARET)
+         |}
+       """
     val result =
       """
         |def f(n: Int): Int = n match {
@@ -78,7 +74,7 @@ class RemoveUnnecessaryParenthesesIntentionTest extends ScalaIntentionTestBase {
   }
 
   def test_6() {
-    val text = "val a = ((<caret>(1)))"
+    val text   = s"val a = (($CARET(1)))"
     val result = "val a = 1"
     doTest(text, result)
   }
@@ -86,8 +82,8 @@ class RemoveUnnecessaryParenthesesIntentionTest extends ScalaIntentionTestBase {
   def test_7() {
     val text =
       """1 match {
-        |    case i if (<caret>i match {case 1 => true}) =>
-        |  }"""
+        |  case i if (${CARET}i match {case 1 => true}) =>
+        |}"""
     checkIntentionIsNotAvailable(text)
   }
 }
