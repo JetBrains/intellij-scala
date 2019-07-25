115590da5fef9ce30ad37f234925896483998e38
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-25 11:59:36 +0300

    register quickfix for parentheses as well #SCL-15911
    - extra "Inspection options popup menu item was shown due to inspections
    on parentheses didn't have their own quick fixes

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/parentheses/parentheses.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/parentheses/parentheses.scala
@@ -14,9 +14,12 @@ package object parentheses {
     if (isOnTheFly) {
       val left = TextRange.create(0, 1)
       val right = TextRange.create(element.getTextLength - 1, element.getTextLength)
-      holder.registerProblem(element, description, INFORMATION, quickfix)
-      holder.registerProblem(element, description, LIKE_UNUSED_SYMBOL, left)
-      holder.registerProblem(element, description, LIKE_UNUSED_SYMBOL, right)
+      holder.registerProblem(element, description, LIKE_UNUSED_SYMBOL, left, quickfix)
+      holder.registerProblem(element, description, LIKE_UNUSED_SYMBOL, right, quickfix)
+      if (element.getTextLength >= 4) { // for `(1)` it is enough to add quickfix only to parentheses
+        val middle = TextRange.create(2, element.getTextLength - 2)
+        holder.registerProblem(element, description, INFORMATION, middle, quickfix)
+      }
     } else {
       holder.registerProblem(element, description, WEAK_WARNING, quickfix)
     }
