95c6454b68db8504f9f53f2b6d85a1140c147780
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-07-26 19:29:43 +0300

    SCL-CR-1613: fixes after cherry pick conflicts #SCL-15911 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/parentheses/RedundantBracketsInspectionLike.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/parentheses/RedundantBracketsInspectionLike.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.codeInspection.parentheses
 
+import com.intellij.codeInspection.ProblemHighlightType.{INFORMATION, LIKE_UNUSED_SYMBOL, WEAK_WARNING}
 import com.intellij.codeInspection.{LocalQuickFix, ProblemHighlightType, ProblemsHolder}
 import com.intellij.openapi.util.TextRange
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaPsiElement
@@ -11,11 +12,14 @@ trait RedundantBracketsInspectionLike {
     if (isOnTheFly) {
       val left = TextRange.create(0, 1)
       val right = TextRange.create(element.getTextLength - 1, element.getTextLength)
-      holder.registerProblem(element, description, ProblemHighlightType.INFORMATION, quickfix)
-      holder.registerProblem(element, description, ProblemHighlightType.LIKE_UNUSED_SYMBOL, left)
-      holder.registerProblem(element, description, ProblemHighlightType.LIKE_UNUSED_SYMBOL, right)
+      holder.registerProblem(element, description, LIKE_UNUSED_SYMBOL, left, quickfix)
+      holder.registerProblem(element, description, LIKE_UNUSED_SYMBOL, right, quickfix)
+      if (element.getTextLength >= 4) { // for `(1)` it is enough to add quickfix only to parentheses
+        val middle = TextRange.create(2, element.getTextLength - 2)
+        holder.registerProblem(element, description, INFORMATION, middle, quickfix)
+      }
     } else {
-      holder.registerProblem(element, description, ProblemHighlightType.WEAK_WARNING, quickfix)
+      holder.registerProblem(element, description, WEAK_WARNING, quickfix)
     }
   }
 
