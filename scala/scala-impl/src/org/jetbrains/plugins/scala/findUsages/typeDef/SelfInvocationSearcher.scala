77bc270ce0a5689857ba21ddfba6a5f3195c6279
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2019-07-27 18:18:35 +0300

    use intersection scope in SelfInvocationSearcher, containing class may be outside of current scope EA-209542 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/typeDef/SelfInvocationSearcher.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/findUsages/typeDef/SelfInvocationSearcher.scala
@@ -2,13 +2,11 @@ package org.jetbrains.plugins.scala.findUsages.typeDef
 
 import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.search.{LocalSearchScope, PsiSearchHelper, TextOccurenceProcessor, UsageSearchContext}
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.{PsiElement, PsiReference}
 import com.intellij.util.{Processor, QueryExecutor}
 import org.jetbrains.plugins.scala.extensions.inReadAction
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScMethodLike
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScSelfInvocation
-import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 
 /**
   * Nikolay.Tropin
@@ -19,7 +17,12 @@ class SelfInvocationSearcher extends QueryExecutor[PsiReference, ReferencesSearc
     queryParameters.getElementToSearch match {
       case ml: ScMethodLike if inReadAction(ml.isConstructor) =>
         val localScope = inReadAction {
-          Option(ml.containingClass).map(new LocalSearchScope(_))
+          for {
+            containingClass <- Option(ml.containingClass)
+          } yield {
+            new LocalSearchScope(containingClass)
+              .intersectWith(queryParameters.getEffectiveSearchScope)
+          }
         }
 
         localScope.forall { scope =>
