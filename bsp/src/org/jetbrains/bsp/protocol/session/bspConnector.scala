cf4b3ce18831f461ee96a5623714ff8b0ad200a4
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-07-31 21:30:18 +0200

    correctly add the initialize clientClassesRootDir json. add empty supportedScalaVersions property, too, because bloop does not parse the data correctly otherwise. #SCL-15606 fixed

--- bsp/src/org/jetbrains/bsp/protocol/session/bspConnector.scala
+++ bsp/src/org/jetbrains/bsp/protocol/session/bspConnector.scala
@@ -4,7 +4,7 @@ import java.io.File
 import java.net.URI
 
 import ch.epfl.scala.bsp4j.{BspConnectionDetails, BuildClientCapabilities, InitializeBuildParams}
-import com.google.gson.{Gson, JsonElement, JsonObject}
+import com.google.gson.{Gson, JsonArray, JsonElement, JsonObject}
 import org.jetbrains.bsp.protocol.session.BspServerConnector.{BspCapabilities, BspConnectionMethod, ProcessBsp}
 import org.jetbrains.bsp.protocol.session.BspSession.Builder
 import org.jetbrains.bsp.{BspError, BspErrorMessage}
@@ -23,15 +23,13 @@ object BspServerConnector {
   case class BspCapabilities(languageIds: List[String])
 
   private[session] def createInitializeBuildParams(rootUri: URI, compilerOutput: URI, capabilities: BspCapabilities) = {
-    val gson: Gson = new Gson()
-
     val buildClientCapabilities = new BuildClientCapabilities(capabilities.languageIds.asJava)
     val pluginVersion = ScalaPluginVersionVerifier.getPluginVersion.map(_.presentation).getOrElse("N/A")
     val dataJson = new JsonObject()
     dataJson.addProperty("clientClassesRootDir", compilerOutput.toString)
-    val data = gson.toJson(dataJson)
+    dataJson.add("supportedScalaVersions", new JsonArray()) // shouldn't need this, but bloop may not parse the data correctly otherwise
     val initializeBuildParams = new InitializeBuildParams("IntelliJ-BSP", pluginVersion, "2.0", rootUri.toString, buildClientCapabilities)
-    initializeBuildParams.setData(data)
+    initializeBuildParams.setData(dataJson)
 
     initializeBuildParams
   }
