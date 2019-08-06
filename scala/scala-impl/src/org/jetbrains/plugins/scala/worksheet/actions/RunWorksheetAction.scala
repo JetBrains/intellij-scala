8441e9313e83897ebce17d11a412dd939bee7823
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-06 10:36:07 +0300

    RunWorksheetAction: reformat, remove infix method calls #SCL-16018

--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/RunWorksheetAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/RunWorksheetAction.scala
@@ -8,7 +8,7 @@ import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.icons.AllIcons
 import com.intellij.ide.util.EditorHelper
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
-import com.intellij.openapi.application.{ApplicationManager, ModalityState}
+import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.compiler.{CompileContext, CompileStatusNotification, CompilerManager}
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.keymap.{KeymapManager, KeymapUtil}
@@ -19,7 +19,7 @@ import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.Key
 import com.intellij.psi.{PsiDocumentManager, PsiFile}
 import javax.swing.Icon
-import org.jetbrains.plugins.scala
+import org.jetbrains.plugins.scala.extensions.{inWriteAction, invokeAndWait, invokeLater}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.project._
 import org.jetbrains.plugins.scala.statistics.{FeatureKey, Stats}
@@ -29,18 +29,13 @@ import org.jetbrains.plugins.scala.worksheet.server.WorksheetProcessManager
 import org.jetbrains.plugins.scala.worksheet.settings.RunTypes.PlainRunType
 import org.jetbrains.plugins.scala.worksheet.settings.{WorksheetCommonSettings, WorksheetFileSettings}
 
-/**
- * @author Ksenia.Sautina
- * @author Dmitry Naydanov        
- * @since 10/17/12
- */
-
 class RunWorksheetAction extends AnAction with TopComponentAction {
-  def actionPerformed(e: AnActionEvent) {
+
+  def actionPerformed(e: AnActionEvent): Unit = {
     RunWorksheetAction.runCompiler(e.getProject, auto = false)
   }
 
-  override def update(e: AnActionEvent) {
+  override def update(e: AnActionEvent): Unit = {
     super.update(e)
     
     val shortcuts = KeymapManager.getInstance.getActiveKeymap.getShortcuts("Scala.RunWorksheet")
@@ -70,52 +65,62 @@ object RunWorksheetAction {
 
     if (editor == null) return
 
-    val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument)
+    val psiFile: PsiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument)
     WorksheetProcessManager.stop(psiFile.getVirtualFile)
 
-    psiFile match {
-      case file: ScalaFile if file.isWorksheetFile =>
-        val viewer = WorksheetCache.getInstance(project) getViewer editor
+    val file = psiFile match {
+      case file: ScalaFile if file.isWorksheetFile => file
+      case _ => return
+    }
 
-        if (viewer != null && !WorksheetFileSettings.isRepl(file)) { 
-          ApplicationManager.getApplication.invokeAndWait(new Runnable {
-            override def run() {
-              scala.extensions.inWriteAction {
-                CleanWorksheetAction.resetScrollModel(viewer)
-                if (!auto) CleanWorksheetAction.cleanWorksheet(file.getNode, editor, viewer, project)
-              }
-            }
-          }, ModalityState.any())
+    val viewer = WorksheetCache.getInstance(project) getViewer editor
+
+    if (viewer != null && !WorksheetFileSettings.isRepl(file)) {
+      invokeAndWait(ModalityState.any()) {
+        inWriteAction {
+          CleanWorksheetAction.resetScrollModel(viewer)
+          if (!auto) {
+            CleanWorksheetAction.cleanWorksheet(file.getNode, editor, viewer, project)
+          }
         }
+      }
+    }
 
-        def runnable(): Unit = {
-          new WorksheetCompiler(editor, file, (className: String, addToCp: String) => {
-            ApplicationManager.getApplication.invokeLater(() => {
-              executeWorksheet(file.getName, project, file.getContainingFile, className, addToCp)
-            })
-          }, auto).compileAndRunFile()
+    def runnable(): Unit = {
+      val callback: (String, String) => Unit = (className: String, addToCp: String) =>
+        invokeLater {
+          executeWorksheet(file.getName, project, file.getContainingFile, className, addToCp)
         }
+      val compiler = new WorksheetCompiler(editor, file, callback, auto)
+      compiler.compileAndRunFile()
+    }
 
-        val fileSettings = WorksheetCommonSettings(file)
-        
-        if (fileSettings.isMakeBeforeRun) {
-          CompilerManager.getInstance(project).make(fileSettings.getModuleFor,
-            new CompileStatusNotification {
-              override def finished(aborted: Boolean, errors: Int, warnings: Int, compileContext: CompileContext) {
-                if (!aborted && errors == 0) runnable()
-              }
-            })
-        } else runnable()
-      case _ =>
+    val fileSettings = WorksheetCommonSettings(file)
+
+    if (fileSettings.isMakeBeforeRun) {
+      val compilerNotification: CompileStatusNotification =
+        (aborted: Boolean, errors: Int, _: Int, _: CompileContext) => {
+          if (!aborted && errors == 0)
+            runnable()
+        }
+      CompilerManager.getInstance(project).make(fileSettings.getModuleFor, compilerNotification)
+    } else {
+      runnable()
     }
   }
 
   //FYI: repl mode works only if we use compiler server and run worksheet with "InProcess" setting
-  def executeWorksheet(name: String, project: Project, file: PsiFile, mainClassName: String, addToCp: String) {
+  private def executeWorksheet(name: String, project: Project, file: PsiFile, mainClassName: String, addToCp: String) {
     val virtualFile = file.getVirtualFile
-    val params = createParameters(WorksheetCommonSettings(file).getModuleFor, mainClassName,
-      Option(project.baseDir) map (_.getPath) getOrElse "", addToCp, "",
-      virtualFile.getCanonicalPath) //todo extract default java options??
+    //todo extract default java options??
+    val params = createParameters(
+      module = WorksheetCommonSettings(file).getModuleFor,
+      mainClassName = mainClassName,
+      workingDirectory = Option(project.baseDir).map(_.getPath).getOrElse(""),
+      additionalCp = addToCp,
+      consoleArgs = "",
+      worksheetField = virtualFile.getCanonicalPath
+    )
 
     setUpUiAndRun(params.createOSProcessHandler(), file)
   }
@@ -143,15 +148,16 @@ object RunWorksheetAction {
 
     params.getClassPath.addRunners()
     params.getClassPath.add(additionalCp)
-    params.getProgramParametersList addParametersString worksheetField
-    if (!consoleArgs.isEmpty) params.getProgramParametersList addParametersString consoleArgs
-    params.getProgramParametersList prepend mainClassName //IMPORTANT! this must be first program argument
+    params.getProgramParametersList.addParametersString(worksheetField)
+    if (!consoleArgs.isEmpty)
+      params.getProgramParametersList.addParametersString(consoleArgs)
+    params.getProgramParametersList.prepend(mainClassName) //IMPORTANT! this must be first program argument
 
     params
   }
 
   private def setUpUiAndRun(handler: OSProcessHandler, file: PsiFile) {
-    val editor = EditorHelper openInEditor file
+    val editor = EditorHelper.openInEditor(file)
     
     val scalaFile = file match {
       case sc: ScalaFile => sc
@@ -164,8 +170,8 @@ object RunWorksheetAction {
     val myProcessListener: ProcessAdapter = new ProcessAdapter {
       override def onTextAvailable(event: ProcessEvent, outputType: Key[_]) {
         val text = event.getText
-        if (ConsoleViewContentType.NORMAL_OUTPUT == (ConsoleViewContentType getConsoleViewType outputType)) 
-          worksheetPrinter processLine text
+        if (ConsoleViewContentType.NORMAL_OUTPUT == ConsoleViewContentType.getConsoleViewType(outputType))
+          worksheetPrinter.processLine(text)
       }
 
       override def processTerminated(event: ProcessEvent): Unit = {
