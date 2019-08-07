482eca1379389ee57a3a65204a99cc927761ec08
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-08-07 11:54:12 +0300

    SCL-CR-1643 an sbt test added #SCL-16028
    (cherry picked from commit 600f3f944be625aa6e84914673953b0b112ddbe2)

---
+++ scala/scala-impl/test/org/jetbrains/sbt/codeInspection/typeAnnotation/SbtAnnotationInspectionTest.scala
@@ -0,0 +1,48 @@
+package org.jetbrains.sbt
+package codeInspection
+package typeAnnotation
+
+import java.{util => ju}
+
+import com.intellij.testFramework.EditorTestUtil.{SELECTION_END_TAG, SELECTION_START_TAG}
+import org.jetbrains.plugins.scala.codeInspection.typeAnnotation.TypeAnnotationInspectionTest
+import org.jetbrains.plugins.scala.project.Version
+import org.jetbrains.sbt.language.SbtFileType
+
+class SbtAnnotationInspectionTest extends TypeAnnotationInspectionTest with MockSbt_1_0 {
+
+  override protected val fileType = SbtFileType
+
+  override implicit val sbtVersion: Version = Sbt.LatestVersion
+
+  private var excludeInDialectSources: Boolean = _
+  private var excludeWhenTypeMatches: ju.Set[String] = _
+
+  protected override def setUp(): Unit = {
+    super.setUp()
+
+    val settings = getScalaSettings
+    import settings._
+
+    excludeInDialectSources = TYPE_ANNOTATION_EXCLUDE_IN_DIALECT_SOURCES
+    TYPE_ANNOTATION_EXCLUDE_IN_DIALECT_SOURCES = false
+
+    excludeWhenTypeMatches = TYPE_ANNOTATION_EXCLUDE_WHEN_TYPE_MATCHES
+    TYPE_ANNOTATION_EXCLUDE_WHEN_TYPE_MATCHES = ju.Collections.emptySet()
+  }
+
+  override def tearDown(): Unit = {
+    val settings = getScalaSettings
+    import settings._
+
+    TYPE_ANNOTATION_EXCLUDE_IN_DIALECT_SOURCES = excludeInDialectSources
+    TYPE_ANNOTATION_EXCLUDE_WHEN_TYPE_MATCHES = excludeWhenTypeMatches
+
+    super.tearDown()
+  }
+
+  def testPublicValue(): Unit = testQuickFix(
+    s"lazy val ${SELECTION_START_TAG}project${SELECTION_END_TAG} = Project(null, null)",
+    "lazy val project: Project = Project(null, null)"
+  )
+}
