d8635f4f1f74baa3fbfd1f98b49cfcec711302f2
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-08-08 17:56:32 +0300

    SCL-CR-1649 ScheduledForRemoval API usages removed, comments added, De Morgan laws applied #SCL-11621

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScDecoratedIconOwner.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScDecoratedIconOwner.scala
@@ -7,6 +7,7 @@ package statements
 import com.intellij.openapi.util.Iconable
 import com.intellij.psi.PsiModifier
 import com.intellij.psi.impl.{ElementBase, ElementPresentationUtil}
+import com.intellij.ui.IconManager
 import javax.swing.Icon
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScModifierListOwner
 
@@ -21,19 +22,39 @@ trait ScDecoratedIconOwner {
   // TODO baseIcon shouldn't return null in ScVariable and ScFunction
   override final def getIcon(flags: Int): Icon = decorate(baseIcon, flags)
 
-  def decorate(baseIcon: Icon, flags: Int): Icon = if (baseIcon == null || !isValid) baseIcon else {
-    val isLocked = (flags & Iconable.ICON_FLAG_READ_STATUS) != 0 && !isWritable
-    val layeredIcon = ElementBase.createLayeredIcon(this, baseIcon, ScDecoratedIconOwner.flagsFor(this, isLocked))
-    ElementPresentationUtil.addVisibilityIcon(this, flags, layeredIcon)
-  }
+  def decorate(baseIcon: Icon, flags: Int): Icon =
+    if (baseIcon != null && isValid) {
+      this.visibilityIcon(
+        baseIcon,
+        flags,
+        this.layerFlags((flags & Iconable.ICON_FLAG_READ_STATUS) == 0 || isWritable)
+      )
+    } else {
+      baseIcon
+    }
 
   protected def baseIcon: Icon
 }
 
 private object ScDecoratedIconOwner {
-  // See ElementPresentationUtil.getFlags
-  private def flagsFor(element: ScModifierListOwner, locked: Boolean): Int =
-    (if (element.hasModifierPropertyScala(PsiModifier.FINAL)) 0x400 else 0) |
-      (if (element.hasModifierProperty(PsiModifier.ABSTRACT)) 0x100 else 0) |
-      (if (locked) ElementBase.FLAGS_LOCKED else 0)
+
+  implicit class Ext(private val element: ScModifierListOwner) extends AnyVal {
+
+    import ElementPresentationUtil._
+
+    private[ScDecoratedIconOwner] def visibilityIcon(icon: Icon, flags: Int, layerFlags: Int): Icon =
+      addVisibilityIcon(
+        element,
+        flags,
+        IconManager.getInstance.createLayeredIcon(element, icon, layerFlags)
+      )
+
+    /**
+     * @see [[getFlags]], [[FLAGS_ABSTRACT]], [[FLAGS_FINAL]]
+     */
+    private[ScDecoratedIconOwner] def layerFlags(isNotLocked: Boolean): Int =
+      (if (element.hasModifierPropertyScala(PsiModifier.FINAL)) 0x400 else 0) |
+        (if (element.hasModifierProperty(PsiModifier.ABSTRACT)) 0x100 else 0) |
+        (if (isNotLocked) 0 else ElementBase.FLAGS_LOCKED)
+  }
 }
