eea7ef8a81246bab24d6be74ed5cddcea6e48727
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-14 11:24:52 +0300

    ScalaFilePasteProvider: move utility methods to extension methods #SCL-13083

--- scala/conversion/src/org/jetbrains/plugins/scala/conversion/copy/plainText/ScalaFilePasteProvider.scala
+++ scala/conversion/src/org/jetbrains/plugins/scala/conversion/copy/plainText/ScalaFilePasteProvider.scala
@@ -1,10 +1,9 @@
 package org.jetbrains.plugins.scala.conversion.copy.plainText
 
-import java.awt.datatransfer.DataFlavor.stringFlavor
+import java.awt.datatransfer.DataFlavor
 
 import com.intellij.ide.{IdeView, PasteProvider}
-import com.intellij.openapi.actionSystem.LangDataKeys.{IDE_VIEW, MODULE}
-import com.intellij.openapi.actionSystem.{CommonDataKeys, DataContext}
+import com.intellij.openapi.actionSystem.{CommonDataKeys, DataContext, LangDataKeys}
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.module.Module
@@ -12,31 +11,33 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages.showErrorDialog
 import com.intellij.psi._
 import com.intellij.util.IncorrectOperationException
+import org.jetbrains.plugins.scala.conversion.copy.plainText.ScalaFilePasteProvider._
 import org.jetbrains.plugins.scala.extensions.{ObjectExt, ToNullSafe, inWriteCommandAction, startCommand}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.project.ModuleExt
 
 import scala.util.Try
 
-/**
-  * Created by Kate Ustyuzhanina on 12/13/16.
-  */
-
 final class ScalaFilePasteProvider extends PasteProvider {
 
-  import ScalaFilePasteProvider._
+  override def isPastePossible(dataContext: DataContext): Boolean = true
 
-  def fileName(scalaFile: ScalaFile): String =
-    scalaFile.typeDefinitions.headOption.map(_.name).getOrElse("scriptFile") + ".scala"
+  override def isPasteEnabled(context: DataContext): Boolean = {
+    context.maybeIdeView.isDefined &&
+      context.maybeModule.exists(_.hasScala) && //don't affect NON scala projects even when scala plugin is turn on
+      copiedContents.zip(context.maybeProject).exists {
+        case (text, project) => PlainTextCopyUtil.isValidScalaFile(text)(project)
+      }
+  }
 
-  override def performPaste(dataContext: DataContext): Unit = {
-    val text: String = CopyPasteManager.getInstance.getContents(stringFlavor)
-    implicit val context = dataContext
+  override def performPaste(context: DataContext): Unit = {
+    val text: String = CopyPasteManager.getInstance.getContents(DataFlavor.stringFlavor)
 
-    maybeProject.flatMap(PlainTextCopyUtil.createScalaFile(text)(_)).zip {
-      maybeIdeView.flatMap(_.getOrChooseDirectory.toOption)
-    }.foreach {
-      case (scalaFile, directory) => createFileInDirectory(fileName(scalaFile), text, directory)(scalaFile.getProject)
+    for {
+      scalaFile <- context.maybeProject.flatMap(PlainTextCopyUtil.createScalaFile(text)(_))
+      directory <-  context.maybeIdeView.flatMap(_.getOrChooseDirectory.toOption)
+    } {
+      createFileInDirectory(fileName(scalaFile), text, directory)(scalaFile.getProject)
     }
   }
 
@@ -48,16 +49,19 @@ final class ScalaFilePasteProvider extends PasteProvider {
         val documentManager = PsiDocumentManager.getInstance(project)
 
         Option(documentManager.getDocument(file)).foreach { document =>
-            document.setText(fileText)
-            documentManager.commitDocument(document)
+          document.setText(fileText)
+          documentManager.commitDocument(document)
           updatePackageStatement(file, directory)
-
           new OpenFileDescriptor(project, file.getVirtualFile).navigate(true)
-          }
+        }
       }
     }.recover {
-      case e: IncorrectOperationException => showErrorDialog(project, e.getMessage, "Paste")
+      case e: IncorrectOperationException =>
+        showErrorDialog(project, e.getMessage, "Paste")
     }
+
+  private def fileName(scalaFile: ScalaFile): String =
+    scalaFile.typeDefinitions.headOption.map(_.name).getOrElse("scriptFile") + ".scala"
 
   private def updatePackageStatement(file: ScalaFile, targetDir: PsiDirectory)
                                     (implicit project: Project): Unit =
@@ -69,29 +73,18 @@ final class ScalaFilePasteProvider extends PasteProvider {
           .foreach(file.setPackageName)
       }
     }
-
-  override def isPastePossible(dataContext: DataContext): Boolean = true
-
-  override def isPasteEnabled(dataContext: DataContext): Boolean = {
-    implicit val context = dataContext
-
-    maybeIdeView.nonEmpty &&
-      maybeModule.exists(_.hasScala) && //don't affect NON scala projects even when scala plugin is turn on
-      maybeContent.zip(maybeProject).exists {
-        case (text, project) => PlainTextCopyUtil.isValidScalaFile(text)(project)
-      }
-  }
 }
 
-object ScalaFilePasteProvider {
+private object ScalaFilePasteProvider {
 
   import CommonDataKeys._
+  import LangDataKeys._
 
-  private def maybeProject(implicit context: DataContext): Option[Project] = Option(PROJECT.getData(context))
+  implicit class DataContextExt(private val context: DataContext) extends AnyVal {
+    def maybeIdeView: Option[IdeView] = Option(IDE_VIEW.getData(context))
+    def maybeProject: Option[Project] = Option(PROJECT.getData(context))
+    def maybeModule: Option[Module] = Option(MODULE.getData(context))
+  }
 
-  private def maybeIdeView(implicit context: DataContext): Option[IdeView] = Option(IDE_VIEW.getData(context))
-
-  private def maybeModule(implicit context: DataContext): Option[Module] = Option(MODULE.getData(context))
-
-  private def maybeContent: Option[String] = Option(CopyPasteManager.getInstance.getContents(stringFlavor))
+  private def copiedContents: Option[String] = Option(CopyPasteManager.getInstance.getContents(DataFlavor.stringFlavor))
 }
