412e2c7ced8f4516708d762d1626a2e2104587b6
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-14 11:54:42 +0300

    ScalaFilePasteProvider: do not trigger when copying files #SCL-13083 fixed

--- scala/conversion/src/org/jetbrains/plugins/scala/conversion/copy/plainText/ScalaFilePasteProvider.scala
+++ scala/conversion/src/org/jetbrains/plugins/scala/conversion/copy/plainText/ScalaFilePasteProvider.scala
@@ -1,6 +1,8 @@
 package org.jetbrains.plugins.scala.conversion.copy.plainText
 
 import java.awt.datatransfer.DataFlavor
+import java.io.File
+import java.{util => ju}
 
 import com.intellij.ide.{IdeView, PasteProvider}
 import com.intellij.openapi.actionSystem.{CommonDataKeys, DataContext, LangDataKeys}
@@ -16,6 +18,7 @@ import org.jetbrains.plugins.scala.extensions.{ObjectExt, ToNullSafe, inWriteCom
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.project.ModuleExt
 
+import scala.collection.JavaConverters.asScalaBufferConverter
 import scala.util.Try
 
 final class ScalaFilePasteProvider extends PasteProvider {
@@ -23,23 +26,21 @@ final class ScalaFilePasteProvider extends PasteProvider {
   override def isPastePossible(dataContext: DataContext): Boolean = true
 
   override def isPasteEnabled(context: DataContext): Boolean = {
+    val copyPasteManager = CopyPasteManager.getInstance
     context.maybeIdeView.isDefined &&
+      copyPasteManager.copiedFiles.forall(_.isEmpty) &&
       context.maybeModule.exists(_.hasScala) && //don't affect NON scala projects even when scala plugin is turn on
-      copiedContents.zip(context.maybeProject).exists {
+      copyPasteManager.copiedText.zip(context.maybeProject).exists {
         case (text, project) => PlainTextCopyUtil.isValidScalaFile(text)(project)
       }
   }
 
-  override def performPaste(context: DataContext): Unit = {
-    val text: String = CopyPasteManager.getInstance.getContents(DataFlavor.stringFlavor)
-
+  override def performPaste(context: DataContext): Unit =
     for {
+      text <- CopyPasteManager.getInstance.copiedText
       scalaFile <- context.maybeProject.flatMap(PlainTextCopyUtil.createScalaFile(text)(_))
       directory <-  context.maybeIdeView.flatMap(_.getOrChooseDirectory.toOption)
-    } {
-      createFileInDirectory(fileName(scalaFile), text, directory)(scalaFile.getProject)
-    }
-  }
+    } createFileInDirectory(fileName(scalaFile), text, directory)(scalaFile.getProject)
 
   private def createFileInDirectory(fileName: String, fileText: String, directory: PsiDirectory)
                                    (implicit project: Project) =
@@ -86,5 +87,8 @@ private object ScalaFilePasteProvider {
     def maybeModule: Option[Module] = Option(MODULE.getData(context))
   }
 
-  private def copiedContents: Option[String] = Option(CopyPasteManager.getInstance.getContents(DataFlavor.stringFlavor))
+  implicit class CopyPasteManagerExt(private val manager: CopyPasteManager) extends AnyVal {
+    def copiedText: Option[String] = Option(manager.getContents[String](DataFlavor.stringFlavor))
+    def copiedFiles: Option[Seq[File]] = Option(manager.getContents[ju.List[File]](DataFlavor.javaFileListFlavor)).map(_.asScala)
+  }
 }
