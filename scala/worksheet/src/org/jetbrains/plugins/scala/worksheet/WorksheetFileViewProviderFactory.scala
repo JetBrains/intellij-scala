f8221fb74cb3d33e77b25a562768bb3acd9f239d
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-19 11:09:32 +0300

    worksheet: extract some anonymous classes to classes with names #SCL-16014

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetFileViewProviderFactory.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetFileViewProviderFactory.scala
@@ -3,13 +3,19 @@ package worksheet
 
 import com.intellij.lang.Language
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.{FileViewProviderFactory, PsiManager, SingleRootFileViewProvider}
+import com.intellij.psi.{FileViewProvider, FileViewProviderFactory, PsiManager, SingleRootFileViewProvider}
 
 final class WorksheetFileViewProviderFactory extends FileViewProviderFactory {
 
   override def createFileViewProvider(file: VirtualFile,
                                       language: Language,
                                       manager: PsiManager,
-                                      eventSystemEnabled: Boolean): SingleRootFileViewProvider =
-    new SingleRootFileViewProvider(manager, file, eventSystemEnabled, WorksheetFileType) {}
+                                      eventSystemEnabled: Boolean): FileViewProvider =
+    new WorksheetFileViewProvider(manager, file, eventSystemEnabled)
 }
+
+final class WorksheetFileViewProvider(
+  manager: PsiManager,
+  file: VirtualFile,
+  eventSystemEnabled: Boolean
+) extends SingleRootFileViewProvider(manager, file, eventSystemEnabled, WorksheetFileType)
--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetParserDefinition.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetParserDefinition.scala
@@ -3,14 +3,18 @@ package worksheet
 
 import com.intellij.psi.FileViewProvider
 import org.jetbrains.plugins.scala.lang.psi._
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaFileImpl
 
 //noinspection TypeAnnotation
 final class WorksheetParserDefinition extends lang.parser.ScalaParserDefinitionBase(
   new stubs.elements.ScStubFileElementType("scala.worksheet.file", WorksheetLanguage.INSTANCE)
 ) {
 
-  override def createFile(viewProvider: FileViewProvider) =
-    new impl.ScalaFileImpl(viewProvider, WorksheetFileType) {
-      override def isWorksheetFile: Boolean = true
-    }
+  override def createFile(viewProvider: FileViewProvider): ScalaFileImpl = new WorksheetScalaFile(viewProvider)
+}
+
+final class WorksheetScalaFile(viewProvider: FileViewProvider)
+  extends ScalaFileImpl(viewProvider, WorksheetFileType) {
+
+  override def isWorksheetFile: Boolean = true
 }
