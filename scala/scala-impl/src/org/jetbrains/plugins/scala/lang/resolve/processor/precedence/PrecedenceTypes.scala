93bf1d89b73dadbb648b22b3d149eff16b41b9f2
andrey.sugak <andrey.sugak@jetbrains.com>
2019-08-21 17:11:08 +0300

    SCL-CR-1660: Adress review comments, add claraifications SCL-CR-1660

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/resolve/processor/precedence/PrecedenceTypes.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/resolve/processor/precedence/PrecedenceTypes.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.lang.resolve.processor.precedence
 
 import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.project.ScalaLanguageLevel.Scala_2_13
 import org.jetbrains.plugins.scala.project._
 
@@ -13,17 +14,24 @@ object PrecedenceTypes {
   val WILDCARD_IMPORT_PACKAGE: Int = 5
   val IMPORT_PACKAGE: Int          = 6
   val PACKAGE_LOCAL: Int           = 7
-
-  def OTHER_MEMBERS(target: PsiElement, place: PsiElement): Int =
-    if (target.scalaLanguageLevelOrDefault >= Scala_2_13) {
-      val sameFile = target.getContainingFile == place.getContainingFile
-
-      if (sameFile) 11
-      else          8
-    } else 11
-
-  val otherMembersValuesSet: Set[Int] = Set(8, 11)
-
+  val OTHER_MEMBERS: Int           = 8
   val WILDCARD_IMPORT: Int         = 9
   val IMPORT: Int                  = 10
+  val OTHER_MEMBERS_SAME_UNIT: Int = 11
+
+  val otherMembersValuesSet: Set[Int] = Set(OTHER_MEMBERS, OTHER_MEMBERS_SAME_UNIT)
+
+  /**
+   * Pre 2.13 scalac did not conform to the spec, and treated all
+   * declarations that are local, inherited, or made available by a package clause
+   * as if they were defined in the same unit, and thus having higher precedence than
+   * imported ones.
+   */
+  def OTHER_MEMBERS(target: PsiElement, place: PsiElement): Int =
+    if (target.scalaLanguageLevelOrDefault >= Scala_2_13) {
+      val sameFile = ScalaPsiUtil.fileContext(target) == ScalaPsiUtil.fileContext(place)
+
+      if (sameFile) OTHER_MEMBERS_SAME_UNIT
+      else          OTHER_MEMBERS
+    } else OTHER_MEMBERS_SAME_UNIT
 }
