bfb9f48e4cbead1fe24f81768e68f143703aea05
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-21 16:34:04 +0300

    worksheet: IsolatingClassLoader: isolate packages and resources in addition to classes #SCL-16122

--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/local/worksheet/util/IsolatingClassLoader.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/local/worksheet/util/IsolatingClassLoader.scala
@@ -1,20 +1,46 @@
 package org.jetbrains.jps.incremental.scala.local.worksheet.util
 
+import java.io.InputStream
+import java.net.URL
+import java.util
+
+import sun.misc.CompoundEnumeration
+
 /**
  * Hides classes that satisfy `filter` from parent class-loader.
  * Force some classes to be resolved in current classloader, instead of searching in parent.
  *
  * @param parent target parent class loader
- * @param filter returns `true` if the class can be resolved in parent,
- *               returns `false` if the class should be hidden from parent and resolved in current classloader
+ * @param filter returns `true` if the class/package can be resolved in parent,
+ *               returns `false` if the class/package should be hidden from parent and resolved in current classloader
  */
 final class IsolatingClassLoader(parent: ClassLoader, filter: String => Boolean) extends ClassLoader(parent) {
 
-  override def loadClass(className: String, resolve: Boolean): Class[_] =
-    if (filter.apply(className)) {
-      super.loadClass(className, resolve)
+  override def loadClass(name: String, resolve: Boolean): Class[_] =
+    apply(name)(super.loadClass(name, resolve)).orNull
+
+  override def getPackage(name: String): Package =
+    apply(name)(super.getPackage(name)).orNull
+
+  override def getPackages: Array[Package] =
+    super.getPackages.filter(p => filter(p.getName))
+
+  override def getResource(name: String): URL =
+    apply(name)(super.getResource(name)).orNull
+
+  override def getResourceAsStream(name: String): InputStream =
+    apply(name)(super.getResourceAsStream(name)).orNull
+
+  override def getResources(name: String): util.Enumeration[URL] =
+    apply(name)(super.getResources(name)).getOrElse(new CompoundEnumeration[URL](Array()))
+
+
+  @inline
+  private def apply[T](name: String)(body: => T): Option[T] =
+    if (filter.apply(name)) {
+      Some(body)
     } else {
-      null
+      None
     }
 }
 
