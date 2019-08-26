8ea682b5a92f5ef44523e4886544e4aebbb11890
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-26 16:45:44 +0300

    build.sbt: updated ideaVersion 193.2455 -> 193.2682

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,7 +6,7 @@ object Versions {
   // ATTENTION: when updating sbtVersion also update versions in MockSbt_1_0
   val sbtVersion: String = Sbt.latest
   val zincVersion = "1.1.1"
-  val ideaVersion = "193.2455"
+  val ideaVersion = "193.2682"
   val bspVersion = "2.0.0-M4"
   val sbtStructureVersion: String = "2018.2.1+4-88400d3f"
   val sbtIdeaShellVersion: String = "2018.3"
