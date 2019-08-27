e3460fa7e39f26d853799b02e2aaef585d428f77
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-08-27 16:46:43 +0300

    refactor Enumerator parsing rule #SCL-16171

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/Enumerator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/expressions/Enumerator.scala
@@ -18,58 +18,58 @@ object Enumerator {
   def parse(builder: ScalaPsiBuilder): Boolean = {
     val enumeratorMarker = builder.mark
 
-    def parseNonGuard(withVal: Boolean): Boolean = {
-      if (!Pattern1.parse(builder)) {
-        val res = if (withVal) {
-          builder.error(ErrMsg("wrong.pattern"))
-          enumeratorMarker.done(ScalaElementType.FOR_BINDING)
-          true
-        } else if (!Guard.parse(builder, noIf = true)) {
-          enumeratorMarker.rollbackTo()
-          false
-        } else {
-          enumeratorMarker.drop()
-          true
-        }
-        return res
-      }
+    def parseAfterPattern(hasPrefix: Boolean): Boolean = {
       builder.getTokenType match {
         case ScalaTokenTypes.tASSIGN =>
           builder.advanceLexer() //Ate =
+          if (!Expr.parse(builder)) {
+            builder.error(ErrMsg("wrong.expression"))
+          }
+          enumeratorMarker.done(ScalaElementType.FOR_BINDING)
+          true
         case ScalaTokenTypes.tCHOOSE =>
           enumeratorMarker.rollbackTo()
-          return Generator.parse(builder)
-        case _ =>
-          if (withVal) {
+          Generator.parse(builder)
+        case _                       =>
+          if (hasPrefix) {
             builder.error(ErrMsg("choose.expected"))
             enumeratorMarker.done(ScalaElementType.FOR_BINDING)
-            return true
+            true
           } else {
             enumeratorMarker.rollbackTo()
-            return false
+            false
           }
       }
-      if (!Expr.parse(builder)) {
-        builder.error(ErrMsg("wrong.expression"))
-      }
-      enumeratorMarker.done(ScalaElementType.FOR_BINDING)
-      true
     }
+
+    def parseGeneratorOrBinding(hasPrefix: Boolean): Boolean =
+      if (Pattern1.parse(builder)) {
+        parseAfterPattern(hasPrefix)
+      } else if (hasPrefix) {
+        builder.error(ErrMsg("wrong.pattern"))
+        enumeratorMarker.done(ScalaElementType.FOR_BINDING)
+        true
+      } else if (!Guard.parse(builder, noIf = true)) {
+        enumeratorMarker.rollbackTo()
+        false
+      } else {
+        enumeratorMarker.drop()
+        true
+      }
 
     builder.getTokenType match {
       case ScalaTokenTypes.kIF =>
         Guard.parse(builder)
         enumeratorMarker.drop()
         true
-      case ScalaTokenTypes.kCASE =>
-        val res = Generator.parse(builder)
-        enumeratorMarker.drop()
-        res
-      case ScalaTokenTypes.kVAL =>
+      case ScalaTokenTypes.kVAL | ScalaTokenTypes.kCASE=>
         builder.advanceLexer() //Ate val
-        parseNonGuard(true)
+        // we parse incorrect code like: 1)  for-binding with `case` keyword 2) generator with `val` keyword
+        // error highlighting is done in annotator
+        // TODO: patch ScGeneratorAnnotator
+        parseGeneratorOrBinding(true)
       case _ =>
-        parseNonGuard(false)
+        parseGeneratorOrBinding(false)
     }
   }
 }
