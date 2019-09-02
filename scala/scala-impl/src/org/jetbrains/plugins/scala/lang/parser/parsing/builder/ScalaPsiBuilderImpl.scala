9a1e6d39b881db57124fdcec70efae88e3e28ec8
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-09-02 20:23:30 +0300

    ScalaPsiBuilderImpl cleanup #SCL-16158

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/builder/ScalaPsiBuilderImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/builder/ScalaPsiBuilderImpl.scala
@@ -3,34 +3,30 @@ package lang
 package parser.parsing
 package builder
 
-import com.intellij.lang.{PsiBuilder, impl}
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.impl.source.resolve.FileContextUtil
-import org.jetbrains.plugins.scala.util.ScalaUtil.areTrailingCommasAndIdBindingEnabled
+import com.intellij.lang.PsiBuilder
+import com.intellij.lang.impl.PsiBuilderAdapter
+import com.intellij.openapi.util.text.StringUtil.isWhiteSpace
+import com.intellij.psi.impl.source.resolve.FileContextUtil.CONTAINING_FILE_KEY
 
 /**
-  * @author Alexander Podkhalyuzin
-  */
-class ScalaPsiBuilderImpl(delegate: PsiBuilder)
-  extends impl.PsiBuilderAdapter(delegate)
-    with ScalaPsiBuilder {
+ * @author Alexander Podkhalyuzin
+ */
+class ScalaPsiBuilderImpl(delegate: PsiBuilder) extends PsiBuilderAdapter(delegate)
+  with ScalaPsiBuilder {
 
-  import lexer.{ScalaTokenTypes => T}
-
-  implicit def project: Project = getProject
+  import lexer.ScalaTokenTypes._
 
   private val newlinesEnabled = new collection.mutable.Stack[Boolean]
 
   private lazy val containingFile = Option {
-    myDelegate.getUserData(FileContextUtil.CONTAINING_FILE_KEY)
+    myDelegate.getUserData(CONTAINING_FILE_KEY)
   }
 
   private lazy val (_isTrailingCommasEnabled, _isIdBindingEnabled) =
-    areTrailingCommasAndIdBindingEnabled(containingFile)
+    util.ScalaUtil.areTrailingCommasAndIdBindingEnabled(containingFile)(getProject)
 
   override def isTrailingComma: Boolean = getTokenType match {
-    case T.tCOMMA => _isTrailingCommasEnabled
+    case `tCOMMA` => _isTrailingCommasEnabled
     case _ => false
   }
 
@@ -39,7 +35,7 @@ class ScalaPsiBuilderImpl(delegate: PsiBuilder)
 
   override lazy val isMetaEnabled: Boolean = containingFile.exists {
     import meta.intellij.psi._
-    _.isMetaEnabled
+    _.isMetaEnabled(getProject)
   }
 
   override def newlineBeforeCurrentToken: Boolean =
@@ -48,7 +44,7 @@ class ScalaPsiBuilderImpl(delegate: PsiBuilder)
   override def twoNewlinesBeforeCurrentToken: Boolean =
     findPreviousNewLineSafe.exists { text =>
       s"start $text end".split('\n').exists { line =>
-        line.forall(StringUtil.isWhiteSpace)
+        line.forall(isWhiteSpace)
       }
     }
 
@@ -69,37 +65,39 @@ class ScalaPsiBuilderImpl(delegate: PsiBuilder)
     newlinesEnabled.isEmpty || newlinesEnabled.top
 
   private def findPreviousNewLineSafe =
-    if (isNewlinesEnabled && canStartStatement) this.findPreviousNewLine
-    else None
+    if (isNewlinesEnabled && canStartStatement)
+      this.findPreviousNewLine
+    else
+      None
 
   private def canStartStatement: Boolean = getTokenType match {
     case null => false
-    case T.kCATCH |
-         T.kELSE |
-         T.kEXTENDS |
-         T.kFINALLY |
-         T.kMATCH |
-         T.kWITH |
-         T.kYIELD |
-         T.tCOMMA |
-         T.tDOT |
-         T.tSEMICOLON |
-         T.tCOLON |
-         T.tASSIGN |
-         T.tFUNTYPE |
-         T.tCHOOSE |
-         T.tUPPER_BOUND |
-         T.tLOWER_BOUND |
-         T.tVIEW |
-         T.tINNER_CLASS |
-         T.tLSQBRACKET |
-         T.tRSQBRACKET |
-         T.tRPARENTHESIS |
-         T.tRBRACE => false
-    case T.kCASE =>
+    case `kCATCH` |
+         `kELSE` |
+         `kEXTENDS` |
+         `kFINALLY` |
+         `kMATCH` |
+         `kWITH` |
+         `kYIELD` |
+         `tCOMMA` |
+         `tDOT` |
+         `tSEMICOLON` |
+         `tCOLON` |
+         `tASSIGN` |
+         `tFUNTYPE` |
+         `tCHOOSE` |
+         `tUPPER_BOUND` |
+         `tLOWER_BOUND` |
+         `tVIEW` |
+         `tINNER_CLASS` |
+         `tLSQBRACKET` |
+         `tRSQBRACKET` |
+         `tRPARENTHESIS` |
+         `tRBRACE` => false
+    case `kCASE` =>
       this.predict {
         _.getTokenType match {
-          case T.kOBJECT | T.kCLASS => true
+          case `kOBJECT` | `kCLASS` => true
           case _ => false
         }
       }
