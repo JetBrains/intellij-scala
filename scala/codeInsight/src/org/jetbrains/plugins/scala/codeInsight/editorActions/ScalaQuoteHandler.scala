590a74a3e3f48f3d3a548efb41a1f1a7f9407d47
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-10-01 14:02:39 +0300

    character and identifier quotes supported #SCL-16233

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/editorActions/ScalaQuoteHandler.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/editorActions/ScalaQuoteHandler.scala
@@ -1,9 +1,11 @@
-package org.jetbrains.plugins.scala.codeInsight.editorActions
+package org.jetbrains.plugins.scala
+package codeInsight
+package editorActions
 
 import com.intellij.codeInsight.editorActions.{JavaLikeQuoteHandler, MultiCharQuoteHandler}
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.highlighter.HighlighterIterator
-import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.{ProperTextRange, TextRange}
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.{IElementType, TokenSet}
 import org.jetbrains.plugins.scala.extensions.childOf
@@ -11,24 +13,33 @@ import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes._
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
 import org.jetbrains.plugins.scala.settings.ScalaApplicationSettings
-import org.jetbrains.plugins.scala.util.MultilineStringUtil.{MultilineQuotes => Quotes}
+import org.jetbrains.plugins.scala.util.MultilineStringUtil.MultilineQuotes
 
 // TODO: autoinsert single quote like in Java
-class ScalaQuoteHandler extends JavaLikeQuoteHandler with MultiCharQuoteHandler {
+final class ScalaQuoteHandler extends JavaLikeQuoteHandler with MultiCharQuoteHandler {
+
+  import ScalaQuoteHandler._
 
   override def isClosingQuote(iterator: HighlighterIterator, offset: Int): Boolean =
     iterator.getTokenType match {
-      case `tSTRING` | `tCHAR`        => iterator.getStart <= offset && offset == iterator.getEnd - 1
-      case `tMULTILINE_STRING`        => iterator.getEnd - offset <= 3
+      case `tSTRING` | `tCHAR` => iterator.getStart <= offset && offset == iterator.getEnd - 1
+      case `tMULTILINE_STRING` => iterator.getEnd - offset <= 3
       case `tINTERPOLATED_STRING_END` => true
-      case _                          => false
+      case _ => false
     }
 
   override def isOpeningQuote(iterator: HighlighterIterator, offset: Int): Boolean =
-    iterator.getTokenType match {
-      case `tWRONG_STRING` |
-           `tINTERPOLATED_STRING` => offset == iterator.getStart
-      case _                      => false
+    iterator.getStart == offset && {
+      iterator.getTokenType match {
+        case `tWRONG_STRING` |
+             `tINTERPOLATED_STRING` => true
+        case `tSTUB` =>
+          textInRange(iterator, TextRange.from(offset, 1)) match {
+            case "`" | "'" => true
+            case _ => false
+          }
+        case _ => false
+      }
     }
 
   override def hasNonClosedLiteral(editor: Editor, iterator: HighlighterIterator, offset: Int) = true
@@ -39,7 +50,7 @@ class ScalaQuoteHandler extends JavaLikeQuoteHandler with MultiCharQuoteHandler 
            `tCHAR` |
            `tMULTILINE_STRING` |
            `tINTERPOLATED_STRING` => true
-      case _                      => false
+      case _ => false
     }
 
   override def getConcatenatableStringTokenTypes: TokenSet = TokenSet.create(tSTRING)
@@ -59,7 +70,7 @@ class ScalaQuoteHandler extends JavaLikeQuoteHandler with MultiCharQuoteHandler 
            // to be able to complete empty interpolated string s"" or s"""""" in the very end of the file
            `tIDENTIFIER` | `tINTERPOLATED_STRING_END` |
            `tCHAR` => true
-      case _  =>
+      case _ =>
         COMMENTS_TOKEN_SET.contains(tokenType) ||
           WHITES_SPACES_TOKEN_SET.contains(tokenType)
     }
@@ -67,36 +78,46 @@ class ScalaQuoteHandler extends JavaLikeQuoteHandler with MultiCharQuoteHandler 
   override def needParenthesesAroundConcatenation(element: PsiElement): Boolean =
     element.getParent match {
       case (_: ScLiteral) childOf (_: ScReferenceExpression) => true
-      case _                                                 => false
+      case _ => false
     }
 
   // NOTE: despite the naming of the method it only handles closing quote for multiline string literals
-  override def getClosingQuote(iterator: HighlighterIterator, offset: Int): CharSequence = {
-    if (!ScalaApplicationSettings.getInstance.INSERT_MULTILINE_QUOTES) return null
+  override def getClosingQuote(iterator: HighlighterIterator, offset: Int): String =
+    if (ScalaApplicationSettings.getInstance.INSERT_MULTILINE_QUOTES &&
+      rangeFromEnd(offset, MultilineQuotes).exists { range =>
+        canStartWithMultilineQuotes(iterator) && textInRange(iterator, range) == MultilineQuotes
+      })
+      MultilineQuotes
+    else
+      null
+}
 
-    if (startsWithMultilineQuotes(iterator, offset - Quotes.length)) Quotes
-    else null
-  }
+object ScalaQuoteHandler {
 
-  private def startsWithMultilineQuotes(iterator: HighlighterIterator, offset: Int): Boolean = {
-    if (offset < 0) return false
+  private def rangeFromEnd(endOffset: Int, text: String): Option[ProperTextRange] =
+    endOffset - text.length match {
+      case startOffset if startOffset >= 0 => Some(new ProperTextRange(startOffset, endOffset))
+      case _ => None
+    }
 
-    def startsWithQuotes = iterator.getDocument.getText(new TextRange(offset, offset + Quotes.length)) == Quotes
+  private def textInRange(iterator: HighlighterIterator,
+                          range: TextRange) =
+    iterator.getDocument.getText(range)
 
+  private def canStartWithMultilineQuotes(iterator: HighlighterIterator) =
     iterator.getTokenType match {
-      case `tINTERPOLATED_MULTILINE_STRING` | `tMULTILINE_STRING` =>
-        startsWithQuotes
+      case `tINTERPOLATED_MULTILINE_STRING` | `tMULTILINE_STRING` => true
       case _ =>
         // hack with optional retreat before WRONG_STRING check is required cause highlighter behaves awkwardly
         // in case of empty file (see CompleteMultilineStringTest.testCompleteMultiCaret_EmptyFileWithEmptyEndLine)
         // TODO: simplify when parsing of incomplete multiline strings is unified for interpolated and non-interpolated strings
         //  to understand what is happening here just try to open psi tree viewer and see how incomplete `s"""` or `"""` are parsed
-        if (iterator.getTokenType == tWRONG_STRING || { iterator.retreat(); iterator.getTokenType == tWRONG_STRING } ) {
+        (iterator.getTokenType == tWRONG_STRING || {
           iterator.retreat()
-          iterator.getTokenType == tSTRING && startsWithQuotes
-        } else {
-          false
+          iterator.getTokenType == tWRONG_STRING
+        }) && {
+          iterator.retreat()
+          iterator.getTokenType == tSTRING
         }
     }
-  }
 }
--- scala/codeInsight/test/org/jetbrains/plugins/scala/codeInsight/editorActions/ScalaQuoteHandlerTest.scala
+++ scala/codeInsight/test/org/jetbrains/plugins/scala/codeInsight/editorActions/ScalaQuoteHandlerTest.scala
@@ -3,74 +3,153 @@ package codeInsight
 package editorActions
 
 import com.intellij.openapi.util.text.StringUtil.convertLineSeparators
-import com.intellij.testFramework.EditorTestUtil.CARET_TAG
+import com.intellij.testFramework.EditorTestUtil.{CARET_TAG => Caret}
 
 class ScalaQuoteHandlerTest extends base.ScalaLightCodeInsightFixtureTestAdapter {
 
-  import ScalaQuoteHandlerTest.QuoteCharacter
+  import ScalaQuoteHandlerTest._
 
   def testScalaFile(): Unit = doTest(
     s"""class Foo {
-       |  val foo = $CARET_TAG
+       |  val foo = $Caret
        |}""".stripMargin
   )(
     s"""class Foo {
-       |  val foo = "$CARET_TAG"
+       |  val foo = "$Caret"
        |}""".stripMargin
   )
 
   def testSbtFile(): Unit = doTest(
-    s"name := $CARET_TAG",
+    s"name := $Caret",
     "build.sbt"
   )(
-    s"""name := "$CARET_TAG""""
+    s"""name := "$Caret""""
   )
 
   def testWorksheetFile(): Unit = doTest(
-    s"val foo = $CARET_TAG",
+    s"val foo = $Caret",
     "worksheet.sc"
   )(
-    s"""val foo = "$CARET_TAG""""
+    s"""val foo = "$Caret""""
   )
 
   def testMultilineString(): Unit = doTest(
     s"""class Foo {
-       |  val foo = ""$CARET_TAG
+       |  val foo = ""$Caret
        |}""".stripMargin
   )(
     s"""class Foo {
-       |  val foo = ""${QuoteCharacter + CARET_TAG + QuoteCharacter}""
-       |}""".stripMargin,
+       |  val foo = "$QuotedCaret"
+       |}""".stripMargin
   )
 
   def testInterpolatedString(): Unit = doTest(
     s"""class Foo {
-       |  val foo = s$CARET_TAG
+       |  val foo = s$Caret
        |}""".stripMargin
   )(
     s"""class Foo {
-       |  val foo = s"$CARET_TAG"
-       |}""".stripMargin,
+       |  val foo = s"$Caret"
+       |}""".stripMargin
   )
 
   def testInterpolatedMultilineString(): Unit = doTest(
     s"""class Foo {
-       |  val foo = s""$CARET_TAG
+       |  val foo = s""$Caret
        |}""".stripMargin
   )(
     s"""class Foo {
-       |  val foo = s""${QuoteCharacter + CARET_TAG + QuoteCharacter}""
-       |}""".stripMargin,
+       |  val foo = s"$QuotedCaret"
+       |}""".stripMargin
   )
 
-  private def doTest(fileText: String, fileName: String = "Foo.scala")
-                    (expected: String): Unit = {
+  def testQuotedIdentifierBegin(): Unit = doTest(
+    s"""class Foo {
+       |  val $Caret
+       |}""".stripMargin
+  )(
+    s"""class Foo {
+       |  val `$Caret`
+       |}""".stripMargin,
+    PatternCharacter
+  )
+
+  def testQuotedIdentifierEnd(): Unit = doTest(
+    s"""class Foo {
+       |  val `$Caret
+       |}""".stripMargin
+  )(
+    s"""class Foo {
+       |  val ``$Caret
+       |}""".stripMargin,
+    PatternCharacter
+  )
+
+  def testPatternBegin(): Unit = doTest(
+    s"""class Foo {
+       |  ??? match {
+       |    case $Caret
+       |  }
+       |}""".stripMargin
+  )(
+    s"""class Foo {
+       |  ??? match {
+       |    case `$Caret`
+       |  }
+       |}""".stripMargin,
+    PatternCharacter
+  )
+
+  def testPatternEnd(): Unit = doTest(
+    s"""class Foo {
+       |  ??? match {
+       |    case `$Caret
+       |  }
+       |}""".stripMargin
+  )(
+    s"""class Foo {
+       |  ??? match {
+       |    case ``$Caret
+       |  }
+       |}""".stripMargin,
+    PatternCharacter
+  )
+
+  def testCharBegin(): Unit = doTest(
+    s"""class Foo {
+       |  val foo = $Caret
+       |}""".stripMargin
+  )(
+    s"""class Foo {
+       |  val foo = '$Caret'
+       |}""".stripMargin,
+    CharCharacter
+  )
+
+  //  def testCharEnd(): Unit = doTest(
+  //    s"""class Foo {
+  //       |  val foo = '$Caret
+  //       |}""".stripMargin
+  //  )(
+  //    s"""class Foo {
+  //       |  val foo = ''$CARET
+  //       |}""".stripMargin,
+  //    CharCharacter
+  //  )
+
+  private def doTest(fileText: String,
+                     fileName: String = "Foo.scala")
+                    (expected: String,
+                     character: Char = QuoteCharacter): Unit = {
     myFixture.configureByText(fileName, convertLineSeparators(fileText))
-    myFixture.`type`(QuoteCharacter)
+    myFixture.`type`(character)
     myFixture.checkResult(convertLineSeparators(expected), false)
   }
 }
 
 object ScalaQuoteHandlerTest {
   private val QuoteCharacter = '\"'
+  private val PatternCharacter = '`'
+  private val CharCharacter = '\''
+  private val QuotedCaret = QuoteCharacter + (QuoteCharacter + Caret + QuoteCharacter) + QuoteCharacter
 }
