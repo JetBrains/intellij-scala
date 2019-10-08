f858dc213c8f022b5b2b9ee0c7ce2aad3b9bc990
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-10-08 17:48:40 +0300

    fix ScalaI18nMessageGotoDeclarationHandler.scala #SCL-16365 fixed
    - see changes in I18nMessageGotoDeclarationHandler.java from IDEA platform

--- scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/folding/ScalaI18nMessageGotoDeclarationHandler.scala
+++ scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/folding/ScalaI18nMessageGotoDeclarationHandler.scala
@@ -1,53 +1,29 @@
 package org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.folding
 
-import com.intellij.codeInsight.folding.impl.EditorFoldingInfo
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandlerBase
-import com.intellij.lang.ASTNode
-import com.intellij.lang.folding.{CompositeFoldingBuilder, FoldingBuilder}
+import com.intellij.codeInspection.i18n.folding.EditPropertyValueAction
 import com.intellij.lang.properties.references.PropertyReference
 import com.intellij.openapi.editor.{Editor, FoldRegion}
-import com.intellij.openapi.util.Key
 import com.intellij.psi._
 import org.jetbrains.annotations.Nullable
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall
 import org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.ScalaI18nUtil
 
-/**
- * @author Ksenia.Sautina
- * @since 7/17/12
- */
-
 class ScalaI18nMessageGotoDeclarationHandler extends GotoDeclarationHandlerBase {
-  private final val KEY: Key[FoldingBuilder] = CompositeFoldingBuilder.FOLDING_BUILDER
 
-  def getGotoDeclarationTarget(myElement: PsiElement, editor: Editor): PsiElement = {
-    var element = myElement
-    var i: Int = 4 //some street magic from Konstantin Bulenkov
-    var flag = true
-    while (element != null && i > 0 && flag) {
-      val node: ASTNode = element.getNode
-      if (node != null && node.getUserData(KEY) != null) {
-        flag = false
-      }
-      else {
-        i -= 1
-        element = element.getParent
-      }
-    }
-    if (element.isInstanceOf[ScLiteral]) {
-      return resolve(element)
-    }
-    element match {
+  override def getGotoDeclarationTarget(myElement: PsiElement, editor: Editor): PsiElement = {
+    val element = myElement
+
+    val region: FoldRegion = editor.getFoldingModel.getCollapsedRegionAtOffset(element.getTextRange.getStartOffset)
+    if (region == null) return null
+
+    val editableElement: PsiElement = EditPropertyValueAction.getEditableElement(region)
+    if (editableElement.isInstanceOf[ScLiteral])
+      return resolve(editableElement)
+
+    editableElement match {
       case methodCall: ScMethodCall =>
-        var foldRegion: FoldRegion = null
-        for (region <- editor.getFoldingModel.getAllFoldRegions) {
-          val psiElement: PsiElement = EditorFoldingInfo.get(editor).getPsiElement(region)
-          if (methodCall == psiElement) {
-            foldRegion = region
-          }
-        }
-        if (foldRegion == null || foldRegion.isExpanded) return null
         for (expression <- methodCall.args.exprsArray) {
           expression match {
             case literal: ScLiteral if ScalaI18nUtil.isI18nProperty(expression.getProject, literal) =>
