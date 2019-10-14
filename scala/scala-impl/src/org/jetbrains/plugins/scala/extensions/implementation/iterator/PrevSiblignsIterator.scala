9dba02c29defd2cbde51103a98564048bf22847b
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-10-14 19:46:20 +0300

    PrevSiblignsIterator: no need in Option wrapping/unwrapping for primitive operation

--- scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/implementation/iterator/PrevSiblignsIterator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/implementation/iterator/PrevSiblignsIterator.scala
@@ -2,12 +2,9 @@ package org.jetbrains.plugins.scala.extensions.implementation.iterator
 
 import com.intellij.psi.PsiElement
 
-/**
- * Pavel.Fatin, 11.05.2010
- */
-
 class PrevSiblignsIterator(element: PsiElement) extends Iterator[PsiElement] {
-  private var current = Option(element).map(_.getPrevSibling).orNull
+
+  private var current: PsiElement = if (element == null) null else element.getPrevSibling
 
   def hasNext: Boolean = current != null
 
