172b1c082c255e4e3e64e02d3a315e5d42939509
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2019-10-24 14:26:26 +0300

    SCL-CR-1717: Don't show run markers for 'main' methods in script-like files (worksheets, zeppelin notebooks etc) #BDIDE-399 fixed
    (cherry picked from commit 64d2d7f2e3ed8f5a64228e8db47d7db36d2f0015)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/runner/ScalaRunLineMarkerContributor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/runner/ScalaRunLineMarkerContributor.scala
@@ -5,6 +5,7 @@ import com.intellij.execution.lineMarker.{ExecutorAction, RunLineMarkerContribut
 import com.intellij.icons.AllIcons
 import com.intellij.psi.{PsiClass, PsiElement}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
 
@@ -14,6 +15,13 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
  */
 class ScalaRunLineMarkerContributor extends RunLineMarkerContributor {
   override def getInfo(element: PsiElement): Info = {
+    element.getContainingFile match {
+      case scriptLikeFile: ScalaFile
+        if scriptLikeFile.isWorksheetFile || scriptLikeFile.isScriptFile || scriptLikeFile.isMultipleDeclarationsAllowed =>
+        return null
+      case _ =>
+    }
+
     val isIdentifier = element.getNode.getElementType == ScalaTokenTypes.tIDENTIFIER
     val hasMain = element.getParent match {
       case fun: ScFunctionDefinition => ScalaMainMethodUtil.isMainMethod(fun)
