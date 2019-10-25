f37cc9a3653b2f2418f32adde8bc801ff8a454d1
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2019-10-25 19:40:35 +0300

    fix flaky move test
    process usages in imports first, or they may become invalidated

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/move/ScalaMoveRefactoringHelper.scala
@@ -0,0 +1,34 @@
+package org.jetbrains.plugins.scala.lang.refactoring.move
+
+import java.util
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.refactoring.RefactoringHelper
+import com.intellij.refactoring.util.MoveRenameUsageInfo
+import com.intellij.usageView.UsageInfo
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScReference
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportExpr
+
+class ScalaMoveRefactoringHelper extends RefactoringHelper[Unit] {
+
+  def prepareOperation(usages: Array[UsageInfo]): Unit = {
+    def priority(usageInfo: UsageInfo) = usageInfo match {
+      case moveUsage: MoveRenameUsageInfo =>
+        moveUsage.getReference match {
+          case scRef: ScReference =>
+            val isImportRef = PsiTreeUtil.getParentOfType(scRef, classOf[ScImportExpr]) != null
+
+            if (isImportRef) 0
+            else 1
+          case _ => 1
+        }
+
+      case _ => 1
+    }
+
+    util.Arrays.sort(usages, (o1: UsageInfo, o2: UsageInfo) => priority(o1) - priority(o2))
+  }
+
+  def performOperation(project: Project, operationData: Unit): Unit = {}
+}
