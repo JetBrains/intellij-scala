93a8f095de2e8e2cfd0ba36b084f5ed478dba262
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-10-25 14:16:44 +0300

    worksheet evaluation: reorganize responsibilities between classes #SCL-16018
    - when makeType = OutOfProcessServer execute the worksheet in WorksheetCompiler instead of RunWorksheetAction
    - remade callback result types

--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/topmenu/RunWorksheetAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/topmenu/RunWorksheetAction.scala
@@ -1,11 +1,6 @@
 package org.jetbrains.plugins.scala.worksheet.actions.topmenu
 
-import com.intellij.execution._
-import com.intellij.execution.configurations.JavaParameters
-import com.intellij.execution.process.{OSProcessHandler, ProcessAdapter, ProcessEvent}
-import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.icons.AllIcons
-import com.intellij.ide.util.EditorHelper
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.compiler.{CompileContext, CompileStatusNotification, CompilerManager}
@@ -14,27 +9,21 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.keymap.{KeymapManager, KeymapUtil}
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.projectRoots.{JavaSdkType, JdkUtil}
-import com.intellij.openapi.roots.ModuleRootManager
-import com.intellij.openapi.util.Key
 import com.intellij.psi.{PsiDocumentManager, PsiFile}
 import javax.swing.Icon
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.plugins.scala.ScalaBundle
-import org.jetbrains.plugins.scala.extensions.{inWriteAction, invokeAndWait, invokeLater}
+import org.jetbrains.plugins.scala.extensions.{inWriteAction, invokeAndWait}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import org.jetbrains.plugins.scala.project._
 import org.jetbrains.plugins.scala.statistics.{FeatureKey, Stats}
-import org.jetbrains.plugins.scala.worksheet.actions.topmenu.RunWorksheetAction.RunWorksheetActionError._
 import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler
-import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler.{WorksheetEvaluationError, WorksheetRunResult}
+import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler.WorksheetCompilerResult
+import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler.WorksheetCompilerResult.WorksheetEvaluationError
 import org.jetbrains.plugins.scala.worksheet.runconfiguration.WorksheetCache
 import org.jetbrains.plugins.scala.worksheet.server.WorksheetProcessManager
-import org.jetbrains.plugins.scala.worksheet.settings.WorksheetExternalRunType.PlainRunType
 import org.jetbrains.plugins.scala.worksheet.settings.{WorksheetCommonSettings, WorksheetFileSettings}
 
-import scala.concurrent.{Future, Promise}
-import scala.util.{Failure, Try}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 class RunWorksheetAction extends AnAction with TopComponentAction {
 
@@ -61,16 +50,14 @@ class RunWorksheetAction extends AnAction with TopComponentAction {
 
 object RunWorksheetAction {
 
-  private val runnerClassName = "org.jetbrains.plugins.scala.worksheet.MyWorksheetRunner"
-
-  sealed trait RunWorksheetActionError
-
-  object RunWorksheetActionError {
+  sealed trait RunWorksheetActionResult
+  object RunWorksheetActionResult {
+    object Done extends RunWorksheetActionResult
+    sealed trait RunWorksheetActionError extends RunWorksheetActionResult
     object NoModuleError extends RunWorksheetActionError
     object NoWorksheetFileError extends RunWorksheetActionError
     case class ProjectCompilationError(aborted: Boolean, errors: Int, warnings: Int, context: CompileContext) extends RunWorksheetActionError
-    case class WorksheetCompilerError(error: WorksheetCompiler.WorksheetEvaluationError) extends RunWorksheetActionError
-    case class ProcessTerminatedError(returnCode: Int, message: String) extends RunWorksheetActionError
+    case class WorksheetCompilerError(error: WorksheetEvaluationError) extends RunWorksheetActionError
   }
 
   def runCompiler(project: Project, auto: Boolean): Unit = {
@@ -85,8 +72,8 @@ object RunWorksheetAction {
     runCompiler(project, editor, auto)
   }
 
-  def runCompiler(@NotNull project: Project, @NotNull editor: Editor, auto: Boolean): Future[Either[RunWorksheetActionError, Any]] = {
-    val promise = Promise[Either[RunWorksheetActionError, Any]]()
+  def runCompiler(@NotNull project: Project, @NotNull editor: Editor, auto: Boolean): Future[RunWorksheetActionResult] = {
+    val promise = Promise[RunWorksheetActionResult]()
     try {
       doRunCompiler(project,editor, auto)(promise)
     } catch {
@@ -97,7 +84,7 @@ object RunWorksheetAction {
   }
 
   private def doRunCompiler(@NotNull project: Project, @NotNull editor: Editor, auto: Boolean)
-                           (promise: Promise[Either[RunWorksheetActionError, Any]]): Unit = {
+                           (promise: Promise[RunWorksheetActionResult]): Unit = {
 
     val psiFile: PsiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument)
     WorksheetProcessManager.stop(psiFile.getVirtualFile)
@@ -105,14 +92,14 @@ object RunWorksheetAction {
     val file: ScalaFile = psiFile match {
       case file: ScalaFile if file.isWorksheetFile => file
       case _ =>
-        promise.success(Left(NoWorksheetFileError))
+        promise.success(RunWorksheetActionResult.NoWorksheetFileError)
         return
     }
 
     val fileSettings = WorksheetCommonSettings(file)
     implicit val module: Module = fileSettings.getModuleFor
     if (module == null) {
-      promise.success(Left(NoModuleError))
+      promise.success(RunWorksheetActionResult.NoModuleError)
       return
     }
 
@@ -130,19 +117,9 @@ object RunWorksheetAction {
     }
 
     def runnable(): Unit = {
-      val callback: Either[WorksheetEvaluationError, WorksheetRunResult] => Unit = {
-        case Right(WorksheetRunResult.CompileOutsideCompilerProcess(className, addToCp)) =>
-          invokeLater {
-            try {
-              executeWorksheet(file.getName, file, className, addToCp)(promise)
-            } catch {
-              case ex: Throwable =>
-                promise.failure(ex)
-                throw ex
-            }
-          }
-        case Right(other) => promise.success(Right(other))
-        case Left(error) => promise.success(Left(WorksheetCompilerError(error)))
+      val callback: WorksheetCompilerResult => Unit = {
+        case WorksheetCompilerResult.Done    => promise.success(RunWorksheetActionResult.Done)
+        case error: WorksheetEvaluationError => promise.success(RunWorksheetActionResult.WorksheetCompilerError(error))
       }
       val compiler = new WorksheetCompiler(module, editor, file, callback, auto)
       compiler.compileAndRunFile()
@@ -154,98 +131,12 @@ object RunWorksheetAction {
           if (!aborted && errors == 0) {
             runnable()
           } else {
-            promise.success(Left(ProjectCompilationError(aborted, errors, warnings, context)))
+            promise.success(RunWorksheetActionResult.ProjectCompilationError(aborted, errors, warnings, context))
           }
         }
       CompilerManager.getInstance(project).make(module, compilerNotification)
     } else {
       runnable()
     }
-  }
-
-
-  /**
-   * FYI: REPL mode works only if we use compiler server and run worksheet with "InProcess" setting
-   * this method is only used when run type is [[org.jetbrains.plugins.scala.worksheet.server.OutOfProcessServer]]
-   */
-  private def executeWorksheet(name: String, scalaFile: ScalaFile, mainClassName: String, addToCp: String)
-                              (promise: Promise[Either[RunWorksheetActionError, Any]])
-                              (implicit module: Module): Unit = {
-    val params: JavaParameters = createDefaultParameters(scalaFile, mainClassName, addToCp)
-    val processHandler = params.createOSProcessHandler()
-    setUpUiAndRun(processHandler, scalaFile)(promise)
-  }
-
-  private def createDefaultParameters(file: PsiFile, mainClassName: String, addToCp: String)
-                                     (implicit module: Module): JavaParameters =
-    createParameters(
-      module = module,
-      mainClassName = mainClassName,
-      workingDirectory = Option(module.getProject.baseDir).fold("")(_.getPath),
-      additionalCp = addToCp,
-      consoleArgs = "",
-      worksheetField = file.getVirtualFile.getCanonicalPath
-    )
-
-  private def createParameters(module: Module,
-                               mainClassName: String,
-                               workingDirectory: String,
-                               additionalCp: String,
-                               consoleArgs: String,
-                               worksheetField: String): JavaParameters = {
-    val project = module.getProject
-
-    val rootManager = ModuleRootManager.getInstance(module)
-    val sdk = rootManager.getSdk
-    if (sdk == null || !sdk.getSdkType.isInstanceOf[JavaSdkType]) {
-      throw CantRunException.noJdkForModule(module)
-    }
-
-    val params = new JavaParameters()
-
-    params.getClassPath.addScalaClassPath(module)
-    params.setUseDynamicClasspath(JdkUtil.useDynamicClasspath(project))
-    params.getClassPath.addRunners()
-    params.setWorkingDirectory(workingDirectory)
-    params.setMainClass(runnerClassName)
-    params.configureByModule(module, JavaParameters.JDK_AND_CLASSES_AND_TESTS)
-
-    params.getClassPath.addRunners()
-    params.getClassPath.add(additionalCp)
-    params.getProgramParametersList.addParametersString(worksheetField)
-    if (!consoleArgs.isEmpty)
-      params.getProgramParametersList.addParametersString(consoleArgs)
-    params.getProgramParametersList.prepend(mainClassName) //IMPORTANT! this must be first program argument
-
-    params
-  }
-
-  private def setUpUiAndRun(handler: OSProcessHandler, scalaFile: ScalaFile)
-                           (promise: Promise[Either[RunWorksheetActionError, Any]]): Unit = {
-    val editor = EditorHelper.openInEditor(scalaFile)
-      
-    val worksheetPrinter = PlainRunType.createPrinter(editor, scalaFile)
-
-    val myProcessListener: ProcessAdapter = new ProcessAdapter {
-      override def onTextAvailable(event: ProcessEvent, outputType: Key[_]) {
-        val isStdOutput = ConsoleViewContentType.getConsoleViewType(outputType) == ConsoleViewContentType.NORMAL_OUTPUT
-        if (isStdOutput) {
-          val text = event.getText
-          worksheetPrinter.processLine(text)
-        }
-      }
-
-      override def processTerminated(event: ProcessEvent): Unit = {
-        worksheetPrinter.flushBuffer()
-        val result = event.getExitCode match {
-          case 0  => Right(())
-          case rc => Left(ProcessTerminatedError(rc, event.getText))
-        }
-        promise.success(result)
-      }
-    }
-
-    handler.addProcessListener(myProcessListener)
-    handler.startNotify()
   }
 }
--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/processor/WorksheetCompiler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/processor/WorksheetCompiler.scala
@@ -5,22 +5,32 @@ import java.io.File
 
 import com.intellij.compiler.CompilerMessageImpl
 import com.intellij.compiler.progress.CompilerTask
+import com.intellij.execution.CantRunException
+import com.intellij.execution.configurations.JavaParameters
+import com.intellij.execution.process.{OSProcessHandler, ProcessAdapter, ProcessEvent}
+import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.ide.util.EditorHelper
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.compiler.CompilerMessageCategory
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import WorksheetCompiler.WorksheetEvaluationError._
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.{JavaSdkType, JdkUtil}
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.psi.PsiFile
 import org.jetbrains.plugins.scala.compiler.CompileServerLauncher
+import org.jetbrains.plugins.scala.extensions.invokeLater
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.project._
 import org.jetbrains.plugins.scala.util.NotificationUtil
-import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler.WorksheetRunResult.{CompileOutsideCompilerProcess, Done}
-import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler.{WorksheetEvaluationError, WorksheetRunResult}
+import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompiler.EvaluationCallback
 import org.jetbrains.plugins.scala.worksheet.processor.WorksheetCompilerUtil._
 import org.jetbrains.plugins.scala.worksheet.runconfiguration.{ReplModeArgs, WorksheetCache}
+import org.jetbrains.plugins.scala.worksheet.server.RemoteServerConnector.RemoteServerConnectorResult
 import org.jetbrains.plugins.scala.worksheet.server._
-import org.jetbrains.plugins.scala.worksheet.settings.WorksheetExternalRunType.WorksheetPreprocessError
+import org.jetbrains.plugins.scala.worksheet.settings.WorksheetExternalRunType.{PlainRunType, WorksheetPreprocessError}
 import org.jetbrains.plugins.scala.worksheet.settings._
 import org.jetbrains.plugins.scala.worksheet.ui.printers.WorksheetEditorPrinter
 
@@ -28,7 +38,7 @@ class WorksheetCompiler(
   module: Module,
   editor: Editor,
   worksheetFile: ScalaFile,
-  callback: Either[WorksheetEvaluationError, WorksheetRunResult] => Unit,
+  callback: EvaluationCallback,
   auto: Boolean
 ) {
   import worksheet.processor.WorksheetCompiler._
@@ -53,15 +63,13 @@ class WorksheetCompiler(
     Option(runType.createPrinter(editor, worksheetFile))
 
   private def runCompilerTask(className: String, code: String, tempFile: File, outputDir: File): Unit = {
-    val afterCompileCallback: Either[String, Unit] => Unit = {
-      case Right(_) =>
-        val result = makeType match {
-          case OutOfProcessServer => CompileOutsideCompilerProcess(className, outputDir.getAbsolutePath)
-          case _                  => Done()
-        }
-        callback(Right(result))
-      case Left(value) =>
-        callback(Left(UnknownError(value)))
+    val afterCompileCallback: RemoteServerConnectorResult => Unit = {
+      case RemoteServerConnectorResult.Compiled =>
+        executeWorksheet(worksheetFile.getName, worksheetFile, className, outputDir.getAbsolutePath)(callback)(module)
+      case RemoteServerConnectorResult.CompiledAndEvaluated =>
+        callback(WorksheetCompilerResult.Done)
+      case error: RemoteServerConnectorResult.Error =>
+        callback(WorksheetCompilerResult.RemoteServerConnectorError(error))
     }
 
     val task = createCompilerTask
@@ -71,7 +79,7 @@ class WorksheetCompiler(
 
     val compileWork: Runnable = () => try {
       val connector = new RemoteServerConnector(module, worksheetFile, tempFile, outputDir, className, None, true)
-      connector.compileAndRun(afterCompileCallback, worksheetVirtual, consumer)
+      connector.compileAndRun(worksheetVirtual, consumer, afterCompileCallback)
     } catch {
       case ex: IllegalArgumentException =>
         showErrorNotification(ex.getMessage)
@@ -85,16 +93,24 @@ class WorksheetCompiler(
   }
 
   private def runDumbTask(replModeArgs: ReplModeArgs): Unit = {
-    val afterCompileCallback: Either[String, Unit] => Unit = {
-      case Right(_)    => callback(Right(Done()))
-      case Left(value) => callback(Left(UnknownError(value)))
+    val afterCompileCallback: RemoteServerConnectorResult => Unit = {
+      case RemoteServerConnectorResult.Compiled =>
+        val error = new AssertionError("Worksheet is expected to be evaluated in REPL mode")
+        callback(WorksheetCompilerResult.EvaluationError(error))
+      case RemoteServerConnectorResult.CompiledAndEvaluated =>
+        callback(WorksheetCompilerResult.Done)
+      case error: RemoteServerConnectorResult.Error =>
+        callback(WorksheetCompilerResult.RemoteServerConnectorError(error))
     }
     val task = createCompilerTask
 
     val consumer = new RemoteServerConnector.CompilerInterfaceImpl(task, createWorksheetPrinter, None)
-    val compileWork: Runnable = () => {
+    val compileWork: Runnable = () => try {
       val connector = new RemoteServerConnector(module, worksheetFile, new File(""), new File(""), "", Some(replModeArgs), false)
-      connector.compileAndRun(afterCompileCallback, worksheetVirtual, consumer)
+      connector.compileAndRun( worksheetVirtual, consumer, afterCompileCallback)
+    } catch {
+      case ex: IllegalArgumentException =>
+        showErrorNotification(ex.getMessage)
     }
     task.start(compileWork, EmptyRunnable)
   }
@@ -125,9 +141,10 @@ class WorksheetCompiler(
     runType.process(worksheetFile, editor) match {
       case Right(request) =>
         compileAndRunCode(request)
+
       case Left(preprocessError) =>
         showCompilationError(preprocessError)
-        callback(Left(PreprocessError(preprocessError)))
+        callback(WorksheetCompilerResult.PreprocessError(preprocessError))
     }
 
   private def showCompilationError(error: WorksheetPreprocessError): Unit = {
@@ -143,15 +160,110 @@ object WorksheetCompiler extends WorksheetPerFileConfig {
   private val EmptyRunnable: Runnable = () => {}
   private val ConfigErrorHeader = "Worksheet configuration error:"
 
-  sealed trait WorksheetRunResult
-  object WorksheetRunResult {
-    case class Done() extends WorksheetRunResult
-    case class CompileOutsideCompilerProcess(name: String, addToClasspath: String) extends WorksheetRunResult
+  type EvaluationCallback = WorksheetCompilerResult => Unit
+
+  sealed trait WorksheetCompilerResult
+  object WorksheetCompilerResult {
+    object Done extends WorksheetCompilerResult
+
+    sealed trait WorksheetEvaluationError extends WorksheetCompilerResult
+    case class PreprocessError(error: WorksheetPreprocessError) extends WorksheetEvaluationError
+    case class EvaluationError(reason: Throwable) extends WorksheetEvaluationError
+    case class ProcessTerminatedError(returnCode: Int, message: String) extends WorksheetEvaluationError
+    case class RemoteServerConnectorError(error: RemoteServerConnectorResult.Error) extends WorksheetEvaluationError
   }
 
-  sealed trait WorksheetEvaluationError
-  object WorksheetEvaluationError {
-    case class PreprocessError(error: WorksheetPreprocessError) extends WorksheetEvaluationError
-    case class UnknownError(reason: Any) extends WorksheetEvaluationError // TODO: make more specific errors
+  private val RunnerClassName = "org.jetbrains.plugins.scala.worksheet.MyWorksheetRunner"
+
+  /**
+   * FYI: REPL mode works only if we use compiler server and run worksheet with "InProcess" setting
+   * this method is only used when run type is [[org.jetbrains.plugins.scala.worksheet.server.OutOfProcessServer]]
+   */
+  private def executeWorksheet(name: String, scalaFile: ScalaFile, mainClassName: String, addToCp: String)
+                              (callback: EvaluationCallback)
+                              (implicit module: Module): Unit =
+    invokeLater {
+      try {
+        val params: JavaParameters = createDefaultParameters(scalaFile, mainClassName, addToCp)
+        val processHandler = params.createOSProcessHandler()
+        setUpUiAndRun(processHandler, scalaFile)(callback)
+      } catch {
+        case ex: Throwable =>
+          callback(WorksheetCompilerResult.EvaluationError(ex))
+          throw ex
+      }
+    }
+
+  private def createDefaultParameters(file: PsiFile, mainClassName: String, addToCp: String)
+                                     (implicit module: Module): JavaParameters =
+    createParameters(
+      module = module,
+      mainClassName = mainClassName,
+      workingDirectory = Option(module.getProject.baseDir).fold("")(_.getPath),
+      additionalCp = addToCp,
+      consoleArgs = "",
+      worksheetField = file.getVirtualFile.getCanonicalPath
+    )
+
+  private def createParameters(module: Module,
+                               mainClassName: String,
+                               workingDirectory: String,
+                               additionalCp: String,
+                               consoleArgs: String,
+                               worksheetField: String): JavaParameters = {
+    val project = module.getProject
+
+    val rootManager = ModuleRootManager.getInstance(module)
+    val sdk = rootManager.getSdk
+    if (sdk == null || !sdk.getSdkType.isInstanceOf[JavaSdkType]) {
+      throw CantRunException.noJdkForModule(module)
+    }
+
+    val params = new JavaParameters()
+
+    params.getClassPath.addScalaClassPath(module)
+    params.setUseDynamicClasspath(JdkUtil.useDynamicClasspath(project))
+    params.getClassPath.addRunners()
+    params.setWorkingDirectory(workingDirectory)
+    params.setMainClass(RunnerClassName)
+    params.configureByModule(module, JavaParameters.JDK_AND_CLASSES_AND_TESTS)
+
+    params.getClassPath.addRunners()
+    params.getClassPath.add(additionalCp)
+    params.getProgramParametersList.addParametersString(worksheetField)
+    if (!consoleArgs.isEmpty)
+      params.getProgramParametersList.addParametersString(consoleArgs)
+    params.getProgramParametersList.prepend(mainClassName) //IMPORTANT! this must be first program argument
+
+    params
+  }
+
+  private def setUpUiAndRun(handler: OSProcessHandler, scalaFile: ScalaFile)
+                           (callback: EvaluationCallback): Unit = {
+    val editor = EditorHelper.openInEditor(scalaFile)
+
+    val worksheetPrinter = PlainRunType.createPrinter(editor, scalaFile)
+
+    val myProcessListener: ProcessAdapter = new ProcessAdapter {
+      override def onTextAvailable(event: ProcessEvent, outputType: Key[_]) {
+        val isStdOutput = ConsoleViewContentType.getConsoleViewType(outputType) == ConsoleViewContentType.NORMAL_OUTPUT
+        if (isStdOutput) {
+          val text = event.getText
+          worksheetPrinter.processLine(text)
+        }
+      }
+
+      override def processTerminated(event: ProcessEvent): Unit = {
+        worksheetPrinter.flushBuffer()
+        val result = event.getExitCode match {
+          case 0  => WorksheetCompilerResult.Done
+          case rc => WorksheetCompilerResult.ProcessTerminatedError(rc, event.getText)
+        }
+        callback.apply(result)
+      }
+    }
+
+    handler.addProcessListener(myProcessListener)
+    handler.startNotify()
   }
 }
