83f60cf3bc5e16709eeac2151762f24910eed271
andrey.sugak <andrey.sugak@jetbrains.com>
2019-10-25 15:29:01 +0300

    SCL-CR-1722: Widen literal types in Function/Tuple types if needed
    (cherry picked from commit a3f3432fd5451f55e9fced0f9a201aeaa5082522)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScTupleImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/expr/ScTupleImpl.scala
@@ -9,19 +9,34 @@ import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.lang.psi.types.ScLiteralType
 import org.jetbrains.plugins.scala.lang.psi.types.api.{TupleType, Unit}
 import org.jetbrains.plugins.scala.lang.psi.types.result._
+import org.jetbrains.plugins.scala.lang.psi.types.api.Singleton
 
 /**
  * @author ilyas, Alexander Podkhalyuzin
  */
 class ScTupleImpl(node: ASTNode) extends ScExpressionImplBase(node) with ScTuple {
 
-  protected override def innerType: TypeResult = {
-    val result = exprs.map(_.`type`().getOrAny) match {
+  protected override def innerType: TypeResult =
+    Right(exprs.map(_.`type`().getOrAny) match {
       case Seq() => Unit
-      case components => TupleType(components)
-    }
-    Right(result)
-  }
+      case components =>
+        lazy val expectedComponents = this.expectedType() match {
+          case Some(TupleType(comps)) => comps
+          case _                      => Seq.empty
+        }
+
+        val widenedComponents = components.zipWithIndex.map {
+          case (lit: ScLiteralType, idx) =>
+            val expectedComp   = expectedComponents.lift(idx)
+            val inferSingleton = expectedComp.exists(_.conforms(Singleton))
+
+            if (inferSingleton) lit
+            else                lit.widen
+          case (other, _) => other
+        }
+
+        TupleType(widenedComponents)
+    })
 
   override def toString: String = "Tuple"
 }
