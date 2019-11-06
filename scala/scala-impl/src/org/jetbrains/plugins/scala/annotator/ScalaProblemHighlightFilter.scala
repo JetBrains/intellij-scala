a3ba2adc8ad6d4a43e2f83863134c35f5df108d8
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-11-06 17:58:51 +0300

    reformat, class is final, boolean logic simplified #SCL-16522

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaProblemHighlightFilter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaProblemHighlightFilter.scala
@@ -1,39 +1,35 @@
-package org.jetbrains.plugins.scala.annotator
+package org.jetbrains.plugins.scala
+package annotator
 
 import com.intellij.codeInsight.daemon.ProblemHighlightFilter
 import com.intellij.ide.scratch.ScratchFileService
 import com.intellij.openapi.roots.{JavaProjectRootsUtil, ProjectRootManager}
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
-import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.console.ScalaConsoleInfo
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.worksheet.ammonite.AmmoniteUtil
 
 /**
-  * @author Alefas
-  * @since  15/12/15
-  */
-class ScalaProblemHighlightFilter extends ProblemHighlightFilter {
-  def shouldHighlight(file: PsiFile): Boolean = {
-    file match {
-      case scalaFile: ScalaFile if scalaFile.getFileType == ScalaFileType.INSTANCE && !AmmoniteUtil.isAmmoniteFile(scalaFile) => // can be for example sbt file type
-        !JavaProjectRootsUtil.isOutsideJavaSourceRoot(file) ||
-          ScratchFileService.isInScratchRoot(scalaFile.getVirtualFile) ||
-          scalaFile.isScriptFile || ScalaConsoleInfo.isConsole(file)
-      case _ => true
-    }
+ * @author Alefas
+ * @since 15/12/15
+ */
+final class ScalaProblemHighlightFilter extends ProblemHighlightFilter {
+
+  override def shouldHighlight(file: PsiFile): Boolean = file match {
+    case file: ScalaFile if file.getFileType == ScalaFileType.INSTANCE && !AmmoniteUtil.isAmmoniteFile(file) => // can be for example sbt file type
+      !JavaProjectRootsUtil.isOutsideJavaSourceRoot(file) ||
+        ScratchFileService.isInScratchRoot(file.getVirtualFile) ||
+        file.isScriptFile ||
+        ScalaConsoleInfo.isConsole(file)
+    case _ => true
   }
 
-  override def shouldProcessInBatch(file: PsiFile): Boolean = {
-    if (ProblemHighlightFilter.shouldHighlightFile(file)) {
-      if (file.getFileType == ScalaFileType.INSTANCE) {
-        val vFile: VirtualFile = file.getVirtualFile
-        if (vFile != null && ProjectRootManager.getInstance(file.getProject).getFileIndex.isInLibrarySource(vFile)) {
-          return false
-        }
+  override def shouldProcessInBatch(file: PsiFile): Boolean =
+    ProblemHighlightFilter.shouldHighlightFile(file) && {
+      file.getFileType != ScalaFileType.INSTANCE || {
+        val virtualFile = file.getVirtualFile
+        virtualFile == null ||
+          !ProjectRootManager.getInstance(file.getProject).getFileIndex.isInLibrarySource(virtualFile)
       }
-      true
-    } else false
-  }
+    }
 }
