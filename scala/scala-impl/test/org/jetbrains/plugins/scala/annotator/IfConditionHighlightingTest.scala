5a019ae660978a9f62ad9821bcf7db3f2772125f
andrey.sugak <andrey.sugak@jetbrains.com>
2019-11-06 17:39:35 +0300

    Add test for SCL-16304
    SCL-CR-1691

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/IfConditionHighlightingTest.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.annotator
+
+class IfConditionHighlightingTest extends ScalaHighlightingTestBase {
+  def testSCL16304(): Unit = {
+    val code =
+      """
+        |object Test {
+        |  def foo: Boolean = false
+        |  foo && {
+        |    if(42) foo
+        |    else   foo
+        |  }
+        |
+        |  val a = if ("123") 42 else 43
+        |
+        |  implicit def double2Boolean(d: Double): Boolean = false
+        |
+        |  if (2d) () else ()
+        |}
+        |""".stripMargin
+
+    assertMessages(errorsFromScalaCode(code))(
+      Error("42", "Expression of type Int doesn't conform to expected type Boolean"),
+      Error("\"123\"", "Expression of type String doesn't conform to expected type Boolean")
+    )
+  }
+}
