53ed922dca9592f34789e2517023947d467b73fa
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-11-07 17:41:59 +0300

    worksheets repl: fixed error reporting for scala 2.13.0 #SCL-16493
    - there were no `doReport` method in ReplReporterImpl, it was called `print`

--- build.sbt
+++ build.sbt
@@ -117,6 +117,7 @@ lazy val scalaImpl: sbt.Project =
       ),
       packageFileMappings ++= Seq(
         baseDirectory.in(compilerJps).value / "resources" / "ILoopWrapperImpl.scala" -> "lib/jps/repl-interface-sources.jar",
+        baseDirectory.in(compilerJps).value / "resources" / "ILoopWrapper213_0Impl.scala" -> "lib/jps/repl-interface-sources.jar",
         baseDirectory.in(compilerJps).value / "resources" / "ILoopWrapper213Impl.scala" -> "lib/jps/repl-interface-sources.jar"
       ),
       buildInfoPackage := "org.jetbrains.plugins.scala.buildinfo",
---
+++ scala/compiler-jps/resources/ILoopWrapper213_0Impl.scala
@@ -0,0 +1,84 @@
+package org.jetbrains.jps.incremental.scala.local.worksheet
+
+import java.io.{File, PrintWriter}
+
+import org.jetbrains.jps.incremental.scala.local.worksheet.ILoopWrapper
+import org.jetbrains.jps.incremental.scala.local.worksheet.ILoopWrapper213_0Impl.DummyConfig
+
+import scala.reflect.classTag
+import scala.reflect.internal.util.Position
+import scala.tools.nsc.Settings
+import scala.tools.nsc.interpreter.NamedParam.Typed
+import scala.tools.nsc.interpreter.StdReplTags.tagOfIMain
+import scala.tools.nsc.interpreter.shell.{ILoop, ReplReporterImpl, ShellConfig}
+import scala.tools.nsc.interpreter.{IMain, Results}
+
+import scala.collection.JavaConverters._
+
+/**
+ * ATTENTION: when editing ensure to increase the ILoopWrapperFactoryHandler.WRAPPER_VERSION
+ */
+class ILoopWrapper213_0Impl(
+  myOut: PrintWriter,
+  wrapperReporter: ILoopWrapperReporter,
+  projectFullCp: java.util.List[String]
+) extends  {
+  val myConfig = new DummyConfig
+} with ILoop(myConfig, out = myOut)
+  with ILoopWrapper {
+
+  override def init(): Unit = {
+    val mySettings = new Settings
+    mySettings.classpath.append(projectFullCp.asScala.mkString(File.pathSeparator))
+    // do not use java class path because it contains scala library jars with version
+    // different from one that is used during compilation (it is passed from the plugin classpath)
+    mySettings.usejavacp.tryToSetFromPropertyValue(false.toString)
+
+    createInterpreter(mySettings)
+
+    val itp = intp.asInstanceOf[IMain]
+    itp.initializeCompiler()
+    itp.quietBind(new Typed[IMain]("$intp", itp)(tagOfIMain, classTag[IMain]))
+    itp.setContextClassLoader()
+  }
+
+  override def createInterpreter(interpreterSettings: Settings): Unit = {
+    val reporter = new ReplReporterImpl(myConfig, interpreterSettings, out) {
+      override def print(pos: Position, msg: String, severity: Severity): Unit =
+        wrapperReporter.report(severity.toString, pos.line, pos.column, pos.lineContent, msg)
+    }
+    intp = new IMain(interpreterSettings, None, interpreterSettings, reporter)
+  }
+
+  override def reset(): Unit = {
+    intp.reset()
+  }
+
+  override def shutdown(): Unit = {
+    closeInterpreter()
+  }
+
+  override def processChunk(code: String): Boolean =
+    intp.interpret(code) match {
+      case Results.Success => true
+      case _ => false
+    }
+
+  override def getOutputWriter: PrintWriter = myOut
+}
+
+object ILoopWrapper213_0Impl {
+  class DummyConfig extends ShellConfig {
+    override def filesToPaste: List[String] = List.empty
+
+    override def filesToLoad: List[String] = List.empty
+
+    override def batchText: String = ""
+
+    override def batchMode: Boolean = false
+
+    override def doCompletion: Boolean = false
+
+    override def haveInteractiveConsole: Boolean = false
+  }
+}
