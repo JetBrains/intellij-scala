0f43cc6a2e53f74b3e40c0250481f861fc283560
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-11-18 19:19:03 +0300

    worksheet: promote RunWorksheetAction over RunScratchAction from kotlin plugin #SCL-16339
    - Kotlin plugin (bundled plugin) has the same shortcut for the action (Ctrl + Alt + W) and it doesn't check if it was called in a Kotlin scratch file

--- scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/repl/WorksheetReplRunActionPromoter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/worksheet/actions/repl/WorksheetReplRunActionPromoter.scala
@@ -1,10 +1,10 @@
 package org.jetbrains.plugins.scala.worksheet.actions.repl
 
-import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.{AnAction, DataContext}
 import org.jetbrains.plugins.scala.actions.SingleActionPromoterBase
 
 final class WorksheetReplRunActionPromoter extends SingleActionPromoterBase {
 
-  override def shouldPromote(anAction: AnAction): Boolean = 
+  override def shouldPromote(anAction: AnAction, context: DataContext): Boolean =
     anAction.isInstanceOf[WorksheetReplRunAction]
 }
