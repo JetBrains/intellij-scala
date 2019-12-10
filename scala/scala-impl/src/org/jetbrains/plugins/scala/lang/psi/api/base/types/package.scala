e3cc8b5918c6efabfc81efe09d8e062b73d07371
andrey.sugak <andrey.sugak@jetbrains.com>
2019-12-10 21:56:34 +0300

    Fix type of vararg parameter type elements (now designates to scala.Seq)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/base/types/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/base/types/package.scala
@@ -10,9 +10,9 @@ package object types {
 
   implicit class ScTypeElementExt(private val typeElement: ScTypeElement) extends AnyVal {
     def calcType: ScType = typeElement.`type`().getOrAny
-    
+
     def getParamTypeText: String =
-      if (typeElement.isRepeated) s"_root_.scala.collection.Seq[${typeElement.getText}]"
+      if (typeElement.isRepeated) s"_root_.scala.Seq[${typeElement.getText}]"
       else                        typeElement.getText
   }
 }
