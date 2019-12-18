f5cffe03b7a62537529a3c8297a72eebaa3f9c18
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-12-18 19:35:27 +0300

    ScalaElementPattern: extract core logic to scala code #SCL-16749

--- scala/scala-impl/src/org/jetbrains/plugins/scala/patterns/ScalaElementPattern.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/patterns/ScalaElementPattern.java
@@ -4,16 +4,11 @@ import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.InitialPatternCondition;
 import com.intellij.patterns.PatternCondition;
 import com.intellij.patterns.PsiJavaElementPattern;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
-import com.intellij.psi.ResolveResult;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaPsiElement;
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral;
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScReference;
-import org.jetbrains.plugins.scala.lang.psi.api.expr.*;
-import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScArguments;
 
 public class ScalaElementPattern<T extends ScalaPsiElement, Self extends ScalaElementPattern<T, Self>> extends PsiJavaElementPattern<T, Self> {
   public ScalaElementPattern(Class<T> tClass) {
@@ -27,9 +22,7 @@ public class ScalaElementPattern<T extends ScalaPsiElement, Self extends ScalaEl
   public Self isRegExpLiteral() {
     return with(new PatternCondition<T>("isRegExpLiteral") {
       public boolean accepts(@NotNull final T literal, final ProcessingContext context) {
-        final PsiElement parent = literal.getParent();
-        return parent instanceof ScReferenceExpression && parent.getText().endsWith(".r") ||
-                parent instanceof ScPostfixExpr && ((ScPostfixExpr) parent).operation().getText().equals("r");
+        return ScalaElementPatternImpl.isRegExpLiteral(literal, context);
       }
     });
   }
@@ -37,24 +30,7 @@ public class ScalaElementPattern<T extends ScalaPsiElement, Self extends ScalaEl
   public Self callArgument(final int index, final ElementPattern<? extends PsiMethod> methodPattern) {
     return with(new PatternCondition<T>("callArgument") {
       public boolean accepts(@NotNull final T host, final ProcessingContext context) {
-        final PsiElement parent = host.getParent();
-        if (parent instanceof ScArguments) {
-          final ScArgumentExprList psiExpressionList = (ScArgumentExprList) parent;
-          final ScExpression[] psiExpressions = psiExpressionList.exprsArray();
-          if (!(psiExpressions.length > index && psiExpressions[index] == host)) return false;
-
-          final PsiElement element = psiExpressionList.getParent();
-          if (element instanceof ScMethodCall) {
-            final ScalaPsiElement expression = ((ScMethodCall) element).getEffectiveInvokedExpr();
-            if (expression instanceof ScReference) {
-              final ScReference ref = (ScReference) expression;
-              for (ResolveResult result : ref.multiResolveScala(false))
-                if (methodPattern.accepts(result.getElement(), context))
-                  return true;
-            }
-          }
-        }
-        return false;
+        return ScalaElementPatternImpl.isMethodCallArgument(host, context, index, methodPattern);
       }
     });
   }
---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/patterns/ScalaElementPatternImpl.scala
@@ -0,0 +1,47 @@
+package org.jetbrains.plugins.scala.patterns
+
+import com.intellij.patterns.ElementPattern
+import com.intellij.psi.PsiMethod
+import com.intellij.util.ProcessingContext
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaPsiElement
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScReference
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScArgumentExprList, ScMethodCall, ScPostfixExpr, ScReferenceExpression}
+
+private object ScalaElementPatternImpl {
+
+  def isRegExpLiteral[T <: ScalaPsiElement](literal: T, context: ProcessingContext): Boolean =
+    literal.getParent match {
+      case ref: ScReferenceExpression => ref.getText.endsWith(".r")
+      case postfix: ScPostfixExpr     => postfix.operation.textMatches("r")
+      case _                          => false
+    }
+
+  def isMethodCallArgument[T <: ScalaPsiElement](
+    host: T, context: ProcessingContext,
+    index: Int,
+    methodPattern: ElementPattern[_ <: PsiMethod]
+  ): Boolean = {
+    host.getParent match {
+      case argsList: ScArgumentExprList =>
+        val args = argsList.exprsArray
+        val hostIsAnArgument = index < args.length && (args(index) eq host)
+        if (!hostIsAnArgument)
+          return false
+
+        argsList.getParent match {
+          case call: ScMethodCall =>
+            call.getEffectiveInvokedExpr match {
+              case ref: ScReference =>
+                for (result <- ref.multiResolveScala(false)) {
+                  if (methodPattern.accepts(result.getElement, context))
+                    return true
+                }
+              case _ =>
+            }
+          case _ =>
+        }
+      case _ =>
+    }
+    false
+  }
+}
