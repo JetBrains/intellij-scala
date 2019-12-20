55ba27a93564021a7b60529424b726775965ee3e
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2019-12-20 12:34:38 +0300

    RunConsoleAction: check if project is disposed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/console/actions/RunConsoleAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/console/actions/RunConsoleAction.scala
@@ -20,6 +20,8 @@ import scala.collection.JavaConverters.collectionAsScalaIterableConverter
 class RunConsoleAction extends AnAction with RunConsoleAction.RunActionBase[ScalaConsoleConfigurationType] {
 
   override def update(e: AnActionEvent): Unit = {
+    if (e.getProject.isDisposed) return
+
     e.getPresentation.setIcon(Icons.SCALA_CONSOLE)
 
     if(!e.getProject.hasScala) {
