475cd47f2ecbf15a39ed7757f02b52dfa18788eb
Pavel Fatin <pavel.fatin@jetbrains.com>
2019-12-30 17:28:41 +0100

    IntelliJ IDEA version updated to 201.3283

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -7,7 +7,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.3.5"
   val zincVersion = "1.1.1"
-  val intellijVersion = "201.3013"
+  val intellijVersion = "201.3283"
   val bspVersion = "2.0.0-M4"
   val sbtStructureVersion: String = "2018.2.1+4-88400d3f"
   val sbtIdeaShellVersion: String = "2018.3"
