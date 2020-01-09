74845daec7be65dd6fa941efd77a57dec045d6b3
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-01-09 12:11:05 +0300

    fix compilation errors in ScalaCallHierarchyBrowser: use new Api
    (cherry picked from commit 119dba4ee8cbed6804ef199c483cd488bf7acbd9)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/hierarchy/ScalaMethodHierarchyProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/hierarchy/ScalaMethodHierarchyProvider.scala
@@ -1,12 +1,6 @@
 package org.jetbrains.plugins.scala
 package hierarchy
 
-
 import com.intellij.ide.hierarchy.method.JavaMethodHierarchyProvider
-
-/**
- * User: Alexander Podkhalyuzin
- * Date: 09.06.2009
- */
 
 class ScalaMethodHierarchyProvider extends JavaMethodHierarchyProvider
