e6f879c7ec4ed0c8dd951811763e10a713a9c17f
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-01-17 19:38:04 +0300

    remove project component: TypeMismatchTooltipsHandler
    #SCL-16844

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchTooltipsHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchTooltipsHandler.scala
@@ -3,23 +3,29 @@ package org.jetbrains.plugins.scala.annotator
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.daemon.impl.{DaemonCodeAnalyzerImpl, HighlightInfo}
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.editor.event.{EditorMouseEvent, EditorMouseMotionListener}
 import com.intellij.openapi.editor.impl.EditorMouseHoverPopupControl
 import com.intellij.openapi.editor.{Editor, EditorFactory}
-import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.scala.annotator.TypeMismatchTooltipsHandler.TypeMismatchTooltipPrefix
+import com.intellij.openapi.project.{Project, ProjectManagerListener}
 import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 
 // Annotation.setTooltip is shown both on mouse hover (EditorMouseHoverPopupManager), and on Ctrl / Cmd + F1 (ShowErrorDescriptionAction)
 // But when type mismatch hints are enabled, we don't want tooltips on mouse hover (on the expression), but still want tooltips on the explicit action.
 // https://youtrack.jetbrains.com/issue/SCL-15782
-class TypeMismatchTooltipsHandler(project: Project, editorFactory: EditorFactory) extends ProjectComponent {
+class TypeMismatchTooltipsHandler extends ProjectManagerListener {
+  override def projectOpened(project: Project): Unit = {
+    val listener = new TypeMismatchTooltipsHandler.Listener(project)
+    EditorFactory.getInstance().getEventMulticaster.addEditorMouseMotionListener(listener, project)
+  }
+}
 
-  // See com.intellij.openapi.editor.impl.EditorMouseHoverPopupControl
-  private var popupsWereDisabled = false;
+private object TypeMismatchTooltipsHandler {
+  private val TypeMismatchTooltipPrefix = "<html><body>Type mismatch"
 
-  private val mouseMovedListener: EditorMouseMotionListener = new EditorMouseMotionListener {
+  private class Listener(project: Project) extends EditorMouseMotionListener {
+    // See com.intellij.openapi.editor.impl.EditorMouseHoverPopupControl
+    private var popupsWereDisabled = false
+
     override def mouseMoved(e: EditorMouseEvent): Unit = {
       if (!e.isConsumed && project.isInitialized && !project.isDisposed) {
         val editor = e.getEditor
@@ -42,35 +48,24 @@ class TypeMismatchTooltipsHandler(project: Project, editorFactory: EditorFactory
         }
       }
     }
-  }
 
-  private def disableTooltipOnMouseHoverForTypeMismatchErrors(editor: Editor, maybeInfo: Option[HighlightInfo]): Unit = {
-    val isTypeMismatchError = maybeInfo.exists(info =>
-      info.getSeverity == HighlightSeverity.ERROR &&
-        Option(info.getToolTip).exists(_.startsWith(TypeMismatchTooltipPrefix)))
+    private def disableTooltipOnMouseHoverForTypeMismatchErrors(editor: Editor, maybeInfo: Option[HighlightInfo]): Unit = {
+      val isTypeMismatchError = maybeInfo.exists(info =>
+        info.getSeverity == HighlightSeverity.ERROR &&
+          Option(info.getToolTip).exists(_.startsWith(TypeMismatchTooltipPrefix)))
 
-    if (isTypeMismatchError) {
-      if (!popupsWereDisabled && !EditorMouseHoverPopupControl.arePopupsDisabled(editor)) {
-        EditorMouseHoverPopupControl.disablePopups(editor);
-        popupsWereDisabled = true
-      }
-    } else {
-      if (popupsWereDisabled) {
-        EditorMouseHoverPopupControl.enablePopups(editor);
-        popupsWereDisabled = false
+      if (isTypeMismatchError) {
+        if (!popupsWereDisabled && !EditorMouseHoverPopupControl.arePopupsDisabled(editor)) {
+          EditorMouseHoverPopupControl.disablePopups(editor);
+          popupsWereDisabled = true
+        }
+      } else {
+        if (popupsWereDisabled) {
+          EditorMouseHoverPopupControl.enablePopups(editor);
+          popupsWereDisabled = false
+        }
       }
     }
-  }
 
-  override def projectOpened(): Unit = {
-    editorFactory.getEventMulticaster.addEditorMouseMotionListener(mouseMovedListener, project)
   }
-
-  override def projectClosed(): Unit = {
-    editorFactory.getEventMulticaster.removeEditorMouseMotionListener(mouseMovedListener)
-  }
-}
-
-private object TypeMismatchTooltipsHandler {
-  private val TypeMismatchTooltipPrefix = "<html><body>Type mismatch"
 }
