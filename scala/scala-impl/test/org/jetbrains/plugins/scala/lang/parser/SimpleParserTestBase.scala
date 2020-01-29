077b51113bc881829a8730240e97662a4308eeac
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-01-29 18:40:34 +0300

    added SimpleParserTestBase
     #SCL-16161

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/SimpleParserTestBase.scala
@@ -0,0 +1,53 @@
+package org.jetbrains.plugins.scala
+package lang
+package parser
+
+import com.intellij.psi.PsiErrorElement
+import org.jetbrains.plugins.scala.base.SimpleTestCase
+import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+
+trait SimpleParserTestBase extends SimpleTestCase {
+  def err(err: String): String = {
+    s"[[Err($err)]]"
+  }
+  private val errRegex = raw"\[\[Err\((.+)\)\]\]".r
+
+  private def extractExpectedErrors(text: String): (String, Set[(Int, String)]) = {
+    var code = text
+    val errorsBuilder = Set.newBuilder[(Int, String)]
+    while(true) {
+      errRegex.findFirstMatchIn(code) match {
+        case Some(matc) =>
+          code = code.substring(0, matc.start) + code.substring(matc.end)
+          errorsBuilder += (matc.start -> matc.group(1))
+        case None =>
+          return code -> errorsBuilder.result()
+      }
+    }
+    ???
+  }
+
+  def checkParseErrors(text: String): ScalaFile = {
+    val (code, expectedErrors) = extractExpectedErrors(
+      s"""
+         |object O {
+         |  $text
+         |}
+         |""".stripMargin
+    )
+
+    val file = parseText(code)
+    val errors = file.depthFirst().toSeq.filterBy[PsiErrorElement]
+    if (expectedErrors.isEmpty) {
+      assert(errors.isEmpty, "Expected no errors but found: " + errors.map(_.getErrorDescription).mkString(", "))
+    } else {
+      val errorsWithPos = errors.map(psi => psi.getTextOffset -> psi.getErrorDescription).toSet
+      val notFoundErrors = (expectedErrors -- errorsWithPos).toSeq.sortBy(_._1).map(_._2)
+      val unexpectedErrors = (errorsWithPos -- expectedErrors).toSeq.sortBy(_._1).map(_._2)
+      assert(unexpectedErrors.isEmpty, "Found unexpected errors: " + unexpectedErrors.mkString(", "))
+      assert(notFoundErrors.isEmpty, "Expected errors but didn't find: " + notFoundErrors.mkString(", "))
+    }
+    file
+  }
+}
