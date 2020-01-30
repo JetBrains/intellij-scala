25f62ec346d93717261af5674dcad655355d8dbb
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2020-01-30 17:55:10 +0300

    Fixes for #SCL-CR-1880

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsPassFactory.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsPassFactory.scala
@@ -4,8 +4,8 @@ import com.intellij.codeHighlighting._
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
-import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.codeInsight.hints.ScalaHintsSettings
+import org.jetbrains.plugins.scala.extensions.{PsiFileExt, ViewProviderExt}
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 
 class ImplicitHintsPassFactory
@@ -21,9 +21,10 @@ class ImplicitHintsPassFactory
   override def createHighlightingPass(file: PsiFile, editor: Editor): TextEditorHighlightingPass = file match {
     case file: ScalaFile if !ImplicitHints.isUpToDate(editor, file) =>
       new ImplicitHintsPass(editor, file, new ScalaHintsSettings.CodeInsightSettingsAdapter)
-    case file: PsiFile if file.getViewProvider.hasLanguage(ScalaLanguage.INSTANCE) && !ImplicitHints.isUpToDate(editor, file) =>
-      new ImplicitHintsPass(editor, file.getViewProvider.getPsi(ScalaLanguage.INSTANCE).asInstanceOf[ScalaFile],
-        new ScalaHintsSettings.CodeInsightSettingsAdapter)
+    case file: PsiFile if file.getViewProvider.hasScalaPsi && !ImplicitHints.isUpToDate(editor, file) =>
+      file.findAnyScalaFile.map (
+        scalaFile => new ImplicitHintsPass(editor, scalaFile, new ScalaHintsSettings.CodeInsightSettingsAdapter)
+      ).orNull
     case _ =>
       null
   }
