d0d5e8db33d9fedaac0576bc82219568107c8b60
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-02-03 12:41:21 +0300

    Added main method to move succeeding parser test files to success folder.
     #SCL-16158

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/Scala3ImportedParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/Scala3ImportedParserTest.scala
@@ -4,8 +4,8 @@ package parser
 
 import com.intellij.lang.Language
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiErrorElement
 import com.intellij.psi.impl.DebugUtil.psiToString
+import com.intellij.psi.{PsiErrorElement, PsiFile}
 import org.jetbrains.plugins.scala.base.ScalaFileSetTestCase
 import org.jetbrains.plugins.scala.extensions._
 import org.junit.Assert._
@@ -18,20 +18,24 @@ import org.junit.runners.AllTests
 abstract class Scala3ImportedParserTestBase(dir: String) extends ScalaFileSetTestCase(dir) {
   override protected def getLanguage: Language = Scala3Language.INSTANCE
 
-  protected override def transform(testName: String, fileText: String, project: Project): String = {
+  protected def findErrorElements(fileText: String, project: Project): (Seq[PsiErrorElement], PsiFile) = {
     val lightFile = createLightFile(fileText, project)
-
     val errors = lightFile
       .elements
       .collect { case error: PsiErrorElement => error }
       .toSeq
 
+    errors -> lightFile
+  }
+
+  protected override def transform(testName: String, fileText: String, project: Project): String = {
+    val (errors, lightFile) = findErrorElements(fileText, project)
     val hasErrorElements = errors.nonEmpty
 
     if (hasErrorElements != shouldHaveErrorElements) {
       println(fileText)
       println("-------")
-      println(psiToString(lightFile, false).replace(": " + lightFile.getName, ""))
+      println(psiToString(lightFile, false).replace(": " + lightFile.name, ""))
     }
 
     val msg = s"Found following errors: " + errors.mkString(", ")
@@ -52,21 +56,25 @@ abstract class Scala3ImportedParserTestBase(dir: String) extends ScalaFileSetTes
 }
 
 
-class Scala3ImportedParserTest extends Scala3ImportedParserTestBase("/parser/scala3Import/success") {
+class Scala3ImportedParserTest extends Scala3ImportedParserTestBase(Scala3ImportedParserTest.directory) {
   override protected def shouldHaveErrorElements: Boolean = false
 }
 
 object Scala3ImportedParserTest {
+  val directory = "/parser/scala3Import/success"
   def suite = new Scala3ImportedParserTest()
 }
 
 
-
-
-class Scala3ImportedParserTest_Fail extends Scala3ImportedParserTestBase("/parser/scala3Import/fail") {
+/**
+ * If this tests fails because you fixed parser stuff,
+ * run Scala3ImportedParserTest_Move_Fixed_Tests.
+ */
+class Scala3ImportedParserTest_Fail extends Scala3ImportedParserTestBase(Scala3ImportedParserTest_Fail.directory) {
   override protected def shouldHaveErrorElements: Boolean = true
 }
 
 object Scala3ImportedParserTest_Fail {
+  val directory = "/parser/scala3Import/fail"
   def suite = new Scala3ImportedParserTest_Fail()
 }
