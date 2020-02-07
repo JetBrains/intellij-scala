4da5091cdbb1eabbcb5d5d212cec158312fc0091
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-02-07 16:22:17 +0300

    downgrade to bloop 1.4.0-RC1 because newer 1.4.0-RC1+<counter>-<hash> versions aren't resolved correctly by ivy

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -7,7 +7,7 @@ object Versions {
   // NOTE: sbt-launch / bloop-launcher won't be fetched on refresh.
   // run runtimeDependencies/update manually
   val sbtVersion: String = Sbt.latest
-  val bloopVersion = "1.4.0-RC1+33-dfd03f53"
+  val bloopVersion = "1.4.0-RC1"
   val zincVersion = "1.1.1"
   val intellijVersion = "201.5259.8"
   val bspVersion = "2.0.0-M4"
