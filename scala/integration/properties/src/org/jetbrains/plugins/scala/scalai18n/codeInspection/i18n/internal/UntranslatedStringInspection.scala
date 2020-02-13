21233f8f061653bac681adaef6e5d2d7b22b6fad
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-02-13 17:51:25 +0100

    Implemented quickfix and improved UntranslatedStringInspection
     #SCL-17005

--- scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/internal/UntranslatedStringInspection.scala
+++ scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/internal/UntranslatedStringInspection.scala
@@ -1,14 +1,28 @@
-package org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.internal
+package org.jetbrains.plugins.scala
+package scalai18n
+package codeInspection
+package i18n
+package internal
 
 import com.intellij.codeInspection.{InspectionManager, LocalQuickFix, ProblemDescriptor, ProblemHighlightType}
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiElement
-import org.jetbrains.plugins.scala.codeInspection.AbstractRegisteredInspection
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.io.URLUtil
+import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, AbstractRegisteredInspection}
 import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScAnnotationsHolder
-import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScStringLiteral
-import org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.ScalaI18nUtil
+import org.jetbrains.plugins.scala.format._
+import org.jetbrains.plugins.scala.lang.psi.ScImportsHolder
+import org.jetbrains.plugins.scala.lang.psi.api.base.{ScAnnotationsHolder, ScLiteral}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScPackaging
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 import org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.internal.UntranslatedStringInspection._
+import org.jetbrains.plugins.scala.util.internal.I18nStringBundle
+import org.jetbrains.plugins.scala.util.internal.I18nStringBundle.{BundleInfo, BundleUsageInfo, Entry}
 
 import scala.util.matching.Regex
 
@@ -20,13 +34,23 @@ class UntranslatedStringInspection extends AbstractRegisteredInspection {
                                            highlightType: ProblemHighlightType = ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
                                           (implicit manager: InspectionManager, isOnTheFly: Boolean): Option[ProblemDescriptor] = {
     Option(element) collect {
-      case element@ScStringLiteral(string) if isNaturalLangString(string) && !shouldBeIgnore(element) =>
-        manager.createProblemDescriptor(element, descriptionTemplate, isOnTheFly, Array.empty[LocalQuickFix], highlightType)
+      case element@TopmostStringParts(parts) if containsNaturalLangString(parts) && !shouldBeIgnored(element, parts) =>
+        val quickFixes = Array[LocalQuickFix](new MoveToBundleQuickFix(element))
+        manager.createProblemDescriptor(element, descriptionTemplate, isOnTheFly, quickFixes, highlightType)
     }
   }
 }
 
 object UntranslatedStringInspection {
+  val x = "blub"
+  val test = s"test${x}x$x"
+
+  private def containsNaturalLangString(parts: Seq[StringPart]): Boolean =
+    parts.exists {
+      case Text(s) => isNaturalLangString(s)
+      case _ => false
+    }
+
   private def isNaturalLangString(string: String): Boolean =
     //string.length > 3 &&
     !hasCamelCase(string)
@@ -35,8 +59,7 @@ object UntranslatedStringInspection {
   def hasCamelCase(string: String): Boolean =
     camelCaseRegex.findFirstIn(string).isDefined
 
-
-  private def shouldBeIgnore(element: ScStringLiteral): Boolean =
+  private def shouldBeIgnored(element: PsiElement, parts: Seq[StringPart]): Boolean =
     hasNonNLSAnnotation(element) ||
       isTestSource(element) ||
       isInBundleMessageCall(element)
@@ -52,6 +75,114 @@ object UntranslatedStringInspection {
     element.getContainingFile.toOption.exists(file => ProjectFileIndex.getInstance(element.getProject).isInTestSourceContent(file.getVirtualFile))
   }
 
-  private def isInBundleMessageCall(element: ScStringLiteral): Boolean =
-    ScalaI18nUtil.mustBePropertyKey(element)
+  private def isInBundleMessageCall(element: PsiElement): Boolean =
+    element.asOptionOf[ScLiteral].exists(ScalaI18nUtil.mustBePropertyKey(_))
+
+  private object TopmostStringParts {
+    def unapply(expr: ScExpression): Option[Seq[StringPart]] = {
+      def parentIsStringConcatenationOrFormat = expr.parent.exists {
+        case StringConcatenationExpression(_, _) => true
+        case p => FormattedStringParser.parse(p).isDefined
+      }
+      AnyStringParser.parse(expr).filterNot(_ => parentIsStringConcatenationOrFormat)
+    }
+  }
+
+  private class MoveToBundleQuickFix(_element: ScExpression) extends AbstractFixOnPsiElement("Quickfix on ", _element) {
+    override protected def doApplyFix(element: ScExpression)(implicit project: Project): Unit = {
+      val parts = element match {
+        case TopmostStringParts(parts) => parts
+        case _ => return
+      }
+
+      val showErrorDialog = Messages.showErrorDialog(project, _: String, _: String)
+      val BundleUsageInfo(elementPath, srcRoot, resourceRoot, maybeBundlePath) =
+        I18nStringBundle
+          .findBundlePathFor(element)
+          .getOrElse {
+            val elementPath = element.containingVirtualFile.fold("<memory-only file>")(_.getCanonicalPath)
+            showErrorDialog(s"Couldn't determine module root for element in $elementPath", "No module root found")
+            return
+          }
+
+      assert(elementPath.startsWith(srcRoot))
+
+      val BundleInfo(bundlePropertyPath, _, bundleClassName, bundleQualifiedClassName) =
+        maybeBundlePath
+          .getOrElse {
+            showErrorDialog("Couldn't find bundle in " + srcRoot, "No bundle found")
+            return
+          }
+
+      val bundle = I18nStringBundle.readBundle(bundlePropertyPath)
+      val path = elementPath.substring(srcRoot.length)
+      val (keyBase, text, arguments) = toKeyAndTextAndArgs(parts)
+      val keyCandidates = keyBase +: Stream.from(1).map(keyBase + _)
+      val key = keyCandidates.filter(!bundle.hasKey(_)).head
+      val newEntry = Entry(key, text, path)
+
+      // mark bundle file for undo
+      val fileUrl = VirtualFileManager.constructUrl(URLUtil.FILE_PROTOCOL, bundlePropertyPath)
+      val vfile  = VirtualFileManager.getInstance().findFileByUrl(fileUrl)
+      if (vfile == null) {
+        throw new Exception(s"Failed to open bundle file at $bundlePropertyPath")
+      }
+
+      val outputStream = vfile.getOutputStream(this)
+      bundle
+        .withEntry(newEntry)
+        .writeTo(outputStream)
+
+
+      // add import
+      val importsHolder: ScImportsHolder =
+        Option(PsiTreeUtil.getParentOfType(element, classOf[ScPackaging]))
+          .getOrElse(element.getContainingFile.asInstanceOf[ScImportsHolder])
+      importsHolder.addImportForPath(bundleQualifiedClassName)
+
+      val argString =
+        if (arguments.isEmpty) ""
+        else arguments.mkString(", ", ", ", "")
+      // replace string with message call
+      _element.replace(ScalaPsiElementFactory.createExpressionFromText(
+        s"""$bundleClassName.message("$key"$argString)"""
+      ))
+    }
+
+
+
+    private val nonWordSeq = raw"\W+".r
+    private val Trimmed = raw"\W*(.*?)\W*".r
+    private def convertStringToKey(string: String): String = {
+      val maxKeyLength = 60
+      val Trimmed(fullKey) = nonWordSeq.replaceAllIn(string, ".")
+
+      lazy val lastDotIdx = fullKey.lastIndexOf(".", maxKeyLength - 3)
+      if (fullKey.length < maxKeyLength) fullKey
+      else if (lastDotIdx < maxKeyLength - 20) fullKey.substring(0, maxKeyLength - 3) + "..."
+      else fullKey.substring(0, lastDotIdx) + "..."
+    }
+
+    private val lastwordRegex = raw"\w+$$".r
+    private def lastWord(string: String): Option[String] =
+      lastwordRegex.findFirstIn(string)
+
+
+    def toKeyAndTextAndArgs(parts: Seq[StringPart]): (String, String, Seq[String]) = {
+      val argNums = Iterator.from(0)
+      val bindings = parts.collect {
+        case Text(s) => (s, s, None)
+        case injection: Injection =>
+          val value = injection.value
+          def keyAlternative = value.substring(0, value.length max 20)
+          val argNum = argNums.next()
+          (lastWord(value).getOrElse(keyAlternative), s"{$argNum}", Some(value))
+      }
+      val (keyParts, textParts, arguments) = bindings.unzip3
+      val key = convertStringToKey(keyParts.mkString("."))
+      val text = textParts.mkString.replaceAll("\n", "\\\n")
+
+      (key, text, arguments.flatten)
+    }
+  }
 }
--- scala/scala-impl/src/org/jetbrains/plugins/scala/util/internal/I18nStringBundle.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/internal/I18nStringBundle.scala
@@ -3,13 +3,17 @@ package util
 package internal
 
 import java.io._
+import java.nio.file.{Path, Paths}
 
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.util.internal.I18nStringBundle._
 
 import scala.io.Source
-import extensions._
 
 case class I18nStringBundle(entries: Seq[Entry]) {
+  def hasKey(key: String): Boolean = entries.exists(_.key == key)
+
   def sorted: I18nStringBundle =
     copy(entries = entries.sorted)
 
@@ -21,25 +25,27 @@ case class I18nStringBundle(entries: Seq[Entry]) {
   def isCorrectlySorted: Boolean =
     this == sorted
 
-  def writeTo(bundlePath: String): Unit = {
-    val pw = new PrintWriter(new File(bundlePath))
+  def writeTo(path: String): Unit =
+    writeTo(new PrintWriter(new File(path)))
 
-    try {
-      import pw._
-      var path: String = null
-      for (entry <- entries) {
-        if (path != entry.path) {
-          if (path != null) {
-            println()
-          }
-          path = entry.path
-          println("### " + path)
+  def writeTo(outputStream: OutputStream): Unit =
+    writeTo(new PrintWriter(outputStream))
+
+  private def writeTo(printWriter: PrintWriter): Unit = try {
+    import printWriter._
+    var path: String = null
+    for (entry <- entries) {
+      if (path != entry.path) {
+        if (path != null) {
+          println()
         }
-        print(entry.comments)
-        println(entry.key + "=" + entry.text)
+        path = entry.path
+        println("### " + path)
       }
-    } finally pw.close()
-  }
+      print(entry.comments)
+      println(entry.key + "=" + entry.text)
+    }
+  } finally printWriter.close()
 }
 
 object I18nStringBundle {
@@ -49,7 +55,7 @@ object I18nStringBundle {
   implicit val entryOrdering: Ordering[Entry] =
     Ordering.by((entry: Entry) => entry.isUnused -> entry.path)
 
-  case class Entry(key: String, text: String, path: String, comments: String) {
+  case class Entry(key: String, text: String, path: String, comments: String = "") {
     def isUnused: Boolean = path == unusedCategoryPath
   }
 
@@ -99,5 +105,50 @@ object I18nStringBundle {
         (line.contains('.') && line.contains('/') || line.contains(unusedCategoryPath))
       isNewCategory.option(line.substring(prefix.length))
     }
+  }
+
+  case class BundleInfo(bundleFilePath: String, bundleClassPath: String, bundleClassName: String, bundleQualifiedClassName: String)
+  case class BundleUsageInfo(originalPath: String, moduleSrcRoot: String, moduleResourceRoot: String, bundleInfo: Option[BundleInfo])
+
+  def findBundlePathFor(path: String): Option[BundleUsageInfo] = for {
+    moduleRoot <- {
+        val idx = path.indexOf("/src/")
+        if (idx < 0) None
+        else Some(path.substring(0, idx + 1 /* include the '/' */))
+      }
+    srcRoot = moduleRoot + "src/"
+  } yield {
+    def withoutExtension(path: String): String = path.substring(0, path.lastIndexOf('.'))
+    val resourceRoot = moduleRoot + "resources/"
+    val bundlePath = for {
+      bundleClassPath <- findBundleClass(path)
+      relPath = bundleClassPath.toString.substring(srcRoot.length)
+      className = withoutExtension(bundleClassPath.getFileName.toString)
+      relPathWithoutExtension = withoutExtension(relPath)
+      qualifiedClassName = relPathWithoutExtension.replaceAll(raw"[/\\]", ".")
+      withPropertyEnding = relPathWithoutExtension + ".properties"
+      bundlePath = resourceRoot + withPropertyEnding
+    } yield BundleInfo(bundlePath, bundleClassPath.toString, className, qualifiedClassName)
+    BundleUsageInfo(path, srcRoot, resourceRoot, bundlePath)
+  }
+
+  def findBundlePathFor(element: PsiElement): Option[BundleUsageInfo] =
+    element.containingVirtualFile.map(_.getPath).flatMap(findBundlePathFor)
+
+  private val bundleClassRegex =
+    raw".*Bundle\.(java|scala)".r
+  private def findBundleClass(_path: String): Option[Path] = {
+    Paths.get(_path)
+      .parents
+      .flatMap { path =>
+        new File(path.toUri)
+          .list()
+          .map(Paths.get(path.toString, _))
+          .find { path =>
+            val fileName = path.getFileName.toString
+            bundleClassRegex.findFirstIn(fileName).isDefined
+          }
+      }
+      .headOption
   }
 }
