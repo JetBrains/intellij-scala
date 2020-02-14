c0c77a9a345d2b74d692e8fd30e94a301e04a93b
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-02-14 14:38:35 +0300

    do not build full UAST on EDT
    #SCL-17041 fixed

--- scala/uast/src/org/jetbrains/plugins/scala/lang/psi/uast/internals/LazyUElement.scala
+++ scala/uast/src/org/jetbrains/plugins/scala/lang/psi/uast/internals/LazyUElement.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.lang.psi.uast.internals
 
+import com.intellij.openapi.application.ApplicationManager
 import org.jetbrains.annotations.Nullable
 import org.jetbrains.uast.UElement
 
@@ -13,12 +14,31 @@ object LazyUElement {
   def fromThunk(thunk: () => UElement @Nullable): LazyUElement =
     new LazyUElementImpl(thunk)
 
-  def just(element: UElement @Nullable): LazyUElement =
-    new LazyUElementImpl(() => element)
-}
+  def just(element: UElement @Nullable): LazyUElement = new Just(element)
 
-private class LazyUElementImpl(thunk: () => UElement @Nullable)
+  private def mayForce: Boolean =
+    !ApplicationManager.getApplication.isDispatchThread
+
+  private class Just(element: UElement) extends LazyUElement {
+    override def force: UElement @Nullable = element
+  }
+
+  private class LazyUElementImpl(private var thunk: () => UElement @Nullable)
     extends LazyUElement {
 
-  override lazy val force: UElement = thunk()
+    @volatile
+    private  var isForced = false
+
+    private lazy val forced: UElement = {
+      val result = thunk()
+      thunk = null
+      isForced = true
+      result
+    }
+
+    override def force: UElement = {
+      if (mayForce || isForced) forced
+      else null
+    }
+  }
 }
