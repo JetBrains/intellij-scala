be18ab6c177dea06b6522c2743dbd9c865b1fe35
andrey.sugak <andrey.sugak@jetbrains.com>
2020-02-14 16:33:54 +0300

    Add tests for type lambda parsing

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/TypeLambdaParserTest.scala
@@ -0,0 +1,30 @@
+package org.jetbrains.plugins.scala.lang.parser
+
+class TypeLambdaParserTest extends SimpleScala3ParserTestBase {
+  def testSimple(): Unit = checkParseErrors(
+    """
+      |trait Monad[F[_]]
+      |def tupleMonad[A]: Monad[[B] =>> (A, B)] = ???
+      |""".stripMargin
+  )
+
+  def testCurried(): Unit = checkParseErrors(
+    "type TL = [X] =>> [Y] =>> (X, Y)"
+  )
+
+  def testVariance(): Unit = checkParseErrors(
+    "type TL = [+X, -Y] =>> Map[Y, X]"
+  )
+
+  def testBounds(): Unit = checkParseErrors(
+    """
+      |trait T[F[_]]
+      |type TL = [F[_] <: Seq[_]] =>> T[F]
+      |""".stripMargin
+  )
+
+  def testAllTogether(): Unit = checkParseErrors(
+    "type TL = [-A, +F[A] >: Option[A] <: List[A]] =>> F[A] => A"
+  )
+}
+
