fc2209c067a299384779ecb82a353e8c4c6f36a2
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-02-17 17:54:59 +0100

    Use JDK8 for failing highlighting tests. #SCL-17057 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/BetterFilesProjectHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/BetterFilesProjectHighlightingTest.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.projectHighlighting
 
+import com.intellij.pom.java.LanguageLevel
 import org.jetbrains.plugins.scala.HighlightingTests
 import org.junit.experimental.categories.Category
 
@@ -9,6 +10,9 @@ import org.junit.experimental.categories.Category
   */
 @Category(Array(classOf[HighlightingTests]))
 class BetterFilesProjectHighlightingTest extends GithubSbtAllProjectHighlightingTest {
+
+  override def jdkLanguageLevel: LanguageLevel = LanguageLevel.JDK_1_8
+
   override def githubUsername = "pathikrit"
 
   override def githubRepoName = "better-files"
--- scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/ScalaFiddleEditorProjectHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/ScalaFiddleEditorProjectHighlightingTest.scala
@@ -1,11 +1,15 @@
 package org.jetbrains.plugins.scala.projectHighlighting
 
 import com.intellij.openapi.util.TextRange
+import com.intellij.pom.java.LanguageLevel
 import org.jetbrains.plugins.scala.HighlightingTests
 import org.junit.experimental.categories.Category
 
 @Category(Array(classOf[HighlightingTests]))
 class ScalaFiddleEditorProjectHighlightingTest extends GithubSbtAllProjectHighlightingTest {
+
+  override def jdkLanguageLevel: LanguageLevel = LanguageLevel.JDK_1_8
+
   override def githubUsername = "scalafiddle"
 
   override def githubRepoName = "scalafiddle-editor"
