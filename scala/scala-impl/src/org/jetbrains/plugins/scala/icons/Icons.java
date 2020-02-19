02a3215a9b562cca4cff4b1074f2bef644942037
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-02-19 16:18:09 +0100

    synchronize icons from icon repo and add sbt refresh icon to SbtIconProvider. #(UX-639, UX-461, UX-1093, SCL-15301, SCL-15303, SCL-16759, SCL-16857)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/icons/Icons.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/icons/Icons.java
@@ -44,6 +44,8 @@ public interface Icons {
     Icon CLASS_AND_OBJECT = getIcon("/org/jetbrains/plugins/scala/images/class_object_scala.svg");
     Icon ABSTRACT_CLASS_AND_OBJECT = getIcon("/org/jetbrains/plugins/scala/images/abstract_class_object_scala.svg");
     Icon TRAIT_AND_OBJECT = getIcon("/org/jetbrains/plugins/scala/images/trait_object_scala.svg");
+    Icon PACKAGE_WITH_OBJECT = getIcon("/org/jetbrains/plugins/scala/images/packageWithObject.svg");
+    Icon MULTIPLE_TYPE_DEFINITONS = getIcon("/org/jetbrains/plugins/scala/images/multipleTypeDefinitions.svg");
 
     //Internal nodes
     Icon FIELD_VAR = getIcon("/org/jetbrains/plugins/scala/images/field_variable.svg");
@@ -82,5 +84,6 @@ public interface Icons {
     Icon SBT_SHELL = getIcon("/org/jetbrains/plugins/scala/images/sbt_shell.svg");
     // used from SBT.xml
     Icon SBT_SHELL_TOOLWINDOW = getIcon("/org/jetbrains/plugins/scala/images/sbt_shell_toolwin.svg");
+    Icon SBT_LOAD_CHANGES = getIcon("/org/jetbrains/plugins/scala/images/sbtLoadChanges.svg");
 
 }
--- scala/scala-impl/src/org/jetbrains/sbt/SbtIconProvider.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/SbtIconProvider.scala
@@ -2,8 +2,9 @@ package org.jetbrains.sbt
 
 import com.intellij.openapi.externalSystem.ui.ExternalSystemIconProvider
 import javax.swing.Icon
+import org.jetbrains.plugins.scala.icons.Icons
 
 class SbtIconProvider extends ExternalSystemIconProvider {
   // TODO create sbt reload icon similar to icons.GradleIcons.GradleLoadChanges, SCL-16759
-  override def getReloadIcon: Icon = com.intellij.icons.AllIcons.Actions.Refresh
+  override def getReloadIcon: Icon = Icons.SBT_LOAD_CHANGES
 }
