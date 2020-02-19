39e9c167b72155f88d25d6dddfba0a9f57508283
andrey.sugak <andrey.sugak@jetbrains.com>
2020-02-19 03:23:12 +0300

    Add basic tests for function type parsing

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/FunctionLikeTypeParserTest.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.lang.parser
+
+class FunctionLikeTypeParserTest extends SimpleScala3ParserTestBase {
+  def testContextualFunction(): Unit =
+    checkParseErrors(
+      """
+        |type Executable[T] = ExecutionContext ?=> T
+        |def table(init: Table ?=> Unit) = ???
+        |""".stripMargin
+    )
+
+  def testPolyFunction(): Unit =
+    checkParseErrors(
+      """
+        |def foo(fn: [T <: AnyVal] => List[T] => List[(T, T)]): Unit
+        |type Eq1[F[_]] = [A] => Eq[A] ?=> Eq[F[A]]
+        |""".stripMargin
+    )
+
+  def testDependentFunctionType(): Unit =
+    checkParseErrors(
+      """
+        |val extractor: (e: Entry) => e.Key = extractKey
+        |def foo(fn: (b: Bar) => b.TypeMember): Unit
+        |""".stripMargin
+    )
+}
