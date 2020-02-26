2e6f1acd737da35cf590fa02f842e6fb2e6bc275
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-02-26 12:29:24 +0300

    rehighlight only expression with invalidated caches on psi change
    #SCL-17097 fixed

---
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/daemon/ScalaChangeLocalityDetector.scala
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.scala.codeInsight.daemon
+
+import com.intellij.codeInsight.daemon.ChangeLocalityDetector
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.caches.BlockModificationTracker
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+
+class ScalaChangeLocalityDetector extends ChangeLocalityDetector {
+
+  override def getChangeHighlightingDirtyScopeFor(changedElement: PsiElement): PsiElement = {
+    changedElement.getParent match {
+      case expr: ScExpression if BlockModificationTracker.hasStableType(expr) => expr
+      case _ => null
+    }
+  }
+}
