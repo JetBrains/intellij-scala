5cf231f39cac777360e89a8bc30bae5abcc587dc
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-03-02 10:10:23 +0100

    Direct listener installation
    (cherry picked from commit bfe0dcfa96562ab443c49074f7ece084136fb0ac)

--- bsp/src/org/jetbrains/bsp/project/test/environment/BspFetchEnvironmentTaskInstaller.scala
+++ bsp/src/org/jetbrains/bsp/project/test/environment/BspFetchEnvironmentTaskInstaller.scala
@@ -1,41 +1,20 @@
 package org.jetbrains.bsp.project.test.environment
 
 import com.intellij.execution.{RunManagerEx, RunManagerListener, RunnerAndConfigurationSettings}
-import com.intellij.openapi.components.ProjectComponent
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
 
-final class BspFetchEnvironmentTaskInstaller(project: Project) {
-  def init(): Unit = {
-    val runManager = RunManagerEx.getInstanceEx(project)
-    project.getMessageBus.connect().subscribe(RunManagerListener.TOPIC,
-      new RunManagerListener {
-        override def runConfigurationAdded(settings: RunnerAndConfigurationSettings): Unit = {
-          val runConfiguration = settings.getConfiguration
-          if (BspTesting.isBspRunnerSupportedConfiguration(runConfiguration)) {
-            val beforeRunTasks = runManager.getBeforeRunTasks(runConfiguration)
-            val task = new BspFetchTestEnvironmentTask
-            task.setEnabled(true)
-            beforeRunTasks.add(task)
-            val tasks = runManager.getBeforeRunTasks(BspFetchTestEnvironmentTask.runTaskKey)
-            if (tasks.isEmpty) {
-              runManager.setBeforeRunTasks(runConfiguration, beforeRunTasks)
-            }
-          }
-        }
+class BspFetchEnvironmentTaskInstaller extends RunManagerListener {
+  override def runConfigurationAdded(settings: RunnerAndConfigurationSettings): Unit = {
+    val runManager = RunManagerEx.getInstanceEx(settings.getConfiguration.getProject)
+    val runConfiguration = settings.getConfiguration
+    if (BspTesting.isBspRunnerSupportedConfiguration(runConfiguration)) {
+      val beforeRunTasks = runManager.getBeforeRunTasks(runConfiguration)
+      val task = new BspFetchTestEnvironmentTask
+      task.setEnabled(true)
+      beforeRunTasks.add(task)
+      val tasks = runManager.getBeforeRunTasks(BspFetchTestEnvironmentTask.runTaskKey)
+      if (tasks.isEmpty) {
+        runManager.setBeforeRunTasks(runConfiguration, beforeRunTasks)
       }
-    )
-  }
-}
-
-object BspFetchEnvironmentTaskInstaller {
-  def getInstance(project: Project): BspFetchEnvironmentTaskInstaller = {
-    project.getService(classOf[BspFetchEnvironmentTaskInstaller])
-  }
-
-  private final class Startup extends StartupActivity {
-    override def runActivity(project: Project): Unit = {
-      getInstance(project).init()
     }
   }
 }
