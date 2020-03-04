94f6776c50d7d009b7b9b8ca2e3d0873f7d3570e
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-03-04 18:53:27 +0300

    Compilation error highlightings: fixed bug in listeners #SCL-16983

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/RegisterApplicationListeners.scala
@@ -0,0 +1,32 @@
+package org.jetbrains.plugins.scala.externalHighlighters
+
+import com.intellij.ide.ApplicationInitializedListener
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.event.{EditorFactoryEvent, EditorFactoryListener}
+
+private class RegisterApplicationListeners
+  extends ApplicationInitializedListener {
+
+  import RegisterApplicationListeners._
+
+  override def componentsInitialized(): Unit = {
+    val application = ApplicationManager.getApplication
+    EditorFactory.getInstance.addEditorFactoryListener(new HighlightCreatedEditorListener, application)
+  }
+}
+
+object RegisterApplicationListeners {
+
+  private class HighlightCreatedEditorListener
+    extends EditorFactoryListener {
+
+    override def editorCreated(event: EditorFactoryEvent): Unit = {
+      val editor = event.getEditor
+      Option(editor.getProject).foreach { project =>
+        val state = HighlightingStateManager.get(project)
+        ExternalHighlighters.applyHighlighting(project, editor, state)
+      }
+    }
+  }
+}
