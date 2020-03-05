4395b0b056bee7e1bf51254e62e3ea7c3bc28d74
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-03-05 21:32:32 +0100

    add devkit localization bundle. #SCL-16792

---
+++ scala/integration/devKit/src/org/jetbrains/idea/devkit/build/DevkitBundle.java
@@ -0,0 +1,23 @@
+package org.jetbrains.idea.devkit.build;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+public class DevkitBundle extends DynamicBundle {
+    @NonNls
+    private static final String BUNDLE = "messages.ScalaDevkitBundle";
+
+    private static final DevkitBundle INSTANCE = new DevkitBundle();
+
+    private DevkitBundle() {
+        super(BUNDLE);
+    }
+
+    @Nls
+    public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+        return INSTANCE.getMessage(key, params);
+    }
+}
