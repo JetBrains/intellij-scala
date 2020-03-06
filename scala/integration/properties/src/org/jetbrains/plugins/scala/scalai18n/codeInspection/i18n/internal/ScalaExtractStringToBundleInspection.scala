181646ba45f024ac140852cd1f4e08d322cfdc87
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-03-06 16:19:44 +0100

    Extracted BundleExtraction code
     #SCL-17005

--- scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/internal/ScalaExtractStringToBundleInspection.scala
+++ scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/internal/ScalaExtractStringToBundleInspection.scala
@@ -9,27 +9,20 @@ import java.util
 import com.intellij.codeInsight.AnnotationUtil
 import com.intellij.codeInspection.{BatchQuickFix, InspectionManager, LocalQuickFix, ProblemDescriptor, ProblemHighlightType}
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
-import com.intellij.openapi.ui.Messages.InputDialog
-import com.intellij.openapi.ui.{InputValidatorEx, Messages}
-import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{PsiDocumentManager, PsiElement}
-import com.intellij.util.io.URLUtil
-import org.jetbrains.annotations.Nullable
 import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, AbstractRegisteredInspection}
 import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.format._
 import org.jetbrains.plugins.scala.lang.psi.ScImportsHolder
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScAnnotationsHolder, ScLiteral}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScPackaging
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.internal.BundleExtraction.{BundleExtractionInfo, executeBundleExtraction}
 import org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.internal.ScalaExtractStringToBundleInspection._
-import org.jetbrains.plugins.scala.util.internal.I18nBundleContent
-import org.jetbrains.plugins.scala.util.internal.I18nBundleContent.{BundleInfo, BundleUsageInfo, Entry}
 
 //noinspection ScalaExtractStringToBundle
 class ScalaExtractStringToBundleInspection extends AbstractRegisteredInspection {
@@ -44,7 +37,7 @@ class ScalaExtractStringToBundleInspection extends AbstractRegisteredInspection 
         val isNaturalLang = containsNaturalLangString(element, parts)
 
         if (isNaturalLang || (isOnTheFly && !ApplicationManager.getApplication.isUnitTestMode )) {
-          val quickFixes = Array[LocalQuickFix](new MoveToBundleQuickFix(element)) ++ maybeQuickFix
+          val quickFixes = Array[LocalQuickFix](new ScalaMoveToBundleQuickFix(element)) ++ maybeQuickFix
           val highlight =
             if (isNaturalLang) highlightType
             else ProblemHighlightType.INFORMATION // make quickfix available for all strings
@@ -57,9 +50,9 @@ class ScalaExtractStringToBundleInspection extends AbstractRegisteredInspection 
 
 //noinspection ScalaExtractStringToBundle
 object ScalaExtractStringToBundleInspection {
-  private def containsNaturalLangString(element: PsiElement, parts: Seq[StringPart]): Boolean =
+  private def containsNaturalLangString(element: PsiElement, parts: Seq[format.StringPart]): Boolean =
     parts.exists {
-      case Text(s) => isNaturalLangString(element, s)
+      case format.Text(s) => isNaturalLangString(element, s)
       case _ => false
     }
 
@@ -80,7 +73,7 @@ object ScalaExtractStringToBundleInspection {
   private def hasCamelCase(string: String): Boolean =
     camelCaseRegex.findFirstIn(string).isDefined
 
-  private def shouldBeIgnored(element: PsiElement, parts: Seq[StringPart]): Boolean =
+  private def shouldBeIgnored(element: PsiElement, parts: Seq[format.StringPart]): Boolean =
     hasNonNLSAnnotation(element) ||
       isTestSource(element) ||
       isInBundleMessageCall(element)
@@ -92,144 +85,55 @@ object ScalaExtractStringToBundleInspection {
       .exists(_.hasAnnotation(AnnotationUtil.NON_NLS))
 
   private def isTestSource(element: PsiElement): Boolean = {
-    //element.getContainingFile.toOption.exists(file => TestSourcesFilter.isTestSources(file.getVirtualFile, element.getProject))
-    element.getContainingFile.toOption.exists(file => ProjectFileIndex.getInstance(element.getProject).isInTestSourceContent(file.getVirtualFile))
+    val isInTestSourceContent = ProjectFileIndex.getInstance(element.getProject).isInTestSourceContent _
+    element
+      .getContainingFile.nullSafe
+      .map(_.getVirtualFile)
+      .exists(isInTestSourceContent)
   }
 
   private def isInBundleMessageCall(element: PsiElement): Boolean =
     element.asOptionOf[ScLiteral].exists(ScalaI18nUtil.mustBePropertyKey(_))
 
   private object TopmostStringParts {
-    def unapply(expr: ScExpression): Option[Seq[StringPart]] = {
+    def unapply(expr: ScExpression): Option[Seq[format.StringPart]] = {
       def parentIsStringConcatenationOrFormat = expr.parent.exists {
-        case StringConcatenationExpression(_, _) => true
-        case p => FormattedStringParser.parse(p).isDefined
+        case format.StringConcatenationExpression(_, _) => true
+        case p => format.FormattedStringParser.parse(p).isDefined
       }
-      AnyStringParser.parse(expr).filterNot(_ => parentIsStringConcatenationOrFormat)
+      format.AnyStringParser.parse(expr).filterNot(_ => parentIsStringConcatenationOrFormat)
     }
   }
 
-  private class MoveToBundleQuickFix(_element: ScExpression)
-      extends AbstractFixOnPsiElement("Extract to bundle", _element) with BatchQuickFix[Null] {
+  private class ScalaMoveToBundleQuickFix(_element: ScExpression)
+    extends AbstractFixOnPsiElement("Extract to bundle", _element) with BatchQuickFix[Null] {
     override def startInWriteAction(): Boolean = false
     override protected def doApplyFix(element: ScExpression)(implicit project: Project): Unit = {
       val parts = element match {
-        case TopmostStringParts(parts) => parts
+        case TopmostStringParts(parts) => parts.flatMap(ExtractPart.from)
         case _ => return
       }
 
-      val showErrorDialog = Messages.showErrorDialog(project, _: String, _: String)
-      val BundleUsageInfo(elementPath, srcRoot, _, maybeBundlePath) =
-        I18nBundleContent
-          .findBundlePathFor(element)
-          .getOrElse {
-            val elementPath = element.containingVirtualFile.fold("<memory-only file>")(_.getCanonicalPath)
-            showErrorDialog(s"Couldn't determine module root for element in $elementPath", "No module root found")
-            return
-          }
+      executeBundleExtraction(element, parts, project) {
+        case BundleExtractionInfo(bundleClassName, bundleQualifiedClassName, key, arguments ) =>
+          // add import
+          val importsHolder: ScImportsHolder =
+            Option(PsiTreeUtil.getParentOfType(element, classOf[ScPackaging]))
+              .getOrElse(element.getContainingFile.asInstanceOf[ScImportsHolder])
+          importsHolder.addImportForPath(bundleQualifiedClassName)
 
-      assert(elementPath.startsWith(srcRoot))
-
-      val BundleInfo(bundlePropertyPath, _, bundleClassName, bundleQualifiedClassName) =
-        maybeBundlePath
-          .getOrElse {
-            showErrorDialog("Couldn't find bundle for " + elementPath, "No bundle found")
-            return
-          }
-
-      val fileUrl = VirtualFileManager.constructUrl(URLUtil.FILE_PROTOCOL, bundlePropertyPath)
-      val vfile = VirtualFileManager.getInstance().findFileByUrl(fileUrl)
-
-      if (vfile == null) {
-        throw new Exception(s"Failed to open bundle file at $bundlePropertyPath")
+          // replace string with message call
+          val argString =
+            if (arguments.isEmpty) ""
+            else arguments.mkString(", ", ", ", "")
+          element.replace(ScalaPsiElementFactory.createExpressionFromText(
+            s"""$bundleClassName.message("$key"$argString)"""
+          ))
       }
-
-      // save file that may already been loaded
-      val fileDocumentManager = FileDocumentManager.getInstance()
-      fileDocumentManager
-        .getCachedDocument(vfile).nullSafe
-        .foreach(fileDocumentManager.saveDocument)
-
-
-      val bundle = I18nBundleContent.read(bundlePropertyPath)
-      val path = elementPath.substring(srcRoot.length)
-      val (keyProposal, text, arguments) = toKeyAndTextAndArgs(parts)
-
-      val inputDialog = new InputDialog(project, "Key:", "Enter key for new bundle entry", null, keyProposal, new BundleNameInputValidator(bundle))
-      if (!inputDialog.showAndGet()) {
-        return
-      }
-
-      val key = inputDialog.getInputString
-      assert(key != null)
-
-      val newEntry = Entry(key, text, path)
-
-      inWriteAction {
-        val outputStream = vfile.getOutputStream(this)
-        try bundle
-          .withEntry(newEntry)
-          .writeTo(outputStream)
-        finally outputStream.close()
-        val document = FileDocumentManager.getInstance().getDocument(vfile)
-        PsiDocumentManager.getInstance(project).commitDocument(document)
-        // TODO: make undo working
-        //CommandProcessor.getInstance().addAffectedDocuments(project, document);
-
-        // add import
-        val importsHolder: ScImportsHolder =
-          Option(PsiTreeUtil.getParentOfType(element, classOf[ScPackaging]))
-            .getOrElse(element.getContainingFile.asInstanceOf[ScImportsHolder])
-        importsHolder.addImportForPath(bundleQualifiedClassName)
-
-        // replace string with message call
-        val argString =
-          if (arguments.isEmpty) ""
-          else arguments.mkString(", ", ", ", "")
-        _element.replace(ScalaPsiElementFactory.createExpressionFromText(
-          s"""$bundleClassName.message("$key"$argString)"""
-        ))
-      }
-    }
-
-    private val lastwordRegex = raw"\w+".r
-    private def lastWord(string: String): Option[String] =
-      lastwordRegex.findAllIn(string).toSeq.lastOption
-
-
-    def toKeyAndTextAndArgs(parts: Seq[StringPart]): (String, String, Seq[String]) = {
-      val argNums = Iterator.from(0)
-      val bindings = parts.collect {
-        case Text(s) => (s, s, None)
-        case injection: Injection =>
-          val value = injection.value
-          def keyAlternative = value.substring(0, value.length min 20)
-          val argNum = argNums.next()
-          (lastWord(value).getOrElse(keyAlternative), s"{$argNum}", Some(value))
-      }
-      val (keyParts, textParts, arguments) = bindings.unzip3
-      val key = I18nBundleContent.convertStringToKey(keyParts.mkString("."))
-      val text = I18nBundleContent.escapeText(textParts.mkString, arguments.nonEmpty)
-      (key, text, arguments.flatten)
     }
 
     // find a way to disable batch mode... until then just show this message
     override def applyFix(project: Project, descriptors: Array[Null], psiElementsToIgnore: util.List[PsiElement], refreshViews: Runnable): Unit =
       Messages.showErrorDialog(project, "Don' run ExtractStringToBundle inspection in batch mode", "Do not run in batch mode")
-  }
-
-  class BundleNameInputValidator(bundle: I18nBundleContent) extends InputValidatorEx {
-    private val wrongRegex = raw"[^\w\.]+".r
-    @Nullable
-    override def getErrorText(inputString: String): String = inputString match {
-      case "" => "Cannot be empty"
-      case key if key.contains(" ") || key.contains("\t") => "Key cannot contain spaces"
-      case key if wrongRegex.findAllMatchIn(key).nonEmpty => "Invalid characters: " + wrongRegex.findAllMatchIn(key).flatMap(_.toString()).toSet.mkString.sorted
-      case key if bundle.hasKey(key) => "Key already in bundle: " + key
-      case _ => null
-    }
-
-    override def checkInput(inputString: String): Boolean = getErrorText(inputString) == null
-    override def canClose(inputString: String): Boolean = checkInput(inputString)
   }
 }
