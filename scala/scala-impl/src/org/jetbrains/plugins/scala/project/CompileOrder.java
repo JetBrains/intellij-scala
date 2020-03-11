4bcd463732686286ccef4226338b962c356a58a2
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-03-11 15:36:42 +0300

    i18n: fix descriptions for CompileOrder and DebuggingInfoLevel
    #SCL-16921

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/CompileOrder.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/CompileOrder.java
@@ -4,17 +4,11 @@ package org.jetbrains.plugins.scala.project;
  * @author Pavel Fatin
  */
 public enum CompileOrder implements Named {
-  Mixed("Mixed"),
-  JavaThenScala("Java then Scala"),
-  ScalaThenJava("Scala then Java");
-
-  private String myName;
-
-  CompileOrder(String name) {
-    myName = name;
-  }
+  Mixed,
+  JavaThenScala,
+  ScalaThenJava;
 
   public String getName() {
-    return myName;
+    return CompileOrderDescriptions.get(this);
   }
 }
