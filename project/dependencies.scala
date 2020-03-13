30d8727e58de70c3a3ee1c560c4962c67c6b0d60
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-03-13 19:05:28 +0300

    update IDEA version to the latest EAP (201.6251.22)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -9,7 +9,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.4.0-RC1-74-e0147604"
   val zincVersion = "1.1.1"
-  val intellijVersion = "201.6073.9"
+  val intellijVersion = "201.6251.22"
   val bspVersion = "2.0.0-M7"
   val sbtStructureVersion: String = "2018.2.1+4-88400d3f"
   val sbtIdeaShellVersion: String = "2018.3"
