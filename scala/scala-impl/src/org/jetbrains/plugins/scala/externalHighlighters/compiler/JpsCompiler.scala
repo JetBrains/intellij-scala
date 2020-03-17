580f26f2f453a5b76727402a26cce0710f4dc10a
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-03-17 21:30:23 +0300

    Use CompilerTask in the JpsCompilerImpl #SCL-16983

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/compiler/JpsCompiler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/compiler/JpsCompiler.scala
@@ -2,13 +2,19 @@ package org.jetbrains.plugins.scala.externalHighlighters.compiler
 
 import java.io.File
 
+import com.intellij.compiler.progress.CompilerTask
 import com.intellij.compiler.server.BuildManager
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.Project
 import com.intellij.util.io.PathKt
 import org.jetbrains.jps.incremental.Utils
 import org.jetbrains.jps.incremental.scala.remote.{CommandIds, CompileServerCommand}
+import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.compiler.RemoteServerRunner
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Promise}
+import scala.util.Try
 
 trait JpsCompiler {
 
@@ -33,7 +39,17 @@ class JpsCompilerImpl
       dataStorageRootPath = dataStorageRootPath
     ).asArgsWithoutToken
     val client = new CompilationClient(project)
-    new RemoteServerRunner(project).buildProcess(command, args, client).runSync()
+    val promise = Promise[Unit]
+
+    val compileWork: Runnable = { () =>
+      val result = Try(new RemoteServerRunner(project).buildProcess(command, args, client).runSync())
+      promise.complete(result)
+    }
+    val restartWork: Runnable = () => ()
+    val task = new CompilerTask(project, ScalaBundle.message("highlighting.compilation"),
+      false, false, true, true)
+    task.start(compileWork, restartWork)
+    Await.result(promise.future, Duration.Inf)
   }
 }
 
