174ed62ad944700aec93e132892de8e9ee853b8d
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-03-25 16:16:06 +0100

    Switch from Component to StartupActivity
    Components are deprecated in 2020
    
    (cherry picked from commit eaa444be21cbc801fe68a17a09053c3bed85751a)

--- bsp/src/org/jetbrains/bsp/project/test/environment/BspFetchEnvironmentTaskInstaller.scala
+++ bsp/src/org/jetbrains/bsp/project/test/environment/BspFetchEnvironmentTaskInstaller.scala
@@ -3,9 +3,10 @@ package org.jetbrains.bsp.project.test.environment
 import com.intellij.execution.{RunManagerEx, RunManagerListener, RunnerAndConfigurationSettings}
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
 
-class BspFetchEnvironmentTaskInstaller(project: Project) extends ProjectComponent {
-  override def projectOpened(): Unit = {
+final class BspFetchEnvironmentTaskInstaller(project: Project) {
+  def init(): Unit = {
     val runManager = RunManagerEx.getInstanceEx(project)
     project.getMessageBus.connect().subscribe(RunManagerListener.TOPIC,
       new RunManagerListener {
@@ -24,5 +25,17 @@ class BspFetchEnvironmentTaskInstaller(project: Project) extends ProjectComponen
         }
       }
     )
+  }
+}
+
+object BspFetchEnvironmentTaskInstaller {
+  def getInstance(project: Project): BspFetchEnvironmentTaskInstaller = {
+    project.getService(classOf[BspFetchEnvironmentTaskInstaller])
+  }
+
+  private final class Startup extends StartupActivity {
+    override def runActivity(project: Project): Unit = {
+      getInstance(project).init()
+    }
   }
 }
