e0cd52cf7c508973919cecc9bbc8da75d02af5f7
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-03-25 16:20:53 +0100

    Basic support for JUnit/BSP test runs
    No detection of BSP targets yet. When running JUnit test
    configuration, IntelliJ always asks for the BSP target.
    
    (cherry picked from commit 69c6ec82ca93f8069a6bb8a235dfeae37a103fb4)

--- bsp/src/org/jetbrains/bsp/project/test/environment/BspTesting.scala
+++ bsp/src/org/jetbrains/bsp/project/test/environment/BspTesting.scala
@@ -1,12 +1,14 @@
 package org.jetbrains.bsp.project.test.environment
 
 import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.junit.JUnitConfiguration
 import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestRunConfiguration
 
 object BspTesting {
   def isBspRunnerSupportedConfiguration(config: RunConfiguration): Boolean =
     config match {
       case _: ScalaTestRunConfiguration => true
+      case _: JUnitConfiguration => true
       case _ => false
     }
 }
