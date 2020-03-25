7bd91f393986ae47b0b87b9d0e2a3f1bf2d1c4d2
Justin Kaeser <justin.kaeser@jetbrains.com>
2020-03-25 16:22:50 +0100

    Don't try to install BspFetchTestEnvironmentTask at startup
    The Scala plugin tries to install BspFetchTestEnvironmentTask on
    `runConfigurationAdded` event. Unfortunately, it requires
    RunConfigurationEx service to be installed, and the event happens
    during the initialization of it.
    
    Apart from that, the `beforeRunTasks.add` was previously called
    uncoditionally. This caused multiple additions of the task.
    
    (cherry picked from commit a7aa3d54aacd183290267373c96dbbb06f37f967)

--- bsp/src/org/jetbrains/bsp/project/test/environment/BspFetchEnvironmentTaskInstaller.scala
+++ bsp/src/org/jetbrains/bsp/project/test/environment/BspFetchEnvironmentTaskInstaller.scala
@@ -1,21 +1,40 @@
 package org.jetbrains.bsp.project.test.environment
 
-import com.intellij.execution.{RunManagerEx, RunManagerListener, RunnerAndConfigurationSettings}
+import com.intellij.execution.{RunManager, RunManagerEx, RunManagerListener, RunnerAndConfigurationSettings}
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
 
+
 class BspFetchEnvironmentTaskInstaller(project: Project) extends RunManagerListener {
+  private var settingsToInit: List[RunnerAndConfigurationSettings] = Nil
+
   override def runConfigurationAdded(settings: RunnerAndConfigurationSettings): Unit = {
-    val runManager = RunManagerEx.getInstanceEx(project)
+    runManagerEx.foreach(installFetchEnvironmentTask(settings, _))
+  }
+
+  private def installFetchEnvironmentTask(settings: RunnerAndConfigurationSettings, runManager: RunManagerEx): Unit = {
     val runConfiguration = settings.getConfiguration
     if (BspTesting.isBspRunnerSupportedConfiguration(runConfiguration)) {
-      val beforeRunTasks = runManager.getBeforeRunTasks(runConfiguration)
-      val task = new BspFetchTestEnvironmentTask
-      task.setEnabled(true)
-      beforeRunTasks.add(task)
       val tasks = runManager.getBeforeRunTasks(BspFetchTestEnvironmentTask.runTaskKey)
       if (tasks.isEmpty) {
-        runManager.setBeforeRunTasks(runConfiguration, beforeRunTasks)
+        val beforeRunTasks = runManager.getBeforeRunTasks(runConfiguration)
+        val task = new BspFetchTestEnvironmentTask
+        task.setEnabled(true)
+        beforeRunTasks.add(task)
       }
     }
   }
+
+  override def stateLoaded(runManager: RunManager, isFirstLoadState: Boolean): Unit = {
+    if (isFirstLoadState) {
+      runManagerEx.foreach {
+        runManager => settingsToInit.foreach(installFetchEnvironmentTask(_, runManager))
+      }
+      settingsToInit = Nil
+    }
+  }
+  
+  private def runManagerEx: Option[RunManagerEx] =
+    Option(ServiceManager.getServiceIfCreated(project, classOf[RunManager]))
+      .map(_.asInstanceOf[RunManagerEx])
 }
