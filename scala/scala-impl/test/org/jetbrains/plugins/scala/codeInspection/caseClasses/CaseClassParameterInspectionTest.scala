19b9195de124fff1cc96a59f9a126735da3b57e8
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-03-31 15:02:37 +0200

    SCL-CR-2012: fixed string type test in ScalaRedundantConversionInspection
     #SCL-17290 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/caseClasses/CaseClassParameterInspectionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/caseClasses/CaseClassParameterInspectionTest.scala
@@ -1,18 +1,14 @@
-package org.jetbrains.plugins.scala.codeInspection.caseClasses
+package org.jetbrains.plugins.scala
+package codeInspection
+package caseClasses
 
 import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.testFramework.EditorTestUtil
-import org.jetbrains.plugins.scala.ScalaBundle
-import org.jetbrains.plugins.scala.codeInspection.ScalaQuickFixTestBase
 import org.jetbrains.plugins.scala.codeInspection.caseClassParamInspection.CaseClassParamInspection
 
 /**
   * @author Nikolay.Tropin
   */
 class CaseClassParameterInspectionTest extends ScalaQuickFixTestBase {
-
-  import EditorTestUtil.{SELECTION_END_TAG => END, SELECTION_START_TAG => START}
-
   override protected val classOfInspection: Class[_ <: LocalInspectionTool] = classOf[CaseClassParamInspection]
 
   override protected val description: String = ScalaBundle.message("val.on.case.class.param.redundant")
