16119ed9e1757c0b5023137bc1f4bfaf11bd205a
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-04-03 21:54:07 +0300

    SCL-CR-2037: worksheet: remove awaitWithoutUiStarving and override runInDispatchThread instead #SCL-17241
    (cherry picked from commit 59f2cb85f3aabe101f629b24ade070666d871ac6)

--- scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/WorksheetItEvaluations.scala
+++ scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/WorksheetItEvaluations.scala
@@ -2,14 +2,16 @@ package org.jetbrains.plugins.scala.worksheet.integration
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.testFramework.PlatformTestUtil
-import org.jetbrains.plugins.scala.util.TestUtilsScala
+import com.intellij.util.ui.UIUtil
+import javax.swing.SwingUtilities
 import org.jetbrains.plugins.scala.worksheet.actions.topmenu.RunWorksheetAction
 import org.jetbrains.plugins.scala.worksheet.integration.WorksheetIntegrationBaseTest.TestRunResult
 import org.junit.Assert.fail
 
 import scala.concurrent.Future
+import scala.concurrent.duration.{Duration, DurationInt}
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 trait WorksheetItEvaluations {
   self: WorksheetIntegrationBaseTest =>
@@ -28,12 +30,37 @@ trait WorksheetItEvaluations {
     RunWorksheetAction.runCompiler(project, worksheetEditor, auto = false)
 
   protected def waitForEvaluationEnd(future: Future[RunWorksheetAction.RunWorksheetActionResult]): RunWorksheetAction.RunWorksheetActionResult = {
-    val result = TestUtilsScala.awaitWithoutUiStarving(future, evaluationTimeout)
+    val result = awaitWithoutUiStarving(future, evaluationTimeout)
     PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
     result match {
       case Some(Success(result))    => result
       case Some(Failure(exception)) => throw exception
       case None                     => fail("Timeout period was exceeded while waiting for worksheet evaluation").asInstanceOf[Nothing]
     }
+  }
+
+
+  /**
+   * TODO: delete and override com.intellij.testFramework.UsefulTestCase#runInDispatchThread() instead
+   * Unit tests are run in EDT, so we can't just use [[scala.concurrent.Await.result]] - it will block EDT and lead to
+   * all EDT events starving. So no code in "invokeLater" or "invokeLaterAndWait" etc... will be executed.
+   * We must periodically flush EDT events to workaround this.
+   */
+  def awaitWithoutUiStarving[T](
+    future: Future[T],
+    timeout: Duration,
+    sleepInterval: Duration = 100.milliseconds
+  ): Option[Try[T]] = {
+    var timeSpent: Duration = Duration.Zero
+
+    while (!future.isCompleted && (timeSpent < timeout)) {
+      if (SwingUtilities.isEventDispatchThread) {
+        UIUtil.dispatchAllInvocationEvents()
+      }
+      Thread.sleep(sleepInterval.toMillis)
+      timeSpent = timeSpent.plus(sleepInterval)
+    }
+
+    future.value
   }
 }
