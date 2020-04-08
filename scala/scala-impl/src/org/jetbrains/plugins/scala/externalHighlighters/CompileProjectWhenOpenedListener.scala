73d21c037127bbf1025481feaa54b5cf3448e70b
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-04-08 19:05:34 +0300

    Not start compilation automatically for unit-test-mode #SCL-17245
    (cherry picked from commit 91d491367dc5f7dd9196936cd5bfe3a1cd752e43)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CompileProjectWhenOpenedListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CompileProjectWhenOpenedListener.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.externalHighlighters
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.compiler.CompilerManager
 import com.intellij.openapi.project.{DumbService, Project, ProjectManagerListener}
 import org.jetbrains.plugins.scala.annotator.ScalaHighlightingMode
@@ -8,8 +9,9 @@ class CompileProjectWhenOpenedListener
   extends ProjectManagerListener {
   
   override def projectOpened(project: Project): Unit =
-    DumbService.getInstance(project).runWhenSmart { () =>
-      if (ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project))
-        CompilerManager.getInstance(project).make(null)
-    }
+    if (!ApplicationManager.getApplication.isUnitTestMode)
+      DumbService.getInstance(project).runWhenSmart { () =>
+        if (ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project))
+          CompilerManager.getInstance(project).make(null)
+      }
 }
