81312e7cb488b0390dcfdf5e71205a23e1c22fa1
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-04-13 14:02:19 +0300

    SCL-CR-2040: i18n: extract dotty template strings to bundle
    (cherry picked from commit f5ff9ff59d76b427230d5623db6ae9d87e7509ae)

--- scala/scala-impl/src/org/jetbrains/sbt/project/template/DottyProjectTemplatesFactory.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/project/template/DottyProjectTemplatesFactory.scala
@@ -3,16 +3,15 @@ package org.jetbrains.sbt.project.template
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.platform.{ProjectTemplate, ProjectTemplatesFactory}
 import org.jetbrains.plugins.scala.project.template.ScalaProjectTemplatesFactory
+import org.jetbrains.sbt.SbtBundle
 
 class DottyProjectTemplatesFactory extends ProjectTemplatesFactory {
   override def getGroups: Array[String] = Array(ScalaProjectTemplatesFactory.Group)
 
   override def createTemplates(group: String, context: WizardContext): Array[ProjectTemplate] = {
     val url = getClass.getClassLoader.getResource("projectTemplates/dottyTemplate.zip")
-    val templateName = "Dotty (experimental)"
-    val description =
-      """Minimal Dotty (Scala 3) project <br>
-        |Scala 3 support in IntelliJ IDEA is experimental as Scala 3 itself is not stable yet.""".stripMargin
+    val templateName = SbtBundle.message("project.template.name.dotty.experimental")
+    val description = SbtBundle.message("project.template.description.dotty").stripMargin
 
     //todo: try to download latest version of https://github.com/lampepfl/dotty.g8
     Array(ArchivedSbtProjectTemplate(templateName, description, url))
