cf9219efd6c210f77919c0b1d3a44567b650c843
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-04-13 23:38:20 +0300

    Show compiler-based highlighting progress in the status bar if compilation is too long #SCL-17385
    (cherry picked from commit 1f273fe485ed5237beb6863d998ee6f0e92ac3d5)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaHighlightingMode.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaHighlightingMode.scala
@@ -3,40 +3,61 @@ package org.jetbrains.plugins.scala.annotator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiFile
-import org.jetbrains.plugins.scala.project.ProjectExt
 import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.extensions.PsiFileExt
+import org.jetbrains.plugins.scala.lang.psi.api.{ScFile, ScalaFile}
+import org.jetbrains.plugins.scala.project.ProjectExt
 
 import scala.concurrent.duration._
 
 object ScalaHighlightingMode {
+
+  final val ShowDotcErrorsKey = "dotty.highlighting.compiler.errors.in.editor"
+  final val ShowScalacErrorsKey = "scala.highlighting.compiler.errors.in.editor"
+
+  private def showDotcErrors: Boolean = Registry.is(ShowDotcErrorsKey)
+  private def showScalacErrors: Boolean = Registry.is(ShowScalacErrorsKey)
+
   def isShowErrorsFromCompilerEnabled(project: Project): Boolean =
     showDotcErrors && project.hasScala3 || showScalacErrors && project.hasScala
 
-  def isShowErrorsFromCompilerEnabled(file: PsiFile): Boolean = {
-    val isRegularScalaFile = Option(file.getVirtualFile)
-      .exists(_.getExtension == ScalaFileType.INSTANCE.getDefaultExtension)
-    val enabled = file.isScala3File && showDotcErrors || file.isScala2File && showScalacErrors
+  def isShowErrorsFromCompilerEnabled(file: PsiFile): Boolean =
+    file match {
+      case scalaFile: ScalaFile =>
+        isShowErrorsFromCompilerEnabled(scalaFile)
+      case _ => false
+    }
 
-    isRegularScalaFile && enabled
+  def isShowErrorsFromCompilerEnabled(file: ScalaFile): Boolean = {
+    val virtualFile = file match {
+      case ScFile.VirtualFile(vFile) => vFile
+      case _                         => return false
+    }
+
+    val isRegularScalaFile = virtualFile.getFileType == ScalaFileType.INSTANCE
+    if (isRegularScalaFile) {
+      file.isScala3File && showDotcErrors || file.isScala2File && showScalacErrors
+    } else if (file.isWorksheetFile) {
+      // actually this should work for regular files as well
+      file.isInScala3Module && showDotcErrors || file.isInScalaModule && showScalacErrors
+    } else {
+      false
+    }
   }
 
+  // do we need this method now? we have a flag for scala 2 so `isShowErrorsFromCompilerEnabled` should be enough
   def showParserErrors(file: PsiFile): Boolean = {
     val shouldSkip = file.isScala3File && isShowErrorsFromCompilerEnabled(file)
 
     !shouldSkip
   }
+  
+  private def nonNegativeDuration(key: String): FiniteDuration =
+    Seq(Registry.get(key).asInteger, 0).max.millis
 
   def compilationDelay: FiniteDuration =
-    Seq(Registry.get("scala.highlighting.compilation.delay.millis").asInteger, 0).max.millis
-
-  final val ShowDotcErrorsKey = "dotty.highlighting.compiler.errors.in.editor"
-
-  private def showDotcErrors: Boolean =
-    Registry.is(ShowDotcErrorsKey)
-
-  final val ShowScalacErrorsKey = "scala.highlighting.compiler.errors.in.editor"
-
-  private def showScalacErrors: Boolean =
-    Registry.is(ShowScalacErrorsKey)
+    nonNegativeDuration("scala.highlighting.compilation.delay.millis")
+  
+  def compilationTimeoutToShowProgress: FiniteDuration =
+    nonNegativeDuration("scala.highlighting.compilation.timeout.to.show.progress.millis")
 }
