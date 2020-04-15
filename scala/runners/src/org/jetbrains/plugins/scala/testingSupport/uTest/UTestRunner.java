c176cba741c33aa0d456ab763835ba9814305e9c
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-04-15 18:53:12 +0300

    utest: move arguments parsing from UTestRunner to a UTestRunnerArgs.parse #SCL-17400
    - failedTest parameter was dropped long ago in commit:
    8f0181131d6f98c90b8fd82e94c48cd5316fd559 (jamesbrain committed on Mar 9, 2017)

--- scala/runners/src/org/jetbrains/plugins/scala/testingSupport/uTest/UTestRunner.java
+++ scala/runners/src/org/jetbrains/plugins/scala/testingSupport/uTest/UTestRunner.java
@@ -3,99 +3,16 @@ package org.jetbrains.plugins.scala.testingSupport.uTest;
 import org.jetbrains.plugins.scala.testingSupport.TestRunnerUtil;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.*;
-
-import static org.jetbrains.plugins.scala.testingSupport.uTest.UTestSuiteRunner.getTreeClass;
+import java.util.Map;
+import java.util.Set;
 
 public class UTestRunner {
 
-  protected static Class<?> getClassByFqn(String errorMessage, String... options) {
-    for (String fqn: options) {
-      try {
-        return Class.forName(fqn);
-      } catch (ClassNotFoundException ignored) {
-        // ignore
-      }
-    }
-    throw new RuntimeException(errorMessage);
-  }
+  public static void main(String[] argsRaw) throws IOException {
+    String[] newArgs = TestRunnerUtil.getNewArgs(argsRaw);
+    UTestRunnerArgs args = UTestRunnerArgs.parse(newArgs);
 
-  private static UTestPath parseTestPath(String className, String argsString) {
-    String[] nameArgs = argsString.split("\\\\");
-    List<String> asList = Arrays.asList(nameArgs);
-    List<String> testPath = asList.subList(1, asList.size());
-    Class<?> clazz;
-    try {
-      clazz = Class.forName(className);
-    } catch (ClassNotFoundException e) {
-      System.out.println("ClassNotFoundException for " + className + ": " + e.getMessage());
-      return null;
-    }
-    Method method;
-    try {
-      method = clazz.getMethod(asList.get(0));
-      assert (method.getReturnType().equals(getTreeClass()));
-    } catch (NoSuchMethodException e) {
-      System.out.println("NoSuchMethodException for " + asList.get(0) + " in " + className + ": " + e.getMessage());
-      return null;
-    }
-    return new UTestPath(className, testPath, method);
-  }
-
-  public static void main(String[] args) throws IOException {
-    String[] newArgs = TestRunnerUtil.getNewArgs(args);
-    Map<String, Set<UTestPath>> classesToTests = new HashMap<>();
-    HashMap<String, Set<UTestPath>> failedTestMap = new HashMap<>();
-    int i = 0;
-    String currentClass = null;
-    boolean failedUsed = false;
-    while (i < newArgs.length) {
-      switch (newArgs[i]) {
-        case "-s":
-          ++i;
-          while (i < newArgs.length && !newArgs[i].startsWith("-")) {
-            classesToTests.put(newArgs[i], new HashSet<>());
-            currentClass = newArgs[i];
-            ++i;
-          }
-          break;
-        case "-testName":
-          ++i;
-          if (currentClass == null)
-            throw new RuntimeException("Failed to run tests: no suite class specified for test " + newArgs[i]);
-          while (!newArgs[i].startsWith("-")) {
-            String testName = newArgs[i];
-            UTestPath aTest = parseTestPath(currentClass, testName);
-            if (aTest != null) {
-              classesToTests.get(currentClass).add(aTest);
-            }
-            ++i;
-          }
-          break;
-        case "-failedTests":
-          failedUsed = true;
-          ++i;
-          while (i < newArgs.length && !newArgs[i].startsWith("-")) {
-            String failedClassName = newArgs[i];
-            UTestPath testPath = parseTestPath(failedClassName, newArgs[i + 1]);
-            Set<UTestPath> testSet = failedTestMap.get(failedClassName);
-            if (testSet == null)
-              testSet = new HashSet<>();
-            if (testPath != null) {
-              testSet.add(testPath);
-            }
-            failedTestMap.put(failedClassName, testSet);
-            i += 2;
-          }
-          break;
-        default:
-          ++i;
-          break;
-      }
-    }
-
-    Map<String, Set<UTestPath>> suitesAndTests = failedUsed ? failedTestMap : classesToTests;
+    Map<String, Set<UTestPath>> suitesAndTests = args.classesToTests;
     // TODO: (from Nikolay Tropin)
     //  I think it would be better to encapsulate waiting logic in UTestRunner.
     //  Reporter shouldn't be aware about number of tests and manage concurrency.
@@ -107,5 +24,16 @@ public class UTestRunner {
     reporter.waitUntilReportingFinished();
 
     System.exit(0);
+  }
+
+  protected static Class<?> getClassByFqn(String errorMessage, String... options) {
+    for (String fqn: options) {
+      try {
+        return Class.forName(fqn);
+      } catch (ClassNotFoundException ignored) {
+        // ignore
+      }
+    }
+    throw new RuntimeException(errorMessage);
   }
 }
---
+++ scala/runners/src/org/jetbrains/plugins/scala/testingSupport/uTest/UTestRunnerArgs.java
@@ -0,0 +1,77 @@
+package org.jetbrains.plugins.scala.testingSupport.uTest;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+import static org.jetbrains.plugins.scala.testingSupport.uTest.UTestSuiteRunner.getTreeClass;
+
+public final class UTestRunnerArgs {
+
+    final Map<String, Set<UTestPath>> classesToTests;
+
+    public UTestRunnerArgs(Map<String, Set<UTestPath>> classesToTests) {
+        this.classesToTests = new HashMap<>(classesToTests);
+    }
+
+    private static final String TEST_SUITE_KEY = "-s";
+    private static final String TEST_NAME_KEY = "-testName";
+
+    public static UTestRunnerArgs parse(String[] args) {
+        Map<String, Set<UTestPath>> classesToTests = new HashMap<>();
+
+        String currentClass = null;
+        int argIdx = 0;
+        while (argIdx < args.length) {
+            switch (args[argIdx]) {
+                case TEST_SUITE_KEY:
+                    ++argIdx;
+                    while (argIdx < args.length && !args[argIdx].startsWith("-")) {
+                        String className = args[argIdx];
+                        classesToTests.put(className, new HashSet<>());
+                        currentClass = className;
+                        ++argIdx;
+                    }
+                    break;
+                case TEST_NAME_KEY:
+                    ++argIdx;
+                    if (currentClass == null)
+                        throw new RuntimeException("Failed to run tests: no suite class specified for test " + args[argIdx]);
+                    while (!args[argIdx].startsWith("-")) {
+                        String testName = args[argIdx];
+                        UTestPath aTest = parseTestPath(currentClass, testName);
+                        if (aTest != null) {
+                            classesToTests.get(currentClass).add(aTest);
+                        }
+                        ++argIdx;
+                    }
+                    break;
+                default:
+                    ++argIdx;
+                    break;
+            }
+        }
+        return new UTestRunnerArgs(classesToTests);
+    }
+
+    private static UTestPath parseTestPath(String className, String argsString) {
+        String[] nameArgs = argsString.split("\\\\");
+        List<String> asList = Arrays.asList(nameArgs);
+        List<String> testPath = asList.subList(1, asList.size());
+        final Class<?> clazz;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException for " + className + ": " + e.getMessage());
+            return null;
+        }
+        final Method method;
+        try {
+            method = clazz.getMethod(asList.get(0));
+            assert (method.getReturnType().equals(getTreeClass()));
+        } catch (NoSuchMethodException e) {
+            System.out.println("NoSuchMethodException for " + asList.get(0) + " in " + className + ": " + e.getMessage());
+            return null;
+        }
+        return new UTestPath(className, testPath, method);
+    }
+}
