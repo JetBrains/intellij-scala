6e046ecccb2b7ee4c23c825621fa651d1960604d
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-04-20 16:54:37 +0300

    Code review fix (SCL-CR-2034): not use blocking #SCL-17385

--- scala/compiler-shared/src/org/jetbrains/plugins/scala/util/RescheduledExecutor.scala
+++ scala/compiler-shared/src/org/jetbrains/plugins/scala/util/RescheduledExecutor.scala
@@ -10,18 +10,35 @@ import scala.concurrent.duration.FiniteDuration
 class RescheduledExecutor(name: String,
                           mayInterruptIfRunning: Boolean = false) {
 
+  import RescheduledExecutor.IgnoreKey
+  
   private val scheduler = AppExecutorUtil.createBoundedScheduledExecutorService(name, 1)
 
   private val lastScheduledTask = new AtomicReference[(ScheduledFuture[_], String)]
 
   /**
-   * Schedule action with specified delay.
+   * Schedules action with specified delay.
    * If already scheduled, than task will be rescheduled.
    */
   def schedule(delay: FiniteDuration)
               (action: => Unit): Unit =
-    schedule(delay, "ignore_key")(action)
+    schedule(delay, IgnoreKey)(action)
 
+  /**
+   * Cancels last scheduled task.
+   */
+  def cancelLast(): Unit =
+    cancelLast(IgnoreKey)
+
+  private def cancelLast(key: String): Unit = {
+    lastScheduledTask.get match {
+      case (task, taskKey) if taskKey == key =>
+        task.cancel(mayInterruptIfRunning)
+      case _ =>
+    }
+    lastScheduledTask.set(null)
+  }
+  
   /**
    * @param key this allows us to queue several worksheets compilation (on project opening, for example)
    *            while disallowing several compilation requests for a single worksheet
@@ -29,15 +46,16 @@ class RescheduledExecutor(name: String,
    */
   def schedule(delay: FiniteDuration, key: String)
               (action: => Unit): Unit = {
-    lastScheduledTask.get match {
-      case (task, taskKey) if taskKey == key =>
-        task.cancel(mayInterruptIfRunning)
-      case _ =>
-    }
+    cancelLast(key)
 
     val runnable: Runnable = () => action
     val future = scheduler.schedule(runnable, delay.length, delay.unit)
 
     lastScheduledTask.set((future, key))
   }
+}
+
+object RescheduledExecutor {
+  
+  private final val IgnoreKey = "ignore_key"
 }
