2f964bc0ac1803df05b9d5d4183549ec5f02efef
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-05-06 13:47:49 +0300

    scalatest tests: @DoNotDiscover annotation is not supported in ScalaTest 1.x

--- scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/scalatest/ScalaTestPackageTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/scalatest/ScalaTestPackageTest.scala
@@ -2,10 +2,9 @@ package org.jetbrains.plugins.scala.testingSupport.scalatest
 
 trait ScalaTestPackageTest extends ScalaTestTestCase {
 
-  private val packageName = "myPackage"
-  private val secondPackageName = "secondPackage"
-  private val thirdPackageName = "thirdPackage"
-  private val packageNameWithReservedKeyword = "type"
+  protected val packageName = "myPackage"
+  protected val secondPackageName = "secondPackage"
+  protected val packageNameWithReservedKeyword = "type"
 
   addSourceFile(packageName + "/Test1.scala",
     s"""
@@ -32,31 +31,6 @@ trait ScalaTestPackageTest extends ScalaTestTestCase {
        |  }
        |}
     """.stripMargin.trim())
-
-  addSourceFile(thirdPackageName + "/NestedStepsSuite.scala",
-    s"""package $thirdPackageName
-       |
-       |import org.scalatest._
-       |
-       |class NestedStepsSuite extends Suites(
-       |  new StepSuiteNotDiscoverable1,
-       |  new StepSuiteNotDiscoverable2,
-       |  new StepSuiteDiscoverable
-       |)
-       |@DoNotDiscover
-       |class StepSuiteNotDiscoverable1 extends FunSuite {
-       |  test("test1.1") { println("1.1" ) }
-       |  test("test1.2") { println("1.2" ) }
-       |}
-       |@DoNotDiscover
-       |class StepSuiteNotDiscoverable2 extends FunSuite {
-       |  test("test2.1") { println("2.1" ) }
-       |}
-       |class StepSuiteDiscoverable extends FunSuite {
-       |  test("test3.1") { println("3.1" ) }
-       |}
-       |""".stripMargin
-  )
 
   addSourceFile(secondPackageName + "/Test1.scala",
     s"""
@@ -91,25 +65,6 @@ trait ScalaTestPackageTest extends ScalaTestTestCase {
           Seq("[root]", "Test2", "Test2")
         ))
         assertResultTreeDoesNotHaveNodes(root, "SecondTest")
-      }
-    )
-
-  def testPackageTestRun_ShouldSkipNonDiscoverableTests(): Unit =
-    runTestByConfig2(createTestFromPackage(thirdPackageName),
-      assertPackageConfigAndSettings(_, thirdPackageName),
-      root => {
-        assertResultTreeHasExactNamedPaths(root)(Seq(
-          Seq("[root]", "NestedStepsSuite", "StepSuiteNotDiscoverable1", "test1.1"),
-          Seq("[root]", "NestedStepsSuite", "StepSuiteNotDiscoverable1", "test1.2"),
-          Seq("[root]", "NestedStepsSuite", "StepSuiteNotDiscoverable2", "test2.1"),
-          Seq("[root]", "NestedStepsSuite", "StepSuiteDiscoverable", "test3.1"),
-          Seq("[root]", "StepSuiteDiscoverable", "test3.1"),
-        ))
-        assertResultTreeHasNotGotExactNamedPaths(root)(Seq(
-          Seq("[root]", "StepSuiteNotDiscoverable1", "test1.1"),
-          Seq("[root]", "StepSuiteNotDiscoverable1", "test1.2"),
-          Seq("[root]", "StepSuiteNotDiscoverable2", "test2.1")
-        ))
       }
     )
 
