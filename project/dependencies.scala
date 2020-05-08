0adee1a63cae2f0a22c85e9c5cd621207bfa262f
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-05-08 18:12:26 +0300

    Update zinc version #SCL-17405

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -114,11 +114,11 @@ object Dependencies {
   val nailgun: ModuleID = "org.jetbrains" % "nailgun-patched" % "1.0.1"
   val zinc = "org.scala-sbt" %% "zinc" % zincVersion
   val zincInterface = "org.scala-sbt" % "compiler-interface" % zincVersion
-  val sbtInterface = "org.scala-sbt" % "util-interface" % "1.1.2"
+  val sbtInterface = "org.scala-sbt" % "util-interface" % "1.3.0"
 
   val compilerBridgeSources_2_10 = "org.scala-sbt" % "compiler-bridge_2.10" % zincVersion classifier "sources"
   val compilerBridgeSources_2_11 = "org.scala-sbt" % "compiler-bridge_2.11" % zincVersion classifier "sources"
-  val compilerBridgeSources_2_13 = "org.scala-sbt" % "compiler-bridge_2.13.0-M2" % zincVersion classifier "sources"
+  val compilerBridgeSources_2_13 = "org.scala-sbt" % "compiler-bridge_2.13" % zincVersion classifier "sources"
   val dottySbtBridge = "ch.epfl.lamp" % "dotty-sbt-bridge" % "0.24.0-RC1"
 
   /** The filtering function returns true for jars to be removed.
