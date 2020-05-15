6ee57d3607b6dbdf4568abc5b8594c87977af692
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-05-15 15:23:37 +0300

    PresentationUtil refactor step 1: decompose huge presentationString method to two: for PsiElement & PsiType

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/PresentationUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/PresentationUtil.scala
@@ -12,7 +12,6 @@ import org.jetbrains.plugins.scala.lang.psi.types.nonvalue.Parameter
 import org.jetbrains.plugins.scala.lang.psi.types.recursiveUpdate.ScSubstitutor
 import org.jetbrains.plugins.scala.lang.psi.types.result._
 import org.jetbrains.plugins.scala.lang.psi.types.{ScType, TypePresentationContext}
-import org.jetbrains.plugins.scala.lang.refactoring.util.ScTypeUtil
 import org.jetbrains.plugins.scala.project.{ProjectContext, ProjectContextOwner}
 
 object PresentationUtil {
@@ -25,49 +24,74 @@ object PresentationUtil {
 
   def presentationString(obj: Any, substitutor: ScSubstitutor)
                         (implicit project: ProjectContext, tpc: TypePresentationContext): String = {
+
     val res: String = obj match {
-      case clauses: ScParameters => clauses.clauses.map(presentationString(_, substitutor)).mkString("")
+      case null => ""
+      case psiElement: PsiElement =>
+        presentationStringForPsiElement(psiElement, substitutor)
+      case scType: ScType =>
+        presentationStringForScalaType(scType, substitutor)
+      case psiType: PsiType =>
+        presentationStringForJavaType(psiType, substitutor)
+      case param: Parameter =>
+        val builder = new StringBuilder
+        builder.append(param.name)
+        builder.append(": " + presentationStringForScalaType(param.paramType, substitutor))
+        if (param.isRepeated) builder.append("*")
+        if (param.isDefault) builder.append(" = _")
+        builder.toString()
+      case _ => obj.toString
+    }
+    res.replace(SyntheticClasses.TypeParameter, "T")
+  }
+
+  private def presentationStringForScalaType(scType: ScType, substitutor: ScSubstitutor): String =
+    // empty context is used just because it was so before refactoring
+    substitutor(scType).presentableText(TypePresentationContext.emptyContext)
+
+  private def presentationStringForJavaType(psiType: PsiType, substitutor: ScSubstitutor)
+                                           (implicit project: ProjectContext): String =
+    psiType match {
+      case tp: PsiEllipsisType =>
+        presentationStringForJavaType(tp.getComponentType, substitutor) + "*"
+      case _         =>
+        presentationStringForScalaType(psiType.toScType(), substitutor)
+    }
+
+  private def presentationStringForPsiElement(element: PsiElement, substitutor: ScSubstitutor)
+                                             (implicit project: ProjectContext, tpc: TypePresentationContext): String =
+    element match {
+      case clauses: ScParameters =>
+        clauses.clauses.map(presentationStringForPsiElement(_, substitutor)).mkString("")
       case clause: ScParameterClause =>
         val buffer = new StringBuilder("")
         buffer.append("(")
         if (clause.isImplicit) buffer.append("implicit ")
-        buffer.append(clause.parameters.map(presentationString(_, substitutor)).mkString(", "))
+        buffer.append(clause.parameters.map(presentationStringForPsiElement(_, substitutor)).mkString(", "))
         buffer.append(")")
         buffer.toString()
       case param: ScParameter =>
-        ScalaPsiPresentationUtils.renderParameter(param)(presentationString(_, substitutor))
-      case param: Parameter =>
-        val builder = new StringBuilder
-        builder.append(param.name)
-        builder.append(": " + presentationString(param.paramType, substitutor))
-        if (param.isRepeated) builder.append("*")
-        if (param.isDefault) builder.append(" = _")
-        builder.toString()
-      case tp: ScType => substitutor(tp).presentableText(TypePresentationContext.emptyContext)
-      case tp: PsiEllipsisType =>
-        presentationString(tp.getComponentType, substitutor) + "*"
-      case tp: PsiType =>
-        presentationString(tp.toScType(), substitutor)
+        ScalaPsiPresentationUtils.renderParameter(param)(presentationStringForScalaType(_, substitutor))
       case tp: ScTypeParamClause =>
-        tp.typeParameters.map(t => presentationString(t, substitutor)).mkString("[", ", ", "]")
+        tp.typeParameters.map(t => presentationStringForPsiElement(t, substitutor)).mkString("[", ", ", "]")
       case param: ScTypeParam =>
         var paramText = param.name
         if (param.isContravariant) paramText = "-" + paramText
         else if (param.isCovariant) paramText = "+" + paramText
         val stdTypes = param.projectContext.stdTypes
-        param.lowerBound foreach {
+        param.lowerBound.foreach {
           case stdTypes.Nothing =>
-          case tp: ScType => paramText = paramText + " >: " + presentationString(tp, substitutor)
+          case tp: ScType => paramText = paramText + " >: " + presentationStringForScalaType(tp, substitutor)
         }
-        param.upperBound foreach {
+        param.upperBound.foreach {
           case stdTypes.Any =>
-          case tp: ScType => paramText = paramText + " <: " + presentationString(tp, substitutor)
+          case tp: ScType => paramText = paramText + " <: " + presentationStringForScalaType(tp, substitutor)
         }
-        param.viewBound foreach {
-          (tp: ScType) => paramText = paramText + " <% " + presentationString(tp, substitutor)
+        param.viewBound.foreach {
+          (tp: ScType) => paramText = paramText + " <% " + presentationStringForScalaType(tp, substitutor)
         }
-        param.contextBound foreach {
-          (tp: ScType) => paramText = paramText + " : " + presentationString(ScTypeUtil.stripTypeArgs(substitutor(tp)), substitutor)
+        param.contextBound.foreach {
+          (tp: ScType) => paramText = paramText + " : " + presentationStringForScalaType(ScTypeUtil.stripTypeArgs(substitutor(tp)), substitutor)
         }
         paramText
       case param: PsiTypeParameter =>
@@ -75,7 +99,7 @@ object PresentationUtil {
         //todo: possibly add supers and extends?
         paramText
       case params: PsiParameterList =>
-        params.getParameters.map(presentationString(_, substitutor)).mkString("(", ", ", ")")
+        params.getParameters.map(presentationStringForPsiElement(_, substitutor)).mkString("(", ", ", ")")
       case param: PsiParameter =>
         val buffer: StringBuilder = new StringBuilder("")
         val list = param.getModifierList
@@ -92,7 +116,7 @@ object PresentationUtil {
           buffer.append(name)
         }
         buffer.append(": ")
-        buffer.append(presentationString(param.getType, substitutor)) //todo: create param type, java.lang.Object => Any
+        buffer.append(presentationStringForJavaType(param.getType, substitutor)) //todo: create param type, java.lang.Object => Any
         buffer.toString()
       case fun: ScFunction =>
         val buffer: StringBuilder = new StringBuilder("")
@@ -105,16 +129,18 @@ object PresentationUtil {
           case _ =>
         }
         buffer.append(fun.name)
-        fun.typeParametersClause match {case Some(tpc) => buffer.append(presentationString(tpc)) case _ =>}
-        buffer.append(presentationString(fun.paramClauses, substitutor)).append(": ")
-        buffer.append(presentationString(fun.returnType.getOrAny, substitutor))
+        fun.typeParametersClause match {
+          case Some(tpc) =>
+            // empty substitutor and context are used just because it was so before refactoring
+            buffer.append(presentationStringForPsiElement(tpc, ScSubstitutor.empty)(project, TypePresentationContext.emptyContext))
+          case _ =>
+        }
+        buffer.append(presentationStringForPsiElement(fun.paramClauses, substitutor)).append(": ")
+        buffer.append(presentationStringForScalaType(fun.returnType.getOrAny, substitutor))
         buffer.toString()
-      case elem: PsiElement => elem.getText
-      case null => ""
-      case _ => obj.toString
+      case _ =>
+        element.getText
     }
-    res.replace(SyntheticClasses.TypeParameter, "T")
-  }
 
   def accessModifierText(modifier: ScAccessModifier): String =
     ScalaPsiPresentationUtils.accessModifierText(modifier, fast = true)
