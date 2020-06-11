aad5269972cb446b29890da53a528775b936cd61
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2020-06-11 18:24:34 +0300

    a comma being typed should not hide the completion variants #SCL-9059

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/ScalaCharFilter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/ScalaCharFilter.scala
@@ -5,6 +5,7 @@ package completion
 import com.intellij.codeInsight.lookup.{CharFilter, Lookup}
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScArgumentExprList
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportExpr
 
 class ScalaCharFilter extends CharFilter {
@@ -35,6 +36,15 @@ class ScalaCharFilter extends CharFilter {
         case _ => SELECT_ITEM_AND_FINISH_LOOKUP
       }
     case '`' => ADD_TO_PREFIX
+    case ',' =>
+      /**
+       * @see [[com.intellij.codeInsight.completion.DefaultCharFilter.matchesAfterAppendingChar]]
+       * @see [[SameSignatureCallParametersProvider.MethodParametersCompletionProvider]]
+       */
+      element.getParent match {
+        case _: ScArgumentExprList /* todo if matchesAfterAppendingChar */ => ADD_TO_PREFIX
+        case _ => HIDE_LOOKUP
+      }
     case ':' => HIDE_LOOKUP
     case _ => null
   }
