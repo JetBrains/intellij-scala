5112e23180461a92d80086898b8870e83aef2e37
Pavel Fatin <pavel.fatin@jetbrains.com>
2020-06-22 15:30:13 +0200

    Show class / trait + object keywords as usages, allow to navigate between them, #SCL-17718 fixed

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/highlighter/usages/CompanionHighlightHandler.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.highlighter.usages
+
+import java.util
+import java.util.Collections
+
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.{PsiElement, PsiFile}
+import com.intellij.util.Consumer
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
+
+private class CompanionHighlightHandler(keyword: PsiElement, definition: ScTypeDefinition, editor: Editor, file: PsiFile)
+  extends HighlightUsagesHandlerBase[PsiElement](editor, file) {
+
+  override def computeUsages(targets: util.List[_ <: PsiElement]): Unit =
+    definition.baseCompanionModule.map(_.nameId.getPrevSiblingNotWhitespace).foreach { companionKeyword =>
+      myReadUsages.add(keyword.getTextRange)
+      myReadUsages.add(companionKeyword.getTextRange)
+    }
+
+  override def selectTargets(targets: util.List[_ <: PsiElement], selectionConsumer: Consumer[_ >: util.List[_ <: PsiElement]]): Unit = {
+    selectionConsumer.consume(targets)
+  }
+
+  override def getTargets: util.List[PsiElement] = Collections.singletonList(keyword)
+}
