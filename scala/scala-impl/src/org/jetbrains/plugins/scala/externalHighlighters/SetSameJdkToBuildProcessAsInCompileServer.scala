da55278da33acd3ce67f6f25ad6af1c0af8850de
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-06-23 12:44:57 +0300

    SetSameJdkToBuildProcessAsInCompileServer also resets the compiler.process.jdk value after compilation #SCL-17676

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/SetSameJdkToBuildProcessAsInCompileServer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/SetSameJdkToBuildProcessAsInCompileServer.scala
@@ -1,34 +1,50 @@
 package org.jetbrains.plugins.scala.externalHighlighters
 
+import java.util.UUID
+
+import com.intellij.compiler.server.BuildManagerListener
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.compiler.{CompileContext, CompileTask}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.{ProjectJdkTable, Sdk}
-import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.util.registry.{Registry, RegistryValue}
 import org.jetbrains.plugins.scala.compiler.{CompileServerLauncher, ScalaCompileServerSettings}
 
 /**
  * Sets Scala Compile Server's JDK to the build process.
+ * After build finished it resets the build process JDK value.
  *
  * @see SCL-17676
  */
 class SetSameJdkToBuildProcessAsInCompileServer
-  extends CompileTask {
+  extends CompileTask
+    with BuildManagerListener {
+
+  import SetSameJdkToBuildProcessAsInCompileServer.previousJdkHome
 
   override def execute(context: CompileContext): Boolean = {
     val project = context.getProject
-    val isUnitTestMode = !ApplicationManager.getApplication.isUnitTestMode
+    val isUnitTestMode = ApplicationManager.getApplication.isUnitTestMode
     if (!isUnitTestMode && ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project))
       setBuildProcessJdk(project)
     true
   }
 
+  override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = {
+    previousJdkHome.foreach(registryValue.setValue)
+    previousJdkHome = None
+  }
+
+  private def registryValue: RegistryValue =
+    Registry.get("compiler.process.jdk")
+
   private def setBuildProcessJdk(project: Project): Unit = {
+    previousJdkHome = Some(registryValue.asString)
     val compileServerJdkHome = for {
       jdk <- getCompileServerSdk(project)
       jdkHome <- Option(jdk.getHomePath)
     } yield jdkHome
-    Registry.get("compiler.process.jdk").setValue(compileServerJdkHome.getOrElse(""))
+    registryValue.setValue(compileServerJdkHome.getOrElse(""))
   }
 
   private def getCompileServerSdk(project: Project): Option[Sdk] = {
@@ -43,4 +59,9 @@ class SetSameJdkToBuildProcessAsInCompileServer
     else
       None
   }
+}
+
+object SetSameJdkToBuildProcessAsInCompileServer {
+
+  @volatile private var previousJdkHome = Option.empty[String]
 }
