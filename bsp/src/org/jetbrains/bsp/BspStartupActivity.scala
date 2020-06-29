6556f15d6a69ff6bccc21b094aa3122d63924ca9
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-06-29 13:02:52 +0200

    Subscribe to RunManagerListener topic manually
    We have observed that sometimes the "Use BSP environment" is not added
    to run configurations. Interestingly, the problem occurs during whole
    IntelliJ process lifetime. Restart is usually a valid workaround.
    The reproducibility ratio is very low, but one of the hypothesis is
    that, the listener definied via XML file is not correctly subscribed
    to the topic. That's why I'd like to directly subscribe it via
    `subscribe` method call instead of defining it in XML file.

--- bsp/src/org/jetbrains/bsp/BspStartupActivity.scala
+++ bsp/src/org/jetbrains/bsp/BspStartupActivity.scala
@@ -1,13 +1,22 @@
 package org.jetbrains.bsp
 
+import com.intellij.execution.{RunManager, RunManagerListener}
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import org.jetbrains.bsp.project.test.environment.BspFetchTestEnvironmentTaskInstaller
 
 class BspStartupActivity extends StartupActivity {
+    val logger= Logger.getInstance(classOf[BspStartupActivity])
+
     @Override
-    override def runActivity(project: Project): Unit = {
+    override def runActivity(project: Project): Unit = try {
         // initialize build loop only for bsp projects
-        if (BspUtil.isBspProject(project))
+        if (BspUtil.isBspProject(project)) {
           BspBuildLoopService.getInstance(project)
+          project.getMessageBus.connect().subscribe(RunManagerListener.TOPIC, new BspFetchTestEnvironmentTaskInstaller(project))
+        }
+    } catch {
+      case e: Throwable => logger.error(e)
     }
 }
