9459002375ad27da4cc7ceded807ad272138a30e
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-07-07 00:03:28 +0300

    warnIfCompileServerJdkVersionTooOld: show notification only once #SCL-17817
    (cherry picked from commit fd33e0527c69b7ca18e3161e2431b496eed08a2c)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/compiler/CompileServerNotificationsService.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/compiler/CompileServerNotificationsService.scala
@@ -2,26 +2,36 @@ package org.jetbrains.plugins.scala.compiler
 
 import com.intellij.notification.{Notification, NotificationListener, NotificationType, Notifications}
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.{Service, ServiceManager}
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.CompositeModificationTracker
 import javax.swing.event.HyperlinkEvent
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.extensions.invokeAndWait
+import org.jetbrains.plugins.scala.macroAnnotations.Cached
 
 import scala.concurrent.duration.DurationLong
 
-object CompileServerNotifications {
+@Service
+final class CompileServerNotificationsService(project: Project) {
 
   private val title = ScalaBundle.message("scala.compile.server.title")
-
-  def warnIfCompileServerJdkVersionTooOld(project: Project): Unit =
+  
+  private val modificationTracker = new CompositeModificationTracker(ProjectRootManager.getInstance(project))
+  
+  def resetNotifications(): Unit =
+    modificationTracker.incModificationCount()
+  
+  @Cached(modificationTracker, null)
+  def warnIfCompileServerJdkVersionTooOld(): Unit =
     for {
       (serverSdk, serverJdkVersion) <- CompileServerJdkManager.compileServerJdk(project)
       (recommendedSdk, recommendedJdkVersion) <- CompileServerJdkManager.recommendedJdk(project)
       if !serverJdkVersion.isAtLeast(recommendedJdkVersion)
-    } showWarning(project, serverSdk.getName, recommendedSdk.getName)
+    } showWarning(serverSdk.getName, recommendedSdk.getName)
 
-  private def showWarning(project: Project,
-                          serverSdk: String,
+  private def showWarning(serverSdk: String,
                           recommendedSdk: String): Unit = {
     val text =
       s"""Compilation problems may occur.<p/>
@@ -31,12 +41,12 @@ object CompileServerNotifications {
          |Compile Server JDK: $serverSdk<p/>
          |Modules JDK: $recommendedSdk<p/>
          |""".stripMargin
-    val listener = new FixSdkNotificationListener(project, recommendedSdk)
+    val listener = new FixSdkNotificationListener(recommendedSdk)
     val warningNotification = new Notification(title, title, text, NotificationType.WARNING, listener)
     Notifications.Bus.notify(warningNotification)
   }
   
-  private class FixSdkNotificationListener(project: Project, fixedSdk: String)
+  private class FixSdkNotificationListener(fixedSdk: String)
     extends NotificationListener {
     
     override def hyperlinkUpdate(notification: Notification, event: HyperlinkEvent): Unit = {
@@ -53,4 +63,10 @@ object CompileServerNotifications {
       CompileServerLauncher.tryToStart(project)
     }
   }
+}
+
+object CompileServerNotificationsService {
+  
+  def get(project: Project): CompileServerNotificationsService =
+    ServiceManager.getService(project, classOf[CompileServerNotificationsService])
 }
