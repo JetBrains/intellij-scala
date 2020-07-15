c702eb6d2d50e0a725392d515c455ff8dfdf3b76
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-07-15 12:44:27 +0300

    store and index class names for all property stubs
    #SCL-17878

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/stubs/ScPropertyStub.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/stubs/ScPropertyStub.scala
@@ -4,6 +4,7 @@ package psi
 package stubs
 
 import com.intellij.psi.stubs.StubElement
+import com.intellij.util.ArrayUtil.EMPTY_STRING_ARRAY
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScIdList, ScPatternList}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScValueOrVariable
 import org.jetbrains.plugins.scala.lang.psi.stubs.impl.{ScExpressionOwnerStub, ScTypeElementOwnerStub}
@@ -22,4 +23,13 @@ trait ScPropertyStub[P <: ScValueOrVariable] extends StubElement[P]
   def isImplicit: Boolean
 
   def names: Array[String]
+
+  /**
+   * Non-trivial class names in property type.
+   * It is in the same form as written in source or decompiled class file, so it may have prefix.
+   */
+  def classNames: Array[String]
+
+  override def implicitClassNames: Array[String] =
+    if (isImplicit) classNames else EMPTY_STRING_ARRAY
 }
--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/stubs/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/stubs/package.scala
@@ -4,9 +4,34 @@ package psi
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.{IStubElementType, StubElement}
+import com.intellij.util.ArrayUtil.EMPTY_STRING_ARRAY
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.{ScCompoundTypeElement, ScDesugarizableTypeElement, ScInfixTypeElement, ScParameterizedTypeElement, ScSimpleTypeElement, ScTypeElement, ScTypeProjection}
 
 package object stubs {
 
   private[stubs] type RawStubElement = StubElement[_ <: PsiElement]
   private[stubs] type RawStubElementType = IStubElementType[_ <: StubElement[_ <: PsiElement], _ <: PsiElement]
+
+  final def classNames(te: ScTypeElement): Array[String] = {
+    val allNames = te match {
+      case s: ScSimpleTypeElement => Array(s.getText)
+      case p: ScParameterizedTypeElement => classNames(p.typeElement)
+      case i: ScInfixTypeElement => Array(i.operation.getText)
+      case c: ScCompoundTypeElement =>
+        c.components.toArray.flatMap(classNames)
+      case d: ScDesugarizableTypeElement =>
+        d.computeDesugarizedType match {
+          case Some(tp) => classNames(tp)
+          case _ => EMPTY_STRING_ARRAY
+        }
+      case tp: ScTypeProjection => Array(tp.refName)
+      case _ => EMPTY_STRING_ARRAY
+    }
+
+    allNames.filterNot(defaultBaseClasses.contains)
+  }
+
+  private val defaultBaseClasses = Array("scala.AnyRef", "java.lang.Object")
+
+
 }
