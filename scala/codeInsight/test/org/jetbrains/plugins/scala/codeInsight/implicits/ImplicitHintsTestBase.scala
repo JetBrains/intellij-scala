c772c379a567da0d6f6a2159c7833eed448d5736
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-07-15 20:21:02 +0300

    compare implicit candidates by element when doing "fullInfo" search
    if implicit is both visible and from implicit scope it was counted twice
    #SCL-17880 fixed

--- scala/codeInsight/test/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsTestBase.scala
+++ scala/codeInsight/test/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitHintsTestBase.scala
@@ -3,13 +3,16 @@ package org.jetbrains.plugins.scala.codeInsight.implicits
 import org.jetbrains.plugins.scala.codeInsight.InlayHintsTestBase
 
 trait ImplicitHintsTestBase extends InlayHintsTestBase {
-  protected def doTest(text: String): Unit = {
+  protected def doTest(text: String, expand: Boolean = false): Unit = {
     val oldEnabled = ImplicitHints.enabled
+    val oldExpanded = ImplicitHints.expanded
     try {
       ImplicitHints.enabled = true
+      ImplicitHints.expanded = expand
       doInlayTest(text)
     } finally {
       ImplicitHints.enabled = oldEnabled
+      ImplicitHints.expanded = oldExpanded
     }
   }
 }
