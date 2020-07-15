aee41a4e95190d7e8d564668f85a1727d0c1cc85
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-07-15 20:21:02 +0300

    compare implicit candidates by element when doing "fullInfo" search
    if implicit is both visible and from implicit scope it was counted twice
    #SCL-17880 fixed

--- scala/codeInsight/test/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitArgumentHintsTest.scala
+++ scala/codeInsight/test/org/jetbrains/plugins/scala/codeInsight/implicits/ImplicitArgumentHintsTest.scala
@@ -85,4 +85,19 @@ class ImplicitArgumentHintsTest extends ImplicitHintsTestBase {
        |} println(x)
     """.stripMargin
   )
+
+  def testBothByNameAndImplicitScope(): Unit =
+    doTest(
+      s"""
+         |object Foo {
+         |  class A; class B; class C; class D
+         |  implicit def aFromB(implicit b: B): A = new A
+         |  implicit def bFromC(implicit c: C): B = new B
+         |
+         |  implicit val someD: D = new D
+         |  def materializeB(implicit a: A, d: D): B = new B
+         |  materializeB$S(aFromB(bFromC(?: C)), someD)$E
+         |}""".stripMargin,
+      expand = true
+    )
 }
