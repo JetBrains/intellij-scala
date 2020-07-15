19bc50e3ea978d12125d5aedcb68aae12be56ace
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-07-15 16:55:45 +0300

    SetSameJdkToBuildProcessAsInCompileServer: simplified logic. Erase the 'compiler.process.jdk' value for non-scala projects #SCL-17676

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/SetSameJdkToBuildProcessAsInCompileServer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/SetSameJdkToBuildProcessAsInCompileServer.scala
@@ -1,7 +1,5 @@
 package org.jetbrains.plugins.scala.externalHighlighters
 
-import java.util.UUID
-
 import com.intellij.compiler.server.BuildManagerListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.compiler.{CompileContext, CompileTask}
@@ -10,29 +8,13 @@ import com.intellij.openapi.project.{Project, ProjectManagerListener}
 import com.intellij.openapi.projectRoots.{ProjectJdkTable, Sdk}
 import com.intellij.openapi.util.registry.{Registry, RegistryValue}
 import org.jetbrains.plugins.scala.compiler.{CompileServerLauncher, ScalaCompileServerSettings}
+import org.jetbrains.plugins.scala.project.ProjectExt
 
 /**
- * Sets Scala Compile Server's JDK to the Build Process. And then resets it to original value.
+ * Sets Scala Compile Server's JDK to the Build Process.
  *
- * The steps:
- * 1. Set the 'compiler.process.jdk' registry property to the Build Process BEFORE it is started.
- * 2. After the Build Process is stared, reset the property to the original value.
- * 3. Additionally reset the property value after build is finished. There are some cases when
- * [[buildStarted]] isn't invoked. But the [[buildFinished]] invocation is guaranteed. The "reset" operation is
- * idempotent, so that's OK.
- *
- * Note 1:
- * [[TempCompilerProcessJdkService]] is also resets the property value when disposed. We need this to
- * reset the property if user closes the project right after "BEFORE" is executed.
- *
- * Note 2:
- * If user kills the Idea process the property may be not resetted. There are no solutions for this problem.
- * But we hope this is a very rare situation.
- *
- * Note 3:
- * [[org.jetbrains.jps.api.BuildType.UP_TO_DATE_CHECK]] runs in JPS process. But for this build type
- * the platform doesn't invoke compiler task. So we also override JDK of the JPS-process after project was opened
- * (see [[SetSameJdkToBuildProcessAsInCompileServerProjectManagerListener]]).
+ * For Scala projects it sets the 'compiler.process.jdk' property value to JDK used in Compile Server.
+ * For Non-scala projects it erases the 'compiler.process.jdk' value.
  *
  * @see SCL-17676
  */
@@ -42,57 +24,49 @@ class SetSameJdkToBuildProcessAsInCompileServer
 
   // BEFORE
   override def execute(context: CompileContext): Boolean = {
-    TempCompilerProcessJdkService.get(context.getProject).overrideBuildProcessJdk()
+    TempCompilerProcessJdkService.get().overrideOrEraseBuildProcessJdk(context.getProject)
     true
   }
-
-  override def buildStarted(project: Project, sessionId: UUID, isAutomake: Boolean): Unit =
-    TempCompilerProcessJdkService.get(project).resetCompilerProcessJdk()
-
-  override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit =
-    TempCompilerProcessJdkService.get(project).resetCompilerProcessJdk()
 }
 
 class SetSameJdkToBuildProcessAsInCompileServerProjectManagerListener
   extends ProjectManagerListener {
 
   override def projectOpened(project: Project): Unit =
-    TempCompilerProcessJdkService.get(project).overrideBuildProcessJdk()
+    TempCompilerProcessJdkService.get().overrideOrEraseBuildProcessJdk(project)
 }
 
 @Service
-final class TempCompilerProcessJdkService(project: Project)
+final class TempCompilerProcessJdkService
   extends Disposable {
-
-  import TempCompilerProcessJdkService._
 
   private def registryValue: RegistryValue =
     Registry.get("compiler.process.jdk")
 
-  def overrideBuildProcessJdk(): Unit =
+  def overrideBuildProcessJdk(project: Project): Unit =
     if (ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project))
-      TempCompilerProcessJdkService.get(project).setTempCompilerProcessJdk(getCompileServerJdkHome)
+      setTempCompilerProcessJdk(getCompileServerJdkHome(project))
 
-  private def setTempCompilerProcessJdk(tempJdkHome: => String): Unit = lock.synchronized {
-    if (originalJdkHome.isEmpty)
-      originalJdkHome = Some(registryValue.asString)
+  def eraseBuildProcessJdk(): Unit =
+    registryValue.setValue("")
+
+  def overrideOrEraseBuildProcessJdk(project: Project): Unit = if (project.hasScala)
+    overrideBuildProcessJdk(project)
+  else
+    eraseBuildProcessJdk()
+
+  private def setTempCompilerProcessJdk(tempJdkHome: => String): Unit =
     registryValue.setValue(tempJdkHome)
-  }
 
-  def resetCompilerProcessJdk(): Unit = lock.synchronized {
-    originalJdkHome.foreach(registryValue.setValue)
-    originalJdkHome = None
-  }
-
-  private def getCompileServerJdkHome: String = {
+  private def getCompileServerJdkHome(project: Project): String = {
     val compileServerJdkHome = for {
-      jdk <- getCompileServerSdk
+      jdk <- getCompileServerSdk(project)
       jdkHome <- Option(jdk.getHomePath)
     } yield jdkHome
     compileServerJdkHome.getOrElse("")
   }
 
-  private def getCompileServerSdk: Option[Sdk] = {
+  private def getCompileServerSdk(project: Project): Option[Sdk] = {
     val settings = ScalaCompileServerSettings.getInstance
     if (settings.COMPILE_SERVER_ENABLED)
       Option(
@@ -106,14 +80,11 @@ final class TempCompilerProcessJdkService(project: Project)
   }
 
   override def dispose(): Unit =
-    resetCompilerProcessJdk()
+    eraseBuildProcessJdk()
 }
 
 object TempCompilerProcessJdkService {
 
-  private val lock = new Object
-  @volatile private var originalJdkHome = Option.empty[String]
-
-  def get(project: Project): TempCompilerProcessJdkService =
-    ServiceManager.getService(project, classOf[TempCompilerProcessJdkService])
+  def get(): TempCompilerProcessJdkService =
+    ServiceManager.getService(classOf[TempCompilerProcessJdkService])
 }
