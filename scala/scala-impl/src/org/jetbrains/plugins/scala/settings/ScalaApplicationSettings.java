245a7f48bdfdd6c2e10b8af561d9aee7a4f41c20
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-07-20 01:47:44 +0200

    activate autobraces by default
     #SCL-16366

--- scala/scala-impl/src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
@@ -55,7 +55,7 @@ public class ScalaApplicationSettings implements PersistentStateComponent<ScalaA
 
   public boolean WRAP_SINGLE_EXPRESSION_BODY = true;
 
-  public boolean HANDLE_BLOCK_BRACES_INSERTION_AUTOMATICALLY = org.jetbrains.plugins.scala.package$.MODULE$.isInternalMode();
+  public boolean HANDLE_BLOCK_BRACES_INSERTION_AUTOMATICALLY = true;
 
   public boolean HANDLE_BLOCK_BRACES_REMOVAL_AUTOMATICALLY = false;
 
