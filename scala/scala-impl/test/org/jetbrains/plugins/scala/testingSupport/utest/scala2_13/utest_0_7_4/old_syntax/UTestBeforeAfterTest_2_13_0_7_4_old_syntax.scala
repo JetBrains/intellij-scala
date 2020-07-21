15358030b076cb9c3cd1f363cba70cf7f4e89be1
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-07-21 11:15:54 +0300

    utest tests: update scala utest version to the latest

--- scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/utest/scala2_13/utest_0_7_4/old_syntax/UTestBeforeAfterTest_2_13_0_7_4_old_syntax.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/utest/scala2_13/utest_0_7_4/old_syntax/UTestBeforeAfterTest_2_13_0_7_4_old_syntax.scala
@@ -1,6 +1,8 @@
-package org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_1
+package org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_4.old_syntax
 
-class UTestBeforeAfterTest_2_13_0_7_1 extends UTestTestBase_2_13_0_7_1 {
+import org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_4.UTestTestBase_2_13_0_7_4
+
+class UTestBeforeAfterTest_2_13_0_7_4_old_syntax extends UTestTestBase_2_13_0_7_4 {
 
   val beforeAfterTestName = "BeforeAfterTest"
   val beforeAfterFileName = beforeAfterTestName + ".scala"
@@ -11,18 +13,16 @@ class UTestBeforeAfterTest_2_13_0_7_1 extends UTestTestBase_2_13_0_7_1 {
        |
        |object $beforeAfterTestName extends TestSuite {
        |  val tests = Tests {
-       |    test("test1") {}
+       |    "test1" - {}
        |  }
        |
        |  override def utestBeforeEach(path: Seq[String]): Unit = { println(">>TEST: BEFORE <<") }
        |
        |  override def utestAfterEach(path: Seq[String]): Unit = { println(">>TEST: AFTER <<") }
        |}
-       |""".stripMargin.trim())
+      """.stripMargin.trim())
 
   def testBefore(): Unit = {
-
-
     runTestByLocation2(4, 10, beforeAfterFileName,
       assertConfigAndSettings(_, beforeAfterTestName, "tests\\test1"),
       DoNotCheck, expectedText = "BEFORE", checkOutputs = true)
--- scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/utest/scala2_13/utest_0_7_4/old_syntax/UTestStaticStringTest_2_13_0_7_4_old_syntax.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/utest/scala2_13/utest_0_7_4/old_syntax/UTestStaticStringTest_2_13_0_7_4_old_syntax.scala
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_1.old_syntax
+package org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_4.old_syntax
 
 import org.jetbrains.plugins.scala.testingSupport.utest.UTestStaticStringTest
-import org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_1.UTestTestBase_2_13_0_7_1
+import org.jetbrains.plugins.scala.testingSupport.utest.scala2_13.utest_0_7_4.UTestTestBase_2_13_0_7_4
 
-class UTestStaticStringTest_2_13_0_7_1_old_syntax extends UTestTestBase_2_13_0_7_1 with UTestStaticStringTest {
+class UTestStaticStringTest_2_13_0_7_4_old_syntax extends UTestTestBase_2_13_0_7_4 with UTestStaticStringTest {
 
   protected val testsTestName = "UTestTests"
 
