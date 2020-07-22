d1352f4104c6829bfbd4c5d0e9cd37070cd436e5
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-07-22 17:12:02 +0200

    added MarkInnerCaseObjectsAsFinal
     #SCL-17919 fixed

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/modifiers/MarkInnerCaseObjectsAsFinal.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package modifiers
+
+import com.intellij.codeInspection.{InspectionManager, LocalQuickFix, ProblemDescriptor, ProblemHighlightType}
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.lang.lexer.ScalaModifier
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
+
+class MarkInnerCaseObjectsAsFinal extends AbstractRegisteredInspection {
+  override protected def problemDescriptor(element: PsiElement,
+                                           maybeQuickFix: Option[LocalQuickFix],
+                                           descriptionTemplate: String,
+                                           highlightType: ProblemHighlightType)
+                                          (implicit manager: InspectionManager,
+                                           isOnTheFly: Boolean): Option[ProblemDescriptor] =
+    element match {
+      case obj@ScObject.withModifierList(ml) if !obj.isTopLevel && !ml.isFinal && ml.isCase =>
+        val quickFix = new SetModifierQuickfix(obj, ScalaModifier.FINAL, set = true)
+        super.problemDescriptor(obj.targetToken, Some(quickFix), descriptionTemplate, highlightType)
+      case _ => None
+    }
+}
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/modifier/MarkInnerCaseObjectsAsFinalTest.scala
@@ -0,0 +1,50 @@
+package org.jetbrains.plugins.scala.codeInspection.modifier
+
+import com.intellij.codeInspection.LocalInspectionTool
+import org.jetbrains.plugins.scala.codeInspection.modifiers.MarkInnerCaseObjectsAsFinal
+import org.jetbrains.plugins.scala.codeInspection.{ScalaInspectionBundle, ScalaInspectionTestBase}
+
+class MarkInnerCaseObjectsAsFinalTest extends ScalaInspectionTestBase {
+
+  override protected val classOfInspection: Class[_ <: LocalInspectionTool] =
+    classOf[MarkInnerCaseObjectsAsFinal]
+
+  override protected val description: String =
+    ScalaInspectionBundle.message("mark.inner.case.objects.as.final")
+
+  def testCaseObject(): Unit = testQuickFix(
+    s"""
+       |object Outer {
+       |  case ${START}object$END Inner
+       |}
+       |""".stripMargin,
+    """
+      |object Outer {
+      |  final case object Inner
+      |}
+      |""".stripMargin,
+    ScalaInspectionBundle.message("add.modifier", "final")
+  )
+
+  def testToplevelCaseObject(): Unit = checkTextHasNoErrors(
+    s"""
+       |case object Test
+       |""".stripMargin
+  )
+
+  def testFinalInnerCaseObject(): Unit = checkTextHasNoErrors(
+    s"""
+       |object Outer {
+       |  final case object Inner
+       |}
+       |""".stripMargin
+  )
+
+  def testFinalInnerCaseClass(): Unit = checkTextHasNoErrors(
+    s"""
+       |object Outer {
+       |  final case class Inner()
+       |}
+       |""".stripMargin
+  )
+}
