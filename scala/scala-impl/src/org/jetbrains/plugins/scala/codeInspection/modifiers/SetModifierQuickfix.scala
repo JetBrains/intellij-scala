24519bbfb9df0cd205e7a88410ff9b26284213ee
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-07-22 16:41:19 +0200

    added RedundantFinalOnToplevelObjectInspection
     #SCL-14629 fixed
    
    (cherry picked from commit 8033fd265c0a1847632585ac1e664ab203bfd5de)

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/modifiers/SetModifierQuickfix.scala
@@ -0,0 +1,32 @@
+package org.jetbrains.plugins.scala
+package codeInspection
+package modifiers
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.codeStyle.CodeStyleManager
+import org.jetbrains.annotations.Nls
+import org.jetbrains.plugins.scala.codeInspection.modifiers.SetModifierQuickfix.makeName
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScModifierListOwner
+
+class SetModifierQuickfix(_modifierListOwner: ScModifierListOwner, modifierText: String, set: Boolean)
+  extends AbstractFixOnPsiElement[ScModifierListOwner](makeName(modifierText, set), _modifierListOwner)
+{
+  override protected def doApplyFix(modifierListOwner: ScModifierListOwner)(implicit project: Project): Unit = {
+    val ml = modifierListOwner.getModifierList
+    ml.setModifierProperty(modifierText, set)
+
+    val textRange = ml.getTextRange
+    CodeStyleManager.getInstance(project).reformatText(
+      modifierListOwner.getContainingFile,
+      textRange.getStartOffset,
+      textRange.getEndOffset + 1
+    )
+  }
+}
+
+object SetModifierQuickfix {
+  @Nls
+  private def makeName(modifierText: String, set: Boolean): String =
+    if (set) ScalaInspectionBundle.message("add.modifier", modifierText)
+    else ScalaInspectionBundle.message("remove.modifier", modifierText)
+}
