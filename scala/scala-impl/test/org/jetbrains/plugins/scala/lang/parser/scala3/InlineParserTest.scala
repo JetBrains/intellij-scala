c37b6c873fd7e8c0b25cf6cab91076a369cee788
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-07-23 15:56:26 +0200

    implemented inline (class/def) parameter
     #SCL-17655

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/InlineParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/InlineParserTest.scala
@@ -18,4 +18,133 @@ class InlineParserTest extends SimpleScala3ParserTestBase {
       |}
       |""".stripMargin
   )
+
+  def test_toplevel_inline_def(): Unit = checkTree(
+    """
+      |package test
+      |inline def foo() = ()
+      |""".stripMargin,
+    """
+      |ScalaFile
+      |  PsiWhiteSpace('\n')
+      |  ScPackaging
+      |    PsiElement(package)('package')
+      |    PsiWhiteSpace(' ')
+      |    CodeReferenceElement: test
+      |      PsiElement(identifier)('test')
+      |    PsiWhiteSpace('\n')
+      |    ScFunctionDefinition: foo
+      |      AnnotationsList
+      |        <empty list>
+      |      Modifiers
+      |        PsiElement(inline)('inline')
+      |      PsiWhiteSpace(' ')
+      |      PsiElement(def)('def')
+      |      PsiWhiteSpace(' ')
+      |      PsiElement(identifier)('foo')
+      |      Parameters
+      |        ParametersClause
+      |          PsiElement(()('(')
+      |          PsiElement())(')')
+      |      PsiWhiteSpace(' ')
+      |      PsiElement(=)('=')
+      |      PsiWhiteSpace(' ')
+      |      UnitExpression
+      |        PsiElement(()('(')
+      |        PsiElement())(')')
+      |  PsiWhiteSpace('\n')
+      |""".stripMargin
+  )
+
+  def test_inline_def_param(): Unit = checkTree(
+    """
+      |def test(inline i: Int): Unit = ()
+      |""".stripMargin,
+    """
+      |ScalaFile
+      |  PsiWhiteSpace('\n')
+      |  ScFunctionDefinition: test
+      |    AnnotationsList
+      |      <empty list>
+      |    Modifiers
+      |      <empty list>
+      |    PsiElement(def)('def')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(identifier)('test')
+      |    Parameters
+      |      ParametersClause
+      |        PsiElement(()('(')
+      |        Parameter: i
+      |          AnnotationsList
+      |            <empty list>
+      |          Modifiers
+      |            <empty list>
+      |          PsiElement(inline)('inline')
+      |          PsiWhiteSpace(' ')
+      |          PsiElement(identifier)('i')
+      |          PsiElement(:)(':')
+      |          PsiWhiteSpace(' ')
+      |          ParameterType
+      |            SimpleType: Int
+      |              CodeReferenceElement: Int
+      |                PsiElement(identifier)('Int')
+      |        PsiElement())(')')
+      |    PsiElement(:)(':')
+      |    PsiWhiteSpace(' ')
+      |    SimpleType: Unit
+      |      CodeReferenceElement: Unit
+      |        PsiElement(identifier)('Unit')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(=)('=')
+      |    PsiWhiteSpace(' ')
+      |    UnitExpression
+      |      PsiElement(()('(')
+      |      PsiElement())(')')
+      |  PsiWhiteSpace('\n')
+      |""".stripMargin
+  )
+
+  def test_inline_class_parameter(): Unit = checkTree(
+    """
+      |class Test(inline x: int)
+      |""".stripMargin,
+    """
+      |ScalaFile
+      |  PsiWhiteSpace('\n')
+      |  ScClass: Test
+      |    AnnotationsList
+      |      <empty list>
+      |    Modifiers
+      |      <empty list>
+      |    PsiElement(class)('class')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(identifier)('Test')
+      |    PrimaryConstructor
+      |      AnnotationsList
+      |        <empty list>
+      |      Modifiers
+      |        <empty list>
+      |      Parameters
+      |        ParametersClause
+      |          PsiElement(()('(')
+      |          ClassParameter: x
+      |            AnnotationsList
+      |              <empty list>
+      |            Modifiers
+      |              <empty list>
+      |            PsiElement(inline)('inline')
+      |            PsiWhiteSpace(' ')
+      |            PsiElement(identifier)('x')
+      |            PsiElement(:)(':')
+      |            PsiWhiteSpace(' ')
+      |            ParameterType
+      |              SimpleType: int
+      |                CodeReferenceElement: int
+      |                  PsiElement(identifier)('int')
+      |          PsiElement())(')')
+      |    ExtendsBlock
+      |      <empty list>
+      |  PsiWhiteSpace('\n')
+      |""".stripMargin
+  )
 }
