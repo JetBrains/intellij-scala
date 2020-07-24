efd5ac714c2b1894e06a71af73324d8347089d18
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-07-24 16:47:24 +0200

    Made SetModifierQuickfix a little bit more typesafe
     #SCL-14629

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/modifiers/RedundantFinalOnToplevelObjectInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/modifiers/RedundantFinalOnToplevelObjectInspection.scala
@@ -18,7 +18,7 @@ class RedundantFinalOnToplevelObjectInspection extends AbstractRegisteredInspect
     element match {
       case obj@ScObject.withModifierList(ml) if obj.isTopLevel && ml.isFinal =>
         val finalModifier = ml.findFirstChildByType(ScalaTokenTypes.kFINAL).nullSafe.getOrElse(obj.targetToken)
-        val quickFix = new SetModifierQuickfix(obj, ScalaModifier.FINAL, set = false)
+        val quickFix = new SetModifierQuickfix(obj, ScalaModifier.Final, set = false)
         super.problemDescriptor(finalModifier, Some(quickFix), descriptionTemplate, highlightType)
       case _ => None
     }
