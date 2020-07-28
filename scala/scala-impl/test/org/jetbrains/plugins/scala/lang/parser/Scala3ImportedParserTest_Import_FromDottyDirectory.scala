ca31234ca974ca4bd0d4053bfb9d015b1e95a62e
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-07-28 16:08:57 +0300

    update dotty version to 0.26 #SCL-17939
    (cherry picked from commit 7ceb37ee1d0cad3964409575621be4a38039a9f2)
    (cherry picked from commit 1a995a5f8d674610fc823b2fd363a731f7b374d2)

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/Scala3ImportedParserTest_Import_FromDottyDirectory.scala
@@ -0,0 +1,73 @@
+package org.jetbrains.plugins.scala
+package lang
+package parser
+
+import java.io.{File, FilenameFilter, PrintWriter}
+import java.nio.file.{Files, Path, Paths}
+import java.util.function.Consumer
+
+import org.jetbrains.plugins.scala.util.TestUtils
+
+import scala.io.Source
+
+object Scala3ImportedParserTest_Import_FromDottyDirectory {
+
+  /**
+   *  Imports Tests from the dotty repositiory
+   */
+  def main(args: Array[String]): Unit = {
+    val dottyDirectory = args.headOption.getOrElse {
+      println("no dotty directory specified")
+      return
+    }
+    val succDir = TestUtils.getTestDataPath + Scala3ImportedParserTest.directory
+    val failDir = TestUtils.getTestDataPath +  Scala3ImportedParserTest_Fail.directory
+
+    clearDirectory(succDir)
+    clearDirectory(failDir)
+
+    val srcDir = dottyDirectory + "tests/pos"
+
+    println("srcdir =  " + srcDir)
+    println("faildir = " + failDir)
+
+    for (file <- allFilesIn(srcDir) if file.toString.toLowerCase.endsWith(".scala")) {
+      val target = failDir + file.toString.substring(srcDir.length).replace(".scala", "++++test")
+      val content = {
+        val src = Source.fromFile(file)
+        try src.mkString finally src.close()
+      }
+
+      val targetFile = new File(target)
+
+      val targetWithDirs = failDir + "/" + Iterator
+        .iterate(targetFile)(_.getParentFile)
+        .takeWhile(_ != null)
+        .takeWhile(!_.isDirectory)
+        .map(_.getName.replace('.', '_').replace("++++", "."))
+        .toSeq
+        .reverse
+        .mkString("_")
+      println(file + " -> " + targetWithDirs)
+
+      val pw = new PrintWriter(targetWithDirs)
+      pw.write(content)
+      if (content.last != '\n')
+        pw.write('\n')
+      pw.println("-----")
+      pw.close()
+    }
+  }
+
+  def allFilesIn(path: String): Iterator[File] =
+    allFilesIn(new File(path))
+
+  def allFilesIn(path: File): Iterator[File] = {
+    if (!path.exists) Iterator()
+    else if (!path.isDirectory) Iterator(path)
+    else path.listFiles.iterator.flatMap(allFilesIn)
+  }
+
+  private def clearDirectory(path: String): Unit =
+    new File(path).listFiles().foreach(_.delete())
+}
