d1332acbc340dae5c0170c5bca9b8482a39431e6
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2020-07-29 13:18:19 +0300

    NPE fixed #EA-236304 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/SameSignatureCallParametersProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/completion/SameSignatureCallParametersProvider.scala
@@ -114,6 +114,7 @@ object SameSignatureCallParametersProvider {
       ).asInstanceOf[ScReferenceExpression]
 
       iconable <- expression.resolve match {
+        case null => None
         case method: PsiMethod if method.isConstructor || !method.isParameterless => None
         case member: PsiMember if !isAccessible(member, invocationCount)(reference) => None
         case iconable => Some(iconable)
