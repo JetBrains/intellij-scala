787094dda285d9b7d5cb351f338614399bfa43f5
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-07-30 13:01:46 +0300

    process usages only in current file
    #SCL-17953

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/rename/inplace/ScalaLocalInplaceRenamer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/rename/inplace/ScalaLocalInplaceRenamer.scala
@@ -7,10 +7,11 @@ import com.intellij.lang.Language
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.{Pair, TextRange}
+import com.intellij.psi.search.LocalSearchScope
 import com.intellij.psi.{PsiDocumentManager, PsiElement, PsiFile, PsiNamedElement}
 import com.intellij.refactoring.RefactoringActionHandler
 import com.intellij.refactoring.rename.inplace.VariableInplaceRenamer
-import com.intellij.refactoring.util.TextOccurrencesUtil
+import com.intellij.refactoring.util.TextOccurrencesUtil.processUsagesInStringsAndComments
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.plugins.scala.extensions.ObjectExt
 import org.jetbrains.plugins.scala.lang.refactoring.ScalaNamesValidator
@@ -32,14 +33,19 @@ class ScalaLocalInplaceRenamer(elementToRename: PsiNamedElement, editor: Editor,
 
   override def collectAdditionalElementsToRename(stringUsages: util.List[Pair[PsiElement, TextRange]]): Unit = {
     val stringToSearch: String = ScalaNamesUtil.scalaName(elementToRename)
-    if (stringToSearch != null) {
-      TextOccurrencesUtil.processUsagesInStringsAndComments(elementToRename, stringToSearch, true, (psiElement: PsiElement, textRange: TextRange) => {
-        if (currentFile.contains(psiElement.getContainingFile)) {
-          stringUsages.add(Pair.create(psiElement, textRange))
-        }
+    if (stringToSearch == null)
+      return
+
+    val localScope =
+      currentFile.map(new LocalSearchScope(_))
+        .getOrElse(LocalSearchScope.EMPTY)
+
+    processUsagesInStringsAndComments(elementToRename, localScope, stringToSearch, true,
+      (psiElement: PsiElement, textRange: TextRange) => {
+        stringUsages.add(Pair.create(psiElement, textRange))
         true
-      })
-    }
+      }
+    )
   }
 
   override def isIdentifier(newName: String, language: Language): Boolean =
