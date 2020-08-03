79a000d819b3245c28157bf6ac1acebf03ae4264
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-08-03 13:55:02 +0300

    SCL-CR-2287: test support tests: report uncaptured contents in failed tests #SCL-17949
    (cherry picked from commit d6f8c22901198cbb2929a4f02830b5cc98ce86a7)

--- scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/TestResultListener.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/TestResultListener.scala
@@ -4,48 +4,28 @@ package testingSupport
 import com.intellij.execution.process.{ProcessEvent, ProcessListener}
 import com.intellij.openapi.util.Key
 import org.jetbrains.plugins.scala.testingSupport.TestResultListener._
-import org.jetbrains.plugins.scala.util.assertions.failWithCause
-import org.junit.Assert.fail
 
-import scala.concurrent.{Await, Promise}
-import scala.concurrent.duration.DurationInt
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.Promise
 
 
-class TestResultListener(private val testConfigurationName: String) extends ProcessListener {
+class TestResultListener extends ProcessListener {
 
-  private val outputTextAll = new StringBuilder
-  private val outputTextProgress = new StringBuilder
-  private val exitCodePromise: Promise[Int] = Promise()
+  private val _outputTextAll = new StringBuilder
+  private val _outputTextProgress = new StringBuilder
+  val exitCodePromise: Promise[Int] = Promise()
 
-  def waitForTestEnd(durationMs: Int): String = {
-    val exitCode = Try(Await.result(exitCodePromise.future, durationMs.milliseconds))
-    exitCode match {
-      case Success(0)     =>
-      case Success(code)     =>
-        fail(
-          s"""test `$testConfigurationName` terminated with error exit code: $code; captured outputs:
-             |${outputTextAll.toString}""".stripMargin
-        )
-      case Failure(exception) =>
-        failWithCause(
-          s"""test `$testConfigurationName` did not terminate correctly after $durationMs ms; captured outputs:
-             |${outputTextAll.toString}""".stripMargin,
-          exception
-        )
-    }
-    outputTextProgress.toString
-  }
+  def outputText: String = _outputTextAll.mkString
+  def outputTextProgress: String = _outputTextProgress.mkString
 
   override def onTextAvailable(event: ProcessEvent, outputType: Key[_]): Unit = {
     val text = event.getText
-    outputTextAll.append(text)
+    _outputTextAll.append(text)
 
     if (text.contains(testResultPrefix) && text.contains(testResultSuffix)) {
       val from = text.indexOf(testResultPrefix)
       val to = text.indexOf(testResultSuffix)
       if (from != -1 && to != -1) {
-        outputTextProgress.append(text.substring(from + testResultPrefix.length, to))
+        _outputTextProgress.append(text.substring(from + testResultPrefix.length, to))
       }
     }
   }
