149fdc97e81ceb47657fd7e19a40ce31cf8bfe04
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-08-20 13:36:40 +0300

    WorksheetLanguage is now not DependentLanguage
    - When user creates new Scala scratch and setting "Treat scala scratch files as worksheets" is enabled we create a file with worksheet extension `.sc`. (ScalaScratchFileCreationHelper)
    After IDEA restarts it tries to map the extension back to the appropriate language (it search for LanguageUtil.getFileLanguages()) and if worksheet language extends DependentLanguage it's not found.
    As a result old scratch files with `.sc` extension are associated with default language (Text)
    
    - The drawback is that now "New Scratch file" popup contains two languages: "Scala" and "Scala Worksheet" which behave the same way if "treat as..."setting is enabled. This is how it worked some time ago (AFAIR at ~2019.1). If we want to list only one language in the popup we should probably patch the IntelliJ platform.

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetLanguage.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetLanguage.scala
@@ -1,12 +1,10 @@
 package org.jetbrains.plugins.scala.worksheet
 
-import com.intellij.lang.DependentLanguage
 import com.intellij.lang.Language
 import org.jetbrains.plugins.scala.ScalaLanguage
 
 final class WorksheetLanguage private()
   extends Language(ScalaLanguage.INSTANCE, "Scala Worksheet")
-    with DependentLanguage
     with WorksheetLanguageLike
 
 object WorksheetLanguage {
