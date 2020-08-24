66e97648a128a18f5af8de6b914bf278a0f3e916
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-08-24 13:45:12 +0200

    Only parse inline before parameters if it precedes an identifier
     #SCL-18060

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/ModifierParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/ModifierParserTest.scala
@@ -160,4 +160,88 @@ class ModifierParserTest extends SimpleScala3ParserTestBase {
       |open class Test
       |""".stripMargin
   )
+
+  def test_parameter_named_inline(): Unit = checkTree(
+    """
+      |def test(inline: T) = ()
+      |""".stripMargin,
+    """
+      |ScalaFile
+      |  PsiWhiteSpace('\n')
+      |  ScFunctionDefinition: test
+      |    AnnotationsList
+      |      <empty list>
+      |    Modifiers
+      |      <empty list>
+      |    PsiElement(def)('def')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(identifier)('test')
+      |    Parameters
+      |      ParametersClause
+      |        PsiElement(()('(')
+      |        Parameter: inline
+      |          AnnotationsList
+      |            <empty list>
+      |          Modifiers
+      |            <empty list>
+      |          PsiElement(identifier)('inline')
+      |          PsiElement(:)(':')
+      |          PsiWhiteSpace(' ')
+      |          ParameterType
+      |            SimpleType: T
+      |              CodeReferenceElement: T
+      |                PsiElement(identifier)('T')
+      |        PsiElement())(')')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(=)('=')
+      |    PsiWhiteSpace(' ')
+      |    UnitExpression
+      |      PsiElement(()('(')
+      |      PsiElement())(')')
+      |  PsiWhiteSpace('\n')
+      |""".stripMargin
+  )
+
+  def test_inline_parameter_named_inline(): Unit = checkTree(
+    """
+      |def test(inline inline: T) = ()
+      |""".stripMargin,
+    """
+      |ScalaFile
+      |  PsiWhiteSpace('\n')
+      |  ScFunctionDefinition: test
+      |    AnnotationsList
+      |      <empty list>
+      |    Modifiers
+      |      <empty list>
+      |    PsiElement(def)('def')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(identifier)('test')
+      |    Parameters
+      |      ParametersClause
+      |        PsiElement(()('(')
+      |        Parameter: inline
+      |          AnnotationsList
+      |            <empty list>
+      |          Modifiers
+      |            <empty list>
+      |          PsiElement(inline)('inline')
+      |          PsiWhiteSpace(' ')
+      |          PsiElement(identifier)('inline')
+      |          PsiElement(:)(':')
+      |          PsiWhiteSpace(' ')
+      |          ParameterType
+      |            SimpleType: T
+      |              CodeReferenceElement: T
+      |                PsiElement(identifier)('T')
+      |        PsiElement())(')')
+      |    PsiWhiteSpace(' ')
+      |    PsiElement(=)('=')
+      |    PsiWhiteSpace(' ')
+      |    UnitExpression
+      |      PsiElement(()('(')
+      |      PsiElement())(')')
+      |  PsiWhiteSpace('\n')
+      |""".stripMargin
+  )
 }
