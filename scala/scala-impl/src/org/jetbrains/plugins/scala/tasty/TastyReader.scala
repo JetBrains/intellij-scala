55e048475cfec00aac2148c97630b812765fc3af
Pavel Fatin <pavel.fatin@jetbrains.com>
2020-09-02 11:39:11 +0200

    TastyReader: the actually supported version is 0.23

--- scala/scala-impl/src/org/jetbrains/plugins/scala/tasty/TastyReader.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/tasty/TastyReader.scala
@@ -15,8 +15,6 @@ object TastyReader {
   // TODO Remove when the project use Scala 2.13
   import scala.language.reflectiveCalls
 
-  import org.jetbrains.plugins.scala.LatestScalaVersions.Dotty
-
   def read(classpath: String, className: String): Option[TastyFile] =
     Option(reader.read(classpath, className))
 
@@ -37,12 +35,7 @@ object TastyReader {
         // TODO TASTy inspect: an ability to detect .tasty file version, https://github.com/lampepfl/dotty-feature-requests/issues/99
         // TODO TASTy inspect: make dotty-compiler depend on tasty-inspector https://github.com/lampepfl/dotty-feature-requests/issues/100
         // TODO Introduce the version variable
-        val tastyInspectorDependency = DependencyDescription(
-          "ch.epfl.lamp",
-          s"dotty-tasty-inspector_${Dotty.major}",
-          Dotty.minor,
-          isTransitive = true
-        )
+        val tastyInspectorDependency = DependencyDescription("ch.epfl.lamp", "dotty-tasty-inspector_0.23", "0.23.0-RC1", isTransitive = true)
         Resolver.resolve(tastyInspectorDependency).map(_.file)
       }
 
@@ -104,7 +97,7 @@ object TastyReader {
 
     exampleClasses.foreach { fqn =>
       println(fqn)
-      val file = read(home + s"/IdeaProjects/dotty-example-project/target/scala-${Dotty.major}/classes", fqn).get
+      val file = read(home + "/IdeaProjects/dotty-example-project/target/scala-0.23/classes", fqn).get
       println(file.text)
 
       (file.references ++ file.types).sortBy {
