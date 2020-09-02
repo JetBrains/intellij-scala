b22516271d224325fa93efbb967f37585b1e944a
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-09-02 10:53:36 +0300

    ScalaTestFrameworkCommandLineState: write test runners arguments into custom argsfile file tests classes are run #SCL-18099 fixed
    - to work with any JDK
    - not to rely on te IDEAs "command line shortener", which currently is supposed to only shorten classpath

--- scala/runners/src/org/jetbrains/plugins/scala/testingSupport/specs2/Spec2RunnerArgs.java
+++ scala/runners/src/org/jetbrains/plugins/scala/testingSupport/specs2/Spec2RunnerArgs.java
@@ -2,56 +2,53 @@ package org.jetbrains.plugins.scala.testingSupport.specs2;
 
 import org.jetbrains.plugins.scala.testingSupport.TestRunnerUtil;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Spec2RunnerArgs {
 
-    final HashMap<String, Set<String>> classesToTests;
+    final Map<String, Set<String>> classesToTests;
     final boolean showProgressMessages;
-    final ArrayList<String> otherArgs;
+    final List<String> otherArgs;
 
-    public Spec2RunnerArgs(HashMap<String, Set<String>> classesToTests,
+    public Spec2RunnerArgs(Map<String, Set<String>> classesToTests,
                            boolean showProgressMessages,
-                           ArrayList<String> otherArgs) {
+                           List<String> otherArgs) {
         this.classesToTests = classesToTests;
         this.showProgressMessages = showProgressMessages;
         this.otherArgs = otherArgs;
     }
 
-    public static Spec2RunnerArgs parse(String[] args) {
-        HashMap<String, Set<String>> classesToTests = new HashMap<>();
+    public static Spec2RunnerArgs parse(List<String> args) {
+        Map<String, Set<String>> classesToTests = new HashMap<>();
         boolean showProgressMessages = true;
-        ArrayList<String> otherArgs = new ArrayList<>();
+        List<String> otherArgs = new ArrayList<>();
 
         int argIdx = 0;
         String currentClass = null;
-        while (argIdx < args.length) {
-            switch (args[argIdx]) {
+        while (argIdx < args.size()) {
+            switch (args.get(argIdx)) {
                 case "-s":
                     ++argIdx;
-                    while (argIdx < args.length && !args[argIdx].startsWith("-")) {
-                        currentClass = args[argIdx];
+                    while (argIdx < args.size() && !args.get(argIdx).startsWith("-")) {
+                        currentClass = args.get(argIdx);
                         classesToTests.put(currentClass, new HashSet<>());
                         ++argIdx;
                     }
                     break;
                 case "-testName":
                     ++argIdx;
-                    String testNames = args[argIdx];
+                    String testNames = args.get(argIdx);
                     String testNamesUnescaped = TestRunnerUtil.unescapeTestName(testNames);
                     classesToTests.get(currentClass).add(testNamesUnescaped);
                     ++argIdx;
                     break;
                 case "-showProgressMessages":
                     ++argIdx;
-                    showProgressMessages = Boolean.parseBoolean(args[argIdx]);
+                    showProgressMessages = Boolean.parseBoolean(args.get(argIdx));
                     ++argIdx;
                     break;
                 default:
-                    otherArgs.add(args[argIdx]);
+                    otherArgs.add(args.get(argIdx));
                     ++argIdx;
                     break;
             }
--- scala/runners/src/org/jetbrains/plugins/scala/testingSupport/uTest/UTestRunner.java
+++ scala/runners/src/org/jetbrains/plugins/scala/testingSupport/uTest/UTestRunner.java
@@ -1,9 +1,11 @@
 package org.jetbrains.plugins.scala.testingSupport.uTest;
 
+import org.jetbrains.plugins.scala.testingSupport.TestRunnerUtil;
+
 public class UTestRunner {
 
   public static void main(String[] argsRaw) {
-    UTestRunnerArgs args = UTestRunnerArgs.parse(argsRaw);
+    UTestRunnerArgs args = UTestRunnerArgs.parse(TestRunnerUtil.preprocessArgsFiles(argsRaw));
 
     UTestReporter reporter = new UTestReporter();
     UTestSuiteRunner runner = new UTestSuiteRunner(reporter);
