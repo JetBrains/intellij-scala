b4d0e4a721259f01ed19e4e2fed3547f457e9be3
Mikhail Mutcianko <mutcianko.m@gmail.com>
2020-09-23 14:52:36 +0300

    properly rewrite converted Java->Scala class

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaNewClassInstanceEvaluator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaNewClassInstanceEvaluator.scala
@@ -1,12 +1,17 @@
 package org.jetbrains.plugins.scala.debugger.evaluation.evaluator
 
 import java.util
+import java.util.Collections
 
 import com.intellij.debugger.JavaDebuggerBundle
+import com.intellij.debugger.engine.evaluation.EvaluateExceptionUtil.createEvaluateException
 import com.intellij.debugger.engine.evaluation.{EvaluateException, EvaluateExceptionUtil, EvaluationContextImpl}
-import com.intellij.debugger.engine.evaluation.expression.Evaluator
+import com.intellij.debugger.engine.evaluation.expression.{Evaluator, Modifier}
 import com.intellij.debugger.engine.{DebuggerUtils, JVMName}
-import com.sun.jdi.{ClassType, ObjectReference, Value}
+import com.intellij.debugger.impl.{DebuggerUtilsEx, DebuggerUtilsImpl}
+import com.jetbrains.jdi.MethodImpl
+import com.sun.jdi.{ClassType, Method, ObjectReference, Value}
+import com.sun.tools.jdi.MethodImpl
 
 /**
  * User: Alefas
@@ -15,34 +20,31 @@ import com.sun.jdi.{ClassType, ObjectReference, Value}
  * converted from java verbatim
  */
 class ScalaNewClassInstanceEvaluator(val myClassTypeEvaluator: Evaluator, val myConstructorSignature: JVMName, val myParamsEvaluators: Array[Evaluator]) extends Evaluator {
-  @throws[EvaluateException]
-  override def evaluate(context: EvaluationContextImpl) = {
+
+  override def evaluate(context: EvaluationContextImpl): AnyRef = {
     val debugProcess = context.getDebugProcess
-    val obj = myClassTypeEvaluator.evaluate(context)
-    if (!obj.isInstanceOf[ClassType]) throw EvaluateExceptionUtil.createEvaluateException(JavaDebuggerBundle.message("evaluation.error.cannot.evaluate.class.type"))
-    val classType = obj.asInstanceOf[ClassType]
-    // find constructor
-    val method = DebuggerUtils.findMethod(classType, "<init>", myConstructorSignature.getName(debugProcess))
-    if (method == null) throw EvaluateExceptionUtil.createEvaluateException(JavaDebuggerBundle.message("evaluation.error.cannot.resolve.constructor", myConstructorSignature.getDisplayName(debugProcess)))
-    // evaluate arguments
-    var arguments: util.ArrayList[Value] = null
-    if (myParamsEvaluators != null) {
-      arguments = new util.ArrayList[Value](myParamsEvaluators.length)
-      for (evaluator <- myParamsEvaluators) {
-        val result = evaluator.evaluate(context)
-        if (result.isInstanceOf[Option[_]]) if (result.asInstanceOf[Option[_]].isDefined) arguments.add(result.asInstanceOf[Value])
-        else arguments.add(result.asInstanceOf[Value])
-      }
+    val classTypeInstance = myClassTypeEvaluator.evaluate(context) match {
+      case ct: ClassType => ct
+      case _ =>
+        throw createEvaluateException(JavaDebuggerBundle.message("evaluation.error.cannot.evaluate.class.type"))
     }
-    else arguments  = new util.ArrayList[Value]
-    var objRef: ObjectReference = null
-    try objRef = debugProcess.newInstance(context, classType, method, arguments)
-    catch {
-      case e: EvaluateException =>
-        throw EvaluateExceptionUtil.createEvaluateException(e)
+    val ctor = DebuggerUtils.findMethod(classTypeInstance, "<init>", myConstructorSignature.getName(debugProcess)) match {
+      case m: Method => m
+      case _ =>
+        throw createEvaluateException(JavaDebuggerBundle.message("evaluation.error.cannot.resolve.constructor", myConstructorSignature.getDisplayName(debugProcess)))
     }
-    objRef
+    val arguments: util.List[_ <: Value] = if (myParamsEvaluators != null) {
+      val buffer = new util.ArrayList[Value]()
+      myParamsEvaluators.map(x => x.evaluate(context) match {
+        case Some(result:Value) => buffer.add(result)
+        case result:Value       => buffer.add(result)
+        case _ =>
+      })
+      buffer
+    } else Collections.emptyList()
+
+    debugProcess.newInstance(context, classTypeInstance, ctor, arguments)
   }
 
-  override def getModifier = null
+  override def getModifier: Modifier = null
 }
