960ede4da39c9395b8de30d136cac23b86989200
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-09-24 20:12:03 +0300

    fix test run using sbt shell (#SCL-18203, #SCL-11662, #SCL-13002) fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/sbt/SbtCommandsBuilder.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/sbt/SbtCommandsBuilder.scala
@@ -1,41 +1,63 @@
 package org.jetbrains.plugins.scala.testingSupport.test.sbt
 
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.scala.testingSupport.test.utils.StringOps
 
 @ApiStatus.Internal
 trait SbtCommandsBuilder {
 
+  /**
+   * Returns a sequence of arguments which will be passed to sbt `testOnly` command: {{{
+   * testOnly <args1>
+   * testOnly <args2>
+   * ...
+   * }}}
+   *
+   * Reminder from [[https://www.scala-sbt.org/1.x/docs/Testing.html#Options]]:<br>
+   * Arguments to the test framework may be provided on the command line to the testOnly tasks
+   * following a -- separator. For example:
+   * {{{
+   * testOnly org.example.MyTest -- -verbosity 1
+   * }}}
+   */
   def buildTestOnly(classToTests: Map[String, Set[String]]): Seq[String]
 }
 
-// TODO: test all inheritors
+/**
+ * Please, consider implementing [[SbtCommandsBuilder]] instead of inheriting this class
+ */
 @ApiStatus.Internal
 abstract class SbtCommandsBuilderBase extends SbtCommandsBuilder {
 
   protected def classKey: Option[String] = None
   protected def testNameKey: Option[String] = None
-  protected def escapeClassAndTest(input: String): String = input
-  protected def escapeTestName(test: String): String = quoteSpaces(test)
 
-  def buildTestOnly(classToTests: Map[String, Set[String]]): Seq[String] =
-    classToTests.flatMap { case (aClass, tests) =>
-      if (tests.isEmpty)
-        Seq(withClassKey(aClass))
-      else
-        tests.map { test =>
-          val classAndTest = (Seq(aClass) ++ testNameKey :+ escapeTestName(test)).map(_.trim).mkString(" ")
-          withClassKey(escapeClassAndTest(classAndTest))
-        }
-    }.toSeq
+  protected def escapeTestName(test: String): String = test.withQuotedSpaces.withoutBackticks
+  protected  def escapeClassName(test: String): String = test.withQuotedSpaces.withoutBackticks
 
-  protected final def quoteSpaces(text: String): String =
-    if (text.contains(" ")) s""""$text"""" else text
-
-  private def withClassKey(value: String): String = {
-    val className = sanitize(value).trim
-    classKey.fold(className)(key => key.trim + " " + className)
+  /**
+   * Examples for single return item:
+   *  - `-classKey org.ClassName -testKey testName`
+   *  - `org.ClassName -testKey testName`
+   */
+  override def buildTestOnly(classToTests: Map[String, Set[String]]): Seq[String] = {
+    val result = classToTests
+      .flatMap { case (clazz, testNames) =>
+        if (testNames.isEmpty)
+          Seq(argsForTestClass(clazz))
+        else
+          testNames.map(argsForTest(clazz, _))
+      }
+      .toSeq
+    result
   }
 
-  // TODO: extract to utils, rename
-  private def sanitize(qualifiedName: String): String = qualifiedName.replace("`", "")
+  private def argsForTestClass(clazz: String): String =
+    join(classKey.toList :+ escapeClassName(clazz))
+
+  private def argsForTest(clazz: String, testName: String): String =
+    join((classKey.toList :+ escapeClassName(clazz)) ++ testNameKey.toList :+ escapeTestName(testName))
+
+  private def join(parts: Iterable[String]) =
+    parts.mkString(" ")
 }
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/testingSupport/test/utest/UTestSbtCommandsBuilderTest.scala
@@ -0,0 +1,62 @@
+package org.jetbrains.plugins.scala.testingSupport.test.utest
+
+import junit.framework.TestCase
+import org.jetbrains.plugins.scala.testingSupport.test.utest.UTestRunConfiguration.UTestSbtCommandsBuilder
+import org.jetbrains.plugins.scala.util.assertions.CollectionsAssertions._
+
+class UTestSbtCommandsBuilderTest extends TestCase {
+
+  private def doTest(
+    classToTests: Map[String, Set[String]],
+    expectedCommands: Seq[String]
+  ): Unit = {
+    val actual = (new UTestSbtCommandsBuilder).buildTestOnly(classToTests)
+    assertCollectionEquals(
+      expectedCommands.sorted,
+      actual.sorted
+    )
+  }
+
+  def testEmpty(): Unit =
+    doTest(
+      Map.empty[String, Set[String]],
+      Seq.empty[String]
+    )
+
+  def testSingleClass(): Unit =
+    doTest(
+      Map("org.example.MyClass" -> Set()),
+      Seq("-- org.example.MyClass"),
+    )
+
+  def testSeveralClasses(): Unit =
+    doTest(
+      Map("org.example.MyClass1" -> Set(), "org.example.MyClass2" -> Set()),
+      Seq("-- org.example.MyClass1", "-- org.example.MyClass2"),
+    )
+
+  def testSeveralClassesWithTests(): Unit = {
+    val classesWithTests = Map(
+      "org.example.MyClass1" -> Set(
+        """tests\testName""",
+        """tests\testName\innerTestName"""
+      ),
+      "org.example.MyClass2" -> Set(
+        """tests\test with spaces""",
+        """tests\test with spaces\inner test with spaces 1""",
+        """tests\test with spaces\inner test with spaces 2"""
+      )
+    )
+    val expected = Seq(
+      """-- org.example.MyClass1.testName""",
+      """-- org.example.MyClass1.testName.innerTestName""",
+      """-- "org.example.MyClass2.test with spaces"""",
+      """-- "org.example.MyClass2.test with spaces.inner test with spaces 1"""",
+      """-- "org.example.MyClass2.test with spaces.inner test with spaces 2"""",
+    )
+    doTest(
+      classesWithTests,
+      expected,
+    )
+  }
+}
