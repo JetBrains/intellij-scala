feef33a935cccf0633519d89a9391d840595ef10
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-10-06 15:04:18 +0300

    update IDEA version

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -9,7 +9,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.4.4"
   val zincVersion = "1.4.0-M12"
-  val intellijVersion = "203.4203"
+  val intellijVersion = "203.4449.2"
   val bspVersion = "2.0.0-M12+27-4994bd9d-SNAPSHOT"
   val sbtStructureVersion: String = "2020.2+3-3aa02a32"
   val sbtIdeaShellVersion: String = "2018.3"
