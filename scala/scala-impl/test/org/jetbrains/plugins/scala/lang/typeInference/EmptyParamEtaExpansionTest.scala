faa2720929ea4787bfbe5111c0456f9e10068b12
andrey.sugak <andrey.sugak@jetbrains.com>
2020-10-27 03:06:15 +0300

    Fix deprecation warning in EmptyParamEtaExpansionTest

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/EmptyParamEtaExpansionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/EmptyParamEtaExpansionTest.scala
@@ -4,8 +4,8 @@ import org.jetbrains.plugins.scala.LatestScalaVersions.{Scala_2_12, Scala_2_13}
 import org.jetbrains.plugins.scala.ScalaVersion
 import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
 
-class EmptyParamEtaExpansion extends ScalaLightCodeInsightFixtureTestAdapter {
-  override implicit def version: ScalaVersion = Scala_2_13
+class EmptyParamEtaExpansionTest extends ScalaLightCodeInsightFixtureTestAdapter {
+  override protected def supportedIn(version: ScalaVersion): Boolean = version == Scala_2_13
 
   def testSCL18172(): Unit = checkTextHasNoErrors(
     """
@@ -20,15 +20,15 @@ class EmptyParamEtaExpansion extends ScalaLightCodeInsightFixtureTestAdapter {
 }
 
 class EmpptyParamEtaExpansion_2_12 extends ScalaLightCodeInsightFixtureTestAdapter {
-  override implicit def version: ScalaVersion = Scala_2_12
+  override protected def supportedIn(version: ScalaVersion): Boolean = version == Scala_2_12
 
   def testSCL18172(): Unit = checkHasErrorAroundCaret(
     s"""
-      |import java.util.concurrent.{ExecutorService, Future}
-      |type Par[A] = ExecutorService => Future[A]
-      |def join[A](a: Par[Par[A]]): Par[A] = es => {
-      |  val value: Par[A] = ${CARET}a(es).get
-      |  ???
-      |}""".stripMargin
+       |import java.util.concurrent.{ExecutorService, Future}
+       |type Par[A] = ExecutorService => Future[A]
+       |def join[A](a: Par[Par[A]]): Par[A] = es => {
+       |  val value: Par[A] = ${CARET}a(es).get
+       |  ???
+       |}""".stripMargin
   )
 }
