f4dc90595400821429876fb656a0639097f89874
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-11-03 11:47:08 +0100

    replaced TypeAwareWidgetProvider with StatusBarWidgetFactory to aid  unloading
     #SCL-16809

--- scala/scala-impl/src/org/jetbrains/plugins/scala/components/TypeAwareWidgetFactory.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/TypeAwareWidgetFactory.scala
@@ -7,29 +7,36 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.StatusBarWidget.WidgetPresentation
-import com.intellij.openapi.wm.{StatusBar, StatusBarWidget, StatusBarWidgetProvider, WindowManager}
+import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager
+import com.intellij.openapi.wm.{StatusBar, StatusBarWidget, StatusBarWidgetFactory}
 import com.intellij.util.Consumer
 import javax.swing.{Icon, Timer}
 import org.jetbrains.annotations.Nullable
 import org.jetbrains.plugins.scala.actions.ToggleTypeAwareHighlightingAction
-import org.jetbrains.plugins.scala.extensions.{ObjectExt, invokeLater}
+import org.jetbrains.plugins.scala.components.TypeAwareWidgetFactory.Widget
+import org.jetbrains.plugins.scala.extensions.invokeLater
 import org.jetbrains.plugins.scala.icons.Icons
 import org.jetbrains.plugins.scala.project.ProjectExt
 import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 
-import scala.annotation.nowarn
+class TypeAwareWidgetFactory extends StatusBarWidgetFactory {
+  override def getId: String = "TypeAwareHighlighting"
 
-@nowarn("cat=deprecation")
-class TypeAwareWidgetProvider extends StatusBarWidgetProvider {
-  override def getWidget(project: Project): StatusBarWidget = new TypeAwareWidgetProvider.Widget(project)
+  override def getDisplayName: String = ScalaBundle.message("scala.type.aware.highlighting.indicator")
+
+  override def isAvailable(project: Project): Boolean = project.isOpen && project.hasScala
+
+  override def createWidget(project: Project): StatusBarWidget = new Widget(project, this)
+
+  override def disposeWidget(widget: StatusBarWidget): Unit = ()
+
+  override def canBeEnabledOn(statusBar: StatusBar): Boolean = isAvailable(statusBar.getProject)
 }
 
-object TypeAwareWidgetProvider {
-
-  private class Widget(project: Project) extends StatusBarWidget with StatusBarWidget.IconPresentation {
-
-    private var isApplicable = checkIsApplicable()
-
+object TypeAwareWidgetFactory {
+  private class Widget(project: Project, factory: TypeAwareWidgetFactory) extends StatusBarWidget with StatusBarWidget.IconPresentation {
+    private val statusBarWidgetsManager = project.getService(classOf[StatusBarWidgetsManager])
+    private var statusBar = Option.empty[StatusBar]
     private def isEnabled = ScalaProjectSettings.getInstance(project).isTypeAwareHighlightingEnabled
 
     private val myTimer = new Timer(1000, _ => {
@@ -41,6 +48,7 @@ object TypeAwareWidgetProvider {
     override def getPresentation: WidgetPresentation = this
 
     override def install(statusBar: StatusBar): Unit = {
+      this.statusBar = Some(statusBar)
       myTimer.setRepeats(true)
       myTimer.start()
       subscribeToRootsChange()
@@ -48,13 +56,12 @@ object TypeAwareWidgetProvider {
 
     override def dispose(): Unit = {
       myTimer.stop()
+      statusBar = None
     }
 
     @Nullable
-    override def getIcon: Icon = {
-      if (!isApplicable) null
-      else if (isEnabled) Icons.TYPED else Icons.UNTYPED
-    }
+    override def getIcon: Icon =
+      if (isEnabled) Icons.TYPED else Icons.UNTYPED
 
     override def getTooltipText: String = {
       val title = ScalaBundle.message("type.aware.highlighting.title")
@@ -79,19 +86,14 @@ object TypeAwareWidgetProvider {
       action.getShortcutSet.getShortcuts.headOption.map(KeymapUtil.getShortcutText)
     }
 
-    private def statusBar: Option[StatusBar] =
-      Option(WindowManager.getInstance).flatMap(_.getStatusBar(project).toOption)
-
     private def updateWidget(): Unit = {
+      statusBarWidgetsManager.updateWidget(factory)
       statusBar.foreach(_.updateWidget(ID))
     }
-
-    private def checkIsApplicable(): Boolean = project.isOpen && project.hasScala
 
     private def subscribeToRootsChange(): Unit = {
       project.subscribeToModuleRootChanged() { _ =>
         invokeLater {
-          isApplicable = checkIsApplicable()
           updateWidget()
         }
       }
