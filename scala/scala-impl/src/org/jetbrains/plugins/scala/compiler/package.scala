b3f30aa330079b59cd0c3970a5870e330bf095d1
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-11-09 21:09:37 +0300

    Handle <No SDK> in Compile Server Settings more gracefully #SCL-18405

--- scala/scala-impl/src/org/jetbrains/plugins/scala/compiler/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/compiler/package.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import com.intellij.openapi.projectRoots.{JavaSdk, JavaSdkVersion, Sdk}
 import com.intellij.openapi.util.io.FileUtil
+import org.jetbrains.plugins.scala.compiler.CompileServerLauncher.CompileServerProblem
 
 /**
  * @author Pavel Fatin
@@ -11,14 +12,14 @@ import com.intellij.openapi.util.io.FileUtil
 package object compiler {
   case class JDK(executable: File, tools: Option[File], name: String, version: Option[JavaSdkVersion])
 
-  def toJdk(sdk: Sdk): Either[NlsString, JDK] = sdk.getSdkType match {
+  def toJdk(sdk: Sdk): Either[CompileServerProblem, JDK] = sdk.getSdkType match {
     case jdkType: JavaSdk =>
       val vmExecutable = new File(jdkType.getVMExecutablePath(sdk))
       val tools = Option(jdkType.getToolsPath(sdk)).map(new File(_)) // TODO properly handle JDK 6 on Mac OS
       val version = Option(jdkType.getVersion(sdk))
       Right(JDK(vmExecutable, tools, sdk.getName, version))
     case unexpected =>
-      Left(ScalaBundle.nls("unexpected.sdk.type.for.sdk", unexpected, sdk))
+      Left(CompileServerProblem.Error(ScalaBundle.message("unexpected.sdk.type.for.sdk", unexpected, sdk)))
   }
 
   implicit class RichFile(private val file: File) extends AnyVal {
