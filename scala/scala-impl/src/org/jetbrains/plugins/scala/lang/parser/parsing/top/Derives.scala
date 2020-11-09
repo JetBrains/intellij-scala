d075dc854f26790ef0d5ec3be64aefff72b67eff
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-11-09 12:29:40 +0100

    added parsing templatederives
     #SCL-18406 fixed
    
    (cherry picked from commit ca97cf25ec20de13d725565b4fe7c1000d9a2e14)

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/top/Derives.scala
@@ -0,0 +1,34 @@
+package org.jetbrains.plugins.scala.lang.parser
+package parsing
+package top
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.lang.lexer.{ScalaTokenType, ScalaTokenTypes}
+import org.jetbrains.plugins.scala.lang.parser.parsing.builder.ScalaPsiBuilder
+
+import scala.annotation.tailrec
+
+object Derives extends ParsingRule {
+  override def apply()(implicit builder: ScalaPsiBuilder): Boolean = {
+    assert(builder.isScala3)
+
+    val marker = builder.mark()
+    if (builder.tryParseSoftKeyword(ScalaTokenType.DerivesKeyword)) {
+      parseNext()
+      marker.done(ScalaElementType.TEMPLATE_DERIVES)
+    } else marker.drop()
+
+    true
+  }
+
+  @tailrec
+  private def parseNext()(implicit builder: ScalaPsiBuilder): Unit = {
+    if (!QualId()) {
+      builder.error(ScalaBundle.message("identifier.expected"))
+    }
+
+    if (builder.getTokenType == ScalaTokenTypes.tCOMMA) {
+      builder.advanceLexer() // ate ,
+      parseNext()
+    }
+  }
+}
