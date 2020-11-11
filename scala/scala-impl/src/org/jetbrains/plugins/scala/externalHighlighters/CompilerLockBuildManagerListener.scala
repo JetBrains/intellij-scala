dc1cd1b6b3e37b45a9092b8c09203a95424e5995
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-11-11 19:44:13 +0300

    Suppress AlreadyDisposedException in CompilerLockBuildManagerListener
    EA-236361

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CompilerLockBuildManagerListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CompilerLockBuildManagerListener.scala
@@ -1,18 +1,30 @@
 package org.jetbrains.plugins.scala.externalHighlighters
 
 import java.util.UUID
-
 import com.intellij.compiler.server.BuildManagerListener
 import com.intellij.openapi.project.Project
+import com.intellij.serviceContainer.AlreadyDisposedException
+import org.jetbrains.plugins.scala.externalHighlighters.CompilerLockBuildManagerListener.suppressAlreadyDisposed
 
 class CompilerLockBuildManagerListener
   extends BuildManagerListener {
 
-  override def buildStarted(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = {
+  override def buildStarted(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = suppressAlreadyDisposed {
     HighlightingCompiler.get(project).cancel()
     CompilerLock.get(project).lock(sessionId.toString)
   }
 
-  override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit =
+  override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = suppressAlreadyDisposed {
     CompilerLock.get(project).unlock(sessionId.toString, exceptionIfNotLocked = false)
+  }
+}
+
+object CompilerLockBuildManagerListener {
+
+  private def suppressAlreadyDisposed(body: => Unit): Unit =
+    try {
+      body
+    } catch {
+      case _: AlreadyDisposedException => // ignore
+    }
 }
