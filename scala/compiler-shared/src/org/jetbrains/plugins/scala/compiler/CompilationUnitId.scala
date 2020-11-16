c61bb278ee90322a0028dadaa3c435724f35f9a5
Mikhail Mutcianko <mutcianko.m@gmail.com>
2020-11-16 12:49:09 +0300

    remove package object with duplicate fqn
    this fixes classloading ambiguity that leads to hardfreezes in compiler tests

--- scala/compiler-shared/src/org/jetbrains/plugins/scala/compiler/CompilationUnitId.scala
+++ scala/compiler-shared/src/org/jetbrains/plugins/scala/compiler/CompilationUnitId.scala
@@ -1,4 +1,10 @@
 package org.jetbrains.plugins.scala.compiler
 
+import org.jetbrains.plugins.scala.compiler.CompilationUnitId.ModuleId
+
 case class CompilationUnitId(moduleId: ModuleId,
                              testScope: Boolean)
+
+object CompilationUnitId {
+  type ModuleId = String
+}
