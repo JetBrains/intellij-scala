f5f9eb76d946e9ad1dde44dcc1e557ef77c9ea62
andrey.sugak <andrey.sugak@jetbrains.com>
2020-11-18 18:23:20 +0300

    Fix parsing of enum cases
    1. PSI is now structured differently, both types of case definitions
    (case Foo and case Foo, Bar, Baz) are now wrapped in ScEnumCases,
    which encapsulates one or more ScEnumCase.
    2. Empty constructor of ScEnumCase is not parsed (similar to traits).

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/statements/EnumCase.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/statements/EnumCase.scala
@@ -7,7 +7,7 @@ package statements
 import org.jetbrains.plugins.scala.lang.parser.parsing.base.{Ids, Modifier}
 import org.jetbrains.plugins.scala.lang.parser.parsing.builder.ScalaPsiBuilder
 import org.jetbrains.plugins.scala.lang.parser.parsing.expressions.Annotations
-import org.jetbrains.plugins.scala.lang.parser.parsing.params.ClassConstr
+import org.jetbrains.plugins.scala.lang.parser.parsing.params.EnumCaseConstr
 import org.jetbrains.plugins.scala.lang.parser.parsing.top.ConstrApps
 
 /**
@@ -35,21 +35,26 @@ object EnumCase extends ParsingRule {
 
         builder.getTokenType match {
           case `tIDENTIFIER` =>
-            val targetElementType = builder.lookAhead(1) match {
-              case `tCOMMA` =>
-                Ids()
+            val singleCaseMarker = builder.mark()
+            builder.advanceLexer()
+            val parseMultipleSingletonCases = builder.getTokenType == tCOMMA
 
-                ScalaElementType.EnumCases
-              case _ =>
-                builder.advanceLexer()
-
-                ClassConstr()
-                parseParents()
-
-                SingleCase
+            if (parseMultipleSingletonCases) {
+              builder.mark().done(ScalaElementType.EXTENDS_BLOCK)
+              singleCaseMarker.done(SingleCase)
+              builder.disableNewlines()
+              while (builder.getTokenType == tCOMMA) {
+                builder.advanceLexer() // consume ,
+                if (!parseSingletonCase()) builder.error(ScalaBundle.message("identifier.expected"))
+              }
+              builder.restoreNewlinesState()
+            } else {
+              EnumCaseConstr()
+              parseParents()
+              singleCaseMarker.done(SingleCase)
             }
 
-            marker.done(targetElementType)
+            marker.done(ScalaElementType.EnumCases)
             true
           case _ =>
             marker.rollbackTo()
@@ -60,6 +65,18 @@ object EnumCase extends ParsingRule {
         false
     }
   }
+
+  private def parseSingletonCase()(implicit builder: ScalaPsiBuilder): Boolean =
+    builder.getTokenType match {
+      case `tIDENTIFIER` =>
+        val marker = builder.mark()
+        builder.advanceLexer() // Ate identifier
+        builder.mark().done(ScalaElementType.EXTENDS_BLOCK)
+        marker.done(SingleCase)
+        true
+      case _ =>
+        false
+    }
 
   private def parseParents()(implicit builder: ScalaPsiBuilder): Unit = {
     val marker = builder.mark()
