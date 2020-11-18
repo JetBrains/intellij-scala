ce22632cf628e6d785189a41bf26cc338d27b693
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-11-18 18:57:06 +0300

    support Scala3 @main annotation (#SCL-18221, #SCL-17430) fixed
    - show green gutter
    - support creation configuration from context
    - adjust validation of application configuration
    - provide dialog to fill in program arguments if they are missing
    
    (cherry picked from commit 67f443ba2331232b3da9f21b3d55b6f863e902bd)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/runner/ScalaApplicationConfigurationProducer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/runner/ScalaApplicationConfigurationProducer.scala
@@ -4,7 +4,7 @@ package runner
 import java.lang.Boolean.TRUE
 
 import com.intellij.execution._
-import com.intellij.execution.actions.{ConfigurationContext, ConfigurationFromContext}
+import com.intellij.execution.actions.{ConfigurationContext, ConfigurationFromContext, RunConfigurationProducer}
 import com.intellij.execution.application.{ApplicationConfiguration, ApplicationConfigurationProducer, ApplicationConfigurationType}
 import com.intellij.execution.impl.RunManagerImpl
 import com.intellij.openapi.module.Module
@@ -12,21 +12,26 @@ import com.intellij.openapi.util.{Key, Ref}
 import com.intellij.psi._
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+import org.jetbrains.plugins.scala.macroAnnotations.Measure
 import org.jetbrains.plugins.scala.project.ModuleExt
 import org.jetbrains.plugins.scala.project.ProjectPsiElementExt
+import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestConfigurationProducer
 import org.jetbrains.plugins.scala.util.ScalaMainMethodUtil
 
 /**
- * @author Alefas
- * @since 02.03.12
+ * @see java analog [[com.intellij.execution.application.ApplicationConfigurationProducer]]
  */
 class ScalaApplicationConfigurationProducer
   extends BaseScalaApplicationConfigurationProducer[ApplicationConfiguration](ApplicationConfigurationType.getInstance)
 
-private object ScalaApplicationConfigurationProducer {
+object ScalaApplicationConfigurationProducer {
   val key: Key[java.lang.Boolean] = Key.create("is.scala3.application.run.configuration")
 
-  def isScala3ApplicationConfiguration(configuration: ApplicationConfiguration): Boolean =
+  def apply(): ScalaApplicationConfigurationProducer =
+    RunConfigurationProducer.EP_NAME.findExtensionOrFail(classOf[ScalaApplicationConfigurationProducer])
+
+  private[runner] def isScala3ApplicationConfiguration(configuration: ApplicationConfiguration): Boolean =
     configuration.getCopyableUserData(key) == TRUE
 }
 
@@ -34,13 +39,18 @@ abstract class BaseScalaApplicationConfigurationProducer[T <: ApplicationConfigu
   extends JavaRuntimeConfigurationProduceBaseAdapter[T](configurationType)
     with Cloneable {
 
-  private def createConfiguration(obj: PsiClass, context: ConfigurationContext,
-                                  location: Location[_ <: PsiElement], configuration: T): Unit = {
-    configuration.setMainClassName(nameForConfiguration(obj))
+  private def setupConfiguration(
+    mainClassName: String,
+    element: PsiElement,
+    context: ConfigurationContext,
+    location: Location[_ <: PsiElement],
+    configuration: T
+  ): Unit = {
+    configuration.setMainClassName(mainClassName)
     configuration.setName(configuration.suggestedName())
     setupConfigurationModule(context, configuration)
 
-    if (obj.isInScala3Module) {
+    if (element.isInScala3Module) {
       configuration.putCopyableUserData(ScalaApplicationConfigurationProducer.key, TRUE)
     }
 
@@ -66,36 +76,77 @@ abstract class BaseScalaApplicationConfigurationProducer[T <: ApplicationConfigu
     element.withParentsInFile.exists(classWithSameName) || fileHasClassWithSameName
   }
 
+  private def hasScala3MainAncestorWithName(element: PsiElement, name: String): Boolean = {
+    val candidates = ScalaMainMethodUtil.containingScala3MainMethodCandidates(element)
+    val found = candidates.find(nameForScala3MainConfiguration(_) == name)
+    found.isDefined
+  }
+
+  @Measure
   override def isConfigurationFromContext(configuration: T, context: ConfigurationContext): Boolean = {
     val location = context.getLocation
     if (location == null) return false
-    //use fast psi location check to filter off obvious candidates
-    if (context.getPsiLocation == null || !hasClassAncestorWithName(context.getPsiLocation, configuration.getMainClassName)) return false
-    val containingObj = ScalaMainMethodUtil.findObjectWithMain(context.getPsiLocation)
-    val predefinedModule: Module = RunManagerEx.getInstanceEx(location.getProject).asInstanceOf[RunManagerImpl].
-      getConfigurationTemplate(getConfigurationFactory).getConfiguration.asInstanceOf[T].getConfigurationModule.getModule
+    val element = context.getPsiLocation
+    if (element == null) return false
+    val file = element.getContainingFile
+    if (!file.isInstanceOf[ScalaFile]) return false
 
-    val module = configuration.getConfigurationModule.getModule
-    val sameName = containingObj.map(nameForConfiguration).contains(configuration.getMainClassName)
-
-    sameName && (location.getModule == module || predefinedModule == module)
+    if (hasScala3MainAncestorWithName(element, configuration.getMainClassName)) {
+      sameModule(configuration, location)
+    }
+    else if (hasClassAncestorWithName(element, configuration.getMainClassName)) {
+      val containingObj = ScalaMainMethodUtil.findObjectWithMain(element)
+      val sameName = containingObj.map(nameForConfiguration).contains(configuration.getMainClassName)
+      sameName && sameModule(configuration, location) && {
+        // We need to ensure that we are not in some scala 3 main method
+        // This is required because we recognize created configuration even if context element is not a child
+        // of object with a main method (see ScalaMainMethodUtil.findObjectWithMain.findTopLevel). And this can happen
+        // when we right click on scala3 main method, but some configuration for object+main already exists
+        val scala3Method = ScalaMainMethodUtil.findContainingScala3MainMethod(element)
+        scala3Method.isEmpty
+      }
+    }
+    else false
   }
 
+  private def sameModule(configuration: T, location: Location[_ <: PsiElement]): Boolean = {
+    val module = configuration.getConfigurationModule.getModule
+    val predefinedModule = getPredefinedModule(location)
+    location.getModule == module || predefinedModule == module
+  }
+
+  private def getPredefinedModule(location: Location[_ <: PsiElement]): Module = {
+    val manager = RunManagerEx.getInstanceEx(location.getProject).asInstanceOf[RunManagerImpl]
+    val template = manager.getConfigurationTemplate(getConfigurationFactory)
+    template.getConfiguration.asInstanceOf[T].getConfigurationModule.getModule
+  }
+
+  @Measure
   override def setupConfigurationFromContext(configuration: T, context: ConfigurationContext, sourceElement: Ref[PsiElement]): Boolean = {
     val location = JavaExecutionUtil.stepIntoSingleClass(context.getLocation)
     if (location == null) return false
     val element: PsiElement = location.getPsiElement
+    if (element == null) return false
     val containingFile = element.getContainingFile
     if (!containingFile.isInstanceOf[ScalaFile]) return false
     if (!element.isPhysical) return false
 
-    ScalaMainMethodUtil.findMainClassAndSourceElem(element) match {
-      case Some((clazz, elem)) =>
-        createConfiguration(clazz, context, location, configuration)
-        sourceElement.set(elem)
+    ScalaMainMethodUtil.findContainingScala3MainMethod(element) match {
+      case Some(scala3Main) =>
+        val mainClassName = nameForScala3MainConfiguration(scala3Main)
+        setupConfiguration(mainClassName, scala3Main, context, location, configuration)
+        sourceElement.set(scala3Main)
         true
       case _ =>
-        false
+        ScalaMainMethodUtil.findMainClassAndSourceElem(element) match {
+          case Some((clazz, elem)) =>
+            val mainClassName = nameForConfiguration(clazz)
+            setupConfiguration(mainClassName, clazz, context, location, configuration)
+            sourceElement.set(elem)
+            true
+          case _ =>
+            false
+        }
     }
   }
 
@@ -104,4 +155,10 @@ abstract class BaseScalaApplicationConfigurationProducer[T <: ApplicationConfigu
   }
 
   private def nameForConfiguration(c: PsiClass): String = c.qualifiedName
+
+  // scala3 @main method always uses qualified name of the containing file even if the method is not top level (at least with version 3.0.0-M1)
+  private def nameForScala3MainConfiguration(fun: ScFunctionDefinition): String = {
+    val packageFqn = fun.topLevelQualifier.filter(_.nonEmpty)
+    (packageFqn.toSeq :+ fun.name).mkString(".")
+  }
 }
---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/runner/view/ScalaProvideMainMethodParametersDialog.scala
@@ -0,0 +1,37 @@
+package org.jetbrains.plugins.scala.runner
+package view
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import javax.swing.{JComponent, SwingConstants}
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.runner.Scala3MainMethodSyntheticClass.MainMethodParameters.CustomParameter
+
+private[runner] final class ScalaProvideMainMethodParametersDialog(
+  project: Project,
+  expectedParameters: Seq[CustomParameter],
+  actualParameters: Seq[String]
+) extends DialogWrapper(project, true, true) {
+
+  assert(expectedParameters.nonEmpty)
+  assert(actualParameters.size <= expectedParameters.size)
+
+  private val parametersTable =
+    new ScalaMainMethodParametersTable(expectedParameters, actualParameters)
+
+  locally {
+    setTitle(ScalaBundle.message("provide.program.arguments"))
+    setButtonsAlignment(SwingConstants.RIGHT)
+    init()
+  }
+
+  def filledParametersValues: Seq[String] = parametersTable.filledParametersValues
+
+  override def createCenterPanel(): JComponent = parametersTable.getComponent
+
+  override def doOKAction(): Unit = {
+    // needs to be called when we press Enter to invoke "Ok" action, otherwise last edited cell value will not applied
+    parametersTable.finishCellEditing()
+    super.doOKAction()
+  }
+}
