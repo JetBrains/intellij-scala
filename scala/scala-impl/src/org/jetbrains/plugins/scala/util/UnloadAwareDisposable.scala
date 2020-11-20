86ceea9d0cbf15c22e40331bf1079af669b4664d
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-11-20 19:05:41 +0300

    introduce ScalaShutDownTracker

--- scala/scala-impl/src/org/jetbrains/plugins/scala/util/UnloadAwareDisposable.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/UnloadAwareDisposable.scala
@@ -6,10 +6,23 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 
 object UnloadAwareDisposable {
-  /*
+
+  /**
    * A disposable that is disposed when the scala plugin is unloaded
-   * Chain to it when you think that your disposable might live longer then the plugin
-   * (like Project or Module)
+   * (that includes Application exit in non-unit-test mode)
+   *
+   * Use it as a parent when you think that your current parent disposable might live longer then the plugin
+   * (like Application, Project or Module)
+   *
+   * If you have a class that does not implement Disposable, but you still register it somewhere where it is referenced
+   * longer than the plugin lives, then we fail to clean up correctly.
+   * So in that case you could make it a disposable, chain it to `scalaPluginDisposable`
+   * and tidy up correctly when dispose() is called.
+   *
+   * @note
+   * it isn't disposed during tests run (see description of [[ScalaShutDownTracker]]).<br>
+   * If you want to ensure some work to be triggered on application exit
+   * you consider using [[ScalaShutDownTracker.registerShutdownTask]]
    */
   lazy val scalaPluginDisposable: Disposable =
     ApplicationManager.getApplication.getService(classOf[UnloadAwareDisposableService])
