646b146998322177991b1405936d24b0e9fbc808
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-11-22 19:22:18 +0300

    compiler charts: fix ui flickering for vertical and horizontal scroll bars in DiagramsComponent #SCL-18478
    - update graphics data and revalidate component  outside paintComponent. You should not change layout or change component properties in paintComponent.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/ui/CompilationChartsComponent.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/ui/CompilationChartsComponent.scala
@@ -9,17 +9,23 @@ import net.miginfocom.swing.MigLayout
 class CompilationChartsComponent(project: Project)
   extends JBPanel(new MigLayout("gap rel 0, ins 0")) {
 
-  locally {
-    val diagramsComponent = new DiagramsComponent(project, ActionPanel.defaultZoom)
+  private val diagramsComponent = new DiagramsComponent(this, project, ActionPanel.defaultZoom)
+  private val diagramsScrollPane = new JBScrollPane(diagramsComponent)
+  diagramsComponent.scrollComponent = diagramsScrollPane
 
-    val diagramsScrollPane = new JBScrollPane(diagramsComponent)
+  locally {
     diagramsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED)
     diagramsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED)
     diagramsScrollPane.setBorder(JBUI.Borders.empty)
     diagramsScrollPane.getViewport.setScrollMode(JViewport.SIMPLE_SCROLL_MODE)
+    diagramsScrollPane.setName("compilation-charts-scroll-pane") // for easier debugging
 
     val actionPanel = new ActionPanel(diagramsComponent.setZoom)
     add(actionPanel, "al right, wrap")
     add(diagramsScrollPane, "grow, push, span")
+  }
+
+  def updateData(): Unit = {
+    diagramsComponent.updateData()
   }
 }
