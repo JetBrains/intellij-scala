7b059bfe8d0fefcf979154431d38add305a1af12
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-11-24 22:23:13 +0300

    Compilation Charts - incremental compilation bug fix #SCL-18485

--- scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/package.scala
@@ -1,10 +1,6 @@
 package org.jetbrains.plugins.scala
 
-import org.jetbrains.plugins.scala.compiler.CompilationUnitId
-
 package object compilationCharts {
-
-  type CompilationProgressState = Map[CompilationUnitId, CompilationProgressInfo]
 
   /**
    * Unix time in nano-seconds.
