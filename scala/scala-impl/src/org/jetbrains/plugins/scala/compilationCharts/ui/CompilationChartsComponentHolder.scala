68a1697bf04a51f1b8cb99edd98611ee71add370
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2020-11-26 19:58:34 +0300

    Update compilation charts on events only. #SCL-17802

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/ui/CompilationChartsComponentHolder.scala
@@ -0,0 +1,24 @@
+package org.jetbrains.plugins.scala.compilationCharts.ui
+
+import com.intellij.openapi.components.{Service, ServiceManager}
+import com.intellij.openapi.project.Project
+
+@Service
+final class CompilationChartsComponentHolder {
+  private var mainComponent: Option[CompilationChartsComponent] = None
+
+  private def createOrGet(project: Project): CompilationChartsComponent = synchronized {
+    mainComponent.getOrElse {
+      val component = new CompilationChartsComponent(project)
+      component.updateData()
+      mainComponent = Some(component)
+      component
+    }
+  }
+}
+
+object CompilationChartsComponentHolder {
+
+  def createOrGet(project: Project): CompilationChartsComponent =
+    ServiceManager.getService(project, classOf[CompilationChartsComponentHolder]).createOrGet(project)
+}
