8764ab0d6798c2a8484783323654525e4c67109c
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-11-26 12:37:13 +0100

    parse match-types with significant indentation
     #SCL-16603

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/types/MatchTypeSuffix.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/types/MatchTypeSuffix.scala
@@ -2,14 +2,15 @@ package org.jetbrains.plugins.scala.lang.parser.parsing.types
 
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.parser.parsing.ParsingRule
 import org.jetbrains.plugins.scala.lang.parser.parsing.builder.ScalaPsiBuilder
-import org.jetbrains.plugins.scala.lang.parser.util.ParserUtils
+import org.jetbrains.plugins.scala.lang.parser.util.{InScala3, ParserUtils}
 
 /**
  * [[MatchTypeSuffix]] ::= [[InfixType]] `match` [[TypeCaseClauses]]
  */
-object MatchTypeSuffix {
-  def parse(builder: ScalaPsiBuilder): Boolean = {
+object MatchTypeSuffix extends ParsingRule {
+  override def apply()(implicit builder: ScalaPsiBuilder): Boolean = {
 
     def parseCases(): Unit =
       if (!TypeCaseClauses.parse(builder))
@@ -21,6 +22,16 @@ object MatchTypeSuffix {
         builder.enableNewlines()
         ParserUtils.parseLoopUntilRBrace(builder, parseCases)
         builder.restoreNewlinesState()
+      case InScala3(ScalaTokenTypes.kCASE) =>
+
+        builder.findPreviousIndent match {
+          case Some(indentationWidth) =>
+            builder.withIndentationWidth(indentationWidth) {
+              TypeCaseClauses()
+            }
+          case None =>
+            builder.error(ScalaBundle.message("expected.case.on.a.new.line"))
+        }
       case _ => builder.error(ScalaBundle.message("match.type.cases.expected"))
     }
     true
