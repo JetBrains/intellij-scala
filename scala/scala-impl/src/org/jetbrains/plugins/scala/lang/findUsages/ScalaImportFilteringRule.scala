a781d7f098c6f1df0e96a56db2b73af69cea5a78
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-01-18 13:55:41 +0100

    fixed npe and deprecation in ScalaImportFilteringRule
     #EA-250598 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/findUsages/ScalaImportFilteringRule.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/findUsages/ScalaImportFilteringRule.scala
@@ -2,22 +2,22 @@ package org.jetbrains.plugins.scala
 package lang
 package findUsages
 
-import com.intellij.psi.PsiElement
-import com.intellij.usages.Usage
 import com.intellij.usages.rules.{ImportFilteringRule, PsiElementUsage}
+import com.intellij.usages.{Usage, UsageTarget}
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportExpr
 
 final class ScalaImportFilteringRule extends ImportFilteringRule {
 
-  override def isVisible(usage: Usage): Boolean = usage match {
+  override def isVisible(usage: Usage, targets: Array[UsageTarget]): Boolean = usage match {
     case p: PsiElementUsage =>
-      val element: PsiElement = p.getElement
-      element.containingScalaFile match {
-        case Some(_) =>
-          val isImport = element.parentsInFile.containsInstanceOf[ScImportExpr]
-          !isImport
-        case None => true
+      p.getElement.nullSafe.forall { element =>
+        element.containingScalaFile match {
+          case Some(_) =>
+            val isImport = element.parentsInFile.containsInstanceOf[ScImportExpr]
+            !isImport
+          case None => true
+        }
       }
     case _ => true
   }
