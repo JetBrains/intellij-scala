0e673de5de4623bb34bc0fbc5fd7819536ded22f
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-01-22 14:24:19 +0100

    Fixed ScalaRedundantCastInspection for literal types
     #SCL-18613

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/cast/ScalaRedundantCastInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/cast/ScalaRedundantCastInspection.scala
@@ -27,7 +27,7 @@ class ScalaRedundantCastInspection extends AbstractInspection(ScalaInspectionBun
         case List(left: ScExpression, ElementText("."), ElementText("asInstanceOf")) =>
           for (actualType <- left.`type`().toOption;
                typeArgument <- call.arguments.headOption;
-               castType <- typeArgument.`type`().toOption if actualType.equiv(castType)) {
+               castType <- typeArgument.`type`().toOption if actualType.widen.equiv(castType)) {
 
             val descriptor = {
               val range = new TextRange(left.getTextLength, call.getTextLength)
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/cast/ScalaRedundantCastInspectionTest.scala
@@ -0,0 +1,31 @@
+package org.jetbrains.plugins.scala.codeInspection.cast
+
+import com.intellij.codeInspection.LocalInspectionTool
+import org.jetbrains.plugins.scala.codeInspection.ScalaQuickFixTestBase
+
+class ScalaRedundantCastInspectionTest extends ScalaQuickFixTestBase {
+  override protected val classOfInspection: Class[_ <: LocalInspectionTool] =
+    classOf[ScalaRedundantCastInspection]
+
+  override protected val description = "Casting '<from>' to '<to>' is redundant"
+
+  override protected def descriptionMatches(s: String): Boolean = s != null && s.startsWith("Casting '")
+
+  def test_int_literal(): Unit = check("3", "Int")
+  def test_string_literal(): Unit = check("\"\"", "String")
+
+  def test_string(): Unit = checkType("String")
+  def test_int(): Unit = checkType("Int")
+  def test_float(): Unit = checkType("Float")
+  def test_Any(): Unit = checkType("Any")
+
+  def checkType(castType: String): Unit = check(s"(??? : $castType)", castType)
+  def check(expr: String, castType: String): Unit = {
+    checkTextHasError(s"val x = $expr$START.asInstanceOf[$castType]$END")
+    testQuickFix(
+      s"val x = $expr.asInstanceOf[$castType]",
+      s"val x = $expr",
+      "Remove Redundant Cast"
+    )
+  }
+}
