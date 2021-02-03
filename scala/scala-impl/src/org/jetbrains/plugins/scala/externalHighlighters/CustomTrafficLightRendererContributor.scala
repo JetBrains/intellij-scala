39e8306283786e2a38054496a9b9b53467de3f5f
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2021-02-03 13:29:41 +0300

    CustomTrafficLightRendererContributor: check if project isn't disposed EA-246923

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CustomTrafficLightRendererContributor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CustomTrafficLightRendererContributor.scala
@@ -27,26 +27,27 @@ class CustomTrafficLightRendererContributor
     val project = editor.getProject
     new TrafficLightRenderer(project, editor.getDocument) {
       override def getDaemonCodeAnalyzerStatus(severityRegistrar: SeverityRegistrar): DaemonCodeAnalyzerStatus = {
-        def isHighlightingCompilerRunning(project: Project): Boolean = try {
+        def isHighlightingCompilerRunning(project: Project): Boolean =
           HighlightingCompiler.get(project).isRunning
-        } catch {
-          case _: AlreadyDisposedException => false
-        }
 
-        val compilerHighlightingInProgress =
-          isHighlightingCompilerRunning(project) && ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project)
-        val status = super.getDaemonCodeAnalyzerStatus(severityRegistrar)
-        status.errorAnalyzingFinished = status.errorAnalyzingFinished && !compilerHighlightingInProgress
-        if (compilerHighlightingInProgress) {
-          val passesField = classOf[DaemonCodeAnalyzerStatus].getDeclaredField("passes")
-          passesField.setAccessible(true)
-          val oldPasses = passesField.get(status).asInstanceOf[JList[ProgressableTextEditorHighlightingPass]]
-          val newPasses = new JArrayList[ProgressableTextEditorHighlightingPass](oldPasses)
-          val progress = CompilerGeneratedStateManager.get(project).progress
-          newPasses.add(new FakeHighlightingPass(editor, file, progress))
-          passesField.set(status, newPasses)
+        if (!project.isDisposed) { // EA-246923
+          val compilerHighlightingInProgress =
+            isHighlightingCompilerRunning(project) && ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project)
+          val status = super.getDaemonCodeAnalyzerStatus(severityRegistrar)
+          status.errorAnalyzingFinished = status.errorAnalyzingFinished && !compilerHighlightingInProgress
+          if (compilerHighlightingInProgress) {
+            val passesField = classOf[DaemonCodeAnalyzerStatus].getDeclaredField("passes")
+            passesField.setAccessible(true)
+            val oldPasses = passesField.get(status).asInstanceOf[JList[ProgressableTextEditorHighlightingPass]]
+            val newPasses = new JArrayList[ProgressableTextEditorHighlightingPass](oldPasses)
+            val progress = CompilerGeneratedStateManager.get(project).progress
+            newPasses.add(new FakeHighlightingPass(editor, file, progress))
+            passesField.set(status, newPasses)
+          }
+          status
+        } else {
+          new DaemonCodeAnalyzerStatus
         }
-        status
       }
 
       override def createUIController(): UIController = super.createUIController(editor)
