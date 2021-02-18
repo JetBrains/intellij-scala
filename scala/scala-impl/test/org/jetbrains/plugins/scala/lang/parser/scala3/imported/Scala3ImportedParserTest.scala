d117475cd821a74e1a40f5a4f992ca46482248b6
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-02-18 15:48:21 +0100

    started fixing parsing of sequence pattern in scala3
     #SCL-16166

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/imported/Scala3ImportedParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/imported/Scala3ImportedParserTest.scala
@@ -138,7 +138,7 @@ object Scala3ImportedParserTest {
  * run [[Scala3ImportedParserTest_Move_Fixed_Tests]].
  */
 class Scala3ImportedParserTest_Fail extends Scala3ImportedParserTestBase(Scala3ImportedParserTest_Fail.directory) {
-  override protected def shouldHaveErrors: Boolean = false
+  override protected def shouldHaveErrors: Boolean = true
 }
 
 object Scala3ImportedParserTest_Fail {
