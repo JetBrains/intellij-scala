e2a67ffd1dba8b4e2d120479de0cba084c4ccffa
andrey.sugak <andrey.sugak@jetbrains.com>
2021-03-02 02:29:23 +0300

    Simplify psi for enums, add extractors

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScEnumCase.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScEnumCase.scala
@@ -4,11 +4,20 @@ package psi
 package api
 package statements
 
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScConstructorOwner, ScEnum}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScTypeParam
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScConstructorOwner, ScEnum, ScMember, ScObject}
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
 
 trait ScEnumCase extends ScConstructorOwner {
-  def enumParent: Option[ScEnum]
+  def enumParent: ScEnum
+
+  /**
+   * Returns type parameters from an explicit type parameter clause only,
+   * as opposed to [[typeParameters]], which will also return
+   * type params implicitly inherited from parent enum class.
+   */
+  def physicalTypeParameters: Seq[ScTypeParam]
 }
 
 object ScEnumCase {
@@ -16,4 +25,24 @@ object ScEnumCase {
     def unapply(cse: ScEnumCase): Option[(String, Seq[ScType])] =
       Option.when(cse.constructor.isEmpty)((cse.name, cse.superTypes))
   }
+
+  object DesugaredEnumCase {
+    def unapply(e: ScNamedElement): Option[(String, ScEnum)] =
+      ScalaPsiUtil.nameContext(e) match {
+        case member: ScMember =>
+          member.syntheticNavigationElement match {
+            case null => None
+            case obj: ScObject =>
+              obj.baseCompanion match {
+                case Some(enum: ScEnum) => Option((e.getName, enum))
+                case _                  => None
+              }
+            case _ => None
+          }
+        case _ => None
+      }
+  }
+
+  def isDesugaredEnumCase(cls: ScNamedElement): Boolean =
+    DesugaredEnumCase.unapply(cls).isDefined
 }
