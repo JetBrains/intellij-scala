2054b33907205e753af3704a805c3e7bb5a2b921
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2021-03-16 12:54:06 +0300

    SCL-CR-1681: enable ml completion by default

--- scala/integration/ml-completion/src/org/jetbrains/plugins/scala/mlCompletion/ScalaMlRankingProvider.scala
+++ scala/integration/ml-completion/src/org/jetbrains/plugins/scala/mlCompletion/ScalaMlRankingProvider.scala
@@ -6,4 +6,6 @@ import org.jetbrains.plugins.scala.ScalaLanguage
 
 class ScalaMlRankingProvider extends CatBoostJarCompletionModelProvider("Scala", "scala_features", "scala_model") {
   override def isLanguageSupported(language: Language): Boolean = language == ScalaLanguage.INSTANCE
+
+  override def isEnabledByDefault: Boolean = true
 }
