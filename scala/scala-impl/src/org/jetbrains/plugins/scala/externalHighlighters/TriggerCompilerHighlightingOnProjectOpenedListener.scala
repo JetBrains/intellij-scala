3bfce7de534c00060b2e8e42c190453cd20682bf
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2021-03-19 18:36:13 +0300

    TriggerCompilerHighlightingOnProjectOpenedListener: check if it's a light edit mode #SCL-18801

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/TriggerCompilerHighlightingOnProjectOpenedListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/TriggerCompilerHighlightingOnProjectOpenedListener.scala
@@ -1,15 +1,17 @@
 package org.jetbrains.plugins.scala.externalHighlighters
 
+import com.intellij.ide.lightEdit.LightEdit
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.{DumbService, Project, ProjectManagerListener}
 
 class TriggerCompilerHighlightingOnProjectOpenedListener
   extends ProjectManagerListener {
   
-  override def projectOpened(project: Project): Unit = if (!ApplicationManager.getApplication.isUnitTestMode) {
-    DumbService.getInstance(project).runWhenSmart { () =>
-      if (ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project))
-        CompilerHighlightingService.get(project).triggerIncrementalCompilation(delayedProgressShow = false)
+  override def projectOpened(project: Project): Unit =
+    if (!ApplicationManager.getApplication.isUnitTestMode && !LightEdit.owns(project)) {
+      DumbService.getInstance(project).runWhenSmart { () =>
+        if (ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project))
+          CompilerHighlightingService.get(project).triggerIncrementalCompilation(delayedProgressShow = false)
+      }
     }
-  }
 }
