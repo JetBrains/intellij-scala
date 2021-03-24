d4b09bf4c4bc97c1c5343774e63d800631935d96
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-03-24 20:48:08 +0100

    TASTy: v3.0.0-RC1
    (cherry picked from commit 7f40e531118485c5f612c3f28440c283f1bdd922)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -25,7 +25,7 @@ object Versions {
     val binary_2_13 = "2.13"
 
     val latest_dotty = "0.27.0-RC1"
-    val latest_scala3 = "3.0.0-M2"
+    val latest_scala3 = "3.0.0-RC1"
 
     def binaryVersion(v: String): String =
       if (v.startsWith("2.9")) binary_2_9
