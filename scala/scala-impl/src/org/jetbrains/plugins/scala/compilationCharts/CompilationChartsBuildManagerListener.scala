4bea0f50324be425db08271788f9b5f57d00d6c8
Artyom Semyonov <artyom.semyonov@jetbrains.com>
2021-04-14 15:00:55 +0300

    CompilationChartsBuildManagerListener: exception fix EA-263973

--- scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/CompilationChartsBuildManagerListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/compilationCharts/CompilationChartsBuildManagerListener.scala
@@ -2,7 +2,8 @@ package org.jetbrains.plugins.scala.compilationCharts
 
 import java.util.UUID
 import com.intellij.compiler.server.BuildManagerListener
-import com.intellij.openapi.components.{Service, ServiceManager}
+import com.intellij.openapi.compiler.{CompileContext, CompileTask}
+import com.intellij.openapi.components.{ComponentManager, Service, ServiceManager}
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.compilationCharts.ui.CompilationChartsComponentHolder
 import org.jetbrains.plugins.scala.compiler.CompileServerClient
@@ -11,15 +12,24 @@ import org.jetbrains.plugins.scala.util.ScheduledService
 import scala.concurrent.duration.DurationInt
 
 class CompilationChartsBuildManagerListener
-  extends BuildManagerListener {
+  extends BuildManagerListener
+    with CompileTask {
 
-  override def buildStarted(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = {
+  // It runs BEFORE compilation.
+  // We use it instead of buildStarted or beforeBuildProcessStarted methods to avoid an exception (EA-263973).
+  // Also it's an optimization. We need to schedule updates only for compilation not for UP_TO_DATE_CHECK.
+  override def execute(compileContext: CompileContext): Boolean = {
+    val project = compileContext.getProject
     CompilationProgressStateManager.erase(project)
     CompileServerMetricsStateManager.reset(project)
 
     val updater = CompilationChartsUpdater.get(project)
     updater.stopScheduling()
     updater.startScheduling()
+    true
+  }
+
+  override def buildStarted(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = {
   }
 
   override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit =
