fcc3c0b0a4d1a739a4a2c52c2a6c0885d56a895c
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2021-04-14 18:16:52 +0300

    change test base class
    annotator inspection based tests doesn't detect this problem for some reason

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/HigherKindedTypesTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/HigherKindedTypesTest.scala
@@ -7,29 +7,9 @@ import org.jetbrains.plugins.scala.codeInspection.internal.AnnotatorBasedErrorIn
 /**
   * Created by Roman.Shein on 02.09.2016.
   */
-class HigherKindedTypesTest extends ScalaInspectionTestBase {
+class HigherKindedTypesTest extends FailableResolveTest("higherKinded") {
 
   override protected def shouldPass: Boolean = false
 
-  override protected val classOfInspection: Class[_ <: LocalInspectionTool] =
-    classOf[AnnotatorBasedErrorInspection]
-
-  override protected val description: String = "Cannot resolve symbol hello"
-
-  def testSCL12929(): Unit = {
-    checkTextHasNoErrors(
-      s"""
-         |trait A {
-         |  val x: Int
-         |}
-         |
-         |trait B {
-         |  val y: Int
-         |}
-         |
-         |class C[T[_] <: A with B](a: T[Int]){
-         |  val b = a.x
-         |}
-      """.stripMargin)
-  }
+  def testSCL12929(): Unit = doTest()
 }
