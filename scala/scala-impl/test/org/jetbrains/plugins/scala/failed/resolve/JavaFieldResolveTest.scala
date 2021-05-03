eb2200c8244097ad7901b30caf0388c6085b1f7b
Justin Kaeser <justin.kaeser@jetbrains.com>
2021-05-03 16:11:50 +0200

    maybe fix failing JavaFieldResolveTest due to field/filename mismatch

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/JavaFieldResolveTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/JavaFieldResolveTest.scala
@@ -4,7 +4,7 @@ package org.jetbrains.plugins.scala.failed.resolve
   * @author Nikolay.Tropin
   */
 class JavaFieldResolveTest extends FailableResolveTest("javaField") {
-  def testScl6925(): Unit = doTest()
-  def testScl12413(): Unit = doTest()
-  def testScl12630(): Unit = doTest()
+  def testSCL6925(): Unit = doTest()
+  def testSCL12413(): Unit = doTest()
+  def testSCL12630(): Unit = doTest()
 }
