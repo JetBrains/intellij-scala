64745883984f2eaeff575b90f6331dd5bf150299
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-05-05 14:36:13 +0200

    fixed ExpandPatternIntention
     #SCL-18989 fixed

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInsight/intention/matcher/ExpandPatternIntentionTest.scala
@@ -0,0 +1,49 @@
+package org.jetbrains.plugins.scala.codeInsight.intention.matcher
+
+import org.jetbrains.plugins.scala.codeInsight.intentions.ScalaIntentionTestBase
+
+class ExpandPatternIntentionTest extends ScalaIntentionTestBase {
+
+  override def familyName: String = "Expand to Constructor pattern"
+
+  def test_example(): Unit = doTest(
+    text =
+      s"""
+         |case class Person(name: String, age: Int)
+         |
+         |p match {
+         |  case Seq(perso${CARET}n: Person) => "a person"
+         |}
+         |
+         |""".stripMargin,
+    resultText =
+      s"""
+         |case class Person(name: String, age: Int)
+         |
+         |p match {
+         |  case Seq(perso${CARET}n@Person(name, age)) => "a person"
+         |}
+         |""".stripMargin
+  )
+
+
+  def test_wildcard(): Unit = doTest(
+    text =
+      s"""
+         |case class Person(name: String, age: Int)
+         |
+         |p match {
+         |  case Seq(_: Per${CARET}son) => "a person"
+         |}
+         |
+         |""".stripMargin,
+    resultText =
+      s"""
+         |case class Person(name: String, age: Int)
+         |
+         |p match {
+         |  case Seq(Person(name, age)) => "a person"
+         |}
+         |""".stripMargin
+  )
+}
