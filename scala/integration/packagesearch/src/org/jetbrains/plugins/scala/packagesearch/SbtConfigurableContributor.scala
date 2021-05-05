5db6a1a8a20a9cc5216d86e30c15d27ea67b5863
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2021-05-05 16:53:45 +0200

    Added Config Section of packagesearch for Sbt.

--- scala/integration/packagesearch/src/org/jetbrains/plugins/scala/packagesearch/SbtConfigurableContributor.scala
+++ scala/integration/packagesearch/src/org/jetbrains/plugins/scala/packagesearch/SbtConfigurableContributor.scala
@@ -1,7 +1,54 @@
 package org.jetbrains.plugins.scala.packagesearch
 
+import com.intellij.openapi.project.Project
+import com.intellij.ui.{DocumentAdapter, RelativeFont, TitledSeparator}
+import com.intellij.util.ui.FormBuilder
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.{ConfigurableContributor, ConfigurableContributorDriver}
 
-class SbtConfigurableContributor extends ConfigurableContributor {
-  override def createDriver(): ConfigurableContributorDriver = ???
+import javax.swing.{JLabel, JTextField}
+import javax.swing.event.DocumentEvent
+import org.jetbrains.plugins.scala.packagesearch.configuration.{PackageSearchSbtConfiguration, packageSearchSbtConfigurationForProject}
+
+class SbtConfigurableContributor(project: Project) extends ConfigurableContributor {
+  override def createDriver(): ConfigurableContributorDriver = new SbtConfigurableContributorDriver(project)
+}
+
+class SbtConfigurableContributorDriver(project: Project) extends ConfigurableContributorDriver {
+  var modified: Boolean = false
+  var configuration: PackageSearchSbtConfiguration = packageSearchSbtConfigurationForProject.getService(project)
+  val textFieldChangeListener: DocumentAdapter = new DocumentAdapter() {
+    override protected def textChanged(e: DocumentEvent): Unit = {
+      modified = true
+    }
+  }
+
+  val sbtScopeEditor: JTextField = new JTextField() {
+    getDocument.addDocumentListener(textFieldChangeListener)
+  }
+
+  override def apply(): Unit = configuration.defaultSbtScope = sbtScopeEditor.getText
+
+  override def contributeUserInterface(formBuilder: FormBuilder): Unit = {
+    formBuilder.addComponent(
+      new TitledSeparator(PackageSearchSbtBundle.message("packagesearch.configuration.sbt.title")),
+      0
+    )
+
+    formBuilder.addLabeledComponent(PackageSearchSbtBundle.message("packagesearch.configuration.sbt.scopes.default"), sbtScopeEditor)
+
+    val label = new JLabel(s"${PackageSearchSbtBundle.message("packagesearch.configuration.sbt.scopes")} ${SbtCommon.libScopes.replace(",", ", ")}")
+    formBuilder.addComponentToRightColumn(RelativeFont.TINY.install(RelativeFont.ITALIC.install(label)))
+  }
+
+  override def isModified: Boolean = modified
+
+  override def reset(): Unit = {
+    sbtScopeEditor.setText(configuration.determineDefaultSbtConfiguration)
+    modified = false
+  }
+
+  override def restoreDefaults(): Unit = {
+    sbtScopeEditor.setText(SbtCommon.defaultLibScope)
+    modified = true
+  }
 }
---
+++ scala/integration/packagesearch/src/org/jetbrains/plugins/scala/packagesearch/configuration/PackageSearchSbtConfiguration.scala
@@ -0,0 +1,31 @@
+package org.jetbrains.plugins.scala.packagesearch.configuration
+
+import com.intellij.openapi.components.{BaseState, PersistentStateComponent, State, Storage}
+import com.intellij.openapi.project.Project
+import com.intellij.util.xmlb.annotations.OptionTag
+import org.jetbrains.plugins.scala.packagesearch.SbtCommon
+import com.jetbrains.packagesearch.intellij.plugin.configuration.PackageSearchGeneralConfiguration
+
+@State(
+  name = "PackageSearchMavenConfiguration",
+  storages = Array(new Storage(PackageSearchGeneralConfiguration.StorageFileName))
+)
+class PackageSearchSbtConfiguration extends BaseState with PersistentStateComponent[PackageSearchSbtConfiguration] {
+  override def getState: PackageSearchSbtConfiguration = this
+
+  override def loadState(state: PackageSearchSbtConfiguration): Unit = this.copyFrom(state)
+
+  @OptionTag("SBT_SCOPES_DEFAULT")
+  var defaultSbtScope: String = SbtCommon.defaultLibScope
+
+  def determineDefaultSbtConfiguration: String = {
+    if (defaultSbtScope != null && defaultSbtScope.nonEmpty) defaultSbtScope
+    else SbtCommon.defaultLibScope
+  }
+
+  def getSbtConfigurations: Array[String] = SbtCommon.libScopes.split(',').map(_.trim).filter(_.nonEmpty)
+}
+
+object packageSearchSbtConfigurationForProject {
+  def getService(project: Project): PackageSearchSbtConfiguration = project.getService(classOf[PackageSearchSbtConfiguration])
+}
