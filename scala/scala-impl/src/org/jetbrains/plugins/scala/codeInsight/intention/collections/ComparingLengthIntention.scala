b54126328a002f0ad42a061f82b176da4cb63c82
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-05-05 16:21:37 +0200

    make ComparingLengthIntention use .sizeIs in <= 2.12
     #SCL-18986 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/intention/collections/ComparingLengthIntention.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/intention/collections/ComparingLengthIntention.scala
@@ -4,9 +4,5 @@ import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.codeInsight.intention.InspectionBasedIntention
 import org.jetbrains.plugins.scala.codeInspection.collections.ComparingLengthInspection
 
-/**
-  * Nikolay.Tropin
-  * 18-Jan-18
-  */
 class ComparingLengthIntention
-  extends InspectionBasedIntention(ScalaBundle.message("family.name.comparing.length"), ComparingLengthInspection.hint, new ComparingLengthInspection)
+  extends InspectionBasedIntention(ScalaBundle.message("family.name.comparing.length"), new ComparingLengthInspection)
--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/ComparingLengthInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/collections/ComparingLengthInspection.scala
@@ -4,23 +4,21 @@ package collections
 
 import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScIntegerLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import org.jetbrains.plugins.scala.project.ScalaLanguageLevel
 
 import scala.collection.immutable.ArraySeq
 
-/**
-  * @author t-kameyama
-  */
 final class ComparingLengthInspection extends OperationOnCollectionInspection {
-
   override def possibleSimplificationTypes: ArraySeq[SimplificationType] = ArraySeq(ComparingLengthInspection.ComparingLength)
 }
 
 object ComparingLengthInspection {
 
-  val hint: String = ScalaInspectionBundle.message("replace.with.lengthCompare")
+  val hintLengthCompare: String = ScalaInspectionBundle.message("replace.with.lengthCompare")
+  val hintSizeIs: String = ScalaInspectionBundle.message("replace.with.sizeIs")
 
   private val ComparingLength: SimplificationType = new SimplificationType() {
-    override def hint: String = ComparingLengthInspection.hint
+    override def hint: String = null
 
     override def getSimplification(e: ScExpression): Option[Simplification] = Some(e).collect {
       // TODO infix notation?
@@ -30,15 +28,23 @@ object ComparingLengthInspection {
       case `.sizeOrLength`(q) `!=` n => (q, "!=", n)
       case `.sizeOrLength`(q) `<` n => (q, "<", n)
       case `.sizeOrLength`(q) `<=` n => (q, "<=", n)
-    } filter { case (q, _, n) =>
+    }.filter { case (q, _, n) =>
       isNonIndexedSeq(q) && !isZero(n)
-    } map { case (q, op, n) =>
-      replace(e).withText(s"${invocationText(q, "lengthCompare", n)} $op 0").highlightFrom(q)
+    }.map { case (q, op, n) =>
+      val repl = replace(e).highlightFrom(q)
+      if (doesScalaHasSizeIs(e)) repl.withText(s"${invocationText(q, "sizeIs")} $op ${n.getText}").withHint(hintSizeIs)
+      else repl.withText(s"${invocationText(q, "lengthCompare", n)} $op 0").withHint(hintLengthCompare)
     }
   }
 
   private def isZero(expression: ScExpression): Boolean = expression match {
     case ScIntegerLiteral(0) => true
     case _ => false
+  }
+
+  private def doesScalaHasSizeIs(e: ScExpression): Boolean = {
+    println(e)
+    e.scalaLanguageLevel
+      .forall(_ >= ScalaLanguageLevel.Scala_2_13)
   }
 }
