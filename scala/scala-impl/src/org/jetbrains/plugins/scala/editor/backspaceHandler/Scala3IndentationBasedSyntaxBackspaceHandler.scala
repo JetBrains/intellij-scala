a39662da0588b4d6a4881f25fd9fa2c5612c0dce
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-05-07 15:38:40 +0300

    scala 3 editor: handle Backspace in indentation-based syntax #SCL-18977 fixed

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/editor/backspaceHandler/Scala3IndentationBasedSyntaxBackspaceHandler.scala
@@ -0,0 +1,240 @@
+package org.jetbrains.plugins.scala.editor.backspaceHandler
+
+import com.intellij.application.options.CodeStyle
+import com.intellij.codeInsight.CodeInsightSettings
+import com.intellij.codeInsight.editorActions.{BackspaceHandler, BackspaceHandlerDelegate, _}
+import com.intellij.injected.editor.EditorWindow
+import com.intellij.lang.Language
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.{CaretModel, Document, Editor}
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings.IndentOptions
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.{PsiFile, PsiWhiteSpace}
+import com.intellij.util.text.CharArrayUtil
+import org.jetbrains.plugins.scala.Scala3Language
+import org.jetbrains.plugins.scala.editor.backspaceHandler.Scala3IndentationBasedSyntaxBackspaceHandler._
+import org.jetbrains.plugins.scala.editor.enterHandler.Scala3IndentationBasedSyntaxEnterHandler
+import org.jetbrains.plugins.scala.editor.{ScalaEditorUtils, useIndentationBasedSyntax}
+import org.jetbrains.plugins.scala.extensions.{ObjectExt, PsiFileExt}
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScCaseClauses
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMatch
+
+/**
+ * There are also two default indenting backspace handlers:
+ *  - [[SimpleIndentingBackspaceHandler]]
+ *  - [[SmartIndentingBackspaceHandler]]
+ *
+ * They provide quite poor customization of how to handle backspace in indentation whitespace:<br>
+ * you just need to by implement custom [[BackspaceModeOverride]].<br>
+ * We do not use it because there are several issues with it:
+ *  1. It's language-based.<br>
+ *     Default handlers calculate the language based on the psi element at caret position
+ *     (see [[BackspaceHandler.getLanguageAtCursorPosition]])<br>
+ *     Unfortunately Scala 3 language detection is not reliable for non-file psi elements (see SCL-17237).
+ *     (simply delegating to `getContainingFile.getLanguage` for every element seems to be too much.
+ *  1. the logic of default handlers is quite primitive and you can't customize it to handle Scala3 indentation-based syntax
+ *     in a more sophisticated way
+ */
+//noinspection InstanceOf
+class Scala3IndentationBasedSyntaxBackspaceHandler extends BackspaceHandlerDelegate {
+
+  private var myEnabled: Boolean = false
+
+  //noinspection ConvertNullInitializerToUnderscore
+  private var myReplacement: String = null
+  private var myStartOffset: Int = 0
+
+  override def beforeCharDeleted(c: Char, file: PsiFile, editor: Editor): Unit = {
+    myEnabled = false
+
+    val scalaFile = file match {
+      case sf: ScalaFile => sf
+      case _ => return
+    }
+    if (!scalaFile.isScala3File)
+      return
+
+    if (!useIndentationBasedSyntax(scalaFile))
+      return
+
+    val mode = getBackspaceMode(Scala3Language.INSTANCE)
+    if (mode != SmartBackspaceMode.AUTOINDENT)
+      return
+
+    val caretModel = editor.getCaretModel
+    val caretOffset = caretModel.getOffset
+    val document = editor.getDocument
+    val documentText = document.getCharsSequence
+
+    val enable = doBeforeCharDeletedAutoindent(caretOffset, caretModel, documentText, file, editor, document)
+    myEnabled = enable
+  }
+
+  /**
+   * Most of the code is copied from [[SmartIndentingBackspaceHandler]]<br>
+   * with some special handling for Scala3 indentation-based syntax
+   */
+  private def doBeforeCharDeletedAutoindent(
+    caretOffset: Int,
+    caretModel: CaretModel,
+    documentText: CharSequence,
+    file: PsiFile,
+    editor: Editor,
+    document: Document
+  ): Boolean = {
+    val pos = caretModel.getLogicalPosition
+    val lineStartOffset = document.getLineStartOffset(pos.line)
+    val beforeWhitespaceOffset = CharArrayUtil.shiftBackward(documentText, caretOffset - 1, SpaceOrTab) + 1
+
+    val caretIsInsideIndentWhitespace = beforeWhitespaceOffset != lineStartOffset
+    if (caretIsInsideIndentWhitespace) {
+      myReplacement = null
+      return false
+    }
+
+    val indentOptions = CodeStyle.getIndentOptions(file)
+    val indentedElementIndentSize = findElementInIndentationContextOnPrevLine(caretOffset, documentText, file, indentOptions, lineStartOffset)
+
+    /** NOTE: this is the main change comparing to [[SmartIndentingBackspaceHandler]] */
+    myReplacement = indentedElementIndentSize match {
+      case Some(elementIndentSize) =>
+        StringUtil.repeatSymbol(' ', elementIndentSize) // TODO: handle TABS
+      case None        =>
+        CodeStyle.getLineIndent(editor, file.getLanguage, lineStartOffset, true)
+    }
+    if (myReplacement == null)
+      return false
+
+    val targetColumn = getWidth(myReplacement, indentOptions.TAB_SIZE)
+    val endOffset = CharArrayUtil.shiftForward(documentText, caretOffset, SpaceOrTab)
+    // NOTE: extra check for `\n` to workaround IDEA-268212
+    val logicalPosition =
+      if (caretOffset < endOffset && documentText.charAt(endOffset) != '\n') editor.offsetToLogicalPosition(endOffset)
+      else pos
+    val currentColumn = logicalPosition.column
+
+    if (currentColumn > targetColumn) {
+      myStartOffset = lineStartOffset
+    }
+    else if (logicalPosition.line == 0) {
+      myStartOffset = 0
+      myReplacement = ""
+    }
+    else {
+      val prevLineEndOffset = document.getLineEndOffset(logicalPosition.line - 1)
+      myStartOffset = CharArrayUtil.shiftBackward(documentText, prevLineEndOffset - 1, SpaceOrTab) + 1
+      if (myStartOffset != document.getLineStartOffset(logicalPosition.line - 1) || myStartOffset == 0) {
+        var spacing = CodeStyle.getJoinedLinesSpacing(editor, file.getLanguage, endOffset, true)
+        if (spacing < 0) {
+          Log.error("The call `codeStyleFacade.getJoinedLinesSpacing` should not return the negative value")
+          spacing = 0
+        }
+        myReplacement = StringUtil.repeatSymbol(' ', spacing)
+      }
+    }
+    true
+  }
+
+  /** NOTE: the method copied without changes from SmartIndentingBackspaceHandler.doCharDeleted */
+  override def charDeleted(c: Char, file: PsiFile, editor: Editor): Boolean = {
+    if (!myEnabled)
+      return false
+
+    val document = editor.getDocument
+    val caretModel = editor.getCaretModel
+    val endOffset = CharArrayUtil.shiftForward(document.getImmutableCharSequence, caretModel.getOffset, SpaceOrTab)
+
+    if (editor.isInstanceOf[EditorWindow]) {
+      val ranges = InjectedLanguageManager.getInstance(file.getProject).intersectWithAllEditableFragments(file, new TextRange(myStartOffset, endOffset))
+      if (ranges.size != 1 || !ranges.get(0).equalsToRange(myStartOffset, endOffset))
+        return false
+    }
+
+    document.replaceString(myStartOffset, endOffset, myReplacement)
+    caretModel.moveToOffset(myStartOffset + myReplacement.length)
+    true
+  }
+}
+
+object Scala3IndentationBasedSyntaxBackspaceHandler {
+
+  private val Log = Logger.getInstance(classOf[Scala3IndentationBasedSyntaxBackspaceHandler])
+
+  private val SpaceOrTab = " \t"
+
+  /** copied from [[SmartIndentingBackspaceHandler.getWidth]] */
+  private def getWidth(indent: String, tabSize: Int) = {
+    var width = 0
+    var idx = 0
+    while (idx < indent.length) {
+      val c = indent.charAt(idx)
+      c match {
+        case '\t' => width = tabSize * (width / tabSize + 1)
+        case ' '  => width += 1
+        case _    => Log.error("Unexpected whitespace character: " + c.toInt)
+      }
+      idx += 1
+    }
+    width
+  }
+
+  /** copied from [[AbstractIndentingBackspaceHandler.getBackspaceMode]] */
+  //noinspection SameParameterValue
+  private def getBackspaceMode(language: Language) = {
+    var mode = CodeInsightSettings.getInstance.getBackspaceMode
+    val overrider = LanguageBackspaceModeOverride.INSTANCE.forLanguage(language)
+    if (overrider != null)
+      mode = overrider.getBackspaceMode(mode)
+    mode
+  }
+
+  private def findElementInIndentationContextOnPrevLine(
+    caretOffset: Int,
+    documentText: CharSequence,
+    file: PsiFile,
+    indentOptions: IndentOptions,
+    lineStartOffset: Int
+  ): Option[Int] = {
+    val elementAtCaret0 = ScalaEditorUtils.findElementAtCaret_WithFixedEOF(file, documentText.length, caretOffset)
+    val elementAtCaret = elementAtCaret0 match {
+      case null              => null
+      case ws: PsiWhiteSpace => ws
+      case nonWs             => PsiTreeUtil.prevLeaf(nonWs)
+    }
+    if (elementAtCaret == null)
+      return None
+
+    val previousLineIsBlank = {
+      // -2 = -1 (for new line) -1 (for prev whitespace)
+      val prevLineBeforeWsOffset = CharArrayUtil.shiftBackward(documentText, lineStartOffset - 2, SpaceOrTab)
+      prevLineBeforeWsOffset > 0 && {
+        val ch = documentText.charAt(prevLineBeforeWsOffset)
+        ch == '\n'
+      }
+    }
+
+    // NOTE: If previous line is blank we always unindent caret,
+    // and do not check if previous element is in an indented region
+    // Exception is when the caret is in a whitespace between case clauses TODO: update this part of comment
+    //
+    // `caretOffset - 1` to skip latest indented element (if the caret is at the same indentation level with it)
+    val searchFromOffset = if (!previousLineIsBlank)
+      caretOffset
+    else if (elementAtCaret.getParent.is[ScMatch, ScCaseClauses])
+      caretOffset
+    else {
+      // if the caret is in the beginning of the line, we do not want to search the indented element from the previous line
+      (caretOffset - 1).max(lineStartOffset)
+    }
+
+    // TODO: move the method from Scala3IndentationBasedSyntaxEnterHandler to some utility in `editor` package
+    import Scala3IndentationBasedSyntaxEnterHandler.previousElementInIndentationContext
+    CodeStyle.getIndentOptions(file)
+    val result = previousElementInIndentationContext(elementAtCaret, searchFromOffset, documentText, indentOptions)
+    result.map(_._2)
+  }
+}
---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/backspace/Scala3IndentationBasedSyntaxBackspaceTest.scala
@@ -0,0 +1,647 @@
+package org.jetbrains.plugins.scala.lang.actions.editor.backspace
+
+import com.intellij.codeInsight.CodeInsightSettings
+import com.intellij.codeInsight.editorActions.SmartBackspaceMode
+import org.jetbrains.plugins.scala.ScalaVersion
+import org.jetbrains.plugins.scala.compiler.ScalaCompileServerSettings
+import org.jetbrains.plugins.scala.lang.actions.editor.enter.scala3.Scala3TestDataBracelessCode.WrapperCodeContexts
+
+class Scala3IndentationBasedSyntaxBackspaceTest extends ScalaBackspaceHandlerBaseTest {
+
+  override protected def supportedIn(version: ScalaVersion): Boolean =
+    version >= ScalaVersion.Latest.Scala_3_0
+
+  // copied from Scala3BracelessSyntaxEnterExhaustiveTest
+  override def setUp(): Unit = {
+    super.setUp()
+    // indirect way of disabling compiler-based highlighting which is triggered on each editor changes
+    // see org.jetbrains.plugins.scala.externalHighlighters.TriggerCompilerHighlightingService.condition
+    ScalaCompileServerSettings.getInstance.COMPILE_SERVER_ENABLED = false
+    getScalaSettings.USE_SCALA3_INDENTATION_BASED_SYNTAX = true
+  }
+
+  def testMethodBody_Simple_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  111#
+        |  #    #
+        |""".stripMargin
+    )
+
+  def testMethodBody_MultipleExpressions_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  111
+        |  222
+        |  333#
+        |  #    #
+        |""".stripMargin
+    )
+
+  def testMethodBody_MultipleExpressions_BlankLinesWithSpacesBeforeCaret(): Unit =
+    doSequentialBackspaceTest_InContexts(WrapperContextsWithJumpToPreviousLine)(
+      s"""def f1 =
+         |  111
+         |  222
+         |  333#
+         |#    ${""}
+         |#    ${""}
+         |#    ${""}
+         |# #    #
+         |""".stripMargin
+    )
+
+  def testNestedMethodBody_1_Simple_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  def f2 =
+        |    def f3 =
+        |      "f3"#
+        |      #    #
+        |    "f2" + f3
+        |  "f1" + f2""".stripMargin
+    )
+
+  def testNestedMethodBody_1_MultipleExpressions_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  def f2 =
+        |    def f3 =
+        |      111
+        |      222
+        |      "f3"#
+        |      #    #
+        |    "f2" + f3
+        |  "f1" + f2""".stripMargin
+    )
+
+  def testNestedMethodBody_2_Simple_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  def f2 =
+        |    def f3 =
+        |      "f3"#
+        |      #    #
+        |    "f2" + f3
+        |  "f1" + f2""".stripMargin
+    )
+
+  def testNestedMethodBody_2_MultipleExpressions_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  def f2 =
+        |    def f3 =
+        |      111
+        |      222
+        |      "f3"#
+        |      #    #
+        |""".stripMargin
+    )
+
+  def testNestedMethod_BlankLineBeforeCaret_1_F1WithEndMarker_ClassWithEndMarker(): Unit =
+    doSequentialBackspaceTest(
+      s"""class A:
+         |  def f1 =
+         |    def f2 =
+         |      def f3 =
+         |        "f3"#
+         |    #
+         |    # ##
+         |  end f1
+         |end A""".stripMargin
+    )
+
+  def testNestedMethod_BlankLineBeforeCaret_2_F2WithoutEndMarker_ClassWithEndMarker(): Unit =
+    doSequentialBackspaceTest(
+      s"""class A:
+         |  def f1 =
+         |    def f2 =
+         |      def f3 =
+         |        "f3"#
+         |  #
+         |  # # ##
+         |end A""".stripMargin
+    )
+
+  def testNestedMethod_BlankLineBeforeCaret_3_F2WithoutEndMarker_ClassWithoutEndMarker(): Unit =
+    doSequentialBackspaceTest(
+      s"""class A:
+         |  def f1 =
+         |    def f2 =
+         |      def f3 =
+         |        "f3"#
+         |#
+         |# # # ##
+         |""".stripMargin
+    )
+
+  def testShouldRespectBackspaceModeSetting(): Unit = {
+    val settings = CodeInsightSettings.getInstance
+    val backspaceModeBefore = settings.getBackspaceMode
+
+    try {
+      settings.setBackspaceMode(SmartBackspaceMode.INDENT)
+      doSequentialBackspaceTest(
+        """def f1 =
+          |  def f2 =
+          |    def f3 =
+          |      "f3"
+          |# # # # # ##
+          |    "f2" + f3
+          |  "f1" + f2""".stripMargin
+      )
+    } finally {
+      settings.setBackspaceMode(backspaceModeBefore)
+    }
+  }
+
+  def testExample1_NestedMethod_TailPosition(): Unit =
+    doSequentialBackspaceTest(
+      """class A {
+        |  def foo =
+        |    println(1)
+        |    println(2)
+        |
+        |  def fooOuter =
+        |    println(1)
+        |    println(2)
+        |
+        |    def fooInner =
+        |      println(1)
+        |      println(2)#
+        |      #    #
+        |}
+        |""".stripMargin
+    )
+
+  def testExample1_NestedMethod_TailPosition_CaretUnindented(): Unit =
+    doSequentialBackspaceTest(
+      """class A {
+        |  def foo =
+        |    println(1)
+        |    println(2)
+        |
+        |  def fooOuter =
+        |    println(1)
+        |    println(2)
+        |
+        |    def fooInner =
+        |      println(1)
+        |      println(2)#
+        |    ##
+        |}
+        |""".stripMargin
+    )
+
+  def testExample1_NestedMethod_BlankLineBeforeCaret(): Unit =
+    doSequentialBackspaceTest(
+      """class A {
+        |  def foo =
+        |    println(1)
+        |    println(2)
+        |
+        |  def fooOuter =
+        |    println(1)
+        |    println(2)
+        |
+        |    def fooInner =
+        |      println(1)
+        |      println(2)#
+        |  #
+        |  # # #    #
+        |}
+        |""".stripMargin
+    )
+
+  def testExample2_NestedMethod_TailPosition(): Unit =
+    doSequentialBackspaceTest(
+      """class A {
+        |
+        |  def foo =
+        |    println(1)
+        |    println(2)
+        |
+        |  def fooOuter =
+        |    println(1)
+        |    println(2)#
+        |    #   #
+        |
+        |    def fooInner =
+        |      println(1)
+        |      println(2)
+        |}
+        |""".stripMargin
+    )
+
+  def testExample2_NestedMethod_BlankLineBeforeCaret(): Unit =
+    doSequentialBackspaceTest(
+      """class A {
+        |
+        |  def foo =
+        |    println(1)
+        |    println(2)#
+        |  #
+        |  # #   #
+        |
+        |  def fooOuter =
+        |    println(1)
+        |    println(2)
+        |
+        |    def fooInner =
+        |      println(1)
+        |      println(2)
+        |}
+        |""".stripMargin
+    )
+
+  def testExample2_NestedMethod_TailPosition_CaretUnindented(): Unit =
+    doSequentialBackspaceTest(
+      """class A {
+        |
+        |  def foo =
+        |    println(1)
+        |    println(2)#
+        |  ##
+        |
+        |  def fooOuter =
+        |    println(1)
+        |    println(2)
+        |
+        |    def fooInner =
+        |      println(1)
+        |      println(2)
+        |
+        |}
+        |""".stripMargin
+    )
+
+  def testExample3_NestedMethod_TailPosition_WithRedundantSpacesAfterCaretOnBlankLine(): Unit =
+    doTest(
+      s"""class Example1:
+         |  def outer =
+         |    println("outer 1")
+         |    println("outer 2")
+         |
+         |    def inner =
+         |      println("inner 1")
+         |      println("inner 2")
+         |      "inner result"
+         |    $CARET  ${""}
+         |
+         |    "outer result" + inner
+         |""".stripMargin,
+      s"""class Example1:
+         |  def outer =
+         |    println("outer 1")
+         |    println("outer 2")
+         |
+         |    def inner =
+         |      println("inner 1")
+         |      println("inner 2")
+         |      "inner result"$CARET
+         |
+         |    "outer result" + inner
+         |""".stripMargin
+    )
+
+  def testCodeAfterCaret(): Unit =
+    doTest(
+      s"""class A:
+         |  def foo1 =
+         |    def foo2 =
+         |      111
+         |      222
+         |             ${CARET}333
+         |""".stripMargin,
+      s"""class A:
+         |  def foo1 =
+         |    def foo2 =
+         |      111
+         |      222
+         |      ${CARET}333
+         |""".stripMargin
+    )
+
+  def testCodeAfterCaret_WithLeadingSpaces(): Unit =
+    doTest(
+      s"""class A:
+         |  def foo1 =
+         |    def foo2 =
+         |      111
+         |      222
+         |             $CARET   333
+         |""".stripMargin,
+      s"""class A:
+         |  def foo1 =
+         |    def foo2 =
+         |      111
+         |      222
+         |      ${CARET}333
+         |""".stripMargin
+    )
+
+  def testMethod_InsideBody(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  111#
+        |  #
+        |  #
+        |  #   #
+        |  222
+        |""".stripMargin
+    )
+
+  def testNestedMethod_1_InsideBody(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 =
+        |  def f2 =
+        |    111#
+        |    #
+        |    #
+        |    #    #
+        |    222
+        |""".stripMargin
+    )
+
+  def testNestedMethod_2_InsideBody(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      """def f1 = {
+        |  def f2 =
+        |    111
+        |    222
+        |
+        |    def f3 =
+        |      333#
+        |      #
+        |      #        #
+        |      444
+        |}""".stripMargin
+    )
+
+  def testAfterLastCaseClause_TailPosition(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""Option(42) match
+         |  case Some(1) =>
+         |    111#
+         |    #   #
+         |""".stripMargin
+    )
+
+  def testAfterLastCaseClause_BlankLinesBeforeCaret_1(): Unit =
+    doSequentialBackspaceTest_InContexts(WrapperContextsWithJumpToPreviousLine)(
+      s"""class Dummy
+         |
+         |Option(42) match
+         |  case Some(1) =>
+         |    111#
+         |#
+         |# # #   #
+         |""".stripMargin
+    )
+
+  def testAfterLastCaseClause_BlankLinesBeforeCaret_2(): Unit =
+    doSequentialBackspaceTest_InContexts(WrapperContextsWithJumpToPreviousLine)(
+      s"""Option(42) match
+         |  case Some(1) =>
+         |    111#
+         |#
+         |# # #   #
+         |
+         |class Dummy
+         |""".stripMargin
+    )
+
+  def testAfterLastCaseClause_InNestedScope_BlankLinesBeforeCaret(): Unit =
+    doSequentialBackspaceTest_InContexts(WrapperContextsWithJumpToPreviousLine)(
+      s"""def foo =
+         |  val res =
+         |    Option(42) match
+         |      case Some(1) =>
+         |        111#
+         |#
+         |# # # # #    #
+         |""".stripMargin
+    )
+
+  def testAfterLastCaseClause_InNestedScope_TailPosition_CaretUnindented_1(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""def foo =
+         |  val res =
+         |    Option(42) match
+         |      case Some(1) =>
+         |        111#
+         |    ##
+         |""".stripMargin
+    )
+
+  def testAfterLastCaseClause_InNestedScope_TailPosition_CaretUnindented_2(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""def foo =
+         |  val res =
+         |    Option(42) match
+         |      case Some(1) =>
+         |        111#
+         |  ##
+         |""".stripMargin
+    )
+
+  def testAfterLastCaseClause_InNestedScope_TailPosition_CaretUnindented_3(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""def foo =
+         |  val res =
+         |    Option(42) match
+         |      case Some(1) =>
+         |        111#
+         |##
+         |""".stripMargin
+    )
+
+
+  def testAfterMiddleCaseClause_InNestedScope_1(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""Option(42) match
+         |  case Some(1) =>
+         |    111#
+         |    #
+         |    #   #
+         |  case _ =>
+         |""".stripMargin
+    )
+
+  def testAfterMiddleCaseClause_InNestedScope_2_UnindentedCaret(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""Option(42) match
+         |  case Some(1) =>
+         |    111#
+         |    #
+         |  #
+         |  case _ =>
+         |""".stripMargin
+    )
+
+  def testAfterMiddleCaseClause_WithCodeAfterCaret_1(): Unit =
+    doTest(
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |      ${CARET}111
+         |    case _ =>
+         |}
+         |""".stripMargin,
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) => ${CARET}111
+         |    case _ =>
+         |}
+         |""".stripMargin
+    )
+
+  def testAfterMiddleCaseClause_WithCodeAfterCaret_2(): Unit =
+    doTest(
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         | $CARET     111
+         |    case _ =>
+         |}
+         |""".stripMargin,
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) => ${CARET}111
+         |    case _ =>
+         |}
+         |""".stripMargin
+    )
+
+  def testAfterMiddleCaseClause_WithCodeAfterCaret_3(): Unit =
+    doTest(
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |      111
+         | $CARET     222
+         |    case _ =>
+         |}
+         |""".stripMargin,
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |      111${CARET}222
+         |    case _ =>
+         |}
+         |""".stripMargin
+    )
+
+  def testAfterMiddleCaseClause_WithIndentedCodeAfterCaret_1(): Unit =
+    doTest(
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |        $CARET    42
+         |    case _ =>
+         |}
+         |""".stripMargin,
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |      ${CARET}42
+         |    case _ =>
+         |}
+         |""".stripMargin
+    )
+
+  def testAfterMiddleCaseClause_WithIndentedCodeAfterCaret_2(): Unit =
+    doTest(
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |          ${CARET}42
+         |    case _ =>
+         |}
+         |""".stripMargin,
+      s"""def foo = {
+         |  Option(42) match
+         |    case Some(1) =>
+         |      ${CARET}42
+         |    case _ =>
+         |}
+         |""".stripMargin
+    )
+
+
+  def testBetweenCaseClauses_WithCodeAfterCaseClauseArrow(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""42 match
+         |  case 1 => 1#
+         |  #
+         |  #
+         |  #   #
+         |  case 2 => 2
+         |""".stripMargin
+    )
+
+  def testAfterCaseClauses_WithCodeAfterCaseClauseArrow(): Unit =
+    doSequentialBackspaceTest_InAllWrapperContexts(
+      s"""42 match
+         |  case 1 => 1
+         |  case 2 => 2#
+         |  #   #
+         |""".stripMargin
+    )
+
+  def testAfterCaseClauses_WithCodeAfterCaseClauseArrow_BlankLineBefore(): Unit =
+    doSequentialBackspaceTest_InContexts(WrapperContextsWithJumpToPreviousLine)(
+      s"""42 match
+         |  case 1 => 1
+         |  case 2 => 2#
+         |#
+         |# #   #
+         |""".stripMargin
+    )
+
+  def testAfterCaseClauses_WithCodeAfterCaseClauseArrow_BlankLineBefore_1(): Unit =
+    doSequentialBackspaceTest(
+      s"""class A:
+         |  class B:
+         |    42 match
+         |      case 1 => 1
+         |      case 2 => 2#
+         |#
+         |# # # #   #
+         |""".stripMargin
+    )
+
+  def testAfterIncompleteMatch(): Unit =
+    doSequentialBackspaceTest_InContexts(WrapperCodeContexts.AllContexts.filterNot(_ == WrapperCodeContexts.InsideCaseClausesNonLast))(
+      s"""Option(42) match#
+         |  #    #
+         |""".stripMargin
+    )
+
+  def testUnnamed1(): Unit =
+    doTest(
+      s"""def foo1 =
+         |  111
+         |  222
+         |  def foo2 =
+         |    333
+         |    Seq(
+         |      1,
+         |      2,
+         |      ${CARET}3
+         |    )
+         |    444
+         |""".stripMargin,
+      s"""def foo1 =
+         |  111
+         |  222
+         |  def foo2 =
+         |    333
+         |    Seq(
+         |      1,
+         |      2, ${CARET}3
+         |    )
+         |    444
+         |""".stripMargin
+    )
+}
