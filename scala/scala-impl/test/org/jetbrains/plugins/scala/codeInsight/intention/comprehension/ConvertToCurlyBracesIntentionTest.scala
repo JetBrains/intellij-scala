6c57aed789e001da11553b645268222c334b5e3f
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-05-11 18:13:42 +0200

    allow ConvertToCurlyBracesIntention after parenthesis
     #SCL-18984

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInsight/intention/comprehension/ConvertToCurlyBracesIntentionTest.scala
@@ -0,0 +1,63 @@
+package org.jetbrains.plugins.scala.codeInsight.intention.comprehension
+
+import org.jetbrains.plugins.scala.codeInsight.intentions.ScalaIntentionTestBase
+
+class ConvertToCurlyBracesIntentionTest extends ScalaIntentionTestBase {
+
+  override def familyName: String = "Convert to curly braces"
+
+  private val resultOfSimple =
+    """for {
+      |  i <- 1 to 10
+      |} yield i + 1
+      |""".stripMargin
+
+  def testIntentionBeforeOpenParen(): Unit = doTest(
+    s"for$CARET(i <- 1 to 10) yield i + 1",
+    resultOfSimple,
+  )
+
+  def testIntentionAfterOpenParen(): Unit = doTest(
+    s"for(${CARET}i <- 1 to 10) yield i + 1",
+    resultOfSimple,
+  )
+
+  def testIntentionBeforeLastParen(): Unit = doTest(
+    s"for(i <- 1 to 10$CARET) yield i + 1",
+    resultOfSimple,
+  )
+
+  def testIntentionAfterLastParen(): Unit = doTest(
+    s"for(i <- 1 to 10)$CARET yield i + 1",
+    resultOfSimple,
+  )
+
+  def testIntentionNotAvailableOnForKeyword(): Unit = {
+    checkIntentionIsNotAvailable(s"f${CARET}or{i <- 1 to 10} yield i + 1")
+  }
+
+  def testIntentionNotAvailableInForBlock(): Unit = {
+    checkIntentionIsNotAvailable(s"for(i${CARET}i <- 1 to 10) yield i + 1")
+  }
+
+  def testIntentionActionForOnSimpleForStatement(): Unit = {
+    val text = s"for$CARET(i <- 1 to 10) yield i + 1"
+    val result =
+      """for {
+        |  i <- 1 to 10
+        |} yield i + 1
+        |""".stripMargin
+    doTest(text, result)
+  }
+
+  def testIntentionActionOnLargeForYieldStatement(): Unit = {
+    val text = s"""for$CARET(i <- 1 to 10; c <- 'a' to 'e') yield "" + c + i"""
+    val result =
+      """for {
+        |  i <- 1 to 10
+        |  c <- 'a' to 'e'
+        |} yield "" + c + i
+        |""".stripMargin
+    doTest(text, result)
+  }
+}
