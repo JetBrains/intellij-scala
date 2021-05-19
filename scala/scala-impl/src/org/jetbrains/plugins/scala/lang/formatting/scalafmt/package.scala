043bce8376622c08e56c77b68bc4633d103b7438
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-05-19 17:05:33 +0300

    scalafmt dynamic: create resolvers from `Remote Jar Repositories` in non-sbt projects

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/scalafmt/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/scalafmt/package.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.lang.formatting
 
+import com.intellij.jarRepository.RemoteRepositoriesConfiguration
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.DependencyManagerBase
@@ -7,18 +8,36 @@ import org.jetbrains.plugins.scala.DependencyManagerBase.{IvyResolver, MavenReso
 import org.jetbrains.sbt.project.module.SbtModule
 import org.jetbrains.sbt.resolvers.{SbtIvyResolver, SbtMavenResolver}
 
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
 package object scalafmt {
 
   private[scalafmt] def projectResolvers(project: Project): Seq[DependencyManagerBase.Resolver] = {
+    val resolversSbt = resolversFromSbt(project)
+    val resolvers = if (resolversSbt.isEmpty) // imply that it's a non-sbt project
+      resolversFromRemoteJarRepositories(project)
+    else
+      resolversSbt
+    resolvers
+  }
+
+  private def resolversFromSbt(project: Project): Seq[DependencyManagerBase.Resolver] = {
     val modules = ModuleManager.getInstance(project).getModules.toSet
     val moduleSbtResolvers = modules.flatMap(SbtModule.Resolvers.apply)
-
-    val resolvers = moduleSbtResolvers.toSeq.collect {
-      case r: SbtMavenResolver             => MavenResolver(r.name, ensureIsURL(r.root))
+    moduleSbtResolvers.toSeq.collect {
+      case r: SbtMavenResolver => MavenResolver(r.name, ensureIsURL(r.root))
       case r: SbtIvyResolver if !r.isLocal => IvyResolver(r.name, ensureIsURL(r.root))
     }
+  }
 
-    resolvers
+  // Both Gradle and  Maven projects import theirs resolvers to
+  // `File | Settings | Build, Execution, Deployment | Remote Jar Repositories`
+  private def resolversFromRemoteJarRepositories(project: Project): Seq[DependencyManagerBase.MavenResolver] = {
+    val configuration = RemoteRepositoriesConfiguration.getInstance(project)
+    val repositories = configuration.getRepositories.asScala.toSeq
+    repositories.map { repo =>
+      MavenResolver(repo.getName, ensureIsURL(repo.getUrl))
+    }
   }
 
   // e.g. `file://C:/temp/`
