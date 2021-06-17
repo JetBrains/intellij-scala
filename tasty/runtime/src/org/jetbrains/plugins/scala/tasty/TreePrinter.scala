c67cde97b575977e2345c758d8b9cb734f0f6bd9
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-06-17 12:44:09 +0200

    TreePrinter: top-level definitions, #SCL-19042

--- tasty/runtime/src/org/jetbrains/plugins/scala/tasty/TreePrinter.scala
+++ tasty/runtime/src/org/jetbrains/plugins/scala/tasty/TreePrinter.scala
@@ -19,10 +19,14 @@ object TreePrinter {
 
   def textOf(node: Node, definition: Option[Node] = None): String = node match {
     case Node(PACKAGE, _, Seq(Node(TERMREFpkg, Seq(name), _), tail: _*)) => tail match {
-      case Seq(node @ Node(PACKAGE, _, _), _: _*) => textOf(node, definition) // TODO make sure that there's a single nested package
+      case Seq(node @ Node(PACKAGE, _, _), _: _*) => textOf(node) // TODO make sure that there's a single nested package
       case _ =>
-        (if (name == "<empty>") "" else ("package " + name + "\n\n")) +
-          tail.map(textOf(_)).filter(_.nonEmpty).mkString("\n")
+        (if (name == "<empty>") "" else ("package " + name + "\n\n")) + (tail match {
+          case Seq(Node(VALDEF, Seq(name1), _: _*), Node(TYPEDEF, Seq(name2), Seq(template, _: _*)), _: _*) if name1.endsWith("$package") && name2.endsWith("$package$") =>
+            template.children.filter(it => it.is(DEFDEF, VALDEF, TYPEDEF) && it.names != Seq("<init>")).map(textOf(_)).filter(_.nonEmpty).mkString("\n\n")
+          case _ =>
+            tail.map(textOf(_)).filter(_.nonEmpty).mkString("\n")
+        })
     }
 
     case node @ Node(TYPEDEF, Seq(name), Seq(template, _: _*)) if !node.hasFlag(SYNTHETIC) || isGivenImplicitClass0(node) => // TODO why both are synthetic?
