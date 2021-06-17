cbcd1f134ffec5afd950d06f650f4aca492afcb3
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-06-17 14:53:08 +0200

    TreePrinter: with -> , #SCL-19042
    (cherry picked from commit 9ba707851aba87c9e1318635fd6c34d25b21d0c8)

--- tasty/runtime/data/typeDefinition/Parents.scala
+++ tasty/runtime/data/typeDefinition/Parents.scala
@@ -21,9 +21,9 @@ trait Parents {
 
   class Class3 extends TypeParameters[Int, Long]
 
-  class Class4 extends NoParameters with TypeParameter[Int]
+  class Class4 extends NoParameters, TypeParameter[Int]
 
-  class Class5 extends NoParameters with TypeParameter[Int] with TypeParameters[Int, Long]
+  class Class5 extends NoParameters, TypeParameter[Int], TypeParameters[Int, Long]
 
   class Class6 extends ValueParameter/**/(1)/**/
 
@@ -39,9 +39,9 @@ trait Parents {
 
   trait Trait3 extends TypeParameters[Int, Long]
 
-  trait Trait4 extends NoParameters with TypeParameter[Int]
+  trait Trait4 extends NoParameters, TypeParameter[Int]
 
-  trait Trait5 extends NoParameters with TypeParameter[Int] with TypeParameters[Int, Long]
+  trait Trait5 extends NoParameters, TypeParameter[Int], TypeParameters[Int, Long]
 
   trait Trait6 extends ValueParameter
 
@@ -57,9 +57,9 @@ trait Parents {
 
   object Object3 extends TypeParameters[Int, Long]
 
-  object Object4 extends NoParameters with TypeParameter[Int]
+  object Object4 extends NoParameters, TypeParameter[Int]
 
-  object Object5 extends NoParameters with TypeParameter[Int] with TypeParameters[Int, Long]
+  object Object5 extends NoParameters, TypeParameter[Int], TypeParameters[Int, Long]
 
   object Object6 extends ValueParameter/**/(1)/**/
 
@@ -81,11 +81,11 @@ trait Parents {
     case Case
   }
 
-  enum Enum4 extends NoParameters with TypeParameter[Int] {
+  enum Enum4 extends NoParameters, TypeParameter[Int] {
     case Case
   }
 
-  enum Enum5 extends NoParameters with TypeParameter[Int] with TypeParameters[Int, Long] {
+  enum Enum5 extends NoParameters, TypeParameter[Int], TypeParameters[Int, Long] {
     case Case
   }
 
@@ -106,75 +106,75 @@ trait Parents {
   }
 
   enum EnumCaseObject1 {
-    case CaseObject extends EnumCaseObject1 with NoParameters
+    case CaseObject extends EnumCaseObject1, NoParameters
   }
 
   enum EnumCaseObject2 {
-    case CaseObject extends EnumCaseObject2 with TypeParameter[Int]
+    case CaseObject extends EnumCaseObject2, TypeParameter[Int]
   }
 
   enum EnumCaseObject3 {
-    case CaseObject extends EnumCaseObject3 with TypeParameters[Int, Long]
+    case CaseObject extends EnumCaseObject3, TypeParameters[Int, Long]
   }
 
   enum EnumCaseObject4 {
-    case CaseObject extends EnumCaseObject4 with NoParameters with TypeParameter[Int]
+    case CaseObject extends EnumCaseObject4, NoParameters, TypeParameter[Int]
   }
 
   enum EnumCaseObject5 {
-    case CaseObject extends EnumCaseObject5 with NoParameters with TypeParameter[Int] with TypeParameters[Int, Long]
+    case CaseObject extends EnumCaseObject5, NoParameters, TypeParameter[Int], TypeParameters[Int, Long]
   }
 
   enum EnumCaseObject6 {
-    case CaseObject extends EnumCaseObject6 with ValueParameter/**/(1)/**/
+    case CaseObject extends EnumCaseObject6, ValueParameter/**/(1)/**/
   }
 
   enum EnumCaseObject7 {
-    case CaseObject extends EnumCaseObject7 with ValueParameters/**/(1, 2L)/**/
+    case CaseObject extends EnumCaseObject7, ValueParameters/**/(1, 2L)/**/
   }
 
   enum EnumCaseObject8 {
-    case CaseObject extends EnumCaseObject8 with MultipleClauses/**/(1)(2L)/**/
+    case CaseObject extends EnumCaseObject8, MultipleClauses/**/(1)(2L)/**/
   }
 
   enum EnumCaseObject9 {
-    case CaseObject extends EnumCaseObject9 with TypeAndValueParameter[Int]/**/(1)/**/
+    case CaseObject extends EnumCaseObject9, TypeAndValueParameter[Int]/**/(1)/**/
   }
 
   enum EnumCaseClass1 {
-    case CaseClass() extends EnumCaseClass1 with NoParameters
+    case CaseClass() extends EnumCaseClass1, NoParameters
   }
 
   enum EnumCaseClass2 {
-    case CaseClass() extends EnumCaseClass2 with TypeParameter[Int]
+    case CaseClass() extends EnumCaseClass2, TypeParameter[Int]
   }
 
   enum EnumCaseClass3 {
-    case CaseClass() extends EnumCaseClass3 with TypeParameters[Int, Long]
+    case CaseClass() extends EnumCaseClass3, TypeParameters[Int, Long]
   }
 
   enum EnumCaseClass4 {
-    case CaseClass() extends EnumCaseClass4 with NoParameters with TypeParameter[Int]
+    case CaseClass() extends EnumCaseClass4, NoParameters, TypeParameter[Int]
   }
 
   enum EnumCaseClass5 {
-    case CaseClass() extends EnumCaseClass5 with NoParameters with TypeParameter[Int] with TypeParameters[Int, Long]
+    case CaseClass() extends EnumCaseClass5, NoParameters, TypeParameter[Int], TypeParameters[Int, Long]
   }
 
   enum EnumCaseClass6 {
-    case CaseClass() extends EnumCaseClass6 with ValueParameter/**/(1)/**/
+    case CaseClass() extends EnumCaseClass6, ValueParameter/**/(1)/**/
   }
 
   enum EnumCaseClass7 {
-    case CaseClass() extends EnumCaseClass7 with ValueParameters/**/(1, 2L)/**/
+    case CaseClass() extends EnumCaseClass7, ValueParameters/**/(1, 2L)/**/
   }
 
   enum EnumCaseClass8 {
-    case CaseClass() extends EnumCaseClass8 with MultipleClauses/**/(1)(2L)/**/
+    case CaseClass() extends EnumCaseClass8, MultipleClauses/**/(1)(2L)/**/
   }
 
   enum EnumCaseClass9 {
-    case CaseClass() extends EnumCaseClass9 with TypeAndValueParameter[Int]/**/(1)/**/
+    case CaseClass() extends EnumCaseClass9, TypeAndValueParameter[Int]/**/(1)/**/
   }
 
   given givenInstance1: NoParameters with {}
