d91a3766eb591e06f0270cfe92e22b57557c6d52
Justin Kaeser <justin.kaeser@jetbrains.com>
2021-06-24 11:49:58 +0200

    Adjust SbtBuildModuleDataBspService to new ScalaAbstractProjectDataService. #SCL-18372

--- bsp/src/org/jetbrains/bsp/data/SbtBuildModuleDataBspService.scala
+++ bsp/src/org/jetbrains/bsp/data/SbtBuildModuleDataBspService.scala
@@ -5,36 +5,31 @@ import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.scala.project.external.{AbstractDataService, AbstractImporter}
+import org.jetbrains.plugins.scala.project.external.ScalaAbstractProjectDataService
 import org.jetbrains.sbt.project.module
+import org.jetbrains.sbt.project.module.SbtModule.{Build, Imports}
+
+import java.util
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 /**
   * @author Jason Zaugg
   */
-final class SbtBuildModuleDataBspService extends AbstractDataService[SbtBuildModuleDataBsp, Module](SbtBuildModuleDataBsp.Key) {
+final class SbtBuildModuleDataBspService extends ScalaAbstractProjectDataService[SbtBuildModuleDataBsp, Module](SbtBuildModuleDataBsp.Key) {
 
-  //noinspection TypeAnnotation
-  override def createImporter(toImport: Seq[DataNode[SbtBuildModuleDataBsp]],
-                              projectData: ProjectData,
-                              project: Project,
-                              modelsProvider: IdeModifiableModelsProvider) =
-    new AbstractImporter[SbtBuildModuleDataBsp](toImport, projectData, project, modelsProvider) {
-
-      override def importData(): Unit = for {
-        moduleNode <- dataToImport
-        module <- getIdeModuleByNode(moduleNode)
-      } SbtBuildModuleDataBspService.importData(module, moduleNode.getData)
+  override def importData(toImport: util.Collection[_ <: DataNode[SbtBuildModuleDataBsp]],
+                          projectData: ProjectData,
+                          project: Project,
+                          modelsProvider: IdeModifiableModelsProvider): Unit = {
+    for {
+      moduleNode <- toImport.asScala
+      module <- modelsProvider.getIdeModuleByNode(moduleNode)
+    } {
+      val SbtBuildModuleDataBsp(imports, buildFor) = moduleNode.getData
+      Imports(module) = imports
+      buildFor.forEach(uri => Build(module) = uri)
     }
-}
-
-object SbtBuildModuleDataBspService {
-
-  private def importData(sbtModule: Module,
-                         data: SbtBuildModuleDataBsp): Unit = {
-    import module.SbtModule._
-    val SbtBuildModuleDataBsp(imports, buildFor) = data
-
-    Imports(sbtModule) = imports
-    buildFor.forEach(uri => Build(sbtModule) = uri)
   }
+
 }
+
