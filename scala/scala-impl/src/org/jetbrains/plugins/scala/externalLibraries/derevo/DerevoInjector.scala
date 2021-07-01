499f65c52bf63f9a8fc4a75dccde2bf2016e26dc
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-07-01 13:41:38 +0200

    removed objnames from derevo implicits
     #SCL-19252

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalLibraries/derevo/DerevoInjector.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalLibraries/derevo/DerevoInjector.scala
@@ -164,9 +164,9 @@ class DerevoInjector extends SyntheticMembersInjector {
 
           val implicitStr = if (implicits.isEmpty) "" else s"(implicit ${implicits.mkString(", ")})"
 
-          s"implicit def idea$$derevo_injected_${obj.name}_$ix[$tparamStr]$implicitStr: ${tpeTo(s"${obj.name}[$tparamStr]")} = ???"
+          s"implicit def idea$$derevo_injected_$ix[$tparamStr]$implicitStr: ${tpeTo(s"${obj.name}[$tparamStr]")} = ???"
         } else {
-          s"implicit val idea$$derevo_injected_${obj.name}_$ix: ${tpeTo(obj.name)} = ???"
+          s"implicit val idea$$derevo_injected_$ix: ${tpeTo(obj.name)} = ???"
         }
       }
 
