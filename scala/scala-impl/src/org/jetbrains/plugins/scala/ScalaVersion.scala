08975b89e68810213593aa0c6a9aa63cbda4ebbd
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-07-15 14:11:36 +0200

    Updated latest scala versions
     #SCL-19320

--- scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
@@ -60,8 +60,8 @@ object LatestScalaVersions {
   val Scala_2_9  = new ScalaVersion(ScalaLanguageLevel.Scala_2_9, "3")
   val Scala_2_10 = new ScalaVersion(ScalaLanguageLevel.Scala_2_10, "7")
   val Scala_2_11 = new ScalaVersion(ScalaLanguageLevel.Scala_2_11, "12")
-  val Scala_2_12 = new ScalaVersion(ScalaLanguageLevel.Scala_2_12, "13")
-  val Scala_2_13 = new ScalaVersion(ScalaLanguageLevel.Scala_2_13, "3") // TODO: update to at least 2.13.6
+  val Scala_2_12 = new ScalaVersion(ScalaLanguageLevel.Scala_2_12, "14")
+  val Scala_2_13 = new ScalaVersion(ScalaLanguageLevel.Scala_2_13, "6")
   val Scala_3_0  = new ScalaVersion(ScalaLanguageLevel.Scala_3_0, "0")
 
   val all: Seq[ScalaVersion] = Seq(
