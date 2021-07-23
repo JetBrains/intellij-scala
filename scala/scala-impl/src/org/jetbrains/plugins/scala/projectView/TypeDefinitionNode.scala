b1ecfe4f8a7ce02f60a4639f977b26b8d38c6982
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-07-23 17:48:25 +0300

    project view: support enum cases #SCL-19225 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/projectView/TypeDefinitionNode.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/projectView/TypeDefinitionNode.scala
@@ -2,17 +2,19 @@ package org.jetbrains.plugins.scala
 package projectView
 
 import java.util
-
 import com.intellij.ide.projectView.ViewSettings
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
+
 import javax.swing.Icon
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScValueOrVariable
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScEnum, ScTypeDefinition}
 import org.jetbrains.plugins.scala.lang.psi.light.PsiClassWrapper
+
 import scala.jdk.CollectionConverters._
 
+// TODO: create tests for project view!
 /**
  * @author Pavel Fatin
  */
@@ -32,7 +34,12 @@ private[projectView] class TypeDefinitionNode(definition: ScTypeDefinition)
       super.getChildrenImpl
 
   private def childrenOf(value: ScTypeDefinition): util.List[Node] = {
-    val result: Seq[Node] = value.members.flatMap {
+    val enumCasesNodes = value match {
+      case enum: ScEnum => enum.cases.map(new NamedElementNode(_))
+      case _ => Nil
+    }
+
+    val memberNodes: Seq[Node] = value.members.flatMap {
       case definition: ScTypeDefinition =>
         Seq(new TypeDefinitionNode(definition))
       case element: ScNamedElement =>
@@ -42,7 +49,7 @@ private[projectView] class TypeDefinitionNode(definition: ScTypeDefinition)
       case _ => Seq.empty
     }
 
-    result.asJava
+    (enumCasesNodes ++ memberNodes).asJava
   }
 
   override def getPsiClass: PsiClass =
