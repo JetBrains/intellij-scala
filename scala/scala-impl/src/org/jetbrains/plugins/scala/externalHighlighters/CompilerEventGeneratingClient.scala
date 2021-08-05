23e67b6dda31e75faa5d11d3b98ba2fa4e956de3
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-08-05 14:05:37 +0300

    resolve the correct protobuf jar in IntellijPlatformJars.protobufJava #SCL-19414 fixed
    + properly log exceptions in CompilerEventGeneratingClient
    
    (cherry picked from commit b27a05cbdea120b4eaafe2360c9321a8e9ef0149)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CompilerEventGeneratingClient.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/externalHighlighters/CompilerEventGeneratingClient.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.plugins.scala.externalHighlighters
 
-import java.io.File
-
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import org.jetbrains.jps.incremental.scala.{Client, DummyClient}
@@ -9,9 +8,13 @@ import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.compiler.{CompilerEvent, CompilerEventListener}
 import org.jetbrains.plugins.scala.util.CompilationId
 
-private class CompilerEventGeneratingClient(project: Project,
-                                            indicator: ProgressIndicator)
-  extends DummyClient {
+import java.io.File
+
+private class CompilerEventGeneratingClient(
+  project: Project,
+  indicator: ProgressIndicator,
+  log: Logger
+) extends DummyClient {
 
   final val compilationId = CompilationId.generate()
 
@@ -37,7 +40,7 @@ private class CompilerEventGeneratingClient(project: Project,
   override def isCanceled: Boolean = indicator.isCanceled
 
   override def trace(exception: Throwable): Unit =
-    exception.printStackTrace()
+    log.error(exception)
 
   private def sendEvent(event: CompilerEvent): Unit =
     project.getMessageBus
