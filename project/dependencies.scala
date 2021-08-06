9ff8a0d801a0c09c4240ec980a99691c94c4e260
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-08-06 10:14:41 +0300

    update intellijVersion to 212.4746.92 (latest release version)
    + fix compilation settings

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -9,7 +9,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.4.8-81-e170cd66"
   val zincVersion = "1.4.3"
-  val intellijVersion = "212.4638.7"
+  val intellijVersion = "212.4746.92"
   val bspVersion = "2.0.0-M14"
   val sbtStructureVersion: String = "2021.1.1"
   val sbtIdeaShellVersion: String = "2021.1.0"
