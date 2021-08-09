3c2ce8454bc34c2bf36392158126002f081d3ac0
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2021-08-09 16:48:33 +0200

    fixed TemplateTesting self call
     #SCL-19156

--- scala/scala-impl/test/org/jetbrains/plugins/scala/util/TemplateTesting.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/util/TemplateTesting.scala
@@ -6,10 +6,10 @@ import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
 
 // mix in this trait if you want to observe myFixture.getLookupElements.
 // without it, the template mechanism will just select the first lookup element
-trait TemplateTesting { self: TestCase with ScalaLightCodeInsightFixtureTestAdapter =>
+trait TemplateTesting extends TestCase { this: ScalaLightCodeInsightFixtureTestAdapter =>
 
   override def setUp(): Unit = {
-    self.setUp()
-    TemplateManagerImpl.setTemplateTesting(self.getFixture.getTestRootDisposable)
+    super.setUp()
+    TemplateManagerImpl.setTemplateTesting(getFixture.getTestRootDisposable)
   }
 }
