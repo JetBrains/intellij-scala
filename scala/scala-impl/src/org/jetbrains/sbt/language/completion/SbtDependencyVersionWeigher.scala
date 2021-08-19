71f246c08242cfe8e358357c1a40762d22899cf2
xuanson.trinh <xuanson.trinh@outlook.com>
2021-08-19 23:43:32 +0200

    Improved ordering for versions in completion list (SCL-19461)

--- scala/scala-impl/src/org/jetbrains/sbt/language/completion/SbtDependencyVersionWeigher.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/language/completion/SbtDependencyVersionWeigher.scala
@@ -2,7 +2,7 @@ package org.jetbrains.sbt.language.completion
 
 import com.intellij.codeInsight.lookup.{LookupElement, LookupElementWeigher}
 
-object SbtDependencyVersionWeigher extends LookupElementWeigher("sbtDependencyVersionWeigher"){
+object SbtDependencyVersionWeigher extends LookupElementWeigher("sbtDependencyVersionWeigher", true, false){
   override def weigh(element: LookupElement): Comparable[_] = {
     var splitted = element.getLookupString.split("\\.").slice(0, 3)
     while (splitted.length < 3) splitted = splitted :+ "0"
@@ -12,6 +12,6 @@ object SbtDependencyVersionWeigher extends LookupElementWeigher("sbtDependencyVe
       res += pattern.findFirstIn(s).getOrElse("0").toInt
       res *= 1000
     })
-    -res
+    res
   }
 }
