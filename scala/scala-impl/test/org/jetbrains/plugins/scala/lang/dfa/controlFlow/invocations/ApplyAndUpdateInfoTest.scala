5a0e18ca366f152512b14e88ece6de35484cd771
Gerard Dróżdż <gerardd@interia.pl>
2021-09-28 13:47:37 +0200

    Added desugared equivalence property testing to invocation info tests
     #SCL-15150

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/dfa/controlFlow/invocations/ApplyAndUpdateInfoTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/dfa/controlFlow/invocations/ApplyAndUpdateInfoTest.scala
@@ -5,32 +5,41 @@ import org.jetbrains.plugins.scala.lang.dfa.controlFlow.invocations.Argument.Pas
 class ApplyAndUpdateInfoTest extends InvocationInfoTestBase {
 
   def testGeneratedFactoryApplyMethods(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "SomeStringWrapper(\"Wrap me\")"
+    val desugaredSyntax = "SomeStringWrapper.apply(\"Wrap me\")"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |case class SomeStringWrapper(wrapped: String)
          |
          |def main(): String = {
-         |val somethingWrapped = ${markerStart}SomeStringWrapper("Wrap me")${markerEnd}
+         |val somethingWrapped = ${markerStart}${invocationSyntax}${markerEnd}
          |somethingWrapped.wrapped
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 1
+      val expectedProperArgsInText = List("\"Wrap me\"")
+      val expectedMappedParamNames = List("wrapped")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "SomeStringWrapper#apply")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "SomeStringWrapper")
     }
-
-    val expectedArgCount = 1 + 1
-    val expectedProperArgsInText = List("\"Wrap me\"")
-    val expectedMappedParamNames = List("wrapped")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "SomeStringWrapper#apply")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "SomeStringWrapper")
   }
 
   def testCustomApplyMethodsInSingletonObjects(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "SomeClass(4)"
+    val desugaredSyntax = "SomeClass.apply(4)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |object SomeClass {
@@ -38,26 +47,32 @@ class ApplyAndUpdateInfoTest extends InvocationInfoTestBase {
          |}
          |
          |def main(): String = {
-         |val x = ${markerStart}SomeClass(4)${markerEnd}
+         |val x = ${markerStart}${invocationSyntax}${markerEnd}
          |x + 2
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 1
+      val expectedProperArgsInText = List("4")
+      val expectedMappedParamNames = List("x")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "SomeClass#apply")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "SomeClass")
     }
-
-    val expectedArgCount = 1 + 1
-    val expectedProperArgsInText = List("4")
-    val expectedMappedParamNames = List("x")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "SomeClass#apply")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "SomeClass")
   }
 
   def testCustomApplyMethodsOnInstances(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "obj(5)"
+    val desugaredSyntax = "obj.apply(5)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |class SomeClass(y: Int) {
@@ -66,99 +81,123 @@ class ApplyAndUpdateInfoTest extends InvocationInfoTestBase {
          |
          |def main(): String = {
          |val obj = new SomeClass(33)
-         |val x = ${markerStart}obj(5)${markerEnd}
+         |val x = ${markerStart}${invocationSyntax}${markerEnd}
          |x + 3
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 1
+      val expectedProperArgsInText = List("5")
+      val expectedMappedParamNames = List("x")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "SomeClass#apply")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "obj")
     }
-
-    val expectedArgCount = 1 + 1
-    val expectedProperArgsInText = List("5")
-    val expectedMappedParamNames = List("x")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "SomeClass#apply")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "obj")
   }
 
   def testBuiltinFactoryApplyMethodsWithFewArgs(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "List(1113, 8 * 15)"
+    val desugaredSyntax = "List.apply(1113, 8 * 15)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |
          |def main(): String = {
-         |val someList = ${markerStart}List(1113, 8 * 15)${markerEnd}
+         |val someList = ${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 2
+      val expectedProperArgsInText = List("1113", "8 * 15")
+      val expectedMappedParamNames = List("xs", "xs")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "List#apply")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "List")
     }
-
-    val expectedArgCount = 1 + 2
-    val expectedProperArgsInText = List("1113", "8 * 15")
-    val expectedMappedParamNames = List("xs", "xs")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "List#apply")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "List")
   }
 
   def testBuiltinFactoryApplyMethodsWithMoreArgs(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "List(1113, 8 * 15, 24, 9, 32992, 9, someFunc(33), 44, 47858, 45555, 6 - 6, 323, 44)"
+    val desugaredSyntax = "List.apply(1113, 8 * 15, 24, 9, 32992, 9, someFunc(33), 44, 47858, 45555, 6 - 6, 323, 44)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |
          |def someFunc(x: Int): Int = x + 3
          |
          |def main(): String = {
-         |val someList = ${markerStart}List(1113, 8 * 15, 24, 9, 32992, 9, someFunc(33), 44, 47858, 45555, 6 - 6, 323, 44)${markerEnd}
+         |val someList = ${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 13
+      val expectedProperArgsInText = List("1113", "8 * 15", "24", "9", "32992", "9", "someFunc(33)", "44",
+        "47858", "45555", "6 - 6", "323", "44")
+      val expectedMappedParamNames = (1 until expectedArgCount).map(_ => "xs")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "List#apply")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "List")
     }
-
-    val expectedArgCount = 1 + 13
-    val expectedProperArgsInText = List("1113", "8 * 15", "24", "9", "32992", "9", "someFunc(33)", "44",
-      "47858", "45555", "6 - 6", "323", "44")
-    val expectedMappedParamNames = (1 until expectedArgCount).map(_ => "xs")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "List#apply")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "List")
   }
 
   def testBuiltinAccessorApplyMethods(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "someSet(120)"
+    val desugaredSyntax = "someSet.apply(120)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |
          |def main(): String = {
          |val someSet = Set(13, 13, 13, 1113, 8 * 15, 24, 9, 32992, 9, 33)
-         |${markerStart}someSet(120)${markerEnd}
+         |${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 1
+      val expectedProperArgsInText = List("120")
+      val expectedMappedParamNames = List("elem")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "GenSetLike#apply")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "someSet")
     }
-
-    val expectedArgCount = 1 + 1
-    val expectedProperArgsInText = List("120")
-    val expectedMappedParamNames = List("elem")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "GenSetLike#apply")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "someSet")
   }
 
   def testCustomUpdateMethods(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "collection(4) = 12"
+    val desugaredSyntax = "collection.update(4, 12)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |class MyMutableCollection {
@@ -168,25 +207,31 @@ class ApplyAndUpdateInfoTest extends InvocationInfoTestBase {
          |
          |def main(): String = {
          |val collection = new MyMutableCollection
-         |${markerStart}collection(4) = 12${markerEnd}
+         |${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 2
+      val expectedProperArgsInText = List("4", "12")
+      val expectedMappedParamNames = List("position", "value")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "MyMutableCollection#update")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      //    verifyThisExpression(invocationInfo, "collection") TODO check why if thisExpr correctly returns None in the PSI?
     }
-
-    val expectedArgCount = 1 + 2
-    val expectedProperArgsInText = List("4", "12")
-    val expectedMappedParamNames = List("position", "value")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "MyMutableCollection#update")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    //    verifyThisExpression(invocationInfo, "collection") TODO check why if thisExpr correctly returns None in the PSI?
   }
 
   def testComplexCustomUpdateMethods(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "collection(4, \"dddddx\", 7 <= 8 && 9 > 4 * 2) = 12"
+    val desugaredSyntax = "collection.update(4, \"dddddx\", 7 <= 8 && 9 > 4 * 2, 12)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |class MyMutableCollection {
@@ -199,25 +244,31 @@ class ApplyAndUpdateInfoTest extends InvocationInfoTestBase {
          |
          |def main(): String = {
          |val collection = new MyMutableCollection
-         |${markerStart}collection(4, "dddddx", 7 <= 8 && 9 > 4 * 2) = 12${markerEnd}
+         |${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 4
+      val expectedProperArgsInText = List("4", "\"dddddx\"", "7 <= 8 && 9 > 4 * 2", "12")
+      val expectedMappedParamNames = List("position1", "position2", "position3", "value")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "MyMutableCollection#update")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      //    verifyThisExpression(invocationInfo, "collection") TODO check why if thisExpr correctly returns None in the PSI?
     }
-
-    val expectedArgCount = 1 + 4
-    val expectedProperArgsInText = List("4", "\"dddddx\"", "7 <= 8 && 9 > 4 * 2", "12")
-    val expectedMappedParamNames = List("position1", "position2", "position3", "value")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "MyMutableCollection#update")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    //    verifyThisExpression(invocationInfo, "collection") TODO check why if thisExpr correctly returns None in the PSI?
   }
 
   def testBuiltinUpdateMethods(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "someMutableArray(3) = 15 + 2 * 9"
+    val desugaredSyntax = "someMutableArray.update(3, 15 + 2 * 9)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |import scala.collection.mutable.ArrayBuffer
          |
@@ -225,20 +276,23 @@ class ApplyAndUpdateInfoTest extends InvocationInfoTestBase {
          |
          |def main(): String = {
          |val someMutableArray = ArrayBuffer(11339, 9 * 40, 9 - 4 - 4 - 4, -15)
-         |${markerStart}someMutableArray(3) = 15 + 2 * 9${markerEnd}
+         |${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 2
+      val expectedProperArgsInText = List("3", "15 + 2 * 9")
+      val expectedMappedParamNames = List("idx", "elem")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "ResizableArray#update")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      // TODO check if this expression also here should be an unknown value
     }
-
-    val expectedArgCount = 1 + 2
-    val expectedProperArgsInText = List("3", "15 + 2 * 9")
-    val expectedMappedParamNames = List("idx", "elem")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "ResizableArray#update")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    // TODO check if this expression also here should be an unknown value
   }
 }
--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/dfa/controlFlow/invocations/LeftAssocInfixExpressionInfoTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/dfa/controlFlow/invocations/LeftAssocInfixExpressionInfoTest.scala
@@ -5,27 +5,36 @@ import org.jetbrains.plugins.scala.lang.dfa.controlFlow.invocations.Argument.Pas
 class LeftAssocInfixExpressionInfoTest extends InvocationInfoTestBase {
 
   def testBuiltinInfixExpressions(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "23 + 16"
+    val desugaredSyntax = "23.+(16)"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |def main(): Int = {
-         |${markerStart}23 + 16${markerEnd}
+         |${markerStart}${invocationSyntax}${markerEnd}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 1
+      val expectedProperArgsInText = List("16")
+      val expectedMappedParamNames = List("") // mapped params in synthetic methods have empty names
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "Synthetic method: +")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "23")
     }
-
-    val expectedArgCount = 1 + 1
-    val expectedProperArgsInText = List("16")
-    val expectedMappedParamNames = List("") // mapped params in synthetic methods have empty names
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "Synthetic method: +")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "23")
   }
 
   def testCustomInfixExpressions(): Unit = {
-    val invocationInfo = generateInvocationInfoFor {
+    val sugaredSyntax = "hello & \"World\""
+    val desugaredSyntax = "hello.&(\"World\")"
+
+    val code = (invocationSyntax: String) =>
       s"""
          |object Test {
          |case class AndWrapper(wrapped: String) {
@@ -34,20 +43,23 @@ class LeftAssocInfixExpressionInfoTest extends InvocationInfoTestBase {
          |
          |def main(): String = {
          |val hello = AndWrapper("Hello ")
-         |${markerStart}hello & "World"${markerEnd}
+         |${markerStart}${invocationSyntax}${markerEnd}
          |}
          |}
          |""".stripMargin
+
+    for (invocationSyntax <- List(sugaredSyntax, desugaredSyntax)) {
+      val invocationInfo = generateInvocationInfoFor(code(invocationSyntax))
+
+      val expectedArgCount = 1 + 1
+      val expectedProperArgsInText = List("\"World\"")
+      val expectedMappedParamNames = List("rhs")
+      val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
+
+      verifyInvokedElement(invocationInfo, "AndWrapper#&")
+      verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
+        expectedMappedParamNames, expectedPassingMechanisms)
+      verifyThisExpression(invocationInfo, "hello")
     }
-
-    val expectedArgCount = 1 + 1
-    val expectedProperArgsInText = List("\"World\"")
-    val expectedMappedParamNames = List("rhs")
-    val expectedPassingMechanisms = (1 to expectedArgCount).map(_ => PassByValue)
-
-    verifyInvokedElement(invocationInfo, "AndWrapper#&")
-    verifyArguments(invocationInfo, expectedArgCount, expectedProperArgsInText,
-      expectedMappedParamNames, expectedPassingMechanisms)
-    verifyThisExpression(invocationInfo, "hello")
   }
 }
