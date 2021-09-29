71865ffe17d4cf0b5b11a83c3aed1479e4fbe324
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2021-09-29 17:18:07 +0300

    Fix auto popup for scalac options resizing bug
    #SCL-19445

--- scala/scala-impl/src/org/jetbrains/sbt/language/completion/LookupAutoDocumentationProvider.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/language/completion/LookupAutoDocumentationProvider.scala
@@ -1,38 +1,65 @@
 package org.jetbrains.sbt.language.completion
 
+import com.intellij.codeInsight.CodeInsightSettings
+import com.intellij.codeInsight.completion.CompletionService
 import com.intellij.codeInsight.documentation.DocumentationManager
 import com.intellij.codeInsight.lookup._
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.util.Alarm
 import org.jetbrains.plugins.scala.extensions.ObjectExt
 import org.jetbrains.sbt.language.completion.LookupAutoDocumentationProvider._
 import org.jetbrains.sbt.language.psi.SbtScalacOptionDocHolder
 
 final class LookupAutoDocumentationProvider extends LookupManagerListener {
   override def activeLookupChanged(oldLookup: Lookup, newLookup: Lookup): Unit =
-    if (!isHeadlessEnv && newLookup != null) newLookup.addLookupListener(lookupListener)
+    if (!isHeadlessEnv && !CodeInsightSettings.getInstance.AUTO_POPUP_JAVADOC_INFO && newLookup != null)
+      newLookup.addLookupListener(lookupListener(newLookup))
 }
 
 object LookupAutoDocumentationProvider {
+  private val alarm = new Alarm()
+
   private def isHeadlessEnv = ApplicationManager.getApplication.isHeadlessEnvironment
 
-  private val lookupListener = new LookupListener {
-    override def currentItemChanged(event: LookupEvent): Unit =
-      if (event.getItem != null && event.getItem.getPsiElement.is[SbtScalacOptionDocHolder]) {
-        showDocumentation(event.getLookup)
-      }
+  private def lookupListener(lookup: Lookup) = new LookupListener {
 
-    override def itemSelected(event: LookupEvent): Unit = {
-      val docInfoHint = DocumentationManager.getInstance(event.getLookup.getProject).getDocInfoHint
-      // close documentation popup
-      if (docInfoHint != null) docInfoHint.cancel()
+    override def currentItemChanged(event: LookupEvent): Unit = {
+      alarm.cancelAllRequests()
+
+      if (event.getItem != null && event.getItem.getPsiElement.is[SbtScalacOptionDocHolder]) {
+        val request: Runnable = () => showDocumentation()
+        val delay = CodeInsightSettings.getInstance.JAVADOC_INFO_DELAY
+
+        alarm.addRequest(request, delay)
+      }
     }
 
-    private def showDocumentation(lookup: Lookup): Unit = {
+    override def itemSelected(event: LookupEvent): Unit = lookupClosed()
+
+    override def lookupCanceled(event: LookupEvent): Unit = lookupClosed()
+
+    private def showDocumentation(): Unit = {
       val docManager = DocumentationManager.getInstance(lookup.getProject)
       // show docs only if not shown yet
       if (docManager.getDocInfoHint == null) {
-        docManager.showJavaDocInfo(lookup.getEditor, lookup.getPsiFile, false)
+        val currentItem = lookup.getCurrentItem
+        val completion = CompletionService.getCompletionService.getCurrentCompletion
+
+        if (currentItem != null && currentItem.isValid && completion != null) {
+          docManager.showJavaDocInfo(lookup.getEditor, lookup.getPsiFile, false)
+        }
       }
+    }
+
+    private def closeDocPopup(): Unit = {
+      val docInfoHint = DocumentationManager.getInstance(lookup.getProject).getDocInfoHint
+      if (docInfoHint != null) docInfoHint.cancel()
+    }
+
+    private def lookupClosed(): Unit = {
+      alarm.cancelAllRequests()
+      closeDocPopup()
+      lookup.removeLookupListener(this)
     }
   }
 }
