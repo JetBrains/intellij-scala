65091c8b22a2ac2063760d994cf0ba8be8788e4e
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-10-22 15:49:18 +0200

    getStubVersion = 362, #SCL-19042

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/compiled/ScClassFileDecompiler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/compiled/ScClassFileDecompiler.scala
@@ -40,7 +40,7 @@ object ScClassFileDecompiler {
 
   object ScClsStubBuilder extends compiled.ClsStubBuilder {
 
-    override val getStubVersion = 361
+    override val getStubVersion = 362
 
     override def buildFileStub(content: FileContent): stubs.PsiFileStubImpl[_ <: PsiFile] =
       decompiledScalaFile(content)
