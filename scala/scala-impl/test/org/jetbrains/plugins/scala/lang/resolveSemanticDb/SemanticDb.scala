84b7fddd2a8411785ec2900576cd55d8c82bb54d
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-10-26 12:27:20 +0300

    update SemanticDb.scala from idea213.x branch

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolveSemanticDb/SemanticDb.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolveSemanticDb/SemanticDb.scala
@@ -1,74 +1,144 @@
 package org.jetbrains.plugins.scala.lang.resolveSemanticDb
 
-import java.nio.file.Path
-import scala.meta.internal.semanticdb
-import scala.meta.internal.semanticdb.{Locator, SymbolInformation, SymbolOccurrence}
+import java.nio.file.{Files, Path}
+import scala.collection.mutable
+import scala.meta.internal.semanticdb.Locator
 
 
-class SDbFile(val path: String, _occurrences: => Seq[SDbOccurrence]) {
-  lazy val occurrences: Seq[SDbOccurrence] = _occurrences
-
-  def referencesAt(pos: TextPos): Seq[SDbOccurrence] =
-    occurrences.filter(_.isReference).filter(_.range.contains(pos))
-}
-
-class SDbSymbol(_file: => SDbFile, val info: SymbolInformation)(_definition: => SDbOccurrence) {
-  def position: TextPos = definition.position
-
-  lazy val file: SDbFile = _file
-  lazy val definition: SDbOccurrence = _definition
+case class SDbRef(symbol: String, position: TextPos, endPosition: TextPos, targetPosition: Option[TextPos]) {
+  def range: (TextPos, TextPos) = (position, endPosition)
+  lazy val pointsToLocal: Boolean = symbol.matches(raw"local\d+")
 
   override def toString: String =
-    s"def ${definition.toString}"
+    s"$symbol($position..$endPosition) -> ${targetPosition.fold("<no position>")(_.toString)}"
 }
 
-class SDbOccurrence(val info: SymbolOccurrence, _symbol: => Option[SDbSymbol]) {
-  lazy val symbol: Option[SDbSymbol] = _symbol
-  def range: semanticdb.Range = info.range.get
-  def position: TextPos = TextPos(range.startLine, range.startCharacter)
-  def isDefinition: Boolean = info.role.isDefinition
-  def isReference: Boolean = info.role.isReference
-
-  override def toString: String =s"${info.symbol}(${range.mkString})" + (
-    if (isReference) s" -> ${symbol.fold("<no symbol>")(_.toString)}"
-    else ""
-    )
+object SDbRef {
+  implicit val ordering: Ordering[SDbRef] =
+    Ordering.by[SDbRef, TextPos](_.position)
+      .orElseBy(_.endPosition)
+      .orElseBy(_.symbol)
 }
 
-class SemanticDbStore(val files: Seq[SDbFile], val symbols: Map[String, SDbSymbol])
+case class SDbFile(path: String, references: Seq[SDbRef]) {
+  def referencesAt(pos: TextPos, empty: Boolean): Seq[SDbRef] =
+    references.filter(if (empty) _.range.is(pos) else _.range.contains(pos))
+}
 
-object SemanticDbStore {
-  def apply(path: Path): SemanticDbStore = {
-    lazy val store: SemanticDbStore = {
-      var symbols = Map.empty[String, SDbSymbol]
-      var definitions = Map.empty[String, SDbOccurrence]
-      val files = Seq.newBuilder[SDbFile]
-      Locator(path) { (_, payload) =>
-        for (doc <- payload.documents) {
-          lazy val file: SDbFile = {
-            for (sym <- doc.symbols) {
-              assert(!symbols.contains(sym.symbol))
-              symbols += sym.symbol -> new SDbSymbol(file, sym)(definitions(sym.symbol))
-            }
+case class SemanticDbStore(files: Seq[SDbFile]) {
+  def serialized: String = {
+    val b = new StringBuilder
 
-            val occurrences =
-              for (occurrence <- doc.occurrences)
-                yield new SDbOccurrence(occurrence, store.symbols.get(occurrence.symbol))
+    for (file <- files) {
+      b ++= "Document "
+      b ++= file.path
+      b += '\n'
 
-            for (occurrence <- occurrences if occurrence.isDefinition) {
-              definitions += occurrence.info.symbol -> occurrence
-            }
-
-            new SDbFile(doc.uri, occurrences)
-          }
-
-          files += file
+      for (ref <- file.references) {
+        b ++= s"(${ref.position}..${ref.endPosition}) ${ref.symbol}"
+        ref.targetPosition.foreach { targetPosition =>
+          b ++= s" -> $targetPosition"
         }
+        b += '\n'
       }
 
-      new SemanticDbStore(files.result(), symbols)
+      b += '\n'
     }
 
-    store
+    val resultText = b.result()
+
+    // check that serialize and fromText work together
+    assert(this == SemanticDbStore.fromText(resultText))
+
+    resultText
   }
+}
+
+object SemanticDbStore {
+  def fromSemanticDbPath(path: Path): SemanticDbStore = {
+    type UnfinishedRef = (String, TextPos, TextPos)
+    val positionOfSymbols = mutable.Map.empty[String, TextPos]
+    val unfinishedFiles = mutable.Map.empty[String, Seq[UnfinishedRef]]
+    Locator(path) { (_, payload) =>
+      for (doc <- payload.documents) {
+        val refs = Seq.newBuilder[UnfinishedRef]
+        for (occurrence <- doc.occurrences) {
+          def start = TextPos.ofStart(occurrence.range.get)
+          def end = TextPos.ofEnd(occurrence.range.get)
+          if (occurrence.role.isDefinition) positionOfSymbols += occurrence.symbol -> start
+          else if (occurrence.role.isReference) refs += ((occurrence.symbol, start, end))
+        }
+        unfinishedFiles += doc.uri -> refs.result()
+      }
+    }
+
+    val files = unfinishedFiles.iterator
+      .map {
+        case (path, unfinishedRefs) =>
+          val refs =
+            for ((symbol, start, end) <- unfinishedRefs)
+              yield SDbRef(symbol, start, end, targetPosition = positionOfSymbols.get(symbol))
+          SDbFile(path, refs.sorted)
+      }
+      .toSeq
+      .sortBy(_.path)
+
+    SemanticDbStore(files)
+  }
+
+  def fromTextFile(path: Path): SemanticDbStore =
+    fromText(Files.readString(path))
+
+  def fromText(text: String): SemanticDbStore = {
+    val lines = text.linesIterator
+    val files = Seq.newBuilder[SDbFile]
+
+    while (lines.hasNext) {
+      val pathLine = lines.next()
+      assert(pathLine.startsWith("Document "))
+      val path = pathLine.stripPrefix("Document ")
+
+      val refs =
+        for (refLine <- lines.takeWhile(_.nonEmpty)) yield {
+          refLine match {
+            case RefFromLine(ref) => ref
+            case s => throw new Exception("not a refline: " + s)
+          }
+        }
+
+      files += SDbFile(path, refs.toSeq)
+    }
+    SemanticDbStore(files.result())
+  }
+
+  private object RefFromLine {
+    private val pos = raw"(\d+):(\d+)"
+    private val RefLineParts = raw"\($pos\.\.$pos\) (.+?)(?: -> $pos)?".r
+
+    def unapply(s: String): Option[SDbRef] = {
+      s match {
+        case RefLineParts(startLine, startCol, endLine, endCol, symbol, targetLine, targetCol) =>
+          val targetPosition =
+            if (targetLine == null) None
+            else Some(TextPos(targetLine.toInt, targetCol.toInt))
+          Some(SDbRef(symbol, TextPos(startLine.toInt, startCol.toInt), TextPos(endLine.toInt, endCol.toInt), targetPosition))
+        case _ => None
+      }
+    }
+  }
+
+
+
+  /* Code to convert semanticdb binary data to new text based format
+
+  def main(args: Array[String]): Unit = {
+    val main = Path.of("/home/tobi/workspace/intellij-scala/community/scala/scala-impl/testdata/lang/resolveSemanticDb/out")
+    Files.list(main)
+      .filter(Files.isDirectory(_))
+      .forEach { path =>
+        val store = SemanticDbStore.fromSemanticDbPath(path)
+        Files.writeString(main.resolve(path.getFileName.toString + ".semdb"), store.serialized)
+      }
+  }
+  */
 }
