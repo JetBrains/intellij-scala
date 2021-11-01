9fa38c20b348712b32e205c2103f83cd6a92255f
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-11-01 18:01:12 +0300

    DynamicPropertyKeyInspection: do not report inspection on non-expressions
    this code produced error in EA:
    ```scala
    @PropertyKey(resourceBundle = "MyBundle")
    val value: String = "invalid.key"
    ```

--- scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/internal/DynamicPropertyKeyInspection.scala
+++ scala/integration/properties/src/org/jetbrains/plugins/scala/scalai18n/codeInspection/i18n/internal/DynamicPropertyKeyInspection.scala
@@ -4,24 +4,28 @@ import com.intellij.codeInsight.AnnotationUtil
 import com.intellij.codeInspection.{InspectionManager, LocalQuickFix, ProblemDescriptor, ProblemHighlightType}
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.codeInspection.AbstractRegisteredInspection
-import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScInterpolatedStringLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScStringLiteral
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import org.jetbrains.plugins.scala.scalai18n.codeInspection.i18n.ScalaI18nUtil
 
 class DynamicPropertyKeyInspection extends AbstractRegisteredInspection {
 
-  override protected def problemDescriptor(element: PsiElement,
-                                           maybeQuickFix: Option[LocalQuickFix] = None,
-                                           descriptionTemplate: String = getDisplayName,
-                                           highlightType: ProblemHighlightType = ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
-                                          (implicit manager: InspectionManager, isOnTheFly: Boolean): Option[ProblemDescriptor] = {
-    def isNonInterpolatedString = element.isInstanceOf[ScStringLiteral] && !element.isInstanceOf[ScInterpolatedStringLiteral]
-    def isPassedToProperty = ScalaI18nUtil.isPassedToAnnotated(element, AnnotationUtil.PROPERTY_KEY)
+  override protected def problemDescriptor(
+    element: PsiElement,
+    maybeQuickFix: Option[LocalQuickFix] = None,
+    descriptionTemplate: String = getDisplayName,
+    highlightType: ProblemHighlightType = ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+  )(implicit manager: InspectionManager, isOnTheFly: Boolean): Option[ProblemDescriptor] = {
+    val expression = element match {
+      case s: ScStringLiteral if s.isSimpleLiteral => return None
+      case e: ScExpression                         => e
+      case _                                       => return None
+    }
 
-    (!isNonInterpolatedString && isPassedToProperty)
-      .option {
-        manager.createProblemDescriptor(element, descriptionTemplate, isOnTheFly, maybeQuickFix.toArray, highlightType)
-      }
+    val isPassedToProperty: Boolean = ScalaI18nUtil.isPassedToAnnotated(expression, AnnotationUtil.PROPERTY_KEY)
+    if (isPassedToProperty)
+      Some(manager.createProblemDescriptor(element, descriptionTemplate, isOnTheFly, maybeQuickFix.toArray, highlightType))
+    else
+      None
   }
 }
