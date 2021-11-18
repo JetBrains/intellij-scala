3a6979bbeeb263a8d84cb587245202b2c06731e2
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-11-18 14:26:37 +0100

    ScEnumCaseImpl has no end markers, #SCL-19674, #SCL-19675

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/ScEnumImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/ScEnumImpl.scala
@@ -11,7 +11,9 @@ import com.intellij.psi.tree.IElementType
 import org.jetbrains.plugins.scala.caches.ModTracker
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenType
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenType.EnumKeyword
+import org.jetbrains.plugins.scala.lang.psi.api.ScBegin
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScEnumCase
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScEnum, ScTypeDefinition}
 import org.jetbrains.plugins.scala.lang.psi.stubs.ScTemplateDefinitionStub
 import org.jetbrains.plugins.scala.lang.psi.stubs.elements.ScTemplateDefinitionElementType
@@ -22,7 +24,8 @@ final class ScEnumImpl(stub: ScTemplateDefinitionStub[ScEnum],
                        node: ASTNode,
                        debugName: String)
   extends ScTypeDefinitionImpl(stub, nodeType, node, debugName)
-    with ScEnum {
+    with ScEnum
+    with ScBegin {
 
   override def cases: Seq[ScEnumCase] =
     extendsBlock.cases.flatMap(_.declaredElements)
@@ -56,6 +59,8 @@ final class ScEnumImpl(stub: ScTemplateDefinitionStub[ScEnum],
   override protected def baseIcon = icons.Icons.ENUM
 
   override protected def keywordTokenType: IElementType = ScalaTokenType.EnumKeyword
+
+  override def namedTag: Option[ScNamedElement] = Some(this)
 
   override protected def endParent: Option[PsiElement] = extendsBlock.templateBody
 }
