dabae063d39059efd132e3e97c8cc5b2c436d913
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-11-18 14:40:55 +0100

    Compound highlighting of function definition keywords, #SCL-19674, #SCL-19675

--- scala/scala-impl/src/org/jetbrains/plugins/scala/highlighter/usages/ScalaHighlightExitPointsHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/highlighter/usages/ScalaHighlightExitPointsHandler.scala
@@ -2,11 +2,13 @@ package org.jetbrains.plugins.scala.highlighter.usages
 
 import java.util
 import java.util.Collections
-
+import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.{PsiElement, PsiFile}
 import com.intellij.util.Consumer
+import org.jetbrains.plugins.scala.extensions.Parent
+import org.jetbrains.plugins.scala.lang.psi.api.ScBegin
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 
 /**
@@ -18,7 +20,10 @@ class ScalaHighlightExitPointsHandler(fun: ScFunctionDefinition, editor: Editor,
                                       file: PsiFile, keyword: PsiElement)
   extends HighlightUsagesHandlerBase[PsiElement](editor, file) {
   override def computeUsages(targets: util.List[_ <: PsiElement]): Unit = {
-    val usages = fun.returnUsages ++ Set(keyword)
+    val usages = fun.returnUsages ++ (keyword match {
+      case Parent(ScBegin(_, Some(_))) if CodeInsightSettings.getInstance.HIGHLIGHT_BRACES => Set.empty // Highlight as "brace" rather than "usage" (in ScalaBlockSupportHandler)
+      case _ => Set(keyword)
+    })
     usages.map(_.getTextRange).foreach(myReadUsages.add)
   }
 
@@ -28,4 +33,7 @@ class ScalaHighlightExitPointsHandler(fun: ScFunctionDefinition, editor: Editor,
   }
 
   override def getTargets: util.List[PsiElement] = Collections.singletonList(keyword)
+
+  // Cooperate with ScalaBlockSupportHandler
+  override def highlightReferences(): Boolean = true
 }
