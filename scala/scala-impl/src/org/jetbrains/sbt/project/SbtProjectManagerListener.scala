3bbcd918675ce2c5f7133aece8301ffdf565f2e9
Pavel Fatin <pavel.fatin@jetbrains.com>
2021-11-23 15:39:46 +0100

    SbtProjectSettings.converterVersion = 1, #SCL-19573

---
+++ scala/scala-impl/src/org/jetbrains/sbt/project/SbtProjectManagerListener.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.sbt.project
+
+import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
+import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMode
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
+import com.intellij.openapi.project.{Project, ProjectManagerListener}
+import org.jetbrains.plugins.scala.project.ProjectExt
+import org.jetbrains.sbt.project.settings.SbtProjectSettings
+
+class SbtProjectManagerListener extends ProjectManagerListener {
+  override def projectOpened(project: Project): Unit = {
+    SbtProjectSettings.forProject(project).foreach { settings =>
+      if (settings.converterVersion < SbtProjectSettings.ConverterVersion) {
+        if (project.hasScala3 && settings.preferScala2) { // TODO Remove (don't trigger the refresh unnecessarily)
+          // TODO Only do this if auto-import is enabled? (more predictable, on the other hand, it's not about "build scripts", as the setting claims)
+          ExternalSystemUtil.refreshProjects(new ImportSpecBuilder(project, SbtProjectSystem.Id).use(ProgressExecutionMode.IN_BACKGROUND_ASYNC))
+          // The converterVersion will be updated by SbtProjectDataService on a successful refresh.
+          settings.converterVersion = SbtProjectSettings.ConverterVersion // TODO Remove (don't trigger another refresh in any case)
+        }
+      }
+    }
+  }
+}
