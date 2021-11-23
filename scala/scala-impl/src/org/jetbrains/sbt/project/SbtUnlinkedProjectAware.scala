87ff367ad2681b56eb63ffc2fbeca8cb324d6d3a
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2021-11-23 14:28:27 +0300

    do not treat BSP-with-sbt-server project as non-imported sbt project #SCL-19300 fixed
    (cherry picked from commit adb6179418d0a01914850acca3b1a3ec6ac639d1)

--- scala/scala-impl/src/org/jetbrains/sbt/project/SbtUnlinkedProjectAware.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/project/SbtUnlinkedProjectAware.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.sbt.project
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.externalSystem.autolink.{ExternalSystemProjectLinkListener, ExternalSystemUnlinkedProjectAware}
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListener
@@ -8,6 +9,7 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemBundle
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.sbt.Sbt
+import org.jetbrains.sbt.project.SbtUnlinkedProjectAware.Delegate
 import org.jetbrains.sbt.project.settings.SbtProjectSettings
 import org.jetbrains.sbt.settings.SbtSettings
 
@@ -21,8 +23,11 @@ class SbtUnlinkedProjectAware extends ExternalSystemUnlinkedProjectAware {
   override def isBuildFile(project: Project, virtualFile: VirtualFile): Boolean =
     virtualFile.getName == Sbt.BuildFile
 
-  override def isLinkedProject(project: Project, externalProjectPath: String): Boolean =
-    SbtSettings.getInstance(project).getLinkedProjectSettings(externalProjectPath) != null
+  override def isLinkedProject(project: Project, externalProjectPath: String): Boolean = {
+    val sbtSettings = SbtSettings.getInstance(project).getLinkedProjectSettings(externalProjectPath)
+    sbtSettings != null ||
+      SbtUnlinkedProjectAwareHelper.isLinkedProject(project, externalProjectPath)
+  }
 
   override def linkAndLoadProject(project: Project, externalProjectPath: String): Unit =
     new SbtOpenProjectProvider().linkToExistingProject(externalProjectPath, project)
@@ -30,25 +35,46 @@ class SbtUnlinkedProjectAware extends ExternalSystemUnlinkedProjectAware {
   override def subscribe(project: Project,
                          listener: ExternalSystemProjectLinkListener,
                          parentDisposable: Disposable): Unit = {
-    val sbtSettings = SbtSettings.getInstance(project)
-    sbtSettings.subscribe(new Delegate(listener), parentDisposable)
+    val settings = SbtSettings.getInstance(project)
+    settings.subscribe(new Delegate(listener), parentDisposable)
   }
 
   override def getNotificationText: String =
     ExternalSystemBundle.message("unlinked.project.notification.load.action", getSystemId.getReadableName)
 }
 
-//noinspection ApiStatus,UnstableApiUsage
-private class Delegate(listener: ExternalSystemProjectLinkListener)
-  extends ExternalSystemSettingsListener[SbtProjectSettings] {
+object SbtUnlinkedProjectAware {
 
-  override def onProjectsLinked(settings: util.Collection[SbtProjectSettings]) =
-    settings.asScala.foreach(s => listener.onProjectLinked(s.getExternalProjectPath))
+  //noinspection ApiStatus,UnstableApiUsage
+  private class Delegate(listener: ExternalSystemProjectLinkListener)
+    extends ExternalSystemSettingsListener[SbtProjectSettings] {
 
-  override def onProjectsUnlinked(linkedProjectPaths: util.Set[String]) =
-    linkedProjectPaths.asScala.foreach(listener.onProjectUnlinked)
+    override def onProjectsLinked(settings: util.Collection[SbtProjectSettings]): Unit =
+      settings.asScala.foreach(s => listener.onProjectLinked(s.getExternalProjectPath))
 
-  override def onProjectRenamed(oldName: String, newName: String): Unit = {}
-  override def onBulkChangeStart(): Unit = {}
-  override def onBulkChangeEnd(): Unit = {}
+    override def onProjectsUnlinked(linkedProjectPaths: util.Set[String]): Unit =
+      linkedProjectPaths.asScala.foreach(listener.onProjectUnlinked)
+
+    override def onProjectRenamed(oldName: String, newName: String): Unit = {}
+    override def onBulkChangeStart(): Unit = {}
+    override def onBulkChangeEnd(): Unit = {}
+  }
+}
+
+trait SbtUnlinkedProjectAwareHelper {
+
+  /**
+   * Has same semantics as [[ExternalSystemUnlinkedProjectAware.isLinkedProject]]
+   *
+   * @return true when the project which looks like sbt project (i.e. has `build.sbt` file)
+   *         is already imported as some other type of project (e.g. as BSP project, which uses SBT as a server)
+   */
+  def isLinkedProject(project: Project, externalProjectPath: String): Boolean
+}
+
+object SbtUnlinkedProjectAwareHelper {
+  private val EpName = ExtensionPointName.create[SbtUnlinkedProjectAwareHelper]("org.intellij.scala.sbtUnlinkedProjectAwareHelper")
+
+  def isLinkedProject(project: Project, externalProjectPath: String): Boolean =
+    EpName.extensions().anyMatch(_.isLinkedProject(project, externalProjectPath))
 }
