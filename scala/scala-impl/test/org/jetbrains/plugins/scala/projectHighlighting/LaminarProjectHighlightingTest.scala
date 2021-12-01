eac86b0b71818adeae286a7df16aab48ebfa1e2c
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2021-12-01 16:35:19 +0300

    replace obsolete ThreeJSProjectHighlightingTest with LaminarProjectHighlightingTest
    it cannot be imported due to some dependencies on bintray

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/LaminarProjectHighlightingTest.scala
@@ -0,0 +1,17 @@
+package org.jetbrains.plugins.scala.projectHighlighting
+
+import org.jetbrains.plugins.scala.HighlightingTests
+import org.junit.experimental.categories.Category
+
+@Category(Array(classOf[HighlightingTests]))
+class LaminarProjectHighlightingTest extends GithubSbtAllProjectHighlightingTest {
+  override def githubUsername = "raquo"
+  override def githubRepoName = "laminar"
+
+  //0.14.2
+  override def revision = "69063beb52891f8a5d587648cb52607af198e177"
+
+  override def filesWithProblems = Map(
+    "src/main/scala/com/raquo/laminar/api/Laminar.scala" -> Set((13351, 13355))
+  )
+}
