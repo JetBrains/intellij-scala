47cc9f163e46fa1ede21d0d41e147c3f42479bfe
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-02-21 17:05:55 +0100

    Use CompoundRendererProvider instead of extending CompoundReferenceRenderer to avoid internal APIs #SCL-19982

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/ui/ScalaCollectionRendererProvider.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.plugins.scala.debugger.ui
+
+import com.intellij.debugger.ui.tree.render.{ChildrenRenderer, CompoundRendererProvider, ValueLabelRenderer}
+import com.sun.jdi.{ClassType, Type}
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.debugger.filters.ScalaDebuggerSettings
+
+import java.util.concurrent.CompletableFuture
+import java.util.{function => juf}
+import java.{lang => jl}
+
+class ScalaCollectionRendererProvider extends CompoundRendererProvider {
+  import ScalaCollectionRenderer._
+
+  override val getName: String = ScalaBundle.message("scala.collection")
+
+  override val getValueLabelRenderer: ValueLabelRenderer = createSizeLabelRenderer()
+
+  override val getChildrenRenderer: ChildrenRenderer = ScalaToArrayRenderer
+
+  override val isEnabled: Boolean = ScalaDebuggerSettings.getInstance().FRIENDLY_COLLECTION_DISPLAY_ENABLED
+
+  override val getIsApplicableChecker: juf.Function[Type, CompletableFuture[jl.Boolean]] = {
+    case ct: ClassType => forallAsync(instanceOfAsync(ct, collectionClassName), notStreamAsync(ct), notViewAsync(ct))
+    case _ => CompletableFuture.completedFuture(false)
+  }
+}
