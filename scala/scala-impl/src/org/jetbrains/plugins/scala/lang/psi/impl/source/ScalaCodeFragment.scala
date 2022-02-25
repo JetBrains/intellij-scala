fa934b7af3ce4ae70a7a3cda15ea70fd2f1f8e76
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-02-25 13:12:41 +0300

    debugger: references are now resolved to the proper jars during evaluation in case there are conflicting libraries #SCL-19961 fixed
    - for the details see ScalaDoc for org.jetbrains.plugins.scala.lang.psi.impl.source.GlobalSearchScopeWithRecommendedResultsSorting

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/source/ScalaCodeFragment.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/source/ScalaCodeFragment.scala
@@ -1,8 +1,4 @@
-package org.jetbrains.plugins.scala
-package lang
-package psi
-package impl
-package source
+package org.jetbrains.plugins.scala.lang.psi.impl.source
 
 import com.intellij.openapi.command.undo.{BasicUndoableAction, UndoManager}
 import com.intellij.openapi.project.Project
@@ -13,24 +9,24 @@ import com.intellij.psi.impl.PsiManagerEx
 import com.intellij.psi.impl.source.PsiFileImpl
 import com.intellij.psi.impl.source.tree.FileElement
 import com.intellij.psi.scope.PsiScopeProcessor
-import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.{DelegatingGlobalSearchScope, GlobalSearchScope}
 import com.intellij.testFramework.LightVirtualFile
+import org.jetbrains.plugins.scala.ScalaLanguage
+import org.jetbrains.plugins.scala.lang.psi.ScDeclarationSequenceHolder
 import org.jetbrains.plugins.scala.lang.psi.ScImportsHolder.ImportPath
 import org.jetbrains.plugins.scala.lang.psi.api.base.{ScReference, ScStableCodeReference}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScReferenceExpression
+import org.jetbrains.plugins.scala.lang.psi.impl.{ScalaFileImpl, ScalaPsiElementFactory}
 
 import scala.collection.mutable
 
-/**
-  * @author Alexander Podkhalyuzin
-  */
 final class ScalaCodeFragment private(private var viewProvider: SingleRootFileViewProvider)
   extends ScalaFileImpl(viewProvider)
     with JavaCodeFragment
     with IntentionFilterOwner
     with ScDeclarationSequenceHolder {
 
-  import ScalaPsiElementFactory._
+  import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory._
 
   getViewProvider.forceCachedPsi(this)
 
@@ -72,10 +68,14 @@ final class ScalaCodeFragment private(private var viewProvider: SingleRootFileVi
   override def getExceptionHandler: ExceptionHandler = exceptionHandler
 
   override def forceResolveScope(resolveScope: GlobalSearchScope): Unit = {
-    this.resolveScope = resolveScope
+    val wrapperScope = new DelegatingGlobalSearchScope(resolveScope) with GlobalSearchScopeWithRecommendedResultsSorting
+    this.resolveScope = wrapperScope
   }
 
   override def getForcedResolveScope: GlobalSearchScope = resolveScope
+
+  override def getResolveScope: GlobalSearchScope =
+    super.getResolveScope
 
   override def setIntentionActionsFilter(filter: IntentionActionsFilter): Unit = {
     this.filter = filter
@@ -128,7 +128,8 @@ final class ScalaCodeFragment private(private var viewProvider: SingleRootFileVi
                                    lastParent: PsiElement, place: PsiElement): Boolean = {
     for (qName <- imports) {
       val imp = ScalaPsiElementFactory.createImportFromText(s"import _root_.$qName", this, this)
-      if (!imp.processDeclarations(processor, state, lastParent, place)) return false
+      if (!imp.processDeclarations(processor, state, lastParent, place))
+        return false
     }
 
     super[ScDeclarationSequenceHolder].processDeclarations(processor, state, lastParent, place)
@@ -165,10 +166,11 @@ final class ScalaCodeFragment private(private var viewProvider: SingleRootFileVi
 
 object ScalaCodeFragment {
 
-  def apply(text: String,
-            context: PsiElement = null,
-            child: PsiElement = null)
-           (implicit project: Project): ScalaCodeFragment = {
+  def apply(
+    text: String,
+    context: PsiElement = null,
+    child: PsiElement = null
+  )(implicit project: Project): ScalaCodeFragment = {
     val language = Option(context)
       .map(_.getContainingFile.getLanguage)
       .filter(_.isKindOf(ScalaLanguage.INSTANCE))
@@ -184,5 +186,42 @@ object ScalaCodeFragment {
     fragment.child = child
     fragment
   }
+}
 
+/**
+ * Marker trait which can be mixed into some scope (e.g. using wrapper [[DelegatingGlobalSearchScope]])
+ * This marker indicates that resolve results, obtained from this scope, should be sorted using [[GlobalSearchScope#compare]]
+ *
+ * Sorting might be required when some scope contains collisions for the same fully-qualified-name
+ * (e.g. if the scope contains multiple jar files for the same library and some class is resolved into all of them)
+ *
+ * This can happen e.g. when global project scope is used ([[GlobalSearchScope.allScope]],
+ * which includes all libraries jars (potentially with different library versions)
+ * Even if it's a simple sbt project with one module, the issue is actual: we have a special `-build` module which
+ * contains jars for sbt itself and which uses it's own scala-library.jar. This module is also included into the global
+ * scope resulting into conflicting scala-library jars
+ *
+ * Example of global project scope can be found in debugger evaluator subsystem
+ * (this was the primary reason for this marker trait).
+ * In this method
+ * [[com.intellij.debugger.engine.evaluation.CodeFragmentFactoryContextWrapper.prepareResolveScope]]
+ * our resolve scope [[ScalaCodeFragment.getResolveScope]] is wrapped with a another scope which includes global scope.
+ * Notice that [[GlobalSearchScope#compare]] method is being implemented implying that it will be used
+ * (although there is no any contract which says that it should be used)
+ *
+ * @note we can't force all users of this scope to sort the results, because there is no any documented contract for
+ *       [[GlobalSearchScope#compare]] usage. Even in Java plugin it's used not everywhere
+ * @note it's also possible that somewhere under the hood during resolve process this scope will be wrapped with another
+ *       wrapper scope. In this case this marker will be "lost". In theory there might be some issues related to that.
+ *       Ideally this marker trait should be integrated into the IntelliJ Platform and it probably should be
+ *       preserved when doing operations on scope: wrap/union/intersect, etc...
+ *       Also, idially, [[GlobalSearchScope#compare]] contract should be provided
+ * @note Why do we even need wrapping [[ScalaCodeFragment.getResolveScope]] with [[GlobalSearchScope.allScope]]?<br>
+ *       According to Egor Ushakov: "as far as I remember it's in order to be able to call any methods"<br>
+ *       For example, we might split our project into 3 modules A -> B -> C which are later packaged into a single jar (`->` means "depends on").
+ *       If we debug some file in module "C" we might still want to evaluate something from module "A" without leaving current context
+ *       (though the evaluated method is not accessible from module "C" resolve scope)
+ */
+trait GlobalSearchScopeWithRecommendedResultsSorting {
+  self: GlobalSearchScope =>
 }
