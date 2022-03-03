f7a7a200ffc30a171d21d244be9b9a3e01cf90f7
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-03-03 12:51:22 +0100

    Simplify if evaluator
    - Extend the Java if evaluator instead of duplicating the code

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaIfEvaluator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaIfEvaluator.scala
@@ -1,44 +1,15 @@
 package org.jetbrains.plugins.scala.debugger.evaluation.evaluator
 
-import com.intellij.debugger.engine.evaluation.expression.{Evaluator, Modifier}
-import com.intellij.debugger.engine.evaluation.{EvaluateExceptionUtil, EvaluationContextImpl}
-import com.sun.jdi.BooleanValue
+import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
+import com.intellij.debugger.engine.evaluation.expression.{Evaluator, IfStatementEvaluator}
 
-/**
- * User: Alefas
- * Date: 19.10.11
- */
+class ScalaIfEvaluator(condition: Evaluator, ifBranch: Evaluator, elseBranch: Option[Evaluator])
+  extends IfStatementEvaluator(condition, ifBranch, elseBranch.orNull) {
 
-class ScalaIfEvaluator(condition: Evaluator, ifBranch: Evaluator, elseBranch: Option[Evaluator]) extends Evaluator {
-
-  private var modifier: Modifier = _
+  private val elseBranchIsDefined: Boolean = elseBranch.isDefined
 
   override def evaluate(context: EvaluationContextImpl): AnyRef = {
-    var value: AnyRef = condition.evaluate(context)
-    value match {
-      case v: BooleanValue =>
-        if (v.booleanValue) {
-          value = ifBranch.evaluate(context)
-          modifier = ifBranch.getModifier
-        }
-        else {
-          elseBranch match {
-            case Some(branch) =>
-              value = branch.evaluate(context)
-              modifier = branch.getModifier
-              return value
-            case None =>
-              modifier = null
-          }
-        }
-      case _ => throw EvaluateExceptionUtil.BOOLEAN_EXPECTED
-    }
-    
-    if (elseBranch.isEmpty)
-      value = context.getDebugProcess.getVirtualMachineProxy.mirrorOfVoid()
-
-    value
+    val result = super.evaluate(context)
+    if (elseBranchIsDefined) result else context.getDebugProcess.getVirtualMachineProxy.mirrorOfVoid()
   }
-
-  override def getModifier: Modifier = modifier
 }
