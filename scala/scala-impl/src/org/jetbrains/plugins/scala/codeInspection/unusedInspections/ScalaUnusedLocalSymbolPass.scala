d488cf2d82e024f0ed0e95a1e3e779dc5ffc3bbc
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2022-03-16 12:40:10 +0100

    A checkbox for "Report public symbols" in ScalaUnusedSymbolInspection in the properties UI
    https://youtrack.jetbrains.com/issue/SCL-20006
    
    This is the first version.
    The property controlled by the checkbox is global for now - it needs to be done per profile.
    The quick fix is not yet implemented.
    In the properties UI, if I check/uncheck the checkbox, the "Apply" button is not being enabled.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/unusedInspections/ScalaUnusedLocalSymbolPass.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/unusedInspections/ScalaUnusedLocalSymbolPass.scala
@@ -7,4 +7,8 @@ import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
   * Created by Svyatoslav Ilinskiy on 11.07.16.
   */
 class ScalaUnusedLocalSymbolPass(file: ScalaFile, doc: Option[Document]) extends
-  InspectionBasedHighlightingPass(file, doc, new ScalaUnusedSymbolInspection)
+  InspectionBasedHighlightingPass(file, doc, ScalaUnusedLocalSymbolPass.inspection)
+
+object ScalaUnusedLocalSymbolPass {
+  lazy val inspection: ScalaUnusedSymbolInspection = new ScalaUnusedSymbolInspection
+}
