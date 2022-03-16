ecd0e7a4756b2b513e1210a7902538362b1328a1
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-03-16 15:03:52 +0300

    minor: cleanup of ScalaTypeHierarchyProvider.scala

--- scala/scala-impl/src/org/jetbrains/plugins/scala/hierarchy/ScalaTypeHierarchyProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/hierarchy/ScalaTypeHierarchyProvider.scala
@@ -1,19 +1,12 @@
-package org.jetbrains.plugins.scala
-package hierarchy
-
+package org.jetbrains.plugins.scala.hierarchy
 
 import com.intellij.ide.hierarchy.HierarchyBrowser
 import com.intellij.ide.hierarchy.`type`.JavaTypeHierarchyProvider
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
 
+final class ScalaTypeHierarchyProvider extends JavaTypeHierarchyProvider {
 
-
-/**
- * User: Alexander Podkhalyuzin
- * Date: 09.06.2009
- */
-class ScalaTypeHierarchyProvider extends JavaTypeHierarchyProvider {
   override def createHierarchyBrowser(target: PsiElement): HierarchyBrowser = {
     target match {
       case clazz: ScTypeDefinition =>
@@ -23,11 +16,11 @@ class ScalaTypeHierarchyProvider extends JavaTypeHierarchyProvider {
     super.createHierarchyBrowser(target)
   }
 
-  def collectSupers(clazz: ScTypeDefinition, visited: Set[ScTypeDefinition]): Unit = {
+  private def collectSupers(clazz: ScTypeDefinition, visited: Set[ScTypeDefinition]): Unit = {
     clazz.supers.foreach {
       case clazz: ScTypeDefinition =>
         if (visited.contains(clazz)) {
-          println("clazz.getText = " + clazz.getText)
+          //println("clazz.getText = " + clazz.getText)
         } else {
           collectSupers(clazz, visited + clazz)
         }
