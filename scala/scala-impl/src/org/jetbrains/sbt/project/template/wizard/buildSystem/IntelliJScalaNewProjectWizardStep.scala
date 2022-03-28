46076488434f4118fe825ede6a3dd2831ca070c2
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-03-28 20:30:22 +0300

    NPW: don't allow to create Scala project if no Scala SDK is specified (IntelliJ build system) #SCL-7964 fixed

--- scala/scala-impl/src/org/jetbrains/sbt/project/template/wizard/buildSystem/IntelliJScalaNewProjectWizardStep.scala
+++ scala/scala-impl/src/org/jetbrains/sbt/project/template/wizard/buildSystem/IntelliJScalaNewProjectWizardStep.scala
@@ -6,6 +6,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.openapi.roots.ui.configuration.projectRoot.{LibrariesContainer, LibrariesContainerFactory}
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.ui.validation.DialogValidation
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.ui.dsl.builder.{Panel, Row, RowLayout}
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
@@ -13,11 +15,18 @@ import kotlin.Unit.{INSTANCE => KUnit}
 import org.jetbrains.plugins.scala.extensions.ObjectExt
 import org.jetbrains.plugins.scala.project.ScalaLibraryProperties
 import org.jetbrains.plugins.scala.project.template.{ScalaModuleBuilder, ScalaSDKStepLike}
+import org.jetbrains.sbt.SbtBundle
 import org.jetbrains.sbt.project.template.wizard.ScalaNewProjectWizardStep
 
 import java.nio.file.Paths
-import javax.swing.JComponent
+import javax.swing.{JComboBox, JComponent}
 
+/**
+ * TODO log FUS events like in
+ *  - org.jetbrains.idea.maven.wizards.MavenJavaNewProjectWizard
+ *  - com.intellij.ide.projectWizard.generators.IntelliJJavaNewProjectWizard
+ *  - org.jetbrains.plugins.gradle.service.project.wizard.GradleJavaNewProjectWizard
+ */
 /** inspired by [[com.intellij.ide.projectWizard.generators.IntelliJJavaNewProjectWizard]] */
 final class IntelliJScalaNewProjectWizardStep(parent: ScalaNewProjectWizardStep)
   extends IntelliJNewProjectWizardStep[ScalaNewProjectWizardStep](parent)
@@ -66,7 +75,21 @@ final class IntelliJScalaNewProjectWizardStep(parent: ScalaNewProjectWizardStep)
       // WORKAROUND: `row.cell(simplePanel)` adds some strange indent to the left which looks ugly
       // So we add all children (library dropdown & "create" button)
       // (I didn't find a a proper way to fix the border)
-      components.foreach(row.cell(_))
+      components.foreach { component =>
+        val cell = component match {
+          case comboBox: JComboBox[_] =>
+            //apply validation only for the combobox component with the library selection
+            row.cell(comboBox).validation((() => {
+              if (comboBox.getSelectedIndex == -1)
+                new ValidationInfo(SbtBundle.message("scala.sdk.must.be.set"))
+              else
+                null
+            }): DialogValidation)
+          case _ =>
+            row.cell(component)
+        }
+        cell
+      }
       KUnit
     })
   }
