1dbf97ba17c6321fcee8fe985efe18e5f0aaaf8c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-03-30 10:39:48 +0200

    Improve error messages for new value class instance evaluator #SCL-20072

--- scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/NewValueClassInstanceEvaluator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/NewValueClassInstanceEvaluator.scala
@@ -1,10 +1,11 @@
 package org.jetbrains.plugins.scala.debugger.evaluation.evaluator
 
 import com.intellij.debugger.JavaDebuggerBundle
-import com.intellij.debugger.engine.evaluation.EvaluateExceptionUtil.createEvaluateException
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
 import com.intellij.debugger.engine.evaluation.expression.Evaluator
 import com.sun.jdi.{ClassType, ObjectReference, Value}
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.debugger.evaluation.EvaluationException
 
 import scala.jdk.CollectionConverters._
 import scala.util.Try
@@ -13,18 +14,17 @@ class NewValueClassInstanceEvaluator(typeEvaluator: ScalaTypeEvaluator, param: E
 
   override def evaluate(context: EvaluationContextImpl): ObjectReference = {
     val debugProcess = context.getDebugProcess
-    val classTypeInstance = typeEvaluator.evaluate(context) match {
-      case ct: ClassType => ct
+    val ct = typeEvaluator.evaluate(context) match {
+      case c: ClassType => c
       case _ =>
-        throw createEvaluateException(JavaDebuggerBundle.message("evaluation.error.cannot.evaluate.class.type"))
+        throw EvaluationException(JavaDebuggerBundle.message("evaluation.error.cannot.evaluate.class.type"))
     }
 
-    val ctor =
-      Try(classTypeInstance.methodsByName("<init>").get(0))
-        .getOrElse(throw createEvaluateException(JavaDebuggerBundle.message("evaluation.error.cannot.resolve.constructor", "")))
+    val ctor = Try(ct.methodsByName("<init>").get(0))
+      .getOrElse(throw EvaluationException(ScalaBundle.message("error.cannot.resolve.value.class.primary.constructor", ct)))
 
     val arg = Option(param.evaluate(context)).collect { case v: Value => v }.orNull
 
-    debugProcess.newInstance(context, classTypeInstance, ctor, List(arg).asJava)
+    debugProcess.newInstance(context, ct, ctor, List(arg).asJava)
   }
 }
