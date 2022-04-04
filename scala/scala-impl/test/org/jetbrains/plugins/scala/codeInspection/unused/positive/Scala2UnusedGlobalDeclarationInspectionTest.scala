ca3ca85cb31a01dab9d1472514ce438b928045dd
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2022-04-04 18:08:11 +0200

    Mark unused auxiliary constructors and overloaded methods Fixed #SCL-20068

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/unused/positive/Scala2UnusedGlobalDeclarationInspectionTest.scala
@@ -0,0 +1,71 @@
+package org.jetbrains.plugins.scala.codeInspection.unused.positive
+
+import org.jetbrains.plugins.scala.codeInspection.unused.ScalaUnusedDeclarationInspectionTestBase
+
+class Scala2UnusedGlobalDeclarationInspectionTest extends ScalaUnusedDeclarationInspectionTestBase {
+  private def addFile(text: String): Unit = myFixture.addFileToProject("Foo.scala", text)
+
+  def test_auxiliary_constructors(): Unit = {
+    addFile(
+      """
+        | object UnusedConstructor {
+        |   val foo = new Foo()
+        | }
+        |"""
+        .stripMargin)
+    checkTextHasError(
+      s"""
+        |  import scala.annotation.unused
+        |  @unused class Foo(@unused foo: String, @unused n: Int) {
+        |    def this() = this("foo", 0)
+        |    def ${START}this${END}(str: String) = this(str, 0)
+        |  }
+        |""".stripMargin)
+  }
+
+  def test_overloaded_methods(): Unit = {
+    addFile(
+      """
+        | object UnusedConstructor {
+        |   val foo = new Foo()
+        |   foo.aaa()
+        | }
+        |"""
+        .stripMargin)
+    checkTextHasError(
+      s"""
+        |  import scala.annotation.unused
+        |  @unused class Foo{
+        |    def aaa(): Unit = {}
+        |    def ${START}aaa${END}(@unused str: String): Unit = {}
+        |  }
+        |""".stripMargin)
+  }
+
+  private def testOperatorNotUsed(operatorName: String): Unit = {
+    checkTextHasError(
+      s"""
+         | class Num(n: Int) {
+         |   def ${START}$operatorName${END}(n: Int): Num = new Num(n + this.n)
+         | }
+         |""".stripMargin)
+  }
+
+  def test_plus_not_used(): Unit = testOperatorNotUsed("+")
+  def test_minus_not_used(): Unit = testOperatorNotUsed("-")
+  def test_tilde_not_used(): Unit = testOperatorNotUsed("~")
+  def test_eqeq_not_used(): Unit = testOperatorNotUsed("==")
+  def test_less_not_used(): Unit = testOperatorNotUsed("<")
+  def test_lesseq_not_used(): Unit = testOperatorNotUsed("<=")
+  def test_greater_not_used(): Unit = testOperatorNotUsed(">")
+  def test_greatereq_not_used(): Unit = testOperatorNotUsed(">=")
+  def test_bang_not_used(): Unit = testOperatorNotUsed("!")
+  def test_percent_not_used(): Unit = testOperatorNotUsed("%")
+  def test_up_not_used(): Unit = testOperatorNotUsed("^")
+  def test_amp_not_used(): Unit = testOperatorNotUsed("&")
+  def test_bar_not_used(): Unit = testOperatorNotUsed("|")
+  def test_times_not_used(): Unit = testOperatorNotUsed("*")
+  def test_div_not_used(): Unit = testOperatorNotUsed("/")
+  def test_bslash_not_used(): Unit = testOperatorNotUsed("\\")
+  def test_qmark_not_used(): Unit = testOperatorNotUsed("?")
+}
