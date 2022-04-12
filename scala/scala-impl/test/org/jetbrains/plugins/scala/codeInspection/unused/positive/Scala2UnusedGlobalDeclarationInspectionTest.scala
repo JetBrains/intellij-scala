c73885d1b0f1ac1fdc15edc456acbc1f0bc658ce
Izmar Verhage <izmar.verhage@jetbrains.com>
2022-04-12 17:57:03 +0200

    Let literal occurence not count as usage #SCL-20045 Fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/unused/positive/Scala2UnusedGlobalDeclarationInspectionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/unused/positive/Scala2UnusedGlobalDeclarationInspectionTest.scala
@@ -42,7 +42,7 @@ class Scala2UnusedGlobalDeclarationInspectionTest extends ScalaUnusedDeclaration
         |""".stripMargin)
   }
 
-  private def testOperatorNotUsed(operatorName: String): Unit = {
+  private def testOperator(operatorName: String): Unit = {
     checkTextHasError(
       s"""
          | class Num(n: Int) {
@@ -51,21 +51,36 @@ class Scala2UnusedGlobalDeclarationInspectionTest extends ScalaUnusedDeclaration
          |""".stripMargin)
   }
 
-  def test_plus_not_used(): Unit = testOperatorNotUsed("+")
-  def test_minus_not_used(): Unit = testOperatorNotUsed("-")
-  def test_tilde_not_used(): Unit = testOperatorNotUsed("~")
-  def test_eqeq_not_used(): Unit = testOperatorNotUsed("==")
-  def test_less_not_used(): Unit = testOperatorNotUsed("<")
-  def test_lesseq_not_used(): Unit = testOperatorNotUsed("<=")
-  def test_greater_not_used(): Unit = testOperatorNotUsed(">")
-  def test_greatereq_not_used(): Unit = testOperatorNotUsed(">=")
-  def test_bang_not_used(): Unit = testOperatorNotUsed("!")
-  def test_percent_not_used(): Unit = testOperatorNotUsed("%")
-  def test_up_not_used(): Unit = testOperatorNotUsed("^")
-  def test_amp_not_used(): Unit = testOperatorNotUsed("&")
-  def test_bar_not_used(): Unit = testOperatorNotUsed("|")
-  def test_times_not_used(): Unit = testOperatorNotUsed("*")
-  def test_div_not_used(): Unit = testOperatorNotUsed("/")
-  def test_bslash_not_used(): Unit = testOperatorNotUsed("\\")
-  def test_qmark_not_used(): Unit = testOperatorNotUsed("?")
+  def test_plus(): Unit = testOperator("+")
+  def test_minus(): Unit = testOperator("-")
+  def test_tilde(): Unit = testOperator("~")
+  def test_eqeq(): Unit = testOperator("==")
+  def test_less(): Unit = testOperator("<")
+  def test_lesseq(): Unit = testOperator("<=")
+  def test_greater(): Unit = testOperator(">")
+  def test_greatereq(): Unit = testOperator(">=")
+  def test_bang(): Unit = testOperator("!")
+  def test_percent(): Unit = testOperator("%")
+  def test_up(): Unit = testOperator("^")
+  def test_amp(): Unit = testOperator("&")
+  def test_bar(): Unit = testOperator("|")
+  def test_times(): Unit = testOperator("*")
+  def test_div(): Unit = testOperator("/")
+  def test_bslash(): Unit = testOperator("\\")
+  def test_qmark(): Unit = testOperator("?")
+
+  def test_presence_in_string_literal(): Unit = {
+    addFile("\"Abc\"")
+    checkTextHasError(s"trait ${START}Abc$END")
+  }
+
+  def test_reference_pattern_with_same_name(): Unit = {
+    addFile("object Ctx { val Abc = 42 }")
+    checkTextHasError(s"trait ${START}Abc$END")
+  }
+
+  def test_other_type_definition_with_same_name(): Unit = {
+    addFile("object Ctx1 { trait Abc }")
+    checkTextHasError(s"@scala.annotation.unused object Ctx2 { trait ${START}Abc$END }")
+  }
 }
