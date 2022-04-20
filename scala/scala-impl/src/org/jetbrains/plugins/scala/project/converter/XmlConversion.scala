c76fedf08e7f1fde621d0d12d962dd6864bd343c
Pavel Fatin <pavel.fatin@jetbrains.com>
2022-04-20 15:55:16 +0200

    intellijVersion = 222.1665

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/converter/XmlConversion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/converter/XmlConversion.scala
@@ -2,15 +2,12 @@ package org.jetbrains.plugins.scala.project.converter
 
 import com.intellij.openapi.util.JDOMUtil
 import org.jdom.Element
-import org.jdom.output.{Format, XMLOutputter}
 
 import scala.xml.{Elem, PrettyPrinter}
 
 trait XmlConversion {
-  protected def formatXml(element: Element): String = {
-    val outputter = new XMLOutputter(Format.getPrettyFormat)
-    outputter.outputString(element)
-  }
+  protected def formatXml(element: Element): String =
+    JDOMUtil.write(element)
 
   protected def formatXml(element: Elem): String = {
     val printer = new PrettyPrinter(180, 2)
