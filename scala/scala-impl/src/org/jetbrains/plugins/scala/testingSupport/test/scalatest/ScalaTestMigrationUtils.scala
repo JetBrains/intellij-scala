c343bd757fe23218c2da66bf420ce2008d6ac900
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-04-25 13:23:03 +0300

    test integration: detect frameworks in scala 3 code (#SCL-20136, #SCL-20132) fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestMigrationUtils.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestMigrationUtils.scala
@@ -1,11 +1,18 @@
 package org.jetbrains.plugins.scala.testingSupport.test.scalatest
 
+import scala.collection.immutable.ListMap
+
 /**
  * ScalaTest 3.1.0 had a lot of refactorings. A lot of classes moved to other packages.
  * This utility class helps to migrate old (prior 3.1.0) class full qualified names to new ones.
- * see for details: http://www.scalatest.org/release_notes/3.1.0
+ *
+ *  - In 3.1.0 old-style names were  deprecated but continued to work
+ *  - In 3.2.0 old-style names were deprecated
+ *
+ * @see http://www.scalatest.org/release_notes/3.1.0
+ * @see http://www.scalatest.org/release_notes/3.2.0
  */
-private object ScalaTestMigrationUtils {
+object ScalaTestMigrationUtils {
 
   object MigrationOps {
 
@@ -52,11 +59,12 @@ private object ScalaTestMigrationUtils {
   //
 
   private val FunSuite =
-    Map(
+    ListMap(
       "org.scalatest.FunSuite" -> "org.scalatest.funsuite.AnyFunSuite",
       "org.scalatest.FunSuiteLike" -> "org.scalatest.funsuite.AnyFunSuiteLike",
       "org.scalatest.AsyncFunSuite" -> "org.scalatest.funsuite.AsyncFunSuite",
       "org.scalatest.AsyncFunSuiteLike" -> "org.scalatest.funsuite.AsyncFunSuiteLike",
+
       "org.scalatest.fixture.FunSuite" -> "org.scalatest.funsuite.FixtureAnyFunSuite",
       "org.scalatest.fixture.FunSuiteLike" -> "org.scalatest.funsuite.FixtureAnyFunSuiteLike",
       "org.scalatest.fixture.AsyncFunSuite" -> "org.scalatest.funsuite.FixtureAsyncFunSuite",
@@ -64,11 +72,12 @@ private object ScalaTestMigrationUtils {
     )
 
   private val FlatSpec =
-    Map(
+    ListMap(
       "org.scalatest.FlatSpec" -> "org.scalatest.flatspec.AnyFlatSpec",
       "org.scalatest.FlatSpecLike" -> "org.scalatest.flatspec.AnyFlatSpecLike",
       "org.scalatest.AsyncFlatSpec" -> "org.scalatest.flatspec.AsyncFlatSpec",
       "org.scalatest.AsyncFlatSpecLike" -> "org.scalatest.flatspec.AsyncFlatSpecLike",
+
       "org.scalatest.fixture.FlatSpec" -> "org.scalatest.flatspec.FixtureAnyFlatSpec",
       "org.scalatest.fixture.FlatSpecLike" -> "org.scalatest.flatspec.FixtureAnyFlatSpecLike",
       "org.scalatest.fixture.AsyncFlatSpec" -> "org.scalatest.flatspec.FixtureAsyncFlatSpec",
@@ -76,25 +85,28 @@ private object ScalaTestMigrationUtils {
     )
 
   private val FunSpec =
-    Map(
+    ListMap(
       "org.scalatest.FunSpec" -> "org.scalatest.funspec.AnyFunSpec",
       "org.scalatest.FunSpecLike" -> "org.scalatest.funspec.AnyFunSpecLike",
       "org.scalatest.AsyncFunSpec" -> "org.scalatest.funspec.AsyncFunSpec",
       "org.scalatest.AsyncFunSpecLike" -> "org.scalatest.funspec.AsyncFunSpecLike",
+
       "org.scalatest.fixture.FunSpec" -> "org.scalatest.funspec.FixtureAnyFunSpec",
       "org.scalatest.fixture.FunSpecLike" -> "org.scalatest.funspec.FixtureAnyFunSpecLike",
       "org.scalatest.fixture.AsyncFunSpec" -> "org.scalatest.funspec.FixtureAsyncFunSpec",
       "org.scalatest.fixture.AsyncFunSpecLike" -> "org.scalatest.funspec.FixtureAsyncFunSpecLike",
+
       "org.scalatest.path.FunSpec" -> "org.scalatest.funspec.PathAnyFunSpec",
       "org.scalatest.path.FunSpecLike" -> "org.scalatest.funspec.PathAnyFunSpecLike",
     )
 
   private val WordSpec =
-    Map(
+    ListMap(
       "org.scalatest.WordSpec" -> "org.scalatest.wordspec.AnyWordSpec",
       "org.scalatest.WordSpecLike" -> "org.scalatest.wordspec.AnyWordSpecLike",
       "org.scalatest.AsyncWordSpec" -> "org.scalatest.wordspec.AsyncWordSpec",
       "org.scalatest.AsyncWordSpecLike" -> "org.scalatest.wordspec.AsyncWordSpecLike",
+
       "org.scalatest.fixture.WordSpec" -> "org.scalatest.wordspec.FixtureAnyWordSpec",
       "org.scalatest.fixture.WordSpecLike" -> "org.scalatest.wordspec.FixtureAnyWordSpecLike",
       "org.scalatest.fixture.AsyncWordSpec" -> "org.scalatest.wordspec.FixtureAsyncWordSpec",
@@ -102,25 +114,28 @@ private object ScalaTestMigrationUtils {
     )
 
   private val FreeSpec =
-    Map(
+    ListMap(
       "org.scalatest.FreeSpec" -> "org.scalatest.freespec.AnyFreeSpec",
       "org.scalatest.FreeSpecLike" -> "org.scalatest.freespec.AnyFreeSpecLike",
       "org.scalatest.AsyncFreeSpec" -> "org.scalatest.freespec.AsyncFreeSpec",
       "org.scalatest.AsyncFreeSpecLike" -> "org.scalatest.freespec.AsyncFreeSpecLike",
+
       "org.scalatest.fixture.FreeSpec" -> "org.scalatest.freespec.FixtureAnyFreeSpec",
       "org.scalatest.fixture.FreeSpecLike" -> "org.scalatest.freespec.FixtureAnyFreeSpecLike",
       "org.scalatest.fixture.AsyncFreeSpec" -> "org.scalatest.freespec.FixtureAsyncFreeSpec",
       "org.scalatest.fixture.AsyncFreeSpecLike" -> "org.scalatest.freespec.FixtureAsyncFreeSpecLike",
+
       "org.scalatest.path.FreeSpec" -> "org.scalatest.freespec.PathAnyFreeSpec",
       "org.scalatest.path.FreeSpecLike" -> "org.scalatest.freespec.PathAnyFreeSpecLike",
     )
 
   private val PropSpec =
-    Map(
+    ListMap(
       "org.scalatest.PropSpec" -> "org.scalatest.propspec.AnyPropSpec",
       "org.scalatest.PropSpecLike" -> "org.scalatest.propspec.AnyPropSpecLike",
       "org.scalatest.AsyncPropSpec" -> "org.scalatest.propspec.AsyncPropSpec",
       "org.scalatest.AsyncPropSpecLike" -> "org.scalatest.propspec.AsyncPropSpecLike",
+
       "org.scalatest.fixture.PropSpec" -> "org.scalatest.propspec.FixtureAnyPropSpec",
       "org.scalatest.fixture.PropSpecLike" -> "org.scalatest.propspec.FixtureAnyPropSpecLike",
       "org.scalatest.fixture.AsyncPropSpec" -> "org.scalatest.propspec.FixtureAsyncPropSpec",
@@ -128,11 +143,12 @@ private object ScalaTestMigrationUtils {
     )
 
   private val FeatureSpec =
-    Map(
+    ListMap(
       "org.scalatest.FeatureSpec" -> "org.scalatest.featurespec.AnyFeatureSpec",
       "org.scalatest.FeatureSpecLike" -> "org.scalatest.featurespec.AnyFeatureSpecLike",
       "org.scalatest.AsyncFeatureSpec" -> "org.scalatest.featurespec.AsyncFeatureSpec",
       "org.scalatest.AsyncFeatureSpecLike" -> "org.scalatest.featurespec.AsyncFeatureSpecLike",
+
       "org.scalatest.fixture.FeatureSpec" -> "org.scalatest.featurespec.FixtureAnyFeatureSpec",
       "org.scalatest.fixture.FeatureSpecLike" -> "org.scalatest.featurespec.FixtureAnyFeatureSpecLike",
       "org.scalatest.fixture.AsyncFeatureSpec" -> "org.scalatest.featurespec.FixtureAsyncFeatureSpec",
