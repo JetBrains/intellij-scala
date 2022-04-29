15a504f46e1daa63977ae3750e153bfecf7ebe26
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2022-04-30 01:41:54 +0400

    Update VCS code author inlay hints for Scala
    Use new API
    
    #SCL-19557

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInsight/hints/codeVision/ScalaVcsCodeVisionContext.scala
@@ -0,0 +1,41 @@
+//noinspection ApiStatus,UnstableApiUsage
+package org.jetbrains.plugins.scala.codeInsight.hints.codeVision
+
+import com.intellij.codeInsight.daemon.impl.JavaCodeVisionUsageCollector.{CLASS_LOCATION, METHOD_LOCATION, logCodeAuthorClicked}
+import com.intellij.codeInsight.hints.VcsCodeVisionLanguageContext
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.extensions.ObjectExt
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScExtension, ScFunction}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
+
+import java.awt.event.MouseEvent
+
+final class ScalaVcsCodeVisionContext extends VcsCodeVisionLanguageContext {
+
+  import ScalaVcsCodeVisionContext._
+
+  override def isAccepted(element: PsiElement): Boolean =
+    isAcceptedTemplateDefinition(element) || isAcceptedMember(element)
+
+  override def handleClick(mouseEvent: MouseEvent, editor: Editor, element: PsiElement): Unit = {
+    val project = element.getProject
+    val location = if (isAcceptedTemplateDefinition(element)) CLASS_LOCATION else METHOD_LOCATION
+
+    logCodeAuthorClicked(project, location)
+  }
+}
+
+object ScalaVcsCodeVisionContext {
+  private def isAcceptedTemplateDefinition(element: PsiElement): Boolean =
+    element.is[ScClass, ScObject, ScTrait, ScEnum, ScGivenDefinition]
+
+  private def isAcceptedMember(element: PsiElement): Boolean = element match {
+    case fn: ScFunction => isAcceptedMember(fn)
+    case ext: ScExtension => isAcceptedMember(ext)
+    case _ => false
+  }
+
+  private def isAcceptedMember(member: ScMember): Boolean =
+    member.isTopLevel || isAcceptedTemplateDefinition(member.containingClass)
+}
