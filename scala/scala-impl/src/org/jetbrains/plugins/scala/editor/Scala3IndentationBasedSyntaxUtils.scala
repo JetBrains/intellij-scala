7c7be079f2e1f99501e6f1b356c81d655c55e69f
markuswt <53354649+markuswt@users.noreply.github.com>
2022-05-02 19:41:34 +0200

    Add Scala 3 braceless syntax utility functions #SCL-18756

--- scala/scala-impl/src/org/jetbrains/plugins/scala/editor/Scala3IndentationBasedSyntaxUtils.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/editor/Scala3IndentationBasedSyntaxUtils.scala
@@ -2,21 +2,50 @@ package org.jetbrains.plugins.scala.editor
 
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.extensions.PsiElementExt
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenType.ThenKeyword
-import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.lexer.{ScalaTokenType, ScalaTokenTypes}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScFor, ScIf, ScWhile}
 
 object Scala3IndentationBasedSyntaxUtils {
-  def isFollowedByOutdent(leaf: PsiElement): Boolean = {
-    val leafToken = leaf.lastLeaf.elementType
-    leafToken match {
-      case ThenKeyword => false
-      case ScalaTokenTypes.kELSE => false
-      case ScalaTokenTypes.kDO => false
-      case ScalaTokenTypes.kYIELD => false
-      case ScalaTokenTypes.kCATCH => false
-      case ScalaTokenTypes.kFINALLY => false
-      case ScalaTokenTypes.kMATCH => false
-      case _ => true
-    }
+  def indentedRegionCanStart(leaf: PsiElement): Boolean = leaf.elementType match {
+      case ScalaTokenTypes.tASSIGN |
+           ScalaTokenTypes.tFUNTYPE |
+           ScalaTokenType.ImplicitFunctionArrow |
+           ScalaTokenTypes.tCHOOSE |
+           ScalaTokenTypes.kYIELD |
+           ScalaTokenTypes.kDO |
+           ScalaTokenType.ThenKeyword |
+           ScalaTokenTypes.kELSE |
+           ScalaTokenTypes.kTRY |
+           ScalaTokenTypes.kFINALLY |
+           ScalaTokenTypes.kCATCH |
+           ScalaTokenTypes.kMATCH |
+           ScalaTokenTypes.kRETURN |
+           ScalaTokenTypes.kTHROW =>
+        true
+      case ScalaTokenTypes.tRPARENTHESIS => leaf.parent match {
+        case Some(innerIf: ScIf) => innerIf.condition.isDefined && innerIf.thenExpression.isEmpty
+        case Some(innerWhile: ScWhile) => innerWhile.condition.isDefined && innerWhile.expression.isEmpty
+        case Some(innerFor: ScFor) => innerFor.enumerators.isDefined && innerFor.yieldOrDoKeyword.isEmpty && innerFor.body.isEmpty
+        case _ => false
+      }
+      case ScalaTokenTypes.tRBRACE => leaf.parent match {
+        case Some(innerFor: ScFor) => innerFor.enumerators.isDefined && innerFor.yieldOrDoKeyword.isEmpty && innerFor.body.isEmpty
+        case _ => false
+      }
+      case _ =>
+        false
   }
+
+  def outdentedRegionCanStart(leaf: PsiElement): Boolean = leaf.elementType match {
+    case ScalaTokenType.ThenKeyword |
+         ScalaTokenTypes.kELSE |
+         ScalaTokenTypes.kDO |
+         ScalaTokenTypes.kYIELD |
+         ScalaTokenTypes.kCATCH |
+         ScalaTokenTypes.kFINALLY |
+         ScalaTokenTypes.kMATCH => false
+    case _ => true
+  }
+
+  def continuesCompoundStatement(leaf: PsiElement): Boolean = !outdentedRegionCanStart(leaf)
 }
