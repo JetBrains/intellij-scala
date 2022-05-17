22f7e0ac4fd98fb0b335293eb54c3e6137e85335
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-05-17 10:57:20 +0300

    after fix cleanups: AbstractFixOnPsiElement.invoke : use startElement @NotNull parameter instead of myStartElement + ValidSmartPointer

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/AbstractFixOnPsiElement.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/AbstractFixOnPsiElement.scala
@@ -4,21 +4,13 @@ import com.intellij.codeInsight.FileModificationService
 import com.intellij.codeInspection.LocalQuickFixOnPsiElement
 import com.intellij.openapi.project.Project
 import com.intellij.psi.{PsiElement, PsiFile}
-import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.{Nls, NotNull}
 import org.jetbrains.plugins.scala.extensions.ValidSmartPointer
-
-/**
-  * Nikolay.Tropin
-  * 2014-11-12
-  */
 
 /**
   * The purpose of this class is to avoid holding references to instances of PsiElement in a quickfix.
   * Quickfix may be invoked after reparsing of the file so psiElements which existed when quickfix was created may
   * be invalidated.
-  *
-  * Important: Use methods getElement, getStartElement, getEndElement to get psiElements passed via constructor
-  * arguments.
   */
 abstract class AbstractFixOnPsiElement[T <: PsiElement](@Nls name: String, element: T)
   extends LocalQuickFixOnPsiElement(element) {
@@ -27,13 +19,19 @@ abstract class AbstractFixOnPsiElement[T <: PsiElement](@Nls name: String, eleme
 
   override def getFamilyName: String = getText
 
-  override final def invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement): Unit = {
-    if (!FileModificationService.getInstance.prepareFileForWrite(file)) return
+  override final def invoke(
+    @NotNull project: Project,
+    @NotNull file: PsiFile,
+    @NotNull startElement: PsiElement,
+    @NotNull endElement: PsiElement
+  ): Unit = {
+    if (!FileModificationService.getInstance.prepareFileForWrite(file))
+      return
 
-    myStartElement match {
-      case ValidSmartPointer(element: T@unchecked) => doApplyFix(element)(project)
-      case _ =>
-    }
+    if (!startElement.isValid)
+      return
+
+    doApplyFix(startElement.asInstanceOf[T])(project)
   }
 
   protected def doApplyFix(element: T)
