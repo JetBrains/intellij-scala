3e0353ede98e7bfa21468100348a32d3329e3e84
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2022-06-28 13:02:44 +0400

    Delete type params/args: use deleteElementInCommaSeparatedList

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/delete/SafeDeleteProcessorUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/delete/SafeDeleteProcessorUtil.scala
@@ -18,9 +18,9 @@ import org.jetbrains.annotations.Nullable
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.lang.psi.api.ImplicitArgumentsOwner
+import org.jetbrains.plugins.scala.lang.psi.api.base._
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScConstructorPattern
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.{ScParameterizedTypeElement, ScTypeArgs, ScTypeElement}
-import org.jetbrains.plugins.scala.lang.psi.api.base._
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
@@ -102,27 +102,25 @@ object SafeDeleteProcessorUtil {
       val index: Int = owner.getTypeParameterList.getTypeParameterIndex(typeParameter)
       referenceSearch(owner).forEach(new Processor[PsiReference] {
         override def process(reference: PsiReference): Boolean = {
-          def addScalaTypeArgUsageInfo(typeArgs: Seq[ScTypeElement]): Unit =
+          def addTypeArgUsageInfo(typeArgs: Seq[PsiElement]): Unit =
             if (typeArgs.lengthIs > index)
-              usages.add(new SafeDeleteScalaTypeArgumentDeleteUsageInfo(typeArgs(index), typeParameter))
+              usages.add(new SafeDeleteReferenceJavaDeleteUsageInfo(typeArgs(index), typeParameter, true))
 
           reference match {
             case referenceElement: PsiJavaCodeReferenceElement =>
               val typeArgs: Array[PsiTypeElement] = referenceElement.getParameterList.getTypeParameterElements
-              if (typeArgs.length > index) {
-                usages.add(new SafeDeleteReferenceJavaDeleteUsageInfo(typeArgs(index), typeParameter, true))
-              }
+              addTypeArgUsageInfo(typeArgs.toSeq)
             case ref: ScReference =>
               ref.parentOfType(Seq(classOf[ScGenericCall], classOf[ScConstructorInvocation])) match {
                 case Some(ScGenericCall(referencedExpr, typeArgs)) if referencedExpr == ref || referencedExpr == ref.getParent =>
-                  addScalaTypeArgUsageInfo(typeArgs)
+                  addTypeArgUsageInfo(typeArgs)
                 case Some(ScConstructorInvocation(ScParameterizedTypeElement(typeElement, typeArgs), _)) if ref.getParent == typeElement =>
-                  addScalaTypeArgUsageInfo(typeArgs)
+                  addTypeArgUsageInfo(typeArgs)
                 case None =>
                   // Nested type arguments like `def foo: Foo[Bar[A, Baz[B], C]]`
                   ref.parentsInFile.takeWhile(_.is[ScTypeArgs, ScTypeElement]).collectFirst {
                     case ScParameterizedTypeElement(_, typeArgs) =>
-                      addScalaTypeArgUsageInfo(typeArgs)
+                      addTypeArgUsageInfo(typeArgs)
                   }
                 case _ =>
               }
