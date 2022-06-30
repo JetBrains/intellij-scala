0d75a485ba54ac3cb975d6474446695f9ebc5f2c
Pavel Fatin <pavel.fatin@jetbrains.com>
2022-06-30 16:05:57 +0200

    Match types: resolve type variables, #SCL-20373 fixed
    (cherry picked from commit 631925c8df5c6ebc15ca742d80ad526686f0b424)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScMatchTypeCaseImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/types/ScMatchTypeCaseImpl.scala
@@ -1,8 +1,11 @@
 package org.jetbrains.plugins.scala.lang.psi.impl.base.types
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.scope.PsiScopeProcessor
+import com.intellij.psi.{PsiElement, ResolveState}
+import org.jetbrains.plugins.scala.extensions.{IterableOnceExt, PsiElementExt}
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScTypePattern
-import org.jetbrains.plugins.scala.lang.psi.api.base.types.{ScMatchTypeCase, ScTypeElement}
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.{ScMatchTypeCase, ScTypeElement, ScTypeVariableTypeElement}
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementImpl
 
 class ScMatchTypeCaseImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with ScMatchTypeCase {
@@ -10,4 +13,13 @@ class ScMatchTypeCaseImpl(node: ASTNode) extends ScalaPsiElementImpl(node) with 
   override def pattern: Option[ScTypePattern] = findChild[ScTypePattern]
 
   override def result: Option[ScTypeElement] = findLastChild[ScTypeElement]
+
+  override def processDeclarations(processor: PsiScopeProcessor, state : ResolveState, lastParent: PsiElement, place: PsiElement): Boolean = {
+    (pattern, result) match {
+      case (Some(p), Some(r)) if lastParent != null && r.startOffsetInParent == lastParent.startOffsetInParent =>
+        val typeVariables = p.typeElement.elements.filterByType[ScTypeVariableTypeElement]
+        typeVariables.forall(processor.execute(_, state))
+      case _ => true
+    }
+  }
 }
