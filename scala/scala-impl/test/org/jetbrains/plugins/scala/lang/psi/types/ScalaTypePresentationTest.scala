e4ab8b9a4904083325a9e877f28e17ef91f7a712
Pavel Fatin <pavel.fatin@jetbrains.com>
2022-07-05 14:54:38 +0200

    Proper polymorphic function type presentation, SCL-20393 fixed
    (cherry picked from commit fb2e24b1504e64fadf9af0e450a45ba1c0e137a1)

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/psi/types/ScalaTypePresentationTest.scala
@@ -0,0 +1,24 @@
+package org.jetbrains.plugins.scala.lang.psi.types
+
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestAdapter
+import org.jetbrains.plugins.scala.extensions.{IterableOnceExt, PsiElementExt}
+import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
+import org.jetbrains.plugins.scala.{LatestScalaVersions, ScalaVersion}
+import org.junit.Assert._
+
+class ScalaTypePresentationTest extends ScalaLightCodeInsightFixtureTestAdapter {
+  override protected def supportedIn(version: ScalaVersion): Boolean = version >= LatestScalaVersions.Scala_3_0
+
+  def testPolymorphicFunction(): Unit = assertPresentationIs(
+    "[X] => Any => Nothing")
+
+  private def assertPresentationIs(tpe: String): Unit = assertPresentationIs(tpe, tpe)
+
+  private def assertPresentationIs(tpe: String, expected: String): Unit = {
+    val file = ScalaPsiElementFactory.createScalaFileFromText("type T[A] = " + tpe)(getProject)
+    val typeElement = file.elements.findByType[ScTypeElement].get
+    val actual = typeElement.`type`().get.presentableText(TypePresentationContext(typeElement))
+    assertEquals(expected, actual)
+  }
+}
