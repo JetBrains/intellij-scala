074e8f98d9789b3e7def3ade8d39e7ae770beccf
Ali Kamali <ali.kamali@visier.com>
2022-07-07 09:11:49 -0700

    Fix compile error on idea222.x
    (cherry picked from commit b8202e6032802dc749451b2790c2ce33142109ac)

--- scala/scala-impl/test/org/jetbrains/plugins/scala/compiler/TransitiveDependencyClasspathTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/compiler/TransitiveDependencyClasspathTest.scala
@@ -1,12 +1,14 @@
 package org.jetbrains.plugins.scala.compiler
 
 
-import com.intellij.openapi.module.JavaModuleType
+import com.intellij.openapi.module.{JavaModuleType, Module}
 import com.intellij.openapi.roots.{DependencyScope, ModuleRootModificationUtil}
 import com.intellij.testFramework.PsiTestUtil
 import org.jetbrains.plugins.scala.DependencyManagerBase.RichStr
 import org.jetbrains.plugins.scala.base.libraryLoaders.IvyManagedLoader
 import org.jetbrains.plugins.scala.findUsages.compilerReferences.ScalaCompilerReferenceServiceFixture
+
+import java.io.File
 
 class TransitiveDependencyClasspathTest extends ScalaCompilerReferenceServiceFixture {
   def testClasspathIncludesTransitiveModules(): Unit = {
@@ -20,12 +22,15 @@ class TransitiveDependencyClasspathTest extends ScalaCompilerReferenceServiceFix
     val libLoader = IvyManagedLoader("org.scalatest" %% "scalatest" % "3.2.0")
     libLoader.init(moduleB, version)
 
-    val remoteServerConnectorBase = new RemoteServerConnectorBase(moduleA, None, new java.io.File("/tmp")) {
-      def getResult(): Seq[java.io.File] = assemblyRuntimeClasspath()
-    }
+    val remoteServerConnectorBase = new TestRemoteServerConnectorBase(moduleA, None, new java.io.File("/tmp"))
 
     val r = remoteServerConnectorBase.getResult()
     assert(r.exists(_.toString.contains("scalatest")))
   }
 
+}
+
+class TestRemoteServerConnectorBase (module: Module, filesToCompile: Option[Seq[File]], outputDir: File)
+    extends RemoteServerConnectorBase(module, filesToCompile, outputDir) {
+  def getResult(): Seq[java.io.File] = assemblyRuntimeClasspath()
 }
