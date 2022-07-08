cf8c1d275bcfa1642f1346367fd39cc113c6b0d1
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-07-08 15:58:02 +0200

    extract strings to bundle: DecompileScalaToJavaActionProvider.scala

--- scala/integration/java-decompiler/src/org/jetbrains/plugins/scala/decompileToJava/DecompileScalaToJavaActionProvider.scala
+++ scala/integration/java-decompiler/src/org/jetbrains/plugins/scala/decompileToJava/DecompileScalaToJavaActionProvider.scala
@@ -2,7 +2,6 @@ package org.jetbrains.plugins.scala
 package decompileToJava
 
 import java.{util => ju}
-
 import com.intellij.codeInsight.AttachSourcesProvider
 import com.intellij.openapi.roots.LibraryOrderEntry
 import com.intellij.openapi.util.ActionCallback
@@ -18,9 +17,9 @@ class DecompileScalaToJavaActionProvider extends AttachSourcesProvider {
     classFile match {
       case file: ScFile if file.isCompiled =>
         val action = new AttachSourcesAction {
-          override def getName: String = "Decompile to Java"
+          override def getName: String = JavaDecompilerBundle.message("decompile.to.java")
 
-          override def getBusyText: String = "Scala Classfile"
+          override def getBusyText: String = JavaDecompilerBundle.message("scala.classfile")
 
           override def perform(list: ju.List[_ <: LibraryOrderEntry]): ActionCallback = {
             ScalaBytecodeDecompileTask.showDecompiledJavaCode(file)
