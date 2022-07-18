1089a2396438adf3e48dde25b49c4c1c1113f3e6
Izmar Verhage <izmar.verhage@jetbrains.com>
2022-07-18 10:39:33 +0200

    Add QuickFix tests #SCL-20076

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/quickfix/MakePrivateQuickFixTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/quickfix/MakePrivateQuickFixTest.scala
@@ -1,9 +1,70 @@
 package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.quickfix
 
-import org.jetbrains.plugins.scala.codeInspection.{ScalaAnnotatorQuickFixTestBase, ScalaInspectionBundle}
+import org.jetbrains.plugins.scala.codeInspection.ScalaAnnotatorQuickFixTestBase
+import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.ScalaAccessCanBeTightenedInspection
 
 class MakePrivateQuickFixTest extends ScalaAnnotatorQuickFixTestBase {
 
   override protected val description = "Access can be private"
 
+  private val hint = "Make 'private'"
+
+  override def setUp(): Unit = {
+    super.setUp()
+    myFixture.enableInspections(classOf[ScalaAccessCanBeTightenedInspection])
+  }
+
+  def test_unused_method(): Unit = {
+    val code = s"private class Foo { def bar = {} }"
+    val expected = s"private class Foo { private def bar = {} }"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_used_method(): Unit = {
+    val code = s"private class Foo { def bar = {}; bar }"
+    val expected = s"private class Foo { private def bar = {}; bar }"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_unused_val(): Unit = {
+    val code = s"private class Foo { val bar = 42 }"
+    val expected = s"private class Foo { private val bar = 42 }"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_used_val(): Unit = {
+    val code = s"private class Foo { val bar = 42; bar }"
+    val expected = s"private class Foo { private val bar = 42; bar }"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_unused_var(): Unit = {
+    val code = s"private class Foo { var bar = 42 }"
+    val expected = s"private class Foo { private var bar = 42 }"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_used_var(): Unit = {
+    val code = s"private class Foo { var bar = 42; bar }"
+    val expected = s"private class Foo { private var bar = 42; bar }"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_unused_class(): Unit = {
+    val code = s"class A"
+    val expected = s"private class A"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_unused_trait(): Unit = {
+    val code = s"trait A"
+    val expected = s"private trait A"
+    testQuickFix(code, expected, hint)
+  }
+
+  def test_unused_object(): Unit = {
+    val code = s"object A"
+    val expected = s"private object A"
+    testQuickFix(code, expected, hint)
+  }
 }
