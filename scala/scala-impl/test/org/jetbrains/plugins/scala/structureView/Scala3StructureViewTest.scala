a9e4bb4068efb814f7ce2fba5b18d76d756ee548
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2022-07-22 14:09:18 +0200

    fixing enum unit tests

--- scala/scala-impl/test/org/jetbrains/plugins/scala/structureView/Scala3StructureViewTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/structureView/Scala3StructureViewTest.scala
@@ -15,7 +15,7 @@ class Scala3StructureViewTest extends ScalaStructureViewCommonTests {
   override protected def check(@org.intellij.lang.annotations.Language("Scala 3") code: String, nodes: Node*): Unit =
     super.check(code, nodes: _*)
 
-  private val EnumCaseIcon = CLASS
+  private val EnumCaseIcon = ENUM
   private val BlockIcon = PlatformIcons.CLASS_INITIALIZER
 
   private val TopLevelDefinitionsText =
