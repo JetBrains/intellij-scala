5127ed4166d9112057b576010f1819dc7b9f0455
Ulugbek <ulugbek.abdullaev@jetbrains.com>
2022-07-22 13:15:12 +0200

    fix(parser): parser used to not report an error when a function parameter type was optional in function declaration, which is not true
    i.e., one can't do `def foo(bar) = ()` omitting the type annotation for `bar`
    
    #SCL-20445 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/params/Param.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/parser/parsing/params/Param.scala
@@ -10,7 +10,7 @@ import org.jetbrains.plugins.scala.lang.parser.parsing.expressions.{Annotations,
 import org.jetbrains.plugins.scala.lang.parser.parsing.types.ParamType
 
 /**
- * [[Param]] ::= [[Annotations]] id [':' ParamType] ['=' Expr]
+ * [[Param]] ::= [[Annotations]] id ':' ParamType ['=' Expr]
  *
 * @author Alexander Podkhalyuzin
 * Date: 06.03.2008
@@ -41,9 +41,12 @@ object Param extends ParsingRule {
     builder.getTokenType match {
       case ScalaTokenTypes.tCOLON =>
         builder.advanceLexer() //Ate :
-        if (!ParamType()) builder error ErrMsg("wrong.type")
       case _ =>
+        builder error ErrMsg("colon.expected")
     }
+
+    if (!ParamType()) builder error ErrMsg("wrong.type")
+
     builder.getTokenType match {
       case ScalaTokenTypes.tASSIGN =>
         builder.advanceLexer() //Ate =
