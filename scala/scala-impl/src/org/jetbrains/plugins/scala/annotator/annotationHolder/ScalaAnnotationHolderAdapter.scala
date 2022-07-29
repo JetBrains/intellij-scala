a9312361e6a38736afcb766d3b13e30b45afef1f
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-07-29 17:01:08 +0200

    Remove builder adapter subclass that ignores highlighting info #SCL-20436

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/annotationHolder/ScalaAnnotationHolderAdapter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/annotationHolder/ScalaAnnotationHolderAdapter.scala
@@ -2,17 +2,9 @@ package org.jetbrains.plugins.scala
 package annotator
 package annotationHolder
 
-import com.intellij.codeInspection.ProblemHighlightType
-import com.intellij.lang.annotation.{AnnotationBuilder, AnnotationHolder, AnnotationSession, HighlightSeverity}
-import org.jetbrains.plugins.scala.externalHighlighters.ScalaHighlightingMode
-
-import scala.language.implicitConversions
+import com.intellij.lang.annotation.{AnnotationHolder, AnnotationSession, HighlightSeverity}
 
 class ScalaAnnotationHolderAdapter(innerHolder: AnnotationHolder) extends ScalaAnnotationHolder {
-
-  //todo: more fine-grained disabling of annotators
-  private val showCompilerErrors =
-    ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(innerHolder.getCurrentAnnotationSession.getFile)
 
   override def getCurrentAnnotationSession: AnnotationSession =
     innerHolder.getCurrentAnnotationSession
@@ -21,20 +13,8 @@ class ScalaAnnotationHolderAdapter(innerHolder: AnnotationHolder) extends ScalaA
     innerHolder.isBatchMode
 
   override def newAnnotation(severity: HighlightSeverity, message: String): ScalaAnnotationBuilder =
-    if (showCompilerErrors) new CompilerErrorsAwareBuilder(innerHolder.newSilentAnnotation(HighlightSeverity.INFORMATION))
-    else new ScalaAnnotationBuilderAdapter(innerHolder.newAnnotation(severity, message))
+    new ScalaAnnotationBuilderAdapter(innerHolder.newAnnotation(severity, message))
 
   override def newSilentAnnotation(severity: HighlightSeverity): ScalaAnnotationBuilder =
-    if (showCompilerErrors) new CompilerErrorsAwareBuilder(innerHolder.newSilentAnnotation(HighlightSeverity.INFORMATION))
-    else new ScalaAnnotationBuilderAdapter(innerHolder.newSilentAnnotation(severity))
-
-  private class CompilerErrorsAwareBuilder(annotationBuilder: AnnotationBuilder)
-    extends ScalaAnnotationBuilderAdapter(annotationBuilder) {
-
-    override def tooltip(tooltip: String): this.type = this
-    override def highlightType(highlightType: ProblemHighlightType): this.type = {
-      annotationBuilder.highlightType(ProblemHighlightType.INFORMATION)
-      this
-    }
-  }
+    new ScalaAnnotationBuilderAdapter(innerHolder.newSilentAnnotation(severity))
 }
