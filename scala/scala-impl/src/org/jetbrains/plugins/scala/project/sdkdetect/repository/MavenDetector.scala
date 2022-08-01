71affd01ddb87ba03cbe7b650c1188771c7d7195
Pavel Fatin <pavel.fatin@jetbrains.com>
2022-08-01 15:35:17 +0200

    SdkSelectionDialogWrapper: fix CNFE

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/sdkdetect/repository/MavenDetector.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/sdkdetect/repository/MavenDetector.scala
@@ -16,6 +16,7 @@ private[repository] object MavenDetector extends ScalaSdkDetectorDependencyManag
   override protected def buildSdkChoice(descriptor: ScalaSdkDescriptor): SdkChoice = MavenSdkChoice(descriptor)
 
   override protected def buildJarStream(implicit indicator: ProgressIndicator): JStream[Path] = {
+    // TODO Depend on Maven plugin classes optionally
     val mavenHomeDir = MavenUtil.resolveM2Dir().toOption.map(_.toPath)
     val scalaRoot = mavenHomeDir.map(_ / "repository" / "org" / "scala-lang")
     scalaRoot.filter(_.exists).map(collectJarFiles).getOrElse(JStream.empty())
