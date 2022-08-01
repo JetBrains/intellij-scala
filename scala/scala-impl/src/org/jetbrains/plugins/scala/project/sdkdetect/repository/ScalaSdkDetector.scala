88b7fe6c87e602a18c838ef5680e1e1b46d6ef88
Pavel Fatin <pavel.fatin@jetbrains.com>
2022-08-01 15:35:17 +0200

    SdkSelectionDialogWrapper: fix CNFE

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/sdkdetect/repository/ScalaSdkDetector.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/sdkdetect/repository/ScalaSdkDetector.scala
@@ -1,9 +1,12 @@
 package org.jetbrains.plugins.scala.project.sdkdetect.repository
 
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.Nls
 import org.jetbrains.plugins.scala.project.template._
+import org.jetbrains.sbt.RichBoolean
 
 trait ScalaSdkDetector {
 
@@ -14,14 +17,21 @@ trait ScalaSdkDetector {
 }
 
 object ScalaSdkDetector {
-  def allDetectors(contextDirectory: VirtualFile) =
+  def allDetectors(contextDirectory: VirtualFile): Seq[ScalaSdkDetectorBase] =
     Seq(
       new ProjectLocalDetector(contextDirectory),
       SystemDetector,
       BrewDetector,
-      IvyDetector,
-      MavenDetector,
+      IvyDetector
+    ) ++
+    isIdeaPluginEnabled("org.jetbrains.idea.maven").seq(
+      MavenDetector
+    ) ++
+    Seq(
       SdkmanDetector,
       CoursierDetector
     )
+
+  private def isIdeaPluginEnabled(id: String): Boolean =
+    Option(PluginId.findId(id)).map(PluginManagerCore.getPlugin).exists(_.isEnabled)
 }
