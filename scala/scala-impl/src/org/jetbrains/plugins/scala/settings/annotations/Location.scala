c731e4f97043b417eb16d748162447c69d52b5d4
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-08-04 19:01:12 +0200

    make org.jetbrains.plugins.scala.settings.annotations.Location a trait
    - introduce DefaultLocation instead of using anonimous Location class

--- scala/scala-impl/src/org/jetbrains/plugins/scala/settings/annotations/Location.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/settings/annotations/Location.scala
@@ -1,10 +1,8 @@
-package org.jetbrains.plugins.scala
-package settings
-package annotations
+package org.jetbrains.plugins.scala.settings.annotations
 
 import com.intellij.openapi.roots.TestSourcesFilter.isTestSources
 import com.intellij.psi.search.GlobalSearchScope.moduleWithDependenciesAndLibrariesScope
-import com.intellij.psi.{PsiClass, PsiCodeFragment, PsiElement, PsiModifier}
+import com.intellij.psi.{FileViewProvider, PsiClass, PsiCodeFragment, PsiElement, PsiModifier}
 import org.jetbrains.plugins.scala.extensions.ViewProviderExt
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil.getModule
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
@@ -14,71 +12,88 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBod
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScMember
 import org.jetbrains.plugins.scala.lang.psi.{ElementScope, ScFileViewProvider, ScalaPsiUtil}
 
-sealed abstract class Location(protected val element: PsiElement) {
-
-  def isInLocalScope: Boolean = element match {
-    case _: ScTemplateBody => false
-    case _ =>
-      element.getParent match {
-        case _: ScTemplateBody |
-             _: PsiClass => false
-        case _: ScalaFile => false
-        case _ => true
-      }
-  }
-
-  final def isInCodeFragment: Boolean =
-    element.getContainingFile.isInstanceOf[PsiCodeFragment]
-
-  final def isInDialectSources: Boolean =
-    findScalaViewProvider.exists {
-      case _: ScFileViewProvider => false
-      case _ => true
-    }
-
-  final def isInTestSources: Boolean =
-    findScalaViewProvider
-      .map(_.getVirtualFile)
-      .exists(isTestSources(_, element.getProject))
-
-  def isInsideAnonymousClass: Boolean = false
-
-  def isInsidePrivateClass: Boolean = false
-
-  def isInsideOf(classes: Set[String]): Boolean = false
-
-  private def containingFile = element.getContainingFile
-
-  private def findScalaViewProvider = Option(containingFile)
-    .map(_.getViewProvider)
-    .filter(_.hasScalaPsi)
+sealed trait Location {
+  def isInLocalScope: Boolean
+  def isInCodeFragment: Boolean
+  def isInDialectSources: Boolean
+  def isInTestSources: Boolean
+  def isInsideAnonymousClass: Boolean
+  def isInsidePrivateClass: Boolean
+  def isInsideOf(classesFqn: Set[String]): Boolean
 }
 
 object Location {
 
   def apply(anchor: PsiElement): Location = {
     val maybeMemberLocation = anchor match {
-      case member: ScMember => member.getContainingClass match {
-        case null => None
-        case containingClass => Some(new MemberAnchorLocation(member, InsideClassLocation(containingClass)))
-      }
+      case member: ScMember =>
+        val containingClass = member.containingClass
+        if (containingClass == null)
+          None
+        else
+          Some(new MemberAnchorLocation(member, new InsideClassLocation(containingClass)))
       case _ => None
     }
 
-    maybeMemberLocation.getOrElse(new Location(anchor) {})
+    maybeMemberLocation.getOrElse(new DefaultLocation(anchor))
   }
 
-  private final class MemberAnchorLocation(override protected val element: ScMember,
-                                           delegate: InsideClassLocation) extends Location(element) {
+  sealed class DefaultLocation(
+    protected val element: PsiElement
+  ) extends Location {
+    override def isInLocalScope: Boolean = element match {
+      case _: ScTemplateBody => false
+      case _ =>
+        element.getParent match {
+          case _: ScTemplateBody |
+               _: PsiClass => false
+          case _: ScalaFile => false
+          case _ => true
+        }
+    }
+
+    override final def isInCodeFragment: Boolean =
+      element.getContainingFile.isInstanceOf[PsiCodeFragment]
+
+    override final def isInDialectSources: Boolean =
+      findScalaViewProvider.exists {
+        case _: ScFileViewProvider => false
+        case _ => true
+      }
+
+    override final def isInTestSources: Boolean =
+      findScalaViewProvider
+        .map(_.getVirtualFile)
+        .exists(isTestSources(_, element.getProject))
+
+    override def isInsideAnonymousClass: Boolean = false
+
+    override def isInsidePrivateClass: Boolean = false
+
+    override def isInsideOf(classesFqn: Set[String]): Boolean = false
+
+    private def containingFile = element.getContainingFile
+
+    private def findScalaViewProvider: Option[FileViewProvider] = Option(containingFile)
+      .map(_.getViewProvider)
+      .filter(_.hasScalaPsi)
+  }
+
+  private final class MemberAnchorLocation(
+    override protected val element: ScMember,
+    delegate: InsideClassLocation
+  ) extends DefaultLocation(element) {
 
     override def isInsideAnonymousClass: Boolean = delegate.isInsideAnonymousClass
 
     override def isInsidePrivateClass: Boolean = delegate.isInsidePrivateClass
 
-    override def isInsideOf(classes: Set[String]): Boolean = delegate.isInsideOf(classes)
+    override def isInsideOf(classesFqn: Set[String]): Boolean = delegate.isInsideOf(classesFqn)
   }
 
-  final class InsideClassLocation(override protected val element: PsiClass) extends Location(element) {
+  final class InsideClassLocation(
+    override protected val element: PsiClass
+  ) extends DefaultLocation(element) {
 
     override def isInLocalScope: Boolean = false
 
@@ -89,21 +104,16 @@ object Location {
       case _ => false
     }
 
-    override def isInsideOf(classes: Set[String]): Boolean =
+    override def isInsideOf(classesFqn: Set[String]): Boolean =
       Option(getModule(element))
         .map(moduleWithDependenciesAndLibrariesScope)
         .map(ElementScope(element.getProject, _))
         .exists(scope =>
-          classes
+          classesFqn
             .flatMap(scope.getCachedClass)
             .exists { clazz =>
               ScalaPsiUtil.thisSubsumes(element, clazz)
             }
         )
-  }
-
-  object InsideClassLocation {
-
-    def apply(`class`: PsiClass) = new InsideClassLocation(`class`)
   }
 }
