bd838ade010793184d145369d5c1c8bbf351e022
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-08-17 18:11:39 +0200

    worksheet: fix Goto and highlighting for `resN` references #SCL-20478
    It now correctly highlights expressions which will produce a `resN` value in worksheet REPL.
    Also Goto action suggests navigating to these expressions.

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/actions/repl/ResNUtils.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/actions/repl/ResNUtils.scala
@@ -1,7 +1,12 @@
 package org.jetbrains.plugins.scala.worksheet.actions.repl
 
+import org.jetbrains.plugins.scala.worksheet.WorksheetFile
+
 import scala.util.matching.Regex
 
 private[worksheet] object ResNUtils {
   val ResNRegex: Regex = """res\d+""".r
+
+  def isResNSupportedInFile(file: WorksheetFile): Boolean =
+    file.isRepl
 }
