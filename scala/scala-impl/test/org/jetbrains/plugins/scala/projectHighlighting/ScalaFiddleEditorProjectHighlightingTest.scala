9e98b0b480f9db5154c48b0a782ad39fc9124b47
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-08-23 18:22:35 +0200

    project highlighting tests: ScalaFiddleEditorProjectHighlightingTest: ignore errors in Librarian.scala" #SCL-20532

--- scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/ScalaFiddleEditorProjectHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/ScalaFiddleEditorProjectHighlightingTest.scala
@@ -17,7 +17,12 @@ class ScalaFiddleEditorProjectHighlightingTest extends GithubSbtAllProjectHighli
   override def revision = "e9bbda4d4190d262a93405365f38c93c8e7988b5"
 
   override def filesWithProblems: Map[String, Set[TextRange]] = Map(
+    //these two files contain unresolved references to generated routes sources (e.g. routes.Application)
+    //in order there are no errors, the project should be not just imported but compiled (probably with sbt shell)
     "server/src/main/scala/controllers/SocialAuthController.scala" -> Set(),
-    "server/src/main/scala/controllers/Application.scala" -> Set()
+    "server/src/main/scala/controllers/Application.scala" -> Set(),
+
+    //mute SCL-20532
+    "server/src/main/scala/scalafiddle/server/Librarian.scala" -> Set()
   )
 }
