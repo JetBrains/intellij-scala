08be54343cb25909853af92df8674286755a38c9
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-08-29 22:10:08 +0200

    project highlighting tests: un-ignore Log4sProjectHighlightingTest.scala and fix filesWithProblems

--- scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/downloaded/Log4sProjectHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/projectHighlighting/downloaded/Log4sProjectHighlightingTest.scala
@@ -7,15 +7,11 @@ import org.junit.Ignore
 import org.junit.experimental.categories.Category
 
 @Category(Array(classOf[HighlightingTests]))
-@Ignore
 class Log4sProjectHighlightingTest extends GithubSbtAllProjectHighlightingTest {
 
   override protected def githubRepositoryWithRevision: GithubRepositoryWithRevision =
     GithubRepositoryWithRevision("Log4s", "log4s", "8439aef843da2c9f489d1dff4cf62df6135fb9d8")
 
-  import org.jetbrains.plugins.scala.util.TextRangeUtils.ImplicitConversions.tupleToTextRange
-
   override def filesWithProblems: Map[String, Set[TextRange]] = Map(
-    "src/test/scala/org/log4s/MDCSpec.scala" -> Set((356,374), (477,495), (904,922), (1049,1067), (1121,1147), (1173,1204), (1355,1373)),
   )
 }
