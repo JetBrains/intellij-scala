1b75c7fa2d6b6af9536d8efb90f3a619dd64e5c8
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2022-09-02 18:09:25 +0200

    Changes to ScOverridenVarAnnotator to differentiate between compiler errors
    On top of "Mutable variable cannot be overridden" the compiler can somtimes display an error
    1) if a var is overridden by a val in a valid way but the setter method is missing
    2) if a var is overridden by a setter method but a getter (or a val) is missing
    
    This PR adds logic to the annotator to display the same errors.
    I also removed logic from OverridingAnnotator that is now handled in ScOverridenVarAnnotator.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScOverriddenVarAnnotator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScOverriddenVarAnnotator.scala
@@ -3,44 +3,74 @@ package org.jetbrains.plugins.scala.annotator.element
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.annotator.ScalaAnnotationHolder
-import org.jetbrains.plugins.scala.lang.lexer.ScalaModifier
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScClassParameter
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScVariable}
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScTypeDefinition}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScValue, ScValueOrVariable, ScVariable}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScTemplateDefinition, ScTypeDefinition}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.{ScModifierListOwner, ScTypedDefinition}
-import org.jetbrains.plugins.scala.util.EnumSet.EnumSetOps
 
 object ScOverriddenVarAnnotator extends ElementAnnotator[ScTypedDefinition] {
   override def annotate(elem: ScTypedDefinition, typeAware: Boolean)
                        (implicit holder: ScalaAnnotationHolder): Unit = {
-    val isAllowed = elem.nameContext match {
-      case o: ScModifierListOwner if !o.getModifierList.modifiers.contains(ScalaModifier.Override) =>
-        true
+    val errorMessage = elem.nameContext match {
+      case o: ScModifierListOwner if !o.getModifierList.isOverride =>
+        None
+      case v: ScValueOrVariable if v.isAbstract =>
+        None
+      case p: ScClassParameter if p.isAbstractMember =>
+        None
+      case f: ScFunction if f.isAbstractMember =>
+        None
       case p: ScClassParameter if p.isVar =>
-        val supers = findSupers(p, elem.name)
-        if (supers.nonEmpty) isOverrideOfAbstract(supers) else true
+        val supers = findSupers(elem.name, p.containingClass)
+        if (supers.nonEmpty && !isOverrideOfAbstract(supers))
+          Some(ScalaBundle.message("var.cannot.be.overridden"))
+        else
+          None
       case v: ScVariable =>
-        val supers = findSupers(v, elem.name)
-        if (supers.nonEmpty) isOverrideOfAbstract(supers) else true
-      case f: ScFunction if isSetter(f) =>
-        val setterName = f.name.dropRight(2)
-        val supers = findSupers(f, setterName)
-        if (supers.exists(isVar)) hasGetter(f, setterName) else true
+        val supers = findSupers(elem.name, v.containingClass)
+        if (supers.nonEmpty && !isOverrideOfAbstract(supers))
+          Some(ScalaBundle.message("var.cannot.be.overridden"))
+        else
+          None
+      case f: ScFunction if f.name.endsWith("_=") && isSetter(f) =>
+        val elemName = f.name.dropRight(2)
+        val containingClass = f.containingClass
+        val supers = findSupers(elemName, containingClass)
+        if (supers.exists(isVar) && !hasGetter(elemName, containingClass))
+          Some(ScalaBundle.message("missing.getter.implementation", elemName))
+        else
+          None
       case f: ScFunction if isGetter(f) =>
-        val supers = findSupers(f, elem.name)
-        if (supers.exists(isVar)) hasSetter(f) else true
+        checkForNonAbstractVarSuper(elem.name, f.containingClass)
+      case v: ScValue =>
+        checkForNonAbstractVarSuper(elem.name, v.containingClass)
+      case p: ScClassParameter =>
+        val supers = findSupers(elem.name, p.containingClass)
+        if (supers.exists(isVar))
+          Some(ScalaBundle.message("var.cannot.be.overridden"))
+        else
+          None
       case _ =>
-        true
+        None
     }
 
-    if (!isAllowed)
-      holder.createErrorAnnotation(elem.getIdentifyingElement, ScalaBundle.message("var.cannot.be.overridden"))
+    errorMessage.foreach(holder.createErrorAnnotation(elem.getIdentifyingElement, _))
   }
 
-  private def findSupers(m: ScMember, name: String): Seq[PsiElement] =
-    m.containingClass.supers.collect {
+  private def checkForNonAbstractVarSuper(elemName: String, containingClass: ScTemplateDefinition): Option[String] =
+    findSupers(elemName, containingClass).collectFirst {
+      case p: ScClassParameter if p.isVar =>
+        ScalaBundle.message("var.cannot.be.overridden")
+      case v: ScVariable if v.isAbstract && !hasSetter(elemName, containingClass) =>
+        ScalaBundle.message("missing.setter.implementation", elemName)
+      case v: ScVariable if !v.isAbstract =>
+        ScalaBundle.message("var.cannot.be.overridden")
+    }
+
+  private def findSupers(elemName: String, containingClass: ScTemplateDefinition): Seq[PsiElement] =
+    containingClass.supers.collect {
       case t: ScTypeDefinition =>
-        t.allTermsByName(name).collect {
+        t.allTermsByName(elemName).collect {
           case term: ScTypedDefinition => term.nameContext
         }
     }.flatten
@@ -59,27 +89,24 @@ object ScOverriddenVarAnnotator extends ElementAnnotator[ScTypedDefinition] {
       case _                   => false
     }
 
-  private def hasGetter(elem: ScFunction, name: String): Boolean =
-    elem.containingClass.allFunctionsByName(name).exists {
-      case f: ScFunction if isGetter(f) =>
-        val elemType = elem.parameters.head.typeElement.map(_.getText).getOrElse("")
-        f.returnTypeElement.exists(_.textMatches(elemType))
-      case _ =>
-        false
-    }
+  private def hasGetter(elemName: String, containingClass: ScTemplateDefinition): Boolean =
+    if (containingClass.allVals.exists(v => !v.isAbstract && v.name == elemName))
+      true
+    else
+      containingClass.allFunctionsByName(elemName).exists {
+        case f: ScFunction if isGetter(f) => true
+        case _ => false
+      }
 
-  private def hasSetter(elem: ScFunction): Boolean =
-    elem.containingClass.allFunctionsByName(elem.name + "_=").exists {
-      case f: ScFunction if isSetter(f) =>
-        val elemType = elem.returnTypeElement.map(_.getText).getOrElse("")
-        f.parameters.head.typeElement.exists(_.textMatches(elemType))
-      case _ =>
-        false
+  private def hasSetter(elemName: String, containingClass: ScTemplateDefinition): Boolean =
+    containingClass.allFunctionsByName(elemName + "_=").exists {
+      case f: ScFunction if isSetter(f) => true
+      case _ => false
     }
 
   private def isSetter(f: ScFunction): Boolean =
-    f.name.endsWith("_=") && f.hasUnitResultType && f.parameters.size == 1
+    f.hasUnitResultType && f.parameters.size == 1
 
   private def isGetter(f: ScFunction): Boolean =
-    !f.name.endsWith("_=") && !f.hasUnitResultType && f.parameters.isEmpty
+    !f.hasUnitResultType && f.parameters.isEmpty
 }
