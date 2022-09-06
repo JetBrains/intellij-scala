af3800653aeb94c5a2b95ee98f544452539dc0b5
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2022-09-06 18:27:17 +0200

    Make AddReturnTypeFix preview-friendly #SCL-20398

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/AddReturnTypeFix.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/AddReturnTypeFix.scala
@@ -2,9 +2,11 @@ package org.jetbrains.plugins.scala
 package annotator.quickfix
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.codeInsight.intention.types.AddOnlyStrategy
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
@@ -15,11 +17,17 @@ class AddReturnTypeFix(fun: ScFunctionDefinition, tp: ScType) extends IntentionA
 
   override def getFamilyName: String = getText
 
-  override def invoke(project: Project, editor: Editor, file: PsiFile): Unit = {
-    new AddOnlyStrategy(Option(editor)).addTypeAnnotation(tp, fun.getParent, fun.parameterList)
-  }
+  override def invoke(project: Project, editor: Editor, file: PsiFile): Unit = doAddReturn(fun, editor)
 
   override def startInWriteAction(): Boolean = true
 
   override def isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean = fun.returnTypeElement.isEmpty
+
+  override def generatePreview(project: Project, editor: Editor, file: PsiFile): IntentionPreviewInfo = {
+    doAddReturn(PsiTreeUtil.findSameElementInCopy(fun, file), editor)
+    IntentionPreviewInfo.DIFF
+  }
+
+  private def doAddReturn(fn: ScFunctionDefinition, editor: Editor): Unit =
+    new AddOnlyStrategy(Option(editor)).addTypeAnnotation(tp, fn.getParent, fn.parameterList)
 }
