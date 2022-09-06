82c20097f024c770229ad24a4e18360a0982fc2a
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2022-09-06 23:20:05 +0200

    ReportHighlightingErrorQuickFix: Use BrowserUtil#browse instead of DesktopUtils#browse #SCL-20398

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/ReportHighlightingErrorQuickFix.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/ReportHighlightingErrorQuickFix.scala
@@ -3,9 +3,11 @@ package annotator
 package quickfix
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import org.jetbrains.plugins.scala.extensions.ObjectExt
 import org.jetbrains.plugins.scala.lang.psi.impl.source.ScalaCodeFragment
 
 object ReportHighlightingErrorQuickFix extends IntentionAction {
@@ -15,13 +17,12 @@ object ReportHighlightingErrorQuickFix extends IntentionAction {
   override def startInWriteAction: Boolean = false
 
   override def isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean = {
-    if (file.isInstanceOf[ScalaCodeFragment]) return false
+    if (file.is[ScalaCodeFragment]) return false
     true
   }
 
-  override def invoke(project: Project, editor: Editor, file: PsiFile): Unit = {
-    DesktopUtils.browse("https://youtrack.jetbrains.net/issues/SCL#newissue=yes")
-  }
+  override def invoke(project: Project, editor: Editor, file: PsiFile): Unit =
+    BrowserUtil.browse("https://youtrack.jetbrains.net/issues/SCL#newissue=yes")
 
   override def getFamilyName: String = ScalaBundle.message("report.highlighting.error.fix")
 }
