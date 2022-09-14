a6b63620e2ce220e1f5a9cf3323db87fd17a71d8
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-09-14 11:29:03 +0200

    Rewrite the SbtBundle class to Java to avoid collections and mapping

---
+++ scala/scala-impl/src/org/jetbrains/sbt/SbtBundle.java
@@ -0,0 +1,23 @@
+package org.jetbrains.sbt;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+public final class SbtBundle extends DynamicBundle {
+    @NonNls
+    private static final String BUNDLE = "messages.ScalaSbtBundle";
+
+    private static final SbtBundle INSTANCE = new SbtBundle();
+
+    private SbtBundle() {
+        super(BUNDLE);
+    }
+
+    @Nls
+    public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+        return INSTANCE.getMessage(key, params);
+    }
+}
