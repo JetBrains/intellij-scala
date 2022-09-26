5cf55bede7ba9f5b61d758d730854f77097d0e5d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-09-26 15:44:11 +0200

    Run worksheet integration tests on JDK 17 too

--- scala/scala-impl/test/org/jetbrains/plugins/scala/util/runners/TestJdkVersion.java
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/util/runners/TestJdkVersion.java
@@ -6,12 +6,13 @@ import com.intellij.pom.java.LanguageLevel;
 // required at compile time to use in annotations
 public enum TestJdkVersion {
 
-    JDK_1_8, JDK_11;
+    JDK_1_8, JDK_11, JDK_17;
 
     public LanguageLevel toProductionVersion() {
         return switch (this) {
             case JDK_1_8 -> LanguageLevel.JDK_1_8;
             case JDK_11 -> LanguageLevel.JDK_11;
+            case JDK_17 -> LanguageLevel.JDK_17;
         };
     };
 
@@ -19,6 +20,7 @@ public enum TestJdkVersion {
         return switch (level) {
             case JDK_1_8 -> JDK_1_8;
             case JDK_11 -> JDK_11;
+            case JDK_17 -> JDK_17;
             default -> throw new RuntimeException("Jdk is not supported in tests for now");
         };
     }
