86d4f4b32e93c49bc11c85c3f1a5c61c5830f39e
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-09-28 11:12:41 +0200

    Fix synthetic class name extraction function to fix Gradle main method running #SCL-20383 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/runner/Scala3MainMethodSyntheticClassFinder.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/runner/Scala3MainMethodSyntheticClassFinder.scala
@@ -2,22 +2,16 @@ package org.jetbrains.plugins.scala.runner
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi._
-import com.intellij.psi.impl.light.LightElement
-import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.util.IncorrectOperationException
-import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.caches.ModTracker
 import org.jetbrains.plugins.scala.extensions.PsiClassExt
-import org.jetbrains.plugins.scala.lang.psi.adapters.PsiClassAdapter
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
-import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.PsiClassFake
 import org.jetbrains.plugins.scala.lang.psi.stubs.index.ScalaIndexKeys
 import org.jetbrains.plugins.scala.lang.psi.types.TypePresentationContext
 import org.jetbrains.plugins.scala.macroAnnotations.CachedInUserData
+import org.jetbrains.plugins.scala.runner.Scala3MainMethodSyntheticClass.MainMethodParameters
 import org.jetbrains.plugins.scala.runner.Scala3MainMethodSyntheticClass.MainMethodParameters.CustomParameter
-import org.jetbrains.plugins.scala.runner.Scala3MainMethodSyntheticClass.{MainMethodParameters, extractNameFromFqn}
 
 import scala.annotation.unused
 
@@ -90,60 +84,4 @@ private final class Scala3MainMethodSyntheticClassFinder(project: Project)
 
   // Not implemented because it isn't required during application configuration verification
   override def findClasses(qualifiedName: String, scope: GlobalSearchScope): Array[PsiClass] = PsiClass.EMPTY_ARRAY
-}
-
-private final class Scala3MainMethodSyntheticClass(
-  psiManager: PsiManager,
-  containingFile: PsiFile,
-  qualifiedName: String,
-  val parameters: MainMethodParameters,
-) extends LightElement(psiManager, ScalaLanguage.INSTANCE)
-  with PsiNameIdentifierOwner
-  with PsiClassAdapter
-  with PsiClassFake {
-
-  override val getName: String = extractNameFromFqn(qualifiedName)
-  override def getQualifiedName: String = qualifiedName
-
-  override def getText = ""
-  override def getNameIdentifier: PsiIdentifier = null
-
-  override def getContainingFile: PsiFile = containingFile
-  override def getContext: PsiFile = containingFile
-
-  override def toString = s"synthetic class for scala @main method: $qualifiedName"
-
-  override def setName(newName: String): PsiElement = throw new IncorrectOperationException("nonphysical element")
-  override def copy = throw new IncorrectOperationException("nonphysical element")
-  override def accept(v: PsiElementVisitor): Unit = throw new IncorrectOperationException("should not call")
-
-  override def processDeclarations(
-    processor: PsiScopeProcessor,
-    state: ResolveState,
-    lastParent: PsiElement,
-    place: PsiElement
-  ): Boolean = {
-    // NOTE: we probably need add some fake psi file with all fake @main method classes declarations
-    // strictly speaking ScalaMainMethodSyntheticClass can't declare itself, but this solution works...
-    processor.execute(this, state)
-    false
-  }
-}
-
-private object Scala3MainMethodSyntheticClass {
-
-  sealed trait MainMethodParameters
-
-  object MainMethodParameters {
-    object Default extends MainMethodParameters // (args: String*)
-    case class Custom(parameterNames: Seq[CustomParameter]) extends MainMethodParameters
-
-    case class CustomParameter(name: String, typ: String, isVararg: Boolean)
-  }
-
-  private def extractNameFromFqn(qualifiedName: String): String =
-    qualifiedName.lastIndexOf('.') match {
-      case -1  => qualifiedName // in root package
-      case idx => qualifiedName.substring(idx)
-    }
 }
