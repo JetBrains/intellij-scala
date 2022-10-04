c0031130fe13fbb6dd9498a8ee201cb2aecf76bb
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-10-04 17:23:40 +0200

    after drop scala script: fix even more tests

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/ParameterBoundedImplicitConversionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/ParameterBoundedImplicitConversionTest.scala
@@ -8,41 +8,49 @@ import org.junit.experimental.categories.Category
 class ParameterBoundedImplicitConversionTest extends ScalaLightCodeInsightFixtureTestCase {
   def testSCL13089(): Unit = {
     val text =
-      """
-        |import scala.language.implicitConversions
+      """import scala.language.implicitConversions
         |import scala.language.higherKinds
-        |trait Foo[T]
-        |trait FooBuild[M[_], Face] {
-        |  implicit def fromNum[T <: Face, Out <: T](value : T) : M[Out] = ???
+        |
+        |object Wrapper {
+        |  trait Foo[T]
+        |
+        |  trait FooBuild[M[_], Face] {
+        |    implicit def fromNum[T <: Face, Out <: T](value: T): M[Out] = ???
+        |  }
+        |
+        |  implicit object Foo extends FooBuild[Foo, scala.Int]
+        |
+        |  def tryme[T](t: Foo[T]) = ???
+        |
+        |  tryme(40)
         |}
-        |implicit object Foo extends FooBuild[Foo, scala.Int]
-        |def tryme[T](t : Foo[T]) = ???
-        |tryme(40)
-      """.stripMargin
+        |""".stripMargin
     checkTextHasNoErrors(text)
   }
 
   def testSCL17364(): Unit = checkTextHasNoErrors(
-    """
-      |implicit class Test[T](name: Int) {
-      |  def withValue(value: T): Test[T] = this
-      |}
+    """object Wrapper {
+      |  implicit class Test[T](name: Int) {
+      |    def withValue(value: T): Test[T] = this
+      |  }
       |
-      |val test: Test[Boolean] = 123.withValue(true)
+      |  val test: Test[Boolean] = 123.withValue(true)
+      |}
       |""".stripMargin
   )
 
   def testSCL17368(): Unit = checkTextHasNoErrors(
-    """
-      |trait TestClassA
-      |trait TestClassB
-      |object TestClassA extends TestUtil[TestClassA]
+    """object Wrapper {
+      |  trait TestClassA
+      |  trait TestClassB
+      |  object TestClassA extends TestUtil[TestClassA]
       |
-      |trait TestUtil[I <: TestClassA] {
-      |  implicit def convert(classA: TestClassA)(implicit smt: I): TestClassB = null
+      |  trait TestUtil[I <: TestClassA] {
+      |    implicit def convert(classA: TestClassA)(implicit smt: I): TestClassB = null
+      |  }
+      |  implicit val instA: TestClassA = null
+      |  val instB: TestClassB = instA
       |}
-      |implicit val instA: TestClassA = null
-      |val instB: TestClassB = instA
       |""".stripMargin
   )
 }
