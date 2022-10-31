44ee29a4929bdd7033c9041e50cecb40d2c3ea68
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-10-31 13:02:22 +0100

    Update sbt to 1.7.3

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -53,7 +53,7 @@ object Versions {
 
     val latest_0_12 = "0.12.4"
     val latest_0_13 = "0.13.18"
-    val latest_1_0 = "1.7.2"
+    val latest_1_0 = "1.7.3"
     val latest: String = latest_1_0
     // ATTENTION: after adding sbt major version, also update:
     // buildInfoKeys, Sbt.scala and SbtUtil.latestCompatibleVersion
