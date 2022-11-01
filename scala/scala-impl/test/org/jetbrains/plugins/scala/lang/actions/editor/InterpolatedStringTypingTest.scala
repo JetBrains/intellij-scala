c33e7cca8f7334deb3f5739a8a3ba1b8c984bd4a
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-01 16:18:37 +0100

    tests: InterpolatedStringTypingTest: convert all string literals to multiline interpolated strings

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/InterpolatedStringTypingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/InterpolatedStringTypingTest.scala
@@ -1,5 +1,4 @@
-package org.jetbrains.plugins.scala
-package lang.actions.editor
+package org.jetbrains.plugins.scala.lang.actions.editor
 
 import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
@@ -11,85 +10,85 @@ class InterpolatedStringTypingTest extends EditorActionTestBase {
   import CodeInsightTestFixture.CARET_MARKER
 
   def testSimpleStringTypingOpeningQuote(): Unit = {
-    val text = "class A { val a = s" + CARET_MARKER + " }"
-    val assumedStub = "class A { val a = s\"" + CARET_MARKER + "\" }"
+    val text = s"""class A { val a = s$CARET_MARKER }"""
+    val assumedStub = s"""class A { val a = s"$CARET_MARKER" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testSimpleStringTypingClosingQuote(): Unit = {
-    val text = "class A { val a = s\"" + CARET_MARKER + "\" }"
-    val assumedStub = "class A { val a = s\"\"" + CARET_MARKER + " }"
+    val text = s"""class A { val a = s"$CARET_MARKER" }"""
+    val assumedStub = s"""class A { val a = s""$CARET_MARKER }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testSimpleQuasiQuoteTypingOpeningQuote(): Unit = {
-    val text = "class A { val a = q" + CARET_MARKER + " }"
-    val assumedStub = "class A { val a = q\"" + CARET_MARKER + "\" }"
+    val text = s"""class A { val a = q$CARET_MARKER }"""
+    val assumedStub = s"""class A { val a = q"$CARET_MARKER" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testSimpleQuasiQuoteTypingClosingQuote(): Unit = {
-    val text = "class A { val a = q\"" + CARET_MARKER + "\" }"
-    val assumedStub = "class A { val a = q\"\"" + CARET_MARKER + " }"
+    val text = s"""class A { val a = q"$CARET_MARKER" }"""
+    val assumedStub = """class A { val a = q""""" + CARET_MARKER + " }"
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testMultilineStringTypingOpeningQuote(): Unit = {
-    val text = "class A { val a = f\"\"" + CARET_MARKER + " }"
-    val assumedStub = "class A { val a = f\"\"\"" + CARET_MARKER + "\"\"\" }"
+    val text = s"""class A { val a = f""$CARET_MARKER }"""
+    val assumedStub = s"""class A { val a = f""\"$CARET_MARKER""\" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testMultilineQuasiQuoteOpeningQuote(): Unit = {
-    val text = "class A { val a = q\"\"" + CARET_MARKER + " }"
-    val assumedStub = "class A { val a = q\"\"\"" + CARET_MARKER + "\"\"\" }"
+    val text = s"""class A { val a = q""$CARET_MARKER }"""
+    val assumedStub = s"""class A { val a = q""\"$CARET_MARKER""\" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
-  def testMultilingStringClosingQuote1(): Unit = {
-    val text = "class A { val a = s\"\"\"blah blah" + CARET_MARKER + "\"\"\" }"
-    val assumedStub = "class A { val a = s\"\"\"blah blah\"" + CARET_MARKER + "\"\" }"
+  def testMultilineStringClosingQuote1(): Unit = {
+    val text = s"""class A { val a = s""\"blah blah$CARET_MARKER""\" }"""
+    val assumedStub = s"""class A { val a = s""\"blah blah"$CARET_MARKER"" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testMultilineStringClosingQuote2(): Unit = {
-    val text = "class A { val a = s\"\"\"blah blah\"\"" + CARET_MARKER + "\" }"
-    val assumedStub = "class A { val a = s\"\"\"blah blah\"\"\"" + CARET_MARKER + " }"
+    val text = s"""class A { val a = s""\"blah blah""$CARET_MARKER" }"""
+    val assumedStub = s"""class A { val a = s""\"blah blah""\"$CARET_MARKER }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testMultilineQuasiQuoteClosingQuote1(): Unit = {
-    val text = "class A { val a = q\"\"\"blah blah" + CARET_MARKER + "\"\"\" }"
-    val assumedStub = "class A { val a = q\"\"\"blah blah\"" + CARET_MARKER + "\"\" }"
+    val text = s"""class A { val a = q""\"blah blah$CARET_MARKER""\" }"""
+    val assumedStub = s"""class A { val a = q""\"blah blah"$CARET_MARKER"" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testMultilineQuasiQuoteClosingQuote2(): Unit = {
-    val text = "class A { val a = q\"\"\"blah blah\"\"" + CARET_MARKER + "\" }"
-    val assumedStub = "class A { val a = q\"\"\"blah blah\"\"\"" + CARET_MARKER + " }"
+    val text = s"""class A { val a = q""\"blah blah""$CARET_MARKER" }"""
+    val assumedStub = s"""class A { val a = q""\"blah blah""\"$CARET_MARKER }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '\"')
   }
 
   def testSimpleStringBraceTyped(): Unit = {
-    val text = "class A { val a = s\"blah blah $" + CARET_MARKER + "\" }"
-    val assumedStub = "class A { val a = s\"blah blah ${" + CARET_MARKER + "}\" }"
+    val text = s"""class A { val a = s"blah blah $$$CARET_MARKER" }"""
+    val assumedStub = s"""class A { val a = s"blah blah $${$CARET_MARKER}" }"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '{')
   }
 
   def testMultiLineStringBraceTyped(): Unit = {
-    val text = "class A { val a = f\"\"\"blah blah $" + CARET_MARKER + " blah blah\"\"\"}"
-    val assumedStub = "class A { val a = f\"\"\"blah blah ${" + CARET_MARKER + "} blah blah\"\"\"}"
+    val text = s"""class A { val a = f""\"blah blah $$$CARET_MARKER blah blah""\"}"""
+    val assumedStub = s"""class A { val a = f""\"blah blah $${$CARET_MARKER} blah blah""\"}"""
 
     checkGeneratedTextAfterTyping(text, assumedStub, '{')
   }
