6547ce2c7f5bcf9ebf8e54338eefd8f42fb4e803
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-10 10:57:20 +0100

    Resolve the module of a given virtual file inside a read action
    - This fixes sbt project importing, for which the tests helpfully started failing.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/util/ScalaUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/ScalaUtil.scala
@@ -5,6 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
+import org.jetbrains.plugins.scala.extensions.inReadAction
 import org.jetbrains.plugins.scala.lang.psi.api.ScFile
 import org.jetbrains.plugins.scala.project._
 
@@ -19,7 +20,7 @@ object ScalaUtil {
   }
 
   def getModuleForFile(file: VirtualFile)
-                      (implicit project: Project): Option[Module] = Option {
-    ProjectRootManager.getInstance(project).getFileIndex.getModuleForFile(file)
+                      (implicit project: Project): Option[Module] = inReadAction {
+    Option(ProjectRootManager.getInstance(project).getFileIndex.getModuleForFile(file))
   }
 }
