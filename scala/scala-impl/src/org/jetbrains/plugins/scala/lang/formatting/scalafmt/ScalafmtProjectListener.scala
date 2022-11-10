fbe2a46e04a6bcd62e6bce313c68af6b42d312fd
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-10 15:34:46 +0100

    Revert "Migrate ScalafmtProjectListener to startup activity API" #SCL-20728
    This reverts commit 0d406e77e641d41b94f6e245c10dcb6943718d31.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/scalafmt/ScalafmtProjectListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/scalafmt/ScalafmtProjectListener.scala
@@ -1,11 +1,10 @@
 package org.jetbrains.plugins.scala.lang.formatting.scalafmt
 
 import com.intellij.openapi.project.{Project, ProjectManagerListener}
-import com.intellij.openapi.startup.StartupActivity
 
-class ScalafmtProjectListener extends StartupActivity with ProjectManagerListener {
+class ScalafmtProjectListener extends ProjectManagerListener {
 
-  override def runActivity(project: Project): Unit = {
+  override def projectOpened(project: Project): Unit = {
     ScalaFmtSuggesterService.instance(project).init()
     ScalafmtDynamicConfigService.instanceIn(project).init()
   }
