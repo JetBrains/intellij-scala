1604e5bd5863624114e88ed6b000aa1aa4c4cf13
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-11 13:46:10 +0100

    Update sbt to version 1.8.0 and zinc to version 1.8.0

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -12,7 +12,7 @@ object Versions {
   // run runtimeDependencies/update manually
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.5.4"
-  val zincVersion = "1.7.2"
+  val zincVersion = "1.8.0"
   val intellijVersion = "223.7571.4"
 
   val nailgunVersion = "1.2.1"
@@ -35,7 +35,7 @@ object Versions {
     val binary_1_0 = "1.0" // 1.0 is the binary version of sbt 1.x series
 
     val latest_0_13 = "0.13.18"
-    val latest_1_0 = "1.7.3"
+    val latest_1_0 = "1.8.0"
     val latest: String = latest_1_0
     // ATTENTION: after adding sbt major version, also update:
     // buildInfoKeys, Sbt.scala and SbtUtil.latestCompatibleVersion
