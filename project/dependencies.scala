925d03caa51fe5f9b98e19446af60023eaec2cec
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-11 13:50:35 +0100

    Update Scala 3 to version 3.2.1 and scala3-sbt-bridge for Scala 3.2 to version 3.2.1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,7 +5,7 @@ import java.net.SocketTimeoutException
 
 object Versions {
   val scalaVersion: String = "2.13.10"
-  val scala3Version: String = "3.2.0"
+  val scala3Version: String = "3.2.1"
 
   // ATTENTION: when updating sbtVersion also update versions in MockSbt_1_0
   // NOTE: sbt-launch / bloop-launcher won't be fetched on refresh.
@@ -120,7 +120,7 @@ object Dependencies {
   val compilerBridgeSources_2_13 = "org.scala-sbt" % "compiler-bridge_2.13" % zincVersion classifier "sources"
   val sbtBridge_Scala_3_0 = "org.scala-lang" % "scala3-sbt-bridge" % "3.0.2"
   val sbtBridge_Scala_3_1 = "org.scala-lang" % "scala3-sbt-bridge" % "3.1.3"
-  val sbtBridge_Scala_3_2 = "org.scala-lang" % "scala3-sbt-bridge" % "3.2.0"
+  val sbtBridge_Scala_3_2 = "org.scala-lang" % "scala3-sbt-bridge" % "3.2.1"
 
   // "provided" danger: we statically depend on a single version, but need to support all the version
   // some part of our code is now statically dependent on lib classes, another part uses reflections for other versions
