e4de81e8ab4fcd7916a452d4d333b0323b224c98
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-11 15:21:39 +0100

    Update provided test dependencies for test frameworks

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -126,9 +126,9 @@ object Dependencies {
   // some part of our code is now statically dependent on lib classes, another part uses reflections for other versions
   object provided {
     val scalaTest = scalaTestNotSpecified % Provided
-    val utest = "com.lihaoyi" %% "utest" % "0.8.0" % Provided
+    val utest = "com.lihaoyi" %% "utest" % "0.8.1" % Provided
     val specs2_2x = "org.specs2" % "specs2-core_2.12" % "2.5" % Provided excludeAll ExclusionRule(organization = "org.ow2.asm")
-    val specs2_4x = "org.specs2" %% "specs2-core" % "4.16.1" % Provided excludeAll ExclusionRule(organization = "org.ow2.asm")
+    val specs2_4x = "org.specs2" %% "specs2-core" % "4.18.0" % Provided excludeAll ExclusionRule(organization = "org.ow2.asm")
   }
 
   /** The filtering function returns true for jars to be removed.
