0b5782a9fc9a4825975e6e5fe9180f41757218b4
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2022-11-11 19:19:23 +0100

    Implements an imports processor for statistics about Scala libraries usage #SCL-20565 Fixed

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/statistics/ScalaUsageImportProcessor.scala
@@ -0,0 +1,37 @@
+package org.jetbrains.plugins.scala.statistics
+
+import com.intellij.internal.statistic.libraryUsage.LibraryUsageImportProcessor
+import com.intellij.psi.{PsiElement, PsiFile}
+import org.jetbrains.plugins.scala.extensions.{IteratorExt, PsiElementExt}
+import org.jetbrains.plugins.scala.lang.psi.ScImportsHolder
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScPackaging
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.{ScImportExpr, ScImportStmt}
+
+import java.util
+import scala.jdk.CollectionConverters.SeqHasAsJava
+
+class ScalaUsageImportProcessor extends LibraryUsageImportProcessor[ScImportExpr] {
+  override def imports(psiFile: PsiFile): util.List[ScImportExpr] = psiFile match {
+    case scalaFile: ScalaFile =>
+      val importStatements = collectImportsStatements(scalaFile)
+      val importExpressions = importStatements.flatMap(_.importExprs)
+      importExpressions.asJava
+    case _ =>
+      util.List.of()
+  }
+
+  override def importQualifier(importExpr: ScImportExpr): String =
+    importExpr.qualifier.map(_.qualName).orNull
+
+  override def isSingleElementImport(importExpr: ScImportExpr): Boolean =
+    importExpr.selectorSet.isEmpty && !importExpr.hasWildcardSelector
+
+  override def resolve(importExpr: ScImportExpr): PsiElement =
+    importExpr.qualifier.map(_.resolve()).orNull
+
+  private def collectImportsStatements(fileOrPackage: ScImportsHolder): Seq[ScImportStmt] = {
+    val nestedPackagings = fileOrPackage.children.filterByType[ScPackaging]
+    fileOrPackage.getImportStatements ++ nestedPackagings.flatMap(collectImportsStatements)
+  }
+}
