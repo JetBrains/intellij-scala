907887e2d7b26f69edf705866e9987413cf688f2
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2022-11-15 12:04:03 +0100

    Run AfterUpdateDottyVersionScript with Scala 3.2

--- scala/scala-impl/testdata/lang/resolveSemanticDb/source/realizable-mut.scala
+++ scala/scala-impl/testdata/lang/resolveSemanticDb/source/realizable-mut.scala
@@ -6,4 +6,8 @@ object Foo {
   val a = new A(x)
 
   val y: a.y.type = x
+// 1 |val y: a.y.type = x
+//   |         ^
+//   |         Object(x)(a.y) is not a legal path
+//   |         since it refers to nonfinal variable y
 }
