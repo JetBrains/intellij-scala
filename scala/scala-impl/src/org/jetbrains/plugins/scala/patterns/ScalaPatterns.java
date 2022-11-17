ef436d15eda18a3302ba6b0b0c58b1692d0c9249
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-17 21:21:42 +0100

    language injection: ScalaPatterns now only accepts ScStringLiteral (not ScLiteral)
    (cherry picked from commit b396fd645d745265e911d4493d74bfd114373010)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/patterns/ScalaPatterns.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/patterns/ScalaPatterns.java
@@ -1,10 +1,10 @@
 package org.jetbrains.plugins.scala.patterns;
 
 import com.intellij.patterns.PsiJavaPatterns;
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral;
+import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScStringLiteral;
 
 public class ScalaPatterns extends PsiJavaPatterns {
-    public static ScalaElementPattern.Capture<ScLiteral> scalaLiteral() {
-        return new ScalaElementPattern.Capture<>(ScLiteral.class);
+    public static ScalaElementPattern.Capture<ScStringLiteral> scalaLiteral() {
+        return new ScalaElementPattern.Capture<>(ScStringLiteral.class);
     }
 }
