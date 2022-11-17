454066bfe306124f7fe4f19d510fcb790b3616b1
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-17 20:43:07 +0100

    Add a registry key for suppressing Java highlighting when Compiler Based Highlighting is enabled
    - The key is enabled by default, which means that Java files are not highlighted when Compiler Based Highlighting is enabled for Scala 3.
    - Toggling the key to off enables highlighting of Java files.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaDefaultHighlightingSettingProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaDefaultHighlightingSettingProvider.scala
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.scala.annotator
 import com.intellij.codeInsight.daemon.impl.analysis.{DefaultHighlightingSettingProvider, FileHighlightingSetting}
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.{PsiJavaFile, PsiManager}
 import org.jetbrains.plugins.scala.project.ProjectPsiElementExt
@@ -16,6 +17,7 @@ class ScalaDefaultHighlightingSettingProvider extends DefaultHighlightingSetting
       null
     else if (!ScalaHighlightingMode.showCompilerErrorsScala3(project))
       null
+    else if (!Registry.is("scala.compiler.highlighting.suppress.java.highlighting")) null
     else
       Option(PsiManager.getInstance(project).findFile(file)).filter(_.isInScala3Module)
         .collect {
