8015b70a2e94a7d0e236ab0f4331c876cfc2d64f
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-18 17:23:44 +0100

    language injection: move ScalaInjectedStringLiteralManipulator to `intelliLang` module

---
+++ scala/integration/intellilang/src/org/jetbrains/plugins/scala/injection/ScalaInjectedStringLiteralManipulator.scala
@@ -0,0 +1,50 @@
+package org.jetbrains.plugins.scala.injection
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.AbstractElementManipulator
+import org.jetbrains.plugins.scala.lang.psi.api.base._
+import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScStringLiteral
+import org.jetbrains.plugins.scala.lang.psi.impl._
+
+final class ScalaInjectedStringLiteralManipulator extends AbstractElementManipulator[ScStringLiteral] {
+
+  override def handleContentChange(
+    literal: ScStringLiteral,
+    range: TextRange,
+    newContent: String
+  ): ScStringLiteral = {
+    val text = literal.getText
+
+    val needEscape = !literal.isInstanceOf[ScInterpolatedStringLiteral] && !literal.isMultiLineString
+    val newText = text.substring(0, range.getStartOffset) +
+      (if (needEscape) StringUtil.escapeStringCharacters(newContent) else newContent) +
+      text.substring(range.getEndOffset)
+
+    replaceWith(literal, newText)
+  }
+
+  override def getRangeInElement(element: ScStringLiteral): TextRange =
+    if (element.isString)
+      element.contentRange.shiftLeft(element.getTextRange.getStartOffset)
+    else
+      super.getRangeInElement(element)
+
+  private def replaceWith(literal: ScStringLiteral, newText: String): ScStringLiteral = {
+    val expression = ScalaPsiElementFactory.createExpressionFromText(newText, literal)(literal)
+    (expression, literal) match {
+      case (newInterpolatedLiteral: ScInterpolatedStringLiteral, _: ScInterpolatedStringLiteral) =>
+        literal.replace(newInterpolatedLiteral).asInstanceOf[ScStringLiteral]
+      case (newLiteral: ScStringLiteral, _) =>
+        newLiteral.getFirstChild match {
+          case null => null
+          case newLeaf =>
+            literal.getFirstChild.replace(newLeaf)
+            literal
+        }
+      case _ => null
+    }
+  }
+
+}
+
