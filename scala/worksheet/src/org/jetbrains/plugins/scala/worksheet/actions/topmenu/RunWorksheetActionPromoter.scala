8e5048337dd61512e2bdaae9bd72fa28a90a3b1b
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-24 20:56:57 +0100

    worksheet: RunWorksheetActionPromoter: change logic from PsiFile-based to VirtualFile-based #SCL-20775 fixed
    context.getData(CommonDataKeys.PSI_FILE) always returns null in 2022.3
    According to Greg Shrago (@gregsh) "It's not allowed to get PSI in ActionPromoter"
    (hope it will be reflected in JavaDoc of com.intellij.openapi.actionSystem.ActionPromoter)

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/actions/topmenu/RunWorksheetActionPromoter.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/actions/topmenu/RunWorksheetActionPromoter.scala
@@ -1,13 +1,27 @@
 package org.jetbrains.plugins.scala.worksheet.actions.topmenu
 
-import com.intellij.openapi.actionSystem.{AnAction, CommonDataKeys, DataContext}
+import com.intellij.openapi.actionSystem.{AnAction, DataContext, PlatformCoreDataKeys}
+import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.plugins.scala.actions.SingleActionPromoterBase
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 
 class RunWorksheetActionPromoter extends SingleActionPromoterBase {
 
-  override def shouldPromote(anAction: AnAction, context: DataContext): Boolean = {
-    anAction.isInstanceOf[RunWorksheetAction] &&
-      context.getData(CommonDataKeys.PSI_FILE).isInstanceOf[ScalaFile]
+  override def shouldPromote(anAction: AnAction, context: DataContext): Boolean =
+    anAction.isInstanceOf[RunWorksheetAction] && isInInScalaFile(context)
+
+  private def isInInScalaFile(context: DataContext): Boolean = {
+    //According to Greg Shrago context.getData(CommonDataKeys.VIRTUAL_FILE) might be not reliable approach
+    //and better to use file editor instead
+    val fileEditor = context.getData(PlatformCoreDataKeys.FILE_EDITOR)
+    fileEditor != null && {
+      val vFile = fileEditor.getFile
+      vFile != null || hasScalaWorksheetExtension(vFile)
+    }
+  }
+
+  private def hasScalaWorksheetExtension(vFile: VirtualFile) = {
+    val extension = vFile.getExtension
+    extension == "sc" ||
+      extension == "scala" // for Scala Scratch files which behave like extensions
   }
 }
