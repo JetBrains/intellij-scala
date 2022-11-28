539fd6a13060076608e705a65c09f8258f0cb727
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-28 16:36:42 +0100

    sbt cross-project: add project highlight tests with shared sources referencing declarations in dependent modules ##SCL-19567
    Also test that "unused declaration" and "can be private" inspections are not triggered for declarations in dependent modules if they are used from shared sources
    
    (cherry picked from commit 3855e7dfb7b5931cdada771c2dd6fdfc7650b609)

---
+++ sbt/sbt-impl/test/org/jetbrains/plugins/scala/projectHighlighting/local/SbtCrossBuildProjectHighlightingTest_BackReferencesEnabled.scala
@@ -0,0 +1,142 @@
+package org.jetbrains.plugins.scala.projectHighlighting.local
+
+import com.intellij.codeInspection.deadCode.UnusedDeclarationInspection
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+import com.intellij.testFramework.InspectionsKt
+import org.jetbrains.plugins.scala.HighlightingTests
+import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.{ScalaAccessCanBeTightenedInspection, ScalaUnusedDeclarationInspection}
+import org.jetbrains.plugins.scala.projectHighlighting.base.AllProjectHighlightingTest.relativePathOf
+import org.jetbrains.plugins.scala.projectHighlighting.base.SbtProjectHighlightingLocalProjectsTestBase
+import org.jetbrains.plugins.scala.projectHighlighting.reporter.HighlightingProgressReporter
+import org.jetbrains.plugins.scala.util.RevertableChange
+import org.junit.experimental.categories.Category
+
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
+@Category(Array(classOf[HighlightingTests]))
+abstract class SbtCrossBuildProjectHighlightingTestBase extends SbtProjectHighlightingLocalProjectsTestBase {
+
+  override def projectName = "sbt-crossproject-test-project"
+
+  override def setUp(): Unit = {
+    super.setUp()
+
+    codeInsightFixture.enableInspections(
+      classOf[ScalaUnusedDeclarationInspection],
+      classOf[ScalaAccessCanBeTightenedInspection],
+    )
+
+    //NOTE: java UnusedDeclarationInspection requires some special initialization in tests unlike most of the inspections
+    val javaUnusedDeclarationInspection = new UnusedDeclarationInspection(true)
+    InspectionsKt.enableInspectionTool(getProject, javaUnusedDeclarationInspection, getTestRootDisposable)
+  }
+
+  override protected def highlightSingleFile(
+    virtualFile: VirtualFile,
+    psiFile: PsiFile,
+    reporter: HighlightingProgressReporter,
+  ): Unit = {
+    codeInsightFixture.openFileInEditor(virtualFile)
+    val infosAll = getProjectFixture.doHighlighting().asScala.toSeq
+    val infosWithDescription = infosAll.filter(_.getDescription != null)
+    infosWithDescription.foreach { error =>
+      val range = TextRange.create(error.getStartOffset, error.getEndOffset)
+      reporter.reportError(relativePathOf(psiFile), range, error.getDescription)
+    }
+  }
+}
+
+
+class SbtCrossBuildProjectHighlightingTest_BackReferencesEnabled extends SbtCrossBuildProjectHighlightingTestBase {
+
+  override def testHighlighting(): Unit = {
+    val enableBackReferencesFromSharedSources = RevertableChange.withModifiedScalaProjectSettings[Boolean](
+      getProject,
+      _.isEnableBackReferencesFromSharedSources,
+      _.setEnableBackReferencesFromSharedSources(_),
+      true
+    )
+    enableBackReferencesFromSharedSources.run {
+      super.testHighlighting()
+    }
+  }
+}
+
+class SbtCrossBuildProjectHighlightingTest_BackReferencesDisabled extends SbtCrossBuildProjectHighlightingTestBase {
+
+  override def testHighlighting(): Unit = {
+    super.testHighlighting()
+  }
+
+  import org.jetbrains.plugins.scala.util.TextRangeUtils.ImplicitConversions.tupleToTextRange
+
+  override protected def filesWithProblems: Map[String, Set[TextRange]] = Map(
+    "coreFull/js/src/main/scala/org/example/MyClassInPlatformSpecificModule.scala" -> Set(
+      (27, 58), // Declaration is never used
+      (130, 154), // Declaration is never used
+    ),
+    "coreFull/js/src/main/scala/org/example/MyJavaClassInPlatformSpecificModule.java" -> Set(
+      (35, 70), // Class 'MyJavaClassInPlatformSpecificModule' is never used
+      (189, 217), // Method 'myJavaMethodInSpecificModule()' is never used
+    ),
+    "coreFull/jvm/src/main/scala/org/example/MyClassInPlatformSpecificModule.scala" -> Set(
+      (27, 58), // Declaration is never used
+      (130, 154), // Declaration is never used
+    ),
+    "coreFull/jvm/src/main/scala/org/example/MyJavaClassInPlatformSpecificModule.java" -> Set(
+      (35, 70), // Class 'MyJavaClassInPlatformSpecificModule' is never used
+      (189, 217), // Method 'myJavaMethodInSpecificModule()' is never used
+    ),
+    "coreFull/native/src/main/scala/org/example/MyClassInPlatformSpecificModule.scala" -> Set(
+      (27, 58), // Declaration is never used
+      (130, 154), // Declaration is never used
+    ),
+    "coreFull/native/src/main/scala/org/example/MyJavaClassInPlatformSpecificModule.java" -> Set(
+      (35, 70), // Class 'MyJavaClassInPlatformSpecificModule' is never used
+      (189, 217), // Method 'myJavaMethodInSpecificModule()' is never used
+    ),
+    "coreFull/shared/src/main/scala/org/example/MyClassInSharedSources.scala" -> Set(
+      (58, 89), // Cannot resolve symbol MyClassInPlatformSpecificModule
+      (125, 160), // Cannot resolve symbol MyJavaClassInPlatformSpecificModule
+    ),
+    "coreFull/shared/src/main/scala/org/example/MyJavaClassInSharedSources.java" -> Set(
+      (103, 134), // Cannot resolve symbol 'MyClassInPlatformSpecificModule'
+      (181, 216), // Cannot resolve symbol 'MyJavaClassInPlatformSpecificModule'
+    ),
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    "corePure/.js/src/main/scala/org/example/MyClassInPlatformSpecificModule.scala" -> Set(
+      (27, 58), // Declaration is never used
+      (130, 154), // Declaration is never used
+    ),
+    "corePure/.js/src/main/scala/org/example/MyJavaClassInPlatformSpecificModule.java" -> Set(
+      (35, 70), // Class 'MyJavaClassInPlatformSpecificModule' is never used
+      (189, 217), // Method 'myJavaMethodInSpecificModule()' is never used
+    ),
+    "corePure/.jvm/src/main/scala/org/example/MyClassInPlatformSpecificModule.scala" -> Set(
+      (27, 58), // Declaration is never used
+      (130, 154), // Declaration is never used
+    ),
+    "corePure/.jvm/src/main/scala/org/example/MyJavaClassInPlatformSpecificModule.java" -> Set(
+      (35, 70), // Class 'MyJavaClassInPlatformSpecificModule' is never used
+      (189, 217), // Method 'myJavaMethodInSpecificModule()' is never used
+    ),
+    "corePure/.native/src/main/scala/org/example/MyClassInPlatformSpecificModule.scala" -> Set(
+      (27, 58), // Declaration is never used
+      (130, 154), // Declaration is never used
+    ),
+    "corePure/.native/src/main/scala/org/example/MyJavaClassInPlatformSpecificModule.java" -> Set(
+      (35, 70), // Class 'MyJavaClassInPlatformSpecificModule' is never used
+      (189, 217), // Method 'myJavaMethodInSpecificModule()' is never used
+    ),
+    "corePure/src/main/scala/org/example/MyClassInSharedSources.scala" -> Set(
+      (58, 89), // Cannot resolve symbol MyClassInPlatformSpecificModule
+      (125, 160), // Cannot resolve symbol MyJavaClassInPlatformSpecificModule
+    ),
+    "corePure/src/main/scala/org/example/MyJavaClassInSharedSources.java" -> Set(
+      (103, 134), // Cannot resolve symbol 'MyClassInPlatformSpecificModule'
+      (181, 216), // Cannot resolve symbol 'MyJavaClassInPlatformSpecificModule'
+    )
+  )
+}
