721e4544276638b6d7ce8c58309327c6de391d21
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-29 20:24:20 +0100

    package search: cleanup SbtProjectModuleOperationProvider

--- scala/integration/packagesearch/src/org/jetbrains/plugins/scala/packagesearch/SbtProjectModuleOperationProvider.scala
+++ scala/integration/packagesearch/src/org/jetbrains/plugins/scala/packagesearch/SbtProjectModuleOperationProvider.scala
@@ -6,12 +6,19 @@ import com.intellij.psi.PsiFile
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.{AbstractAsyncProjectModuleOperationProvider, ProjectModuleType}
 import org.jetbrains.plugins.scala.packagesearch.utils.SbtProjectModuleType
 
+//noinspection UnstableApiUsage
 class SbtProjectModuleOperationProvider extends AbstractAsyncProjectModuleOperationProvider {
   override def hasSupportFor(project: Project, psiFile: PsiFile): Boolean = {
     val file = psiFile.getVirtualFile
-    if (file == null || file.getExtension == null) return false
-    if (file.getExtension.toLowerCase() == "sbt" || file.getExtension.toLowerCase() == "scala") return true
-    false
+    if (file == null)
+      return false
+
+    val extension = file.getExtension
+    if (extension == null)
+      return false
+
+    val extensionLowercase = extension.toLowerCase()
+    extensionLowercase == "sbt" || extensionLowercase == "scala"
   }
 
   override def hasSupportFor(projectModuleType: ProjectModuleType): Boolean = projectModuleType match {
