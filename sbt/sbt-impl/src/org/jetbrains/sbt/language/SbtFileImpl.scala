19096e2f5161d4119e512e12517db24587868fe1
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-11-29 13:11:38 +0100

    sbt cross-project: resolve declarations in dependent modules from shared Java sources #SCL-19567
    Introduce ScalaSharedSourcesResolveScopeEnlarger
    
    (cherry picked from commit 9cb43439cea58dbb33c529a8efd151092b1e3716)

--- sbt/sbt-impl/src/org/jetbrains/sbt/language/SbtFileImpl.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/language/SbtFileImpl.scala
@@ -56,6 +56,7 @@ final class SbtFileImpl private[language](provider: FileViewProvider)
     else Some(ScalaPsiElementFactory.createScalaFileFromText(imports.mkString("import ", ", ", ";"), ScalaFeatures.default))
   })
 
+  /** NOTE: consider rewriting this using [[com.intellij.psi.ResolveScopeEnlarger]] */
   override def getFileResolveScope: GlobalSearchScope = {
     val target = targetModule
     target match {
