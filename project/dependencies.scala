b357f10026ddbbadf99d434f8262e475c4b17e6a
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-11-30 18:19:18 +0100

    Update IDEA to version 223.7571.182 stable

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -13,7 +13,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.5.4"
   val zincVersion = "1.8.0"
-  val intellijVersion = "223.7571.123"
+  val intellijVersion = "223.7571.182"
 
   val Utils.DataForManagedIntellijDependencies(
     intellijVersion_ForManagedIntellijDependencies,
