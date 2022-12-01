a6fbc864e73dcb3ba3a7a1f23457d8d43c1d1ffa
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-12-01 14:06:33 +0100

    rename refactoring: cleanup RenameScalaClassProcessor: extract some methods, simplify the code
    (cherry picked from commit 27d68a424bef348053b74894c834a9dd5063bf8a)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/rename/RenameScalaClassProcessor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/rename/RenameScalaClassProcessor.scala
@@ -3,25 +3,22 @@ package org.jetbrains.plugins.scala.lang.refactoring.rename
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.SearchScope
-import com.intellij.psi.{PsiElement, PsiReference}
+import com.intellij.psi.{PsiClass, PsiElement, PsiReference}
 import com.intellij.refactoring.listeners.RefactoringElementListener
 import com.intellij.refactoring.rename.{RenameDialog, RenameJavaClassProcessor}
 import com.intellij.usageView.UsageInfo
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
-import org.jetbrains.plugins.scala.lang.psi.api.ScBegin
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScTypeParam
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
 import org.jetbrains.plugins.scala.lang.psi.light.PsiClassWrapper
-import org.jetbrains.plugins.scala.lang.scaladoc.parser.parsing.MyScaladocParsing
-import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocComment
 import org.jetbrains.plugins.scala.settings.ScalaApplicationSettings
 
 import java.awt.BorderLayout
 import java.util
 import javax.swing.{JCheckBox, JComponent, JPanel}
-import scala.annotation.tailrec
+import scala.jdk.CollectionConverters.SetHasAsScala
 
 class RenameScalaClassProcessor extends RenameJavaClassProcessor with ScalaRenameProcessor {
   override def canProcessElement(element: PsiElement): Boolean = {
@@ -33,53 +30,59 @@ class RenameScalaClassProcessor extends RenameJavaClassProcessor with ScalaRenam
     case _ => element
   }
 
-  override def findReferences(element: PsiElement,
-                              searchScope: SearchScope,
-                              searchInCommentsAndStrings: Boolean): util.Collection[PsiReference] =
-    ScalaRenameUtil.replaceImportClassReferences(super.findReferences(element, searchScope, searchInCommentsAndStrings))
+  override def findReferences(
+    element: PsiElement,
+    searchScope: SearchScope,
+    searchInCommentsAndStrings: Boolean
+  ): util.Collection[PsiReference] = {
+    val references0 = super.findReferences(element, searchScope, searchInCommentsAndStrings)
+    val references = ScalaRenameUtil.replaceImportClassReferences(references0)
+    references
+  }
 
   override def prepareRenaming(element: PsiElement, newName: String, allRenames: util.Map[PsiElement, String]): Unit = {
     element match {
-      case td: ScTypeDefinition =>
-        ScalaPsiUtil.getCompanionModule(td) match {
-          case Some(companion: ScBegin) if ScalaApplicationSettings.getInstance().RENAME_COMPANION_MODULE =>
-            allRenames.put(companion, newName)
-          case _ =>
-        }
-        @tailrec
-        def isTop(element: PsiElement): Boolean = {
-          element match {
-            case null => true
-            case _: ScTemplateDefinition => false
-            case _ => isTop(element.getContext)
-          }
-        }
-        val file = td.getContainingFile
-        if (file != null && isTop(element.getContext) && file.name == td.name + ".scala") {
-          allRenames.put(file, newName + ".scala")
-        }
+      case typeDef: ScTypeDefinition =>
+        collectRenamesForTypeDefinition(typeDef, newName, allRenames)
       case _ =>
     }
 
-    //put rename for fake object companion class
-    def addLightClasses(element: PsiElement): Unit = {
-      element match {
-        case o: ScObject =>
-          o.fakeCompanionClass match {
-            case Some(clazz) => allRenames.put(clazz, newName)
-            case None =>
-          }
-        case t: ScTrait =>
-          allRenames.put(t.fakeCompanionClass, newName + "$class")
-        case _ =>
+    val allRenamedTypeDefs = allRenames.keySet.asScala.clone.filterByType[ScTypeDefinition]
+    for {
+      renamedTypeDef <- allRenamedTypeDefs
+    } {
+      fakeCompanionClassRename(renamedTypeDef, newName).foreach { case (clazz, newName) =>
+        allRenames.put(clazz, newName)
       }
     }
 
-    import scala.jdk.CollectionConverters._
-    val elems = allRenames.keySet().asScala.clone()
-    elems.foreach(addLightClasses)
-
     ScalaElementToRenameContributor.addAllElements(element, newName, allRenames)
+  }
+
+  private def fakeCompanionClassRename(typeDef: ScTypeDefinition, newName: String): Option[(PsiClass, String)] =
+    typeDef match {
+      case o: ScObject => o.fakeCompanionClass.map(_ -> newName)
+      case t: ScTrait  => Some(t.fakeCompanionClass -> (newName + "$class"))
+      case _           => None
+    }
+
+  private def collectRenamesForTypeDefinition(
+    typeDefinition: ScTypeDefinition,
+    newName: String,
+    allRenames: util.Map[PsiElement, String]
+  ): Unit = {
+    val companion = ScalaPsiUtil.getCompanionModule(typeDefinition)
+    companion match {
+      case Some(companion) if ScalaApplicationSettings.getInstance.RENAME_COMPANION_MODULE =>
+        allRenames.put(companion, newName)
+      case _ =>
+    }
+
+    val file = typeDefinition.getContainingFile
+    val containingFileHasSameNameAsToplevelDef = file != null && typeDefinition.isTopLevel && file.name == typeDefinition.name + ".scala"
+    if (containingFileHasSameNameAsToplevelDef) {
+      allRenames.put(file, s"$newName.scala")
+    }
   }
 
   override def getElementToSearchInStringsAndComments(element: PsiElement): PsiElement = {
