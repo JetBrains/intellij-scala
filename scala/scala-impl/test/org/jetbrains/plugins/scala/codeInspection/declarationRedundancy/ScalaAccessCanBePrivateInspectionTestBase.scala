d10d457fc3b1ec5df789c76a9f8aa3344f4112d5
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-12-12 19:13:38 +0100

    unused declaration tests: replace WorksheetInspectionTest with AccessCanBePrivateInspectionInScratchFileWorksheetTest and AccessCanBePrivateInspectionInWorksheetTest.scala

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/ScalaAccessCanBePrivateInspectionTestBase.scala
@@ -0,0 +1,17 @@
+package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy
+
+import com.intellij.codeInspection.LocalInspectionTool
+import org.jetbrains.plugins.scala.codeInspection.{ScalaInspectionBundle, ScalaInspectionTestBase}
+
+abstract class ScalaAccessCanBePrivateInspectionTestBase extends ScalaInspectionTestBase {
+
+  override protected val description : String = ScalaInspectionBundle.message("access.can.be.private")
+
+  override protected val classOfInspection: Class[_ <: LocalInspectionTool] = classOf[ScalaAccessCanBeTightenedInspection]
+
+  override def setUp(): Unit = {
+    super.setUp()
+
+    deleteAllGlobalScratchFiles()
+  }
+}
