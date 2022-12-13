2d31c61728412820faddeda55261f8292683c86b
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2022-12-13 13:02:18 +0100

    MergeIfToOrIntention: support indentation-based syntax #SCL-19060

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/intention/controlFlow/MergeIfToOrIntention.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/intention/controlFlow/MergeIfToOrIntention.scala
@@ -13,8 +13,10 @@ import com.intellij.psi.{PsiDocumentManager, PsiElement}
 import org.jetbrains.plugins.scala.codeInsight.ScalaCodeInsightBundle
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.lang.psi.api.expr._
-import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.createExpressionFromText
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.createElementFromText
+import org.jetbrains.plugins.scala.project.{ProjectContext, ScalaFeatures}
 
 import scala.collection.mutable
 
@@ -25,16 +27,16 @@ final class MergeIfToOrIntention extends PsiElementBaseIntentionAction {
     if (ifStmt == null) return false
 
     val offset = editor.getCaretModel.getOffset
-    val thenBranch =  ifStmt.thenExpression.orNull
-    val elseBranch =  ifStmt.elseExpression.orNull
+    val thenBranch = ifStmt.thenExpression.orNull
+    val elseBranch = ifStmt.elseExpression.orNull
     if (thenBranch == null || elseBranch == null) return false
 
     if (!elseBranch.is[ScIf]) return false
     if (ifStmt.condition.orNull == null) return false
 
     if (!(thenBranch.getTextRange.getEndOffset <= offset && offset <= elseBranch.getTextRange.getStartOffset) &&
-    !(ifStmt.getTextRange.getStartOffset <= offset && offset <= ifStmt.condition.get.getTextRange.getStartOffset))
-    return false
+      !(ifStmt.getTextRange.getStartOffset <= offset && offset <= ifStmt.condition.get.getTextRange.getStartOffset))
+      return false
 
     val innerThenBranch = elseBranch.asInstanceOf[ScIf].thenExpression.orNull
     if (innerThenBranch == null) return false
@@ -43,24 +45,27 @@ final class MergeIfToOrIntention extends PsiElementBaseIntentionAction {
   }
 
   override def invoke(project: Project, editor: Editor, element: PsiElement): Unit = {
-    val ifStmt : ScIf = PsiTreeUtil.getParentOfType(element, classOf[ScIf], false)
+    val ifStmt: ScIf = PsiTreeUtil.getParentOfType(element, classOf[ScIf], false)
     if (ifStmt == null || !ifStmt.isValid) return
 
     val start = ifStmt.getTextRange.getStartOffset
-    val expr = new mutable.StringBuilder
     val outerCondition = ifStmt.condition.get.getText
     val innerIfStmt = ifStmt.elseExpression.get.asInstanceOf[ScIf]
     val innerCondition = innerIfStmt.condition.get.getText
     val innerElseBranch = innerIfStmt.elseExpression.orNull
-    val newlineBeforeElse = ifStmt.children.find(_.getNode.getElementType == ScalaTokenTypes.kELSE).
-      exists(_.getPrevSibling.getText.contains("\n"))
+    val newlineBeforeElse = ifStmt.children.find(_.getNode.getElementType == ScalaTokenTypes.kELSE)
+      .exists(_.getPrevSibling.getText.contains("\n"))
 
-    expr.append("if (").append(outerCondition).append(" || ").append(innerCondition).append(") ").
-      append(ifStmt.thenExpression.get.getText)
-    if (innerElseBranch != null) expr.append(if (newlineBeforeElse) "\n" else " ").append("else ").
-      append(innerElseBranch.getText)
+    val expr = new mutable.StringBuilder()
+      .append("if (").append(outerCondition).append(" || ").append(innerCondition).append(") ")
+      .append(ifStmt.thenExpression.get.getText)
+    if (innerElseBranch != null)
+      expr.append(if (newlineBeforeElse) "\n" else " ")
+        .append("else ").append(innerElseBranch.getText)
 
-    val newIfStmt: ScExpression = createExpressionFromText(expr.toString(), element)(element.getManager)
+    implicit val ctx: ProjectContext = project
+    implicit val features: ScalaFeatures = element
+    val newIfStmt = ScalaPsiUtil.convertIfToBracelessIfNeeded(createElementFromText[ScIf](expr.toString(), element))
 
     IntentionPreviewUtils.write { () =>
       ifStmt.replaceExpression(newIfStmt, removeParenthesis = true)
