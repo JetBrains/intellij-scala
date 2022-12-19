3a66c586b8727f3874b9abff4da10b91ec70d990
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-12-19 19:20:38 +0100

    bsp: add action to regenerate bsp configuration file #SCL-20865
    It might be helpful in case the file exists but is potentially corrupted/outdated/etc
    
    NOTE:
    This is a weak implementation!
    I don't have much experience with BSP, so I am not sure what would the perfect solution look like

---
+++ bsp/src/org/jetbrains/bsp/project/importing/experimental/GenerateBspConfigurationFile.scala
@@ -0,0 +1,32 @@
+package org.jetbrains.bsp.project.importing.experimental
+
+import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
+import com.intellij.openapi.project.ProjectUtil
+import org.jetbrains.bsp.{BspBundle, BspUtil}
+
+class GenerateBspConfigurationFile extends AnAction(
+  BspBundle.message("generate.bsp.configuration.file")
+) {
+
+  override def update(e: AnActionEvent): Unit = {
+    val project = e.getProject
+    val isBspProject = project != null && BspUtil.isBspProject(project)
+    if (!isBspProject) {
+      e.getPresentation.setEnabled(false)
+      e.getPresentation.setVisible(false)
+    }
+  }
+
+  override def actionPerformed(e: AnActionEvent): Unit = {
+    val project = e.getProject
+    if (project == null)
+      return
+
+    val projectDir = ProjectUtil.guessProjectDir(project)
+    if (projectDir == null)
+      return
+
+    val workspace = projectDir.toNioPath
+    new GenerateBspConfig(project, workspace.toFile).runSynchronously()
+  }
+}
