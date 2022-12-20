753726210a5219ee9b65f28f4469f0cdd05bfc0b
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2022-12-20 14:37:34 +0100

    bsp: minor cleanup: Cancelable.scala

--- bsp/src/org/jetbrains/bsp/protocol/session/Cancelable.scala
+++ bsp/src/org/jetbrains/bsp/protocol/session/Cancelable.scala
@@ -3,24 +3,30 @@ package org.jetbrains.bsp.protocol.session
 import scala.collection.mutable.ListBuffer
 import scala.util.control.NonFatal
 
-
 private abstract class Cancelable {
   def cancel(): Unit
 }
 
 private object Cancelable {
   def apply(fn: () => Unit): Cancelable = () => fn()
+
   val empty: Cancelable = Cancelable(() => ())
+
   def cancelAll(iterable: Iterable[Cancelable]): Unit = {
-    var errors = ListBuffer.empty[Throwable]
+    val errors = ListBuffer.empty[Throwable]
     iterable.foreach { cancelable =>
       try cancelable.cancel()
-      catch { case NonFatal(ex) => errors += ex }
+      catch {
+        case NonFatal(ex) =>
+          errors += ex
+      }
     }
     errors.toList match {
       case head :: tail =>
         tail.foreach { e =>
-          if (e ne head) {head.addSuppressed(e)}
+          if (e ne head) {
+            head.addSuppressed(e)
+          }
         }
         throw head
       case _ =>
