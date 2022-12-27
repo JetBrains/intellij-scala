f24d55c194673fc2da23272b490e9b44f7341ed0
Izmar Verhage <izmar.verhage@jetbrains.com>
2022-12-27 10:34:28 +0100

    Reimplement targetCanBePrivate
    While looking into a can-be-private false positive I noticed that
    my previous targetCanBePrivate implementation was relatively hard
    to understand, and it wasn't idiomatic.
    
    Both should be addressed in this commit. In the previous
    implementation an arbitrary tree-walk-up limit of up to 10 steps up
    was imposed. I've decided to eliminate this limit, as we don't
    seem to do something similar in many other places. We now simply
    perform the required isAncestorOf checks, regardless of how nested
    within a typedef a reference lies.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/cheapRefSearch/ElementUsage.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/cheapRefSearch/ElementUsage.scala
@@ -1,13 +1,11 @@
 package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.cheapRefSearch
 
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{PsiClass, PsiElement, SmartPsiElementPointer}
-import org.jetbrains.annotations.NotNull
+import com.intellij.psi.{PsiElement, SmartPsiElementPointer}
 import org.jetbrains.plugins.scala.extensions.{ObjectExt, PsiElementExt}
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReference
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScMacroDefinition
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScTypeDefinition}
 
 import scala.ref.WeakReference
 
@@ -24,48 +22,22 @@ private final class ElementUsageWithKnownReference private(
   target: WeakReference[ScNamedElement]
 ) extends ElementUsage {
 
-  def referenceToTypeDefIsWithinTheSameTypeDef(@NotNull typeDef: PsiClass): Boolean = {
-    var refContainingClass = PsiTreeUtil.getParentOfType(reference.getElement, classOf[PsiClass])
-    var counter = 0
-
-    while (
-      counter < ElementUsageWithKnownReference.MaxSearchDepth &&
-        refContainingClass != null &&
-        refContainingClass != typeDef
-    ) {
-      refContainingClass = PsiTreeUtil.getParentOfType(refContainingClass, classOf[PsiClass])
-      counter += 1
-    }
-
-    refContainingClass == typeDef
+  def referenceIsInMemberThatHasTypeDefAsAncestor(typeDefs: ScTypeDefinition*): Boolean = {
+    val memberThatReferenceIsPartOf = reference.getElement.parentOfType[ScMember]
+    memberThatReferenceIsPartOf.exists(m => typeDefs.exists(typeDef => m == typeDef || typeDef.isAncestorOf(m)))
   }
 
   private def isReferenceToDefMacroImpl: Boolean =
     reference.getElement.asOptionOf[ScStableCodeReference].exists(ScMacroDefinition.isMacroImplReference)
 
-  override lazy val targetCanBePrivate: Boolean = !isReferenceToDefMacroImpl && {
-    val targetContainingClass = PsiTreeUtil.getParentOfType(target.underlying.get, classOf[PsiClass])
-
-    if (targetContainingClass == null) {
-      false
-    } else {
-
-      def referenceIsInCompanionScope: Boolean =
-        (PsiTreeUtil.getParentOfType(reference.getElement, classOf[ScTypeDefinition]), targetContainingClass) match {
-          case (null, _) => false
-          case (refTypeDef: ScTypeDefinition, targetTypeDef: ScTypeDefinition) =>
-            refTypeDef.baseCompanion.contains(targetTypeDef)
-          case _ => false
-        }
-
-      referenceToTypeDefIsWithinTheSameTypeDef(targetContainingClass) || referenceIsInCompanionScope
+  override def targetCanBePrivate: Boolean = !isReferenceToDefMacroImpl &&
+    target.underlying.get().parentOfType[ScTypeDefinition].exists { typeDef =>
+      val typeDefAndCompanion = typeDef +: typeDef.baseCompanion.toSeq
+      referenceIsInMemberThatHasTypeDefAsAncestor(typeDefAndCompanion: _*)
     }
-  }
 }
 
 private object ElementUsageWithKnownReference {
-  private val MaxSearchDepth = 10
-
   def apply(reference: PsiElement, target: ScNamedElement): ElementUsageWithKnownReference =
     new ElementUsageWithKnownReference(reference.createSmartPointer, WeakReference(target))
 
