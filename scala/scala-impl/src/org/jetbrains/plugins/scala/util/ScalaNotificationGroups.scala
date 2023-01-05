8b46330b7cc34217560d7714a199e47d08ff7efd
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2023-01-05 18:02:22 +0100

    notifications cleanup: remove unused `ScalaNotificationGroups.toolWindowGroup`

--- scala/scala-impl/src/org/jetbrains/plugins/scala/util/ScalaNotificationGroups.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/ScalaNotificationGroups.scala
@@ -5,15 +5,12 @@ import com.intellij.notification.{NotificationGroup, NotificationGroupManager}
 /**
  * Helper methods to replace deprecated [[com.intellij.notification.NotificationGroup]] static methods.
  *
- * Uses groups registered as EPs instead of dynamically created. IDs must correspond to ones declared in `scala-plugin-common.xml`
+ * Uses groups registered as EPs instead of dynamically created.<br>
+ * IDs must correspond to ones declared in `scala-plugin-common.xml`
  */
 object ScalaNotificationGroups {
   private val BALLOON_GROUP_ID        = "Scala Balloon Notifications"
-  private val TOOLWINDOW_GROUP_ID     = "Scala ToolWindow Notifications"
   private val STICKY_BALLOON_GROUP_ID = "Persistent Scala Notifications"
-
-  def toolWindowGroup: NotificationGroup =
-    NotificationGroupManager.getInstance().getNotificationGroup(TOOLWINDOW_GROUP_ID)
 
   def balloonGroup: NotificationGroup =
     NotificationGroupManager.getInstance().getNotificationGroup(BALLOON_GROUP_ID)
