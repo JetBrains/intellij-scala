6986da002cb78795e3aa822db7c106ff9248466f
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-01-09 08:43:20 +0100

    Files with macros

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/cheapRefSearch/TextSearch.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/cheapRefSearch/TextSearch.scala
@@ -1,14 +1,17 @@
 package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.cheapRefSearch
 
 import com.intellij.openapi.project.Project
-import com.intellij.psi.search.{PsiSearchHelper, TextOccurenceProcessor, UsageSearchContext}
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.search.{GlobalSearchScope, PsiSearchHelper, TextOccurenceProcessor, UsageSearchContext}
 import com.intellij.psi.{PsiElement, PsiReference}
 import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.cheapRefSearch.Search.Pipeline.ShouldProcess
 import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.cheapRefSearch.Search.{Method, SearchMethodResult}
+import org.jetbrains.plugins.scala.lang.psi.impl.base.ScInterpolatedStringLiteralImpl
 import org.jetbrains.plugins.scala.util.ScalaUsageNamesUtil
 
+import java.nio.file.Paths
 import java.util.concurrent.ConcurrentLinkedQueue
-import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.jdk.CollectionConverters.{CollectionHasAsScala, IterableHasAsJava}
 
 private[cheapRefSearch] final class TextSearch(
   override val shouldProcess: ShouldProcess,
@@ -25,17 +28,27 @@ private[cheapRefSearch] final class TextSearch(
 
     val psiElement = ctx.element
 
+    val filesWithMacros = FilesWithMacrosIndexer.filesWithMacros(psiElement.getProject).map { path =>
+      VirtualFileManager.getInstance().findFileByNioPath(Paths.get(path))
+    }
+
     val processor = new TextOccurenceProcessor {
 
       override def execute(e2: PsiElement, offsetInElement: Int): Boolean = {
 
-        if (psiElement.getContainingFile == e2.getContainingFile) {
+        val e2ContainingFile = e2.getContainingFile
+
+        if (psiElement.getContainingFile == e2ContainingFile) {
           true
         } else {
 
           val maybeUsage = e2 match {
-            case r: PsiReference => Some(ElementUsageWithKnownReference(r, psiElement))
-            case _ => None
+            case r: PsiReference =>
+              Some(ElementUsageWithKnownReference(r, psiElement))
+            case i: ScInterpolatedStringLiteralImpl if filesWithMacros.contains(e2ContainingFile.getVirtualFile) =>
+              Some(ElementUsageWithKnownReference(i, psiElement))
+            case _ =>
+              None
           }
 
           val continue = maybeUsage.forall { usage =>
@@ -50,9 +63,12 @@ private[cheapRefSearch] final class TextSearch(
     }
 
     val useScope = psiSearchHelper.getUseScope(psiElement)
+
+    val totalScope = useScope.union(GlobalSearchScope.filesScope(psiElement.getProject, filesWithMacros.asJavaCollection))
+
     val stringsToSearch = ScalaUsageNamesUtil.getStringsToSearch(psiElement).asScala.toSeq
     stringsToSearch.foreach { name =>
-      psiSearchHelper.processElementsWithWord(processor, useScope, name,
+      psiSearchHelper.processElementsWithWord(processor, totalScope, name,
         (UsageSearchContext.IN_CODE | UsageSearchContext.IN_FOREIGN_LANGUAGES).toShort, true)
     }
 
