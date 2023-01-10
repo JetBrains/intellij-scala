caeb5d002c75001aee8239e0ced6f33957f4e6fb
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-01-10 15:23:21 +0100

    CacheMacroUtil: unused methods deleted, #SCL-20714

--- scala/macros/src/org/jetbrains/plugins/scala/macroAnnotations/CachedMacroUtil.scala
+++ scala/macros/src/org/jetbrains/plugins/scala/macroAnnotations/CachedMacroUtil.scala
@@ -25,17 +25,6 @@ object CachedMacroUtil {
     tq"_root_.org.jetbrains.plugins.scala.caches.CleanupScheduler"
   }
 
-  @nowarn("msg=checked")
-  def defaultValue(c: whitebox.Context)(tp: c.universe.Tree): c.universe.Tree = {
-    import c.universe.Quasiquote
-    tp match {
-      case tq"Boolean" => q"false"
-      case tq"Int" => q"0"
-      case tq"Long" => q"0L"
-      case _ => q"null"
-    }
-  }
-
   private def internalTracer(implicit c: whitebox.Context): c.universe.Tree = {
     import c.universe.Quasiquote
     q"_root_.org.jetbrains.plugins.scala.caches.stats.Tracer"
@@ -83,21 +72,6 @@ object CachedMacroUtil {
     System.getProperty("ij.scala.compile.server") == "true" || c.settings.contains("enable-expression-tracers")
   }
 
-  def recursionGuardFQN(implicit c: whitebox.Context): c.universe.Tree = {
-    import c.universe.Quasiquote
-    q"org.jetbrains.plugins.scala.caches.RecursionManager.RecursionGuard"
-  }
-
-  def recursionManagerFQN(implicit c: whitebox.Context): c.universe.Tree = {
-    import c.universe.Quasiquote
-    q"org.jetbrains.plugins.scala.caches.RecursionManager"
-  }
-
-  def psiElementType(implicit c: whitebox.Context): c.universe.Tree = {
-    import c.universe.Quasiquote
-    tq"_root_.com.intellij.psi.PsiElement"
-  }
-
   def generateTermName(prefix: String = "", postfix: String = "")(implicit c: whitebox.Context): c.universe.TermName = {
     c.universe.TermName(prefix + "$" + postfix)
   }
@@ -135,133 +109,6 @@ object CachedMacroUtil {
       case _ => tp
     }
   }
-
-  def withUIFreezingGuard(c: whitebox.Context)(tree: c.universe.Tree, retTp: c.universe.Tree): c.universe.Tree = {
-    import c.universe.Quasiquote
-    val fqName = q"_root_.org.jetbrains.plugins.scala.util.UIFreezingGuard"
-    q"""val __guardedResult__ : $retTp =
-          if ($fqName.isAlreadyGuarded) { $tree }
-          else $fqName.withResponsibleUI { $tree }
-
-        __guardedResult__
-     """
-  }
-
-  abstract class UpdateHolderGenerator[C <: whitebox.Context](val c: C) {
-    //generated expression should evaluate to the existing value if it is present
-    def apply(resultName: c.universe.TermName): c.universe.Tree
-  }
-
-  def doPreventingRecursionCaching(c: whitebox.Context)(computation: c.universe.Tree,
-                                                        guard: c.universe.TermName,
-                                                        data: c.universe.TermName,
-                                                        updateHolderGenerator: UpdateHolderGenerator[c.type]): c.universe.Tree = {
-    import c.universe.Quasiquote
-    implicit val context: c.type = c
-
-    val needLocalFunction = hasReturnStatements(c)(computation)
-    if (needLocalFunction) {
-      abort(MacrosBundle.message("annotated.function.cannot.be.inlined.because.of.return"))(c)
-    }
-
-    val resultName = c.universe.TermName("result")
-    q"""{
-          val fromLocalCache = $guard.getFromLocalCache($data)
-          if (fromLocalCache != null) fromLocalCache
-          else {
-            val realKey = $guard.createKey($data)
-
-            val (sizeBefore, sizeAfter, minDepth, localCacheBefore) = $guard.beforeComputation(realKey)
-            val ($resultName, shouldCache) = try {
-              val stackStamp = $recursionManagerFQN.markStack()
-              val result = $computation
-              val shouldCache = stackStamp.mayCacheNow()
-              (result, shouldCache)
-            }
-            finally {
-              $guard.afterComputation(realKey, sizeBefore, sizeAfter, minDepth, localCacheBefore)
-            }
-
-            if (shouldCache) {
-              val race = ${updateHolderGenerator(resultName)}
-
-              if (race != null) race
-              else $resultName
-            } else {
-              // we should not cache, because the value is tainted by an intercepted recursion
-              // but we can cache it in the local cache to prevent calculating it again in
-              // same situations
-              // See CacheWithinRecursionTest.test_local_cache/test_local_cache_reset
-              $guard.cacheInLocalCache($data, $resultName)
-              $resultName
-            }
-          }
-        }
-     """
-  }
-
-
-  def hasReturnStatements(c: whitebox.Context)(tree: c.universe.Tree): Boolean = {
-    var result = false
-    val traverser = new c.universe.Traverser {
-      override def traverse(tree: c.universe.Tree): Unit = tree match {
-        case _: c.universe.Return @unchecked => result = true
-        //skip local functions and classes
-        case _: c.universe.DefDef @unchecked =>
-        case _: c.universe.ClassDef @unchecked =>
-        case _: c.universe.ModuleDef @unchecked =>
-        case _ => super.traverse(tree)
-      }
-    }
-    traverser.traverse(tree)
-    result
-  }
-
-  def handleProbablyRecursiveException(c: whitebox.Context)
-                                      (elemName: c.universe.TermName,
-                                       dataName: c.universe.TermName,
-                                       keyName: c.universe.TermName,
-                                       calculation: c.universe.Tree): c.universe.Tree = {
-    import c.universe.Quasiquote
-
-    q"""
-        try {
-          $calculation
-        }
-        catch {
-          case exc: org.jetbrains.plugins.scala.caches.CachesUtil.ProbablyRecursionException[_] if exc.key == $keyName =>
-            if (exc.elem == $elemName && exc.data == $dataName) {
-              try {
-                $calculation
-              } finally {
-                exc.set.foreach { fun =>
-                  fun.isProbablyRecursive = false
-                }
-              }
-            }
-            else {
-              val fun = com.intellij.psi.util.PsiTreeUtil.getContextOfType($elemName, true,
-                classOf[org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction])
-              if (fun == null || fun.isProbablyRecursive) throw exc
-              else {
-                fun.isProbablyRecursive = true
-                throw exc.copy(set = exc.set + fun)
-              }
-            }
-        }
-     """
-  }
-
-  def getOrCreateKey(c: whitebox.Context, hasParams: Boolean)
-                    (keyId: c.universe.Tree, dataType: c.universe.Tree, resultType: c.universe.Tree)
-                    (implicit c1: c.type): c.universe.Tree = {
-
-    import c.universe.Quasiquote
-
-    if (hasParams) q"$cachesUtilFQN.getOrCreateKey[$cachesUtilFQN.CachedMap[$dataType, $resultType]]($keyId)"
-    else q"$cachesUtilFQN.getOrCreateKey[$cachesUtilFQN.CachedRef[$resultType]]($keyId)"
-  }
-
 }
 
 object ModCount extends Enumeration {
