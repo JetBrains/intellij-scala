08da0891edadbb98a5a6c37c22762a2958de0a4e
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-02-03 17:11:44 +0100

    Only highlight files in the project view if CBH is enabled specifically for them, otherwise respect the user setting #SCL-20976

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaProblemFileHighlightFilter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaProblemFileHighlightFilter.scala
@@ -6,19 +6,23 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.util.Condition
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
 import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes
-import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.annotator.ScalaProblemFileHighlightFilter._
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.settings.{ScalaHighlightingMode, ScalaProjectSettings}
 
 class ScalaProblemFileHighlightFilter(project: Project) extends Condition[VirtualFile] {
   /**
     * @see [[com.intellij.codeInsight.problems.DefaultProblemFileHighlightFilter]]
     */
-  override def value(file: VirtualFile): Boolean =
-    (isEnabled || ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(project)) &&
-      isScalaSourceFile(project, file) &&
-      !CompilerManager.getInstance(project).isExcludedFromCompilation(file)
+  override def value(file: VirtualFile): Boolean = PsiManager.getInstance(project).findFile(file) match {
+    case sf: ScalaFile =>
+      (isEnabled || ScalaHighlightingMode.isShowErrorsFromCompilerEnabled(sf)) &&
+        isScalaSourceFile(project, file) &&
+        !CompilerManager.getInstance(project).isExcludedFromCompilation(file)
+    case _ => false
+  }
 
   private def isEnabled: Boolean =
     ScalaProjectSettings.getInstance(project).isProjectViewHighlighting
@@ -29,7 +33,7 @@ private object ScalaProblemFileHighlightFilter {
     * @see [[com.intellij.openapi.roots.FileIndexUtil]]
     */
   private def isScalaSourceFile(project: Project, file: VirtualFile): Boolean = {
-    if (!file.isDirectory && file.getFileType == ScalaFileType.INSTANCE && !FileTypeManager.getInstance.isFileIgnored(file)) {
+    if (!file.isDirectory && !FileTypeManager.getInstance.isFileIgnored(file)) {
       val fileIndex = ProjectRootManager.getInstance(project).getFileIndex
       fileIndex.isUnderSourceRootOfType(file, JavaModuleSourceRootTypes.SOURCES) || fileIndex.isInLibrarySource(file)
     } else {
