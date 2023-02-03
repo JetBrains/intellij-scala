49b9e8d08d02abe401b4277def7f56ddaee41e01
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-02-03 12:01:05 +0100

    @Measure -> measure, #SCL-20714

--- scala/scala-impl/test/org/jetbrains/plugins/scala/macroAnnotations/MeasureTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/macroAnnotations/MeasureTest.scala
@@ -1,13 +1,15 @@
 package org.jetbrains.plugins.scala.macroAnnotations
 
 import org.jetbrains.plugins.scala.base.ScalaFixtureTestCase
+import org.jetbrains.plugins.scala.caches.measure
 
 class MeasureTest extends ScalaFixtureTestCase {
 
   def testTracing(): Unit = {
     class Foo {
-      @Measure
-      def currentTime(): Long = System.currentTimeMillis()
+      def currentTime(): Long = measure("Foo.currentTime") {
+        System.currentTimeMillis()
+      }
     }
 
     checkTracer("Foo.currentTime", totalCount = 4, actualCount = 4) {
