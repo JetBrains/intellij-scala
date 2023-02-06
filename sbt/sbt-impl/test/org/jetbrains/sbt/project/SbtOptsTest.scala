d06347f5b0ffe652cf77bfb1c948bf480ff8de3a
azdrojowa123 <azdrojowa@gmail.com>
2023-02-06 11:21:38 +0100

    sbt: add more tests to SbtOpts #SCL-20936

--- sbt/sbt-impl/test/org/jetbrains/sbt/project/SbtOptsTest.scala
+++ sbt/sbt-impl/test/org/jetbrains/sbt/project/SbtOptsTest.scala
@@ -1,7 +1,10 @@
 package org.jetbrains.sbt.project
 
 import com.intellij.openapi.util.io.FileUtil
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.jetbrains.sbt.project.structure.SbtOpts
+import org.jetbrains.sbt.project.structure.SbtOpts.{JvmOptionGlobal, JvmOptionShellOnly, SbtLauncherOption, CommandOption}
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -11,18 +14,21 @@ class SbtOptsTest {
 
   private val input =
     """
-      |-sbt-boot /some/where/sbt/boot
+      |--sbt-boot /some/where/sbt/boot
       |-sbt-dir /some/where/else/sbt
-      |-ivy /some/where/ivy
+      |--ivy /some/where/ivy
       |-jvm-debug 4711
+      |-debug
+      |debug
       |
     """.stripMargin
 
   private val expected = Seq(
-    "-Dsbt.boot.directory=/some/where/sbt/boot",
-    "-Dsbt.global.base=/some/where/else/sbt",
-    "-Dsbt.ivy.home=/some/where/ivy",
-    "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=4711"
+    JvmOptionGlobal("-Dsbt.boot.directory=/some/where/sbt/boot"),
+    JvmOptionGlobal("-Dsbt.global.base=/some/where/else/sbt"),
+    JvmOptionGlobal("-Dsbt.ivy.home=/some/where/ivy"),
+    JvmOptionGlobal("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=4711"),
+    SbtLauncherOption("--debug")
   )
 
   @Test
@@ -32,5 +38,21 @@ class SbtOptsTest {
     FileUtil.writeToFile(optsFile, input)
     val opts = SbtOpts.loadFrom(optsDir)
     assertEquals(expected, opts)
+  }
+
+  private def doTest(providedArgs: Seq[String], expected: Seq[CommandOption]): Unit = {
+    val actual = SbtOpts.processArgs(providedArgs)
+    assertThat(actual, equalTo(expected))
+  }
+
+  @Test
+  def testProcessArgs(): Unit = {
+    doTest(Seq("-d", "-color=always"), Seq(SbtLauncherOption("--debug"), JvmOptionShellOnly("-Dsbt.color=always")))
+    doTest(Seq("-debug", "-color always"), Seq(SbtLauncherOption("--debug"), JvmOptionShellOnly("-Dsbt.color=always")))
+    doTest(Seq("--debug", "-color always", "-color=always"), Seq(SbtLauncherOption("--debug"), JvmOptionShellOnly("-Dsbt.color=always"), JvmOptionShellOnly("-Dsbt.color=always")))
+    doTest(Seq("--debug", "-debug-inc", "--color=always"), Seq(SbtLauncherOption("--debug"), JvmOptionGlobal("-Dxsbt.inc.debug=true"), JvmOptionShellOnly("-Dsbt.color=always")))
+    doTest(Seq("---debug", "-color"), Seq.empty)
+    doTest(Seq("debug", "-debug=true"), Seq.empty)
+    doTest(Seq("-debug-inc", "-color"), Seq(JvmOptionGlobal("-Dxsbt.inc.debug=true")))
   }
 }
