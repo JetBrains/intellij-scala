329e77fc8f52e0774d05e2e6950c35767c1642be
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2023-02-10 11:17:49 +0100

    sbt: don't highlight `.sbt` files which do not belong to a known `-build` module
    - for example in sbt projects located in test data

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaProblemHighlightFilter.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/ScalaProblemHighlightFilter.scala
@@ -4,9 +4,10 @@ import com.intellij.codeInsight.daemon.ProblemHighlightFilter
 import com.intellij.ide.scratch.ScratchUtil
 import com.intellij.openapi.roots.{JavaProjectRootsUtil, ProjectRootManager}
 import com.intellij.psi.PsiFile
-import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.console.ScalaConsoleInfo
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.project.ModuleExt
+import org.jetbrains.sbt.language.SbtFileType
 
 final class ScalaProblemHighlightFilter extends ProblemHighlightFilter {
 
@@ -22,15 +23,21 @@ final class ScalaProblemHighlightFilter extends ProblemHighlightFilter {
     shouldHighlight && !isLibraryAndNotSource(file)
   }
 
-
   private def isSpecialFile(file: ScalaFile): Boolean = {
-    file.getFileType != ScalaFileType.INSTANCE ||
+    isSbtFileBelongingToProject(file) ||
       // looks like outdated check, ammonite files should have ".sc" extension,
       // so file.getFileType != ScalaFileType.INSTANCE should be true in this case
       //AmmoniteUtil.isAmmoniteFile(file) ||
       ScratchUtil.isScratch(file.getVirtualFile) ||
       ScalaConsoleInfo.isConsole(file)
   }
+
+  //We need to highlight only those `*.sbt` files which are actual project definition.
+  //If it's some arbitrary `*.sbt` file, e.g. in testData folder, we shouldn't highlight it
+  //It can contain references which can't be resolved
+  private def isSbtFileBelongingToProject(file: ScalaFile): Boolean =
+    file.getFileType == SbtFileType &&
+      file.module.exists(_.isBuildModule)
 
   private def isInSourceRoots(file: ScalaFile): Boolean =
     !JavaProjectRootsUtil.isOutsideJavaSourceRoot(file)
