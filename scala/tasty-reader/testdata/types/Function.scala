11e7ced7d63ed7fb104bc61561df9114e0686eb4
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-02-13 14:34:35 +0100

    Decompiler: function types, #SCL-20672

--- scala/tasty-reader/testdata/types/Function.scala
+++ scala/tasty-reader/testdata/types/Function.scala
@@ -1,13 +1,21 @@
 package types
 
 trait Function {
+  type HKT[F[_, _]]
+
+  type T0 = HKT[Function1]
+
   type T1 = () => Unit
 
   type T2 = Int => Unit
 
   type T3 = (Int, Long) => Unit
 
-  trait T4 extends (Int => Unit)
+  type T4 = Int => Long => Unit
+
+  type T5 = (Int => Long) => Unit
+
+  trait T6 extends (Int => Unit)
 
   def repeated(xs: (Int => Unit)*): Unit
 }
