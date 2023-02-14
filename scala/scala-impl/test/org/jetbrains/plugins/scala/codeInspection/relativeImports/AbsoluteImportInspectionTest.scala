c3744a0816f5d124807f7ff00b5b2405ffcf45c2
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-02-14 17:14:18 +0100

    Fix AbsoluteImportInspection out-of-range highlight #SCL-18457 Fixed

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/relativeImports/AbsoluteImportInspectionTest.scala
@@ -0,0 +1,24 @@
+package org.jetbrains.plugins.scala.codeInspection.relativeImports
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.openapi.roots.ModuleRootModificationUtil
+import org.jetbrains.plugins.scala.base.SharedTestProjectToken
+import org.jetbrains.plugins.scala.codeInspection.{ScalaInspectionBundle, ScalaInspectionTestBase}
+
+class AbsoluteImportInspectionTest extends ScalaInspectionTestBase {
+
+  override protected def sharedProjectToken: SharedTestProjectToken = SharedTestProjectToken.DoNotShare
+
+  override protected val classOfInspection: Class[_ <: LocalInspectionTool] = classOf[AbsoluteImportInspection]
+
+  override protected def description: String = ScalaInspectionBundle.message("absolute.import.detected")
+
+  def test_package_prefix(): Unit = {
+    ModuleRootModificationUtil.updateModel(myFixture.getModule, model => {
+      model.getContentEntries.flatMap(_.getSourceFolders).foreach(_.setPackagePrefix("org.example"))
+    })
+
+    checkTextHasError(s"package org.example\nimport ${START}org.example.${END}_")
+  }
+}
+
