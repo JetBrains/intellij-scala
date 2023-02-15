17f0427a0ee21ce6f5a41ff49dc6b19658b9fe46
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-02-15 13:07:35 +0100

    Update IDEA to version 231.7515.3 EAP Candidate and fix compilation

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -13,7 +13,7 @@ object Versions {
   val sbtVersion: String = Sbt.latest
   val bloopVersion = "1.5.4"
   val zincVersion = "1.8.0"
-  val intellijVersion = "231.6890.12"
+  val intellijVersion = "231.7515.3"
 
   val Utils.DataForManagedIntellijDependencies(
     intellijVersion_ForManagedIntellijDependencies,
--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/ScalaCompileServerMessages.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/ScalaCompileServerMessages.scala
@@ -5,4 +5,4 @@ import org.jetbrains.jps.incremental.scala.ScalaCompileServerMessagesShared
 
 //duplicated in org.jetbrains.jps.incremental.scala.ScalaCompileServerJpsBundle (but with JpsDynamicBundle)
 object ScalaCompileServerMessages
-  extends ScalaCompileServerMessagesShared(new DynamicBundle(ScalaCompileServerMessagesShared.BUNDLE))
+  extends ScalaCompileServerMessagesShared(new DynamicBundle(classOf[ScalaCompileServerMessages.type], ScalaCompileServerMessagesShared.BUNDLE))
