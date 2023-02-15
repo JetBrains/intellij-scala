91cc1ee6fe573f9fc01016d82dc696b51d664e26
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2023-02-15 12:08:07 +0100

    drop CodeStyleSettingsInferService.scala #SCL-20999 fixed
    I added it in 2019 in my early days in the project.
    It was actual then in order users formatting in existing projects doesn't break.
    Nowadays it should be less actual, because almost all users have already migrated to some version >= 2020.1
    
    Dropping the class automatically fixes SCL-20999

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/settings/ScalaCodeStyleProjectListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/settings/ScalaCodeStyleProjectListener.scala
@@ -1,13 +1,11 @@
 package org.jetbrains.plugins.scala.lang.formatting.settings
 
 import com.intellij.openapi.project.{Project, ProjectManagerListener}
-import org.jetbrains.plugins.scala.lang.formatting.settings.inference.CodeStyleSettingsInferService
 import org.jetbrains.plugins.scala.lang.formatting.settings.migration.ProjectCodeStyleSettingsMigrationService
 
 private final class ScalaCodeStyleProjectListener extends ProjectManagerListener {
 
   override def projectOpened(project: Project): Unit = {
-    project.getService(classOf[CodeStyleSettingsInferService]).init()
     project.getService(classOf[ProjectCodeStyleSettingsMigrationService]).init()
   }
 }
