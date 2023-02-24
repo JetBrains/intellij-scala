c7891993f7777436ec21b2332afd3f6a99fb7172
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-02-24 18:39:29 +0100

    Manually resolve the maven home directory instead of linking against the IDEA maven plugin #SCL-12689

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/sdkdetect/repository/ScalaSdkDetector.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/sdkdetect/repository/ScalaSdkDetector.scala
@@ -1,7 +1,5 @@
 package org.jetbrains.plugins.scala.project.sdkdetect.repository
 
-import com.intellij.ide.plugins.PluginManagerCore
-import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.Nls
@@ -21,14 +19,9 @@ object ScalaSdkDetector {
       new ProjectLocalDetector(contextDirectory),
       SystemDetector,
       BrewDetector,
-      IvyDetector
-    ) ++
-      (if (isIdeaPluginEnabled("org.jetbrains.idea.maven")) Seq(MavenDetector) else Seq.empty) ++
-    Seq(
+      IvyDetector,
+      MavenDetector,
       SdkmanDetector,
       CoursierDetector
     )
-
-  private def isIdeaPluginEnabled(id: String): Boolean =
-    Option(PluginId.findId(id)).map(PluginManagerCore.getPlugin).exists(_.isEnabled)
 }
