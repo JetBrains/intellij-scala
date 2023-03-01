512356503254d977e93abc9475bdd59c982d9cee
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2023-03-01 14:31:27 +0100

    sbt: don't show sbt-shell toolwindow when creating new sbt project, only ensure that it's available #SCL-20978 fixed
    The issue is not reproduced reliably, though I am sure that the root cause lies in the modified code in ShowSbtShellAfterCreatingNewProject.
    Originally this code was copied from `ExternalToolWindowManager` which ensures that ES toolwindow is shown. In case of sbt projects it is "sbt" tool window.
    However "sbt-shell" is strictly speaking not a ES tool window because it doesn't implement `com.intellij.openapi.externalSystem.service.task.ui.AbstractExternalSystemToolWindowFactory`
    So we don't actually need to show the tool window.
    Showing it will lead to it's initialization which is not desired (see SCL-20978)
    We only need to ensure that "sbt-shell" tool window available

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/ShowSbtShellAfterCreatingNewProject.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/ShowSbtShellAfterCreatingNewProject.scala
@@ -2,27 +2,30 @@ package org.jetbrains.sbt.project
 
 import com.intellij.openapi.application.{AppUIExecutor, ApplicationManager, ModalityState}
 import com.intellij.openapi.externalSystem.ExternalSystemManager
-import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys
-import com.intellij.openapi.externalSystem.settings.{AbstractExternalSystemSettings, ExternalProjectSettings, ExternalSystemSettingsListenerEx}
+import com.intellij.openapi.externalSystem.settings.{ExternalProjectSettings, ExternalSystemSettingsListenerEx}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupManager
 import com.intellij.openapi.wm.{ToolWindow, ToolWindowManager}
-import com.intellij.util.ui.EdtInvocationManager
+import com.intellij.ui.AppUIUtil
 import org.jetbrains.sbt.settings.SbtSettings
 import org.jetbrains.sbt.shell.SbtShellToolWindowFactory
 
 import java.util
 
 /**
- * This is hacky workaround to show sbt-shell tool window when a new project is created.
+ * This is hacky workaround to show sbt-shell tool window when a new sbt project is created.<br>
  * It was copied from [[com.intellij.openapi.externalSystem.service.ui.ExternalToolWindowManager]]
  *
- * Note that when a new project is being created<br>
- * SbtShellToolWindowFactory.shouldBeAvailable and
- * AbstractExternalSystemToolWindowFactory.shouldBeAvailable return `false`
- * External tool window is explicitly activated in ExternalToolWindowManager.<br>
+ * Note that when a new project is being created these methods return `false` (UPD: or rather "sometimes can return", not 100% sure):
+ *  - [[org.jetbrains.sbt.shell.SbtShellToolWindowFactory.shouldBeAvailable]]
+ *  - [[com.intellij.openapi.externalSystem.service.task.ui.AbstractExternalSystemToolWindowFactory#shouldBeAvailable]]
  *
- * related: SCL-19909, IDEA-287117
+ * External tool window is explicitly activated in ExternalToolWindowManager.
+ * However "sbt-shell" tool window is not an "external tool window" because it doesn't extend
+ * [[com.intellij.openapi.externalSystem.service.task.ui.AbstractExternalSystemToolWindowFactory]]
+ * This is why we need this custom class.
+ *
+ * related: SCL-19909, IDEA-287117, SCL-20978
  */
 final class ShowSbtShellAfterCreatingNewProject extends ExternalSystemSettingsListenerEx {
 
@@ -36,21 +39,18 @@ final class ShowSbtShellAfterCreatingNewProject extends ExternalSystemSettingsLi
     if (ApplicationManager.getApplication.isUnitTestMode || project.isDefault)
       return
 
-    import scala.language.existentials
-
     val sbtManager = externalSystemManager match {
       case manager: SbtExternalSystemManager => manager
-      case _ => return
+      case _ =>
+        return
     }
 
-    val startupManager = StartupManager.getInstance(project)
-    val showToolWindow = startupManager.postStartupActivityPassed
-    startupManager.runAfterOpened(() => {
+    StartupManager.getInstance(project).runAfterOpened(() => {
       val settings: SbtSettings = sbtManager.getSettingsProvider.fun(project)
 
       val sbtShellToolWindow = getSbtShellToolWindowInstance(project)
       if (sbtShellToolWindow != null) {
-        activate(sbtShellToolWindow, settings, showToolWindow)
+        setAvailable(sbtShellToolWindow)
       }
       else {
         //in some rare cases, toolwindow can be non-initialized by this moment ¯\_(ツ)_/¯
@@ -59,7 +59,7 @@ final class ShowSbtShellAfterCreatingNewProject extends ExternalSystemSettingsLi
           ToolWindowManager.getInstance(settings.getProject).invokeLater(() => {
             val toolWindow1 = getSbtShellToolWindowInstance(project)
             if (toolWindow1 != null) {
-              activate(toolWindow1, settings, showToolWindow)
+              setAvailable(toolWindow1)
             }
           })
         })
@@ -67,20 +67,12 @@ final class ShowSbtShellAfterCreatingNewProject extends ExternalSystemSettingsLi
     })
   }
 
-  private def activate(toolWindow: ToolWindow, settings: AbstractExternalSystemSettings[_, _, _], showToolWindow: Boolean): Unit = {
-    val condition = toolWindow.isAvailable && !showToolWindow
-    if (condition)
-      return
-
-    EdtInvocationManager.invokeLaterIfNeeded(() => {
-      val shouldShow = showToolWindow && settings.getLinkedProjectsSettings.size == 1 &&
-        settings.getProject.getUserData(ExternalSystemDataKeys.NEWLY_CREATED_PROJECT) == null
-      toolWindow.setAvailable(true, () => {
-        if (shouldShow) {
-          toolWindow.show()
-        }
+  private def setAvailable(toolWindow: ToolWindow): Unit = {
+    if (!toolWindow.isAvailable) {
+      AppUIUtil.invokeLaterIfProjectAlive(toolWindow.getProject, () => {
+        toolWindow.setAvailable(true)
       })
-    })
+    }
   }
 
   private def getSbtShellToolWindowInstance(project: Project): ToolWindow =
@@ -99,7 +91,8 @@ final class ShowSbtShellAfterCreatingNewProject extends ExternalSystemSettingsLi
   ): Unit = {
     val sbtManager = externalSystemManager match {
       case manager: SbtExternalSystemManager => manager
-      case _ => return
+      case _ =>
+        return
     }
     val settings: SbtSettings = sbtManager.getSettingsProvider.fun(project)
 
