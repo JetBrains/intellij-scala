7ec199b7ec10474434056b9fa7341aab9e89655f
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-03-09 16:36:19 +0100

    Decompilers: exclude private primary constructor parameters, #SCL-21067 fixed

--- scala/decompiler/testdata/member/Modifiers.scala
+++ scala/decompiler/testdata/member/Modifiers.scala
@@ -3,6 +3,30 @@ package member
 trait Modifiers {
   class PrivatePrimaryConstructor private ()
 
+  class PrivatePrimaryConstructorParameter private (/**/x: Int/**/)
+
+  class PrivatePrimaryConstructorParameters private (/**/x: Int, y: Int/**/)
+
+  class PrivatePrimaryConstructorValParameter private (val x: Int)
+
+  class PrivatePrimaryConstructorPrivateValParameter private (/**/private val x: Int/**/)
+
+  class PrivatePrimaryConstructorImplicitParameter private (/**/implicit x: Int/**/)
+
+  class PrivatePrimaryConstructorImplicitParameters private (/**/implicit x: Int, y: Int/**/)
+
+  class PrivatePrimaryConstructorImplicitVal1 private (implicit val x: Int/**/, y: Int/**/)
+
+  class PrivatePrimaryConstructorImplicitVal2 private (/**/x: Int, /**/implicit val y: Int)
+
+  class PrivatePrimaryConstructorImplicitVal3 private (implicit /**/x: Int, /**/val y: Int)
+
+  class PrivatePrimaryConstructorImplicitPrivateVal1 private (/**/implicit private val x: Int, y: Int/**/)
+
+  class PrivatePrimaryConstructorImplicitPrivateVal2 private (/**/x: Int, implicit private val y: Int/**/)
+
+  class PrivatePrimaryConstructorImplicitPrivateVal3 private (/**/implicit x: Int, private val y: Int/**/)
+
   class ProtectedPrimaryConstructor protected ()
 
   class PrivatePrimaryConstructorTypeParameter[A] private ()
