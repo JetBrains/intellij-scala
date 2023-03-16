e9d38ec06be6386db1d23e158e3740970e7e8f42
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-03-16 10:28:59 +0100

    TastyReader: inline given, #SCL-21079 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/compiled/ScClassFileDecompiler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/compiled/ScClassFileDecompiler.scala
@@ -39,7 +39,7 @@ object ScClassFileDecompiler {
 
   object ScClsStubBuilder extends compiled.ClsStubBuilder {
 
-    override val getStubVersion = 402
+    override val getStubVersion = 403
 
     override def buildFileStub(content: FileContent): stubs.PsiFileStubImpl[_ <: PsiFile] =
       decompiledScalaFile(content)
