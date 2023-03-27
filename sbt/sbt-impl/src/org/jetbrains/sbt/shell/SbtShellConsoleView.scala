970a1daa08ce0ce64f2f5485b02383dbd1f7b972
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2023-03-27 13:03:36 +0200

    shell: create own clear console action #SCL-19370

--- sbt/sbt-impl/src/org/jetbrains/sbt/shell/SbtShellConsoleView.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/shell/SbtShellConsoleView.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.sbt
 package shell
 
-import com.intellij.execution.actions.ClearConsoleAction
 import com.intellij.execution.configurations.RemoteConnection
 import com.intellij.execution.console.LanguageConsoleImpl
 import com.intellij.execution.filters.UrlFilter.UrlFilterProvider
@@ -30,8 +29,6 @@ final class SbtShellConsoleView private(project: Project, debugConnection: Optio
     val defaultActions = super.createConsoleActions()
     val toggleSoftWrapsAction = defaultActions.find(_.isInstanceOf[ToggleUseSoftWrapsToolbarAction])
       .getOrElse(throw new RuntimeException("action of type `ToggleUseSoftWrapsToolbarAction` couldn't be found"))
-    val clearAllAction = defaultActions.find(_.isInstanceOf[ClearConsoleAction])
-      .getOrElse(throw new RuntimeException("action of type `ClearConsoleAction` couldn't be found"))
 
     val startAction = new StartAction(project)
     val stopAction = new StopAction(project)
@@ -41,6 +38,7 @@ final class SbtShellConsoleView private(project: Project, debugConnection: Optio
     val copyFromHistoryViewerAction = new CopyFromHistoryViewerAction(this)
     val findAction = new FindAction(this)
     val escapeAction = new EscapeAction(this)
+    val clearAllAction = new ClearThisConsoleAction(this)
 
     val allActions: Array[AnAction] = Array(
       startAction,
