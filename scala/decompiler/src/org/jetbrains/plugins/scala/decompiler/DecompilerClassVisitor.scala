c03b6e7cd970815bccf44e6b534682c58b06d800
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-04-04 18:29:05 +0200

    Refactor the `signature` method to only return the raw bytes of the Scala signature and decode them in the decompiler

--- scala/decompiler/src/org/jetbrains/plugins/scala/decompiler/DecompilerClassVisitor.scala
+++ scala/decompiler/src/org/jetbrains/plugins/scala/decompiler/DecompilerClassVisitor.scala
@@ -1,14 +1,12 @@
 package org.jetbrains.plugins.scala.decompiler
 
 import org.jetbrains.org.objectweb.asm.{AnnotationVisitor, ClassVisitor, Opcodes}
-import org.jetbrains.plugins.scala.decompiler.scalasig.{Parser, ScalaSig}
 
 import java.nio.charset.StandardCharsets
 import scala.collection.mutable
-import scala.reflect.internal.pickling.ByteCodecs
 
-private final class DecompilerClassVisitor(fileName: String) extends ClassVisitor(Opcodes.ASM9) {
-  import Decompiler.{BYTES_VALUE, SCALA_SIG_ANNOTATION, SCALA_LONG_SIG_ANNOTATION}
+private final class DecompilerClassVisitor extends ClassVisitor(Opcodes.ASM9) {
+  import Decompiler.{BYTES_VALUE, SCALA_LONG_SIG_ANNOTATION, SCALA_SIG_ANNOTATION}
 
   /*
    * Contains the values of ScalaSignature and ScalaLongSignature class annotations. In the case of ScalaSignature,
@@ -18,21 +16,16 @@ private final class DecompilerClassVisitor(fileName: String) extends ClassVisito
 
   private[this] var _source: Option[String] = None
 
-  def signature: Option[ScalaSig] = {
+  def signature: Option[Array[Byte]] = {
     val chunks = rawSignature.result().map(_.getBytes(StandardCharsets.UTF_8))
 
-    val bytes = chunks match {
+    chunks match {
       case Nil => None
       case List(signature) =>
         // Do not create a copy of the bytes, if there is only one byte array.
         Some(signature)
       case signatures =>
         Some(Array.concat(signatures: _*))
-    }
-
-    bytes.map { bs =>
-      ByteCodecs.decode(bs)
-      Parser.parseScalaSig(bs, fileName)
     }
   }
 
