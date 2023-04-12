c57d4b5ea4ab52e1fd75e40f98e9cd18f6f65863
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2023-04-12 18:15:06 +0200

    create new test: support munit in adding test framework to project

--- scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/test/munit/MUnitTestFramework.scala
+++ scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/test/munit/MUnitTestFramework.scala
@@ -1,9 +1,10 @@
 package org.jetbrains.plugins.scala.testingSupport.test.munit
 
 import com.intellij.testIntegration.TestFramework
-import org.jetbrains.plugins.scala.testingSupport.test.AbstractTestFramework
+import org.jetbrains.plugins.scala.testingSupport.test.AbstractTestFramework.TestFrameworkSetupInfo
+import org.jetbrains.plugins.scala.testingSupport.test.{AbstractTestFramework, TestFrameworkSetupSupportBase}
 
-final class MUnitTestFramework extends AbstractTestFramework {
+final class MUnitTestFramework extends AbstractTestFramework with TestFrameworkSetupSupportBase {
 
   override def baseSuitePaths: Seq[String] = Seq("munit.Suite")
 
@@ -12,6 +13,9 @@ final class MUnitTestFramework extends AbstractTestFramework {
   override def getName: String = "MUnit"
 
   override def getDefaultSuperClass: String = MUnitUtils.FunSuiteFqn
+
+  override def frameworkSetupInfo(scalaVersion: Option[String]): TestFrameworkSetupInfo =
+    TestFrameworkSetupInfo(Seq(""""org.scalameta" %% "munit" % "latest.integration" % Test"""), Seq())
 }
 
 object MUnitTestFramework {
