d4879007de7a3429065272970c1af03a4db9abb2
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-04-24 17:50:27 +0200

    Refactor ScalaTestFailureLocationFilter to use the ExceptionInfoCache from the platform
    - Removes our duplicated implementation of ExceptionInfoCache (stale).
    - Avoids the usage of deprecated APIs.
    - Resolves an old TODO.

--- scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/util/scalatest/ScalaTestFailureLocationFilter.java
+++ scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/util/scalatest/ScalaTestFailureLocationFilter.java
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.testingSupport.util.scalatest;
 
+import com.intellij.execution.filters.ExceptionInfoCache;
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.filters.HyperlinkInfo;
 import com.intellij.execution.filters.HyperlinkInfoFactory;
@@ -7,26 +8,12 @@ import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.search.PsiShortNamesCache;
-import com.intellij.reference.SoftReference;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 
 /**
@@ -40,8 +27,8 @@ public class ScalaTestFailureLocationFilter implements Filter {
 //  private final static Pattern fqnPattern = Pattern.compile("(\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*\\.)+\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*");
   private final static String testFailureLocationPrefix = "ScalaTestFailureLocation: ";
 
-  public ScalaTestFailureLocationFilter(@NotNull final GlobalSearchScope scope) {
-    myCache = new ExceptionInfoCache(scope);
+  public ScalaTestFailureLocationFilter(@NotNull final Project project, @NotNull final GlobalSearchScope scope) {
+    myCache = new ExceptionInfoCache(project, scope);
   }
 
   @Nullable
@@ -62,46 +49,22 @@ public class ScalaTestFailureLocationFilter implements Filter {
     if (colonIndex < 0) return null;
     int lineNumber = ScalaTestFailureLocationFilter.getLineNumber(fileAndLine.substring(colonIndex + 1));
     if (lineNumber < 0) return null;
-    Pair<PsiClass[], PsiFile[]> classesAndFiles = myCache.resolveClass(className);
-    PsiClass[] classes = classesAndFiles.first;
-    PsiFile[] myFiles = classesAndFiles.second;
-    if (myFiles.length == 0) {
-      // try find the file with the required name
-      //todo[nik] it would be better to use FilenameIndex here to honor the scope by it isn't accessible in Open API
-      myFiles = PsiShortNamesCache.getInstance(myProject).getFilesByName(fileAndLine.substring(0, colonIndex).trim());
-    }
-    if (myFiles.length == 0) return null;
+    final var fileName = fileAndLine.substring(0, colonIndex).trim();
+    final var resolutionResult = myCache.resolveClassOrFile(className, fileName);
+    final var virtualFiles = resolutionResult.getClasses().keySet().stream().toList();
+    if (virtualFiles.isEmpty()) return null;
 
     final int textStartOffset = entireLength - line.length();
 
     final int highlightStartOffset = textStartOffset + lparenthIndex + 1;
     final int highlightEndOffset = textStartOffset + rparenthIndex;
 
-    ProjectFileIndex index = ProjectRootManager.getInstance(myProject).getFileIndex();
-    List<VirtualFile> virtualFilesInLibraries = new ArrayList<VirtualFile>();
-    List<VirtualFile> virtualFilesInContent = new ArrayList<VirtualFile>();
-    for (PsiFile file : myFiles) {
-      VirtualFile virtualFile = file.getVirtualFile();
-      if (index.isInContent(virtualFile)) {
-        virtualFilesInContent.add(virtualFile);
-      }
-      else {
-        virtualFilesInLibraries.add(virtualFile);
-      }
-    }
-
-    List<VirtualFile> virtualFiles;
     TextAttributes attributes = EditorColorsManager.getInstance().getGlobalScheme().getAttributes(CodeInsightColors.HYPERLINK_ATTRIBUTES);
-    if (virtualFilesInContent.isEmpty()) {
+    if (resolutionResult.isInLibrary()) {
       Color libTextColor = UIUtil.getInactiveTextColor();
       attributes = attributes.clone();
       attributes.setForegroundColor(libTextColor);
       attributes.setEffectColor(libTextColor);
-
-      virtualFiles = virtualFilesInLibraries;
-    }
-    else {
-      virtualFiles = virtualFilesInContent;
     }
     HyperlinkInfo linkInfo = HyperlinkInfoFactory.getInstance().createMultipleFilesHyperlinkInfo(virtualFiles, lineNumber - 1, myProject);
     return new Filter.Result(highlightStartOffset, highlightEndOffset, linkInfo, attributes);
@@ -117,58 +80,6 @@ public class ScalaTestFailureLocationFilter implements Filter {
       return Integer.parseInt(lineString);
     } catch (NumberFormatException e) {
       return -1;
-    }
-  }
-
-  //TODO: remove this hack
-  /**
-   * Copy of {@link com.intellij.execution.filters.ExceptionInfoCache}. Will remove it once the resolve method is made
-   * public.
-   */
-  private static class ExceptionInfoCache {
-    private final ConcurrentMap<String, SoftReference<Pair<PsiClass[], PsiFile[]>>> myCache = new ConcurrentHashMap<>();
-    private final Project myProject;
-    private final GlobalSearchScope mySearchScope;
-
-    public ExceptionInfoCache(GlobalSearchScope searchScope) {
-      myProject = Objects.requireNonNull(searchScope.getProject());
-      mySearchScope = searchScope;
-    }
-
-    @NotNull
-    public Project getProject() {
-      return myProject;
-    }
-
-    @NotNull
-    private PsiClass[] findClassesPreferringMyScope(String className) {
-      JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(myProject);
-      PsiClass[] result = psiFacade.findClasses(className, mySearchScope);
-      return result.length != 0 ? result : psiFacade.findClasses(className, GlobalSearchScope.allScope(myProject));
-    }
-
-    Pair<PsiClass[], PsiFile[]> resolveClass(String className) {
-      Pair<PsiClass[], PsiFile[]> cached = SoftReference.dereference(myCache.get(className));
-      if (cached != null) {
-        return cached;
-      }
-
-      PsiClass[] classes = findClassesPreferringMyScope(className);
-      if (classes.length == 0) {
-        final int dollarIndex = className.indexOf('$');
-        if (dollarIndex >= 0) {
-          classes = findClassesPreferringMyScope(className.substring(0, dollarIndex));
-        }
-      }
-
-      PsiFile[] files = new PsiFile[classes.length];
-      for (int i = 0; i < classes.length; i++) {
-        files[i] = (PsiFile) classes[i].getContainingFile().getNavigationElement();
-      }
-
-      Pair<PsiClass[], PsiFile[]> result = Pair.create(classes, files);
-      myCache.put(className, new SoftReference<Pair<PsiClass[], PsiFile[]>>(result));
-      return result;
     }
   }
 }
--- scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/util/scalatest/ScalaTestFailureLocationFilterFactory.java
+++ scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/util/scalatest/ScalaTestFailureLocationFilterFactory.java
@@ -2,13 +2,22 @@ package org.jetbrains.plugins.scala.testingSupport.util.scalatest;
 
 import com.intellij.execution.filters.ExceptionFilterFactory;
 import com.intellij.execution.filters.Filter;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class ScalaTestFailureLocationFilterFactory implements ExceptionFilterFactory {
   @NotNull
   @Override
   public Filter create(@NotNull GlobalSearchScope searchScope) {
-    return new ScalaTestFailureLocationFilter(searchScope);
+    return create(Objects.requireNonNull(searchScope.getProject()), searchScope);
+  }
+
+  @NotNull
+  @Override
+  public Filter create(@NotNull Project project, @NotNull GlobalSearchScope searchScope) {
+    return new ScalaTestFailureLocationFilter(project, searchScope);
   }
 }
