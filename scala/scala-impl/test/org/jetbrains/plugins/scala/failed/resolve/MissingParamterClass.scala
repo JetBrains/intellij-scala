af001d8bca6e5e1cdf8e72e4427d2fb4008f3c67
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-04-25 08:06:43 -0500

    Fix API Watcher issues #SCL-20587 fixed
    For info regarding this commit see
    https://jetbrains.team/p/scl/reviews/523

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/MissingParamterClass.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/MissingParamterClass.scala
@@ -2,6 +2,4 @@ package org.jetbrains.plugins.scala.failed.resolve
 
 class MissingParamterClass extends FailableResolveTest("missingParameter") {
   def testSCL8967(): Unit = doTest()
-
-  def testSCL9719(): Unit = doTest()
 }
