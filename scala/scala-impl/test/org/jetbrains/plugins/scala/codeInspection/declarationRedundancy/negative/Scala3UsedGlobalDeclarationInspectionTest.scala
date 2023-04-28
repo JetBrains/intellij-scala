e9480f49f90237193a555ca6a8e150945a797e66
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-04-28 08:16:49 -0500

    Disable ast.loading.filter in some of the tests

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/negative/Scala3UsedGlobalDeclarationInspectionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/negative/Scala3UsedGlobalDeclarationInspectionTest.scala
@@ -1,11 +1,29 @@
 package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.negative
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
 import org.jetbrains.plugins.scala.ScalaVersion
 import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.ScalaUnusedDeclarationInspectionTestBase
 
 class Scala3UsedGlobalDeclarationInspectionTest extends ScalaUnusedDeclarationInspectionTestBase {
 
   override protected def supportedIn(version: ScalaVersion): Boolean = version >= ScalaVersion.Latest.Scala_3_0
+
+  override protected def afterSetUpProject(project: Project, module: com.intellij.openapi.module.Module): Unit = {
+
+    super.afterSetUpProject(project, module)
+
+    // TODO Turn back on (by removing the Scala3GlobalDeclarationUsedFromJavaInspectionTest.afterSetUpProject
+    //  implementation you are currently reading) when platform has implemented support for inspections which
+    //  run references search.
+    //  See https://jetbrains.slack.com/archives/CMDBCUBGE/p1682686264596449?thread_ts=1682534407.272869&cid=CMDBCUBGE
+    //  ----
+    //  I've only disabled the ast.loading.filter here and in a few other places, because as far as I know the filter
+    //  is operating as desired and not causing issues anywhere else. The implementation where we enable it resides in
+    //  ScalaLightCodeInsightFixtureTestCase, which is used for many tests.
+
+    Registry.get("ast.loading.filter").setValue(false, getTestRootDisposable)
+  }
 
   private def addFile(text: String): Unit = myFixture.addFileToProject("Foo.scala", text)
 
