846bbeea59bb9a9fedc3732a1af222acef6eed7b
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-05-02 14:28:54 +0200

    Initialize the Scalafmt suggester service on a background thread #SCL-21147
    - When a project is opened, this service checks the existence of a .scalafmt.conf file. If such a file exists, a notification is shown to the user to enable project formatting using scalafmt.
    - Previously, this initialization was done on the UI thread through the deprecated ProjectManagerListener#projectOpened API.
    - The replacement for this is StartupActivity.DumbAware, which automatically runs on a background thread. This avoids the slow operations on EDT exception.
    - Unfortunately, this API is already marked as obsolete, in favor of a Kotlin only, coroutines based API. We cannot use that from Scala easily, so we stick with the StartupActivity API for now. It will not be removed.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/scalafmt/ScalafmtProjectListener.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/formatting/scalafmt/ScalafmtProjectListener.scala
@@ -1,10 +1,12 @@
 package org.jetbrains.plugins.scala.lang.formatting.scalafmt
 
 import com.intellij.openapi.project.{Project, ProjectManagerListener}
+import com.intellij.openapi.startup.StartupActivity
 
-class ScalafmtProjectListener extends ProjectManagerListener {
+//noinspection UsagesOfObsoleteApi
+class ScalafmtProjectListener extends StartupActivity.DumbAware with ProjectManagerListener {
 
-  override def projectOpened(project: Project): Unit = {
+  override def runActivity(project: Project): Unit = {
     ScalaFmtSuggesterService.instance(project).init()
     ScalafmtDynamicConfigService.instanceIn(project).init()
   }
