0b130eef85aead71b67d72bdfb8e940e08e226b2
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-05-08 15:34:07 +0200

    Tasty reader: refactoring

--- scala/tasty-reader/src/TreePrinter.scala
+++ scala/tasty-reader/src/TreePrinter.scala
@@ -900,7 +900,7 @@ class TreePrinter(privateMembers: Boolean = false, infixTypes: Boolean = false, 
   }
 
   private def id(s: String): String =
-    if (!isIdentifier(s) || Keywords(s) || s == "=" || s == "=>" || s == "?=>" | s == "=>>") "`" + s + "`" else s
+    if (Keywords(s) || !isIdentifier(s)) "`" + s + "`" else s
 
   private def isIdentifier(s: String): Boolean = !(s.isEmpty || s.contains("//") || s.contains("/*")) && {
     if (s(0) == '_' || s(0) == '$' || Character.isUnicodeIdentifierStart(s(0))) {
@@ -925,6 +925,10 @@ class TreePrinter(privateMembers: Boolean = false, infixTypes: Boolean = false, 
 
 private object TreePrinter {
   private val Keywords = Set(
+    "=",
+    "=>",
+    "=>>",
+    "?=>",
     "abstract",
     "case",
     "catch",
