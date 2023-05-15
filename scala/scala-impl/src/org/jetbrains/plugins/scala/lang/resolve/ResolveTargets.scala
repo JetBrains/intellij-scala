252547992a64423597b8aa422c99b0dcd29265ce
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2023-05-15 17:55:49 +0200

    ResolveTargets.scala - remove legacy comment from 2010

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/resolve/ResolveTargets.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/resolve/ResolveTargets.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.plugins.scala.lang.resolve
 
 object ResolveTargets extends Enumeration {
-  //No case class marker to reject case class in static import when it has companion object
   val METHOD, VAR, VAL, OBJECT, CLASS, PACKAGE, ANNOTATION = Value
 
   /**
