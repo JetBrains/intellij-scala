841f2ea8ef580fb41d148f12621ed9227cb76bee
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-05-17 16:20:34 +0200

    Migrate all direct implementations of com.intellij.openapi.startup.ProjectActivity to our Scala shim

--- bsp/src/org/jetbrains/bsp/BspStartupActivity.scala
+++ bsp/src/org/jetbrains/bsp/BspStartupActivity.scala
@@ -1,19 +1,13 @@
 package org.jetbrains.bsp
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.util.JavaCoroutines
-import kotlin.coroutines.Continuation
-import org.jetbrains.plugins.scala.extensions.executeOnPooledThread
+import org.jetbrains.plugins.scala.startup.ProjectActivity
 
 final class BspStartupActivity extends ProjectActivity {
-  override def execute(project: Project, continuation: Continuation[_ >: kotlin.Unit]): AnyRef = {
-    JavaCoroutines.suspendJava[kotlin.Unit](jc => {
-      // initialize build loop only for bsp projects
-      if (BspUtil.isBspProject(project)) {
-        BspBuildLoopService.getInstance(project)
-      }
-      jc.resume(kotlin.Unit.INSTANCE)
-    }, continuation)
+  override def execute(project: Project): Unit = {
+    // initialize build loop only for bsp projects
+    if (BspUtil.isBspProject(project)) {
+      BspBuildLoopService.getInstance(project)
+    }
   }
 }
