b76ea28a24d7a58969fea9ac5f8047c024c85391
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-06-07 11:21:06 +0200

    Revert "Remove the workaround for #SCL-18866"
    This reverts commit 7f014b684ec62978061931764ee620efafd04ddd.

--- project/Common.scala
+++ project/Common.scala
@@ -46,6 +46,7 @@ object Common {
       name := projectName,
       organization := "JetBrains",
       scalaVersion := Versions.scalaVersion,
+      dependencyOverrides += "org.scala-lang" % "scala3-library_3" % "3.2.2-RC2", // TODO Workaround for SCL-18866
       (Compile / javacOptions) := globalJavacOptions,
       (Compile / scalacOptions) := globalScalacOptions,
       (Compile / unmanagedSourceDirectories) += baseDirectory.value / "src",
