5f1c8f609d18af89cd15bc9fbb149500a983ae40
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-06-14 17:51:17 +0200

    Refactor IntelliJ projects with Kotlin and Scala incremental compiler settings tests to avoid repetition #SCL-21244

--- scala/scala-impl/test/org/jetbrains/plugins/scala/project/ConfigureIncrementalCompilerTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/project/ConfigureIncrementalCompilerTest.scala
@@ -3,58 +3,51 @@ package org.jetbrains.plugins.scala.project
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
 import org.jetbrains.plugins.scala.DependencyManagerBase.RichStr
 import org.jetbrains.plugins.scala.LatestScalaVersions
-import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, ScalaSDKLoader}
+import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, LibraryLoader, ScalaSDKLoader}
 import org.jetbrains.plugins.scala.compiler.data.IncrementalityType
 import org.jetbrains.plugins.scala.project.settings.ScalaCompilerConfiguration
-import org.junit.Assert.assertEquals
+import org.junit.Assert.{assertEquals, fail}
 
 class ConfigureIncrementalCompilerTest extends JavaCodeInsightFixtureTestCase {
 
-  def testMixedScalaKotlinProjectIncrementalityType(): Unit = {
-    val loaders = Array(ScalaSDKLoader(), IvyManagedLoader("org.jetbrains.kotlin" % "kotlin-stdlib" % "1.8.21"))
+  private def kotlinProjectIncrementalityTypeTest(loaders: Seq[LibraryLoader], expectedIncrementalities: Seq[IncrementalityType]): Unit = {
+    if (loaders.sizeCompare(expectedIncrementalities) != 0) {
+      fail("The number of provided library loaders should exactly match the number of expected incrementality types. " +
+        "Each incrementality type is checked after a library loader is initialized.")
+    }
 
     try {
       val scalaVersion = LatestScalaVersions.Scala_2_13
 
       // Check default value of incrementality type.
       val project = getProject
-      var incrementalityType = ScalaCompilerConfiguration.instanceIn(project).incrementalityType
-      assertEquals(IncrementalityType.SBT, incrementalityType)
 
-      loaders(0).init(getModule, scalaVersion)
+      def projectIncrementalityType: IncrementalityType = ScalaCompilerConfiguration.instanceIn(project).incrementalityType
 
-      // Check incrementality type after setting up the Scala SDK.
-      incrementalityType = ScalaCompilerConfiguration.instanceIn(project).incrementalityType
-      assertEquals(IncrementalityType.SBT, incrementalityType)
+      assertEquals(IncrementalityType.SBT, projectIncrementalityType)
 
-      loaders(1).init(getModule, scalaVersion)
+      loaders.zip(expectedIncrementalities).foreach { case (loader, expected) =>
+        loader.init(getModule, scalaVersion)
 
-      // Check incrementality type after setting up the Kotlin standard library.
-      incrementalityType = ScalaCompilerConfiguration.instanceIn(project).incrementalityType
-      assertEquals(IncrementalityType.IDEA, incrementalityType)
+        // Check incrementality type after setting up a library loader.
+        assertEquals(expected, projectIncrementalityType)
+      }
     } finally {
       loaders.foreach(_.clean(getModule))
     }
   }
 
+  def testMixedScalaKotlinProjectIncrementalityType(): Unit = {
+    kotlinProjectIncrementalityTypeTest(
+      Seq(ScalaSDKLoader(), IvyManagedLoader("org.jetbrains.kotlin" % "kotlin-stdlib" % "1.8.21")),
+      Seq(IncrementalityType.SBT, IncrementalityType.IDEA)
+    )
+  }
+
   def testOnlyKotlinNoScalaProjectIncrementalityType(): Unit = {
-    val loaders = Array(ScalaSDKLoader(), IvyManagedLoader("org.jetbrains.kotlin" % "kotlin-stdlib" % "1.8.21"))
-
-    try {
-      val scalaVersion = LatestScalaVersions.Scala_2_13
-
-      // Check default value of incrementality type.
-      val project = getProject
-      var incrementalityType = ScalaCompilerConfiguration.instanceIn(project).incrementalityType
-      assertEquals(IncrementalityType.SBT, incrementalityType)
-
-      loaders(0).init(getModule, scalaVersion)
-
-      // Check incrementality type after setting up the Kotlin standard library.
-      incrementalityType = ScalaCompilerConfiguration.instanceIn(project).incrementalityType
-      assertEquals(IncrementalityType.SBT, incrementalityType)
-    } finally {
-      loaders.foreach(_.clean(getModule))
-    }
+    kotlinProjectIncrementalityTypeTest(
+      Seq(IvyManagedLoader("org.jetbrains.kotlin" % "kotlin-stdlib" % "1.8.21")),
+      Seq(IncrementalityType.SBT)
+    )
   }
 }
