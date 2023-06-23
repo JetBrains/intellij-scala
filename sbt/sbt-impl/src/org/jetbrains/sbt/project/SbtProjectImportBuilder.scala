b75ad404ae38e69065750756af21c187b68ddc98
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2023-06-23 12:57:03 +0200

    sbt: apply project jdk to context wizard #SCL-16894 fixed

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtProjectImportBuilder.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtProjectImportBuilder.scala
@@ -6,11 +6,15 @@ import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
 import com.intellij.openapi.externalSystem.service.project.wizard.AbstractExternalProjectImportBuilder
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.scala.project.external.SdkUtils
 import org.jetbrains.sbt.project.SbtImportControl.SbtImportControlFactory
+import org.jetbrains.sbt.project.data.SbtProjectData
 
 import java.io.File
 import javax.swing.Icon
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 class SbtProjectImportBuilder
   extends AbstractExternalProjectImportBuilder[SbtImportControl](ProjectDataManager.getInstance(), SbtImportControlFactory, SbtProjectSystem.Id) {
@@ -25,5 +29,14 @@ class SbtProjectImportBuilder
 
   override def getExternalProjectConfigToUse(file: File): File = file
 
-  override def applyExtraSettings(context: WizardContext): Unit = {}
+  override def applyExtraSettings(context: WizardContext): Unit = {
+    getList.asScala.foreach { projectData =>
+      ExternalSystemApiUtil.findAll(projectData, SbtProjectData.Key).asScala.headOption match {
+        case Some(sbtProjectData) =>
+          val sdkReference = sbtProjectData.getData.jdk
+          SdkUtils.findProjectSdk(sdkReference).foreach(context.setProjectJdk)
+        case _ =>
+      }
+    }
+  }
 }
