c58d9e1443146403d752a6508f342139c855d006
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-06-26 16:30:46 +0200

    givens: deprecate ScGiven.generateAnonymousGivenName, use ScalaPsiUtil.generateGivenOrExtensionName instead

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScGivenPatternImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/ScGivenPatternImpl.scala
@@ -2,9 +2,9 @@ package org.jetbrains.plugins.scala.lang.psi.impl.base.patterns
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScGivenPattern
 import org.jetbrains.plugins.scala.lang.psi.api.base.types.ScTypeElement
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScGiven
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementImpl
 import org.jetbrains.plugins.scala.lang.psi.types.result.TypeResult
 
@@ -18,5 +18,5 @@ final class ScGivenPatternImpl(node: ASTNode)
   override def `type`(): TypeResult       = typeElement.`type`()
   override def isWildcard: Boolean        = false
   override def nameId: PsiElement         = typeElement
-  override def name: String               = ScGiven.generateAnonymousGivenName(typeElement)
+  override def name: String               = ScalaPsiUtil.generateGivenOrExtensionName(typeElement)
 }
