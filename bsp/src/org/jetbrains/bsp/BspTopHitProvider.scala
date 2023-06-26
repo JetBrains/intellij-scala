ed564c6c7b52c2ded34df576a893a75e44be3003
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2023-06-26 12:28:34 +0200

    sbt: make reload/load sbt/bsp actions searchable #SCL-21323 fixed
    - ExternalSystem.RefreshAllProjects and ExternalSystem.ProjectRefreshAction is now searchable for sbt/bsp

---
+++ bsp/src/org/jetbrains/bsp/BspTopHitProvider.scala
@@ -0,0 +1,12 @@
+package org.jetbrains.bsp
+
+import com.intellij.ide.ActionsTopHitProvider
+
+class BspTopHitProvider extends ActionsTopHitProvider {
+  override def getActionsMatrix: Array[Array[String]] = Array(
+      Array("b", "bsp ", "ExternalSystem.RefreshAllProjects"),
+      Array("reload all b", "reload all bsp Projects ", "ExternalSystem.RefreshAllProjects"),
+      Array("b", "bsp ", "ExternalSystem.ProjectRefreshAction"),
+      Array("load b", "load bsp changes ", "ExternalSystem.ProjectRefreshAction")
+    )
+}
