281358210c9f459c7bd77902cc53789643528e63
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-06-27 13:20:05 +0200

    Editor X-Ray mode: function literal parameters, variable patterns, generators
    #SCL-16626 #SCL-14797 #SCL-14798 #SCL-14799

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hints/ScalaTypeHintsPass.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hints/ScalaTypeHintsPass.scala
@@ -6,15 +6,20 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.colors.EditorColorsScheme
 import com.intellij.psi.{PsiElement, PsiWhiteSpace}
 import org.jetbrains.plugins.scala.annotator.hints.{Hint, Text}
+import org.jetbrains.plugins.scala.codeInsight.hints.ScalaTypeHintsPass._
 import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScPatternList
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.{ScReferencePattern, ScTypedPatternLike, ScWildcardPattern}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScInfixExpr, ScReferenceExpression, ScTypedExpression, ScUnderscoreSection}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScParameter, ScParameterClause}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScFunctionDefinition, ScPatternDefinition, ScValueOrVariable, ScVariableDefinition}
 import org.jetbrains.plugins.scala.lang.psi.types.nonvalue.ScMethodType
+import org.jetbrains.plugins.scala.lang.psi.types.result.Typeable
 import org.jetbrains.plugins.scala.lang.psi.types.{ScType, TypePresentationContext}
+import org.jetbrains.plugins.scala.settings.ScalaHighlightingMode
 import org.jetbrains.plugins.scala.settings.annotations.Definition
 import org.jetbrains.plugins.scala.settings.annotations.Definition.{FunctionDefinition, ValueDefinition, VariableDefinition}
-import org.jetbrains.plugins.scala.settings.ScalaHighlightingMode
-import ScalaTypeHintsPass._
 
 private[codeInsight] trait ScalaTypeHintsPass {
   protected implicit def settings: ScalaHintsSettings
@@ -28,7 +33,7 @@ private[codeInsight] trait ScalaTypeHintsPass {
       compilerErrorsEnabled) {
       Seq.empty
     } else {
-      for {
+      (for {
         element <- root.elements
         definition = Definition(element) // NB: "definition" might be in fact _any_ PsiElement (e.g. ScalaFile)
         (tpe, body) <- typeAndBodyOf(definition)
@@ -36,8 +41,30 @@ private[codeInsight] trait ScalaTypeHintsPass {
         if !ScMethodType.hasMethodType(body)
         if settings.showObviousType || !(definition.hasStableType || isTypeObvious(definition.name, tpe, body))
         info <- hintFor(definition, tpe)(editor.getColorsScheme, TypePresentationContext(element), settings)
-      } yield info
+      } yield info) ++ (if (ScalaHintsSettings.xRayMode) collectXRayHints(editor, root) else Seq.empty)
     }.toSeq
+  }
+
+  private def collectXRayHints(editor: Editor, root: PsiElement) = root.elements.flatMap {
+    case e @ Typeable(t) => xRayHintsFor(e, t)(editor.getColorsScheme, TypePresentationContext(e), settings)
+    case _ => Seq.empty
+  }
+
+  private def xRayHintsFor(e: PsiElement, t: ScType)(implicit scheme: EditorColorsScheme, context: TypePresentationContext, settings: ScalaHintsSettings): Seq[Hint] = e match {
+    case e: ScParameter if e.typeElement.isEmpty =>
+      hints(e, t, inParentheses = e.getParent.is[ScParameterClause] && e.getParent.getFirstChild.elementType != ScalaTokenTypes.tLPARENTHESIS)
+    case e: ScUnderscoreSection if !e.getParent.is[ScTypedExpression] =>
+      hints(e, t, inParentheses = e.getParent.is[ScReferenceExpression, ScInfixExpr])
+    case _: ScReferencePattern | _: ScWildcardPattern if !e.getParent.is[ScPatternList, ScTypedPatternLike] =>
+      hints(e, t, inParentheses = false)
+    case _ => Seq.empty
+  }
+
+  private def hints(e: PsiElement, t: ScType, inParentheses: Boolean)(implicit scheme: EditorColorsScheme, context: TypePresentationContext, settings: ScalaHintsSettings) = {
+    if (inParentheses)
+      Seq(Hint(Seq(Text("(")), e, suffix = false), Hint(Text(": ") +: textPartsOf(t, settings.presentationLength) :+ Text(")"), e, suffix = true, relatesToPrecedingElement = true))
+    else
+      Seq(Hint(Text(": ") +: textPartsOf(t, settings.presentationLength), e, suffix = true, relatesToPrecedingElement = true))
   }
 }
 
