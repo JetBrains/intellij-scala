4afaf52923fa13d17eb9e4834bf7f853c11157d1
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-06-29 15:58:50 +0200

    Add test case for unimplemented feature #SCL-21139

--- scala/scala-impl/test/org/jetbrains/plugins/scalaCli/lang/parser/ScalaCliParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scalaCli/lang/parser/ScalaCliParserTest.scala
@@ -38,4 +38,12 @@ class ScalaCliParserTest extends SimpleScalaParserTestBase {
     "//> using dep, org.foo:bar:42",
     "ScalaFile\n  PsiElement(SCALA_CLI_DIRECTIVE)\n    ASTWrapperPsiElement(tCLI_DIRECTIVE_PREFIX)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_PREFIX)('//>')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveCommandImpl(tCLI_DIRECTIVE_COMMAND)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_COMMAND)('using')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveKeyImpl(tCLI_DIRECTIVE_KEY)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_KEY)('dep')\n    ASTWrapperPsiElement(tCLI_DIRECTIVE_COMMA)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_COMMA)(',')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveValueImpl(tCLI_DIRECTIVE_VALUE)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_VALUE)('org.foo:bar:42')"
   )
+
+  // Not implemented yet, so for now expected to fail.
+  // Basically, in the lexer `// ` should be a terminator for directives.
+  // Probably likewise for `/*` but that still needs to be confirmed against scala-cli.
+  def test_comment_after_directive(): Unit = checkTree(
+    "//> using dep foo // foo",
+    "ScalaFile: Dummy.scala(0,24)\n  PsiElement(SCALA_CLI_DIRECTIVE)(0,17)\n    ASTWrapperPsiElement(tCLI_DIRECTIVE_PREFIX)(0,3)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_PREFIX)('//>')(0,3)\n    PsiWhiteSpace(' ')(3,4)\n    ScCliDirectiveCommandImpl(tCLI_DIRECTIVE_COMMAND)(4,9)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_COMMAND)('using')(4,9)\n    PsiWhiteSpace(' ')(9,10)\n    ScCliDirectiveKeyImpl(tCLI_DIRECTIVE_KEY)(10,13)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_KEY)('dep')(10,13)\n    PsiWhiteSpace(' ')(13,14)\n    ScCliDirectiveValueImpl(tCLI_DIRECTIVE_VALUE)(14,17)\n      ScCliDirectiveToken(tCLI_DIRECTIVE_VALUE)('foo')(14,17)\n  PsiWhiteSpace(' ')(17,18)\n  PsiComment(comment)('// foo')(18,24)"
+  )
 }
