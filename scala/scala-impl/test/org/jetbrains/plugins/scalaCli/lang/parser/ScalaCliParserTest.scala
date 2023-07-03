f55f45967e5fa64d8c1d38b256177e90e32b37ee
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-07-03 14:36:57 +0200

    Fix test #SCL-21139

--- scala/scala-impl/test/org/jetbrains/plugins/scalaCli/lang/parser/ScalaCliParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scalaCli/lang/parser/ScalaCliParserTest.scala
@@ -31,7 +31,7 @@ class ScalaCliParserTest extends SimpleScalaParserTestBase {
 
   def test_invalid_due_to_comma_after_command(): Unit = checkTree(
     "//> using, dep org.foo:bar:42",
-    "ScalaFile\n  PsiElement(SCALA_CLI_DIRECTIVE)\n    ScCliDirectiveToken(tCLI_DIRECTIVE_PREFIX)('//>')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveToken(tCLI_DIRECTIVE_COMMAND)('using')\n    PsiErrorElement:Lexer error\n      <empty list>\n    ScCliDirectiveToken(tCLI_DIRECTIVE_ERROR)(',')\n    PsiErrorElement:Unexpected token\n      <empty list>\n    PsiWhiteSpace(' ')\n    ScCliDirectiveToken(tCLI_DIRECTIVE_KEY)('dep')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveToken(tCLI_DIRECTIVE_VALUE)('org.foo:bar:42')"
+    "ScalaFile\n  PsiElement(SCALA_CLI_DIRECTIVE)\n    ScCliDirectiveToken(tCLI_DIRECTIVE_PREFIX)('//>')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveToken(tCLI_DIRECTIVE_COMMAND)('using')\n    PsiErrorElement:Scala CLI key expected: option, dep, jar, etc.\n      <empty list>\n    ScCliDirectiveToken(tCLI_DIRECTIVE_ERROR)(',')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveToken(tCLI_DIRECTIVE_KEY)('dep')\n    PsiWhiteSpace(' ')\n    ScCliDirectiveToken(tCLI_DIRECTIVE_VALUE)('org.foo:bar:42')"
   )
 
   def test_valid_even_though_there_is_a_comma_after_key(): Unit = checkTree(
