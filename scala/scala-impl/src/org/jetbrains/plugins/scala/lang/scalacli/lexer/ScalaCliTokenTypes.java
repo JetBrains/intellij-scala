7eb8e335de70a9d73066e564f6fa89628b9fce7a
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-07-03 15:37:13 +0200

    Remove unused token types #SCL-21139

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scalacli/lexer/ScalaCliTokenTypes.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scalacli/lexer/ScalaCliTokenTypes.java
@@ -3,12 +3,10 @@ package org.jetbrains.plugins.scala.lang.scalacli.lexer;
 import com.intellij.psi.tree.IElementType;
 
 public interface ScalaCliTokenTypes {
-    IElementType tCLI_DIRECTIVE = new ScalaCliElementType("tCLI_DIRECTIVE");
     IElementType tCLI_DIRECTIVE_PREFIX = new ScalaCliElementType("tCLI_DIRECTIVE_PREFIX");
     IElementType tCLI_DIRECTIVE_COMMA = new ScalaCliElementType("tCLI_DIRECTIVE_COMMA");
     IElementType tCLI_DIRECTIVE_COMMAND = new ScalaCliElementType("tCLI_DIRECTIVE_COMMAND");
     IElementType tCLI_DIRECTIVE_KEY = new ScalaCliElementType("tCLI_DIRECTIVE_KEY");
     IElementType tCLI_DIRECTIVE_VALUE = new ScalaCliElementType("tCLI_DIRECTIVE_VALUE");
-    IElementType tCLI_DIRECTIVE_TERMINATOR = new ScalaCliElementType("tCLI_DIRECTIVE_TERMINATOR");
     IElementType tCLI_DIRECTIVE_ERROR = new ScalaCliElementType("tCLI_DIRECTIVE_ERROR");
 }
