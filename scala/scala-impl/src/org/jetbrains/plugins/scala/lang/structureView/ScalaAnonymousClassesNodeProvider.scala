9896a3fcaeb72d4be938b3cebd3a3936dfc06ea3
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-07-11 15:48:19 +0200

    structure view: recognise anonymous classes inside val/var bodies #SCL-20994

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/structureView/ScalaAnonymousClassesNodeProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/structureView/ScalaAnonymousClassesNodeProvider.scala
@@ -7,9 +7,10 @@ import com.intellij.openapi.actionSystem.Shortcut
 import com.intellij.openapi.util.PropertyOwner
 import com.intellij.psi.PsiElement
 import com.intellij.ui.{IconManager, PlatformIcons}
-import org.jetbrains.plugins.scala.extensions.{IteratorExt, PsiElementExt}
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlock, ScNewTemplateDefinition}
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunctionDefinition, ScValueOrVariableDefinition}
+import org.jetbrains.plugins.scala.extensions.{IteratorExt, ObjectExt, OptionExt, PsiElementExt}
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlock, ScBlockExpr, ScNewTemplateDefinition}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunctionDefinition, ScValueOrVariable, ScValueOrVariableDefinition}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBody
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScTemplateDefinition}
 import org.jetbrains.plugins.scala.lang.structureView.ScalaAnonymousClassesNodeProvider.getAnonymousClassElements
@@ -17,6 +18,7 @@ import org.jetbrains.plugins.scala.lang.structureView.element.ScalaAnonymousClas
 
 import java.util
 import java.util.Collections
+import scala.collection.mutable
 import scala.jdk.CollectionConverters.SeqHasAsJava
 
 /**
@@ -57,12 +59,38 @@ object ScalaAnonymousClassesNodeProvider {
   val ID = "SCALA_SHOW_ANONYMOUS"
   private val SCALA_ANONYMOUS_PROPERTY_NAME = "scala.anonymous.provider"
 
+  /**
+   * Note regarding val/var definitions:
+   *  1. val/var definitions nodes are only created for fields, but not for local definitions: {{{
+   *       class A {
+   *         val value1 = 1 //has a node in structure view
+   *         def foo = {
+   *           val value2 = 2 //doesn't have a node in a structure view
+   *         }
+   *       }
+   *     }}}
+   *     See [[org.jetbrains.plugins.scala.lang.structureView.element.TypeDefinition.childrenOf]]
+   *     and [[org.jetbrains.plugins.scala.lang.structureView.element.Block.childrenOf]]
+   *
+   *  1. var/var fields definition can have nested children only when it's body is a block expression<br>
+   *     See [[org.jetbrains.plugins.scala.lang.structureView.element.ValOrVar.children]]
+   */
   private def getAnonymousClassElements(parent0: PsiElement): Seq[ScNewTemplateDefinition] = {
     val elementToStartFromOpt = parent0 match {
-      case td: ScTemplateDefinition       => td.extendsBlock.templateBody
-      case f: ScFunctionDefinition        => f.body
-      case v: ScValueOrVariableDefinition => v.expr
-      case _                              => Some(parent0)
+      case td: ScTemplateDefinition => td.extendsBlock.templateBody
+      case f: ScFunctionDefinition => f.body
+      case p: ScReferencePattern =>
+        //We do extra `filterByType` after `findByType`, not just `findByType[ScValueOrVariableDefinition]`
+        //to fail fast on declarations (abstract val/var)
+        val valueOrVariableDef = p.parentsInFile.findByType[ScValueOrVariable].filterByType[ScValueOrVariableDefinition]
+        for {
+          case value <- valueOrVariableDef
+          //If val/var definition body is not a block, we skip it. In that case anonimous classes will be collected
+          //during processing of the parent node, and will be added as a sibling of val/var node (see `getAnonymousClassElements` ScalaDoc)
+          case block <- value.expr.filterByType[ScBlockExpr]
+        } yield block
+      case _ =>
+        Some(parent0)
     }
     val elementToStartFrom = elementToStartFromOpt match {
       case Some(value) => value
@@ -70,22 +98,43 @@ object ScalaAnonymousClassesNodeProvider {
         return Nil
     }
 
-    val elementsIterator = elementToStartFrom.depthFirst { element =>
+    val result = mutable.ArrayBuffer.empty[ScNewTemplateDefinition]
+
+    def process(element: PsiElement): Unit = {
+      element match {
+        case d: ScNewTemplateDefinition if d.isAnonimous =>
+          result += d
+        case _ =>
+      }
+    }
+
+    //Collect anonimous classes in quite a dirty imperative way
+    elementToStartFrom.depthFirst { element =>
       val processChildren = element match {
+        //Don't stop at "ScTemplateDefinition"
         //Template body of every type definition starts it's own scope of anonimous
         //Template definition has constructor invocation, and anonimous class can be inside constructor arguments
         //So instead of stopping at template definition node we will stop at template body node
-        case _: ScTemplateDefinition                      => true
-        case _: ScTemplateBody | _: ScMember | _: ScBlock => false
-        case _                                            => true
+        case _: ScTemplateDefinition => true
+        case _: ScTemplateBody => false
+        case v: ScValueOrVariableDefinition =>
+          val isField = v.getParent.is[ScTemplateBody]
+          val hasBlockBody = v.expr.exists(_.is[ScBlockExpr])
+          val collectAnonimousClassesInside = !(isField && hasBlockBody)
+          //Nodes for val/var fields with block bodies will have their own children and for them anonimous classes
+          //will be collected in a separate pass (see `getAnonymousClassElements` ScalaDoc)
+          if (collectAnonimousClassesInside) {
+            v.depthFirst().foreach(process)
+          }
+          false
+        case _: ScMember => false
+        case _: ScBlock => false
+        case _ => true
       }
       processChildren ||
         (element eq elementToStartFrom) //don't skip children of the node from which we started the traversal
-    }
+    }.foreach(process)
 
-    elementsIterator
-      .filterByType[ScNewTemplateDefinition]
-      .filter(_.isAnonimous)
-      .toSeq
+    result.toSeq
   }
 }
