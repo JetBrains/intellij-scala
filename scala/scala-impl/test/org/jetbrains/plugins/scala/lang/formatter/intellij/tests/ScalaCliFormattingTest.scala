d21947f8e2a0078dcf4bb64aac8ad19502c3ddda
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-07-12 09:28:20 +0200

    Fix directives formatting #SCL-21405 fixed
    Note that at this point, the desired behaviour and requirements for
    directives formatting have not been established, refined or
    documented much.
    
    SCL-21405 describes some of the formatting
    behaviour that happens to exist after implementing directives
    parsing in SCL-21139, but there was almost no code merged as
    part of SCL-21139 to explicitly influence formatting behaviour.
    
    So SCL-21405 represents an initial attempt to weed out undesirable
    behaviour. It will be followed up by more exact tickets that each
    deal with more specific formatting scenarios.

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/formatter/intellij/tests/ScalaCliFormattingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/formatter/intellij/tests/ScalaCliFormattingTest.scala
@@ -20,25 +20,86 @@ class ScalaCliFormattingTest extends AbstractScalaFormatterTestBase {
       |""".stripMargin
   )
 
-  def test_add_empty_line_between_directive_and_other_scala1(): Unit = doTextTest(
+  def test_do_not_add_empty_line_between_directive_and_other_scala1(): Unit = doTextTest(
     """//> using dep foo
-      |class Foo
-      |""".stripMargin,
-    """//> using dep foo
-      |
       |class Foo
       |""".stripMargin
   )
 
-  def test_add_empty_line_between_directive_and_other_scala2(): Unit = doTextTest(
+  def test_do_not_add_empty_line_between_directive_and_other_scala2(): Unit = doTextTest(
     """//> using dep foo
       |// foo
       |class Foo
-      |""".stripMargin,
-    """//> using dep foo
+      |""".stripMargin
+  )
+
+  def test_do_not_add_empty_line_between_package_declaration_and_directive(): Unit = doTextTest(
+    """package foo
+      |//> using dep""".stripMargin
+  )
+
+  def test_do_not_add_empty_line_between_directive_and_package_declaration(): Unit = doTextTest(
+    """//> using dep
+      |package foo""".stripMargin
+  )
+
+  def test_do_not_add_empty_line_when_directive_surrounded_by_package_declarations(): Unit = doTextTest(
+    """package foo
+      |//> using dep
+      |package bar""".stripMargin
+  )
+
+  def test_do_not_add_empty_line_between_class_declaration_and_directive(): Unit = doTextTest(
+    """class Foo
+      |//> using dep""".stripMargin
+  )
+
+  def test_do_not_add_empty_line_between_directive_and_class_declaration(): Unit = doTextTest(
+    """//> using dep
+      |class Foo""".stripMargin
+  )
+
+  def test_do_not_add_empty_line_when_directive_surrounded_by_class_declarations(): Unit = doTextTest(
+    """class Foo
+      |//> using dep
+      |class Bar""".stripMargin
+  )
+
+  def test_indent_but_do_not_add_empty_line_when_directive_is_placed_in_class_body(): Unit = doTextTest(
+    """class Foo {
+      |//> using dep
+      |}""".stripMargin,
+    """class Foo {
+      |  //> using dep
+      |}""".stripMargin,
+  )
+
+  def test_indent_but_do_not_add_empty_line_when_directive_is_placed_in_nested_class_body(): Unit = doTextTest(
+    """class Foo {
+      |  class Bar {
+      |//> using dep
+      |  }
+      |}""".stripMargin,
+    """class Foo {
+      |  class Bar {
+      |    //> using dep
+      |  }
+      |}""".stripMargin,
+  )
+
+  def test_surrounded_by_comments(): Unit = doTextTest(
+    """// Foo
+      |//> using dep
+      |// Foo
+      |""".stripMargin
+  )
+
+  def test_surrounded_by_newlines_and_comments(): Unit = doTextTest(
+    """// Foo
       |
-      |// foo
-      |class Foo
+      |//> using dep
+      |
+      |// Foo
       |""".stripMargin
   )
 }
