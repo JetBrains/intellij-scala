83eeb8672232222f13dfd6b63925575957960b6f
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-07-17 16:37:25 +0200

    Use valid code and add basic Scala-facing parameters tests #SCL-21438

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/psi/api/statements/ScParametersTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/psi/api/statements/ScParametersTest.scala
@@ -10,16 +10,27 @@ class ScParametersTest extends ScalaFixtureTestCase {
 
   private val TestClassName = "Foo"
 
-  private def assertJvmFacingParameterCountAndType(methodName: String, expectedTypes: Seq[String]): Unit = {
+  /**
+   * @param methodName    Name of the method for which parameter count and type assertions are to be executed
+   * @param expectedTypes Types of the parameters that are expected to be in the method. Expected parameter count equals
+   *                      expectedType.length
+   *
+   * @param jvmFacing     If `true`, the actual parameter list is derived from the generic JVM API, i.e.
+   *                      [[org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameters.getParameters]].
+   *
+   *                      If `false`, the actual parameter list is derived from the Scala-specific API, i.e.
+   *                      [[org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameters.params]].
+   */
+  private def assertParameterCountAndType(methodName: String, expectedTypes: Seq[String], jvmFacing: Boolean): Unit = {
     val clazz = ScalaPsiManager.instance(getProject).getClassesByName(TestClassName, GlobalSearchScope.fileScope(getFile)).head.asInstanceOf[ScClass]
 
     val method = clazz.functions.filter(_.name == methodName).head
 
-    val parameters = method.getParameterList.getParameters
+    val parameters = if (jvmFacing) method.getParameterList.getParameters.toSeq else method.getParameterList.params
 
-    assertTrue(s"Expected ${expectedTypes.size} parameters, but got ${parameters.length} instead", parameters.length == expectedTypes.size)
+    assertTrue(s"Expected ${expectedTypes.length} parameters, but got ${parameters.length} instead", parameters.length == expectedTypes.length)
 
-    parameters.toSeq.zip(expectedTypes).foreach { parameterAndExpectedType =>
+    parameters.zip(expectedTypes).foreach { parameterAndExpectedType =>
       val actual = parameterAndExpectedType._1.getType.getPresentableText
       val expected = parameterAndExpectedType._2
       assertTrue(s"Expected $expected but got $actual instead", actual == expected)
@@ -34,24 +45,24 @@ class ScParametersTest extends ScalaFixtureTestCase {
         |  def bar2(i: Int, s: String): Unit = ()
         |}""".stripMargin)
 
-    assertJvmFacingParameterCountAndType("bar0", Seq.empty)
-    assertJvmFacingParameterCountAndType("bar1", Seq("int"))
-    assertJvmFacingParameterCountAndType("bar2", Seq("int", "String"))
+    assertParameterCountAndType("bar0", Seq.empty, jvmFacing = true)
+    assertParameterCountAndType("bar1", Seq("int"), jvmFacing = true)
+    assertParameterCountAndType("bar2", Seq("int", "String"), jvmFacing = true)
   }
 
   def test_jvm_facing_parameters_of_method_in_implicit_class(): Unit = {
     myFixture.configureByText(s"$TestClassName.scala",
       s"""object Scope {
-        |  implicit class $TestClassName(d: Double) {
+        |  implicit class $TestClassName(val d: Double) {
         |    def bar0(): Unit = ()
         |    def bar1(i: Int): Unit = ()
         |    def bar2(i: Int, s: String): Unit = ()
         |  }
         |}""".stripMargin)
 
-    assertJvmFacingParameterCountAndType("bar0", Seq.empty)
-    assertJvmFacingParameterCountAndType("bar1", Seq("int"))
-    assertJvmFacingParameterCountAndType("bar2", Seq("int", "String"))
+    assertParameterCountAndType("bar0", Seq.empty, jvmFacing = true)
+    assertParameterCountAndType("bar1", Seq("int"), jvmFacing = true)
+    assertParameterCountAndType("bar2", Seq("int", "String"), jvmFacing = true)
   }
 
   def test_jvm_facing_parameters_of_method_in_implicit_anyval_class(): Unit = {
@@ -66,10 +77,53 @@ class ScParametersTest extends ScalaFixtureTestCase {
         |  }
         |}""".stripMargin)
 
-    assertJvmFacingParameterCountAndType("bar0", Seq("double"))
-    assertJvmFacingParameterCountAndType("bar1", Seq("double", "int"))
-    assertJvmFacingParameterCountAndType("bar2", Seq("double", "int", "String"))
-    assertJvmFacingParameterCountAndType("bar3", Seq.empty)
-    assertJvmFacingParameterCountAndType("bar4", Seq.empty)
+    assertParameterCountAndType("bar0", Seq("double"), jvmFacing = true)
+    assertParameterCountAndType("bar1", Seq("double", "int"), jvmFacing = true)
+    assertParameterCountAndType("bar2", Seq("double", "int", "String"), jvmFacing = true)
+    assertParameterCountAndType("bar3", Seq.empty, jvmFacing = true)
+    assertParameterCountAndType("bar4", Seq.empty, jvmFacing = true)
+  }
+  
+  def test_scala_facing_parameters_of_method_in_class(): Unit = {
+    myFixture.configureByText(s"$TestClassName.scala",
+      s"""class $TestClassName {
+        |  def bar0(): Unit = ()
+        |  def bar1(i: Int): Unit = ()
+        |  def bar2(i: Int, s: String): Unit = ()
+        |}""".stripMargin)
+
+    assertParameterCountAndType("bar0", Seq.empty, jvmFacing = false)
+    assertParameterCountAndType("bar1", Seq("int"), jvmFacing = false)
+    assertParameterCountAndType("bar2", Seq("int", "String"), jvmFacing = false)
+  }
+
+  def test_scala_facing_parameters_of_method_in_implicit_class(): Unit = {
+    myFixture.configureByText(s"$TestClassName.scala",
+      s"""object Scope {
+        |  implicit class $TestClassName(val d: Double) {
+        |    def bar0(): Unit = ()
+        |    def bar1(i: Int): Unit = ()
+        |    def bar2(i: Int, s: String): Unit = ()
+        |  }
+        |}""".stripMargin)
+
+    assertParameterCountAndType("bar0", Seq.empty, jvmFacing = false)
+    assertParameterCountAndType("bar1", Seq("int"), jvmFacing = false)
+    assertParameterCountAndType("bar2", Seq("int", "String"), jvmFacing = false)
+  }
+
+  def test_scala_facing_parameters_of_method_in_implicit_anyval_class(): Unit = {
+    myFixture.configureByText(s"$TestClassName.scala",
+      s"""object Scope {
+        |  implicit class $TestClassName(val d: Double) extends AnyVal {
+        |    def bar0(): Unit = ()
+        |    def bar1(i: Int): Unit = ()
+        |    def bar2(i: Int, s: String): Unit = ()
+        |  }
+        |}""".stripMargin)
+
+    assertParameterCountAndType("bar0", Seq.empty, jvmFacing = false)
+    assertParameterCountAndType("bar1", Seq("int"), jvmFacing = false)
+    assertParameterCountAndType("bar2", Seq("int", "String"), jvmFacing = false)
   }
 }
