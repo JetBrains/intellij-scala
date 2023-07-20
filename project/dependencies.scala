f11c1d5a1bf660b95c760eda7ddec36bcbfabe72
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2023-07-20 11:19:36 +0200

    Update intellijVersion to 232.8660.121

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.9.2"
 
   // ATTENTION: check the comment in CommonUltimate when updating this version
-  val intellijVersion = "232.8660.100"
+  val intellijVersion = "232.8660.121"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
