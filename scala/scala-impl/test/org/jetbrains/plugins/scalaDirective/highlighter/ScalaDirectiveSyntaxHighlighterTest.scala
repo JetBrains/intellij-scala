3526a19b009711d97a308cde8e442b94b016deb2
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-07-25 18:09:39 +0200

    Update directive highlighter test to new API

--- scala/scala-impl/test/org/jetbrains/plugins/scalaDirective/highlighter/ScalaDirectiveSyntaxHighlighterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scalaDirective/highlighter/ScalaDirectiveSyntaxHighlighterTest.scala
@@ -2,14 +2,24 @@ package org.jetbrains.plugins.scalaDirective.highlighter
 
 import com.intellij.testFramework.EditorTestUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import org.jetbrains.plugins.scala.util.TestUtils
 
 class ScalaDirectiveSyntaxHighlighterTest extends BasePlatformTestCase {
 
-  private val answerFilePath: String = TestUtils.getTestDataPath + "/ScalaDirectiveSyntaxHighlighter/fallback_attribute_keys.txt"
-
   def test_lexer_provided_syntax_highlighting_fallback_attribute_keys(): Unit = {
     val testFile = myFixture.configureByText("foo.scala", "//> using foo bar1 bar2")
-    EditorTestUtil.testFileSyntaxHighlighting(testFile, answerFilePath, false)
+
+    val answer =
+      """//>
+        |    Scala directive prefix => DEFAULT_DOC_COMMENT
+        |using
+        |    Scala directive command => DEFAULT_DOC_COMMENT_TAG
+        |foo
+        |    Scala directive key => DEFAULT_DOC_COMMENT_TAG_VALUE
+        |bar1
+        |    Scala directive value => DEFAULT_DOC_COMMENT
+        |bar2
+        |    Scala directive value => DEFAULT_DOC_COMMENT""".stripMargin
+
+    EditorTestUtil.testFileSyntaxHighlighting(testFile, false, answer)
   }
 }
