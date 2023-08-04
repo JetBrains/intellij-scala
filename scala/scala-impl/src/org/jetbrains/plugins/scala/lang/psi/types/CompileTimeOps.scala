067afb4b361d03b5eb7eb43800e54ba4c87dcb26
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-08-04 19:58:58 +0200

    type-level compiler intrinsics: support long, float, double, NumberOfLeadingZeros #SCL-20415

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/CompileTimeOps.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/CompileTimeOps.scala
@@ -4,7 +4,7 @@ import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.extensions.ObjectExt
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAliasDeclaration
 import org.jetbrains.plugins.scala.lang.psi.impl.base.ScStringLiteralImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.base.literals.{ScBooleanLiteralImpl, ScIntegerLiteralImpl}
+import org.jetbrains.plugins.scala.lang.psi.impl.base.literals.{ScBooleanLiteralImpl, ScDoubleLiteralImpl, ScFloatLiteralImpl, ScIntegerLiteralImpl, ScLongLiteralImpl}
 import org.jetbrains.plugins.scala.lang.psi.types.api.ValueType
 import org.jetbrains.plugins.scala.lang.psi.types.api.designator.ScProjectionType
 
@@ -32,6 +32,9 @@ private object CompileTimeOps {
         case "scala.compiletime.ops.any" => anyOp(alias.name, argumentsDealiased)
         case "scala.compiletime.ops.boolean" => booleanOp(alias.name, argumentsDealiased)
         case "scala.compiletime.ops.int" => intOp(alias.name, argumentsDealiased)
+        case "scala.compiletime.ops.long" => longOp(alias.name, argumentsDealiased)
+        case "scala.compiletime.ops.float" => floatOp(alias.name, argumentsDealiased)
+        case "scala.compiletime.ops.double" => doubleOp(alias.name, argumentsDealiased)
         case "scala.compiletime.ops.string" => stringOp(alias.name, argumentsDealiased)
         case _ => None
       }
@@ -46,12 +49,17 @@ private object CompileTimeOps {
     case _ => None
   }
 
-  private def intOp(operator: String, operands: Seq[ScType])(implicit project: Project) = operands match {
+  private def intOp(operator: String, operands: Seq[ScType])(implicit project: Project): Option[ScLiteralType] = operands match {
     case Seq(IntValue(i)) => (operator: @switch) match {
       case "S" => Some(IntValue(i + 1))
       case "Abs" => Some(IntValue(i.abs))
       case "Negate" => Some(IntValue(-i))
-      case "ToString" => Some(StringValue(i.toString))
+      case "NumberOfLeadingZeros" => Some(IntValue(java.lang.Integer.numberOfLeadingZeros(i)))
+
+      case "ToLong" => Some(LongValue(i.toLong))
+      case "ToFloat" => Some(FloatValue(i.toFloat))
+      case "ToDouble" => Some(DoubleValue(i.toDouble))
+      case "ToString" => Some(StringValue(i.toString)) //NOTE: deprecated since 3.2.0
       case _ => None
     }
     case Seq(IntValue(l), IntValue(r)) => (operator: @switch) match {
@@ -64,14 +72,113 @@ private object CompileTimeOps {
       case ">>" => Some(IntValue(l >> r))
       case ">>>" => Some(IntValue(l >>> r))
       case "^" => Some(IntValue(l ^ r))
+
       case "<" => Some(BooleanValue(l < r))
       case ">" => Some(BooleanValue(l > r))
       case ">=" => Some(BooleanValue(l >= r))
       case "<=" => Some(BooleanValue(l <= r))
+
       case "BitwiseAnd" => Some(IntValue(l & r))
       case "BitwiseOr" => Some(IntValue(l | r))
       case "Min" => Some(IntValue(l.min(r)))
       case "Max" => Some(IntValue(l.max(r)))
+      case _ => None
+    }
+    case _ => None
+  }
+
+  private def longOp(operator: String, operands: Seq[ScType])(implicit project: Project): Option[ScLiteralType] = operands match {
+    case Seq(LongValue(v)) => (operator: @switch) match {
+      case "S" => Some(LongValue(v + 1))
+      case "Abs" => Some(LongValue(v.abs))
+      case "Negate" => Some(LongValue(-v))
+      case "NumberOfLeadingZeros" => Some(IntValue(java.lang.Long.numberOfLeadingZeros(v)))
+
+      case "ToInt" => Some(IntValue(v.toInt))
+      case "ToFloat" => Some(FloatValue(v.toFloat))
+      case "ToDouble" => Some(DoubleValue(v.toDouble))
+      case _ => None
+    }
+    case Seq(LongValue(l), LongValue(r)) => (operator: @switch) match {
+      case "+" => Some(LongValue(l + r))
+      case "-" => Some(LongValue(l - r))
+      case "*" => Some(LongValue(l * r))
+      case "/" => Some(LongValue(l / r))
+      case "%" => Some(LongValue(l % r))
+      case "<<" => Some(LongValue(l << r))
+      case ">>" => Some(LongValue(l >> r))
+      case ">>>" => Some(LongValue(l >>> r))
+      case "^" => Some(LongValue(l ^ r))
+
+      case "<" => Some(BooleanValue(l < r))
+      case ">" => Some(BooleanValue(l > r))
+      case ">=" => Some(BooleanValue(l >= r))
+      case "<=" => Some(BooleanValue(l <= r))
+
+      case "BitwiseAnd" => Some(LongValue(l & r))
+      case "BitwiseOr" => Some(LongValue(l | r))
+      case "Min" => Some(LongValue(l.min(r)))
+      case "Max" => Some(LongValue(l.max(r)))
+      case _ => None
+    }
+    case _ => None
+  }
+
+  private def floatOp(operator: String, operands: Seq[ScType])(implicit project: Project): Option[ScLiteralType] = operands match {
+    case Seq(FloatValue(v)) => (operator: @switch) match {
+      case "Abs" => Some(FloatValue(v.abs))
+      case "Negate" => Some(FloatValue(-v))
+
+      case "ToInt" => Some(IntValue(v.toInt))
+      case "ToLong" => Some(LongValue(v.toLong))
+      case "ToDouble" => Some(DoubleValue(v.toDouble))
+
+      case _ => None
+    }
+    case Seq(FloatValue(l), FloatValue(r)) => (operator: @switch) match {
+      case "+" => Some(FloatValue(l + r))
+      case "-" => Some(FloatValue(l - r))
+      case "*" => Some(FloatValue(l * r))
+      case "/" => Some(FloatValue(l / r))
+      case "%" => Some(FloatValue(l % r))
+
+      case "<" => Some(BooleanValue(l < r))
+      case ">" => Some(BooleanValue(l > r))
+      case ">=" => Some(BooleanValue(l >= r))
+      case "<=" => Some(BooleanValue(l <= r))
+
+      case "Min" => Some(FloatValue(l.min(r)))
+      case "Max" => Some(FloatValue(l.max(r)))
+      case _ => None
+    }
+    case _ => None
+  }
+
+  private def doubleOp(operator: String, operands: Seq[ScType])(implicit project: Project): Option[ScLiteralType] = operands match {
+    case Seq(DoubleValue(v)) => (operator: @switch) match {
+      case "Abs" => Some(DoubleValue(v.abs))
+      case "Negate" => Some(DoubleValue(-v))
+
+      case "ToInt" => Some(IntValue(v.toInt))
+      case "ToLong" => Some(LongValue(v.toLong))
+      case "ToFloat" => Some(FloatValue(v.toFloat))
+
+      case _ => None
+    }
+    case Seq(DoubleValue(l), DoubleValue(r)) => (operator: @switch) match {
+      case "+" => Some(DoubleValue(l + r))
+      case "-" => Some(DoubleValue(l - r))
+      case "*" => Some(DoubleValue(l * r))
+      case "/" => Some(DoubleValue(l / r))
+      case "%" => Some(DoubleValue(l % r))
+
+      case "<" => Some(BooleanValue(l < r))
+      case ">" => Some(BooleanValue(l > r))
+      case ">=" => Some(BooleanValue(l >= r))
+      case "<=" => Some(BooleanValue(l <= r))
+
+      case "Min" => Some(DoubleValue(l.min(r)))
+      case "Max" => Some(DoubleValue(l.max(r)))
       case _ => None
     }
     case _ => None
@@ -139,6 +246,33 @@ private object CompileTimeOps {
 
     def unapply(t: ScLiteralType): Option[String] = t.value match {
       case ScStringLiteralImpl.Value(v) => Some(v)
+      case _ => None
+    }
+  }
+
+  private object LongValue {
+    def apply(v: Long)(implicit context: Project): ScLiteralType = ScLiteralType(ScLongLiteralImpl.Value(v))
+
+    def unapply(t: ScLiteralType): Option[Long] = t.value match {
+      case ScLongLiteralImpl.Value(v) => Some(v)
+      case _ => None
+    }
+  }
+
+  private object FloatValue {
+    def apply(v: Float)(implicit context: Project): ScLiteralType = ScLiteralType(ScFloatLiteralImpl.Value(v))
+
+    def unapply(t: ScLiteralType): Option[Float] = t.value match {
+      case ScFloatLiteralImpl.Value(v) => Some(v)
+      case _ => None
+    }
+  }
+
+  private object DoubleValue {
+    def apply(v: Double)(implicit context: Project): ScLiteralType = ScLiteralType(ScDoubleLiteralImpl.Value(v))
+
+    def unapply(t: ScLiteralType): Option[Double] = t.value match {
+      case ScDoubleLiteralImpl.Value(v) => Some(v)
       case _ => None
     }
   }
--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/CompileTimeOpsTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/typeInference/CompileTimeOpsTest.scala
@@ -15,6 +15,9 @@ class CompileTimeOpsTest extends ScalaLightCodeInsightFixtureTestCase {
   private final val AnyOps =     "import scala.compiletime.ops.any.*; "
   private final val BooleanOps = "import scala.compiletime.ops.boolean.*; "
   private final val IntOps =     "import scala.compiletime.ops.int.*; "
+  private final val DoubleOps =  "import scala.compiletime.ops.double.*; "
+  private final val FloatOps =   "import scala.compiletime.ops.float.*; "
+  private final val LongOps =    "import scala.compiletime.ops.long.*; "
   private final val StringOps =  "import scala.compiletime.ops.string.*; "
 
   private final val CommonAliases =
@@ -121,6 +124,12 @@ class CompileTimeOpsTest extends ScalaLightCodeInsightFixtureTestCase {
   def testToString_IntLiteral_2(): Unit = assertTypeIs(AnyOps +
     """type T = ToString[1] == "2"""", "false")
 
+  def testToString_LongLiteral_1(): Unit = assertTypeIs(AnyOps +
+    """type T = ToString[1L] == "1"""", "true")
+
+  def testToString_LongLiteral_2(): Unit = assertTypeIs(AnyOps +
+    """type T = ToString[1L] == "2"""", "false")
+
   def testToString_BooleanLiteral_1(): Unit = assertTypeIs(AnyOps +
     """type T = ToString[true] == "true"""", "true")
 
@@ -218,6 +227,262 @@ class CompileTimeOpsTest extends ScalaLightCodeInsightFixtureTestCase {
 
   def testIntToString(): Unit = assertTypeIs(IntOps +
     "type T = ToString[1]", "\"1\"")
+
+  def testIntToLong(): Unit = assertTypeIs(IntOps +
+    "type T = ToLong[1]", "1L")
+
+  def testIntToFloat(): Unit = assertTypeIs(IntOps +
+    "type T = ToFloat[1]", "1.0f")
+
+  def testIntToDouble(): Unit = assertTypeIs(IntOps +
+    "type T = ToDouble[1]", "1.0")
+
+  def testIntNumberOfLeadingZeros_Positive(): Unit = {
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[0]", "32")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[1]", "31")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[2]", "30")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[3]", "30")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[4]", "29")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[5]", "29")
+
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[510]", "23")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[511]", "23")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[512]", "22")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[513]", "22")
+
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[2147483647]", "1")  //Int.MaxValue
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[2147483646]", "1")  //Int.MaxValue - 1
+  }
+
+  def testIntNumberOfLeadingZeros_Negative(): Unit = {
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-0]", "32")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-1]", "0")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-2]", "0")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-64]", "0")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-65]", "0")
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-2147483648]", "0") //Int.MinValue
+    assertTypeIs(IntOps + "type T = NumberOfLeadingZeros[-2147483647]", "0") //Int.MaxValue + 1
+  }
+
+  //
+  // Long
+  //
+  def testLongS(): Unit = assertTypeIs(LongOps +
+    "type T = S[1L]", "2L")
+
+  def testLongS_IntMax(): Unit = assertTypeIs(LongOps +
+    "type T = S[2147483647L]", "2147483648L")
+
+  def testLongS_LongMaxMinus1(): Unit = assertTypeIs(LongOps +
+    "type T = S[9223372036854775806L]", "9223372036854775807L")
+
+  def testLongS_LongMax(): Unit = assertTypeIs(LongOps +
+    "type T = S[9223372036854775807L]", "-9223372036854775808L")
+
+  def testLongPlus(): Unit = assertTypeIs(LongOps +
+    "type T = 1L + 2L", "3L")
+
+  def testLongMinus(): Unit = assertTypeIs(LongOps +
+    "type T = 3L - 1L", "2L")
+
+  def testLongMultiply(): Unit = assertTypeIs(LongOps +
+    "type T = 2L * 3L", "6L")
+
+  def testLongDivide(): Unit = assertTypeIs(LongOps +
+    "type T = 6L / 3L", "2L")
+
+  def testLongRemainder(): Unit = assertTypeIs(LongOps +
+    "type T = 6L % 4L", "2L")
+
+  def testLongShiftLeft(): Unit = assertTypeIs(LongOps +
+    "type T = 2L << 1L", "4L")
+
+  def testLongShiftRight(): Unit = assertTypeIs(LongOps +
+    "type T = 4L >> 1L", "2L")
+
+  def testLongShiftRightZero(): Unit = assertTypeIs(LongOps +
+    "type T = 10L >>> 1L", "5L")
+
+  def testLongXor(): Unit = assertTypeIs(LongOps +
+    "type T = 2147483647L ^ 2147483646L", "1L")
+
+  def testLongLt(): Unit = assertTypeIs(LongOps +
+    "type T = 1L < 2L", "true")
+
+  def testLongGt(): Unit = assertTypeIs(LongOps +
+    "type T = 1L > 2L", "false")
+
+  def testLongGtEq(): Unit = assertTypeIs(LongOps +
+    "type T = 1L >= 1L", "true")
+
+  def testLongLtEq(): Unit = assertTypeIs(LongOps +
+    "type T = 1L <= 1L", "true")
+
+  def testLongBitwiseAnd(): Unit = assertTypeIs(LongOps +
+    "type T = BitwiseAnd[1L, 3L]", "1L")
+
+  def testLongBitwiseOr(): Unit = assertTypeIs(LongOps +
+    "type T = BitwiseOr[1L, 2L]", "3L")
+
+  def testLongAbs(): Unit = assertTypeIs(LongOps +
+    "type T = Abs[-1L]", "1L")
+
+  def testLongNegate(): Unit = assertTypeIs(LongOps +
+    "type T = Negate[-1L]", "1L")
+
+  def testLongMin(): Unit = assertTypeIs(LongOps +
+    "type T = Min[1L, 2L]", "1L")
+
+  def testLongMax(): Unit = assertTypeIs(LongOps +
+    "type T = Max[1L, 2L]", "2L")
+
+  def testLongToInt(): Unit = assertTypeIs(LongOps +
+    "type T = ToInt[1L]", "1")
+
+  def testLongToInt_IntOverflowedValue(): Unit = assertTypeIs(LongOps +
+    "type T = ToInt[9223372036854775804L]", "-4")
+
+  def testLongToFloat(): Unit = assertTypeIs(LongOps +
+    "type T = ToFloat[1L]", "1.0f")
+
+  def testLongToFloat_LongMaxValue(): Unit = assertTypeIs(LongOps +
+    "type T = ToFloat[9223372036854775807L]", "9.223372E18f")
+
+  def testLongToDouble(): Unit = assertTypeIs(LongOps +
+    "type T = ToDouble[1L]", "1.0")
+
+  def testLongToDouble_LongMaxValue(): Unit = assertTypeIs(LongOps +
+    "type T = ToDouble[9223372036854775807L]", "9.223372036854776E18")
+
+  def testLongNumberOfLeadingZeros_Positive(): Unit = {
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[0L]", "64")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[1L]", "63")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[2L]", "62")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[3L]", "62")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[4L]", "61")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[5L]", "61")
+
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[510L]", "55")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[511L]", "55")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[512L]", "54")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[513L]", "54")
+
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[9223372036854775807L]", "1")  //Long.MaxValue
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[9223372036854775806L]", "1")  //Long.MaxValue - 1
+  }
+
+  def testLongNumberOfLeadingZeros_Negative(): Unit = {
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-0L]", "64")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-1L]", "0")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-2L]", "0")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-64L]", "0")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-65L]", "0")
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-9223372036854775808L]", "0") //Long.MinValue
+    assertTypeIs(LongOps + "type T = NumberOfLeadingZeros[-9223372036854775807L]", "0") //Long.MaxValue + 1
+  }
+
+  //
+  // Float
+  //
+
+  def testFloatPlus(): Unit = assertTypeIs(FloatOps +
+    "type T = 1f + 2f", "3.0f")
+
+  def testFloatMinus(): Unit = assertTypeIs(FloatOps +
+    "type T = 3f - 1f", "2.0f")
+
+  def testFloatMultiply(): Unit = assertTypeIs(FloatOps +
+    "type T = 2f * 3f", "6.0f")
+
+  def testFloatDivide(): Unit = assertTypeIs(FloatOps +
+    "type T = 6f / 3f", "2.0f")
+
+  def testFloatRemainder(): Unit = assertTypeIs(FloatOps +
+    "type T = 6f % 4f", "2.0f")
+
+  def testFloatLt(): Unit = assertTypeIs(FloatOps +
+    "type T = 1f < 2f", "true")
+
+  def testFloatGt(): Unit = assertTypeIs(FloatOps +
+    "type T = 1f > 2f", "false")
+
+  def testFloatGtEq(): Unit = assertTypeIs(FloatOps +
+    "type T = 1f >= 1f", "true")
+
+  def testFloatLtEq(): Unit = assertTypeIs(FloatOps +
+    "type T = 1f <= 1f", "true")
+
+  def testFloatAbs(): Unit = assertTypeIs(FloatOps +
+    "type T = Abs[-1f]", "1.0f")
+
+  def testFloatNegate(): Unit = assertTypeIs(FloatOps +
+    "type T = Negate[-1f]", "1.0f")
+
+  def testFloatMin(): Unit = assertTypeIs(FloatOps +
+    "type T = Min[1f, 2f]", "1.0f")
+
+  def testFloatMax(): Unit = assertTypeIs(FloatOps +
+    "type T = Max[1f, 2f]", "2.0f")
+
+  def testFloatToLong(): Unit = assertTypeIs(FloatOps +
+    "type T = ToLong[1f]", "1L")
+
+  def testFloatToInt(): Unit = assertTypeIs(FloatOps +
+    "type T = ToInt[1f]", "1")
+
+  def testFloatToDouble(): Unit = assertTypeIs(FloatOps +
+    "type T = ToDouble[1f]", "1.0")
+
+  //
+  // Double
+  //
+  def testDoublePlus(): Unit = assertTypeIs(DoubleOps +
+    "type T = 1d + 2d", "3.0")
+
+  def testDoubleMinus(): Unit = assertTypeIs(DoubleOps +
+    "type T = 3d - 1d", "2.0")
+
+  def testDoubleMultiply(): Unit = assertTypeIs(DoubleOps +
+    "type T = 2d * 3d", "6.0")
+
+  def testDoubleDivide(): Unit = assertTypeIs(DoubleOps +
+    "type T = 6d / 3d", "2.0")
+
+  def testDoubleRemainder(): Unit = assertTypeIs(DoubleOps +
+    "type T = 6d % 4d", "2.0")
+
+  def testDoubleLt(): Unit = assertTypeIs(DoubleOps +
+    "type T = 1d < 2d", "true")
+
+  def testDoubleGt(): Unit = assertTypeIs(DoubleOps +
+    "type T = 1d > 2d", "false")
+
+  def testDoubleGtEq(): Unit = assertTypeIs(DoubleOps +
+    "type T = 1d >= 1d", "true")
+
+  def testDoubleLtEq(): Unit = assertTypeIs(DoubleOps +
+    "type T = 1d <= 1d", "true")
+
+  def testDoubleAbs(): Unit = assertTypeIs(DoubleOps +
+    "type T = Abs[-1d]", "1.0")
+
+  def testDoubleNegate(): Unit = assertTypeIs(DoubleOps +
+    "type T = Negate[-1d]", "1.0")
+
+  def testDoubleMin(): Unit = assertTypeIs(DoubleOps +
+    "type T = Min[1d, 2d]", "1.0")
+
+  def testDoubleMax(): Unit = assertTypeIs(DoubleOps +
+    "type T = Max[1d, 2d]", "2.0")
+
+  def testDoubleToLong(): Unit = assertTypeIs(DoubleOps +
+    "type T = ToLong[1d]", "1L")
+
+  def testDoubleToInt(): Unit = assertTypeIs(DoubleOps +
+    "type T = ToInt[1d]", "1")
+
+  def testDoubleToFloat(): Unit = assertTypeIs(DoubleOps +
+    "type T = ToFloat[1d]", "1.0f")
 
   //
   // String
