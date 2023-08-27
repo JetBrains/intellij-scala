d2f1030f2574266643f0a007dd8724a3b9d721a8
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-08-27 09:38:21 +0200

    Remove ProjectContext from SyntheticExpressionFactory

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/dfa/utils/SyntheticExpressionFactory.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/dfa/utils/SyntheticExpressionFactory.scala
@@ -1,23 +1,23 @@
 package org.jetbrains.plugins.scala.lang.dfa.utils
 
+import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaCode.ScalaCodeContext
-import org.jetbrains.plugins.scala.project.ProjectContext
 
 object SyntheticExpressionFactory {
 
-  def wrapInTupleExpression(tupleContents: Seq[ScExpression])(implicit context: ProjectContext): ScExpression = {
+  def wrapInTupleExpression(tupleContents: Seq[ScExpression])(implicit context: Project): ScExpression = {
     val tupleContentsInText = tupleContents.map(_.getText).mkString(", ")
     code"($tupleContentsInText)".asInstanceOf[ScExpression]
   }
 
-  def wrapInSplatListExpression(varargContents: Seq[ScExpression])(implicit context: ProjectContext): ScExpression = {
+  def wrapInSplatListExpression(varargContents: Seq[ScExpression])(implicit context: Project): ScExpression = {
     val tupleContentsInText = varargContents.map(_.getText).mkString(" :: ")
     if (varargContents.isEmpty) code"Nil: _*".asInstanceOf[ScExpression]
     else code"$tupleContentsInText :: Nil: _*".asInstanceOf[ScExpression]
   }
 
-  def createIntegerLiteralExpression(value: Int)(implicit context: ProjectContext): ScExpression = {
+  def createIntegerLiteralExpression(value: Int)(implicit context: Project): ScExpression = {
     code"$value".asInstanceOf[ScExpression]
   }
 }
