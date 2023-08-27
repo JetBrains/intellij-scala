3e321ec23b662e80b5d7ab9f6dda2c7c9ac1bc2b
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-08-27 12:17:23 +0200

    Remove ProjectContext from OtherMethodsSpecialSupport

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/dfa/analysis/invocations/specialSupport/OtherMethodsSpecialSupport.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/dfa/analysis/invocations/specialSupport/OtherMethodsSpecialSupport.scala
@@ -1,10 +1,10 @@
 package org.jetbrains.plugins.scala.lang.dfa.analysis.invocations.specialSupport
 
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiMethod
 import org.jetbrains.plugins.scala.lang.dfa.utils.ScalaDfaConstants.Packages._
 import org.jetbrains.plugins.scala.lang.psi.api.expr.MethodInvocation
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaCode.ScalaCodeContext
-import org.jetbrains.plugins.scala.project.ProjectContext
 
 object OtherMethodsSpecialSupport {
 
@@ -28,7 +28,7 @@ object OtherMethodsSpecialSupport {
     (s"$ScalaMath.$functionName", ScalaFloat) -> s"$JavaLangMath.$functionName(0.0F, 0.0F)"
   )
 
-  def psiMethodFromText(text: String)(implicit context: ProjectContext): Option[PsiMethod] = {
+  def psiMethodFromText(text: String)(implicit project: Project): Option[PsiMethod] = {
     code"$text".asInstanceOf[MethodInvocation].target.map(_.element) match {
       case Some(method: PsiMethod) => Some(method)
       case _ => None
