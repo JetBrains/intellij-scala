8fb8089b427a3fad79b7f0ee80c003fbcf62bace
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-08-27 11:59:15 +0200

    Remove ProjectContext from NestedStatefulMonadsInspection

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/monads/NestedStatefulMonadsInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/monads/NestedStatefulMonadsInspection.scala
@@ -1,13 +1,13 @@
 package org.jetbrains.plugins.scala.codeInspection.monads
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
+import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.Nls
 import org.jetbrains.plugins.scala.codeInspection.{PsiElementVisitorSimple, ScalaInspectionBundle, conformsToTypeFromClass}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScMethodCall
 import org.jetbrains.plugins.scala.lang.psi.types.ScType
 import org.jetbrains.plugins.scala.lang.psi.types.api.ParameterizedType
 import org.jetbrains.plugins.scala.lang.psi.types.result._
-import org.jetbrains.plugins.scala.project.ProjectContext
 
 final class NestedStatefulMonadsInspection extends LocalInspectionTool {
 
@@ -15,7 +15,7 @@ final class NestedStatefulMonadsInspection extends LocalInspectionTool {
 
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
     case call: ScMethodCall =>
-      import call.projectContext
+      implicit val project: Project = holder.getProject
       for {
         Typeable(genericType@ParameterizedType(_, arguments)) <- Some(call)
         if isStatefulMonadType(genericType) && arguments.exists(isStatefulMonadType)
@@ -31,6 +31,6 @@ object NestedStatefulMonadsInspection {
   private final val StatefulMonadsTypesNames = Set("scala.concurrent.Future", "scala.util.Try")
 
   private def isStatefulMonadType(scType: ScType)
-                                 (implicit context: ProjectContext): Boolean =
+                                 (implicit project: Project): Boolean =
     StatefulMonadsTypesNames.exists(conformsToTypeFromClass(scType, _))
 }
