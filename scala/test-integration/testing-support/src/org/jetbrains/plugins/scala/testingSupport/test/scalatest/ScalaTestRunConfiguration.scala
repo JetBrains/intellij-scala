023e1c5048a27e74a38efbbfa2986d938b5139b7
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-08-27 09:24:06 +0200

    Remove ProjectContext from ScalaTestRunConfiguration

--- scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestRunConfiguration.scala
+++ scala/test-integration/testing-support/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestRunConfiguration.scala
@@ -5,12 +5,11 @@ import com.intellij.execution.configurations._
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.ProjectScope
 import com.intellij.psi.{PsiClass, PsiModifier}
-import org.jetbrains.plugins.scala.extensions.{PsiElementExt, PsiMethodExt, PsiTypeExt}
+import org.jetbrains.plugins.scala.extensions.{PsiMethodExt, PsiTypeExt}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScModifierListOwner
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScTypeDefinition}
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiManager
 import org.jetbrains.plugins.scala.lang.psi.types.{ScParameterizedType, ScTypeExt, ScalaType}
-import org.jetbrains.plugins.scala.project.ProjectContext
 import org.jetbrains.plugins.scala.testingSupport.test.CustomTestRunnerBasedStateProvider.TestFrameworkRunnerInfo
 import org.jetbrains.plugins.scala.testingSupport.test._
 import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestRunConfiguration.DoNotDiscoverAnnotationFqn
@@ -60,7 +59,6 @@ object ScalaTestRunConfiguration {
   }
 
   private def lackConfigMapConstructor(clazz: PsiClass): Boolean = {
-    implicit val project: ProjectContext = clazz.projectContext
 
     val constructors = clazz match {
       case c: ScClass => c.secondaryConstructors.filter(_.isConstructor).toList ::: c.constructor.toList
@@ -74,10 +72,10 @@ object ScalaTestRunConfiguration {
             if (owner.hasModifierProperty(PsiModifier.PUBLIC)) {
               val params = con.parameters
               val firstParam = params.head
-              val psiManager = ScalaPsiManager.instance
-              val mapPsiClass = psiManager.getCachedClass(ProjectScope.getAllScope(project), "scala.collection.immutable.Map").orNull
+              val psiManager = ScalaPsiManager.instance(clazz.getProject)
+              val mapPsiClass = psiManager.getCachedClass(ProjectScope.getAllScope(clazz.getProject), "scala.collection.immutable.Map").orNull
               val mapClass = ScalaType.designator(mapPsiClass)
-              val paramClass = firstParam.getType.toScType()
+              val paramClass = firstParam.getType.toScType()(clazz.getProject)
               val conformanceType = paramClass match {
                 case parameterizedType: ScParameterizedType => parameterizedType.designator
                 case _ => paramClass
