c9840a3cd526d5158db9a709604ec09a4baf4430
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-08-28 22:14:12 +0200

    CommonQualifiedNames: inline "scala." prefix
    It's too much unnecessary constants which complete readability

--- scala/scala-impl/src/org/jetbrains/plugins/scala/util/CommonQualifiedNames.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/CommonQualifiedNames.scala
@@ -5,39 +5,36 @@ import com.intellij.psi.CommonClassNames.JAVA_LANG_OBJECT
 object CommonQualifiedNames {
 
   private final val Root = "_root_."
-  private final val Scala = "scala."
-
   final val JavaObjectFqn = JAVA_LANG_OBJECT
   final val JavaObjectCanonical = Root + JavaObjectFqn
 
-  final val ProductFqn = Scala + "Product"
+  final val ProductFqn = "scala.Product"
   final val ProductCanonical = Root + ProductFqn
 
-  final val SerializableFqn = Scala + "Serializable"
+  final val SerializableFqn = "scala.Serializable"
   final val SerializableCanonical = Root + SerializableFqn
 
-  final val AnyRefFqn = Scala + "AnyRef"
+  final val AnyRefFqn = "scala.AnyRef"
   final val AnyRefCanonical = Root + AnyRefFqn
 
-  final val BooleanFqn = Scala + "Boolean"
+  final val BooleanFqn = "scala.Boolean"
   final val BooleanCanonical = Root + BooleanFqn
 
-  final val SeqFqn = Scala + "Seq"
+  final val SeqFqn = "scala.Seq"
   final val SeqCanonical = Root + SeqFqn
 
-  final val AnyFqn = Scala + "Any"
+  final val AnyFqn = "scala.Any"
   final val AnyCanonical = Root + AnyFqn
 
-  final val NothingFqn = Scala + "Nothing"
+  final val NothingFqn = "scala.Nothing"
   final val NothingCanonical = Root + NothingFqn
 
-  final val OptionFqn = Scala + "Option"
+  final val OptionFqn = "scala.Option"
   final val OptionCanonical = Root + OptionFqn
 
-  final val FunctionFqn = Scala + "Function"
+  final val FunctionFqn = "scala.Function"
   final val FunctionCanonical = Root + FunctionFqn
 
-  final val StringContext = Scala + "StringContext"
+  final val StringContext = "scala.StringContext"
   final val StringContextCanonical = Root + StringContext
-
 }
