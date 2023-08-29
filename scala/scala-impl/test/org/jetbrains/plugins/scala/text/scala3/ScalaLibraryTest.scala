49a2bde339f2250746eeed8be7c0f7fc7239552a
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-08-29 13:54:38 +0200

    Text-to-text test: scala3-library-test, #SCL-21531

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/text/scala3/ScalaLibraryTest.scala
@@ -0,0 +1,13 @@
+package org.jetbrains.plugins.scala.text.scala3
+
+import org.jetbrains.plugins.scala.text.TextToTextTestBase
+
+class ScalaLibraryTest extends TextToTextTestBase(
+  Seq.empty,
+  Seq("scala"), Set.empty, 91,
+  Set(
+    "scala.Tuple", // Top-level type alias
+    "scala.runtime.stdLibPatches.Predef", // Inline parameter
+    "scala.util.boundary", // Inline parameter
+  )
+)
