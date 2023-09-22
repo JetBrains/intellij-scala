b2329622e72c38549bd225c7051c2edcf1e941a3
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-09-22 17:23:41 +0200

    Update Scala 3 test version to Scala 3.3.1 and fix test expectations

--- scala/scala-impl/test/org/jetbrains/plugins/scala/text/scala3/ScalaCompilerTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/text/scala3/ScalaCompilerTest.scala
@@ -6,7 +6,9 @@ class ScalaCompilerTest extends TextToTextTestBase(
   Seq.empty,
   Seq("dotty", "org.scalajs.ir", "scala.quoted.runtime.impl"), Set.empty, 919,
   Set(
-    "dotty.tools.backend.jvm.BCodeIdiomatic", // TODO No .type
+//    "dotty.tools.backend.jvm.BCodeIdiomatic", // TODO No .type // No longer an error since Scala 3.3.1
+    "dotty.tools.backend.jvm.BTypesFromSymbols", // New error since Scala 3.3.1
+    "dotty.tools.backend.jvm.CodeGen", // New error since Scala 3.3.1
     "dotty.tools.backend.sjs.ScopedVar", // Cannot resolve AssignmentStackElement (private class)
     "dotty.tools.dotc.ast.Trees", // Extra Nothing type argument in extends
     "dotty.tools.dotc.core.Definitions", // TODO Unknown type
@@ -17,7 +19,8 @@ class ScalaCompilerTest extends TextToTextTestBase(
     "dotty.tools.dotc.quoted.PickledQuotes", // TODO Enum cases in companion object
     "dotty.tools.dotc.semanticdb.Scala3", // Order in enum
     "dotty.tools.dotc.transform.LambdaLift", // TODO Unknown type
-    "dotty.tools.dotc.transform.init.Semantic", // TODO Semantic.Cache.TreeWrapper is Any
+//    "dotty.tools.dotc.transform.init.Semantic", // TODO Semantic.Cache.TreeWrapper is Any // No longer an error since Scala 3.3.1
+    "dotty.tools.dotc.transform.patmat.SpaceEngine", // New error since Scala 3.3.1
     "dotty.tools.dotc.transform.sjs.JSSymUtils", // TODO Enum cases in companion object
     "dotty.tools.dotc.typer.Applications", // Extra Nothing type argument in extends, different .this qualifier
     "dotty.tools.dotc.typer.OpenSearch", // TODO Parsing error
