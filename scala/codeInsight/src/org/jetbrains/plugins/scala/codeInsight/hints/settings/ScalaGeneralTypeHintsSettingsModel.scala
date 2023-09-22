32c7d931d3131b84925a3322fe650d2f625b4a29
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-09-22 17:07:44 +0200

    navigate instead of opening a new settings window in ScalaGeneralTypeHintsSettingsModel.navigateTo
     #SCL-21622

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hints/settings/ScalaGeneralTypeHintsSettingsModel.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hints/settings/ScalaGeneralTypeHintsSettingsModel.scala
@@ -1,15 +1,17 @@
 package org.jetbrains.plugins.scala.codeInsight.hints.settings
 
-import com.intellij.codeInsight.hints.settings.{InlayProviderSettingsModel, InlaySettingsConfigurableKt}
+import com.intellij.codeInsight.hints.settings.{InlayProviderSettingsModel, InlaySettingsConfigurable}
 import com.intellij.codeInsight.hints.{ImmediateConfigurable, InlayGroup}
+import com.intellij.ide.DataManager
 import com.intellij.lang.Language
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.options.ex.Settings
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.codeInsight.implicits.ImplicitHints
 import org.jetbrains.plugins.scala.codeInsight.{ScalaCodeInsightBundle, ScalaCodeInsightSettings}
-import org.jetbrains.plugins.scala.extensions.ObjectExt
+import org.jetbrains.plugins.scala.extensions.{NullSafe, ObjectExt}
 
 import java.util
 import javax.swing.JComponent
@@ -90,6 +92,17 @@ class ScalaGeneralTypeHintsSettingsModel extends InlayProviderSettingsModel(
 
 object ScalaGeneralTypeHintsSettingsModel {
   def navigateTo(project: Project): Unit = {
-    InlaySettingsConfigurableKt.showInlaySettings(project, ScalaLanguage.INSTANCE, _.is[ScalaGeneralTypeHintsSettingsModel])
+    val promise = DataManager.getInstance().getDataContextFromFocusAsync
+    promise.onSuccess { context =>
+      NullSafe(context)
+        .map(Settings.KEY.getData)
+        .foreach { settings =>
+          val configurable = settings.find("inlay.hints")
+          // Should not throw, but if it does, let exception analyzer know
+          val inlayConfigurable = configurable.asInstanceOf[InlaySettingsConfigurable]
+          inlayConfigurable.selectModel(ScalaLanguage.INSTANCE, _.is[ScalaGeneralTypeHintsSettingsModel])
+          settings.select(configurable)
+        }
+    }
   }
 }
