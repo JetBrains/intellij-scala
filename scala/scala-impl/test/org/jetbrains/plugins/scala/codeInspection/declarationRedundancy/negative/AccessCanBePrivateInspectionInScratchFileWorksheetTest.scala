a1c229ebe4405e9f5a172bf8e2f1d1b491df9932
Izmar Verhage <izmar.verhage@jetbrains.com>
2023-09-29 16:59:15 +0200

    Disable unused declaration inspection for top-level declarations #SCL-20638

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/negative/AccessCanBePrivateInspectionInScratchFileWorksheetTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/negative/AccessCanBePrivateInspectionInScratchFileWorksheetTest.scala
@@ -1,11 +1,17 @@
 package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.negative
 
+import com.intellij.openapi.fileTypes.{FileTypeRegistry, LanguageFileType}
 import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.ScalaAccessCanBePrivateInspectionTestBase
 
 class AccessCanBePrivateInspectionInScratchFileWorksheetTest extends ScalaAccessCanBePrivateInspectionTestBase {
 
   //scala scratch files will be automatically treated as worksheets
   override protected val isScratchFile: Boolean = true
+
+  override protected lazy val fileType: LanguageFileType =
+    FileTypeRegistry.getInstance().getFileTypeByExtension("sc").asInstanceOf[LanguageFileType]
+
+  def test_that_fails_in_order_to_prevent_merge(): Unit = throw new Exception
 
   def test_top_level_definition_and_member(): Unit =
     checkTextHasNoErrors(
