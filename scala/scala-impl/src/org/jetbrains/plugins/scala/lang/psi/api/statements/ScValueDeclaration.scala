d5dfa53c7d28af2a96dbb182dd3a8041045a9200
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2023-10-09 20:21:31 +0200

    Extract ScValueOrVariableDeclaration trait
    Similar to ScValueOrVariableDefinition

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScValueDeclaration.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/api/statements/ScValueDeclaration.scala
@@ -1,10 +1,3 @@
 package org.jetbrains.plugins.scala.lang.psi.api.statements
 
-import org.jetbrains.plugins.scala.lang.psi.api.base.ScIdList
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypedDefinition
-
-trait ScValueDeclaration extends ScValue with ScTypedDeclaration {
-  def getIdList: ScIdList
-  override def declaredElements : Seq[ScTypedDefinition]
-  override def isAbstract: Boolean = true
-}
+trait ScValueDeclaration extends ScValue with ScValueOrVariableDeclaration
