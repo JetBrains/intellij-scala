7387cf0bc8e794aa288ec7c255d45fc666f4684a
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-10-16 17:01:46 +0200

    [main methods] don't use `PsiMethodUtil.findMainMethod` inside `ScalaMainMethodUtil.findScala2MainMethod`
    `PsiMethodUtil.findMainMethod` calls implementations of `JavaMainMethodProvider`
    but `ScalaMainMethodUtil.findScala2MainMethod` itself can be called from our implementations of `JavaMainMethodProvider` (e.g. `org.jetbrains.plugins.scala.runner.ScalaMainMethodProvider`)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/util/ScalaMainMethodUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/util/ScalaMainMethodUtil.scala
@@ -1,12 +1,13 @@
 package org.jetbrains.plugins.scala.util
 
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.impl.PsiClassImplUtil
 import com.intellij.psi.util.PsiMethodUtil
 import org.jetbrains.plugins.scala.caches.{BlockModificationTracker, cachedInUserData}
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScAnnotation
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScMember, ScObject}
 import org.jetbrains.plugins.scala.lang.psi.light.PsiClassWrapper
 import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
 
@@ -17,20 +18,16 @@ object ScalaMainMethodUtil {
       isScala3MainMethod(funDef)
 
   def isScala2MainMethod(funDef: ScFunctionDefinition): Boolean = {
-    def isInTopLevelObject =
-      funDef.containingClass match {
-        case o: ScObject => o.isTopLevel
-        case _           => false
-      }
-
-    def hasJavaMainWrapper = {
-      funDef.getFunctionWrappers(isStatic = true, isAbstract = false)
-        .headOption
-        .exists(PsiMethodUtil.isMainMethod)
-    }
-
-    ScalaNamesUtil.toJavaName(funDef.name) == "main" && isInTopLevelObject && hasJavaMainWrapper
+    ScalaNamesUtil.toJavaName(funDef.name) == "main" &&
+      isInTopLevelObject(funDef) &&
+      PsiMethodUtil.isMainMethod(funDef)
   }
+
+  private def isInTopLevelObject(m: ScMember): Boolean =
+    m.containingClass match {
+      case o: ScObject => o.isTopLevel
+      case _ => false
+    }
 
   def isScala3MainMethod(funDef: ScFunctionDefinition): Boolean =
     if (funDef.isInScala3File) {
@@ -49,15 +46,15 @@ object ScalaMainMethodUtil {
   def hasScala2MainMethod(obj: ScObject): Boolean = findScala2MainMethod(obj).isDefined
 
   def findScala2MainMethod(obj: ScObject): Option[PsiMethod] = {
-    def declaredScala2Main(obj: ScObject): Option[ScFunctionDefinition] =
-      obj.functions.collectFirst {
-        case funDef: ScFunctionDefinition if isScala2MainMethod(funDef) => funDef
+    if (!obj.isTopLevel) None
+    else {
+      cachedInUserData("findScala2MainMethod", obj, BlockModificationTracker(obj)) {
+        //NOTE: PsiClassWrapper is used in order PsiMethodUtil.isMainMethod can detect main method from base class
+        // otherwise some of the conditions doesn't hold (AFAIR it can't check the presence of static modifier)
+        val objWrapper = new PsiClassWrapper(obj, obj.qualifiedName, obj.name)
+        val mainMethods = PsiClassImplUtil.findMethodsByName(objWrapper, "main", true)
+        mainMethods.find(PsiMethodUtil.isMainMethod)
       }
-
-    if (!obj.isTopLevel) None else cachedInUserData("findScala2MainMethod", obj, BlockModificationTracker(obj)) {
-      val declared = declaredScala2Main(obj)
-      val res = declared.orElse(Option(PsiMethodUtil.findMainMethod(new PsiClassWrapper(obj, obj.qualifiedName, obj.name))))
-      res
     }
   }
 }
