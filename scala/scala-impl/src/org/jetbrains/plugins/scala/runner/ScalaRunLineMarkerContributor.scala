dfe78b654fe8a84e12e6e843562b6ff168584d6f
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-10-16 17:16:04 +0200

    [main methods] don't do special check for ScObject in ScalaRunLineMarkerContributor
    instead rely on MyScalaMainMethodUtil.hasMainMethodFromProviders
    + adjust ScalaMainMethodProvider to recognise objects

--- scala/scala-impl/src/org/jetbrains/plugins/scala/runner/ScalaRunLineMarkerContributor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/runner/ScalaRunLineMarkerContributor.scala
@@ -7,7 +7,6 @@ import com.intellij.psi.{PsiClass, PsiElement}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
 import org.jetbrains.plugins.scala.runner.ScalaRunLineMarkerContributor.RunIcon
 import org.jetbrains.plugins.scala.util.ScalaMainMethodUtil
 
@@ -24,14 +23,17 @@ class ScalaRunLineMarkerContributor extends RunLineMarkerContributor {
 
     val isIdentifier = element.getNode.getElementType == ScalaTokenTypes.tIDENTIFIER
     val hasMain = element.getParent match {
-      case fun: ScFunctionDefinition                                       => ScalaMainMethodUtil.isMainMethod(fun)
-      case obj: ScObject if ScalaMainMethodUtil.hasScala2MainMethod(obj) => true
-      case c: PsiClass                                                     => MyScalaMainMethodUtil.hasMainMethodFromProviders(c)
-      case _ => false
+      case fun: ScFunctionDefinition =>
+        ScalaMainMethodUtil.isMainMethod(fun)
+      case c: PsiClass =>
+        MyScalaMainMethodUtil.hasMainMethodFromProviders(c)
+      case _ =>
+        false
     }
     if (isIdentifier && hasMain)
       new Info(RunIcon, null, ExecutorAction.getActions(0): _*)
-    else null
+    else
+      null
   }
 }
 
