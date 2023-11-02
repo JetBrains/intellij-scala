3754530177bb7bcdf48ab141584d84cb1d6bf7ec
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-11-02 09:59:27 +0100

    update intellijVersion to 241.950 (latest 2024.1 Nightly version)
    - fix compilation

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.9.5"
 
   // ATTENTION: check the comment in CommonUltimate when updating this version
-  val intellijVersion = "233.11555.6"
+  val intellijVersion = "241.950"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
