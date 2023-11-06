f3726d290bd063e0f9086f97c93575544040caae
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-11-06 12:22:07 +0100

    Update IDEA to version 241.1332

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.9.5"
 
   // ATTENTION: check the comment in CommonUltimate when updating this version
-  val intellijVersion = "241.950"
+  val intellijVersion = "241.1332"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
