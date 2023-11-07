59183626e7e1ac469fbf914e3f0b809b0cf1cd5e
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-11-07 17:20:22 +0100

    fixed scratch file cleanup after move to workspace model #investigation
    Scala2UnusedGlobalDeclarationInspectionTest and
    AccessCanBePrivateInspectionTest were failing because
    scratch files existed that interfere with these tests.
    The scratch files are created by other tests and should
    be cleaned up. The cleanup method, however, stopped
    working after Platform migrated scratch file managment
    to the new workspace model.

--- scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/package.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/package.scala
@@ -1,22 +1,46 @@
 package org.jetbrains.plugins.scala.codeInspection
 
 import com.intellij.ide.scratch.ScratchFileService
+import com.intellij.ide.scratch.workspace.ScratchRootsEntity
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.backend.workspace.{VirtualFileUrls, WorkspaceModel}
 import org.jetbrains.plugins.scala.extensions.inWriteAction
 import org.junit.Assert.assertFalse
 
-import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.jdk.CollectionConverters.{CollectionHasAsScala, IteratorHasAsScala}
 
 package object declarationRedundancy {
 
-  private[declarationRedundancy] def deleteAllGlobalScratchFiles(): Unit =
+  private[declarationRedundancy] def deleteAllGlobalScratchFiles(project: Project): Unit =
     inWriteAction {
       for {
-        scratchFileFolder <- ScratchFileService.getAllRootPaths.asScala
-        scratchFile <- scratchFileFolder.getChildren
+        root <- scratchRootsFromScratchFileService ++ scratchRootsFromWorkspaceModel(project)
+        scratchFile <- root.getChildren
       } {
         scratchFile.delete(null)
         scratchFile.delete(null)
         assertFalse(s"Can't delete scratch file: $scratchFile", scratchFile.exists())
       }
     }
+
+  // the old way
+  private def scratchRootsFromScratchFileService: Seq[VirtualFile] =
+    ScratchFileService.getAllRootPaths.asScala.toSeq
+
+  // the new way
+  private def scratchRootsFromWorkspaceModel(project: Project): Seq[VirtualFile] =
+    for {
+      scratchFileFolder <- scratchRootsEntities(project)
+      rootUrl <- scratchFileFolder.getRoots.asScala
+      root <- Option(VirtualFileUrls.getVirtualFile(rootUrl))
+    } yield root
+
+  private def scratchRootsEntities(project: Project): Seq[ScratchRootsEntity] =
+    WorkspaceModel
+      .getInstance(project)
+      .getCurrentSnapshot
+      .entities(classOf[ScratchRootsEntity])
+      .iterator()
+      .asScala.toSeq
 }
