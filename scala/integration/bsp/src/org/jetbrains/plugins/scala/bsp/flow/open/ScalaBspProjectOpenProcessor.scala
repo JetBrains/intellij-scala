4e23a454a7e7b004da6ca5af62823d4a2042b97f
Przemysław Fuchs <fuchs.przemyslaw@gmail.com>
2023-11-13 00:35:40 +0100

    BSP plugin initial integration

---
+++ scala/integration/bsp/src/org/jetbrains/plugins/scala/bsp/flow/open/ScalaBspProjectOpenProcessor.scala
@@ -0,0 +1,11 @@
+package org.jetbrains.plugins.scala.bsp.flow.open
+
+import org.jetbrains.plugins.bsp.flow.open.{BspProjectOpenProcessorExtension, BuildToolId}
+import org.jetbrains.plugins.scala.bsp.BspFeatureFlags
+
+class ScalaBspProjectOpenProcessor extends BspProjectOpenProcessorExtension {
+
+  override def getShouldBspProjectOpenProcessorBeAvailable: Boolean = BspFeatureFlags.isBspPluginIntegrationEnabled
+
+  override def getBuildToolId: BuildToolId = new BuildToolId("scalabsp")
+}
