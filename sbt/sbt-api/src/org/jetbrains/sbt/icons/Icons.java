000036f4e1ed99cf7cf748aee354eae454de5dd7
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-11-14 18:11:06 +0100

    fixed toolwindow icons #SCL-21532 fixed
    Mapping for toolwindow icons should reference a version without @20x20 because that should be automatically selected
    Unfortunately getIcon does not load the correct @20x20 versions, but findIcon does. When #IDEA-338105 is fixed we might want to switch back to getIcon.
    Also sbtShell@20x20.svg and sbtIcon@20x20.svg were completely different from their normal versions
    in that they have no color in them. So I separated them into two different icons.
    With all these changes, the toolwindow icon on the bottom left should now show
    the bulky @20x20 version, while the icons in the View > ToolWindows menu should show the normal sleek version. Both without color.

--- sbt/sbt-api/src/org/jetbrains/sbt/icons/Icons.java
+++ sbt/sbt-api/src/org/jetbrains/sbt/icons/Icons.java
@@ -2,6 +2,7 @@ package org.jetbrains.sbt.icons;
 
 import javax.swing.*;
 
+import static com.intellij.openapi.util.IconLoader.findIcon;
 import static com.intellij.openapi.util.IconLoader.getIcon;
 
 public interface Icons {
@@ -12,6 +13,6 @@ public interface Icons {
     Icon SBT_LOAD_CHANGES = getIcon("/org/jetbrains/sbt/images/sbtLoadChanges.svg", Icons.class);
 
     @SuppressWarnings("unused") // used from SBT.xml
-    Icon SBT_TOOL_WINDOW = getIcon("/org/jetbrains/sbt/images/sbtToolwin.svg", Icons.class);
-    Icon SBT_SHELL_TOOL_WINDOW = getIcon("/org/jetbrains/sbt/images/sbtShellToolwin.svg", Icons.class);
+    Icon SBT_TOOL_WINDOW = findIcon("/org/jetbrains/sbt/images/sbtToolwin.svg", Icons.class);
+    Icon SBT_SHELL_TOOL_WINDOW = findIcon("/org/jetbrains/sbt/images/sbtShellToolwin.svg", Icons.class);
 }
