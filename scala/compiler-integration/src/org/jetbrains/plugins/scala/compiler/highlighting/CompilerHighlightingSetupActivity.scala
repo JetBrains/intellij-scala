85ccee2855c5879a1684bbd6faf577c42b59038a
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-11-16 16:58:48 +0100

    Use a project activity for setting up compiler based highlighting in a project #SCL-21810
    - This replaces the usage of a deprecated method in ProjectManagerListener.

--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerHighlightingSetupActivity.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerHighlightingSetupActivity.scala
@@ -2,12 +2,13 @@ package org.jetbrains.plugins.scala.compiler.highlighting
 
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.ex.EditorEventMulticasterEx
-import com.intellij.openapi.project.{Project, ProjectManagerListener}
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiManager
 import org.jetbrains.plugins.scala.project.ProjectExt
+import org.jetbrains.plugins.scala.startup.ProjectActivity
 
-private final class TriggerCompilerHighlightingOnPsiChangeListener extends ProjectManagerListener {
-  override def projectOpened(project: Project): Unit = {
+private final class CompilerHighlightingSetupActivity extends ProjectActivity {
+  override def execute(project: Project): Unit = {
     val psiChangeListener = new CompilerHighlightingPsiChangeListener(project)
     PsiManager.getInstance(project).addPsiTreeChangeListener(psiChangeListener, project.unloadAwareDisposable)
     val focusChangeListener = new CompilerHighlightingFocusChangeListener()
