c3cfe8abbd34e938239709d575a22cbd8fd2a127
Pavel Fatin <pavel.fatin@jetbrains.com>
2023-11-21 11:41:37 +0100

    Editor X-Ray mode: don't show in Find Action, #SCL-16626

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hints/XRayModeAction.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/hints/XRayModeAction.scala
@@ -6,6 +6,10 @@ import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
  * A no-op action to collect statistics.
  */
 class XRayModeAction extends AnAction {
+  override def update(e: AnActionEvent): Unit = {
+    e.getPresentation.setVisible(false) // Don't show in Find Action
+  }
+
   override def actionPerformed(e: AnActionEvent): Unit = ()
 }
 
