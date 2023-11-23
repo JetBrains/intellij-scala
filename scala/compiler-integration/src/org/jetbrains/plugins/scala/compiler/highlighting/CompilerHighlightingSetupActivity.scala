07f7df74d3cd6bad2a3b964c3155cdac3a7921ac
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-11-23 18:46:40 +0100

    Migrate the compiler highlighting focus listener from the platform's FocusChangeListener to Swing's more generic FocusAdapter in combination with an EditorFactoryListener #SCL-21810
    - With a FocusChangeListener, there was an edge case where the focusLost event would not be fired for editors that are closed with the close button.
    - This meant that the document compiler might not be disabled for a given file, and it would then be immediately recompiled with the document compiler the next time it was opened, without triggering incremental compilation first.
    - With an EditorFactoryListener, we can intercept the event when the editor is closed, and disable the document compiler manually.

--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerHighlightingSetupActivity.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerHighlightingSetupActivity.scala
@@ -1,6 +1,5 @@
 package org.jetbrains.plugins.scala.compiler.highlighting
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiManager
 import org.jetbrains.plugins.scala.project.ProjectExt
@@ -10,6 +9,5 @@ private final class CompilerHighlightingSetupActivity extends ProjectActivity {
   override def execute(project: Project): Unit = {
     val psiChangeListener = new CompilerHighlightingPsiChangeListener(project)
     PsiManager.getInstance(project).addPsiTreeChangeListener(psiChangeListener, project.unloadAwareDisposable)
-    ApplicationManager.getApplication.getService(classOf[RegisterFocusChangeListenerApplicationService])
   }
 }
