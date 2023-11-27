01e58ac2dfe31c31f98891e2e1709be764f69949
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-11-27 16:59:47 +0100

    Use module scope when detecting Kotlin #SCL-21244
    - This fixes a freeze caused by the seemingly huge content scope.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/ConfigureIncrementalCompilerProjectActivity.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/ConfigureIncrementalCompilerProjectActivity.scala
@@ -1,8 +1,9 @@
 package org.jetbrains.plugins.scala.project
 
 import com.intellij.openapi.fileTypes.{FileTypeRegistry, UnknownFileType}
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
-import com.intellij.psi.search.{FileTypeIndex, ProjectScope}
+import com.intellij.psi.search.{FileTypeIndex, GlobalSearchScope}
 import org.jetbrains.plugins.scala.compiler.data.IncrementalityType
 import org.jetbrains.plugins.scala.project.settings.ScalaCompilerConfiguration
 import org.jetbrains.plugins.scala.startup.ProjectActivity
@@ -12,14 +13,16 @@ private final class ConfigureIncrementalCompilerProjectActivity extends ProjectA
   override def execute(project: Project): Unit = {
     project.subscribeToModuleRootChanged() { _ =>
       if (!project.isDisposed && project.hasScala) {
-        val projectContentScope = ProjectScope.getContentScope(project)
         val fileType = FileTypeRegistry.getInstance().getFileTypeByExtension("kt")
-
         fileType match {
           case UnknownFileType.INSTANCE =>
           // The Kotlin plugin is not enabled. Kotlin code cannot be compiled in this case, so it is ok to use Zinc.
           case kotlin =>
-            val hasKotlin = !FileTypeIndex.processFiles(kotlin, _ => false, projectContentScope)
+            val modules = ModuleManager.getInstance(project).getModules
+            val hasKotlin = modules.filterNot(_.hasBuildModuleType).exists { module =>
+              val moduleScope = GlobalSearchScope.moduleScope(module)
+              !FileTypeIndex.processFiles(kotlin, _ => false, moduleScope)
+            }
             if (hasKotlin) {
               ScalaCompilerConfiguration.instanceIn(project).incrementalityType = IncrementalityType.IDEA
             }
