fe87b9ea3a548cf730f759fa32450f5fcb8b74a9
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-11-28 10:28:26 +0100

    assert some more stuff in imported parser tests #SCL-21832

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/imported/Scala3ImportedParserTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/imported/Scala3ImportedParserTest.scala
@@ -44,11 +44,7 @@ private[imported] abstract class Scala3ImportedParserTestBase(dir: String) exten
     val hasErrorElements = errors.nonEmpty
 
     lazy val actualPsiTreeText = psiToString(lightFile, true).replace(": " + lightFile.name, "")
-    if (hasErrorElements != shouldHaveErrors) {
-      println(fileText)
-      println("-------")
-      println(actualPsiTreeText)
-    }
+
 
     val msg = s"Found following errors: " + errors.mkString(", ")
 
@@ -56,10 +52,13 @@ private[imported] abstract class Scala3ImportedParserTestBase(dir: String) exten
     lazy val interlaced = findInterlacedRanges(lightFile, testName)
 
     if (shouldHaveErrors) {
+      println(fileText)
+      println("-------")
+      println(actualPsiTreeText)
       assert(hasErrorElements || interlaced.nonEmpty, "Expected to find error elements or interlaced ranges, but found none.")
       ""
     } else {
-      assertFalse(msg, hasErrorElements)
+      //assertFalse(msg, hasErrorElements)
       assert(interlaced.isEmpty, "Following elements have conflicting ranges:\n" + {
         val maxStringLen = 50
         def clip(s: String): String = {
@@ -77,6 +76,13 @@ private[imported] abstract class Scala3ImportedParserTestBase(dir: String) exten
 
       actualPsiTreeText
     }
+  }
+
+  protected override def transformExpectedResult(text: String): String = {
+    if (!shouldHaveErrors) {
+      assertFalse(text.contains("PsiErrorElement"))
+    }
+    super.transformExpectedResult(text)
   }
 
   def isStringPart(e: PsiElement): Boolean =
