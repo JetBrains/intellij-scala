5ffbb255e92101a856bedef2a198d1c15de59a77
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-11-28 15:35:56 +0100

    [line commenter] respect "Add a space at line comment start" setting #SCL-21821 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/highlighter/ScalaCommenterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/highlighter/ScalaCommenterTest.scala
@@ -1,8 +1,11 @@
 package org.jetbrains.plugins.scala.highlighter
 
+import com.intellij.application.options.CodeStyle
 import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings
 import com.intellij.testFramework.EditorTestUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.jetbrains.plugins.scala.ScalaLanguage
 
 class ScalaCommenterTest extends BasePlatformTestCase {
 
@@ -12,6 +15,9 @@ class ScalaCommenterTest extends BasePlatformTestCase {
   private val ActionCommentLine = IdeActions.ACTION_COMMENT_LINE
   private val ActionCommentBlock = IdeActions.ACTION_COMMENT_BLOCK
 
+  protected def getCommonCodeStyleSettings: CommonCodeStyleSettings =
+    CodeStyle.getSettings(getProject).getCommonSettings(ScalaLanguage.INSTANCE)
+
   private def testAction(before: String, after: String, action: String): Unit = {
     myFixture.configureByText("Foo.scala", before)
     myFixture.performEditorAction(action)
@@ -19,15 +25,149 @@ class ScalaCommenterTest extends BasePlatformTestCase {
   }
 
   def test_scala_line_comment(): Unit = {
-    val before = s"${Caret}class Foo {}"
-    val after = "//class Foo {}"
-    testAction(before, after, ActionCommentLine)
+    testAction(
+      s"""class Foo {
+         |    ${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |//    println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
   }
 
   def test_scala_line_uncomment(): Unit = {
-    val before = s"$Caret//class Foo {}"
-    val after = "class Foo {}"
+    testAction(
+      s"""class Foo {
+         |//    ${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |    println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_comment_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+
+    val before =
+      s"""class Foo {
+         |    ${Caret}println()
+         |}""".stripMargin
+    val after =
+      s"""class Foo {
+         |    //println()
+         |}""".stripMargin
     testAction(before, after, ActionCommentLine)
+  }
+
+  def test_scala_line_uncomment_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+
+    testAction(
+      s"""class Foo {
+         |    //${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |    println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_comment_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED_LINE_COMMENT_ADD_SPACE_ENABLED(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+    getCommonCodeStyleSettings.LINE_COMMENT_ADD_SPACE = true
+
+    testAction(
+      s"""class Foo {
+         |    ${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |    // println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_comment_multiline_content_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED_LINE_COMMENT_ADD_SPACE_ENABLED(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+    getCommonCodeStyleSettings.LINE_COMMENT_ADD_SPACE = true
+
+    testAction(
+      s"""class Foo {
+         |    $Caret${SelectionStart}println()
+         |    println()
+         |    println()$SelectionEnd
+         |}""".stripMargin,
+      s"""class Foo {
+         |    // println()
+         |    // println()
+         |    // println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_uncomment_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED_LINE_COMMENT_ADD_SPACE_ENABLED(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+    getCommonCodeStyleSettings.LINE_COMMENT_ADD_SPACE = true
+
+    testAction(
+      s"""class Foo {
+         |    // ${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |    println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_uncomment_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED_LINE_COMMENT_ADD_SPACE_ENABLED_1(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+    getCommonCodeStyleSettings.LINE_COMMENT_ADD_SPACE = true
+
+    testAction(
+      s"""class Foo {
+         |//     ${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |    println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_uncomment_LINE_COMMENT_AT_FIRST_COLUMN_DISABLED_LINE_COMMENT_ADD_SPACE_ENABLED_2(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+    getCommonCodeStyleSettings.LINE_COMMENT_ADD_SPACE = true
+
+    testAction(
+      s"""class Foo {
+         |     //${Caret}println()
+         |}""".stripMargin,
+      s"""class Foo {
+         |     println()
+         |}""".stripMargin,
+      ActionCommentLine
+    )
+  }
+
+  def test_scala_line_uncomment_remove_spaces_on_empty_line(): Unit = {
+    getCommonCodeStyleSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
+    getCommonCodeStyleSettings.LINE_COMMENT_ADD_SPACE = true
+
+    testAction(
+      s"""class Foo {
+         |     //$Caret
+         |}""".stripMargin,
+      s"""class Foo {
+         |
+         |}""".stripMargin,
+      ActionCommentLine
+    )
   }
 
   def test_scala_single_line_block_comment(): Unit = {
