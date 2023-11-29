0d1f1a6d7de4aa46a890a04a19a13c9e0c9d63aa
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2023-11-29 13:01:23 +0100

    Update IDEA to version 241.4006 along with dependencyAnalyis and fix compilation
    - I was asked by the platform to check if the latest changes in 241 have any impact on our test performance.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.9.5"
 
   // ATTENTION: check the comment in CommonUltimate when updating this version
-  val intellijVersion = "241.3675"
+  val intellijVersion = "241.4006"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
