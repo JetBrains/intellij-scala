9139d615e82bcad5ce925b7e047129e93623f561
Zuzanna Surowiec <ts438730@students.mimuw.edu.pl>
2023-12-01 21:02:08 +0100

    add bsp integration icons (#6)
    * add bsp integration icons
    
    Signed-off-by: LaMavia <ts438730@students.mimuw.edu.pl>
    
    * Update ScalaAssetsExtension.scala
    
    change build tool id to "sbt"
    
    ---------
    
    Signed-off-by: LaMavia <ts438730@students.mimuw.edu.pl>

---
+++ scala/integration/bsp/src/org/jetbrains/plugins/scala/bsp/assets/ScalaAssetsExtension.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.plugins.scala.bsp.assets
+
+import com.intellij.openapi.util.IconLoader
+import org.jetbrains.plugins.bsp.assets.BuildToolAssetsExtension
+import org.jetbrains.plugins.bsp.flow.open.BuildToolId
+import org.jetbrains.plugins.scala.bsp.config.ScalaPluginConstants
+
+import javax.swing.Icon
+
+class ScalaAssetsExtension extends BuildToolAssetsExtension {
+  override def getIcon: Icon = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocol.svg", classOf[ScalaAssetsExtension])
+
+  override def getLoadedTargetIcon: Icon = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocolTarget.svg", classOf[ScalaAssetsExtension])
+
+  override def getInvalidTargetIcon: Icon = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocolTarget_red.svg", classOf[ScalaAssetsExtension])
+
+  override def getUnloadedTargetIcon: Icon = IconLoader.getIcon("/org/jetbrains/plugins/scala/bsp/images/buildServerProtocolTarget_grey.svg", classOf[ScalaAssetsExtension])
+
+  override def getPresentableName: String = "sbt"
+
+  override def getBuildToolId: BuildToolId = new BuildToolId("sbt")
+}
