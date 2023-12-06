c7e67d190e7c9cfeb96fa18f0965f5b27731a91c
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2023-12-07 00:41:29 +0100

    [annotator] do not highlight unqualified renaming imports with Scala 3 syntax #SCL-21900 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScImportExprAnnotator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScImportExprAnnotator.scala
@@ -2,14 +2,20 @@ package org.jetbrains.plugins.scala.annotator.element
 
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.annotator.ScalaAnnotationHolder
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.ScImportExpr
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.{ScImportExpr, ScImportSelectors}
 
 object ScImportExprAnnotator extends ElementAnnotator[ScImportExpr] {
 
   override def annotate(element: ScImportExpr, typeAware: Boolean)
                        (implicit holder: ScalaAnnotationHolder): Unit = {
-    if (element.qualifier.isEmpty) {
+    if (element.qualifier.isEmpty && !isScala3StyleAliasImport(element)) {
       holder.createErrorAnnotation(element, ScalaBundle.message("import.expr.should.be.qualified"))
     }
+  }
+
+  private[this] def isScala3StyleAliasImport(element: ScImportExpr): Boolean = element.selectorSet match {
+    case Some(ScImportSelectors(selector)) =>
+      selector.isScala3StyleAliasImport
+    case _ => false
   }
 }
