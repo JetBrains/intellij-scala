627104176d936db95716317917cbb54b34fd38a7
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2023-12-13 22:27:03 +0100

    Update intellijVersion to 233.13135.71, fix compilation

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.9.5"
 
   // ATTENTION: check the comment in CommonUltimate when updating this version
-  val intellijVersion = "233.11799.300"
+  val intellijVersion = "233.13135.71"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
