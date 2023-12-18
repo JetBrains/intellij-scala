60703cb5ed2b0da28db3e6c3ce341d00788dc3b7
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-12-18 12:41:01 +0100

    explicitly give scala version for CheckTestData tests #SCL-21939

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/typeInference/CheckScala3UnapplySeqTestDataTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/typeInference/CheckScala3UnapplySeqTestDataTest.scala
@@ -1,5 +1,5 @@
 package org.jetbrains.plugins.scala.lang.typeInference
 
-import org.jetbrains.plugins.scala.CheckTestDataTestBase
+import org.jetbrains.plugins.scala.{CheckTestDataTestBase, LatestScalaVersions}
 
-class CheckScala3UnapplySeqTestDataTest extends CheckTestDataTestBase(Scala3UnapplySeqTest)
+class CheckScala3UnapplySeqTestDataTest extends CheckTestDataTestBase(Scala3UnapplySeqTest, LatestScalaVersions.Scala_3)
--- scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/typeInference/CheckScala3UnapplyTestDataTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/typeInference/CheckScala3UnapplyTestDataTest.scala
@@ -1,5 +1,5 @@
 package org.jetbrains.plugins.scala.lang.typeInference
 
-import org.jetbrains.plugins.scala.CheckTestDataTestBase
+import org.jetbrains.plugins.scala.{CheckTestDataTestBase, LatestScalaVersions}
 
-class CheckScala3UnapplyTestDataTest extends CheckTestDataTestBase(Scala3UnapplyTest)
+class CheckScala3UnapplyTestDataTest extends CheckTestDataTestBase(Scala3UnapplyTest, LatestScalaVersions.Scala_3)
