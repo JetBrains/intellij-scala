348871909338e9de27c61f1ca4f11579a8bb9e1d
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-12-18 12:41:01 +0100

    explicitly give scala version for CheckTestData tests #SCL-21939

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/psi/CheckGenerateGivenNameTestDataTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/psi/CheckGenerateGivenNameTestDataTest.scala
@@ -1,6 +1,6 @@
 package org.jetbrains.plugins.scala.lang.psi
 
-import org.jetbrains.plugins.scala.CheckTestDataTestBase
+import org.jetbrains.plugins.scala.{CheckTestDataTestBase, LatestScalaVersions}
 
 // checks if the test data in GenerateGivenNameTest.allTests is correct
-class CheckGenerateGivenNameTestDataTest extends CheckTestDataTestBase(GenerateGivenNameTest)
+class CheckGenerateGivenNameTestDataTest extends CheckTestDataTestBase(GenerateGivenNameTest, LatestScalaVersions.Scala_3)
