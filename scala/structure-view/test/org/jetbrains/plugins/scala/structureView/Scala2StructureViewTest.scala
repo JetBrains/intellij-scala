4fb95f723309c4a3f8ea9b0f64a3e4f5d7b7fbab
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2023-12-21 21:30:42 +0100

    [structure view] improve navigation from source #SCL-21513

--- scala/structure-view/test/org/jetbrains/plugins/scala/structureView/Scala2StructureViewTest.scala
+++ scala/structure-view/test/org/jetbrains/plugins/scala/structureView/Scala2StructureViewTest.scala
@@ -66,6 +66,65 @@ class Scala2StructureViewTest extends ScalaStructureViewCommonTests {
       Node(CLASS, "B")
     )
 
+  def testNavigationFromSource(): Unit = checkNavigationFromSource(
+    s"""
+       |abstract class Exa${CARET}mple(
+       |  classParam1${CARET}UnusedInBody: String,
+       |  classParam2UsedI${CARET}nBody: String,
+       |  val clas${CARET}sParam3: String
+       |){
+       |  def th${CARET}is() = th${CARET}is(???, ???, ???)
+       |
+       |  val myV${CARET}al1 = ""
+       |  val (myVal2, myV${CARET}al3) = ???
+       |  protected lazy val myV${CARET}al4: Int
+       |
+       |  private var my${CARET}Var: Boolean = true
+       |
+       |  def myD${CARET}ef(par${CARET}am: String): String = classParam${CARET}2UsedInBody
+       |  def myAbstra${CARET}ctDef(s: String)(implicit b: Boolean): Unit
+       |
+       |  implicit class Str${CARET}Ext(private val ${CARET}s: String) extends AnyVal {
+       |    def myExten${CARET}sionMethod: String = ???
+       |  }
+       |
+       |  implicit def myImp${CARET}licitDef: String = ""
+       |  protected implicit def myAbstra${CARET}ctImplicitDef: Int
+       |
+       |  type MyTy${CARET}peAlias = String
+       |  type MyAbst${CARET}ractTypeAlias[T]
+       |
+       |  class My${CARET}Class
+       |  trait My${CARET}Trait
+       |  object MyO${CARET}bject
+       |}
+       |""".stripMargin,
+    Node(ABSTRACT_CLASS, "Example(String, String, String)"), // class Example
+    Node(ABSTRACT_CLASS, "Example(String, String, String)"), // class param classParam1UnusedInBody
+    Node(ABSTRACT_CLASS, "Example(String, String, String)"), // class param classParam2UsedInBody
+    Node(FIELD_VAL, "classParam3: String"), // class val param classParam3
+    Node(MethodIcon, "this()"), // def this()
+    Node(MethodIcon, "this()"), // def this() body
+    Node(FIELD_VAL, "myVal1"), // val myVal1
+    Node(FIELD_VAL, "myVal3"), // val myVal3
+    Node(ABSTRACT_FIELD_VAL, ProtectedIcon, "myVal4: Int"), // lazy val myVal4
+    Node(FIELD_VAR, PrivateIcon, "myVar: Boolean"), // var myVar
+    Node(MethodIcon, "myDef(String): String"), // def myDef
+    Node(MethodIcon, "myDef(String): String"), // myDef method param `param`
+    Node(MethodIcon, "myDef(String): String"), // myDef method body
+    Node(AbstractMethodIcon, "myAbstractDef(String)(?=> Boolean): Unit"), // def myAbstractDef
+    Node(CLASS, "StrExt(String)"), // implicit class StrExt
+    Node(FIELD_VAL, PrivateIcon, "s: String"), // implicit class val param s
+    Node(MethodIcon, "myExtensionMethod: String"), // def myExtensionMethod
+    Node(MethodIcon, "myImplicitDef: String"), // implicit def myImplicitDef
+    Node(AbstractMethodIcon, ProtectedIcon, "myAbstractImplicitDef: Int"), // protected implicit def myAbstractImplicitDef
+    Node(TYPE_ALIAS, "MyTypeAlias"), // type MyTypeAlias
+    Node(ABSTRACT_TYPE_ALIAS, "MyAbstractTypeAlias"), // type MyAbstractTypeAlias
+    Node(CLASS, "MyClass"), // class MyClass
+    Node(TRAIT, "MyTrait"), // class MyTrait
+    Node(OBJECT, "MyObject"), // class MyObject
+  )
+
   // TODO:
   //  We could parse top level definitions in Scala, even though it's not compilable (we already do so in root package, for "script" files)
   //  We could show the error later in annotator.
