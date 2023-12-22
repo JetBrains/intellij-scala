60cbd01b9732d83e04f63dd570480ff3124c190d
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2023-12-22 02:25:02 +0100

    fixed enter after => in colon-arg on same line as call #SCL-21886 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/enter/scala3/Scala3TestDataBracelessCode.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/actions/editor/enter/scala3/Scala3TestDataBracelessCode.scala
@@ -287,7 +287,8 @@ object Scala3TestDataBracelessCode {
       s"""val test = (param: Int) ?=> $Caret""",
 
       // argument blocks
-      s"""val test = x.foreach:$Caret""",
+      s"""val test = x.foreach: $Caret""",
+      s"""val test = x.foreach: a => $Caret""",
 
       /** case clause body
        * (test data for typing in single clause body
