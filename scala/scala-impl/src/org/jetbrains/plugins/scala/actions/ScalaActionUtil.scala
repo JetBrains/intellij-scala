e204bbf55595056e4b194d292f809cc6a2467805
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-08 18:26:59 +0100

    remove deprecated ScalaActionUtil.enablePresentation/disablePresentation
    The only plugin using the methods was "A+ Courses"
    I contacted them to not use it:
    https://github.com/Aalto-LeTech/aplus-courses/issues/1083

--- scala/scala-impl/src/org/jetbrains/plugins/scala/actions/ScalaActionUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/actions/ScalaActionUtil.scala
@@ -16,12 +16,6 @@ import java.awt.event.{MouseEvent, MouseMotionAdapter}
 
 object ScalaActionUtil {
 
-  @deprecated("Use `event.getPresentation.setEnabledAndVisible(true)` instead")
-  @inline def enablePresentation(e: AnActionEvent): Unit = e.getPresentation.setEnabledAndVisible(true)
-
-  @deprecated("Use `event.getPresentation.setEnabledAndVisible(false)` instead")
-  @inline def disablePresentation(e: AnActionEvent): Unit = e.getPresentation.setEnabledAndVisible(false)
-
   def enableAndShowIfInScalaFile(e: AnActionEvent): Unit = {
     val isInScalaFile = getFileFrom(e).exists(_.is[ScalaFile])
     e.getPresentation.setEnabledAndVisible(isInScalaFile)
