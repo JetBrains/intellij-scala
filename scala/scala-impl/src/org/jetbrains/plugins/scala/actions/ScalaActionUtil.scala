be5ed4ff0457fb96e6cf3743930d228098e1c763
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-08 18:17:04 +0100

    remove try/catch block from ScalaActionUtil.enableAndShowIfInScalaFile
    No exceptions are expected there.
    It's some legacy rudiment

--- scala/scala-impl/src/org/jetbrains/plugins/scala/actions/ScalaActionUtil.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/actions/ScalaActionUtil.scala
@@ -22,14 +22,9 @@ object ScalaActionUtil {
   @deprecated("Use `event.getPresentation.setEnabledAndVisible(false)` instead")
   @inline def disablePresentation(e: AnActionEvent): Unit = e.getPresentation.setEnabledAndVisible(false)
 
-  def enableAndShowIfInScalaFile(e: AnActionEvent): Unit = try {
+  def enableAndShowIfInScalaFile(e: AnActionEvent): Unit = {
     val isInScalaFile = getFileFrom(e).exists(_.is[ScalaFile])
     e.getPresentation.setEnabledAndVisible(isInScalaFile)
-  } catch {
-    case c: ControlFlowException =>
-      throw c
-    case _: Exception =>
-      e.getPresentation.setEnabledAndVisible(false)
   }
 
   def getFileFrom(e: AnActionEvent): Option[PsiFile] = Option(CommonDataKeys.PSI_FILE.getData(e.getDataContext))
