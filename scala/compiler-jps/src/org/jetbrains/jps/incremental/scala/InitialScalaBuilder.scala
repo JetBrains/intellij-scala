a55bdd0fc9a1f7c1e319bfcec5544cf836d28399
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-01-11 16:14:24 +0100

    Revisit incremental compiler setting saving to disk and forcing a recompilation on setting change #SCL-21961
    - When I originally implemented #SCL-21246, I had not completely understood the decisions behind the implementation of InitialScalaBuilder. With my changes, I introduced both behavior changes compared to the original implementation and a slight performance regression by forcing reading from disk for every build module in a project.
    - After carefully reviewing the changes and original implementation, I was able to restore the behavior to the original one, added build logs and addressed the performance regression. This will definitely go towards improving #SCL-21982.

--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/InitialScalaBuilder.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/InitialScalaBuilder.scala
@@ -5,8 +5,8 @@ import com.intellij.openapi.util.Key
 import org.jetbrains.jps.ModuleChunk
 import org.jetbrains.jps.builders.DirtyFilesHolder
 import org.jetbrains.jps.builders.java.{JavaBuilderUtil, JavaSourceRootDescriptor}
+import org.jetbrains.jps.builders.storage.BuildDataCorruptedException
 import org.jetbrains.jps.incremental._
-import org.jetbrains.jps.incremental.messages.{BuildMessage, CompilerMessage}
 import org.jetbrains.jps.model.module.JpsModule
 import org.jetbrains.plugins.scala.compiler.data.IncrementalityType
 
@@ -26,75 +26,106 @@ class InitialScalaBuilder extends ModuleLevelBuilder(BuilderCategory.SOURCE_INST
   override def getPresentableName: String = JpsBundle.message("scala.compiler.metadata.builder")
 
   override def buildStarted(context: CompileContext): Unit = {
-    collectAndStoreScalaModules(context)
-  }
+    val scalaModules = collectAndStoreScalaModules(context)
+    if (scalaModules.nonEmpty) {
+      val previousIncrementalityType = readIncrementalityType(context)
+      val incrementalityType = ScalaBuilder.projectSettings(context).getIncrementalityType
 
-  override def buildFinished(context: CompileContext): Unit = {
-    val previousIncrementalityType = readPreviousIncrementalityType(context)
-    val incrementalityType = ScalaBuilder.projectSettings(context).getIncrementalityType
-
-    previousIncrementalityType match {
-      case Some(`incrementalityType`) =>
-      case _ =>
-        incrementalityTypeStorageFile(context).foreach { file =>
-          val parentDir = file.getParentFile
-          if (!parentDir.exists()) parentDir.mkdirs()
-          try Files.write(file.toPath, incrementalityType.name().getBytes(StandardCharsets.UTF_8))
-          catch {
-            case e: IOException =>
-              Log.error(s"Could not write incremental compiler setting $incrementalityType to disk", e)
+      previousIncrementalityType match {
+        case _ if JavaBuilderUtil.isForcedRecompilationAllJavaModules(context) =>
+          // Forced rebuild, save current incremental compiler setting to disk and continue.
+          // This case is entered after the rebuild requested exception is thrown later in the file.
+          Log.info("Forced project rebuild initiated, saving incremental compiler setting to disk and continuing")
+          writeIncrementalityType(context, incrementalityType)
+        case None =>
+          Log.warn("Previous incremental compiler setting was not read from disk, continuing with the current incremental compiler project setting, compilation errors are possible")
+        case Some(`incrementalityType`) =>
+          Log.info("Previous incremental compiler setting matches the current incremental compiler project setting, continuing")
+        case Some(_) if isMakeProject(context) =>
+          // All build targets are affected, full rebuild, save current incremental compiler setting to disk and continue
+          Log.info("Full project rebuild, saving incremental compiler setting to disk and continuing")
+          writeIncrementalityType(context, incrementalityType)
+        case Some(_) =>
+          // Not a full rebuild, and incremental compiler setting has been changed since the last build, forcing a rebuild
+          Log.info("Previous incremental compiler setting does not match current project setting and the build is not a full rebuild, forcing a project rebuild")
+          val isCBH = Option(context.getBuilderParameter(BuildParameters.BuildTriggeredByCBH)).flatMap(_.toBooleanOption).getOrElse(false)
+          if (isCBH) {
+            // Compiler based highlighting specific workaround. Because of the way we create the compilation scopes in
+            // CBH to be as minimal as possible, JPS does not propagate the rebuild requested exception that we throw
+            // in this case. This might be a bug, I plan to speak to the JPS team about this.
+            writeIncrementalityType(context, incrementalityType)
           }
-        }
+          val message = JpsBundle.message("incremental.compiler.changed.rebuild")
+          throw new RebuildRequestedException(new BuildDataCorruptedException(message))
+      }
     }
   }
 
   override def build(context: CompileContext,
                      chunk: ModuleChunk,
                      dirtyFilesHolder: DirtyFilesHolder[JavaSourceRootDescriptor, ModuleBuildTarget],
-                     outputConsumer: ModuleLevelBuilder.OutputConsumer): ModuleLevelBuilder.ExitCode = {
-
-    val previousIncrementalityType = readPreviousIncrementalityType(context)
-    previousIncrementalityType match {
-      case Some(incrementality) =>
-        Log.info(s"Previous incremental compiler setting read from disk: $incrementality [${chunk.getPresentableShortName}]")
-      case None =>
-        Log.info(s"Could not read previous incremental compiler setting from disk [${chunk.getPresentableShortName}]")
-    }
-    val incrementalityType = ScalaBuilder.projectSettings(context).getIncrementalityType
-    Log.info(s"Current project incremental compiler setting: $incrementalityType [${chunk.getPresentableShortName}]")
-
-    previousIncrementalityType match {
-      case _ if JavaBuilderUtil.isForcedRecompilationAllJavaModules(context) =>
-        ModuleLevelBuilder.ExitCode.NOTHING_DONE
-      case Some(`incrementalityType`) =>
-        ModuleLevelBuilder.ExitCode.NOTHING_DONE
-      case _ =>
-        val message = JpsBundle.message("incremental.compiler.changed.rebuild")
-        context.processMessage(new CompilerMessage("scala", BuildMessage.Kind.JPS_INFO, message))
-        ModuleLevelBuilder.ExitCode.CHUNK_REBUILD_REQUIRED
-    }
-  }
+                     outputConsumer: ModuleLevelBuilder.OutputConsumer): ModuleLevelBuilder.ExitCode =
+    ModuleLevelBuilder.ExitCode.NOTHING_DONE
 
   override def getCompilableFileExtensions: ju.List[String] = ju.Arrays.asList("scala", "java")
 
-  private def incrementalityTypeStorageFile(context: CompileContext): Option[File] =
-    Option(context.getProjectDescriptor.dataManager.getDataPaths.getDataStorageRoot)
-      .map(new File(_, "incrementalType.dat"))
+  private def incrementalityTypeStorageFile(context: CompileContext): File = {
+    val dataStorageRoot = context.getProjectDescriptor.dataManager.getDataPaths.getDataStorageRoot
+    new File(dataStorageRoot, "incrementalType.dat")
+  }
 
-  private def readPreviousIncrementalityType(context: CompileContext): Option[IncrementalityType] =
-    incrementalityTypeStorageFile(context).filter(_.exists()).flatMap { file =>
-      val result = try {
-        val bytes = Files.readAllBytes(file.toPath)
-        val str = new String(bytes, StandardCharsets.UTF_8)
-        Some(IncrementalityType.valueOf(str))
-      } catch {
-        case e: IOException =>
-          Log.error("Could not read incremental compiler settings from disk", e)
-          None
-      }
-      if (result.isEmpty) file.delete()
-      result
+  private def readIncrementalityType(context: CompileContext): Option[IncrementalityType] = {
+    val storageFile = incrementalityTypeStorageFile(context)
+    if (!storageFile.exists()) {
+      // This is not necessarily an error, the storage file will be created.
+      val projectName = context.getProjectDescriptor.getProject.getName
+      Log.info(s"Incremental compiler project setting storage file does not exist for project $projectName, path: ${storageFile.toPath}")
+      return None
     }
+
+    val result = try {
+      val bytes = Files.readAllBytes(storageFile.toPath)
+      val str = new String(bytes, StandardCharsets.UTF_8)
+      val incrementality = IncrementalityType.valueOf(str)
+      Some(incrementality)
+    } catch {
+      case e: IOException =>
+        Log.error("Could not read incremental compiler settings from disk", e)
+        None
+      case e: IllegalArgumentException =>
+        Log.error("Unknown incrementality type string", e)
+        None
+    }
+
+    if (result.isEmpty) storageFile.delete()
+    result
+  }
+
+  private def writeIncrementalityType(context: CompileContext, incrementalityType: IncrementalityType): Unit = {
+    val storageFile = incrementalityTypeStorageFile(context)
+    val parentDir = storageFile.getParentFile
+    if (!parentDir.exists()) parentDir.mkdirs()
+    val storagePath = storageFile.toPath
+    try {
+      Files.write(storagePath, incrementalityType.name().getBytes(StandardCharsets.UTF_8))
+      Log.info(s"Wrote incremental compiler setting $incrementalityType to disk, path: $storagePath")
+    }
+    catch {
+      case e: IOException =>
+        Log.error(s"Could not write incremental compiler setting $incrementalityType to disk, path: $storagePath", e)
+    }
+  }
+
+  private def isMakeProject(context: CompileContext): Boolean = {
+    def allTargetsAffected: Boolean =
+      context.getProjectDescriptor.getBuildTargetIndex.getSortedTargetChunks(context).asScala.forall { chunk =>
+        chunk.getTargets.asScala.forall { target =>
+          context.getScope.isAffected(target)
+        }
+      }
+
+    JavaBuilderUtil.isCompileJavaIncrementally(context) && allTargetsAffected
+  }
 }
 
 object InitialScalaBuilder {
@@ -117,11 +148,12 @@ object InitialScalaBuilder {
     context.putUserData(scalaModulesKey, scalaModules)
   }
 
-  private def collectAndStoreScalaModules(context: CompileContext): Unit = {
+  private def collectAndStoreScalaModules(context: CompileContext): Set[JpsModule] = {
     val result = context.getProjectDescriptor.getProject.getModules.asScala
       .filter(SettingsManager.getScalaSdk(_).isDefined)
       .toSet
 
     storeScalaModules(context, result)
+    result
   }
 }
