d8db24986278cb3eb981ed2e6c4a631ec6fd3514
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-01-17 18:07:56 +0100

    [lexer] fixed discontinuous sequence of tokens when newline is escaped #SCL-22003 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/highlighter/lexer/ScalaMultilineStringLiteralLexer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/highlighter/lexer/ScalaMultilineStringLiteralLexer.scala
@@ -4,8 +4,8 @@ import com.intellij.psi.tree.IElementType
 
 class ScalaMultilineStringLiteralLexer(
   quoteChar: Char,
-  originalLiteralToken: IElementType
-) extends ScalaStringLiteralRawAwareLexer(quoteChar, originalLiteralToken) {
+  originalLiteralToken: IElementType,
+) extends ScalaStringLiteralRawAwareLexer(quoteChar, originalLiteralToken, canEscapeEolOrFramingSpaces = true) {
 
   /** Only unicode escape sequences are not supported in NON-interpolated multiline strings */
   override def getTokenType: IElementType =
