59dead7bed8176f10805c379ecd42750abdf9d17
Leon Burdinov <leonidb@wix.com>
2024-01-21 16:20:27 +0200

    [refactoring] anonymous class to inner class #SCL-227

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/move/anonymousToInner/ScalaAnonymousToInnerHandler.scala
@@ -0,0 +1,149 @@
+package org.jetbrains.plugins.scala.lang.refactoring.move.anonymousToInner
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.psi.PsiElement
+import com.intellij.util.IncorrectOperationException
+import org.jetbrains.annotations.VisibleForTesting
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScNewTemplateDefinition, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameter
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypedDefinition
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScExtendsBlock
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScEnum, ScObject, ScTemplateDefinition, ScTrait}
+import org.jetbrains.plugins.scala.lang.psi.api.{ScFile, ScalaRecursiveElementVisitor}
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.createElementFromText
+import org.jetbrains.plugins.scala.lang.refactoring.extractMethod.ScalaVariableData
+import org.jetbrains.plugins.scala.project.ProjectContext
+
+/**
+ * @note original implementation was inspired by [[com.intellij.refactoring.anonymousToInner.AnonymousToInnerHandler]]
+ */
+object ScalaAnonymousToInnerHandler {
+  private val LOG = Logger.getInstance(ScalaAnonymousToInnerHandler.getClass)
+
+  def invoke(project: Project, element: ScNewTemplateDefinition): Unit = {
+    val extendsBlock = element.extendsBlock
+    val (variables, targetContainer) = parseInitialExtendsBlock(extendsBlock)
+
+    for {
+      DialogResult(className, renamedVariables) <- showRefactoringDialog(project, extendsBlock, variables, targetContainer)
+    } yield performRefactoring(project, className, renamedVariables, extendsBlock, element, targetContainer)
+  }
+
+  @VisibleForTesting
+  def parseInitialExtendsBlock(extendsBlock: ScExtendsBlock): (Array[ScalaVariableData], Either[ScFile, ScTemplateDefinition]) = {
+    val usedVariables = collectUsedVariables(extendsBlock)
+    val targetContainer = findTargetContainer(extendsBlock)
+    (usedVariables, targetContainer)
+  }
+
+  def performRefactoring(project: Project, className: String, variables: Array[ScalaVariableData], anonClass: ScExtendsBlock, originalElement: ScNewTemplateDefinition, targetContainer: Either[ScFile, ScTemplateDefinition]): Unit =
+    CommandProcessor.getInstance.executeCommand(project, () => {
+      val action: Runnable = () => {
+        try {
+          val innerClassNewTemplate = newTemplateForInnerClass(project, className, variables, originalElement)
+          val newClassFromAnonymous = createClass(className, anonClass, variables, project)
+          targetContainer.fold(file => file.add(newClassFromAnonymous), classOrObject => classOrObject.addMember(newClassFromAnonymous, None))
+          originalElement.replace(innerClassNewTemplate)
+        }
+        catch {
+          case e: IncorrectOperationException =>
+            LOG.error(e)
+        }
+
+      }
+      ApplicationManager.getApplication.runWriteAction(action)
+
+    }, getRefactoringName, null)
+
+  private def newTemplateForInnerClass(project: Project, name: String, variables: Array[ScalaVariableData], newTemplate: ScNewTemplateDefinition) = {
+    implicit val projectContext: ProjectContext = new ProjectContext(project)
+
+    val args = variables.map(_.variable.getName).mkString(", ")
+    val text = s"""new $name($args)""".stripMargin
+
+    createElementFromText[ScNewTemplateDefinition](text, newTemplate)
+  }
+
+  private def collectUsedVariables(anonClass: ScExtendsBlock): Array[ScalaVariableData] = {
+    var res: List[ScTypedDefinition] = Nil
+    anonClass.accept(new ScalaRecursiveElementVisitor() {
+      override def visitReferenceExpression(expression: ScReferenceExpression) = {
+        val refElement = expression.resolve
+        refElement match {
+          case p: ScParameter =>
+            res = p :: res
+          case r: ScReferencePattern =>
+            res = r :: res
+          case _ =>
+        }
+        super.visitReferenceExpression(expression)
+      }
+    })
+    res.reverse.distinct.map(parameter =>
+      new ScalaVariableData(parameter, true, parameter.`type`().getOrNothing)
+    ).toArray
+
+  }
+
+  private case class DialogResult(className: String, variables: Array[ScalaVariableData])
+
+  private def showRefactoringDialog(project: Project, extendsBlock: ScExtendsBlock, usedVariables: Array[ScalaVariableData], target: Either[ScFile, ScTemplateDefinition]) = {
+    val dialog = new ScalaAnonymousToInnerDialog(project, extendsBlock, usedVariables, target)
+    if (dialog.showAndGet())
+      Some(DialogResult(dialog.getClassName, dialog.getVariables))
+    else
+      None
+  }
+
+  private def createClass(name: String, anonClass: ScExtendsBlock, variables: Array[ScalaVariableData], project: Project): ScClass = {
+    implicit val projectContext: ProjectContext = new ProjectContext(project)
+
+    val parameters = for {
+      v <- variables
+      scTypedDeinition = v.element
+      name = v.name
+      isVar = if (scTypedDeinition.isVar) "var " else ""
+      typeText = v.`type`.getPresentableText
+    } yield s"$isVar$name: $typeText"
+
+    val text = s"class $name(${parameters.mkString(", ")}) extends ${anonClass.getText}"
+    val newClass = createElementFromText[ScClass](text, anonClass)
+
+    val parameterOldNameToNewName = variables.map(v => v.variable.getName -> v.name).toMap
+    newClass.accept(new ScalaRecursiveElementVisitor() {
+      override def visitReferenceExpression(expression: ScReferenceExpression) = {
+        val refElement = expression.resolve
+        refElement match {
+          case p: ScParameter =>
+            expression.handleElementRename(parameterOldNameToNewName(p.getName))
+          case r: ScReferencePattern =>
+            expression.handleElementRename(parameterOldNameToNewName(r.getName))
+          case _ =>
+        }
+        super.visitReferenceExpression(expression)
+      }
+    })
+
+    newClass
+  }
+
+  private def findTargetContainer(elem: PsiElement): Either[ScFile, ScTemplateDefinition] = {
+    val container = elem.parents.collectFirst {
+      case obj: ScObject => Right(obj)
+      case enm: ScEnum => Right(enm)
+      case clazz: ScClass => Right(clazz)
+      case trt: ScTrait => Right(trt)
+      case file: ScFile => Left(file) //Scala3 top level definition
+    }
+    container.get //assuming that there is at least "ScFile" parent, so it's safe to call "get"
+  }
+
+  @NlsContexts.DialogTitle def getRefactoringName = ScalaBundle.message("move.anonymousToInner.name")
+}
---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/refactoring/move/anonymousToInner/ScalaMoveAnonymousToInnerDelegate.scala
@@ -0,0 +1,36 @@
+package org.jetbrains.plugins.scala.lang.refactoring.move.anonymousToInner
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.{PsiElement, PsiReference}
+import com.intellij.refactoring.move.MoveHandlerDelegate
+import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScNewTemplateDefinition
+
+/**
+ * @note original implementation was inspired by [[com.intellij.refactoring.anonymousToInner.MoveAnonymousToInnerHandler]]
+ */
+class ScalaMoveAnonymousToInnerDelegate extends MoveHandlerDelegate {
+
+  override def getActionName(elements: Array[PsiElement]): String = ScalaBundle.message("move.anonymousToInner.name")
+
+  override def canMove(elements: Array[PsiElement], targetContainer: PsiElement, reference: PsiReference): Boolean =
+    findNewTemplateDefinition(elements(0), reference).exists(_.isAnonimous)
+
+  private def findNewTemplateDefinition(element: PsiElement, reference: PsiReference): Option[ScNewTemplateDefinition] = {
+    val maybeElement = Option(reference).map(_.getElement).orElse(Option(element))
+    maybeElement.flatMap(element => Option(PsiTreeUtil.getParentOfType(element, classOf[ScNewTemplateDefinition])))
+  }
+
+  override def tryToMove(element: PsiElement, project: Project, dataContext: DataContext, reference: PsiReference, editor: Editor): Boolean = {
+      val canRefactor = canMove(Array(element), null, reference)
+      if (canRefactor) {
+        val maybeNewTemplateDefinition = findNewTemplateDefinition(element, reference)
+        maybeNewTemplateDefinition.foreach(element => ScalaAnonymousToInnerHandler.invoke(project, element))
+        maybeNewTemplateDefinition.isDefined
+      } else
+        false
+    }
+}
