d077dd7bebeecdac9dcabb0b736cd1d0370a32c8
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2024-01-22 18:48:06 +0100

    [scaladoc] Preserve order of modifiers #SCL-22017 Fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/editor/documentationProvider/renderers/WithHtmlPsiLink.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/editor/documentationProvider/renderers/WithHtmlPsiLink.scala
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.scala.editor.documentationProvider.renderers
 import com.intellij.psi.{PsiClass, PsiModifier, PsiPackage}
 import org.jetbrains.plugins.scala.editor.documentationProvider.{HtmlPsiUtils, _}
 import org.jetbrains.plugins.scala.extensions.{PsiClassExt, ResolvesTo}
+import org.jetbrains.plugins.scala.lang.lexer.ScalaModifier
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScAccessModifier
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScModifierListOwner
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScGiven
@@ -11,6 +12,8 @@ import org.jetbrains.plugins.scala.lang.psi.types.api.presentation.AccessModifie
 import org.jetbrains.plugins.scala.lang.psi.types.api.presentation.{AccessModifierRenderer, ModifiersRenderer}
 
 private [documentationProvider] object WithHtmlPsiLink extends AccessQualifierRenderer {
+  import ScalaModifier._
+
   private val accessModifierRenderer = new AccessModifierRenderer(WithHtmlPsiLink) {
     override def render(modifier: ScAccessModifier): String = {
       val buffer = new StringBuilder
@@ -38,12 +41,29 @@ private [documentationProvider] object WithHtmlPsiLink extends AccessQualifierRe
         case _                          => false
       }
 
-      ModifiersRenderer
-        .modifiers
-        .filter(mod => modifierList.hasModifierProperty(mod) && !(mod.contentEquals("implicit") && isGivenElement))
-        .foreach { modifier => buffer.appendKeyword(modifier).append(' ') }
+      modifierList
+        .modifiersOrdered
+        .filterNot(mod => mod == Private || mod == Protected || (mod == Implicit && isGivenElement))
+        .foreach(mod => buffer.appendKeyword(modifiersToNames(mod)).append(' '))
     }
   }
+
+  private val modifiersToNames: Map[ScalaModifier, String] = Map(
+    Abstract    -> "abstract",
+    Case        -> "case",
+    Final       -> "final",
+    Implicit    -> "implicit",
+    Infix       -> "infix",
+    Inline      -> "inline",
+    Lazy        -> "lazy",
+    Opaque      -> "opaque",
+    Open        -> "open",
+    Override    -> "override",
+    Private     -> "private",
+    Protected   -> "protected",
+    Sealed      -> "sealed",
+    Transparent -> "transparent"
+  )
 
   override def renderQualifier(modifier: ScAccessModifier): String = {
     val res = for {
