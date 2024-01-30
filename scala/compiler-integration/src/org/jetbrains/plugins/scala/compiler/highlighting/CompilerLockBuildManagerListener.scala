1a26aae5ebd947b92de02f5ea800073538990d6c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-01-30 15:47:58 +0100

    Release the compiler lock on the build manager thread
    #SCL-22051
    #SCL-19895
    
    - All BuildManagerListener implementations run their `buildFinished` code on the build manager thread.
    - In our case, this will release the compiler lock even later after compilation is finished, which should further reduce the risk of JPS and SCS running at the same time and writing to the same project build cache directory at the same time.

--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerLockBuildManagerListener.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerLockBuildManagerListener.scala
@@ -1,26 +1,31 @@
 package org.jetbrains.plugins.scala.compiler.highlighting
 
-import com.intellij.compiler.server.BuildManagerListener
+import com.intellij.compiler.server.{BuildManager, BuildManagerListener}
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.serviceContainer.AlreadyDisposedException
 import org.jetbrains.plugins.scala.compiler.JpsSessionErrorTrackerService
-import org.jetbrains.plugins.scala.compiler.highlighting.CompilerLockBuildManagerListener.suppressAlreadyDisposed
 import org.jetbrains.plugins.scala.settings.ScalaHighlightingMode
 
 import java.util.UUID
 
-class CompilerLockBuildManagerListener
-  extends BuildManagerListener {
+class CompilerLockBuildManagerListener extends BuildManagerListener {
+
+  import org.jetbrains.plugins.scala.compiler.highlighting.CompilerLockBuildManagerListener._
 
   override def buildStarted(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = suppressAlreadyDisposed {
     CompilerHighlightingService.get(project).cancel()
     CompilerLock.get(project).lock(sessionId.toString)
   }
 
-  override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = suppressAlreadyDisposed {
-    CompilerLock.get(project).unlock(sessionId.toString, exceptionIfNotLocked = false)
-    if (ScalaHighlightingMode.showCompilerErrorsScala3(project) && !JpsSessionErrorTrackerService.instance(project).hasError(sessionId)) {
-      CompilerHighlightingService.get(project).triggerDocumentCompilationInAllOpenEditors(None)
+  override def buildFinished(project: Project, sessionId: UUID, isAutomake: Boolean): Unit = {
+    executeOnBuildThread { () =>
+      suppressAlreadyDisposed {
+        CompilerLock.get(project).unlock(sessionId.toString, exceptionIfNotLocked = false)
+        if (ScalaHighlightingMode.showCompilerErrorsScala3(project) && !JpsSessionErrorTrackerService.instance(project).hasError(sessionId)) {
+          CompilerHighlightingService.get(project).triggerDocumentCompilationInAllOpenEditors(None)
+        }
+      }
     }
   }
 }
@@ -33,4 +38,12 @@ object CompilerLockBuildManagerListener {
     } catch {
       case _: AlreadyDisposedException => // ignore
     }
+
+  private def executeOnBuildThread(runnable: Runnable): Unit = {
+    if (ApplicationManager.getApplication.isUnitTestMode) {
+      runnable.run()
+    } else {
+      BuildManager.getInstance().runCommand(runnable)
+    }
+  }
 }
