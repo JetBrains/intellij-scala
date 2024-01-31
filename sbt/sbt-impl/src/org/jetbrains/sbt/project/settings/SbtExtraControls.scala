ab95ae011d1a7d23c2a9181b9bc6beb7da66e180
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-01-31 18:26:33 +0100

    [sbt, CBH] Add a setting to use separate output directories
    #SCL-19828
    
    - This change adds a new checkbox in sbt settings which allows IDEA to use different compiler output directories than the ones reported by sbt.
    - Toggling the setting forces a project reload.
    - A known limitation of the setting is that it might lead to ClassNotFoundExceptions when using sbt shell for building the project. We will re-examine the sbt settings page to disallow this settings combination.
    - This setting is especially useful for people who like to use `sbt ~compile`, or `sbt ~fastLinkJs` during the development of Scala 3 projects (where CBH is implicitly enabled).

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/settings/SbtExtraControls.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/settings/SbtExtraControls.scala
@@ -26,10 +26,11 @@ final class SbtExtraControls {
   val scalaVersionPreferenceCheckBox: JCheckBoxWithTooltip = ct(SbtBundle.message("sbt.settings.scalaVersionPreference"), SbtBundle.message("sbt.settings.scalaVersionPreference.tooltip"))
   val groupProjectsFromSameBuildCheckBox: JCheckBoxWithTooltip = ct(SbtBundle.message("group.projects.from.the.same.build"), SbtBundle.message("group.projects.from.the.same.build.tooltip"))
   val insertProjectTransitiveDependencies: JCheckBoxWithTooltip = ct(SbtBundle.message("insert.project.transitive.dependencies"), SbtBundle.message("insert.project.transitive.dependencies.tooltip"))
+  val useSeparateCompilerOutputPaths: JCheckBoxWithTooltip = ct(SbtBundle.message("use.separate.compiler.output.paths"), SbtBundle.message("use.separate.compiler.output.paths.tooltip"))
 
 
   locally {
-    content.setLayout(new GridLayoutManager(10, 2, JBUI.emptyInsets(), -1, -1))
+    content.setLayout(new GridLayoutManager(11, 2, JBUI.emptyInsets(), -1, -1))
 
     def gc(row: Int, column: Int, rowSpan: Int, colSpan: Int) =
       new GridConstraints(row, column, rowSpan, colSpan, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false)
@@ -40,12 +41,13 @@ final class SbtExtraControls {
     content.add(scalaVersionPreferenceCheckBox.panelWithTooltip, gc(2, 0, 1, 2))
     content.add(groupProjectsFromSameBuildCheckBox.panelWithTooltip, gc(3, 0, 1, 2))
     content.add(insertProjectTransitiveDependencies.panelWithTooltip, gc(4, 0, 1, 2))
-    content.add(new TitledSeparator(SbtBundle.message("sbt.settings.shell.title")), new GridConstraints(5, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
-    content.add(new JBLabel(SbtBundle.message("sbt.settings.useShell")), new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 1, false))
-    content.add(useSbtShellForImportCheckBox.panelWithTooltip, gc(6, 1, 1, 1))
-    content.add(useSbtShellForBuildCheckBox.panelWithTooltip, gc(7, 1, 1, 1))
-    content.add(remoteDebugSbtShellCheckBox.panelWithTooltip, gc(8, 0, 1, 2))
-    content.add(new Spacer, new GridConstraints(9, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, new Dimension(-1, 5), null, new Dimension(-1, 1), 0, false))
+    content.add(useSeparateCompilerOutputPaths.panelWithTooltip, gc(5, 0, 1, 2))
+    content.add(new TitledSeparator(SbtBundle.message("sbt.settings.shell.title")), new GridConstraints(6, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
+    content.add(new JBLabel(SbtBundle.message("sbt.settings.useShell")), new GridConstraints(7, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 1, false))
+    content.add(useSbtShellForImportCheckBox.panelWithTooltip, gc(7, 1, 1, 1))
+    content.add(useSbtShellForBuildCheckBox.panelWithTooltip, gc(8, 1, 1, 1))
+    content.add(remoteDebugSbtShellCheckBox.panelWithTooltip, gc(9, 0, 1, 2))
+    content.add(new Spacer, new GridConstraints(10, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, new Dimension(-1, 5), null, new Dimension(-1, 1), 0, false))
 
     resolveClassifiersCheckBox.setEnabled(true)
   }
---
+++ sbt/sbt-impl/test/org/jetbrains/sbt/project/UseSeparateCompilerOutputPathsTest.scala
@@ -0,0 +1,57 @@
+package org.jetbrains.sbt.project
+
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.roots.CompilerModuleExtension
+import com.intellij.openapi.vfs.VfsUtilCore
+import org.jetbrains.plugins.scala.project.ModuleExt
+import org.jetbrains.plugins.scala.util.TestUtils
+import org.junit.Assert.assertEquals
+
+import java.nio.file.Path
+
+class UseSeparateCompilerOutputPathsTest extends SbtExternalSystemImportingTestLike {
+
+  override protected def getTestProjectPath: String =
+    s"${TestUtils.getTestDataPath}/sbt/projects/separateCompilerOutputPaths"
+
+  override def setUp(): Unit = {
+    super.setUp()
+    SbtProjectResolver.processOutputOfLatestStructureDump = ""
+  }
+
+  private val moduleDirectoryMapping: Map[String, String] = Map(
+    "separateCompilerOutputPaths" -> "",
+    "module1" -> "module1",
+    "module2" -> "module2",
+    "module3" -> "module3"
+  )
+
+  private def expectedCompilerOutputPath(moduleName: String, scope: String, hasIdeaPrefix: Boolean): Path = {
+    val ideaPrefix = if (hasIdeaPrefix) "idea-" else ""
+    Path.of(getTestProjectPath)
+      .resolve(moduleDirectoryMapping(moduleName))
+      .resolve("target")
+      .resolve("scala-3.3.1")
+      .resolve(s"$ideaPrefix${scope}classes")
+  }
+
+  private def doTest(useSeparateCompilerOutputPaths: Boolean): Unit = {
+    getCurrentExternalProjectSettings.useSeparateCompilerOutputPaths = useSeparateCompilerOutputPaths
+    importProject(false)
+    ModuleManager.getInstance(myProject).getModules.filterNot(_.hasBuildModuleType).foreach { module =>
+      val extension = CompilerModuleExtension.getInstance(module)
+      val compileScopePath = Path.of(VfsUtilCore.urlToPath(extension.getCompilerOutputUrl))
+      assertEquals(expectedCompilerOutputPath(module.getName, "", useSeparateCompilerOutputPaths), compileScopePath)
+      val testScopePath = Path.of(VfsUtilCore.urlToPath(extension.getCompilerOutputUrlForTests))
+      assertEquals(expectedCompilerOutputPath(module.getName, "test-", useSeparateCompilerOutputPaths), testScopePath)
+    }
+  }
+
+  def testDisabled(): Unit = {
+    doTest(useSeparateCompilerOutputPaths = false)
+  }
+
+  def testEnabled(): Unit = {
+    doTest(useSeparateCompilerOutputPaths = true)
+  }
+}
