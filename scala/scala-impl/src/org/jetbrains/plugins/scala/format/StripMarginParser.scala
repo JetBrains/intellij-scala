6a69c80b5b75652810a7deb9f8f0144aadd58da0
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-02-07 11:58:06 +0100

    improve detection of string literals with postfix and infix `stripMargin` call

--- scala/scala-impl/src/org/jetbrains/plugins/scala/format/StripMarginParser.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/format/StripMarginParser.scala
@@ -3,7 +3,8 @@ package org.jetbrains.plugins.scala.format
 import org.jetbrains.plugins.scala.codeInspection.collections.MethodRepr
 import org.jetbrains.plugins.scala.extensions.Parent
 import org.jetbrains.plugins.scala.lang.psi.api.base.literals.{ScCharLiteral, ScStringLiteral}
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScMethodCall, ScReferenceExpression}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScExpression, ScInfixExpr, ScMethodCall, ScPostfixExpr, ScReferenceExpression}
+import org.jetbrains.plugins.scala.util.MultilineStringUtil
 
 object StripMarginParser extends StringParserLike[ScStringLiteral] {
 
@@ -23,10 +24,36 @@ object StripMarginParser extends StringParserLike[ScStringLiteral] {
 
 object WithStrippedMargin {
 
-  def unapply(literal: ScStringLiteral): Option[(ScExpression, Char)] = literal.getParent match {
-    case StripMarginCall(reference: ScReferenceExpression, _, Seq()) => Some(reference, '|')
-    case Parent(StripMarginCall(mc: ScMethodCall, _, Seq(ScCharLiteral(value)))) =>Some(mc, value)
-    case _ => None
+  /**
+   * Detects if string literal is followed by a `.stripMargin` call.<br>
+   * Example: {{{
+   *   //dot-notation
+   *   """123""".stripMargin
+   *   """123""".stripMargin('#')
+   *
+   *   //infix-notation
+   *   """123""" stripMargin
+   *   """123""" stripMargin '#'
+   * }}}
+ *
+   * @return pair:
+   *         1. parent expression containing the original string literal and stripMargin call
+   *         2. margin character
+   */
+  def unapply(literal: ScStringLiteral): Option[(ScExpression, Char)] = {
+    val literalParent = literal.getParent
+    literalParent match {
+      case StripMarginCall(reference: ScReferenceExpression, _, Seq()) => //detect `"""123""".stripMargin`
+        Some(reference, MultilineStringUtil.DefaultMarginChar)
+      case StripMarginCall(postfix: ScPostfixExpr, _, Seq()) => //detect `"""123""" stripMargin`
+        Some(postfix, MultilineStringUtil.DefaultMarginChar)
+      case StripMarginCall(infix: ScInfixExpr, _, Seq(ScCharLiteral(value))) => //detect `"""123""" stripMargin '#'`
+        Some(infix, value)
+      case Parent(StripMarginCall(mc: ScMethodCall, _, Seq(ScCharLiteral(value)))) => //detect `"""123""".stripMargin('#')`
+        Some(mc, value)
+      case _ =>
+        None
+    }
   }
 
   private[format] object StripMarginCall {
