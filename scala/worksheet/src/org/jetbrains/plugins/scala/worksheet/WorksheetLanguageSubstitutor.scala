76ccc76b3b8b88d53819bf810c58de4018804d5c
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-02-13 21:08:48 +0100

    [slow ops] mark getModule in WorksheetLanguageSubstitutor.getLanguage as a known issue #SCL-21147

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetLanguageSubstitutor.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/WorksheetLanguageSubstitutor.scala
@@ -4,13 +4,18 @@ import com.intellij.lang.Language
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.LanguageSubstitutor
+import com.intellij.util.SlowOperations
 import org.jetbrains.plugins.scala.project.ModuleExt
 import org.jetbrains.plugins.scala.worksheet.settings.WorksheetFileSettings
+
+import scala.util.Using
 
 final class WorksheetLanguageSubstitutor extends LanguageSubstitutor {
 
   override def getLanguage(file: VirtualFile, project: Project): Language = {
-    val module = WorksheetFileSettings(project, file).getModule
+    val module = Using.resource(SlowOperations.knownIssue("SCL-21147")) { _ =>
+      WorksheetFileSettings(project, file).getModule
+    }
     if (module.exists(_.hasScala3))
       WorksheetLanguage3.INSTANCE
     else
