9f16da67af7dbd18fc8d417db51cf9b243364230
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-02-14 09:14:22 +0100

    [annotator] only show top-level error in real scala files #SCL-22128 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScMemberAnnotator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScMemberAnnotator.scala
@@ -1,12 +1,13 @@
 package org.jetbrains.plugins.scala.annotator.element
 
 import com.intellij.openapi.application.ApplicationManager
-import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.{ScalaBundle, ScalaFileType}
 import org.jetbrains.plugins.scala.annotator.ScalaAnnotationHolder
 import org.jetbrains.plugins.scala.extensions.{ObjectExt, PsiElementExt}
 import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScTypeAlias, ScValueOrVariable}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScMember
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaFileImpl
 
 object ScMemberAnnotator extends ElementAnnotator[ScMember] {
 
@@ -19,11 +20,11 @@ object ScMemberAnnotator extends ElementAnnotator[ScMember] {
   private def annotateWrongTopLevelMembers(element: ScMember)
                                           (implicit holder: ScalaAnnotationHolder): Unit = {
     if (
-        element.isTopLevel &&
-          cannotBeTopLevelMemberInScala2(element) &&
-          element.scalaLanguageLevel.exists(_.isScala2) &&
-          element.containingScalaFile.exists(!_.isWorksheetFile) &&
-          !ApplicationManager.getApplication.isUnitTestMode
+      element.isTopLevel &&
+        cannotBeTopLevelMemberInScala2(element) &&
+        element.scalaLanguageLevel.exists(_.isScala2) &&
+        element.containingScalaFile.exists(file => file.getFileType == ScalaFileType.INSTANCE && !file.isWorksheetFile) &&
+        !ApplicationManager.getApplication.isUnitTestMode
     ) {
       val elementToAnnotate = element.depthFirst().find(c => keywords(c.elementType)).getOrElse(element)
       holder.createErrorAnnotation(elementToAnnotate, ScalaBundle.message("cannot.be.a.top.level.definition.in.scala.2"))
