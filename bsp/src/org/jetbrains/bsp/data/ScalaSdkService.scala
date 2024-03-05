c5fd4c803f1abf9c6ce9e68107bafa38e781bdca
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-03-05 17:23:44 +0100

    [compiler] Dynamically resolve the Scala 2 compiler bridge for the latest Scala 2.13 versions
    #SCL-21741

--- bsp/src/org/jetbrains/bsp/data/ScalaSdkService.scala
+++ bsp/src/org/jetbrains/bsp/data/ScalaSdkService.scala
@@ -47,6 +47,7 @@ class ScalaSdkService extends ScalaAbstractProjectDataService[ScalaSdkData, Libr
 
     library <- scalaLibraries(module, modelsProvider)
   } {
+    val compilerBridgeBinaryJar = ScalaSdkUtils.resolveCompilerBridgeJar(presentation)
     ScalaSdkUtils.ensureScalaLibraryIsConvertedToScalaSdk(
       modelsProvider,
       library,
@@ -56,7 +57,7 @@ class ScalaSdkService extends ScalaAbstractProjectDataService[ScalaSdkData, Libr
       //  and we should probably create a separate request to obtain scaladoc classpath
       //  see https://github.com/build-server-protocol/build-server-protocol/issues/229
       scaladocExtraClasspath = Nil,
-      compilerBridgeBinaryJar = None // will be resolved during the creation of the Scala SDK
+      compilerBridgeBinaryJar = compilerBridgeBinaryJar
     )
   }
 
--- scala/scala-impl/src/org/jetbrains/plugins/scala/project/external/ScalaSdkUtils.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/project/external/ScalaSdkUtils.scala
@@ -5,7 +5,7 @@ import com.intellij.openapi.roots.impl.libraries.LibraryEx
 import com.intellij.openapi.roots.libraries.Library
 import org.jetbrains.plugins.scala.DependencyManager
 import org.jetbrains.plugins.scala.DependencyManagerBase.RichStr
-import org.jetbrains.plugins.scala.project.{LibraryExt, ScalaLibraryProperties, ScalaLibraryType}
+import org.jetbrains.plugins.scala.project.{LibraryExt, ScalaLibraryProperties, ScalaLibraryType, Version}
 
 import java.io.File
 
@@ -19,8 +19,7 @@ object ScalaSdkUtils {
     scaladocExtraClasspath: Seq[File],
     compilerBridgeBinaryJar: Option[File],
   ): Unit = {
-    val compilerBridge = compilerBridgeBinaryJar.orElse(maybeVersion.flatMap(resolveCompilerBridgeJar))
-    val properties = ScalaLibraryProperties(maybeVersion, compilerClasspath, scaladocExtraClasspath, compilerBridge)
+    val properties = ScalaLibraryProperties(maybeVersion, compilerClasspath, scaladocExtraClasspath, compilerBridgeBinaryJar)
     val modifiableModel = modelsProvider.getModifiableLibraryModel(library).asInstanceOf[LibraryEx.ModifiableModelEx]
     if (!library.isScalaSdk) {
       modifiableModel.setKind(ScalaLibraryType.Kind)
@@ -29,13 +28,29 @@ object ScalaSdkUtils {
     modifiableModel.setProperties(properties)
   }
 
-  def resolveCompilerBridgeJar(scalaVersion: String): Option[File] = {
-    if (!scalaVersion.startsWith("3.")) return None
-
-    val compilerBridgeDependency = "org.scala-lang" % "scala3-sbt-bridge" % scalaVersion
-    DependencyManager.resolveSafe(compilerBridgeDependency)
-      .toOption
+  def resolveCompilerBridgeJar(scalaVersion: String): Option[File] =
+    compilerBridgeName(scalaVersion)
+      .map(name => "org.scala-lang" % name % scalaVersion)
+      .flatMap(dep => DependencyManager.resolveSafe(dep).toOption)
       .flatMap(_.headOption)
       .map(_.file)
+
+  def compilerBridgeName(scalaVersion: String): Option[String] = {
+    val version = Version(scalaVersion)
+    if (version.major(1) == Version("2")) {
+      if (version >= Version("2.13.12")) {
+        // Scala 2.13.12 and later versions distribute their own precompiled compiler bridge with support for
+        // compiler diagnostics.
+        Some(Scala2CompilerBridgeName)
+      } else
+        None // Previous Scala 2 versions should use the bundled source based Zinc compiler bridge
+    } else Some(Scala3CompilerBridgeName)
   }
+
+  def compilerBridgeJarName(scalaVersion: String): Option[String] =
+    compilerBridgeName(scalaVersion).map(n => s"$n-$scalaVersion.jar")
+
+  private final val Scala3CompilerBridgeName = "scala3-sbt-bridge"
+
+  private final val Scala2CompilerBridgeName = "scala2-sbt-bridge"
 }
