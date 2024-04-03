1c6fe8f7ea1cd2b4009e9de93d8b28008513e0a9
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-04-04 00:11:20 +0200

    [tests] Do not run the Scala class renderer tests for Scala 3.3+
    #SCL-22343

--- scala/debugger/test/org/jetbrains/plugins/scala/debugger/renderers/ScalaClassRendererTest.scala
+++ scala/debugger/test/org/jetbrains/plugins/scala/debugger/renderers/ScalaClassRendererTest.scala
@@ -12,8 +12,8 @@ import java.nio.file.Path
   TestScalaVersion.Scala_2_11,
   TestScalaVersion.Scala_2_12,
   TestScalaVersion.Scala_2_13,
-  TestScalaVersion.Scala_3_Latest,
-  // TODO: Fix the renderers for Scala 3.4+
+  // TODO: Fix the renderers for Scala 3.3+
+//  TestScalaVersion.Scala_3_Latest,
 //  TestScalaVersion.Scala_3_Latest_RC
 ))
 class ScalaClassRendererTest extends RendererTestBase {
