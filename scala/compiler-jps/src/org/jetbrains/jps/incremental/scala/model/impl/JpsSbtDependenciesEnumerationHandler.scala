fb7d261578f0ca8b01e54037df6709e0d6a3a89e
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2024-04-03 12:03:44 +0200

    sbt: change the default value of "sbt.process.dependencies.recursively" property (bugfix)
    (cherry picked from commit dc09cc6f51c3155482e2f554174a57410bf614b3)

--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/model/impl/JpsSbtDependenciesEnumerationHandler.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/model/impl/JpsSbtDependenciesEnumerationHandler.scala
@@ -19,9 +19,14 @@ final class JpsSbtDependenciesEnumerationHandler extends JpsJavaDependenciesEnum
   override def shouldIncludeTestsFromDependentModulesToTestClasspath: Boolean =
     super.shouldIncludeTestsFromDependentModulesToTestClasspath
 
-  override def shouldProcessDependenciesRecursively: Boolean =
+  override def shouldProcessDependenciesRecursively: Boolean = {
+    // "sbt.process.dependencies.recursively" property is the negation of the SbtProjectSettings.insertProjectTransitiveDependencies value
+    // (see ScalaBuildProcessParametersProvider.transitiveProjectDependenciesParams).
+    // So the default value for "sbt.process.dependencies.recursively" is set to
+    // the opposite of the default value of SbtProjectSettings.insertProjectTransitiveDependencies
     Option(System.getProperty("sbt.process.dependencies.recursively")).flatMap(_.toBooleanOption)
-      .getOrElse(true) // should be in sync with org.jetbrains.sbt.project.settings.SbtProjectSettings.insertProjectTransitiveDependencies
+      .getOrElse(false)
+  }
 }
 
 object JpsSbtDependenciesEnumerationHandler {
