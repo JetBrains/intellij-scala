2f94dea17749732bc9f866a14612900f6ca2e386
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-04-08 12:24:52 +0200

    [literals] support leading zeros in Scala 3 decimal numbers #SCL-20593 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/IntegerLiteralCheckTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/IntegerLiteralCheckTest.scala
@@ -2,20 +2,24 @@ package org.jetbrains.plugins.scala
 package annotator
 
 import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestCase
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
+import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScIntegerLiteral
 import org.jetbrains.plugins.scala.util.assertions.AssertionMatchers
 
+import scala.collection.immutable.SortedMap
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 abstract class IntegerLiteralCheckTestBase(supportsOctal: Boolean,
                                            supportsUnderscore: Boolean,
-                                           supportsBinary: Boolean)
+                                           supportsBinary: Boolean,
+                                           supports0Prefix: Boolean)
   extends ScalaLightCodeInsightFixtureTestCase with AssertionMatchers
 {
-  def testFine(): Unit = {
-    val makeUnderscores: String => Iterator[String] =
-      if (supportsUnderscore) padWithRandomUnderscores(5)
-      else Iterator(_)
+  def test0(): Unit = checkTextHasNoErrors("0")
 
+  def testFine(): Unit = {
     val intStrings = (PredefinedInts ++ randomInts)
       .flatMap(integerRepresentations)
       .flatMap(makeUnderscores)
@@ -126,6 +130,60 @@ abstract class IntegerLiteralCheckTestBase(supportsOctal: Boolean,
     }
   }
 
+  def testIntValues(): Unit = {
+    val groups = (PredefinedInts ++ randomInts)
+      .filter(_ >= 0)
+      .map { i =>
+        i -> Seq(i)
+          .flatMap(integerRepresentations)
+          .flatMap(padWith0)
+          .flatMap(makeUnderscores)
+      }.to(SortedMap)
+
+    doLiteralValueTest(groups)
+  }
+
+  def testLongValues(): Unit = {
+    val groups = (PredefinedLongs ++ randomLongs)
+      .filter(_ >= 0)
+      .map { i =>
+        i -> Seq(i)
+          .flatMap(longRepresentations)
+          .flatMap(padWith0)
+          .flatMap(makeUnderscores)
+          .flatMap(appendL)
+      }.to(SortedMap)
+
+    doLiteralValueTest(groups)
+  }
+
+  private def doLiteralValueTest[N: Numeric](groups: SortedMap[N, Seq[String]]): Unit = {
+    val numberStrings = groups.values.flatten.toSeq
+
+    val text = numberStrings.mkString("\n")
+    val file = myFixture.configureByText(ScalaFileType.INSTANCE, text).asInstanceOf[ScalaFile]
+    val literals = file.depthFirst().collect {
+      case e: ScLiteral.Numeric => e
+    }.toSeq
+
+    literals.length shouldBe numberStrings.length
+
+    val expected = groups.flatMap { case (n, strings) =>
+      strings.map(s => s"$s -> $n")
+    }.mkString("\n")
+
+    val literalIt = literals.iterator
+    val actual = groups.flatMap { case (_, strings) =>
+      strings.map { _ =>
+        val literal = literalIt.next()
+        s"${literal.getText} -> ${literal.getValue}"
+      }
+    }.mkString("\n")
+
+    actual shouldBe expected
+  }
+
+
   import java.lang.Integer.{MAX_VALUE => IntMaxValue, MIN_VALUE => IntMinValue}
   import java.lang.Long.{MAX_VALUE => LongMaxValue, MIN_VALUE => LongMinValue}
   import scala.util.Random._
@@ -150,33 +208,33 @@ abstract class IntegerLiteralCheckTestBase(supportsOctal: Boolean,
     LongMaxValue
   )
 
-  def randomInts: LazyList[Int] = randomValues(nextInt())()
+  private def randomInts: LazyList[Int] = randomValues(nextInt())()
 
-  def randomLongs: LazyList[Long] = randomValues(nextLong()) {
+  private def randomLongs: LazyList[Long] = randomValues(nextLong()) {
     _.toHexString.length > 8
   }
 
-  def integerRepresentations(int: Int): List[String] =
+  private def integerRepresentations(int: Int): List[String] =
     representations(int.toString, int.toOctalString, int.toHexString, int.toBinaryString)
 
-  def longRepresentations(long: Long): List[String] =
+  private def longRepresentations(long: Long): List[String] =
     representations(long.toString, long.toOctalString, long.toHexString, long.toBinaryString)
 
-  def prependSign(s: String): List[String] = s ::
+  private def prependSign(s: String): List[String] = s ::
     (if (s.startsWith("-") || s.startsWith("+")) Nil else "-" + s :: "+" + s :: Nil)
 
-  def appendL(s: String): List[String] = s + "l" ::
+  private def appendL(s: String): List[String] = s + "l" ::
     s + "L" ::
     Nil
 
   private def randomValues[T](generator: => T)
-                                   (predicate: T => Boolean = Function.const(true)(_: T)) =
+                             (predicate: T => Boolean = Function.const(true)(_: T)) =
     LazyList
       .continually(generator)
       .filter(predicate)
       .take(10)
 
-  def padWithRandomUnderscores(n: Int)(s: String): Iterator[String] =
+  private def padWithRandomUnderscores(n: Int)(s: String): Iterator[String] =
     Iterator(s) ++
       Iterator.continually(s)
         .map(padWithRandomUnderscores)
@@ -196,6 +254,20 @@ abstract class IntegerLiteralCheckTestBase(supportsOctal: Boolean,
     }
   }
 
+  private val makeUnderscores: String => Iterator[String] =
+    if (supportsUnderscore) padWithRandomUnderscores(5)
+    else Iterator(_)
+
+  private def padWith0(s: String): Iterator[String] = {
+    if (!supports0Prefix)
+      return Iterator(s)
+
+    if (s.lift(1).exists(!_.isDigit))
+      return Iterator(s)
+
+    Iterator(s, "0" + s, "00" + s)
+  }
+
   private def representations(decimalString: String,
                               octalString: String,
                               hexString: String,
@@ -213,22 +285,57 @@ abstract class IntegerLiteralCheckTestBase(supportsOctal: Boolean,
   }
 }
 
-class IntegerLiteralCheckTest2_10 extends IntegerLiteralCheckTestBase(supportsOctal = true, supportsUnderscore = false, supportsBinary = false) {
+class IntegerLiteralCheckTest2_10
+  extends IntegerLiteralCheckTestBase(
+    supportsOctal = true,
+    supportsUnderscore = false,
+    supportsBinary = false,
+    supports0Prefix = false,
+  )
+{
   override def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_2_10
 }
 
-class IntegerLiteralCheckTest2_12 extends IntegerLiteralCheckTestBase(supportsOctal = false, supportsUnderscore = false, supportsBinary = false) {
+class IntegerLiteralCheckTest2_12
+  extends IntegerLiteralCheckTestBase(
+    supportsOctal = false,
+    supportsUnderscore = false,
+    supportsBinary = false,
+    supports0Prefix = false,
+  )
+{
   override def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_2_12
 }
 
-class IntegerLiteralCheckTest2_13 extends IntegerLiteralCheckTestBase(supportsOctal = false, supportsUnderscore = true, supportsBinary = true) {
+class IntegerLiteralCheckTest2_13
+  extends IntegerLiteralCheckTestBase(
+    supportsOctal = false,
+    supportsUnderscore = true,
+    supportsBinary = true,
+    supports0Prefix = false,
+  )
+{
   override def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_2_13
 }
 
-class IntegerLiteralCheckTest3_4 extends IntegerLiteralCheckTestBase(supportsOctal = false,supportsUnderscore = true, supportsBinary = false) {
+class IntegerLiteralCheckTest3_4
+  extends IntegerLiteralCheckTestBase(
+    supportsOctal = false,
+    supportsUnderscore = true,
+    supportsBinary = false,
+    supports0Prefix = true,
+  )
+{
   override def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_3_4
 }
 
-class IntegerLiteralCheckTest3_5 extends IntegerLiteralCheckTestBase(supportsOctal = false,supportsUnderscore = true, supportsBinary = false /* TODO: change to true in Scala 3.5 */) {
+class IntegerLiteralCheckTest3_5
+  extends IntegerLiteralCheckTestBase(
+    supportsOctal = false,
+    supportsUnderscore = true,
+    supportsBinary = false, // TODO: change to true in Scala 3.5
+    supports0Prefix = true,
+  )
+{
   override def supportedIn(version: ScalaVersion): Boolean = version >= ScalaVersion.Latest.Scala_3 // TODO: change to Scala_3_5 if available
 }
