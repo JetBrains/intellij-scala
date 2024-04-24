124ff2df4b35c3a03daff4642f2888b1f6bfa25d
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-04-24 16:10:26 +0200

    [project model] detect `_` prefix in library names when guessing library versions from library name #SCL-22444 fixed
    This should work as a workaround for #SCL-22444.
    But we should create a proper fix for #SCL-22048

--- scala/scala-impl/test/org/jetbrains/plugins/scala/project/LibraryExtTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/project/LibraryExtTest.scala
@@ -7,6 +7,9 @@ import org.junit.Assert.assertEquals
  * NOTE: some "strange" examples in tests data are not because it's "DESIGNED" this way but because
  * it behaved this way for ages and I just wanted to cover current behaviour in tests.<br>
  * The designed behaviour can be reviewed if needed
+ *
+ * @see [[org.jetbrains.plugins.scala.project.VersionTest]]
+ * @see [[org.jetbrains.plugins.scala.ScalaVersionTest]]
  */
 //noinspection AssertBetweenInconvertibleTypes (workaround for SCL-22457)
 class LibraryExtTest extends TestCase {
@@ -43,6 +46,22 @@ class LibraryExtTest extends TestCase {
 
   def testGuessLibraryVersionFromName_ColonPrefix(): Unit = {
     assertEquals(Some("1.22.3"), LibraryExt.guessLibraryVersionFromName("library:1.22.3"))
+  }
+
+  def testGuessLibraryVersionFromName_UnderscorePrefix(): Unit = {
+    assertEquals(Some("1.22.3"), LibraryExt.guessLibraryVersionFromName("library_1.22.3"))
+  }
+
+  //SCL-22444
+  def testGuessLibraryVersionFromName_SCL_22444(): Unit = {
+    assertEquals(Some("2.13.13"), LibraryExt.guessLibraryVersionFromName("scala-sdk_2.13.13:jar"))
+    assertEquals(Some("2.13.13"), LibraryExt.guessLibraryVersionFromName("scala-sdk__2.13.13:jar"))
+    assertEquals(Some("2.13.13"), LibraryExt.guessLibraryVersionFromName("sbt: scala-sdk__2.13.13:jar"))
+
+    assertEquals(Some("2.12.15-bin-db-2-fd41f6b"), LibraryExt.guessLibraryVersionFromName("org.scala-lang__scala-library_2.12.15-bin-db-2-fd41f6b"))
+    assertEquals(Some("2.12.15-bin-db-2-fd41f6b"), LibraryExt.guessLibraryVersionFromName("org.scala-lang__scala-library__2.12.15-bin-db-2-fd41f6b"))
+    assertEquals(Some("2.12.15-bin-db-2-fd41f6b"), LibraryExt.guessLibraryVersionFromName("sbt: org.scala-lang__scala-library__2.12.15-bin-db-2-fd41f6b"))
+    assertEquals(Some("2.12.15-bin-db-2-fd41f6b"), LibraryExt.guessLibraryVersionFromName("sbt: org.scala-lang__scala-library__2.12.15-bin-db-2-fd41f6b:jar"))
   }
 
   def testGuessLibraryVersionFromName_VersionWithSuffix(): Unit = {
