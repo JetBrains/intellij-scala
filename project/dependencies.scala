8c15b346d1fa833cd6e21a5292253104593b49b5
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-04-29 15:39:31 +0200

    Revert "Update intellijVersion to 241.15989.143"
    This reverts commit 216fc4d5d975b126c723c55f9563d5f9fd190905.
    
    Keep the pinned version with EAP or RELEASE tag

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.9.5"
 
   // ATTENTION: check the comment in `Common.newProjectWithKotlin` when updating this version
-  val intellijVersion = "241.15989.143"
+  val intellijVersion = "241.15989.69"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
