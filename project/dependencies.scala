ca573f9251179061638ae173b46d577792625ad9
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-04-30 17:04:28 +0200

    [build] Update Scala 2.13 version to Scala 2.13.14

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,7 +4,7 @@ import org.jetbrains.sbtidea.download.idea.IntellijVersionUtils
 import sbt.*
 
 object Versions {
-  val scalaVersion: String = "2.13.13"
+  val scalaVersion: String = "2.13.14"
   val scala3Version: String = "3.3.3"
 
   // ATTENTION: when updating sbtVersion also update versions in MockSbt_1_0
