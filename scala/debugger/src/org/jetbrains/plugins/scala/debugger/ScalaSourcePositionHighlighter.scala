b559fc0ff9370e55a07a45cdaed172b57aea9980
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-05-07 18:14:08 +0200

    [debugger] Consider whether the source position covers the whole line before searching for a lambda expression
    #SCL-22488
    
    - This is an optimization which applies for regular line breakpoints. In this case, the PSI tree will not be traversed and no resolution will be done.
    
    
    (cherry picked from commit 9c90ac15070fbf9bed5bd70889b71acaa4b44a47)

--- scala/debugger/src/org/jetbrains/plugins/scala/debugger/ScalaSourcePositionHighlighter.scala
+++ scala/debugger/src/org/jetbrains/plugins/scala/debugger/ScalaSourcePositionHighlighter.scala
@@ -2,8 +2,9 @@ package org.jetbrains.plugins.scala.debugger
 
 import com.intellij.debugger.SourcePosition
 import com.intellij.debugger.engine.SourcePositionHighlighter
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.{PsiDocumentManager, PsiElement, PsiMethod}
+import com.intellij.psi.{PsiElement, PsiMethod}
 import com.intellij.util.DocumentUtil
 import org.jetbrains.plugins.scala.ScalaLanguage
 import org.jetbrains.plugins.scala.extensions.PsiElementExt
@@ -14,16 +15,27 @@ import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
 class ScalaSourcePositionHighlighter extends SourcePositionHighlighter {
   override def getHighlightRange(sourcePosition: SourcePosition): TextRange = {
     if (isScalaLanguage(sourcePosition)) {
-      Option(sourcePosition.getElementAt)
-        .flatMap(containingLambda)
-        .flatMap(calculateRange(sourcePosition))
-        .orNull
+      val element = sourcePosition.getElementAt
+      if (element eq null) return null
+
+      val document = sourcePosition.getFile.getViewProvider.getDocument
+      if (document eq null) return null
+
+      val line = sourcePosition.getLine
+      if (isWholeLine(document, line, element)) {
+        return null
+      }
+
+      containingLambda(element).flatMap(calculateRange(document, line)).orNull
     }
     else null
   }
 
   private def isScalaLanguage(sourcePosition: SourcePosition): Boolean =
     sourcePosition.getFile.getLanguage.isKindOf(ScalaLanguage.INSTANCE)
+
+  private def isWholeLine(document: Document, line: Int, element: PsiElement): Boolean =
+    DocumentUtil.getLineTextRange(document, line) == element.getTextRange
 
   private def containingLambda(element: PsiElement): Option[PsiElement] =
     element.withParentsInFile.collectFirst {
@@ -34,13 +46,9 @@ class ScalaSourcePositionHighlighter extends SourcePositionHighlighter {
       case _: ScClass => None
     }.flatten
 
-  private def calculateRange(sourcePosition: SourcePosition)(lambda: PsiElement): Option[TextRange] =
-    for {
-      range <- Option(lambda.getTextRange)
-      file = sourcePosition.getFile
-      project = file.getProject
-      document <- Option(PsiDocumentManager.getInstance(project).getDocument(file))
-      lineRange = DocumentUtil.getLineTextRange(document, sourcePosition.getLine)
-      res = range.intersection(lineRange) if lineRange != res
-    } yield res
+  private def calculateRange(document: Document, line: Int)(lambda: PsiElement): Option[TextRange] = {
+    val lineRange = DocumentUtil.getLineTextRange(document, line)
+    val res = lambda.getTextRange.intersection(lineRange)
+    if (lineRange != res) Some(res) else None
+  }
 }
