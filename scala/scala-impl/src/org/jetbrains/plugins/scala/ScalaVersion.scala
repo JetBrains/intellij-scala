a3b24c17e4dd983536809b2714df8d66a749193d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-05-17 15:34:08 +0200

    [tests] Update Scala 3.4 test version to 3.4.2
    (cherry picked from commit 914b3e8b201927f25ac7976ed585aae0d18182db)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
@@ -83,7 +83,7 @@ object LatestScalaVersions {
   val Scala_3_1  = new ScalaVersion(ScalaLanguageLevel.Scala_3_1, "3")
   val Scala_3_2  = new ScalaVersion(ScalaLanguageLevel.Scala_3_2, "2")
   val Scala_3_3 = new ScalaVersion(ScalaLanguageLevel.Scala_3_3, "3")
-  val Scala_3_4 = new ScalaVersion(ScalaLanguageLevel.Scala_3_4, "1")
+  val Scala_3_4 = new ScalaVersion(ScalaLanguageLevel.Scala_3_4, "2")
 
   //
   // Aliases
