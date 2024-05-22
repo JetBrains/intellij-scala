cdcd18e9428ecc2830c3578e6aab7a5c29f1dba6
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-05-22 19:28:37 +0200

    [surroundWith] migrate IgnoreErrorHighlightingSurrounder to ModCommand API #SCL-22538

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/errorHighlighting/IgnoreErrorHighlightingSurrounder.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/surroundWith/surrounders/errorHighlighting/IgnoreErrorHighlightingSurrounder.scala
@@ -1,23 +1,28 @@
 package org.jetbrains.plugins.scala.lang.surroundWith.surrounders.errorHighlighting
 
-import com.intellij.lang.surroundWith.Surrounder
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
+import org.jetbrains.plugins.scala.lang.surroundWith.ScalaModCommandSurrounder
 
-class IgnoreErrorHighlightingSurrounder extends Surrounder {
+class IgnoreErrorHighlightingSurrounder extends ScalaModCommandSurrounder {
   //noinspection ScalaExtractStringToBundle
   override def getTemplateDescription: String = "/*_*/.../*_*/"
 
-  override def isApplicable(elements: Array[PsiElement]): Boolean = true
+  override def isApplicable(elements: Array[PsiElement]): Boolean = elements.nonEmpty
 
-  override def surroundElements(project: Project, editor: Editor, elements: Array[PsiElement]): TextRange = {
-    val start = elements(0).getTextRange.getStartOffset
-    val end = elements(elements.length - 1).getTextRange.getEndOffset
-    val text = "/*_*/"
-    editor.getDocument.insertString(end, text)
-    editor.getDocument.insertString(start, text)
-    new TextRange(end + 2 * text.length, end + 2 * text.length)
+  override def surroundElements(elements: Array[PsiElement]): Option[TextRange] = {
+    val firstElement = elements.head
+    firstElement.containingFile.map { file =>
+      val text = "/*_*/"
+      val start = firstElement.startOffset
+      val end = elements.last.endOffset
+
+      val document = file.getFileDocument
+      document.insertString(end, text)
+      document.insertString(start, text)
+
+      new TextRange(end + 2 * text.length, end + 2 * text.length)
+    }
   }
 }
