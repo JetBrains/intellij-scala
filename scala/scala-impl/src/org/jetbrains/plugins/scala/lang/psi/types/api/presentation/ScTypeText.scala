ab2e487ed24b243128068878c913ea75d2196dba
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-06-05 21:00:00 +0200

    [type renderers] propagate type presentation context in ScTypeText #SCL-22597
    (cherry picked from commit a31e7bef5a4e89d3713e2fdfbf15ea6f01a03e28)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/api/presentation/ScTypeText.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/api/presentation/ScTypeText.scala
@@ -3,6 +3,6 @@ package org.jetbrains.plugins.scala.lang.psi.types.api.presentation
 import org.jetbrains.plugins.scala.lang.psi.types.{ScType, TypePresentationContext}
 
 case class ScTypeText(tp: ScType)(implicit tpc: TypePresentationContext) {
-  val canonicalText: String = tp.canonicalText
-  val presentableText: String = tp.presentableText
+  val canonicalText: String = tp.canonicalText(tpc)
+  val presentableText: String = tp.presentableText(tpc)
 }
