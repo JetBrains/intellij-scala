c0c7c1910ecf3dffdba7c6260b9ce20f5e4d6c14
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-06-28 13:31:13 +0200

    [quick doc] introduce ScalaPsiDocumentationTargetProvider #SCL-22797 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/editor/documentationProvider/ScalaDocumentationProviderTest_ScalaDocContent.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/editor/documentationProvider/ScalaDocumentationProviderTest_ScalaDocContent.scala
@@ -102,6 +102,26 @@ final class ScalaDocumentationProviderTest_ScalaDocContent extends ScalaDocument
     )
   }
 
+  def testMethod_WithEmptyDoc_OverridingJavaMethod_FromLibrary(): Unit = {
+    doGenerateDocBodyTest(
+      s"""class A {
+         |  //from java.lang.Object
+         |  override def ${|}toString: String = super.toString
+         |}
+         |""".stripMargin,
+      s"""$DefinitionStart<a href="psi_element://A"><code>A</code></a>
+         |
+         |<span style="color:#000080;font-weight:bold;">override</span> <span style="color:#000080;font-weight:bold;">def</span> <span style="color:#000000;">toString</span>: <span style="color:#000000;"><a href="psi_element://java.lang.String"><code>String</code></a></span>$DefinitionEnd
+         |$ContentStart
+         |<b>Description copied from class: </b><a href="psi_element://java.lang.Object"><code>Object</code></a>$ContentEnd
+         |$ContentStart Returns a string representation of the object. $ContentEnd
+         |$SectionsStart<tr><td valign='top' class='section'><p>Returns:</td><td valign='top'><p>a string representation of the object.</td><tr><td valign='top' class='section'><p>API Note:</td><td valign='top'><p> In general, the <code>toString</code> method returns a string that "textually represents" this object. The result should be a concise but informative representation that is easy for a person to read. It is recommended that all subclasses override this method. The string output is not necessarily stable over time or across JVM invocations. </td><tr><td valign='top' class='section'><p>Implementation<br>Requirements:</td><td valign='top'><p> The <code>toString</code> method for class <code>Object</code> returns a string consisting of the name of the class of which the object is an instance, the at-sign character `<code>@</code>', and the unsigned hexadecimal representation of the hash code of the object. In other words, this method returns a string equal to the value of: <blockquote> <pre>
+         |  getClass().getName() + '@' + Integer.toHexString(hashCode())
+         |  </pre></blockquote> </td>$SectionsEnd
+         |""".stripMargin
+    )
+  }
+
   def testMethod_WithEmptyDoc_OverridingJavaMethod_TagsInJavadoc(): Unit = {
     myFixture.addFileToProject("BaseJavaClass.java",
       s"""public class BaseJavaClass {
