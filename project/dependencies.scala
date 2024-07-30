45b4aca501c12a97bf6543d7bf14173ec9d058e3
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-07-30 11:33:44 +0200

    Update intellijVersion to 243.6360, fix compilation

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.10.1"
 
   // ATTENTION: check the comment in `Common.newProjectWithKotlin` when updating this version
-  val intellijVersion = "243.5526"
+  val intellijVersion = "243.6360"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
