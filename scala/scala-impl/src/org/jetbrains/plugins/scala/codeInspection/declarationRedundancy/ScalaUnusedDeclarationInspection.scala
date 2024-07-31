9a65f6d596918f4ff890af5613d9e6d1e8d59e10
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-07-31 23:54:04 +0200

    [release checklist, inspections] Enable UnusedDeclarationInspection in Scala 3 by default in nightly builds
    #noport

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/ScalaUnusedDeclarationInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/ScalaUnusedDeclarationInspection.scala
@@ -35,7 +35,7 @@ final class ScalaUnusedDeclarationInspection extends HighlightingPassInspection 
   var reportLocalDeclarations: Int = 0
 
   @BooleanBeanProperty
-  var enableInScala3: Boolean = false //TODO
+  var enableInScala3: Boolean = true //TODO
 
   override def getOptionsPane: OptPane = pane(
     checkbox(reportPublicDeclarationsPropertyName, ScalaInspectionBundle.message("name.unused.declaration.report.public.declarations")),
