df8847244aa758aaaf2e9773e686e30651dac55d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-08-06 11:48:23 +0200

    [tests] Convert a simple test suite to JUnit 5

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/CompilerMessagesTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/CompilerMessagesTest.scala
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.scala.compiler.highlighting
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class CompilerMessagesTest {
 
