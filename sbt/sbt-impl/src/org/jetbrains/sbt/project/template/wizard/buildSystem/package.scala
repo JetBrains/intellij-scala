70dbd00d081732dea479e01b79a56d4f083397d9
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2024-08-22 14:41:08 +0200

    [Onboarding tips] Adding a new onboarding tips file describing X-Ray

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/template/wizard/buildSystem/package.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/template/wizard/buildSystem/package.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.sbt.project.template.wizard
 
 import com.intellij.ide.fileTemplates.FileTemplateManager
-import com.intellij.ide.starters.JavaStartersBundle.message
 import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.ide.wizard.{AbstractNewProjectWizardStep, NewProjectOnboardingTips, OnboardingTipsInstallationInfo}
 import com.intellij.openapi.actionSystem.IdeActions
@@ -13,15 +12,15 @@ import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.{VfsUtil, VirtualFile}
+import org.jetbrains.plugins.scala.actions.ShowTypeInfoAction
 import org.jetbrains.plugins.scala.extensions.{CharSeqExt, inWriteAction}
 
 import scala.jdk.CollectionConverters.MapHasAsJava
 import scala.jdk.CollectionConverters.ListHasAsScala
+import kotlin.jvm.functions.Function1
+import java.lang.{Integer => JInt}
 
 package object buildSystem {
-
-  type BreakportSelectorType = kotlin.jvm.functions.Function1[_ >: CharSequence, java.lang.Integer]
-
   private[buildSystem] def setProjectOrModuleSdk(
     project: Project,
     parentStep: AbstractNewProjectWizardStep,
@@ -42,83 +41,28 @@ package object buildSystem {
     }
   }
 
-  private def templatesAndFiles(isScala3: Boolean, withOnboardingTips: Boolean, shouldRenderOnboardingTips: Boolean): Seq[(String, String, String)] =
-    (isScala3, withOnboardingTips, shouldRenderOnboardingTips) match {
-      case (true, true, true) =>
-        Seq(("scala3-sample-code-tips-rendered.scala", "main.scala", """println(s"i = $i")"""))
-      case (true, true, false) =>
-        Seq(("scala3-sample-code-tips.scala", "main.scala", """println(s"i = $i")"""))
-      case (true, false, _) =>
-        Seq(("scala3-sample-code.scala", "main.scala", """println(s"i = $i")"""))
-      case (false, true, true) =>
-        Seq(("scala-sample-code-tips-rendered.scala","Main.scala", """println(s"i = $i")"""))
-      case (false, true, false) =>
-        Seq(("scala-sample-code-tips.scala", "Main.scala", """println(s"i = $i")"""))
-      case (false, false, _) =>
-        Seq(("scala-sample-code.scala", "Main.scala", """println(s"i = $i")"""))
-    }
-
-  private def onboardingTipsVariables(withOnboardingTips: Boolean, shouldRenderOnboardingTips: Boolean, packagePrefix: Option[String]): Map[String, String] = {
-    if (withOnboardingTips && shouldRenderOnboardingTips) {
-      def shortcut(actionId: String) = s"""<shortcut actionId="$actionId"/>"""
-      def icon(allIconsId: String) = s"""<icon src="$allIconsId"/>"""
-      Map(
-        "RunComment1"           -> message("onboarding.run.comment.render.1", shortcut(IdeActions.ACTION_DEFAULT_RUNNER)),
-        "RunComment2"           -> message("onboarding.run.comment.render.2", icon("AllIcons.Actions.Execute")),
-        "ShowIntentionComment1" -> message("onboarding.show.intention.tip.comment.render.1", shortcut(IdeActions.ACTION_SHOW_INTENTION_ACTIONS)),
-        "ShowIntentionComment2" -> message("onboarding.show.intention.tip.comment.render.2", ApplicationNamesInfo.getInstance.getFullProductName),
-        "DebugComment1"         -> message("onboarding.debug.comment.render.1", shortcut(IdeActions.ACTION_DEFAULT_DEBUGGER), icon("AllIcons.Debugger.Db_set_breakpoint")),
-        "DebugComment2"         -> message("onboarding.debug.comment.render.2", shortcut(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT)),
-      )
-    } else if (withOnboardingTips) {
-      val tipsContext = new KeymapTextContext {
-        override def isSimplifiedMacShortcuts: Boolean = SystemInfo.isMac
-      }
-      Map(
-        "SearchEverywhereComment1"  -> message("onboarding.search.everywhere.tip.comment.1", "Shift"),
-        "SearchEverywhereComment2" -> message("onboarding.search.everywhere.tip.comment.2"),
-        "ShowIntentionComment1"     -> message("onboarding.show.intention.tip.comment.1", tipsContext.getShortcutText(IdeActions.ACTION_SHOW_INTENTION_ACTIONS)),
-        "ShowIntentionComment2"     -> message("onboarding.show.intention.tip.comment.2", ApplicationNamesInfo.getInstance.getFullProductName),
-        "RunComment"                -> message("onboarding.run.comment", tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_RUNNER)),
-        "DebugComment1"             -> message("onboarding.debug.comment.1", tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_DEBUGGER)),
-        "DebugComment2"             -> message("onboarding.debug.comment.2", tipsContext.getShortcutText(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT))
-      )
-    } else
-      Map.empty[String, String]
-  } ++ packagePrefix.map(prefix => Map("PACKAGE_NAME" -> prefix)).getOrElse(Map.empty)
-
-  private def installOnboardingTips(project: Project, sourceCode: String, fileName: String, breakpointLine: String = ""): Unit = {
-    val breakpointSelector = if (breakpointLine.nonEmpty) createBreakpointSelector(breakpointLine) else null
-    val onboardingInfo = new OnboardingTipsInstallationInfo(sourceCode, fileName, breakpointSelector)
-    NewProjectOnboardingTips
-      .EP_NAME
-      .getExtensionList
-      .asScala
-      .foreach(_.installTips(project, onboardingInfo))
-  }
-
   private[buildSystem]
   def addScalaSampleCode(project: Project, path: String, isScala3: Boolean, packagePrefix: Option[String], withOnboardingTips: Boolean): Seq[VirtualFile] = {
     val shouldRenderOnboardingTips: Boolean = Registry.is("doc.onboarding.tips.render")
     val manager = FileTemplateManager.getInstance(project)
     val variables = onboardingTipsVariables(withOnboardingTips, shouldRenderOnboardingTips, packagePrefix).asJava
 
-    templatesAndFiles(isScala3, withOnboardingTips, shouldRenderOnboardingTips)
-      .map { case (templateName, fileName, breakpointLine) =>
+    val samples = templatesAndFiles(isScala3, withOnboardingTips, shouldRenderOnboardingTips)
+      .map { case Sample(templateName, fileName, breakpoint) =>
         val sourceCode = manager.getInternalTemplate(templateName).getText(variables)
-        if (withOnboardingTips) installOnboardingTips(project, sourceCode, fileName, breakpointLine)
-        inWriteAction {
-          val fileDirectory = createDirectoryIfMissing(path)
-          val file: VirtualFile = fileDirectory.findOrCreateChildData(this, fileName)
-          VfsUtil.saveText(file, sourceCode)
-          file
-        }
+        if (withOnboardingTips) installOnboardingTips(project, sourceCode, fileName, breakpoint)
+        (sourceCode, fileName)
       }
-  }
 
-  private def createDirectoryIfMissing(path: String): VirtualFile =
-    Option(VfsUtil.createDirectoryIfMissing(path))
-      .getOrElse(throw new IllegalStateException("Unable to create src directory"))
+    inWriteAction {
+      val fileDirectory = createDirectoryIfMissing(path)
+      samples.map { case (sourceCode, fileName) =>
+        val file: VirtualFile = fileDirectory.findOrCreateChildData(this, fileName)
+        VfsUtil.saveText(file, sourceCode)
+        file
+      }
+    }
+  }
 
   private[buildSystem]
   def addGitIgnore(project: Project, path: String): VirtualFile = {
@@ -132,9 +76,121 @@ package object buildSystem {
     }
   }
 
-  private def createBreakpointSelector(line: String): kotlin.jvm.functions.Function1[_ >: CharSequence, java.lang.Integer] =
-    str => {
-      val index = str.indexOf(line)
-      if (index >= 0) index else null
+  private def createDirectoryIfMissing(path: String): VirtualFile =
+    Option(VfsUtil.createDirectoryIfMissing(path))
+      .getOrElse(throw new IllegalStateException("Unable to create src directory"))
+
+  private def templatesAndFiles(isScala3: Boolean, withOnboardingTips: Boolean, shouldRenderOnboardingTips: Boolean): Seq[Sample] =
+    (isScala3, withOnboardingTips, shouldRenderOnboardingTips) match {
+      case (true, true, true) =>
+        Seq(
+          Sample("scala3-xray-tips-rendered.scala", "InlayHintsAndXRay.scala"),
+          Sample("scala3-sample-code-tips-rendered.scala", "main.scala", """println(s"i = $i")""")
+        )
+      case (true, true, false) =>
+        Seq(
+          Sample("scala3-xray-tips.scala", "InlayHintsAndXRay.scala"),
+          Sample("scala3-sample-code-tips.scala", "main.scala", """println(s"i = $i")""")
+        )
+      case (true, false, _) =>
+        Seq(Sample("scala3-sample-code.scala", "main.scala", """println(s"i = $i")"""))
+      case (false, true, true) =>
+        Seq(
+          Sample("scala2-xray-tips-rendered.scala", "InlayHintsAndXRay.scala"),
+          Sample("scala2-sample-code-tips-rendered.scala","Main.scala", """println(s"i = $i")""")
+        )
+      case (false, true, false) =>
+        Seq(
+          Sample("scala2-xray-tips.scala", "InlayHintsAndXRay.scala"),
+          Sample("scala2-sample-code-tips.scala", "Main.scala", """println(s"i = $i")""")
+        )
+      case (false, false, _) =>
+        Seq(Sample("scala-sample-code.scala", "Main.scala", """println(s"i = $i")"""))
     }
+
+  private def onboardingTipsVariables(withOnboardingTips: Boolean, shouldRenderOnboardingTips: Boolean, packagePrefix: Option[String]): Map[String, String] = {
+    if (withOnboardingTips) {
+      if (shouldRenderOnboardingTips) renderedVariables else unrenderedVariables
+    } else Map.empty[String, String]
+  } ++ packagePrefix.map(prefix => Map("PACKAGE_NAME" -> prefix)).getOrElse(Map.empty)
+
+  private def installOnboardingTips(project: Project, sourceCode: String, fileName: String, breakpoint: Function1[_ >: CharSequence, JInt]): Unit = {
+    val onboardingInfo = new OnboardingTipsInstallationInfo(sourceCode, fileName, breakpoint)
+    NewProjectOnboardingTips
+      .EP_NAME
+      .getExtensionList
+      .asScala
+      .foreach(_.installTips(project, onboardingInfo))
+  }
+
+  private def shortcut(actionId: String) = s"""<shortcut actionId="$actionId"/>"""
+  private def raw(key: String) = s"""<shortcut raw="$key"/>"""
+  private def icon(allIconsId: String) = s"""<icon src="$allIconsId"/>"""
+
+  private lazy val renderedVariables =
+    Map(
+      // actions
+      "ActionTypeInfo"      -> shortcut(ShowTypeInfoAction.ActionId),
+      "ActionImplicitHints" -> shortcut("Scala.ShowImplicits"), // TODO: Put action ids in one place
+      "ActionSettings"      -> shortcut(IdeActions.ACTION_SHOW_SETTINGS),
+      "ActionRun"           -> shortcut(IdeActions.ACTION_DEFAULT_RUNNER),
+      "ActionShowIntention" -> shortcut(IdeActions.ACTION_SHOW_INTENTION_ACTIONS),
+      "ActionDebug"         -> shortcut(IdeActions.ACTION_DEFAULT_DEBUGGER),
+      "ActionSetBreakpoint" -> shortcut(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT),
+      // icons
+      "IconEye"             -> icon("AllIcons.General.InspectionsEye"),
+      "IconExecute"         -> icon("AllIcons.Actions.Execute"),
+      "IconBreakpoint"      -> icon("AllIcons.Debugger.Db_set_breakpoint"),
+      // others
+      "META"                -> raw("META"),
+      "CONTROL"             -> raw("CONTROL"),
+      "COMMAND"             -> raw(if (SystemInfo.isMac) "META" else "CONTROL"),
+      "ProductName"         -> ApplicationNamesInfo.getInstance.getFullProductName
+    )
+
+  private lazy val tipsContext = new KeymapTextContext {
+    override def isSimplifiedMacShortcuts: Boolean = SystemInfo.isMac
+  }
+
+  private lazy val unrenderedVariables =
+    Map(
+      // actions
+      "ActionTypeInfo"      -> tipsContext.getShortcutText(ShowTypeInfoAction.ActionId),
+      "ActionImplicitHints" -> tipsContext.getShortcutText("Scala.ShowImplicits"),
+      "ActionSettings"      -> tipsContext.getShortcutText(IdeActions.ACTION_SHOW_SETTINGS),
+      "ActionRun"           -> tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_RUNNER),
+      "ActionShowIntention" -> tipsContext.getShortcutText(IdeActions.ACTION_SHOW_INTENTION_ACTIONS),
+      "ActionDebug"         -> tipsContext.getShortcutText(IdeActions.ACTION_DEFAULT_DEBUGGER),
+      "ActionSetBreakpoint" -> tipsContext.getShortcutText(IdeActions.ACTION_TOGGLE_LINE_BREAKPOINT),
+      // others
+      "META"                -> "Meta",
+      "CONTROL"             -> "Control",
+      "COMMAND"             -> (if (SystemInfo.isMac) "Meta" else "Control"),
+      "ProductName"         -> ApplicationNamesInfo.getInstance.getFullProductName
+    )
+
+  final private case class Sample(templateName: String, fileName: String, breakpoint: Function1[_ >: CharSequence, JInt])
+
+  private object Sample {
+    def apply(templateName: String, fileName: String, breakpointLine: String): Sample =
+      new Sample(templateName, fileName, createBreakpointSelector(breakpointLine))
+
+    def apply(templateName: String, fileName: String): Sample =
+      new Sample(templateName, fileName, createBreakpointSelector())
+
+    def apply(templateName: String, fileName: String, index: Int): Sample =
+      new Sample(templateName, fileName, createBreakpointSelector(index))
+
+    private def createBreakpointSelector(line: String): Function1[_ >: CharSequence, JInt] =
+      str => {
+        val index = str.indexOf(line)
+        if (index >= 0) index else null
+      }
+
+    private def createBreakpointSelector(): Function1[_ >: CharSequence, JInt] =
+      _ =>  null
+
+    private def createBreakpointSelector(index: Int): Function1[_ >: CharSequence, JInt] =
+      _ => index
+  }
 }
