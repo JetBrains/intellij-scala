322ca321dad104eeca8d316ba6b705313b0c4584
antonigrodowski <tantien02@hotmail.com>
2024-09-18 13:43:11 +0200

    [new BSP plugin] BuildToolAssetsExtension implementation with adequate icons for the BSP plugin Mill integration #SCL-22430

---
+++ scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/assets/MillAssetsExtension.scala
@@ -0,0 +1,21 @@
+package org.jetbrains.plugins.scala.bsp.assets
+
+import com.intellij.openapi.util.IconLoader
+import org.jetbrains.plugins.bsp.assets.BuildToolAssetsExtension
+import org.jetbrains.plugins.bsp.config.BuildToolId
+import org.jetbrains.plugins.scala.bsp.config.MillScalaPluginConstants
+
+import javax.swing.Icon
+
+class MillAssetsExtension extends BuildToolAssetsExtension {
+  override def getToolWindowIcon: Icon = IconLoader.getIcon("icons/buildServerProtocol.svg", classOf[MillAssetsExtension])
+
+  override def getErrorTargetIcon: Icon = IconLoader.getIcon("icons/buildServerProtocolTarget_red.svg", classOf[MillAssetsExtension])
+
+  override def getPresentableName: String = "mill-bsp"
+
+  override def getBuildToolId: BuildToolId = MillScalaPluginConstants.BUILD_TOOL_ID
+
+  override def getTargetIcon: Icon = IconLoader.getIcon("icons/buildServerProtocolTarget.svg", classOf[MillAssetsExtension])
+
+}
