988cb5c2d67d871abc6fb053f8ef333f03a50a21
antonigrodowski <tantien02@hotmail.com>
2024-09-18 13:32:49 +0200

    [new BSP plugin] plugin v2 model migration of bsp and testing-support modules #SCL-22430
    - Refactoring of the bsp module to extract JUnit and terminal extension points to separate modules (necessary for the plugin v2 model migration)
    - Migration of the testing-support module to the plugin v2 model forced by bsp module changes

---
+++ bsp-builtin/bsp/src/org/jetbrains/bsp/project/importing/setup/SbtConfigSetup.scala
@@ -0,0 +1,56 @@
+package org.jetbrains.bsp.project.importing.setup
+
+import com.intellij.openapi.projectRoots.{JavaSdk, ProjectJdkTable, Sdk}
+import org.jetbrains.bsp.BspBundle
+import org.jetbrains.plugins.scala.build.{BuildMessages, BuildReporter}
+import org.jetbrains.plugins.scala.extensions.invokeAndWait
+import org.jetbrains.plugins.scala.project.Version
+import org.jetbrains.sbt.SbtUtil
+import org.jetbrains.sbt.SbtUtil.{detectSbtVersion, getDefaultLauncher, sbtVersionParam, upgradedSbtVersion}
+import org.jetbrains.sbt.project.SbtExternalSystemManager
+import org.jetbrains.sbt.project.structure.SbtStructureDump
+
+import java.io.File
+import scala.util.Try
+
+class SbtConfigSetup(dumper: SbtStructureDump, runInit: BuildReporter => Try[BuildMessages]) extends BspConfigSetup {
+
+  override def cancel(): Unit = dumper.cancel()
+  override def run(implicit reporter: BuildReporter): Try[BuildMessages] =
+    runInit(reporter)
+}
+
+object SbtConfigSetup {
+
+  /** Runs sbt with a dummy command so that the project is initialized and .bsp/sbt.json is created. */
+  def apply(baseDir: File, jdk: Sdk): SbtConfigSetup = {
+    invokeAndWait {
+      ProjectJdkTable.getInstance.preconfigure()
+    }
+    val jdkType = JavaSdk.getInstance()
+    val jdkExe = new File(jdkType.getVMExecutablePath(jdk))
+    val jdkHome = Option(jdk.getHomePath).map(new File(_))
+    val sbtLauncher = SbtUtil.getDefaultLauncher
+
+    // dummy command so that sbt will run, init and exit
+    val sbtLauncherArgs = List("early(startServer)")
+    val sbtCommands = ""
+
+    val projectSbtVersion = Version(detectSbtVersion(baseDir, getDefaultLauncher))
+    val sbtVersion = upgradedSbtVersion(projectSbtVersion)
+    val upgradeParam =
+      if (sbtVersion > projectSbtVersion)
+        List(sbtVersionParam(sbtVersion))
+      else List.empty
+
+    val vmArgs = SbtExternalSystemManager.getVmOptions(Seq.empty, jdkHome) ++ upgradeParam
+
+    val dumper = new SbtStructureDump()
+    val runInit = (reporter: BuildReporter) => dumper.runSbt(
+      baseDir, jdkExe, vmArgs,
+      Map.empty, sbtLauncher, Seq.empty, sbtLauncherArgs, sbtCommands,
+      BspBundle.message("bsp.resolver.creating.sbt.configuration"), passParentEnvironment = true
+    )(reporter)
+    new SbtConfigSetup(dumper, runInit)
+  }
+}
---
+++ bsp-builtin/bsp/src/org/jetbrains/bsp/protocol/BspCommunicationService.scala
@@ -0,0 +1,120 @@
+package org.jetbrains.bsp.protocol
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.{Project, ProjectManager, ProjectManagerListener, ProjectUtil}
+import com.intellij.openapi.util.Disposer
+import com.intellij.util.concurrency.AppExecutorUtil
+import org.jetbrains.bsp.BspServerWidgetProvider
+import org.jetbrains.bsp.settings.BspProjectSettings.BspServerConfig
+import org.jetbrains.plugins.scala.util.ScalaShutDownTracker
+
+import java.io.File
+import java.net.URI
+import java.nio.file._
+import java.util.concurrent.TimeUnit
+import scala.collection.concurrent.TrieMap
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+
+class BspCommunicationService extends Disposable {
+
+  import BspCommunicationService.{projectPath, updateWidget}
+
+  { // init
+    ScalaShutDownTracker.registerShutdownTask(() => this.dispose())
+
+    val bus = ApplicationManager.getApplication.getMessageBus.connect(this)
+    bus.subscribe(ProjectManager.TOPIC, MyProjectListener)
+  }
+
+  private val timeout = 10.minutes
+  private val cleanerPause = 10.seconds
+
+  private val comms: TrieMap[(URI, BspServerConfig), BspCommunication] = TrieMap.empty
+
+  private val executorService = AppExecutorUtil.getAppScheduledExecutorService
+
+  private val commCleaner = executorService
+    .scheduleWithFixedDelay(() => closeIdleSessions(), cleanerPause.toMillis, cleanerPause.toMillis, TimeUnit.MILLISECONDS)
+
+  private def closeIdleSessions(): Unit = {
+    val now = System.currentTimeMillis()
+    comms.values.foreach { comm =>
+      if (comm.isIdle(now, timeout))
+        comm.closeSession()
+    }
+    updateWidget()
+  }
+
+  private[protocol] def communicate(base: File, config: BspServerConfig): BspCommunication =
+    comms.getOrElseUpdate(
+      (base.getCanonicalFile.toURI, config),
+      {
+        val comm = new BspCommunication(base, config)
+        Disposer.register(this, comm)
+        updateWidget()
+        comm
+      }
+    )
+
+  def listOpenComms: Iterable[(URI, BspServerConfig)] = comms.keys
+
+  def isAlive(base: URI, config: BspServerConfig): Boolean =
+    comms.get((base, config)).exists(_.alive)
+
+  /** Close BSP connection if there is an open one associated with `base`. */
+  def closeCommunication(base: URI, config: BspServerConfig): Future[Unit] = {
+    val tryComm = comms.get(base, config)
+      .toRight(new NoSuchElementException)
+      .toTry
+
+    Future.fromTry(tryComm)
+      .flatMap(_.closeSession())(ExecutionContext.global)
+      .map(_ => updateWidget())(ExecutionContext.global)
+  }
+
+  def exitCommands(base: URI, config: BspServerConfig): Try[List[List[String]]] = {
+    comms.get(base, config)
+      .toRight(new NoSuchElementException)
+      .toTry
+      .map(_.exitCommands)
+  }
+
+  def closeAll: Future[Unit] = {
+    import ExecutionContext.Implicits.global
+    Future.traverse(comms.values)(_.closeSession()).map(_ => updateWidget())
+  }
+
+  override def dispose(): Unit = {
+    comms.values.foreach(_.closeSession())
+    commCleaner.cancel(true)
+    updateWidget()
+  }
+
+  private object MyProjectListener extends ProjectManagerListener {
+    override def projectClosed(project: Project): Unit = for {
+      path <- projectPath(project)
+      uri = Paths.get(path).toUri
+      session <- comms.view.filterKeys(_._1 == uri).values
+    } session.closeSession().map(_ => updateWidget())(ExecutionContext.global)
+  }
+}
+
+object BspCommunicationService {
+
+  def getInstance: BspCommunicationService =
+    ApplicationManager.getApplication.getService(classOf[BspCommunicationService])
+
+  private def projectPath(implicit project: Project): Option[String] =
+    Option(ProjectUtil.guessProjectDir(project))
+      .map(_.getCanonicalPath)
+
+  private def updateWidget(): Unit = {
+    val application = ApplicationManager.getApplication
+    if (!application.isDisposed) {
+      application.getMessageBus.syncPublisher(BspServerWidgetProvider.Topic).updateWidget()
+    }
+  }
+}
