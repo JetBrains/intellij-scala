84b38220ce0fbdb70731ac29e718b6b880fa4fe9
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-09-27 16:36:01 +0200

    [cbh, tests] Trigger compiler based highlighting in HighlightingCompilerConflictsTests by opening a file #SCL-23006

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/HighlightingCompilerConflictsBase.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/HighlightingCompilerConflictsBase.scala
@@ -1,20 +1,18 @@
 package org.jetbrains.plugins.scala
 package compiler.highlighting
 
-import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.{FileEditorManager, OpenFileDescriptor}
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.impl.JavaAwareProjectJdkTableImpl
 import com.intellij.openapi.roots.CompilerModuleExtension
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.pom.java.LanguageLevel
-import com.intellij.psi.PsiManager
 import com.intellij.testFramework.EdtTestUtil
 import org.jetbrains.plugins.scala.base.libraryLoaders.SmartJDKLoader
 import org.jetbrains.plugins.scala.compiler.{CompilerEvent, CompilerEventListener, ScalaCompilerTestBase}
-import org.jetbrains.plugins.scala.extensions.{inReadAction, inWriteAction}
+import org.jetbrains.plugins.scala.extensions.{inWriteAction, invokeAndWait}
 import org.jetbrains.plugins.scala.util.CompilerTestUtil.runWithErrorsFromCompiler
-import org.jetbrains.plugins.scala.util.ScalaUtil
 import org.jetbrains.plugins.scala.util.runners.{MultipleScalaVersionsRunner, RunWithScalaVersions, TestScalaVersion}
 import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.runner.RunWith
@@ -92,17 +90,20 @@ abstract class HighlightingCompilerConflictsBase(compileServerLanguageLevel: Lan
     val project = getProject
     project.getMessageBus.connect().subscribe(CompilerEventListener.topic, new CompilerEventListener {
       override def eventReceived(event: CompilerEvent): Unit = event match {
-        case CompilerEvent.CompilationFinished(_, _, _) => promise.success(())
+        case CompilerEvent.CompilationFinished(_, _, _) =>
+          if (!promise.isCompleted) {
+            promise.success(())
+          }
         case _ => ()
       }
     })
 
-    val module = ScalaUtil.getModuleForFile(virtualFile)(project)
-    val document = inReadAction(FileDocumentManager.getInstance().getDocument(virtualFile))
-    val psiFile = inReadAction(PsiManager.getInstance(project).findFile(virtualFile))
-    module.foreach { m =>
-      CompilerHighlightingService.get(project)
-        .triggerIncrementalCompilation(virtualFile, m, document, psiFile, "manual trigger from tests")
+    invokeAndWait {
+      val descriptor = new OpenFileDescriptor(getProject, virtualFile)
+      val editor = FileEditorManager.getInstance(getProject).openTextEditor(descriptor, true)
+      // The tests are running in a headless environment where focus events are not propagated.
+      // We need to call our listener manually.
+      new CompilerHighlightingEditorFocusListener(editor).focusGained()
     }
     Await.result(promise.future, 60.seconds)
   }
