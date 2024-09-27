bdef14f53b9945768b286eca1da5ee97ddd68e88
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-09-27 11:16:35 +0200

    [build] Update Scala 3 to version 3.3.4

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,7 +5,7 @@ import sbt.*
 
 object Versions {
   val scalaVersion: String = "2.13.15"
-  val scala3Version: String = "3.3.3"
+  val scala3Version: String = "3.3.4"
 
   // ATTENTION: when updating sbtVersion also update versions in MockSbt_1_0
   // NOTE: sbt-launch / bloop-launcher won't be fetched on refresh.
