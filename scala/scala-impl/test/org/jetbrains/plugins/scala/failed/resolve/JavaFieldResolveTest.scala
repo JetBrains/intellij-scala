6d47619856d97eb7572eeb79c8ef776ae3c15126
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-10-07 13:51:46 +0200

    [parser] moved now succeeding resolve test #SCL-14476

--- scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/JavaFieldResolveTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/failed/resolve/JavaFieldResolveTest.scala
@@ -2,6 +2,5 @@ package org.jetbrains.plugins.scala.failed.resolve
 
 class JavaFieldResolveTest extends FailableResolveTest("javaField") {
   def testSCL6925(): Unit = doTest()
-  def testSCL12413(): Unit = doTest()
   def testSCL12630(): Unit = doTest()
 }
--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolve/JavaFieldResolveTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolve/JavaFieldResolveTest.scala
@@ -3,4 +3,5 @@ package org.jetbrains.plugins.scala.lang.resolve
 class JavaFieldResolveTest extends SimpleResolveTest("javaField") {
   def testScl10176(): Unit = doTest()
   def testScl13747(): Unit = doTest()
+  def testSCL12413(): Unit = doTest()
 }
