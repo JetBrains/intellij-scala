21b8dfecfd0acfc693c72d6b269f770b4d4a50fe
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-09 19:22:04 +0200

    Update intellijVersion to 243.19420.21 (EAP 5)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.10.2"
 
   // ATTENTION: check the comment in `Common.newProjectWithKotlin` when updating this version
-  val intellijVersion = "243.19420.13"
+  val intellijVersion = "243.19420.21"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
