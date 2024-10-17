91b3224268785be662adb38a5df8592f7e8d59bd
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-10-17 18:25:27 +0200

    [tests] Fix the bsp path generator on Windows
    - It's a dumb fix, it doesn't modify the generator in any way, simply skips invalidly generated paths.
    - This is fine because the test is flaky on Windows, it depends on what kind of path is generated.
    
    
    (cherry picked from commit c60bbf96e673f078f03982122022672ab0559849)

--- bsp/test/org/jetbrains/bsp/project/importing/BspUtilProperties.scala
+++ bsp/test/org/jetbrains/bsp/project/importing/BspUtilProperties.scala
@@ -1,12 +1,16 @@
 package org.jetbrains.bsp.project.importing
 
-import ch.epfl.scala.bsp.testkit.gen.UtilGenerators._
+import ch.epfl.scala.bsp.testkit.gen.UtilGenerators.genUri
+import com.intellij.openapi.util.SystemInfo
 import org.jetbrains.bsp.BspUtil._
 import org.jetbrains.plugins.scala.SlowTests
-import org.junit.experimental.categories.Category
 import org.junit.Test
+import org.junit.experimental.categories.Category
+import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 import org.scalatestplus.scalacheck.Checkers
+
+import java.nio.file.{InvalidPathException, Path, Paths}
 
 @Category(Array(classOf[SlowTests]))
 class BspUtilProperties extends Checkers {
@@ -21,9 +25,27 @@ class BspUtilProperties extends Checkers {
   )
 
   @Test
-  def uriOpsToFile(): Unit = check(
-    forAll(genPath) { path =>
-      path.toUri.toFile == path.toFile
-    }
-  )
+  def uriOpsToFile(): Unit = {
+    // Copied with added InvalidPathException fix from `ch.epfl.scala.bsp.testkit.gen.UtilGenerators.genPath`
+    val genPath: Gen[Path] = (for {
+      segmentCount <- Gen.choose(0, 10)
+      segments <- Gen.listOfN(segmentCount, Gen.alphaNumStr)
+    } yield {
+      val combined = segments.foldLeft("") { (combined, seg) =>
+        if (combined.length + seg.length + 1 > 100) combined
+        else combined + "/" + seg
+      }
+
+      try Some(Paths.get(combined).toAbsolutePath)
+      catch {
+        case _: InvalidPathException if SystemInfo.isWindows => None
+      }
+    }).filter(_.nonEmpty).map(_.get)
+
+    check(
+      forAll(genPath) { path =>
+        path.toUri.toFile == path.toFile
+      }
+    )
+  }
 }
