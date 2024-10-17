377f4eec752ebc07065ab2c5f3749e39b4fe60e2
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-10-17 23:48:40 +0200

    [tests] Rewrite the EncodingCompilationTests to be much faster
    - Do not use sbt importing.
    - Instead, write them as ScalaCompilerTestBase tests, which execute much more quickly.
    
    
    (cherry picked from commit 75963568f7ce1ed9bbb8b3ee65f5a5251f40dc43)

---
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/EncodingCompilationTest.scala
@@ -0,0 +1,62 @@
+package org.jetbrains.plugins.scala.compiler
+
+import com.intellij.openapi.projectRoots.Sdk
+import org.jetbrains.plugins.scala.CompilationTests
+import org.jetbrains.plugins.scala.compiler.CompilerMessagesUtil.{assertCompilingScalaSources, assertNoErrorsOrWarnings}
+import org.jetbrains.plugins.scala.compiler.data.IncrementalityType
+import org.jetbrains.plugins.scala.project.ModuleExt
+import org.jetbrains.plugins.scala.util.runners.{MultipleScalaVersionsRunner, RunWithJdkVersions, RunWithScalaVersions, TestJdkVersion, TestScalaVersion}
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+
+import scala.jdk.CollectionConverters._
+
+@RunWith(classOf[MultipleScalaVersionsRunner])
+@RunWithScalaVersions(Array(TestScalaVersion.Scala_3_Latest))
+@RunWithJdkVersions(Array(
+  TestJdkVersion.JDK_1_8,
+  TestJdkVersion.JDK_11,
+  TestJdkVersion.JDK_17
+))
+@Category(Array(classOf[CompilationTests]))
+abstract class EncodingCompilationTestBase(override val incrementalityType: IncrementalityType) extends ScalaCompilerTestBase {
+
+  override protected def buildProcessJdk: Sdk = CompileServerLauncher.defaultSdk(getProject)
+
+  def testEncoding1(): Unit = {
+    runEncodingTest(Seq("-encoding", "UTF-8"))
+  }
+
+  def testEncoding2(): Unit = {
+    runEncodingTest(Seq("--encoding", "UTF-8"))
+  }
+
+  def testEncoding3(): Unit = {
+    runEncodingTest(Seq("-encoding:UTF-8"))
+  }
+
+  def testEncoding4(): Unit = {
+    runEncodingTest(Seq("--encoding:UTF-8"))
+  }
+
+  private def runEncodingTest(encodingSettings: Seq[String]): Unit = {
+    addFileToProjectSources("src/main/scala/Foo.scala", "class Foo")
+    val profile = getModule.scalaCompilerSettingsProfile
+    val newSettings = profile.getSettings.copy(
+      additionalCompilerOptions = encodingSettings
+    )
+    profile.setSettings(newSettings)
+
+    val messages = compiler.make().asScala.toSeq
+    assertNoErrorsOrWarnings(messages)
+
+    incrementalityType match {
+      case IncrementalityType.SBT => assertCompilingScalaSources(messages, 1)
+      case IncrementalityType.IDEA =>
+    }
+  }
+}
+
+class EncodingCompilationTest_Zinc extends EncodingCompilationTestBase(IncrementalityType.SBT)
+
+class EncodingCompilationTest_IDEA extends EncodingCompilationTestBase(IncrementalityType.IDEA)
