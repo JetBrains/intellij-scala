3c88cc2768308204bdc918e6cfe537cac66189c7
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 23:20:00 +0200

    [inspection] enable ScalaDocMissingParameterDescriptionInspection during indexing #SCL-23124
    (cherry picked from commit f876ac139f42cdc6a3d32579f588d383da37ee23)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/scaladoc/ScalaDocMissingParameterDescriptionInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/scaladoc/ScalaDocMissingParameterDescriptionInspection.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.codeInspection.scaladoc
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemHighlightType, ProblemsHolder}
+import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.tree.TokenSet
 import org.jetbrains.plugins.scala.codeInspection.ScalaInspectionBundle
@@ -10,9 +11,7 @@ import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
 import org.jetbrains.plugins.scala.lang.scaladoc.lexer.ScalaDocTokenType
 import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocTag
 
-
-class ScalaDocMissingParameterDescriptionInspection extends LocalInspectionTool {
-
+final class ScalaDocMissingParameterDescriptionInspection extends LocalInspectionTool with DumbAware {
   override def getDisplayName: String = ScalaInspectionBundle.message("display.name.missing.parameter.description")
 
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = {
@@ -38,8 +37,8 @@ class ScalaDocMissingParameterDescriptionInspection extends LocalInspectionTool 
   }
 }
 
-
 object ScalaDocMissingParameterDescriptionInspection {
+
   import org.jetbrains.plugins.scala.lang.scaladoc.parser.parsing.MyScaladocParsing._
 
   private val TagsWithValueElement = Set(PARAM_TAG, THROWS_TAG, TYPE_PARAM_TAG)
