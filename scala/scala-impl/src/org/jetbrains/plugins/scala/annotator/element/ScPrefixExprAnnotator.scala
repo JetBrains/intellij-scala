16cc2a78a5b6a690773e5f76785e75f884980f81
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-19 00:06:33 +0200

    [annotator] enable ScPrefixExprAnnotator during indexing #SCL-23124
    (cherry picked from commit 2050f8b4acd1b636455d6c03bdfd100daa7d4ea2)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScPrefixExprAnnotator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScPrefixExprAnnotator.scala
@@ -1,9 +1,10 @@
 package org.jetbrains.plugins.scala.annotator.element
 
+import com.intellij.openapi.project.DumbAware
 import org.jetbrains.plugins.scala.annotator.ScalaAnnotationHolder
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScPrefixExpr
 
-object ScPrefixExprAnnotator extends ElementAnnotator[ScPrefixExpr] {
+object ScPrefixExprAnnotator extends ElementAnnotator[ScPrefixExpr] with DumbAware {
 
   override def annotate(element: ScPrefixExpr, typeAware: Boolean)(implicit holder: ScalaAnnotationHolder): Unit = {
     for ((target, literal) <- ScNumericLiteralAnnotator.integerPrefixElement(element)) {
