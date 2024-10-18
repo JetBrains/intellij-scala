13e63c05a6664f6c58614f6256df9698e77c21cf
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 23:51:51 +0200

    [annotator] enable ScCharLiteralAnnotator during indexing #SCL-23124
    (cherry picked from commit e472bde32003eac3c7726c2b338062ba6ac28be3)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScCharLiteralAnnotator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/element/ScCharLiteralAnnotator.scala
@@ -1,10 +1,11 @@
 package org.jetbrains.plugins.scala.annotator.element
 
+import com.intellij.openapi.project.DumbAware
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.annotator.ScalaAnnotationHolder
 import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScCharLiteral
 
-object ScCharLiteralAnnotator extends ElementAnnotator[ScCharLiteral]  {
+object ScCharLiteralAnnotator extends ElementAnnotator[ScCharLiteral] with DumbAware {
   override def annotate(element: ScCharLiteral, typeAware: Boolean)(implicit holder: ScalaAnnotationHolder): Unit = {
     if (element.getValue == null) {
       holder.createErrorAnnotation(element, ScalaBundle.message("missing.char.value"))
