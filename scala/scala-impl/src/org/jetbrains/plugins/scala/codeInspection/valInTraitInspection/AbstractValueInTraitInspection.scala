33f5bdfa423ff11adf560ea1f9d204f327717926
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 18:05:34 +0200

    [inspection] enable AbstractValueInTraitInspection during indexing #SCL-23124
    (cherry picked from commit bfdc695be127c66d61bd99d5dc96005ca8b7a926)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/valInTraitInspection/AbstractValueInTraitInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/valInTraitInspection/AbstractValueInTraitInspection.scala
@@ -1,24 +1,23 @@
 package org.jetbrains.plugins.scala.codeInspection.valInTraitInspection
 
-import com.intellij.codeInspection.{LocalInspectionTool, ProblemHighlightType, ProblemsHolder}
+import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
+import com.intellij.openapi.project.DumbAware
 import org.jetbrains.plugins.scala.codeInspection.{PsiElementVisitorSimple, ScalaInspectionBundle}
-import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScValueDeclaration, ScVariableDeclaration}
+import org.jetbrains.plugins.scala.extensions.{&, ObjectExt, Parent}
+import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScValueDeclaration, ScValueOrVariableDeclaration}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBody
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTrait
 
-class AbstractValueInTraitInspection extends LocalInspectionTool {
+final class AbstractValueInTraitInspection extends LocalInspectionTool with DumbAware {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
     //todo: we should use dataflow analysis to get if it's safe to use declaration here
-    case v: ScValueDeclaration if v.getParent.isInstanceOf[ScTemplateBody] =>
+    case (v: ScValueOrVariableDeclaration) & Parent(_: ScTemplateBody) =>
       v.containingClass match {
         case _: ScTrait =>
-          holder.registerProblem(v, ScalaInspectionBundle.message("abstract.value.used.in.trait"))
-        case _ =>
-      }
-    case v: ScVariableDeclaration =>
-      v.containingClass match {
-        case _: ScTrait =>
-          holder.registerProblem(v, ScalaInspectionBundle.message("abstract.variable.used.in.trait"))
+          val message =
+            if (v.is[ScValueDeclaration]) ScalaInspectionBundle.message("abstract.value.used.in.trait")
+            else ScalaInspectionBundle.message("abstract.variable.used.in.trait")
+          holder.registerProblem(v, message)
         case _ =>
       }
     case _ =>
