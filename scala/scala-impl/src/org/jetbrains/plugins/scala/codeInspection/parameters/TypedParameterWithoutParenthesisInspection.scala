05c5711083d2b4821047e7770cb9a1ec1f4d5056
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 22:26:17 +0200

    [inspection] enable TypedParameterWithoutParenthesisInspection during indexing #SCL-23124

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/parameters/TypedParameterWithoutParenthesisInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/parameters/TypedParameterWithoutParenthesisInspection.scala
@@ -2,24 +2,27 @@ package org.jetbrains.plugins.scala.codeInspection.parameters
 
 import com.intellij.codeInsight.intention.HighPriorityAction
 import com.intellij.codeInspection.{LocalInspectionTool, LocalQuickFix, ProblemsHolder}
-import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, PsiElementVisitorSimple, ScalaInspectionBundle}
+import com.intellij.openapi.project.{DumbAware, Project}
 import org.jetbrains.plugins.scala.codeInspection.parameters.TypedParameterWithoutParenthesisInspection._
+import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, PsiElementVisitorSimple, ScalaInspectionBundle}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScParameterClause
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory
 
-class TypedParameterWithoutParenthesisInspection extends LocalInspectionTool {
+final class TypedParameterWithoutParenthesisInspection extends LocalInspectionTool with DumbAware {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
-    case parameterClause: ScParameterClause if !parameterClause.hasParenthesis && parameterClause.parameters.size == 1 =>
-      parameterClause.parameters.head.typeElement.foreach { _ =>
-        holder.registerProblem(parameterClause, getDisplayName, createQuickFix(parameterClause))
-      }
+    case parameterClause: ScParameterClause if !parameterClause.hasParenthesis && parameterClause.parameters.sizeIs == 1 =>
+      for {
+        param <- parameterClause.parameters
+        _     <- param.typeElement
+      } holder.registerProblem(parameterClause, getDisplayName, createQuickFix(parameterClause))
     case _ =>
   }
 }
 
 object TypedParameterWithoutParenthesisInspection {
-  private def createQuickFix(pc: ScParameterClause): LocalQuickFix = new AbstractFixOnPsiElement(ScalaInspectionBundle.message("surround.with.parenthesis"), pc) with HighPriorityAction {
+  private def createQuickFix(pc: ScParameterClause): LocalQuickFix = new AbstractFixOnPsiElement(ScalaInspectionBundle.message("surround.with.parenthesis"), pc)
+    with HighPriorityAction
+    with DumbAware {
     override protected def doApplyFix(pclause: ScParameterClause)
                                      (implicit project: Project): Unit = {
       val replacement = ScalaPsiElementFactory.createExpressionFromText("(" + pclause.getText + ")", pclause)
