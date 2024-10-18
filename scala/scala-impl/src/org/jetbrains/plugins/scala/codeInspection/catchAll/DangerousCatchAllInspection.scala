3398a395b4e25788d93792812ca388fccb88a9bf
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 18:47:53 +0200

    [inspection] enable DangerousCatchAllInspection during indexing #SCL-23124
    (cherry picked from commit c2b23e4f810c38af9952c660f727058ba4ccdc5f)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/catchAll/DangerousCatchAllInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/catchAll/DangerousCatchAllInspection.scala
@@ -1,46 +1,34 @@
 package org.jetbrains.plugins.scala.codeInspection.catchAll
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemHighlightType, ProblemsHolder}
+import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.plugins.scala.codeInspection.ScalaInspectionBundle
+import org.jetbrains.plugins.scala.extensions.ObjectExt
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
-import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.{ScCaseClause, ScReferencePattern, ScWildcardPattern}
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.{ScReferencePattern, ScWildcardPattern}
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScBlockExpr, ScCatchBlock}
 
-class DangerousCatchAllInspection extends LocalInspectionTool {
+final class DangerousCatchAllInspection extends LocalInspectionTool with DumbAware {
   override def isEnabledByDefault: Boolean = true
 
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = {
     new ScalaElementVisitor {
       override def visitCatchBlock(catchBlock: ScCatchBlock): Unit = {
-        val expr = catchBlock.expression.orNull
-        if (expr == null) return
-        def isInspection: (Boolean, ScCaseClause) = expr match {
-          case block: ScBlockExpr =>
-            val caseClauses = block.caseClauses.orNull
-            if (caseClauses == null || caseClauses.caseClauses.size != 1) return (false, null)
-            val caseClause = caseClauses.caseClause
-            if (caseClause == null) return (false, null)
-            val pattern = caseClause.pattern.orNull
-            if (pattern == null) return (false, null)
-            val guard = caseClause.guard.orNull
-            pattern match {
-              case _: ScWildcardPattern if guard == null => (true, caseClause)
-              case _: ScReferencePattern if guard == null => (true, caseClause)
-              case _ => (false, null)
-            }
-          case _ => (false, null)
-        }
-        if (isInspection._1) {
-          val startElement = isInspection._2.getFirstChild
-          val endElement = isInspection._2.pattern.orNull
-          if (startElement == null || endElement == null) return
-          holder.registerProblem(holder.getManager.createProblemDescriptor(startElement, endElement,
-            ScalaInspectionBundle.message("catch.all"), ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly,
-            new ReplaceDangerousCatchAllQuickFix(isInspection._2)))
+        catchBlock.expression match {
+          case Some(block: ScBlockExpr) =>
+            for {
+              caseClauses <- block.caseClauses
+              if caseClauses.caseClauses.sizeIs == 1
+              caseClause <- caseClauses.caseClauses.headOption
+              pattern <- caseClause.pattern
+              if pattern.is[ScWildcardPattern, ScReferencePattern] && caseClause.guard.isEmpty
+            } holder.registerProblem(holder.getManager.createProblemDescriptor(caseClause.getFirstChild, pattern,
+              ScalaInspectionBundle.message("catch.all"), ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly,
+              new ReplaceDangerousCatchAllQuickFix(caseClause)))
+          case _ =>
         }
       }
     }
   }
 }
-
