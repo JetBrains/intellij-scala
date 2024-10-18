22d0004a3dd7aed4ecc391483925ac75eec69db5
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 22:28:58 +0200

    [inspection] enable MultipleArgLists during indexing #SCL-23124

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/annotations/MultipleArgLists.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/annotations/MultipleArgLists.scala
@@ -1,13 +1,13 @@
 package org.jetbrains.plugins.scala.codeInspection.annotations
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
+import com.intellij.openapi.project.DumbAware
 import org.jetbrains.plugins.scala.codeInspection.{PsiElementVisitorSimple, ScalaInspectionBundle}
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScAnnotation
 
-class MultipleArgLists extends LocalInspectionTool {
-
+final class MultipleArgLists extends LocalInspectionTool with DumbAware {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
-    case annotation: ScAnnotation if annotation.constructorInvocation.arguments.length > 1 =>
+    case annotation: ScAnnotation if annotation.constructorInvocation.arguments.lengthIs > 1 =>
       holder.registerProblem(annotation, ScalaInspectionBundle.message("implementation.limitation.multiple.argument.lists"))
     case _ =>
   }
