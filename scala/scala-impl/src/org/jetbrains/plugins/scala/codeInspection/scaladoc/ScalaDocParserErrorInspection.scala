0ac30f7b4a2b1c1f8559dd73b1189b915f38842c
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 23:22:32 +0200

    [inspection] enable ScalaDocParserErrorInspection during indexing #SCL-23124
    (cherry picked from commit 7e82f4278d701cbb11ed14111a20ab6540eae453)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/scaladoc/ScalaDocParserErrorInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/scaladoc/ScalaDocParserErrorInspection.scala
@@ -1,40 +1,34 @@
 package org.jetbrains.plugins.scala.codeInspection.scaladoc
 
 import com.intellij.codeInspection._
+import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.{PsiElement, PsiElementVisitor, PsiErrorElement}
 import org.jetbrains.plugins.scala.lang.psi.api.{ScalaElementVisitor, ScalaPsiElement}
 import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocComment
 
-class ScalaDocParserErrorInspection extends LocalInspectionTool {
+final class ScalaDocParserErrorInspection extends LocalInspectionTool with DumbAware {
   override def isEnabledByDefault: Boolean = true
 
-  override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = {
-
+  override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
     new ScalaElementVisitor {
-      override def visitDocComment(s: ScDocComment): Unit = {
+      override def visitDocComment(s: ScDocComment): Unit =
         visitScaladocElement(s)
-      }
 
-      override def visitScaladocElement(element: ScalaPsiElement): Unit = {
-        for (child <- element.getChildren) {
-          child match {
-            case a: PsiErrorElement => 
-              val startElement: PsiElement = if (a.getPrevSibling == null) a else a.getPrevSibling
-              val endElement: PsiElement = if (a.getPrevSibling != null) {
-                a
-              } else if (a.getNextSibling != null) {
-                a.getNextSibling
-              } else {
-                a.getParent
-              }
-              //noinspection ReferencePassedToNls
-              holder.registerProblem(holder.getManager.createProblemDescriptor(startElement, endElement,
-                a.getErrorDescription, ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly));
-            case b: ScalaPsiElement if b.getChildren.nonEmpty => visitScaladocElement(b)
-            case _ => //do nothing
+      override def visitScaladocElement(element: ScalaPsiElement): Unit = element.getChildren.foreach {
+        case a: PsiErrorElement =>
+          val startElement: PsiElement = if (a.getPrevSibling == null) a else a.getPrevSibling
+          val endElement: PsiElement = if (a.getPrevSibling != null) {
+            a
+          } else if (a.getNextSibling != null) {
+            a.getNextSibling
+          } else {
+            a.getParent
           }
-        }        
+          //noinspection ReferencePassedToNls
+          holder.registerProblem(holder.getManager.createProblemDescriptor(startElement, endElement,
+            a.getErrorDescription, ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly));
+        case b: ScalaPsiElement if b.getChildren.nonEmpty => visitScaladocElement(b)
+        case _ => //do nothing
       }
     }
-  }
 }
