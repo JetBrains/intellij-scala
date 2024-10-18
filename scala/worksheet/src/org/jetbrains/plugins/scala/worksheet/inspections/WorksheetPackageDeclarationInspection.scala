3d9ec8922fd6e9d52b07c5b24be6c1e8ea847814
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 16:52:52 +0200

    [inspection] enable WorksheetPackageDeclarationInspection during indexing #SCL-23124
    (cherry picked from commit 9b5407fc6b907156da418d4923311fd695eb7885)

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/inspections/WorksheetPackageDeclarationInspection.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/inspections/WorksheetPackageDeclarationInspection.scala
@@ -1,11 +1,11 @@
 package org.jetbrains.plugins.scala.worksheet.inspections
 
 import com.intellij.codeInspection._
+import com.intellij.openapi.project.DumbAware
 import com.intellij.psi._
 import org.jetbrains.plugins.scala.worksheet.{WorksheetBundle, WorksheetFile}
 
-final class WorksheetPackageDeclarationInspection extends LocalInspectionTool {
-
+final class WorksheetPackageDeclarationInspection extends LocalInspectionTool with DumbAware {
   override def checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array[ProblemDescriptor] =
     file match {
       case file: WorksheetFile =>
@@ -14,6 +14,6 @@ final class WorksheetPackageDeclarationInspection extends LocalInspectionTool {
             file, range, WorksheetBundle.message("package.declarations.are.not.allowed.in.worksheets"), ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly, null
           )
         }.toArray
-      case _  => null
+      case _ => null
     }
 }
