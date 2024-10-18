3c59a9bfd05fa679ec3243456c740b50ea1f8957
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 23:18:10 +0200

    [inspection] enable ScalaDocUnknownTagInspection during indexing #SCL-23124
    (cherry picked from commit 26b983174da328c06032e0f770f8027fa384d2ad)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/scaladoc/ScalaDocUnknownTagInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/scaladoc/ScalaDocUnknownTagInspection.scala
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.scala.codeInspection.scaladoc
 
 import com.intellij.codeInspection._
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.{DumbAware, Project}
 import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, ScalaInspectionBundle}
@@ -10,7 +10,7 @@ import org.jetbrains.plugins.scala.lang.scaladoc.lexer.ScalaDocTokenType
 import org.jetbrains.plugins.scala.lang.scaladoc.parser.parsing.MyScaladocParsing
 import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocTag
 
-class ScalaDocUnknownTagInspection extends LocalInspectionTool {
+final class ScalaDocUnknownTagInspection extends LocalInspectionTool with DumbAware {
   override def isEnabledByDefault: Boolean = true
 
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = {
@@ -19,7 +19,7 @@ class ScalaDocUnknownTagInspection extends LocalInspectionTool {
         val tagNameElement = s.getNameElement
         assert(tagNameElement != null)
         assert(tagNameElement.getNode.getElementType == ScalaDocTokenType.DOC_TAG_NAME)
-        
+
         if (!MyScaladocParsing.allTags.contains(tagNameElement.getText)) {
           holder.registerProblem(holder.getManager.createProblemDescriptor(tagNameElement, getDisplayName, true,
             ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly, new ScalaDocDeleteUnknownTagInspection(s)))
@@ -42,14 +42,11 @@ class ScalaDocUnknownTagInspection extends LocalInspectionTool {
   override def getDisplayName: String = ScalaInspectionBundle.message("unknown.scaladoc.tag")
 }
 
-
-class ScalaDocDeleteUnknownTagInspection(unknownTag: ScDocTag)
-        extends AbstractFixOnPsiElement(ScalaBundle.message("delete.unknown.tag"), unknownTag) {
-
+final class ScalaDocDeleteUnknownTagInspection(unknownTag: ScDocTag)
+  extends AbstractFixOnPsiElement(ScalaBundle.message("delete.unknown.tag"), unknownTag)
+    with DumbAware {
   override def getFamilyName: String = FamilyName
 
-  override protected def doApplyFix(tag: ScDocTag)
-                                   (implicit project: Project): Unit = {
-      tag.delete()
-  }
+  override protected def doApplyFix(tag: ScDocTag)(implicit project: Project): Unit =
+    tag.delete()
 }
