2f662f4a7b988e93739680ccf2cbc749ee167a3b
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 21:39:46 +0200

    [inspection] enable DeprecatedViewBoundInspection during indexing #SCL-23124

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/implicits/DeprecatedViewBoundInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/implicits/DeprecatedViewBoundInspection.scala
@@ -1,15 +1,14 @@
 package org.jetbrains.plugins.scala.codeInspection.implicits
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.{DumbAware, Project}
 import org.jetbrains.annotations.Nls
 import org.jetbrains.plugins.scala.codeInsight.intention.types.ConvertImplicitBoundsToImplicitParameter._
 import org.jetbrains.plugins.scala.codeInspection.implicits.DeprecatedViewBoundInspection._
 import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, PsiElementVisitorSimple, ScalaInspectionBundle}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScTypeBoundsOwner
 
-class DeprecatedViewBoundInspection extends LocalInspectionTool {
-
+final class DeprecatedViewBoundInspection extends LocalInspectionTool with DumbAware {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
     case boundsOwner: ScTypeBoundsOwner if boundsOwner.viewBound.nonEmpty && canBeConverted(boundsOwner) =>
       holder.registerProblem(boundsOwner, description, new ConvertToImplicitParametersQuickFix(boundsOwner))
@@ -17,8 +16,9 @@ class DeprecatedViewBoundInspection extends LocalInspectionTool {
   }
 }
 
-class ConvertToImplicitParametersQuickFix(owner: ScTypeBoundsOwner) extends AbstractFixOnPsiElement(fixDescription, owner) {
-
+final class ConvertToImplicitParametersQuickFix(owner: ScTypeBoundsOwner)
+  extends AbstractFixOnPsiElement(fixDescription, owner)
+    with DumbAware {
   override protected def doApplyFix(boundOwner: ScTypeBoundsOwner)
                                    (implicit project: Project): Unit = {
     val addedParams = doConversion(boundOwner)
