cdbec5704b0a08553d1386ed5a56ee31cab6c34d
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 17:51:28 +0200

    [inspection] enable FloatLiteralEndingWithDecimalPointInspection during indexing #SCL-23124
    (cherry picked from commit 89f75f4c453631d8bfa580d7dc6495941d18dfba)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/literal/FloatLiteralEndingWithDecimalPointInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/literal/FloatLiteralEndingWithDecimalPointInspection.scala
@@ -1,12 +1,12 @@
 package org.jetbrains.plugins.scala.codeInspection.literal
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.{DumbAware, Project}
 import org.jetbrains.plugins.scala.codeInspection.{AbstractFixOnPsiElement, PsiElementVisitorSimple, ScalaInspectionBundle}
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.createExpressionFromText
 
-class FloatLiteralEndingWithDecimalPointInspection extends LocalInspectionTool {
+final class FloatLiteralEndingWithDecimalPointInspection extends LocalInspectionTool with DumbAware {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
     case lit: ScLiteral if lit.getText.endsWith(".") =>
       holder.registerProblem(lit, getDisplayName, new MakeDoubleFix(lit), new MakeFloatFix(lit), new AddZeroAfterDecimalPoint(lit))
@@ -14,26 +14,26 @@ class FloatLiteralEndingWithDecimalPointInspection extends LocalInspectionTool {
   }
 }
 
-class MakeDoubleFix(lit: ScLiteral) extends AbstractFixOnPsiElement(ScalaInspectionBundle.message("convert.to.floating.point", lit.getText.dropRight(1) + "d"), lit) {
-
+final class MakeDoubleFix(lit: ScLiteral)
+  extends AbstractFixOnPsiElement(ScalaInspectionBundle.message("convert.to.floating.point", lit.getText.dropRight(1) + "d"), lit)
+    with DumbAware {
   override protected def doApplyFix(l: ScLiteral)
-                                   (implicit project: Project): Unit = {
+                                   (implicit project: Project): Unit =
     l.replace(createExpressionFromText(l.getText.dropRight(1) + "d", l))
-  }
 }
 
-class MakeFloatFix(lit: ScLiteral) extends AbstractFixOnPsiElement(ScalaInspectionBundle.message("convert.to.floating.point", lit.getText.dropRight(1) + "f"), lit) {
-
+final class MakeFloatFix(lit: ScLiteral)
+  extends AbstractFixOnPsiElement(ScalaInspectionBundle.message("convert.to.floating.point", lit.getText.dropRight(1) + "f"), lit)
+    with DumbAware {
   override protected def doApplyFix(l: ScLiteral)
-                                   (implicit project: Project): Unit = {
+                                   (implicit project: Project): Unit =
     l.replace(createExpressionFromText(l.getText.dropRight(1) + "f", l))
-  }
 }
 
-class AddZeroAfterDecimalPoint(lit: ScLiteral) extends AbstractFixOnPsiElement(ScalaInspectionBundle.message("convert.to.floating.point", lit.getText + "0"), lit) {
-
+final class AddZeroAfterDecimalPoint(lit: ScLiteral)
+  extends AbstractFixOnPsiElement(ScalaInspectionBundle.message("convert.to.floating.point", lit.getText + "0"), lit)
+    with DumbAware {
   override protected def doApplyFix(l: ScLiteral)
-                                   (implicit project: Project): Unit = {
+                                   (implicit project: Project): Unit =
     l.replace(createExpressionFromText(l.getText + "0", l))
-  }
 }
