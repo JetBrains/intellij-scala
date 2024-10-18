441cb3650c50bf403f3a48cf6771075efa5a81db
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-18 21:53:35 +0200

    [inspection] enable MarkInnerCaseObjectsAsFinal during indexing #SCL-23124

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/modifiers/MarkInnerCaseObjectsAsFinal.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/modifiers/MarkInnerCaseObjectsAsFinal.scala
@@ -1,15 +1,16 @@
 package org.jetbrains.plugins.scala.codeInspection.modifiers
 
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
+import com.intellij.openapi.project.DumbAware
 import org.jetbrains.plugins.scala.codeInspection.PsiElementVisitorSimple
 import org.jetbrains.plugins.scala.lang.lexer.ScalaModifier
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScObject
 
-class MarkInnerCaseObjectsAsFinal extends LocalInspectionTool {
+final class MarkInnerCaseObjectsAsFinal extends LocalInspectionTool with DumbAware {
   override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
-      case obj@ScObject.withModifierList(ml) if !obj.isTopLevel && !ml.isFinal && ml.isCase =>
-        val quickFix = new SetModifierQuickfix(obj, ScalaModifier.Final, set = true)
-        holder.registerProblem(obj.targetToken, getDisplayName, quickFix)
-      case _ =>
-    }
+    case obj@ScObject.withModifierList(ml) if !obj.isTopLevel && !ml.isFinal && ml.isCase =>
+      val quickFix = new SetModifierQuickfix(obj, ScalaModifier.Final, set = true)
+      holder.registerProblem(obj.targetToken, getDisplayName, quickFix)
+    case _ =>
+  }
 }
