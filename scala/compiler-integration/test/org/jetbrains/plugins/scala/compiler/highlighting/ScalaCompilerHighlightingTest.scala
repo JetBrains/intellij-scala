83ad8ba7e941ca0ce57cdca2309c73f82335e714
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-10-21 16:13:24 +0200

    [tests] Run compiler highlighting tests for Scala 3.6
    (cherry picked from commit 6bb9359c1e1b9f7df6f45e845ecb4b2a2b9b0712)

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/ScalaCompilerHighlightingTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/ScalaCompilerHighlightingTest.scala
@@ -277,6 +277,10 @@ class ScalaCompilerHighlightingTest_3_5 extends ScalaCompilerHighlightingTest_3_
   override protected def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_3_5
 }
 
+class ScalaCompilerHighlightingTest_3_6 extends ScalaCompilerHighlightingTest_3_5 {
+  override protected def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_3_6
+}
+
 class ScalaCompilerHighlightingTest_3_RC extends ScalaCompilerHighlightingTest_3_3 {
   override protected def supportedIn(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_3_LTS_RC
 }
