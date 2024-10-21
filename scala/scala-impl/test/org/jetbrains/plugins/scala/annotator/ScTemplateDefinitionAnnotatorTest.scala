4aba7609ab363cb7b1267d6219b9025f48f87ef5
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-10-21 16:46:45 +0200

    [tests] rewrite ScTemplateDefinitionAnnotatorTest from SimpleTestCase to ScalaHighlightingTestLike
    It tests all errors, not only those coming from a single annotator.
    It revealed a bug #SCL-23134
    
    This change was done within #SCL-22565, before adding tests

--- scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/ScTemplateDefinitionAnnotatorTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/ScTemplateDefinitionAnnotatorTest.scala
@@ -1,92 +1,68 @@
-package org.jetbrains.plugins.scala
-package annotator
+package org.jetbrains.plugins.scala.annotator
 
-import org.jetbrains.plugins.scala.annotator.element.ScTemplateDefinitionAnnotator
-import org.jetbrains.plugins.scala.base.SimpleTestCase
-import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTemplateDefinition
+import org.jetbrains.plugins.scala.{ScalaVersion, TypecheckerTests}
+import org.jetbrains.plugins.scala.base.ScalaLightCodeInsightFixtureTestCase
 import org.junit.experimental.categories.Category
 
 @Category(Array(classOf[TypecheckerTests]))
-class ScTemplateDefinitionAnnotatorTest extends SimpleTestCase {
-  import Message._
+class ScTemplateDefinitionAnnotatorTest
+  extends ScalaLightCodeInsightFixtureTestCase
+    with ScalaHighlightingTestLike {
+
+  override protected def supportedIn(version: ScalaVersion): Boolean = version.isScala3
 
   def testTraitPassingConstructorParameters(): Unit =
-    assertMessages(messages(
+    assertMessagesText(
       """
         |trait Foo(x: Int)
         |trait Bar extends Foo(123)
         |
         |trait Baz
         |trait Qux extends Baz with Foo(123)
-        |""".stripMargin)
-    )(
-      Error("Foo(123)", "Trait Bar may not call constructor of Foo"),
-      Error("Foo(123)", "Trait Qux may not call constructor of Foo")
+        |""".stripMargin,
+      """Error(Foo(123),Trait Bar may not call constructor of Foo)
+        |Error(Foo(123),Trait Qux may not call constructor of Foo)
+        |""".stripMargin
     )
 
-  def testDontCallConstructorTwice(): Unit = {
-    assertNothing(
-      messages(
-        """
-          |trait Foo(x: Int)
-          |class F extends Foo(123)
-          |case class Bar extends F with Foo
-          |""".stripMargin
-      )
+  def testDontCallConstructorTwice(): Unit =
+    assertMessagesText(
+      """trait Foo(x: Int)
+        |class F extends Foo(123)
+        |case class Bar() extends F with Foo
+        |""".stripMargin,
+      //TODO: remove when SCL-23134 is fixed
+      """Error(Foo,Unspecified value parameters: x: Int)""".stripMargin
     )
 
-    assertMessages(
-      messages(
-        """
-          |trait Foo(x: Int)
-          |class F extends Foo(1)
-          |class Baz extends F with Foo(2)
-          |
-          |""".stripMargin
-      )
-    )(
-      Error("Foo(2)", "Trait Foo is already implemented by superclass F,its constructor cannot be called again")
+  def testDontCallConstructorTwice_1(): Unit =
+    assertMessagesText(
+      """trait Foo(x: Int)
+        |class F extends Foo(1)
+        |class Baz extends F with Foo
+        |""".stripMargin,
+      """Error(Foo(2),Trait Foo is already implemented by superclass F,its constructor cannot be called again)"""
     )
-  }
 
-  def testIndirectImplementation(): Unit = {
-    assertMessages(messages(
-      """
-        |trait Greeting(val name: String)
+  def testIndirectImplementation(): Unit =
+    assertMessagesText(
+      """trait Greeting(val name: String)
         |trait FormalGreeting extends Greeting
         |class E extends FormalGreeting
+        |""".stripMargin,
+      """Error(E,Parameterized trait Greeting is indirectly implemented,needs to be implemented directly so that arguments can be passed)
         |""".stripMargin
-    ))(
-      Error("E", "Parameterized trait Greeting is indirectly implemented,needs to be implemented directly so that arguments can be passed")
     )
-  }
 
   def testSCL21122(): Unit =
-    assertNothing(
-      messages(
-        """
-          |object TraitWithDefaultPramas {
-          |  trait IFoo(x: String = "foo") {
-          |    val name = x
-          |  }
-          |
-          |  class Foo extends IFoo // compile passed, but idea reports error
-          |}
-          |""".stripMargin
-      )
+    assertNoErrors(
+      """object TraitWithDefaultParams {
+        |  trait IFoo(x: String = "foo") {
+        |    val name = x
+        |  }
+        |
+        |  class Foo extends IFoo // compile passed, but idea reports error
+        |}
+        |""".stripMargin
     )
-
-  def messages(code: String): List[Message] = {
-    val file: ScalaFile = code.parse
-
-    implicit val mock: AnnotatorHolderMock = new AnnotatorHolderMock(file)
-
-    file.depthFirst().filterByType[ScTemplateDefinition].foreach { pte =>
-      ScTemplateDefinitionAnnotator.annotate(pte, typeAware = true)
-    }
-
-    mock.annotations
-  }
 }
