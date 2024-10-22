4f1ab0dceaea96201db89d436da2440a7c887c3d
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-22 22:08:34 +0200

    [completion] enable sbt version completion contributor during indexing #SCL-21859

--- scala/integration/properties/test/org/jetbrains/sbt/language/completion/SbtVersionPropertyCompletionTest.scala
+++ scala/integration/properties/test/org/jetbrains/sbt/language/completion/SbtVersionPropertyCompletionTest.scala
@@ -1,12 +1,15 @@
 package org.jetbrains.sbt.language.completion
 
 import com.intellij.lang.properties.PropertiesFileType
+import com.intellij.testFramework.TestIndexingModeSupporter.IndexingMode
 import org.jetbrains.plugins.scala.lang.completion3.base.ScalaCompletionTestBase
 import org.jetbrains.plugins.scala.lang.completion3.base.ScalaCompletionTestBase.DefaultInvocationCount
 import org.jetbrains.plugins.scala.packagesearch.api.PackageSearchClientTesting
 import org.jetbrains.plugins.scala.packagesearch.util.DependencyUtil
+import org.jetbrains.plugins.scala.util.runners.WithIndexingMode
 import org.jetbrains.sbt.language.completion.SbtVersionCompletionContributor.{SbtGroupId, SbtLaunchArtifactId}
 
+@WithIndexingMode(mode = IndexingMode.DUMB_EMPTY_INDEX)
 final class SbtVersionPropertyCompletionTest
   extends ScalaCompletionTestBase
     with PackageSearchClientTesting {
