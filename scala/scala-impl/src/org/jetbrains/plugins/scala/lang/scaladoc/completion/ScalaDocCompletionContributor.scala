28008c9d24a942828a5aa8dd2fe2f42880c1fd8c
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2024-10-22 22:36:09 +0200

    [completion] enable scaladoc tags completion contributor during indexing #SCL-21859

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/completion/ScalaDocCompletionContributor.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/completion/ScalaDocCompletionContributor.scala
@@ -2,38 +2,42 @@ package org.jetbrains.plugins.scala.lang.scaladoc.completion
 
 import com.intellij.codeInsight.completion._
 import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.project.DumbAware
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
 import org.jetbrains.plugins.scala.lang.completion.{ScalaCompletionContributor, positionFromParameters}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScTypeAlias}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ScClass, ScTrait}
+import org.jetbrains.plugins.scala.extensions.{ObjectExt, OptionExt, PsiElementExt}
 import org.jetbrains.plugins.scala.lang.scaladoc.lexer.ScalaDocTokenType
 import org.jetbrains.plugins.scala.lang.scaladoc.parser.parsing.MyScaladocParsing
 import org.jetbrains.plugins.scala.lang.scaladoc.psi.api.ScDocComment
 
-class ScalaDocCompletionContributor extends ScalaCompletionContributor {
-  extend(CompletionType.BASIC, PlatformPatterns.psiElement(ScalaDocTokenType.DOC_TAG_NAME), new CompletionProvider[CompletionParameters]() {
+final class ScalaDocCompletionContributor extends ScalaCompletionContributor with DumbAware {
+  extend(CompletionType.BASIC, PlatformPatterns.psiElement(ScalaDocTokenType.DOC_TAG_NAME), new CompletionProvider[CompletionParameters] {
     override def addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet): Unit = {
-      var posParent = positionFromParameters(parameters).getContext
-      while (posParent != null && !posParent.isInstanceOf[ScDocComment]) {
-        posParent = posParent.getContext
-      }
-      
-      if (posParent != null) {
-        val allowedTags = posParent.asInstanceOf[ScDocComment].getOwner match {
+      val position = positionFromParameters(parameters)
+      val posParent = position.contexts
+        .dropWhile(!_.is[ScDocComment])
+        .nextOption()
+        .filterByType[ScDocComment]
+
+      posParent.foreach { parent =>
+        val allowedTags = parent.getOwner match {
           case _ : ScFunction => MyScaladocParsing.allTags
           case _ : ScClass => MyScaladocParsing.allTags - MyScaladocParsing.RETURN_TAG
           case _ : ScTypeAlias | _: ScTrait => MyScaladocParsing.allTags --
-                  Set(MyScaladocParsing.RETURN_TAG, MyScaladocParsing.THROWS_TAG, MyScaladocParsing.PARAM_TAG)
+            Set(MyScaladocParsing.RETURN_TAG, MyScaladocParsing.THROWS_TAG, MyScaladocParsing.PARAM_TAG)
           case _ => MyScaladocParsing.allTags -- MyScaladocParsing.tagsWithParameters - MyScaladocParsing.RETURN_TAG
         }
 
-        for (tag <- allowedTags) {
+        allowedTags.foreach { tag =>
           result.addElement(new LookupElement {
             override def getLookupString: String = tag.substring(1)
           })
         }
       }
+
       result.stopHere()
     }
   })
