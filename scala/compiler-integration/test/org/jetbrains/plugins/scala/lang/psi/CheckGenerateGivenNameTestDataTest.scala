a5f6c6d6bd6e6f01b2f71cefe4a5bb0162a0830d
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-11-20 16:13:36 +0100

    [named-tuples] fixed generateGivenName for named tuples #SCL-22569

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/psi/CheckGenerateGivenNameTestDataTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/psi/CheckGenerateGivenNameTestDataTest.scala
@@ -3,4 +3,4 @@ package org.jetbrains.plugins.scala.lang.psi
 import org.jetbrains.plugins.scala.{CheckTestDataTestBase, LatestScalaVersions}
 
 // checks if the test data in GenerateGivenNameTest.allTests is correct
-class CheckGenerateGivenNameTestDataTest extends CheckTestDataTestBase(GenerateGivenNameTest, LatestScalaVersions.Scala_3)
+class CheckGenerateGivenNameTestDataTest extends CheckTestDataTestBase(GenerateGivenNameTest, LatestScalaVersions.Scala_3_6)
