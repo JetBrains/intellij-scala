e766a5951f6823aeb68b58f9395673176c447e8d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-11-25 12:11:59 +0100

    [tests] Convert MaxJvmHeapParameterTest to JUnit 5 and ignore the process environment in the test expectations #SCL-23251
    - These tests were machine/environment specific and were failing when there were different environment variables than the ones expected.

--- sbt/sbt-impl/test/org/jetbrains/sbt/shell/MaxJvmHeapParameterTest.scala
+++ sbt/sbt-impl/test/org/jetbrains/sbt/shell/MaxJvmHeapParameterTest.scala
@@ -1,21 +1,21 @@
 package org.jetbrains.sbt.shell
 
 import com.intellij.openapi.util.io.FileUtil
-import junit.framework.TestCase
 import org.jetbrains.sbt.JvmMemorySize
 import org.jetbrains.sbt.project.settings.SbtExecutionSettings
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.{Test, TestInfo}
 
 import java.io.File
 
-class MaxJvmHeapParameterTest extends TestCase {
-  import org.junit.Assert._
+class MaxJvmHeapParameterTest {
 
   val hiddenDefaultSize = JvmMemorySize.Megabytes(1500)
   val hiddenDefaultParam = "-Xmx" + hiddenDefaultSize
   val superShellDisabled = "-Dsbt.supershell=false"
 
-  def buildParamSeq(userOpts: String*)(jvmOpts: String*): Seq[String] = {
-    val workingDir = FileUtil.createTempDirectory("maxHeapJvmParamTest", getName, true)
+  def buildParamSeq(userOpts: String*)(jvmOpts: String*)(implicit testInfo: TestInfo): Seq[String] = {
+    val workingDir = FileUtil.createTempDirectory("maxHeapJvmParamTest", testInfo.getDisplayName, true)
 
     if (jvmOpts.nonEmpty) {
       val jvmOptsFile = new File(workingDir,".jvmopts")
@@ -37,7 +37,7 @@ class MaxJvmHeapParameterTest extends TestCase {
       shellDebugMode = false,
       preferScala2 = true,
       userSetEnvironment = Map.empty,
-      passParentEnvironment = true,
+      passParentEnvironment = false,
       useSeparateCompilerOutputPaths = false,
       separateProdTestSources = false
     )
@@ -52,35 +52,40 @@ class MaxJvmHeapParameterTest extends TestCase {
        max(hidden default xmx, xms aus jvmopts
    */
 
-  def testUserSettingsSmallerThanHiddenDefault(): Unit = {
+  @Test
+  def userSettingsSmallerThanHiddenDefault(implicit testInfo: TestInfo): Unit = {
     assertEquals(
       Seq(superShellDisabled,"-Xmx4g", "-Xms4g", "-Xmx1g"),
       buildParamSeq("-Xmx1g")("-Xmx4g", "-Xms4g")
     )
   }
 
-  def testUserSettingsGreaterThanHiddenDefault(): Unit = {
+  @Test
+  def userSettingsGreaterThanHiddenDefault(implicit testInfo: TestInfo): Unit = {
     assertEquals(
       Seq(superShellDisabled,"-Xmx4g", "-Xms4g", "-Xmx2g"),
       buildParamSeq("-Xmx2g")("-Xmx4g", "-Xms4g")
     )
   }
 
-  def testNoSettings(): Unit = {
+  @Test
+  def noSettings(implicit testInfo: TestInfo): Unit = {
     assertEquals(
       Seq(hiddenDefaultParam, superShellDisabled),
       buildParamSeq()()
     )
   }
 
-  def testNoSettingsWithXmsSmallerThanDefaultParam(): Unit = {
+  @Test
+  def noSettingsWithXmsSmallerThanDefaultParam(implicit testInfo: TestInfo): Unit = {
     assertEquals(
       Seq(hiddenDefaultParam, superShellDisabled, "-Xms1g"),
       buildParamSeq("-Xms1g")()
     )
   }
 
-  def testNoSettingsWithXmsGreaterThanDefaultParam(): Unit = {
+  @Test
+  def noSettingsWithXmsGreaterThanDefaultParam(implicit testInfo: TestInfo): Unit = {
     assertEquals(
       Seq(superShellDisabled, "-Xms2g"),
       buildParamSeq("-Xms2g")()
