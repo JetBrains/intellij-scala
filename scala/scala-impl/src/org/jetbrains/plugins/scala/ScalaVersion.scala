e6e1c7c52481c4ef9164af70b06e74800d5abcec
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-01-09 09:55:52 +0100

    [tests] Run tests against Scala version 3.6.3-RC2

--- scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
@@ -118,7 +118,7 @@ object LatestScalaVersions {
   val Scala_3_LTS_RC: ScalaVersion = Scala_3_3.withMinor("5-RC1")
 
   // Scala Next RC
-  val Scala_3_Next_RC = new ScalaVersion(ScalaLanguageLevel.Scala_3_6, "3-RC1")
+  val Scala_3_Next_RC = new ScalaVersion(ScalaLanguageLevel.Scala_3_6, "3-RC2")
 
   val allScala2: Seq[ScalaVersion] = Seq(
     Scala_2_9,
