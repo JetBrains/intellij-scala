245c87c1fea87fb7010c137b6074e05736207b40
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-03-04 15:17:31 +0100

    [optimize imports tests] added tests for optimize imports with scalafmt with local imports (#SCL-23213, #SCL-21765, #SCL-CR-989)

---
+++ scala/scala-impl/testdata/optimize/scalafmt/SCL21765.scala
@@ -0,0 +1,28 @@
+// Notification message: null
+import scala.collection.immutable.{AbstractSeq, List, Seq, StringOps, StringView}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val x1: Seq[_] = ???
+    val x2: List[_] = ???
+    val x6: AbstractSeq[_] = ???
+    val x3: StringOps = ???
+    val x5: StringView = ???
+  }
+}
+/*
+import scala.collection.immutable.{
+  AbstractSeq, List, Seq, StringOps,
+  StringView
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val x1: Seq[_] = ???
+    val x2: List[_] = ???
+    val x6: AbstractSeq[_] = ???
+    val x3: StringOps = ???
+    val x5: StringView = ???
+  }
+}
+*/
