33f9f28b040e2d2cdd16e5b8fd465e9034fb7f69
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-03-06 16:29:19 +0100

    [cbh] Refactor CompilerOptionsTest as a JUnit 5 dynamic test suite #SCL-22142

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/CompilerOptionsTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/CompilerOptionsTest.scala
@@ -1,49 +1,78 @@
 package org.jetbrains.plugins.scala.compiler.highlighting
 
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.{DynamicTest, TestFactory}
 
 class CompilerOptionsTest {
 
-  @Test
-  def fatalWarningsFlag(): Unit = {
-    val scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Xfatal-warnings", "-Wunused:implicits", "-Wnumeric-widen")
-    val flag = CompilerOptions.containsFatalWarnings(scalacOptions)
-    assertTrue(flag, "Fatal warnings flag should be enabled")
+  @TestFactory
+  def fatalWarningsTests(): Array[DynamicTest] = {
+    def assertFatalWarningsFlag(scalacOptions: Seq[String], expectedFlag: Boolean): Unit = {
+      val actualFlag = CompilerOptions.containsFatalWarnings(scalacOptions)
+      assertEquals(expectedFlag, actualFlag, s"Fatal warnings flag should be $expectedFlag for scalacOptions: $scalacOptions")
+    }
+
+    case class FatalWarningsTestCase(displayName: String, scalacOptions: Seq[String], expectedFlag: Boolean)
+
+    Array(
+      FatalWarningsTestCase(
+        displayName = "fatalWarningsFlag",
+        scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Xfatal-warnings", "-Wunused:implicits", "-Wnumeric-widen"),
+        expectedFlag = true
+      ),
+      FatalWarningsTestCase(
+        displayName = "werrorFlag",
+        scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Werror", "-Wunused:implicits", "-Wnumeric-widen"),
+        expectedFlag = true
+      ),
+      FatalWarningsTestCase(
+        displayName = "noFlag",
+        scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Wunused:implicits", "-Wnumeric-widen"),
+        expectedFlag = false
+      )
+    ).map { case FatalWarningsTestCase(displayName, scalacOptions, expectedFlag) =>
+      dynamicTest(displayName, () => assertFatalWarningsFlag(scalacOptions, expectedFlag))
+    }
   }
 
-  @Test
-  def wErrorFlag(): Unit = {
-    val scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Werror", "-Wunused:implicits", "-Wnumeric-widen")
-    val flag = CompilerOptions.containsFatalWarnings(scalacOptions)
-    assertTrue(flag, "Fatal warnings flag should be enabled")
-  }
+  @TestFactory
+  def unusedImportsTests(): Array[DynamicTest] = {
+    def assertUnusedImportsFlag(scalacOptions: Seq[String], expectedFlag: Boolean): Unit = {
+      val actualFlag = CompilerOptions.containsUnusedImports(scalacOptions)
+      assertEquals(expectedFlag, actualFlag, s"Unused imports flag should be $expectedFlag for scalacOptions: $scalacOptions")
+    }
 
-  @Test
-  def unusedImportsFlag1(): Unit = {
-    val scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Xfatal-warnings", "-Wunused:implicits", "-Wnumeric-widen")
-    val flag = CompilerOptions.containsUnusedImports(scalacOptions)
-    assertTrue(flag, "Unused imports flag should be enabled")
-  }
+    case class UnusedImportsTestCase(displayName: String, scalacOptions: Seq[String], expectedFlag: Boolean)
 
-  @Test
-  def unusedImportsFlag2(): Unit = {
-    val scalacOptions = Seq("-Wunused:patvars,imports,privates", "-Wunused:locals,explicits", "-Xfatal-warnings", "-Wunused:implicits", "-Wnumeric-widen")
-    val flag = CompilerOptions.containsUnusedImports(scalacOptions)
-    assertTrue(flag, "Unused imports flag should be enabled")
-  }
-
-  @Test
-  def unusedImportsFlag3(): Unit = {
-    val scalacOptions = Seq("-Wnumeric-widen", "-Wunused:all", "-Werror")
-    val flag = CompilerOptions.containsUnusedImports(scalacOptions)
-    assertTrue(flag, "Unused imports flag should be enabled")
-  }
-
-  @Test
-  def unusedImportsFlag4(): Unit = {
-    val scalacOptions = Seq("-Wunused:all,privates,imports,explicits", "-Werror")
-    val flag = CompilerOptions.containsUnusedImports(scalacOptions)
-    assertTrue(flag, "Unused imports flag should be enabled")
+    Array(
+      UnusedImportsTestCase(
+        displayName = "manyUnusedFlagsIncludingImports",
+        scalacOptions = Seq("-Wunused:locals,explicits", "-Xfatal-warnings", "-Wunused:patvars,imports,privates", "-Wnumeric-widen", "-Wunused:implicits"),
+        expectedFlag = true
+      ),
+      UnusedImportsTestCase(
+        displayName = "unusedAllFlag",
+        scalacOptions = Seq("-Wnumeric-widen", "-Wunused:all", "-Werror"),
+        expectedFlag = true
+      ),
+      UnusedImportsTestCase(
+        displayName = "unusedAllAndImports",
+        scalacOptions = Seq("-Wunused:all,privates,imports,explicits", "-Werror"),
+        expectedFlag = true
+      ),
+      UnusedImportsTestCase(
+        displayName = "noUnusedImportsFlag",
+        scalacOptions = Seq("-Wunused:patvars,privates", "-Wunused:locals,explicits", "-Wunused:implicits", "-Wnumeric-widen"),
+        expectedFlag = false
+      ),
+      UnusedImportsTestCase(
+        displayName = "repeateadUnusedImports",
+        scalacOptions = Seq("-Wunused:imports,patvars,imports", "-Wunused:imports,explicits", "-Wunused:imports", "-Wnumeric-widen"),
+        expectedFlag = true
+      )
+    ).map { case UnusedImportsTestCase(displayName, scalacOptions, expectedFlag) =>
+      dynamicTest(displayName, () => assertUnusedImportsFlag(scalacOptions, expectedFlag))
+    }
   }
 }
