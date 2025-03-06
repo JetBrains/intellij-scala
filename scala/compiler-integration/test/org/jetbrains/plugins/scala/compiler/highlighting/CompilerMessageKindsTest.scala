36212df43836661de043a93d2a7e5d887b4dc88e
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-03-06 17:12:27 +0100

    [cbh] Refactor CompilerMessageKindsTest as a JUnit 5 dynamic test suite #SCL-22142 fixed

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/CompilerMessageKindsTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/highlighting/CompilerMessageKindsTest.scala
@@ -3,187 +3,148 @@ package org.jetbrains.plugins.scala.compiler.highlighting
 import com.intellij.codeInsight.daemon.impl.HighlightInfoType
 import org.jetbrains.jps.incremental.scala.MessageKind
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.{DynamicTest, TestFactory}
 
 class CompilerMessageKindsTest {
 
-  @Test
-  def wrongRef1(): Unit = {
-    val text = "Value myValue is not a member of MyType"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.WRONG_REF
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
+  @TestFactory
+  def highlightInfoTypeTests(): Array[DynamicTest] = {
+    case class TestCase(displayName: String, text: String, kind: MessageKind, fatalWarningsFlag: Boolean, unusedImportsFlag: Boolean, expected: HighlightInfoType)
 
-  @Test
-  def wrongRef2(): Unit = {
-    val text = "Not found: myValue"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.WRONG_REF
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
+    def assertHighlightInfoType(testCase: TestCase): Unit = {
+      val TestCase(_, text, kind, fatalWarningsFlag, unusedImportsFlag, expected) = testCase
+      val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag, unusedImportsFlag)
+      assertEquals(expected, actual)
+    }
 
-  @Test
-  def wrongRef3(): Unit = {
-    val text = "Cannot find symbol myValue"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.WRONG_REF
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def errorUnusedImportFatalWarningsWithFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.ERROR
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = true)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def errorUnusedImportWerrorWithFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.ERROR
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = true)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def errorUnusedImportNoFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.UNUSED_SYMBOL
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def errorUnusedImportNoFatalWarningsNoFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.UNUSED_SYMBOL
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def regularError(): Unit = {
-    val text = "Some random compiler error"
-    val kind = MessageKind.Error
-    val expected = HighlightInfoType.ERROR
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def upgradeWarningUnusedImportFatalWarningsWithFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.ERROR
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = true)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def warningUnusedImportNoFatalWarningsWithFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.WARNING
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = true)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def upgradeWarningUnusedImportWerrorWithFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.ERROR
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = true)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def warningUnusedImportNoFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.UNUSED_SYMBOL
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def warningUnusedImportNoFatalWarningsNoFlag(): Unit = {
-    val text = "Unused import"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.UNUSED_SYMBOL
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def regularWarning(): Unit = {
-    val text = "Some random compiler warning"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.WARNING
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def upgradeRegularWarningToErrorFatalWarnings(): Unit = {
-    val text = "Some random compiler warning"
-    val kind = MessageKind.Warning
-    val expected = HighlightInfoType.ERROR
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = true, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def regularInfo(): Unit = {
-    val text = "Some random compiler info"
-    val kind = MessageKind.Info
-    val expected = HighlightInfoType.WEAK_WARNING
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def other1(): Unit = {
-    val text = "Some random compiler internal builder error"
-    val kind = MessageKind.InternalBuilderError
-    val expected = HighlightInfoType.INFORMATION
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def other2(): Unit = {
-    val text = "Some random jps info"
-    val kind = MessageKind.JpsInfo
-    val expected = HighlightInfoType.INFORMATION
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def other3(): Unit = {
-    val text = "Some random progress message"
-    val kind = MessageKind.Progress
-    val expected = HighlightInfoType.INFORMATION
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
-  }
-
-  @Test
-  def other4(): Unit = {
-    val text = "Some random other message"
-    val kind = MessageKind.Other
-    val expected = HighlightInfoType.INFORMATION
-    val actual = CompilerMessageKinds.highlightInfoType(kind, text, fatalWarningsFlag = false, unusedImportsFlag = false)
-    assertEquals(expected, actual)
+    Array(
+      TestCase(
+        displayName = "wrongRef1",
+        text = "Value myValue is not a member of MyType",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.WRONG_REF),
+      TestCase(
+        displayName = "wrongRef2",
+        text = "Not found: myValue",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.WRONG_REF),
+      TestCase(
+        displayName = "wrongRef3",
+        text = "Cannot find symbol myValue",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.WRONG_REF),
+      TestCase(
+        displayName = "errorUnusedImportFatalWarningsWithFlag",
+        text = "Unused import",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = true,
+        unusedImportsFlag = true,
+        expected = HighlightInfoType.ERROR),
+      TestCase(
+        displayName = "errorUnusedImportNoFlag",
+        text = "Unused import",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = true,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.UNUSED_SYMBOL),
+      TestCase(
+        displayName = "errorUnusedImportNoFatalWarningsNoFlag",
+        text = "Unused import",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.UNUSED_SYMBOL),
+      TestCase(
+        displayName = "regularError",
+        text = "Some random compiler error",
+        kind = MessageKind.Error,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.ERROR),
+      TestCase(
+        displayName = "upgradeWarningUnusedImportFatalWarningsWithFlag",
+        text = "Unused import",
+        kind = MessageKind.Warning,
+        fatalWarningsFlag = true,
+        unusedImportsFlag = true,
+        expected = HighlightInfoType.ERROR),
+      TestCase(
+        displayName = "warningUnusedImportNoFatalWarningsWithFlag",
+        text = "Unused import",
+        kind = MessageKind.Warning,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = true,
+        expected = HighlightInfoType.WARNING),
+      TestCase(
+        displayName = "warningUnusedImportFatalWarningsNoFlag",
+        text = "Unused import",
+        kind = MessageKind.Warning,
+        fatalWarningsFlag = true,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.UNUSED_SYMBOL),
+      TestCase(
+        displayName = "warningUnusedImportNoFatalWarningsNoFlag",
+        text = "Unused import",
+        kind = MessageKind.Warning,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.UNUSED_SYMBOL),
+      TestCase(
+        displayName = "regularWarning",
+        text = "Some random compiler warning",
+        kind = MessageKind.Warning,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.WARNING),
+      TestCase(
+        displayName = "upgradeRegularWarningToErrorFatalWarnings",
+        text = "Some random compiler warning",
+        kind = MessageKind.Warning,
+        fatalWarningsFlag = true,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.ERROR),
+      TestCase(
+        displayName = "regularInfo",
+        text = "Some random compiler info",
+        kind = MessageKind.Info,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.WEAK_WARNING),
+      TestCase(
+        displayName = "internalBuilderError",
+        text = "Some random compiler internal builder error",
+        kind = MessageKind.InternalBuilderError,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.INFORMATION),
+      TestCase(
+        displayName = "jpsInfo",
+        text = "Some random jps info",
+        kind = MessageKind.JpsInfo,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.INFORMATION),
+      TestCase(
+        displayName = "progress",
+        text = "Some random progress message",
+        kind = MessageKind.Progress,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.INFORMATION),
+      TestCase(
+        displayName = "other",
+        text = "Some random other message",
+        kind = MessageKind.Other,
+        fatalWarningsFlag = false,
+        unusedImportsFlag = false,
+        expected = HighlightInfoType.INFORMATION)
+    ).map(testCase => dynamicTest(testCase.displayName, () => assertHighlightInfoType(testCase)))
   }
 }
