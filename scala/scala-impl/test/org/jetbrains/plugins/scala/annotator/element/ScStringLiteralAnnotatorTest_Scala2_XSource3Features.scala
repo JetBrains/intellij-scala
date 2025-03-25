88477b002fe816dc6bbc58af85867cc636479dd9
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-03-25 11:03:24 +0100

    [annotator, tests] Revive the ScStringLiteralAnnotatorTests #SCL-23715
    - These tests were written in a JUnit format which is only recognized by IDEA, but not by the junit-interface sbt plugin, so these tests could not be run in sbt and in our CI.

--- scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/element/ScStringLiteralAnnotatorTest_Scala2_XSource3Features.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/element/ScStringLiteralAnnotatorTest_Scala2_XSource3Features.scala
@@ -2,23 +2,21 @@ package org.jetbrains.plugins.scala.annotator.element
 
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
-import junit.framework.Test
+import junit.framework.{Test, TestCase}
 import org.jetbrains.plugins.scala.ScalaVersion
 
 //NOTE: reusing Scala 3 test data as they Scala 3 and Scala 2 + xSourceFeatures flag have the same semantics
-class ScStringLiteralAnnotatorTest_Scala2_XSource3Features
-  extends ScStringLiteralAnnotatorTestBase("/annotator/string_literals/scala3") {
-
-  override def supportedInScalaVersion(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_2_13
-
-  override def setUp(project: Project): Unit = {
-    super.setUp(project)
-
-    val module = ModuleManager.getInstance(project).getModules()(0)
-    addCompilerOptions(module, Seq("-Xsource:3", "-Xsource-features:unicode-escapes-raw"))
-  }
-}
+class ScStringLiteralAnnotatorTest_Scala2_XSource3Features extends TestCase
 
 object ScStringLiteralAnnotatorTest_Scala2_XSource3Features {
-  def suite: Test = new ScStringLiteralAnnotatorTest_Scala2_XSource3Features
+  final def suite: Test = new ScStringLiteralAnnotatorTestBase("/annotator/string_literals/scala3") {
+    override def supportedInScalaVersion(version: ScalaVersion): Boolean = version == ScalaVersion.Latest.Scala_2_13
+
+    override def setUp(project: Project): Unit = {
+      super.setUp(project)
+
+      val module = ModuleManager.getInstance(project).getModules()(0)
+      addCompilerOptions(module, Seq("-Xsource:3", "-Xsource-features:unicode-escapes-raw"))
+    }
+  }
 }
