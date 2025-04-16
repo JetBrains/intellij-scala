86b895bce43c678ba76adc3d28d0f5877dbbe5ce
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-04-16 11:23:24 +0200

    made ScTypeDefinitionImpl.baseCompanion linear #SCL-23690
    (cherry picked from commit 21f2ed8466ec14157143f5d08e17e33eebae365b)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/ScTypeDefinitionImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/toplevel/typedef/ScTypeDefinitionImpl.scala
@@ -10,7 +10,8 @@ import com.intellij.psi.stubs.StubElement
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.plugins.scala.JavaArrayFactoryUtil.ScTypeDefinitionFactory
 import org.jetbrains.plugins.scala.ScalaBundle
-import org.jetbrains.plugins.scala.caches.{BlockModificationTracker, ModTracker, cached, cachedWithRecursionGuard}
+import org.jetbrains.plugins.scala.caches.stats.Tracer
+import org.jetbrains.plugins.scala.caches.{BlockModificationTracker, CacheInUserData, ModTracker, cached, cachedInUserData, cachedWithRecursionGuard}
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.TokenSets.TYPE_DEFINITIONS
 import org.jetbrains.plugins.scala.lang.lexer._
@@ -196,8 +197,11 @@ abstract class ScTypeDefinitionImpl[T <: ScTemplateDefinition](stub: ScTemplateD
       siblings.find(isCompanion)
     }
 
-    def findByAst: Option[ScTypeDefinition] = {
-
+    // A fallback for when no context exists (which should basically never be the case).
+    // Finds the companion via a search of neighboring elements
+    // Because this search is done for all type definitions within a context,
+    // this search is quadratic in the number of type definitions.
+    def findByAstViaDirectSearch: Option[ScTypeDefinition] = {
       var sibling: PsiElement = sameElementInContext
 
       while (sibling != null) {
@@ -222,6 +226,34 @@ abstract class ScTypeDefinitionImpl[T <: ScTemplateDefinition](stub: ScTemplateD
       }
 
       None
+    }
+
+    // find the companion via a cached map of all TypeDefinitions under the context
+    def findByAst: Option[ScTypeDefinition] = {
+      val ctx = this.getContext
+      if (ctx == null)
+        return findByAstViaDirectSearch
+
+      val (types, objects) = cachedInUserData("ScTypeDefinitionImpl.baseCompanion.findByAst", ctx, BlockModificationTracker(ctx)) {
+        val types = Map.newBuilder[String, ScTypeDefinition]
+        val objects = Map.newBuilder[String, ScObject]
+
+        var current = ctx.getFirstChild
+        while (current != null) {
+
+          current match {
+            case td: ScTypeDefinition if td.is[ScClass, ScTrait, ScEnum] => types += td.name -> td
+            case o: ScObject => objects += o.name -> o
+            case _ =>
+          }
+
+          current = current.getNextSibling
+        }
+        (types.result(), objects.result())
+      }
+
+      if (isObject) types.get(thisName)
+      else objects.get(thisName)
     }
 
     val contextStub = getContext match {
