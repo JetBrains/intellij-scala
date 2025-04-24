c34d0968a83ae23d368398e0dc5a35b254a196dd
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-04-24 11:43:15 +0200

    [fus] update SbtSettingsCollector scheme to suggestions made in #FUS-5557
    Also related to #SCL-23660

--- scala/scala-impl/src/org/jetbrains/plugins/scala/statistics/SbtSettingsCollector.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/statistics/SbtSettingsCollector.scala
@@ -2,11 +2,11 @@ package org.jetbrains.plugins.scala.statistics
 
 import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.eventLog.EventLogGroup
-import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.eventLog.events.{BooleanEventField, EventFields, EventPair, StringEventField}
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.project.Version
-import org.jetbrains.plugins.scala.statistics.SbtSettingsCollector.Events
+import org.jetbrains.plugins.scala.statistics.SbtSettingsCollector.{Events, Fields}
 import org.jetbrains.sbt.project.settings.SbtProjectSettings
 import org.jetbrains.sbt.settings.SbtSettings
 
@@ -64,20 +64,22 @@ class SbtSettingsCollector extends ProjectUsagesCollector {
   }
 
   private def collectSbtProjectSettingsMetrics(usages: ju.Set[MetricEvent], project: Project, projectSettings: SbtProjectSettings): Unit = {
-    usages.add(Events.ResolveClassifiers.metric(projectSettings.resolveClassifiers))
-    usages.add(Events.ResolveSbtClassifiers.metric(projectSettings.resolveSbtClassifiers))
-    usages.add(Events.PreferScala2.metric(projectSettings.preferScala2))
-    usages.add(Events.UseSeparateCompilerOutputPaths.metric(projectSettings.useSeparateCompilerOutputPaths))
-    usages.add(Events.SeparateProdAndTestSources.metric(projectSettings.separateProdAndTestSources))
-    usages.add(Events.UseSbtShellForImport.metric(projectSettings.useSbtShellForImport))
-    usages.add(Events.UseSbtShellForBuild.metric(projectSettings.useSbtShellForBuild))
-    usages.add(Events.EnableDebugSbtShell.metric(projectSettings.enableDebugSbtShell))
-
-    usages.add(Events.SbtVersion.metric(projectSettings.sbtVersion))
-    val sbtVersionMajor = Try(Version(projectSettings.sbtVersion).major(2)).getOrElse("0.0") // 0.0 ~ invalid version
-    usages.add(Events.SbtVersionMajor.metric(projectSettings.sbtVersion))
-
-    //"projectSettings.jdk" is ignored because it's only for the new project wizard
+    val sbtVersionMajor: String = Try(Version(projectSettings.sbtVersion).major(2)).map(_.presentation).getOrElse("0.0") // 0.0 ~ invalid version
+    usages.add(Events.SbtProjectSettings.metric(
+      Array(
+        //NOTE: "projectSettings.jdk" is ignored because it's only for the new project wizard
+        new EventPair(Fields.ResolveClassifiers, projectSettings.resolveClassifiers: java.lang.Boolean),
+        new EventPair(Fields.ResolveSbtClassifiers, projectSettings.resolveSbtClassifiers: java.lang.Boolean),
+        new EventPair(Fields.PreferScala2, projectSettings.preferScala2: java.lang.Boolean),
+        new EventPair(Fields.UseSeparateCompilerOutputPaths, projectSettings.useSeparateCompilerOutputPaths: java.lang.Boolean),
+        new EventPair(Fields.SeparateProdAndTestSources, projectSettings.separateProdAndTestSources: java.lang.Boolean),
+        new EventPair(Fields.UseSbtShellForImport, projectSettings.useSbtShellForImport: java.lang.Boolean),
+        new EventPair(Fields.UseSbtShellForBuild, projectSettings.useSbtShellForBuild: java.lang.Boolean),
+        new EventPair(Fields.EnableDebugSbtShell, projectSettings.enableDebugSbtShell: java.lang.Boolean),
+        new EventPair(Fields.SbtVersion, projectSettings.sbtVersion),
+        new EventPair(Fields.SbtVersionMajor, sbtVersionMajor),
+      ): _*
+    ))
   }
 }
 
@@ -97,28 +99,46 @@ private object SbtSettingsCollector {
 
   //noinspection TypeAnnotation
   private object Events {
-    val ResolveClassifiers = Group.registerEvent("resolveClassifiers", EventFields.Enabled)
-    val ResolveSbtClassifiers = Group.registerEvent("resolveSbtClassifiers", EventFields.Enabled)
-    val PreferScala2 = Group.registerEvent("preferScala2", EventFields.Enabled)
-    val UseSeparateCompilerOutputPaths = Group.registerEvent("useSeparateCompilerOutputPaths", EventFields.Enabled)
-    val SeparateProdAndTestSources = Group.registerEvent("separateProdAndTestSources", EventFields.Enabled)
-    val UseSbtShellForImport = Group.registerEvent("useSbtShellForImport", EventFields.Enabled)
-    val UseSbtShellForBuild = Group.registerEvent("useSbtShellForBuild", EventFields.Enabled)
-    val EnableDebugSbtShell = Group.registerEvent("enableDebugSbtShell", EventFields.Enabled)
+    val SbtProjectSettings = Group.registerVarargEvent(
+      "sbt.project.settings",
+      Array(
+        Fields.ResolveClassifiers,
+        Fields.ResolveSbtClassifiers,
+        Fields.PreferScala2,
+        Fields.UseSeparateCompilerOutputPaths,
+        Fields.SeparateProdAndTestSources,
+        Fields.UseSbtShellForImport,
+        Fields.UseSbtShellForBuild,
+        Fields.EnableDebugSbtShell,
+        Fields.SbtVersion,
+        Fields.SbtVersionMajor
+      ): _*
+    )
+  }
+
+  private object Fields {
+    val ResolveClassifiers: BooleanEventField =  EventFields.Boolean("resolve_classifiers")
+    val ResolveSbtClassifiers: BooleanEventField =  EventFields.Boolean("resolve_sbt_classifiers")
+    val PreferScala2: BooleanEventField =  EventFields.Boolean("prefer_scala2")
+    val UseSeparateCompilerOutputPaths: BooleanEventField =  EventFields.Boolean("use_separate_compiler_output_paths")
+    val SeparateProdAndTestSources: BooleanEventField =  EventFields.Boolean("separate_prod_and_test_sources")
+    val UseSbtShellForImport: BooleanEventField =  EventFields.Boolean("use_sbt_shell_for_import")
+    val UseSbtShellForBuild: BooleanEventField =  EventFields.Boolean("use_sbt_shell_for_build")
+    val EnableDebugSbtShell: BooleanEventField =  EventFields.Boolean("enable_debug_sbt_shell")
 
     /**
-     * Collects the full sbt version (e.g., 1.10.7, 2.0.0-M3)
+     * Stores the full sbt version (e.g., 1.10.7, 2.0.0-M3)
      *
      * @note this event is supposed to replace [[org.jetbrains.plugins.scala.statistics.ScalaProjectStateCollector.SbtInfoEvent]]
      */
-    val SbtVersion = Group.registerEvent("sbtVersion", EventFields.StringValidatedByRegexpReference("version", "version"))
+    val SbtVersion: StringEventField = EventFields.StringValidatedByRegexpReference("sbt_version", "version")
 
     /**
-     * Collects the first 2 digits of the sbt version (e.g., 1.10. 0.13, 2.0)
+     * Stores the first 2 digits of the sbt version (e.g., 1.10. 0.13, 2.0)
      *
      * @note a separate event for the major version might be redundant, but it's convenient to view this info
      *       in FUS directly, without the need of do extra processing of a full version
      */
-    val SbtVersionMajor = Group.registerEvent("sbtVersionMajor", EventFields.StringValidatedByRegexpReference("versionMajor", "version"))
+    val SbtVersionMajor: StringEventField = EventFields.StringValidatedByRegexpReference("sbt_version_major", "version")
   }
 }
