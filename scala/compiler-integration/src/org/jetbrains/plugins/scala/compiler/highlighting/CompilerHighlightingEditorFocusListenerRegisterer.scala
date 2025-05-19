ac39afb4d546e03f850b752c9507d1358bcf7491
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-05-19 19:30:33 +0200

    [cbh] don't register CompilerHighlightingEditorFocusListener on synthetic files #SCL-23885
    Note, this doesn't fix any big memory leaks.
    It seems that `org.jetbrains.plugins.scala.compiler.highlighting.CompilerHighlightingEditorFocusListenerRegisterer$MyDisposable` was just unlucky to hold references to too many editors during the "low memory" event when the report is generated. But it does it because other parts of the IDE didn't dispose of those editors.

--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerHighlightingEditorFocusListenerRegisterer.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/highlighting/CompilerHighlightingEditorFocusListenerRegisterer.scala
@@ -5,6 +5,7 @@ import com.intellij.openapi.editor.event.{EditorFactoryEvent, EditorFactoryListe
 import com.intellij.openapi.editor.{Editor, EditorFactory}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.testFramework.LightVirtualFileBase
 import org.jetbrains.plugins.scala.startup.ProjectActivity
 import org.jetbrains.plugins.scala.util.UnloadAwareDisposable
 
@@ -17,7 +18,10 @@ import java.util.concurrent.ConcurrentHashMap
 private object CompilerHighlightingEditorFocusListenerRegisterer {
   private val holders = new ConcurrentHashMap[Editor, MyDisposable]
 
-  private def registerIntoEditor(editor: Editor): Unit = {
+  private def registerIntoEditorIfApplicable(editor: Editor): Unit = {
+    if (ignoreEditor(editor))
+      return
+
     val listener = new CompilerHighlightingEditorFocusListener(editor)
     val disposable = new MyDisposable(editor, listener)
     if (holders.putIfAbsent(editor, disposable) == null) {
@@ -27,6 +31,43 @@ private object CompilerHighlightingEditorFocusListenerRegisterer {
     } else {
       //println(s"already registered [$editor]")
     }
+  }
+
+  /**
+   * List of extensions that are potentially supported by CBH
+   * It contains other non-Scala JVM language extensions (java, kotlin, groovy) for mixed projects.
+   * (though I suppose scala + kotlin/groovy would be a very rare case)
+   */
+  private val SupportedExtensions = Set(
+    "scala",
+    "sc",
+    "scala.html", //play framework twirl templates
+    //"sbt", probably no need to trigger for sbt files as we don't use CBH for sbt files anyway
+    "java",
+    "kt",
+    "kts",
+    "groovy",
+  )
+
+  //noinspection InstanceOf
+  private def ignoreEditor(editor: Editor): Boolean = {
+    if (editor.getProject == null)
+      return true
+
+    //The editor doesn't correspond to a physical file.
+    //(examples: debugger editor, debugger condition, AIA prompt)
+    //There can be dozens of such editors from many places in the IDE or installed plugins
+    val virtualFile = editor.getVirtualFile
+    if (virtualFile == null)
+      return true
+
+    //Sometimes editors still have some synthetic, in-memory file, also skipp them
+    //(examples: editor from the injected code fragments, after you invoke "Edit <langId> fragment")
+    if (virtualFile.isInstanceOf[LightVirtualFileBase])
+      return true
+
+    //Only handle known extensions that can "matter" for the compiler-based highlighting
+    SupportedExtensions.contains(virtualFile.getExtension)
   }
 
   private final class MyDisposable(editor: Editor, listener: CompilerHighlightingEditorFocusListener) extends Disposable {
@@ -40,7 +81,7 @@ private object CompilerHighlightingEditorFocusListenerRegisterer {
 
   final class ByListener extends EditorFactoryListener {
     override def editorCreated(event: EditorFactoryEvent): Unit = {
-      registerIntoEditor(event.getEditor)
+      registerIntoEditorIfApplicable(event.getEditor)
     }
 
     override def editorReleased(event: EditorFactoryEvent): Unit = {
@@ -52,7 +93,7 @@ private object CompilerHighlightingEditorFocusListenerRegisterer {
     override def execute(project: Project): Unit = {
       // pick up all existing editors... important for when dynamically loading the plugin
       for (editor <- EditorFactory.getInstance().getAllEditors) {
-        registerIntoEditor(editor)
+        registerIntoEditorIfApplicable(editor)
       }
     }
   }
