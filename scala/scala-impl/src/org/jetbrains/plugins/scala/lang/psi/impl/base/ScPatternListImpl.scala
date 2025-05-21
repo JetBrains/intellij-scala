6836178cf8351b7a4ca1d6a2fc86317537f4f7a9
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-05-21 12:23:55 +0200

    [build] Suppress deprecation warnings after the intellijVersion update to 251.26094.37 #SCL-23400
    #noport
    
    - These suppressions were already done in 252.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScPatternListImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScPatternListImpl.scala
@@ -10,6 +10,8 @@ import org.jetbrains.plugins.scala.lang.psi.api.base.patterns._
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaStubBasedElementImpl
 import org.jetbrains.plugins.scala.lang.psi.stubs.ScPatternListStub
 
+import scala.annotation.nowarn
+
 class ScPatternListImpl private(stub: ScPatternListStub, node: ASTNode)
   extends ScalaStubBasedElementImpl(stub, ScalaElementType.PATTERN_LIST, node) with ScPatternList {
 
@@ -19,6 +21,7 @@ class ScPatternListImpl private(stub: ScPatternListStub, node: ASTNode)
 
   override def toString: String = "ListOfPatterns"
 
+  @nowarn("cat=deprecation") // TODO: SCL-23400
   override def patterns: Seq[ScPattern] = {
     if (simplePatternsByStub) getStubOrPsiChildren(REFERENCE_PATTERN, ScReferencePatternFactory)
     else findChildrenByClass(classOf[ScPattern])
