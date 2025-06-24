b91575e91c6493d7475ac7885731c4b122194710
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-06-24 08:47:27 +0200

    [build] Update sbt-idea-plugin to version 4.1.13 and remove the --add-opens option which we added in our build
    - The vm option is now added by sbt-idea-plugin.

--- project/Common.scala
+++ project/Common.scala
@@ -131,8 +131,7 @@ object Common {
       //"com.intellij.dev".toPlugin,
 
       "org.jetbrains.kotlin".toPlugin
-    ),
-    intellijVMOptions ~= { _.withOption("--add-opens=java.desktop/javax.swing.text.html.parser=ALL-UNNAMED") }
+    )
   )
 
   def newPlainScalaProject(projectName: String, base: File): Project =
