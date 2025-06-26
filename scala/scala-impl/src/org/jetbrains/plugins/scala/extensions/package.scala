26296c50d2bd987bca48c5d622e656c32d8d68c0
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-06-26 13:25:43 +0200

    [scala sdk] fix system sdk creation for Scala 2 #SCL-24040 fixed
    (cherry picked from commit b64f8a16667db0a3e41573380c69ebf95e57339d)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/extensions/package.scala
@@ -1822,7 +1822,7 @@ package object extensions {
      * OS resources.
      */
     def allFiles(): Seq[Path] = {
-      val (files, directories) = children().span(_.isRegularFile)
+      val (files, directories) = children().partition(_.isRegularFile)
       files ++ directories.flatMap(_.allFiles())
     }
 
