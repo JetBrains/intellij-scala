dafada145043c302e39de7789ce0255632eb8db6
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-06-27 10:39:02 +0200

    [widening] minor cosmetics #SCL-23271

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/Compatibility.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/Compatibility.scala
@@ -243,7 +243,8 @@ object Compatibility {
                 paramType.conforms(functionType, ConstraintSystem.empty) match {
                   case ConstraintSystem(substitutor) =>
                     Some(substitutor(functionType.typeArguments(1)))
-                  case _ => None
+                  case _ =>
+                    None
                 }
               }.filterNot(_.is[UndefinedType])
           }.map(_ -> res)
@@ -253,7 +254,8 @@ object Compatibility {
       fromImplicit match {
         case Some((mr, result)) =>
           ExpressionTypeResult(Right(mr), result.importsUsed, Some(result))
-        case _ => ExpressionTypeResult(Right(afterNTtoTConversion))
+        case _ =>
+          ExpressionTypeResult(Right(afterNTtoTConversion))
       }
     }
   }
