ebf2ee2733f8e3aed0bfe86406f06f96629ade8e
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-06-27 14:30:12 +0200

    [test integration] choose the correct test module for the new test when separate prod/test modules are created #SCL-24058 fixed
    The main fix is in org.jetbrains.sbt.project.SbtProjectResolver:
    `testModule.setProductionModuleId(prodModule.getInternalName)`

---
+++ scala/test-integration/testing-support/testData/testCreationProjects/createNewTestInMultiModuleProjectWithMultipleExistingTestDirectories/project2/src/test/scala/com/example/level2/DummyExisting22.scala
@@ -0,0 +1,10 @@
+package com.example.level2
+
+/**
+ * Dummy test file for level2 package in project1
+ */
+object DummyExisting22 {
+  def testHello(): Boolean = {
+    DummyLevel2.hello() == "Hello from level2 in project1"
+  }
+}
