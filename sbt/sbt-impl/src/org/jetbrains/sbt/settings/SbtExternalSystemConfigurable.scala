f32aa447b35b77cd6628ccea8afc367b6d02062f
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-07-03 08:45:40 +0200

    [settings] fix indexing sbt project settings #SCL-23178 fixed
    (cherry picked from commit b1eaa8fbf9011934753f72491e57f05c035a8e43)

--- sbt/sbt-impl/src/org/jetbrains/sbt/settings/SbtExternalSystemConfigurable.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/settings/SbtExternalSystemConfigurable.scala
@@ -1,11 +1,16 @@
 package org.jetbrains.sbt.settings
 
+import com.intellij.ide.ui.search.TraverseUIMode
+import com.intellij.openapi.externalSystem.ExternalSystemManager
 import com.intellij.openapi.externalSystem.service.settings.AbstractExternalSystemConfigurable
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.help.ScalaWebHelpProvider
 import org.jetbrains.sbt.project.SbtProjectSystem
 import org.jetbrains.sbt.project.settings.Context.Configuration
 import org.jetbrains.sbt.project.settings._
+
+import javax.swing.JComponent
 
 class SbtExternalSystemConfigurable(project: Project)
   extends AbstractExternalSystemConfigurable[SbtProjectSettings, SbtProjectSettingsListener, SbtSettings](project, SbtProjectSystem.Id) {
@@ -20,4 +25,23 @@ class SbtExternalSystemConfigurable(project: Project)
 
   override def getHelpTopic: String =
     ScalaWebHelpProvider.HelpPrefix + "sbt-support.html"
+
+  override def createComponent(): JComponent = {
+    if (project.isDefault && TraverseUIMode.getInstance().isActive) {
+      // During buildIntellijOptionsIndex (aka traverseUI), we also want the project section to be indexed.
+      // Unfortunately, that section of the ui is only created if there are SbtSettings linked to the project of SbtExternalSystemConfigurable
+      // For the project itself intellij uses the "Default Project (Wizard Template)" but SbtSettings are only linked during importing.
+      // So in the case that we are in TraverseUIMode we link a default SbtSettings to the default project.
+      // In that way createProjectSettingsControl is called and the full UI created and indexed.
+      val manager = ExternalSystemApiUtil.getManager(SbtProjectSystem.Id)
+        .asInstanceOf[ExternalSystemManager[SbtProjectSettings, _, SbtSettings, _, _]]
+      val sbtSettings = manager.getSettingsProvider.fun(project)
+      if (sbtSettings.getLinkedProjectSettings("") == null) {
+        val settings = SbtProjectSettings.defaultForNewProjectWizard
+        settings.setExternalProjectPath("")
+        sbtSettings.linkProject(settings)
+      }
+    }
+    super.createComponent()
+  }
 }
