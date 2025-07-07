17f96abc69b85fe05a0d0fdc760354d6d7c9e83d
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-07-07 10:17:40 +0200

    ["got it" tooltip]: improvements (#SCL-24039, #SCL-24056)
    - navigate directly to the specific label in the settings from the "got it" tooltip
    - rename "learn more..." to "Read more"
    
    
    (cherry picked from commit f89ddabf9bf5df050437b83c18a8f36e1afba756)

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/GeneratedManagedSourcesImportListener.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/GeneratedManagedSourcesImportListener.scala
@@ -3,7 +3,9 @@ package org.jetbrains.sbt.project
 import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataImportListener
 import com.intellij.openapi.project.Project
 import com.intellij.ui.GotItTooltip
+import org.jetbrains.plugins.scala.settings.ShowSettingsUtilImplExt
 import org.jetbrains.sbt.SbtBundle
+import org.jetbrains.sbt.settings.SbtExternalSystemConfigurable
 
 private final class GeneratedManagedSourcesImportListener(project: Project) extends ProjectDataImportListener {
   //noinspection ApiStatus,UnstableApiUsage
@@ -18,7 +20,9 @@ private final class GeneratedManagedSourcesImportListener(project: Project) exte
           "sbt.generated.managed.sources.tooltip",
           SbtBundle.message("sbt.generated.managed.sources.during.project.sync"),
           toolWindowManagerDisposable.orNull
-        ).withLink(SbtBundle.message("open.sbt.project.settings"), () => SbtTooltip.openSbtProjectSettings(project, projectPath))
+        ).withLink(SbtBundle.message("open.sbt.project.settings"), () =>
+          ShowSettingsUtilImplExt.showSettingsDialog(project, classOf[SbtExternalSystemConfigurable], SbtBundle.message("generate.managed.sources.during.project.sync.label"))
+        )
 
         gtip.show(button, GotItTooltip.LEFT_MIDDLE)
       }
