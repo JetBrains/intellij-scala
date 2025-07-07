1f20c0d7445a7ae30dd7206b19c2238a5b3268fb
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-07-07 12:33:13 +0200

    [psi] use plain text language for too large scala files + show warning in editor #SCL-24093 fixed
    The change in ScFileViewProvider now checks if the file is too large (larger than 2.56 MB by default).
    This will automatically enable the LargeFilesAnnotator for Scala files as well as the inner content will be treated as PlainText

--- scala/scala-api/src/lang/psi/ScFileViewProvider.scala
+++ scala/scala-api/src/lang/psi/ScFileViewProvider.scala
@@ -2,20 +2,47 @@ package org.jetbrains.plugins.scala
 package lang.psi
 
 import com.intellij.lang.Language
-import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.{FileType, PlainTextLanguage}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.{PsiFile, PsiManager, SingleRootFileViewProvider}
 
-final class ScFileViewProvider(manager: PsiManager, file: VirtualFile,
-                               eventSystemEnabled: Boolean, language: Language)
-  extends SingleRootFileViewProvider(manager, file, eventSystemEnabled, language) {
+final class ScFileViewProvider(
+  manager: PsiManager,
+  file: VirtualFile,
+  eventSystemEnabled: Boolean,
+  language: Language
+) extends SingleRootFileViewProvider(
+  manager,
+  file,
+  eventSystemEnabled,
+  ScFileViewProvider.calcBaseLanguage(file, language)
+) {
 
-  override def createFile(project: Project,
-                          file: VirtualFile,
-                          fileType: FileType): PsiFile =
+  override def createFile(
+    project: Project,
+    file: VirtualFile,
+    fileType: FileType
+  ): PsiFile =
     createFile(getBaseLanguage)
 
   override def createCopy(copy: VirtualFile) =
     new ScFileViewProvider(getManager, copy, eventSystemEnabled = false, getBaseLanguage)
+}
+
+object ScFileViewProvider {
+
+  /**
+   * This is a partial copy of the method [[com.intellij.psi.SingleRootFileViewProvider#calcBaseLanguage]]
+   *
+   * I haven't copied the full logic, only the part required for SCL-24093
+   *
+   * @see [[com.intellij.psi.impl.PsiFileFactoryImpl#trySetupPsiForFile]]
+   */
+  private def calcBaseLanguage(file: VirtualFile, language: Language): Language = {
+    if (SingleRootFileViewProvider.isTooLargeForIntelligence(file))
+      PlainTextLanguage.INSTANCE
+    else
+      language
+  }
 }
