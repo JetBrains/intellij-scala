04d10ed0dae5c5642031fa8126973e02ba338816
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-07-11 14:07:08 +0200

    [Gradle]: create separate Scala SDK instances, instead of converting existing scala libraries to Scala SDKs (#SCL-24084, #SCL-17284) fixed
    - similar approach is already implemented in sbt and Maven

--- scala/integration/gradle/src/org/jetbrains/plugins/scala/project/gradle/ScalaGradleDataService.scala
+++ scala/integration/gradle/src/org/jetbrains/plugins/scala/project/gradle/ScalaGradleDataService.scala
@@ -19,6 +19,8 @@ import scala.jdk.CollectionConverters._
 
 class ScalaGradleDataService extends ScalaAbstractProjectDataService[ScalaModelData, Library](ScalaModelData.KEY) {
 
+  private val GradleExternalSystemReadableName = GradleConstants.SYSTEM_ID.getReadableName
+
   override def importData(
     toImport: util.Collection[_ <: DataNode[ScalaModelData]],
     projectData: ProjectData,
@@ -27,6 +29,10 @@ class ScalaGradleDataService extends ScalaAbstractProjectDataService[ScalaModelD
   ): Unit = {
     implicit val p: Project = project
     implicit val mp: IdeModifiableModelsProvider = modelsProvider
+
+    //TODO remove this in some feature release (probably 2026/2027)
+    ScalaSdkUtils.revertScalaSdkFromLibraries(modelsProvider, externalSystemName = GradleExternalSystemReadableName)
+
     toImport.forEach { scalaNode =>
       Option(scalaNode.getData(ProjectKeys.MODULE)).foreach { moduleData =>
         val moduleName = moduleData.getInternalName
@@ -36,8 +42,8 @@ class ScalaGradleDataService extends ScalaAbstractProjectDataService[ScalaModelD
         val maybeTestModule       = modelsProvider.findIdeModuleOpt(s"${moduleName}_test").orElse(modelsProvider.findIdeModuleOpt(s"$moduleName.test"))
 
         (maybeCompoundModule, maybeProductionModule, maybeTestModule) match {
-          case (_, Some(productionModule), Some(testModule)) => configureModules(productionModule, scalaNode, testModule :: Nil)
-          case (Some(compoundModule), _, _)                  => configureModules(compoundModule, scalaNode)
+          case (_, Some(productionModule), Some(testModule)) => configureModules(scalaNode, productionModule, testModule)
+          case (Some(compoundModule), _, _)                  => configureModules(scalaNode, compoundModule)
           case _                                             =>
         }
       }
@@ -45,21 +51,17 @@ class ScalaGradleDataService extends ScalaAbstractProjectDataService[ScalaModelD
   }
 
   private def configureModules(
-    mainModule: Module,
     scalaNode: DataNode[ScalaModelData],
-    otherModules: List[Module] = Nil
-  )(implicit project: Project, modelsProvider: IdeModifiableModelsProvider): Unit =
-    for {
-      module <- mainModule :: otherModules
-      data = scalaNode.getData
-    } {
-      module.configureScalaCompilerSettingsFrom("Gradle", compilerOptionsFrom(data))
-      module match {
-        case `mainModule` =>
-          configureScalaSdk(mainModule, data.getScalaClasspath.asScala.toSeq.map(_.toPath))
-        case _ =>
-      }
+    modules: Module*
+  )(implicit project: Project, modelsProvider: IdeModifiableModelsProvider): Unit = {
+    val scalaData = scalaNode.getData
+    val compilerOptions = compilerOptionsFrom(scalaData)
+    val classpath = scalaData.getScalaClasspath.asScala.toSeq.map(_.toPath)
+    modules.foreach { module =>
+      module.configureScalaCompilerSettingsFrom(GradleExternalSystemReadableName, compilerOptions)
+      configureScalaSdk(module, classpath)
     }
+  }
 
   private def configureScalaSdk(
     module: Module,
@@ -80,38 +82,25 @@ class ScalaGradleDataService extends ScalaAbstractProjectDataService[ScalaModelD
     module: Module,
     compilerVersion: String,
     compilerClasspath: Seq[Path]
-  )(implicit project: Project, modelsProvider: IdeModifiableModelsProvider): Unit = {
-    val projectLevelLibraries = modelsProvider.getAllLibraries
-    val moduleLevelLibraries = modelsProvider.getModifiableRootModel(module).getModuleLibraryTable.getLibraries
-    val scalaLibraries = (moduleLevelLibraries ++ projectLevelLibraries).toSet.filter(_.hasRuntimeLibrary)
-    if (scalaLibraries.nonEmpty) {
-      val scalaLibraryWithSameVersion = scalaLibraries.find { library =>
-        library.libraryVersion.contains(compilerVersion) || library.jarLibraryVersion.contains(compilerVersion)
-      }
-      scalaLibraryWithSameVersion match {
-        case Some(library) =>
-          // Only resolve the compiler bridge for Scala 3. Gradle reports a compiler classpath that doesn't work with
-          // the Scala 2.13.12+ compiler bridges, due to clashes.
-          val compilerBridgeBinaryJar = library.libraryVersion.filter(_.startsWith("3.")).flatMap { version =>
-            ScalaSdkUtils.compilerBridgeJarName(version).flatMap { bridgeJarName =>
-              compilerClasspath.find(_.getFileName.toString == bridgeJarName).orElse(ScalaSdkUtils.resolveCompilerBridgeJar(version))
-            }
-          }
+  )(implicit modelsProvider: IdeModifiableModelsProvider): Unit = {
+    // Only resolve the compiler bridge for Scala 3. Gradle reports a compiler classpath that doesn't work with
+    // the Scala 2.13.12+ compiler bridges, due to clashes.
+    val compilerBridgeBinaryJar =
+      if (compilerVersion.startsWith("3.")) {
+        ScalaSdkUtils.compilerBridgeJarName(compilerVersion).flatMap { bridgeJarName =>
+          compilerClasspath.find(_.getFileName.toString == bridgeJarName).orElse(ScalaSdkUtils.resolveCompilerBridgeJar(compilerVersion))
+        }
+      } else None
 
-          ScalaSdkUtils.ensureScalaLibraryIsConvertedToScalaSdk(
-            modelsProvider,
-            library,
-            compilerClasspath,
-            scaladocExtraClasspath = Nil, // TODO SCL-17219
-            compilerBridgeBinaryJar
-          )
-        case None =>
-          showScalaLibraryNotFoundWarning(compilerVersion, module)
-      }
-    } else {
-      // In practice, this warning shouldn’t be displayed because if the Scala library is missing, a built-in message is shown instead.
-      showScalaLibraryNotFoundWarning(compilerVersion, module)
-    }
+    ScalaSdkUtils.configureScalaSdk(
+      module,
+      compilerVersion,
+      compilerClasspath,
+      scaladocExtraClasspath = Nil,
+      compilerBridgeBinaryJar,
+      sdkPrefix = GradleExternalSystemReadableName,
+      modelsProvider
+    )
   }
 
   private def compilerOptionsFrom(data: ScalaModelData): Seq[String] =
@@ -150,10 +139,4 @@ class ScalaGradleDataService extends ScalaAbstractProjectDataService[ScalaModelD
 
   private def showWarning(message: NlsString)(implicit project: Project): Unit =
     super.showWarning(Title, message, BalloonGroup, SystemId)
-
-  private def showScalaLibraryNotFoundWarning(
-    version: String,
-    module: Module
-  )(implicit project: Project): Unit =
-    showScalaLibraryNotFoundWarning(Title, version, module.getName, BalloonGroup, SystemId)
 }
