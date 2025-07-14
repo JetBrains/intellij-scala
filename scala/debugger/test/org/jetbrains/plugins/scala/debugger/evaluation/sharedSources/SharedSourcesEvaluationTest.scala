a33edf8df022d0ffbd71884118138cb58ba339c5
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-07-14 16:56:21 +0200

    [debugger] Use the representative module for shared sources when evaluating debugger expressions in a shared source file #SCL-24060
    (cherry picked from commit 39321b04f4f09d36e1b571b8dbf1e9b84d73a7ca)

---
+++ scala/debugger/test/org/jetbrains/plugins/scala/debugger/evaluation/sharedSources/SharedSourcesEvaluationTest.scala
@@ -0,0 +1,94 @@
+package org.jetbrains.plugins.scala.debugger.evaluation.sharedSources
+
+import com.intellij.debugger.DebuggerTestCase
+import com.intellij.debugger.engine.evaluation.CodeFragmentKind
+import com.intellij.debugger.impl.OutputChecker
+import com.intellij.execution.configurations.JavaParameters
+import com.intellij.openapi.module.{Module, ModuleManager}
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.util.io.{FileUtil, NioFiles}
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.psi.PsiManager
+import com.sun.jdi.IntegerValue
+import junit.framework.TestCase.assertEquals
+import org.jetbrains.plugins.scala.FlakyTests
+import org.jetbrains.plugins.scala.extensions.{PathExt, inReadAction}
+import org.jetbrains.plugins.scala.project.ModuleExt
+import org.jetbrains.plugins.scala.util.TestUtils
+import org.jetbrains.sbt.project.SbtProjectSystem
+import org.jetbrains.sbt.project.settings.SbtProjectSettings
+import org.junit.experimental.categories.Category
+
+import java.nio.file.Path
+import scala.jdk.CollectionConverters.SeqHasAsJava
+
+/**
+ * This is an extremely custom-written test. It requires combining two unrelated test base classes, one which allows
+ * the debugger to be started in a project and another which imports a project using the ExternalSystem mechanism.
+ *
+ * I am not confident that this test will not be flaky. If you notice any errors in it, feel free to ignore the test
+ * and report the failures to Vasil.
+ */
+@Category(Array(classOf[FlakyTests]))
+class SharedSourcesEvaluationTest extends DebuggerTestCase {
+
+  private var mainModule: Module = _
+
+  override def initOutputChecker(): OutputChecker = new OutputChecker(() => getTestAppPath, () => getTestAppPath) {
+    override def checkValid(jdk: Sdk, sortClassPath: Boolean): Unit = {}
+  }
+
+  override lazy val getTestAppPath: String = {
+    val originalTestDataProjectDir = (TestUtils.getTestDataDir / "debuggerTestData" / "sharedSourcesEvalTest").toCanonicalPath.toFile
+    val tempProjectDir = FileUtil.createTempDirectory(s"temp_projects/${originalTestDataProjectDir.getName}", "", false)
+    FileUtil.copyDir(originalTestDataProjectDir, tempProjectDir)
+    tempProjectDir.getCanonicalPath
+  }
+
+  override def setUpProject(): Unit = {
+    myProject = doCreateAndOpenProject()
+
+    val settings = new SbtProjectSettings()
+
+    ExternalSystemImportingUtil.importProject(
+      getProject,
+      SbtProjectSystem.Id,
+      settings,
+      getTestAppPath,
+      false
+    )
+
+    val modules = ModuleManager.getInstance(getProject).getModules
+    mainModule = modules.find(_.getName == "sharedsourcesevaltest.sharedSourcesEvalTest.sharedSourcesEvalTestJVM.main").orNull
+  }
+
+  override def getModule: Module = mainModule
+
+  override def getModuleOutputDir: Path = Path.of(getTestAppPath, "jvm", "target", "scala-3.7.1", "classes")
+
+  def testSharedSourcesEval(): Unit = {
+    createLocalProcess("SharedMain")
+
+    doWhenPausedThenResume { suspendContext =>
+      val x = evaluate(
+        CodeFragmentKind.EXPRESSION,
+        "NumberHolder().number + List(1, 2, 3).filter(_ % 2 != 0).sum",
+        suspendContext
+      ).asInstanceOf[IntegerValue].value()
+      assertEquals(9, x)
+    }
+  }
+
+  override protected def createJavaParameters(mainClass: String): JavaParameters = {
+    val params = super.createJavaParameters(mainClass)
+    params.getClassPath.addAll(mainModule.scalaCompilerClasspath.map(_.toCanonicalPath.toString).asJava)
+    params
+  }
+
+  override protected def createBreakpoints(className: String): Unit = {
+    val path = Path.of(getTestAppPath, "shared", "src", "main", "scala", s"$className.scala")
+    val virtualFile = VfsUtil.findFile(path, true)
+    val psiFile = inReadAction(PsiManager.getInstance(getProject).findFile(virtualFile))
+    super.createBreakpoints(psiFile)
+  }
+}
