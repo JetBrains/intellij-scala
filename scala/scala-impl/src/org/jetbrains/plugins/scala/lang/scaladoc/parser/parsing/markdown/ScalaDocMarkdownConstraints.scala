aed5505d9d3fc5da8f0793f5f825a89e0a047bbf
Emilie Kobler <sebastien.kobler@jetbrains.com>
2025-07-22 16:47:39 +0200

    [scaladoc] Clean up code and fix parsing bug #SCL-18476

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/parser/parsing/markdown/ScalaDocMarkdownConstraints.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/parser/parsing/markdown/ScalaDocMarkdownConstraints.scala
@@ -3,27 +3,74 @@ package org.jetbrains.plugins.scala.lang.scaladoc.parser.parsing.markdown
 import org.intellij.markdown.parser.LookaheadText
 import org.intellij.markdown.parser.constraints.CommonMarkdownConstraints
 
-class ScalaDocMarkdownConstraints(indents: Array[Int], types: Array[Char], isExplicit: Array[Boolean], charsEaten: Int) extends CommonMarkdownConstraints(indents, types, isExplicit, charsEaten) {
+/*
+We need to override the constraints of CommonMark because tags modify the constraints
+(essentially they immediately terminate any previous blocks, and kill any constraints, which is hard to express)
+
+Unfortunately, `CommonMarkdownConstraints` is relatively complex, *and* assumes that there are only ever 2 kinds of
+constraints; lists, and block quotes.
+
+Tags aren't *quite* like either, which is very inconvenient. They don't generate constraints after their line of creation,
+and they cannot be terminated by anything other than another tag.
+
+This means we need to understand the internal details of:
+- CommonMarkdownConstraints
+- MarkdownConstraints
+- MarkerProcessor
+- MarkerProcessorFactory
+
+And know how they all deal with these blocks.
+
+The solution I came up with is to add `startsTag`, which is an ephemeral flag indicating whether the current line
+started a new tag, in which case all previous blocks are killed.
+
+We override `containsListMarkers` and cheat a little. See its note for more.
+ */
+class ScalaDocMarkdownConstraints(indents: Array[Int], types: Array[Char], isExplicit: Array[Boolean], charsEaten: Int, private val startsTag : Boolean) extends CommonMarkdownConstraints(indents, types, isExplicit, charsEaten) {
+  override def getBase: CommonMarkdownConstraints = ScalaDocMarkdownConstraints.BASE
+
+  override def createNewConstraints(indents: Array[Int], types: Array[Char], isExplicit: Array[Boolean], charsEaten: Int): CommonMarkdownConstraints = {
+    // When creating new constraints, we're either:
+    // 1. Basing ourselves off of BASE, which doesn't have a startTag (applyToNextLine)
+    // 2. Modifying the current constraints, in which case we need to keep the startsTag.
+    new ScalaDocMarkdownConstraints(indents, types, isExplicit, charsEaten, this.startsTag)
+  }
+
+  /*
+  We need to say that if we start a new tag, we are *never* continuing the previous constraints, i.e. all previous blocks
+  are immediately broken.
+
+  There are two methods that are used to determine this; `startsWith` and `containsListMarkers`, which work from opposite
+  ends. The first returns whether *other* constraints can continue `this`, and the second whether `this` can continue another.
+
+  The first option would require us to perform a runtime class check to determine whether the other constraints start
+  with a tag.
+
+  The second is officially a "leaky abstraction" and is poorly named. Its documentation states:
+  "whether there are "breaking" modifiers which do not continue other constraints even if all types and indents are correct."
+
+  But decides this is equivalent to containing list markers or not. This isn't true in our case.
+   */
+  override def containsListMarkers(upToIndex: Int): Boolean = startsTag || super.containsListMarkers(upToIndex)
+
   override def applyToNextLine(pos: LookaheadText#Position): CommonMarkdownConstraints = {
-    if (pos == null) {
-      return super.applyToNextLine(pos)
-    }
+    if (ScalaDocMarkdownFlavour.getTagOnLine(pos).isDefined) ScalaDocMarkdownConstraints.BASE
+    else super.applyToNextLine(pos)
+  }
 
-    val offset = pos.charsToNonWhitespace()
-    val line = pos.getCurrentLine
+  override def addModifierIfNeeded(pos: LookaheadText#Position): CommonMarkdownConstraints =
+    Option(super.addModifierIfNeeded(pos)).getOrElse(tryAddTag(pos))
 
-    if (offset < line.length() && line.charAt(offset) == '@') {
-      // We're in a tag, we override!
-      var spacePos = offset + 1
-      while (spacePos < line.length() && !Character.isWhitespace(line.charAt(spacePos))) {
-        spacePos += 1
-      }
-      new ScalaDocMarkdownConstraints(Array(), Array(), Array(), spacePos)
-    } else {
-      super.applyToNextLine(pos)
+  private def tryAddTag(pos: LookaheadText#Position): ScalaDocMarkdownConstraints = {
+    // Tags need to be the first modifier of the line.
+    if (pos == null) return null
+
+    ScalaDocMarkdownFlavour.getTagOnLine(pos) match {
+      case Some((_, end)) => new ScalaDocMarkdownConstraints(Array(), Array(), Array(), end, true)
+      case None => null
     }
   }
 }
 object ScalaDocMarkdownConstraints {
-  val BASE = new ScalaDocMarkdownConstraints(Array(), Array(), Array(), 0)
+  val BASE = new ScalaDocMarkdownConstraints(Array(), Array(), Array(), 0, false)
 }
--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/parser/parsing/markdown/ScalaDocTagMarkerBlockProvider.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/parser/parsing/markdown/ScalaDocTagMarkerBlockProvider.scala
@@ -14,9 +14,9 @@ import kotlin.ranges.IntRange
 class ScalaDocTagMarkerBlockProvider extends MarkerBlockProvider[MarkerProcessor.StateInfo] {
   override def createMarkerBlocks(position: LookaheadText#Position, productionHolder: ProductionHolder, t: MarkerProcessor.StateInfo): util.List[MarkerBlock] = {
     // Check if the current position is at the start of a line (after whitespace) and contains a tag (@)
-    getTagInfo(position, t.getCurrentConstraints) match {
+    ScalaDocMarkdownFlavour.getTagOnLine(position) match {
       case Some((start, end)) =>
-        productionHolder.addProduction(util.List.of(new SequentialParser.Node(new IntRange(position.getOffset + start, position.getOffset + end), ScalaDocTagMarkerBlock.TAG_NAME)))
+        productionHolder.addProduction(util.List.of(new SequentialParser.Node(new IntRange(position.getOffset + start + 1, position.getOffset + end), ScalaDocTagMarkerBlock.TAG_NAME)))
 
         util.List.of(new ScalaDocTagMarkerBlock(t.getCurrentConstraints, productionHolder.mark()))
       case None => util.List.of()
@@ -24,28 +24,7 @@ class ScalaDocTagMarkerBlockProvider extends MarkerBlockProvider[MarkerProcessor
   }
 
   override def interruptsParagraph(position: LookaheadText#Position, markdownConstraints: MarkdownConstraints): Boolean = {
-    // If there is another tag right here, we get interrupted (? I think that's what this method does. Unclear)
-    getTagInfo(position, markdownConstraints).isDefined
-  }
-
-  private def getTagInfo(position: LookaheadText#Position, constraints: MarkdownConstraints): Option[(Int, Int)] = {
-    // Check if we're at the start of a line
-    if (!MarkdownCompanionProxy.MarkerBlockProviderCompanion.isStartOfLineWithConstraints(position, constraints)) {
-      return None
-    }
-
-    // Skip whitespace at the beginning of the line
-    val currentLine = position.getCurrentLineFromPosition
-    val start = position.charsToNonWhitespace()
-
-    // Check if the first non-whitespace character is '@'
-    if (start < currentLine.length() && currentLine.charAt(start) == '@') {
-      val lineLength = currentLine.length()
-      val tagEnd = (start + 1 until lineLength).find { i =>
-        val c = currentLine.charAt(i)
-        Character.isWhitespace(c)
-      }.getOrElse(lineLength)
-      Some((start + 1, tagEnd))
-    } else None
+    // If there is another tag right here, paragraphs are interrupted
+    ScalaDocMarkdownFlavour.getTagOnLine(position).isDefined
   }
 }
