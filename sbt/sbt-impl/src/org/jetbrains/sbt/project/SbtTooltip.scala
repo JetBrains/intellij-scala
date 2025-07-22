ad6ccf4eae829b83b5687c0ab039375e82c2d8d8
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-07-22 15:26:47 +0200

    [got it tooltips] React to the position of the sbt button within the different tool windows when showing the Got it tooltips #SCL-24136 fixed

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtTooltip.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtTooltip.scala
@@ -1,13 +1,16 @@
 package org.jetbrains.sbt.project
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.actionSystem.impl.{ActionButton, ActionButtonUtil}
+import com.intellij.openapi.actionSystem.impl.ActionButtonUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.impl.{SquareStripeButton, ToolWindowManagerImpl}
-import com.intellij.openapi.wm.{ToolWindowManager, WindowManager}
+import com.intellij.openapi.wm.{ToolWindowAnchor, ToolWindowManager, WindowManager}
+import com.intellij.ui.GotItTooltip
+
+import java.awt.{Component, Point}
 
 private object SbtTooltip {
-  def findSbtToolWindowButton(project: Project): Option[ActionButton] = {
+  def findSbtToolWindowButton(project: Project): Option[SquareStripeButton] = {
     val frame = WindowManager.getInstance().getIdeFrame(project)
     if (frame == null) return None
     val component = frame.getComponent
@@ -16,11 +19,24 @@ private object SbtTooltip {
       case _ => false
     })
 
-    Option(maybeActionButton)
+    Option(maybeActionButton).collect { case button: SquareStripeButton => button }
   }
 
   //noinspection ApiStatus,UnstableApiUsage
   def findToolWindowManagerDisposable(project: Project): Option[Disposable] =
     Option(ToolWindowManager.getInstance(project))
       .collect { case x: ToolWindowManagerImpl => x }
+
+  type PointProvider = kotlin.jvm.functions.Function2[Component, AnyRef, Point]
+
+  def tooltipPointOfOrigin(button: SquareStripeButton): PointProvider =
+    button.getToolWindow.getAnchor match {
+      case ToolWindowAnchor.RIGHT => GotItTooltip.LEFT_MIDDLE
+      case ToolWindowAnchor.LEFT => GotItTooltip.RIGHT_MIDDLE
+      case ToolWindowAnchor.TOP => GotItTooltip.BOTTOM_MIDDLE
+      case ToolWindowAnchor.BOTTOM =>
+        // If `isSplitMode = true `, then the sbt icon is on the right side of the "bottom" toolbar.
+        val isRight = button.getToolWindow.isSplitMode
+        if (isRight) GotItTooltip.LEFT_MIDDLE else GotItTooltip.RIGHT_MIDDLE
+    }
 }
