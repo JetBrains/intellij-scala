148a6b75b3df31dfb534568a28f72435c3490e43
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-07-30 15:07:04 +0200

    [sbt shell, tests] Include the new version specific tests for both regular import tests and sbt-shell #SCL-24146

--- sbt/sbt-project-importing-tests/test/org/jetbrains/sbt/project/SbtProjectStructureImportingTestBase_ProdTestSourcesSeparated.scala
+++ sbt/sbt-project-importing-tests/test/org/jetbrains/sbt/project/SbtProjectStructureImportingTestBase_ProdTestSourcesSeparated.scala
@@ -6,6 +6,7 @@ import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.roots.DependencyScope
 import com.intellij.pom.java.LanguageLevel
 import org.jetbrains.jps.model.java.compiler.JpsJavaCompilerOptions
+import org.jetbrains.jps.model.java.{JavaResourceRootType, JavaSourceRootType}
 import org.jetbrains.plugins.scala.SlowTests
 import org.jetbrains.plugins.scala.compiler.data.CompileOrder
 import org.jetbrains.plugins.scala.extensions.PathExt
@@ -1725,4 +1726,121 @@ abstract class SbtProjectStructureImportingTestBase_ProdTestSourcesSeparated ext
         )
       }
     )
+
+  private def customSbtContentRootsForParent(binaryVersion: Int): Seq[ExpectedDirectoryCompletionVariant] =
+    Seq(
+      ("src/main/java", JavaSourceRootType.SOURCE),
+      ("src/main/scala", JavaSourceRootType.SOURCE),
+      (s"src/main/scala-2.$binaryVersion", JavaSourceRootType.SOURCE),
+      ("src/test/java", JavaSourceRootType.TEST_SOURCE),
+      ("src/test/scala", JavaSourceRootType.TEST_SOURCE),
+      (s"src/test/scala-2.$binaryVersion", JavaSourceRootType.TEST_SOURCE),
+      ("src/main/resources", JavaResourceRootType.RESOURCE),
+      ("src/test/resources", JavaResourceRootType.TEST_RESOURCE),
+    ).map((ExpectedDirectoryCompletionVariant.apply _).tupled)
+
+  private def customSbtContentRootsForMain(binaryVersion: Int): Seq[ExpectedDirectoryCompletionVariant] =
+    Seq(
+      ("java", JavaSourceRootType.SOURCE),
+      ("scala", JavaSourceRootType.SOURCE),
+      (s"scala-2.$binaryVersion", JavaSourceRootType.SOURCE),
+      ("resources", JavaResourceRootType.RESOURCE),
+    ).map((ExpectedDirectoryCompletionVariant.apply _).tupled)
+
+  private def customSbtContentRootsForTest(binaryVersion: Int): Seq[ExpectedDirectoryCompletionVariant] =
+    Seq(
+      ("java", JavaSourceRootType.TEST_SOURCE),
+      ("scala", JavaSourceRootType.TEST_SOURCE),
+      (s"scala-2.$binaryVersion", JavaSourceRootType.TEST_SOURCE),
+      ("resources", JavaResourceRootType.TEST_RESOURCE),
+    ).map((ExpectedDirectoryCompletionVariant.apply _).tupled)
+
+  private def simpleSbtIvyBasedTest(mutedNotificationTitles: Seq[String] = Seq.empty): Unit = {
+    val scalaLibraries = ProjectStructureTestUtils.expectedScalaLibraryWithScalaSdkFromIvy(useEnv = true)("2.12.10")
+
+    runSimpleTest("simple", "2.12", scalaLibraries,
+      expectedSbtCompletionVariantsForParentModule = customSbtContentRootsForParent(12),
+      expectedSbtCompletionVariantsForMainModule = customSbtContentRootsForMain(12),
+      expectedSbtCompletionVariantsForTestModule = customSbtContentRootsForTest(12),
+      mutedNotificationTitles = mutedNotificationTitles
+    )
+
+    // Adding the assertion here not to create a separate heavy test for such a tiny check
+    // org.jetbrains.plugins.scala.project.ProjectExt#modulesWithScala
+    Assert.assertEquals(
+      "modulesWithScala should return list of non *-build modules",
+      Seq("simple.test", "simple.main"),
+      myProject.modulesWithScala.map(_.getName),
+    )
+
+    val expectedLineInProcessOutput = "[error] Some error message which shouldn't fail the whole build, see SCL-21478 and SCL-13038"
+    Assert.assertTrue(
+      s"Can't find this line in sbt process output during sbt structure extraction:\n$expectedLineInProcessOutput",
+      SbtProjectResolver.processOutputOfLatestStructureDump.contains(expectedLineInProcessOutput)
+    )
+  }
+
+  def testSimpleSbt013(): Unit = {
+    simpleSbtIvyBasedTest(mutedNotificationTitles = Seq("Legacy sbt version 0.13.18 detected"))
+  }
+
+  def testSimpleSbt104(): Unit = {
+    simpleSbtIvyBasedTest()
+  }
+
+  def testSimpleSbt116(): Unit = {
+    simpleSbtIvyBasedTest()
+  }
+
+  def testSimpleSbt128(): Unit = {
+    simpleSbtIvyBasedTest()
+  }
+
+  def testSimpleSbt1313(): Unit = {
+    val scalaLibraries = ProjectStructureTestUtils.expectedScalaLibraryWithScalaSdkForSbt(useEnv = true)("2.13.14")
+
+    runSimpleTest("simple", "2.13", scalaLibraries,
+      expectedSbtCompletionVariantsForParentModule = customSbtContentRootsForParent(13),
+      expectedSbtCompletionVariantsForMainModule = customSbtContentRootsForMain(13),
+      expectedSbtCompletionVariantsForTestModule = customSbtContentRootsForTest(13)
+    )
+
+    // Adding the assertion here not to create a separate heavy test for such a tiny check
+    // org.jetbrains.plugins.scala.project.ProjectExt#modulesWithScala
+    Assert.assertEquals(
+      "modulesWithScala should return list of non *-build modules",
+      Seq("simple.test", "simple.main"),
+      myProject.modulesWithScala.map(_.getName),
+    )
+
+    val expectedLineInProcessOutput = "[error] Some error message which shouldn't fail the whole build, see SCL-21478 and SCL-13038"
+    Assert.assertTrue(
+      s"Can't find this line in sbt process output during sbt structure extraction:\n$expectedLineInProcessOutput",
+      SbtProjectResolver.processOutputOfLatestStructureDump.contains(expectedLineInProcessOutput)
+    )
+  }
+
+  def testSimpleSbt149(): Unit = {
+    val scalaLibraries = ProjectStructureTestUtils.expectedScalaLibraryWithScalaSdkForSbt(useEnv = true)("2.13.14")
+
+    runSimpleTest("simple", "2.13", scalaLibraries,
+      expectedSbtCompletionVariantsForParentModule = DefaultSbtContentRootsScala213,
+      expectedSbtCompletionVariantsForMainModule = DefaultMainSbtContentRootsScala213,
+      expectedSbtCompletionVariantsForTestModule = DefaultTestSbtContentRootsScala213
+    )
+
+    // Adding the assertion here not to create a separate heavy test for such a tiny check
+    // org.jetbrains.plugins.scala.project.ProjectExt#modulesWithScala
+    Assert.assertEquals(
+      "modulesWithScala should return list of non *-build modules",
+      Seq("simple.test", "simple.main"),
+      myProject.modulesWithScala.map(_.getName),
+    )
+
+    val expectedLineInProcessOutput = "[error] Some error message which shouldn't fail the whole build, see SCL-21478 and SCL-13038"
+    Assert.assertTrue(
+      s"Can't find this line in sbt process output during sbt structure extraction:\n$expectedLineInProcessOutput",
+      SbtProjectResolver.processOutputOfLatestStructureDump.contains(expectedLineInProcessOutput)
+    )
+  }
 }
