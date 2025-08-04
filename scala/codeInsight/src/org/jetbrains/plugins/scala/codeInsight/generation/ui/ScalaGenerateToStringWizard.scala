fe9ee564ac783b3b84c034df932253422a39c9f8
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-08-04 10:25:07 +0200

    [code insight] Fix compilation in ScalaGenerateToStringWizard after the intellijVersion update to 253.9789

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/generation/ui/ScalaGenerateToStringWizard.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/generation/ui/ScalaGenerateToStringWizard.scala
@@ -3,20 +3,18 @@ package codeInsight
 package generation
 package ui
 
-import java.awt.BorderLayout
-
 import com.intellij.ide.wizard.{AbstractWizard, Step, StepAdapter}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.AsyncResult
 import com.intellij.refactoring.classMembers.AbstractMemberInfoModel
 import com.intellij.refactoring.ui.{AbstractMemberSelectionPanel, AbstractMemberSelectionTable}
-import javax.swing.JCheckBox
-import javax.swing.event.{TableModelEvent, TableModelListener}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 
+import java.awt.BorderLayout
+import javax.swing.JCheckBox
+import javax.swing.event.{TableModelEvent, TableModelListener}
 import scala.annotation.nowarn
-import scala.jdk.CollectionConverters.SeqHasAsJava
 
 /**
   * Wizard dialog to select class members for generation of toString method.
@@ -45,13 +43,6 @@ final class ScalaGenerateToStringWizard(classMembers: Seq[ScNamedElement])
 
     result
   }
-
-  /**
-    * Get IntelliJ help ID.
-    *
-    * @return IntelliJ help ID.
-    */
-  override def getHelpID: String = null
 
   override protected def init(): Unit = {
     super.init()
