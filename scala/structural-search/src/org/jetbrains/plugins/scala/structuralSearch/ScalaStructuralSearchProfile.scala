8fcb07cd8971f7429ed619a140003a0cb2ff7c04
Jonas Stahl <Jonas.Stahl@jetbrains.com>
2025-08-06 16:16:16 +0200

    [structural-search] Show Scala3 dialect #SCL-4599

--- scala/structural-search/src/org/jetbrains/plugins/scala/structuralSearch/ScalaStructuralSearchProfile.scala
+++ scala/structural-search/src/org/jetbrains/plugins/scala/structuralSearch/ScalaStructuralSearchProfile.scala
@@ -4,13 +4,14 @@ import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.lang.Language
 import com.intellij.structuralsearch.{StructuralSearchProfile, StructuralSearchProfileBase}
 import org.jetbrains.annotations.{NotNull, Nullable}
-import org.jetbrains.plugins.scala.ScalaLanguage
+import org.jetbrains.plugins.scala.{Scala3Language, ScalaLanguage}
 import org.jetbrains.plugins.scala.codeInsight.template.impl.ScalaFileTemplateContextType
 
 final class ScalaStructuralSearchProfile extends StructuralSearchProfileBase {
   override protected def getVarPrefixes: Array[String] = Array[String]("aaaaaaaaa")
 
-  override def isMyLanguage(@NotNull language: Language): Boolean = language == ScalaLanguage.INSTANCE
+  override def isMyLanguage(@NotNull language: Language): Boolean =
+    language == ScalaLanguage.INSTANCE || language == Scala3Language.INSTANCE
 
   override def getContext(pattern: String, @Nullable language: Language, contextId: String): String =
     StructuralSearchProfile.PATTERN_PLACEHOLDER
