7848430d8eca8215b1ae9252e2e20c064e86ca1d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-08-22 00:00:58 +0200

    [build] Update the directories-jni dependency to version 0.1.4

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -93,7 +93,7 @@ object Dependencies {
   // this actually needs the explicit version because something in packager breaks otherwise (???)
   val sbtStructureCore: ModuleID = "org.jetbrains.scala" %% "sbt-structure-core" % sbtStructureVersion
   val evoInflector: ModuleID = "org.atteo" % "evo-inflector" % "1.3"
-  val coursierDirectoriesJni: ModuleID = "io.get-coursier.util" % "directories-jni" % "0.1.3"
+  val coursierDirectoriesJni: ModuleID = "io.get-coursier.util" % "directories-jni" % "0.1.4"
   val apacheCommonsText: ModuleID = "org.apache.commons" % "commons-text" % "1.13.0"
   // NOTE: current latest version is in https://github.com/unkarjedy/scalatest-finders.git repository
 
