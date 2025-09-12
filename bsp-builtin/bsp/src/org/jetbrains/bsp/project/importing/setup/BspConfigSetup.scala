8d771589e36fe2125249fb2e5ac4dbce52c58651
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-09-12 17:03:16 +0200

    [bsp] Pass a ProgressIndicator instance to the bsp config setup process #SCL-24290
    - There is now some API duplication between the ProgressIndicator which can be used for cancellation and the BspConfigSetup#cancel() method which does the same thing.
    - This can be addressed in the future.

--- bsp-builtin/bsp/src/org/jetbrains/bsp/project/importing/setup/BspConfigSetup.scala
+++ bsp-builtin/bsp/src/org/jetbrains/bsp/project/importing/setup/BspConfigSetup.scala
@@ -1,10 +1,11 @@
 package org.jetbrains.bsp.project.importing.setup
 
+import com.intellij.openapi.progress.ProgressIndicator
 import org.jetbrains.plugins.scala.build.{BuildMessages, BuildReporter}
 
 import scala.util.Try
 
 abstract class BspConfigSetup {
   def cancel(): Unit
-  def run(implicit reporter: BuildReporter): Try[BuildMessages]
+  def run(indicator: ProgressIndicator)(implicit reporter: BuildReporter): Try[BuildMessages]
 }
