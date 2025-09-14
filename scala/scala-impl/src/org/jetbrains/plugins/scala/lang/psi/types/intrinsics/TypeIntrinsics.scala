ceb54d11a5337c4aedf84dc90e59f0d0fe31b3d2
Andrei Sugak <andrey.sugak@jetbrains.com>
2025-09-14 20:31:51 +0200

    [resolve] Add basic match type reduction tests
    #SCL-15104

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/intrinsics/TypeIntrinsics.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/types/intrinsics/TypeIntrinsics.scala
@@ -21,47 +21,49 @@ object TypeIntrinsics {
     substitutor: ScSubstitutor
   )(implicit
     context: Context
-  ): Option[ScType] = {
-    if (Registry.is("scala.enable.match.type.intrinsics"))
-      designator match {
-        case ScProjectionType.withActual(alias: ScTypeAlias, _) =>
-          implicit def project: Project = designator.projectContext.project
+  ): Option[ScType] =
+    designator match {
+      case ScProjectionType.withActual(alias: ScTypeAlias, _) =>
+        implicit def project: Project = designator.projectContext.project
 
-          val containingClassName = Option(alias.containingClass).map(_.qualifiedName).orNull
+        val matchTypeIntrinsicsEnabled = Registry.is("scala.enable.match.type.intrinsics")
+        val containingClassName        = Option(alias.containingClass).map(_.qualifiedName).orNull
 
-          //TODO: question of de-aliasing/reducing type aliases is more general and complicated
-          // For example see SCL-21176 and SCL-20263)
-          // For now (in Scala 3.2.1-RC4) it's done for scala.compiletime.ops
-          // But ideally it should be done more uniformly.
-          // See also: https://github.com/lampepfl/dotty/pull/14586
-          @tailrec
-          def dealias(ty: ScType): ScType = substitutor(ty.removeAliasDefinitions()) match {
-            case ScDesignatorType(ty: Typeable) if ty.is[ScBindingPattern, ScParameter, ScFieldId] =>
-              dealias(ty.`type`().getOrNothing)
-            //case undef: UndefinedType => dealias(undef.typeParameter.lowerType)
-            case ScExistentialType(ty, _) => dealias(ty)
-            case ty => ty
-          }
+        //TODO: question of de-aliasing/reducing type aliases is more general and complicated
+        // For example see SCL-21176 and SCL-20263)
+        // For now (in Scala 3.2.1-RC4) it's done for scala.compiletime.ops
+        // But ideally it should be done more uniformly.
+        // See also: https://github.com/lampepfl/dotty/pull/14586
+        @tailrec
+        def dealias(ty: ScType): ScType = substitutor(ty.removeAliasDefinitions()) match {
+          case ScDesignatorType(ty: Typeable) if ty.is[ScBindingPattern, ScParameter, ScFieldId] =>
+            dealias(ty.`type`().getOrNothing)
+          //case undef: UndefinedType => dealias(undef.typeParameter.lowerType)
+          case ScExistentialType(ty, _) => dealias(ty)
+          case ty => ty
+        }
 
-          lazy val argumentsDealiased = arguments.map(dealias)
+        lazy val argumentsDealiased = arguments.map(dealias)
 
-          (containingClassName: @switch) match {
-            // compiletime.ops
-            case "scala.compiletime.ops.any" => CompileTimeOpsIntrinsics.anyOp(alias.name, argumentsDealiased)
-            case "scala.compiletime.ops.boolean" => CompileTimeOpsIntrinsics.booleanOp(alias.name, argumentsDealiased)
-            case "scala.compiletime.ops.int" => CompileTimeOpsIntrinsics.intOp(alias.name, argumentsDealiased)
-            case "scala.compiletime.ops.long" => CompileTimeOpsIntrinsics.longOp(alias.name, argumentsDealiased)
-            case "scala.compiletime.ops.float" => CompileTimeOpsIntrinsics.floatOp(alias.name, argumentsDealiased)
-            case "scala.compiletime.ops.double" => CompileTimeOpsIntrinsics.doubleOp(alias.name, argumentsDealiased)
-            case "scala.compiletime.ops.string" => CompileTimeOpsIntrinsics.stringOp(alias.name, argumentsDealiased)
+        (containingClassName: @switch) match {
+          // compiletime.ops
+          case "scala.compiletime.ops.any"     => CompileTimeOpsIntrinsics.anyOp(alias.name, argumentsDealiased)
+          case "scala.compiletime.ops.boolean" => CompileTimeOpsIntrinsics.booleanOp(alias.name, argumentsDealiased)
+          case "scala.compiletime.ops.int"     => CompileTimeOpsIntrinsics.intOp(alias.name, argumentsDealiased)
+          case "scala.compiletime.ops.long"    => CompileTimeOpsIntrinsics.longOp(alias.name, argumentsDealiased)
+          case "scala.compiletime.ops.float"   => CompileTimeOpsIntrinsics.floatOp(alias.name, argumentsDealiased)
+          case "scala.compiletime.ops.double"  => CompileTimeOpsIntrinsics.doubleOp(alias.name, argumentsDealiased)
+          case "scala.compiletime.ops.string"  => CompileTimeOpsIntrinsics.stringOp(alias.name, argumentsDealiased)
 
-            // Named tuples
-            case "scala.NamedTuple" => NamedTupleIntrinsics.namedTupleOp(alias.name, argumentsDealiased)
-            case "scala.Tuple" => TupleIntrinsics.tupleOp(alias.name, argumentsDealiased, argumentsDealiased)
-            case "scala.NamedTupleDecomposition" => NamedTupleDecompositionIntrinsics.namedTupleDecompositionOp(alias.name, argumentsDealiased)
-            case _ => None
-          }
-        case _ => None
-      } else None
-  }
+          // Named tuples
+          case "scala.NamedTuple" if matchTypeIntrinsicsEnabled =>
+            NamedTupleIntrinsics.namedTupleOp(alias.name, argumentsDealiased)
+          case "scala.Tuple" if matchTypeIntrinsicsEnabled =>
+            TupleIntrinsics.tupleOp(alias.name, argumentsDealiased, argumentsDealiased)
+          case "scala.NamedTupleDecomposition" =>
+            NamedTupleDecompositionIntrinsics.namedTupleDecompositionOp(alias.name, argumentsDealiased)
+          case _ => None
+        }
+      case _ => None
+    }
 }
