ff846e7856465e4af404071ed4e1f836296f7938
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-09-16 10:21:57 +0200

    [scala build] add #logErr method to BuildReporter #SCL-24289
    - move BuildReporter from bsp to sbt module

--- sbt/sbt-impl/src/org/jetbrains/plugins/scala/build/BuildToolWindowReporter.scala
+++ sbt/sbt-impl/src/org/jetbrains/plugins/scala/build/BuildToolWindowReporter.scala
@@ -10,8 +10,8 @@ import com.intellij.openapi.actionSystem.{ActionUpdateThread, AnAction, AnAction
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.project.{DumbAwareAction, Project}
 import org.jetbrains.annotations.Nls
-import org.jetbrains.bsp.BspBundle
 import org.jetbrains.plugins.scala.build.BuildMessages.EventId
+import org.jetbrains.sbt.SbtBundle
 
 import java.nio.file.Path
 import javax.swing.JComponent
@@ -42,7 +42,7 @@ class BuildToolWindowReporter(project: Project,
           descriptor
         }
         .withRestartActions(cancelAction)
-    val startEvent = new StartBuildEventImpl(buildDescriptor, BspBundle.message("report.build.toolwindow.running"))
+    val startEvent = new StartBuildEventImpl(buildDescriptor, SbtBundle.message("report.build.toolwindow.running"))
     viewManager.onEvent(buildId, startEvent)
   }
 
@@ -65,14 +65,14 @@ class BuildToolWindowReporter(project: Project,
   override def finishWithFailure(err: Throwable): Unit = {
     val failureResult = new FailureResultImpl(err)
     val finishEvent =
-      new FinishBuildEventImpl(buildId, null, System.currentTimeMillis(), BspBundle.message("report.build.toolwindow.failed"), failureResult)
+      new FinishBuildEventImpl(buildId, null, System.currentTimeMillis(), SbtBundle.message("report.build.toolwindow.failed"), failureResult)
     viewManager.onEvent(buildId, finishEvent)
   }
 
   override def finishCanceled(): Unit = {
     val canceledResult = new SkippedResultImpl
     val finishEvent =
-      new FinishBuildEventImpl(buildId, null, System.currentTimeMillis(), BspBundle.message("report.build.toolwindow.canceled"), canceledResult)
+      new FinishBuildEventImpl(buildId, null, System.currentTimeMillis(), SbtBundle.message("report.build.toolwindow.canceled"), canceledResult)
     viewManager.onEvent(buildId, finishEvent)
   }
 
@@ -82,7 +82,7 @@ class BuildToolWindowReporter(project: Project,
   }
 
   override def progressTask(taskId: EventId, total: Long, progress: Long, unit: String, message: String, time: Long = System.currentTimeMillis()): Unit = {
-    val unitOrDefault = if (unit == null) BspBundle.message("report.build.toolwindow.items") else unit
+    val unitOrDefault = if (unit == null) SbtBundle.message("report.build.toolwindow.items") else unit
     val event = new ProgressBuildEventImpl(taskId, null, time, message, total, progress, unitOrDefault)
     viewManager.onEvent(buildId, event)
   }
@@ -104,11 +104,17 @@ class BuildToolWindowReporter(project: Project,
     viewManager.onEvent(buildId, event(message, Kind.INFO, position))
 
   override def log(message: String): Unit =
-    viewManager.onEvent(buildId, logEvent(message))
+    viewManager.onEvent(buildId, logEvent(message, isStdout = true))
 
-  private def logEvent(msg: String): BuildEvent = {
+  override def logErr(message: String): Unit =
+    viewManager.onEvent(buildId, logEvent(message, isStdout = false))
+
+  private def logEvent(msg: String, isStdout: Boolean): BuildEvent = {
     //noinspection ReferencePassedToNls
-    new OutputBuildEventImpl(buildId, msg.trim + System.lineSeparator(), ProcessOutputType.STDOUT)
+    val outputType =
+      if (isStdout) ProcessOutputType.STDOUT
+      else ProcessOutputType.STDOUT
+    new OutputBuildEventImpl(buildId, msg.trim + System.lineSeparator(), outputType)
   }
 
   private def event(message: String, kind: MessageEvent.Kind, position: Option[FilePosition])= {
@@ -119,7 +125,7 @@ class BuildToolWindowReporter(project: Project,
 
 object BuildToolWindowReporter {
   class CancelBuildAction(cancelToken: Promise[_])
-    extends DumbAwareAction(BspBundle.message("report.build.toolwindow.cancel"), BspBundle.message("report.build.toolwindow.cancel"), AllIcons.Actions.Suspend) {
+    extends DumbAwareAction(SbtBundle.message("report.build.toolwindow.cancel"), SbtBundle.message("report.build.toolwindow.cancel"), AllIcons.Actions.Suspend) {
 
     override def actionPerformed(e: AnActionEvent): Unit = {
       cancelToken.failure(new ProcessCanceledException())
