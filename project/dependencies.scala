c38692a178bb14fc8b2a9cc01a4088e26e64988c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-09-29 20:24:53 +0200

    [build] Update the jetbrains annotations library dependency to version 26.0.2-1

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -97,7 +97,7 @@ object Dependencies {
   val apacheCommonsText: ModuleID = "org.apache.commons" % "commons-text" % "1.14.0"
   // NOTE: current latest version is in https://github.com/unkarjedy/scalatest-finders.git repository
 
-  val jetbrainsAnnotations: ModuleID = "org.jetbrains" % "annotations" % "26.0.2"
+  val jetbrainsAnnotations: ModuleID = "org.jetbrains" % "annotations" % "26.0.2-1"
 
   val structureExtractor_0_13: Dependency = sbtDep("org.jetbrains.scala", "sbt-structure-extractor", Versions.sbtStructureVersion, Versions.Sbt.structure_extractor_binary_0_13)
   val structureExtractor_1_0: Dependency = sbtDep("org.jetbrains.scala", "sbt-structure-extractor", Versions.sbtStructureVersion, Versions.Sbt.structure_extractor_binary_1_0)
