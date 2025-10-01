a133fc3e9dad838a968912efab40fab01544d35f
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-10-01 21:15:31 +0200

    [tests, grazie] Update the language tool version to 6.6.17

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -61,7 +61,7 @@ object Versions {
    * However according to Peter Gromov it shouldn't be important for us and we can use maven dependencies.
    * Those custom distributions usually contain performance fixes and not the logic.
    */
-  val LanguageToolVersion = "6.6.16"
+  val LanguageToolVersion = "6.6.17"
 
   object Sbt {
     val binary_0_13 = "0.13"
