34e2ffe004ab8fcaebbc069bdb9354672b6ea343
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-10-01 16:01:31 +0200

    [fix freezes] [editor actions] introduce TaskRunnerWithLoadingProgress that will be later used in #SCL-24416
    It is copied from com.intellij.codeInsight.hint.ParameterInfoTaskRunnerUtil and converted to Scala without major changes. Only some minor changes were made.
    I copied it to be able to adopt it quickly for our needs.
    
    Ideally, it would be nice if IntelliJ platform provided some API for something like this.
    However, I couldn't find any.
    Other actions don't have anything like this or use their custom implementation.
    
    For example com.intellij.codeInsight.hint.actions.ShowExpressionTypeAction/com.intellij.codeInsight.hint.ShowExpressionTypeHandler from the platform/IDEA doesn't show any progress placeholder, so if there is a long computation, it's not visible anyhow.
    
    And for example, quick documentation uses completely different implementation (see `com.intellij.lang.documentation.ide.ui.DocumentationUI.updateContent`)
    
    
    (cherry picked from commit 8e12f0aa01a4b5f7f4b430c294715a07cbebbbe3)

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/actions/utils/TaskRunnerWithLoadingProgress.scala
@@ -0,0 +1,188 @@
+package org.jetbrains.plugins.scala.actions.utils
+
+import com.intellij.openapi.application.{ModalityState, NonBlockingReadAction}
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.LoadingDecorator
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.util.{Disposer, NlsContexts}
+import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.ui.components.panels.NonOpaquePanel
+import com.intellij.ui.components.{JBLabel, JBLoadingPanel}
+import com.intellij.util.concurrency.{AppExecutorUtil, EdtScheduler}
+import com.intellij.util.ui.{AnimatedIcon, AsyncProcessIcon, EmptyIcon, UIUtil}
+import org.jetbrains.annotations.{ApiStatus, Nullable}
+import org.jetbrains.concurrency.CancellablePromise
+
+import java.awt.{Component, FlowLayout}
+import java.util.Objects
+import java.util.concurrent.atomic.AtomicReference
+import java.util.function.Consumer
+import javax.swing.{JLabel, JPanel}
+import scala.concurrent.duration.{Duration, DurationInt}
+
+/**
+ * This utility class is designed to be able to run a composite action that:
+ *  1. Launches a background task in a non blockable read action with some computation
+ *  1. Shows a progress popup while the computation is running
+ *  1. Present the computation results on the UI thread
+ *
+ * The trickiest part is the logic around showing the loading placeholder tooltip and handling cancellations.
+ * Without this requirement we could simply use the API in [[com.intellij.openapi.application.ReadAction]]
+ * and [[com.intellij.openapi.application.NonBlockingReadAction]]
+ *
+ * Copied from [[com.intellij.codeInsight.hint.ParameterInfoTaskRunnerUtil]] and converted to Scala.
+ * No major significant changes were made in the initial commit.
+ */
+@ApiStatus.Experimental
+private[actions] object TaskRunnerWithLoadingProgress {
+  private val DefaultProgressPopupDelay: Duration = 1000.millis
+
+  private def getFocusOwner(project: Project): Component = IdeFocusManager.getInstance(project).getFocusOwner
+
+  /**
+   * @param progressTitle     null means no loading panel should be shown
+   * @param cancelOnScrolling cancel execution on scrolling
+   */
+  def runTask[T](
+    project: Project,
+    backgroundAction: NonBlockingReadAction[T],
+    continuationConsumer: Consumer[_ >: T],
+    @Nullable @NlsContexts.ProgressTitle
+    progressTitle: String,
+    editor: Editor,
+    cancelOnScrolling: Boolean
+  ): Unit = {
+    val cancellablePromiseRef = new AtomicReference[CancellablePromise[_]]()
+    val stopAction = startProgressAndCreateStopAction(project, progressTitle, cancellablePromiseRef, editor)
+
+    val visibleAreaListener = new CancelProgressOnScrolling(cancellablePromiseRef)
+    if (cancelOnScrolling) {
+      editor.getScrollingModel.addVisibleAreaListener(visibleAreaListener)
+    }
+
+    val originalFocusOwner = getFocusOwner(project)
+
+    val continuationConsumerPatched: Consumer[_ >: T] = result => {
+      val promise = cancellablePromiseRef.get()
+      val actionWasNotCancelled = promise != null && promise.isSucceeded
+      //TODO: this check will probably not needed if we just cancel the computation together with the progress action disposal?
+      if (actionWasNotCancelled) {
+        val focusOwner = getFocusOwner(project)
+        //TODO: same applies here?
+        val focusHasNotChangedSinceActionInvocation = Objects.equals(originalFocusOwner, focusOwner)
+        if (focusHasNotChangedSinceActionInvocation) {
+          continuationConsumer.accept(result)
+        }
+      }
+    }
+    // Unfortunately, the Editor interface is not disposable
+    val disposable = editor match {
+      case impl: EditorImpl => impl.getDisposable
+      case _ => project
+    }
+
+    val submittedActionPromise = backgroundAction
+      .finishOnUiThread(
+        ModalityState.defaultModalityState(),
+        continuationConsumerPatched
+      )
+      .expireWith(disposable)
+      .submit(AppExecutorUtil.getAppExecutorService)
+      .onProcessed(_ => {
+        stopAction.accept(false)
+        if (cancelOnScrolling) {
+          editor.getScrollingModel.removeVisibleAreaListener(visibleAreaListener)
+        }
+      })
+
+    cancellablePromiseRef.set(submittedActionPromise)
+  }
+
+  private def startProgressAndCreateStopAction(
+    project: Project,
+    @Nullable @NlsContexts.ProgressTitle
+    progressTitle: String,
+    promiseRef: AtomicReference[_ <: CancellablePromise[_]],
+    editor: Editor
+  ): Consumer[Boolean] = {
+    val stopActionRef = new AtomicReference[Consumer[Boolean]]
+
+    val originalStopAction: Consumer[Boolean] = (cancel: Boolean) => {
+      stopActionRef.set(null)
+
+      if (cancel) {
+        val promise = promiseRef.get()
+        if (promise != null) {
+          promise.cancel()
+        }
+      }
+    }
+
+    if (progressTitle == null) {
+      stopActionRef.set(originalStopAction)
+    } else {
+      val disposable = Disposer.newDisposable()
+      Disposer.register(project, disposable)
+
+      val loadingPanel = new JBLoadingPanel(null, panel =>
+        new LoadingDecorator(panel, disposable, 0, false, new AsyncProcessIcon(s"ProgressUtils(title: $progressTitle)")) {
+          override protected def customizeLoadingLayer(parent: JPanel, text: JLabel, icon: AnimatedIcon): NonOpaquePanel = {
+            parent.setLayout(new FlowLayout(FlowLayout.LEFT))
+            val result = new NonOpaquePanel
+            result.add(icon)
+            parent.add(result)
+            result
+          }
+        })
+      loadingPanel.add(new JBLabel(EmptyIcon.ICON_18))
+      loadingPanel.add(new JBLabel(progressTitle))
+
+      val popupBuilder = JBPopupFactory.getInstance()
+        .createComponentPopupBuilder(loadingPanel, null)
+        .setProject(project)
+        .setCancelCallback(() => {
+          val stopAction = stopActionRef.get()
+          if (stopAction != null) {
+            stopAction.accept(true)
+          }
+          true
+        })
+
+      val popup = popupBuilder.createPopup()
+      Disposer.register(disposable, popup)
+
+      val showPopupFuture: kotlinx.coroutines.Job = EdtScheduler.getInstance.schedule(
+        DefaultProgressPopupDelay.toMillis.toInt,
+        ModalityState.defaultModalityState(),
+        () => {
+          val needToShowPopup = !popup.isDisposed && !popup.isVisible && !editor.isDisposed
+          if (needToShowPopup) {
+            val popupPosition = JBPopupFactory.getInstance().guessBestPopupLocation(editor)
+            loadingPanel.startLoading()
+            popup.show(popupPosition)
+          }
+        }
+      )
+
+      stopActionRef.set((cancel: Boolean) => {
+        try {
+          loadingPanel.stopLoading()
+          originalStopAction.accept(cancel)
+        } finally {
+          showPopupFuture.cancel(null)
+
+          UIUtil.invokeLaterIfNeeded(() => {
+            if (popup.isVisible) {
+              popup.setUiVisible(false)
+            }
+            Disposer.dispose(disposable)
+          })
+        }
+      })
+    }
+
+    stopActionRef.get()
+  }
+}
