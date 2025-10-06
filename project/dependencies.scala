11a0209a34703a4abe0284ca68f8675c58364c56
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-10-06 09:37:16 +0200

    [build] Update the sbt dependency to version 1.11.7
    - This release brings fixes to the launcher for starting sbt 0.13.
    - We had test errors because of this problem.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -12,7 +12,7 @@ object Versions {
   // ATTENTION: when updating `sbtVersion` also update it in `org.jetbrains.sbt.SbtVersion.Latest`
   // NOTE: sbt-launch won't be fetched on refresh.
   // run runtimeDependencies/update manually
-  val sbtVersion: String = "1.11.6"
+  val sbtVersion: String = "1.11.7"
   val bloopVersion = "2.0.14"
   val zincVersion = "1.10.8"
 
