359bf93b2bde92a61a683a0c6cb0744b7e830350
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-10-08 22:49:26 +0200

    [tests] The RunWithVersions annotations are specified on type definitions and methods

--- scala/scala-impl/test/org/jetbrains/plugins/scala/util/runners/RunWithJdkVersions.java
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/util/runners/RunWithJdkVersions.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.CONSTRUCTOR)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface RunWithJdkVersions {
 
     /** unsupported JDK for scala SDK are filtered out in
