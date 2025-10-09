194907e9d01cd4cf86dbacef800afb2f00f6aa0c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-10-09 10:43:12 +0200

    [tests] Migrate ScalaMoveDirectoryWithClassesTests to MultipleScalaVersionsJUnit4Runner

--- scala/scala-impl/test/org/jetbrains/plugins/scala/refactoring/move/directory/ScalaMoveDirectoryWithClassesTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/refactoring/move/directory/ScalaMoveDirectoryWithClassesTest.scala
@@ -2,28 +2,35 @@ package org.jetbrains.plugins.scala.refactoring.move.directory
 
 import com.intellij.openapi.vfs.{VirtualFileEvent, VirtualFileListener, VirtualFileManager}
 import junit.framework.TestCase.assertFalse
-import org.jetbrains.plugins.scala.util.runners.{MultipleScalaVersionsRunner, RunWithScalaVersions, TestScalaVersion}
+import org.jetbrains.plugins.scala.util.runners.{MultipleScalaVersionsJUnit4Runner, RunWithScalaVersions, TestScalaVersion}
+import org.junit.Test
 import org.junit.runner.RunWith
 
 import scala.annotation.nowarn
 
-@RunWith(classOf[MultipleScalaVersionsRunner])
+@RunWith(classOf[MultipleScalaVersionsJUnit4Runner])
 @RunWithScalaVersions(Array(
   TestScalaVersion.Scala_2_12,
   TestScalaVersion.Scala_2_13,
-  TestScalaVersion.Scala_3_Latest,
+  TestScalaVersion.Scala_3_Latest
 ))
 class ScalaMoveDirectoryWithClassesTest extends ScalaMoveDirectoryWithClassesTestBase {
+  @Test
   def testRenamePackage(): Unit = doRenamePackageTest("pack1", "pack1.pack2")
 
+  @Test
   def testRenamePackageUp(): Unit = doRenamePackageTest("pack1.pack2", "pack1")
 
+  @Test
   def testRenamePackageImportsToNestedClasses(): Unit = doRenamePackageTest("pack1.pack2", "pack0.pack2")
 
+  @Test
   def testRenamePackageWithPackageObject(): Unit = doRenamePackageTest("pack1.pack2", "pack0.pack1.pack2")
 
+  @Test
   def testMovePackageWithPackageObject(): Unit = doMovePackageTest("pack1.pack2", "pack3")
 
+  @Test
   def testMultipleClassesInOneFile(): Unit = {
     var fileWasDeleted = false
     VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileListener {
@@ -34,5 +41,6 @@ class ScalaMoveDirectoryWithClassesTest extends ScalaMoveDirectoryWithClassesTes
     assertFalse("Deleted instead of move", fileWasDeleted)
   }
 
+  @Test
   def testEmptySubDirs(): Unit = doMovePackageTest()
 }
