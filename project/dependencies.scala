b9aef51d7bff920920cc4cf89889f2776c3300dc
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-10-21 14:34:21 +0200

    Update intellijVersion to 253.27864.10 and fix compilation

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -20,7 +20,7 @@ object Versions {
    * ATTENTION: check the comment in [[Common.newProjectWithKotlin]] when updating this version.
    *            update `since-build` in plugin.xml if there are binary incompatible changes after update
    */
-  val intellijVersion = "253.27642.30"
+  val intellijVersion = "253.27864.10"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
