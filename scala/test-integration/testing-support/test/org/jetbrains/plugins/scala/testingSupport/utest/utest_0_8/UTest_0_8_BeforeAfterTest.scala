062417bc8779c161b1181f06eb18e8e5dbfb4080
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-11-04 11:20:23 +0100

    [uTest] refactor tests layout and naming + drop tests pre-0.7 syntax #SCL-24506

--- scala/test-integration/testing-support/test/org/jetbrains/plugins/scala/testingSupport/utest/utest_0_8/UTest_0_8_BeforeAfterTest.scala
+++ scala/test-integration/testing-support/test/org/jetbrains/plugins/scala/testingSupport/utest/utest_0_8/UTest_0_8_BeforeAfterTest.scala
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.scala.testingSupport.utest.scala2_12.old_syntax
+package org.jetbrains.plugins.scala.testingSupport.utest.utest_0_8
 
-import org.jetbrains.plugins.scala.testingSupport.utest.scala2_12.UTestTestBase_2_12
+import org.jetbrains.plugins.scala.testingSupport.utest.UTestTestCase
 
-class UTestBeforeAfterTest_2_12_old_syntax extends UTestTestBase_2_12 {
+trait UTest_0_8_BeforeAfterTest extends UTestTestCase {
 
   val beforeAfterTestName = "BeforeAfterTest"
   val beforeAfterFileName = beforeAfterTestName + ".scala"
@@ -13,21 +13,21 @@ class UTestBeforeAfterTest_2_12_old_syntax extends UTestTestBase_2_12 {
        |
        |object $beforeAfterTestName extends TestSuite {
        |  val tests = Tests {
-       |    "test1" - {}
+       |    test("test1") {}
        |  }
        |
        |  override def utestBeforeEach(path: Seq[String]): Unit = { println("$TestOutputPrefix BEFORE $TestOutputSuffix") }
        |
        |  override def utestAfterEach(path: Seq[String]): Unit = { println("$TestOutputPrefix AFTER $TestOutputSuffix") }
        |}
-      """.stripMargin.trim())
+       |""".stripMargin.trim())
 
   def testBefore(): Unit =
     runTestByLocation(
       loc(beforeAfterFileName, 4, 10),
       assertConfigAndSettings(_, beforeAfterTestName, "tests\\test1"),
       IgnoreTreeResult,
-      output => assertTestOutputTextContains(expectedText = "BEFORE", output)
+      output => assertTestOutputTextContains("BEFORE", output)
     )
 
   def testAfter(): Unit =
@@ -35,6 +35,6 @@ class UTestBeforeAfterTest_2_12_old_syntax extends UTestTestBase_2_12 {
       loc(beforeAfterFileName, 4, 10),
       assertConfigAndSettings(_, beforeAfterTestName, "tests\\test1"),
       IgnoreTreeResult,
-      output => assertTestOutputTextContains(expectedText = "AFTER", output)
+      output => assertTestOutputTextContains("AFTER", output)
     )
 }
