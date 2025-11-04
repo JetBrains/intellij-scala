18ccebae6b2187ff44efc86b145091995617b872
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-11-04 07:55:27 +0100

    [eel, sbt] Remove an instance of a path translation to io file and back to nio path in a single chain #SCL-23308

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtProjectResolver.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtProjectResolver.scala
@@ -303,7 +303,7 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
         doDumpStructure(structureFilePath)
       } else {
         usingTempFile("sbt-structure", Some(".xml")) { structureFile =>
-          doDumpStructure(structureFile.toFile.toPath)
+          doDumpStructure(structureFile)
         }
       }
     }
