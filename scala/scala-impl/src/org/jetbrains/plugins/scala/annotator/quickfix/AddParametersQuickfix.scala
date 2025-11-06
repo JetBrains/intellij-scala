c501deb494a104156e52790e121850640395c178
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-11-06 17:17:56 +0100

    [quickfix] improved AddParametersQuickfix #SCL-24374
    (cherry picked from commit 5e440f2b67f751a6eb45c82325fa3d6ed25af513)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/AddParametersQuickfix.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/AddParametersQuickfix.scala
@@ -1,64 +1,68 @@
 package org.jetbrains.plugins.scala.annotator.quickfix
 
 import com.intellij.codeInsight.CodeInsightUtilCore
-import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.codeInsight.intention.impl.BaseIntentionAction
 import com.intellij.codeInsight.intention.preview.IntentionPreviewUtils
 import com.intellij.codeInsight.template.TemplateBuilderImpl
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.{PsiFile, SmartPsiElementPointer}
 import org.jetbrains.plugins.scala.ScalaBundle
 import org.jetbrains.plugins.scala.annotator.TemplateUtils
 import org.jetbrains.plugins.scala.annotator.createFromUsage.CreateFromUsageUtil.addParametersToTemplate
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScArgumentExprList, ScExpression, ScMethodCall}
+import org.jetbrains.plugins.scala.extensions.{ObjectExt, PsiElementExt}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{MethodInvocation, ScArgumentExprList, ScExpression, ScMethodCall}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunction
 import org.jetbrains.plugins.scala.lang.psi.api.statements.params.{ScParameter, ScParameterClause}
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiElementFactory.createClauseFromText
-import org.jetbrains.plugins.scala.lang.psi.types.TypePresentationContext
+import org.jetbrains.plugins.scala.lang.psi.types.{TypePresentationContext, api}
+import org.jetbrains.plugins.scala.project.ProjectContext
 
 import scala.annotation.tailrec
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable
 
-final class AddParametersQuickfix extends PsiElementBaseIntentionAction {
+final class AddParametersQuickfix(argExprList: SmartPsiElementPointer[ScArgumentExprList]) extends BaseIntentionAction {
+  override def invoke(project: Project, editor: Editor, psiFile: PsiFile): Unit =
+    addParameterFix(editor, psiFile).foreach(_.apply())
 
-  override def invoke(project: Project, editor: Editor, element: PsiElement): Unit =
-    if (element.isValid) addParameterFix(element, editor).foreach(_.apply())
-
-  override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean =
-    addParameterFix(element, editor).isDefined
+  override def isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean =
+    addParameterFix(editor, file).isDefined
 
   override def getFamilyName: String = ScalaBundle.message("add.parameter.to.method.family.name")
 
   override def getText: String = ScalaBundle.message("add.parameter.to.method")
 
-  private def addParameterFix(element: PsiElement, editor: Editor): Option[() => Unit] = {
-    val argList: ScArgumentExprList = PsiTreeUtil.getParentOfType(element, classOf[ScArgumentExprList])
-    if (argList == null || argList.isBlockArgs) return None
+  private def addParameterFix(editor: Editor, file: PsiFile): Option[() => Unit] = {
+    if (!file.isValid) return None
+    val argExprList = this.argExprList.getElement
+    if (argExprList == null || !argExprList.isValid || argExprList.isBlockArgs) return None
 
-    val methodCall = PsiTreeUtil.getParentOfType(element, classOf[ScMethodCall])
-    val arguments = methodCall.args.exprs
+    val methodCall = PsiTreeUtil.getParentOfType(argExprList, classOf[ScMethodCall])
 
     // Find the actual function and the parameter clause that corresponds to the edited argument list
     val clause = findParamList(methodCall) match {
-      case Some(clause) => clause
-      case None => return None
+      case Some(clause) if !clause.hasRepeatedParam => clause
+      case _ => return None
     }
+    implicit val tpc: TypePresentationContext = clause
+    implicit val pCtx: ProjectContext = clause
 
+    val arguments = argExprList.exprs
     val parameters = clause.parameters
-    if (parameters.size >= arguments.size) return None
 
-    if (!clause.isValid || !clause.isPhysical) return None
+    if (arguments.length <= parameters.length || !clause.isValid || !clause.isPhysical) return None
 
     Some(() => IntentionPreviewUtils.write { () =>
       if (!IntentionPreviewUtils.isIntentionPreviewActive) {
-        val indices       = findNewParameterIndices(parameters, arguments, clause)
-        val newParamTexts = generateNewParameterTexts(arguments, indices, parameters.map(_.name), clause)
-        val newClauseText = createNewClauseText(clause.getText, newParamTexts)
-        val newClause     = clause.replace(createClauseFromText(newClauseText, clause.getParent)(element.getManager)).asInstanceOf[ScParameterClause]
+        val injectionPositions = findInjectionPositions(arguments, parameters)
+        val newParamTexts = generateNewParameterTexts(injectionPositions, parameters.map(_.name))
+        val newClauseText = createNewClauseText(clause, newParamTexts)
+        val newClause     = clause.replace(createClauseFromText(newClauseText, clause.getParent)).asInstanceOf[ScParameterClause]
 
+        val isNewParam = injectionPositions.map { case (_, _, idx) => idx }.toSet
         val builder = new TemplateBuilderImpl(newClause)
-        addParametersToTemplate(newClause, builder, p => indices.contains(newClause.parameters.indexOf(p)))
+        addParametersToTemplate(newClause, builder, p => isNewParam(newClause.parameters.indexOf(p)))
         CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(newClause)
         TemplateUtils.positionCursorAndStartTemplate(newClause, builder.buildTemplate(), editor)
       }
@@ -90,51 +94,124 @@ final class AddParametersQuickfix extends PsiElementBaseIntentionAction {
     }
   }
 
-  private def createNewClauseText(oldClauseText: String, newParamTexts: Seq[(Int, String)]): String = {
-    val paramTexts = oldClauseText
-      .substring(1, oldClauseText.length - 1)
-      .split(',')
-      .map(_.trim)
-      .filter(_.nonEmpty)
-      .toList
+  /**
+   * Determines where the new parameters should be inserted in the parameter list.
+   *
+   * To do this, we use a modified version of the levenshtein distance,
+   * where we only allow going through the arguments (right) and replace params with args (diagonal)
+   * and prioritize keeping existing parameters at the front.
+   *
+   * Example:
+   *   Let's say that a, b, c, d, e, and f are types that are only conforming to themselves.
+   *   This gives us a matrix like this (empty fields are irrelevant and will not be calculated):
+   *
+   *   {{{
+   *           a   b   c   d   e   <- argument types
+   *       0 - 1   3
+   *             \
+   *     b     1   2 - 3
+   *                     \
+   *     d         2   3   3
+   *                         \
+   *     f             3   4   4
+   *     ^
+   *   param types
+   *   }}}
+   *
+   *   The resulting parameters are [new a, old b, new c, old d, old f (type mismatch)]
+   */
+  private def findInjectionPositions(arguments: Seq[ScExpression], parameters: Seq[ScParameter])
+                                    (implicit projectContext: ProjectContext): Seq[(ScExpression, Option[ScParameter], Int)] = {
+    val indexedArgs = (api.Nothing +: arguments.map(_.getTypeAfterImplicitConversion().tr.getOrNothing)).toArray
+    val indexedParams = (api.Any +: parameters.map(_.`type`().getOrAny)).toArray
 
-    val newTxtList = newParamTexts.foldLeft(paramTexts) { case (pts, (i, newParamText)) =>
-      val (prev, next) = pts.splitAt(i)
-      prev ::: newParamText :: next
-    }
-    "(" + newTxtList.mkString(", ") + ")"
+    case class Entry(cost: Int, path: List[(ScExpression, Option[ScParameter], Int)])
+    val cache = mutable.Map.empty[(Int, Int), Entry]
+    cache.put((0, 0), Entry(0, List.empty))
+
+    def findPath(pos: (Int, Int)): Entry = cache.getOrElseUpdate(pos, {
+      val (argIdx, paramIdx) = pos
+      lazy val beforeTakingArgument = findPath((argIdx - 1, paramIdx))
+      lazy val beforeReplacing = findPath((argIdx - 1, paramIdx - 1))
+
+      def takeArgumentCost = beforeTakingArgument.cost + 1
+      def replaceUnfittingCost = beforeReplacing.cost + 1
+      def replaceFittingCost = beforeReplacing.cost
+
+      lazy val replaceCost =
+        if (indexedArgs(argIdx).conforms(indexedParams(paramIdx)))
+          replaceFittingCost
+        else
+          replaceUnfittingCost
+
+      val useReplace = {
+        if (paramIdx == 0) false
+        else if (argIdx == paramIdx) true
+        else replaceCost < takeArgumentCost
+      }
+
+      if (useReplace) {
+        Entry(replaceCost, beforeReplacing.path)
+      } else {
+        val argument = arguments(argIdx - 1)
+        val anchor = parameters.lift(paramIdx - 1)
+        Entry(takeArgumentCost, (argument, anchor, argIdx - 1) :: beforeTakingArgument.path)
+      }
+    })
+
+    findPath((arguments.length, parameters.length)).path.reverse
   }
 
-  private def findNewParameterIndices(parameters: Seq[ScParameter], arguments: Seq[ScExpression], place: PsiElement): Seq[Int] = {
-    val buf = ArrayBuffer.empty[Int]
-    var ai = 0
-    var pi = 0
-    while (ai < arguments.size) {
-      if (pi == parameters.size) buf.addOne(ai)
-      else {
-        val ptr = parameters(pi).`type`().getOrNothing.toString
-        val atr = argumentTypeText(arguments(ai))(place)
-        if (ptr != atr) buf.addOne(ai) else pi += 1
-      }
-      ai += 1
+  private def createNewClauseText(oldClause: ScParameterClause, newParamTexts: Seq[(String, Option[ScParameter])]): String = {
+    def insert(str: String, pos: Int, insertText: String): String = {
+      val (before, after) = str.splitAt(pos)
+      before + insertText + after
     }
-    buf.toSeq
+
+    val clauseText = oldClause.getText
+    val firstParamIndex = clauseText.indexOf('(') + 1
+    lazy val hasParameter = oldClause.parameters.nonEmpty
+
+    newParamTexts.foldRight(clauseText) {
+      case ((paramText, anchor), clauseText) =>
+        anchor match {
+          case Some(anchor) =>
+            insert(clauseText, anchor.getTextRangeInParent.getEndOffset, ", " + paramText)
+          case None =>
+            if (hasParameter) insert(clauseText, firstParamIndex, paramText + ", ")
+            else insert(clauseText, firstParamIndex, paramText)
+        }
+    }
+  }
+
+  private def generateNewParameterTexts(arguments: Seq[(ScExpression, Option[ScParameter], Int)], paramNames: Seq[String])
+                                       (implicit typePresentationCtx: TypePresentationContext): Seq[(String, Option[ScParameter])] = {
+    var index = 1
+    @tailrec
+    def mkNextArgName(): String = {
+      val name = s"arg$index"
+      index += 1
+      if (!paramNames.contains(name)) name
+      else {
+        mkNextArgName()
+      }
+    }
+
+    for ((argument, anchor, _) <- arguments) yield {
+      val name = mkNextArgName()
+      val typeText = argumentTypeText(argument)
+      val paramText = s"$name: $typeText"
+      (paramText, anchor)
+    }
   }
 
   private def argumentTypeText(expr: ScExpression)(implicit typePresentationCtx: TypePresentationContext): String =
     expr.getTypeAfterImplicitConversion().tr.getOrAny.widenIfLiteral.presentableText
+}
 
-  private def generateNewParameterTexts(arguments: Seq[ScExpression], indices: Seq[Int], paramNames: Seq[String], place: PsiElement): Seq[(Int, String)] = {
-    var index = 0
-    indices.map { i =>
-      index += 1
-      var name = s"arg$index"
-      while (paramNames.contains(name)) {
-        index += 1
-        name = s"arg$index"
-      }
-      val typeText = argumentTypeText(arguments(i))(place)
-      i -> s"$name: $typeText"
-    }
-  }
+object AddParametersQuickfix {
+  def from(invocation: MethodInvocation): Option[AddParametersQuickfix] =
+    invocation.asOptionOf[ScMethodCall].map(from)
+  def from(argExprList: ScMethodCall): AddParametersQuickfix =
+    new AddParametersQuickfix(argExprList.args.createSmartPointer)
 }
