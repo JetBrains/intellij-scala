9c7179bc58969096ea12119645c64a1344e44d0c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-11-12 12:15:29 +0100

    [scala 3 tests script] Migrate Scala3ImportedParserTest_Move_Fixed_Tests and Scala3ImportedParserTest_RegeneratePsi away from ScalaFileSetTestCase so we can get rid of it in the future
    (cherry picked from commit 57c5dfca21039f49ac027a298371966e47908f64)

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/imported/Scala3ImportedParserTest_RegeneratePsi.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/parser/scala3/imported/Scala3ImportedParserTest_RegeneratePsi.scala
@@ -1,60 +1,52 @@
 package org.jetbrains.plugins.scala.lang.parser.scala3.imported
 
-import com.intellij.openapi.project.Project
 import com.intellij.psi.impl.DebugUtil.psiToString
-import junit.framework.{Test, TestCase, TestSuite}
 import org.jetbrains.plugins.scala.extensions.{PathExt, PsiNamedElementExt}
 import org.jetbrains.plugins.scala.util.TestUtils
 import org.junit.Ignore
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
 
 import java.nio.file.{Files, Path}
 
-@Ignore("for local running only")
-class Scala3ImportedParserTest_RegeneratePsi extends TestCase
+@Ignore("For local running only")
+@RunWith(classOf[Suite])
+@Suite.SuiteClasses(Array(
+  classOf[Scala3ImportedParserTest_RegeneratePsi_LTS],
+  classOf[Scala3ImportedParserTest_RegeneratePsi_Newest]
+))
+class Scala3ImportedParserTest_RegeneratePsi
 
-object Scala3ImportedParserTest_RegeneratePsi {
-  /**
-   * Run this main method to move all scala 3 test files that generate no PsiErrorElements anymore to
-   * the succeeding directory
-   *
-   * Use this if you have made progress in the parser and fixed files that produced PsiErrorElement
-   * and, now, make Scala3ImportedParserTest_Fail fail. In this case this method will move those
-   * into the succeeding folder, so they can fail if someone screws anything up in the parser, that
-   * had previously worked.
-   */
-  def suite(): Test = {
-    val suite = new TestSuite
-    suite.addTest(new Scala3ImportedParserTest_RegeneratePsi(Scala3ImportedParserTestConfig.LTS))
-    suite.addTest(new Scala3ImportedParserTest_RegeneratePsi(Scala3ImportedParserTestConfig.Newest))
-    suite
-  }
+@Ignore("For local running only")
+class Scala3ImportedParserTest_RegeneratePsi_LTS
+  extends Scala3ImportedParserTest_RegeneratePsi_Base(Scala3ImportedParserTestConfig.LTS)
 
-  @Ignore("for local running only")
-  class Scala3ImportedParserTest_RegeneratePsi(config: Scala3ImportedParserTestConfig)
-    extends Scala3ImportedParserTestBase_UsedAsScript(config, runOnSucceedDirectory = true) {
+@Ignore("For local running only")
+class Scala3ImportedParserTest_RegeneratePsi_Newest
+  extends Scala3ImportedParserTest_RegeneratePsi_Base(Scala3ImportedParserTestConfig.Newest)
 
-    protected override def transform(testName: String, fileText: String, project: Project): String = {
-      val (errors, file) = findErrorElements(fileText, project)
-      val interlaced = findInterlacedRanges(file, testName)
+abstract class Scala3ImportedParserTest_RegeneratePsi_Base(config: Scala3ImportedParserTestConfig)
+  extends Scala3ImportedParserTestBase_UsedAsScript(config, runOnSucceedDirectory = true) {
 
-      if (errors.isEmpty && interlaced.isEmpty) {
-        val rootTestDataPath = Path.of(TestUtils.getTestDataPath)
-        val path = rootTestDataPath / config.successDataDirectory / s"$testName.test"
+  override protected def transform(testName: String, fileText: String): String = {
+    val (errors, file) = findErrorElements(fileText, project)
+    val interlaced = findInterlacedRanges(file, testName)
 
-        println("Regenerate " + path)
-        val psiTreeText = psiToString(file, true).replace(": " + file.name, "")
-        val content = Files.readString(path)
-        val searchString = "-----\n"
-        val idx = content.indexOf(searchString).ensuring(_ >= 0)
-        val newContent = content.substring(0, idx + searchString.length) + psiTreeText
-        Files.writeString(path, newContent)
-      }
+    if (errors.isEmpty && interlaced.isEmpty) {
+      val rootTestDataPath = Path.of(TestUtils.getTestDataPath)
+      val path = rootTestDataPath / config.successDataDirectory / s"$testName.test"
 
-      ""
+      println("Regenerate " + path)
+      val psiTreeText = psiToString(file, true).replace(": " + file.name, "")
+      val content = Files.readString(path)
+      val searchString = "-----\n"
+      val idx = content.indexOf(searchString).ensuring(_ >= 0)
+      val newContent = content.substring(0, idx + searchString.length) + psiTreeText
+      Files.writeString(path, newContent)
     }
 
-    override protected def transformExpectedResult(text: String): String = {
-      ""
-    }
+    ""
   }
+
+  override protected def transformExpectedResult(text: String): String = ""
 }
