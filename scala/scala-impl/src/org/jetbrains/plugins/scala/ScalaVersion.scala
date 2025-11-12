c8484240932c803bc7579d590781aed5d5315b34
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-11-12 09:58:59 +0100

    [tests] Run tests against Scala 3.7.4

--- scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
@@ -104,7 +104,7 @@ object LatestScalaVersions {
   val Scala_3_4 = new ScalaVersion(ScalaLanguageLevel.Scala_3_4, "2")
   val Scala_3_5 = new ScalaVersion(ScalaLanguageLevel.Scala_3_5, "2")
   val Scala_3_6 = new ScalaVersion(ScalaLanguageLevel.Scala_3_6, "4")
-  val Scala_3_7 = new ScalaVersion(ScalaLanguageLevel.Scala_3_7, "3")
+  val Scala_3_7 = new ScalaVersion(ScalaLanguageLevel.Scala_3_7, "4")
 
   //
   // Aliases
@@ -120,7 +120,7 @@ object LatestScalaVersions {
   val Scala_3_LTS_RC: ScalaVersion = Scala_3_LTS
 
   // Scala Next RC
-  val Scala_3_Next_RC: ScalaVersion = new ScalaVersion(ScalaLanguageLevel.Scala_3_7, "4-RC3")
+  val Scala_3_Next_RC: ScalaVersion = Scala_3_7
 
   val allScala2: Seq[ScalaVersion] = Seq(
     Scala_2_9,
