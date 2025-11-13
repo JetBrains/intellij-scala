e7a73ee57c6caeb70c1fb82303450bd80c39aec4
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-11-13 22:45:41 +0100

    [scaladoc] fix rendering codeblock-only comments #SCL-18476

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/api/ScDocInnerCodeElement.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/psi/api/ScDocInnerCodeElement.scala
@@ -2,4 +2,4 @@ package org.jetbrains.plugins.scala.lang.scaladoc.psi.api
 
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaPsiElement
 
-trait ScDocInnerCodeElement extends ScalaPsiElement
+trait ScDocInnerCodeElement extends ScalaPsiElement with ScDocDescriptionPart
