db64b4393189e5f7281065a906236e5c1ba2797c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-11-18 09:19:25 +0100

    [sdk, tests] Fix JDK selection for Scala 3.8+ tests for compiler and worksheet tests #SCL-24642
    (cherry picked from commit 1b3374d81968f16a7b3b121abb97bcfcdb69ae5e)

--- scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/plain/WorksheetPlainCompileOnServerRunLocallyIntegrationTest.scala
+++ scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/plain/WorksheetPlainCompileOnServerRunLocallyIntegrationTest.scala
@@ -37,6 +37,7 @@ class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_LTS_RC_AllI
     with WorksheetPlainIntegrationTestBase_Scala_3_LTS_RC_AllInOne
 
 @RunWithScalaVersions(Array(TestScalaVersion.Scala_3_Next_RC))
+@RunWithJdkVersions(Array(TestJdkVersion.JDK_17))
 class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_Next_RC_AllInOne
   extends CompileOnServerRunLocallyTestBase
     with WorksheetPlainIntegrationTestBase_Scala_3_Next_RC_AllInOne
@@ -46,9 +47,13 @@ class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_Next_RC_All
   TestScalaVersion.Scala_3_1,
   TestScalaVersion.Scala_3_2,
   TestScalaVersion.Scala_3_3,
-  TestScalaVersion.Scala_3_Latest_RC,
-  TestScalaVersion.Scala_3_Next_RC
+  TestScalaVersion.Scala_3_Latest_RC
 ))
 class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_BracelessSyntax
   extends CompileOnServerRunLocallyTestBase
     with WorksheetPlainIntegrationTestBase_Scala_3_BracelessSyntax
+
+@RunWithScalaVersions(Array(TestScalaVersion.Scala_3_Next_RC))
+@RunWithJdkVersions(Array(TestJdkVersion.JDK_17))
+class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_BracelessSyntax_Next_RC
+  extends WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_BracelessSyntax
--- scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/repl/WorksheetReplIntegration_Scala_3_Test.scala
+++ scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/repl/WorksheetReplIntegration_Scala_3_Test.scala
@@ -13,10 +13,14 @@ import scala.language.postfixOps
 
 @RunWithScalaVersions(Array(
   TestScalaVersion.Scala_3_Latest,
-  TestScalaVersion.Scala_3_Latest_RC,
-  TestScalaVersion.Scala_3_Next_RC
+  TestScalaVersion.Scala_3_Latest_RC
 ))
 class WorksheetReplIntegration_Scala_3_Latest_Test extends WorksheetReplIntegration_Since_3_2_TestBase
+
+@RunWithScalaVersions(Array(TestScalaVersion.Scala_3_Next_RC))
+@RunWithJdkVersions(Array(TestJdkVersion.JDK_17))
+class WorksheetReplIntegration_Scala_3_Next_RC_Test extends WorksheetReplIntegration_Since_3_2_TestBase
+
 @RunWithScalaVersions(Array(
   TestScalaVersion.Scala_3_2
 ))
