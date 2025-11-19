249edb19b865d167d459411c6c7e1e4ff71a8d10
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-11-19 15:50:22 +0100

    [sbt, eel] Use java.nio.file.Path.toString to render a local path as string instead of going through EelPath #SCL-24290
    (cherry picked from commit 93d9ded670bec267e81393d05635a9bd748ccb97)

--- sbt/sbt-impl/src/org/jetbrains/sbt/eelPathExtensions.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/eelPathExtensions.scala
@@ -1,17 +1,20 @@
+//noinspection ApiStatus,UnstableApiUsage
 package org.jetbrains.sbt
 
 import com.intellij.platform.eel.EelDescriptor
-import com.intellij.platform.eel.provider.{EelNioBridgeServiceKt, EelProviderUtil}
+import com.intellij.platform.eel.provider.{EelNioBridgeServiceKt, EelProviderUtil, LocalEelDescriptor}
 
 import java.nio.file.Path
 
 extension (path: Path)
-  //noinspection ApiStatus
-  //noinspection UnstableApiUsage,ApiStatus
   def eelDescriptor: EelDescriptor = EelProviderUtil.getEelDescriptor(path)
 
   /**
    * A machine-specific local path translated via the eel API.
    */
-  //noinspection ApiStatus
-  def asLocalPath: String = EelNioBridgeServiceKt.asEelPath(path).toString
+  def asLocalPath: String =
+    val eelPath = EelNioBridgeServiceKt.asEelPath(path)
+    if eelPath.getDescriptor == LocalEelDescriptor.INSTANCE then
+      path.toString
+    else
+      eelPath.toString
