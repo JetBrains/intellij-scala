af4b7c1a6749ab031d83ea23c71b4d0a9d2165b8
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-11-26 11:22:40 +0100

    [tests] Suppress compiler warnings which JDK 21 prints when producing JDK 8 bytecode

--- scala/integration/maven/test/org/jetbrains/plugins/scala/compiler/buildtools/MavenProjectWithPureJavaModuleTest.scala
+++ scala/integration/maven/test/org/jetbrains/plugins/scala/compiler/buildtools/MavenProjectWithPureJavaModuleTest.scala
@@ -174,11 +174,22 @@ class MavenProjectWithPureJavaModuleTest(jdkVersion: TestJdkVersion) extends Mav
     modules.foreach(ModuleRootModificationUtil.setModuleSdk(_, sdk))
 
     withCompiler { compiler =>
+      val name = incrementality match {
+        case IncrementalityType.SBT => "scala"
+        case IncrementalityType.IDEA => "java"
+      }
+
+      val jdk21warnings = Set(
+        s"$name: source value 8 is obsolete and will be removed in a future release",
+        s"$name: target value 8 is obsolete and will be removed in a future release",
+        s"$name: To suppress warnings about obsolete options, use -Xlint:-options"
+      )
+
       val messages = compiler.make()
       val errorsAndWarnings = messages.asScala.filter { message =>
         val category = message.getCategory
         category == CompilerMessageCategory.ERROR || category == CompilerMessageCategory.WARNING
-      }
+      }.filterNot(msg => jdk21warnings.exists(prefix => msg.getMessage.startsWith(prefix)))
 
       assertTrue(
         s"Expected no compilation errors or warnings, got: ${errorsAndWarnings.mkString(System.lineSeparator())}",
