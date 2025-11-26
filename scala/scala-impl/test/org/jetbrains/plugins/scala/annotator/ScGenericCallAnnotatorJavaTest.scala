3b5777c903a76487aadfaa88175f78a726176463
Jonas Stahl <Jonas.Stahl@jetbrains.com>
2025-11-26 10:19:08 +0100

    [type-inference] Add tests for typed constructor #SCL-24380 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/ScGenericCallAnnotatorJavaTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/annotator/ScGenericCallAnnotatorJavaTest.scala
@@ -53,4 +53,41 @@ class ScGenericCallAnnotatorJavaTest extends JavaHighlightingTestBase {
     assertErrors(scala,
       Message.Error("Int", "Type Int does not conform to upper bound String of type parameter A"))
   }
+
+  val javaTypedConstr =
+    """public class MyJavaClass<A, B, C> {
+      | public <T> MyJavaClass(T i) {}
+      |}
+      |""".stripMargin
+  def testJavaTypedConstructorSuc(): Unit = {
+    val scala =
+      """val _ = MyJavaClass("a")
+        |val _ = MyJavaClass[String, Int, String]("a")
+        |""".stripMargin
+
+    addDummyJavaFile(javaTypedConstr)
+    assertNothing(errorsFromScala3Code(scala))
+  }
+
+  def testJavaTypedConstructorFail1(): Unit = {
+    val scala =
+      """val _ = MyJavaClass[String]("a")
+        |""".stripMargin
+
+    addDummyJavaFile(javaTypedConstr)
+    assertErrors(scala,
+      Message.Error("g]", "Unspecified type parameters: B, C")
+    )
+  }
+
+  def testJavaTypedConstructorFail2(): Unit = {
+    val scala =
+      """val _ = MyJavaClass[String, Int, String, String]("a")
+        |""".stripMargin
+
+    addDummyJavaFile(javaTypedConstr)
+    assertErrors(scala,
+      Message.Error(", S", "Too many type arguments for method MyJavaClass, expected: 3, found: 4")
+    )
+  }
 }
