2f4f7cb8a71dfb03f4efd0332afcebc05bcb73bd
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-12-08 17:16:03 +0100

    [tests] Run tests against Scala 2.12.21
    (cherry picked from commit a614be2d6a2c81740b511b0ce0aee908e4e8d94f)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
@@ -102,7 +102,7 @@ object LatestScalaVersions {
   val Scala_2_9  = new ScalaVersion(ScalaLanguageLevel.Scala_2_9, "3")
   val Scala_2_10 = new ScalaVersion(ScalaLanguageLevel.Scala_2_10, "7")
   val Scala_2_11 = new ScalaVersion(ScalaLanguageLevel.Scala_2_11, "12")
-  val Scala_2_12 = new ScalaVersion(ScalaLanguageLevel.Scala_2_12, "20")
+  val Scala_2_12 = new ScalaVersion(ScalaLanguageLevel.Scala_2_12, "21")
   val Scala_2_13 = new ScalaVersion(ScalaLanguageLevel.Scala_2_13, "18")
   val Scala_3_0  = new ScalaVersion(ScalaLanguageLevel.Scala_3_0, "2")
   val Scala_3_1  = new ScalaVersion(ScalaLanguageLevel.Scala_3_1, "3")
