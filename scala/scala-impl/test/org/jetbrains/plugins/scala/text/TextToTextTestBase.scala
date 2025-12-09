5c88a102fa80b08bc34a21943ab95d68b8c47a61
Pavel Fatin <pavel.fatin@jetbrains.com>
2025-12-09 17:02:09 +0100

    Compiler-to-decompiler tests: normalize aliases, #SCL-24768 fixed
    #SCL-23133

--- scala/scala-impl/test/org/jetbrains/plugins/scala/text/TextToTextTestBase.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/text/TextToTextTestBase.scala
@@ -37,7 +37,9 @@ abstract class TextToTextTestBase(dependencies: Seq[DependencyDescription],
                                   includeScalaReflect: Boolean = false,
                                   includeScalaCompiler: Boolean = false,
                                   astLoadingFilter: Boolean = true,
-                                  transformed: (Content, String) => String = (_, s) => s)(implicit scalaVersion: ScalaVersion) extends ScalaFixtureTestCase {
+                                  transformed: (Content, String) => String = (_, s) => s,
+                                  aliasJava: Boolean = true,
+                                  aliasScala: Boolean = true)(implicit scalaVersion: ScalaVersion) extends ScalaFixtureTestCase {
 
   override protected val includeCompilerAsLibrary = includeScalaCompiler
 
@@ -134,11 +136,11 @@ abstract class TextToTextTestBase(dependencies: Seq[DependencyDescription],
           Assert.assertTrue(s"Must have a source: ${cls.qualifiedName}", sourceCls != cls)
           Assert.assertFalse(s"Must be in a source file: ${cls.qualifiedName}", sourceCls.isInCompiledFile)
 
-          val decompiledVsSourceOutline = transformed(Content.DecompiledVsSourceOutline, decompiled)
+          val decompiledVsSourceOutline = aliased(transformed(Content.DecompiledVsSourceOutline, decompiled))
 
           val sourceOutline = {
             val s = textOfCompilationUnit(sourceCls, withPrivate = false, normalize = true)
-            transformed(Content.SourceOutline, s)
+            aliased(transformed(Content.SourceOutline, s))
           }
 
           // TODO Remove the exception when the ^ syntax in Scala 3.8 is parsed correctly
@@ -189,6 +191,11 @@ abstract class TextToTextTestBase(dependencies: Seq[DependencyDescription],
 
     sb.toString
   }
+
+  private def aliased(text: String): String = {
+    val s = if (aliasJava) aliasedJava(text) else text
+    if (aliasScala) aliasedScala(s) else s
+  }
 }
 
 private object TextToTextTestBase {
@@ -206,4 +213,57 @@ private object TextToTextTestBase {
     case object DecompiledVsSourceOutline extends Content
     case object SourceOutline extends Content
   }
+
+  private def aliasedJava(text: String): String = text
+      .replace("_root_.java.lang.String", "_root_.scala.Predef.String")
+      .replace("_root_.java.lang.Class", "_root_.scala.Predef.Class")
+      .replace("_root_.java.lang.Cloneable", "_root_.scala.Cloneable")
+      .replace("_root_.java.lang.Serializable", "_root_.scala.Serializable")
+      .replace("_root_.java.lang.Throwable", "_root_.scala.Throwable")
+      .replace("_root_.java.lang.Exception", "_root_.scala.Exception")
+      .replace("_root_.java.lang.Error", "_root_.scala.Error")
+      .replace("_root_.java.lang.RuntimeException", "_root_.scala.RuntimeException")
+      .replace("_root_.java.lang.NullPointerException", "_root_.scala.NullPointerException")
+      .replace("_root_.java.lang.ClassCastException", "_root_.scala.ClassCastException")
+      .replace("_root_.java.lang.IndexOutOfBoundsException", "_root_.scala.IndexOutOfBoundsException")
+      .replace("_root_.java.lang.ArrayIndexOutOfBoundsException", "_root_.scala.ArrayIndexOutOfBoundsException")
+      .replace("_root_.java.lang.StringIndexOutOfBoundsException", "_root_.scala.StringIndexOutOfBoundsException")
+      .replace("_root_.java.lang.UnsupportedOperationException", "_root_.scala.UnsupportedOperationException")
+      .replace("_root_.java.lang.IllegalArgumentException", "_root_.scala.IllegalArgumentException")
+      .replace("_root_.java.lang.NoSuchElementException", "_root_.scala.NoSuchElementException")
+      .replace("_root_.java.lang.NumberFormatException", "_root_.scala.NumberFormatException")
+      .replace("_root_.java.lang.AbstractMethodError", "_root_.scala.AbstractMethodError")
+      .replace("_root_.java.lang.InterruptedException", "_root_.scala.InterruptedException")
+
+  private def aliasedScala(text: String): String = text
+      .replace("_root_.scala.collection.immutable.Map", "_root_.scala.Predef.Map")
+      .replace("_root_.scala.collection.immutable.Set", "_root_.scala.Predef.Set")
+      .replace("_root_.scala.collection.Iterable", "_root_.scala.Iterable")
+      .replace("_root_.scala.collection.immutable.Seq", "_root_.scala.Seq")
+      .replace("_root_.scala.collection.immutable.IndexedSeq", "_root_.scala.IndexedSeq")
+      .replace("_root_.scala.collection.immutable.List", "_root_.scala.List")
+      .replace("_root_.scala.collection.immutable.Nil", "_root_.scala.Nil")
+      .replace("_root_.scala.collection.immutable.::", "_root_.scala.::")
+      .replace("_root_.scala.collection.immutable.:+", "_root_.scala.:+")
+      .replace("_root_.scala.collection.immutable.+:", "_root_.scala.+:")
+      .replace("_root_.scala.collection.Iterator", "_root_.scala.Iterator")
+      .replace("_root_.scala.collection.BufferedIterator", "_root_.scala.BufferedIterator")
+      .replace("_root_.scala.collection.immutable.Stream", "_root_.scala.Stream")
+      .replace("_root_.scala.collection.immutable.LazyList", "_root_.scala.LazyList")
+      .replace("_root_.scala.collection.mutable.StringBuilder", "_root_.scala.StringBuilder")
+      .replace("_root_.scala.collection.immutable.Vector", "_root_.scala.Vector")
+      .replace("_root_.scala.collection.immutable.Range", "_root_.scala.Range")
+      .replace("_root_.scala.math.BigDecimal", "_root_.scala.BigDecimal")
+      .replace("_root_.scala.math.BigInt", "_root_.scala.BigInt")
+      .replace("_root_.scala.math.Equiv", "_root_.scala.Equiv")
+      .replace("_root_.scala.math.Fractional", "_root_.scala.Fractional")
+      .replace("_root_.scala.math.Integral", "_root_.scala.Integral")
+      .replace("_root_.scala.math.Numeric", "_root_.scala.Numeric")
+      .replace("_root_.scala.math.Ordered", "_root_.scala.Ordered")
+      .replace("_root_.scala.math.Ordering", "_root_.scala.Ordering")
+      .replace("_root_.scala.math.PartialOrdering", "_root_.scala.PartialOrdering")
+      .replace("_root_.scala.math.PartiallyOrdered", "_root_.scala.PartiallyOrdered")
+      .replace("_root_.scala.util.Either", "_root_.scala.Either")
+      .replace("_root_.scala.util.Left", "_root_.scala.Left")
+      .replace("_root_.scala.util.Right", "_root_.scala.Right")
 }
