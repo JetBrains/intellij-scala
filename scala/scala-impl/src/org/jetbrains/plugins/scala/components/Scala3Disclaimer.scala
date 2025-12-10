6c28977b2416b83bda6193f295d661f3508e96aa
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-12-10 16:47:27 +0100

    [scala 3.8, notifications] Detect Scala 3.8 in the current project and show a notification suggesting the user update to IntelliJ IDEA 2025.3 #SCL-24739 fixed
    #noport

--- scala/scala-impl/src/org/jetbrains/plugins/scala/components/Scala3Disclaimer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/components/Scala3Disclaimer.scala
@@ -39,9 +39,12 @@ object Scala3Disclaimer {
       return // otherwise, it can lead to project leaks in tests
 
     if (!isShownIn(project) && project.hasScala3 && !ScalaPluginUpdater.pluginIsNightly) {
-      showDisclaimerIn(ScalaBundle.message("scala.3.support.is.experimental"),
-        configureUpdatesActionIn(project))
-      setShownIn(project)
+      if (!Scala38UpdateIdeDisclaimer.canBeShownIn(project)) {
+        // The Scala 3.8 disclaimer has priority. If that disclaimer can be shown, we don't want to show this one.
+        showDisclaimerIn(ScalaBundle.message("scala.3.support.is.experimental"),
+          configureUpdatesActionIn(project))
+        setShownIn(project)
+      }
     }
   }
 
