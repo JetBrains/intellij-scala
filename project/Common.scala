8707f07b89d5fe09a356768c66e592867e08b76f
builduser-scala <builduser-scala@jetbrains.com>
2025-12-11 03:06:51 +0000

    Update intellijVersion to 261.14002

--- project/Common.scala
+++ project/Common.scala
@@ -211,7 +211,7 @@ object Common {
       .settings(
         // NOTE: check community/.idea/libraries/kotlin_stdlib.xml in intellij monorepo when updating intellijVersion
         // NOTE: keep versions in sync with ultimate/.idea/kotlinc.xml and community/.idea/kotlinc.xml
-        kotlinVersion := "2.2.21",
+        kotlinVersion := "2.3.0-RC3",
         kotlincJvmTarget := "21",
         kotlinRuntimeProvided := true,
         resolvers += DependencyResolvers.IntelliJDependencies,
