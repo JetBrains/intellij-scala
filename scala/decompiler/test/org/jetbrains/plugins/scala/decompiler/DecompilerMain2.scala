5e29f90eccbb7d68fb58ae4c6085048c765d16b4
Pavel Fatin <pavel.fatin@jetbrains.com>
2025-12-11 11:46:49 +0100

    [decompilers] Test classes renamed, #SCL-23133
    (cherry picked from commit 15cce5219d2068a48479f4ef0a68de1d29d2f2ff)

--- scala/decompiler/test/org/jetbrains/plugins/scala/decompiler/DecompilerMain2.scala
+++ scala/decompiler/test/org/jetbrains/plugins/scala/decompiler/DecompilerMain2.scala
@@ -8,13 +8,13 @@ import scala.util.chaining.scalaUtilChainingOps
 /**
  * A helper to quickly assess the effect of non-trivial ScalaSigPrinter modifications.
  *
- * Although the corresponding code is tested both by the DecompilerTest (unit) and TextToTextTestBase (integration) tests,
+ * Although the corresponding code is tested both by the DecompilerTest2 (unit) and TextToTextTestBase (integration) tests,
  * the former rely on predefined test cases, while the latter only ensure correctness, without a diff.
  *
  * You may only want to use this if you intend to modify the decompiler implementation extensively.
  * First, you should run the TextToTextTestBase tests to download the Ivy artifacts and also to adjust the OutputDir.
  */
-object Main {
+object DecompilerMain2 {
   sealed abstract class Mode
   object Mode {
     case object Parse extends Mode
