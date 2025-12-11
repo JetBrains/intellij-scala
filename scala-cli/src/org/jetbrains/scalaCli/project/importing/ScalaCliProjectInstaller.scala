2828a6081de47dce7778df270f068215395cc366
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-12-11 16:42:15 +0100

    [scala-cli] refactor Scala CLI detection logic to distinguish between bundled and standalone setups #SCL-24417 fixed
    - since Scala 3.5.0, the Scala CLI has been bundled inside Scala distribution
    
    (cherry picked from commit 838fe6367209705d9e1b37e104246418a108ef98)

--- scala-cli/src/org/jetbrains/scalaCli/project/importing/ScalaCliProjectInstaller.scala
+++ scala-cli/src/org/jetbrains/scalaCli/project/importing/ScalaCliProjectInstaller.scala
@@ -19,14 +19,13 @@ class ScalaCliProjectInstaller extends BspProjectInstallProvider {
 
   override def serverName: String = "Scala CLI"
 
-  override def installCommand(workspace: Path): Try[Seq[String]] = {
-    val isScalaCliInstalled = ScalaCliUtils.isScalaCliInstalled(workspace)
-    if (isScalaCliInstalled) {
-      Success(Seq(getScalaCliCommand, "setup-ide", "."))
-    } else {
-      Failure(new IllegalStateException("Unable to install BSP, because Scala CLI is not installed"))
+  override def installCommand(workspace: Path): Try[Seq[String]] =
+    ScalaCliUtils.isScalaCliBundled(workspace) match {
+      case Some(isBundled) =>
+        Success(Seq(getScalaCliCommand(isBundled), "setup-ide", "."))
+      case None =>
+        Failure(new IllegalStateException("Unable to install BSP, because Scala CLI is not installed"))
     }
-  }
 
   override def getConfigSetup: bspConfigSteps.ConfigSetup = ScalaCliSetup
 
