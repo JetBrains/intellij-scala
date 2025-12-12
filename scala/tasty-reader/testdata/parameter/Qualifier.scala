3d8761025babfe6015459c68ad775a52a70705c9
Pavel Fatin <pavel.fatin@jetbrains.com>
2025-12-12 19:14:08 +0100

    Text-to-text tests: protected[this], #SCL-24785 fixed
    #SCL-23133

--- scala/tasty-reader/testdata/parameter/Qualifier.scala
+++ scala/tasty-reader/testdata/parameter/Qualifier.scala
@@ -5,9 +5,13 @@ trait Qualifier {
 
   class ClassProtected(protected[parameter] val x: Int)
 
+  class ClassProtectedThis(protected[this] val x: Int)
+
   class TraitPrivate(private[parameter] val x: Int)
 
   class TraitProtected(protected[parameter] val x: Int)
+
+  class TraitProtectedThis(protected[this] val x: Int)
 
   enum EnumPrivate(private[parameter] val x: Int) {
     case Case/**/ extends EnumPrivate(1)/**/
@@ -17,12 +21,20 @@ trait Qualifier {
     case Case/**/ extends EnumProtected(1)/**/
   }
 
+  enum EnumProtectedThis(protected[this] val x: Int) {
+    case Case/**/ extends EnumProtectedThis(1)/**/
+  }
+
   enum EnumCaseClassPrivate {
     case Class(private[parameter] val x: Int)
   }
 
   enum EnumCaseClassProtected {
     case Class(protected[parameter] val x: Int)
+  }
+
+  enum EnumCaseClassProtectedThis {
+    case Class(protected[this] val x: Int)
   }
 
   object Object {
