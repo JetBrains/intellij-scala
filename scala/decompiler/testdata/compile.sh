41bf0d3e78b8b0e242796216f2c405673cd8a18e
Pavel Fatin <pavel.fatin@jetbrains.com>
2025-12-17 13:27:56 +0100

    Scala 2 decompiler: [?] wildcards, #SCL-24806 fixed
    #SCL-23133

--- scala/decompiler/testdata/compile.sh
+++ scala/decompiler/testdata/compile.sh
@@ -2,11 +2,12 @@
 
 find -regex '.*\.\(sig\|actual\)' -delete
 
-scalaFiles=$(find . -name '*.scala')
+scalaFiles=$(find . -name '*.scala' -not -name '*Wildcard_3*')
 
 export PATH=/opt/scala-2.13.8/bin/:$PATH
 
 echo Compiling .scala files...
 scalac -deprecation -Youtline -Ystop-after:pickler -Ypickle-write . $scalaFiles
+scalac -deprecation -Youtline -Ystop-after:pickler -Ypickle-write . -Xsource:3 types/Wildcard_3.scala
 
 echo Done.
--- scala/scala-impl/test/org/jetbrains/plugins/scala/text/scala2/MillTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/text/scala2/MillTest.scala
@@ -11,9 +11,6 @@ class MillTest extends TextToTextTestBase(
   minClassCount = 140,
   classExceptions = Set(
     "mill.api.AggWrapper", // AggWrapper.this.
-    "mill.api.Ctx", // IndexedSeq[`?$2`] forSome {type `?$2`}
-    "mill.api.JsonFormatters", // Enum[`?$1`] forSome {type `?$1`}
-    "mill.eval.CodeSigUtils", // Class[`?$4`] forSome {type `?$4`}
     "mill.resolve.ExpandBraces", // private trait ExpandBraces.Fragment
   ),
   includeScalaReflect = true
