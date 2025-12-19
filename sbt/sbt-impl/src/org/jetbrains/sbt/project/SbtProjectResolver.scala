8e6665cf34ab537be7cc3e8ed158b03ab1e8eccd
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-12-19 12:43:37 +0100

    [sbt] dynamically update sbt version when importing from shell #SCL-24385 fixed
    - when import in the sbt shell is enabled, the actual moment the import command is executed might be deferred compared to when it was triggered. Because of this, the sbt version in the `ImportContext` might be outdated, so it's necessary to re-detect the sbt version for command generation and later data processing.

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtProjectResolver.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/SbtProjectResolver.scala
@@ -168,7 +168,7 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
     settings: SbtExecutionSettings,
     @Nullable project: Project,
     indicator: ProgressIndicator
-  )(implicit reporter: BuildReporter): Try[(Elem, BuildMessages)] = {
+  )(implicit reporter: BuildReporter, context: ImportContext): Try[(Elem, BuildMessages)] = {
     SbtProjectResolver.processOutputOfLatestStructureDump = ""
 
     val useShellImport = settings.useShellForImport && project != null
@@ -184,7 +184,6 @@ class SbtProjectResolver extends ExternalSystemProjectResolver[SbtExecutionSetti
         if (useShellImport) {
           val messagesF = dumper.dumpFromShell(
             project,
-            sbtVersion,
             structureFilePath,
             options,
             reporter,
@@ -1491,7 +1490,16 @@ object SbtProjectResolver {
     executionSettings: SbtExecutionSettings,
     eelDescriptor: EelDescriptor
   ) {
-    def sbtVersion: SbtVersion = executionSettings.sbtVersion
+    /**
+     * The sbt version used for the project import.
+     *
+     * Initially sourced from `executionSettings.sbtVersion`. If importing via the sbt shell is enabled,
+     * this value is updated to reflect the actual version used by the shell session,
+     * which may differ from the initial settings if the import was deferred.
+     *
+     * @see [[org.jetbrains.sbt.project.structure.SbtStructureDump#dumpFromShell]]
+     */
+    var sbtVersion: SbtVersion = executionSettings.sbtVersion
     def useSeparateProdTestSources: Boolean = executionSettings.separateProdTestSources
     def useSeparateCompilerOutputPaths: Boolean = executionSettings.useSeparateCompilerOutputPaths
   }
