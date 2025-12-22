d74c230cdd8171b5733e748570a47059035a2c5a
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-12-22 17:35:48 +0100

    [scaladoc] fixed enter handler for listis with * + ) #SCL-18476
    (cherry picked from commit 62d9156e625e9c93aaf80eeda4a49c4b95ffd9b5)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/editor/enterHandler/ScalaDocMarkdownEnterHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/editor/enterHandler/ScalaDocMarkdownEnterHandler.scala
@@ -68,14 +68,19 @@ class ScalaDocMarkdownEnterHandler extends EnterHandlerDelegateAdapter {
       val toInsert =
         if (afterQuotePrefix.startsWith("-")) {
           Some(if (caretAtEnd) "- " else "  ")
+        } else if (afterQuotePrefix.startsWith("+")) {
+          Some(if (caretAtEnd) "+ " else "  ")
+        } else if (afterQuotePrefix.startsWith("*")) {
+          Some(if (caretAtEnd) "* " else "  ")
         } else {
           for {
             m <- numberedListRegex.findPrefixMatchOf(afterQuotePrefix)
             num <- m.group(1).toIntOption
             if !caretAtEnd || num < Int.MaxValue
           } yield {
-            if (caretAtEnd) s"${num + 1}. "
-            else s"$num. ".map(_ => ' ')
+            val listSep = m.group(2)
+            if (caretAtEnd) s"${num + 1}$listSep "
+            else s"$num$listSep ".map(_ => ' ')
           }
         }
 
@@ -97,5 +102,5 @@ class ScalaDocMarkdownEnterHandler extends EnterHandlerDelegateAdapter {
     Result.Continue
   }
 
-  private val numberedListRegex = """(\d+)\.""".r
+  private val numberedListRegex = """(\d+)([.)])""".r
 }
