21f8343e1351107d372830d052a6e90bf4f605dc
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-01-12 11:52:23 +0100

    [compiler, tests] Expand the nested source directories deduplication test to include a pure Java sbt subproject #SCL-8988

--- scala/compiler-integration/testData/nestedSourceDirectoriesDeduplication/src/main/scala/org/example/Main.scala
+++ scala/compiler-integration/testData/nestedSourceDirectoriesDeduplication/src/main/scala/org/example/Main.scala
@@ -4,3 +4,8 @@ object Main:
   def main(args: Array[String]): Unit =
     val person = Person("Name", 25)
     println(person)
+
+    val pojoPerson = PojoPerson("OtherName", 26)
+    println(pojoPerson.getName())
+    println(pojoPerson.getAge())
+  end main
