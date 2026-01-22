0527d82c3349aae5c5e156e6a162c854bf6c60c1
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2026-01-22 09:52:48 +0100

    [dotty-update] fixed update script to use bootstrapped compiler #SCL-24854
    (cherry picked from commit 36673efd4d266ad16c580fde0aaaf06d3845a157)

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolveSemanticDb/configurations/ReferenceComparisonTestConfig_Scala3_Newest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolveSemanticDb/configurations/ReferenceComparisonTestConfig_Scala3_Newest.scala
@@ -5,5 +5,5 @@ import org.jetbrains.plugins.scala.LatestScalaVersions
 object ReferenceComparisonTestConfig_Scala3_Newest extends ReferenceComparisonTestConfig(
   testClassName = "ReferenceComparisonTest_Scala3_Newest",
   testDataPathFolder = "newest",
-  scalaTargetVersion = LatestScalaVersions.allScalaNext.last,
+  scalaTargetVersion = LatestScalaVersions.Scala_3_8,
 )
