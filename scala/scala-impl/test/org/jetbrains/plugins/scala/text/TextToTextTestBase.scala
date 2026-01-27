f7fd37de1264c760fa1b564d18fefcfe89a7b0ba
Pavel Fatin <pavel.fatin@jetbrains.com>
2026-01-27 11:03:07 +0100

    [text-to-text] Scala Library 3.8 class count test, #SCL-24642

--- scala/scala-impl/test/org/jetbrains/plugins/scala/text/TextToTextTestBase.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/text/TextToTextTestBase.scala
@@ -114,8 +114,7 @@ abstract class TextToTextTestBase(dependencies: Seq[DependencyDescription],
     val total = classes.length
 
     Assert.assertTrue(s"Too few classes: $total < $minClassCount", total >= minClassCount)
-    // TODO Enable after Scala 3.8 RC3 (duplicate JARs)
-    //Assert.assertTrue(s"Too many classes: $total > 1.1 * $minClassCount", total < 1.1D * minClassCount)
+    Assert.assertTrue(s"Too many classes: $total > 1.1 * $minClassCount", total < 1.1D * minClassCount)
 
     Assert.assertEquals("Class not found", Set.empty, classExceptions -- classes.map(_.qualifiedName).toSet)
 
