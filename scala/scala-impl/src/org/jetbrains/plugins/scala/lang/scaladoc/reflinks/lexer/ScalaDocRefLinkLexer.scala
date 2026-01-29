35d8113aee84b3f538dbda4d32ab3ac3a5a7d64b
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2026-01-29 16:34:56 +0100

    [scaldoc] implemented lexer and language for reflinks  #SCL-13263

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/scaladoc/reflinks/lexer/ScalaDocRefLinkLexer.scala
@@ -0,0 +1,8 @@
+package org.jetbrains.plugins.scala.lang.scaladoc.reflinks.lexer
+
+import com.intellij.lexer.FlexAdapter
+
+/**
+ * blub [[ScalaDocRefLinkLexer]]
+ */
+class ScalaDocRefLinkLexer extends FlexAdapter(new _ScalaDocRefLinkLexer)
