a717b708fd0b6972a07d164ea67fc4719708ec4b
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2026-01-31 21:12:31 +0100

    [scala cli] Use `scala -version` instead of `scala version --cli-version` for checking if installed Scala has bundled Scala CLI #SCL-24930
    - this approach seems more robust across different Scala versions (should not start REPL accidentally)
    - enhance process execution by adding a timeout to prevent it from running indefinitely in case of any issues
    - create tests for Scala CLI NPWs to ensure they work not only with pre-installed Scala CLI but also with different native Scala versions, both with and without bundled Scala CLI.

--- scala-cli/src/org/jetbrains/scalaCli/ScalaCliUtils.scala
+++ scala-cli/src/org/jetbrains/scalaCli/ScalaCliUtils.scala
@@ -1,10 +1,14 @@
 package org.jetbrains.scalaCli
 
+import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import org.jetbrains.bsp.BspUtil
 
 import java.nio.file.Path
+import java.util.concurrent.{TimeUnit, TimeoutException}
+import scala.io.Source
+import scala.util.Try
 
 private object ScalaCliUtils {
   private val log = Logger.getInstance(getClass)
@@ -43,14 +47,15 @@ private object ScalaCliUtils {
 
   /**
    * Returns the command used to invoke Scala CLI commands. It can be:
-   *  - Bundled with Scala ≥ 3.5.0: `scala`
+   *  - Bundled with Scala ≥ 3.5.0: `scala` or `./scala` in the unit test mode
    *  - Standalone installation: `scala-cli` or `./scala-cli` in the unit test mode
    *
    * @see [[getScalaCliStandaloneCommand]]
+   * @see [[getScalaStandaloneCommand]]
    */
   def getScalaCliCommand(scalaCliInstallKind: ScalaCliInstallKind): String =
     scalaCliInstallKind match {
-      case ScalaCliInstallKind.Bundled => "scala"
+      case ScalaCliInstallKind.Bundled => getScalaStandaloneCommand
       case ScalaCliInstallKind.Standalone => getScalaCliStandaloneCommand
     }
 
@@ -58,24 +63,101 @@ private object ScalaCliUtils {
    * The unit test mode requires `./scala-cli`,
    * because instead of a global installation, there is a scala-cli script in the test project root.
    *
-   * @see [[org.jetbrains.scalaCli.project.NewScalaCliProjectWizardTest.installScalaCli]]
+   * @see [[org.jetbrains.scalaCli.project.NewScalaCliProjectWizardTestBase.installScalaCli]]
    */
   private def getScalaCliStandaloneCommand: String =
     if (ApplicationManager.getApplication.isUnitTestMode) "./scala-cli"
     else "scala-cli"
 
   /**
-   * Checks if Scala CLI is bundled with the scala installation by attempting to run `scala version --cli-version`.
+   * The unit test mode requires `./scala`,
+   * because instead of a global installation, there is a scala script in the test project root.
+   * *
+   * @see [[org.jetbrains.scalaCli.project.NewScalaCliProjectWizardTestBase.installScala]]
+   * @see [[org.jetbrains.scalaCli.project.NewScalaCliProjectWizardTestBase.createScalaWrapperScript]]
+   */
+  private def getScalaStandaloneCommand: String =
+    if (ApplicationManager.getApplication.isUnitTestMode) "./scala"
+    else "scala"
+
+  /**
+   * Checks if Scala CLI is bundled with the scala installation by attempting to run `scala -version`.
    * Since Scala 3.5.0, Scala CLI has been included in the Scala distribution.
    */
   private def isBundledScalaCliInstalled(workspace: Path): Boolean = {
-    val work = BspUtil.runCommand(workspace, "scala", "version", "--cli-version", "< /dev/null")
-    work.fold(
-      exc => {
-        log.warn(s"The scala version with Scala CLI is not installed in $workspace - ${exc.getMessage}")
-        false
+    val result = tryCheckScalaVersionWithBundledScalaCli(workspace)
+    result.fold(
+      {
+        // It's required for tests, see org.jetbrains.scalaCli.project.NewScalaCliProjectWizardWith_ScalaWithoutScalaCLI
+        case e: ParseOutputException if ApplicationManager.getApplication.isUnitTestMode =>
+          throw e
+        case exc =>
+          log.warn(s"The scala version with Scala CLI is not installed in $workspace - ${exc.getMessage}")
+          false
       },
-      _ => true
+      output => output
     )
   }
+
+  private def tryCheckScalaVersionWithBundledScalaCli(directory: Path): Try[Boolean] =
+    for {
+      output <- executeScalaVersionCommand(directory)
+      (major, minor, patch) <- parseScalaVersion(output)
+    } yield {
+      if (major > 3 || (major == 3 && minor >= 5))
+        true
+      else
+        throw ParseOutputException(s"Scala version $major.$minor.$patch is lower than 3.5.0")
+    }
+
+  /**
+   * Executes 'scala -version' command and returns the output.
+   */
+  private def executeScalaVersionCommand(directory: Path): Try[String] =
+    Try {
+      val commandLine = new GeneralCommandLine(getScalaStandaloneCommand, "-version")
+        .withWorkDirectory(directory.toString)
+      val process = commandLine.toProcessBuilder.start()
+      process.getOutputStream.close()
+
+      val completed = process.waitFor(1, TimeUnit.MINUTES)
+
+      if (!completed) {
+        process.destroy()
+        throw new TimeoutException("Command 'scala -version' timed out after 2 minutes")
+      }
+
+      val stdout = Source.fromInputStream(process.getInputStream).mkString
+      val stderr = Source.fromInputStream(process.getErrorStream).mkString
+      stdout + stderr
+    }
+
+  /**
+   * Parses a Scala version from `scala -version` command output.
+   *
+   * Supports two formats:
+   * - Scala 3.5.0+: "Scala version (default): 3.7.4"
+   * - Scala < 3.5.0: "Scala code runner version 2.12.21"
+   *
+   * @return Right((major, minor, patch)) or Left(ParseOutputException)
+   */
+  private def parseScalaVersion(output: String): Try[(Int, Int, Int)] = {
+    val newFormatPattern = """Scala version \(default\):\s*(\d+)\.(\d+)\.(\d+).*""".r
+    val oldFormatPattern = """Scala code runner version\s+(\d+)\.(\d+)\.(\d+).*""".r
+
+    output.linesIterator.collectFirst {
+      case newFormatPattern(major, minor, patch) => (major.toInt, minor.toInt, patch.toInt)
+      case oldFormatPattern(major, minor, patch) => (major.toInt, minor.toInt, patch.toInt)
+    }.toRight(ParseOutputException(s"Unable to parse Scala version from output: $output")).toTry
+  }
+
+  /**
+   * Exception thrown when parsing output from `scala -version` command fails.
+   *
+   * In production mode, this exception is simply ignored - when there is a parsing problem, it is assumed that Scala with the required version is not installed.
+   * In test mode, this exception is rethrown to allow more specific test cases that check for particular exceptions and their messages.
+   *
+   * @see [[org.jetbrains.scalaCli.project.NewScalaCliProjectWizard_ScalaWithoutScalaCLI]]
+   */
+  private case class ParseOutputException(msg: String) extends Exception(msg)
 }
