b525a548935de4bae9c7dd77753bcacbddc537a7
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-02-04 00:43:05 +0100

    [do not merge] Update intellijVersion to 261.20436 to pick up fixes for IDEA-385505

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -20,7 +20,7 @@ object Versions {
    * ATTENTION: check the comment in [[Common.newProjectWithKotlin]] when updating this version.
    *            update `since-build` in plugin.xml if there are binary incompatible changes after update
    */
-  val intellijVersion = "261.20362.12"
+  val intellijVersion = "261.20436"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
