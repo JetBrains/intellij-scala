c070682567630452cf15138a0a5c8a138aef8629
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-02-11 12:17:40 +0100

    Update intellijVersion to 261.20869.19

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -22,7 +22,7 @@ object Versions {
    * ATTENTION: check the comment in [[Common.newProjectWithKotlin]] when updating this version.
    *            update `since-build` in plugin.xml if there are binary incompatible changes after update
    */
-  val intellijVersion = "261.20362.48"
+  val intellijVersion = "261.20869.19"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
