cb11e4a5e37f13a553b7f06d401ff1a2f1bf0c05
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-02-12 16:18:19 +0100

    [debugger] Factor out a common function to build the expression evaluator in a read action #SCL-24414
    - Due to bad code structure, there was a bug in the implementation of ScalaLazyResolveEvaluator#evaluateModifiable where the expression evaluator (which accesses PSI and does resolve) was constructed outside a read action.
    - Unfortunately, this code was never called from the platform, neither in production code nor in tests and the bug was never noticed.
    - After the changes in IDEA-366793, the code became active and correctly started throwing exceptions.
    - This change also makes lazy resolve debugger expression evaluation the default again.

--- scala/debugger/src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaLazyResolveEvaluator.scala
+++ scala/debugger/src/org/jetbrains/plugins/scala/debugger/evaluation/ScalaLazyResolveEvaluator.scala
@@ -4,6 +4,7 @@ import com.intellij.debugger.SourcePosition
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
 import com.intellij.debugger.engine.evaluation.expression.{Evaluator, ExpressionEvaluator, ModifiableValue}
 import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.project.Project
 import com.sun.jdi.Value
 import org.jetbrains.plugins.scala.lang.psi.impl.source.ScalaCodeFragment
 
@@ -13,19 +14,18 @@ private final class ScalaLazyResolveEvaluator(
   codeFragment: ScalaCodeFragment,
   position: SourcePosition
 ) extends Evaluator {
-  override def evaluate(context: EvaluationContextImpl): Value = {
-    val callable: Callable[ExpressionEvaluator] = () => ScalaEvaluatorBuilder.build(codeFragment, position)
-    val project = context.getProject
-    val evaluator = ReadAction.nonBlocking(callable).expireWhen(() => project.isDisposed).executeSynchronously()
-    evaluator.evaluate(context)
-  }
+  override def evaluate(context: EvaluationContextImpl): Value =
+    createEvaluator(context.getProject).evaluate(context)
 
   override def evaluateModifiable(context: EvaluationContextImpl): ModifiableValue = {
-    val expressionEvaluator = ScalaEvaluatorBuilder.build(codeFragment, position)
-    val callable: Callable[Value] = () => expressionEvaluator.evaluate(context)
-    val project = context.getProject
-    val value = ReadAction.nonBlocking(callable).expireWhen(() => project.isDisposed).executeSynchronously()
-    val modifier = expressionEvaluator.getModifier
+    val evaluator = createEvaluator(context.getProject)
+    val value = evaluator.evaluate(context)
+    val modifier = evaluator.getModifier
     new ModifiableValue(value, modifier)
+  }
+
+  private def createEvaluator(project: Project): ExpressionEvaluator = {
+    val callable: Callable[ExpressionEvaluator] = () => ScalaEvaluatorBuilder.build(codeFragment, position)
+    ReadAction.nonBlocking(callable).expireWhen(() => project.isDisposed).executeSynchronously()
   }
 }
