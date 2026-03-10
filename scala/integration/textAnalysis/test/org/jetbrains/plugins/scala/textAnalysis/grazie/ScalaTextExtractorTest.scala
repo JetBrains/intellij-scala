bb4cb98481e811b6197f016c13b3043ce5fa5826
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2026-03-10 14:18:45 +0100

    [internal, tests] extract all performance tests of ScalaTextExtractorTest to ScalaTextExtractorPerformanceTest

--- scala/integration/textAnalysis/test/org/jetbrains/plugins/scala/textAnalysis/grazie/ScalaTextExtractorTest.scala
+++ scala/integration/textAnalysis/test/org/jetbrains/plugins/scala/textAnalysis/grazie/ScalaTextExtractorTest.scala
@@ -1,18 +1,14 @@
 package org.jetbrains.plugins.scala.textAnalysis.grazie
 
-import com.intellij.grazie.text.{TextContent, TextContentBuilder, TextExtractor}
+import com.intellij.grazie.text.{TextContent, TextExtractor}
 import com.intellij.lang.injection.{InjectedLanguageManager, MultiHostInjector, MultiHostRegistrar}
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.javadoc.{PsiDocComment, PsiDocTag}
+import com.intellij.psi.javadoc.PsiDocComment
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{PsiComment, PsiElement, PsiFileFactory, PsiLanguageInjectionHost}
-import com.intellij.testFramework.PerformanceUnitTest
-import com.intellij.testFramework.UsefulTestCase.assertInstanceOf
+import com.intellij.psi.{PsiElement, PsiFileFactory, PsiLanguageInjectionHost}
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.tools.ide.metrics.benchmark.Benchmark
-import com.intellij.util.ThrowableRunnable
 import org.intellij.lang.regexp.RegExpLanguage
 import org.jetbrains.plugins.scala.ScalaFileType
 import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScStringLiteral
@@ -374,64 +370,6 @@ class ScalaTextExtractorTest extends BasePlatformTestCase {
     val file2 = PsiFileFactory.getInstance(getProject).createFileFromText("Dummy1.scala", ScalaFileType.INSTANCE, text, 0, createPhysicalFile)
     assertNull(TextExtractor.findTextAt(file2, offset, TextContent.TextDomain.ALL))
   }
-
-  @PerformanceUnitTest
-  def testBuildingPerformance_removingIndents(): Unit = {
-    val text = "  b\n".repeat(10_000)
-    val expected = "b\n".repeat(10_000).trim
-
-    val file = myFixture.configureByText("dummy.scala", s"/*\n$text*/")
-
-    val comment = assertInstanceOf(file.findElementAt(10), classOf[PsiComment])
-    val builder = TextContentBuilder.FromPsi.removingIndents(" ")
-
-    runPerformanceTest("TextContent building with indent removing") { () =>
-      assertEquals(expected, builder.build(comment, TextContent.TextDomain.COMMENTS).toString)
-    }
-  }
-
-  @PerformanceUnitTest
-  def testBuildingPerformance_removingHtml(): Unit = {
-    val text = "b<unknownTag>x</unknownTag>".repeat(10_000)
-    val expected = List("b".repeat(10_000))
-    val file = myFixture.configureByText("dummy.scala", "/**\n" + text + "*/")
-    val comment = PsiTreeUtil.findElementOfClassAtOffset(file, 10, classOf[PsiDocComment], false)
-    val extractor = new ScalaTextExtractor
-    runPerformanceTest("TextContent building with HTML removal") { () =>
-      assertEquals(expected, extractor.buildTextContents(comment, TextContent.TextDomain.ALL).asScala.toList.map(_.toString))
-    }
-  }
-
-  @PerformanceUnitTest
-  def testBuildingPerformance_longTextFragment(): Unit = {
-    val line = "here's some relative long text that helps make this text fragment a bit longer than it could have been otherwise"
-    val text = ("\n\n\n" + line).repeat(10_000)
-    val expected = List((line + "\n\n\n").repeat(10_000).trim)
-    val file = myFixture.configureByText("dummy.scala", "class C { String s = \"\"\"\n" + text + "\"\"\"; }")
-    val literal = PsiTreeUtil.findElementOfClassAtOffset(file, 100, classOf[ScStringLiteral], false)
-    val extractor = new ScalaTextExtractor
-    runPerformanceTest("TextContent building from a long text fragment") { () =>
-      assertEquals(expected, extractor.buildTextContents(literal, TextContent.TextDomain.ALL).asScala.toList.map(_.toString))
-    }
-  }
-
-  @PerformanceUnitTest
-  def testBuildingPerformance_ComplexScalaDocPsi(): Unit = {
-    val link = "[[foo.bar.goo1.goo2.goo3.goo4.goo5.goo6.goo7]]"
-    val text = "/** @return something if " + link.repeat(10_000) + " is not too expensive */"
-    val file = myFixture.configureByText("dummy.scala", text)
-    val extractor = new ScalaTextExtractor
-    val tag = PsiTreeUtil.findElementOfClassAtOffset(file, text.indexOf("something"), classOf[PsiDocTag], false)
-    runPerformanceTest("TextContent building from complex PSI") { () =>
-      for (_ <- 0 until 10) {
-        val content = extractor.buildTextContents(tag, TextContent.TextDomain.ALL).asScala.toList
-        assertEquals(List("something if  is not too expensive"), content.map(_.toString))
-      }
-    }
-  }
-
-  private def runPerformanceTest(launchName: String)(runnable: ThrowableRunnable[?]): Unit =
-    Benchmark.newBenchmark(launchName, runnable).start()
 }
 
 object ScalaTextExtractorTest {
