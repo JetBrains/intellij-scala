e31ac03338bce769be4d624c9a05b646850c42ee
Pavel Fatin <pavel.fatin@jetbrains.com>
2026-03-13 11:28:21 +0100

    ScalaUnusedDeclarationIncrementalInspection: extensions in XML, #SCL-25162

--- scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/ScalaUnusedDeclarationIncrementalInspection.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/declarationRedundancy/ScalaUnusedDeclarationIncrementalInspection.scala
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.scala.codeInspection.declarationRedundancy
 
 import com.intellij.codeInspection.{IntentionWrapper, ProblemsHolder}
+import com.intellij.lang.xml.XMLLanguage
 import com.intellij.psi.search.{LocalSearchScope, PsiSearchHelper, TextOccurenceProcessor, UsageSearchContext}
 import com.intellij.psi.{PsiAnnotationOwner, PsiElement, PsiElementVisitor, PsiReference}
 import org.jetbrains.plugins.scala.codeInspection.declarationRedundancy.ScalaUnusedDeclarationInspection.hasUnusedAnnotation
@@ -12,19 +13,25 @@ import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil.inNameContext
 import org.jetbrains.plugins.scala.lang.psi.api.base.ScStableCodeReference
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.ScNamedElement
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.imports.{ScImportExpr, ScImportSelector, ScImportStmt}
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
 import org.jetbrains.plugins.scala.util.ScalaUsageNamesUtil
 
 trait ScalaUnusedDeclarationIncrementalInspection { this: ScalaUnusedDeclarationInspection =>
   override def buildIncrementalHighlightingVisitor(holder: ProblemsHolder): PsiElementVisitor = {
     val helper = PsiSearchHelper.getInstance(holder.getProject)
-    val namedProcessor: TextOccurenceProcessor = (e: PsiElement, _) => !e.isInstanceOf[PsiReference]
     val context = (UsageSearchContext.IN_CODE | UsageSearchContext.IN_FOREIGN_LANGUAGES).toShort
     PsiElementVisitorSimple(holder) {
       case e: ScNamedElement if e.nameId != null && e.nameId.isVisible(holder.getProject, holder.getFile) && !ScalaPsiUtil.isImplicit(e) && shouldProcessElement(e) =>
         val scope = helper.getUseScope(e)
         val ids = ScalaUsageNamesUtil.getStringsToSearch(e)
+        val processor: TextOccurenceProcessor = e match {
+          case cls: ScClass if ScalaPsiUtil.hasStablePath(cls) => // Can implement an extension point, include XML
+            (e: PsiElement, _) => !(e.isInstanceOf[PsiReference] || e.getLanguage == XMLLanguage.INSTANCE)
+          case _ =>
+            (e: PsiElement, _) => !e.isInstanceOf[PsiReference]
+        }
         val notFound = ids.stream.allMatch { id =>
-          helper.processElementsWithWord(namedProcessor, scope, id, context, true)
+          helper.processElementsWithWord(processor, scope, id, context, true)
         }
         if (notFound) {
           e match {
