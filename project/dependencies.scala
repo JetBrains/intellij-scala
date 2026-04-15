39fc37864987266546c59789d69ff172d07577d5
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-04-15 13:49:55 +0200

    [repl, tests] Write a JUnit 5 test framework based test for ScalaConsoleRunConfiguration #SCL-25309
    (cherry picked from commit 8724ed73f1cd2fc52a9068d6624c123423220631)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -206,6 +206,7 @@ object Dependencies {
   val intellijTestFrameworkCore: ModuleID = ("com.jetbrains.intellij.platform" % "test-framework-core" % IntellijTestFrameworkVersion).notTransitive()
   val intellijTestFrameworkCommon: ModuleID = ("com.jetbrains.intellij.platform" % "test-framework-common" % IntellijTestFrameworkVersion).notTransitive()
   val intellijTestFramework: ModuleID = ("com.jetbrains.intellij.platform" % "test-framework" % IntellijTestFrameworkVersion).notTransitive()
+  val intellijJUnit5TestFramework: ModuleID = ("com.jetbrains.intellij.platform" % "test-framework-junit5" % IntellijTestFrameworkVersion).notTransitive()
   val intellijJavaTestFrameworkShared: ModuleID = ("com.jetbrains.intellij.java" % "java-test-framework-shared" % IntellijTestFrameworkVersion).notTransitive()
   val intellijJavaTestFrameworkBackend: ModuleID = ("com.jetbrains.intellij.java" % "java-test-framework-backend" % IntellijTestFrameworkVersion).notTransitive()
   val intellijJavaTestFramework: ModuleID = ("com.jetbrains.intellij.java" % "java-test-framework" % IntellijTestFrameworkVersion).notTransitive()
