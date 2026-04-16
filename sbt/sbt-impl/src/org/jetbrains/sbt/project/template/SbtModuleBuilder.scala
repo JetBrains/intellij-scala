670524dbc8641aa03b928c8a6886fcced355826a
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2026-04-16 16:06:33 +0200

    [NPW, sbt2] use a modern new project template for sbt 2 #SCL-24832 fixed
    The template used for sbt 2:
    ```sbt
    scalaVersion := "3.8.3"
    
    lazy val root = rootProject
      .settings(
        name := "sbt2_project_template_with_prefix",
        libraryDependencies ++= Seq(
          //You can add library dependencies here, for example,
          //"org.scalatest" %% "scalatest" % "3.2.19" % Test,
          //"org.scalameta" %% "munit" % "1.2.3" % Test
        )
      )
    ```
    
    - Tests: split NPW tests into:
      - tests that do the full new project wizard creation, including project import and test the project structure
      - tests that only test the generated new project files
    - Test: add ability to skip project import in some tests
    
    
    (cherry picked from commit aedf68e9b5a2273472b35363982d73b8dd42bc45)

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/template/SbtModuleBuilder.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/template/SbtModuleBuilder.scala
@@ -3,27 +3,27 @@ package org.jetbrains.sbt.project.template
 import com.intellij.openapi.module.Module
 import org.jetbrains.annotations.{ApiStatus, NonNls}
 import org.jetbrains.plugins.scala.ScalaVersion
-import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.extensions.*
+import org.jetbrains.plugins.scala.project.Versions
 import org.jetbrains.plugins.scala.project.template.DefaultModuleContentEntryFolders
-import org.jetbrains.plugins.scala.project.{Version, Versions}
-import org.jetbrains.sbt.Sbt
+import org.jetbrains.sbt.{Sbt, SbtVersion}
 
 import java.nio.file.{FileAlreadyExistsException, Files, Path}
-import javax.swing._
+import javax.swing.*
 
 /**
  * Do not extend, it will be made final in the future.<br>
  * Consider using [[SbtModuleBuilderBase]] instead
  *
  * @param _selections initial selections value<br>
- *                    The parameter value is copied copied, changes to the original object do not effect the builder
+ *                    The parameter value is copied, changes to the original object do not affect the builder
  */
 @ApiStatus.Internal
 class SbtModuleBuilder(
   _selections: SbtModuleBuilderSelections
 ) extends SbtModuleBuilderBase {
 
-  private val selections = _selections.copy() // Selections is mutable data structure
+  private val selections = _selections.copy() // Selections are mutable data structure
   def this() = this(SbtModuleBuilderSelections.default)
 
   override def getNodeIcon: Icon = Sbt.Icon
@@ -38,24 +38,44 @@ class SbtModuleBuilder(
 
   override protected def createProjectTemplateIn(root: Path): Option[DefaultModuleContentEntryFolders] = {
     val name = getName
-    val sbtVersion = selections.sbtVersion.map(_.minor).getOrElse(Versions.SBT.LatestSbtVersion)
+    val sbtVersion = selections.sbtVersion.getOrElse(SbtVersion(Versions.SBT.LatestSbtVersion))
     val scalaVersion = selections.scalaVersion.getOrElse(ScalaVersion.Latest.Scala_2_13.minor)
     val packagePrefix = selections.packagePrefix
 
-    SbtModuleBuilder.createProjectTemplateIn(root, name, scalaVersion, sbtVersion, packagePrefix)
+    SbtModuleBuilder.createProjectTemplateIn(
+      root = root,
+      name = name,
+      scalaVersion = scalaVersion,
+      sbtVersion = sbtVersion,
+      packagePrefix = packagePrefix,
+      // TODO: add library dependencies example for all sbt versions? What about the versions inside?
+      addLibraryDependenciesExample = sbtVersion.isSbt2
+    )
   }
 }
 
 object SbtModuleBuilder {
+
+  @NonNls val IdeSettingsPluginVersion = "1.1.4"
+
+  private case class ProjectTemplateFileContents(
+    `build.sbt`: String,
+    `build.properties`: String,
+    `plugins.sbt`: Option[String]
+  )
+
   private def createProjectTemplateIn(
     root: Path,
     @NonNls name: String,
     @NonNls scalaVersion: String,
-    @NonNls sbtVersion: String,
-    packagePrefix: Option[String]
+    sbtVersion: SbtVersion,
+    packagePrefix: Option[String],
+    addLibraryDependenciesExample: Boolean
   ): Option[DefaultModuleContentEntryFolders] = {
     val buildFile = root / Sbt.BuildFile
     val projectDir = root / Sbt.ProjectDirectory
+
+    import FileUtils.*
 
     if (createNewFile(buildFile) && mkdir(projectDir)) {
       val mainSourcesPath = "src/main/scala"
@@ -64,47 +84,15 @@ object SbtModuleBuilder {
       mkdirs(root / mainSourcesPath)
       mkdirs(root / testSourcesPath)
 
-      val rootProjectSettings: Seq[String] = Seq(
-        s"""name := "$name""""
-      ) ++ packagePrefix.map { p =>
-        s"""idePackagePrefix := Some("$p")""".stripMargin
-      }
+      val fileContents = buildProjectTemplateFileContents(
+        sbtVersion = sbtVersion,
+        scalaVersion = scalaVersion,
+        name = name,
+        packagePrefix = packagePrefix,
+        addLibraryDependenciesExample = addLibraryDependenciesExample,
+      )
 
-      val version = """0.1.0-SNAPSHOT"""
-
-      // Slash syntax was introduced in sbt 1.1 (https://www.scala-sbt.org/1.x/docs/Migrating-from-sbt-013x.html)
-      val isAtLeastSbt_1_1 = Version(sbtVersion) >= Version("1.1")
-      val buildSbtBaseContent = if (isAtLeastSbt_1_1)
-        s"""ThisBuild / version := "$version"
-           |
-           |ThisBuild / scalaVersion := "$scalaVersion""""
-      else
-        s"""version in ThisBuild := "$version"
-           |
-           |scalaVersion in ThisBuild := "$scalaVersion""""
-
-      val indent = "    "
-      val buildSbtContent =
-        s"""$buildSbtBaseContent
-           |
-           |lazy val root = (project in file("."))
-           |  .settings(
-           |$indent${rootProjectSettings.mkString("", s",\n$indent", "")}
-           |  )
-           |""".stripMargin
-
-      val buildPropertiesContent = s"""sbt.version = $sbtVersion"""
-
-      val pluginsSbtContent = """addSbtPlugin("org.jetbrains.scala" % "sbt-ide-settings" % "1.1.4")"""
-
-      def ensureSingleNewLineAfter(text: String): String = text.stripTrailing() + "\n"
-
-      Files.writeString(buildFile, ensureSingleNewLineAfter(buildSbtContent))
-      Files.writeString(projectDir / Sbt.PropertiesFile, ensureSingleNewLineAfter(buildPropertiesContent))
-
-      if (packagePrefix.isDefined) {
-        Files.writeString(projectDir / Sbt.PluginsFile, ensureSingleNewLineAfter(pluginsSbtContent))
-      }
+      writeProjectTemplateFiles(buildFile, projectDir, fileContents)
 
       Some(DefaultModuleContentEntryFolders(
         Seq(mainSourcesPath),
@@ -117,20 +105,160 @@ object SbtModuleBuilder {
     else None
   }
 
-  private def createNewFile(path: Path): Boolean =
-    pathOp(path)(Files.createFile(_))
-
-  private def mkdir(path: Path): Boolean =
-    pathOp(path)(Files.createDirectory(_))
-
-  private def mkdirs(path: Path): Boolean =
-    pathOp(path)(Files.createDirectories(_))
-
-  private def pathOp(path: Path)(op: Path => Unit): Boolean =
-    try {
-      op(path)
-      true
-    } catch {
-      case _: FileAlreadyExistsException => false
+  private def buildRootProjectSbtSettings(name: String, packagePrefix: Option[String], addLibraryDependenciesExample: Boolean) = {
+    val nameSetting = Seq(
+      s"""name := "$name""""
+    )
+    val packagePrefixSetting = packagePrefix.map { p =>
+      s"""idePackagePrefix := Some("$p")""".stripMargin
     }
+    nameSetting ++
+      packagePrefixSetting ++
+      addLibraryDependenciesExample.option(LibraryDependenciesSetting).toSeq
+  }
+
+  private val LibraryDependenciesSetting: String =
+    """libraryDependencies ++= Seq(
+      |  //You can add library dependencies here, for example,
+      |  //"org.scalatest" %% "scalatest" % "3.2.19" % Test,
+      |  //"org.scalameta" %% "munit" % "1.2.3" % Test
+      |)""".stripMargin
+
+  private def buildSbtContentForSbt2(
+    @NonNls scalaVersion: String,
+    rootProjectSettings: Seq[String]
+  ): String = {
+    val indentedSettings = formatRootProjectSettings(rootProjectSettings)
+
+    s"""scalaVersion := "$scalaVersion"
+       |
+       |lazy val root = rootProject
+       |  .settings(
+       |$indentedSettings
+       |  )
+       |""".stripMargin
+  }
+
+  private def buildSbtContentForSbt1_After_1_1(
+    @NonNls scalaVersion: String,
+    rootProjectSettings: Seq[String]
+  ): String = {
+    //TODO: don't add "version" in all sbt versions, not only in SBT 2
+    val version = """0.1.0-SNAPSHOT"""
+    val rootProjectSettingsText = formatRootProjectSettings(rootProjectSettings)
+
+    s"""ThisBuild / version := "$version"
+       |
+       |ThisBuild / scalaVersion := "$scalaVersion"
+       |
+       |lazy val root = (project in file("."))
+       |  .settings(
+       |$rootProjectSettingsText
+       |  )
+       |""".stripMargin
+  }
+
+  /**
+   * Uses `in` syntax instead of slash `/` syntax.
+   * Slash syntax was introduced only in sbt 1.1
+   *
+   * @see https://www.scala-sbt.org/1.x/docs/Migrating-from-sbt-013x.html
+   */
+  private def buildSbtContentForSbt1_Before_1_1(
+    @NonNls scalaVersion: String,
+    rootProjectSettings: Seq[String]
+  ): String = {
+    val version = """0.1.0-SNAPSHOT"""
+    val rootProjectSettingsText = formatRootProjectSettings(rootProjectSettings)
+
+    s"""version in ThisBuild := "$version"
+       |
+       |scalaVersion in ThisBuild := "$scalaVersion"
+       |
+       |lazy val root = (project in file("."))
+       |  .settings(
+       |$rootProjectSettingsText
+       |  )
+       |""".stripMargin
+  }
+
+  private def formatRootProjectSettings(settings: Seq[String]): String =
+    settings.mkString(",\n").indented(4)
+
+  private def buildProjectTemplateFileContents(
+    sbtVersion: SbtVersion,
+    scalaVersion: String,
+    name: String,
+    packagePrefix: Option[String],
+    addLibraryDependenciesExample: Boolean,
+  ): ProjectTemplateFileContents = {
+    val rootProjectSettings: Seq[String] = buildRootProjectSbtSettings(
+      name,
+      packagePrefix,
+      addLibraryDependenciesExample
+    )
+    val buildSbtFileContent: String =
+      if (sbtVersion.isSbt2)
+        buildSbtContentForSbt2(scalaVersion, rootProjectSettings)
+      else if (sbtVersion >= SbtVersion("1.1"))
+        buildSbtContentForSbt1_After_1_1(scalaVersion, rootProjectSettings)
+      else
+        buildSbtContentForSbt1_Before_1_1(scalaVersion, rootProjectSettings)
+
+    val buildPropertiesFileContent: String =
+      s"""sbt.version = ${sbtVersion.minor}"""
+
+    val pluginsSbtFileContent: Option[String] =
+      packagePrefix.isDefined.option(
+        s"""addSbtPlugin("org.jetbrains.scala" % "sbt-ide-settings" % "$IdeSettingsPluginVersion")"""
+      )
+
+    ProjectTemplateFileContents(
+      `build.sbt` = buildSbtFileContent,
+      `build.properties` = buildPropertiesFileContent,
+      `plugins.sbt` = pluginsSbtFileContent
+    )
+  }
+
+  private def writeProjectTemplateFiles(
+    buildFile: Path,
+    projectDir: Path,
+    fileContents: ProjectTemplateFileContents
+  ): Unit = {
+    Files.writeString(buildFile, fileContents.`build.sbt`.ensureSingleNewLineAfter)
+    Files.writeString(projectDir / Sbt.PropertiesFile, fileContents.`build.properties`.ensureSingleNewLineAfter)
+
+    fileContents.`plugins.sbt`.foreach { pluginsSbt =>
+      Files.writeString(projectDir / Sbt.PluginsFile, pluginsSbt.ensureSingleNewLineAfter)
+    }
+  }
+
+
+  private implicit class StringAnyOps(private val text: String) extends AnyVal {
+     def indented(spaces: Int): String = {
+      val indent = " " * spaces
+      indent + text.replace("\n", "\n" + indent)
+    }
+
+     def ensureSingleNewLineAfter: String = text.stripTrailing() + "\n"
+  }
+
+  private object FileUtils {
+     def createNewFile(path: Path): Boolean =
+      pathOp(path)(Files.createFile(_))
+
+     def mkdir(path: Path): Boolean =
+      pathOp(path)(Files.createDirectory(_))
+
+     def mkdirs(path: Path): Boolean =
+      pathOp(path)(Files.createDirectories(_))
+
+     def pathOp(path: Path)(op: Path => Unit): Boolean =
+      try {
+        op(path)
+        true
+      } catch {
+        case _: FileAlreadyExistsException => false
+      }
+  }
 }
---
+++ sbt/sbt-impl/test/org/jetbrains/sbt/project/NewSbtProjectWizardGeneratedFilesTest.scala
@@ -0,0 +1,200 @@
+package org.jetbrains.sbt.project
+
+import org.jetbrains.sbt.project.template.SbtModuleBuilder.IdeSettingsPluginVersion
+
+/**
+ * The test verifies generated SBT project files from the new-project wizard without running the full project.
+ *
+ * It's effectively a "Unit" test for  [[org.jetbrains.sbt.project.template.SbtModuleBuilder.createProjectTemplateIn]]
+ *
+ * For full-import and IntelliJ project-structure validation, see [[NewSbtProjectWizardFullImportTest]]
+ */
+class NewSbtProjectWizardGeneratedFilesTest extends NewSbtProjectWizardTestBase with ExactMatch {
+  import NewSbtProjectWizardGeneratedFilesTest.TestData.*
+
+  def testCreateSbt_0_13_Project(): Unit = {
+    runFileGenerationOnlyTest(
+      config = sbt013ProjectConfig,
+      expectedFiles = GeneratedProjectFilesExpectation(
+        buildSbt =
+          """version in ThisBuild := "0.1.0-SNAPSHOT"
+            |
+            |scalaVersion in ThisBuild := "2.13.14"
+            |
+            |lazy val root = (project in file("."))
+            |  .settings(
+            |    name := "sbt0_13_project_template"
+            |  )
+            |""".stripMargin,
+        buildProperties =
+          s"""sbt.version = ${sbt013ProjectConfig.sbtVersion}
+             |""".stripMargin,
+        pluginsSbt = None,
+      )
+    )
+  }
+
+  def testCreateSbt_1_0_Project(): Unit = {
+    runFileGenerationOnlyTest(
+      config = sbt10ProjectConfig,
+      expectedFiles = GeneratedProjectFilesExpectation(
+        buildSbt =
+          """version in ThisBuild := "0.1.0-SNAPSHOT"
+            |
+            |scalaVersion in ThisBuild := "2.13.14"
+            |
+            |lazy val root = (project in file("."))
+            |  .settings(
+            |    name := "sbt1_0_project_template"
+            |  )
+            |""".stripMargin,
+        buildProperties =
+          s"""sbt.version = ${sbt10ProjectConfig.sbtVersion}
+             |""".stripMargin,
+        pluginsSbt = None,
+      )
+    )
+  }
+
+  def testCreateSbt_1_Latest_Project(): Unit = {
+    runFileGenerationOnlyTest(
+      config = sbt1LatestProjectConfig,
+      expectedFiles = GeneratedProjectFilesExpectation(
+        buildSbt =
+          """ThisBuild / version := "0.1.0-SNAPSHOT"
+            |
+            |ThisBuild / scalaVersion := "2.13.14"
+            |
+            |lazy val root = (project in file("."))
+            |  .settings(
+            |    name := "sbt1_project_template"
+            |  )
+            |""".stripMargin,
+        buildProperties =
+          s"""sbt.version = ${sbt1LatestProjectConfig.sbtVersion}
+             |""".stripMargin,
+        pluginsSbt = None,
+      )
+    )
+  }
+
+  def testCreateSbt_1_Latest_Project_WithPackagePrefix(): Unit = {
+    runFileGenerationOnlyTest(
+      config = sbt1LatestProjectWithPackagePrefixConfig,
+      expectedFiles = GeneratedProjectFilesExpectation(
+        buildSbt =
+          """ThisBuild / version := "0.1.0-SNAPSHOT"
+            |
+            |ThisBuild / scalaVersion := "2.13.14"
+            |
+            |lazy val root = (project in file("."))
+            |  .settings(
+            |    name := "sbt1_project_template_with_prefix",
+            |    idePackagePrefix := Some("org.example.prefix")
+            |  )
+            |""".stripMargin,
+        buildProperties =
+          s"""sbt.version = ${sbt1LatestProjectWithPackagePrefixConfig.sbtVersion}
+             |""".stripMargin,
+        pluginsSbt = Some(s"""addSbtPlugin("org.jetbrains.scala" % "sbt-ide-settings" % "$IdeSettingsPluginVersion")\n"""),
+      )
+    )
+  }
+
+  def testCreateSbt_2_Latest_Project(): Unit = {
+    runFileGenerationOnlyTest(
+      config = sbt2LatestProjectConfig,
+      expectedFiles = GeneratedProjectFilesExpectation(
+        buildSbt =
+          """scalaVersion := "3.8.2"
+            |
+            |lazy val root = rootProject
+            |  .settings(
+            |    name := "sbt2_project_template",
+            |    libraryDependencies ++= Seq(
+            |      //You can add library dependencies here, for example,
+            |      //"org.scalatest" %% "scalatest" % "3.2.19" % Test,
+            |      //"org.scalameta" %% "munit" % "1.2.3" % Test
+            |    )
+            |  )
+            |""".stripMargin,
+        buildProperties =
+          s"""sbt.version = ${sbt2LatestProjectConfig.sbtVersion}
+             |""".stripMargin,
+        pluginsSbt = None,
+      )
+    )
+  }
+
+  def testCreateSbt_2_Latest_Project_WithPackagePrefix(): Unit = {
+    runFileGenerationOnlyTest(
+      config = sbt2LatestProjectWithPackagePrefixConfig,
+      expectedFiles = GeneratedProjectFilesExpectation(
+        buildSbt =
+          """scalaVersion := "3.8.2"
+            |
+            |lazy val root = rootProject
+            |  .settings(
+            |    name := "sbt2_project_template_with_prefix",
+            |    idePackagePrefix := Some("org.example.prefix"),
+            |    libraryDependencies ++= Seq(
+            |      //You can add library dependencies here, for example,
+            |      //"org.scalatest" %% "scalatest" % "3.2.19" % Test,
+            |      //"org.scalameta" %% "munit" % "1.2.3" % Test
+            |    )
+            |  )
+            |""".stripMargin,
+        buildProperties =
+          s"""sbt.version = ${sbt2LatestProjectWithPackagePrefixConfig.sbtVersion}
+             |""".stripMargin,
+        pluginsSbt = Some(s"""addSbtPlugin("org.jetbrains.scala" % "sbt-ide-settings" % "$IdeSettingsPluginVersion")\n"""),
+      )
+    )
+  }
+}
+
+object NewSbtProjectWizardGeneratedFilesTest {
+  import NewSbtProjectWizardTestBase.SbtWizardProjectConfig
+  import org.jetbrains.plugins.scala.project.Versions
+  import org.jetbrains.sbt.SbtVersion
+
+  object TestData {
+    val sbt013ProjectConfig: SbtWizardProjectConfig = SbtWizardProjectConfig(
+      projectName = "sbt0_13_project_template",
+      scalaVersion = "2.13.14",
+      sbtVersion = SbtVersion.Latest.Sbt_0_13.minor,
+    )
+
+    val sbt10ProjectConfig: SbtWizardProjectConfig = SbtWizardProjectConfig(
+      projectName = "sbt1_0_project_template",
+      scalaVersion = "2.13.14",
+      sbtVersion = SbtVersion("1.0.4").minor,
+    )
+
+    val sbt1LatestProjectConfig: SbtWizardProjectConfig = SbtWizardProjectConfig(
+      projectName = "sbt1_project_template",
+      scalaVersion = "2.13.14",
+      sbtVersion = Versions.SBT.LatestSbtVersion,
+    )
+
+    val sbt1LatestProjectWithPackagePrefixConfig: SbtWizardProjectConfig = SbtWizardProjectConfig(
+      projectName = "sbt1_project_template_with_prefix",
+      scalaVersion = "2.13.14",
+      sbtVersion = Versions.SBT.LatestSbtVersion,
+      packagePrefix = Some("org.example.prefix"),
+    )
+
+    val sbt2LatestProjectConfig: SbtWizardProjectConfig = SbtWizardProjectConfig(
+      projectName = "sbt2_project_template",
+      scalaVersion = "3.8.2",
+      sbtVersion = SbtVersion.Latest.Sbt_2.minor,
+    )
+
+    val sbt2LatestProjectWithPackagePrefixConfig: SbtWizardProjectConfig = SbtWizardProjectConfig(
+      projectName = "sbt2_project_template_with_prefix",
+      scalaVersion = "3.8.2",
+      sbtVersion = SbtVersion.Latest.Sbt_2.minor,
+      packagePrefix = Some("org.example.prefix"),
+    )
+  }
+}
