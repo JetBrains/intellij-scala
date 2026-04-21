b159963e70386f4c9595771c040a7c59fe4b64a5
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2026-04-21 13:49:34 +0200

    [widen] added widening test cases #SCL-23271

---
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/lang/typeInference/CheckWideningTest.scala
@@ -0,0 +1,8 @@
+package org.jetbrains.plugins.scala.lang.typeInference
+
+import org.jetbrains.plugins.scala.util.GeneratedParameterizedTestFactory.TestData
+import org.jetbrains.plugins.scala.{CheckTestDataTestBase, LatestScalaVersions, ScalaVersion}
+
+sealed abstract class CheckWideningTestBase(testData: Seq[TestData], minScalaVersion: ScalaVersion) extends CheckTestDataTestBase(testData, minScalaVersion)
+final class CheckWideningTest_Scala2 extends CheckWideningTestBase(WideningTest.testDataInScala2, LatestScalaVersions.Scala_2_13)
+final class CheckWideningTest_Scala3 extends CheckWideningTestBase(WideningTest.testDataInScala3, LatestScalaVersions.Scala_3_LTS)
