3115e1bd99fa3056b58fb4c3c1188c8838489b29
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-05-06 19:55:15 +0200

    [tests] Suppress platform NextEditCaretFeatures CaretListener leak in tearDown

--- bsp-builtin/bsp-integration-tests/test/org/jetbrains/bsp/projectHighlighting/SbtOverBspProjectHighlightingTestBase.scala
+++ bsp-builtin/bsp-integration-tests/test/org/jetbrains/bsp/projectHighlighting/SbtOverBspProjectHighlightingTestBase.scala
@@ -1,7 +1,11 @@
 package org.jetbrains.bsp.projectHighlighting
 
 import org.jetbrains.plugins.scala.projectHighlighting.base.ScalaProjectHighlightingTestBase
+import org.jetbrains.plugins.scala.util.NextEditCaretListenerLeakSuppression
 
 abstract class SbtOverBspProjectHighlightingTestBase
   extends ScalaProjectHighlightingTestBase
-    with SbtOverBspExternalSystemImportingTestCase
+    with SbtOverBspExternalSystemImportingTestCase {
+  override def tearDown(): Unit =
+    NextEditCaretListenerLeakSuppression.runSuppressing(super.tearDown())
+}
