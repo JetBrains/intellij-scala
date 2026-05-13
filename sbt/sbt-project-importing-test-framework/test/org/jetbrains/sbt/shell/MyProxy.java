097764323a36bedead073926f42f4ea25feac462
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-05-13 22:52:30 +0200

    DO NOT MERGE [tests] override `ExternalSystemImportingTestCase.importProject` to avoid calling `waitForProjectActivity`
    That call was added in the scope of IDEA-388789 and results in hanging tests for us. It waits for all tracked background activity, while sbt shell import starts a persistent background activity. See `useSbtShellForImport = true` and `org.jetbrains.sbt.shell.SbtShellCommunication.startQueueProcessing`

---
+++ sbt/sbt-project-importing-test-framework/test/org/jetbrains/sbt/shell/MyProxy.java
@@ -0,0 +1,75 @@
+package org.jetbrains.sbt.shell;
+
+import com.intellij.openapi.externalSystem.importing.ImportSpec;
+import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder;
+import com.intellij.openapi.externalSystem.model.DataNode;
+import com.intellij.openapi.externalSystem.model.ProjectSystemId;
+import com.intellij.openapi.externalSystem.model.project.ProjectData;
+import com.intellij.openapi.externalSystem.service.project.ExternalProjectRefreshCallback;
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager;
+import com.intellij.openapi.externalSystem.settings.AbstractExternalSystemSettings;
+import com.intellij.openapi.externalSystem.settings.ExternalProjectSettings;
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Couple;
+import com.intellij.openapi.util.Ref;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+public class MyProxy {
+    public static void importProject(Project project,
+                                     ProjectSystemId externalSystemId,
+                                     ExternalProjectSettings projectSettings,
+                                     String projectPath,
+                                     ImportSpec importSpec,
+                                     BiConsumer<String, String> handleImportFailure) {
+        AbstractExternalSystemSettings systemSettings = ExternalSystemApiUtil.getSettings(project, externalSystemId);
+        projectSettings.setExternalProjectPath(projectPath);
+        //noinspection unchecked
+        Set<ExternalProjectSettings> projects = new HashSet<>(systemSettings.getLinkedProjectsSettings());
+        projects.remove(projectSettings);
+        projects.add(projectSettings);
+        //noinspection unchecked
+        systemSettings.setLinkedProjectsSettings(projects);
+
+        final Ref<Couple<String>> error = Ref.create();
+        ExternalProjectRefreshCallback callback = importSpec.getCallback();
+        ImportSpecBuilder importSpecBuilder = new ImportSpecBuilder(importSpec);
+        if (callback == null || callback instanceof ImportSpecBuilder.DefaultProjectRefreshCallback) {
+            importSpecBuilder.callback(new ExternalProjectRefreshCallback() {
+                @Override
+                public void onSuccess(final @Nullable DataNode<ProjectData> externalProject) {
+                    if (externalProject == null) {
+                        System.err.println("Got null External project after import");
+                        return;
+                    }
+                    try {
+                        ProjectDataManager.getInstance().importData(externalProject, project);
+                    } catch (Throwable ex) {
+                        ex.printStackTrace(System.err);
+                        error.set(Couple.of("Exception occurred in `ProjectDataManager.importData` (see output for the details)", null));
+                    }
+                }
+
+                @Override
+                public void onFailure(@NotNull String errorMessage, @Nullable String errorDetails) {
+                    error.set(Couple.of(errorMessage, errorDetails));
+                }
+            }).build();
+        }
+
+        // await for all background activities to complete
+//        waitForProjectActivity(getMyProject(), () ->
+                ExternalSystemUtil.refreshProjects(importSpecBuilder);
+//        );
+
+        if (!error.isNull()) {
+            handleImportFailure.accept(error.get().first, error.get().second);
+        }
+    }
+}
--- sbt/sbt-project-importing-test-framework/test/org/jetbrains/sbt/shell/SbtShellTestBase.scala
+++ sbt/sbt-project-importing-test-framework/test/org/jetbrains/sbt/shell/SbtShellTestBase.scala
@@ -56,4 +56,13 @@ abstract class SbtShellTestBase extends SbtProjectStructureImportingLike {
     processListener = new SbtShellTestUtil.TestSbtShellProcessListener
     myShellProcessHandler.addProcessListener(processListener)
   }
+
+  override protected def importProject(): Unit = MyProxy.importProject(
+    getMyProject,
+    getExternalSystemId,
+    getCurrentExternalProjectSettings,
+    getProjectPath,
+    createImportSpec(),
+    handleImportFailure(_, _)
+  )
 }
