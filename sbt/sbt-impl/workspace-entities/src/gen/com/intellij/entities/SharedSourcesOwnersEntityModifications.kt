3335db3e8dbf99227016b2617bdd78c21e5736f6
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-05-13 22:12:13 +0200

    [sbt] regenerate workspace entities

--- sbt/sbt-impl/workspace-entities/src/gen/com/intellij/entities/SharedSourcesOwnersEntityModifications.kt
+++ sbt/sbt-impl/workspace-entities/src/gen/com/intellij/entities/SharedSourcesOwnersEntityModifications.kt
@@ -3,46 +3,40 @@
 package com.intellij.entities
 
 import com.intellij.platform.workspace.jps.entities.ModuleEntityBuilder
-import com.intellij.platform.workspace.storage.EntitySource
-import com.intellij.platform.workspace.storage.EntityType
-import com.intellij.platform.workspace.storage.GeneratedCodeApiVersion
-import com.intellij.platform.workspace.storage.MutableEntityStorage
-import com.intellij.platform.workspace.storage.WorkspaceEntityBuilder
+import com.intellij.platform.workspace.storage.*
 import com.intellij.platform.workspace.storage.impl.containers.toMutableWorkspaceList
 
 @GeneratedCodeApiVersion(3)
-interface SharedSourcesOwnersEntityBuilder : WorkspaceEntityBuilder<SharedSourcesOwnersEntity>,
-    ModuleExtensionWorkspaceEntityBuilder<SharedSourcesOwnersEntity> {
-    override var entitySource: EntitySource
-    override var module: ModuleEntityBuilder
-    var ownerModuleIds: MutableList<String>
+interface SharedSourcesOwnersEntityBuilder : WorkspaceEntityBuilder<SharedSourcesOwnersEntity>, ModuleExtensionWorkspaceEntityBuilder<SharedSourcesOwnersEntity> {
+  override var entitySource: EntitySource
+  override var module: ModuleEntityBuilder
+  var ownerModuleIds: MutableList<String>
 }
 
-internal object SharedSourcesOwnersEntityType :
-    EntityType<SharedSourcesOwnersEntity, SharedSourcesOwnersEntityBuilder>() {
-    override val entityClass: Class<SharedSourcesOwnersEntity> get() = SharedSourcesOwnersEntity::class.java
-    operator fun invoke(
-        ownerModuleIds: List<String>,
-        entitySource: EntitySource,
-        init: (SharedSourcesOwnersEntityBuilder.() -> Unit)? = null,
-    ): SharedSourcesOwnersEntityBuilder {
-        val builder = builder()
-        builder.ownerModuleIds = ownerModuleIds.toMutableWorkspaceList()
-        builder.entitySource = entitySource
-        init?.invoke(builder)
-        return builder
-    }
+internal object SharedSourcesOwnersEntityType : EntityType<SharedSourcesOwnersEntity, SharedSourcesOwnersEntityBuilder>() {
+  override val entityClass: Class<SharedSourcesOwnersEntity> get() = SharedSourcesOwnersEntity::class.java
+  operator fun invoke(
+    ownerModuleIds: List<String>,
+    entitySource: EntitySource,
+    init: (SharedSourcesOwnersEntityBuilder.() -> Unit)? = null,
+  ): SharedSourcesOwnersEntityBuilder {
+    val builder = builder()
+    builder.ownerModuleIds = ownerModuleIds.toMutableWorkspaceList()
+    builder.entitySource = entitySource
+    init?.invoke(builder)
+    return builder
+  }
 }
 
 fun MutableEntityStorage.modifySharedSourcesOwnersEntity(
-    entity: SharedSourcesOwnersEntity,
-    modification: SharedSourcesOwnersEntityBuilder.() -> Unit,
+  entity: SharedSourcesOwnersEntity,
+  modification: SharedSourcesOwnersEntityBuilder.() -> Unit,
 ): SharedSourcesOwnersEntity = modifyEntity(SharedSourcesOwnersEntityBuilder::class.java, entity, modification)
 
 @JvmOverloads
 @JvmName("createSharedSourcesOwnersEntity")
 fun SharedSourcesOwnersEntity(
-    ownerModuleIds: List<String>,
-    entitySource: EntitySource,
-    init: (SharedSourcesOwnersEntityBuilder.() -> Unit)? = null,
+  ownerModuleIds: List<String>,
+  entitySource: EntitySource,
+  init: (SharedSourcesOwnersEntityBuilder.() -> Unit)? = null,
 ): SharedSourcesOwnersEntityBuilder = SharedSourcesOwnersEntityType(ownerModuleIds, entitySource, init)
