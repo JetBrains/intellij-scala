12b8e0670578c7664bde437c6fa237c614353194
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-05-18 15:34:16 +0200

    [testing support, specs2, bazel] Include scala-reflect on the classpath in the new specs2 tests #SCL-25391

--- scala/test-integration/testing-support/test/org/jetbrains/plugins/scala/testingSupport/specs2/WithSpecs2_4.scala
+++ scala/test-integration/testing-support/test/org/jetbrains/plugins/scala/testingSupport/specs2/WithSpecs2_4.scala
@@ -2,7 +2,7 @@ package org.jetbrains.plugins.scala.testingSupport.specs2
 
 import org.jetbrains.plugins.scala.DependencyManagerBase._
 import org.jetbrains.plugins.scala.base.ScalaSdkOwner
-import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, LibraryLoader}
+import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, LibraryLoader, ScalaReflectLibraryLoader}
 
 /**
  * Mixin for `ScalaSdkOwner`-based test fixtures (e.g. `GutterMarkersTestBase`)
@@ -13,6 +13,7 @@ import org.jetbrains.plugins.scala.base.libraryLoaders.{IvyManagedLoader, Librar
 trait WithSpecs2_4 extends ScalaSdkOwner {
   abstract override protected def librariesLoaders: Seq[LibraryLoader] =
     super.librariesLoaders ++ Seq(
+      ScalaReflectLibraryLoader, // specs library depends on scala-reflect, do not ignore it
       IvyManagedLoader(("org.specs2" %% "specs2-core" % "4.13.0").transitive())
     )
 }
