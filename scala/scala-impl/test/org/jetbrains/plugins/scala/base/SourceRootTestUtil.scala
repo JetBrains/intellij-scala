1089dafb2d2d33e37c87548425b83a60f7c183ce
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-05-20 08:50:29 +0200

    [tests] Replace NioFiles.createIfNotExists with our own function #SCL-25423

--- scala/scala-impl/test/org/jetbrains/plugins/scala/base/SourceRootTestUtil.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/base/SourceRootTestUtil.scala
@@ -1,11 +1,10 @@
 package org.jetbrains.plugins.scala.base
 
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.util.io.NioFiles
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.testFramework.PsiTestUtil
 
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
 
 object SourceRootTestUtil {
   def addSourceRoot(module: Module, path: Path): Unit = {
@@ -13,7 +12,16 @@ object SourceRootTestUtil {
     if (rootFile eq null) {
       throw new IllegalArgumentException(s"Cannot find source root path: $path")
     }
-    NioFiles.createIfNotExists(path)
+    createIfNotExists(path)
     PsiTestUtil.addSourceRoot(module, rootFile)
+  }
+
+  private def createIfNotExists(path: Path): Unit = {
+    if (Files.exists(path)) return
+    if (Files.isDirectory(path)) Files.createDirectories(path)
+    else {
+      Files.createDirectories(path.getParent)
+      Files.createFile(path)
+    }
   }
 }
