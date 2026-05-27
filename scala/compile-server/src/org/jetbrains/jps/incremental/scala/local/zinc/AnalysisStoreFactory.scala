591816fedd6394f13779118c90166499a26f3b5b
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-05-27 15:45:32 +0200

    [nio, eel] Suppress the `java.io.File` usage warning in AnalysisStoreFactory #SCL-25423
    - There is simply no other alternative here.

--- scala/compile-server/src/org/jetbrains/jps/incremental/scala/local/zinc/AnalysisStoreFactory.scala
+++ scala/compile-server/src/org/jetbrains/jps/incremental/scala/local/zinc/AnalysisStoreFactory.scala
@@ -11,6 +11,10 @@ object AnalysisStoreFactory {
 
   def createAnalysisStore(analysisFile: Path): AnalysisStore =
     analysisStoreCache.getOrUpdate(analysisFile) { () =>
+      // Zinc does not offer any other API, so using `java.io.File` here is unavoidable.
+      // In practice, this should be fine, as Zinc (or scalac) usually runs in the same machine
+      // where the code lives (i.e. inside WSL, if working with WSL).
+      //noinspection SSBasedInspection
       val binary = FileAnalysisStore.binary(analysisFile.toFile)
       val cached = AnalysisStore.cached(binary)
       AnalysisStore.sync(cached)
