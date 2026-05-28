cdf5e141835289555884699f7bc99a4ecdc86797
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-05-28 10:55:26 +0200

    [eel, nio] Rewrite JarManifestUtils.readManifestAttribute using java.nio.file.Path #SCL-25423

--- scala/scala-utils-language-rt/src/org/jetbrains/plugins/scala/util/JarManifestUtils.scala
+++ scala/scala-utils-language-rt/src/org/jetbrains/plugins/scala/util/JarManifestUtils.scala
@@ -1,6 +1,8 @@
 package org.jetbrains.plugins.scala.util
 
-import java.io.{BufferedInputStream, File}
+import org.jetbrains.annotations.ApiStatus
+
+import java.io.BufferedInputStream
 import java.nio.file.{FileSystems, Files, Path}
 import scala.util.Using
 
@@ -19,12 +21,20 @@ object JarManifestUtils {
    * @return Some list of classpath files if it's specified in the manifest. No validation is done for the files<br>
    *         None - if manifest or the class path attribute were not found
    */
-  def readClassPath(jarFile: File): Option[Seq[File]] = {
-    val classpathAttributeOpt = readManifestAttribute(jarFile.toPath, "Class-Path")
-    classpathAttributeOpt.map { classPathAttribute =>
+  def readClassPath(jarFile: Path): Option[Seq[Path]] =
+    readManifestAttribute(jarFile, "Class-Path").map: classPathAttribute =>
       val paths = classPathAttribute.split(" ").map(_.trim)
-      val parentDirectory = jarFile.getParentFile
-      paths.map(new File(parentDirectory, _)).map(_.getCanonicalFile).toSeq
-    }
-  }
+      val parentDirectory = jarFile.getParent
+      paths.map(parentDirectory.resolve).map(_.toAbsolutePath.normalize()).toSeq
+
+  /**
+   * @return Some list of classpath files if it's specified in the manifest. No validation is done for the files<br>
+   *         None - if manifest or the class path attribute were not found
+   */
+  @deprecated(message = "Use readClaspath(java.nio.file.Path)", since = "2026.2")
+  @Deprecated
+  @ApiStatus.ScheduledForRemoval(inVersion = "2026.3")
+  //noinspection SSBasedInspection
+  def readClassPath(jarFile: java.io.File): Option[Seq[java.io.File]] =
+    readClassPath(jarFile.toPath).map(_.map(_.toFile))
 }
