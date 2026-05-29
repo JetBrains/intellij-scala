9a1ce89ff4a94e045089fcc7df5d8a04f35683dd
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2026-05-29 09:45:13 +0200

    [sbt, tests, refactor] extract BuildDiagnosticsCollector into test utilities

--- scala/test-utils-platform/test/org/jetbrains/plugins/scala/build/BuildEventsCompilationStatusCollector.scala
+++ scala/test-utils-platform/test/org/jetbrains/plugins/scala/build/BuildEventsCompilationStatusCollector.scala
@@ -1,15 +1,12 @@
-package org.jetbrains.sbt.shell.build.util
+package org.jetbrains.plugins.scala.build
 
 import com.intellij.openapi.compiler.{CompilationStatusListener, CompileContext, CompilerMessage, CompilerMessageCategory}
-import org.jetbrains.plugins.scala.build.BuildMessages
 
 import java.util.concurrent.ConcurrentLinkedQueue
+import scala.jdk.CollectionConverters.IteratorHasAsScala
 
 /**
  * Collects compiler-side diagnostics (`CompileContext`) for a single captured build.
- *
- * TODO Move to a common compiler/build-test module.<br>
- *  This collector is not sbt/sbt-shell specific and can be reused by other build-related integration tests.
  */
 final class BuildEventsCompilationStatusCollector extends CompilationStatusListener {
 
@@ -53,19 +50,13 @@ final class BuildEventsCompilationStatusCollector extends CompilationStatusListe
   private def renderCompilerMessage(message: CompilerMessage): String = {
     val prefix = Option(message.getRenderTextPrefix)
       .orElse(Option(message.getExportTextPrefix))
-      .flatMap(normalizedAnsiTextNonempty)
+      .flatMap(BuildDiagnosticsText.normalizedAnsiTextNonempty)
       .getOrElse("")
-    val text = normalizedAnsiTextNonempty(message.getMessage).getOrElse("")
+    val text = BuildDiagnosticsText.normalizedAnsiTextNonempty(message.getMessage).getOrElse("")
 
     val rendered = s"$prefix$text".trim
     if (rendered.nonEmpty) rendered else "<empty compiler message>"
   }
-
-  private def normalizedAnsiTextNonempty(text: String): Option[String] =
-    Option(text)
-      .map(BuildMessages.stripAnsiCodes)
-      .map(_.trim)
-      .filter(_.nonEmpty)
 }
 
 object BuildEventsCompilationStatusCollector {
@@ -73,4 +64,7 @@ object BuildEventsCompilationStatusCollector {
     errors: Seq[String],
     warnings: Seq[String],
   )
+
+  private def nonEmptyEntries(values: java.util.Collection[String]): Seq[String] =
+    values.iterator().asScala.filter(_.nonEmpty).toSeq
 }
