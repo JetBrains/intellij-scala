7b704c1058fa18d2c2531861bdfe25fc2b05537c
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-06-03 13:20:07 +0200

    [tests] Patch the test blacklist in AfterUpdateDottyVersionScript for Scala 3.8.4

--- scala/compiler-integration/test/AfterUpdateDottyVersionScript.scala
+++ scala/compiler-integration/test/AfterUpdateDottyVersionScript.scala
@@ -821,6 +821,10 @@ object AfterUpdateDottyVersionScript {
         |cc-use-alternatives.scala
         |i10256.scala
         |given-syntax.scala
+        |
+        |# release-3.8.4
+        |i23317.scala
+        |i24990.scala
         |""".stripMargin.trim
     )
 
