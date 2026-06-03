35b281c642ea835aa71a0f56f26ddad83eeef4b9
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-06-03 13:07:25 +0200

    [tests] Run tests against Scala 3.8.4
    - Also sets the Scala_Next_RC version to 3.8.4 as there is no newer RC version at the moment.

--- scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/ScalaVersion.scala
@@ -114,7 +114,7 @@ object LatestScalaVersions {
   val Scala_3_5 = new ScalaVersion(ScalaLanguageLevel.Scala_3_5, "2")
   val Scala_3_6 = new ScalaVersion(ScalaLanguageLevel.Scala_3_6, "4")
   val Scala_3_7 = new ScalaVersion(ScalaLanguageLevel.Scala_3_7, "4")
-  val Scala_3_8 = new ScalaVersion(ScalaLanguageLevel.Scala_3_8, "3")
+  val Scala_3_8 = new ScalaVersion(ScalaLanguageLevel.Scala_3_8, "4")
 
   //
   // Aliases
@@ -130,7 +130,7 @@ object LatestScalaVersions {
   val Scala_3_LTS_RC: ScalaVersion = new ScalaVersion(ScalaLanguageLevel.Scala_3_3, "8-RC1")
 
   // Scala Next RC
-  val Scala_3_Next_RC: ScalaVersion = new ScalaVersion(ScalaLanguageLevel.Scala_3_8, "4-RC3")
+  val Scala_3_Next_RC: ScalaVersion = Scala_3_8
 
   val allScala2: Seq[ScalaVersion] = Seq(
     Scala_2_9,
