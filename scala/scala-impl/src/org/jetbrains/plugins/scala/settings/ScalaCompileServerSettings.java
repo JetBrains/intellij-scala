e0c8f6b9bb373b39e5da31abb783c5a91f4b58df
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-06-10 20:00:00 +0200

    [compiler] Document the choice of default JVM parameters for the Scala Compile Server #SCL-24311

--- scala/scala-impl/src/org/jetbrains/plugins/scala/settings/ScalaCompileServerSettings.java
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/settings/ScalaCompileServerSettings.java
@@ -28,6 +28,33 @@ public class ScalaCompileServerSettings implements PersistentStateComponent<Scal
   public boolean USE_DEFAULT_SDK = true;
 
   public String COMPILE_SERVER_MAXIMUM_HEAP_SIZE = Integer.toString(ScalaCompileServerDefaults.DefaultHeapSize());
+
+  /**
+   * These are meant to be reasonable defaults. Our current default heap size is 2 Gb.
+   * <p>
+   * <ol>
+   *     <li>
+   *         {@code -Xss2m} sets the JVM thread stack size to 2 Megabytes. The Scala compiler can reach deep stack
+   *         traces. In fact, some users reported this exact issue in the past in
+   *         <a href="https://youtrack.jetbrains.com/issue/SCL-18766">SCL-18766</a>.
+   *     </li>
+   *     <li>
+   *         {@code -XX:ReservedCodeCacheSize=384m} sets the JIT-compiled native code cache to 384 Megabytes (up from
+   *         the default of 240 Megabytes). 384 is chosen as 50% higher than 256 (384 = 256 + 128). We're increasing the
+   *         default value by about 50% because the cached Scala compilers benefit from a larger native code cache
+   *         as larger chunks of the compiler can remain fully optimised. 512 Megabytes would be a bit too much when
+   *         compared to the default heap size of 2 Gb (1/4 of the size). The reserved code cache sits outside the heap,
+   *         so it contributes to the total memory usage of the Scala Compile Server, i.e. 384 Megabytes on top of the
+   *         2-gigabyte default heap. For users who need it, they may tweak the heap size and the reserved code cache size
+   *         as it suits their projects and their machines.
+   *     </li>
+   *     <li>
+   *         {@code -XX:MaxInlineLevel=20} instructs the JIT compiler to increase the maximum depth of inlined method
+   *         call chains. The default value is 9. The maximum value is 20. More aggressive inlining helps with
+   *         abstractions and lambdas, which are very common in Scala code.
+   *     </li>
+   * </ol>
+   */
   public String COMPILE_SERVER_JVM_PARAMETERS = "-Xss2m -XX:ReservedCodeCacheSize=384m -XX:MaxInlineLevel=20";
   public int COMPILE_SERVER_PARALLELISM = defaultMaxThreads();
   public boolean COMPILE_SERVER_PARALLEL_COMPILATION = true;
