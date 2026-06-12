6e51cde37b4b27a5fcf2699ff54a122571735b99
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-06-12 10:06:40 +0200

    [tests, worksheets] Temporarily tolerate the new Scala 3.9.0 `$` character in identifier usage warnings in plain worksheets #SCL-25557

--- scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/plain/WorksheetPlainCompileOnServerRunOnServerIntegrationTest.scala
+++ scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/plain/WorksheetPlainCompileOnServerRunOnServerIntegrationTest.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.worksheet.integration.plain
 
+import com.intellij.openapi.compiler.{CompilerMessage, CompilerMessageCategory}
 import org.jetbrains.plugins.scala.util.runners.{RunWithJdkVersions, RunWithScalaVersions, TestJdkVersion, TestScalaVersion}
 
 class WorksheetPlainCompileOnServerRunOnServerIntegrationTest
@@ -53,7 +54,12 @@ class WorksheetPlainCompileOnServerRunOnServerIntegrationTest_Scala_3_LTS_AllInO
 @RunWithJdkVersions(Array(TestJdkVersion.JDK_17))
 class WorksheetPlainCompileOnServerRunOnServerIntegrationTest_Scala_3_Next_RC_AllInOne
   extends CompileOnServerRunOnServerTestBase
-    with WorksheetPlainIntegrationTestBase_Scala_3_RC_AllInOne
+    with WorksheetPlainIntegrationTestBase_Scala_3_RC_AllInOne {
+
+  override def allowedCompilerMessage(message: CompilerMessage): Boolean =
+    message.getCategory == CompilerMessageCategory.WARNING &&
+      message.getMessage.contains("should not contain `$`, which is reserved for internal compiler use.")
+}
 
 @RunWithScalaVersions(Array(
   TestScalaVersion.Scala_3_0,
@@ -69,4 +75,9 @@ class WorksheetPlainCompileOnServerRunOnServerIntegrationTest_Scala_3_BracelessS
 @RunWithScalaVersions(Array(TestScalaVersion.Scala_3_Next_RC))
 @RunWithJdkVersions(Array(TestJdkVersion.JDK_17))
 class WorksheetPlainCompileOnServerRunOnServerIntegrationTest_Scala_3_BracelessSyntax_Next_RC
-  extends WorksheetPlainCompileOnServerRunOnServerIntegrationTest_Scala_3_BracelessSyntax
+  extends WorksheetPlainCompileOnServerRunOnServerIntegrationTest_Scala_3_BracelessSyntax {
+
+  override def allowedCompilerMessage(message: CompilerMessage): Boolean =
+    message.getCategory == CompilerMessageCategory.WARNING &&
+      message.getMessage.contains("should not contain `$`, which is reserved for internal compiler use.")
+}
