e821bab909a68ab0d95c469d97f521db40fb52db
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-06-12 10:06:40 +0200

    [tests, worksheets] Temporarily tolerate the new Scala 3.9.0 `$` character in identifier usage warnings in plain worksheets #SCL-25557
    (cherry picked from commit 3da81cba1be2ac78982061e37b87d81cd832461d)

--- scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/plain/WorksheetPlainCompileOnServerRunLocallyIntegrationTest.scala
+++ scala/worksheet/test/org/jetbrains/plugins/scala/worksheet/integration/plain/WorksheetPlainCompileOnServerRunLocallyIntegrationTest.scala
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.scala.worksheet.integration.plain
 
+import com.intellij.openapi.compiler.{CompilerMessage, CompilerMessageCategory}
 import org.jetbrains.plugins.scala.util.runners.{RunWithJdkVersions, RunWithScalaVersions, TestJdkVersion, TestScalaVersion}
 import org.junit.Ignore
 
@@ -54,7 +55,12 @@ class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_LTS_AllInOn
 @RunWithJdkVersions(Array(TestJdkVersion.JDK_17))
 class WorksheetPlainCompileOnServerRunLocallyIntegrationTest_Scala_3_Next_RC_AllInOne
   extends CompileOnServerRunLocallyTestBase
-    with WorksheetPlainIntegrationTestBase_Scala_3_RC_AllInOne
+    with WorksheetPlainIntegrationTestBase_Scala_3_RC_AllInOne {
+
+  override def allowedCompilerMessage(message: CompilerMessage): Boolean =
+    message.getCategory == CompilerMessageCategory.WARNING &&
+      message.getMessage.contains("should not contain `$`, which is reserved for internal compiler use.")
+}
 
 @RunWithScalaVersions(Array(
   TestScalaVersion.Scala_3_0,
