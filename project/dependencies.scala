d8e2bf9786f6e453771af4f9e5ff0d03bd7775c2
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-06-30 09:58:57 +0200

    Update intellijVersion to 262.8663 and fix test sources compilation
    - This change prepares us for the 262.8665.x Beta builds coming very soon.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -22,7 +22,7 @@ object Versions {
    * ATTENTION: check the comment in [[Common.newProjectWithKotlin]] when updating this version.
    *            update `since-build` in plugin.xml if there are binary incompatible changes after update
    */
-  val intellijVersion = "262.8377.35"
+  val intellijVersion = "262.8663"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
