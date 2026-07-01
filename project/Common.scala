1cb9a2f218ea84e3f75e6f20ef5629e455dcc42f
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-07-01 16:35:31 +0200

    [build] Update the sbt-kotlin-plugin to version 4.0.0 #SCL-24919
    (cherry picked from commit 1e0c0bfcf5a8841b58242eafffa07fd9696e2b8c)

--- project/Common.scala
+++ project/Common.scala
@@ -1,15 +1,15 @@
 import AttributedClasspathUtils.buildIntellijSdkSubsetAttributedClasspath
 import CompilationCache.compilationCacheSettings
 import com.github.sbt.junit.jupiter.sbt.Import.JupiterKeys
+import org.jetbrains.sbt.kotlin.Keys.{kotlinRuntimeProvided, kotlinVersion, kotlincJvmTarget}
+import org.jetbrains.sbt.kotlin.KotlinPlugin
 import org.jetbrains.sbtidea.Keys.*
 import org.jetbrains.sbtidea.packaging.PackagingKeys.*
+import sbt.*
 import sbt.Keys.*
 import sbt.Project.projectToRef
-import sbt.{Def, *}
 
 import java.nio.file.Path
-import kotlin.Keys.{kotlinRuntimeProvided, kotlinVersion, kotlincJvmTarget}
-import kotlin.KotlinPlugin
 
 object Common {
   final val JetBrains = "JetBrains"
