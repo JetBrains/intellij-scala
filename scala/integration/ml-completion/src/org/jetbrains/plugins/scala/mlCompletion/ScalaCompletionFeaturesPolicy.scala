fb4d6c9b5ab5561258d36190948383b3dcf80ef0
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2026-07-02 19:44:24 +0200

    [ml-completion] move modules to the closed-source part of the plugin #SCL-25643 fixed
    Machine Learning Code Completion was unbundled in IJPL-246200 and cannot be accessed in community repo without internal network
    
    We'll keep these integration modules available without subscription and make sure that the community part can be imported by contributors

--- scala/integration/ml-completion/src/org/jetbrains/plugins/scala/mlCompletion/ScalaCompletionFeaturesPolicy.scala
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.plugins.scala.mlCompletion
-
-import com.intellij.completion.ml.features.CompletionFeaturesPolicy
-
-class ScalaCompletionFeaturesPolicy extends CompletionFeaturesPolicy {
-  override def useNgramModel(): Boolean = true
-}
