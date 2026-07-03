911f781d8d236854f8aae793292ab46d839b9e72
builduser-scala <builduser-scala@jetbrains.com>
2026-07-03 11:05:06 +0200

    Update intellijVersion to 262.8665.81

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -22,7 +22,7 @@ object Versions {
    * ATTENTION: check the comment in [[Common.newProjectWithKotlin]] when updating this version.
    *            update `since-build` in plugin.xml if there are binary incompatible changes after update
    */
-  val intellijVersion = "262.8377.25"
+  val intellijVersion = "262.8665.81"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
