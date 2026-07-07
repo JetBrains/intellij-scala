c366c119ca06bb503456ebb3dff5edc17e4e14fa
Pavel Fatin <pavel.fatin@jetbrains.com>
2026-07-07 14:22:01 +0200

    Help topics for: X-Ray, Performance, Worksheet #SCL-25005 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/settings/sections/PerformanceSettingsSectionConfigurable.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/settings/sections/PerformanceSettingsSectionConfigurable.scala
@@ -2,8 +2,10 @@ package org.jetbrains.plugins.scala.settings.sections
 
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.scala.ScalaBundle
+import org.jetbrains.plugins.scala.help.ScalaWebHelpProvider
 
 class PerformanceSettingsSectionConfigurable(project: Project) extends SettingsSectionConfigurable {
   override def getDisplayName: String = ScalaBundle.message("scala.project.settings.form.tabs.performance")
   override def createPanel(): SettingsSectionPanel = new PerformanceSettingsSectionPanel(project)
+  override def getHelpTopic: String = ScalaWebHelpProvider.HelpPrefix + "scala-improvingperformance.html"
 }
