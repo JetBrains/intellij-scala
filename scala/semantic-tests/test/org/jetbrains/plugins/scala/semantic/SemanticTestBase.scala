386a8d4a436d7fea9d0f472d25cd8196dfd82d88
Pavel Fatin <pavel.fatin@jetbrains.com>
2026-07-16 11:22:59 +0200

    [semantic tests] Test cases #SCL-22082

--- scala/semantic-tests/test/org/jetbrains/plugins/scala/semantic/SemanticTestBase.scala
+++ scala/semantic-tests/test/org/jetbrains/plugins/scala/semantic/SemanticTestBase.scala
@@ -3,18 +3,30 @@ package org.jetbrains.plugins.scala.semantic
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.plugins.scala.corpus.scala3.{AkkaTest, CatsTest, ScalatestTest, ZioTest}
 import org.jetbrains.plugins.scala.corpus.{ProjectCorpusTestBase, ProjectCorpusTestDef}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScTypeAlias
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiManager
 import org.jetbrains.plugins.scala.settings.ScalaApplicationSettings.getInstance as ScalaApplicationSettings
-import org.junit.{Assert, Test}
+import org.junit.Assert
 
 import java.nio.file.{Files, Path}
 import java.util.Comparator
 
 abstract class SemanticTestBase(config: ProjectCorpusTestDef) extends ProjectCorpusTestBase(config) {
+  private val Print =
+//    true // Print actual cases
+    false // Test expected cases
+
+  private lazy val decompiler: Decompiler = {
+    val classpath =
+      ModuleRootManager.getInstance(getMyFixture.getModule)
+        .orderEntries.productionOnly.librariesOnly.classes.getRoots.toSeq
+        .map(virtualFile => VfsUtil.getLocalFile(virtualFile).getPath)
+
+    new Decompiler(classpath)
+  }
+
 //  @Test // Uncomment to print data to files
   def compare(): Unit = {
     val classes = allClasses(excludePackages = Set.empty)
@@ -23,9 +35,9 @@ abstract class SemanticTestBase(config: ProjectCorpusTestDef) extends ProjectCor
     if (Files.exists(directory)) Files.walk(directory).sorted(Comparator.reverseOrder).forEach(Files.delete(_))
     Files.createDirectory(directory)
 
-    classes.take(50).foreach { cls =>
+    classes.foreach { cls =>
       println(cls.qualifiedName)
-      val (decompiledText, psiText) = textOf(cls.qualifiedName)
+      val (decompiledText, psiText) = textOf(cls.qualifiedName, decompiler)
       val sourceText = {
         val sourceClass = cls.getSourceMirrorClass.asInstanceOf[ScTypeDefinition]
         sourceClass.getText + sourceClass.baseCompanionTypeDefinition.map("\n\n" + _.getText).getOrElse("")
@@ -36,21 +48,39 @@ abstract class SemanticTestBase(config: ProjectCorpusTestDef) extends ProjectCor
     }
   }
 
-  protected def doTest(fqn: String): Unit = {
-    return // Comment to enable the tests cases
-    val (decompiledText, sourceText) = textOf(fqn)
-    Assert.assertEquals(decompiledText, sourceText)
+  protected def doTest(classes: String*): Unit = {
+    val classNames =
+      if (Print) allClasses(excludePackages = Set.empty).map(_.qualifiedName)
+      else classes.flatMap(_.split('\n')).map(_.trim).filterNot(_.isEmpty)
+
+    classNames.foreach { name =>
+      val isCommented = name.startsWith("//")
+      val fqn = if (isCommented) name.substring(2) else name
+
+      try {
+        val (decompiledText, sourceText) = textOf(fqn, decompiler)
+
+        if (Print) {
+          val comment = decompiledText != sourceText
+          println((if (comment) "//" else "") + fqn)
+        } else {
+          println(fqn)
+          if (isCommented) {
+            Assert.assertNotEquals(fqn, decompiledText, sourceText)
+          } else {
+            Assert.assertEquals(fqn, decompiledText, sourceText)
+          }
+        }
+      } catch {
+        case e: Throwable if Print => System.err.println(fqn + ": " + e.getMessage)
+      }
+    }
   }
 
-  private def textOf(fqn: String): (String, String) = {
+  private def textOf(fqn: String, decompiler: Decompiler): (String, String) = {
     val cls = ScalaPsiManager.instance(getProject).getCachedClass(GlobalSearchScope.allScope(getProject), fqn)
       .getOrElse(throw new IllegalArgumentException(fqn))
       .asInstanceOf[ScTypeDefinition]
-
-    val classpath =
-      ModuleRootManager.getInstance(getMyFixture.getModule)
-        .orderEntries.productionOnly.librariesOnly.classes.getRoots.toSeq
-        .map(virtualFile => VfsUtil.getLocalFile(virtualFile).getPath)
 
     val sourceCls = cls.getSourceMirrorClass.asInstanceOf[ScTypeDefinition]
     Assert.assertTrue(s"Must have a source: ${cls.qualifiedName}", sourceCls != cls)
@@ -70,7 +100,6 @@ abstract class SemanticTestBase(config: ProjectCorpusTestDef) extends ProjectCor
     val tastyFile = cls.getContainingFile.getVirtualFile
     Assert.assertTrue(tastyFile.getName, tastyFile.getExtension == "tasty")
 
-    val decompiler = new Decompiler(classpath)
     val decompiledText = decompiler.decompile(tastyFile.getName, tastyFile.contentsToByteArray())
 
     (decompiledText, psiText)
