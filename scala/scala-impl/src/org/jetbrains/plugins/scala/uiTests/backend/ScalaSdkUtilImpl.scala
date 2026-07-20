4ebbe32a54dd45c63ae2aa7c42863734e8aace94
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-07-20 11:09:26 +0200

    [tests, ui] Use the JetBrains Maven Central resolver when fetching the Scala SDK from the internet #SCL-25601
    (cherry picked from commit f6e4bef4f479dcb6c6ff5a3129cd122e1848ee10)

--- scala/scala-impl/src/org/jetbrains/plugins/scala/uiTests/backend/ScalaSdkUtilImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/uiTests/backend/ScalaSdkUtilImpl.scala
@@ -12,12 +12,13 @@ import com.intellij.openapi.roots.ui.configuration.libraryEditor.NewLibraryEdito
 import com.intellij.openapi.roots.{ModifiableRootModel, ModuleRootModificationUtil}
 import org.apache.ivy.util.MessageLogger
 import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.scala.{DependencyManagerBase, ScalaBundle, ScalaVersion}
+import org.jetbrains.plugins.scala.DependencyManagerBase.MavenResolver
 import org.jetbrains.plugins.scala.components.libextensions.ProgressIndicatorLogger
 import org.jetbrains.plugins.scala.extensions.withProgressSynchronouslyTry
 import org.jetbrains.plugins.scala.project.ScalaLibraryType
 import org.jetbrains.plugins.scala.project.template.ScalaVersionDownloadingDialog.{ScalaVersionResolveResult, createScalaVersionResolveResult}
-import org.jetbrains.plugins.scala.project.template.{Artifact, ScalaSdkDescriptor, ScalaVersionDownloadingDialog}
+import org.jetbrains.plugins.scala.project.template.{Artifact, ScalaSdkDescriptor}
+import org.jetbrains.plugins.scala.{DependencyManagerBase, ScalaBundle, ScalaVersion}
 
 import scala.util.Try
 
@@ -90,6 +91,9 @@ private[backend] object ScalaSdkUtilImpl {
       val dependencyManager = new DependencyManagerBase {
         override protected def progressIndicator: Option[ProgressIndicator] = Some(indicator)
         override def createLogger: MessageLogger = new ProgressIndicatorLogger(indicator)
+        override def resolvers: Seq[DependencyManagerBase.Resolver] = Seq(
+          MavenResolver("JetBrains Maven Central", "https://cache-redirector.jetbrains.com/maven-central")
+        )
       }
       createScalaVersionResolveResult(scalaVersion, dependencyManager)
     }
