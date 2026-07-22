cef6bafc779af2db7cc21a388a8805d01a089857
Aleksandra Zdrojowa <aleksandra.zdrojowa.external@jetbrains.com>
2026-07-22 18:16:06 +0200

    [sbt shell] unset `BUILD_NUMBER` and `CI` env variables in the sbt shell process when running tests on TeamCity #SCL-25667
    - When the sbt shell runs on CI, it inherits the `BUILD_NUMBER` environment variable. Its presence enables the `isCI` flag in sbt, which changes the shell behavior - while the sbt shell is starting, it does not switch the PTY to raw input mode and leaves it in canonical, line-buffered mode. As a result, a single-character input, such as the "i" response to the "Project loading failed: (r)etry, (q)uit, (l)ast, or (i)gnore?" prompt, is never read because it is sent without a trailing newline (it's not needed on local machines). Therefore, sbt shell tests that rely on this behavior work differently on CI than they do locally. The `isCI` flag is also used in a few other places in sbt, for example when deciding whether to use colors or strip ANSI codes. I have not observed any problems related to those differences, but they could potentially cause issues.
    
    - Removing `BUILD_NUMBER` and `CI` (it's just to be on the safe side; I haven't noticed it being set in our CI) from the sbt shell process in tests makes sbt behave the same way as it does locally.
    
    
    (cherry picked from commit 24fdddc00bbcae2e6444c0d0501d22b75c1a5051)

--- sbt/sbt-impl/test/org/jetbrains/sbt/shell/SbtShellCommunicationTest.scala
+++ sbt/sbt-impl/test/org/jetbrains/sbt/shell/SbtShellCommunicationTest.scala
@@ -7,72 +7,23 @@ import org.junit.Test
 class SbtShellCommunicationTest {
 
   @Test
-  def loadFailureIgnoreCommand_OldSbtOldShellUsesNewline(): Unit =
-    assertLoadFailureIgnoreCommand(
-      sbtVersion = SbtVersion("1.3.13"),
-      isNewShell = false,
-      isLinux = false,
-      expected = "i\n",
-    )
+  def loadFailureIgnoreNewline_Sbt13RequiresNewline(): Unit =
+    assertNewlineRequired(SbtVersion("1.3.13"), expected = true)
 
   @Test
-  def loadFailureIgnoreCommand_OldSbtNewShellUsesNewline(): Unit =
-    assertLoadFailureIgnoreCommand(
-      sbtVersion = SbtVersion("1.3.13"),
-      isNewShell = true,
-      isLinux = false,
-      expected = "i\n",
-    )
+  def loadFailureIgnoreNewline_Sbt14UsesRawInput(): Unit =
+    assertNewlineRequired(SbtVersion("1.4.0"), expected = false)
 
   @Test
-  def loadFailureIgnoreCommand_NewSbtLinuxOldShellUsesNewline(): Unit =
-    assertLoadFailureIgnoreCommand(
-      sbtVersion = SbtVersion("1.12.0"),
-      isNewShell = false,
-      isLinux = true,
-      expected = "i\n",
-    )
+  def loadFailureIgnoreNewline_Sbt112UseRawInput(): Unit =
+    assertNewlineRequired(SbtVersion("1.12.0"), expected = false)
 
   @Test
-  def loadFailureIgnoreCommand_NewSbtLinuxNewShellUsesRawInput(): Unit =
-    assertLoadFailureIgnoreCommand(
-      sbtVersion = SbtVersion("1.12.0"),
-      isNewShell = true,
-      isLinux = true,
-      expected = "i",
-    )
+  def loadFailureIgnoreNewline_Sbt2UseRawInput(): Unit =
+    assertNewlineRequired(SbtVersion("2.0.0"), expected = false)
 
-  @Test
-  def loadFailureIgnoreCommand_NewSbtMacOldShellUsesRawInput(): Unit =
-    assertLoadFailureIgnoreCommand(
-      sbtVersion = SbtVersion("1.12.0"),
-      isNewShell = false,
-      isLinux = false,
-      expected = "i",
-    )
-
-  @Test
-  def loadFailureIgnoreCommand_NewSbtWindowsOldShellUsesRawInput(): Unit =
-    assertLoadFailureIgnoreCommand(
-      sbtVersion = SbtVersion("1.12.0"),
-      isNewShell = false,
-      isLinux = false,
-      expected = "i",
-    )
-
-  private def assertLoadFailureIgnoreCommand(
-    sbtVersion: SbtVersion,
-    isNewShell: Boolean,
-    isLinux: Boolean,
-    expected: String,
-  ): Unit = {
-    val actual = SbtShellCommunication.loadFailureIgnoreCommand(
-      sbtVersion,
-      isNewShell,
-      isLinux,
-      lineSeparator = "\n",
-    )
-
+  private def assertNewlineRequired(sbtVersion: SbtVersion, expected: Boolean): Unit = {
+    val actual = SbtShellCommunication.isLoadFailureIgnoreNewlineRequired(sbtVersion)
     assertEquals(expected, actual)
   }
 }
