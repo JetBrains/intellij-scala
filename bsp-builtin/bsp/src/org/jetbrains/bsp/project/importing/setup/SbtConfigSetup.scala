accdffcefc7c6d2e75e779b28516b317d31efd1d
Aleksandra Zdrojowa <aleksandra.zdrojowa.external@jetbrains.com>
2026-07-30 14:12:31 +0200

    [sbt/BSP] generate BSP connection file using `bspConfig` task instead of `startServer` command #SCL-25744 fixed
    - sbt has `bspConfig` task for generating the BSP connection file, so using the `startServer` command is not necessary. In princpile, sbt should stop the server thread when it exits, but on CI we have observed that generating the BSP connection file sometimes hangs. To rule out the possibility that this is caused by a non-daemon server thread, the implementation has been changed so that the connection file is now generated without starting the server.

--- bsp-builtin/bsp/src/org/jetbrains/bsp/project/importing/setup/SbtConfigSetup.scala
+++ bsp-builtin/bsp/src/org/jetbrains/bsp/project/importing/setup/SbtConfigSetup.scala
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.TestOnly
 import org.jetbrains.bsp.BspBundle
 import org.jetbrains.plugins.scala.build.{BuildMessages, BuildReporter}
 import org.jetbrains.plugins.scala.extensions.invokeAndWait
+import org.jetbrains.plugins.scala.isUnitTestMode
 import org.jetbrains.sbt.SbtUtil.{defaultLauncherPath, detectSbtVersion, sbtVersionParam}
 import org.jetbrains.sbt.SbtVersion
 import org.jetbrains.sbt.process.SbtRunner
@@ -33,7 +34,7 @@ object SbtConfigSetup {
   def apply(baseDir: Path, jdk: Sdk, environment: Map[String, String]): SbtConfigSetup =
     createSbtConfigSetup(baseDir, jdk, environment)
 
-  /** Runs sbt with a dummy command so that the project is initialized and .bsp/sbt.json is created.
+  /** Runs sbt so that the project is initialized and .bsp/sbt.json is created.
    *
    * @param baseDir     the project base directory
    * @param jdk         the JDK to use for running sbt
@@ -48,16 +49,14 @@ object SbtConfigSetup {
     val jdkHome = Option(jdk.getHomePath).map(Path.of(_))
     val sbtLauncher = defaultLauncherPath
 
-    // dummy command so that sbt will run, init and exit
-    val sbtLauncherArgs = List("early(startServer)")
-    val sbtCommands = ""
-
     val projectSbtVersion = detectSbtVersion(baseDir, defaultLauncherPath)
     val sbtVersion = SbtVersion.upgradeSbtVersionToTheLatestCompatible(projectSbtVersion)
     val upgradeParam =
       if (sbtVersion > projectSbtVersion)
         List(sbtVersionParam(sbtVersion))
       else List.empty
+
+    val sbtLauncherArgs = getCommandForConnectionFileGeneration(sbtVersion)
 
     val vmArgs = SbtExternalSystemManager.getVmOptions(Seq.empty, jdkHome) ++ upgradeParam
 
@@ -70,11 +69,28 @@ object SbtConfigSetup {
       sbtLauncher,
       SbtExecutionSettings.SbtOptions.empty,
       sbtLauncherArgs,
-      sbtCommands,
+      sbtCommands = "",
       BspBundle.message("bsp.resolver.creating.sbt.configuration"),
       passParentEnvironment = true,
       timingCollector = None
     )(using reporter)
     new SbtConfigSetup(runInit)
+  }
+
+  /**
+   * The sbt launcher arguments used to generate the `.bsp/sbt.json` connection file.
+   *
+   * In sbt 2.x projects and in tests a `bspConfig` task is used instead of the `startServer` command.
+   * `startServer` starts a non-daemon thread. In principle, sbt should stop this thread when it exits,
+   * but on CI we have observed that generating the connection file sometimes hangs. Starting a server thread
+   * is generally not needed to generate the BSP connection file, and when a dedicated sbt task exists for this purpose,
+   * it should be used.
+   */
+  private def getCommandForConnectionFileGeneration(sbtVersion: SbtVersion): List[String] = {
+    val useBspConfig = isUnitTestMode || sbtVersion.isSbt2
+    if useBspConfig then
+      List("bspConfig")
+    else
+      List("early(startServer)")
   }
 }
