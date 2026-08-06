f5a513895b041366aff1cb8af534637ba532b740
Marco Frattarola <marco.frattarola@jetbrains.com>
2026-08-06 10:36:37 +0200

    [compiler-integration] Use enum instead of case objects #SCL-25741 fixed

--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/charts/ui/Side.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/charts/ui/Side.scala
@@ -1,11 +1,5 @@
 package org.jetbrains.plugins.scala.compiler.charts.ui
 
-sealed trait Side
-
-object Side {
-
-  case object North extends Side
-  case object South extends Side
-  case object West extends Side
-  case object East extends Side
+enum Side {
+  case  North, South, West, East 
 }
--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/charts/ui/VAlign.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/charts/ui/VAlign.scala
@@ -1,10 +1,5 @@
 package org.jetbrains.plugins.scala.compiler.charts.ui
 
-sealed trait VAlign
-
-object VAlign {
-
-  case object Center extends VAlign
-  case object Bottom extends VAlign
-  case object Top extends VAlign
+enum VAlign {
+  case Center, Bottom, Top, VAlign
 }
