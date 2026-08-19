44c9c5e9c3ff76507e7ac59d520f6c64a02bf999
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2026-08-19 14:04:41 +0200

    [tests] fixed some compiler tests that only are available in <2.11 #SCL-15634

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/highlighting/decompiler/DecompilerHighlightingTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/highlighting/decompiler/DecompilerHighlightingTest.scala
@@ -1,18 +1,16 @@
 package org.jetbrains.plugins.scala.lang.highlighting.decompiler
 
+import org.jetbrains.plugins.scala.ScalaVersion
+
 class DecompilerHighlightingTest extends DecompilerHighlightingTestBase {
 
   def testSettings(): Unit = doTest("Settings.class")
 
   def testCommandLine(): Unit = doTest("CommandLine.class")
 
-  def testDirectoryFileLookup(): Unit = doTest("DirectoryFileLookup.class")
-
   def testDefaultMacroCompiler(): Unit = doTest("DefaultMacroCompiler.class")
 
   def testFormatInterpolator(): Unit = doTest("FormatInterpolator.class")
-
-  def testJavaParsers(): Unit = doTest("JavaParsers.class")
 
   def testContext(): Unit = doTest("Context.class")
 
@@ -21,4 +19,23 @@ class DecompilerHighlightingTest extends DecompilerHighlightingTestBase {
   def testSocketServer(): Unit = doTest("SocketServer.class")
 
   def testStdTags(): Unit = doTest("StdTags.class")
+}
+
+/**
+ * The decompiled sources are highlighted against the scala-compiler of the test Scala version
+ * (see `includeCompilerAsLibrary`), so these class files, which were compiled from the Scala 2.11
+ * compiler sources, only resolve against the 2.11 compiler:
+ *  - `DirectoryFileLookup` (compiled from `DirectoryFlatClassPath.scala`) references `FlatClassPath`
+ *    and `ClassRepClassPathEntry`, both of which were removed in Scala 2.12
+ *  - `JavaParsers` overrides `ParsersCommon.ParserCommon.deprecationWarning(offset, msg)`,
+ *    which got an additional `since` parameter in Scala 2.12
+ */
+class DecompilerHighlightingTest_Scala2_11 extends DecompilerHighlightingTestBase {
+
+  override protected def supportedIn(version: ScalaVersion): Boolean =
+    version <= ScalaVersion.Latest.Scala_2_11
+
+  def testDirectoryFileLookup(): Unit = doTest("DirectoryFileLookup.class")
+
+  def testJavaParsers(): Unit = doTest("JavaParsers.class")
 }
