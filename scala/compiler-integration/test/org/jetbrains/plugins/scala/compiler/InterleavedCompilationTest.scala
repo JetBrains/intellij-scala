d1816be01baf498fbf3c652f3230fa0818f815d2
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-09-10 10:52:11 +0200

    [tests] Run InterleavedCompilationTest using separate prod and test sources modules #SCL-25790

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/InterleavedCompilationTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/InterleavedCompilationTest.scala
@@ -24,7 +24,7 @@ import scala.jdk.CollectionConverters.*
 
 @Category(Array(classOf[CompilationTests_Zinc]))
 @RunWith(classOf[JUnit4])
-class InterleavedCompilationTest extends SbtProjectCompilationTestBase {
+class InterleavedCompilationTest extends SbtProjectCompilationTestBase(separateProdAndTestSources = true) {
 
   override protected def jdkVersionForTest: TestJdkVersion = TestJdkVersion.JDK_17
 
