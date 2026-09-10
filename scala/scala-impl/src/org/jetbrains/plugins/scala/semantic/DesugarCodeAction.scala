d5a3b4b8304556a6870984ce9b6d25938295d50d
Pavel Fatin <pavel.fatin@jetbrains.com>
2026-09-10 11:27:25 +0200

    [semantic tests] Action: classAtCaret #SCL-22082
    #SCL-25618
    #SCL-25546

--- scala/scala-impl/src/org/jetbrains/plugins/scala/semantic/DesugarCodeAction.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/semantic/DesugarCodeAction.scala
@@ -1,11 +1,16 @@
 package org.jetbrains.plugins.scala.semantic
 
-import com.intellij.diff.{DiffContentFactory, DiffManager}
 import com.intellij.diff.requests.SimpleDiffRequest
-import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys}
+import com.intellij.diff.{DiffContentFactory, DiffManager}
+import com.intellij.openapi.actionSystem.{ActionUpdateThread, AnAction, AnActionEvent, CommonDataKeys}
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.roots.{CompilerModuleExtension, ModuleRootManager}
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.refactoring.util.CommonRefactoringUtil
+import org.jetbrains.plugins.scala.actions.ScalaActionUtil
+import org.jetbrains.plugins.scala.extensions.PsiElementExt
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
 import org.jetbrains.plugins.scala.project.ProjectPsiFileExt
 import org.jetbrains.plugins.scala.settings.ScalaApplicationSettings.{getInstance => ScalaApplicationSettings}
 import org.jetbrains.plugins.scala.{Scala3Language, ScalaBundle}
@@ -17,9 +22,14 @@ class DesugarCodeAction extends AnAction(
 
   override def actionPerformed(e: AnActionEvent): Unit = {
     val project = e.getProject
+    val editor = CommonDataKeys.EDITOR.getData(e.getDataContext)
     val file = CommonDataKeys.PSI_FILE.getData(e.getDataContext).asInstanceOf[ScalaFile]
+    val cls = classAtCaret(editor, file).orElse(file.typeDefinitions.headOption).getOrElse {
+      CommonRefactoringUtil.showErrorHint(project, editor, "No class to desugar", getTemplateText, null)
+      return
+    }
+
     val module = file.module.orNull
-    val cls = file.typeDefinitions.head
 
     val outputDir = CompilerModuleExtension.getInstance(module).getCompilerOutputPath
 
@@ -52,5 +62,18 @@ class DesugarCodeAction extends AnAction(
     val left = DiffContentFactory.getInstance.create(project, compilerText, Scala3Language.INSTANCE.getAssociatedFileType)
     val right = DiffContentFactory.getInstance.create(project, pluginText, Scala3Language.INSTANCE.getAssociatedFileType)
     DiffManager.getInstance.showDiff(project, new SimpleDiffRequest("Desugaring of " + cls.qualifiedName, left, right, "Compiler", "Plugin"))
+  }
+
+  private def classAtCaret(editor: Editor, file: ScalaFile): Option[ScTypeDefinition] = {
+    val offset = editor.getCaretModel.getOffset
+    val elementAtCaret = Option(file.findElementAt(offset))
+    val classAtCaret = elementAtCaret.flatMap(_.contexts.collectFirst { case td: ScTypeDefinition if td.isTopLevel => td })
+    classAtCaret
+  }
+
+  override def getActionUpdateThread: ActionUpdateThread = ActionUpdateThread.BGT
+
+  override def update(e: AnActionEvent): Unit = {
+    ScalaActionUtil.enableAndShowIfInScalaFile(e)
   }
 }
