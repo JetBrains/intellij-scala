ef07a7e0e94591f4a4146fe4d9566b77e55be99a
Pavel Fatin <pavel.fatin@jetbrains.com>
2026-09-10 14:33:40 +0200

    [semantic tests] Action: intrpolated literal caching #SCL-22082
    #SCL-25618
    #SCL-25546

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScInterpolatedStringLiteralImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/ScInterpolatedStringLiteralImpl.scala
@@ -57,7 +57,7 @@ final class ScInterpolatedStringLiteralImpl(node: ASTNode,
 
   override protected def endQuote: String = super.startQuote
 
-  override def desugaredExpression: Option[(ScReferenceExpression, ScMethodCall)] = cachedInUserData("desugaredExpression", this, BlockModificationTracker(this)) {
+  override def desugaredExpression: Option[(ScReferenceExpression, ScMethodCall)] = cachedInUserData("desugaredExpression", this, BlockModificationTracker(this), Tuple1(ScalaApplicationSettings.PRECISE_TEXT)) {
     (referenceText, getContext) match {
       case (methodName, context) if context != null &&
         hasValidClosingQuotes &&
